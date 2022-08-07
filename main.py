02f2abaa99d811541878a353f2585ef4c21254a6
AnasImloul <pydev27@gmail.com>
2022-08-07 13:00:01 +0100

    added solutions to 387 new problems

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from selenium.webdriver import Chrome
 from scraper.solutions import scrap as solution_scrap
 from scraper.problemset import scrap as problemset_scrap
 from scraper.file_utils import load_json
+from os import getcwd
 
+PATH = getcwd()
 
 WEBDRIVER_PATH = r"C:\Users\hp\Desktop\Projects\leetcoder\leetcoder\chromedriver.exe"
 
 with Chrome(WEBDRIVER_PATH) as chrome1:
 
-    # problems = problemset_scrap.fetch(browser=chrome1, save_path=r"C:\Users\hp\Desktop\Projects\Leetcode-solutions\scripts\problemset.json")
+    problems = problemset_scrap.fetch(browser=chrome1, save_path=PATH+r"\scraper\problemset\problemset.json", premium=False)
 
-    problems = load_json(r"C:\Users\hp\Desktop\Projects\Leetcode-solutions\scripts\problemset.json")
+    problems = load_json(PATH+r"\scraper\problemset\problemset.json")
 
     solution_scrap.fetch(browser=chrome1,
-                         save_path=r"C:\Users\hp\Desktop\Projects\Leetcode-solutions\scripts" ,
+                         save_path=PATH + r"\scripts",
                          problems=problems,
                          languages=("c++","python","java","javascript")
                          )
