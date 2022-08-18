5e0b78c264bf9c5014e31b2f450d26fb65ffb524
AnasImloul <pydev27@gmail.com>
2022-08-18 18:23:42 +0100

    Runtime: 110 ms (Top 36.61%) | Memory: 44.6 MB (Top 25.14%)

--- scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.js
+++ scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.js
@@ -1,3 +1,5 @@
+// Runtime: 110 ms (Top 36.61%) | Memory: 44.6 MB (Top 25.14%)
+
 var mincostTickets = function(days, costs) {
   let store = {};
   for(let i of days) {
@@ -6,7 +8,7 @@ var mincostTickets = function(days, costs) {
   let lastDay = days[days.length - 1]
   let dp = new Array(days[days.length - 1] + 1).fill(Infinity);
   dp[0] = 0;
-  
+
   for(let i = 1; i< days[days.length - 1] + 1; i++) {
       if(!store[i]) {
           dp[i] = dp[i-1];
@@ -14,9 +16,9 @@ var mincostTickets = function(days, costs) {
       }
       dp[i] = costs[0] + dp[i-1];
       dp[i] = Math.min(costs[1] + dp[Math.max(i - 7, 0)], dp[i]);
-      dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]); 
+      dp[i] = Math.min(costs[2] + dp[Math.max(i - 30, 0)], dp[i]);
   }
-  
+
   return dp[lastDay]
-  
+
 };
