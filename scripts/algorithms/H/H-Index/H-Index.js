0893c1d0e1663ff0c4732f770ea95f24d2548730
AnasImloul <pydev27@gmail.com>
2022-08-18 18:23:45 +0100

    Runtime: 87 ms (Top 55.41%) | Memory: 42 MB (Top 85.35%)

--- scripts/algorithms/H/H-Index/H-Index.js
+++ scripts/algorithms/H/H-Index/H-Index.js
@@ -1,7 +1,9 @@
+// Runtime: 87 ms (Top 55.41%) | Memory: 42 MB (Top 85.35%)
+
 var hIndex = function(citations) {
     citations.sort((a,b)=>b-a)
     let i=0
     while(citations[i]>i) i++
-    
+
     return i
 };
