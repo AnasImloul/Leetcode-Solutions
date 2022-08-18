4674dfe1cc80fdda7184acec23206be58bc74dcf
AnasImloul <pydev27@gmail.com>
2022-08-18 16:52:33 +0100

    Runtime: 2052 ms (Top 44.44%) | Memory: 32.2 MB (Top 22.19%)

--- scripts/algorithms/N/Number of Excellent Pairs/Number of Excellent Pairs.py
+++ scripts/algorithms/N/Number of Excellent Pairs/Number of Excellent Pairs.py
@@ -1,3 +1,5 @@
+# Runtime: 2052 ms (Top 44.44%) | Memory: 32.2 MB (Top 22.19%)
+
 class Solution:
     def countExcellentPairs(self, nums: List[int], k: int) -> int:
         hamming = sorted([self.hammingWeight(num) for num in set(nums)])
@@ -5,7 +7,7 @@ class Solution:
         for h in hamming:
             ans += len(hamming) - bisect.bisect_left(hamming, k - h)
         return ans
-        
+
     def hammingWeight(self, n):
         ans = 0
         while n:
