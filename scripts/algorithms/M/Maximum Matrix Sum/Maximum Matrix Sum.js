04c4797734657af00ae4b8c15bc4f38df2597b8a
AnasImloul <pydev27@gmail.com>
2022-08-18 18:23:49 +0100

    Runtime: 129 ms (Top 71.43%) | Memory: 52.3 MB (Top 14.29%)

--- scripts/algorithms/M/Maximum Matrix Sum/Maximum Matrix Sum.js
+++ scripts/algorithms/M/Maximum Matrix Sum/Maximum Matrix Sum.js
@@ -1,24 +1,26 @@
+// Runtime: 129 ms (Top 71.43%) | Memory: 52.3 MB (Top 14.29%)
+
 var maxMatrixSum = function(matrix) {
     const MAX= Number.MAX_SAFE_INTEGER;
-    
+
     const m = matrix.length;
     const n = matrix[0].length;
-    
+
     let negs = 0;
-    
+
     let totAbsSum = 0;
     let minAbsNum = MAX;
-    
+
     for (let i = 0; i < n; ++i) {
         for (let j = 0; j < n; ++j) {
             if (matrix[i][j] < 0) ++negs;
-            
+
             totAbsSum += Math.abs(matrix[i][j]);
             minAbsNum = Math.min(minAbsNum, Math.abs(matrix[i][j]));
         }
     }
-    
+
     if (negs % 2 === 1) totAbsSum -= (2 * minAbsNum);
-    
-	return totAbsSum;
+
+    return totAbsSum;
 };
