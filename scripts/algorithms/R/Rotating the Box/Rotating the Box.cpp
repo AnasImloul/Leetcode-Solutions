5363351181986f472b243a9cf33109203d110026
AnasImloul <pydev27@gmail.com>
2022-08-18 16:51:29 +0100

    Runtime: 374 ms (Top 57.75%) | Memory: 52.5 MB (Top 43.01%)

--- scripts/algorithms/R/Rotating the Box/Rotating the Box.cpp
+++ scripts/algorithms/R/Rotating the Box/Rotating the Box.cpp
@@ -1,11 +1,13 @@
+// Runtime: 374 ms (Top 57.75%) | Memory: 52.5 MB (Top 43.01%)
+
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size();
         int n = box[0].size();
-        
+
         vector<vector<char>> ans(n,vector<char>(m,'.'));
-        
+
         for(int i=0;i<m;++i){
             int k = n; //Stores last obstacle or occupied position
             for(int j=n-1;j>=0;--j)
@@ -18,7 +20,7 @@ public:
                 {
                      k = j;
                      ans[j][i] = '*';
-                }   
+                }
             }
         }
         for(int i=0;i<n;++i){
