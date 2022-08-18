1c0ed690225a895b63bfadf68502a0e0512de3f8
AnasImloul <pydev27@gmail.com>
2022-08-18 18:23:49 +0100

    Runtime: 148 ms (Top 70.67%) | Memory: 42.3 MB (Top 56.00%)

--- scripts/algorithms/B/Beautiful Arrangement/Beautiful Arrangement.js
+++ scripts/algorithms/B/Beautiful Arrangement/Beautiful Arrangement.js
@@ -1,20 +1,22 @@
+// Runtime: 148 ms (Top 70.67%) | Memory: 42.3 MB (Top 56.00%)
+
 var countArrangement = function(n) {
-	let result = 0;
-	const visited = Array(n + 1).fill(false);
-	const dfs = (next = n) => {
-		if (next === 0) {
-			result += 1;
-			return;
-		}
+    let result = 0;
+    const visited = Array(n + 1).fill(false);
+    const dfs = (next = n) => {
+        if (next === 0) {
+            result += 1;
+            return;
+        }
 
-		for (let index = 1; index <= n; index++) {
-			if (visited[index] || index % next && next % index) continue;
-			visited[index] = true;
-			dfs(next - 1);
-			visited[index] = false;
-		}
-	};
+        for (let index = 1; index <= n; index++) {
+            if (visited[index] || index % next && next % index) continue;
+            visited[index] = true;
+            dfs(next - 1);
+            visited[index] = false;
+        }
+    };
 
-	dfs();
-	return result;
+    dfs();
+    return result;
 };
