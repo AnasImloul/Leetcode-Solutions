12352ed2672006f2ccbec35e53cb7849fb9aa47f
AnasImloul <pydev27@gmail.com>
2022-08-18 16:12:02 +0100

    Runtime: 9 ms (Top 72.82%) | Memory: 46.7 MB (Top 65.16%)

--- scripts/algorithms/M/Minimum Suffix Flips/Minimum Suffix Flips.java
+++ scripts/algorithms/M/Minimum Suffix Flips/Minimum Suffix Flips.java
@@ -1,24 +1,25 @@
+// Runtime: 9 ms (Top 72.82%) | Memory: 46.7 MB (Top 65.16%)
+
 class Solution {
     public int minFlips(String target) {
-        
+
         boolean lastBit = false;
-        
+
         int flips = 0;
         for(int i=0;i<target.length();i++){
-            
+
             if(target.charAt(i)=='1' && !lastBit){
                 flips++;
-				lastBit = !lastBit;
+                lastBit = !lastBit;
             }
             else if(target.charAt(i)=='0' && lastBit){
                 flips++;
-				lastBit = !lastBit;
+                lastBit = !lastBit;
             }
-            
+
         }
-        
+
         return flips;
-        
-        
+
     }
 }
