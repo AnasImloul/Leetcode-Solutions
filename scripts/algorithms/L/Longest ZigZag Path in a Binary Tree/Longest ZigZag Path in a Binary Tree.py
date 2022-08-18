e999c0d864de6203ab6be52d6bb1acedad480bd5
AnasImloul <pydev27@gmail.com>
2022-08-18 15:58:57 +0100

    Runtime: 437 ms (Top 91.25%) | Memory: 59.8 MB (Top 86.06%)

--- scripts/algorithms/L/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree.py
+++ scripts/algorithms/L/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree.py
@@ -1,15 +1,17 @@
+# Runtime: 437 ms (Top 91.25%) | Memory: 59.8 MB (Top 86.06%)
+
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        
+
         def helper(root):
             if root is None:
                 return -1, -1
-            
+
             leftRight = helper(root.left)[1] + 1
             rightLeft = helper(root.right)[0] + 1
             self.res = max(self.res, leftRight, rightLeft)
             return leftRight, rightLeft
-        
+
         helper(root)
         return self.res
