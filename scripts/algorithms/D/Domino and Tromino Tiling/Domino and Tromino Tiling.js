f3305e0161312d20ae5817d1904dc0bd84bf6bbb
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:37 +0100

    Runtime: 75 ms (Top 81.08%) | Memory: 44.3 MB (Top 51.35%)

--- scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.js
+++ scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.js
@@ -1,5 +1,7 @@
+// Runtime: 75 ms (Top 81.08%) | Memory: 44.3 MB (Top 51.35%)
+
 var numTilings = function(n) {
-    let mod = 10 ** 9 + 7;    
+    let mod = 10 ** 9 + 7;
     let len = 4;
     let ways = new Array(len).fill(0);
 
@@ -11,16 +13,16 @@ var numTilings = function(n) {
     // already calculated above
     if (n < len - 1) {
         return ways[n];
-    }    
+    }
 
     // use % len to circulate values inside our array
-    for (var i = len - 1; i <= n;i++) {        
-        ways[i % len] = ( 
+    for (var i = len - 1; i <= n;i++) {
+        ways[i % len] = (
             ways[(len + i - 1) % len] * 2
-            + 
-            ways[(len + i - 3) % len] 
-        ) % mod;          
+            +
+            ways[(len + i - 3) % len]
+        ) % mod;
     }
-    
+
     return ways[(i - 1) % len];
 };
