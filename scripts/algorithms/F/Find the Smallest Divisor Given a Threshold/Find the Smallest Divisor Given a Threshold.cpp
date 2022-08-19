5dba1b99a8a6e293574ecb961c68e17a74a32c17
AnasImloul <pydev27@gmail.com>
2022-08-19 13:50:43 +0100

    Runtime: 62 ms (Top 71.27%) | Memory: 22.3 MB (Top 64.84%)

--- scripts/algorithms/F/Find the Smallest Divisor Given a Threshold/Find the Smallest Divisor Given a Threshold.cpp
+++ scripts/algorithms/F/Find the Smallest Divisor Given a Threshold/Find the Smallest Divisor Given a Threshold.cpp
@@ -1,9 +1,11 @@
+// Runtime: 62 ms (Top 71.27%) | Memory: 22.3 MB (Top 64.84%)
+
 class Solution {
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
         int l=1,r=0;
         for(int n:nums)r=max(r,n);
-        
+
         int mid,s=0;
         while(l<r){
             s=0;
@@ -16,8 +18,7 @@ public:
             }else{
                 l=mid+1;
             }
-            
-            
+
         }
         return r;
     }
