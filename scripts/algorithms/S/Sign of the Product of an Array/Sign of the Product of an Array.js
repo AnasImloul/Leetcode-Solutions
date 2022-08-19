59ee591023b0e3bb6ba1a3aa51e3b9794b6799ff
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:38 +0100

    Runtime: 83 ms (Top 65.37%) | Memory: 43.7 MB (Top 52.05%)

--- scripts/algorithms/S/Sign of the Product of an Array/Sign of the Product of an Array.js
+++ scripts/algorithms/S/Sign of the Product of an Array/Sign of the Product of an Array.js
@@ -1,10 +1,12 @@
+// Runtime: 83 ms (Top 65.37%) | Memory: 43.7 MB (Top 52.05%)
+
 var arraySign = function(nums) {
-	// use filter to find total negative numbers in the array
+    // use filter to find total negative numbers in the array
     let negativeCount = nums.filter(n => n<0).length;
-	
-	// check if the nums array has zero. If it does, then return 0
+
+    // check if the nums array has zero. If it does, then return 0
     if(nums.includes(0)) return 0;
-	
-	// If negativeCount variable is even answer is 1 else -1
+
+    // If negativeCount variable is even answer is 1 else -1
     return negativeCount % 2 === 0 ? 1 : -1
 };
