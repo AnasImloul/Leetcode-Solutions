a65f50d103e2a38021566c2ebdd31257f872a816
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:36 +0100

    Runtime: 322 ms (Top 36.23%) | Memory: 54.6 MB (Top 31.88%)

--- scripts/algorithms/P/Partition Array Such That Maximum Difference Is K/Partition Array Such That Maximum Difference Is K.js
+++ scripts/algorithms/P/Partition Array Such That Maximum Difference Is K/Partition Array Such That Maximum Difference Is K.js
@@ -1,14 +1,16 @@
+// Runtime: 322 ms (Top 36.23%) | Memory: 54.6 MB (Top 31.88%)
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 var partitionArray = function(nums, k) {
-  
+
     nums.sort((a,b) =>{ return a-b})
-    
+
     let n = nums.length ,ans=0
-    
+
     for(let i=0 ; i<n; i++){
         let ele = nums[i]
         while(i<n && nums[i]-ele<=k) i++
