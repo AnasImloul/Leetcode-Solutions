be220ca957bfc97bb050d1a4a2510f5b54832ff7
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:37 +0100

    Runtime: 117 ms (Top 23.03%) | Memory: 44.8 MB (Top 54.82%)

--- scripts/algorithms/R/Reverse String II/Reverse String II.js
+++ scripts/algorithms/R/Reverse String II/Reverse String II.js
@@ -1,3 +1,5 @@
+// Runtime: 117 ms (Top 23.03%) | Memory: 44.8 MB (Top 54.82%)
+
 /**
  * @param {string} s
  * @param {number} k
@@ -5,10 +7,10 @@
  */
 var reverseStr = function(s, k) {
     const sArr = s.split('');
-    
+
     let start = 0;
     let end = k - 1;
-    
+
     const swapBlock = (start, end) => {
         while (start < end) {
             [sArr[start], sArr[end]] = [sArr[end], sArr[start]];
@@ -20,10 +22,10 @@ var reverseStr = function(s, k) {
 
     while (start < end) {
         swapBlock(start, end);
-        
+
         start = start + (k * 2);
         end = sArr[start + (k-1)] ? start + (k-1) : s.length - 1;
     }
-    
+
     return sArr.join('');
 };
