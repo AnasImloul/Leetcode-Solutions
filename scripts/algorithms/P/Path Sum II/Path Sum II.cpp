b2de95c716e3c4e605279163daf09963a1159765
AnasImloul <pydev27@gmail.com>
2022-08-19 13:50:42 +0100

    Runtime: 24 ms (Top 43.53%) | Memory: 20 MB (Top 62.29%)

--- scripts/algorithms/P/Path Sum II/Path Sum II.cpp
+++ scripts/algorithms/P/Path Sum II/Path Sum II.cpp
@@ -1,12 +1,14 @@
+// Runtime: 24 ms (Top 43.53%) | Memory: 20 MB (Top 62.29%)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -27,7 +29,7 @@ public:
         helper(t,target,root->left);
         helper(t,target,root->right);
         t.pop_back();
-        
+
         //cout<<root->val<<" "<<target<<endl;
         return;
     }
