5b2e12ea6c5bb00580a2d3a5da1654d7fbb49ad0
AnasImloul <pydev27@gmail.com>
2022-08-19 13:58:02 +0100

    Runtime: 1 ms (Top 98.25%) | Memory: 42.8 MB (Top 15.70%)

--- scripts/algorithms/N/Number of Arithmetic Triplets/Number of Arithmetic Triplets.java
+++ scripts/algorithms/N/Number of Arithmetic Triplets/Number of Arithmetic Triplets.java
@@ -1,18 +1,20 @@
+// Runtime: 1 ms (Top 98.25%) | Memory: 42.8 MB (Top 15.70%)
+
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
         int result = 0;
         int[] map = new int[201];
-        
+
         for(int num: nums) {
             map[num] = 1;
-            
+
             if(num - diff >= 0) {
                 map[num] += map[num - diff];
             }
-            
+
             if(map[num] >= 3) result += 1;
         }
-        
+
         return result;
     }
 }
