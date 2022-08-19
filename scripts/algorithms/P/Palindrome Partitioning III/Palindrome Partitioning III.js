0410f93777ca5034e19bd667c706f933011d058b
AnasImloul <pydev27@gmail.com>
2022-08-19 13:33:18 +0100

    Runtime: 193 ms (Top 16.67%) | Memory: 45.6 MB (Top 100.00%)

--- scripts/algorithms/P/Palindrome Partitioning III/Palindrome Partitioning III.js
+++ scripts/algorithms/P/Palindrome Partitioning III/Palindrome Partitioning III.js
@@ -1,6 +1,8 @@
+// Runtime: 193 ms (Top 16.67%) | Memory: 45.6 MB (Top 100.00%)
+
 var palindromePartition = function(s, k) {
     const len = s.length;
-    
+
     const cost = (i = 0, j = 0) => {
         let c = 0;
         while(i <= j) {
@@ -9,27 +11,27 @@ var palindromePartition = function(s, k) {
         }
         return c;
     }
-    
+
     const dp = Array.from({ length: len }, () => {
         return new Array(k + 1).fill(-1);
     })
-    
+
     const splitHelper = (idx = 0, sl = k) => {
         if(sl < 0) return Infinity;
         if(idx == len) {
             if(sl == 0) return 0;
             return Infinity;
         }
-        
+
         if(dp[idx][sl] != -1) return dp[idx][sl];
-        
+
         let ans = Infinity;
-        
+
         for(let i = idx; i < len; i++) {
             ans = Math.min(ans, splitHelper(i + 1, sl - 1) + cost(idx, i));
         }
         return dp[idx][sl] = ans;
     }
-    
+
     return splitHelper();
 };
