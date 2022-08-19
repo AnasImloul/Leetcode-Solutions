ae32f2b181b0d29c1d32d9a61265132be57623e3
AnasImloul <pydev27@gmail.com>
2022-08-19 13:58:01 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 45.6 MB (Top 46.72%)

--- scripts/algorithms/T/Trim a Binary Search Tree/Trim a Binary Search Tree.java
+++ scripts/algorithms/T/Trim a Binary Search Tree/Trim a Binary Search Tree.java
@@ -1,11 +1,13 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 45.6 MB (Top 46.72%)
+
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) return root;
         while (root.val < low || root.val > high) {
             root = root.val < low ? root.right : root.left;
-            if (root == null) 
+            if (root == null)
                 return root;
-        }   
+        }
         root.left = trimBST(root.left, low, high);
         root.right = trimBST(root.right, low, high);
         return root;
