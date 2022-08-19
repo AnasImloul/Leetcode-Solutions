bb06c4ddf7c8121ef56fd3a8895b25b359199f26
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:35 +0100

    Runtime: 122 ms (Top 45.45%) | Memory: 47.8 MB (Top 36.36%)

--- scripts/algorithms/N/New 21 Game/New 21 Game.js
+++ scripts/algorithms/N/New 21 Game/New 21 Game.js
@@ -1,13 +1,15 @@
+// Runtime: 122 ms (Top 45.45%) | Memory: 47.8 MB (Top 36.36%)
+
 var new21Game = function(n, k, maxPts) {
     if (k+maxPts <= n || k===0) return 1;
 
     let dp = [];
     dp[0] = 1;
     dp[1] = 1/maxPts;
-    
+
     for (let i = 2; i <= n; i++) {
         dp[i] = 0;
-        
+
         if (i <= k) {
             dp[i] = (1 + 1/maxPts) * dp[i-1];
         } else {
@@ -17,7 +19,7 @@ var new21Game = function(n, k, maxPts) {
             dp[i] -= dp[i-maxPts-1] / maxPts;
         }
     }
-    
+
     return dp.reduce((acc, cur, idx) => {
         if (idx >= k) {
             acc += cur;
