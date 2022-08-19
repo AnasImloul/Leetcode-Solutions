ed9a1cd2594f92abcafa6a33ac69329f346b2cc8
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:36 +0100

    Runtime: 80 ms (Top 70.37%) | Memory: 46.7 MB (Top 14.81%)

--- scripts/algorithms/P/Prime Arrangements/Prime Arrangements.js
+++ scripts/algorithms/P/Prime Arrangements/Prime Arrangements.js
@@ -1,3 +1,5 @@
+// Runtime: 80 ms (Top 70.37%) | Memory: 46.7 MB (Top 14.81%)
+
 /**
  * @param {number} n
  * @return {number}
@@ -5,27 +7,27 @@
 
 var findNoOfPrimes = function(n){
     let isPrime = new Array(n+1).fill(true);
-    
+
     let count =0;
-    
+
     for(let i=2; i<n;i++){
         for(let j=i;j<=n;j++){
             isPrime[j*i]= false;
         }
     }
-    
+
     isPrime.forEach(prime=> {
         if(prime){
             count++
         }
     })
-    
+
     return count-2;
 }
 
 var factorial = function(num){
     let modulo = Math.pow(10,9) +7;
-    
+
     if(num<=0)
         return 1;
 
@@ -33,16 +35,14 @@ var factorial = function(num){
 }
 
 var numPrimeArrangements = function(n) {
-    
+
     let modulo = BigInt(Math.pow(10,9) +7);
-    
-    let count = findNoOfPrimes(n);  
-    
-    
+
+    let count = findNoOfPrimes(n);
+
     let factorialPrime = factorial(count);
-    
+
     let factorialComposite = factorial(n-count);
-    
-    
+
     return (BigInt(factorialPrime)*BigInt(factorialComposite))% (modulo);
 };
