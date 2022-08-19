cbec20c32fa60f970b648fcb8271f299ee5c4efa
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:34 +0100

    Runtime: 584 ms (Top 70.00%) | Memory: 83.2 MB (Top 15.00%)

--- scripts/algorithms/L/Lexicographically Smallest String After Applying Operations/Lexicographically Smallest String After Applying Operations.js
+++ scripts/algorithms/L/Lexicographically Smallest String After Applying Operations/Lexicographically Smallest String After Applying Operations.js
@@ -1,18 +1,20 @@
+// Runtime: 584 ms (Top 70.00%) | Memory: 83.2 MB (Top 15.00%)
+
 var findLexSmallestString = function(s, a, b) {
     const n = s.length;
     const visited = new Set();
-    const queue = []; 
-   
+    const queue = [];
+
     visited.add(s);
     queue.push(s);
-    
+
     let minNum = s;
-    
+
     while (queue.length > 0) {
         const currNum = queue.shift();
 
         if (currNum < minNum) minNum = currNum;
-        
+
         const justRotate = rotate(currNum);
         const justAdd = add(currNum);
 
@@ -20,36 +22,35 @@ var findLexSmallestString = function(s, a, b) {
             visited.add(justRotate);
             queue.push(justRotate);
         }
-        
+
         if (!visited.has(justAdd)) {
             visited.add(justAdd);
             queue.push(justAdd);
         }
     }
-    
+
     return minNum;
-    
+
     function rotate(num) {
         let rotatedNum = "";
         const start = n - b;
-        
+
         for (let i = 0; i < b; i++) {
             rotatedNum += num.charAt(start + i);
         }
-        
+
         const restDigs = num.substring(0, n - b);
         rotatedNum += restDigs;
-        
+
         return rotatedNum;
     }
-    
-    
+
     function add(num) {
-        let nextNum = "";        
+        let nextNum = "";
 
         for (let i = 0; i < n; i++) {
             let currDig = num.charAt(i);
-            
+
             if (i % 2 == 0) {
                 nextNum += currDig;
             }
@@ -58,7 +59,7 @@ var findLexSmallestString = function(s, a, b) {
                 nextNum += newDig;
             }
         }
-        
+
         return nextNum;
     }
 };
