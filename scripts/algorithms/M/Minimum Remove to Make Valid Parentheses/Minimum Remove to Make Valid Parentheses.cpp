7ba73f19694893bfb594abb053ea0138a1b0c12d
AnasImloul <pydev27@gmail.com>
2022-08-19 15:20:42 +0100

    Runtime: 10 ms (Top 99.79%) | Memory: 9.8 MB (Top 95.82%)

--- scripts/algorithms/M/Minimum Remove to Make Valid Parentheses/Minimum Remove to Make Valid Parentheses.cpp
+++ scripts/algorithms/M/Minimum Remove to Make Valid Parentheses/Minimum Remove to Make Valid Parentheses.cpp
@@ -1,10 +1,12 @@
+// Runtime: 10 ms (Top 99.79%) | Memory: 9.8 MB (Top 95.82%)
+
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         int count = 0;
         for(int i = 0; i < s.size(); i++)
         {
-            
+
             if(s[i]=='(')
             {
                 count++;
@@ -20,7 +22,7 @@ public:
                     count--;
                 }
             }
-                
+
         }
         if(count==0)
         {
