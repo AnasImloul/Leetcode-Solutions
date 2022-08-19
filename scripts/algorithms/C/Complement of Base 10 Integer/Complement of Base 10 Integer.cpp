8e166a5f118adecebd5afe0d4db394b9412ff34a
AnasImloul <pydev27@gmail.com>
2022-08-19 15:20:43 +0100

    Runtime: 3 ms (Top 37.98%) | Memory: 5.9 MB (Top 76.79%)

--- scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.cpp
+++ scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.cpp
@@ -1,30 +1,32 @@
+// Runtime: 3 ms (Top 37.98%) | Memory: 5.9 MB (Top 76.79%)
+
 class Solution {
 public:
   int bitwiseComplement(int n) {
-    
+
     //-------Exceptional Case---------------
     if(n == 0){
       return 1;
     }
-    
+
     //-------Normal Case--------------------
     int testNumber = n;
     int rightShift = 0; // count of length on number in binary 5 == 101 => 3
-    
+
     // counting
     while(testNumber != 0){
       testNumber = testNumber >> 1;
       rightShift++;
     }
-    
+
     // generating mask
     int mask = 0;
     while(rightShift != 0){
       mask = mask << 1;
       mask = mask | 1;
-      rightShift--; 
+      rightShift--;
     }
-    
+
     // complementing
     int temp = ~n;
     int result = temp & mask;
