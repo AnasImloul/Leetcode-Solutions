d72d1a8a229640ab736c0e8361c59290cd306e0f
AnasImloul <pydev27@gmail.com>
2022-08-19 14:30:26 +0100

    Runtime: 42 ms (Top 77.23%) | Memory: 14 MB (Top 29.19%)

--- scripts/algorithms/I/Integer Replacement/Integer Replacement.py
+++ scripts/algorithms/I/Integer Replacement/Integer Replacement.py
@@ -1,13 +1,15 @@
+# Runtime: 42 ms (Top 77.23%) | Memory: 14 MB (Top 29.19%)
+
 class Solution:
     def integerReplacement(self, n: int) -> int:
         dp = {}
         def dfs(num):
             if num == 1:
                 return 0
-            
+
             if num in dp:
                 return dp[num]
-            
+
             # if num is even, we have only one option -> n / 2
             even = odd = 0
             if num % 2 == 0:
@@ -18,7 +20,7 @@ class Solution:
                 odd2 = 1 + dfs(num + 1)
                 # take the min of both operation
                 odd = min(odd1, odd2)
-            
+
             dp[num] = even + odd
             return dp[num]
 
