5ceb6be066d979b79b00b6953c7fb3abfef75144
AnasImloul <pydev27@gmail.com>
2022-08-19 14:54:36 +0100

    Runtime: 150 ms (Top 20.00%) | Memory: 48.4 MB (Top 40.00%)

--- scripts/algorithms/C/Check If It Is a Good Array/Check If It Is a Good Array.js
+++ scripts/algorithms/C/Check If It Is a Good Array/Check If It Is a Good Array.js
@@ -1,7 +1,9 @@
+// Runtime: 150 ms (Top 20.00%) | Memory: 48.4 MB (Top 40.00%)
+
 var isGoodArray = function(nums) {
     let gcd = nums[0]
-    
+
     for(let n of nums){while(n){[gcd, n] = [n, gcd % n]}}
-    
+
     return (gcd === 1)
 };
