f9491bb2681f6ee2e92b817c6cdaaa309233e668
AnasImloul <pydev27@gmail.com>
2022-08-19 13:50:43 +0100

    Runtime: 22 ms (Top 99.63%) | Memory: 25.6 MB (Top 27.04%)

--- scripts/algorithms/R/Rotate Array/Rotate Array.cpp
+++ scripts/algorithms/R/Rotate Array/Rotate Array.cpp
@@ -1,15 +1,16 @@
+// Runtime: 22 ms (Top 99.63%) | Memory: 25.6 MB (Top 27.04%)
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
+
         vector<int> temp(nums.size());
         for(int i = 0; i < nums.size() ;i++){
-            
+
             temp[(i+k)%nums.size()] = nums[i];
-            
-            
+
         }
-        
+
         nums = temp;
     }
 };
