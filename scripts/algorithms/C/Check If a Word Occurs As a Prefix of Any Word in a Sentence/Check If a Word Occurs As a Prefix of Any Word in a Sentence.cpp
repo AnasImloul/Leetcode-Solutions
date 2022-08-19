5d70ab01a78c1f08804ab560eb35c547fb74bbac
AnasImloul <pydev27@gmail.com>
2022-08-19 13:50:43 +0100

    Runtime: 3 ms (Top 38.04%) | Memory: 6.2 MB (Top 59.82%)

--- scripts/algorithms/C/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
+++ scripts/algorithms/C/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -1,17 +1,19 @@
+// Runtime: 3 ms (Top 38.04%) | Memory: 6.2 MB (Top 59.82%)
+
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
         istringstream iss(sentence) ;
         string word = "" ;
         int idx = 1 ;
-        
+
         while(iss >> word){
             for(int sz = 1 ; sz <= size(word) ; ++sz ){
                 if(searchWord == word.substr(0,sz)) return idx ;
             }
             ++idx ;
         }
-        
+
         return -1 ;
     }
 };
