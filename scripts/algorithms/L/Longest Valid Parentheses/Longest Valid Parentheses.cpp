f51330fff6aca3fa92ba4a531cf7cebd5305d887
AnasImloul <pydev27@gmail.com>
2022-08-20 11:22:13 +0100

    Runtime: 7 ms (Top 56.60%) | Memory: 6.7 MB (Top 96.14%)

--- scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.cpp
+++ scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.cpp
@@ -1,9 +1,11 @@
+// Runtime: 7 ms (Top 56.60%) | Memory: 6.7 MB (Top 96.14%)
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
         int n = s.length(), i = 0, ans = 0, k = 0;
         for(int j = 0; j < n; j++) {
-            if(s[j] == '(')  k++;
+            if(s[j] == '(') k++;
             else if(s[j] == ')') {
                 k--;
                 if(k == 0)
