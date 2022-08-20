8ff95dda609e3fcd07573e70819a2c430d881405
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:14 +0100

    Runtime: 86 ms (Top 42.71%) | Memory: 18.1 MB (Top 15.27%)

--- scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.py
+++ scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.py
@@ -1,9 +1,11 @@
+# Runtime: 86 ms (Top 42.71%) | Memory: 18.1 MB (Top 15.27%)
+
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         n=0
@@ -14,7 +16,7 @@ class Solution:
                 stack.append(cur)
                 cur=cur.left
             cur=stack.pop() # pop the most recent element which will be the least value at that moment
-            n+=1 
+            n+=1
             if n==k:
                 return cur.val
             cur=cur.right
