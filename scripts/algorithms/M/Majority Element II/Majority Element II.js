fa90ef27b1e97d9a7f742f759ad7d7164cbc52ce
AnasImloul <pydev27@gmail.com>
2022-08-20 11:08:26 +0100

    Runtime: 105 ms (Top 48.44%) | Memory: 43 MB (Top 93.77%)

--- scripts/algorithms/M/Majority Element II/Majority Element II.js
+++ scripts/algorithms/M/Majority Element II/Majority Element II.js
@@ -1,3 +1,5 @@
+// Runtime: 105 ms (Top 48.44%) | Memory: 43 MB (Top 93.77%)
+
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -6,15 +8,15 @@ var majorityElement = function(nums) {
     const objElement = {};
     const timesVar = Math.floor(nums.length/3);
     const resultSet = new Set();
-    
-    for(var indexI=0; indexI<nums.length; indexI++){    
-       if(objElement[nums[indexI]]) { 
-       	    objElement[nums[indexI]] = objElement[nums[indexI]] + 1;     
+
+    for(var indexI=0; indexI<nums.length; indexI++){
+       if(objElement[nums[indexI]]) {
+               objElement[nums[indexI]] = objElement[nums[indexI]] + 1;
         }
        else objElement[nums[indexI]] = 1;
-       
+
        if(objElement[nums[indexI]]>timesVar) resultSet.add(nums[indexI]);
     }
-    
+
     return [...resultSet];
 };
