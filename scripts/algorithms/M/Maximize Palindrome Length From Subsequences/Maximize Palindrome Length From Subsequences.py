1df2589127712c094b8f9a7f8f987527d4fa0abe
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:14 +0100

    Runtime: 3044 ms (Top 67.44%) | Memory: 67.7 MB (Top 72.09%)

--- scripts/algorithms/M/Maximize Palindrome Length From Subsequences/Maximize Palindrome Length From Subsequences.py
+++ scripts/algorithms/M/Maximize Palindrome Length From Subsequences/Maximize Palindrome Length From Subsequences.py
@@ -1,3 +1,5 @@
+# Runtime: 3044 ms (Top 67.44%) | Memory: 67.7 MB (Top 72.09%)
+
 class Solution:
     def longestPalindrome(self, word1: str, word2: str) -> int:
         s1 = word1 + word2
@@ -5,16 +7,16 @@ class Solution:
         dp = [[0] * n for i in range(n)]
         ans = 0
         for i in range(n-1,-1,-1):
-		# mark every character as a 1 length palindrome
+        # mark every character as a 1 length palindrome
             dp[i][i] = 1
             for j in range(i+1,n):
-			# new palindrome is found
+            # new palindrome is found
                 if s1[i] == s1[j]:
                     dp[i][j] = dp[i+1][j-1] + 2
-					# if the palindrome includes both string consider it as a valid
+                    # if the palindrome includes both string consider it as a valid
                     if i < len(word1) and j >= len(word1):
                         ans = max(ans,dp[i][j])
                 else:
                     dp[i][j] = max(dp[i][j-1],dp[i+1][j])
-        
+
         return ans
