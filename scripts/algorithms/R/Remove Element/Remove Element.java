3d195e580f9834c25b27378fe6f53a4123aad499
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:34 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 42.4 MB (Top 71.77%)

--- scripts/algorithms/R/Remove Element/Remove Element.java
+++ scripts/algorithms/R/Remove Element/Remove Element.java
@@ -1,11 +1,13 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 42.4 MB (Top 71.77%)
+
 class Solution {
-	public int removeElement(int[] nums, int val) {
-		int ind = 0;
-		for(int i=0; i<nums.length; i++){
-			if(nums[i] != val){
-				nums[ind++] = nums[i];
-			}
-		}
-		return ind;
-	}
+    public int removeElement(int[] nums, int val) {
+        int ind = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[ind++] = nums[i];
+            }
+        }
+        return ind;
+    }
 }
