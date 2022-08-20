9b0ff8d4e021b9b3fd498ca941fd17d816fa889f
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:41 +0100

    Runtime: 3 ms (Top 94.41%) | Memory: 46.4 MB (Top 56.41%)

--- scripts/algorithms/I/Intersection of Multiple Arrays/Intersection of Multiple Arrays.java
+++ scripts/algorithms/I/Intersection of Multiple Arrays/Intersection of Multiple Arrays.java
@@ -1,24 +1,24 @@
+// Runtime: 3 ms (Top 94.41%) | Memory: 46.4 MB (Top 56.41%)
 
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        
+
         List<Integer> ans = new ArrayList<>();
-        
-        int[] count  = new int[1001];
-        
+
+        int[] count = new int[1001];
+
         for(int[] arr : nums){
             for(int i : arr){
                 count[i]++;
             }
         }
-        
+
        for(int i=0;i<count.length;i++){
            if(count[i]==nums.length){
                ans.add(i);
            }
        }
-        
+
         return ans;
     }
 }
-
