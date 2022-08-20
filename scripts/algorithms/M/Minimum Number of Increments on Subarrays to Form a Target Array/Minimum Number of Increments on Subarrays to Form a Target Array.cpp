742307457845645fe40ff582e1b0abe5aa68c7ff
AnasImloul <pydev27@gmail.com>
2022-08-20 11:22:11 +0100

    Runtime: 244 ms (Top 29.68%) | Memory: 73.2 MB (Top 61.84%)

--- scripts/algorithms/M/Minimum Number of Increments on Subarrays to Form a Target Array/Minimum Number of Increments on Subarrays to Form a Target Array.cpp
+++ scripts/algorithms/M/Minimum Number of Increments on Subarrays to Form a Target Array/Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -1,9 +1,11 @@
+// Runtime: 244 ms (Top 29.68%) | Memory: 73.2 MB (Top 61.84%)
+
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
         int res = target[0];
         int prev = target[0];
-        
+
         for (int i=1; i<target.size(); i++ ){
             if (target[i]>prev){
                 res += (target[i]-prev);
