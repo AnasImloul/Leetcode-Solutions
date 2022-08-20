ad58665960ba58ec4673e909e24dbaed06849296
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:13 +0100

    Runtime: 1921 ms (Top 32.18%) | Memory: 33.8 MB (Top 16.09%)

--- scripts/algorithms/D/Decode XORed Permutation/Decode XORed Permutation.py
+++ scripts/algorithms/D/Decode XORed Permutation/Decode XORed Permutation.py
@@ -1,8 +1,10 @@
-from functools  import reduce
-from operator   import xor
+# Runtime: 1921 ms (Top 32.18%) | Memory: 33.8 MB (Top 16.09%)
+
+from functools import reduce
+from operator import xor
 
 class Solution:
-    
+
     def decode(self, encoded: List[int]) -> List[int]:
         n = len(encoded) + 1
         a = reduce(xor, range(1, n+1))
