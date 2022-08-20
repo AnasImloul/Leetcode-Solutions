fb9c88818312b2fd1020b0ad0165612477ee7c5c
AnasImloul <pydev27@gmail.com>
2022-08-20 11:08:25 +0100

    Runtime: 105 ms (Top 65.38%) | Memory: 45.6 MB (Top 59.62%)

--- scripts/algorithms/C/Check If Word Is Valid After Substitutions/Check If Word Is Valid After Substitutions.js
+++ scripts/algorithms/C/Check If Word Is Valid After Substitutions/Check If Word Is Valid After Substitutions.js
@@ -1,3 +1,5 @@
+// Runtime: 105 ms (Top 65.38%) | Memory: 45.6 MB (Top 59.62%)
+
 var isValid = function(s) {
     const stack = [];
     for(let c of s) {
@@ -8,6 +10,6 @@ var isValid = function(s) {
             if(stack.pop() !== 'a') return false;
         }
     }
-    
+
     return stack.length === 0;
 };
