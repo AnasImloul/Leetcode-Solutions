3ee34f90997f06217bdb0842cc188e2b1ce267ac
AnasImloul <pydev27@gmail.com>
2022-08-20 11:08:23 +0100

    Runtime: 123 ms (Top 34.33%) | Memory: 44.8 MB (Top 65.98%)

--- scripts/algorithms/R/Redundant Connection/Redundant Connection.js
+++ scripts/algorithms/R/Redundant Connection/Redundant Connection.js
@@ -1,15 +1,17 @@
+// Runtime: 123 ms (Top 34.33%) | Memory: 44.8 MB (Top 65.98%)
+
 var findRedundantConnection = function(edges) {
-	const root = [];
-	const find = (index) => {
-		const next = root[index];
-		return next ? find(next) : index;
-	};
+    const root = [];
+    const find = (index) => {
+        const next = root[index];
+        return next ? find(next) : index;
+    };
 
-	for (const [a, b] of edges) {
-		const x = find(a);
-		const y = find(b);
+    for (const [a, b] of edges) {
+        const x = find(a);
+        const y = find(b);
 
-		if (x === y) return [a, b];
-		root[x] = y;
-	}
+        if (x === y) return [a, b];
+        root[x] = y;
+    }
 };
