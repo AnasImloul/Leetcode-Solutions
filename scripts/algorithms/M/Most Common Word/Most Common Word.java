f635bb8f5b8d2578d21b462659b9682a95af4da6
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:36 +0100

    Runtime: 23 ms (Top 55.73%) | Memory: 44.3 MB (Top 45.07%)

--- scripts/algorithms/M/Most Common Word/Most Common Word.java
+++ scripts/algorithms/M/Most Common Word/Most Common Word.java
@@ -1,6 +1,8 @@
+// Runtime: 23 ms (Top 55.73%) | Memory: 44.3 MB (Top 45.07%)
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-       
+
         HashMap<String, Integer> hm = new HashMap<>();
         String[] words = paragraph.replaceAll("[!?',;.]"," ").toLowerCase().split("\\s+");
         for(int i=0; i<words.length; i++)
@@ -10,7 +12,7 @@ class Solution {
             else
                 hm.put(words[i], 1);
         }
-        
+
         for(int i=0; i< banned.length; i++)
             if(hm.containsKey(banned[i]))
                  hm.remove(banned[i]);
@@ -18,4 +20,3 @@ class Solution {
         return Collections.max(hm.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
-
