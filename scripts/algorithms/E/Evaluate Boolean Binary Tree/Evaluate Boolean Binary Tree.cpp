055788de7cce0ca492ca6c22892026d5c4092a9d
AnasImloul <pydev27@gmail.com>
2022-08-20 11:22:17 +0100

    Runtime: 23 ms (Top 50.31%) | Memory: 15 MB (Top 35.67%)

--- scripts/algorithms/E/Evaluate Boolean Binary Tree/Evaluate Boolean Binary Tree.cpp
+++ scripts/algorithms/E/Evaluate Boolean Binary Tree/Evaluate Boolean Binary Tree.cpp
@@ -1,8 +1,10 @@
+// Runtime: 23 ms (Top 50.31%) | Memory: 15 MB (Top 35.67%)
+
 class Solution {
 public:
     bool evaluateTree(TreeNode* root) {
-        
-        if (!root->left and !root->right)   return root->val;
+
+        if (!root->left and !root->right) return root->val;
         int l = evaluateTree(root->left);
         int r = evaluateTree(root->right);
         return (root->val == 2) ? l or r : l and r;
