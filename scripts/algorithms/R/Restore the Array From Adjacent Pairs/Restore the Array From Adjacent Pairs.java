42070fbaa46c09ac9f2835faae0c443f0c53dfee
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:38 +0100

    Runtime: 122 ms (Top 96.49%) | Memory: 87.3 MB (Top 95.99%)

--- scripts/algorithms/R/Restore the Array From Adjacent Pairs/Restore the Array From Adjacent Pairs.java
+++ scripts/algorithms/R/Restore the Array From Adjacent Pairs/Restore the Array From Adjacent Pairs.java
@@ -1,3 +1,5 @@
+// Runtime: 122 ms (Top 96.49%) | Memory: 87.3 MB (Top 95.99%)
+
 class Solution {
     public int[] restoreArray(int[][] adjacentPairs) {
         // Build an adjacency list graph.
@@ -6,7 +8,7 @@ class Solution {
             iToPairs.computeIfAbsent(pair[0], k -> new ArrayDeque<>()).add(pair[1]);
             iToPairs.computeIfAbsent(pair[1], k -> new ArrayDeque<>()).add(pair[0]);
         }
-        
+
         // Find an item that has only one neighbour.
         int start = -1;
         for (int i : iToPairs.keySet()) {
@@ -15,7 +17,7 @@ class Solution {
                 break;
             }
         }
-        
+
         // Traverse the graph in a linked-list fashion.
         int n = iToPairs.size();
         int writeIdx = 0;
@@ -23,12 +25,12 @@ class Solution {
         restored[writeIdx++] = start;
         while (writeIdx < n) {
             int next = iToPairs.get(start).remove();
-			iToPairs.remove(start);
+            iToPairs.remove(start);
             iToPairs.get(next).remove(start); // Remove the loop back to the current start.
             restored[writeIdx++] = next;
             start = next;
         }
-        
+
         return restored;
     }
 }
