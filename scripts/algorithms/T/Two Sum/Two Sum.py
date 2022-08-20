8394bac37f6a66a9438cea38d2c7021a09798e63
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:18 +0100

    Runtime: 90 ms (Top 72.15%) | Memory: 15.1 MB (Top 64.41%)

--- scripts/algorithms/T/Two Sum/Two Sum.py
+++ scripts/algorithms/T/Two Sum/Two Sum.py
@@ -1,13 +1,15 @@
+# Runtime: 90 ms (Top 72.15%) | Memory: 15.1 MB (Top 64.41%)
+
 class Solution:
-	
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-	
+
         arr = dict()
         # store the index of each number in the array
         for i,num in enumerate(nums):
             arr[num] = i
-	
-	# iterate one more time over the array to check if there is any element which complement is in the array
+
+    # iterate one more time over the array to check if there is any element which complement is in the array
         for j, num in enumerate(nums):
             rest = target - num
             if arr.get(rest, j) != j:
