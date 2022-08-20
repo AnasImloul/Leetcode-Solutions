834b2ab79fdf4f5c5a1fd1976ca725b7dd8c322a
AnasImloul <pydev27@gmail.com>
2022-08-20 11:22:14 +0100

    Runtime: 26 ms (Top 43.81%) | Memory: 7 MB (Top 46.11%)

--- scripts/algorithms/L/Longest Substring with At Least K Repeating Characters/Longest Substring with At Least K Repeating Characters.cpp
+++ scripts/algorithms/L/Longest Substring with At Least K Repeating Characters/Longest Substring with At Least K Repeating Characters.cpp
@@ -1,20 +1,22 @@
+// Runtime: 26 ms (Top 43.81%) | Memory: 7 MB (Top 46.11%)
+
 /*
     https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
-    
+
     TC: O(26 * n) ~ O(n)
     SC: O(26)
-    
-    At a glance even though it seems like it can be solved with sliding window, it's hard to come 
+
+    At a glance even though it seems like it can be solved with sliding window, it's hard to come
     up with window condition with the traditional sliding window approach.
-    Problem is what will be your valid window criteria? Since we dont know how many unique chars might 
+    Problem is what will be your valid window criteria? Since we dont know how many unique chars might
     come up in the window, we dont know whether we should shrink the window or even though few of the
     unique chars have at least k instances.
-    
+
     So we change the problem a bit. We change the window criteria:
     Find the max length window with atmost X unique chars. This is easier problem to deal with.
     The shrinking window criteria is clear, also we also keep track of chars in the window
     with frequency >= k while solving this problem.
-    
+
     Now since we are limiting the no. of unique chars, we need to run the above |unique chars in s| times.
 */
 class Solution {
@@ -23,24 +25,24 @@ public:
         unordered_set<char> chars(s.begin(), s.end());
         return chars.size();
     }
-    
+
     int longestSubstring(string s, int k) {
         // find the no. of unique chars in string
         int max_unique_chars = uniqueChars(s);
         int max_len = 0, n = s.size();
-        
+
         for(int curr_unique_chars = 1; curr_unique_chars <= max_unique_chars; curr_unique_chars++) {
             int start = 0, end = 0;
             int atleast_k = 0;
             unordered_map<char, int> freq;
-            
+
             // Process the max length window with at max curr_unique_chars unique chars
             while(end < n) {
                 // attempt window expansion
                 if(++freq[s[end]] == k)
                     ++atleast_k;
                 ++end;
-                
+
                 // shrink the window if unique chars exceeds the limit
                 while(freq.size() > curr_unique_chars && start <= end) {
                     // Current char contributed to atleast_k count, so it's count will be lesser than k now
