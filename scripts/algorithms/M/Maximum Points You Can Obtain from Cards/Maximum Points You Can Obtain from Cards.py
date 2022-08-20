038c71ce8ec131a3cca5264b60821b12a3a5d1e3
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:11 +0100

    Runtime: 577 ms (Top 67.14%) | Memory: 27 MB (Top 83.15%)

--- scripts/algorithms/M/Maximum Points You Can Obtain from Cards/Maximum Points You Can Obtain from Cards.py
+++ scripts/algorithms/M/Maximum Points You Can Obtain from Cards/Maximum Points You Can Obtain from Cards.py
@@ -1,13 +1,15 @@
+# Runtime: 577 ms (Top 67.14%) | Memory: 27 MB (Top 83.15%)
+
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         total_cards = len(cardPoints)
         total = sum(cardPoints)
         window_size = total_cards - k
         window_total = 0
-        
+
         if total_cards == k:
             return total
-        
+
         for i in range(total_cards - k):
             window_total += cardPoints[i]
         max_diff = total - window_total
