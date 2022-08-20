09d2fe5f0c92d46020a29cfa0b53ecec7ad744cb
AnasImloul <pydev27@gmail.com>
2022-08-20 11:08:24 +0100

    Runtime: 347 ms (Top 18.00%) | Memory: 83.8 MB (Top 70.00%)

--- scripts/algorithms/M/Maximum Sum BST in Binary Tree/Maximum Sum BST in Binary Tree.js
+++ scripts/algorithms/M/Maximum Sum BST in Binary Tree/Maximum Sum BST in Binary Tree.js
@@ -1,10 +1,12 @@
+// Runtime: 347 ms (Top 18.00%) | Memory: 83.8 MB (Top 70.00%)
+
 var maxSumBST = function(root) {
     let max = 0;
     const dfs = (node) => {
-		// NoNode
+        // NoNode
         if(!node) return [true, 0, Infinity, -Infinity];
 
-		// LeafNode
+        // LeafNode
         if(node && !node.left && !node.right) {
             max = Math.max(max, node.val);
             return [true, node.val, node.val, node.val]
@@ -13,30 +15,30 @@ var maxSumBST = function(root) {
         const [isLeftValid, leftVal, leftMin, leftMax] = dfs(node.left);
         const [isRightValid, rightVal, rightMin, rightMax] = dfs(node.right);
 
-		/**
-		* To establish that the current node is also a valid BST, we need to verify the following:
-		* 1. Left sub tree is a valid BST
-		* 2. Right sub tree is a valid BST
-		* 3. The values in the left BST are smaller than current node's value
-		* 4. The values in the right BST are greater than current node's value
-		**/
+        /**
+        * To establish that the current node is also a valid BST, we need to verify the following:
+        * 1. Left sub tree is a valid BST
+        * 2. Right sub tree is a valid BST
+        * 3. The values in the left BST are smaller than current node's value
+        * 4. The values in the right BST are greater than current node's value
+        **/
         if(isLeftValid && isRightValid && node.val > leftMax && node.val < rightMin) {
             max = Math.max(max, leftVal + rightVal + node.val);
             return [
                 true,
                 leftVal + rightVal + node.val,
-				/**
-				*                         12
-				*               /                        \
-				*              8                         16
-				*               \                        /
-				*                9                      15
-				*                 \                     /
-				*                  10                  14
-				*                   \                  /
-				*                    Infinity     -Infinity
-				*          [Infinity and -Infinity are to depict NoNode cases]
-				**/
+                /**
+                * 12
+                * / \
+                * 8 16
+                * \ /
+                * 9 15
+                * \ /
+                * 10 14
+                * \ /
+                * Infinity -Infinity
+                * [Infinity and -Infinity are to depict NoNode cases]
+                **/
                 Math.min(node.val, leftMin),
                 Math.max(node.val, rightMax)
             ];
