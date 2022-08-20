bf613b6791826fe530089bcd38085356ed268047
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:35 +0100

    Runtime: 5 ms (Top 57.66%) | Memory: 44.5 MB (Top 82.21%)

--- scripts/algorithms/V/Validate Stack Sequences/Validate Stack Sequences.java
+++ scripts/algorithms/V/Validate Stack Sequences/Validate Stack Sequences.java
@@ -1,9 +1,11 @@
+// Runtime: 5 ms (Top 57.66%) | Memory: 44.5 MB (Top 82.21%)
+
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> st = new Stack<>(); // Create a stack
-        
+
         int j = 0; // Intialise one pointer pointing on popped array
-        
+
         for(int val : pushed){
             st.push(val); // insert the values in stack
             while(!st.isEmpty() && st.peek() == popped[j]){ // if st.peek() values equal to popped[j];
