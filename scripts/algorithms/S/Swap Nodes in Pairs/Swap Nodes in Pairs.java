fa153b5d96f173dd1ca4a043ad2229ae1f4ce4fe
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:37 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.8 MB (Top 55.04%)

--- scripts/algorithms/S/Swap Nodes in Pairs/Swap Nodes in Pairs.java
+++ scripts/algorithms/S/Swap Nodes in Pairs/Swap Nodes in Pairs.java
@@ -1,15 +1,16 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.8 MB (Top 55.04%)
+
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(0) , prev = dummy , curr = head; 
-        dummy.next = head; 
-        while(curr != null &&  curr.next != null){
+        ListNode dummy = new ListNode(0) , prev = dummy , curr = head;
+        dummy.next = head;
+        while(curr != null && curr.next != null){
             prev.next = curr.next;
             curr.next = curr.next.next ;
-            prev.next.next = curr; 
+            prev.next.next = curr;
            curr = curr.next ;
             prev = prev.next.next;
         }
         return dummy.next;
     }
 }
- 
