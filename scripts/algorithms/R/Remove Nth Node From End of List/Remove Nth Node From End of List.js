8a3a4d9e18c02f31a823f501c3bd2da1aaa50603
AnasImloul <pydev27@gmail.com>
2022-08-20 11:08:25 +0100

    Runtime: 72 ms (Top 85.21%) | Memory: 42.2 MB (Top 92.21%)

--- scripts/algorithms/R/Remove Nth Node From End of List/Remove Nth Node From End of List.js
+++ scripts/algorithms/R/Remove Nth Node From End of List/Remove Nth Node From End of List.js
@@ -1,18 +1,20 @@
+// Runtime: 72 ms (Top 85.21%) | Memory: 42.2 MB (Top 92.21%)
+
 var removeNthFromEnd = function(head, n) {
     let start = new ListNode(0, head);
     let slow = start, fast = start;
-    
+
     for (let i =1; i <= n; i++) {
         fast = fast.next;
     }
-    
+
     while(fast && fast.next) {
         slow = slow.next;
-        fast =  fast.next;
+        fast = fast.next;
     }
-    
+
     let nthNode = slow.next
     slow.next = nthNode.next;
-    
+
     return start.next;
 };
