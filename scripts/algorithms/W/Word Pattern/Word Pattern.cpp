46d10f96f44bfa6687e4d0764c5921f761199c6c
AnasImloul <pydev27@gmail.com>
2022-08-20 11:22:16 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.5 MB (Top 54.74%)

--- scripts/algorithms/W/Word Pattern/Word Pattern.cpp
+++ scripts/algorithms/W/Word Pattern/Word Pattern.cpp
@@ -1,9 +1,11 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.5 MB (Top 54.74%)
+
 class Solution {
 public:
-    
+
     unordered_set<string> processed_words;
     string m[26]; // char to string mapping
-    
+
     bool crunch_next_word(char c, string word)
     {
         int idx = c-'a';
@@ -16,20 +18,19 @@ public:
         else if(m[idx]==word) return true;
         else return false;
     }
-    
-    
-    bool wordPattern(string pattern, string s) 
+
+    bool wordPattern(string pattern, string s)
     {
         int count = 0;
 
         int start = 0;
         int end = s.find(' ');
-        while (end != -1) 
+        while (end != -1)
         {
             string word = s.substr(start, end - start);
             char c = pattern[count];
             if(!crunch_next_word(c,word)) return false;
-            
+
             start = end + 1;
             end = s.find(' ', start);
             count++;
