fdc90b5a862baf9f0b4d77c869c2b44471b3ade5
AnasImloul <pydev27@gmail.com>
2022-08-20 11:48:13 +0100

    Runtime: 4426 ms (Top 5.00%) | Memory: 313.2 MB (Top 8.29%)

--- scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.py
+++ scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.py
@@ -1,9 +1,11 @@
+# Runtime: 4426 ms (Top 5.00%) | Memory: 313.2 MB (Top 8.29%)
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
         graph = self.buildGraph(edges)
         return self.hasPath(graph, start, end, set())
-    
-	# function to convert list of edges to adjacency list graph
+
+    # function to convert list of edges to adjacency list graph
     def buildGraph(self, edges):
         graph = {}
         for edge in edges:
@@ -13,9 +15,9 @@ class Solution:
             if b not in graph:
                 graph[b] = []
             graph[a].append(b)
-            graph[b].append(a)    
+            graph[b].append(a)
         return graph
-    
+
     def hasPath(self, graph, src, dst, visited):
         if src == dst:
             return True
