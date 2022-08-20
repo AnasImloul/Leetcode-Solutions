beaa0b2dea087216bd8b4ddab789c13e5c2c9074
AnasImloul <pydev27@gmail.com>
2022-08-20 11:00:33 +0100

    Runtime: 15 ms (Top 19.20%) | Memory: 43.5 MB (Top 45.49%)

--- scripts/algorithms/R/Restore IP Addresses/Restore IP Addresses.java
+++ scripts/algorithms/R/Restore IP Addresses/Restore IP Addresses.java
@@ -1,17 +1,19 @@
+// Runtime: 15 ms (Top 19.20%) | Memory: 43.5 MB (Top 45.49%)
+
 class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-        
+
         int len = s.length();
         for(int i = 1; i < 4 && i < len-2 ; i++ ){
             for(int j = i + 1; j < i + 4 && j < len-1 ; j++ ){
                 for(int k = j+1 ; k < j + 4 && k < len ; k++){
                   String s1 = s.substring(0,i);
-                  String s2 = s.substring(i,j);   
-                  String s3 = s.substring(j,k);  
-                  String s4 = s.substring(k,len);   
+                  String s2 = s.substring(i,j);
+                  String s3 = s.substring(j,k);
+                  String s4 = s.substring(k,len);
                   if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4))
-                      ans.add(s1+"."+s2+"."+s3+"."+s4);   
+                      ans.add(s1+"."+s2+"."+s3+"."+s4);
                 }
             }
         }
