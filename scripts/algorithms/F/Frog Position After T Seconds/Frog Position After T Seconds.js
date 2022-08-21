9d533a6ac87dbcd238fd9f1b4174f35050cf2f25
AnasImloul <pydev27@gmail.com>
2022-08-21 13:46:05 +0100

    Runtime: 91 ms (Top 100.00%) | Memory: 46.8 MB (Top 100.00%)

--- scripts/algorithms/F/Frog Position After T Seconds/Frog Position After T Seconds.js
+++ scripts/algorithms/F/Frog Position After T Seconds/Frog Position After T Seconds.js
@@ -1,3 +1,5 @@
+// Runtime: 91 ms (Top 100.00%) | Memory: 46.8 MB (Top 100.00%)
+
 /**
  * @param {number} n
  * @param {number[][]} edges
@@ -10,8 +12,8 @@ function dfs(n, map, t, target, visited, prop) {
     if(t === 0 && n !== target) return 0;
     // edge case2: if run out of steps, but found target
     if(t === 0 && n === target) return prop;
-    
-    visited.add(n);   
+
+    visited.add(n);
     // get unvisited children/neighbors
     const validChildren = [];
     (map[n] ?? []).forEach((child) => {
@@ -23,14 +25,14 @@ function dfs(n, map, t, target, visited, prop) {
     // edge case4, if still more steps to use and no more children to move,
     // but current node is not target node, cannot find target, return 0
     if(n !== target && t > 0 && !validChildren.length) return 0;
-    
+
     // go to next valid child/neighbor
     for(let i = 0; i < validChildren.length; i ++) {
         if(visited.has(validChildren[i])) continue;
         let result = dfs(validChildren[i], map, t - 1, target, visited, prop * (1 / validChildren.length))
         if(result !== 0) return result;
     }
-    
+
     return 0;
 }
 
@@ -43,6 +45,6 @@ var frogPosition = function(n, edges, t, target) {
         map[item[0]].push(item[1]);
         map[item[1]].push(item[0]);
     });
-           
+
     return dfs(1, map, t, target, new Set(), 1);
 };
