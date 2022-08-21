bc4056e7a6c51332fc2ba5ef114fa7d1dd59885c
AnasImloul <pydev27@gmail.com>
2022-08-21 14:26:54 +0100

    Runtime: 804 ms (Top 69.55%) | Memory: 27.9 MB (Top 49.83%)

--- scripts/algorithms/M/Maximize the Topmost Element After K Moves/Maximize the Topmost Element After K Moves.py
+++ scripts/algorithms/M/Maximize the Topmost Element After K Moves/Maximize the Topmost Element After K Moves.py
@@ -1,10 +1,12 @@
+# Runtime: 804 ms (Top 69.55%) | Memory: 27.9 MB (Top 49.83%)
+
 class Solution:
     def maximumTop(self, nums: List[int], k: int) -> int:
         if len(nums) == 1:
             if k%2 != 0:
                 return -1
             return nums[0]
-        
+
         if k == 0:
             return nums[0]
         if k == len(nums):
