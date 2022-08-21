455989cf89ffa7e656b439c475a371318e2a398f
AnasImloul <pydev27@gmail.com>
2022-08-21 14:26:55 +0100

    Runtime: 714 ms (Top 44.74%) | Memory: 26.1 MB (Top 5.18%)

--- scripts/algorithms/C/Contains Duplicate/Contains Duplicate.py
+++ scripts/algorithms/C/Contains Duplicate/Contains Duplicate.py
@@ -1,3 +1,5 @@
+# Runtime: 714 ms (Top 44.74%) | Memory: 26.1 MB (Top 5.18%)
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
