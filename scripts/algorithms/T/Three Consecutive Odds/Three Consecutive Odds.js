cef4cf421199aedf9046ab867148f2d4bab3f27a
AnasImloul <pydev27@gmail.com>
2022-08-21 13:46:01 +0100

    Runtime: 82 ms (Top 63.89%) | Memory: 42.1 MB (Top 58.33%)

--- scripts/algorithms/T/Three Consecutive Odds/Three Consecutive Odds.js
+++ scripts/algorithms/T/Three Consecutive Odds/Three Consecutive Odds.js
@@ -1,16 +1,18 @@
+// Runtime: 82 ms (Top 63.89%) | Memory: 42.1 MB (Top 58.33%)
+
 var threeConsecutiveOdds = function(arr) {
-	let c = 0;
+    let c = 0;
 
-	for(let val of arr){
-		if(val % 2 === 1){
-			c++;
-			if(c === 3) {
-				return true;
-			}
-		} else {
-			c=0;
-		}
-	}
+    for(let val of arr){
+        if(val % 2 === 1){
+            c++;
+            if(c === 3) {
+                return true;
+            }
+        } else {
+            c=0;
+        }
+    }
 
-	return false;
+    return false;
 };
