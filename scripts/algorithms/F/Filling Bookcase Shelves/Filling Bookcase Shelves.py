2ca54cbbfea4a8eab3fb62ac43d9eb5c22afb5c3
AnasImloul <pydev27@gmail.com>
2022-08-21 14:27:00 +0100

    Runtime: 88 ms (Top 43.53%) | Memory: 15.4 MB (Top 42.69%)

--- scripts/algorithms/F/Filling Bookcase Shelves/Filling Bookcase Shelves.py
+++ scripts/algorithms/F/Filling Bookcase Shelves/Filling Bookcase Shelves.py
@@ -1,18 +1,20 @@
+# Runtime: 88 ms (Top 43.53%) | Memory: 15.4 MB (Top 42.69%)
+
 class Solution:
     def recursion(self, idx , n , height , width):
         if idx == n:return height
-        
+
         if (idx,height,width) in self.dp: return self.dp[(idx,height,width)]
-        
+
         choice1 = self.recursion(idx+1,n,max(self.books[idx][1],height), width - self.books[idx][0])\
         if width >= self.books[idx][0] else float('inf')
-        
+
         choice2 = self.recursion(idx+1,n,self.books[idx][1], self.shelfWidth - self.books[idx][0]) + height
-        
+
         ans = min(choice1,choice2)
         self.dp[(idx,height,width)] = ans
         return ans
-    
+
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         self.books = books
         self.shelfWidth = shelfWidth
