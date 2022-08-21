56b3ccb6ed505237a1a646477d7a01f93778a822
AnasImloul <pydev27@gmail.com>
2022-08-21 18:37:42 +0100

    Runtime: 88 ms (Top 52.38%) | Memory: 42.9 MB (Top 9.52%)

--- scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.js
+++ scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.js
@@ -1,9 +1,11 @@
+// Runtime: 88 ms (Top 52.38%) | Memory: 42.9 MB (Top 9.52%)
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -18,7 +20,7 @@ var pruneTree = function(root) {
         }
         node.left = rec(node.left);
         node.right = rec(node.right);
-		// For updated tree structure if threre are leaf nodes with zero value present, if yes then return null otherewise return node itself.
+        // For updated tree structure if threre are leaf nodes with zero value present, if yes then return null otherewise return node itself.
         if(node.val===0 && node.left == null && node.right == null){
             return null
         }
@@ -26,6 +28,6 @@ var pruneTree = function(root) {
     }
     rec (root);
     if(root.val == 0 && root.left == null && root.right == null) return null
-    
+
     return root;
 };
