a0245ee7d92607031595e624de6d9ee3ad833e5c
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:45 +0100

    Runtime: 631 ms (Top 89.55%) | Memory: 99.6 MB (Top 81.24%)

--- scripts/algorithms/M/Maximum Earnings From Taxi/Maximum Earnings From Taxi.cpp
+++ scripts/algorithms/M/Maximum Earnings From Taxi/Maximum Earnings From Taxi.cpp
@@ -1,6 +1,8 @@
+// Runtime: 631 ms (Top 89.55%) | Memory: 99.6 MB (Top 81.24%)
+
 class Solution {
     long long dp[100005];
-	// Searching for next passenger who can sit  in the car
+    // Searching for next passenger who can sit in the car
     int binarySearch(vector<vector<int>>& rides,int val){
         int s=0,e=rides.size()-1;
         int ans=rides.size();
@@ -16,19 +18,18 @@ class Solution {
         }
         return ans;
     }
-    
+
     long long helper(int i,vector<vector<int>>& rides){
         if(i==rides.size()){
             return 0;
         }
         if(dp[i]!=-1) return dp[i];
-        long long op1 = helper(i+1,rides);  // We didn't pick this ith passenger
-        int idx = binarySearch(rides,rides[i][1]); 
-        long long op2 = rides[i][1]-rides[i][0] + rides[i][2] + helper(idx,rides);  // We pick this ith passenger
+        long long op1 = helper(i+1,rides); // We didn't pick this ith passenger
+        int idx = binarySearch(rides,rides[i][1]);
+        long long op2 = rides[i][1]-rides[i][0] + rides[i][2] + helper(idx,rides); // We pick this ith passenger
         return dp[i]=max(op1,op2);
     }
-    
-    
+
 public:
     long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
         memset(dp,-1,sizeof dp);
