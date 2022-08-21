05293fb4f2d8b03bdf95ed471535278d2813e906
AnasImloul <pydev27@gmail.com>
2022-08-21 14:08:16 +0100

    Runtime: 1 ms (Top 92.84%) | Memory: 41.5 MB (Top 80.00%)

--- scripts/algorithms/L/Longer Contiguous Segments of Ones than Zeros/Longer Contiguous Segments of Ones than Zeros.java
+++ scripts/algorithms/L/Longer Contiguous Segments of Ones than Zeros/Longer Contiguous Segments of Ones than Zeros.java
@@ -1,8 +1,10 @@
+// Runtime: 1 ms (Top 92.84%) | Memory: 41.5 MB (Top 80.00%)
+
 class Solution {
     public boolean checkZeroOnes(String s) {
         int length1 = 0;
         int length0 = 0;
-        
+
         int i = 0;
         while(i < s.length()){
             int temp = 0;
