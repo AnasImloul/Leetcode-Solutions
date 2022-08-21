d1f8b433d967e685350bf2db068c88c3461c4ead
AnasImloul <pydev27@gmail.com>
2022-08-21 13:46:05 +0100

    Runtime: 596 ms (Top 40.00%) | Memory: 82.5 MB (Top 53.33%)

--- scripts/algorithms/A/All Divisions With the Highest Score of a Binary Array/All Divisions With the Highest Score of a Binary Array.js
+++ scripts/algorithms/A/All Divisions With the Highest Score of a Binary Array/All Divisions With the Highest Score of a Binary Array.js
@@ -1,3 +1,5 @@
+// Runtime: 596 ms (Top 40.00%) | Memory: 82.5 MB (Top 53.33%)
+
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -8,21 +10,19 @@ var maxScoreIndices = function(nums) {
         let zeros = new Array(n+1).fill(0);
         let ones = new Array(n+1).fill(0);
         let total = new Array(n+1).fill(0);
-        
+
        // count no of zeros from left to right
         for(let i=0;i<n;i++){
             if(nums[i]==0)zeros[i+1]=zeros[i]+1;
             else zeros[i+1]=zeros[i];
         }
 
-        
         // count no of ones from right to left
         for(let i=n-1;i>=0;i--){
             if(nums[i]==1)ones[i]=ones[i+1]+1;
             else ones[i]=ones[i+1];
         }
 
-        
         // merge left and right to total and find max element
         let max=0;
         for(let i=0;i<n+1;i++){
@@ -30,7 +30,6 @@ var maxScoreIndices = function(nums) {
             if(total[i]>max)max=total[i];
         }
 
-        
         // Find occurrence of max elements and return those indexes
         let ans= [];
         for(let i=0;i<n+1;i++){
