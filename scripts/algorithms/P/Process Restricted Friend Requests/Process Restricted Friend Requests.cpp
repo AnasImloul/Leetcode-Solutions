b3929057f77024d3c502105cbe47016b25f711f2
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:52 +0100

    Runtime: 621 ms (Top 33.11%) | Memory: 22.2 MB (Top 34.20%)

--- scripts/algorithms/P/Process Restricted Friend Requests/Process Restricted Friend Requests.cpp
+++ scripts/algorithms/P/Process Restricted Friend Requests/Process Restricted Friend Requests.cpp
@@ -1,24 +1,26 @@
+// Runtime: 621 ms (Top 33.11%) | Memory: 22.2 MB (Top 34.20%)
+
 // Standard DSU Class
 class DSU {
     vector<int> parent, size;
-public: 
-    
+public:
+
     DSU(int n) {
         for(int i=0; i<=n; i++) {
             parent.push_back(i);
             size.push_back(1);
         }
     }
-    
+
     int findParent(int num) {
         if(parent[num] == num) return num;
         return parent[num] = findParent(parent[num]);
     }
-    
-	// Directly getting parents of u and v
-	// To avoid finding parent multiple times
+
+    // Directly getting parents of u and v
+    // To avoid finding parent multiple times
     void unionBySize(int parU, int parV) {
-        
+
         if(size[parU] < size[parV]) {
             size[parV] += size[parU];
             parent[parU] = parV;
@@ -33,44 +35,44 @@ public:
 class Solution {
 public:
     vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) {
-        
+
         DSU dsu(n);
-        
+
         vector<bool> successful;
-        
+
         for(auto& request : requests) {
-            
+
             int u = request[0], v = request[1];
-            
+
             int parU = dsu.findParent(u), parV = dsu.findParent(v);
-            
+
             bool flag = true;
-            
+
             if(parU != parV) {
-                
-				// Check if current friend requested is restricted or not.
+
+                // Check if current friend requested is restricted or not.
                 for(auto& restriction : restrictions) {
                     int restricted_U = restriction[0], restricted_V = restriction[1];
-                    
+
                     int restricted_parU = dsu.findParent(restricted_U);
                     int restricted_parV = dsu.findParent(restricted_V);
-                    
+
                     if((parU == restricted_parU && parV == restricted_parV) || (parU == restricted_parV && parV == restricted_parU)) {
                         flag = false;
                         break;
                     }
                 }
-                
-				// Union u and v by passing parents
-				// Since it is already calculated above
+
+                // Union u and v by passing parents
+                // Since it is already calculated above
                 if(flag) {
                     dsu.unionBySize(parU, parV);
                 }
             }
-            
+
             successful.push_back(flag);
         }
-        
+
         return successful;
     }
 };
