adc020fbba5d689790e83b5882d1b904af80b1b9
AnasImloul <pydev27@gmail.com>
2022-08-21 14:08:23 +0100

    Runtime: 9 ms (Top 16.84%) | Memory: 57.3 MB (Top 8.89%)

--- scripts/algorithms/C/Count Items Matching a Rule/Count Items Matching a Rule.java
+++ scripts/algorithms/C/Count Items Matching a Rule/Count Items Matching a Rule.java
@@ -1,14 +1,16 @@
+// Runtime: 9 ms (Top 16.84%) | Memory: 57.3 MB (Top 8.89%)
+
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int res = 0;
-        
+
         for(int i = 0 ;i<items.size();i++){
             if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
             if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
             if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
         }
-        
+
         return res;
-        
+
     }
 }
