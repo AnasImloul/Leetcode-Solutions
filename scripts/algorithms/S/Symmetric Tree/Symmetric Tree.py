54f55237b8d48ccd97e0ee49c1b73cf95c0b93c6
AnasImloul <pydev27@gmail.com>
2022-08-21 14:27:01 +0100

    Runtime: 50 ms (Top 60.40%) | Memory: 13.9 MB (Top 94.25%)

--- scripts/algorithms/S/Symmetric Tree/Symmetric Tree.py
+++ scripts/algorithms/S/Symmetric Tree/Symmetric Tree.py
@@ -1,12 +1,14 @@
+# Runtime: 50 ms (Top 60.40%) | Memory: 13.9 MB (Top 94.25%)
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         return root is None or self.findSymmetric(root.left, root.right)
-    
+
     def findSymmetric(self, left, right):
         if (left is None or right is None):
             return left == right
-        
+
         if (left.val != right.val):
             return False
-        
+
         return self.findSymmetric(left.left, right.right) and self.findSymmetric(left.right, right.left)
