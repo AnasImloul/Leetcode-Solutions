5ce0a9407f4078f9f6ffd5fa118c56b88dad1d82
AnasImloul <pydev27@gmail.com>
2022-08-21 14:26:57 +0100

    Runtime: 665 ms (Top 16.32%) | Memory: 15 MB (Top 51.03%)

--- scripts/algorithms/A/Add to Array-Form of Integer/Add to Array-Form of Integer.py
+++ scripts/algorithms/A/Add to Array-Form of Integer/Add to Array-Form of Integer.py
@@ -1,3 +1,5 @@
+# Runtime: 665 ms (Top 16.32%) | Memory: 15 MB (Top 51.03%)
+
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         return list(str(int("".join(map(str,num)))+k))
