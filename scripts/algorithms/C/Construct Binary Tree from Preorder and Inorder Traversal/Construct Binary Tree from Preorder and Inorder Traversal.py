0e7b2d2c2dd18bbc63310c1e90704d3028454741
AnasImloul <pydev27@gmail.com>
2022-08-21 14:26:55 +0100

    Runtime: 274 ms (Top 36.07%) | Memory: 88.6 MB (Top 22.22%)

--- scripts/algorithms/C/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ scripts/algorithms/C/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,9 +1,11 @@
+# Runtime: 274 ms (Top 36.07%) | Memory: 88.6 MB (Top 22.22%)
+
 # Definition for a binary tree node.
 # class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution(object):
     def buildTree(self, preorder, inorder):
         """
@@ -13,7 +15,7 @@ class Solution(object):
         """
         if not preorder or not inorder:
             return None
-        
+
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
         root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
