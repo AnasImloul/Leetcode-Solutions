9bfe28c533ce44b63a3fb201c3fffe043d8c8447
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:45 +0100

    Runtime: 43 ms (Top 7.25%) | Memory: 21.8 MB (Top 63.99%)

--- scripts/algorithms/D/Delete Leaves With a Given Value/Delete Leaves With a Given Value.cpp
+++ scripts/algorithms/D/Delete Leaves With a Given Value/Delete Leaves With a Given Value.cpp
@@ -1,12 +1,14 @@
+// Runtime: 43 ms (Top 7.25%) | Memory: 21.8 MB (Top 63.99%)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -14,16 +16,16 @@ public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
         if(root==NULL || root->left == NULL && root->right == NULL && root->val == target)
             return NULL;
-        
+
         root->left = removeLeafNodes(root->left,target);
         root->right = removeLeafNodes(root->right,target);
-        
+
         if(root->left == NULL && root->right == NULL && root->val == target)
         {
             return NULL;
         }
-        
+
         return root;
-        
+
     }
 };
