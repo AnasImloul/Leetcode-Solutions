6e05c16878b7fadad34e85b4e6f21110f563fb3b
AnasImloul <pydev27@gmail.com>
2022-08-21 13:45:54 +0100

    Runtime: 91 ms (Top 64.54%) | Memory: 43.9 MB (Top 36.99%)

--- scripts/algorithms/F/Find Target Indices After Sorting Array/Find Target Indices After Sorting Array.js
+++ scripts/algorithms/F/Find Target Indices After Sorting Array/Find Target Indices After Sorting Array.js
@@ -1,12 +1,14 @@
+// Runtime: 91 ms (Top 64.54%) | Memory: 43.9 MB (Top 36.99%)
+
 function binarySearch(lists, sorted, low, high, target){
     if(low > high) return;
-    
+
     const mid = low + Math.floor((high - low) / 2);
-    
+
     if(sorted[mid] === target){
     lists.push(mid);
     }
-    
+
     binarySearch(lists, sorted, low, mid-1, target);
     binarySearch(lists, sorted, mid+1, high, target);
 }
