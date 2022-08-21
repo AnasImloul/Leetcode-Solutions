f61bda11f8079c075f628200e3e1c3f581a327e7
AnasImloul <pydev27@gmail.com>
2022-08-21 13:45:56 +0100

    Runtime: 153 ms (Top 26.67%) | Memory: 47 MB (Top 46.11%)

--- scripts/algorithms/C/Count Nodes Equal to Average of Subtree/Count Nodes Equal to Average of Subtree.js
+++ scripts/algorithms/C/Count Nodes Equal to Average of Subtree/Count Nodes Equal to Average of Subtree.js
@@ -1,6 +1,8 @@
+// Runtime: 153 ms (Top 26.67%) | Memory: 47 MB (Top 46.11%)
+
 var averageOfSubtree = function(root) {
     let count = 0;
-    
+
     function traverse(node) {
         if (node === null) {
             return [0, 0];
@@ -15,8 +17,8 @@ var averageOfSubtree = function(root) {
 
         return [sumLeft + sumRight + node.val, countLeft + countRight + 1];
     }
-    
+
     traverse(root);
-    
+
     return count;
 };
