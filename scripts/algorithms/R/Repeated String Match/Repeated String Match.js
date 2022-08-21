616168ccf0d964a2df96b7296386b936541d8727
AnasImloul <pydev27@gmail.com>
2022-08-21 13:46:03 +0100

    Runtime: 116 ms (Top 37.33%) | Memory: 42.7 MB (Top 57.33%)

--- scripts/algorithms/R/Repeated String Match/Repeated String Match.js
+++ scripts/algorithms/R/Repeated String Match/Repeated String Match.js
@@ -1,8 +1,10 @@
-var repeatedStringMatch = function(a, b) {
-	const initRepeatTimes = Math.ceil(b.length / a.length);
-	const isMatch = (times) => a.repeat(times).includes(b);
+// Runtime: 116 ms (Top 37.33%) | Memory: 42.7 MB (Top 57.33%)
 
-	if (isMatch(initRepeatTimes)) return initRepeatTimes;
-	if (isMatch(initRepeatTimes + 1)) return initRepeatTimes + 1;
-	return -1;
+var repeatedStringMatch = function(a, b) {
+    const initRepeatTimes = Math.ceil(b.length / a.length);
+    const isMatch = (times) => a.repeat(times).includes(b);
+
+    if (isMatch(initRepeatTimes)) return initRepeatTimes;
+    if (isMatch(initRepeatTimes + 1)) return initRepeatTimes + 1;
+    return -1;
 };
