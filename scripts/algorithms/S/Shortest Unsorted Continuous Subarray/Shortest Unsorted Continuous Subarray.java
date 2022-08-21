091b41f18e6318f6a30a52b9093214eb84947143
AnasImloul <pydev27@gmail.com>
2022-08-21 14:08:19 +0100

    Runtime: 15 ms (Top 23.17%) | Memory: 53.7 MB (Top 75.07%)

--- scripts/algorithms/S/Shortest Unsorted Continuous Subarray/Shortest Unsorted Continuous Subarray.java
+++ scripts/algorithms/S/Shortest Unsorted Continuous Subarray/Shortest Unsorted Continuous Subarray.java
@@ -1,22 +1,24 @@
+// Runtime: 15 ms (Top 23.17%) | Memory: 53.7 MB (Top 75.07%)
+
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
         int[] numsClone = nums.clone();
         Arrays.sort(nums);
-        
+
         int s = Integer.MAX_VALUE;
         int e = Integer.MIN_VALUE;
-        
+
         for(int i = 0; i<nums.length; i++) {
             if(numsClone[i] != nums[i]) {
                 s = Math.min(s, i);
                 e = Math.max(e, i);
             }
         }
-        
+
         if(s == Integer.MAX_VALUE || e == Integer.MIN_VALUE) {
             return 0;
         }
-        
+
         return e-s+1;
     }
 }
