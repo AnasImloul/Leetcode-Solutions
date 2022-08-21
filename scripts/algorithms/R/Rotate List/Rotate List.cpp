7085cfdd171700e20af9a4b92f7bc74c971098cd
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:53 +0100

    Runtime: 20 ms (Top 13.46%) | Memory: 11.7 MB (Top 88.59%)

--- scripts/algorithms/R/Rotate List/Rotate List.cpp
+++ scripts/algorithms/R/Rotate List/Rotate List.cpp
@@ -1,26 +1,28 @@
+// Runtime: 20 ms (Top 13.46%) | Memory: 11.7 MB (Top 88.59%)
+
 class Solution {
 public:
-	ListNode* rotateRight(ListNode* head, int k) {
-	  
-		if(head == nullptr || head->next == nullptr || k == 0) return head;
-		
-		ListNode* node = head;
-		int s = 1;
-		
-		while(node->next != nullptr){
-			s++;
-			node = node->next;
-		}
-		
-		node->next = head;
-		k = k%s;
-		
-		while(--s >= k){
-			node = node->next;
-		}        
-		
-		ListNode* res = node->next;
-		node->next = nullptr;
-		return res;
-	}
+    ListNode* rotateRight(ListNode* head, int k) {
+
+        if(head == nullptr || head->next == nullptr || k == 0) return head;
+
+        ListNode* node = head;
+        int s = 1;
+
+        while(node->next != nullptr){
+            s++;
+            node = node->next;
+        }
+
+        node->next = head;
+        k = k%s;
+
+        while(--s >= k){
+            node = node->next;
+        }
+
+        ListNode* res = node->next;
+        node->next = nullptr;
+        return res;
+    }
 };
