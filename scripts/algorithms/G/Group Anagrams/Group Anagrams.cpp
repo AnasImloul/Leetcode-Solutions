38485ed8c0c8e7c3ce3bfae65baaa0171bdb2358
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:47 +0100

    Runtime: 109 ms (Top 13.91%) | Memory: 21.3 MB (Top 27.37%)

--- scripts/algorithms/G/Group Anagrams/Group Anagrams.cpp
+++ scripts/algorithms/G/Group Anagrams/Group Anagrams.cpp
@@ -1,8 +1,10 @@
+// Runtime: 109 ms (Top 13.91%) | Memory: 21.3 MB (Top 27.37%)
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
-		map<string,vector<int>> mp;
+        map<string,vector<int>> mp;
         for(int i=0;i<strs.size();i++)
         {
             string t=strs[i];
@@ -19,7 +21,7 @@ public:
             }
             ans.push_back(flag);
         }
-        
+
         return ans;
     }
 };
