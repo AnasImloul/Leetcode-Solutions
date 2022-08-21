3ca3a870a0d0bf0068b857bc82b0ebd466f7436f
AnasImloul <pydev27@gmail.com>
2022-08-21 13:45:58 +0100

    Runtime: 101 ms (Top 40.00%) | Memory: 43.2 MB (Top 25.45%)

--- scripts/algorithms/C/Consecutive Numbers Sum/Consecutive Numbers Sum.js
+++ scripts/algorithms/C/Consecutive Numbers Sum/Consecutive Numbers Sum.js
@@ -1,4 +1,6 @@
-var consecutiveNumbersSum = function(n) {   
+// Runtime: 101 ms (Top 40.00%) | Memory: 43.2 MB (Top 25.45%)
+
+var consecutiveNumbersSum = function(n) {
     let count = 1;
     for (let numberOfTerms = 2; numberOfTerms < Math.sqrt(2*n) + 1; numberOfTerms++) {
         let startNumber = (n - numberOfTerms * (numberOfTerms - 1) / 2) / numberOfTerms;
