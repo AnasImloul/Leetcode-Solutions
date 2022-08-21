19286070d38da8a28a60a970181099acca6c0f4b
AnasImloul <pydev27@gmail.com>
2022-08-21 14:08:16 +0100

    Runtime: 7 ms (Top 80.80%) | Memory: 65.9 MB (Top 65.16%)

--- scripts/algorithms/F/Find Eventual Safe States/Find Eventual Safe States.java
+++ scripts/algorithms/F/Find Eventual Safe States/Find Eventual Safe States.java
@@ -1,17 +1,19 @@
+// Runtime: 7 ms (Top 80.80%) | Memory: 65.9 MB (Top 65.16%)
+
 class Solution {
    public List<Integer> eventualSafeNodes(int[][] graph) {
     int n=graph.length;
     List<Integer> ans=new ArrayList<>();
-       
+
     boolean visited[]=new boolean[n];
     boolean dfsVisited[]=new boolean[n];
-       
+
     boolean nodeCycles[]=new boolean[n];
-       
+
     for(int i=0;i<n;i++){
         if(visited[i]==false){
              isCycle(i,graph,dfsVisited,visited,nodeCycles);
-        }   
+        }
     }
     for(int i=0;i<nodeCycles.length;i++){
         if(nodeCycles[i]==false)
@@ -20,17 +22,17 @@ class Solution {
     return ans;
 }
 
-public boolean isCycle(int node,int graph[][],boolean dfsVisited[],boolean visited[],boolean [] nodeCycles)      {
+public boolean isCycle(int node,int graph[][],boolean dfsVisited[],boolean visited[],boolean [] nodeCycles) {
     visited[node]=true;
     dfsVisited[node]=true;
 
     for(int adjNode:graph[node]){
         if(visited[adjNode]==false){
            if(isCycle(adjNode,graph,dfsVisited,visited,nodeCycles))
-                return nodeCycles[node]=true; 
+                return nodeCycles[node]=true;
         }else if(visited[adjNode]==true && dfsVisited[adjNode]==true){
-            return nodeCycles[node]=true; 
-        }      
+            return nodeCycles[node]=true;
+        }
     }
     dfsVisited[node]=false;
     return false;
