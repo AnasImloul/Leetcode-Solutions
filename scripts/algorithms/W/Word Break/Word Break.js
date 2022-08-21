288ac6e5d08107b271bb44bb936fb04790119a30
AnasImloul <pydev27@gmail.com>
2022-08-21 13:45:55 +0100

    Runtime: 72 ms (Top 92.39%) | Memory: 42.9 MB (Top 90.15%)

--- scripts/algorithms/W/Word Break/Word Break.js
+++ scripts/algorithms/W/Word Break/Word Break.js
@@ -1,3 +1,5 @@
+// Runtime: 72 ms (Top 92.39%) | Memory: 42.9 MB (Top 90.15%)
+
 /**
  * @param {string} s
  * @param {string[]} wordDict
@@ -6,23 +8,22 @@
 var wordBreak = function(s, wordDict) {
     var dp = new Array(s.length + 1).fill(false);
     dp[s.length] = true;
-    
+
     for (var i = s.length - 1; i >= 0; i--) {
         for (const word of wordDict) {
-            if ((i + word.length) <= s.length 
+            if ((i + word.length) <= s.length
                 && s.substring(i, i + word.length) === word) {
                 dp[i] = dp[i + word.length];
-            }                
-            
+            }
+
             if(dp[i]) break;
         }
     }
-    
+
     return dp[0];
 };
 
-
-// naive approach, take each word from the set and check if they match 
+// naive approach, take each word from the set and check if they match
 /// O(n ^ 2 * m)
 // considering m as the dictionary size
 /*
@@ -35,19 +36,19 @@ return canSegment(s, dict, 0);
 
 function canSegment (str, dict, index) {
     if (index >= str.length) return true;
-    
+
     var success = false;
-    for (const word of dict.values()) { 
-        
+    for (const word of dict.values()) {
+
         if ((index + word.length) <= str.length) {
             var substring = str.substring(index, index + word.length);
-            
+
             if (dict.has(substring)) {
                 success = success | canSegment(str, dict, index + word.length);
             }
         }
     }
-    
+
     return success;
 }
 */
