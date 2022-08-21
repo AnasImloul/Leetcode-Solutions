26994770b522f462da52f88ce2ee2b0639a0aed1
AnasImloul <pydev27@gmail.com>
2022-08-21 13:45:57 +0100

    Runtime: 390 ms (Top 54.29%) | Memory: 96.8 MB (Top 37.14%)

--- scripts/algorithms/F/Find All Groups of Farmland/Find All Groups of Farmland.js
+++ scripts/algorithms/F/Find All Groups of Farmland/Find All Groups of Farmland.js
@@ -1,26 +1,27 @@
+// Runtime: 390 ms (Top 54.29%) | Memory: 96.8 MB (Top 37.14%)
+
 var findFarmland = function(land) {
     let height = land.length;
     let width = land[0].length;
     let results = [];
     let endRow = 0;
     let endCol = 0;
-    
+
     let go = (i, j) => {
         if (i < 0 || j < 0 || i >= height || j >= width || land[i][j] === 0) {
             return;
         }
-        
+
         endRow = Math.max(endRow, i);
         endCol = Math.max(endCol, j);
         land[i][j] = 0; // reset everything to 0
-        
+
         go(i + 1, j);
         go(i - 1, j);
         go(i, j + 1);
         go(i, j - 1);
     }
-    
-    
+
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             if (land[i][j] === 1) {
