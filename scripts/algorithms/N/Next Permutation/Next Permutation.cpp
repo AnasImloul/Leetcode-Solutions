314c6ed78f81eafe409d0aea73efe1f3e0cbab13
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:43 +0100

    Runtime: 3 ms (Top 90.34%) | Memory: 12.1 MB (Top 74.79%)

--- scripts/algorithms/N/Next Permutation/Next Permutation.cpp
+++ scripts/algorithms/N/Next Permutation/Next Permutation.cpp
@@ -1,11 +1,13 @@
+// Runtime: 3 ms (Top 90.34%) | Memory: 12.1 MB (Top 74.79%)
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         if(nums.size()==1)
             return;
-        
+
         int i=nums.size()-2;
-        while(i>=0 && nums[i]>=nums[i+1])   i--;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;
         if(i>=0){
             int j=nums.size()-1;
             while(nums[i] >= nums[j]) j--;
