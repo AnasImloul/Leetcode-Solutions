9ef717c41dc167bb7f84c171b20a2a5260a35a10
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:42 +0100

    Runtime: 1171 ms (Top 32.21%) | Memory: 180.1 MB (Top 55.65%)

--- scripts/algorithms/S/Swapping Nodes in a Linked List/Swapping Nodes in a Linked List.cpp
+++ scripts/algorithms/S/Swapping Nodes in a Linked List/Swapping Nodes in a Linked List.cpp
@@ -1,10 +1,12 @@
+// Runtime: 1171 ms (Top 32.21%) | Memory: 180.1 MB (Top 55.65%)
+
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
         ListNode* left = head;
         ListNode* right = head;
         ListNode* curr = head;
-        
+
         int counter = 1;
         while(curr!=NULL){
             if (counter<k){
@@ -20,7 +22,7 @@ public:
         int temp = left->val;
         left->val = right->val;
         right->val = temp;
-        
+
         return head;
     }
 };
