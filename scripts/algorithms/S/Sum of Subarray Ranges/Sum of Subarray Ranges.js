96e33aadc18392a9fba82bc34235ef0c6ee2c120
AnasImloul <pydev27@gmail.com>
2022-08-21 13:46:00 +0100

    Runtime: 6404 ms (Top 5.01%) | Memory: 42.2 MB (Top 94.99%)

--- scripts/algorithms/S/Sum of Subarray Ranges/Sum of Subarray Ranges.js
+++ scripts/algorithms/S/Sum of Subarray Ranges/Sum of Subarray Ranges.js
@@ -1,15 +1,17 @@
+// Runtime: 6404 ms (Top 5.01%) | Memory: 42.2 MB (Top 94.99%)
+
 // O(n^3) time | O(1) space
 var subArrayRanges = function(nums) {
-	let res = 0
-	for (let i = 1; i < nums.length; i++) {
-		for (let j = 0; j < i; j++) {
-			let smallest = nums[i], biggest = nums[i]
-			for (let k = j; k < i; k++) {
-				smallest = Math.min(smallest, nums[k])
-				biggest = Math.max(biggest, nums[k])
-			}
-			res += biggest - smallest
-		}
-	}
-	return res
+    let res = 0
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            let smallest = nums[i], biggest = nums[i]
+            for (let k = j; k < i; k++) {
+                smallest = Math.min(smallest, nums[k])
+                biggest = Math.max(biggest, nums[k])
+            }
+            res += biggest - smallest
+        }
+    }
+    return res
 };
