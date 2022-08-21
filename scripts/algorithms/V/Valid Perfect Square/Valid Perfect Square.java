d3dda85fa6dadce27a80efaf6fe4fc803faf120d
AnasImloul <pydev27@gmail.com>
2022-08-21 14:08:15 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40.6 MB (Top 74.32%)

--- scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.java
+++ scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.java
@@ -1,11 +1,13 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40.6 MB (Top 74.32%)
+
 class Solution {
     public boolean isPerfectSquare(int num) {
         long start = 1;
         long end = num;
-        
+
         while(start<=end){
             long mid = start +(end - start)/2;
-            
+
             if(mid*mid==num){
                 return true;
             }
