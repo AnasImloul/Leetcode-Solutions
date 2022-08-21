9135236685a4d88cf4f200ee1d66baf959d557ce
AnasImloul <pydev27@gmail.com>
2022-08-21 13:14:50 +0100

    Runtime: 729 ms (Top 97.18%) | Memory: 174.3 MB (Top 85.68%)

--- scripts/algorithms/K/K Highest Ranked Items Within a Price Range/K Highest Ranked Items Within a Price Range.cpp
+++ scripts/algorithms/K/K Highest Ranked Items Within a Price Range/K Highest Ranked Items Within a Price Range.cpp
@@ -1,3 +1,5 @@
+// Runtime: 729 ms (Top 97.18%) | Memory: 174.3 MB (Top 85.68%)
+
 class Solution {
 public:
     struct cell
@@ -11,31 +13,31 @@ public:
     {
         bool operator()(const cell &a, const cell &b)
         {
-            if(a.dist != b.dist) 
+            if(a.dist != b.dist)
                 return a.dist < b.dist;
-            else if(a.cost != b.cost) 
+            else if(a.cost != b.cost)
                 return a.cost < b.cost;
-            else if(a.row != b.row) 
+            else if(a.row != b.row)
                 return a.row < b.row;
             else
                 return a.col < b.col;
         }
     };
-    
+
     vector<vector<int>> highestRankedKItems(vector<vector<int>>& grid, vector<int>& pricing, vector<int>& start, int k) {
         int m=grid.size();
         int n=grid[0].size();
-        
+
         queue<pair<int,int>>q;
         q.push({start[0],start[1]});
-        
+
         vector<vector<bool>> vis(m,vector<bool>(n,false));
         vis[start[0]][start[1]]=true;
-        
+
         int dx[4]={-1,0,1,0};
         int dy[4]={0,-1,0,1};
         int dist=0;
-        
+
         priority_queue<cell,vector<cell>,compare> pq;
          while(!q.empty())
          {
@@ -46,7 +48,7 @@ public:
                  if(grid[p.first][p.second]!=1 && grid[p.first][p.second]>=pricing[0] && grid[p.first][p.second]<=pricing[1])
                  {
                      pq.push({dist,grid[p.first][p.second],p.first,p.second});
-                     
+
                      if(pq.size()>k)
                          pq.pop();
                 }
@@ -64,14 +66,14 @@ public:
             }
         dist++;
         }
-        
+
         vector<vector<int>> ans;
         while(!pq.empty())
         {
         ans.push_back({pq.top().row,pq.top().col});
         pq.pop();
         }
-        
+
     reverse(ans.begin(),ans.end());
     return ans;
     }
