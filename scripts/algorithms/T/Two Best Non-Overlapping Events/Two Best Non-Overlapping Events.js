00910b6eb73ae2e364f311bd351f8b0eba55d680
AnasImloul <pydev27@gmail.com>
2022-08-22 11:22:25 +0100

    Runtime: 745 ms (Top 7.14%) | Memory: 109.1 MB (Top 14.29%)

--- scripts/algorithms/T/Two Best Non-Overlapping Events/Two Best Non-Overlapping Events.js
+++ scripts/algorithms/T/Two Best Non-Overlapping Events/Two Best Non-Overlapping Events.js
@@ -1,16 +1,17 @@
+// Runtime: 745 ms (Top 7.14%) | Memory: 109.1 MB (Top 14.29%)
 var maxTwoEvents = function(events) {
     const n = events.length;
-    
+
     events.sort((a, b) => a[0] - b[0]);
-   
+
     const minHeap = new MinPriorityQueue({ priority: x => x[1] });
-    
-    let maxVal = 0; 
+
+    let maxVal = 0;
     let maxSum = 0;
-    
+
     for (let i = 0; i < n; ++i) {
         const [currStart, currEnd, currVal] = events[i];
-       
+
         while (!minHeap.isEmpty()) {
             const topElement = minHeap.front().element;
             const [topIdx, topEnd] = topElement;
@@ -23,12 +24,11 @@ var maxTwoEvents = function(events) {
                 break;
             }
         }
-       
+
         const sum = maxVal + currVal;
         maxSum = Math.max(maxSum, sum);
         minHeap.enqueue([i, currEnd]);
     }
-    
-   
+
     return maxSum;
 };
