f773a70ea47166aa6f846e549edff7971f82f7d5
AnasImloul <pydev27@gmail.com>
2022-08-22 11:41:00 +0100

    Runtime: 593 ms (Top 76.00%) | Memory: 14.7 MB (Top 44.00%)

--- scripts/algorithms/I/Image Overlap/Image Overlap.py
+++ scripts/algorithms/I/Image Overlap/Image Overlap.py
@@ -1,3 +1,4 @@
+# Runtime: 593 ms (Top 76.00%) | Memory: 14.7 MB (Top 44.00%)
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         n = len(img1)
@@ -8,12 +9,12 @@ class Solution:
                 if img1[r][c]:
                     list1.append((r, c))
                 if img2[r][c]:
-                    list2.append((r, c))    
- 
-        shiftDict = defaultdict(int)          
+                    list2.append((r, c))
+
+        shiftDict = defaultdict(int)
         for x1, y1 in list1:
             for x2, y2 in list2:
                 dx, dy = x2 - x1, y2 - y1
                 shiftDict[(dx, dy)] += 1
-        
+
         return max(shiftDict.values()) if shiftDict else 0
