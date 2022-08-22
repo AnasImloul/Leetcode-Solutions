663d0fae7a7ee6887da3faa766c9e6eab2229838
AnasImloul <pydev27@gmail.com>
2022-08-22 11:41:00 +0100

    Runtime: 161 ms (Top 85.25%) | Memory: 14.5 MB (Top 85.26%)

--- scripts/algorithms/S/Short Encoding of Words/Short Encoding of Words.py
+++ scripts/algorithms/S/Short Encoding of Words/Short Encoding of Words.py
@@ -1,3 +1,4 @@
+# Runtime: 161 ms (Top 85.25%) | Memory: 14.5 MB (Top 85.26%)
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
         """
@@ -7,18 +8,18 @@ class Solution:
         "emit"
         "em"
         """
-        
+
         words = [w[::-1] for w in words]
-        
+
         words.sort(reverse=True)
-        
+
         res = 0
         comparable = ""
         prev_max_len = 0
-        
+
         for w in words:
             if not comparable.startswith(w):
                 res += len(w) + 1
                 comparable = w
-        
+
         return res
