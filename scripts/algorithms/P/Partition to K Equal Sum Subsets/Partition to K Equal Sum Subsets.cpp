407443d4966319c596573d1407b9a9812e2f3720
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:38 +0100

    Runtime: 2009 ms (Top 12.75%) | Memory: 101 MB (Top 5.10%)

--- scripts/algorithms/P/Partition to K Equal Sum Subsets/Partition to K Equal Sum Subsets.cpp
+++ scripts/algorithms/P/Partition to K Equal Sum Subsets/Partition to K Equal Sum Subsets.cpp
@@ -1,16 +1,17 @@
+// Runtime: 2009 ms (Top 12.75%) | Memory: 101 MB (Top 5.10%)
 class Solution {
 public:
     unordered_map<string, int> dp;
     int solve(vector<int>& nums, int target, int remain, int i, int vis, int k){
         if( k == 1) return 1;
-        
+
         //memorization addition
         string t = to_string(i)+"_"+to_string(remain)+"_"+to_string(k)+"_"+to_string(vis);
         if(dp.find(t) != dp.end()) return dp[t];
-        
-        if(remain == 0){ 
+
+        if(remain == 0){
             return dp[t] = solve(nums, target, target, nums.size()-1, vis, k - 1);
-        } 
+        }
         for(int j = i; j >= 0; --j){
             if(((vis>>j)& 1) || remain - nums[j] < 0) continue;
             vis = vis | (1 << j );
