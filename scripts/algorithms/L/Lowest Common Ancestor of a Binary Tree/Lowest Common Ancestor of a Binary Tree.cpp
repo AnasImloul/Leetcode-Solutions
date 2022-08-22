e48b724ebb1b9d80ad1e2a3002fc27f7c08f6ddd
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:40 +0100

    Runtime: 23 ms (Top 59.57%) | Memory: 14.3 MB (Top 23.43%)

--- scripts/algorithms/L/Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.cpp
+++ scripts/algorithms/L/Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.cpp
@@ -1,21 +1,22 @@
-class Solution 
+// Runtime: 23 ms (Top 59.57%) | Memory: 14.3 MB (Top 23.43%)
+class Solution
 {
 public:
-	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
-	{
-		if(!root)
-		{
-			return NULL;
-		}
-		if(root==p or root == q)
-		{
-			return root;
-		}
-		TreeNode *left=lowestCommonAncestor(root->left,p,q);
-		TreeNode *right=lowestCommonAncestor(root->right,p,q);
-		if(left and right) return root;
-		if(left) return left;
-		else return right;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(!root)
+        {
+            return NULL;
+        }
+        if(root==p or root == q)
+        {
+            return root;
+        }
+        TreeNode *left=lowestCommonAncestor(root->left,p,q);
+        TreeNode *right=lowestCommonAncestor(root->right,p,q);
+        if(left and right) return root;
+        if(left) return left;
+        else return right;
 
-	}
+    }
 };
