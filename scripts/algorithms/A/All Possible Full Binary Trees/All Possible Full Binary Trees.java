dc967c47dcac09045cb80421638caa481d631350
AnasImloul <pydev27@gmail.com>
2022-08-22 11:56:58 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 43.9 MB (Top 94.53%)

--- scripts/algorithms/A/All Possible Full Binary Trees/All Possible Full Binary Trees.java
+++ scripts/algorithms/A/All Possible Full Binary Trees/All Possible Full Binary Trees.java
@@ -1,10 +1,11 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 43.9 MB (Top 94.53%)
 class Solution {
     List<TreeNode>[] memo;
     public List<TreeNode> allPossibleFBT(int n) {
         memo = new ArrayList[n+1];
         return dp(n);
     }
-    
+
     public List<TreeNode> dp(int n){
         if(n==0 || n==2){
             return new ArrayList<TreeNode>();
@@ -22,8 +23,7 @@ class Solution {
             else right = dp(i);
             if(memo[n-1-i]!=null) left = memo[n-1-i];
             else left = dp(n-1-i);
-            
-            
+
             for(TreeNode l : left){
                 for(TreeNode r : right){
                     TreeNode temp = new TreeNode(0);
@@ -32,9 +32,9 @@ class Solution {
                     res.add(temp);
                 }
             }
-         } 
+         }
         memo[n] = res;
         return res;
-        
+
     }
 }
