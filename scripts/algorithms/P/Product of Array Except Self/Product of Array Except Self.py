68f284944d0bbc1395ae582b28e1735a0e24ed1b
AnasImloul <pydev27@gmail.com>
2022-08-22 11:41:03 +0100

    Runtime: 477 ms (Top 13.75%) | Memory: 22.5 MB (Top 18.46%)

--- scripts/algorithms/P/Product of Array Except Self/Product of Array Except Self.py
+++ scripts/algorithms/P/Product of Array Except Self/Product of Array Except Self.py
@@ -1,14 +1,15 @@
+# Runtime: 477 ms (Top 13.75%) | Memory: 22.5 MB (Top 18.46%)
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         pre = [1]* (len(nums)+1)
         back = [1]*(len(nums)+1)
-        
+
         for i in range(len(nums)):
             pre[i+1] = pre[i]*nums[i]
-        
+
         for i in range(len(nums)-1,-1,-1):
-            back[i-1] = back[i]*nums[i]      
+            back[i-1] = back[i]*nums[i]
         for i in range(len(pre)-1):
             nums[i]=pre[i]*back[i]
-            
+
         return nums
