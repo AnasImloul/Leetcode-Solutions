e1761f8b5234d999ef3a3a818dcbe53ead28685e
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:39 +0100

    Runtime: 233 ms (Top 27.28%) | Memory: 30.2 MB (Top 62.58%)

--- scripts/algorithms/D/Design Authentication Manager/Design Authentication Manager.cpp
+++ scripts/algorithms/D/Design Authentication Manager/Design Authentication Manager.cpp
@@ -1,3 +1,4 @@
+// Runtime: 233 ms (Top 27.28%) | Memory: 30.2 MB (Top 62.58%)
 class AuthenticationManager {
     int ttl;
     unordered_map<string, int> tokens;
@@ -5,18 +6,18 @@ public:
     AuthenticationManager(int timeToLive) {
         ttl = timeToLive;
     }
-    
+
     void generate(string tokenId, int currentTime) {
         tokens[tokenId] = currentTime + ttl;
     }
-    
+
     void renew(string tokenId, int currentTime) {
         auto tokenIt = tokens.find(tokenId);
         if (tokenIt != end(tokens) && tokenIt->second > currentTime) {
             tokenIt->second = currentTime + ttl;
         }
     }
-    
+
     int countUnexpiredTokens(int currentTime) {
         int res = 0;
         for (auto token: tokens) {
