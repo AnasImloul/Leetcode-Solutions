dbd626123f1f6aabd004113e5914de7843001f2a
AnasImloul <pydev27@gmail.com>
2022-08-22 11:56:56 +0100

    Runtime: 99 ms (Top 84.38%) | Memory: 67.2 MB (Top 74.81%)

--- scripts/algorithms/K/K Divisible Elements Subarrays/K Divisible Elements Subarrays.java
+++ scripts/algorithms/K/K Divisible Elements Subarrays/K Divisible Elements Subarrays.java
@@ -1,9 +1,10 @@
+// Runtime: 99 ms (Top 84.38%) | Memory: 67.2 MB (Top 74.81%)
 class Solution {
     public int countDistinct(int[] nums, int k, int p) {
         int n = nums.length;
-		// we are storing hashcode for all the substrings so that we can compare them faster.
-		// main goal is to avoid entire sub array comparision using hashcode.
-        Set<Long> ways = new HashSet<>(); 
+        // we are storing hashcode for all the substrings so that we can compare them faster.
+        // main goal is to avoid entire sub array comparision using hashcode.
+        Set<Long> ways = new HashSet<>();
         for(int i = 0; i < n; i++) {
             int cnt = 0;
             long hc = 1; // this is the running hashcode for sub array [i...j]
