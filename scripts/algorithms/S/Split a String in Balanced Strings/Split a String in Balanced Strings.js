26b03a3442d6a1213a077a9505a59ee8f2003878
AnasImloul <pydev27@gmail.com>
2022-08-22 11:22:21 +0100

    Runtime: 77 ms (Top 74.11%) | Memory: 41.7 MB (Top 91.56%)

--- scripts/algorithms/S/Split a String in Balanced Strings/Split a String in Balanced Strings.js
+++ scripts/algorithms/S/Split a String in Balanced Strings/Split a String in Balanced Strings.js
@@ -1,3 +1,4 @@
+// Runtime: 77 ms (Top 74.11%) | Memory: 41.7 MB (Top 91.56%)
 var balancedStringSplit = function(s) {
     let r_count = 0;
     let l_count = 0;
@@ -5,13 +6,13 @@ var balancedStringSplit = function(s) {
     for(let i = 0 ; i < s.length;i++){
         if(s[i]==='R') r_count++;
         else l_count++;
-        
+
         if(l_count==r_count) {
          l_count=0
-        r_count=0;   
+        r_count=0;
             ans++
         }
-        
+
     }
     return ans
 };
