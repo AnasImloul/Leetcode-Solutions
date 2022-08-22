813c98df0762248f3a8911c71ea55c3f6626bcae
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:36 +0100

    Runtime: 428 ms (Top 34.44%) | Memory: 77.6 MB (Top 40.48%)

--- scripts/algorithms/S/Sender With Largest Word Count/Sender With Largest Word Count.cpp
+++ scripts/algorithms/S/Sender With Largest Word Count/Sender With Largest Word Count.cpp
@@ -1,24 +1,25 @@
+// Runtime: 428 ms (Top 34.44%) | Memory: 77.6 MB (Top 40.48%)
 class Solution {
 public:
     string largestWordCount(vector<string>& messages, vector<string>& senders) {
-        
+
         int n(size(messages));
         map<string, int> m;
         for (auto i=0; i<n; i++) {
-            
+
             stringstream ss(messages[i]);
             string word;
             int count(0);
-            while (ss >> word)   count++;
+            while (ss >> word) count++;
             m[senders[i]] += count;
         }
-        
+
         int count(0);
         string res;
         for (auto& p : m) {
             if (p.second >= count) {
                 count = p.second;
-                if (!res.empty() or res < p.first)  res = p.first;
+                if (!res.empty() or res < p.first) res = p.first;
             }
         }
         return res;
