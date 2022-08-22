f50c61294e5bbe4f564065e19c0b8c2163ff9f48
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:36 +0100

    Runtime: 890 ms (Top 66.93%) | Memory: 18 MB (Top 68.52%)

--- scripts/algorithms/M/Match Substring After Replacement/Match Substring After Replacement.cpp
+++ scripts/algorithms/M/Match Substring After Replacement/Match Substring After Replacement.cpp
@@ -1,21 +1,22 @@
+// Runtime: 890 ms (Top 66.93%) | Memory: 18 MB (Top 68.52%)
 class Solution {
 public:
     bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
-        
+
         int m(size(s)), n(size(sub));
         unordered_map<char, unordered_set<char>> mp;
-        
+
         auto doit = [&](int ind) {
-          
+
             for (int i=ind, j=0; i<ind+n; i++, j++) {
-                if (s[i] == sub[j] or mp[sub[j]].count(s[i]))   continue;
+                if (s[i] == sub[j] or mp[sub[j]].count(s[i])) continue;
                 return false;
             }
             return true;
         };
-        
-        for (auto& mapping : mappings)  mp[mapping[0]].insert(mapping[1]);;
-        for (int i=0; i<=m-n; i++)  if (doit(i))    return true;
+
+        for (auto& mapping : mappings) mp[mapping[0]].insert(mapping[1]);;
+        for (int i=0; i<=m-n; i++) if (doit(i)) return true;
         return false;
     }
 };
