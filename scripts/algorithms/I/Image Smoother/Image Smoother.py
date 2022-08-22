a8a46db2b6ea57650fb3246d16b74cb017060e35
AnasImloul <pydev27@gmail.com>
2022-08-22 11:40:57 +0100

    Runtime: 1147 ms (Top 25.09%) | Memory: 14.8 MB (Top 29.59%)

--- scripts/algorithms/I/Image Smoother/Image Smoother.py
+++ scripts/algorithms/I/Image Smoother/Image Smoother.py
@@ -1,7 +1,8 @@
+# Runtime: 1147 ms (Top 25.09%) | Memory: 14.8 MB (Top 29.59%)
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
         m, n = len(img), len(img[0])
-        
+
         def avg(i, j):
             s = squares = 0
             top, bottom = max(0, i - 1), min(m, i + 2)
@@ -11,7 +12,7 @@ class Solution:
                 for y in range(left, right):
                     s += img[x][y]
                     squares += 1
-            
+
             return s // squares
-                       
+
         return [[avg(i, j) for j in range(n)] for i in range(m)]
