7a94d7f7058edf1de2c94127e48ff1f3523c6178
AnasImloul <pydev27@gmail.com>
2022-08-22 11:40:58 +0100

    Runtime: 109 ms (Top 63.86%) | Memory: 14.9 MB (Top 22.11%)

--- scripts/algorithms/P/Partitioning Into Minimum Number Of Deci-Binary Numbers/Partitioning Into Minimum Number Of Deci-Binary Numbers.py
+++ scripts/algorithms/P/Partitioning Into Minimum Number Of Deci-Binary Numbers/Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -1,3 +1,4 @@
+# Runtime: 109 ms (Top 63.86%) | Memory: 14.9 MB (Top 22.11%)
 class Solution:
     def minPartitions(self, n: str) -> int:
-        return int(max(n))            
+        return int(max(n))
