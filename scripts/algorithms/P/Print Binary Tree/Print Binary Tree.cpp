7a4852005727de46ffd07e63ba7e174abf3fe62b
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:35 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 8.6 MB (Top 12.25%)

--- scripts/algorithms/P/Print Binary Tree/Print Binary Tree.cpp
+++ scripts/algorithms/P/Print Binary Tree/Print Binary Tree.cpp
@@ -1,13 +1,14 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 8.6 MB (Top 12.25%)
 class Solution {
 public:
     vector<vector<string>> res;
-    
+
     int height(TreeNode* root){
         if(!root)
             return 0;
         return 1 + max(height(root->left), height(root->right));
     }
-    
+
     void fill(TreeNode* root, int r, int c, int h){
         if(!root)
             return;
@@ -15,7 +16,7 @@ public:
         fill(root->left, r + 1, c - pow(2, h - r - 1), h);
         fill(root->right, r + 1, c + pow(2, h - r - 1), h);
     }
-    
+
     vector<vector<string>> printTree(TreeNode* root) {
         int h = height(root);
         int c = pow(2, h) - 1;
