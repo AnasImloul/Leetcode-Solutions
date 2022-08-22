1f1796e5a686229dcff001bfe5de9ebd3d6d1a6e
AnasImloul <pydev27@gmail.com>
2022-08-22 11:56:57 +0100

    Runtime: 9 ms (Top 70.77%) | Memory: 42.3 MB (Top 96.77%)

--- scripts/algorithms/S/Sort Integers by The Number of 1 Bits/Sort Integers by The Number of 1 Bits.java
+++ scripts/algorithms/S/Sort Integers by The Number of 1 Bits/Sort Integers by The Number of 1 Bits.java
@@ -1,12 +1,13 @@
+// Runtime: 9 ms (Top 70.77%) | Memory: 42.3 MB (Top 96.77%)
 class Solution {
     public int[] sortByBits(int[] arr) {
-        
+
         Integer[] arrInt = new Integer[arr.length];
-        
+
         for(int i=0;i<arr.length;i++) {
             arrInt[i]=arr[i];
         }
-        
+
         Arrays.sort(arrInt, new Comparator<Integer>() {
             @Override
             public int compare(Integer a, Integer b) {
@@ -18,20 +19,20 @@ class Solution {
                 return aBits-bBits;
             }
         });
-        
+
         for(int i=0;i<arr.length;i++) {
             arr[i]=arrInt[i];
         }
         return arr;
     }
-    
+
     public int numOfBits(int a) {
         int bits=0;
         while(a!=0) {
             bits+=a&1;
             a=a>>>1;
         }
-        
+
         return bits;
     }
 }
