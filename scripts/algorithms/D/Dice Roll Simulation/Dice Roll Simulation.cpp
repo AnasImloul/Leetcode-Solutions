01c46673ea44a2a8f2b1e752980f34fa6948a101
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:37 +0100

    Runtime: 218 ms (Top 30.21%) | Memory: 35.1 MB (Top 10.13%)

--- scripts/algorithms/D/Dice Roll Simulation/Dice Roll Simulation.cpp
+++ scripts/algorithms/D/Dice Roll Simulation/Dice Roll Simulation.cpp
@@ -1,6 +1,7 @@
+// Runtime: 218 ms (Top 30.21%) | Memory: 35.1 MB (Top 10.13%)
 class Solution {
 public:
-    
+
     int fun( int i , int k , int p , vector<int>& rollMax ,vector< vector< vector<int> > > &dp){
         if(i==0) return 1;
         if(dp[i][k][p]!=-1){
@@ -11,12 +12,12 @@ public:
             if(j==k){
             if(p<rollMax[j-1]) ans=(ans+fun(i-1,j,p+1,rollMax,dp))%1000000007 ;
             }
-           
+
             else ans=(ans+fun(i-1,j,1,rollMax,dp))%1000000007 ;
         }
-        return dp[i][k][p]=ans  ;
+        return dp[i][k][p]=ans ;
     }
-    
+
     int dieSimulator(int n, vector<int>& rollMax) {
        vector< vector< vector<int> > > dp(n+1, vector< vector<int> >(7 , vector<int>(16,-1)));
         int ans=fun(n,0,1,rollMax,dp) ;
