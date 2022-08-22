037906352a7da8b28bf97b8fb10d74f93944db7c
AnasImloul <pydev27@gmail.com>
2022-08-22 12:32:38 +0100

    Runtime: 298 ms (Top 27.82%) | Memory: 85.6 MB (Top 63.00%)

--- scripts/algorithms/N/Number of Ways to Split Array/Number of Ways to Split Array.cpp
+++ scripts/algorithms/N/Number of Ways to Split Array/Number of Ways to Split Array.cpp
@@ -1,16 +1,17 @@
+// Runtime: 298 ms (Top 27.82%) | Memory: 85.6 MB (Top 63.00%)
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-     
+
         long long sumFromBack(0), sumFromFront(0);
-        for (auto& i : nums)     sumFromBack += i;
-        
+        for (auto& i : nums) sumFromBack += i;
+
         int n(size(nums)), res(0);
         for (auto i=0; i<n-1; i++) {
-            
+
             sumFromFront += nums[i]; // sum of the first i + 1 elements
             sumFromBack -= nums[i]; // sum of the last n - i - 1 elements.
-            if (sumFromFront >= sumFromBack)  res++;
+            if (sumFromFront >= sumFromBack) res++;
         }
         return res;
     }
