0ab30590fd5a4e4f44dc12180b16327963e1bbea
AnasImloul <pydev27@gmail.com>
2022-08-22 11:41:01 +0100

    Runtime: 450 ms (Top 49.65%) | Memory: 14.6 MB (Top 82.17%)

--- scripts/algorithms/R/Replace the Substring for Balanced String/Replace the Substring for Balanced String.py
+++ scripts/algorithms/R/Replace the Substring for Balanced String/Replace the Substring for Balanced String.py
@@ -1,3 +1,4 @@
+# Runtime: 450 ms (Top 49.65%) | Memory: 14.6 MB (Top 82.17%)
 class Solution:
     def balancedString(self, s):
         count = collections.Counter(s)
@@ -8,7 +9,7 @@ class Solution:
         for right, char in enumerate(s):
             # replace char whose index==right to check if it is balanced
             count[char] -= 1
-			# if it is balanced, window shrinks to get the smallest length of window
+            # if it is balanced, window shrinks to get the smallest length of window
             while left <= right and all(n / 4 >= count[char] for char in 'QWER'):
                 res = min(res, right - left + 1)
                 count[s[left]] =count[s[left]]+ 1
