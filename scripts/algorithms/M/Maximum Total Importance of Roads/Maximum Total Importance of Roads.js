1ea56f97cf22b7881c78037f4e7d428ea4e968d2
AnasImloul <pydev27@gmail.com>
2022-08-22 11:22:23 +0100

    Runtime: 443 ms (Top 37.00%) | Memory: 86.7 MB (Top 40.00%)

--- scripts/algorithms/M/Maximum Total Importance of Roads/Maximum Total Importance of Roads.js
+++ scripts/algorithms/M/Maximum Total Importance of Roads/Maximum Total Importance of Roads.js
@@ -1,22 +1,23 @@
+// Runtime: 443 ms (Top 37.00%) | Memory: 86.7 MB (Top 40.00%)
 var maximumImportance = function(n, roads) {
     const connectionCount = Array(n).fill(0)
-    
+
     // Count the connections from each city
     // e.g. the 0th city's count will be stored at index zero in the array
     for (let [cityTo, cityFrom] of roads) {
         connectionCount[cityTo]++
         connectionCount[cityFrom]++
     }
-    
+
     let cityToConnectionCount = []
     for (let city = 0; city < n; city++) {
         cityToConnectionCount.push([city, connectionCount[city]])// Store the [city, numberOfConnections]
     }
-    
+
     // Created new array(sortedCities) for readability
-    const sortedCities = cityToConnectionCount.sort((a,b) => b[1] - a[1])// sort by number of connections, the city with the greatest number of connections should be 
+    const sortedCities = cityToConnectionCount.sort((a,b) => b[1] - a[1])// sort by number of connections, the city with the greatest number of connections should be
     // the city with the greatest importance
-    
+
     const values = Array(n).fill(0)
     let importance = n
     for (let i = 0; i < sortedCities.length; i++) {
@@ -24,12 +25,12 @@ var maximumImportance = function(n, roads) {
         values[city] = importance// City at the 0th position array is should be the city with the greatest importance
         importance--
     }
-    
+
     // Sum the importance of each city, toCity => fromCity
     let res = 0
     for (let [to, from] of roads) {
-        res += values[to] + values[from]    
+        res += values[to] + values[from]
     }
-    
+
     return res
 };```
