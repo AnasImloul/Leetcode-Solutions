bf8af4133411583eb9d1d8ddd8d1aa071ada3695
AnasImloul <pydev27@gmail.com>
2022-08-22 11:56:55 +0100

    Runtime: 89 ms (Top 12.88%) | Memory: 106.2 MB (Top 42.39%)

--- scripts/algorithms/M/Minimize Maximum Pair Sum in Array/Minimize Maximum Pair Sum in Array.java
+++ scripts/algorithms/M/Minimize Maximum Pair Sum in Array/Minimize Maximum Pair Sum in Array.java
@@ -1,16 +1,16 @@
+// Runtime: 89 ms (Top 12.88%) | Memory: 106.2 MB (Top 42.39%)
 class Solution {
     public int minPairSum(int[] nums) {
         Arrays.sort(nums);
-        
+
         int output = Integer.MIN_VALUE;
-        
-		//This is greedy, so n/2 pairs must be from start and end and move inwards
+
+        //This is greedy, so n/2 pairs must be from start and end and move inwards
         for(int i=0, j=nums.length - 1; i<nums.length/2; i++, j--)
         {
             output = Math.max(output, nums[i] + nums[j]);
         }
-        
+
         return output;
     }
 }
-
