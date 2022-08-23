83002fc0fa7186a097e18ca64ad38910d40e8586
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:14 +0100

    Runtime: 537 ms (Top 51.43%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/G/Group Sold Products By The Date/Group Sold Products By The Date.sql
+++ scripts/database/G/Group Sold Products By The Date/Group Sold Products By The Date.sql
@@ -1,6 +1,6 @@
+-- Runtime: 537 ms (Top 51.43%) | Memory: 999999999 MB (Top 100.00%)
 select sell_date, count( DISTINCT product ) as num_sold ,
-    
-    GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
-    
-        FROM Activities GROUP BY sell_date order by sell_date ASC;
 
+    GROUP_CONCAT( DISTINCT product order by product ASC separator ',' ) as products
+
+        FROM Activities GROUP BY sell_date order by sell_date ASC;
