880e0b40d8b5973156092fc91ec1030ad7e26a8a
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:23 +0100

    Runtime: 130 ms (Top 13.41%) | Memory: 15 MB (Top 37.43%)

--- scripts/concurrency/P/Print Zero Even Odd/Print Zero Even Odd.py
+++ scripts/concurrency/P/Print Zero Even Odd/Print Zero Even Odd.py
@@ -1,11 +1,12 @@
+# Runtime: 130 ms (Top 13.41%) | Memory: 15 MB (Top 37.43%)
 from threading import Semaphore
 class ZeroEvenOdd:
     def __init__(self, n):
         self.n = n
-        self.semOdd  = Semaphore(0) # Permission to write an Odd  Number
+        self.semOdd = Semaphore(0) # Permission to write an Odd Number
         self.semEven = Semaphore(0) # Permission to write an Even Number
-        self.semZero = Semaphore(1) # Permission to write a  Zero
-	# printNumber(x) outputs "x", where x is an integer.
+        self.semZero = Semaphore(1) # Permission to write a Zero
+    # printNumber(x) outputs "x", where x is an integer.
     def zero(self, printNumber):
         for i in range(1,self.n+1):
             # A) Request Permission to Write a Zero
