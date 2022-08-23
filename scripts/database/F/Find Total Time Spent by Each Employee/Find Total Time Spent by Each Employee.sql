1a2ae8893bc10ff5bea946f893f67cba3fc19202
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:17 +0100

    Runtime: 635 ms (Top 48.27%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/F/Find Total Time Spent by Each Employee/Find Total Time Spent by Each Employee.sql
+++ scripts/database/F/Find Total Time Spent by Each Employee/Find Total Time Spent by Each Employee.sql
@@ -1,6 +1,7 @@
-SELECT 
-    event_day AS day, 
-    emp_id, 
+-- Runtime: 635 ms (Top 48.27%) | Memory: 999999999 MB (Top 100.00%)
+SELECT
+    event_day AS day,
+    emp_id,
     SUM(out_time - in_time) AS total_time
 FROM Employees
 GROUP BY 1, 2
