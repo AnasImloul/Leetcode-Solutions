6141d5cf48c95326dc5b969013a3cfb34046a97c
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:11 +0100

    Runtime: 359 ms (Top 39.61%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/S/Swap Salary/Swap Salary.sql
+++ scripts/database/S/Swap Salary/Swap Salary.sql
@@ -1,2 +1,3 @@
-UPDATE  salary
-SET  sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END
+-- Runtime: 359 ms (Top 39.61%) | Memory: 999999999 MB (Top 100.00%)
+UPDATE salary
+SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END
