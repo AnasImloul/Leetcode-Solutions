076f33f440db1a23d1eab68ab45a3fe449a15569
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:26 +0100

    Runtime: 38 ms (Top 87.03%) | Memory: 14.6 MB (Top 62.76%)

--- scripts/concurrency/F/Fizz Buzz Multithreaded/Fizz Buzz Multithreaded.py
+++ scripts/concurrency/F/Fizz Buzz Multithreaded/Fizz Buzz Multithreaded.py
@@ -1,40 +1,55 @@
-import threading
-class FizzBuzz(object):
-    def __init__(self, n):
+# Runtime: 38 ms (Top 87.03%) | Memory: 14.6 MB (Top 62.76%)
+# IDEA: use four semaphores with only number one starting unlocked, number-thread will loop numbers and distribute work
+# to other threads with releases, number-thread will set done flag when its loop finishes and other threads will break on this flag
+#
+class FizzBuzz:
+    def __init__(self, n: int):
+        self.done = False
         self.n = n
-        self.fz = threading.Semaphore(0)
-        self.bz = threading.Semaphore(0)
-        self.fzbz = threading.Semaphore(0)
-        self.num = threading.Semaphore(1)
+        self.fSem = threading.Semaphore(0)
+        self.bSem = threading.Semaphore(0)
+        self.fbSem = threading.Semaphore(0)
+        self.nSem = threading.Semaphore(1)
 
-    def fizz(self, printFizz):
-        for i in range(self.n/3-self.n/15):
-            self.fz.acquire()
+    # printFizz() outputs "fizz"
+    def fizz(self, printFizz: 'Callable[[], None]') -> None:
+        while True:
+            self.fSem.acquire()
+            if self.done: break
             printFizz()
-            self.num.release()
+            self.nSem.release()
 
-    def buzz(self, printBuzz):
-        for i in range(self.n/5-self.n/15):
-            self.bz.acquire()
+    # printBuzz() outputs "buzz"
+    def buzz(self, printBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.bSem.acquire()
+            if self.done: break
             printBuzz()
-            self.num.release()
+            self.nSem.release()
 
-    def fizzbuzz(self, printFizzBuzz):
-        for i in range(self.n/15):
-            self.fzbz.acquire()
+    # printFizzBuzz() outputs "fizzbuzz"
+    def fizzbuzz(self, printFizzBuzz: 'Callable[[], None]') -> None:
+        while True:
+            self.fbSem.acquire()
+            if self.done: break
             printFizzBuzz()
-            self.num.release()
-            
-    def number(self, printNumber):
+            self.nSem.release()
+
+    # printNumber(x) outputs "x", where x is an integer.
+    def number(self, printNumber: 'Callable[[int], None]') -> None:
         for i in range(1, self.n+1):
-            self.num.acquire()
-            if i%3==0 and i%5 ==0:
-                self.fzbz.release()
-            elif i%3==0:
-                self.fz.release()
-            elif i%5==0:
-                self.bz.release()
+            self.nSem.acquire()
+            if i % 15 == 0:
+                self.fbSem.release()
+            elif i % 3 == 0:
+                self.fSem.release()
+            elif i % 5 == 0:
+                self.bSem.release()
             else:
                 printNumber(i)
-                self.num.release()
-        
+                self.nSem.release()
+        self.nSem.acquire() # needed so this thread waits to set done flag only when other threads are done with printing
+        self.done = True
+        self.fSem.release() # let all other threads to break and finish
+        self.bSem.release() #
+        self.fbSem.release() #
