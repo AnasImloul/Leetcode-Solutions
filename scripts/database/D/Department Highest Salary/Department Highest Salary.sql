3384c1d8683f2d4a98b589e8630528b1c2636407
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:05 +0100

    Runtime: 671 ms (Top 63.10%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/D/Department Highest Salary/Department Highest Salary.sql
+++ scripts/database/D/Department Highest Salary/Department Highest Salary.sql
@@ -1,3 +1,4 @@
+-- Runtime: 671 ms (Top 63.10%) | Memory: 999999999 MB (Top 100.00%)
 SELECT b.Name AS Department, a.Name AS Employee, Salary FROM
 (SELECT *, MAX(Salary) OVER(PARTITION BY DepartmentId) AS max_val
 FROM Employee) a
