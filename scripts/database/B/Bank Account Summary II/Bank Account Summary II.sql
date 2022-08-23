b7cc7dda09207c290034180a3cc5f1e1a0d23844
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:17 +0100

    Runtime: 517 ms (Top 98.58%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/B/Bank Account Summary II/Bank Account Summary II.sql
+++ scripts/database/B/Bank Account Summary II/Bank Account Summary II.sql
@@ -1,5 +1,6 @@
-SELECT u.name as name , SUM(t.amount) as balance 
-FROM 
+-- Runtime: 517 ms (Top 98.58%) | Memory: 999999999 MB (Top 100.00%)
+ELECT u.name as name , SUM(t.amount) as balance
+FROM
 USERS u JOIN TRANSACTIONS t on u.account=t.account
-GROUP BY 
+GROUP BY
 t.ACCOUNT HAVING balance>10000;
