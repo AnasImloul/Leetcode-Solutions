1a8a66fec2323a0c9f02abe0c168eb9a675ee075
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:09 +0100

    Runtime: 537 ms (Top 50.01%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/T/Tree Node/Tree Node.sql
+++ scripts/database/T/Tree Node/Tree Node.sql
@@ -1,8 +1,11 @@
-SELECT id,
-
-    CASE 
-        WHEN p_id IS NULL THEN 'Root'
-        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
-        ELSE 'Leaf'
-        END AS type
- FROM Tree
+-- Runtime: 537 ms (Top 50.01%) | Memory: 999999999 MB (Top 100.00%)
+SELECT DISTINCT t1.id, (
+    CASE
+    WHEN t1.p_id IS NULL THEN 'Root'
+    WHEN t1.p_id IS NOT NULL AND t2.id IS NOT NULL THEN 'Inner'
+    WHEN t1.p_id IS NOT NULL AND t2.id IS NULL THEN 'Leaf'
+    END
+) AS Type
+FROM tree t1
+LEFT JOIN tree t2
+ON t1.id = t2.p_id
