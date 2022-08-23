25468612139c084e78dbb24f80d5c7e3b9c5792b
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:27 +0100

    Runtime: 2 ms (Top 93.59%) | Memory: 83.9 MB (Top 46.18%)

--- scripts/algorithms/B/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.java
+++ scripts/algorithms/B/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.java
@@ -1,9 +1,10 @@
+// Runtime: 2 ms (Top 93.59%) | Memory: 83.9 MB (Top 46.18%)
 class Solution {
     public int maxProfit(int[] prices) {
         int lsf = Integer.MAX_VALUE;
         int op = 0;
         int pist = 0;
-        
+
         for(int i = 0; i < prices.length; i++){
             if(prices[i] < lsf){
                 lsf = prices[i];
