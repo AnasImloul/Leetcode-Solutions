d5b563ebb6b19a8e46c569df9612a5614767288f
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:05 +0100

    Runtime: 8 ms (Top 70.94%) | Memory: 50.6 MB (Top 37.77%)

--- scripts/algorithms/D/Design a Stack With Increment Operation/Design a Stack With Increment Operation.java
+++ scripts/algorithms/D/Design a Stack With Increment Operation/Design a Stack With Increment Operation.java
@@ -1,46 +1,46 @@
+// Runtime: 8 ms (Top 70.94%) | Memory: 50.6 MB (Top 37.77%)
 
 class CustomStack {
-    
+
     int[] stack;
     int top;
-    
+
     public CustomStack(int maxSize) {
-        
+
         //intialise the stack and the top
          stack= new int[maxSize];
         top=-1;
     }
-    
+
     public void push(int x) {
-        
+
         // if the stack is full just skip
         if( top==stack.length-1) return;
-        
+
         //add to the stack
         top++;
         stack[top]=x;
     }
-    
+
     public int pop() {
-        
+
         //if stack is empty return -1
         if( top==-1) return -1;
-        
-        
+
         //remove/pop the top element
         top--;
-        return stack[top+1]; 
-        
+        return stack[top+1];
+
     }
-    
+
     public void increment(int k, int val) {
-        
+
         //got to increment the min of the elements present in the stack and k
         int n= Math.min(top+1,k);
-        
+
         for( int i=0; i<n ; i++){
             stack[i]+=val;
         }
-        
+
     }
 }
