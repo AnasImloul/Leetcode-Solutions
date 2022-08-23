f0b43508c8049d657c9c10e8a79d79cd5c613b0a
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:18 +0100

    Runtime: 246 ms (Top 18.10%) | Memory: 118.1 MB (Top 5.07%)

--- scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram II/Minimum Number of Steps to Make Two Strings Anagram II.java
+++ scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram II/Minimum Number of Steps to Make Two Strings Anagram II.java
@@ -1,7 +1,8 @@
+// Runtime: 246 ms (Top 18.10%) | Memory: 118.1 MB (Top 5.07%)
 class Solution {
     public int minSteps(String s, String t) {
         HashMap<Character,Integer> hmap = new HashMap<>();
-        for(char ch:s.toCharArray()) 
+        for(char ch:s.toCharArray())
             hmap.put(ch,hmap.getOrDefault(ch,0)+1);
         for(char ch:t.toCharArray())
             hmap.put(ch,hmap.getOrDefault(ch,0)-1);
