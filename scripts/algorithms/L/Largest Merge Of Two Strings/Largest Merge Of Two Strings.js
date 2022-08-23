99a3eeb9a232b2a3a9f7c4288ae526876aad83a9
AnasImloul <pydev27@gmail.com>
2022-08-23 11:17:51 +0100

    Runtime: 119 ms (Top 57.14%) | Memory: 49 MB (Top 61.90%)

--- scripts/algorithms/L/Largest Merge Of Two Strings/Largest Merge Of Two Strings.js
+++ scripts/algorithms/L/Largest Merge Of Two Strings/Largest Merge Of Two Strings.js
@@ -1,14 +1,15 @@
+// Runtime: 119 ms (Top 57.14%) | Memory: 49 MB (Top 61.90%)
 var largestMerge = function(word1, word2) {
     let ans = '';
     let w1 = 0, w2 = 0;
-    
+
     while(w1 < word1.length && w2 < word2.length) {
         if(word1[w1] > word2[w2]) ans += word1[w1++];
         else if(word2[w2] > word1[w1]) ans += word2[w2++];
         else {
             const rest1 = word1.slice(w1);
             const rest2 = word2.slice(w2);
-            
+
             if(rest1 > rest2) ans += word1[w1++];
             else ans += word2[w2++];
         }
