f6db67b673770532a41d638f629701bda50c5977
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:19 +0100

    Runtime: 536 ms (Top 85.07%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/C/Calculate Special Bonus/Calculate Special Bonus.sql
+++ scripts/database/C/Calculate Special Bonus/Calculate Special Bonus.sql
@@ -1,1 +1,7 @@
-IF(condition, value_if_true, value_if_false)
+-- Runtime: 536 ms (Top 85.07%) | Memory: 999999999 MB (Top 100.00%)
+select employee_id,
+case
+    when employee_id%2 != 0 and name NOT LIKE 'M%' then salary
+    else 0
+end as bonus
+from employees order by employee_id;
