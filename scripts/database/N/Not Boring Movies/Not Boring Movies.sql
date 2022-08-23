8222f0f73ec754a5bc5e8c402b0f4e37244e1009
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:10 +0100

    Runtime: 226 ms (Top 66.19%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/N/Not Boring Movies/Not Boring Movies.sql
+++ scripts/database/N/Not Boring Movies/Not Boring Movies.sql
@@ -1,4 +1,2 @@
-select id, movie, description, rating
-from Cinema
-where (id % 2) = 1 and description != "boring"
-order by rating desc
+-- Runtime: 226 ms (Top 66.19%) | Memory: 999999999 MB (Top 100.00%)
+SELECT * FROM cinema WHERE (id % 2 = 1) AND (description <> 'boring') ORDER BY rating DESC
