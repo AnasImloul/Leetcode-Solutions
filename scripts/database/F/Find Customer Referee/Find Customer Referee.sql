e8adc36058e1ea5bbf6f103dc5c7bdd2c8bb36ee
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:09 +0100

    Runtime: 499 ms (Top 67.78%) | Memory: 0B (Top 100.00%)

--- scripts/database/F/Find Customer Referee/Find Customer Referee.sql
+++ scripts/database/F/Find Customer Referee/Find Customer Referee.sql
@@ -1,1 +1,2 @@
-select name from customer where referee_id is null || referee_id != 2;
+-- Runtime: 499 ms (Top 67.78%) | Memory: 0B (Top 100.00%)
+elect name from customer where referee_id is null || referee_id != 2;
