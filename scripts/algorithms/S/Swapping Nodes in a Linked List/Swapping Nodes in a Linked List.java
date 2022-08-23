da59f13613bf2c41e653677071cf16bd04735953
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:21 +0100

    Runtime: 2 ms (Top 100.00%) | Memory: 56.9 MB (Top 96.11%)

--- scripts/algorithms/S/Swapping Nodes in a Linked List/Swapping Nodes in a Linked List.java
+++ scripts/algorithms/S/Swapping Nodes in a Linked List/Swapping Nodes in a Linked List.java
@@ -1,32 +1,33 @@
+// Runtime: 2 ms (Top 100.00%) | Memory: 56.9 MB (Top 96.11%)
 class Solution {
-    public ListNode swapNodes(ListNode head, int k) {		
+    public ListNode swapNodes(ListNode head, int k) {
         ListNode fast = head;
         ListNode slow = head;
         ListNode first = head, second = head;
-        
-		// Put fast (k-1) nodes after slow
+
+        // Put fast (k-1) nodes after slow
         for(int i = 0; i < k - 1; ++i)
             fast = fast.next;
-            
-		// Save the node for swapping
+
+        // Save the node for swapping
         first = fast;
 
-		// Move until the end of the list
+        // Move until the end of the list
         while(fast.next != null) {
-			slow = slow.next;
+            slow = slow.next;
             fast = fast.next;
         }
-        
+
         // Save the second node for swapping
-		// Note that the pointer second isn't necessary: we could use slow for swapping as well
-		// However, having second improves readability
+        // Note that the pointer second isn't necessary: we could use slow for swapping as well
+        // However, having second improves readability
         second = slow;
-		
-		// Swap values
+
+        // Swap values
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
-        
+
         return head;
     }
 }
