04e110d985cfa5081e6f8b75816714946365f7a8
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:04 +0100

    Runtime: 418 ms (Top 57.78%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/N/Nth Highest Salary/Nth Highest Salary.sql
+++ scripts/database/N/Nth Highest Salary/Nth Highest Salary.sql
@@ -1,10 +1,11 @@
+-- Runtime: 418 ms (Top 57.78%) | Memory: 999999999 MB (Top 100.00%)
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 SET N = N-1;
   RETURN (
       SELECT DISTINCT(salary) from Employee order by salary DESC
       LIMIT 1 OFFSET N
-      
+
   );
 END
 
