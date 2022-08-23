53e0807b231bbfbf437c70eea3745ea5f3c3f7a3
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:09 +0100

    Runtime: 538 ms (Top 71.52%) | Memory: 0B (Top 100.00%)

--- scripts/database/G/Game Play Analysis I/Game Play Analysis I.sql
+++ scripts/database/G/Game Play Analysis I/Game Play Analysis I.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
+-- Runtime: 538 ms (Top 71.52%) | Memory: 0B (Top 100.00%)
+ Write your MySQL query statement below
 select player_id, min(event_date) as first_login
-from activity 
+from activity
 group by player_id
