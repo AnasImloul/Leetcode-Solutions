18eda61350d5fe42d215e0947a77e2236b17afb3
AnasImloul <pydev27@gmail.com>
2022-08-23 11:45:12 +0100

    Runtime: 87 ms (Top 39.73%) | Memory: 17 MB (Top 32.47%)

--- scripts/algorithms/M/Maximum Units on a Truck/Maximum Units on a Truck.cpp
+++ scripts/algorithms/M/Maximum Units on a Truck/Maximum Units on a Truck.cpp
@@ -1,18 +1,19 @@
+// Runtime: 87 ms (Top 39.73%) | Memory: 17 MB (Top 32.47%)
 class Solution {
 public:
     static bool myfunction(vector<int>& a, vector<int>& b){
         return a[1] > b[1];
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-	    //custom sort (in increasing order of numberOfUnitsPerBox as we have to return  maximum total number of units )
+        //custom sort (in increasing order of numberOfUnitsPerBox as we have to return maximum total number of units )
         sort(boxTypes.begin(),boxTypes.end(),myfunction);
-		//greedily pick boxes till capacity is full
+        //greedily pick boxes till capacity is full
         int ans=0;
         for(auto box: boxTypes){
-            int x=min(box[0],truckSize);  //choose minimum boxes from available boxes and capacity left
-            ans+=(x*box[1]);  //adding units in ans
-            truckSize-=x;  //reduce the capacity
-            if(!truckSize) break;  //capacity full
+            int x=min(box[0],truckSize); //choose minimum boxes from available boxes and capacity left
+            ans+=(x*box[1]); //adding units in ans
+            truckSize-=x; //reduce the capacity
+            if(!truckSize) break; //capacity full
         }
         return ans;
     }
