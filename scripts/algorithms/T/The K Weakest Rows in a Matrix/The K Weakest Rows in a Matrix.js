a22cfdb793cf16757b3a5fc7040ccc1ee1255037
AnasImloul <pydev27@gmail.com>
2022-08-23 11:17:49 +0100

    Runtime: 84 ms (Top 72.55%) | Memory: 43.7 MB (Top 93.76%)

--- scripts/algorithms/T/The K Weakest Rows in a Matrix/The K Weakest Rows in a Matrix.js
+++ scripts/algorithms/T/The K Weakest Rows in a Matrix/The K Weakest Rows in a Matrix.js
@@ -1,8 +1,9 @@
+// Runtime: 84 ms (Top 72.55%) | Memory: 43.7 MB (Top 93.76%)
 /**
  * @param {number[][]} mat
  * @param {number} k
  * @return {number[]}
- 
+
  * S: O(N)
  * T: O(N*logN)
  */
@@ -10,19 +11,19 @@ var kWeakestRows = function(mat, k) {
     return mat.reduce((acc, row, index) => {
         let left = 0;
         let right = row.length - 1;
-        
+
         while(left <= right) {
             let mid = Math.floor( (left + right) / 2);
-            
+
             if(row[mid]) {
                 left = mid + 1;
             } else {
-                right = mid - 1;    
+                right = mid - 1;
             }
         }
-        
+
         acc.push({ index, value: left });
-        
+
         return acc;
     }, []).sort((a, b) => a.value - b.value).splice(0, k).map(item => item.index);
 };
