02f8da7ac3580ccec4960f5512ca139061c1c73d
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:23 +0100

    Runtime: 2 ms (Top 78.45%) | Memory: 71.3 MB (Top 74.59%)

--- scripts/algorithms/A/Add Minimum Number of Rungs/Add Minimum Number of Rungs.java
+++ scripts/algorithms/A/Add Minimum Number of Rungs/Add Minimum Number of Rungs.java
@@ -1,13 +1,14 @@
+// Runtime: 2 ms (Top 78.45%) | Memory: 71.3 MB (Top 74.59%)
 class Solution {
     public int addRungs(int[] rungs, int dist) {
-		int ans = 0;
-		for (int i=0 ; i<rungs.length ; i++) {
-			int d = (i==0) ? rungs[i] : rungs[i] - rungs[i-1];
-			if ( d > dist ) {
-				ans += d/dist;
-				ans += ( d%dist == 0 ) ? -1 : 0; 
-			}
-		}
-		return ans;
-	}
+        int ans = 0;
+        for (int i=0 ; i<rungs.length ; i++) {
+            int d = (i==0) ? rungs[i] : rungs[i] - rungs[i-1];
+            if ( d > dist ) {
+                ans += d/dist;
+                ans += ( d%dist == 0 ) ? -1 : 0;
+            }
+        }
+        return ans;
+    }
 }
