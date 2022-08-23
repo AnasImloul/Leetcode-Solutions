7bd37b3d30253dfbc10d03eeec5bcb76df073036
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:29 +0100

    Runtime: 3 ms (Top 63.81%) | Memory: 54.8 MB (Top 11.36%)

--- scripts/algorithms/L/Largest Odd Number in String/Largest Odd Number in String.java
+++ scripts/algorithms/L/Largest Odd Number in String/Largest Odd Number in String.java
@@ -1,3 +1,4 @@
+// Runtime: 3 ms (Top 63.81%) | Memory: 54.8 MB (Top 11.36%)
 class Solution {
     public String largestOddNumber(String num) {
         for (int i = num.length() - 1; i > -1; i--) {
