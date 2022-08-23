0d80ab62db065946c7b0a551243294a47d0cd44b
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:07 +0100

    Runtime: 573 ms (Top 60.19%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/T/Trips and Users/Trips and Users.sql
+++ scripts/database/T/Trips and Users/Trips and Users.sql
@@ -1,14 +1,15 @@
+-- Runtime: 573 ms (Top 60.19%) | Memory: 999999999 MB (Top 100.00%)
 with stats as (
-  select 
-    Request_at, 
+  select
+    Request_at,
     T.Status <> 'completed' as IsCancelled
-  from Trips T 
-  join Users C on (Client_Id = C.Users_Id and C.Banned = 'No') 
-  join Users D on (Driver_Id = D.Users_Id and D.Banned = 'No') 
+  from Trips T
+  join Users C on (Client_Id = C.Users_Id and C.Banned = 'No')
+  join Users D on (Driver_Id = D.Users_Id and D.Banned = 'No')
   where
     Request_at between '2013-10-01' and '2013-10-03'
 )
-select 
+select
   Request_at as Day,
   Round(
     cast(sum(IsCancelled) as real) / cast(count(*) as real),
