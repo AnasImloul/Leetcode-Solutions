b627f8a01e17fc9e027153062011e8652fab9f82
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:13 +0100

    Runtime: 391 ms (Top 83.58%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/A/Article Views I/Article Views I.sql
+++ scripts/database/A/Article Views I/Article Views I.sql
@@ -1,4 +1,4 @@
-select distinct author_id as id
-from views
+-- Runtime: 391 ms (Top 83.58%) | Memory: 999999999 MB (Top 100.00%)
+SELECT DISTINCT author_id AS id FROM Views
 where author_id = viewer_id
-order by id asc
+ORDER BY id
