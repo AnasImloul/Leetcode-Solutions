ec40f96468ee18e45471475ededc746caab2d920
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:26 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 43.1 MB (Top 92.66%)

--- scripts/algorithms/T/Transpose Matrix/Transpose Matrix.java
+++ scripts/algorithms/T/Transpose Matrix/Transpose Matrix.java
@@ -1,16 +1,17 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 43.1 MB (Top 92.66%)
 class Solution {
     public int[][] transpose(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        
+
         int[][] trans = new int[n][m];
-        
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 trans[i][j] = matrix[j][i];
             }
         }
-        
+
         return trans;
     }
 }
