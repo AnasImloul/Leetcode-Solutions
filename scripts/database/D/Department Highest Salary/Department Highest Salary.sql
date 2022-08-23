26b3dbf15636a0f75952ebe02719cda245224279
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:08 +0100

    Runtime: 664 ms (Top 64.31%) | Memory: 0B (Top 100.00%)

--- scripts/database/D/Department Highest Salary/Department Highest Salary.sql
+++ scripts/database/D/Department Highest Salary/Department Highest Salary.sql
@@ -1,4 +1,5 @@
-SELECT b.Name AS Department, a.Name AS Employee, Salary FROM
+-- Runtime: 664 ms (Top 64.31%) | Memory: 0B (Top 100.00%)
+ELECT b.Name AS Department, a.Name AS Employee, Salary FROM
 (SELECT *, MAX(Salary) OVER(PARTITION BY DepartmentId) AS max_val
 FROM Employee) a
 JOIN Department b
