30037158b1419c202be89fcb6b44ab95396308e0
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:10 +0100

    Runtime: 226 ms (Top 99.70%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/E/Exchange Seats/Exchange Seats.sql
+++ scripts/database/E/Exchange Seats/Exchange Seats.sql
@@ -1,6 +1,7 @@
+-- Runtime: 226 ms (Top 99.70%) | Memory: 999999999 MB (Top 100.00%)
 SELECT id, IFNULL(
-					CASE WHEN MOD(id, 2) = 1 THEN LEAD(student,1) OVER() 
-                    ELSE LAG(student,1) OVER() END, 
+                    CASE WHEN MOD(id, 2) = 1 THEN LEAD(student,1) OVER()
+                    ELSE LAG(student,1) OVER() END,
                     student
                   ) student
 FROM seat;
