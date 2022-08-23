c2a900b7f77467f7894319051e24e732ac71a33b
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:06 +0100

    Runtime: 930 ms (Top 53.30%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/D/Delete Duplicate Emails/Delete Duplicate Emails.sql
+++ scripts/database/D/Delete Duplicate Emails/Delete Duplicate Emails.sql
@@ -1,1 +1,2 @@
-DELETE p from Person p, Person q where p.Id>q.Id AND q.Email=p.Email 
+-- Runtime: 930 ms (Top 53.30%) | Memory: 999999999 MB (Top 100.00%)
+DELETE p from Person p, Person q where p.Id>q.Id AND q.Email=p.Email
