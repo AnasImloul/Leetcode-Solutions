7405eadffd1c2baed26c471f37223b0201f40e4c
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:16 +0100

    Runtime: 2 ms (Top 77.93%) | Memory: 42.7 MB (Top 83.40%)

--- scripts/algorithms/P/Pancake Sorting/Pancake Sorting.java
+++ scripts/algorithms/P/Pancake Sorting/Pancake Sorting.java
@@ -1,3 +1,4 @@
+// Runtime: 2 ms (Top 77.93%) | Memory: 42.7 MB (Top 83.40%)
 // BruteForce Approach!
 // Author - Nikhil Sharma
 // LinkedIn - https://www.linkedin.com/in/nikhil-sharma-41a287226/
@@ -17,7 +18,7 @@ class Solution {
         }
         return list;
     }
-    
+
     static int findIndex(int[] arr, int value) {
         for(int i=0; i<arr.length; i++) {
             if(arr[i] == value){
@@ -26,7 +27,7 @@ class Solution {
         }
         return 0;
     }
-    
+
     static void reverse(int[] arr, int maxIndex) {
         int l = 0;
         while(l<maxIndex) {
