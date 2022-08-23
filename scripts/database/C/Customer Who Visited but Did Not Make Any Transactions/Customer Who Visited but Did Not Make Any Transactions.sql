d950ce210672c56e380d5305688aac3ab60d2efd
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:16 +0100

    Runtime: 1404 ms (Top 54.65%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/C/Customer Who Visited but Did Not Make Any Transactions/Customer Who Visited but Did Not Make Any Transactions.sql
+++ scripts/database/C/Customer Who Visited but Did Not Make Any Transactions/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,3 +1,4 @@
+-- Runtime: 1404 ms (Top 54.65%) | Memory: 999999999 MB (Top 100.00%)
 SELECT customer_id, COUNT(customer_id) AS count_no_trans
 FROM Visits WHERE visit_id NOT IN(SELECT visit_id FROM Transactions)
 GROUP BY customer_id;
