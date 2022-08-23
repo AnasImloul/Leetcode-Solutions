6301228244d1bad38f473c642a6101db478a7297
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:12 +0100

    Runtime: 222 ms (Top 68.26%) | Memory: 0B (Top 100.00%)

--- scripts/database/N/Not Boring Movies/Not Boring Movies.sql
+++ scripts/database/N/Not Boring Movies/Not Boring Movies.sql
@@ -1,4 +1,5 @@
-select id, movie, description, rating
+-- Runtime: 222 ms (Top 68.26%) | Memory: 0B (Top 100.00%)
+elect id, movie, description, rating
 from Cinema
 where (id % 2) = 1 and description != "boring"
 order by rating desc
