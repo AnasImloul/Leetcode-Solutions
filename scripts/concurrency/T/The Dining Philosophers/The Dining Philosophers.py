fce7ea54c15d8addb9839e5b3a89e1fca236d480
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:28 +0100

    Runtime: 233 ms (Top 17.20%) | Memory: 15.2 MB (Top 30.57%)

--- scripts/concurrency/T/The Dining Philosophers/The Dining Philosophers.py
+++ scripts/concurrency/T/The Dining Philosophers/The Dining Philosophers.py
@@ -1,13 +1,30 @@
+# Runtime: 233 ms (Top 17.20%) | Memory: 15.2 MB (Top 30.57%)
+from threading import Lock
 from threading import Semaphore
 
 class DiningPhilosophers:
-    def __init__(self):
-        self.sizelock = Semaphore(4)
-        self.locks = [Semaphore(1) for _ in range(5)]
 
-    def wantsToEat(self, index, *actions):
-        left, right = index, (index - 1) % 5
-        with self.sizelock:
-            with self.locks[left], self.locks[right]:
-                for action in actions:
-                    action()
+    def __init__(self):
+        self.spoons = [Lock() for i in range(5)]
+        self.sem = Semaphore(4)
+
+    # call the functions directly to execute, for example, eat()
+    def wantsToEat(self,
+                   philosopher: int,
+                   pickLeftFork: 'Callable[[], None]',
+                   pickRightFork: 'Callable[[], None]',
+                   eat: 'Callable[[], None]',
+                   putLeftFork: 'Callable[[], None]',
+                   putRightFork: 'Callable[[], None]') -> None:
+
+        left = philosopher
+        right = (philosopher + 1) % 5
+
+        with self.sem:
+            with self.spoons[left]:
+                with self.spoons[right]:
+                    pickLeftFork()
+                    pickRightFork()
+                    eat()
+                    putLeftFork()
+                    putRightFork()
