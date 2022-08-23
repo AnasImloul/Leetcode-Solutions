4a81b0a6ea3335edf00de2d70fc80d4d6f904cc7
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:05 +0100

    Runtime: 356 ms (Top 83.54%) | Memory: 0B (Top 100.00%)

--- scripts/database/C/Combine Two Tables/Combine Two Tables.sql
+++ scripts/database/C/Combine Two Tables/Combine Two Tables.sql
@@ -1,2 +1,3 @@
-SELECT p.firstName, p.lastName, a.city, a.state FROM Person p  --accessing first hench LEFT
+-- Runtime: 356 ms (Top 83.54%) | Memory: 0B (Top 100.00%)
+ELECT p.firstName, p.lastName, a.city, a.state FROM Person p --accessing first hench LEFT
 LEFT JOIN Address a USING (personId) -- accessing second hence RIGHT. but we want every row in person so LEFT JOIN.
