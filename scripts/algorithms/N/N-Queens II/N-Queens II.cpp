f7b209951b85ecdfdc1b597c0eab4e7bf438d63d
AnasImloul <pydev27@gmail.com>
2022-08-23 11:45:13 +0100

    Runtime: 7 ms (Top 64.18%) | Memory: 6 MB (Top 83.58%)

--- scripts/algorithms/N/N-Queens II/N-Queens II.cpp
+++ scripts/algorithms/N/N-Queens II/N-Queens II.cpp
@@ -1,19 +1,20 @@
+// Runtime: 7 ms (Top 64.18%) | Memory: 6 MB (Top 83.58%)
 class Solution {
 public:
     int totalNQueens(int n) {
-	vector<bool> col(n), diag(2*n-1), anti_diag(2*n-1);
-	return solve(col, diag, anti_diag, 0);
+    vector<bool> col(n), diag(2*n-1), anti_diag(2*n-1);
+    return solve(col, diag, anti_diag, 0);
 }
-   
+
 int solve(vector<bool>& col, vector<bool>& diag, vector<bool>& anti_diag, int row) {
-	int n = size(col), count = 0;
+    int n = size(col), count = 0;
     if(row == n) return 1;
-	for(int column = 0; column < n; column++)           
-		if(!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1]){ 
-			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;
-			count += solve(col, diag, anti_diag, row + 1); 
-			col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false; 
-		}                                
-	return count;
+    for(int column = 0; column < n; column++)
+        if(!col[column] && !diag[row + column] && !anti_diag[row - column + n - 1]){
+            col[column] = diag[row + column] = anti_diag[row - column + n - 1] = true;
+            count += solve(col, diag, anti_diag, row + 1);
+            col[column] = diag[row + column] = anti_diag[row - column + n - 1] = false;
+        }
+    return count;
 }
 };
