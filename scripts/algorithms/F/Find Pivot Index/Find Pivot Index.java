30d868f5690cfd92d78939a0af920f706a9b8039
AnasImloul <pydev27@gmail.com>
2022-08-23 12:00:14 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 43.2 MB (Top 91.78%)

--- scripts/algorithms/F/Find Pivot Index/Find Pivot Index.java
+++ scripts/algorithms/F/Find Pivot Index/Find Pivot Index.java
@@ -1,17 +1,18 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 43.2 MB (Top 91.78%)
 class Solution {
     public int pivotIndex(int[] nums) {
-        
-        int leftsum = 0; 
+
+        int leftsum = 0;
         int rightsum = 0;
-        
+
         for(int i =1; i< nums.length; i++) rightsum += nums[i];
-        
+
         if (leftsum == rightsum) return 0;
-        
+
         for(int i = 1 ; i < nums.length; i++){
             leftsum += nums[i-1];
             rightsum -= nums[i];
-            
+
             if(leftsum == rightsum) return i;
         }
         return -1;
