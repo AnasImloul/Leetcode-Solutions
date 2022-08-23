694cfade97215a38c82bf686785274b71153e2c8
AnasImloul <pydev27@gmail.com>
2022-08-23 16:55:10 +0100

    Runtime: 237 ms (Top 98.74%) | Memory: 0 MB (Top 100.00%)

--- scripts/database/C/Classes More Than 5 Students/Classes More Than 5 Students.sql
+++ scripts/database/C/Classes More Than 5 Students/Classes More Than 5 Students.sql
@@ -1,3 +1,4 @@
-select class from courses
-group by class 
+-- Runtime: 237 ms (Top 98.74%) | Memory: 0 MB (Top 100.00%)
+elect class from courses
+group by class
 HAVING count(distinct student) >= 5;
