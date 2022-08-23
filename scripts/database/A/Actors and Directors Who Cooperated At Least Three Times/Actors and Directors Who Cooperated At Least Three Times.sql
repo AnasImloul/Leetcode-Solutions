bc11b59a19d5fab7747511d883bd82d3ab20ca55
AnasImloul <pydev27@gmail.com>
2022-08-23 13:52:11 +0100

    Runtime: 430 ms (Top 57.24%) | Memory: 999999999 MB (Top 100.00%)

--- scripts/database/A/Actors and Directors Who Cooperated At Least Three Times/Actors and Directors Who Cooperated At Least Three Times.sql
+++ scripts/database/A/Actors and Directors Who Cooperated At Least Three Times/Actors and Directors Who Cooperated At Least Three Times.sql
@@ -1,4 +1,2 @@
-SELECT ACTOR_ID, DIRECTOR_ID
-	FROM ActorDirector
-GROUP BY ACTOR_ID, DIRECTOR_ID
-	HAVING COUNT(*) >= 3
+-- Runtime: 430 ms (Top 57.24%) | Memory: 999999999 MB (Top 100.00%)
+select actor_id,director_id from ActorDirector group by 1,2 having count(*)>=3;
