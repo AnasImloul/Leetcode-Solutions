b933ca7a6c3cde212e02513cf34bfa52cfb6a35d
AnasImloul <pydev27@gmail.com>
2022-08-23 12:08:37 +0100

    Runtime: 40 ms (Top 73.88%) | Memory: 13.8 MB (Top 97.86%)

--- scripts/algorithms/S/Sum of Digits in Base K/Sum of Digits in Base K.py
+++ scripts/algorithms/S/Sum of Digits in Base K/Sum of Digits in Base K.py
@@ -1,3 +1,4 @@
+# Runtime: 40 ms (Top 73.88%) | Memory: 13.8 MB (Top 97.86%)
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
         cnt = 0
@@ -5,4 +6,4 @@ class Solution:
             cnt += (n % k)
             n //= k
         print(cnt)
-        return cnt 
+        return cnt
