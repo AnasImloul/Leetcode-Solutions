a8c1d349a92c5f490204d81f7a46c23c58c2202c
AnasImloul <pydev27@gmail.com>
2022-08-23 12:08:33 +0100

    Runtime: 2938 ms (Top 61.69%) | Memory: 14.1 MB (Top 97.87%)

--- scripts/algorithms/Q/Queries on Number of Points Inside a Circle/Queries on Number of Points Inside a Circle.py
+++ scripts/algorithms/Q/Queries on Number of Points Inside a Circle/Queries on Number of Points Inside a Circle.py
@@ -1,3 +1,4 @@
+# Runtime: 2938 ms (Top 61.69%) | Memory: 14.1 MB (Top 97.87%)
 class Solution:
     def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
         return [sum(math.sqrt((x0-x1)**2 + (y0-y1)**2) <= r for x1, y1 in points) for x0, y0, r in queries]
