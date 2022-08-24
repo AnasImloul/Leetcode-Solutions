2e9dcbe48e4b69cc3fa76726c307f51fea15193b
AnasImloul <pydev27@gmail.com>
2022-08-24 12:22:36 +0100

    Runtime: 23 ms (Top 74.27%) | Memory: 18.7 MB (Top 49.37%)

--- scripts/algorithms/H/H-Index II/H-Index II.cpp
+++ scripts/algorithms/H/H-Index II/H-Index II.cpp
@@ -1,19 +1,20 @@
+// Runtime: 23 ms (Top 74.27%) | Memory: 18.7 MB (Top 49.37%)
 class Solution {
 public:
-	int hIndex(vector<int>& citations) {
-		int start = 0 , end = citations.size()-1;
-		int n = citations.size();
-		while(start <= end){
-			int mid = start + (end - start) / 2;
-			int val = citations[mid];
-			if(val == (n - mid)) return citations[mid];
-			else if(val < n - mid){
-				start = mid + 1;
-			}
-			else{
-				end = mid - 1;
-			}
-		}
-		return n - start;
-	}
+    int hIndex(vector<int>& citations) {
+        int start = 0 , end = citations.size()-1;
+        int n = citations.size();
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            int val = citations[mid];
+            if(val == (n - mid)) return citations[mid];
+            else if(val < n - mid){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return n - start;
+    }
 };
