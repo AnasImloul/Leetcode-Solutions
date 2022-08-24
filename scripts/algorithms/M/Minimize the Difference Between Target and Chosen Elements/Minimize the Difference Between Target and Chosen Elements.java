d85385e530e715c8788e7aa7e42c9d6382b57943
AnasImloul <pydev27@gmail.com>
2022-08-24 14:12:48 +0100

    Runtime: 1407 ms (Top 25.79%) | Memory: 92.6 MB (Top 35.19%)

--- scripts/algorithms/M/Minimize the Difference Between Target and Chosen Elements/Minimize the Difference Between Target and Chosen Elements.java
+++ scripts/algorithms/M/Minimize the Difference Between Target and Chosen Elements/Minimize the Difference Between Target and Chosen Elements.java
@@ -1,9 +1,10 @@
+// Runtime: 1407 ms (Top 25.79%) | Memory: 92.6 MB (Top 35.19%)
 class Solution {
     public int minimizeTheDifference(int[][] mat, int target) {
         Integer[][] dp = new Integer[mat.length][5001];
         return minDiff(mat, 0, target,0, dp);
     }
-    
+
     public int minDiff(int[][] mat,int index,int target, int val, Integer[][] dp){
         if(index == mat.length){
             return Math.abs(val - target);
@@ -11,12 +12,12 @@ class Solution {
         if(dp[index][val] != null){
             return dp[index][val];
         }
-        
+
         int res = Integer.MAX_VALUE;
         for(int i = 0; i < mat[0].length; i++){
             res = Math.min(res, minDiff(mat, index + 1, target, val + mat[index][i], dp));
         }
-        
+
         return dp[index][val] = res;
     }
 }
