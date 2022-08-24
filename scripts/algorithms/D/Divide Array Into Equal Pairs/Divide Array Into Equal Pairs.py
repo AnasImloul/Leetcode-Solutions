38fe0165559cdeeea1d440743488ed74f8093cc2
AnasImloul <pydev27@gmail.com>
2022-08-24 12:22:36 +0100

    Runtime: 159 ms (Top 42.07%) | Memory: 14.2 MB (Top 19.67%)

--- scripts/algorithms/D/Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.py
+++ scripts/algorithms/D/Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.py
@@ -1,7 +1,7 @@
+// Runtime: 159 ms (Top 42.07%) | Memory: 14.2 MB (Top 19.67%)
 class Solution:
 
     def divideArray(self, nums: List[int]) -> bool:
         lena = len(nums)
         count = sum(num//2 for num in Counter(nums).values())
         return (lena/2 == count)
-        
