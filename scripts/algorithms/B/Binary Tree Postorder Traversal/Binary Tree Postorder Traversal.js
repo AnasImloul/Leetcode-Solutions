214e96dfcd1187ca703d0d9bbf9cb1b4ca171332
AnasImloul <pydev27@gmail.com>
2022-08-24 14:25:17 +0100

    Runtime: 72 ms (Top 78.56%) | Memory: 42.1 MB (Top 54.23%)

--- scripts/algorithms/B/Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.js
+++ scripts/algorithms/B/Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.js
@@ -1,3 +1,4 @@
+// Runtime: 72 ms (Top 78.56%) | Memory: 42.1 MB (Top 54.23%)
 class Pair{
     constructor(node, state){
         this.node = node;
@@ -8,12 +9,12 @@ var postorderTraversal = function(root) {
     let ans = [];
     let st = []; // stack
     root != null && st.push(new Pair(root, 1));
-    
+
     while(st.length > 0){
         let top = st[st.length - 1];
-        
+
         if(top.state == 1){
-            
+
             top.state++;
             if(top.node.left != null){
                 st.push(new Pair(top.node.left, 1))
