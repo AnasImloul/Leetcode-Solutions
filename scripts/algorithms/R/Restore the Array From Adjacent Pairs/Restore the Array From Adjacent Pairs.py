f317559169ea7e56ac73921f7989e65041725f91
AnasImloul <pydev27@gmail.com>
2022-08-24 14:31:13 +0100

    Runtime: 2248 ms (Top 15.65%) | Memory: 166 MB (Top 35.87%)

--- scripts/algorithms/R/Restore the Array From Adjacent Pairs/Restore the Array From Adjacent Pairs.py
+++ scripts/algorithms/R/Restore the Array From Adjacent Pairs/Restore the Array From Adjacent Pairs.py
@@ -1,19 +1,20 @@
+// Runtime: 2248 ms (Top 15.65%) | Memory: 166 MB (Top 35.87%)
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-		# create the map 
+        # create the map
         adj = collections.defaultdict(list)
         for a, b in adjacentPairs:
             adj[a].append(b)
             adj[b].append(a)
 
-		# find the start num
+        # find the start num
         start = adjacentPairs[0][0]
         for k, v in adj.items():
             if len(v) ==1:
                 start = k
                 break
-				
-		# dfs to connect the graph
+
+        # dfs to connect the graph
         nums=[]
         seen = set()
         def dfs(num):
@@ -21,7 +22,6 @@ class Solution:
             for next_num in adj[num]:
                 if next_num in seen: continue
                 dfs(next_num)
-            nums.append(num) 
+            nums.append(num)
         dfs(start)
         return nums
-
