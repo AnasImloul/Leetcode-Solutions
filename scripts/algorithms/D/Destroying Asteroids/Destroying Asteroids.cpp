403b7e2e267123fd31def76a398ccfc4a23bb913
AnasImloul <pydev27@gmail.com>
2022-08-24 14:25:23 +0100

    Runtime: 263 ms (Top 83.31%) | Memory: 102.6 MB (Top 92.47%)

--- scripts/algorithms/D/Destroying Asteroids/Destroying Asteroids.cpp
+++ scripts/algorithms/D/Destroying Asteroids/Destroying Asteroids.cpp
@@ -1,15 +1,16 @@
+// Runtime: 263 ms (Top 83.31%) | Memory: 102.6 MB (Top 92.47%)
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        
+
         int n = asteroids.size();
         sort(begin(asteroids), end(asteroids));
-        
+
         long long planetMass = mass;
-        
+
         for(auto& asteroid : asteroids) {
-            if(planetMass < asteroid) return false; 
-			planetMass += asteroid;
+            if(planetMass < asteroid) return false;
+            planetMass += asteroid;
         }
         return true;
     }
