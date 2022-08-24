3851d90b73e9402bec0687a0cbf4c3690948f00b
AnasImloul <pydev27@gmail.com>
2022-08-24 14:12:51 +0100

    Runtime: 10 ms (Top 20.22%) | Memory: 42.9 MB (Top 20.22%)

--- scripts/algorithms/P/Print Words Vertically/Print Words Vertically.java
+++ scripts/algorithms/P/Print Words Vertically/Print Words Vertically.java
@@ -1,9 +1,10 @@
+// Runtime: 10 ms (Top 20.22%) | Memory: 42.9 MB (Top 20.22%)
 class Solution {
     public List<String> printVertically(String s) {
-        s = s.replace(" ",","); 
+        s = s.replace(" ",",");
         String str="";
         List<String> a=new ArrayList<>();
-    
+
         int max=0;
         for(int i =0;i<s.length();i++){
            char ch=s.charAt(i);
@@ -21,21 +22,20 @@ class Solution {
                 continue;
             }
             str+=ch;
-            
+
         }
-        
-        
+
         String [] arr=new String[max];
         for(int i =0;i<max;i++){
             arr[i]="";
-          
+
         }
-        
+
         for(int i =0;i<a.size();i++){
             String x=a.get(i);
-    
+
             for(int j=0;j<max;j++){
-                
+
                 if(j<x.length()){
                     arr[j]+=x.charAt(j);
                 }
@@ -43,22 +43,21 @@ class Solution {
                     arr[j]+=" ";
                 }
             }
-            
+
         }
-        
+
         a=new ArrayList<>();
         for(int i=0;i<arr.length;i++){
-            
-            
+
             String x=arr[i];
             x=trim(x);
-            
+
             a.add(x);
         }
-       
+
         return a;
     }
-    
+
     public String trim(String str) {
         int len = str.length();
         int st = 0;
