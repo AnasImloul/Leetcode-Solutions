4342169449906953ef4e6455089ac69604e2d4bc
AnasImloul <pydev27@gmail.com>
2022-08-24 14:25:22 +0100

    Runtime: 160 ms (Top 49.09%) | Memory: 45.8 MB (Top 67.27%)

--- scripts/algorithms/C/Construct K Palindrome Strings/Construct K Palindrome Strings.js
+++ scripts/algorithms/C/Construct K Palindrome Strings/Construct K Palindrome Strings.js
@@ -1,3 +1,4 @@
+// Runtime: 160 ms (Top 49.09%) | Memory: 45.8 MB (Top 67.27%)
 var canConstruct = function(s, k) {
     if(s.length < k) return false;
     // all even all even
@@ -6,12 +7,12 @@ var canConstruct = function(s, k) {
     for(let c of s) {
         freq[c] = (freq[c] || 0) + 1;
     }
-    
+
     let oddCount = 0;
     const freqOfNums = Object.values(freq);
     for(let cnt of freqOfNums) {
         if(cnt & 1) oddCount++;
     }
-    
+
     return oddCount <= k;
 };
