aca162a2de46c4cbd0ed1b6e03c4f0208955a3fc
AnasImloul <pydev27@gmail.com>
2022-08-24 12:22:37 +0100

    Runtime: 461 ms (Top 6.78%) | Memory: 16.8 MB (Top 51.89%)

--- scripts/algorithms/C/Cherry Pickup II/Cherry Pickup II.cpp
+++ scripts/algorithms/C/Cherry Pickup II/Cherry Pickup II.cpp
@@ -1,10 +1,11 @@
+// Runtime: 461 ms (Top 6.78%) | Memory: 16.8 MB (Top 51.89%)
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m, 0)));
-        
+
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(i==j) dp[n-1][i][j] = grid[n-1][i];
@@ -20,10 +21,10 @@ public:
                             int value = 0;
                             if(j1 == j2) value = grid[i][j2];
                             else value = grid[i][j1] + grid[i][j2];
-                            
-                            if(j1+dj1 >= 0 && j1+dj1 < m && j2 +dj2 >= 0 && j2 +dj2 < m) 
+
+                            if(j1+dj1 >= 0 && j1+dj1 < m && j2 +dj2 >= 0 && j2 +dj2 < m)
                                 value += dp[i+1][j1+dj1][j2 +dj2];
-                            else 
+                            else
                                 value+= -1e8;
                             maxi = max(maxi,value);
                         }
