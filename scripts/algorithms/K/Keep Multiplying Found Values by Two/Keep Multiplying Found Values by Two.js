6b1819c3061305aa217b5915add7305e1c9cd08f
AnasImloul <pydev27@gmail.com>
2022-08-24 14:25:21 +0100

    Runtime: 76 ms (Top 82.37%) | Memory: 42.4 MB (Top 79.69%)

--- scripts/algorithms/K/Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.js
+++ scripts/algorithms/K/Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.js
@@ -1,7 +1,8 @@
+// Runtime: 76 ms (Top 82.37%) | Memory: 42.4 MB (Top 79.69%)
 var findFinalValue = function(nums, original) {
     while (nums.includes(original)) {
         original = original * 2
     }
-    
+
     return original
 };
