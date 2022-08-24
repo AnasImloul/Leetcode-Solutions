729615f8f9b865b44ceb00fab7cdba9fa185a27c
AnasImloul <pydev27@gmail.com>
2022-08-24 14:25:24 +0100

    Runtime: 134 ms (Top 54.42%) | Memory: 52.1 MB (Top 6.15%)

--- scripts/algorithms/I/Intersection of Two Linked Lists/Intersection of Two Linked Lists.js
+++ scripts/algorithms/I/Intersection of Two Linked Lists/Intersection of Two Linked Lists.js
@@ -1,17 +1,18 @@
- 
+// Runtime: 134 ms (Top 54.42%) | Memory: 52.1 MB (Top 6.15%)
+
 var getIntersectionNode = function(headA, headB) {
    let node1 = headA;
    let node2 = headB;
-    
+
     const set = new Set()
-    
+
     while(node1 !== undefined || node2!== undefined){
         if(node1 && set.has(node1)){
             return node1
         }
         if(node2 && set.has(node2)){
             return node2
-        }    
+        }
         if(node1){
             set.add(node1)
             node1 = node1.next;
