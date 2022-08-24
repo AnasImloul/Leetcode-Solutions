b45c73230d1000a2d03933394e5a2d92182c6116
AnasImloul <pydev27@gmail.com>
2022-08-24 12:22:35 +0100

    Runtime: 87 ms (Top 5.05%) | Memory: 15.9 MB (Top 5.33%)

--- scripts/algorithms/L/Lucky Numbers in a Matrix/Lucky Numbers in a Matrix.cpp
+++ scripts/algorithms/L/Lucky Numbers in a Matrix/Lucky Numbers in a Matrix.cpp
@@ -1,22 +1,23 @@
+// Runtime: 87 ms (Top 5.05%) | Memory: 15.9 MB (Top 5.33%)
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        
+
         unordered_map<int,vector<int>>m;
-        
+
         for(int i=0;i<matrix.size();i++){
             vector<int>temp = matrix[i];
             for(int j=0;j<temp.size();j++){
                 m[j].push_back(temp[j]);
             }
         }
-        
+
         unordered_map<int,int>mp;
         for(int i=0;i<matrix.size();i++){
             vector<int>helper = matrix[i];
-            
+
             sort(helper.begin(),helper.end());
-            
+
             mp[helper[0]]++;
         }
         vector<int>result;
