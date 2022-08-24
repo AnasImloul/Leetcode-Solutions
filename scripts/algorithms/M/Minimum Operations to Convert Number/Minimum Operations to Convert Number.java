b71de9de6d9db4a78a87977367335e8e93df6ef4
AnasImloul <pydev27@gmail.com>
2022-08-24 14:12:48 +0100

    Runtime: 907 ms (Top 10.13%) | Memory: 306.2 MB (Top 27.03%)

--- scripts/algorithms/M/Minimum Operations to Convert Number/Minimum Operations to Convert Number.java
+++ scripts/algorithms/M/Minimum Operations to Convert Number/Minimum Operations to Convert Number.java
@@ -1,13 +1,14 @@
+// Runtime: 907 ms (Top 10.13%) | Memory: 306.2 MB (Top 27.03%)
 class Solution {
     public int minimumOperations(int[] nums, int start, int goal) {
         int res = 0;
         Queue<Integer> q = new LinkedList<>();
         Set<Integer> set = new HashSet<>();
         q.offer(start);
-        
+
         while(!q.isEmpty()){
             int size = q.size();
-            
+
             for(int i = 0;i<size;i++){
                 int val = q.poll();
                 if(val == goal)return res;
@@ -22,8 +23,7 @@ class Solution {
             }
             res++;
         }
-        
+
         return -1;
     }
 }
-
