d36505f0cb164bae3bc5f1ceb9782cee35c07453
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:04 +0100

    Runtime: 1233 ms (Top 81.70%) | Memory: 153.5 MB (Top 96.08%)

--- scripts/algorithms/L/Longest Common Subpath/Longest Common Subpath.cpp
+++ scripts/algorithms/L/Longest Common Subpath/Longest Common Subpath.cpp
@@ -1,15 +1,16 @@
+// Runtime: 1233 ms (Top 81.70%) | Memory: 153.5 MB (Top 96.08%)
 class Solution {
-    const long p1 = 1299827;     /// 1299827; 299807; 199807;
+    const long p1 = 1299827; /// 1299827; 299807; 199807;
     const long p2 = 1e9 + 7;
-    
+
     struct Hit { int count, row, col; Hit() {count=row=col=0;}};
-    
+
     int minsize(vector<vector<int>>& paths) {
         int msize=INT_MAX;
         for (auto v : paths) { msize = min(msize,int(v.size())); }
         return msize;
     }
-    
+
     bool collision(vector<vector<int>>& paths,int len,Hit& hit,int row,int col) {
         if (hit.count == 0) return false;
         if (hit.row == row) return true; // what should we do for internal collisions?
@@ -20,20 +21,20 @@ class Solution {
                 return true;
         return false;
     }
-    
+
     bool rabinkarp(vector<vector<int>>& paths,int len) {
         unordered_map<long,Hit> hits;
         long power=1;
-        
+
         for (int i=0; i<len-1; i++)
             power = (power * p1) % p2;
-        
+
         for (int i=0, j=0, found=0; i<paths.size(); i++, j=0, found=0) {
             long hash=0;
-            
+
            while (j < paths[i].size()) {
-                if (j >= len) hash = (hash + p2 - (paths[i][j-len] * power) % p2) % p2;  // remove item
-                hash = (hash * p1 + paths[i][j++]) % p2;                                 // add item
+                if (j >= len) hash = (hash + p2 - (paths[i][j-len] * power) % p2) % p2; // remove item
+                hash = (hash * p1 + paths[i][j++]) % p2; // add item
                 if (j >= len && (i == 0 || hits.count(hash))) {
                     auto& hit = hits[hash];
                     if (! collision(paths,len,hit,i,j-len)) {
@@ -50,15 +51,15 @@ class Solution {
 public:
     int longestCommonSubpath(int n, vector<vector<int>>& paths) {
         int lo=1, hi=minsize(paths), found=0;
-        
+
         while (lo<=hi) {
             int len = lo + (hi-lo)/2;
-            
+
             if (rabinkarp(paths,len)) {
                 found = len;
                 lo = len+1;
             }
-            else 
+            else
                 hi = len-1;
         }
         return found;
