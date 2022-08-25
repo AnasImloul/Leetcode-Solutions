dfd939ab888eba2f3bfde09bd2b711d2c1fa745d
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:39 +0100

    Runtime: 2 ms (Top 81.25%) | Memory: 42.4 MB (Top 81.68%)

--- scripts/algorithms/E/Excel Sheet Column Number/Excel Sheet Column Number.java
+++ scripts/algorithms/E/Excel Sheet Column Number/Excel Sheet Column Number.java
@@ -1,6 +1,7 @@
+// Runtime: 2 ms (Top 81.25%) | Memory: 42.4 MB (Top 81.68%)
 class Solution {
     public int titleToNumber(String columnTitle) {
-        int n = columnTitle.length();   
+        int n = columnTitle.length();
         int pow = 0;
         int res = 0;
         for(int i = n-1; i >= 0; i--) {
@@ -8,7 +9,7 @@ class Solution {
             res += (c - 64) * Math.pow(26, pow);
             pow++;
         }
-        
+
         return res;
     }
 }
