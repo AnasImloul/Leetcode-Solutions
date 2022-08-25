3d94fa4e9adb9fa010d16ff2a34c1f53d2c2c692
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:35 +0100

    Runtime: 735 ms (Top 32.40%) | Memory: 116.4 MB (Top 54.23%)

--- scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.js
+++ scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.js
@@ -1,3 +1,4 @@
+// Runtime: 735 ms (Top 32.40%) | Memory: 116.4 MB (Top 54.23%)
 /**
  * @param {number[][]} matches
  * @return {number[][]}
@@ -6,12 +7,12 @@ var findWinners = function(matches) {
     var looser = {};
     var allPlayer={};
     for(var i=0; i<matches.length; i++)
-        {   
+        {
             if(looser[matches[i][1]])
                 looser[matches[i][1]]++;
             else
                 looser[matches[i][1]]=1
-            
+
             if(!allPlayer[matches[i][0]])
                 allPlayer[matches[i][0]]=1
             if(!allPlayer[matches[i][1]])
