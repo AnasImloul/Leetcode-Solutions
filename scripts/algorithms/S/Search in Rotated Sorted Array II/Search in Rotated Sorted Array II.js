c403469c658e1860494ce58c123880fa6944ff30
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:45 +0100

    Runtime: 61 ms (Top 98.58%) | Memory: 42.3 MB (Top 80.97%)

--- scripts/algorithms/S/Search in Rotated Sorted Array II/Search in Rotated Sorted Array II.js
+++ scripts/algorithms/S/Search in Rotated Sorted Array II/Search in Rotated Sorted Array II.js
@@ -1,6 +1,7 @@
+// Runtime: 61 ms (Top 98.58%) | Memory: 42.3 MB (Top 80.97%)
 var search = function(nums, target) {
     let found = nums.findIndex(c=> c==target);
     if(found === -1) return false
-    else 
+    else
         return true
 };
