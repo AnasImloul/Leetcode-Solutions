ca67220a571a0b3a357c61cf6603ef2721280fd2
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:06 +0100

    Runtime: 360 ms (Top 84.93%) | Memory: 110.1 MB (Top 61.54%)

--- scripts/algorithms/F/Find K Pairs with Smallest Sums/Find K Pairs with Smallest Sums.cpp
+++ scripts/algorithms/F/Find K Pairs with Smallest Sums/Find K Pairs with Smallest Sums.cpp
@@ -1,9 +1,10 @@
+// Runtime: 360 ms (Top 84.93%) | Memory: 110.1 MB (Top 61.54%)
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         priority_queue<pair<int,pair<int,int>>> pq;
         int m=nums1.size(),n=nums2.size();
-        
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int sum=nums1[i]+nums2[j];
@@ -15,15 +16,15 @@ public:
                 }else break;
             }
         }
-        
+
         vector<vector<int>> ans;
         while(!pq.empty()){
             auto p=pq.top().second;
             pq.pop();
-            
+
             ans.push_back({p.first,p.second});
         }
-        
+
         return ans;
     }
 };
