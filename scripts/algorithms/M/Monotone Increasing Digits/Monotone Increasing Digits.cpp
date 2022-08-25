b82f670c616dd414d6ca2102d726cce4ddc00e20
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:03 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 5.9 MB (Top 82.10%)

--- scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.cpp
+++ scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.cpp
@@ -1,17 +1,18 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 5.9 MB (Top 82.10%)
 class Solution {
 public:
     int monotoneIncreasingDigits(int n) {
             if(n < 10) return n;
-            
+
             string s = to_string(n);
-            
+
             for(int i = s.size() - 2; i >= 0; i--) {
                    if(s[i] > s[i+1]) {
                        s[i]--;
                        for(int j = i + 1; j < s.size(); j++) s[j] = '9';
                    }
             }
-        int ans = stoi(s); 
-        return ans;  
+        int ans = stoi(s);
+        return ans;
     }
 };
