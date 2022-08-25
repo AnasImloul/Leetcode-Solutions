e1da255fc74f343c61926f041625ef9bb6b0e1c8
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:02 +0100

    Runtime: 4 ms (Top 30.08%) | Memory: 7.8 MB (Top 65.04%)

--- scripts/algorithms/R/Rectangle Overlap/Rectangle Overlap.cpp
+++ scripts/algorithms/R/Rectangle Overlap/Rectangle Overlap.cpp
@@ -1,9 +1,10 @@
-		class Solution {
-		public:
-			bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
-			{
-			   if(rec2[1]>=rec1[3] || rec1[1]>=rec2[3] || rec1[0]>=rec2[2] || rec2[0]>=rec1[2]) 
-				   return false;
-				return true;
-			}
-		};
+// Runtime: 4 ms (Top 30.08%) | Memory: 7.8 MB (Top 65.04%)
+        class Solution {
+        public:
+            bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
+            {
+               if(rec2[1]>=rec1[3] || rec1[1]>=rec2[3] || rec1[0]>=rec2[2] || rec2[0]>=rec1[2])
+                   return false;
+                return true;
+            }
+        };
