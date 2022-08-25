6a78c6aaebdb508455fa71cb6ee15e1f64059e02
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:41 +0100

    Runtime: 167 ms (Top 9.09%) | Memory: 53.9 MB (Top 27.27%)

--- scripts/algorithms/M/Magical String/Magical String.js
+++ scripts/algorithms/M/Magical String/Magical String.js
@@ -1,13 +1,14 @@
+// Runtime: 167 ms (Top 9.09%) | Memory: 53.9 MB (Top 27.27%)
 var magicalString = function(n) {
-	const stack = ['1', '2', '2'];
-	let magic = 2;
+    const stack = ['1', '2', '2'];
+    let magic = 2;
 
-	while (stack.length < n) {
-		const count = stack[magic++];
-		const last = stack[stack.length - 1];
-		const addStr = last === '1' ? '2' : '1';
+    while (stack.length < n) {
+        const count = stack[magic++];
+        const last = stack[stack.length - 1];
+        const addStr = last === '1' ? '2' : '1';
 
-		for (let n = 1; n <= count; n++) stack.push(addStr);
-	}
-	return stack.slice(0, n).filter(str => str === '1').length;
+        for (let n = 1; n <= count; n++) stack.push(addStr);
+    }
+    return stack.slice(0, n).filter(str => str === '1').length;
 };
