16d50f669999fe1d7a341904cd3767b3378be5b4
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:04 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 70.14%)

--- scripts/algorithms/M/Mirror Reflection/Mirror Reflection.cpp
+++ scripts/algorithms/M/Mirror Reflection/Mirror Reflection.cpp
@@ -1,7 +1,8 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 70.14%)
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        while (p % 2 == 0 && q % 2 == 0){ 
+        while (p % 2 == 0 && q % 2 == 0){
             p/=2;
             q/=2;
         }
