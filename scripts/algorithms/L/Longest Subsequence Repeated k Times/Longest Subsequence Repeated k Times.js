d4e7d91112fc2ce81f67fcc3a49c9ef6e1f677b3
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:38 +0100

    Runtime: 1501 ms (Top 55.56%) | Memory: 58.7 MB (Top 11.11%)

--- scripts/algorithms/L/Longest Subsequence Repeated k Times/Longest Subsequence Repeated k Times.js
+++ scripts/algorithms/L/Longest Subsequence Repeated k Times/Longest Subsequence Repeated k Times.js
@@ -1,3 +1,4 @@
+// Runtime: 1501 ms (Top 55.56%) | Memory: 58.7 MB (Top 11.11%)
 // Idea comes from:
 // https://leetcode.com/problems/longest-subsequence-repeated-k-times/discuss/1471930/Python-Answer-is-not-so-long-explained
 
@@ -7,14 +8,14 @@ var longestSubsequenceRepeatedK = function(s, k) {
     if (!freq[c]) freq[c] = 0;
     freq[c]++;
   }
-  
+
   // Find hot string
   let hot = "";
   for (const [c, cnt] of Object.entries(freq)) {
     const repeat = Math.floor(cnt / k);
     hot += c.repeat(repeat);
   }
-  
+
   // Find all subset and permutation of hot string
   let targets = new Set();
   const subsets = getSubset(hot);
@@ -22,20 +23,20 @@ var longestSubsequenceRepeatedK = function(s, k) {
     const permutations = getPermutation(subset);
     for (const per of permutations) targets.add(per);
   }
-  
+
   // Sort targets by length and lexico
   targets = [...targets].sort((a, b) => {
     const delta = b.length - a.length;
     if (delta) return delta;
     return b.localeCompare(a);
   });
-  
+
   // Filter s and check subsequence
   s = [...s].filter(c => hot.includes(c)).join("");
   for (const tar of targets) {
     if (isSubsequence(s, tar.repeat(k))) return tar;
   }
-  
+
   return "";
 };
 
