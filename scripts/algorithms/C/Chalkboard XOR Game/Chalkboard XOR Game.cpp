6ff76e30e8832890b3630f80f18401f0e32c414d
AnasImloul <pydev27@gmail.com>
2022-08-25 11:39:07 +0100

    Runtime: 19 ms (Top 23.87%) | Memory: 13.1 MB (Top 34.84%)

--- scripts/algorithms/C/Chalkboard XOR Game/Chalkboard XOR Game.cpp
+++ scripts/algorithms/C/Chalkboard XOR Game/Chalkboard XOR Game.cpp
@@ -1,8 +1,9 @@
+// Runtime: 19 ms (Top 23.87%) | Memory: 13.1 MB (Top 34.84%)
 class Solution {
 public:
     bool xorGame(vector<int>& nums) {
-        int val = 0; 
-        for (auto& x : nums) val ^= x; 
-        return val == 0 || size(nums) % 2 == 0; 
+        int val = 0;
+        for (auto& x : nums) val ^= x;
+        return val == 0 || size(nums) % 2 == 0;
     }
 };
