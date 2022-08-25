e30f21ecb9e4a92fac251ab38dbf05318ad41f26
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:40 +0100

    Runtime: 113 ms (Top 42.26%) | Memory: 44.9 MB (Top 34.73%)

--- scripts/algorithms/S/Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.js
+++ scripts/algorithms/S/Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.js
@@ -1,21 +1,22 @@
+// Runtime: 113 ms (Top 42.26%) | Memory: 44.9 MB (Top 34.73%)
 var sumRootToLeaf = function(root) {
-    
+
     let sum = 0;
-    
+
     const getSum = (root, currStr) => {
         if(!root) {
             return;
         }
-        
+
         if(!root.left && !root.right) {
             sum = sum + parseInt(currStr + root.val, 2);
         }
-        
+
         getSum(root.left, '' + currStr + root.val);
         getSum(root.right, '' + currStr + root.val);
     }
-    
+
     getSum(root, '');
-    
+
     return sum;
 };
