30afeb489d580efc4916d4119177a8055a95d84d
AnasImloul <pydev27@gmail.com>
2022-08-25 12:43:12 +0100

    Runtime: 10 ms (Top 88.88%) | Memory: 49.9 MB (Top 6.06%)

--- scripts/algorithms/R/Range Sum Query - Immutable/Range Sum Query - Immutable.java
+++ scripts/algorithms/R/Range Sum Query - Immutable/Range Sum Query - Immutable.java
@@ -1,3 +1,4 @@
+// Runtime: 10 ms (Top 88.88%) | Memory: 49.9 MB (Top 6.06%)
 class NumArray {
     int [] prefix;
     public NumArray(int[] nums) {
@@ -8,7 +9,7 @@ class NumArray {
             prefix[i] = nums[i] + prefix[i - 1];
         }
     }
-    
+
     public int sumRange(int left, int right) {
         if(left == 0){
             return prefix[right];
