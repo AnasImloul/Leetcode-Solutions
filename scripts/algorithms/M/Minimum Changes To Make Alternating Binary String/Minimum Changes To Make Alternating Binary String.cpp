65c1d09e28d490e569d9436f3fb0bb6ae8ea647b
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:40 +0100

    Runtime: 6 ms (Top 63.59%) | Memory: 7 MB (Top 71.84%)

--- scripts/algorithms/M/Minimum Changes To Make Alternating Binary String/Minimum Changes To Make Alternating Binary String.cpp
+++ scripts/algorithms/M/Minimum Changes To Make Alternating Binary String/Minimum Changes To Make Alternating Binary String.cpp
@@ -1,3 +1,4 @@
+// Runtime: 6 ms (Top 63.59%) | Memory: 7 MB (Top 71.84%)
 class Solution {
 public:
     int minOperations(string s) {
@@ -8,14 +9,14 @@ public:
         for(int i = 1; i<s.size(); i+=2){
             if(s[i]!='1') start0++;
         }
-        
+
         for(int i = 0; i<s.size(); i+=2){
             if(s[i]!='1') start1++;
         }
         for(int i = 1; i<s.size(); i+=2){
             if(s[i]!='0') start1++;
         }
-        
+
         return min(start0, start1);
     }
 };
