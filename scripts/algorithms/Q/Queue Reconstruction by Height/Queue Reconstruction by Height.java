74fa1a72c91d64ee0eb412e38a0e0095ed25cd6b
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:38 +0100

    Runtime: 9 ms (Top 81.09%) | Memory: 53.8 MB (Top 84.02%)

--- scripts/algorithms/Q/Queue Reconstruction by Height/Queue Reconstruction by Height.java
+++ scripts/algorithms/Q/Queue Reconstruction by Height/Queue Reconstruction by Height.java
@@ -1,15 +1,16 @@
+// Runtime: 9 ms (Top 81.09%) | Memory: 53.8 MB (Top 84.02%)
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
         List<int[]> result = new ArrayList<>(); //return value
-        
+
         Arrays.sort(people, (a, b) -> {
             int x = Integer.compare(b[0], a[0]);
-            if(x == 0) return Integer.compare(a[1], b[1]); 
+            if(x == 0) return Integer.compare(a[1], b[1]);
             else return x; });
 
         for(int[] p: people)
             result.add(p[1], p);
-        
+
         return result.toArray(new int[people.length][2]);
     }
 }
