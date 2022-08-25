ea3b159363fc23cf208a282de61919de811b2dcc
AnasImloul <pydev27@gmail.com>
2022-08-25 12:43:13 +0100

    Runtime: 64 ms (Top 29.92%) | Memory: 46.6 MB (Top 92.25%)

--- scripts/algorithms/M/Maximal Rectangle/Maximal Rectangle.java
+++ scripts/algorithms/M/Maximal Rectangle/Maximal Rectangle.java
@@ -1,3 +1,4 @@
+// Runtime: 64 ms (Top 29.92%) | Memory: 46.6 MB (Top 92.25%)
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -5,10 +6,10 @@ class Solution {
         for(int i=0; i<n; i++){
             list.add(Character.getNumericValue(matrix[0][i]));
         }
-        
+
         int maxArea = maximumArea(list);
         int m = matrix.length;
-        
+
         for(int i=1; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == '1'){
@@ -27,7 +28,7 @@ class Solution {
         int[] left = new int[n];
         int[] right = new int[n];
         int[] width = new int[n];
-        
+
         for(int i=0; i<n; i++){
             while(!stack1.isEmpty() && heights.get(stack1.peek()) >= heights.get(i))
                 stack1.pop();
@@ -37,7 +38,7 @@ class Solution {
                 left[i] = -1;
             stack1.push(i);
         }
-        
+
         for(int i=n-1; i>=0; i--){
             while(!stack2.isEmpty() && heights.get(stack2.peek()) >= heights.get(i))
                 stack2.pop();
@@ -47,14 +48,14 @@ class Solution {
                 right[i] = n;
             stack2.push(i);
         }
-        
+
         for(int i=0; i<n; i++){
             width[i] = right[i] - left[i] - 1;
         }
         int mxArea = 0;
         for(int i=0; i<n; i++){
             mxArea = Math.max(mxArea, width[i] * heights.get(i));
-        }        
+        }
         return mxArea;
     }
 }
