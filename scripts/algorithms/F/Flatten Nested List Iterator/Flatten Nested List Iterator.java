82b80adc8f40df0c72d0bbe27626f10292ede604
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:40 +0100

    Runtime: 4 ms (Top 77.47%) | Memory: 46.1 MB (Top 81.31%)

--- scripts/algorithms/F/Flatten Nested List Iterator/Flatten Nested List Iterator.java
+++ scripts/algorithms/F/Flatten Nested List Iterator/Flatten Nested List Iterator.java
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 77.47%) | Memory: 46.1 MB (Top 81.31%)
 public class NestedIterator implements Iterator<Integer> {
     List<Integer> list=new ArrayList();
     void flatten(List<NestedInteger> nestedList){
