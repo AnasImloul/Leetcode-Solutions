3669d3752fb8953023083c9ec7747caecac2c90a
AnasImloul <pydev27@gmail.com>
2022-08-25 11:38:59 +0100

    Runtime: 3 ms (Top 36.61%) | Memory: 5.9 MB (Top 34.68%)

--- scripts/algorithms/A/Angle Between Hands of a Clock/Angle Between Hands of a Clock.cpp
+++ scripts/algorithms/A/Angle Between Hands of a Clock/Angle Between Hands of a Clock.cpp
@@ -1,12 +1,13 @@
+// Runtime: 3 ms (Top 36.61%) | Memory: 5.9 MB (Top 34.68%)
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        
+
         double hourAngle = 30*(double(hour) + double(minutes/60.0));
-        
+
         double minuteAngle = 6 * (double)minutes;
-        
+
         return 180 - abs(180 - abs(minuteAngle - hourAngle));
-        
+
     }
 };
