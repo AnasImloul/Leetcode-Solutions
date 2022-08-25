bf64151a4bb0b2f57104d44b4ffea3fc1cd0352d
AnasImloul <pydev27@gmail.com>
2022-08-25 12:21:37 +0100

    Runtime: 59 ms (Top 23.94%) | Memory: 14 MB (Top 15.28%)

--- scripts/algorithms/R/Rearrange Characters to Make Target String/Rearrange Characters to Make Target String.py
+++ scripts/algorithms/R/Rearrange Characters to Make Target String/Rearrange Characters to Make Target String.py
@@ -1,4 +1,5 @@
+// Runtime: 59 ms (Top 23.94%) | Memory: 14 MB (Top 15.28%)
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
-        counter_s = Counter(s)        
+        counter_s = Counter(s)
         return min(counter_s[c] // count for c,count in Counter(target).items())
