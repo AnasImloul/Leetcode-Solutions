7f6c8521b39ec3422c47a988b869e5b7b8c9acb3
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:45 +0100

    Runtime: 1009 ms (Top 33.33%) | Memory: 44.6 MB (Top 55.56%)

--- scripts/algorithms/M/Max Sum of Rectangle No Larger Than K/Max Sum of Rectangle No Larger Than K.js
+++ scripts/algorithms/M/Max Sum of Rectangle No Larger Than K/Max Sum of Rectangle No Larger Than K.js
@@ -1,3 +1,4 @@
+// Runtime: 1009 ms (Top 33.33%) | Memory: 44.6 MB (Top 55.56%)
 /**
  * @param {number[][]} matrix
  * @param {number} k
@@ -5,15 +6,15 @@
  */
 var maxSumSubmatrix = function(matrix, k) {
     if (!matrix.length) return 0;
-    
+
     let n = matrix.length, m = matrix[0].length;
     let sum = new Array(n + 1).fill(0).map(a => new Array(m + 1).fill(0));
     let ans = -Infinity;
-    
+
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= m; j++) {
             sum[i][j] = matrix[i-1][j-1] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
-            for(let x  = 1; x <= i; ++x) {
+            for(let x = 1; x <= i; ++x) {
                 for(let y = 1; y <= j; ++y) {
                     let s = rangeSum(sum, x, y, i, j);
                     if (s <= k) {
