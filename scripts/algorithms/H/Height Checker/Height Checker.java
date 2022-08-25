9c0081b3b87cb9f3f82964003dd677eb3bef5944
AnasImloul <pydev27@gmail.com>
2022-08-25 12:43:11 +0100

    Runtime: 2 ms (Top 76.12%) | Memory: 42 MB (Top 50.36%)

--- scripts/algorithms/H/Height Checker/Height Checker.java
+++ scripts/algorithms/H/Height Checker/Height Checker.java
@@ -1,30 +1,21 @@
+// Runtime: 2 ms (Top 76.12%) | Memory: 42 MB (Top 50.36%)
 class Solution {
     public int heightChecker(int[] heights) {
-      
+
       int[] dupheights = Arrays.copyOfRange(heights , 0 ,heights.length);
-      
+
       Arrays.sort(dupheights);
       int count = 0;
-      
+
       for(int i=0 ; i< heights.length ; i++){
-        
+
         if(heights[i] != dupheights[i]){
           count++;
         }
-        
+
       }
-     
-      
-      
-      
+
       return count;
-      
-        
+
     }
 }
-
-
-
-
-
-
