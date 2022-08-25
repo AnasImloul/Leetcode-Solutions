75ace1c22dd9536cca3c068d64044016648a6262
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:41 +0100

    Runtime: 560 ms (Top 30.97%) | Memory: 88.4 MB (Top 38.43%)

--- scripts/algorithms/M/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.js
+++ scripts/algorithms/M/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.js
@@ -1,16 +1,17 @@
+// Runtime: 560 ms (Top 30.97%) | Memory: 88.4 MB (Top 38.43%)
 var findLength = function(nums1, nums2) {
     let dp = new Array(nums1.length+1).fill(0).map(
         () => new Array(nums2.length+1).fill(0)
-    )    
+    )
     let max = 0;
     for (let i = 0; i < nums1.length; i++) {
         for (let j = 0; j < nums2.length; j++) {
             if (nums1[i] != nums2[j]) {
                 continue;
             }
-            dp[i+1][j+1] = dp[i][j]+1;                        
+            dp[i+1][j+1] = dp[i][j]+1;
             max = Math.max(max, dp[i+1][j+1]);
-        }        
+        }
     }
 
     return max;
