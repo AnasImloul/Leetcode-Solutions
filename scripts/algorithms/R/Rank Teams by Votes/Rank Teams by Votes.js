aaca4c7181531507fd354f31e440e3d1c18a585e
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:44 +0100

    Runtime: 195 ms (Top 7.95%) | Memory: 48.1 MB (Top 21.02%)

--- scripts/algorithms/R/Rank Teams by Votes/Rank Teams by Votes.js
+++ scripts/algorithms/R/Rank Teams by Votes/Rank Teams by Votes.js
@@ -1,3 +1,4 @@
+// Runtime: 195 ms (Top 7.95%) | Memory: 48.1 MB (Top 21.02%)
 var rankTeams = function(votes) {
      if(votes.length == 1)
         return votes[0];
@@ -5,16 +6,16 @@ var rankTeams = function(votes) {
     for(let vote of votes){
         for(let i = 0; i < vote.length;i++){
             if(!(map.has(vote[i]))){
-			//create all the values set as zero
+            //create all the values set as zero
                 map.set(vote[i],Array(vote.length).fill(0))
             }
-            let val = map.get(vote[i])           
+            let val = map.get(vote[i])
             val[i] = val[i] + 1
             map.set(vote[i], val)
         }
     }
-  
-    let obj = [...map.entries()];  //converting as array ["A",[5,0,0]]
+
+    let obj = [...map.entries()]; //converting as array ["A",[5,0,0]]
     obj = obj.sort((a,b) => {
         for(let i = 0; i < a[1].length;i++){
             if(a[1][i] > b[1][i])
@@ -22,7 +23,7 @@ var rankTeams = function(votes) {
             else if(a[1][i] < b[1][i])
                 return 1;
         }
-		// if all chars are in same positon return the value charcode
+        // if all chars are in same positon return the value charcode
         return a[0].charCodeAt(0) - b[0].charCodeAt(0);
     })
     return obj.map(item => item[0]).join('')
