efe336682a08b14eb440f9b47db594cd2d5fb8e9
AnasImloul <pydev27@gmail.com>
2022-08-25 12:43:12 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 48.1 MB (Top 66.96%)

--- scripts/algorithms/S/Sort Array By Parity/Sort Array By Parity.java
+++ scripts/algorithms/S/Sort Array By Parity/Sort Array By Parity.java
@@ -1,9 +1,10 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 48.1 MB (Top 66.96%)
 class Solution {
-        
+
     public int[] sortArrayByParity(int[] nums) {
         int i = 0;
         int j = 0;
-        
+
         while(i < nums.length){
             if(nums[i] % 2 == 1){
                 i++;
@@ -11,12 +12,12 @@ class Solution {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                
+
                 i++;
                 j++;
             }
         }
-        
-        return nums;       
+
+        return nums;
     }
 }
