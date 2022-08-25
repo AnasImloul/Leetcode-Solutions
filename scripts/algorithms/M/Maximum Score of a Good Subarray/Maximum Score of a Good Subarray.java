d3912ea5fc9c65e5592f5f4ddced8267826b474f
AnasImloul <pydev27@gmail.com>
2022-08-25 12:43:10 +0100

    Runtime: 9 ms (Top 69.08%) | Memory: 95.9 MB (Top 37.50%)

--- scripts/algorithms/M/Maximum Score of a Good Subarray/Maximum Score of a Good Subarray.java
+++ scripts/algorithms/M/Maximum Score of a Good Subarray/Maximum Score of a Good Subarray.java
@@ -1,24 +1,25 @@
+// Runtime: 9 ms (Top 69.08%) | Memory: 95.9 MB (Top 37.50%)
 class Solution {
     public int maximumScore(int[] nums, int k) {
         int n = nums.length;
         int i = k - 1, j = k + 1;
         int min = nums[k];
         int ans = min;
-        
+
         while(i >= 0 || j < n) {
             int v1 = 0, v2 = 0;
             int min1 = min, min2 = min;
-            
+
             if(i >= 0) {
                 min1 = Math.min(min, nums[i]);
                 v1 = min1 * (j - i);
             }
-            
+
             if(j < n) {
                 min2 = Math.min(min, nums[j]);
                 v2 = min2 * (j - i);
             }
-            
+
             if(v1 > v2) {
                 --i;
                 ans = Math.max(v1, ans);
@@ -30,7 +31,7 @@ class Solution {
                 min = Math.min(min, min2);
             }
         }
-        
+
         return ans;
     }
 }
