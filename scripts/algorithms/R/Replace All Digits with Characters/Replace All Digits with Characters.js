9795d3e7ceb4d207c90726eedcef7cafbf9ee7de
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:43 +0100

    Runtime: 78 ms (Top 73.93%) | Memory: 42.3 MB (Top 48.93%)

--- scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.js
+++ scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.js
@@ -1,12 +1,13 @@
+// Runtime: 78 ms (Top 73.93%) | Memory: 42.3 MB (Top 48.93%)
 var replaceDigits = function(s) {
     let res = ''
-    for(let i = 0; i < s.length; i++){    
+    for(let i = 0; i < s.length; i++){
         if(i % 2 !== 0){
           res += String.fromCharCode(s[i - 1].charCodeAt() + parseInt(s[i]))
         } else{
             res += s[i]
         }
-        
+
     }
     return res;
 };
