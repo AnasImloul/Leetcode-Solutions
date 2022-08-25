a0530624fa4bd017f6a0c4a070e368b0704edfea
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:43 +0100

    Runtime: 154 ms (Top 97.67%) | Memory: 53.7 MB (Top 79.07%)

--- scripts/algorithms/M/Minimum Speed to Arrive on Time/Minimum Speed to Arrive on Time.js
+++ scripts/algorithms/M/Minimum Speed to Arrive on Time/Minimum Speed to Arrive on Time.js
@@ -1,7 +1,8 @@
+// Runtime: 154 ms (Top 97.67%) | Memory: 53.7 MB (Top 79.07%)
 var minSpeedOnTime = function(dist, hour) {
     let left=1,right=10000000,speed,sum,ans=-1;
     while(left<=right){
-        //We need to return the minimum positive integer speed (in kilometers per hour) that all the trains must travel. So speed will always be an integer ranging from 1 to 10000000 as per the question description. 
+        //We need to return the minimum positive integer speed (in kilometers per hour) that all the trains must travel. So speed will always be an integer ranging from 1 to 10000000 as per the question description.
         speed = left + Math.floor((right-left)/2);
         sum=0;
         for(let i=0;i<dist.length-1;i++){
