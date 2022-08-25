92c76077b2789966e39be726dcdc4b32e65ff39a
AnasImloul <pydev27@gmail.com>
2022-08-25 12:36:45 +0100

    Runtime: 111 ms (Top 46.85%) | Memory: 44.4 MB (Top 73.58%)

--- scripts/algorithms/I/Intersection of Multiple Arrays/Intersection of Multiple Arrays.js
+++ scripts/algorithms/I/Intersection of Multiple Arrays/Intersection of Multiple Arrays.js
@@ -1,12 +1,13 @@
-var intersection = function(nums) {   
-    let set = addToSet(nums[0]); 
+// Runtime: 111 ms (Top 46.85%) | Memory: 44.4 MB (Top 73.58%)
+var intersection = function(nums) {
+    let set = addToSet(nums[0]);
     for(let i=1; i<nums.length; i++) {
         let tempSet = addToSet(nums[i]);
-        for(let key of set) {            
-            if( !tempSet.has(key) ) 
+        for(let key of set) {
+            if( !tempSet.has(key) )
                 set.delete(key);
         }
-    }    
+    }
     return [...set].sort( (a,b) => a-b );
 };
 
