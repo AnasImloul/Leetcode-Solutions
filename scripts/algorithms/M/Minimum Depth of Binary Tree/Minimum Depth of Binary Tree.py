fb52fe98507c00b73732a36621888850e2f252c7
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:53 +0100

    Runtime: 956 ms (Top 32.79%) | Memory: 55 MB (Top 23.11%)

--- scripts/algorithms/M/Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.py
+++ scripts/algorithms/M/Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.py
@@ -1,14 +1,15 @@
+// Runtime: 956 ms (Top 32.79%) | Memory: 55 MB (Top 23.11%)
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
+
         if root.left is None:
             return self.minDepth(root.right) + 1
         if root.right is None:
             return self.minDepth(root.left) + 1
-        
+
         l_ht = self.minDepth(root.left)
         r_ht = self.minDepth(root.right)
-        
+
         return min(l_ht, r_ht) + 1
