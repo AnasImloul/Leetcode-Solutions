94801b0fbc378a6e02c8d3c72dde74217832121f
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:56 +0100

    Runtime: 15 ms (Top 85.31%) | Memory: 17.5 MB (Top 45.64%)

--- scripts/algorithms/P/Populating Next Right Pointers in Each Node II/Populating Next Right Pointers in Each Node II.cpp
+++ scripts/algorithms/P/Populating Next Right Pointers in Each Node II/Populating Next Right Pointers in Each Node II.cpp
@@ -1,3 +1,4 @@
+// Runtime: 15 ms (Top 85.31%) | Memory: 17.5 MB (Top 45.64%)
 // method-1
 class Solution {
 public:
@@ -7,34 +8,34 @@ public:
         queue<Node*> q;
         q.push(root);
         root->next=NULL;
-        
+
         while(!q.empty()){
-            
+
             int size=q.size();
             Node* prev=NULL;
-            
+
             for(int i=0;i<size;i++){
                 Node* temp=q.front();
                 q.pop();
-                
+
                 if(prev)
                     prev->next=temp;
-                
+
                 if(i==size-1){
                     temp->next=NULL;
                 }
-                
+
                 if(temp->left)
                     q.push(temp->left);
-                
+
                 if(temp->right)
                     q.push(temp->right);
-                
+
                 prev=temp;
-                        
+
             }
         }
-        
+
         return root;
     }
 };
