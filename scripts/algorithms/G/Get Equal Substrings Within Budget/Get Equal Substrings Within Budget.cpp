ae5cc5aa7085ddb96f716af24e605c6f77f4022f
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:52 +0100

    Runtime: 9 ms (Top 64.85%) | Memory: 7.7 MB (Top 51.71%)

--- scripts/algorithms/G/Get Equal Substrings Within Budget/Get Equal Substrings Within Budget.cpp
+++ scripts/algorithms/G/Get Equal Substrings Within Budget/Get Equal Substrings Within Budget.cpp
@@ -1,20 +1,21 @@
+// Runtime: 9 ms (Top 64.85%) | Memory: 7.7 MB (Top 51.71%)
 class Solution {
 public:
     int equalSubstring(string s, string t, int maxCost) {
         int l = 0, r = 0, currCost = 0, n = s.length(), maxLen = 0;
-        
+
         while(r < n) {
             currCost += abs(s[r] - t[r]);
             r++;
-            
+
             while(currCost > maxCost) {
                 currCost -= abs(s[l] - t[l]);
                 l++;
             }
-            
+
             maxLen = max(r - l, maxLen);
         }
-        
+
         return maxLen;
     }
 };
