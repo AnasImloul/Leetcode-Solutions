121ad5fcb0e0595717fc90568d5415aed185ac2e
AnasImloul <pydev27@gmail.com>
2022-08-26 14:05:53 +0100

    Runtime: 13 ms (Top 29.67%) | Memory: 225.1 MB (Top 38.94%)

--- scripts/algorithms/R/Rearrange Array Elements by Sign/Rearrange Array Elements by Sign.java
+++ scripts/algorithms/R/Rearrange Array Elements by Sign/Rearrange Array Elements by Sign.java
@@ -1,3 +1,4 @@
+// Runtime: 13 ms (Top 29.67%) | Memory: 225.1 MB (Top 38.94%)
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int[] res = new int[nums.length];
@@ -5,20 +6,18 @@ class Solution {
         int posIdx = -1;
         int minusIdx = -1;
 
-
         for(int i=0;i<nums.length;i++){
             if(i % 2 == 0){
                 posIdx++;
                 while(nums[posIdx] <0 )posIdx++;
-                res[resIdx++] = nums[posIdx]; 
+                res[resIdx++] = nums[posIdx];
             }
             else{
                 minusIdx++;
                 while(nums[minusIdx] > 0 )minusIdx++;
-                res[resIdx++] = nums[minusIdx]; 
+                res[resIdx++] = nums[minusIdx];
             }
         }
-
 
         return res;
     }
