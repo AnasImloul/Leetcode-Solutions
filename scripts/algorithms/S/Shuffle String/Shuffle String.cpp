e668d34b821ada30687ffb7375d14f028f027311
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:55 +0100

    Runtime: 15 ms (Top 49.52%) | Memory: 15.2 MB (Top 43.21%)

--- scripts/algorithms/S/Shuffle String/Shuffle String.cpp
+++ scripts/algorithms/S/Shuffle String/Shuffle String.cpp
@@ -1,13 +1,14 @@
+// Runtime: 15 ms (Top 49.52%) | Memory: 15.2 MB (Top 43.21%)
 class Solution {
 public:
-	string restoreString(string s, vector<int>& indices) {
+    string restoreString(string s, vector<int>& indices) {
 
-		string ans = s;
+        string ans = s;
 
-		for(int i=0 ; i<s.size() ; i++){
-			ans[indices[i]] = s[i];
-		}
+        for(int i=0 ; i<s.size() ; i++){
+            ans[indices[i]] = s[i];
+        }
 
-		return ans;
-	}
+        return ans;
+    }
 };
