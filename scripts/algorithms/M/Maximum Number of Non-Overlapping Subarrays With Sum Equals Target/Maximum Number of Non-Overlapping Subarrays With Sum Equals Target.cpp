8ec13b91c7737be68fbdbbfee5607b532b80874f
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:58 +0100

    Runtime: 392 ms (Top 40.92%) | Memory: 84.2 MB (Top 54.29%)

--- scripts/algorithms/M/Maximum Number of Non-Overlapping Subarrays With Sum Equals Target/Maximum Number of Non-Overlapping Subarrays With Sum Equals Target.cpp
+++ scripts/algorithms/M/Maximum Number of Non-Overlapping Subarrays With Sum Equals Target/Maximum Number of Non-Overlapping Subarrays With Sum Equals Target.cpp
@@ -1,8 +1,9 @@
+// Runtime: 392 ms (Top 40.92%) | Memory: 84.2 MB (Top 54.29%)
 class Solution {
 public:
     unordered_map<int,int> mpp ;
     int maxNonOverlapping(vector<int>& nums, int target) {
-        
+
         int sum = 0 , ways = 0 , prev = INT_MIN ;
         mpp[0] = -1 ;
         for(int i = 0 ; i < nums.size() ; ++i ){
@@ -10,6 +11,6 @@ public:
             if(mpp.find(sum - target) != end(mpp) and mpp[sum-target] >= prev ) ++ways , prev = i ;
             mpp[sum] = i ;
         }
-        return ways  ;
+        return ways ;
     }
 };
