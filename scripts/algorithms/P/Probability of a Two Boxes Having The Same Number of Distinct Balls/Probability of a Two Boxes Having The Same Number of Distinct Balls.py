d26164dc2b9754c9c9d74334c9309aa2ecf30f5e
AnasImloul <pydev27@gmail.com>
2022-08-26 13:54:31 +0100

    Runtime: 53 ms (Top 90.74%) | Memory: 14.1 MB (Top 53.70%)

--- scripts/algorithms/P/Probability of a Two Boxes Having The Same Number of Distinct Balls/Probability of a Two Boxes Having The Same Number of Distinct Balls.py
+++ scripts/algorithms/P/Probability of a Two Boxes Having The Same Number of Distinct Balls/Probability of a Two Boxes Having The Same Number of Distinct Balls.py
@@ -1,29 +1,30 @@
+// Runtime: 53 ms (Top 90.74%) | Memory: 14.1 MB (Top 53.70%)
 class Solution:
     def getProbability(self, balls: List[int]) -> float:
         m = len(balls)
         N = sum(balls)
         n = N // 2
-        
+
         prefix = [0] * m
         for i in range(m):
             prefix[i] = prefix[i-1] + balls[i]
-        
+
         # STEP 1: Compute the number of ways to pick j balls from i total, i.e. C(i, j)
         choose = [[0] * (N + 1) for i in range(N + 1)]
         choose[0][0] = 1
         for i in range(1, N + 1):
             for pick in range(N + 1):
-                # DECISION 1: don't pick the ith ball 
+                # DECISION 1: don't pick the ith ball
                 choose[i][pick] += choose[i - 1][pick]
                 # DECISION 2: pick the ith ball
                 choose[i][pick] += choose[i - 1][pick - 1]
-        
+
         # STEP 2: From first i ball types, compute ways to:
-        # - pick c1 balls in box1 
+        # - pick c1 balls in box1
         # - such that the difference in unique ball count between box1 and box2 is d
         ways = [[defaultdict(int) for k in range(n + 1)] for i in range(m + 1)]
         ways[0][0][0] = 1
-        
+
         for i in range(m):
             b = balls[i]
             if i == 0:
@@ -47,9 +48,8 @@ class Solution:
                             delta = -1
                         else:
                             delta = 0
-                        ways_to_add = choose[b][add1] * ways[i][c1][d] 
+                        ways_to_add = choose[b][add1] * ways[i][c1][d]
                         ways[i + 1][c1 + add1][d + delta] += ways_to_add
-        
-        
+
         # compute the actual probability
         return ways[m][n][0] / choose[N][n]
