6e92b7e9aaeeac7447f6b68435a2c84e00338a2b
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:58 +0100

    Runtime: 310 ms (Top 93.88%) | Memory: 61.5 MB (Top 78.57%)

--- scripts/algorithms/M/Minimum Number of People to Teach/Minimum Number of People to Teach.cpp
+++ scripts/algorithms/M/Minimum Number of People to Teach/Minimum Number of People to Teach.cpp
@@ -1,3 +1,4 @@
+// Runtime: 310 ms (Top 93.88%) | Memory: 61.5 MB (Top 78.57%)
 class Solution {
 public:
     bool checkinvalid( int a, int b, vector<vector<int>>& languages){ //helper func., checks if a and b can communicate or not
@@ -7,8 +8,8 @@ public:
         }
         return true;
     }
-    
-    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {        
+
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
         int m= languages.size();
         vector<vector<int>> g(m+1, vector<int>(0)); // graph to store adjacency list with people as nodes & friendships as edges
         // create graph
@@ -16,9 +17,9 @@ public:
             g[friendships[i][0]].push_back(friendships[i][1]);
             g[friendships[i][1]].push_back(friendships[i][0]);
         }
-        
+
         vector<int> voters(m+1,0); // to store people who cant communicate with atleast a friend
-        int voterscount=0; 
+        int voterscount=0;
         for( int i=1;i<m+1;i++){
              for( int j=0;j<g[i].size();j++){
                 if( checkinvalid(i, g[i][j], languages)){ // ith can vote
@@ -28,14 +29,14 @@ public:
                 }
             }
         }
-        unordered_map<int,int> mappy; //  language -> votes
-        int maxvotes=0; 
-        
+        unordered_map<int,int> mappy; // language -> votes
+        int maxvotes=0;
+
         for( int i=1;i<m+1;i++){
-            if(voters[i]==1){ 
+            if(voters[i]==1){
                 for( int j: languages[i-1]){
                     mappy[j]++;
-                    
+
                     if(maxvotes<mappy[j]){
                         maxvotes= mappy[j];
                     }
