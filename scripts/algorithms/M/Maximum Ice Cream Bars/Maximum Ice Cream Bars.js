9b447e10ce0f5ec8f1b8b9a0875aa372aa2c13e8
AnasImloul <pydev27@gmail.com>
2022-08-26 13:54:32 +0100

    Runtime: 421 ms (Top 13.84%) | Memory: 55.5 MB (Top 89.23%)

--- scripts/algorithms/M/Maximum Ice Cream Bars/Maximum Ice Cream Bars.js
+++ scripts/algorithms/M/Maximum Ice Cream Bars/Maximum Ice Cream Bars.js
@@ -1,13 +1,14 @@
+// Runtime: 421 ms (Top 13.84%) | Memory: 55.5 MB (Top 89.23%)
 var maxIceCream = function(costs, coins) {
-  costs.sort((a, b) => a - b); 
+  costs.sort((a, b) => a - b);
   let count = 0;
-  
+
   for (let i = 0; i < costs.length; i++) {
     if (costs[i] <= coins) {
       count++;
       coins -= costs[i]
     } else {
-      break;  // a small optimization, end the loop early if coins go down to zero before we reach end of the length of costs.
+      break; // a small optimization, end the loop early if coins go down to zero before we reach end of the length of costs.
     }
   }
   return count;
