ca71897adf2a0132e966e21386a030dd35ba38e2
AnasImloul <pydev27@gmail.com>
2022-08-26 13:54:30 +0100

    Runtime: 617 ms (Top 12.20%) | Memory: 63.7 MB (Top 39.02%)

--- scripts/algorithms/D/Design an ATM Machine/Design an ATM Machine.js
+++ scripts/algorithms/D/Design an ATM Machine/Design an ATM Machine.js
@@ -1,21 +1,22 @@
+// Runtime: 617 ms (Top 12.20%) | Memory: 63.7 MB (Top 39.02%)
 
 var ATM = function() {
     this.bankNotes = new Array(5).fill(0)
     this.banksNotesValue = [20, 50, 100, 200, 500]
 };
 
-/** 
+/**
  * @param {number[]} banknotesCount
  * @return {void}
  */
 ATM.prototype.deposit = function(banknotesCount) {
     for (let i = 0; i < 5; i++) {
-       this.bankNotes[i] += banknotesCount[i] 
+       this.bankNotes[i] += banknotesCount[i]
     }
     return this.bankNotes
 };
 
-/** 
+/**
  * @param {number} amount
  * @return {number[]}
  */
@@ -29,11 +30,11 @@ ATM.prototype.withdraw = function(amount) {
             const maxCanUse = Math.min(temp[i], bankNote)
             usedBankNotes[i] = maxCanUse
             temp[i] -= maxCanUse
-            
+
             remain -= maxCanUse * this.banksNotesValue[i]
         }
     }
-    
+
     if (remain > 0) {
         return [-1]
     } else {
@@ -42,7 +43,7 @@ ATM.prototype.withdraw = function(amount) {
     }
 };
 
-/** 
+/**
  * Your ATM object will be instantiated and called as such:
  * var obj = new ATM()
  * obj.deposit(banknotesCount)
