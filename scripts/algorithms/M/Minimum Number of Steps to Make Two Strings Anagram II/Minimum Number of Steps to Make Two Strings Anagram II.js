78b86cbb68a99c6ff4bf53eb42c3a808d107b512
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:55 +0100

    Runtime: 158 ms (Top 82.00%) | Memory: 53.8 MB (Top 52.00%)

--- scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram II/Minimum Number of Steps to Make Two Strings Anagram II.js
+++ scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram II/Minimum Number of Steps to Make Two Strings Anagram II.js
@@ -1,3 +1,4 @@
+// Runtime: 158 ms (Top 82.00%) | Memory: 53.8 MB (Top 52.00%)
 var minSteps = function(s, t) {
   let sFreq = Array(26).fill(0), tFreq = Array(26).fill(0);
   for (let char of s) sFreq[char.charCodeAt() - 97]++;
