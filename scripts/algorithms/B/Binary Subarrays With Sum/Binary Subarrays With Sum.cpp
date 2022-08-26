23a329b9758060797aa39105222ae466b962f2d0
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:54 +0100

    Runtime: 42 ms (Top 86.81%) | Memory: 28.8 MB (Top 72.20%)

--- scripts/algorithms/B/Binary Subarrays With Sum/Binary Subarrays With Sum.cpp
+++ scripts/algorithms/B/Binary Subarrays With Sum/Binary Subarrays With Sum.cpp
@@ -1,34 +1,35 @@
+// Runtime: 42 ms (Top 86.81%) | Memory: 28.8 MB (Top 72.20%)
 class Solution {
 public:
 
-	int lessequal(vector<int>& nums, int goal) {
+    int lessequal(vector<int>& nums, int goal) {
 
-		if(goal < 0) return 0;
+        if(goal < 0) return 0;
 
-		int n = nums.size();
-		int i = 0, j = 0;
-		int sum = 0;
-		int ans = 0;
+        int n = nums.size();
+        int i = 0, j = 0;
+        int sum = 0;
+        int ans = 0;
 
-		while(j < n) {
+        while(j < n) {
 
-			sum += nums[j];
+            sum += nums[j];
 
-			while(sum > goal) {
-				sum -= nums[i];
-				i++;
-			}    
-			ans += j-i+1;           
-			j++;  
-		}
+            while(sum > goal) {
+                sum -= nums[i];
+                i++;
+            }
+            ans += j-i+1;
+            j++;
+        }
 
-		return ans;    
-	}
+        return ans;
+    }
 
-	int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-		return lessequal(nums, goal) - lessequal(nums, goal - 1);
-	}
+        return lessequal(nums, goal) - lessequal(nums, goal - 1);
+    }
 };
 
 // our function is giving number of subarrays with sum less or equal to goal.
