da6581bcfeba77ea43fb48dd03a68d8214a83697
AnasImloul <pydev27@gmail.com>
2022-08-26 13:49:10 +0100

    Runtime: 7032 ms (Top 5.01%) | Memory: 643 MB (Top 5.03%)

--- scripts/algorithms/V/Valid Palindrome II/Valid Palindrome II.py
+++ scripts/algorithms/V/Valid Palindrome II/Valid Palindrome II.py
@@ -1,12 +1,13 @@
+// Runtime: 7032 ms (Top 5.01%) | Memory: 643 MB (Top 5.03%)
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         has_deleted = False
-        
+
         def compare(s, has_deleted):
-            
+
             if len(s) <= 1:
                 return True
-            
+
             if s[0] == s[-1]:
                 return compare(s[1:-1], has_deleted)
             else:
@@ -14,5 +15,5 @@ class Solution:
                     return compare(s[1:], True) or compare(s[:-1], True)
                 else:
                     return False
-        
+
         return compare(s, has_deleted)
