1ad8ef4eed276fa3eff1f59a7aa90a57faca11f4
AnasImloul <pydev27@gmail.com>
2022-08-26 14:05:53 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 39.1 MB (Top 94.89%)

--- scripts/algorithms/C/Calculate Money in Leetcode Bank/Calculate Money in Leetcode Bank.java
+++ scripts/algorithms/C/Calculate Money in Leetcode Bank/Calculate Money in Leetcode Bank.java
@@ -1,8 +1,9 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 39.1 MB (Top 94.89%)
 class Solution {
     public int totalMoney(int n) {
-        int m=n/7;  //(no.of full weeks)
-        // first week  1 2 3 4 5 6 7 (sum is 28 i.e. 7*(i+3) if i=1)
-        // second week  2 3 4 5 6 7 8 (sum is 35 i.e. 7*(i+3) if i=2)
+        int m=n/7; //(no.of full weeks)
+        // first week 1 2 3 4 5 6 7 (sum is 28 i.e. 7*(i+3) if i=1)
+        // second week 2 3 4 5 6 7 8 (sum is 35 i.e. 7*(i+3) if i=2)
         //.... so on
         int res=0; //for result
         //calculating full weeks
