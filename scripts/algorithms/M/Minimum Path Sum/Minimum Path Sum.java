d55df2f8a50fea4980c1bc684174f8cf9ab05e70
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:59 +0100

    Runtime: 2 ms (Top 89.53%) | Memory: 45.9 MB (Top 59.56%)

--- scripts/algorithms/M/Minimum Path Sum/Minimum Path Sum.java
+++ scripts/algorithms/M/Minimum Path Sum/Minimum Path Sum.java
@@ -1,14 +1,15 @@
+// Runtime: 2 ms (Top 89.53%) | Memory: 45.9 MB (Top 59.56%)
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[] dp = new int[n];
         dp[0] = grid[0][0];
-        
+
         for(int i=1;i<n;i++){
             dp[i] = dp[i-1]+grid[0][i];
         }
-        
+
         for(int i=1;i<m;i++){
              for(int j=0;j<n;j++){
                  if(j == 0)
@@ -17,7 +18,7 @@ class Solution {
                      dp[j] = Math.min(dp[j],dp[j-1])+grid[i][j];
             }
         }
-        
+
         return dp[n-1];
     }
 }
