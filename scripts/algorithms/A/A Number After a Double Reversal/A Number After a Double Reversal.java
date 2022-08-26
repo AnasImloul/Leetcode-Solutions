1e656729625a75204ab9cd8d3f28500816fc5cce
AnasImloul <pydev27@gmail.com>
2022-08-26 14:05:48 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 38.6 MB (Top 100.00%)

--- scripts/algorithms/A/A Number After a Double Reversal/A Number After a Double Reversal.java
+++ scripts/algorithms/A/A Number After a Double Reversal/A Number After a Double Reversal.java
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 38.6 MB (Top 100.00%)
 class Solution {
     public boolean isSameAfterReversals(int num) {
         return num == 0 || num % 10 > 0; // All you have to do is check the Trailing zeros
