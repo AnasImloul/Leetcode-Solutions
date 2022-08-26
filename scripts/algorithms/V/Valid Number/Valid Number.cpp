f55287760ae6ee77eaabd3aecbcadfaafe0e2a81
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:53 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 94.26%)

--- scripts/algorithms/V/Valid Number/Valid Number.cpp
+++ scripts/algorithms/V/Valid Number/Valid Number.cpp
@@ -1,9 +1,10 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 94.26%)
 /*
 Max Possible combination of characters in the string has followig parts(stages) :
-            +/-  number  .  number  e/E  +/-  number 
-stages:  0   1     2     3     4     5    6     7
+            +/- number . number e/E +/- number
+stages: 0 1 2 3 4 5 6 7
 
-Now check each characters at there correct stages or not and increament the stage 
+Now check each characters at there correct stages or not and increament the stage
 as per the character found at ith position.
 
 */
@@ -15,10 +16,10 @@ public:
         for(int i = 0; i<s.size(); ++i){
             if( (s[i] == '+' || s[i] == '-') && (stage == 0 || stage == 5)){ stage++; }
             else if((s[i] == 'e' || s[i] == 'E') && stage > 1 && stage < 5){ stage = 5; }
-            else if(s[i] == '.' && stage < 3) { 
+            else if(s[i] == '.' && stage < 3) {
                 //both side of '.' do not have any digit then return false
                 if(stage <= 1 && ( i + 1 >= s.size() || !(s[i+1] >= '0' && s[i+1] <= '9')) ) return false;
-                stage = 3; 
+                stage = 3;
             }else if(s[i] >= '0' && s[i] <= '9'){
                 if(!(stage == 2 || stage == 4 || stage == 7) ) stage++;
                 if(stage == 1 || stage == 6 ) stage++;
