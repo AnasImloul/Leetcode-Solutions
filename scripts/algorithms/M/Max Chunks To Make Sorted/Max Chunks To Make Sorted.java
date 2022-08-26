c4eb1d7732b1053902b254ea16a0f4abb5e9d81e
AnasImloul <pydev27@gmail.com>
2022-08-26 14:05:48 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40.9 MB (Top 81.61%)

--- scripts/algorithms/M/Max Chunks To Make Sorted/Max Chunks To Make Sorted.java
+++ scripts/algorithms/M/Max Chunks To Make Sorted/Max Chunks To Make Sorted.java
@@ -1,6 +1,7 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40.9 MB (Top 81.61%)
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        
+
         int max=0, count=0;
         for(int i=0; i<arr.length; i++){
             max = Math.max(arr[i],max);
