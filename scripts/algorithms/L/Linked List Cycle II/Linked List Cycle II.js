c55786f4c708b685067a1b874bec8584dac69198
AnasImloul <pydev27@gmail.com>
2022-08-26 13:49:01 +0100

    Runtime: 94 ms (Top 78.70%) | Memory: 44.9 MB (Top 73.58%)

--- scripts/algorithms/L/Linked List Cycle II/Linked List Cycle II.js
+++ scripts/algorithms/L/Linked List Cycle II/Linked List Cycle II.js
@@ -1,23 +1,24 @@
+// Runtime: 94 ms (Top 78.70%) | Memory: 44.9 MB (Top 73.58%)
 var detectCycle = function(head) {
     let slowRunner = head
     let fastRunner = head
-    
+
     while(fastRunner) {
         fastRunner = fastRunner.next?.next
         slowRunner = slowRunner.next
-        
-        if (fastRunner === slowRunner) {            
+
+        if (fastRunner === slowRunner) {
             fastRunner = slowRunner
             slowRunner = head
-            
+
             while (slowRunner !== fastRunner) {
                 slowRunner = slowRunner.next
                 fastRunner = fastRunner.next
             }
-                
+
             return slowRunner
         }
     }
-    
+
     return null
 };
