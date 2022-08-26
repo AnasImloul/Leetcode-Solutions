4d1e28bc098b4923a0d74a0a1755317edbadfe65
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:55 +0100

    Runtime: 16 ms (Top 47.54%) | Memory: 49 MB (Top 60.10%)

--- scripts/algorithms/C/Count Binary Substrings/Count Binary Substrings.java
+++ scripts/algorithms/C/Count Binary Substrings/Count Binary Substrings.java
@@ -1,15 +1,16 @@
-class Solution 
+// Runtime: 16 ms (Top 47.54%) | Memory: 49 MB (Top 60.10%)
+class Solution
 {
     public int countBinarySubstrings(String s)
     {
         int i , prevRunLength = 0 , curRunLength = 1 , count = 0 ;
-        for ( i = 1 ; i < s.length() ; i++ ) 
+        for ( i = 1 ; i < s.length() ; i++ )
         {
             if( s.charAt(i) == s.charAt( i - 1 ) )
             {
                 curRunLength++;
             }
-            else 
+            else
             {
                 prevRunLength = curRunLength;
                 curRunLength = 1;
@@ -19,6 +20,6 @@ class Solution
                 count++ ;
             }
         }
-        return count ;        
+        return count ;
     }
 }
