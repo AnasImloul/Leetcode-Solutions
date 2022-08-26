476b184a5bd7b84aed144c0c43ceb2e0f71b7713
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:54 +0100

    Runtime: 39 ms (Top 34.86%) | Memory: 18.3 MB (Top 20.63%)

--- scripts/algorithms/W/Wiggle Sort II/Wiggle Sort II.cpp
+++ scripts/algorithms/W/Wiggle Sort II/Wiggle Sort II.cpp
@@ -1,15 +1,16 @@
+// Runtime: 39 ms (Top 34.86%) | Memory: 18.3 MB (Top 20.63%)
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-       
+
         priority_queue<int> pq;
         for(auto &i : nums)
-            pq.push(i);        
-        
+            pq.push(i);
+
         for(int i = 1; i < nums.size(); i += 2)
             nums[i] = pq.top(), pq.pop();
-        
+
         for(int i = 0; i < nums.size(); i += 2)
-            nums[i] = pq.top(), pq.pop();        
+            nums[i] = pq.top(), pq.pop();
     }
 };
