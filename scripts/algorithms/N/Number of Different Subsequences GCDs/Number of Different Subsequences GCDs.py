7ec8dc696a4d4ac50d11d0b327251408b939436f
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:55 +0100

    Runtime: 5748 ms (Top 75.00%) | Memory: 34.4 MB (Top 21.43%)

--- scripts/algorithms/N/Number of Different Subsequences GCDs/Number of Different Subsequences GCDs.py
+++ scripts/algorithms/N/Number of Different Subsequences GCDs/Number of Different Subsequences GCDs.py
@@ -1,16 +1,17 @@
+// Runtime: 5748 ms (Top 75.00%) | Memory: 34.4 MB (Top 21.43%)
 import math
 
 class Solution:
     def countDifferentSubsequenceGCDs(self, nums: List[int]) -> int:
         max_n = max(nums) + 1
         seen = set(nums)
-        
+
         ans = 0
         for k in range(1, max_n): # iterate candidate k
             gcd = 0
             for multiple in range(k, max_n, k): # compute gcd of all array multiples of k
                 if multiple in seen:
                     gcd = math.gcd(gcd, multiple)
-            if gcd == k: # check the candidate 
+            if gcd == k: # check the candidate
                 ans += 1
         return ans
