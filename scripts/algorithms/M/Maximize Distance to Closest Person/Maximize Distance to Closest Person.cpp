5bb86486a4081504a2ee3d1a5ca39a8ff7e2cd02
AnasImloul <pydev27@gmail.com>
2022-08-26 13:28:52 +0100

    Runtime: 7 ms (Top 99.28%) | Memory: 17.6 MB (Top 26.54%)

--- scripts/algorithms/M/Maximize Distance to Closest Person/Maximize Distance to Closest Person.cpp
+++ scripts/algorithms/M/Maximize Distance to Closest Person/Maximize Distance to Closest Person.cpp
@@ -1,20 +1,21 @@
+// Runtime: 7 ms (Top 99.28%) | Memory: 17.6 MB (Top 26.54%)
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
         vector<int> d;
         int cnt = -1, ans = 0;
-        
+
         for(int i=0; i<seats.size(); i++) {
             cnt++;
             if(seats[i]) d.push_back(cnt), cnt = 0;
         }
         d.push_back(cnt);
-        
+
         for(int i=0; i<d.size(); i++) {
             if(i > 0 && i < d.size() - 1) d[i] /= 2;
             ans = max(ans, d[i]);
         }
-        
+
         return ans;
     }
 };
