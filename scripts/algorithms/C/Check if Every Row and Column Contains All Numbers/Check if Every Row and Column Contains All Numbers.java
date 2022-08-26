81283a34b54cc7dc69502ea113b9ec8a62c13f7c
AnasImloul <pydev27@gmail.com>
2022-08-26 13:48:51 +0100

    Runtime: 56 ms (Top 50.77%) | Memory: 92.4 MB (Top 46.57%)

--- scripts/algorithms/C/Check if Every Row and Column Contains All Numbers/Check if Every Row and Column Contains All Numbers.java
+++ scripts/algorithms/C/Check if Every Row and Column Contains All Numbers/Check if Every Row and Column Contains All Numbers.java
@@ -1,15 +1,16 @@
+// Runtime: 56 ms (Top 50.77%) | Memory: 92.4 MB (Top 46.57%)
 class Solution {
     public boolean checkValid(int[][] matrix) {
         int n = matrix.length;
-        int num = (n*(n+1))/2;  // SUM of n number 1 to n;
-        
+        int num = (n*(n+1))/2; // SUM of n number 1 to n;
+
         for(int i=0; i<n; i++)
         {
             HashSet<Integer> hs = new HashSet<Integer>();
               HashSet<Integer> hs1 = new HashSet<Integer>();
-            
+
             int m = num; int k = num;
-            
+
             for(int j = 0; j<n; j++)
             {
                 hs.add(matrix[i][j]);
@@ -17,7 +18,7 @@ class Solution {
                 m -= matrix[i][j];
                 k -= matrix[j][i];
             }
-            
+
             if(m != 0 || k != 0 || hs.size() != n || hs.size() != n)
             {
                 return false;
