88384e8ca6a25e814aeafa4d9283c50efc0e9595
AnasImloul <pydev27@gmail.com>
2022-08-27 11:12:11 +0100

    Runtime: 121 ms (Top 6.20%) | Memory: 43.8 MB (Top 29.46%)

--- scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.js
+++ scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.js
@@ -1,23 +1,24 @@
+// Runtime: 121 ms (Top 6.20%) | Memory: 43.8 MB (Top 29.46%)
 var MapSum = function() {
-	this.hashKeys = new Map();
-	this.hashVals = new Map();
+    this.hashKeys = new Map();
+    this.hashVals = new Map();
 };
 
 MapSum.prototype.insert = function(key, val) {
-	let prefix = '';
-	for (const str of key) {
-		prefix += str;
-		const hashKey = this.hashKeys.get(prefix) ?? new Set();
-		hashKey.add(key);
-		!this.hashKeys.has(prefix) && this.hashKeys.set(prefix, hashKey);
-	}
-	this.hashVals.set(key, val);
+    let prefix = '';
+    for (const str of key) {
+        prefix += str;
+        const hashKey = this.hashKeys.get(prefix) ?? new Set();
+        hashKey.add(key);
+        !this.hashKeys.has(prefix) && this.hashKeys.set(prefix, hashKey);
+    }
+    this.hashVals.set(key, val);
 };
 
 MapSum.prototype.sum = function(prefix) {
-	const hashKey = this.hashKeys.get(prefix);
-	if (!hashKey) return 0;
-	let sum = 0;
-	hashKey.forEach(key => sum += this.hashVals.get(key));
-	return sum;
+    const hashKey = this.hashKeys.get(prefix);
+    if (!hashKey) return 0;
+    let sum = 0;
+    hashKey.forEach(key => sum += this.hashVals.get(key));
+    return sum;
 };
