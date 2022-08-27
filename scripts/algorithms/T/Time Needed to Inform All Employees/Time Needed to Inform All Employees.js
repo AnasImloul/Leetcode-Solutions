f29f0c198e6dcf701dc9db6c87f9fc4d24447f24
AnasImloul <pydev27@gmail.com>
2022-08-27 11:12:13 +0100

    Runtime: 742 ms (Top 20.63%) | Memory: 110.7 MB (Top 51.35%)

--- scripts/algorithms/T/Time Needed to Inform All Employees/Time Needed to Inform All Employees.js
+++ scripts/algorithms/T/Time Needed to Inform All Employees/Time Needed to Inform All Employees.js
@@ -1,11 +1,12 @@
+// Runtime: 742 ms (Top 20.63%) | Memory: 110.7 MB (Top 51.35%)
 var numOfMinutes = function(n, headID, manager, informTime) {
-    
-//  Build the tree structure
+
+// Build the tree structure
     let tree = {}
-    
-//  {manager: direct employee}
+
+// {manager: direct employee}
     for (let i = 0 ; i < manager.length ; i++){
-//      the head of the company does not have a manager
+// the head of the company does not have a manager
         if (i === headID){
             continue
         }
@@ -16,12 +17,11 @@ var numOfMinutes = function(n, headID, manager, informTime) {
         tree[m].push(i)
     }
 
-    
-//     BFS
-//     [employee, time to inform the head]
+// BFS
+// [employee, time to inform the head]
     let queue = [[headID, 0]]
     let res = 0
-    
+
     while (queue.length){
         let [emp, currTime] = queue.shift()
         let children = tree[emp] || []
@@ -30,6 +30,6 @@ var numOfMinutes = function(n, headID, manager, informTime) {
             queue.push([child, informTime[emp] + currTime])
         }
     }
-    
+
     return res
 };
