726796bef55b3132eba1f65b89bf2e97ce33233c
AnasImloul <pydev27@gmail.com>
2022-08-27 11:35:24 +0100

    Runtime: 16 ms (Top 64.38%) | Memory: 54.3 MB (Top 67.83%)

--- scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.java
+++ scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.java
@@ -1,3 +1,4 @@
+// Runtime: 16 ms (Top 64.38%) | Memory: 54.3 MB (Top 67.83%)
 //find the frequency of every letter and check diffrence between the frequency of each letter then divide it by 2 to calculate the minimum number of letter to be changed.
 class Solution {
     public int minSteps(String s, String t) {
