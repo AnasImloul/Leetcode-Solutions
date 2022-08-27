25049bc87ed2dc669963954b65973227efe3050c
AnasImloul <pydev27@gmail.com>
2022-08-27 10:50:17 +0100

    Runtime: 4 ms (Top 25.64%) | Memory: 6.2 MB (Top 18.83%)

--- scripts/algorithms/F/First Letter to Appear Twice/First Letter to Appear Twice.cpp
+++ scripts/algorithms/F/First Letter to Appear Twice/First Letter to Appear Twice.cpp
@@ -1,14 +1,15 @@
-class Solution 
+// Runtime: 4 ms (Top 25.64%) | Memory: 6.2 MB (Top 18.83%)
+class Solution
 {
 public:
-    char repeatedCharacter(string s) 
+    char repeatedCharacter(string s)
     {
         unordered_map<char, int> mp; //for storing occurrences of char
-        
+
         char ans;
         for(auto it:s)
         {
-            if(mp.find(it) != mp.end()) //any char which comes twice first will be the ans; 
+            if(mp.find(it) != mp.end()) //any char which comes twice first will be the ans;
             {
                 ans = it;
                 break;
