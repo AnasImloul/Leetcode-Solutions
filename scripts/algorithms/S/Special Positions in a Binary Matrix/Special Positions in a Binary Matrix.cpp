05d1214e4a059cc5107229f47d454ae96865a7db
AnasImloul <pydev27@gmail.com>
2022-08-27 10:50:20 +0100

    Runtime: 67 ms (Top 10.18%) | Memory: 18.5 MB (Top 5.36%)

--- scripts/algorithms/S/Special Positions in a Binary Matrix/Special Positions in a Binary Matrix.cpp
+++ scripts/algorithms/S/Special Positions in a Binary Matrix/Special Positions in a Binary Matrix.cpp
@@ -1,12 +1,13 @@
+// Runtime: 67 ms (Top 10.18%) | Memory: 18.5 MB (Top 5.36%)
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
         vector<vector<int>>v;
         map<int,vector<int>>m;
-        
+
         for(int i=0;i<mat.size();i++){
             vector<int>temp = mat[i];
-            
+
             for(int j=0;j<temp.size();j++){
                 m[j].push_back(temp[j]);
             }
