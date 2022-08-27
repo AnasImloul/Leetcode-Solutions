851e3ff942187df4702e6f42140766d71ab9b88f
AnasImloul <pydev27@gmail.com>
2022-08-27 11:28:47 +0100

    Runtime: 8 ms (Top 90.62%) | Memory: 53.5 MB (Top 72.75%)

--- scripts/algorithms/F/Find Words That Can Be Formed by Characters/Find Words That Can Be Formed by Characters.java
+++ scripts/algorithms/F/Find Words That Can Be Formed by Characters/Find Words That Can Be Formed by Characters.java
@@ -1,3 +1,4 @@
+// Runtime: 8 ms (Top 90.62%) | Memory: 53.5 MB (Top 72.75%)
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int[] freq = new int[26];
@@ -7,15 +8,15 @@ class Solution {
             // 'b' - 'a' would give you 1. 'c' - 'a' would give you 2, and so on.
             freq[chars.charAt(i) - 'a'] ++;
         }
-        
+
         int result = 0;
         for (String word : words) {
             int[] copy = Arrays.copyOf(freq, freq.length);
             boolean pass = true;
             for (int j = 0; j < word.length(); j++) {
-				// decrement the frequency of this char in array for using
-				// if there are less than 1 chance for using this character, invalid, 
-				// move to next word in words
+                // decrement the frequency of this char in array for using
+                // if there are less than 1 chance for using this character, invalid,
+                // move to next word in words
                 if (-- copy[word.charAt(j) - 'a'] < 0) {
                     pass = false;
                     break;
