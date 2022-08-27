6e342e4ee5a98a948085acf49e382891df426ddf
AnasImloul <pydev27@gmail.com>
2022-08-27 11:28:49 +0100

    Runtime: 67 ms (Top 33.85%) | Memory: 147.3 MB (Top 73.33%)

--- scripts/algorithms/A/Array With Elements Not Equal to Average of Neighbors/Array With Elements Not Equal to Average of Neighbors.java
+++ scripts/algorithms/A/Array With Elements Not Equal to Average of Neighbors/Array With Elements Not Equal to Average of Neighbors.java
@@ -1,7 +1,8 @@
+// Runtime: 67 ms (Top 33.85%) | Memory: 147.3 MB (Top 73.33%)
 class Solution {
     public int[] rearrangeArray(int[] nums) {
        Arrays.sort(nums);
-	   // sort in wave format
+       // sort in wave format
         for(int i = 0;i<nums.length-1;i+=2){
             int temp = nums[i];
             nums[i] = nums[i+1];
