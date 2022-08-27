69a880d142346bb75b81cb18aa750a2b0432fb9e
AnasImloul <pydev27@gmail.com>
2022-08-27 10:50:19 +0100

    Runtime: 3 ms (Top 76.26%) | Memory: 9.3 MB (Top 30.17%)

--- scripts/algorithms/M/Merge Sorted Array/Merge Sorted Array.cpp
+++ scripts/algorithms/M/Merge Sorted Array/Merge Sorted Array.cpp
@@ -1,9 +1,10 @@
+// Runtime: 3 ms (Top 76.26%) | Memory: 9.3 MB (Top 30.17%)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int> temp(n+m);
         int i=0,j=0,k=0;
-        while(i<m && j<n){                                //select smaller element from both vector
+        while(i<m && j<n){ //select smaller element from both vector
             if(nums1[i]<nums2[j]){
                 temp[k]=nums1[i];
                 i++;
@@ -15,12 +16,12 @@ public:
                 k++;
             }
         }
-        while(i<m){                                                      //insert remaining nums1 element
+        while(i<m){ //insert remaining nums1 element
             temp[k]=nums1[i];
             i++;
             k++;
         }
-        while(j<n){                                                       //insert remaining nums2 element
+        while(j<n){ //insert remaining nums2 element
             temp[k]=nums2[j];
             j++;
             k++;
