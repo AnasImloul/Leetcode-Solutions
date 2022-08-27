8cca5a477491acddd79e4659fb06cfd122c0ab37
AnasImloul <pydev27@gmail.com>
2022-08-27 10:50:22 +0100

    Runtime: 45 ms (Top 15.79%) | Memory: 14.5 MB (Top 9.64%)

--- scripts/algorithms/C/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.cpp
+++ scripts/algorithms/C/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.cpp
@@ -1,14 +1,15 @@
+// Runtime: 45 ms (Top 15.79%) | Memory: 14.5 MB (Top 9.64%)
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
         int res = 0;
-        
+
         for (auto num : nums) {
             res += freq[num+k] + freq[num-k];
             freq[num]++;
         }
-        
+
         return res;
     }
 };
