76b5884d71cc52f5874a105316258481c0b47ce1
AnasImloul <pydev27@gmail.com>
2022-08-27 11:12:12 +0100

    Runtime: 175 ms (Top 46.15%) | Memory: 48.7 MB (Top 53.85%)

--- scripts/algorithms/P/Prime Palindrome/Prime Palindrome.js
+++ scripts/algorithms/P/Prime Palindrome/Prime Palindrome.js
@@ -1,14 +1,15 @@
+// Runtime: 175 ms (Top 46.15%) | Memory: 48.7 MB (Top 53.85%)
 /**
  * @param {number} n
  * @return {number}
  */
 var primePalindrome = function(n) {
-    
+
     while (true){
         let str = String(n)
         if (String(n).length % 2 == 0 && n > 11){
-			n = Math.pow(10, Math.ceil(Math.log10(n+1)))
-			// or n = 1 + Array(str.length).fill(0).join("")
+            n = Math.pow(10, Math.ceil(Math.log10(n+1)))
+            // or n = 1 + Array(str.length).fill(0).join("")
             continue
         }
         if (!isPalindrome(str)) {
@@ -18,16 +19,16 @@ var primePalindrome = function(n) {
         if (isPrime(n)) return n
         n++
     }
-    
+
 };
 
 function isPrime(n){
     if (n <= 1) return false
     if (n <= 3) return true
     if (n % 2 == 0 || n % 3 == 0) return false
-    
+
     for (let i = 3; i <= Math.floor(Math.sqrt(n)) + 1;i+=2){
-        if (n % i == 0) return false      
+        if (n % i == 0) return false
     }
     return true
 }
