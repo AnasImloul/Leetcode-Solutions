9e7461dfe5d8bbc283c8a50efbdd06a585324d8c
AnasImloul <pydev27@gmail.com>
2022-08-27 11:28:50 +0100

    Runtime: 198 ms (Top 72.22%) | Memory: 54.2 MB (Top 88.89%)

--- scripts/algorithms/F/Find Latest Group of Size M/Find Latest Group of Size M.js
+++ scripts/algorithms/F/Find Latest Group of Size M/Find Latest Group of Size M.js
@@ -1,3 +1,4 @@
+// Runtime: 198 ms (Top 72.22%) | Memory: 54.2 MB (Top 88.89%)
 var findLatestStep = function(arr, m) {
     if (m === arr.length) return arr.length
     let bits = new Array(arr.length+1).fill(true), pos, flag, i, j
