68c68363d2f3c680469c28426b3f2a987a22df65
AnasImloul <pydev27@gmail.com>
2022-08-27 11:28:51 +0100

    Runtime: 18 ms (Top 56.25%) | Memory: 9.3 MB (Top 68.36%)

--- scripts/algorithms/R/Rectangle Area II/Rectangle Area II.cpp
+++ scripts/algorithms/R/Rectangle Area II/Rectangle Area II.cpp
@@ -1,10 +1,11 @@
+// Runtime: 18 ms (Top 56.25%) | Memory: 9.3 MB (Top 68.36%)
 struct Point
 {
     int X;
     int delta;
 };
 
-class Solution 
+class Solution
 {
 public:
     int rectangleArea(vector<vector<int>>& rectangles)
@@ -16,27 +17,27 @@ public:
             auto y1 = r[1];
             auto x2 = r[2];
             auto y2 = r[3];
-            
+
             lines[y1].push_back(Point {x1, +1});
             lines[y1].push_back(Point {x2, -1});
             lines[y2].push_back(Point {x1, -1});
             lines[y2].push_back(Point {x2, +1});
         }
-        
+
         long area = 0;
         int prevy = 0;
         int length = 0;
-        
+
         map<int, int> scanline; // x -> delta
         for (const auto& [y, points] : lines)
         {
             area += (y - prevy) * (long)length;
-            
+
             // Update scanline for new y: add new rectanhgles,
             // remove old
             for (auto point : points)
             {
-                auto xdelta = scanline.find(point.X); 
+                auto xdelta = scanline.find(point.X);
                 if (xdelta != end(scanline))
                 {
                     xdelta->second += point.delta;
@@ -46,7 +47,7 @@ public:
                 else
                     scanline[point.X] = point.delta;
             }
-            
+
             // For current y-line calc the length of
             // intersection with rectangles
             int startX = -1;
@@ -61,13 +62,13 @@ public:
                 else if (rectCount == 0)
                     length += x - startX;
             }
-            
+
             if (rectCount > 0)
                 length += scanline.rbegin()->first - startX;
-            
+
             prevy = y;
         }
-        
+
         return area % (1000000007);
     }
 };
