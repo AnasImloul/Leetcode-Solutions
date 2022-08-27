5ac751b944e698de7187c68f100bd968b8918bd4
AnasImloul <pydev27@gmail.com>
2022-08-27 11:28:48 +0100

    Runtime: 2 ms (Top 57.48%) | Memory: 41.5 MB (Top 82.47%)

--- scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.java
+++ scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.java
@@ -1,5 +1,6 @@
+// Runtime: 2 ms (Top 57.48%) | Memory: 41.5 MB (Top 82.47%)
 class Solution {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) 
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2)
     {
         return(String.join("", word1).equals(String.join("", word2)));
     }
