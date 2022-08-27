5f145da63af3314b0924867cbc8957a009e8fb7f
AnasImloul <pydev27@gmail.com>
2022-08-27 11:35:22 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 98.77%)

--- scripts/algorithms/P/Populating Next Right Pointers in Each Node II/Populating Next Right Pointers in Each Node II.java
+++ scripts/algorithms/P/Populating Next Right Pointers in Each Node II/Populating Next Right Pointers in Each Node II.java
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 98.77%)
 class Solution {
     public Node connect(Node root) {
         if (root == null) {
@@ -6,7 +7,7 @@ class Solution {
         Node head = null; //the start node of next level, the first left of next level
         Node prev = null; //the next pointer
         Node curr = root;
-        
+
         while (curr != null) {
             //traverse the whole current level, left -> right, until we meet a null pointer
             while (curr != null) {
@@ -19,8 +20,7 @@ class Solution {
                         prev = prev.next;
                     }
                 }
-                
-                
+
                 if (curr.right != null) {
                     if (head == null) {
                         head = curr.right;
@@ -32,7 +32,7 @@ class Solution {
                 }
                 curr = curr.next;
             }
-            
+
             curr = head;
             prev = null;
             head = null;
