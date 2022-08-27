074eb15f8cf00a90e2675cfef9184073e26f1276
AnasImloul <pydev27@gmail.com>
2022-08-27 11:12:13 +0100

    Runtime: 642 ms (Top 13.93%) | Memory: 67.4 MB (Top 22.14%)

--- scripts/algorithms/L/Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.js
+++ scripts/algorithms/L/Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.js
@@ -1,16 +1,17 @@
+// Runtime: 642 ms (Top 13.93%) | Memory: 67.4 MB (Top 22.14%)
 var longestPalindrome = function(words) {
     const n = words.length;
     const map = new Map();
-    
+
     let len = 0;
-    
+
     for (const word of words) {
          const backward = word.split("").reverse().join("");
-        
+
         if (map.has(backward)) {
             len += (word.length * 2);
             map.set(backward, map.get(backward) - 1);
-            
+
             if (map.get(backward) === 0) map.delete(backward);
         }
         else {
@@ -18,27 +19,27 @@ var longestPalindrome = function(words) {
             map.set(word, map.get(word) + 1);
         }
     }
-    
+
     let maxLenSelfPalindrome = 0;
-    
+
     for (const word of map.keys()) {
         if (isPalindrome(word)) {
             maxLenSelfPalindrome = Math.max(maxLenSelfPalindrome, word.length);
         }
-    } 
-    
+    }
+
     return len + maxLenSelfPalindrome;
-    
+
     function isPalindrome(word) {
         let left = 0;
         let right = word.length - 1;
-        
+
         while (left < right) {
             if (word[left] != word[right]) return false;
             left++;
             --right;
         }
-        
+
         return true;
     }
 };
