6cfbc9173eea5c2e7047324c434b7c7b4be0e629
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:39 +0100

    Runtime: 76 ms (Top 75.00%) | Memory: 41.8 MB (Top 89.58%)

--- scripts/algorithms/B/Broken Calculator/Broken Calculator.js
+++ scripts/algorithms/B/Broken Calculator/Broken Calculator.js
@@ -1,3 +1,4 @@
+// Runtime: 76 ms (Top 75.00%) | Memory: 41.8 MB (Top 89.58%)
 /**
  * @param {number} startValue
  * @param {number} target
@@ -5,17 +6,17 @@
  */
 var brokenCalc = function(startValue, target) {
     let steps = 0;
-    
+
     while(target !== startValue){
 
         if(startValue > target){
-            return steps  + startValue - target;
+            return steps + startValue - target;
         }
         if(target %2 === 0){
             target /= 2;
         }else {
             target += 1;
-            
+
         }
         steps++;
     }
