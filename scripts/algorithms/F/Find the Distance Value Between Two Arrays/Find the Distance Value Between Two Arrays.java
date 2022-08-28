60f72bcfa703cab06f810fc3f63a492da7baa54c
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:35 +0100

    Runtime: 5 ms (Top 68.55%) | Memory: 44.9 MB (Top 25.67%)

--- scripts/algorithms/F/Find the Distance Value Between Two Arrays/Find the Distance Value Between Two Arrays.java
+++ scripts/algorithms/F/Find the Distance Value Between Two Arrays/Find the Distance Value Between Two Arrays.java
@@ -1,3 +1,4 @@
+// Runtime: 5 ms (Top 68.55%) | Memory: 44.9 MB (Top 25.67%)
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int x=0,val=0;
@@ -6,7 +7,7 @@ class Solution {
                 if(Math.abs(i-j)<=d){
                    x--;
                     break;
-                }     
+                }
             }
             x++;
         }
