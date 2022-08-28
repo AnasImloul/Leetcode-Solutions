bbceaf3c23c444aa43c29718c400a17bdbc41ac5
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:40 +0100

    Runtime: 234 ms (Top 41.24%) | Memory: 51.4 MB (Top 8.73%)

--- scripts/algorithms/F/Find All Anagrams in a String/Find All Anagrams in a String.js
+++ scripts/algorithms/F/Find All Anagrams in a String/Find All Anagrams in a String.js
@@ -1,5 +1,6 @@
+// Runtime: 234 ms (Top 41.24%) | Memory: 51.4 MB (Top 8.73%)
 var findAnagrams = function(s, p) {
-    
+
     function compareMaps(map1, map2) {
     var testVal;
     if (map1.size !== map2.size) {
@@ -23,12 +24,12 @@ var findAnagrams = function(s, p) {
     let sn=s.length
     for(let i in p){
         p_map.set(p[i],p_map.get(p[i])?p_map.get(p[i])+1:1)
-        s_map.set(s[i],s_map.get(s[i])?s_map.get(s[i])+1:1) 
+        s_map.set(s[i],s_map.get(s[i])?s_map.get(s[i])+1:1)
     }
     let l=0
     if(compareMaps(s_map,p_map)) res.push(l)
     for(let r=pn;r<sn;r++){
-        s_map.set(s[r],s_map.get(s[r])?s_map.get(s[r])+1:1) 
+        s_map.set(s[r],s_map.get(s[r])?s_map.get(s[r])+1:1)
         s_map.set(s[l],s_map.get(s[l])-1)
         if(s_map.get(s[l])===0){
             s_map.delete(s[l])
@@ -37,5 +38,5 @@ var findAnagrams = function(s, p) {
         l++
     }
     return(res)
-        
+
 };
