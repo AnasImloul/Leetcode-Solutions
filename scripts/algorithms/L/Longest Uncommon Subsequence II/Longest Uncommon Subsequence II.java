0324c460b60b57b646dc8294511643185f650fe0
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:50 +0100

    Runtime: 2 ms (Top 84.32%) | Memory: 39.8 MB (Top 90.27%)

--- scripts/algorithms/L/Longest Uncommon Subsequence II/Longest Uncommon Subsequence II.java
+++ scripts/algorithms/L/Longest Uncommon Subsequence II/Longest Uncommon Subsequence II.java
@@ -1,20 +1,21 @@
+// Runtime: 2 ms (Top 84.32%) | Memory: 39.8 MB (Top 90.27%)
 class Solution {
     public int findLUSlength(String[] strs) {
         Arrays.sort(strs,(a,b) -> b.length() - a.length()); // sort descending order by length
          // store the frequency of all strings in array
         Map<String,Integer> map = new HashMap<>();
         for(String s : strs) map.put(s,map.getOrDefault(s,0)+1);
-        
+
         for(int i=0;i<strs.length;i++){
             if(map.get(strs[i]) != 1) continue; // string is not unique
             int j;
             for(j=0;j<i;j++){
                 if(isSubsequence(strs[i],strs[j])) break;
             }
-			// if it is not a subsequence of any other larger string
+            // if it is not a subsequence of any other larger string
              if(j == i) return strs[i].length();
         }
-        return -1;  // no string satisfies the criterion
+        return -1; // no string satisfies the criterion
     }
     public boolean isSubsequence(String a, String b){
         int i=0,j=0;
