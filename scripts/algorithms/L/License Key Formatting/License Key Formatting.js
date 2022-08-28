32ebdfca5d10b9cc51d1ab1dcab928a86c2a02a3
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:32 +0100

    Runtime: 84 ms (Top 86.97%) | Memory: 44.9 MB (Top 98.14%)

--- scripts/algorithms/L/License Key Formatting/License Key Formatting.js
+++ scripts/algorithms/L/License Key Formatting/License Key Formatting.js
@@ -1,3 +1,4 @@
+// Runtime: 84 ms (Top 86.97%) | Memory: 44.9 MB (Top 98.14%)
 // Please upvote if you like the solution. Thanks
 
 var licenseKeyFormatting = function(s, k) {
@@ -5,8 +6,8 @@ var licenseKeyFormatting = function(s, k) {
     let ans=""
     let i=str.length;
     while(i>0){
-        ans="-"+str.substring(i-k,i)+ans    // we are taking k characters from the end of string and adding it to answer
+        ans="-"+str.substring(i-k,i)+ans // we are taking k characters from the end of string and adding it to answer
         i=i-k
     }
-    return (ans.substring(1))   // removing the "-" which is present in the start of ans
+    return (ans.substring(1)) // removing the "-" which is present in the start of ans
 };
