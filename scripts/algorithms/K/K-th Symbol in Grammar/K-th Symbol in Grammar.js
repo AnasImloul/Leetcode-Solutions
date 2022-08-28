4dba055e39b53e617a00055fda68099bfa68e02e
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:41 +0100

    Runtime: 102 ms (Top 22.47%) | Memory: 41.5 MB (Top 88.20%)

--- scripts/algorithms/K/K-th Symbol in Grammar/K-th Symbol in Grammar.js
+++ scripts/algorithms/K/K-th Symbol in Grammar/K-th Symbol in Grammar.js
@@ -1,3 +1,4 @@
+// Runtime: 102 ms (Top 22.47%) | Memory: 41.5 MB (Top 88.20%)
 /**
  * @param {number} n
  * @param {number} k
@@ -7,14 +8,12 @@ var kthGrammar = function(n, k) {
     if (n == 1 && k == 1) {
         return 0;
     }
-    
+
     const mid = Math.pow(2, n-1) / 2;
-    
+
     if (k <= mid) {
         return kthGrammar(n-1, k);
     } else {
         return kthGrammar(n-1, k-mid) == 1 ? 0 : 1;
     }
 };
-
-
