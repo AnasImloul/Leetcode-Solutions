9d6fab1211513e69c3c85173a471280892effabd
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:45 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 42.6 MB (Top 92.75%)

--- scripts/algorithms/S/Shuffle String/Shuffle String.java
+++ scripts/algorithms/S/Shuffle String/Shuffle String.java
@@ -1,9 +1,10 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 42.6 MB (Top 92.75%)
 class Solution {
     public String restoreString(String s, int[] indices) {
         char[] ch = new char[s.length()];
         for(int i = 0 ; i< s.length() ; i ++){
             ch[indices[i]]=s.charAt(i);
         }
-        return new String (ch); 
+        return new String (ch);
     }
 }
