df9e944a4927beb87e1ff427cc1beb7f140270a2
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:34 +0100

    Runtime: 106 ms (Top 32.20%) | Memory: 42.5 MB (Top 52.54%)

--- scripts/algorithms/N/Number of Steps to Reduce a Number in Binary Representation to One/Number of Steps to Reduce a Number in Binary Representation to One.js
+++ scripts/algorithms/N/Number of Steps to Reduce a Number in Binary Representation to One/Number of Steps to Reduce a Number in Binary Representation to One.js
@@ -1,13 +1,14 @@
+// Runtime: 106 ms (Top 32.20%) | Memory: 42.5 MB (Top 52.54%)
 var numSteps = function(s) {
     let res = 0;
     s = s.split("");
     while(s.length>1){
         if(s[s.length-1]==="0") s.pop();
         else plusone(s);
-        res++;      
+        res++;
     }
     return res;
-};    
+};
 var plusone = function(p) {
     p.unshift("0");
     let i = p.length-1;
