ddb186ef053504d542af072ec5775958b5407a8d
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:30 +0100

    Runtime: 112 ms (Top 15.11%) | Memory: 42.1 MB (Top 57.33%)

--- scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.js
+++ scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.js
@@ -1,3 +1,4 @@
+// Runtime: 112 ms (Top 15.11%) | Memory: 42.1 MB (Top 57.33%)
 var countStudents = function(students, sandwiches) {
 while (students.length>0 && students.indexOf(sandwiches[0])!=-1) {
     if (students[0] == sandwiches[0]) {
