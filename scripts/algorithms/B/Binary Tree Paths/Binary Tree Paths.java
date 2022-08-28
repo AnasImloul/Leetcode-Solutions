2d60920a36581fdc2dcaf67aaefcc5314ec68cc2
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:38 +0100

    Runtime: 19 ms (Top 6.94%) | Memory: 43.6 MB (Top 28.49%)

--- scripts/algorithms/B/Binary Tree Paths/Binary Tree Paths.java
+++ scripts/algorithms/B/Binary Tree Paths/Binary Tree Paths.java
@@ -1,3 +1,4 @@
+// Runtime: 19 ms (Top 6.94%) | Memory: 43.6 MB (Top 28.49%)
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
@@ -5,18 +6,18 @@ class Solution {
         dfs(res, root, s);
         return res;
     }
-    
+
     public void dfs(List<String> res, TreeNode root, String s)
     {
         if(root==null)
             return;
-        
+
         if(root.left==null && root.right==null)
         {
             res.add(s+root.val);
             return;
         }
-        
+
         else
         {
             dfs(res,root.left, s + root.val + "->");
