267b466b7993707407d00eda4beaf9e8f66c0c8b
AnasImloul <pydev27@gmail.com>
2022-08-28 12:45:42 +0100

    Runtime: 4 ms (Top 7.89%) | Memory: 54.3 MB (Top 5.35%)

--- scripts/algorithms/I/Insufficient Nodes in Root to Leaf Paths/Insufficient Nodes in Root to Leaf Paths.java
+++ scripts/algorithms/I/Insufficient Nodes in Root to Leaf Paths/Insufficient Nodes in Root to Leaf Paths.java
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 7.89%) | Memory: 54.3 MB (Top 5.35%)
 class Solution {
     private class Pair{
         TreeNode node;
@@ -12,21 +13,21 @@ class Solution {
     }
     private Pair solve(TreeNode root, int sum, int limit){
         if(root==null) return new Pair(null, Integer.MIN_VALUE);
-        
+
         Pair left = solve(root.left, sum+root.val, limit);
         Pair right = solve(root.right, sum+root.val, limit);
-    
+
         root.left = left.node;
         root.right = right.node;
-        
+
         int max = Integer.MIN_VALUE;
         if(left.sum!=Integer.MIN_VALUE) max = left.sum;
-        if(right.sum!=Integer.MIN_VALUE) max = Math.max(max, right.sum); 
+        if(right.sum!=Integer.MIN_VALUE) max = Math.max(max, right.sum);
         if(max==Integer.MIN_VALUE) max = 0;
         if(sum+max+root.val < limit) {
             return new Pair(null, max+root.val);
         }
-            
+
         return new Pair(root, max+root.val);
     }
 }
