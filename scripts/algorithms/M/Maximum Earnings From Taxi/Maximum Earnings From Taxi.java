e51904c767101929be4c1ac9831eabf6a523c21b
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:44 +0100

    Runtime: 222 ms (Top 14.25%) | Memory: 127.5 MB (Top 23.46%)

--- scripts/algorithms/M/Maximum Earnings From Taxi/Maximum Earnings From Taxi.java
+++ scripts/algorithms/M/Maximum Earnings From Taxi/Maximum Earnings From Taxi.java
@@ -1,3 +1,4 @@
+// Runtime: 222 ms (Top 14.25%) | Memory: 127.5 MB (Top 23.46%)
 class Solution {
     public long maxTaxiEarnings(int n, int[][] rides) {
         TreeMap<Integer,Long> maps = new TreeMap<Integer,Long>();
@@ -9,7 +10,7 @@ class Solution {
             if(maps.containsKey(rides[i][1]))
                  p = maps.get(rides[i][1]);
             long q = e.getValue() + (rides[i][1] - rides[i][0] + rides[i][2]);
-            if(q > p  && maps.lastEntry().getValue() < q){
+            if(q > p && maps.lastEntry().getValue() < q){
                  maps.put(rides[i][1],q);
             }
             maps.put(0,0L);
