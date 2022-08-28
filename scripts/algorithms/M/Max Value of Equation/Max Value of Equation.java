e673efff1c26edad8351ee7a4b3e002eb3a2aab7
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:47 +0100

    Runtime: 16 ms (Top 80.93%) | Memory: 104.9 MB (Top 83.74%)

--- scripts/algorithms/M/Max Value of Equation/Max Value of Equation.java
+++ scripts/algorithms/M/Max Value of Equation/Max Value of Equation.java
@@ -1,14 +1,15 @@
+// Runtime: 16 ms (Top 80.93%) | Memory: 104.9 MB (Top 83.74%)
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
         int ans=Integer.MIN_VALUE;
-		int i=0;
+        int i=0;
         int f=1;
         while(i < points.length) {
             if(f<i+1)
                 f=i+1;
             for (int j = f; j <= points.length-1; j++) {
                 if(points[j][0]>(points[i][0]+k))
-					break;
+                    break;
                 if((points[i][1]+points[j][1]+points[j][0]-points[i][0])>ans){
                     ans=points[i][1]+points[j][1]+points[j][0]-points[i][0];
                     f=j-1;
