dc6d8683a2b0cb5e45f6d744aca21865fa2d751c
AnasImloul <pydev27@gmail.com>
2022-08-28 12:45:47 +0100

    Runtime: 38 ms (Top 34.69%) | Memory: 18.8 MB (Top 82.14%)

--- scripts/algorithms/R/Random Flip Matrix/Random Flip Matrix.cpp
+++ scripts/algorithms/R/Random Flip Matrix/Random Flip Matrix.cpp
@@ -1,25 +1,26 @@
+// Runtime: 38 ms (Top 34.69%) | Memory: 18.8 MB (Top 82.14%)
 class Solution {
 public:
   unordered_map<int,int>table;
   int y, x;
-  
+
   Solution(int m, int n) {
-    y = m, x = n;      
+    y = m, x = n;
   }
-    
+
   vector<int> flip() {
     int Y = rand() % y, X = rand() % x, id;
-    
+
     while(table. count(id = (Y << 14) + X )){
       if(++X == x) X = 0, Y++;
       if(Y == y) Y = 0;
-    } 
-    
+    }
+
     table[id] = 1;
-    return {Y, X};    
+    return {Y, X};
   }
-    
+
   void reset() {
-    table.clear();   
+    table.clear();
   }
 };
