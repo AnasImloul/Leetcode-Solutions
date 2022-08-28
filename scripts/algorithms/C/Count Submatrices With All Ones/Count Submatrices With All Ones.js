7f8e1ac3fd593fdc24eb60770a0bb0205cb7adfc
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:37 +0100

    Runtime: 126 ms (Top 74.14%) | Memory: 44.6 MB (Top 82.76%)

--- scripts/algorithms/C/Count Submatrices With All Ones/Count Submatrices With All Ones.js
+++ scripts/algorithms/C/Count Submatrices With All Ones/Count Submatrices With All Ones.js
@@ -1,3 +1,4 @@
+// Runtime: 126 ms (Top 74.14%) | Memory: 44.6 MB (Top 82.76%)
 /**
  * @param {number[][]} mat
  * @return {number}
@@ -5,12 +6,12 @@
 var numSubmat = function(mat) {
     var m = mat.length;
     var n = mat[0].length;
-    
+
     for(let i=0; i<m; i++){
         for(let j = n-2; j>=0; j-- ){
             if(mat[i][j] === 0)continue;
             mat[i][j] += mat[i][j+1];
-            
+
         }
     }
     var sum = 0
@@ -18,14 +19,14 @@ var numSubmat = function(mat) {
         for(let j = 0; j<n; j++ ){
             if(mat[i][j] === 0)continue;
             var min = mat[i][j]
-            
+
             for(let k=i; k<m; k++){
                 if(mat[k][j] === 0)break;
                 min = Math.min(min , mat[k][j])
                 sum += min
-            }            
+            }
         }
-        
+
     }
     return sum
 };
