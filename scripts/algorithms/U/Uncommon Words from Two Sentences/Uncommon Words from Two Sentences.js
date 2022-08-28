25002c2b34f12ba877ed8b7fbc0f9c3b1f79f85e
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:28 +0100

    Runtime: 111 ms (Top 17.77%) | Memory: 42.1 MB (Top 85.95%)

--- scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.js
+++ scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.js
@@ -1,3 +1,4 @@
-var uncommonFromSentences = function(s1, s2) {   
+// Runtime: 111 ms (Top 17.77%) | Memory: 42.1 MB (Top 85.95%)
+var uncommonFromSentences = function(s1, s2) {
     return (s1+' '+s2).split(' ').filter((el,i,arr)=>arr.indexOf(el)===arr.lastIndexOf(el))
 };
