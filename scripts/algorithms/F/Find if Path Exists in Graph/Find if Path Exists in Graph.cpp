8abf1794fcbcf46ddda7563d88a0b3212cb12ded
AnasImloul <pydev27@gmail.com>
2022-08-28 12:03:40 +0100

    Runtime: 1891 ms (Top 5.09%) | Memory: 280.7 MB (Top 5.05%)

--- scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.cpp
+++ scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.cpp
@@ -1,8 +1,9 @@
+// Runtime: 1891 ms (Top 5.09%) | Memory: 280.7 MB (Top 5.05%)
 class Solution {
 private:
     unordered_map<int, vector<int>> adj;
-    unordered_map<int, bool> visited; 
-    
+    unordered_map<int, bool> visited;
+
     bool dfs(int node, int destination){
         visited[node] = true;
         for(auto neighbour:adj[node]){
@@ -19,11 +20,11 @@ public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         if(source == destination)
             return true;
-        
+
         for(auto edge:edges){
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
-        return dfs(source, destination);        
+        return dfs(source, destination);
     }
 };
