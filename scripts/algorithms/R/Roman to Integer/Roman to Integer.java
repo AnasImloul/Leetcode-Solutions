8a4db7eb56f1a4688a0abf257feb9120fe232e57
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:42 +0100

    Runtime: 8 ms (Top 63.45%) | Memory: 44.6 MB (Top 83.79%)

--- scripts/algorithms/R/Roman to Integer/Roman to Integer.java
+++ scripts/algorithms/R/Roman to Integer/Roman to Integer.java
@@ -1,8 +1,9 @@
+// Runtime: 8 ms (Top 63.45%) | Memory: 44.6 MB (Top 83.79%)
 class Solution {
     public int romanToInt(String s) {
         int res=0;
-		// Let s = "IV" after traversing string res will be 6
-		// Let s= "IX" after traversing string res will be 11
+        // Let s = "IV" after traversing string res will be 6
+        // Let s= "IX" after traversing string res will be 11
         for(int i=0;i<s.length();i++){
             switch(s.charAt(i)){
                 case 'I': res=res+1;
@@ -20,16 +21,16 @@ class Solution {
                 case 'M': res+=1000;
                     break;
             }
-        }   
-		// Since s= "IV" it satisfies first condition and 2 is subtracted from res. res=4
-		// Since s= "IX" it satisfies first condition and 2 is subtracted from res. res=9
+        }
+        // Since s= "IV" it satisfies first condition and 2 is subtracted from res. res=4
+        // Since s= "IX" it satisfies first condition and 2 is subtracted from res. res=9
         if(s.contains("IV")||s.contains("IX"))
             res=res-2;
         if(s.contains("XL")||s.contains("XC"))
             res=res-20;
         if(s.contains("CD")||s.contains("CM"))
             res=res-200;
-       
+
         return res;
     }
 }
