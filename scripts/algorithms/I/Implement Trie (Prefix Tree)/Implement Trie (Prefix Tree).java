75217ae36327bee710a870d8eacb4a7c81897f34
AnasImloul <pydev27@gmail.com>
2022-08-28 12:15:31 +0100

    Runtime: 67 ms (Top 53.89%) | Memory: 70 MB (Top 22.15%)

--- scripts/algorithms/I/Implement Trie (Prefix Tree)/Implement Trie (Prefix Tree).java
+++ scripts/algorithms/I/Implement Trie (Prefix Tree)/Implement Trie (Prefix Tree).java
@@ -1,3 +1,4 @@
+// Runtime: 67 ms (Top 53.89%) | Memory: 70 MB (Top 22.15%)
 class Trie {
     private class Node{
         char character;
@@ -13,30 +14,30 @@ class Trie {
         root = new Node('\0');
         map = new HashMap<>();
     }
-    
+
     public void insert(String word) {
         Node temp = root;
         for(char c : word.toCharArray()){
             int index = c-'a';
             if(temp.sub[index] == null)
                 temp.sub[index] = new Node(c);
-            
+
             temp = temp.sub[index];
         }
         map.put(word, true);
     }
-    
+
     public boolean search(String word) {
         return map.containsKey(word);
     }
-    
+
     public boolean startsWith(String prefix) {
         Node temp = root;
         for(char c : prefix.toCharArray()){
             int index = c-'a';
             if(temp.sub[index] == null)
                 return false;
-            temp = temp.sub[index]; 
+            temp = temp.sub[index];
         }
         return true;
     }
