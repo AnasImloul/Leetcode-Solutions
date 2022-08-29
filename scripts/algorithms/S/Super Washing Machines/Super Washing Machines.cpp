0200775a12a909d623fc457695baa9c213ea0134
AnasImloul <pydev27@gmail.com>
2022-08-29 13:40:22 +0100

    Runtime: 18 ms (Top 41.09%) | Memory: 13 MB (Top 43.64%)

--- scripts/algorithms/S/Super Washing Machines/Super Washing Machines.cpp
+++ scripts/algorithms/S/Super Washing Machines/Super Washing Machines.cpp
@@ -1,14 +1,15 @@
+// Runtime: 18 ms (Top 41.09%) | Memory: 13 MB (Top 43.64%)
 class Solution {
 public:
     int findMinMoves(vector<int>& machines) {
-        int total = accumulate(machines.begin(), machines.end(), 0); 
+        int total = accumulate(machines.begin(), machines.end(), 0);
         if (total % machines.size()) return -1; // impossible
 
-        int avg = total / machines.size(), ans = 0, prefix = 0; 
+        int avg = total / machines.size(), ans = 0, prefix = 0;
         for (auto& x : machines) {
-            ans = max({ans, abs(prefix), x - avg}); 
-            prefix += x - avg; 
+            ans = max({ans, abs(prefix), x - avg});
+            prefix += x - avg;
         }
-        return ans; 
+        return ans;
     }
 };
