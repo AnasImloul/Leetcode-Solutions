fce1600ca1c63bab4d86fafef0da29bff2ffe78d
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:52 +0100

    Runtime: 44 ms (Top 94.02%) | Memory: 55.7 MB (Top 95.47%)

--- scripts/algorithms/C/Count Number of Bad Pairs/Count Number of Bad Pairs.java
+++ scripts/algorithms/C/Count Number of Bad Pairs/Count Number of Bad Pairs.java
@@ -1,3 +1,4 @@
+// Runtime: 44 ms (Top 94.02%) | Memory: 55.7 MB (Top 95.47%)
 class Solution {
     public long countBadPairs(int[] nums) {
         HashMap<Integer, Integer> seen = new HashMap<>();
