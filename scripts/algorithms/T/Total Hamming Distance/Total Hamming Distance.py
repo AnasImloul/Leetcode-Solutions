7193897a93f15e110165d8ca858f37e79d0acfd7
AnasImloul <pydev27@gmail.com>
2022-08-29 12:52:55 +0100

    Runtime: 933 ms (Top 28.58%) | Memory: 15.5 MB (Top 24.40%)

--- scripts/algorithms/T/Total Hamming Distance/Total Hamming Distance.py
+++ scripts/algorithms/T/Total Hamming Distance/Total Hamming Distance.py
@@ -1,10 +1,11 @@
+// Runtime: 933 ms (Top 28.58%) | Memory: 15.5 MB (Top 24.40%)
 class Solution:
     def totalHammingDistance(self, arr: List[int]) -> int:
-        
+
         total = 0
         for i in range(0,31):
             count = 0
             for j in arr :
                 count+= (j >> i) & 1
             total += count*(len(arr)-count)
-        return total 
+        return total
