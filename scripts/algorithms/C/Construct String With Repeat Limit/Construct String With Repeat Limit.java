d1aaf6775b1fe028d2212502a323c6e6a313edc7
AnasImloul <pydev27@gmail.com>
2022-08-29 14:41:33 +0100

    Runtime: 28 ms (Top 95.86%) | Memory: 64.6 MB (Top 55.86%)

--- scripts/algorithms/C/Construct String With Repeat Limit/Construct String With Repeat Limit.java
+++ scripts/algorithms/C/Construct String With Repeat Limit/Construct String With Repeat Limit.java
@@ -1,3 +1,4 @@
+// Runtime: 28 ms (Top 95.86%) | Memory: 64.6 MB (Top 55.86%)
 class Solution {
     public String repeatLimitedString(String s, int repeatLimit) {
         int[] counter = new int[26];
@@ -16,11 +17,11 @@ class Solution {
             if (counter[max] == 0) {
                 max = findNextMax(counter, max - 1);
                 repeated = 0;
-				continue;
-            } 
+                continue;
+            }
             if (repeated == repeatLimit) {
-				// Greedy, use the next possible char once and get back to curr.
-				// if no other char available, the curr word is the largest subsequence. 
+                // Greedy, use the next possible char once and get back to curr.
+                // if no other char available, the curr word is the largest subsequence.
                 int lower = findNextMax(counter, max - 1);
                 if (lower < 0) {
                     return builder.toString();
@@ -32,7 +33,7 @@ class Solution {
         }
         return builder.toString();
     }
-    
+
     private int findNextMax(int[] counter, int from) {
         int curr = from;
         while (curr >= 0) {
