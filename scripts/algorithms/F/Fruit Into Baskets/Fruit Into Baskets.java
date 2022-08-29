04282853b99dc4eb41f6e5d14861ef83c63ad608
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:55 +0100

    Runtime: 61 ms (Top 63.76%) | Memory: 95 MB (Top 59.07%)

--- scripts/algorithms/F/Fruit Into Baskets/Fruit Into Baskets.java
+++ scripts/algorithms/F/Fruit Into Baskets/Fruit Into Baskets.java
@@ -1,3 +1,4 @@
+// Runtime: 61 ms (Top 63.76%) | Memory: 95 MB (Top 59.07%)
 class Solution {
     public int totalFruit(int[] fruits) {
         if (fruits == null || fruits.length == 0) {
@@ -5,13 +6,13 @@ class Solution {
         }
         int start = 0, end = 0, res = 0;
         HashMap<Integer, Integer> map = new HashMap<>(); //key = type of fruit on tree, value = last index / newest index of that fruit
-        
+
         while (end < fruits.length) {
             if (map.size() <= 2) {
                 map.put(fruits[end], end);
                 end++;
             }
-            
+
             if (map.size() > 2) {
                 int leftMost = fruits.length;
                 for (int num : map.values()) {
@@ -20,7 +21,7 @@ class Solution {
                 map.remove(fruits[leftMost]);
                 start = leftMost + 1;
             }
-            
+
             res = Math.max(res, end - start);
         }
         return res;
