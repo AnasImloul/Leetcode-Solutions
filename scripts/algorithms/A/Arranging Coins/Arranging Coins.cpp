ed69cf56f23e0045b2b8108575be9f192ab7cc94
AnasImloul <pydev27@gmail.com>
2022-08-29 14:41:37 +0100

    Runtime: 19 ms (Top 17.89%) | Memory: 5.9 MB (Top 24.93%)

--- scripts/algorithms/A/Arranging Coins/Arranging Coins.cpp
+++ scripts/algorithms/A/Arranging Coins/Arranging Coins.cpp
@@ -1,22 +1,23 @@
+// Runtime: 19 ms (Top 17.89%) | Memory: 5.9 MB (Top 24.93%)
 class Solution {
 public:
     int arrangeCoins(int n) {
-        
+
          int count=0;
          while(n>0)
          {
              count++;
              n=n-count;
          }
-         if(n==0)        //all the coins are used to make complete rows so n if fully uitlized till 0
+         if(n==0) //all the coins are used to make complete rows so n if fully uitlized till 0
          {
              return count;
          }
-         else if(n<0)     //if the last row has not been created fully then n will go negative
+         else if(n<0) //if the last row has not been created fully then n will go negative
          {
              return count-1;
          }
-         return -1;   //this will never get encountered
-         
+         return -1; //this will never get encountered
+
     }
 };
