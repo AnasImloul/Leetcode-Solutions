0761a2deef7f5c49a3e100981b2811b39c288f40
AnasImloul <pydev27@gmail.com>
2022-08-29 12:52:52 +0100

    Runtime: 77 ms (Top 96.60%) | Memory: 13.8 MB (Top 57.97%)

--- scripts/algorithms/P/Power of Three/Power of Three.py
+++ scripts/algorithms/P/Power of Three/Power of Three.py
@@ -1,3 +1,4 @@
+// Runtime: 77 ms (Top 96.60%) | Memory: 13.8 MB (Top 57.97%)
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         return round(log(n,3), 9) == round(log(n,3)) if n >= 1 else False
