ec11ab68a1cb4b8f1af8cae1ca4d656247c5b14b
AnasImloul <pydev27@gmail.com>
2022-08-29 12:52:52 +0100

    Runtime: 140 ms (Top 67.23%) | Memory: 48.3 MB (Top 43.50%)

--- scripts/algorithms/M/Maximum Number of Balls in a Box/Maximum Number of Balls in a Box.js
+++ scripts/algorithms/M/Maximum Number of Balls in a Box/Maximum Number of Balls in a Box.js
@@ -1,9 +1,10 @@
+// Runtime: 140 ms (Top 67.23%) | Memory: 48.3 MB (Top 43.50%)
 var countBalls = function(lowLimit, highLimit) {
     let obj={};
     for(let i=lowLimit; i<=highLimit; i++){
         i+=''; let sum=0;
         for(let j=0; j<i.length; j++){ sum+=i[j]*1; }
-        obj[sum]?obj[sum]+=1:obj[sum]=1  
+        obj[sum]?obj[sum]+=1:obj[sum]=1
     }
     return Math.max(...Object.values(obj));
 };
