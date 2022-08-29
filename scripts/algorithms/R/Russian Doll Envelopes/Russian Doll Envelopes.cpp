07cb205343d33fe872f1b67c9e776409dfb835c4
AnasImloul <pydev27@gmail.com>
2022-08-29 14:41:35 +0100

    Runtime: 476 ms (Top 78.16%) | Memory: 76.3 MB (Top 100.00%)

--- scripts/algorithms/R/Russian Doll Envelopes/Russian Doll Envelopes.cpp
+++ scripts/algorithms/R/Russian Doll Envelopes/Russian Doll Envelopes.cpp
@@ -1,10 +1,11 @@
+// Runtime: 476 ms (Top 78.16%) | Memory: 76.3 MB (Top 100.00%)
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
         sort(envelopes.begin(), envelopes.end(), [](auto &l, auto &r)
              {
-                return l[0] == r[0] ? l[1] > r[1] : l[0] < r[0]; 
+                return l[0] == r[0] ? l[1] > r[1] : l[0] < r[0];
              });
         int len = 0;
         for(auto& cur: envelopes)
