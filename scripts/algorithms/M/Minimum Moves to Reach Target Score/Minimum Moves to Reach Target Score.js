c796bd3227e0be3b80206e2c260959e00bb0c023
AnasImloul <pydev27@gmail.com>
2022-08-29 13:40:25 +0100

    Runtime: 5040 ms (Top 9.23%) | Memory: 41.7 MB (Top 89.23%)

--- scripts/algorithms/M/Minimum Moves to Reach Target Score/Minimum Moves to Reach Target Score.js
+++ scripts/algorithms/M/Minimum Moves to Reach Target Score/Minimum Moves to Reach Target Score.js
@@ -1,7 +1,8 @@
+// Runtime: 5040 ms (Top 9.23%) | Memory: 41.7 MB (Top 89.23%)
 var minMoves = function(target, maxDoubles) {
     if (maxDoubles === 0) return target - 1;
     let count = 0;
-    
+
     while (target > 1) {
         if (target % 2 === 0 && maxDoubles > 0) {
             target /= 2;
@@ -9,9 +10,9 @@ var minMoves = function(target, maxDoubles) {
         } else {
             target--;
         }
-        
+
         count++;
     }
-    
+
     return count;
 };
