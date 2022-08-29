e9bf2801d1f22d1ec96713fcf7d177cf500ded3d
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:53 +0100

    Runtime: 27 ms (Top 54.24%) | Memory: 57.7 MB (Top 33.05%)

--- scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.java
+++ scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.java
@@ -1,3 +1,4 @@
+// Runtime: 27 ms (Top 54.24%) | Memory: 57.7 MB (Top 33.05%)
 class Solution {
 
 public String lastSubstring(String s) {
@@ -6,17 +7,16 @@ int maxIndex = s.length() - 1;
 for(int currIndex = s.length() - 1 ; currIndex >= 0 ; currIndex--){
     if(s.charAt(currIndex) > s.charAt(maxIndex))
         maxIndex = currIndex;
-        
+
     else if(s.charAt(currIndex) == s.charAt(maxIndex)){
         int i = currIndex + 1;
         int j = maxIndex + 1;
-        
-        
+
         while(i < maxIndex && j < s.length() && s.charAt(i) == s.charAt(j)){
             i++;
             j++;
         }
-        
+
         if(i == maxIndex || j == s.length() || s.charAt(i) > s.charAt(j))
             maxIndex = currIndex;
     }
