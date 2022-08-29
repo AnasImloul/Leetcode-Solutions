62d39ce82603590c4c9fe44e5c234b9cb6af1103
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:49 +0100

    Runtime: 1 ms (Top 98.48%) | Memory: 42.5 MB (Top 63.64%)

--- scripts/algorithms/M/Maximum Good People Based on Statements/Maximum Good People Based on Statements.java
+++ scripts/algorithms/M/Maximum Good People Based on Statements/Maximum Good People Based on Statements.java
@@ -1,10 +1,11 @@
+// Runtime: 1 ms (Top 98.48%) | Memory: 42.5 MB (Top 63.64%)
 class Solution {
-    public int maximumGood(int[][] statements) {  
-        int[] result = { 0 };      
+    public int maximumGood(int[][] statements) {
+        int[] result = { 0 };
         maximumGood(statements, new boolean[statements.length], 0, 0, result);
         return result[0];
     }
-    
+
     /*
         This function uses backtracking to do the DFS. We are basically constructing all possible scenarios and verifying them.
 
@@ -19,7 +20,7 @@ class Solution {
             result[0] = Math.max(result[0], currentGoodPeople);
             return;
         }
-        
+
         // Now we are going to use backtracking to do DFS.
 
         // Scenario 1: Assume the current person is good and verify it.
@@ -33,18 +34,18 @@ class Solution {
         }
 
         // Scenario 2: Assume the current person is bad and verify it.
-        goodPeople[currentPerson] = false;      
-        // Prune the tree for the same reason mentioned earlier.  
+        goodPeople[currentPerson] = false;
+        // Prune the tree for the same reason mentioned earlier.
         if (goodPeople.length - currentPerson - 1 + currentGoodPeople < result[0]) {
             return;
         }
         if (isValid(statements, goodPeople, currentPerson)) {
             maximumGood(statements, goodPeople, currentPerson + 1, currentGoodPeople, result);
         }
-        
+
         return;
     }
-    
+
     private boolean isValid(int[][] statements, boolean[] goodPeople, int currentPerson) {
         // To verify if the current assumption of good/bad people is correct, we need to do 2 things.
         // 1. We need to verify the statements from all processed good people. What they said about the current person should be correct.
@@ -58,7 +59,7 @@ class Solution {
                 }
             }
         }
-        
+
         // 2. We need to verify the statement from the current person if he is good. What he said about other people should be correct.
         if (goodPeople[currentPerson]) {
             for (int i = 0; i < currentPerson; ++i) {
