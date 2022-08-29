3b65d37d1a2796281abdf34f7a78c811756cb61f
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:47 +0100

    Runtime: 12 ms (Top 87.36%) | Memory: 75.4 MB (Top 41.15%)

--- scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.java
+++ scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.java
@@ -1,3 +1,4 @@
+// Runtime: 12 ms (Top 87.36%) | Memory: 75.4 MB (Top 41.15%)
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int i = 0;
@@ -5,26 +6,25 @@ class Solution {
         int odd = 0;
         int result = 0;
         int temp = 0;
-        
-        
-        /* 
+
+        /*
             Approach : two pointer + sliding window technique
-            
+
             step 1 : we have fix i and moving j until our count of odd numbers == k
             step 2 : when(odd == count) we are counting every possible subarray by reducing the size of subarray from i
-            
+
         why temp?
         from reducing the size of subarray we will count all the possible subarray from between i and j
         but when i encounter a odd element the odd count will reduce and that while will stop executing
-        
+
         now there are two possible cases
         1.The leftover elements have a odd number
         2.The leftover elements do not have any odd number
-        
-        1. if our leftover elements have a odd number 
+
+        1. if our leftover elements have a odd number
                 then we cannot include our old possible subarrays into new possible subarrays because now new window for having odd == k is formed
                 that's why temp = 0;
-                
+
         2. if out leftover elements do not have any odd element left
             then our leftover elements must also take in consideration becuase they will also contribute in forming subarrays
         */
@@ -45,7 +45,7 @@ class Solution {
         //so include them
             result += temp;
             j++;
-            
+
         }
         return result;
     }
