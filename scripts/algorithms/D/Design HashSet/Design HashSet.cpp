f5f9889bb23ca45b392e8fb73c75beb82b93fff0
AnasImloul <pydev27@gmail.com>
2022-08-29 14:41:31 +0100

    Runtime: 365 ms (Top 20.28%) | Memory: 39.4 MB (Top 95.17%)

--- scripts/algorithms/D/Design HashSet/Design HashSet.cpp
+++ scripts/algorithms/D/Design HashSet/Design HashSet.cpp
@@ -1,25 +1,26 @@
+// Runtime: 365 ms (Top 20.28%) | Memory: 39.4 MB (Top 95.17%)
 class MyHashSet {
     vector<int> v;
 public:
     MyHashSet() {
-        
+
     }
-    
+
     void add(int key) {
         auto it = find(v.begin(), v.end(), key);
         if(it == v.end()){
-            v.push_back(key); 
+            v.push_back(key);
         }
-       
+
     }
-    
+
     void remove(int key) {
         auto it = find(v.begin(), v.end(), key);
         if(it != v.end()){
             v.erase(it);
         }
     }
-    
+
     bool contains(int key) {
         return find(v.begin(), v.end(), key) != v.end();
     }
