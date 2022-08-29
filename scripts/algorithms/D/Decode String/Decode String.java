b5723b9ca24a642beb29b77da60604afc903bbfd
AnasImloul <pydev27@gmail.com>
2022-08-29 14:30:44 +0100

    Runtime: 1 ms (Top 88.88%) | Memory: 41.8 MB (Top 72.85%)

--- scripts/algorithms/D/Decode String/Decode String.java
+++ scripts/algorithms/D/Decode String/Decode String.java
@@ -1,24 +1,25 @@
+// Runtime: 1 ms (Top 88.88%) | Memory: 41.8 MB (Top 72.85%)
 class Solution {
     public String decodeString(String s) {
-        
-		int bb = s.indexOf('['); // location of beginning bracket
+
+        int bb = s.indexOf('['); // location of beginning bracket
         int nbb = s.indexOf('[', bb + 1); // location of next beginning bracket
         int eb = s.indexOf(']'); // location of ending bracket
-        
-		int n = 0; // number of times to repeat
+
+        int n = 0; // number of times to repeat
         int nl = 1; // number of digits for n
         char nd; // next digit
-        
-		String insert = ""; // repeated string
+
+        String insert = ""; // repeated string
         String end = ""; // remainder of string after repeated portion
-        
-		while (bb != -1) { // while the string contains a beginning bracket
-            
-			while (nbb < eb && nbb > bb) { // while the next beginning bracket is before the ending bracket
+
+        while (bb != -1) { // while the string contains a beginning bracket
+
+            while (nbb < eb && nbb > bb) { // while the next beginning bracket is before the ending bracket
                 bb = nbb; // update location of beginning bracket
                 nbb = s.indexOf('[', bb + 1); // update location of next beginning bracket
             }
-			
+
             nl = 1; // reset length of n
             while (bb - nl >= 0) { // while there are characters in front of the beginning bracket
                 nd = s.charAt(bb - nl); // next digit
@@ -28,17 +29,17 @@ class Solution {
                 }
                 else break; // not an integer
             }
-			
+
             insert = s.substring(bb + 1, eb); // set repeated string
             end = s.substring(eb + 1); // set remainder of string
             s = s.substring(0, bb - nl + 1); // remove everything after the digits
-            
-			while (n > 0) {
+
+            while (n > 0) {
                 s += insert; // add repeated string n times
                 n--;
             }
             s += end; // add remainder of string
-			
+
             bb = s.indexOf('['); // new location of beginning bracket
             nbb = s.indexOf('[', bb + 1); // new location of next beginning bracket
             eb = s.indexOf(']'); // new location of ending bracket
