d1df609e56beb35d1edfe4bc40411946d6aed05c
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:25 +0100

    Runtime: 945 ms (Top 20.47%) | Memory: 96.9 MB (Top 96.62%)

--- scripts/algorithms/R/Reorder Routes to Make All Paths Lead to the City Zero/Reorder Routes to Make All Paths Lead to the City Zero.cpp
+++ scripts/algorithms/R/Reorder Routes to Make All Paths Lead to the City Zero/Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -1,3 +1,4 @@
+// Runtime: 945 ms (Top 20.47%) | Memory: 96.9 MB (Top 96.62%)
 class Solution {
 public:
     vector<int> Radj[50001],adj[50001] ,visited;
@@ -5,24 +6,24 @@ public:
         int edges = 0 ;
         queue<int> q ;
         q.push(0) ;
-        
+
         while(q.size()){
             auto src = q.front() ; q.pop() ;
             visited[src] = 1 ;
-            
+
             for(auto &nbr : adj[src]){
                 if(visited[nbr]) continue ;
                 //this connection needs reverse orientation
                 ++edges ;
                 q.push(nbr) ;
             }
-            
+
             for(auto &nbr : Radj[src]){
                 if(visited[nbr]) continue ;
                 q.push(nbr) ;
             }
         }
-        
+
         return edges ;
     }
     int minReorder(int n, vector<vector<int>>& connections) {
