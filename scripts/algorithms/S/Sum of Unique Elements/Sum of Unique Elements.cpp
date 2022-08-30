f393e2fe1338aac9a141c73326f5fdaad77093bf
AnasImloul <pydev27@gmail.com>
2022-08-30 11:08:09 +0100

    Runtime: 10 ms (Top 6.43%) | Memory: 8.1 MB (Top 24.14%)

--- scripts/algorithms/S/Sum of Unique Elements/Sum of Unique Elements.cpp
+++ scripts/algorithms/S/Sum of Unique Elements/Sum of Unique Elements.cpp
@@ -1,13 +1,14 @@
+// Runtime: 10 ms (Top 6.43%) | Memory: 8.1 MB (Top 24.14%)
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums)
     {
        int sum=0;
        map<int,int>mp;
-        
+
        for(auto x:nums)
        mp[x]++;
-        
+
        for(auto m:mp)
        {
            if(m.second==1)
