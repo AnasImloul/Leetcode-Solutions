7c1f040c0fc3b4d3c618b139f1670399eb15aa85
AnasImloul <pydev27@gmail.com>
2022-08-30 10:57:56 +0100

    Runtime: 281 ms (Top 46.37%) | Memory: 83.5 MB (Top 84.08%)

--- scripts/algorithms/S/Stone Game VIII/Stone Game VIII.cpp
+++ scripts/algorithms/S/Stone Game VIII/Stone Game VIII.cpp
@@ -1,14 +1,15 @@
+// Runtime: 281 ms (Top 46.37%) | Memory: 83.5 MB (Top 84.08%)
 class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
-        int prefix = 0; 
-        for (auto& x : stones) prefix += x; 
-        
-        int ans = prefix; 
+        int prefix = 0;
+        for (auto& x : stones) prefix += x;
+
+        int ans = prefix;
         for (int i = size(stones)-2; i >= 1; --i) {
-            prefix -= stones[i+1]; 
-            ans = max(ans, prefix - ans); 
+            prefix -= stones[i+1];
+            ans = max(ans, prefix - ans);
         }
-        return ans; 
+        return ans;
     }
 };
