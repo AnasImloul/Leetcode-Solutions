9431e1d0bd09b1fbb9bfb5c104dde99d428d2b1c
AnasImloul <pydev27@gmail.com>
2022-08-30 11:20:04 +0100

    Runtime: 1 ms (Top 58.67%) | Memory: 43.1 MB (Top 15.04%)

--- scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.java
+++ scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.java
@@ -1,16 +1,17 @@
+// Runtime: 1 ms (Top 58.67%) | Memory: 43.1 MB (Top 15.04%)
 class Solution {
     int count = 0;
         public int helper(TreeNode root)
         {
-        	if(root == null)
-        		return 0;
-        	int left = helper(root.left);
-        	int right = helper(root.right);
-        	count+= Math.abs(left)+Math.abs(right);
-        	return (left+right+root.val-1);
+            if(root == null)
+                return 0;
+            int left = helper(root.left);
+            int right = helper(root.right);
+            count+= Math.abs(left)+Math.abs(right);
+            return (left+right+root.val-1);
         }
         public int distributeCoins(TreeNode root) {
-        	helper(root);
+            helper(root);
             return count;
         }
 }
