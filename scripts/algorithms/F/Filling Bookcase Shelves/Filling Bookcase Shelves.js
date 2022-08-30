a999091fe2e81df9fc5552766693aa8020c0d0cf
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:48 +0100

    Runtime: 92 ms (Top 69.01%) | Memory: 42.4 MB (Top 98.59%)

--- scripts/algorithms/F/Filling Bookcase Shelves/Filling Bookcase Shelves.js
+++ scripts/algorithms/F/Filling Bookcase Shelves/Filling Bookcase Shelves.js
@@ -1,27 +1,28 @@
+// Runtime: 92 ms (Top 69.01%) | Memory: 42.4 MB (Top 98.59%)
 var minHeightShelves = function(books, shelfWidth) {
     let booksArr = []
-    
+
     for(let i = 0; i < books.length; i++) {
         let remainingWidth = shelfWidth - books[i][0]
         let bookHeight = books[i][1]
-        
+
         let maxHeight = bookHeight
-        
+
         let prevSum = booksArr[i - 1] !== undefined ? booksArr[i - 1] : 0
         let minSumHeight = bookHeight + prevSum
-        
+
         for(let x = i - 1; x >= 0 ; x--) {
-            
+
             let prevBookWidth = books[x][0]
             let prevBookHeight = books[x][1]
             if(remainingWidth - prevBookWidth < 0) break
             remainingWidth -= prevBookWidth
-            
+
             prevSum = booksArr[x - 1] !== undefined ? booksArr[x - 1] : 0
-            
+
             maxHeight = Math.max(maxHeight, prevBookHeight)
-    
-            minSumHeight = Math.min(prevSum  + maxHeight, minSumHeight)
+
+            minSumHeight = Math.min(prevSum + maxHeight, minSumHeight)
         }
         booksArr[i] = minSumHeight
     }
