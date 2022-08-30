9fb214994f13cd3d1d009fac955bf2b6dbf77b74
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:15 +0100

    Runtime: 85 ms (Top 46.15%) | Memory: 26.5 MB (Top 99.94%)

--- scripts/algorithms/S/Serialize and Deserialize Binary Tree/Serialize and Deserialize Binary Tree.cpp
+++ scripts/algorithms/S/Serialize and Deserialize Binary Tree/Serialize and Deserialize Binary Tree.cpp
@@ -1,7 +1,8 @@
+// Runtime: 85 ms (Top 46.15%) | Memory: 26.5 MB (Top 99.94%)
 TreeNode* ans;
 class Codec {
 public:
-    
+
     string serialize(TreeNode* root) {
         ans = root;
         return "";
