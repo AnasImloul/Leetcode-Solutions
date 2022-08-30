ff3c318f515be9db885be86989f39251b70d96df
AnasImloul <pydev27@gmail.com>
2022-08-30 11:19:56 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 44.5 MB (Top 10.41%)

--- scripts/algorithms/D/Delete Node in a Linked List/Delete Node in a Linked List.java
+++ scripts/algorithms/D/Delete Node in a Linked List/Delete Node in a Linked List.java
@@ -1,18 +1,19 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 44.5 MB (Top 10.41%)
 class Solution {
     public void deleteNode(ListNode node) {
-        
-        // 4    5    1    9   :  Node = 5
-        
+
+        // 4 5 1 9 : Node = 5
+
         node.val = node.next.val;
-        
+
         //Copy next node val to current node.
-        //4    1     1    9
-        //     ------------
-        
+        //4 1 1 9
+        // ------------
+
         //Point node.next = node.next.next
         // 4 -----> 1 ----> 9
-        
+
         node.next = node.next.next;
-        // 4     1    9
+        // 4 1 9
     }
 }
