378a3e0a21b90ece9bf5d4d6b7a5a909c208b93b
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:26 +0100

    Runtime: 3 ms (Top 74.29%) | Memory: 7.5 MB (Top 64.02%)

--- scripts/algorithms/R/Reverse Linked List II/Reverse Linked List II.cpp
+++ scripts/algorithms/R/Reverse Linked List II/Reverse Linked List II.cpp
@@ -1,16 +1,17 @@
+// Runtime: 3 ms (Top 74.29%) | Memory: 7.5 MB (Top 64.02%)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    
+
     ListNode* reverse(ListNode* head)
     {
         ListNode* prev = NULL;
@@ -23,28 +24,27 @@ public:
         }
         return prev;
     }
-    
+
     ListNode* reverseN(ListNode* head,int right)
     {
         ListNode* temp = head;
         while(right != 1)
             temp = temp->next,right--;
-        
+
         ListNode* temp2 = temp->next;
         temp->next = NULL;
-        
+
         ListNode* ans = reverse(head);
         head->next = temp2;
         return ans;
-        
+
     }
-    
+
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
+
           if(left == right)
               return head;
-          
-        
+
           if(left == 1)
           {
               return reverseN(head,right);
@@ -63,10 +63,8 @@ public:
               temp->next = temp2;
               return head;
           }
-        
+
          return NULL;
-        
-        
-        
+
     }
 };
