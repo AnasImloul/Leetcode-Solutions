b46f2b4967093ffb96ebe85e0086c14622061587
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:48 +0100

    Runtime: 509 ms (Top 12.12%) | Memory: 43.2 MB (Top 78.79%)

--- scripts/algorithms/C/Count Number of Maximum Bitwise-OR Subsets/Count Number of Maximum Bitwise-OR Subsets.js
+++ scripts/algorithms/C/Count Number of Maximum Bitwise-OR Subsets/Count Number of Maximum Bitwise-OR Subsets.js
@@ -1,17 +1,18 @@
+// Runtime: 509 ms (Top 12.12%) | Memory: 43.2 MB (Top 78.79%)
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var countMaxOrSubsets = function(nums) {
-    
+
     let n = nums.length;
     let len = Math.pow(2, n);
     let ans = 0;
     let hash = {};
-    
-    for (let i = 0; i < len; i++) { 
+
+    for (let i = 0; i < len; i++) {
         let tmp = 0;
-        for (let j = 0; j < n; j++) { 
+        for (let j = 0; j < n; j++) {
             if(i & (1 << j)) {
                 tmp |= nums[j];
             }
@@ -23,6 +24,6 @@ var countMaxOrSubsets = function(nums) {
         }
         ans = Math.max(ans, tmp);
     }
-    
+
     return hash[ans];
 };
