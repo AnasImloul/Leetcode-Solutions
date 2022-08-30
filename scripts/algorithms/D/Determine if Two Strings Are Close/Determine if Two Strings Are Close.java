416edaf373fb7d6e8da2de537c96362c99dd45b4
AnasImloul <pydev27@gmail.com>
2022-08-30 11:20:02 +0100

    Runtime: 24 ms (Top 51.63%) | Memory: 58.9 MB (Top 49.30%)

--- scripts/algorithms/D/Determine if Two Strings Are Close/Determine if Two Strings Are Close.java
+++ scripts/algorithms/D/Determine if Two Strings Are Close/Determine if Two Strings Are Close.java
@@ -1,14 +1,15 @@
+// Runtime: 24 ms (Top 51.63%) | Memory: 58.9 MB (Top 49.30%)
 class Solution {
     private int N = 26;
     public boolean closeStrings(String word1, String word2) {
-		// count the English letters
+        // count the English letters
         int[] arr1 = new int[N], arr2 = new int[N];
         for (char ch : word1.toCharArray())
             arr1[ch - 'a']++;
         for (char ch : word2.toCharArray())
             arr2[ch - 'a']++;
-		
-		// if one has a letter which another one doesn't have, dont exist
+
+        // if one has a letter which another one doesn't have, dont exist
         for (int i = 0; i < N; i++) {
             if (arr1[i] == arr2[i]) {
                 continue;
