e779a3507f6606040d53d4d268c978c483d52f53
AnasImloul <pydev27@gmail.com>
2022-08-30 11:07:53 +0100

    Runtime: 38 ms (Top 82.70%) | Memory: 13.7 MB (Top 45.37%)

--- scripts/algorithms/M/Max Points on a Line/Max Points on a Line.cpp
+++ scripts/algorithms/M/Max Points on a Line/Max Points on a Line.cpp
@@ -1,27 +1,28 @@
+// Runtime: 38 ms (Top 82.70%) | Memory: 13.7 MB (Top 45.37%)
 class Solution {
 public:
-    
-    //DP solution 
+
+    //DP solution
     //TC-O(N*N)
     //SC- ~ O(N*N)
-	//Custom Sort
+    //Custom Sort
     bool static cmp(vector<int> p1,vector<int> p2){
         if(p1[0]==p2[0])
             return p1[1]<p2[1];
         return p1[0]<p2[0];
     }
-	
-	//Slope Calculating
+
+    //Slope Calculating
     float calcSlope(int x1,int y1,int x2,int y2){
-       return (y2-y1)/(float)(x2-x1); 
+       return (y2-y1)/(float)(x2-x1);
     }
     int maxPoints(vector<vector<int>>& points) {
         if(points.size()==2 || points.size()==1)
             return points.size();
-        
+
         sort(points.begin(),points.end(),cmp);
         int ans=1;
-		//How much points having same slope at current index
+        //How much points having same slope at current index
         vector<unordered_map<float,int>> dp(points.size()+1);
         for(int i=1;i<points.size();i++){
             for(int j=0;j<i;j++){
@@ -34,9 +35,9 @@ public:
                 }
             }
         }
-		//return ans 
-        
+        //return ans
+
         return ans+1;
-        
+
     }
 };
