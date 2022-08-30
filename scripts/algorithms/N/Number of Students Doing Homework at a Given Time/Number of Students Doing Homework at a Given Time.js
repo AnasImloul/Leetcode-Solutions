11d399fce74a2d422aed75f22fd523777cf79a3b
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:53 +0100

    Runtime: 83 ms (Top 59.55%) | Memory: 41.9 MB (Top 67.73%)

--- scripts/algorithms/N/Number of Students Doing Homework at a Given Time/Number of Students Doing Homework at a Given Time.js
+++ scripts/algorithms/N/Number of Students Doing Homework at a Given Time/Number of Students Doing Homework at a Given Time.js
@@ -1,9 +1,10 @@
+// Runtime: 83 ms (Top 59.55%) | Memory: 41.9 MB (Top 67.73%)
 var busyStudent = function(startTime, endTime, queryTime) {
     let res = 0;
-    
+
     for (let i = 0; i < startTime.length; i++) {
         if (startTime[i] <= queryTime && endTime[i] >= queryTime) res++;
     }
-    
+
     return res;
 };
