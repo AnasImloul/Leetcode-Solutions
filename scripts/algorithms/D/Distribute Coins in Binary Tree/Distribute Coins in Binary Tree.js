b0ed4454e61af26d819e01de44596bd3af2fd3bd
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:52 +0100

    Runtime: 138 ms (Top 8.82%) | Memory: 44.3 MB (Top 85.29%)

--- scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.js
+++ scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.js
@@ -1,17 +1,18 @@
+// Runtime: 138 ms (Top 8.82%) | Memory: 44.3 MB (Top 85.29%)
 var distributeCoins = function(root) {
     var moves = 0;
     postorder(root);
     return moves;
-    
+
     function postorder(node){
         if(!node)
             return 0;
-        
+
         const subTotal = postorder(node.left) + postorder(node.right);
         const result = node.val - 1 + subTotal;
         moves += Math.abs(result);
-        
+
         return result;
-            
+
     }
 };
