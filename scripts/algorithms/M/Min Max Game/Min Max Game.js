c22a2539a89b7ca5b8fb510647e342df57ac2c3d
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:47 +0100

    Runtime: 105 ms (Top 27.62%) | Memory: 42 MB (Top 97.79%)

--- scripts/algorithms/M/Min Max Game/Min Max Game.js
+++ scripts/algorithms/M/Min Max Game/Min Max Game.js
@@ -1,11 +1,12 @@
+// Runtime: 105 ms (Top 27.62%) | Memory: 42 MB (Top 97.79%)
 var minMaxGame = function(nums) {
     while (nums.length > 1) {
         let half = nums.length / 2;
         for (let i = 0; i < half; i++)
             nums[i] = i % 2 === 0 ? Math.min(nums[2 * i], nums[2 * i + 1]) : Math.max(nums[2 * i], nums[2 * i + 1]);
-        
+
         nums.length = half;
     }
-    
+
     return nums[0];
 };
