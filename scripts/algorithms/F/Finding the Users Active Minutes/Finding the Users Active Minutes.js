958dff7e5545fec5b5bdbb16813ca48b399d2e39
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:44 +0100

    Runtime: 457 ms (Top 37.59%) | Memory: 71.1 MB (Top 76.60%)

--- scripts/algorithms/F/Finding the Users Active Minutes/Finding the Users Active Minutes.js
+++ scripts/algorithms/F/Finding the Users Active Minutes/Finding the Users Active Minutes.js
@@ -1,16 +1,17 @@
+// Runtime: 457 ms (Top 37.59%) | Memory: 71.1 MB (Top 76.60%)
 var findingUsersActiveMinutes = function(logs, k) {
     const map = new Map();
-    
+
     for (const [userID, minute] of logs) {
         if (!map.has(userID)) map.set(userID, new Set());
         map.get(userID).add(minute);
     }
-    
+
     const count = new Array(k).fill(0);
-    
+
     for (const actions of map.values()) {
         count[actions.size - 1]++;
     }
-    
+
     return count;
 };
