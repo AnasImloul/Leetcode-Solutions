d5e989e08caa4b29fcab1969354735bdcef083ce
AnasImloul <pydev27@gmail.com>
2022-08-30 11:08:07 +0100

    Runtime: 4 ms (Top 94.55%) | Memory: 6 MB (Top 90.93%)

--- scripts/algorithms/U/Ugly Number II/Ugly Number II.cpp
+++ scripts/algorithms/U/Ugly Number II/Ugly Number II.cpp
@@ -1,17 +1,18 @@
+// Runtime: 4 ms (Top 94.55%) | Memory: 6 MB (Top 90.93%)
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        
+
         int dp[n+1];
         dp[1] = 1;
-        
+
         int p2 = 1, p3 = 1, p5 = 1;
         int t;
-        
+
         for(int i = 2; i < n+1; i++){
             t = min(dp[p2]*2, min(dp[p3]*3, dp[p5]*5));
             dp[i] = t;
-            
+
             if(dp[i] == dp[p2]*2){
                 p2++;
             }
@@ -23,6 +24,6 @@ public:
             }
         }
         return dp[n];
-        
+
     }
 };
