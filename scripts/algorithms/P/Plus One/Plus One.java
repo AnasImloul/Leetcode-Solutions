2e329feaea17043d64c1afeffc340d8a616d466e
AnasImloul <pydev27@gmail.com>
2022-08-30 11:20:04 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40.5 MB (Top 96.61%)

--- scripts/algorithms/P/Plus One/Plus One.java
+++ scripts/algorithms/P/Plus One/Plus One.java
@@ -1,14 +1,15 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40.5 MB (Top 96.61%)
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+
         int len = digits.length;
-        
+
         //last digit not a 9, just add 1 to it
         if(digits[len - 1] != 9){
             digits[len - 1] = digits[len - 1] + 1;
             return digits;
         }
-        
+
         //last digit is a 9, find the closest digit that is not a 9
         else{
             int i = len - 1;
@@ -26,7 +27,7 @@ class Solution {
             }
             digits[i] = digits[i] + 1;
         }
-        
+
         return digits;
     }
 }
