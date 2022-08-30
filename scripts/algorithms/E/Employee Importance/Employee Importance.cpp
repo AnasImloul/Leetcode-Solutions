27fda51a08fafdeeb4822b98c4d6fb6071bc32cc
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:23 +0100

    Runtime: 82 ms (Top 10.64%) | Memory: 15.7 MB (Top 29.77%)

--- scripts/algorithms/E/Employee Importance/Employee Importance.cpp
+++ scripts/algorithms/E/Employee Importance/Employee Importance.cpp
@@ -1,10 +1,11 @@
+// Runtime: 82 ms (Top 10.64%) | Memory: 15.7 MB (Top 29.77%)
 class Solution {
 public:
     int ans=0;
     void helper(unordered_map<int,pair<int,vector<int>>> &res,int id,vector<int> &dp)
     {
         if(dp[id]==-1)
-        {ans+=res[id].first;dp[id]=1;}//  employees[id-1]->importance;dp[id-1]=1;}
+        {ans+=res[id].first;dp[id]=1;}// employees[id-1]->importance;dp[id-1]=1;}
         else return;
         for(int i=0;i<res[id].second.size();i++)
             helper(res,res[id].second[i],dp);
