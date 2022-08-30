138fda2840b4ed8ac32125ace9f3d1fb797e6535
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:50 +0100

    Runtime: 104 ms (Top 50.41%) | Memory: 44.1 MB (Top 53.95%)

--- scripts/algorithms/N/Next Greater Element I/Next Greater Element I.js
+++ scripts/algorithms/N/Next Greater Element I/Next Greater Element I.js
@@ -1,3 +1,4 @@
+// Runtime: 104 ms (Top 50.41%) | Memory: 44.1 MB (Top 53.95%)
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
@@ -9,20 +10,20 @@ var nextGreaterElement = function(nums1, nums2) {
     for(let i = 0; i < nums2.length; i++){
         indexMap.set(nums2[i], i)
     }
-    
+
     // Stores the next greatest elements
     let result = []
-    
+
     // Iterate through all the numbers of interest. Remember that all numbers in nums1 are present in nums2.
     for(let num of nums1){
-        
+
         // Check to see those numbers were present in the nums2 indexMap
         if(indexMap.has(num)){
-            
+
             // If they were, we must find the next greatest element.
             // Set it to -1 by default in case we cannot find it.
             let nextGreatest = -1
-            
+
             // Iterate through all numbers in nums2 to the right of the index of the target number. (index of the target + 1)
             for(let i = indexMap.get(num) + 1; i < nums2.length; i++){
                 // Check to see if the current number is greater than the target.
@@ -37,7 +38,7 @@ var nextGreaterElement = function(nums1, nums2) {
             result.push(nextGreatest)
         }
     }
-    
+
     // Return the array of next greatest elements.
     return result
 };
