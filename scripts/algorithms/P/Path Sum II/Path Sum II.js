af0f3011d84c364cffc0c673899d581d24818c09
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:49 +0100

    Runtime: 173 ms (Top 6.43%) | Memory: 52.7 MB (Top 31.75%)

--- scripts/algorithms/P/Path Sum II/Path Sum II.js
+++ scripts/algorithms/P/Path Sum II/Path Sum II.js
@@ -1,16 +1,17 @@
+// Runtime: 173 ms (Top 6.43%) | Memory: 52.7 MB (Top 31.75%)
 var pathSum = function(root, targetSum) {
-  const paths = []; 
-  
+  const paths = [];
+
   function dfs(root, sum, curr = []) {
     if (!root) return;
-    
+
     const newCurr = [...curr, root.val];
     if (!root.left && !root.right && sum === root.val) return paths.push(newCurr);
-    
+
     dfs(root.left, sum - root.val, newCurr);
     dfs(root.right, sum - root.val, newCurr);
   }
-  
+
   dfs(root, targetSum);
   return paths;
 };
