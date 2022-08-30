74669542fb41567bcac87302ca07b239d554035f
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:22 +0100

    Runtime: 4 ms (Top 91.36%) | Memory: 7.1 MB (Top 46.74%)

--- scripts/algorithms/I/Isomorphic Strings/Isomorphic Strings.cpp
+++ scripts/algorithms/I/Isomorphic Strings/Isomorphic Strings.cpp
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 91.36%) | Memory: 7.1 MB (Top 46.74%)
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
@@ -12,11 +13,11 @@ public:
             }
             else{
                 if(mp[s[i]]!=t[i]) return false;
-                
+
             }
-            
+
         }
         return true;
-        
+
     }
 };
