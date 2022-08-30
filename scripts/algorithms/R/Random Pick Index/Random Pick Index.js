b4c663d4766066b66ccfc6c1d7214cb674d385e5
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:50 +0100

    Runtime: 412 ms (Top 30.40%) | Memory: 89.1 MB (Top 48.00%)

--- scripts/algorithms/R/Random Pick Index/Random Pick Index.js
+++ scripts/algorithms/R/Random Pick Index/Random Pick Index.js
@@ -1,16 +1,17 @@
+// Runtime: 412 ms (Top 30.40%) | Memory: 89.1 MB (Top 48.00%)
 var Solution = function(nums) {
-	this.map = nums.reduce((result, num, index) => {
-		const value = result.get(num) ?? [];
+    this.map = nums.reduce((result, num, index) => {
+        const value = result.get(num) ?? [];
 
-		value.push(index);
-		result.set(num, value);
-		return result;
-	}, new Map());
+        value.push(index);
+        result.set(num, value);
+        return result;
+    }, new Map());
 };
 
 Solution.prototype.pick = function(target) {
-	const pick = this.map.get(target);
-	const random = Math.random() * pick.length | 0;
+    const pick = this.map.get(target);
+    const random = Math.random() * pick.length | 0;
 
-	return pick[random];
+    return pick[random];
 };
