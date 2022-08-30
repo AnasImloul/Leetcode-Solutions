d494ce86bd90bc85a1c57a5911c796655b7e7640
AnasImloul <pydev27@gmail.com>
2022-08-30 11:20:01 +0100

    Runtime: 1 ms (Top 90.50%) | Memory: 50.7 MB (Top 48.08%)

--- scripts/algorithms/R/Reshape the Matrix/Reshape the Matrix.java
+++ scripts/algorithms/R/Reshape the Matrix/Reshape the Matrix.java
@@ -1,15 +1,16 @@
+// Runtime: 1 ms (Top 90.50%) | Memory: 50.7 MB (Top 48.08%)
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        
+
         if (r * c != mat.length * mat[0].length) {
             return mat;
         }
-		
+
         int[][] ans = new int[r][c];
-        
+
         int i = 0;
         int j = 0;
-        
+
         for(int k = 0; k < mat.length; k++) {
             for(int l = 0; l < mat[0].length; l++) {
                 if (j == c) {
