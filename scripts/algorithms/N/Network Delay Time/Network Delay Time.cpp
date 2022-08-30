31bea1abf2b129caeb4412435c4a13755ace220f
AnasImloul <pydev27@gmail.com>
2022-08-30 10:57:57 +0100

    Runtime: 220 ms (Top 41.81%) | Memory: 44.6 MB (Top 18.95%)

--- scripts/algorithms/N/Network Delay Time/Network Delay Time.cpp
+++ scripts/algorithms/N/Network Delay Time/Network Delay Time.cpp
@@ -1,3 +1,4 @@
+// Runtime: 220 ms (Top 41.81%) | Memory: 44.6 MB (Top 18.95%)
 class cmp{
   public:
     bool operator()(pair<int,int> &a,pair<int,int> &b)
@@ -14,23 +15,23 @@ public:
         {
             a[it[0]-1].push_back({it[1]-1,it[2]});
         }
-        
+
         priority_queue<pair<int,int>,vector<pair<int,int>>,cmp> pq;
         vector<int> dist(n,1e7);
-        
+
         pq.push({k-1,0});
-        
+
         vector<int> vis(n,0);
         while(pq.size())
         {
             auto curr = pq.top();
             pq.pop();
-            
+
             int v = curr.first;
             int w = curr.second;
-            
+
             if(vis[v])continue;
-            
+
             vis[v] = 1;
             dist[v] = w;
             for(auto it:a[v])
@@ -41,13 +42,13 @@ public:
                 }
             }
         }
-        
+
         int mx = INT_MIN;
         for(auto it:dist)
             mx = max(mx,it);
-        
+
         if(mx>=1e7)return -1;
         return mx;
-        
+
     }
 };
