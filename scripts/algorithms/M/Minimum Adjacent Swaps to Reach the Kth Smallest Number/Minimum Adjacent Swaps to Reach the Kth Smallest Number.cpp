670bd555d51713546b6c6e7b1cf538c2c0b92ee4
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:12 +0100

    Runtime: 63 ms (Top 28.88%) | Memory: 6.7 MB (Top 20.04%)

--- scripts/algorithms/M/Minimum Adjacent Swaps to Reach the Kth Smallest Number/Minimum Adjacent Swaps to Reach the Kth Smallest Number.cpp
+++ scripts/algorithms/M/Minimum Adjacent Swaps to Reach the Kth Smallest Number/Minimum Adjacent Swaps to Reach the Kth Smallest Number.cpp
@@ -1,18 +1,19 @@
+// Runtime: 63 ms (Top 28.88%) | Memory: 6.7 MB (Top 20.04%)
 class Solution {
 public:
-    
+
     // GREEDY APPROACH
     // min steps to make strings equal
-    
+
     int minSteps(string s1, string s2) {
         int size = s1.length();
         int i = 0, j = 0;
         int result = 0;
-  
+
         while (i < size) {
             j = i;
             while (s1[j] != s2[i]) j++;
-			
+
             while (i < j) {
                 swap(s1[j], s1[j-1]);
                 j--;
@@ -22,14 +23,14 @@ public:
         }
         return result;
     }
-    
+
     int getMinSwaps(string num, int k) {
         string original = num;
-        
+
         while(k--) {
             next_permutation(num.begin(), num.end());
         }
-        
+
         return minSteps(original, num);
     }
 };
