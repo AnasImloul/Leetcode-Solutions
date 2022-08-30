739b76e958db01db14424648161fb846fa4034e1
AnasImloul <pydev27@gmail.com>
2022-08-30 10:57:50 +0100

    Runtime: 36 ms (Top 27.23%) | Memory: 24.3 MB (Top 34.42%)

--- scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.cpp
+++ scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.cpp
@@ -1,9 +1,10 @@
+// Runtime: 36 ms (Top 27.23%) | Memory: 24.3 MB (Top 34.42%)
 class Solution {
 public:
     void findInorder(TreeNode* root, vector<int> &ans) {
         if(root == NULL)
             return;
-        
+
         findInorder(root->left, ans);
         ans.push_back(root->val);
         findInorder(root->right, ans);
