8f3fd2f858caf72d5fd02298ce9225cd744da039
AnasImloul <pydev27@gmail.com>
2022-08-30 11:27:47 +0100

    Runtime: 117 ms (Top 41.38%) | Memory: 44.1 MB (Top 58.62%)

--- scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.js
+++ scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.js
@@ -1,3 +1,4 @@
+// Runtime: 117 ms (Top 41.38%) | Memory: 44.1 MB (Top 58.62%)
 /**
  * @param {string} s
  * @param {number} k
@@ -9,18 +10,16 @@ var longestSubsequence = function(s, k) {
     let j = s.length - 1; // starting from the last digit
     let i = 0; // binary number position
     let acc = 0;
-    
+
     while(j >= 0){
         let positionNumber = Number(s[j]) * Math.pow(2, i);
         j--;
         i++;
         if(acc + positionNumber > k) continue;
 
-		acc += positionNumber;
-		count++;
+        acc += positionNumber;
+        count++;
     }
 
     return count;
 };
-
-
