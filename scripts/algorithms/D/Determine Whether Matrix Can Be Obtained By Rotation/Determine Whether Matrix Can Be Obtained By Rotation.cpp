846717c7cfc59ba86c5b5b97861690844b6517fb
AnasImloul <pydev27@gmail.com>
2022-08-31 00:26:08 +0100

    Runtime: 7 ms (Top 66.25%) | Memory: 11.2 MB (Top 34.25%)

--- scripts/algorithms/D/Determine Whether Matrix Can Be Obtained By Rotation/Determine Whether Matrix Can Be Obtained By Rotation.cpp
+++ scripts/algorithms/D/Determine Whether Matrix Can Be Obtained By Rotation/Determine Whether Matrix Can Be Obtained By Rotation.cpp
@@ -1,21 +1,22 @@
+// Runtime: 7 ms (Top 66.25%) | Memory: 11.2 MB (Top 34.25%)
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n = mat.size();
-        if(mat == target) {     // rotation by 0 degree.
+        if(mat == target) { // rotation by 0 degree.
             return true;
         }
-        
-        int deg = 3;    // more rotations with 90, 180, 270 degree's.
-        
+
+        int deg = 3; // more rotations with 90, 180, 270 degree's.
+
         while(deg --) {
             for(int i = 0; i < n; i ++) {
                 for(int j = i; j < n; j ++) {
-                    swap(mat[i][j], mat[j][i]);     // transpose of matrix.
+                    swap(mat[i][j], mat[j][i]); // transpose of matrix.
                 }
             }
             for(int i = 0; i < n; i ++) {
-                reverse(mat[i].begin(),mat[i].end());       // reverse each row.
+                reverse(mat[i].begin(),mat[i].end()); // reverse each row.
             }
             if(mat == target) {
                 return true;
