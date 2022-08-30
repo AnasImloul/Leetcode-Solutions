4f430100fc62b420ce9b738c06de8c96299234f7
AnasImloul <pydev27@gmail.com>
2022-08-30 11:07:55 +0100

    Runtime: 55 ms (Top 25.45%) | Memory: 44.9 MB (Top 5.73%)

--- scripts/algorithms/M/Minimum Number of Operations to Reinitialize a Permutation/Minimum Number of Operations to Reinitialize a Permutation.cpp
+++ scripts/algorithms/M/Minimum Number of Operations to Reinitialize a Permutation/Minimum Number of Operations to Reinitialize a Permutation.cpp
@@ -1,3 +1,4 @@
+// Runtime: 55 ms (Top 25.45%) | Memory: 44.9 MB (Top 5.73%)
 class Solution {
 public:
     vector<int> change(vector<int>arr,vector<int>v){
@@ -18,17 +19,17 @@ public:
             v[i]=i;
         }
         vector<int>arr(n);
-        
+
         arr=change(arr,v);
         if(arr==v){return 1;}
-       
+
         int cnt=1;
-        
+
         while(arr!=v){
             arr=change(arr,arr);
             cnt++;
             if(arr==v){return cnt;}
-           
+
         }
         return cnt;
     }
