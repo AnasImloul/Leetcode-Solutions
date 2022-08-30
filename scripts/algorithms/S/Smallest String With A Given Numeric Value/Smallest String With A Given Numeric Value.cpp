e100f50c92d22b8bac3bd965786a1300dea6c2d2
AnasImloul <pydev27@gmail.com>
2022-08-30 11:08:03 +0100

    Runtime: 70 ms (Top 48.18%) | Memory: 21.1 MB (Top 93.21%)

--- scripts/algorithms/S/Smallest String With A Given Numeric Value/Smallest String With A Given Numeric Value.cpp
+++ scripts/algorithms/S/Smallest String With A Given Numeric Value/Smallest String With A Given Numeric Value.cpp
@@ -1,9 +1,10 @@
+// Runtime: 70 ms (Top 48.18%) | Memory: 21.1 MB (Top 93.21%)
 class Solution {
 public:
     string getSmallestString(int n, int k) {
         string res(n, 'a');
         k -= n;
-        
+
         int i = res.size() - 1;
         while (k > 0) {
             int tmp = min(k, 25);
@@ -11,7 +12,7 @@ public:
             k -= tmp;
             --i;
         }
-        
+
         return res;
     }
 };
