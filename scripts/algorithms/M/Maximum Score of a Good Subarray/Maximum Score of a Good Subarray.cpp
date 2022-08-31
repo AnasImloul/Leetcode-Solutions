5af198e2cc58bba9bede837f9caaad569438e53c
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:05 +0100

    Runtime: 373 ms (Top 42.29%) | Memory: 91.1 MB (Top 60.46%)

--- scripts/algorithms/M/Maximum Score of a Good Subarray/Maximum Score of a Good Subarray.cpp
+++ scripts/algorithms/M/Maximum Score of a Good Subarray/Maximum Score of a Good Subarray.cpp
@@ -1,18 +1,19 @@
+// Runtime: 373 ms (Top 42.29%) | Memory: 91.1 MB (Top 60.46%)
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        nums.push_back(0); 
+        nums.push_back(0);
         stack<int> st ;
-        int n = nums.size(), res = 0; 
+        int n = nums.size(), res = 0;
         for(int i=0; i<n ; i++){
             while(!st.empty() && nums[st.top()] >= nums[i]){
                 int height = nums[st.top()];
-                st.pop(); 
-                int left = st.empty() ? -1: st.top(); 
+                st.pop();
+                int left = st.empty() ? -1: st.top();
                 if(k < i && k > left) res = max(height* (i-left-1), res);
             }
-            st.push(i); 
+            st.push(i);
         }
-        return res; 
+        return res;
     }
 };
