0a4c965ac9ed4efdef88ecd5bc3a979295c924c3
AnasImloul <pydev27@gmail.com>
2022-08-31 23:07:53 +0100

    Runtime: 49 ms (Top 24.43%) | Memory: 65.2 MB (Top 52.71%)

--- scripts/algorithms/N/Network Delay Time/Network Delay Time.java
+++ scripts/algorithms/N/Network Delay Time/Network Delay Time.java
@@ -1,3 +1,4 @@
+// Runtime: 49 ms (Top 24.43%) | Memory: 65.2 MB (Top 52.71%)
 class Solution {
     HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
     public int networkDelayTime(int[][] times, int n, int k) {
@@ -8,20 +9,20 @@ class Solution {
             map.get(times[i][0]).put(times[i][1], times[i][2]);
         }
         int ans = BFS(k, n);
-        
+
         return ans == 1000 ? -1 : ans;
     }
     public int BFS(int k, int n) {
         LinkedList<Integer> queue = new LinkedList<>();
-        
+
         int[] timeReach = new int[n + 1];
         Arrays.fill(timeReach, 1000);
         timeReach[0] = 0;
-        
+
         timeReach[k] = 0;
-        
+
         queue.add(k);
-        
+
         while (!queue.isEmpty()) {
             int rv = queue.remove();
             for (int nbrs : map.get(rv).keySet()) {
@@ -32,13 +33,13 @@ class Solution {
                 }
             }
         }
-        
+
         int time = 0;
-        
+
         for (int i : timeReach) {
             time = Math.max(i, time);
         }
-        
+
         return time;
     }
 }
