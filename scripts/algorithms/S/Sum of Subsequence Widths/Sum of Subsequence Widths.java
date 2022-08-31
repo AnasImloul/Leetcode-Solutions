3585dd9cdee3af3f58e0f1f976f04ecf00ccf25c
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:21 +0100

    Runtime: 49 ms (Top 70.11%) | Memory: 74.1 MB (Top 69.83%)

--- scripts/algorithms/S/Sum of Subsequence Widths/Sum of Subsequence Widths.java
+++ scripts/algorithms/S/Sum of Subsequence Widths/Sum of Subsequence Widths.java
@@ -1,8 +1,9 @@
+// Runtime: 49 ms (Top 70.11%) | Memory: 74.1 MB (Top 69.83%)
 class Solution {
     public int sumSubseqWidths(int[] nums) {
         int MOD = (int)1e9 + 7;
         Arrays.sort(nums);
-        
+
         long ans = 0;
         long p = 1;
         for(int i = 0; i < nums.length; i++){
