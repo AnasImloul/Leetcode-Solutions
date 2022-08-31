992f17fc41e81a4279819d2d70a07fe822ff6065
AnasImloul <pydev27@gmail.com>
2022-08-31 02:01:59 +0100

    Runtime: 123 ms (Top 25.53%) | Memory: 43.7 MB (Top 74.47%)

--- scripts/algorithms/P/Partition Labels/Partition Labels.js
+++ scripts/algorithms/P/Partition Labels/Partition Labels.js
@@ -1,12 +1,13 @@
+// Runtime: 123 ms (Top 25.53%) | Memory: 43.7 MB (Top 74.47%)
 var partitionLabels = function(s) {
     let last = 0, first = 0;
-    
+
     let arr = [...new Set(s)], ss = [];
     let temp = '';
-    
+
     first = s.indexOf(arr[0]);
     last = s.lastIndexOf(arr[0]);
-    
+
     for(let i = 1; i<arr.length; i++){
         if(s.indexOf(arr[i]) < last){
             if(last < s.lastIndexOf(arr[i])){
@@ -20,7 +21,7 @@ var partitionLabels = function(s) {
             last = s.lastIndexOf(arr[i]);
         }
     }
-    
+
     temp = s.slice(first,last+1);
     ss.push(temp.length);
     return ss;
