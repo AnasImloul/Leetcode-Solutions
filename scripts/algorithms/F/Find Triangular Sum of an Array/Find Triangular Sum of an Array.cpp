cbe4a8601d38172b056ff8161201d4e78f77020f
AnasImloul <pydev27@gmail.com>
2022-08-31 22:50:56 +0100

    Runtime: 404 ms (Top 62.14%) | Memory: 18.6 MB (Top 88.13%)

--- scripts/algorithms/F/Find Triangular Sum of an Array/Find Triangular Sum of an Array.cpp
+++ scripts/algorithms/F/Find Triangular Sum of an Array/Find Triangular Sum of an Array.cpp
@@ -1,10 +1,11 @@
+// Runtime: 404 ms (Top 62.14%) | Memory: 18.6 MB (Top 88.13%)
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
         int n=nums.size();
         for(int i=n-1;i>=1;i--){
             for(int j=0;j<i;j++){
-                nums[j]=(nums[j]+nums[j+1])%10;                
+                nums[j]=(nums[j]+nums[j+1])%10;
             }
         }
         return nums[0];
