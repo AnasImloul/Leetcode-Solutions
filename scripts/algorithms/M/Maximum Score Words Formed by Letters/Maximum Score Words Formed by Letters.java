78af86feb6b65752bdc339d1f03d804cb0998199
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:00 +0100

    Runtime: 10 ms (Top 32.14%) | Memory: 44.3 MB (Top 16.39%)

--- scripts/algorithms/M/Maximum Score Words Formed by Letters/Maximum Score Words Formed by Letters.java
+++ scripts/algorithms/M/Maximum Score Words Formed by Letters/Maximum Score Words Formed by Letters.java
@@ -1,3 +1,4 @@
+// Runtime: 10 ms (Top 32.14%) | Memory: 44.3 MB (Top 16.39%)
 class Solution {
     int[] memo;
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
@@ -14,13 +15,13 @@ class Solution {
         //for(int i : memo) System.out.println(i);
         return res;
     }
-    
+
     public int dp(String[] words, Map<Character,Integer> hm, int[] score, int index, int cs){//cs-current Score
         if(index==words.length) return cs;
         if(memo[index]!=-1) return memo[index];
         HashMap<Character,Integer> temp = new HashMap<>(hm);
-        int tcs=cs; //tcs = temporory current score 
-        
+        int tcs=cs; //tcs = temporory current score
+
         for(char c : words[index].toCharArray()){
             int t = temp.getOrDefault(c,0);
             t--;
