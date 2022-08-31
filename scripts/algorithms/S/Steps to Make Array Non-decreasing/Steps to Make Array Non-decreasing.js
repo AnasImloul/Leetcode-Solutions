cd0b0ed4752663d1edba619fdc778f7d1fc6a4c4
AnasImloul <pydev27@gmail.com>
2022-08-31 23:01:02 +0100

    Runtime: 192 ms (Top 48.39%) | Memory: 61.2 MB (Top 67.74%)

--- scripts/algorithms/S/Steps to Make Array Non-decreasing/Steps to Make Array Non-decreasing.js
+++ scripts/algorithms/S/Steps to Make Array Non-decreasing/Steps to Make Array Non-decreasing.js
@@ -1,14 +1,15 @@
+// Runtime: 192 ms (Top 48.39%) | Memory: 61.2 MB (Top 67.74%)
 var totalSteps = function(nums) {
-	let stack = [],
-		dp = new Array(nums.length).fill(0),
-		max = 0
+    let stack = [],
+        dp = new Array(nums.length).fill(0),
+        max = 0
 
-	for (let i = nums.length - 1; i >= 0; i--) {
-		while (stack.length > 0 && nums[i] > nums[stack[stack.length - 1]]) {
-			dp[i] = Math.max(++dp[i], dp[stack.pop()])
-			max = Math.max(dp[i], max)
-		}
-		stack.push(i)
-	}
-	return max
+    for (let i = nums.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && nums[i] > nums[stack[stack.length - 1]]) {
+            dp[i] = Math.max(++dp[i], dp[stack.pop()])
+            max = Math.max(dp[i], max)
+        }
+        stack.push(i)
+    }
+    return max
 };
