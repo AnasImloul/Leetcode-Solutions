529821ba2b5af4491015f51212fc469c729cb64d
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:24 +0100

    Runtime: 79 ms (Top 79.05%) | Memory: 43.6 MB (Top 56.48%)

--- scripts/algorithms/U/Unique Morse Code Words/Unique Morse Code Words.js
+++ scripts/algorithms/U/Unique Morse Code Words/Unique Morse Code Words.js
@@ -1,21 +1,22 @@
+// Runtime: 79 ms (Top 79.05%) | Memory: 43.6 MB (Top 56.48%)
 var uniqueMorseRepresentations = function(words) {
     var morse = [".-","-...","-.-.","-..",".","..-.",
                   "--.","....","..",".---",
                   "-.-",".-..","--","-.","---",".--.",
                   "--.-",".-.","...","-","..-","...-",
                   ".--","-..-","-.--","--.."];
-    
+
     var transformations = new Set();
-    
-    for (let word of words) {        
+
+    for (let word of words) {
         var trans = "";
         for (let letter of word) {
             var index = letter.charCodeAt(0) - 97;
             trans += morse[index];
         }
-        
-        transformations.add(trans);   
+
+        transformations.add(trans);
     }
-    
+
     return transformations.size;
 };
