82780321b8eaf100a0aefcc59d3f8610c19965cd
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:00 +0100

    Runtime: 116 ms (Top 28.95%) | Memory: 42.6 MB (Top 45.11%)

--- scripts/algorithms/S/String Matching in an Array/String Matching in an Array.js
+++ scripts/algorithms/S/String Matching in an Array/String Matching in an Array.js
@@ -1,18 +1,19 @@
+// Runtime: 116 ms (Top 28.95%) | Memory: 42.6 MB (Top 45.11%)
 var stringMatching = function(words) {
     let res = [];
-    
+
     for (let i = 0; i < words.length; i++) {
-        
+
         for (let j = 0; j < words.length; j++) {
             if (j === i) continue;
-            
+
             if (words[j].includes(words[i])) {
                 res.push(words[i]);
                 break;
             }
-            
+
         }
     }
-    
+
     return res;
 };
