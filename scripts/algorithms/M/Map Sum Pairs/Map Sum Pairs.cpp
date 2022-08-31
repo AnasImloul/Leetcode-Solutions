2c3f1c1131ca22a664faea80f51d91bd8ed42876
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:01 +0100

    Runtime: 4 ms (Top 66.87%) | Memory: 7.9 MB (Top 80.77%)

--- scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.cpp
+++ scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.cpp
@@ -1,23 +1,24 @@
+// Runtime: 4 ms (Top 66.87%) | Memory: 7.9 MB (Top 80.77%)
 class MapSum {
 public:
-	map<string,int>mp;
-	MapSum() {
+    map<string,int>mp;
+    MapSum() {
 
-	}
+    }
 
-	void insert(string key, int val) {
-		mp[key]=val;
-	}
+    void insert(string key, int val) {
+        mp[key]=val;
+    }
 
-	int sum(string prefix) {
-		int count=0;
-		for(auto i:mp){
-			int j;
-			for(j=0;j<i.first.size() && j<prefix.size();j++){
-				if(i.first[j]!=prefix[j])break;
-			}
-			if(j==prefix.size())count+=i.second;
-		}
-		return count;
-	}
+    int sum(string prefix) {
+        int count=0;
+        for(auto i:mp){
+            int j;
+            for(j=0;j<i.first.size() && j<prefix.size();j++){
+                if(i.first[j]!=prefix[j])break;
+            }
+            if(j==prefix.size())count+=i.second;
+        }
+        return count;
+    }
 };
