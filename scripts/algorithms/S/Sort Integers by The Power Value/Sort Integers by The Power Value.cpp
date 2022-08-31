29a20184f56cce61490ce59bc9d144d7c1bf0c96
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:00 +0100

    Runtime: 93 ms (Top 57.79%) | Memory: 12.6 MB (Top 53.41%)

--- scripts/algorithms/S/Sort Integers by The Power Value/Sort Integers by The Power Value.cpp
+++ scripts/algorithms/S/Sort Integers by The Power Value/Sort Integers by The Power Value.cpp
@@ -1,9 +1,10 @@
+// Runtime: 93 ms (Top 57.79%) | Memory: 12.6 MB (Top 53.41%)
 class Solution {
 public:
     int getPower(int num) {
         if (num == 1)
             return 0;
-        
+
         int res = 0;
         if (num %2 == 0)
             res += getPower(num/2);
@@ -12,7 +13,7 @@ public:
         res++;
         return res;
     }
-    
+
     int getKth(int lo, int hi, int k) {
         multimap<int,int> um;
         for (int i = lo; i <= hi; i++) {
