57df0c9e0cb792e625de2e042d14bbdc29eeed99
AnasImloul <pydev27@gmail.com>
2022-08-31 02:01:41 +0100

    Runtime: 102 ms (Top 42.21%) | Memory: 42.3 MB (Top 87.69%)

--- scripts/algorithms/F/Find Greatest Common Divisor of Array/Find Greatest Common Divisor of Array.js
+++ scripts/algorithms/F/Find Greatest Common Divisor of Array/Find Greatest Common Divisor of Array.js
@@ -1,5 +1,6 @@
+// Runtime: 102 ms (Top 42.21%) | Memory: 42.3 MB (Top 87.69%)
 var findGCD = function(nums) {
-    let newNum =  [Math.min(...nums) , Math.max(...nums)]
+    let newNum = [Math.min(...nums) , Math.max(...nums)]
     let firstNum = newNum[0]
     let secondNum = newNum[1]
     while(secondNum) {
