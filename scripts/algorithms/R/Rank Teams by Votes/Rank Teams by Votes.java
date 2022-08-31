1765f032237e40565c065ddbb871689b5ad34406
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:03 +0100

    Runtime: 13 ms (Top 71.24%) | Memory: 43.7 MB (Top 79.83%)

--- scripts/algorithms/R/Rank Teams by Votes/Rank Teams by Votes.java
+++ scripts/algorithms/R/Rank Teams by Votes/Rank Teams by Votes.java
@@ -1,23 +1,24 @@
+// Runtime: 13 ms (Top 71.24%) | Memory: 43.7 MB (Top 79.83%)
 class Solution {
     public String rankTeams(String[] votes) {
         int n = votes.length;
         int teams = votes[0].length();
         Map<Character, int[]> map = new HashMap<>();
         List<Character> chars = new ArrayList<>();
-        
+
         for(int i = 0 ; i < teams ; i++) {
             char team = votes[0].charAt(i);
             map.put(team, new int[teams]);
             chars.add(team);
         }
-        
+
         for(int i = 0 ; i < n ; i++) {
             String round = votes[i];
             for(int j = 0 ; j < round.length() ; j++) {
                 map.get(round.charAt(j))[j]+=1;
             }
         }
-        
+
         chars.sort((a,b) -> {
             int[] l1 = map.get(a);
             int[] l2 = map.get(b);
@@ -31,7 +32,7 @@ class Solution {
             }
             return a.compareTo(b);
         });
-        
+
         StringBuilder sb = new StringBuilder();
         for(char c : chars) {
             sb.append(c);
