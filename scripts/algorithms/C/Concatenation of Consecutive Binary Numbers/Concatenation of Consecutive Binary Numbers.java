911c3794d0a46ec3f9e96711407cebb5b6e8ec6f
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:23 +0100

    Runtime: 613 ms (Top 30.00%) | Memory: 117.4 MB (Top 18.57%)

--- scripts/algorithms/C/Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.java
+++ scripts/algorithms/C/Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.java
@@ -1,9 +1,10 @@
+// Runtime: 613 ms (Top 30.00%) | Memory: 117.4 MB (Top 18.57%)
 class Solution {
-	public int concatenatedBinary(int n) {
-		long res = 0;
-		for (int i = 1; i <= n; i++) {
-			res = (res * (int) Math.pow(2, Integer.toBinaryString(i).length()) + i) % 1000000007;
-		}
-		return (int) res;
-	}
+    public int concatenatedBinary(int n) {
+        long res = 0;
+        for (int i = 1; i <= n; i++) {
+            res = (res * (int) Math.pow(2, Integer.toBinaryString(i).length()) + i) % 1000000007;
+        }
+        return (int) res;
+    }
 }
