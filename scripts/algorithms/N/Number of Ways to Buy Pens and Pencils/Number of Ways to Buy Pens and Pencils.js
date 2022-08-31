6abbe203fb35252b531707993f851ccf712e1842
AnasImloul <pydev27@gmail.com>
2022-08-31 02:01:56 +0100

    Runtime: 78 ms (Top 95.83%) | Memory: 41.9 MB (Top 85.42%)

--- scripts/algorithms/N/Number of Ways to Buy Pens and Pencils/Number of Ways to Buy Pens and Pencils.js
+++ scripts/algorithms/N/Number of Ways to Buy Pens and Pencils/Number of Ways to Buy Pens and Pencils.js
@@ -1,11 +1,12 @@
+// Runtime: 78 ms (Top 95.83%) | Memory: 41.9 MB (Top 85.42%)
 var waysToBuyPensPencils = function(total, cost1, cost2) {
     let distinctWays = 0;
-    
+
     for (let cnt = 0; cnt * cost1 <= total; ++cnt) {
         const remMoney = total - (cnt * cost1);
         const waysToBuyPencil = Math.floor(remMoney / cost2) + 1;
         distinctWays += waysToBuyPencil;
     }
-    
+
     return distinctWays;
 };
