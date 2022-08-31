2e57d8a32d535418b98990cd8784a48affdb90a7
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:01 +0100

    Runtime: 107 ms (Top 32.92%) | Memory: 43.9 MB (Top 83.84%)

--- scripts/algorithms/K/Keys and Rooms/Keys and Rooms.js
+++ scripts/algorithms/K/Keys and Rooms/Keys and Rooms.js
@@ -1,17 +1,18 @@
-function dfs(current,all,visited){  
-    if(visited.size==all.length){  
+// Runtime: 107 ms (Top 32.92%) | Memory: 43.9 MB (Top 83.84%)
+function dfs(current,all,visited){
+    if(visited.size==all.length){
         return true;
     }
     for(let i=0;i<all[current].length;i++){
-        if(!visited.has(all[current][i])){ 
-            visited.add(all[current][i]); 
+        if(!visited.has(all[current][i])){
+            visited.add(all[current][i]);
             if(dfs(all[current][i],all,visited))
                 return true;
         }
-        
+
     }
     return false;
-        
+
 }
 var canVisitAllRooms = function(rooms) {
     let visited=new Set();
@@ -19,4 +20,3 @@ var canVisitAllRooms = function(rooms) {
     if (dfs(0,rooms,visited)) return true;
     return false;
 };
-
