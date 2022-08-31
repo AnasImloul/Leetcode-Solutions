0c073680839b689bae0099a7f6e79b85cade5561
AnasImloul <pydev27@gmail.com>
2022-08-31 23:07:53 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 53.25%)

--- scripts/algorithms/S/Sequential Digits/Sequential Digits.java
+++ scripts/algorithms/S/Sequential Digits/Sequential Digits.java
@@ -1,8 +1,9 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 53.25%)
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         int lowSize = String.valueOf(low).length(), highSize = String.valueOf(high).length();
         List<Integer> output = new ArrayList<>();
-        
+
         for(int size=lowSize; size<=highSize; size++) {
             int seedNumber = getSeedNumber(size);
             int increment = getIncrement(size);
@@ -17,14 +18,14 @@ class Solution {
         }
         return output;
     }
-    
+
     private int getSeedNumber(int size) {
         int seed = 1;
         for(int i=2;i<=size;i++)
             seed=10*seed + i;
         return seed;
     }
-    
+
     private int getIncrement(int size) {
         int increment = 1;
         for(int i=2;i<=size;i++)
