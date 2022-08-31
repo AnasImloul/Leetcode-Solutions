6094b5204792e4865ad5c91af593d092e25b718c
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:18 +0100

    Runtime: 254 ms (Top 50.50%) | Memory: 51.9 MB (Top 97.03%)

--- scripts/algorithms/S/Second Minimum Time to Reach Destination/Second Minimum Time to Reach Destination.java
+++ scripts/algorithms/S/Second Minimum Time to Reach Destination/Second Minimum Time to Reach Destination.java
@@ -1,8 +1,9 @@
+// Runtime: 254 ms (Top 50.50%) | Memory: 51.9 MB (Top 97.03%)
 class Solution {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
         Map<Integer, List<Integer>> g = new HashMap();
         for(int[] e : edges) {
-            int u  = e[0], v = e[1];
+            int u = e[0], v = e[1];
             g.computeIfAbsent(u, x -> new ArrayList()).add(v);
             g.computeIfAbsent(v, x -> new ArrayList()).add(u);
         }
@@ -14,9 +15,9 @@ class Solution {
         while(!q.isEmpty()) {
             int size = q.size();
             int[] cur = q.poll();
-            int node = cur[0], t = cur[1];  // arriving time
+            int node = cur[0], t = cur[1]; // arriving time
             if(dis[node] == t || uniqueVisit[node] >= 2) continue; // skip if it's same value or has been relaxed twice already
-            uniqueVisit[node]++; 
+            uniqueVisit[node]++;
             dis[node] = t;
             if(node == n && uniqueVisit[node] == 2) return dis[node];
             // generate leaving time (waiting the green light)
