b6e49c47505b2eb1e7ef719dadfaf0643b8997d3
AnasImloul <pydev27@gmail.com>
2022-08-31 02:01:57 +0100

    Runtime: 98 ms (Top 20.59%) | Memory: 42.1 MB (Top 49.02%)

--- scripts/algorithms/S/Score of Parentheses/Score of Parentheses.js
+++ scripts/algorithms/S/Score of Parentheses/Score of Parentheses.js
@@ -1,6 +1,7 @@
+// Runtime: 98 ms (Top 20.59%) | Memory: 42.1 MB (Top 49.02%)
 var scoreOfParentheses = function(s) {
     let len = s.length, pwr = 0, ans = 0;
-    
+
     for (let i = 1; i < len; i++){
         if (s.charAt(i) === "("){
             pwr++;
@@ -10,8 +11,8 @@ var scoreOfParentheses = function(s) {
         }
         else{
             pwr--;
-        } 
+        }
     }
-    
+
     return ans;
 }
