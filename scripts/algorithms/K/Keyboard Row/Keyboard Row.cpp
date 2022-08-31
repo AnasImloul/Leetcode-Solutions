dbc37c64c5772a1617d97f440b3c2650c1030530
AnasImloul <pydev27@gmail.com>
2022-08-31 22:50:58 +0100

    Runtime: 4 ms (Top 30.57%) | Memory: 6.9 MB (Top 35.57%)

--- scripts/algorithms/K/Keyboard Row/Keyboard Row.cpp
+++ scripts/algorithms/K/Keyboard Row/Keyboard Row.cpp
@@ -1,10 +1,11 @@
+// Runtime: 4 ms (Top 30.57%) | Memory: 6.9 MB (Top 35.57%)
 class DSU {
     vector<int> parent, size;
-    
+
     vector<int> Q{'q','w','e','r','t','y','u','i','o','p','Q','W','E','R','T','Y','U','I','O','P'};
     vector<int> A{'a','s','d','f','g','h','j','k','l','A','S','D','F','G','H','J','K','L'};
     vector<int> Z{'z','x','c','v','b','n','m','Z','X','C','V','B','N','M'};
-    
+
 public:
     DSU(){
         for(int i= 0;i<123;i++) parent.push_back(i),size.push_back(1);
@@ -12,13 +13,12 @@ public:
         for(int i = 0; i<A.size()-1;i++) findUnion(A[i],A[i+1]);
         for(int i = 0; i<Z.size()-1;i++) findUnion(Z[i],Z[i+1]);
     }
-    
-    
+
     int findParent(int x){
         if(x==parent[x]) return x;
         return parent[x] = findParent(parent[x]);
     }
-    
+
     void findUnion(int a, int b){
         a = findParent(a);
         b = findParent(b);
@@ -36,7 +36,7 @@ public:
             int ss = words[i].size();
             string s = words[i];
             if(ss==1) {ans.push_back(s);continue;}
-            
+
             bool sameSet = true;
             for(int j = 0;j<ss-1;j++)
                 if(dsu.findParent(s[j])!=dsu.findParent(s[j+1])){
