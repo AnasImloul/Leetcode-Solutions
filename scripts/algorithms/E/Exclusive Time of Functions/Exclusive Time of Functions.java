59d5e62f866c377efe491a33151f548b78309b34
AnasImloul <pydev27@gmail.com>
2022-08-31 15:00:58 +0100

    Runtime: 26 ms (Top 31.28%) | Memory: 52.6 MB (Top 22.01%)

--- scripts/algorithms/E/Exclusive Time of Functions/Exclusive Time of Functions.java
+++ scripts/algorithms/E/Exclusive Time of Functions/Exclusive Time of Functions.java
@@ -1,25 +1,26 @@
+// Runtime: 26 ms (Top 31.28%) | Memory: 52.6 MB (Top 22.01%)
 class Solution {
-    
+
     //Time Complexity: O(N) for looping through logs
     //Space Complexity: O(N) for stack
-    
+
     public int[] exclusiveTime(int n, List<String> logs) {
         if (n == 0) {
             return new int[0];
         }
-        
+
         int[] result = new int[n];
-        
+
         Stack<Pair<Integer, Integer>> stack = new Stack<>();
-        
-        for (String s : logs) {           
+
+        for (String s : logs) {
             String[] sArr = s.split(":");
             int functionId = Integer.parseInt(sArr[0]);
             String startOrEnd = sArr[1];
             int timestamp = Integer.parseInt(sArr[2]);
-            
+
             if (startOrEnd.equals("start")) {
-                
+
                 //calculate previous in-progress length
                 if (!stack.empty()) {
                     Pair<Integer, Integer> pair = stack.peek();
@@ -27,16 +28,16 @@ class Solution {
                     int oldTimestamp = pair.getValue();
                     result[oldFunctionId] += timestamp - oldTimestamp;
                 }
-                
+
                 //add new start
                 stack.push(new Pair(functionId, timestamp));
             } else {
-                //calculate current length                
+                //calculate current length
                 Pair<Integer, Integer> pair = stack.pop();
                 int oldTimestamp = pair.getValue();
-                
+
                 result[functionId] += timestamp - oldTimestamp + 1;
-                
+
                 //reset previous function's start
                 if (!stack.empty()) {
                     pair = stack.pop();
@@ -45,7 +46,7 @@ class Solution {
                 }
             }
         }
-        
+
         return result;
     }
 }
