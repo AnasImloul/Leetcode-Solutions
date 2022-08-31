2952ad46dfde3915335bf77c6d03978a1396476c
AnasImloul <pydev27@gmail.com>
2022-08-31 15:01:04 +0100

    Runtime: 24 ms (Top 38.47%) | Memory: 65.6 MB (Top 33.01%)

--- scripts/algorithms/B/Boats to Save People/Boats to Save People.java
+++ scripts/algorithms/B/Boats to Save People/Boats to Save People.java
@@ -1,11 +1,12 @@
+// Runtime: 24 ms (Top 38.47%) | Memory: 65.6 MB (Top 33.01%)
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         int boatCount = 0;
         Arrays.sort(people);
-        
+
         int left = 0;
         int right = people.length - 1;
-        
+
         while(left <= right){
             int sum = people[left] + people[right];
             if(sum <= limit){
