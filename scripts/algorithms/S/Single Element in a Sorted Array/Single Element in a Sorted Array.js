5333649e432440dbd228cd603c1c05fe136a9efc
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:05 +0100

    Runtime: 63 ms (Top 96.12%) | Memory: 45.3 MB (Top 33.50%)

--- scripts/algorithms/S/Single Element in a Sorted Array/Single Element in a Sorted Array.js
+++ scripts/algorithms/S/Single Element in a Sorted Array/Single Element in a Sorted Array.js
@@ -1,18 +1,19 @@
+// Runtime: 63 ms (Top 96.12%) | Memory: 45.3 MB (Top 33.50%)
 var singleNonDuplicate = function(nums) {
     var start = 0;
-    var end = nums.length  - 1
+    var end = nums.length - 1
     // to check one element
      if (nums.length == 1) return nums[start]
      while(start <= end) {
         if(nums[start] != nums[start + 1]) {
-            return nums[start]        }
-        
-          if(nums[end] != nums[end  - 1]) {
+            return nums[start] }
+
+          if(nums[end] != nums[end - 1]) {
             return nums[end]
         }
         // increment two point
         start = start + 2;
         end = end - 2;
-        
+
     }
 };
