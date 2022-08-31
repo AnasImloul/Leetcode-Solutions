4866ca53e3805750da3f70c0a97d86d26c78d8d1
AnasImloul <pydev27@gmail.com>
2022-08-31 23:07:56 +0100

    Runtime: 5 ms (Top 5.42%) | Memory: 43.3 MB (Top 75.85%)

--- scripts/algorithms/R/Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.java
+++ scripts/algorithms/R/Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.java
@@ -1,37 +1,38 @@
+// Runtime: 5 ms (Top 5.42%) | Memory: 43.3 MB (Top 75.85%)
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) return head; 
-        ListNode temp = head; 
-        int last = -1; 
+        if (head == null) return head;
+        ListNode temp = head;
+        int last = -1;
         int[]array = new int[201];
         // zero == index 100
-        // one == index 101; 
-        // -100 == index 0; 
-        
+        // one == index 101;
+        // -100 == index 0;
+
         while (temp != null){
-            array[temp.val + 100]++;  
+            array[temp.val + 100]++;
             temp = temp.next;
         }
         for (int i = 0; i < 201; i++){
             if (array[i] == 1){
-                last = i; 
+                last = i;
             }
         }
-        if (last == -1) return null; 
-        temp = head; 
-        
+        if (last == -1) return null;
+        temp = head;
+
         for (int i = 0; i < 201; i++){
             if (array[i] == 1){
-                temp.val = i - 100; 
+                temp.val = i - 100;
                 if (i == last){
-                    temp.next = null; 
-                    break; 
+                    temp.next = null;
+                    break;
                 }
-                temp=temp.next; 
+                temp=temp.next;
             }
         }
-        temp.next = null; 
-       
-        return head; 
+        temp.next = null;
+
+        return head;
     }
 }```
