5c9c4df84bcc8908339bf884295d5763701fffaf
AnasImloul <pydev27@gmail.com>
2022-08-31 23:01:03 +0100

    Runtime: 90 ms (Top 97.50%) | Memory: 51.9 MB (Top 80.00%)

--- scripts/algorithms/F/Find Two Non-overlapping Sub-arrays Each With Target Sum/Find Two Non-overlapping Sub-arrays Each With Target Sum.js
+++ scripts/algorithms/F/Find Two Non-overlapping Sub-arrays Each With Target Sum/Find Two Non-overlapping Sub-arrays Each With Target Sum.js
@@ -1,10 +1,11 @@
+// Runtime: 90 ms (Top 97.50%) | Memory: 51.9 MB (Top 80.00%)
 var minSumOfLengths = function(arr, target) {
     let left=0;
     let curr=0;
     let res=Math.min(); // Math.min() without any args will be Infinite
     const best=new Array(arr.length).fill(Math.min());
     let bestSoFar=Math.min()
-    
+
     for(let i=0;i<arr.length;i++){
         curr+=arr[i]
         while(curr>target){
