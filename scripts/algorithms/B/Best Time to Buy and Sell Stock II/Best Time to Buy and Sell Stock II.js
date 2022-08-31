9e8db0a7e0e6d8d52f5135b008c246441b97c6b7
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:21 +0100

    Runtime: 102 ms (Top 35.16%) | Memory: 42.5 MB (Top 44.08%)

--- scripts/algorithms/B/Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.js
+++ scripts/algorithms/B/Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.js
@@ -1,3 +1,4 @@
+// Runtime: 102 ms (Top 35.16%) | Memory: 42.5 MB (Top 44.08%)
 /**
  * @param {number[]} prices
  * @return {number}
@@ -6,17 +7,17 @@ var maxProfit = function(prices) {
     let lowestNum = prices[0];
     let highestNum = prices[0];
     let profit = highestNum - lowestNum;
-    
+
     for(var indexI=1; indexI<prices.length; indexI++) {
-      if(prices[indexI] < prices[indexI - 1])  {
-      	lowestNum = prices[indexI];
+      if(prices[indexI] < prices[indexI - 1]) {
+          lowestNum = prices[indexI];
       }
-      if(prices[indexI] > lowestNum) { 
+      if(prices[indexI] > lowestNum) {
         lowestNum = prices[indexI - 1];
         profit = profit + prices[indexI] - lowestNum;
-      	highestNum = prices[indexI];
+          highestNum = prices[indexI];
       }
     }
-        
+
     return profit;
 };
