26773614316979e70bbc6fe3d752c9796d3d4cda
AnasImloul <pydev27@gmail.com>
2022-08-31 22:50:54 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.5 MB (Top 20.23%)

--- scripts/algorithms/T/Truncate Sentence/Truncate Sentence.cpp
+++ scripts/algorithms/T/Truncate Sentence/Truncate Sentence.cpp
@@ -1,18 +1,19 @@
-class Solution 
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.5 MB (Top 20.23%)
+class Solution
 {
 public:
-    string truncateSentence(string s, int k) 
+    string truncateSentence(string s, int k)
     {
         string resStr = "";
         int words = 0;
-        
+
         for ( char i : s ) {
-            if ( i == ' ') 
+            if ( i == ' ')
                 words++;
-            if ( words < k ) 
+            if ( words < k )
                 resStr += i;
         }
-        
+
         return resStr;
     }
 };
