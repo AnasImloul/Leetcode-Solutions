537cf147a5752312b1cd098102db07a5c41175a2
AnasImloul <pydev27@gmail.com>
2022-08-31 02:30:54 +0100

    Runtime: 6 ms (Top 75.69%) | Memory: 41.1 MB (Top 34.74%)

--- scripts/algorithms/C/Consecutive Numbers Sum/Consecutive Numbers Sum.java
+++ scripts/algorithms/C/Consecutive Numbers Sum/Consecutive Numbers Sum.java
@@ -1,7 +1,8 @@
+// Runtime: 6 ms (Top 75.69%) | Memory: 41.1 MB (Top 34.74%)
 class Solution {
 
     public int consecutiveNumbersSum(int n) {
-	    final double eightN = (8d * ((double) n)); // convert to double because 8n can overflow int
+        final double eightN = (8d * ((double) n)); // convert to double because 8n can overflow int
         final int maxTriangular = (int) Math.floor((-1d + Math.sqrt(1d + eightN)) / 2d);
         int ways = 1;
         int triangular = 1;
