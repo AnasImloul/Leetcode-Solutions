83a7541017c014ccf6287eb017fd54bd48f9bfc1
AnasImloul <pydev27@gmail.com>
2022-08-31 22:50:55 +0100

    Runtime: 2105 ms (Top 22.94%) | Memory: 477.9 MB (Top 17.92%)

--- scripts/algorithms/V/Valid Arrangement of Pairs/Valid Arrangement of Pairs.cpp
+++ scripts/algorithms/V/Valid Arrangement of Pairs/Valid Arrangement of Pairs.cpp
@@ -1,3 +1,4 @@
+// Runtime: 2105 ms (Top 22.94%) | Memory: 477.9 MB (Top 17.92%)
 class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
@@ -6,25 +7,25 @@ public:
         unordered_map<int, stack<int>> adj;
         unordered_map<int, int> in;
         unordered_map<int, int> out;
-		// reserve spaces for unordered_map may help in runtime.
-		adj.reserve(m);
-		in.reserve(m);
-		out.reserve(m);
+        // reserve spaces for unordered_map may help in runtime.
+        adj.reserve(m);
+        in.reserve(m);
+        out.reserve(m);
         for (int i = 0; i < m; i++) {
             int u = pairs[i][0], v = pairs[i][1];
             in[v]++;
             out[u]++;
             adj[u].push(v);
         }
-		// find the starting node
+        // find the starting node
         int start = -1;
         for (auto& p : adj) {
             int i = p.first;
             if (out[i] - in[i] == 1) start = i;
         }
         if (start == -1) {
-			// Eulerian Circuit -> start at any node
-			start = adj.begin()->first;
+            // Eulerian Circuit -> start at any node
+            start = adj.begin()->first;
         }
         vector<vector<int>> ans;
         euler(adj, ans, start);
@@ -38,7 +39,7 @@ private:
             int nei = stk.top();
             stk.pop();
             euler(adj, ans, nei);
-			// postorder
+            // postorder
             ans.push_back({curr, nei});
         }
     }
