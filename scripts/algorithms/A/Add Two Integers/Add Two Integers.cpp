be0a10e9cd758409aa684234c85b5cfafa137e44
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:05 +0100

    Runtime: 3 ms (Top 39.04%) | Memory: 5.8 MB (Top 95.13%)

--- scripts/algorithms/A/Add Two Integers/Add Two Integers.cpp
+++ scripts/algorithms/A/Add Two Integers/Add Two Integers.cpp
@@ -1,6 +1,7 @@
+// Runtime: 3 ms (Top 39.04%) | Memory: 5.8 MB (Top 95.13%)
 class Solution {
 public:
-	int sum(int num1, int num2) {
-		return num1+num2;
-	}
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
 };
