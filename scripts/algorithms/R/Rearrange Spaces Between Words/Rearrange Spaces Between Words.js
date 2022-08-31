5a59c229288703845ad5b9d5c0d5f6c539309e9e
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:08 +0100

    Runtime: 77 ms (Top 77.31%) | Memory: 42.2 MB (Top 47.52%)

--- scripts/algorithms/R/Rearrange Spaces Between Words/Rearrange Spaces Between Words.js
+++ scripts/algorithms/R/Rearrange Spaces Between Words/Rearrange Spaces Between Words.js
@@ -1,8 +1,9 @@
+// Runtime: 77 ms (Top 77.31%) | Memory: 42.2 MB (Top 47.52%)
 var reorderSpaces = function(text) {
     let arr = text.split(" ");
     let totalSpace = arr.length-1;
     arr = arr.filter(w => w !== '');
-    let spaceBetween = arr.length > 1 ? 
+    let spaceBetween = arr.length > 1 ?
                     Math.floor(totalSpace / (arr.length-1)) : 0;
     let spaceLeftOver = arr.length > 1 ?
                     totalSpace % (arr.length-1) : totalSpace;
