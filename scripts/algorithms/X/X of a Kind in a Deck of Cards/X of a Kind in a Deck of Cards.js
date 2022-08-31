fcd1c038b2c7f888914899ad31dcb58b8981e71b
AnasImloul <pydev27@gmail.com>
2022-08-31 23:01:00 +0100

    Runtime: 124 ms (Top 20.83%) | Memory: 44.3 MB (Top 68.23%)

--- scripts/algorithms/X/X of a Kind in a Deck of Cards/X of a Kind in a Deck of Cards.js
+++ scripts/algorithms/X/X of a Kind in a Deck of Cards/X of a Kind in a Deck of Cards.js
@@ -1,6 +1,7 @@
+// Runtime: 124 ms (Top 20.83%) | Memory: 44.3 MB (Top 68.23%)
 var hasGroupsSizeX = function(deck) {
     let unique = [...new Set(deck)], three = 0, two = 0, five = 0, size = 0, same = 0, s = 0;
-    
+
     for(let i = 0; i<unique.length; i++){
         for(let y=0; y<deck.length; y++){
             if(unique[i] === deck[y]){
@@ -15,7 +16,7 @@ var hasGroupsSizeX = function(deck) {
         s = size;
         size = 0;
     }
-    
+
     if(Math.max(two,three,five) !== unique.length && same !== unique.length-1) return false;
     return true;
 };
