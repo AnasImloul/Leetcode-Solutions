1bcb917ec7b175e4be659899a0ee7d39a68641e5
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:00 +0100

    Runtime: 18 ms (Top 56.69%) | Memory: 13.1 MB (Top 35.45%)

--- scripts/algorithms/B/Best Time to Buy and Sell Stock IV/Best Time to Buy and Sell Stock IV.cpp
+++ scripts/algorithms/B/Best Time to Buy and Sell Stock IV/Best Time to Buy and Sell Stock IV.cpp
@@ -1,3 +1,4 @@
+// Runtime: 18 ms (Top 56.69%) | Memory: 13.1 MB (Top 35.45%)
 class Solution {
 public:
     int solve(int ind, int buy, int k, vector<int>& prices, vector<vector<vector<int>>>& dp){
@@ -6,7 +7,7 @@ public:
         if(buy){
             return dp[ind][buy][k] = max(-prices[ind]+solve(ind+1, 0, k, prices, dp), solve(ind+1, 1, k, prices, dp));
         }
-        return dp[ind][buy][k] = max(prices[ind]+solve(ind+1, 1, k-1, prices, dp), solve(ind+1, 0, k, prices, dp));  
+        return dp[ind][buy][k] = max(prices[ind]+solve(ind+1, 1, k-1, prices, dp), solve(ind+1, 0, k, prices, dp));
     }
     int maxProfit(int k, vector<int>& prices) {
         vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>> (2, vector<int> (k+1, -1)));
