ec5ad1fe8a10e391d1f29860e785b196674d0cb1
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:25 +0100

    Runtime: 125 ms (Top 97.34%) | Memory: 51 MB (Top 91.49%)

--- scripts/algorithms/M/Minimize Deviation in Array/Minimize Deviation in Array.java
+++ scripts/algorithms/M/Minimize Deviation in Array/Minimize Deviation in Array.java
@@ -1,3 +1,4 @@
+// Runtime: 125 ms (Top 97.34%) | Memory: 51 MB (Top 91.49%)
 class Solution {
     public int minimumDeviation(int[] nums) {
         TreeSet<Integer> temp = new TreeSet<>();
@@ -9,15 +10,15 @@ class Solution {
                 temp.add(i * 2);
             }
         }
-        
+
         int md = temp.last() - temp.first();
         int m = 0;
-        
+
         while(temp.size() > 0 && temp.last() % 2 == 0){
             m = temp.last();
             temp.remove(m);
             temp.add(m / 2);
-            
+
             md = Math.min(md, temp.last() - temp.first());
         }
         return md;
