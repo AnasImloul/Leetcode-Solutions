4d2f9bc16424c78450910a67c45b059d5b1f2302
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:21 +0100

    Runtime: 86 ms (Top 60.00%) | Memory: 42.6 MB (Top 25.38%)

--- scripts/algorithms/R/Reformat Phone Number/Reformat Phone Number.js
+++ scripts/algorithms/R/Reformat Phone Number/Reformat Phone Number.js
@@ -1,10 +1,11 @@
+// Runtime: 86 ms (Top 60.00%) | Memory: 42.6 MB (Top 25.38%)
 var reformatNumber = function(number) {
     let numArr = number.replace(/-/g,'').replace(/ /g,'').split('')
     let res = ''
     while(numArr.length >= 4){
-        numArr.length == 4 ? 
-            res += numArr.splice(0,2).join('') +'-'+numArr.splice(0,2).join('') : 
-            res += numArr.splice(0,3).join('') + '-'    
+        numArr.length == 4 ?
+            res += numArr.splice(0,2).join('') +'-'+numArr.splice(0,2).join('') :
+            res += numArr.splice(0,3).join('') + '-'
     }
     res += numArr.join('')
     return res
