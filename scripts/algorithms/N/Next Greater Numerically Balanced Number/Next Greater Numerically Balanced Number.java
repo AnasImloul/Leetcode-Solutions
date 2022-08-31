bab1fdbdabe8f8a9975f14e722fcbde6347a2ac7
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:15 +0100

    Runtime: 207 ms (Top 46.27%) | Memory: 117.2 MB (Top 26.87%)

--- scripts/algorithms/N/Next Greater Numerically Balanced Number/Next Greater Numerically Balanced Number.java
+++ scripts/algorithms/N/Next Greater Numerically Balanced Number/Next Greater Numerically Balanced Number.java
@@ -1,3 +1,4 @@
+// Runtime: 207 ms (Top 46.27%) | Memory: 117.2 MB (Top 26.87%)
 class Solution {
     public int nextBeautifulNumber(int n) {
 
@@ -5,23 +6,23 @@ class Solution {
             n++;
            int num = n; //test this number
            int [] freq = new int[10]; // 0 to 9
-            
-            while(num > 0){  //calculate freq of each digit in the num
+
+            while(num > 0){ //calculate freq of each digit in the num
                 int rem = num % 10; //this is remainder
-                num = num / 10;     //this is quotient
+                num = num / 10; //this is quotient
                 freq[rem] = freq[rem] + 1; //increase its frequency
                 if(freq[rem] > rem) break;
             }
-            
+
             boolean ans = true;
-            
+
             for(int i = 0;i<10;i++){ //check frequency of each digit
               if(freq[i] != i && freq[i] != 0){
                   ans = false;
                   break;
               }
             }
-            
+
             if(ans == true){
                 return n;
             }
