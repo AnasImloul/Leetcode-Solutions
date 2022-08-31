6cb430b87e510c05cb663f5fdc1e3c227cc7cfb7
AnasImloul <pydev27@gmail.com>
2022-08-31 02:01:39 +0100

    Runtime: 86 ms (Top 70.68%) | Memory: 44.5 MB (Top 21.92%)

--- scripts/algorithms/T/Toeplitz Matrix/Toeplitz Matrix.js
+++ scripts/algorithms/T/Toeplitz Matrix/Toeplitz Matrix.js
@@ -1,11 +1,12 @@
+// Runtime: 86 ms (Top 70.68%) | Memory: 44.5 MB (Top 21.92%)
 var isToeplitzMatrix = function(matrix) {
     for (let i=0; i < matrix.length-1; i++) {
-        for (let j=0; j < matrix[i].length-1; j++) {    
+        for (let j=0; j < matrix[i].length-1; j++) {
             if (matrix[i][j] !== matrix[i+1][j+1]) {
                 return false
             }
         }
     }
-    
+
     return true
 };
