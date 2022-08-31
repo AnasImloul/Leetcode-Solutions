ec08b554be983d0513f645d30ba086e4a95a4ea7
AnasImloul <pydev27@gmail.com>
2022-08-31 22:51:02 +0100

    Runtime: 98 ms (Top 46.69%) | Memory: 28.5 MB (Top 100.00%)

--- scripts/algorithms/L/Largest Number After Mutating Substring/Largest Number After Mutating Substring.cpp
+++ scripts/algorithms/L/Largest Number After Mutating Substring/Largest Number After Mutating Substring.cpp
@@ -1,3 +1,4 @@
+// Runtime: 98 ms (Top 46.69%) | Memory: 28.5 MB (Top 100.00%)
 //change first longest encountered substring from left which can make string greater in value
 class Solution {
 public:
@@ -6,11 +7,11 @@ public:
         for(int i=0;i<n;i++){
             int x=num[i]-'0';
             if(x < change[x]){//checking whether mutating this char will increment the value or not
-			//the mutation continues till mutating the char will increase the value
+            //the mutation continues till mutating the char will increase the value
                 while(i<n and num[i]-'0'<=change[num[i]-'0']){
                     num[i++]=change[num[i]-'0']+'0';
                 }
-				//break the loop when substring ends
+                //break the loop when substring ends
                 break;
             }
         }
