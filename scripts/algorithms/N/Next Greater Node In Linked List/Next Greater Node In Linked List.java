b186edb204db75d969451f5ef35555c72ea92fbc
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:02 +0100

    Runtime: 721 ms (Top 25.59%) | Memory: 45.5 MB (Top 95.29%)

--- scripts/algorithms/N/Next Greater Node In Linked List/Next Greater Node In Linked List.java
+++ scripts/algorithms/N/Next Greater Node In Linked List/Next Greater Node In Linked List.java
@@ -1,3 +1,4 @@
+// Runtime: 721 ms (Top 25.59%) | Memory: 45.5 MB (Top 95.29%)
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
         ListNode length=head;
@@ -10,12 +11,12 @@ class Solution {
         int[] res=new int[l];
         int i=0;
         ListNode temp=head;
-        
+
         while(temp!=null)
-        {   
+        {
             ListNode temp1=temp.next;
             int max=temp.val;
-            
+
             while(temp1!=null)
             {
                 if(temp1.val>max)
@@ -24,7 +25,7 @@ class Solution {
                     res[i]=max;
                     break;
                 }
-                
+
                 temp1=temp1.next;
             }
             temp=temp.next;
