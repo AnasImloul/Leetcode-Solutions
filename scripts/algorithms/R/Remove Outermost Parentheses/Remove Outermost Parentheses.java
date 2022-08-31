e028da3f8c0f954ead65da32b312a640b0309c53
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:20 +0100

    Runtime: 7 ms (Top 58.51%) | Memory: 43 MB (Top 43.86%)

--- scripts/algorithms/R/Remove Outermost Parentheses/Remove Outermost Parentheses.java
+++ scripts/algorithms/R/Remove Outermost Parentheses/Remove Outermost Parentheses.java
@@ -1,3 +1,4 @@
+// Runtime: 7 ms (Top 58.51%) | Memory: 43 MB (Top 43.86%)
 class Solution {
     public String removeOuterParentheses(String s) {
     // if '(' check stack size > 0 add ans else not add ans
@@ -6,21 +7,21 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            
+
             if(ch == '('){
                 if(st.size() > 0){
                     sb.append(ch);
                 }
                 st.push(ch);
             }
-            
+
             else{
                 st.pop();
                 if(st.size() > 0){
                     sb.append(ch);
-                } 
+                }
             }
         }
-        return sb.toString(); 
+        return sb.toString();
     }
 }
