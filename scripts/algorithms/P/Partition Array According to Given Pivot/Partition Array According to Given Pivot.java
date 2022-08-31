a2086951200f7e53d8353ca8c89f0c6291f2129a
AnasImloul <pydev27@gmail.com>
2022-08-31 15:01:02 +0100

    Runtime: 12 ms (Top 37.13%) | Memory: 169.4 MB (Top 17.70%)

--- scripts/algorithms/P/Partition Array According to Given Pivot/Partition Array According to Given Pivot.java
+++ scripts/algorithms/P/Partition Array According to Given Pivot/Partition Array According to Given Pivot.java
@@ -1,3 +1,4 @@
+// Runtime: 12 ms (Top 37.13%) | Memory: 169.4 MB (Top 17.70%)
 // Time complexity = 2n = O(n)
 // Space complexity = O(1), or O(n) if the result array is including in the complexity analysis.
 
@@ -5,7 +6,7 @@ class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
         int left = 0, right = nums.length - 1;
-        
+
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] < pivot) {
                 result[left++] = nums[i];
@@ -14,12 +15,12 @@ class Solution {
                 result[right--] = nums[nums.length - 1 - i];
             }
         }
-        
+
         while(left <= right) {
             result[left++] = pivot;
             result[right--] = pivot;
         }
-        
+
         return result;
-    } 
+    }
 }
