975413fea291d3836cc5e34cbc133f0b694ab3e0
AnasImloul <pydev27@gmail.com>
2022-08-31 23:01:07 +0100

    Runtime: 170 ms (Top 23.07%) | Memory: 50.4 MB (Top 34.29%)

--- scripts/algorithms/R/Reorder List/Reorder List.js
+++ scripts/algorithms/R/Reorder List/Reorder List.js
@@ -1,19 +1,19 @@
+// Runtime: 170 ms (Top 23.07%) | Memory: 50.4 MB (Top 34.29%)
 var reorderList = function(head) {
     const dummyL = new ListNode(-1);
     const dummyR = new ListNode(-1);
-    
+
     let currL = dummyL;
     let currR = dummyR;
     let past = false;
 
-    
     let fast = head;
     let slow = head;
     while (slow) {
         if (!fast?.next) {
             past = true
         }
-        
+
         if (past) {
             currR.next = slow;
             currR = slow;
@@ -29,18 +29,18 @@ var reorderList = function(head) {
     }
     currL.next = null;
     currR.next = null;
-    
+
     dummyR.next = reverse(dummyR.next);
     return merge(dummyL.next, dummyR.next);
 };
 
 const merge = (l, r) => {
     const dummy = new ListNode(-1);
-    
+
     let currL = l;
-    let currR = r; 
+    let currR = r;
     let last = dummy;
-    
+
     let count = 0;
     while (currL && currR) {
         if (count % 2 === 0) {
@@ -52,10 +52,10 @@ const merge = (l, r) => {
             last = currR;
             currR = currR.next
         }
-        
+
         count++
     }
-    
+
     last.next = (currL || currR);
     return dummy.next;
 }
@@ -69,6 +69,6 @@ const reverse = (head) => {
         prev = curr;
         curr = tempPrev;
     }
-    
+
     return prev;
 }
