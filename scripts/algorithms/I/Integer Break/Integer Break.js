a7cef55e30502b5506b044e2d30ddc7097188140
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:18 +0100

    Runtime: 62 ms (Top 95.56%) | Memory: 42 MB (Top 66.94%)

--- scripts/algorithms/I/Integer Break/Integer Break.js
+++ scripts/algorithms/I/Integer Break/Integer Break.js
@@ -1,31 +1,23 @@
+// Runtime: 62 ms (Top 95.56%) | Memory: 42 MB (Top 66.94%)
 var integerBreak = function(n) {
 
        if(n == 2) return 1;
        if(n == 3) return 2;
        if(n == 4) return 4;
-    
+
        let c = ~~(n/3);
        let d = n % 3;
-    
-      
+
        if(d === 0){
            return 3 ** c;
        }
-   
-    
+
        if(d === 1){
            return 3 ** (c-1) * 4;
        }
-    
-        
+
        if(d === 2){
            return 3 ** (c) * 2;
        }
-    
-       
-    
-    
 
-
-    
 };
