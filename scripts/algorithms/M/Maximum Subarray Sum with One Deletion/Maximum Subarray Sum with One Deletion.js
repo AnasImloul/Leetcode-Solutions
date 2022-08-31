655c94da6f01d73f88da37e6b9d8864ff9e7779d
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:10 +0100

    Runtime: 103 ms (Top 59.09%) | Memory: 49.7 MB (Top 31.82%)

--- scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.js
+++ scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.js
@@ -1,3 +1,4 @@
+// Runtime: 103 ms (Top 59.09%) | Memory: 49.7 MB (Top 31.82%)
 /**
  * @param {number[]} arr
  * @return {number}
@@ -5,29 +6,29 @@
 var maximumSum = function(arr) {
     let n = arr.length
      let fw = [];
-    let bw = []; 
+    let bw = [];
     let sum = arr[0]
     let max = arr[0];
-  
+
     fw[0] = arr[0];
 
     for(let i = 1; i < n; i++){
         sum = Math.max(arr[i], sum + arr[i]);
         max = Math.max(max, sum);
-  
+
         fw[i] = sum;
-    } 
-     
-    sum =  arr[n-1]
+    }
+
+    sum = arr[n-1]
     max = arr[n-1]
     bw[n - 1] = arr[n - 1];
      for(let i = n - 2; i >= 0; i--){
         sum = Math.max(arr[i], sum + arr[i]);
         max = Math.max(max, sum);
          bw[i] = sum;
-    } 
-  
-    let fans = max; 
+    }
+
+    let fans = max;
     for(let i = 1; i < n - 1; i++)
         fans = Math.max(fans, fw[i - 1] +
                               bw[i + 1]);
