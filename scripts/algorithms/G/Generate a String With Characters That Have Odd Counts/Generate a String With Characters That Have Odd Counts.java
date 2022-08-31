247583ff98c6fcbdb00016594dc9f7e384b1a465
AnasImloul <pydev27@gmail.com>
2022-08-31 23:07:57 +0100

    Runtime: 10 ms (Top 6.68%) | Memory: 45.2 MB (Top 20.03%)

--- scripts/algorithms/G/Generate a String With Characters That Have Odd Counts/Generate a String With Characters That Have Odd Counts.java
+++ scripts/algorithms/G/Generate a String With Characters That Have Odd Counts/Generate a String With Characters That Have Odd Counts.java
@@ -1,11 +1,12 @@
+// Runtime: 10 ms (Top 6.68%) | Memory: 45.2 MB (Top 20.03%)
 class Solution {
     public String generateTheString(int n) {
         String s = "";
         String string ="a";
-        for (int i = 0; i < n-1; i++) 
+        for (int i = 0; i < n-1; i++)
             s += string;
         if(n%2==0)
         return s+"b";
-        return s+"a";            
+        return s+"a";
     }
 }
