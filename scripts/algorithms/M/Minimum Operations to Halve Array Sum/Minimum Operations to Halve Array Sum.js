cda892aca30d97b17fad3eebd96ddcaa6fe77536
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:16 +0100

    Runtime: 1606 ms (Top 23.08%) | Memory: 137.1 MB (Top 30.77%)

--- scripts/algorithms/M/Minimum Operations to Halve Array Sum/Minimum Operations to Halve Array Sum.js
+++ scripts/algorithms/M/Minimum Operations to Halve Array Sum/Minimum Operations to Halve Array Sum.js
@@ -1,28 +1,29 @@
+// Runtime: 1606 ms (Top 23.08%) | Memory: 137.1 MB (Top 30.77%)
 var halveArray = function(nums) {
     const n = nums.length;
     const maxHeap = new MaxPriorityQueue({ priority: x => x });
-    
+
     let startSum = 0;
-    
+
     for (const num of nums) {
         maxHeap.enqueue(num);
         startSum += num;
     }
-    
+
     let currSum = startSum;
-    
+
     let numberOfOperations = 0;
-    
+
     while (currSum > startSum / 2) {
         const biggestNum = maxHeap.dequeue().element;
-        
+
         const halfNum = biggestNum / 2;
-        
+
         numberOfOperations += 1;
         currSum -= halfNum;
-        
+
         maxHeap.enqueue(halfNum);
     }
-    
+
     return numberOfOperations;
 };
