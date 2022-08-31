63f6b1b0d3bce5a0d08f4d86f7a21028e3af5d1e
AnasImloul <pydev27@gmail.com>
2022-08-31 02:31:17 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40.5 MB (Top 75.55%)

--- scripts/algorithms/P/Poor Pigs/Poor Pigs.java
+++ scripts/algorithms/P/Poor Pigs/Poor Pigs.java
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40.5 MB (Top 75.55%)
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int T = (minutesToTest/minutesToDie) + 1;
