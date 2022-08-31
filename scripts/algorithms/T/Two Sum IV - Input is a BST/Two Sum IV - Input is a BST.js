c3a49e358ca58aa0ae0a8b7f543b5e2d8d9c131c
AnasImloul <pydev27@gmail.com>
2022-08-31 02:02:23 +0100

    Runtime: 97 ms (Top 93.88%) | Memory: 51.4 MB (Top 81.78%)

--- scripts/algorithms/T/Two Sum IV - Input is a BST/Two Sum IV - Input is a BST.js
+++ scripts/algorithms/T/Two Sum IV - Input is a BST/Two Sum IV - Input is a BST.js
@@ -1,13 +1,14 @@
+// Runtime: 97 ms (Top 93.88%) | Memory: 51.4 MB (Top 81.78%)
 var findTarget = function(root, k) {
 
     const set = new Set();
-    
+
     const search = (root, k) => {
         if (!root) return false;
         if (set.has(k - root.val)) return true;
         set.add(root.val);
         return search(root.left, k) || search(root.right, k);
     }
-    
+
     return search(root,k);
 };
