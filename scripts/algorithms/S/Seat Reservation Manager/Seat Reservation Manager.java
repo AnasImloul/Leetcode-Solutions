8985417e2c4637382f80054c7476ab0e7b5e03dc
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:59 +0100

    Runtime: 88 ms (Top 72.91%) | Memory: 105 MB (Top 67.49%)

--- scripts/algorithms/S/Seat Reservation Manager/Seat Reservation Manager.java
+++ scripts/algorithms/S/Seat Reservation Manager/Seat Reservation Manager.java
@@ -1,3 +1,4 @@
+// Runtime: 88 ms (Top 72.91%) | Memory: 105 MB (Top 67.49%)
 class SeatManager {
     PriorityQueue<Integer> pq;
     int count;
@@ -5,14 +6,14 @@ class SeatManager {
         count = 1;
         pq = new PriorityQueue();
     }
-    
+
     public int reserve() {
         if(pq.size()==0)
             return count++;
-        
+
         return pq.poll();
     }
-    
+
     public void unreserve(int seatNumber) {
         pq.add(seatNumber);
     }
