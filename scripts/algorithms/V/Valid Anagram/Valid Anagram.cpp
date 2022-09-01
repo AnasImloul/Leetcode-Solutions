00ef0f6f5582db6169a92c1767040338080fb8b9
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:39 +0100

    Runtime: 22 ms (Top 25.79%) | Memory: 7.4 MB (Top 47.98%)

--- scripts/algorithms/V/Valid Anagram/Valid Anagram.cpp
+++ scripts/algorithms/V/Valid Anagram/Valid Anagram.cpp
@@ -1,12 +1,13 @@
+// Runtime: 22 ms (Top 25.79%) | Memory: 7.4 MB (Top 47.98%)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        
+
         if(s != t)
             return false;
-            
+
         return true;
     }
 };
