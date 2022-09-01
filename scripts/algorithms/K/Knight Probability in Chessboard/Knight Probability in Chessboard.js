945ca7d79597af376daf18527436161c7033b5e1
AnasImloul <pydev27@gmail.com>
2022-09-02 00:07:13 +0100

    Runtime: 101 ms (Top 86.16%) | Memory: 49.2 MB (Top 38.36%)

--- scripts/algorithms/K/Knight Probability in Chessboard/Knight Probability in Chessboard.js
+++ scripts/algorithms/K/Knight Probability in Chessboard/Knight Probability in Chessboard.js
@@ -1,26 +1,27 @@
+// Runtime: 101 ms (Top 86.16%) | Memory: 49.2 MB (Top 38.36%)
 var knightProbability = function(n, k, row, column) {
-	if (k === 0) return 1;
-	const dirs = [[-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]];
-	const dp = Array(k + 1)
-		.fill('')
-		.map(_ => Array(n).fill('').map(_ => Array(n).fill(0)));
+    if (k === 0) return 1;
+    const dirs = [[-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]];
+    const dp = Array(k + 1)
+        .fill('')
+        .map(_ => Array(n).fill('').map(_ => Array(n).fill(0)));
 
-	const isOut = (pos) => pos < 0 || pos > n - 1;
-	const move = (x = row, y = column, step = k) => {
-		if (step === 0) return 1;
-		const moves = dp[step][x][y];
-		if (moves !== 0) return moves;
+    const isOut = (pos) => pos < 0 || pos > n - 1;
+    const move = (x = row, y = column, step = k) => {
+        if (step === 0) return 1;
+        const moves = dp[step][x][y];
+        if (moves !== 0) return moves;
 
-		for (const [mvoeX, moveY] of dirs) {
-			const nextX = x + mvoeX;
-			const nextY = y + moveY;
-			if (isOut(nextX) || isOut(nextY)) continue;
+        for (const [mvoeX, moveY] of dirs) {
+            const nextX = x + mvoeX;
+            const nextY = y + moveY;
+            if (isOut(nextX) || isOut(nextY)) continue;
 
-			dp[step][x][y] += move(nextX, nextY, step - 1);
-		}
-		return dp[step][x][y];
-	};
+            dp[step][x][y] += move(nextX, nextY, step - 1);
+        }
+        return dp[step][x][y];
+    };
 
-	move();
-	return dp[k][row][column] / 8 ** k;
+    move();
+    return dp[k][row][column] / 8 ** k;
 };
