4285caaa23c96e15d9306b935e92ebcd9e3cbee6
AnasImloul <pydev27@gmail.com>
2022-09-02 00:42:09 +0100

    Runtime: 1 ms (Top 65.37%) | Memory: 42.6 MB (Top 13.06%)

--- scripts/algorithms/L/Leaf-Similar Trees/Leaf-Similar Trees.java
+++ scripts/algorithms/L/Leaf-Similar Trees/Leaf-Similar Trees.java
@@ -1,12 +1,13 @@
+// Runtime: 1 ms (Top 65.37%) | Memory: 42.6 MB (Top 13.06%)
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
         checkLeaf(root1, list1);
         List<Integer> list2 = new ArrayList<>();
         checkLeaf(root2, list2);
-        
+
         if(list1.size() != list2.size()) return false;
-        
+
         int i = 0;
         while(i < list1.size()){
             if(list1.get(i) != list2.get(i)){
@@ -16,7 +17,7 @@ class Solution {
         }
         return true;
     }
-    
+
     private void checkLeaf(TreeNode node, List<Integer> arr){
         if(node.left == null && node.right == null) arr.add(node.val);
         if(node.left != null) checkLeaf(node.left, arr);
