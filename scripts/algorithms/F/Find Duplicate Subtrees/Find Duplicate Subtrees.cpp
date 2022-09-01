f231f1d8d7f50d84527a81ce3c47b6e191acf280
AnasImloul <pydev27@gmail.com>
2022-09-01 23:49:22 +0100

    Runtime: 41 ms (Top 93.16%) | Memory: 44.5 MB (Top 70.83%)

--- scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.cpp
+++ scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.cpp
@@ -1,21 +1,22 @@
+// Runtime: 41 ms (Top 93.16%) | Memory: 44.5 MB (Top 70.83%)
 class Solution {
 public:
     unordered_map<string,int> dp ;
     vector<TreeNode*> ans ;
-    
+
     string solve(TreeNode * root){
         if(!root) return "" ;
-        
+
         string left = solve(root->left) ;
         string right = solve(root->right) ;
-        
+
         string code = to_string(root->val) + " " + left + " " + right ;
         if(dp[code] == 1) ans.push_back(root) ;
         dp[code]++ ;
-        
+
         return code ;
     }
-    
+
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
         string dummy = solve(root) ;
         return ans ;
