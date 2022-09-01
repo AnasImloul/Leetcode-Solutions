b88480958ce06e250b44d57300aa8b23b2122ecf
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:41 +0100

    Runtime: 120 ms (Top 5.22%) | Memory: 10.8 MB (Top 40.32%)

--- scripts/algorithms/F/First Unique Character in a String/First Unique Character in a String.cpp
+++ scripts/algorithms/F/First Unique Character in a String/First Unique Character in a String.cpp
@@ -1,18 +1,19 @@
+// Runtime: 120 ms (Top 5.22%) | Memory: 10.8 MB (Top 40.32%)
 class Solution {
 public:
 int firstUniqChar(string s) {
     unordered_map<char, int> mp;
-    
+
     for(int i=0; i<s.length(); i++){
         mp[s[i]]++;
     }
-    
+
     for(int i=0; i<s.length(); i++){
         if(mp[s[i]] == 1){
             return i;
         }
     }
-    
+
     return -1;
 }
 };
