23352fafbb5ad3cdff81b35a23b8933ce66723ad
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:57 +0100

    Runtime: 18 ms (Top 43.75%) | Memory: 119 MB (Top 77.96%)

--- scripts/algorithms/M/Minimum Number of Vertices to Reach All Nodes/Minimum Number of Vertices to Reach All Nodes.java
+++ scripts/algorithms/M/Minimum Number of Vertices to Reach All Nodes/Minimum Number of Vertices to Reach All Nodes.java
@@ -1,19 +1,20 @@
+// Runtime: 18 ms (Top 43.75%) | Memory: 119 MB (Top 77.96%)
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        
+
         int[] indegree = new int[n];
-        
+
         for(List<Integer> edge : edges) {
             indegree[edge.get(1)]++;
         }
-        
+
         List<Integer> result = new ArrayList<>();
-        
+
         for(int i=0; i<n;i ++) {
-            if(indegree[i]==0) 
+            if(indegree[i]==0)
                 result.add(i);
         }
-        
+
         return result;
     }
 }
