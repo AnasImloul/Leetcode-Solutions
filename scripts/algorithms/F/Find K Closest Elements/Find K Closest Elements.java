5e20b9eacdc6966fe852da19e8fa06ae1b514690
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:08 +0100

    Runtime: 13 ms (Top 41.96%) | Memory: 62.2 MB (Top 71.32%)

--- scripts/algorithms/F/Find K Closest Elements/Find K Closest Elements.java
+++ scripts/algorithms/F/Find K Closest Elements/Find K Closest Elements.java
@@ -1,20 +1,20 @@
+// Runtime: 13 ms (Top 41.96%) | Memory: 62.2 MB (Top 71.32%)
 class Solution {
 public List<Integer> findClosestElements(int[] arr, int k, int x) {
     List<Integer> result = new ArrayList<>();
-    
+
     int low = 0, high = arr.length -1;
-    
-    while(high - low  >= k){
+
+    while(high - low >= k){
         if(Math.abs(arr[low] - x) > Math.abs(arr[high] - x))
             low++;
         else
             high--;
     }
-    
+
     for(int i = low; i <= high; i++)
         result.add(arr[i]);
-    
-    
+
     return result;
 }
 }
