3b29473548207ae6e712b9931e6120ee09ac4394
AnasImloul <pydev27@gmail.com>
2022-09-02 00:22:17 +0100

    Runtime: 98 ms (Top 95.92%) | Memory: 49.7 MB (Top 53.22%)

--- scripts/algorithms/M/Merge Similar Items/Merge Similar Items.js
+++ scripts/algorithms/M/Merge Similar Items/Merge Similar Items.js
@@ -1,3 +1,4 @@
+// Runtime: 98 ms (Top 95.92%) | Memory: 49.7 MB (Top 53.22%)
 /**
  * @param {number[][]} items1
  * @param {number[][]} items2
@@ -7,7 +8,7 @@ var mergeSimilarItems = function(items1, items2) {
     let map = new Map();
     items1.reduce((map,curr)=>map.set(curr[0],map.get(curr[0])+curr[1]||curr[1]),map);
     items2.reduce((map,curr)=>map.set(curr[0],map.get(curr[0])+curr[1]||curr[1]),map);
-    
+
     return [...map].sort((a,b)=>{
         return a[0] - b[0];
     })
