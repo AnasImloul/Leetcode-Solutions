1c1c44a486f17ebcf0b090d1dfd99dce21549d32
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:09 +0100

    Runtime: 10 ms (Top 30.51%) | Memory: 45.4 MB (Top 20.46%)

--- scripts/algorithms/C/Car Pooling/Car Pooling.java
+++ scripts/algorithms/C/Car Pooling/Car Pooling.java
@@ -1,3 +1,4 @@
+// Runtime: 10 ms (Top 30.51%) | Memory: 45.4 MB (Top 20.46%)
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         Map<Integer, Integer> destinationToPassengers = new TreeMap<>();
@@ -5,13 +6,13 @@ class Solution {
             int currPassengersAtPickup = destinationToPassengers.getOrDefault(trip[1], 0);
             int currPassengersAtDrop = destinationToPassengers.getOrDefault(trip[2], 0);
             destinationToPassengers.put(trip[1], currPassengersAtPickup + trip[0]);
-            destinationToPassengers.put(trip[2], currPassengersAtDrop - trip[0]);            
+            destinationToPassengers.put(trip[2], currPassengersAtDrop - trip[0]);
         }
-        
+
         int currPassengers = 0;
         for(int passengers : destinationToPassengers.values()) {
             currPassengers += passengers;
-            
+
             if(currPassengers > capacity) {
                 return false;
             }
