e4cb65b15070a1e13dde0c4dc81cd7e3ecba820b
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:36 +0100

    Runtime: 11 ms (Top 50.06%) | Memory: 10.6 MB (Top 23.88%)

--- scripts/algorithms/I/Intersection of Two Arrays/Intersection of Two Arrays.cpp
+++ scripts/algorithms/I/Intersection of Two Arrays/Intersection of Two Arrays.cpp
@@ -1,20 +1,21 @@
+// Runtime: 11 ms (Top 50.06%) | Memory: 10.6 MB (Top 23.88%)
 class Solution {
 public:
     //what i think is we have to return the intersection elements from both nums
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
+
         //result vector which will store those values which will be intersecting in both nums
         vector<int> result;
-        
+
         //map intersection, which will store values of all the elements in num1
         unordered_map<int,int> intersection;
-        
+
         //map will store all the values of num1(as key),with corresponding value 1
         for(auto character : nums1)
         {
             intersection[character] = 1;
         }
-        
+
         //this loop will check if any element of num1 is there in num2? if yes, insert it in result
         //after inserting it once, make its corresponding value to 0
         //so that if you find any elements, that is repeating in num2, as well as present in num1
@@ -30,7 +31,7 @@ public:
                 intersection[character] = 0;
             }
         }
-        
+
         //after getting all the intersecting elements,we return result
         return result;
     }
