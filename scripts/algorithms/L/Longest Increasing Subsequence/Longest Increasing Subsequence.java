f7c48c83c94e6d3098c5709600cba7817e5f8a64
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:02 +0100

    Runtime: 15 ms (Top 68.33%) | Memory: 43.7 MB (Top 85.25%)

--- scripts/algorithms/L/Longest Increasing Subsequence/Longest Increasing Subsequence.java
+++ scripts/algorithms/L/Longest Increasing Subsequence/Longest Increasing Subsequence.java
@@ -1,12 +1,13 @@
+// Runtime: 15 ms (Top 68.33%) | Memory: 43.7 MB (Top 85.25%)
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       
+
         ArrayList<Integer> lis = new ArrayList<>();
-        
+
         for(int num:nums){
-            
+
             int size = lis.size();
-            
+
             if(size==0 ||size>0 && num>lis.get(size-1)){
                 lis.add(num);
             }else{
@@ -14,14 +15,14 @@ class Solution {
                 lis.set(insertIndex,num);
             }
         }
-        
+
         return lis.size();
     }
-    
+
     int bs(List<Integer> list, int target){
         int lo = 0;
         int hi = list.size()-1;
-        
+
         while(lo<hi){
             int mid = (lo+hi)/2;
             if(list.get(mid)<target){
