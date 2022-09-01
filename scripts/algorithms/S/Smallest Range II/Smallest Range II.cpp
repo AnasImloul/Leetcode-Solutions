ef790896c07c0c7cf4c5058453a6e3bf25cd4e92
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:44 +0100

    Runtime: 45 ms (Top 16.75%) | Memory: 15.4 MB (Top 94.25%)

--- scripts/algorithms/S/Smallest Range II/Smallest Range II.cpp
+++ scripts/algorithms/S/Smallest Range II/Smallest Range II.cpp
@@ -1,15 +1,16 @@
+// Runtime: 45 ms (Top 16.75%) | Memory: 15.4 MB (Top 94.25%)
 //Need to consider each ith value as boundary and considering that,
 //the max value can be max val - k or i-1 th value + k
 //similarly the mn value can be min val + k or ith value - k
 //Need to check the difference of max min for all ith positions and minimize this.
 //Also check if the calculate min is larger than initial min or not
 /*
-Example : 
-     2   4  7  8  and k = 5
--k: -3  -1  2  3
-+k:  7   9  12 13
+Example :
+     2 4 7 8 and k = 5
+-k: -3 -1 2 3
++k: 7 9 12 13
 
-here min value will be 9 -2 = 7 when i = 2 but intital diff between min and max is 6, so the 
+here min value will be 9 -2 = 7 when i = 2 but intital diff between min and max is 6, so the
 answer will be 6.
 */
 
@@ -31,5 +32,5 @@ public:
         }
         return ans;
     }
-    
+
 };
