70fba5933409f65fe05c2add9b1d5915184f77ea
AnasImloul <pydev27@gmail.com>
2022-09-02 00:50:45 +0100

    Runtime: 101 ms (Top 24.96%) | Memory: 22 MB (Top 28.36%)

--- scripts/algorithms/C/Combination Sum/Combination Sum.cpp
+++ scripts/algorithms/C/Combination Sum/Combination Sum.cpp
@@ -1,3 +1,4 @@
+// Runtime: 101 ms (Top 24.96%) | Memory: 22 MB (Top 28.36%)
 class Solution {
     void combination(vector<int>& candidates, int target, vector<int> currComb, int currSum, int currIndex, vector<vector<int>>& ans){
         if(currSum>target) return; //backtrack
@@ -5,7 +6,7 @@ class Solution {
             ans.push_back(currComb); //store the solution and backtrack
             return;
         }
-        
+
         for(int i=currIndex; i<candidates.size(); i++){ //try all possible options for the next level
             currComb.push_back(candidates[i]); //put 1 option into the combination
             currSum+=candidates[i];
@@ -13,7 +14,7 @@ class Solution {
             currComb.pop_back(); //when this option backtrack to here, remove this and go on to the next option.
             currSum-=candidates[i];
         }
-        
+
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
@@ -23,4 +24,3 @@ public:
         return ans;
     }
 };
-
