0bb4c92ab4abed6c181461cf93a1a4327a100eb4
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:52 +0100

    Runtime: 1 ms (Top 98.59%) | Memory: 41.2 MB (Top 48.22%)

--- scripts/algorithms/N/N-Queens II/N-Queens II.java
+++ scripts/algorithms/N/N-Queens II/N-Queens II.java
@@ -1,20 +1,21 @@
+// Runtime: 1 ms (Top 98.59%) | Memory: 41.2 MB (Top 48.22%)
 class Solution {
     int count = 0;
     public int totalNQueens(int n) {
-        boolean col[]  = new boolean[n];
+        boolean col[] = new boolean[n];
         boolean diag[] = new boolean[2*n-1];
         boolean rdiag[] = new boolean[2*n-1];
-        
+
         countSolution(n,col, diag, rdiag, 0);
         return count;
-        
+
     }
     void countSolution(int n, boolean[] col, boolean[] diag, boolean[] rdiag, int r ){
         if (r == n ){
             count++;
             return;
         }
-        
+
         for(int c = 0 ; c < n; c++){
             if(col[c] == false && diag[r+c] == false && rdiag[r-c+n-1] == false){
                 col[c] = true;
@@ -26,6 +27,6 @@ class Solution {
                 rdiag[r-c+n-1] = false;
             }
         }
-        
+
     }
 }
