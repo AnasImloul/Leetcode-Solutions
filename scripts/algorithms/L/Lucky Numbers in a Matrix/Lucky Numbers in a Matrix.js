84396179edde1e5de1c49eb3dc95cd0989e3fef4
AnasImloul <pydev27@gmail.com>
2022-09-01 12:45:06 +0100

    Runtime: 114 ms (Top 31.62%) | Memory: 46.2 MB (Top 10.29%)

--- scripts/algorithms/L/Lucky Numbers in a Matrix/Lucky Numbers in a Matrix.js
+++ scripts/algorithms/L/Lucky Numbers in a Matrix/Lucky Numbers in a Matrix.js
@@ -1,23 +1,24 @@
+// Runtime: 114 ms (Top 31.62%) | Memory: 46.2 MB (Top 10.29%)
 /**
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var luckyNumbers  = function(matrix) {
+var luckyNumbers = function(matrix) {
     let rowLucky = new Set();
-	let colLucky = new Set();
+    let colLucky = new Set();
     let cols = [...Array(matrix[0].length)].map(e => []);
-    
+
     for (let i = 0; i < matrix.length; i++) {
         let row = matrix[i];
         rowLucky.add(Math.min(...row));
-		
-		// build columns
+
+        // build columns
         for (let j = 0; j < row.length; j++) {
             cols[j].push(row[j]);
         }
     }
-    
-	// Compare sets
+
+    // Compare sets
     for (const col of cols)
         colLucky.add(Math.max(...col));
     return [...rowLucky].filter(x => colLucky.has(x));
