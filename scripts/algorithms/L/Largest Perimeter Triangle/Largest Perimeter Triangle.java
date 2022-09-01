43c4706180cf203d22c3bede4869bf45952ca2ce
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:51 +0100

    Runtime: 12 ms (Top 44.30%) | Memory: 54.1 MB (Top 55.23%)

--- scripts/algorithms/L/Largest Perimeter Triangle/Largest Perimeter Triangle.java
+++ scripts/algorithms/L/Largest Perimeter Triangle/Largest Perimeter Triangle.java
@@ -1,7 +1,8 @@
+// Runtime: 12 ms (Top 44.30%) | Memory: 54.1 MB (Top 55.23%)
 class Solution {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        
+
         for(int i = nums.length - 3; i >= 0; i--) {
             if(nums[i] + nums[i + 1] > nums[i + 2])
                 return nums[i] + nums[i + 1] + nums[i + 2];
