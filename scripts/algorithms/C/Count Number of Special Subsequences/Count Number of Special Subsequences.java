a9fd7fa047346fe6e5ac7f994150bfa3add3aba9
AnasImloul <pydev27@gmail.com>
2022-09-01 17:10:00 +0100

    Runtime: 81 ms (Top 33.85%) | Memory: 118.7 MB (Top 84.62%)

--- scripts/algorithms/C/Count Number of Special Subsequences/Count Number of Special Subsequences.java
+++ scripts/algorithms/C/Count Number of Special Subsequences/Count Number of Special Subsequences.java
@@ -1,3 +1,4 @@
+// Runtime: 81 ms (Top 33.85%) | Memory: 118.7 MB (Top 84.62%)
 class Solution {
     public int countSpecialSubsequences(int[] nums) {
         long z = 0; //starting and ending with zero
@@ -6,13 +7,13 @@ class Solution {
         int mod = 1000000007;
         for (int i : nums) {
             if (i == 0) {
-                z = ((2*z) % mod + 1) % mod;  //zero = 2*zero+1
+                z = ((2*z) % mod + 1) % mod; //zero = 2*zero+1
             } else if (i == 1) {
                 zo = ((2 * zo) % mod + z % mod) % mod; //zero = 2*zeroOne+zero
             } else {
-                zot = ((2 * zot) % mod + zo % mod) % mod;  //zeroOneTwo = 2*zeroOneTwo+zeroOne
+                zot = ((2 * zot) % mod + zo % mod) % mod; //zeroOneTwo = 2*zeroOneTwo+zeroOne
             }
         }
         return (int)(zot%mod);
-	}
+    }
 }
