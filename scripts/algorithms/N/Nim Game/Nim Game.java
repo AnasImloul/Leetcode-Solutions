328d18ff0ef8401e95c816f2b12da36397ab19e1
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:52 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40.8 MB (Top 59.25%)

--- scripts/algorithms/N/Nim Game/Nim Game.java
+++ scripts/algorithms/N/Nim Game/Nim Game.java
@@ -1,7 +1,8 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40.8 MB (Top 59.25%)
 class Solution {
     public boolean canWinNim(int n) {
         if(n%4==0) return false;
-        
+
         return true;
     }
 }
