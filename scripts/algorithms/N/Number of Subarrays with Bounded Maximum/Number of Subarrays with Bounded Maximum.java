e3fddf9866d9668f1dc2dc1ee21bcad4157862e8
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:04 +0100

    Runtime: 4 ms (Top 82.54%) | Memory: 72.1 MB (Top 7.10%)

--- scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.java
+++ scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.java
@@ -1,22 +1,22 @@
+// Runtime: 4 ms (Top 82.54%) | Memory: 72.1 MB (Top 7.10%)
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
         int res = 0;
-        
+
         int s = -1;
         int e = -1;
-        
+
         for(int i=0;i<nums.length;i++){
-            
+
             if(nums[i] >= left && nums[i] <= right){
                 e = i;
             }else if(nums[i] > right){
                 e = s = i;
             }
-            
+
             res += (e - s);
         }
-        
-        
+
         return res;
     }
 }
