48300f3d0c2187a9f954d491b84501f7560028ab
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:38 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.1 MB (Top 19.77%)

--- scripts/algorithms/F/Find Three Consecutive Integers That Sum to a Given Number/Find Three Consecutive Integers That Sum to a Given Number.cpp
+++ scripts/algorithms/F/Find Three Consecutive Integers That Sum to a Given Number/Find Three Consecutive Integers That Sum to a Given Number.cpp
@@ -1,9 +1,10 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.1 MB (Top 19.77%)
 class Solution {
 public:
     vector<long long> sumOfThree(long long num) {
         //binary Search
-        
-        long long lo = -1;// -1 becoz we have num == 0 as well if we put lo=0 then it will fail in 1 testcase 
+
+        long long lo = -1;// -1 becoz we have num == 0 as well if we put lo=0 then it will fail in 1 testcase
         long long hi = num;
         while(lo<=hi){
             long long mid = lo +(hi-lo)/2;
@@ -18,7 +19,7 @@ public:
                 lo = mid+1;
             }
         }
-        
+
         return {};
     }
 };
