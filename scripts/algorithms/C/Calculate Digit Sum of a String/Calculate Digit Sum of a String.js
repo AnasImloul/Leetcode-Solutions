bf75f1810ffd076910df082586d32f237f58e2c8
AnasImloul <pydev27@gmail.com>
2022-09-02 00:07:12 +0100

    Runtime: 117 ms (Top 16.67%) | Memory: 42.4 MB (Top 76.67%)

--- scripts/algorithms/C/Calculate Digit Sum of a String/Calculate Digit Sum of a String.js
+++ scripts/algorithms/C/Calculate Digit Sum of a String/Calculate Digit Sum of a String.js
@@ -1,11 +1,12 @@
+// Runtime: 117 ms (Top 16.67%) | Memory: 42.4 MB (Top 76.67%)
 var digitSum = function(s, k) {
     while (s.length > k) {
         let newString = "";
         for (let i = 0; i < s.length; i += k)
             newString += s.substring(i, i + k).split("").reduce((acc, val) => acc + (+val), 0);
-        
+
         s = newString;
     }
-    
+
     return s;
 };
