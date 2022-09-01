adf8e6f4bb7dae62d3d61f878b5d3aaa1279b45c
AnasImloul <pydev27@gmail.com>
2022-09-01 17:09:56 +0100

    Runtime: 4 ms (Top 97.62%) | Memory: 41.6 MB (Top 89.76%)

--- scripts/algorithms/M/Minimum Number of Days to Eat N Oranges/Minimum Number of Days to Eat N Oranges.java
+++ scripts/algorithms/M/Minimum Number of Days to Eat N Oranges/Minimum Number of Days to Eat N Oranges.java
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 97.62%) | Memory: 41.6 MB (Top 89.76%)
 class Solution {
     HashMap<Integer,Integer>map;
     public int minDays(int n) {
@@ -13,8 +14,7 @@ class Solution {
          int two = 1+(n%3)+dp(n/3);
         map.put(n,Math.min(one,two));
         return map.get(n);
-} 
+}
     }
         // int one = 1+(n%2)+cache(n/2);
         // int two = 1+(n%3)+cache(n/3);
-
