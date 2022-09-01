cff5b8464965a13f5306be79214ba10c2b55970e
AnasImloul <pydev27@gmail.com>
2022-09-01 23:57:55 +0100

    Runtime: 59 ms (Top 52.37%) | Memory: 15.1 MB (Top 89.03%)

--- scripts/algorithms/F/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List.py
+++ scripts/algorithms/F/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List.py
@@ -1,5 +1,6 @@
+# Runtime: 59 ms (Top 52.37%) | Memory: 15.1 MB (Top 89.03%)
 #Call the right of the tree node till the node root left and right is not None
-#After reaching the bottom of the tree make the root.right = prev and 
+#After reaching the bottom of the tree make the root.right = prev and
 #root.left = None and then prev = None
 #Initially prev will point to None but this is used to point the previously visited root node
 #Prev pointer helps us to change the values from left to right
@@ -8,20 +9,20 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        prev = None    #You can also define that variable inside the init function using self keyword
+        prev = None #You can also define that variable inside the init function using self keyword
         def dfs(root):
             nonlocal prev
-            
+
             if not root:
                 return
-            
+
             dfs(root.right)
             dfs(root.left)
-            
+
             root.right = prev
             root.left = None
             prev = root
-            
+
         dfs(root)
 # If the above solution is hard to understand than one can do level order traversal
 #Using Stack DS but this will increase the space complexity to O(N).
