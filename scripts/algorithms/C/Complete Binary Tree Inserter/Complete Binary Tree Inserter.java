0b97eebfa466c6a237a973f83798f102b272570c
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:01 +0100

    Runtime: 35 ms (Top 23.02%) | Memory: 47.3 MB (Top 32.61%)

--- scripts/algorithms/C/Complete Binary Tree Inserter/Complete Binary Tree Inserter.java
+++ scripts/algorithms/C/Complete Binary Tree Inserter/Complete Binary Tree Inserter.java
@@ -1,8 +1,9 @@
+// Runtime: 35 ms (Top 23.02%) | Memory: 47.3 MB (Top 32.61%)
 class CBTInserter {
-    
+
     private TreeNode root;
     private int total;
-    
+
     private int count(TreeNode root) {
         if (root == null) return 0;
         return 1+count(root.left)+count(root.right);
@@ -12,7 +13,7 @@ class CBTInserter {
         this.root = root;
         total = count(root);
     }
-    
+
     private int insertBinary(int val, int k, int right) {
         int left = 0;
         var ptr = root;
@@ -27,13 +28,13 @@ class CBTInserter {
                 ptr = ptr.left;
                 right = mid;
             } else if (mid < k) {
-                left  = mid+1;
+                left = mid+1;
                 ptr = ptr.right;
             }
         }
         return 0;
     }
-    
+
     public int insert(int val) {
         int depth = 0;
         int n = total;
@@ -42,18 +43,18 @@ class CBTInserter {
             n /= 2;
         }
         if ((1<<depth)-1 == total) depth++;
-		// k is the new index in the lowest level of tree, right is the max index of the lowest level of tree
-		// e.g. for tree
-		//    1
-		//   / \
-		// 2   new
-//index    0    1
-	    // it's insertBinary(val, 1, 1)
+        // k is the new index in the lowest level of tree, right is the max index of the lowest level of tree
+        // e.g. for tree
+        // 1
+        // / \
+        // 2 new
+//index 0 1
+        // it's insertBinary(val, 1, 1)
         var res = insertBinary(val, total - (1<<(depth-1))+1 , (1<<(depth-1)) -1);
         total++;
         return res;
     }
-    
+
     public TreeNode get_root() {
         return root;
     }
