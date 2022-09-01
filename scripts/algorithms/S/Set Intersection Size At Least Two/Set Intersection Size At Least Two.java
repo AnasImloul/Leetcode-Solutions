186c945c959b870b3b3e753d8810baba57605949
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:12 +0100

    Runtime: 17 ms (Top 28.66%) | Memory: 53.2 MB (Top 21.64%)

--- scripts/algorithms/S/Set Intersection Size At Least Two/Set Intersection Size At Least Two.java
+++ scripts/algorithms/S/Set Intersection Size At Least Two/Set Intersection Size At Least Two.java
@@ -1,9 +1,10 @@
+// Runtime: 17 ms (Top 28.66%) | Memory: 53.2 MB (Top 21.64%)
 //Time Complexity O(Nlog(N)) - N is the number of intervals
 //Space Complexity O(N) - N is the number of intervals, can be reduced to O(1) if needed
 class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
         //corner case: can intervals be null or empty? No
-        
+
         //First, sort the intervals by end, then by reverse order start
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
@@ -14,22 +15,22 @@ class Solution {
                 return a[1] - b[1];
             }
         });
-        
+
         //Second, for each two intervals, greedily find if the previous interval would satisfy next interval's request
         List<Integer> list = new ArrayList<>(); //basically the ending set S, btw, we actually do not need this but I use it here for better intuition
-        
+
         //add last two nums within the range
         list.add(intervals[0][1] - 1);
         list.add(intervals[0][1]);
-        
+
         for (int i = 1; i < intervals.length; i++) {
             int lastOne = list.get(list.size() - 1);
             int lastTwo = list.get(list.size() - 2);
-            
+
             int[] interval = intervals[i];
             int start = interval[0];
             int end = interval[1];
-            
+
             //if overlaps at least 2
             if (lastOne >= start && lastTwo >= start) {
                 continue;
@@ -40,7 +41,7 @@ class Solution {
                 list.add(end);
             }
         }
-        
-        return list.size();        
+
+        return list.size();
     }
 }
