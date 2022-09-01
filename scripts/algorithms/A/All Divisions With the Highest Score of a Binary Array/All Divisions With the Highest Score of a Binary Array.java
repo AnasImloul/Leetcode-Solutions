88727f162ac6a3adf7e02c6c21ad1c89e5dd3cc1
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:11 +0100

    Runtime: 21 ms (Top 98.47%) | Memory: 61.2 MB (Top 91.95%)

--- scripts/algorithms/A/All Divisions With the Highest Score of a Binary Array/All Divisions With the Highest Score of a Binary Array.java
+++ scripts/algorithms/A/All Divisions With the Highest Score of a Binary Array/All Divisions With the Highest Score of a Binary Array.java
@@ -1,25 +1,26 @@
+// Runtime: 21 ms (Top 98.47%) | Memory: 61.2 MB (Top 91.95%)
 class Solution {
     public List<Integer> maxScoreIndices(int[] nums) {
         int N = nums.length;
         List<Integer> res = new ArrayList<>();
-        
+
         int[] pref = new int[N + 1];
         pref[0] = 0; // at zeroth division we have no elements
         for(int i = 0; i < N; ++i) pref[i+1] = nums[i] + pref[i];
-        
+
         int maxScore = -1;
         int onesToRight, zeroesToLeft, currScore;
-		
+
         for(int i = 0; i < N + 1; ++i) {
             onesToRight = pref[N] - pref[i];
             zeroesToLeft = i - pref[i];
             currScore = zeroesToLeft + onesToRight;
-	
+
             if(currScore > maxScore) {
                 res.clear();
                 maxScore = currScore;
             }
-            if(currScore == maxScore) res.add(i);                        
+            if(currScore == maxScore) res.add(i);
         }
         return res;
     }
