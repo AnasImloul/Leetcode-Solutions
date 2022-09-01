13bdaa01b21a209b721a13f6d086a786036e7d22
AnasImloul <pydev27@gmail.com>
2022-09-02 00:48:02 +0100

    Runtime: 372 ms (Top 18.39%) | Memory: 20.4 MB (Top 30.28%)

--- scripts/algorithms/S/Serialize and Deserialize Binary Tree/Serialize and Deserialize Binary Tree.py
+++ scripts/algorithms/S/Serialize and Deserialize Binary Tree/Serialize and Deserialize Binary Tree.py
@@ -1,19 +1,19 @@
+# Runtime: 372 ms (Top 18.39%) | Memory: 20.4 MB (Top 30.28%)
 class Codec:
 
     def serialize(self, root):
         if not root: return 'N'
-        
+
         left = self.serialize(root.left)
         right = self.serialize(root.right)
-        
+
         return ','.join([str(root.val), left, right])
-        
 
     def deserialize(self, data):
         data = data.split(',')
         root = self.buildTree(data)
         return root
-        
+
     def buildTree(self, data):
         val = data.pop(0)
         if val == 'N': return None
