2c4d635d8f48405fb80a08916c0893e7159a2308
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:09 +0100

    Runtime: 4 ms (Top 85.13%) | Memory: 42 MB (Top 91.86%)

--- scripts/algorithms/G/Greatest English Letter in Upper and Lower Case/Greatest English Letter in Upper and Lower Case.java
+++ scripts/algorithms/G/Greatest English Letter in Upper and Lower Case/Greatest English Letter in Upper and Lower Case.java
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 85.13%) | Memory: 42 MB (Top 91.86%)
 class Solution
 {
     public String greatestLetter(String s)
@@ -5,7 +6,7 @@ class Solution
         Set<Character> set = new HashSet<>();
         for(char ch : s.toCharArray())
             set.add(ch);
-        
+
         for(char ch = 'Z'; ch >= 'A'; ch--)
             if(set.contains(ch) && set.contains((char)('a'+(ch-'A'))))
                return ""+ch;
