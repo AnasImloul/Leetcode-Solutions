84ed47ae8dcba4a660dcc94dfb0bc0260d2f7cce
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:11 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 83.22%)

--- scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.java
+++ scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.java
@@ -1,23 +1,24 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 83.22%)
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         int ones = 0; //count of students who prefer type1
         int zeros = 0; //count of students who prefer type0
-		
+
         for(int stud : students){
             if(stud == 0) zeros++;
             else ones++;
         }
-        
+
         // for each sandwich in sandwiches
         for(int sandwich : sandwiches){
-            if(sandwich == 0){  // if sandwich is of type0
+            if(sandwich == 0){ // if sandwich is of type0
                 if(zeros == 0){ // if no student want a type0 sandwich
                     return ones;
                 }
                 zeros--;
             }
-            else{  // if sandwich is of type1
-                if(ones == 0){  // if no student want a type1 sandwich 
+            else{ // if sandwich is of type1
+                if(ones == 0){ // if no student want a type1 sandwich
                     return zeros;
                 }
                 ones--;
