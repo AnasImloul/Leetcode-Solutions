f8a892d058c385bc578e09e5377b65e10d84b90a
AnasImloul <pydev27@gmail.com>
2022-09-01 23:49:26 +0100

    Runtime: 694 ms (Top 46.21%) | Memory: 141.7 MB (Top 30.30%)

--- scripts/algorithms/F/Find Array Given Subset Sums/Find Array Given Subset Sums.cpp
+++ scripts/algorithms/F/Find Array Given Subset Sums/Find Array Given Subset Sums.cpp
@@ -1,31 +1,32 @@
+// Runtime: 694 ms (Top 46.21%) | Memory: 141.7 MB (Top 30.30%)
 class Solution {
 public:
     vector<int> recoverArray(int n, vector<int>& sums) {
-        sort(sums.begin(), sums.end()); 
-        
-        vector<int> ans; 
+        sort(sums.begin(), sums.end());
+
+        vector<int> ans;
         while (n--) {
-            int diff = sums[1] - sums[0]; 
-            unordered_map<int, int> freq; 
-            vector<int> ss0, ss1; 
-            bool on = false; 
-            for (auto& x : sums) 
+            int diff = sums[1] - sums[0];
+            unordered_map<int, int> freq;
+            vector<int> ss0, ss1;
+            bool on = false;
+            for (auto& x : sums)
                 if (!freq[x]) {
-                    ss0.push_back(x); 
-                    freq[x+diff]++; 
-                    if (x == 0) on = true; 
+                    ss0.push_back(x);
+                    freq[x+diff]++;
+                    if (x == 0) on = true;
                 } else {
-                    ss1.push_back(x); 
-                    freq[x]--; 
+                    ss1.push_back(x);
+                    freq[x]--;
                 }
             if (on) {
-                ans.push_back(diff); 
-                sums = ss0; 
+                ans.push_back(diff);
+                sums = ss0;
             } else {
-                ans.push_back(-diff); 
-                sums = ss1; 
+                ans.push_back(-diff);
+                sums = ss1;
             }
         }
-        return ans; 
+        return ans;
     }
 };
