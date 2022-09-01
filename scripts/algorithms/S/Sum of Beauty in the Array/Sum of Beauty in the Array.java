4444262a720dd7cd6c5878169e48c4b9b1be8592
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:13 +0100

    Runtime: 9 ms (Top 73.89%) | Memory: 94 MB (Top 67.52%)

--- scripts/algorithms/S/Sum of Beauty in the Array/Sum of Beauty in the Array.java
+++ scripts/algorithms/S/Sum of Beauty in the Array/Sum of Beauty in the Array.java
@@ -1,8 +1,9 @@
+// Runtime: 9 ms (Top 73.89%) | Memory: 94 MB (Top 67.52%)
 class Solution {
     public int sumOfBeauties(int[] nums) {
         boolean[] left = new boolean[nums.length];
         boolean[] right = new boolean[nums.length];
-        
+
         left[0] = true;
         int leftMax = nums[0];
         for(int i = 1; i < nums.length; i++) {
@@ -11,7 +12,7 @@ class Solution {
                 leftMax = nums[i];
             }
         }
-        
+
         right[nums.length-1] = true;
         int rightMin = nums[nums.length-1];
         for(int i = nums.length-2; i >= 0; i--) {
@@ -20,17 +21,17 @@ class Solution {
                 rightMin = nums[i];
             }
         }
-        
+
         int beautyCount = 0;
         for(int i = 1; i < nums.length-1; i++) {
             if(left[i] && right[i]) {
                 beautyCount += 2;
             }
-            
+
             else if(nums[i-1] < nums[i] && nums[i] < nums[i+1]) {
                 beautyCount += 1;
             }
-            
+
         }
         return beautyCount;
     }
