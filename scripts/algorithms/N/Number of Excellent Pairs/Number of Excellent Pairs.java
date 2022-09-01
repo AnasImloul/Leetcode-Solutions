0a0f0faae87555d812877faabcf407e830d118c3
AnasImloul <pydev27@gmail.com>
2022-09-01 17:10:03 +0100

    Runtime: 785 ms (Top 5.01%) | Memory: 176.5 MB (Top 5.07%)

--- scripts/algorithms/N/Number of Excellent Pairs/Number of Excellent Pairs.java
+++ scripts/algorithms/N/Number of Excellent Pairs/Number of Excellent Pairs.java
@@ -1,3 +1,4 @@
+// Runtime: 785 ms (Top 5.01%) | Memory: 176.5 MB (Top 5.07%)
 class Solution {
     public long countExcellentPairs(int[] nums, int k) {
         HashMap<Integer,Set<Integer>> map = new HashMap<>();
@@ -11,8 +12,8 @@ class Solution {
         for(int i : nums){
             if(vis.contains(i)) continue;
             int need = Math.max(0,k-Integer.bitCount(i));
-            for(int key : map.keySet())   // runs at max 30 times
-				if(key >= need) ans += (long) map.get(key).size();
+            for(int key : map.keySet()) // runs at max 30 times
+                if(key >= need) ans += (long) map.get(key).size();
             vis.add(i);
         }
         return ans;
