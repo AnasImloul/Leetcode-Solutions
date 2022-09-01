b0feeba3ed929aec2ec65afb096245fb114cd703
AnasImloul <pydev27@gmail.com>
2022-09-01 17:24:14 +0100

    Runtime: 2 ms (Top 83.60%) | Memory: 42.5 MB (Top 46.76%)

--- scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
+++ scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
@@ -1,3 +1,4 @@
+// Runtime: 2 ms (Top 83.60%) | Memory: 42.5 MB (Top 46.76%)
 class Solution {
     public int longestSubsequence(String s, int k) {
         int z=0;
@@ -5,21 +6,21 @@ class Solution {
         for(int i=0;i<s.length();i++)if(s.charAt(i)=='0')z++;
 
         int num=0,base=1,len=0;
-		//take as many ones from right as possible until they do not make the num>k
+        //take as many ones from right as possible until they do not make the num>k
         for(int i=s.length()-1;i>=0;i--){
-            
+
             if(num+base>k)break;
             if(s.charAt(i)=='1'){
                num+=base;
             }
             else {
-			//remove already taken zeros from zeros count
+            //remove already taken zeros from zeros count
                 z--;
             }
             base*=2;
             len++;
         }
-        
+
         return len+z;
     }
 }
