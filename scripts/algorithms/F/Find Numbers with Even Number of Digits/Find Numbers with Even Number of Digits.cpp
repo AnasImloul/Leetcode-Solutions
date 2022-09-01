c09a81a740caf6740e72f517bd4d9e84396105d7
AnasImloul <pydev27@gmail.com>
2022-09-01 17:34:40 +0100

    Runtime: 11 ms (Top 48.50%) | Memory: 9.9 MB (Top 12.31%)

--- scripts/algorithms/F/Find Numbers with Even Number of Digits/Find Numbers with Even Number of Digits.cpp
+++ scripts/algorithms/F/Find Numbers with Even Number of Digits/Find Numbers with Even Number of Digits.cpp
@@ -1,8 +1,9 @@
+// Runtime: 11 ms (Top 48.50%) | Memory: 9.9 MB (Top 12.31%)
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int count = 0;
-        
+
         for(auto it:nums)
         {
             int amount = 0;
