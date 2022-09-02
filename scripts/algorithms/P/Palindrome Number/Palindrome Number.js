9e3b8dae07aee844baf80d98d9d9d7c315b26903
AnasImloul <pydev27@gmail.com>
2022-09-02 18:27:06 +0100

    Runtime: 314 ms (Top 29.18%) | Memory: 50.3 MB (Top 91.06%)

--- scripts/algorithms/P/Palindrome Number/Palindrome Number.js
+++ scripts/algorithms/P/Palindrome Number/Palindrome Number.js
@@ -1,9 +1,10 @@
+// Runtime: 314 ms (Top 29.18%) | Memory: 50.3 MB (Top 91.06%)
 var isPalindrome = function(x) {
     if(x < 0 || x % 10 === 0 && x !== 0) return false
-    
+
     let num = x
     let rev_x = 0
-    
+
     while(x > 0){
         let digit = Math.floor(x % 10)
         rev_x = Math.floor(rev_x * 10 + digit)
