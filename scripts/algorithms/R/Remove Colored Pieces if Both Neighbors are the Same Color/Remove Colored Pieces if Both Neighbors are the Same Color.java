d416b4b2b9bee2adf0ac5c3fe13676fd45734f9d
AnasImloul <pydev27@gmail.com>
2022-09-02 18:48:10 +0100

    Runtime: 48 ms (Top 5.50%) | Memory: 52.8 MB (Top 84.58%)

--- scripts/algorithms/R/Remove Colored Pieces if Both Neighbors are the Same Color/Remove Colored Pieces if Both Neighbors are the Same Color.java
+++ scripts/algorithms/R/Remove Colored Pieces if Both Neighbors are the Same Color/Remove Colored Pieces if Both Neighbors are the Same Color.java
@@ -1,11 +1,12 @@
+// Runtime: 48 ms (Top 5.50%) | Memory: 52.8 MB (Top 84.58%)
 class Solution {
     public boolean winnerOfGame(String colors) {
         int cntA=0,cntB=0;
         for(int i=1;i<colors.length()-1;i++){
-             if(colors.charAt(i)=='A'&&colors.charAt(i-1)=='A'&&colors.charAt(i+1)=='A')cntA++;			
+             if(colors.charAt(i)=='A'&&colors.charAt(i-1)=='A'&&colors.charAt(i+1)=='A')cntA++;
             if(colors.charAt(i)=='B'&&colors.charAt(i-1)=='B'&&colors.charAt(i+1)=='B')cntB++;
         }
-        
+
         return cntA>cntB;
     }
 }
