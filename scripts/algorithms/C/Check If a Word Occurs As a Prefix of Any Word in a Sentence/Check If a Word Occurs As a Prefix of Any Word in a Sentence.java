e657ff689c5866ddec5d7e1480a82fc29d0cd3a5
AnasImloul <pydev27@gmail.com>
2022-09-02 18:48:15 +0100

    Runtime: 1 ms (Top 44.68%) | Memory: 41.8 MB (Top 51.62%)

--- scripts/algorithms/C/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Check If a Word Occurs As a Prefix of Any Word in a Sentence.java
+++ scripts/algorithms/C/Check If a Word Occurs As a Prefix of Any Word in a Sentence/Check If a Word Occurs As a Prefix of Any Word in a Sentence.java
@@ -1,10 +1,11 @@
+// Runtime: 1 ms (Top 44.68%) | Memory: 41.8 MB (Top 51.62%)
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
         if(!sentence.contains(searchWord))
             return -1;
         boolean y=false;
         String[] str=sentence.split(" ");
-        
+
         for(int i=0;i<str.length;i++){
             if(str[i].contains(searchWord)){
                 for(int j=0;j<searchWord.length();j++){
@@ -12,8 +13,7 @@ class Solution {
                         y=true;
                         break;
                     }
-                   
-                    
+
                 }
                  if(!y){
                      return i+1;
@@ -21,7 +21,7 @@ class Solution {
             }
             y=false;
         }
-        
+
         return -1;
     }
 }
