0f6910d92158f873a8c2319b6d531b9af0d0624d
AnasImloul <pydev27@gmail.com>
2022-09-02 19:11:31 +0100

    Runtime: 145 ms (Top 75.66%) | Memory: 15.5 MB (Top 23.89%)

--- scripts/algorithms/S/Smallest Range I/Smallest Range I.py
+++ scripts/algorithms/S/Smallest Range I/Smallest Range I.py
@@ -1,3 +1,4 @@
+# Runtime: 145 ms (Top 75.66%) | Memory: 15.5 MB (Top 23.89%)
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         return max(0, max(nums)-min(nums)-2*k)
