a78433d623d79eb33b132f2c976c8955463a8a3f
AnasImloul <pydev27@gmail.com>
2022-09-02 19:39:09 +0100

    Runtime: 113 ms (Top 25.04%) | Memory: 44.6 MB (Top 16.66%)

--- scripts/algorithms/B/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.js
+++ scripts/algorithms/B/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.js
@@ -1,3 +1,4 @@
+// Runtime: 113 ms (Top 25.04%) | Memory: 44.6 MB (Top 16.66%)
 var levelOrder = function(root) {
   const result = [];
   if (root == null) return result
@@ -8,18 +9,18 @@ var levelOrder = function(root) {
   q.enqueue(root)
 
   while (!q.isEmpty()) {
-	let levelSize = q.size();
-	while (levelSize-- != 0) {
-	  let node = q.dequeue()
-	  temp.push(node.val)
-	  // enqueue both children first, 
-	  // before looking at the next dequeued item
-	  if (node.left != null) q.enqueue(node.left);
-	  if (node.right != null) q.enqueue(node.right);
-	}
-	result.push(temp);
-	temp = [];
-	lvl +=1
+    let levelSize = q.size();
+    while (levelSize-- != 0) {
+      let node = q.dequeue()
+      temp.push(node.val)
+      // enqueue both children first,
+      // before looking at the next dequeued item
+      if (node.left != null) q.enqueue(node.left);
+      if (node.right != null) q.enqueue(node.right);
+    }
+    result.push(temp);
+    temp = [];
+    lvl +=1
   }
 
   return result
