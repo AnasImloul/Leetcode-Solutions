98614e6b6abf8053601c9e62ff382f0870bb77fb
AnasImloul <pydev27@gmail.com>
2022-09-02 19:11:34 +0100

    Runtime: 77 ms (Top 19.02%) | Memory: 14.8 MB (Top 20.86%)

--- scripts/algorithms/S/Soup Servings/Soup Servings.py
+++ scripts/algorithms/S/Soup Servings/Soup Servings.py
@@ -1,3 +1,4 @@
+# Runtime: 77 ms (Top 19.02%) | Memory: 14.8 MB (Top 20.86%)
 class Solution:
     def soupServings(self, n: int) -> float:
         if n >= 4276: return 1.0
@@ -8,5 +9,5 @@ class Solution:
             if a <= 0: return 1
             if b <= 0: return 0
             return (f(a-100,b) + f(a-75,b-25) + f(a-50,b-50) + f(a-25,b-75))*.25
-        
+
         return f(n,n)
