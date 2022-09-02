29b69b77b1c765cf51f5d9c10d95113cf9e8d9ba
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:20 +0100

    Runtime: 159 ms (Top 10.55%) | Memory: 30.9 MB (Top 79.63%)

--- scripts/algorithms/C/Count Items Matching a Rule/Count Items Matching a Rule.cpp
+++ scripts/algorithms/C/Count Items Matching a Rule/Count Items Matching a Rule.cpp
@@ -1,3 +1,4 @@
+// Runtime: 159 ms (Top 10.55%) | Memory: 30.9 MB (Top 79.63%)
 class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
@@ -5,12 +6,12 @@ public:
         if(ruleKey=="type")i=0;
         if(ruleKey=="color")i=1;
         if(ruleKey=="name")i=2;
-        
+
         int ans=0;
         for(int j=0;j<items.size();j++){
             if(items[j][i]==ruleValue)ans++;
         }
-        
+
         return ans;
     }
 };
