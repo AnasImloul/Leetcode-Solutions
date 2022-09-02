27c695fb923861fb36f298f7ef9205e0970a04fa
AnasImloul <pydev27@gmail.com>
2022-09-02 19:39:01 +0100

    Runtime: 134 ms (Top 14.15%) | Memory: 46.1 MB (Top 74.97%)

--- scripts/algorithms/M/Minimum Size Subarray Sum/Minimum Size Subarray Sum.js
+++ scripts/algorithms/M/Minimum Size Subarray Sum/Minimum Size Subarray Sum.js
@@ -1,3 +1,4 @@
+// Runtime: 134 ms (Top 14.15%) | Memory: 46.1 MB (Top 74.97%)
 /**
  * @param {number} target
  * @param {number[]} nums
@@ -8,17 +9,17 @@ var minSubArrayLen = function(target, nums) {
     let sum = 0;
     let tempcounter = 0;
     let counter = Infinity;
-   
+
     for(var indexI=0; indexI<nums.length; indexI++){
-   		sum = sum + nums[indexI];
-        tempcounter++;     
+           sum = sum + nums[indexI];
+        tempcounter++;
 
         while(sum >= target) {
            sum = sum - nums[indexStartPosition];
-           indexStartPosition = indexStartPosition + 1;  
-           counter = Math.min(counter, tempcounter);   
+           indexStartPosition = indexStartPosition + 1;
+           counter = Math.min(counter, tempcounter);
            tempcounter--;
-        }  
+        }
 
     }
 
