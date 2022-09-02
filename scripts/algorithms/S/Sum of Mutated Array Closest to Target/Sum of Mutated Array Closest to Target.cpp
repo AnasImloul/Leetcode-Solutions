ac57b99cac6e7bce253b1cc8d54ea04165ffec91
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:24 +0100

    Runtime: 32 ms (Top 32.36%) | Memory: 12.4 MB (Top 70.15%)

--- scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.cpp
+++ scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.cpp
@@ -1,9 +1,10 @@
+// Runtime: 32 ms (Top 32.36%) | Memory: 12.4 MB (Top 70.15%)
 // This Question Includes Both Binary Search And bit of Greedy Concept also.
-// See We Know Ans Always Lies Between 0 and maximum element according to given question condition Because 
+// See We Know Ans Always Lies Between 0 and maximum element according to given question condition Because
 // sum value at maximum element is same as any other element greater than it.
-// So we get our sum from getval function after that you need to choose as to move forward(l = mid+1) or backward 
+// So we get our sum from getval function after that you need to choose as to move forward(l = mid+1) or backward
 // i.e.(h = mid-1) so if sum value we obtain is less than target then l = mid+1 why so??
-// Because 2 3 5 lets suppose you are having this array if we pick 2 as mid then sum value will be 6 whereas if we pick 3 then sum value will be 8 and 10 when we pick 5 so notice that sum will increase when we increase value and 
+// Because 2 3 5 lets suppose you are having this array if we pick 2 as mid then sum value will be 6 whereas if we pick 3 then sum value will be 8 and 10 when we pick 5 so notice that sum will increase when we increase value and
 // correspondingly decrease when we decrease value...So yess This is all what we did and got Accepted.
 class Solution {
 public:
@@ -37,7 +38,7 @@ public:
             }
             else
                 h = mid -1;
-            
+
             int j = abs(k - target);
             if(j<min1)
             {
