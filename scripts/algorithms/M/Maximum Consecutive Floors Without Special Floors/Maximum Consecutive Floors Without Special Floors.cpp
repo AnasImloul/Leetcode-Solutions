c58f167404108cd2925c0a6521780375fa134636
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:21 +0100

    Runtime: 381 ms (Top 5.26%) | Memory: 58.1 MB (Top 42.68%)

--- scripts/algorithms/M/Maximum Consecutive Floors Without Special Floors/Maximum Consecutive Floors Without Special Floors.cpp
+++ scripts/algorithms/M/Maximum Consecutive Floors Without Special Floors/Maximum Consecutive Floors Without Special Floors.cpp
@@ -1,14 +1,15 @@
+// Runtime: 381 ms (Top 5.26%) | Memory: 58.1 MB (Top 42.68%)
 class Solution {
 public:
     int maxConsecutive(int bottom, int top, vector<int>& special) {
-        
+
         int res(0), n(size(special));
         sort(begin(special), end(special));
-        
+
         for (int i=1; i<n; i++) {
             res = max(res, special[i]-special[i-1]-1);
         }
-                
+
         return max({res, special[0]-bottom, top-special[n-1]});
     }
 };
