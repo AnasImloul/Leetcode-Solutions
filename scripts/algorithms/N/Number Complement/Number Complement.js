b4d7c71866119030cd9398e0f6ecc53650ac204e
AnasImloul <pydev27@gmail.com>
2022-09-02 18:27:12 +0100

    Runtime: 73 ms (Top 73.80%) | Memory: 42.2 MB (Top 25.76%)

--- scripts/algorithms/N/Number Complement/Number Complement.js
+++ scripts/algorithms/N/Number Complement/Number Complement.js
@@ -1,3 +1,4 @@
+// Runtime: 73 ms (Top 73.80%) | Memory: 42.2 MB (Top 25.76%)
 var findComplement = function(num) {
     return num ^ parseInt(Array(num.toString(2).length).fill("1").join(""), 2)
 }
