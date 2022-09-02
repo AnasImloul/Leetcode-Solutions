e79e30091a5d747dbd8ec3e88b889139b3a53f49
AnasImloul <pydev27@gmail.com>
2022-09-02 18:27:02 +0100

    Runtime: 865 ms (Top 8.70%) | Memory: 71.6 MB (Top 8.70%)

--- scripts/algorithms/M/Minimized Maximum of Products Distributed to Any Store/Minimized Maximum of Products Distributed to Any Store.js
+++ scripts/algorithms/M/Minimized Maximum of Products Distributed to Any Store/Minimized Maximum of Products Distributed to Any Store.js
@@ -1,15 +1,16 @@
+// Runtime: 865 ms (Top 8.70%) | Memory: 71.6 MB (Top 8.70%)
 var minimizedMaximum = function(n, quantities) {
     const MAX = Number.MAX_SAFE_INTEGER;
     const m = quantities.length;
-    
+
     let left = 1;
     let right = quantities.reduce((acc, num) => acc + num, 0);
-    
+
     let minRes = MAX;
-    
+
     while (left <= right) {
         const mid = (left + right) >> 1;
-        
+
         if (canDistribute(mid)) {
             minRes = Math.min(minRes, mid);
             right = mid - 1;
@@ -18,19 +19,18 @@ var minimizedMaximum = function(n, quantities) {
             left = mid + 1;
         }
     }
-    
+
     return minRes;
-    
-    
+
     function canDistribute(minGiven) {
-        const clonedQ = [...quantities]; 
-        
+        const clonedQ = [...quantities];
+
         let j = 0;
         let i = 0;
-        
+
         for (; i < n && j < m; ++i) {
             const remQ = clonedQ[j];
-            
+
             if (remQ > minGiven) {
                 clonedQ[j] -= minGiven;
             }
@@ -38,7 +38,7 @@ var minimizedMaximum = function(n, quantities) {
                 ++j;
             }
         }
-       
+
         return j === m;
     }
 };
