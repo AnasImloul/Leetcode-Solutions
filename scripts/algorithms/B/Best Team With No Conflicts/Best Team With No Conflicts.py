377ddf991cc9ec0b228f71a799a3db052e4d0f8c
AnasImloul <pydev27@gmail.com>
2022-09-02 19:11:33 +0100

    Runtime: 5727 ms (Top 5.11%) | Memory: 14.3 MB (Top 62.50%)

--- scripts/algorithms/B/Best Team With No Conflicts/Best Team With No Conflicts.py
+++ scripts/algorithms/B/Best Team With No Conflicts/Best Team With No Conflicts.py
@@ -1,3 +1,4 @@
+# Runtime: 5727 ms (Top 5.11%) | Memory: 14.3 MB (Top 62.50%)
 class Solution(object):
     def bestTeamScore(self, scores, ages):
         """
@@ -9,12 +10,12 @@ class Solution(object):
         mapped = [[ages[i], scores[i]] for i in range(l)]
         mapped = sorted(mapped, key = lambda x : (x[0], x[1]))
         dp = [i[1] for i in mapped]
-    
+
         for i in range(l):
             for j in range(0, i):
                 if mapped[j][1] <= mapped[i][1]:
                     dp[i] = max(dp[i], mapped[i][1] + dp[j])
                 elif mapped[i][0] == mapped[j][0]:
                     dp[i] = max(dp[i], mapped[i][1] + dp[j])
-                    
+
         return max(dp)
