29e77f65483ee9fdb2cabc0e021593eab2dced1b
AnasImloul <pydev27@gmail.com>
2022-09-02 19:11:35 +0100

    Runtime: 966 ms (Top 94.20%) | Memory: 28.2 MB (Top 71.33%)

--- scripts/algorithms/S/Shortest Impossible Sequence of Rolls/Shortest Impossible Sequence of Rolls.py
+++ scripts/algorithms/S/Shortest Impossible Sequence of Rolls/Shortest Impossible Sequence of Rolls.py
@@ -1,13 +1,14 @@
+# Runtime: 966 ms (Top 94.20%) | Memory: 28.2 MB (Top 71.33%)
 class Solution:
     def shortestSequence(self, rolls: List[int], k: int) -> int:
         ans = 1
         data = set()
-        
+
         for roll in rolls:
             data.add(roll)
-            
+
             if len(data) == k:
                 ans += 1
                 data.clear()
-        
+
         return ans
