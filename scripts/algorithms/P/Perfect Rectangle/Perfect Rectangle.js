08f32a7ce2635f83b5ef5bc1f28fd166d8e30729
AnasImloul <pydev27@gmail.com>
2022-09-02 19:39:00 +0100

    Runtime: 202 ms (Top 50.00%) | Memory: 51.7 MB (Top 100.00%)

--- scripts/algorithms/P/Perfect Rectangle/Perfect Rectangle.js
+++ scripts/algorithms/P/Perfect Rectangle/Perfect Rectangle.js
@@ -1,3 +1,4 @@
+// Runtime: 202 ms (Top 50.00%) | Memory: 51.7 MB (Top 100.00%)
 var isRectangleCover = function(R) {
     // left to right, bottom to top (so sort on bottom first, then left)
     R.sort(([left1, bottom1], [left2, bottom2]) => bottom1 - bottom2 || left1 - left2);
@@ -17,8 +18,8 @@ var isRectangleCover = function(R) {
     // All calculations are with-respect-to large rectangle
     let CH = new Array(rightMost - leftMost).fill(0);
     for (let [left, bottom, right, top] of R) {
-        const baseHeight = bottom - bottomMost; //        how high base is
-        const ceilHeight = top - bottomMost; //           how high ceil is
+        const baseHeight = bottom - bottomMost; // how high base is
+        const ceilHeight = top - bottomMost; // how high ceil is
         for (let tempLeft = left; tempLeft < right; tempLeft++) {
             if (CH[tempLeft - leftMost] != baseHeight)
                 return false; // > is a duplicate cell < is a gap/ missing cell/ hole
