f484e819808df6cf41ff6daf981698e9692f725d
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:27 +0100

    Runtime: 3 ms (Top 38.21%) | Memory: 6 MB (Top 42.79%)

--- scripts/algorithms/X/XOR Operation in an Array/XOR Operation in an Array.cpp
+++ scripts/algorithms/X/XOR Operation in an Array/XOR Operation in an Array.cpp
@@ -1,13 +1,14 @@
+// Runtime: 3 ms (Top 38.21%) | Memory: 6 MB (Top 42.79%)
 class Solution {
 public:
-	int xorOperation(int n, int start) {
+    int xorOperation(int n, int start) {
 
-		int res = start;
+        int res = start;
 
-		for(int i=1 ; i<n ; i++){
-			res = res ^ (start + 2 * i);
-		}
+        for(int i=1 ; i<n ; i++){
+            res = res ^ (start + 2 * i);
+        }
 
-		return res;
-	}
+        return res;
+    }
 };
