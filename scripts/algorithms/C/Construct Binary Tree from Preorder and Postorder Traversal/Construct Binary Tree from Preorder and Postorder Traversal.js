f1d5020a1c95a20096ad57089b560bea44a489d4
AnasImloul <pydev27@gmail.com>
2022-09-02 19:39:00 +0100

    Runtime: 117 ms (Top 56.52%) | Memory: 45.2 MB (Top 69.57%)

--- scripts/algorithms/C/Construct Binary Tree from Preorder and Postorder Traversal/Construct Binary Tree from Preorder and Postorder Traversal.js
+++ scripts/algorithms/C/Construct Binary Tree from Preorder and Postorder Traversal/Construct Binary Tree from Preorder and Postorder Traversal.js
@@ -1,19 +1,20 @@
+// Runtime: 117 ms (Top 56.52%) | Memory: 45.2 MB (Top 69.57%)
 var constructFromPrePost = function(preorder, postorder) {
     let preIndex = 0
-    
+
     const dfs = (postArr) => {
         if (postArr.length === 0) return null
-        
+
         const val = preorder[preIndex]
         const nextVal = preorder[++preIndex]
         const mid = postArr.indexOf(nextVal)
-        
+
         const root = new TreeNode(val)
         root.left = dfs(postArr.slice(0, mid + 1))
         root.right = dfs(postArr.slice(mid + 1, postArr.indexOf(val)))
-        
+
         return root
     }
-    
+
     return dfs(postorder)
 };
