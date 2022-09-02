b91e98d74e062c1a5c318e88d47abc83d47ce34f
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:21 +0100

    Runtime: 1335 ms (Top 26.39%) | Memory: 149.2 MB (Top 71.30%)

--- scripts/algorithms/C/Closest Room/Closest Room.cpp
+++ scripts/algorithms/C/Closest Room/Closest Room.cpp
@@ -1,3 +1,4 @@
+// Runtime: 1335 ms (Top 26.39%) | Memory: 149.2 MB (Top 71.30%)
 class Solution {
 public:
     vector<int> closestRoom(vector<vector<int>>& rooms, vector<vector<int>>& queries) {
@@ -7,7 +8,7 @@ public:
         sort(begin(rooms), end(rooms), [](const auto& a, const auto& b){return a[1] > b[1];});
         vector<int> ans(m, -1);
         set<int> ids;
-        int j = 0;        
+        int j = 0;
         for(const auto& q: queries){
             while(j < n && rooms[j][1] >=q[1]) ids.insert(rooms[j++][0]);
             if(ids.empty()) continue;
@@ -18,6 +19,6 @@ public:
             if(it != begin(ids)) id2 = *prev(it);
             ans[q[2]] = abs(id1 - id) < abs(id2 - id) ? id1 : id2;
         }
-        return ans;        
+        return ans;
     }
 };
