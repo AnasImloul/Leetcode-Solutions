a0f87fef26cb9391c937f1753dbceb0171b1a138
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:19 +0100

    Runtime: 777 ms (Top 43.05%) | Memory: 144.4 MB (Top 61.85%)

--- scripts/algorithms/M/Minimize Hamming Distance After Swap Operations/Minimize Hamming Distance After Swap Operations.cpp
+++ scripts/algorithms/M/Minimize Hamming Distance After Swap Operations/Minimize Hamming Distance After Swap Operations.cpp
@@ -1,3 +1,4 @@
+// Runtime: 777 ms (Top 43.05%) | Memory: 144.4 MB (Top 61.85%)
 class Solution {
 public:
     vector<int> parents;
@@ -7,33 +8,33 @@ public:
             return parents[a];
         return parents[a] = find(parents[a]);
     }
-    
+
     void uni(int a, int b) {
         a = find(a);
         b = find(b);
-        
+
         if (ranks[a] >= ranks[b]) {
-            parents[b] = a; 
+            parents[b] = a;
             ranks[a]++;
         }
         else {
-            parents[a] = b; 
+            parents[a] = b;
             ranks[b]++;
         }
     }
-    
+
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
         int n = source.size();
         ranks = vector<int>(n, 0);
-        
+
         for (int i = 0; i < n; i++) {
             parents.push_back(i);
         }
-        
+
         for (auto &v : allowedSwaps)
             uni(v[0], v[1]);
         vector<unordered_multiset<int>> subs(n);
-        
+
         for (int i = 0; i < n; i++) {
             subs[find(i)].insert(source[i]);
         }
