6959fe2bcb5337c9874c4583c1c5c2c5b56aea70
AnasImloul <pydev27@gmail.com>
2022-09-02 18:58:30 +0100

    Runtime: 888 ms (Top 54.12%) | Memory: 105 MB (Top 66.11%)

--- scripts/algorithms/R/Remove Stones to Minimize the Total/Remove Stones to Minimize the Total.cpp
+++ scripts/algorithms/R/Remove Stones to Minimize the Total/Remove Stones to Minimize the Total.cpp
@@ -1,21 +1,22 @@
+// Runtime: 888 ms (Top 54.12%) | Memory: 105 MB (Top 66.11%)
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
         priority_queue<int> pq;
         int sum = 0, curr;
-        
+
         for (auto pile : piles) {
             pq.push(pile);
             sum += pile;
         }
-        
+
         while (k--) {
             curr = pq.top();
             pq.pop();
             sum -= curr/2;
             pq.push(curr - curr/2);
         }
-        
+
         return sum;
     }
 };
