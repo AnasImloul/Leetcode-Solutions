f9403c2ecaf42f5816ac313893590669ff3c629b
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:37 +0100

    Runtime: 11 ms (Top 6.67%) | Memory: 47.2 MB (Top 65.28%)

--- scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.java
+++ scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.java
@@ -1,17 +1,18 @@
+// Runtime: 11 ms (Top 6.67%) | Memory: 47.2 MB (Top 65.28%)
 class Solution {
     int max = 0;
     int len;
     public int findBestValue(int[] arr, int target) {
         this.len = arr.length;
-        
+
         for (int i = 0; i < len; i++)
             max = Math.max(max, arr[i]);
-        
+
         int l = 0;
         int r = max;
         while(l < r){
             int mid = l + (r-l) / 2;
-            
+
             if(check(arr, mid, target) <= check(arr, mid+1, target))
                 r = mid;
             else
@@ -19,7 +20,7 @@ class Solution {
         }
         return l;
     }
-    
+
     private int check(int[] arr, int value, int target){
         int sum = 0;
         for(int e : arr){
@@ -28,7 +29,7 @@ class Solution {
             else
                 sum += e;
         }
-        
+
         return Math.abs(sum-target);
     }
 }
