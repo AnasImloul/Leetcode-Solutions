42860cb4460a264f99d81cf37075b40524935a39
AnasImloul <pydev27@gmail.com>
2022-09-04 00:24:15 +0100

    Runtime: 380 ms (Top 65.56%) | Memory: 102 MB (Top 63.00%)

--- scripts/algorithms/T/Task Scheduler II/Task Scheduler II.cpp
+++ scripts/algorithms/T/Task Scheduler II/Task Scheduler II.cpp
@@ -1,7 +1,8 @@
+// Runtime: 380 ms (Top 65.56%) | Memory: 102 MB (Top 63.00%)
 class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
-        unordered_map<int,long long int> hash;     long long int curday=0;    
+        unordered_map<int,long long int> hash; long long int curday=0;
         for(int i=0;i<tasks.size();i++){
             if(hash.count(tasks[i])) curday=max(curday,hash[tasks[i]]);
             hash[tasks[i]]=curday+space+1;
