862db64b73de39167e8ad192af0d7dd79628f08d
AnasImloul <pydev27@gmail.com>
2022-09-03 18:31:58 +0100

    Runtime: 62 ms (Top 83.61%) | Memory: 14.1 MB (Top 62.30%)

--- scripts/algorithms/B/Basic Calculator IV/Basic Calculator IV.py
+++ scripts/algorithms/B/Basic Calculator IV/Basic Calculator IV.py
@@ -1,3 +1,4 @@
+# Runtime: 62 ms (Top 83.61%) | Memory: 14.1 MB (Top 62.30%)
 class Term:
     def __init__(self, exp: Optional[str]='', *, term: Optional[Mapping[str, int]]={}) -> None:
         self.d = defaultdict(int, **term)
@@ -5,26 +6,26 @@ class Term:
             self.d[''] += int(exp)
         elif exp:
             self.d[exp] += 1
-    
+
     def __add__(self, other: 'Term') -> 'Term':
         return self._pm(other, add=True)
-    
+
     def __mul__(self, other: 'Term') -> 'Term':
         res = defaultdict(int)
         for (l_var, l_coef), (r_var, r_coef) in product(self.d.items(), other.d.items()):
             res['*'.join(sorted(self._exp(l_var)+self._exp(r_var)))] += l_coef*r_coef
         return Term(term=res)
-    
+
     def __sub__(self, other: 'Term') -> 'Term':
         return self._pm(other, add=False)
 
     def get(self) -> List[str]:
         return [str(coef)+'*'*bool(var)+var \
                 for var, coef in sorted(self.d.items(), key=lambda t: (-len(self._exp(t[0])), t[0])) if coef]
-    
+
     def _exp(self, var: str) -> List[str]:
         return list(filter(bool, var.split('*')))
-    
+
     def _pm(self, other: 'Term', *, add: bool) -> 'Term':
         res = copy.copy(self.d)
         for var, coef in other.d.items():
