a9c7c7aec8347a3a1715ed5675c4fd8c13e76142
AnasImloul <pydev27@gmail.com>
2022-09-03 18:37:42 +0100

    Runtime: 116 ms (Top 16.67%) | Memory: 42.3 MB (Top 73.81%)

--- scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.js
+++ scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.js
@@ -1,3 +1,4 @@
+// Runtime: 116 ms (Top 16.67%) | Memory: 42.3 MB (Top 73.81%)
 var monotoneIncreasingDigits = function(n) {
     let arr = String(n).split('');
     for (let i=arr.length-2; i>=0; i--) {
