bbbdeaa683674a7c505b557b6dff24afc59c6e55
AnasImloul <pydev27@gmail.com>
2022-09-03 20:41:46 +0100

    Runtime: 141 ms (Top 32.57%) | Memory: 46 MB (Top 62.84%)

--- scripts/algorithms/C/Count Common Words With One Occurrence/Count Common Words With One Occurrence.js
+++ scripts/algorithms/C/Count Common Words With One Occurrence/Count Common Words With One Occurrence.js
@@ -1,8 +1,9 @@
+// Runtime: 141 ms (Top 32.57%) | Memory: 46 MB (Top 62.84%)
 var countWords = function(words1, words2) {
     const map1 = new Map();
     const map2 = new Map();
     let count = 0;
-    
+
     for (const word of words1) {
         map1.set(word, map1.get(word) + 1 || 1);
     }
@@ -12,6 +13,6 @@ var countWords = function(words1, words2) {
     for (const word of words1) {
         if (map1.get(word) === 1 && map2.get(word) === 1) count++;
     }
-    
+
     return count;
 };
