432c5c5e57cdad5455246acf4a68923704a60dcf
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:07 +0100

    Runtime: 17 ms (Top 78.04%) | Memory: 46.7 MB (Top 69.44%)

--- scripts/algorithms/S/Shortest Path Visiting All Nodes/Shortest Path Visiting All Nodes.java
+++ scripts/algorithms/S/Shortest Path Visiting All Nodes/Shortest Path Visiting All Nodes.java
@@ -1,3 +1,4 @@
+// Runtime: 17 ms (Top 78.04%) | Memory: 46.7 MB (Top 69.44%)
 class Solution {
     class Pair {
         int i;
@@ -13,15 +14,15 @@ class Solution {
         boolean[currentNode][steps]
         */
         int n = graph.length;
-        
+
         // 111....1, 1<< n - 1
         int allVisited = (1 << n) - 1;
-        
+
         boolean[][] visited = new boolean[n][1 << n];
         Queue<Pair> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             if (1 << i == allVisited) return 0;
-            visited[i][1 << i] = true;   
+            visited[i][1 << i] = true;
             q.offer(new Pair(i, 1 << i));
         }
         int step = 0;
@@ -30,14 +31,14 @@ class Solution {
             for (int i = 0; i < size; i++) {
                 Pair p = q.poll();
                 int[] edges = graph[p.i];
-         
+
                 for(int t: edges) {
                     int path = p.path | (1 << t);
                     if (path == allVisited) return step + 1;
                     if (!visited[t][path]) {
                         visited[t][path] = true;
-                        q.offer(new Pair(t, path)); 
-                    }                                    
+                        q.offer(new Pair(t, path));
+                    }
                 }
             }
             step++;
