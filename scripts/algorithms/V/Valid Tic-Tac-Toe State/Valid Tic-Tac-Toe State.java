6e2463eca5c1777fe3aa4d2af24ed087bde3c95f
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:28 +0100

    Runtime: 1 ms (Top 71.13%) | Memory: 41.6 MB (Top 42.96%)

--- scripts/algorithms/V/Valid Tic-Tac-Toe State/Valid Tic-Tac-Toe State.java
+++ scripts/algorithms/V/Valid Tic-Tac-Toe State/Valid Tic-Tac-Toe State.java
@@ -1,8 +1,9 @@
+// Runtime: 1 ms (Top 71.13%) | Memory: 41.6 MB (Top 42.96%)
 class Solution {
     public boolean validTicTacToe(String[] board) {
-		//cnt number of X and O
+        //cnt number of X and O
         int x = cntNumber('X', board);
-		//this check can be omitted, it can be covered in the second number check.
+        //this check can be omitted, it can be covered in the second number check.
         if(x >5){
             return false;
         }
@@ -23,7 +24,7 @@ class Solution {
         }
         return true;
     }
-    
+
     private int cntNumber(char target, String[] board){
         int res = 0;
         for(int i = 0; i<3; i++) {
@@ -35,7 +36,7 @@ class Solution {
         }
         return res;
     }
-    
+
     private boolean hasWon(char target, String[] board){
         String toWin = Character.toString(target).repeat(3);
         for(int i = 0; i<3; i++) {
@@ -55,19 +56,19 @@ class Solution {
                 return true;
             }
         }
-		//check diagonal. If center is not target, not possible to form diag win.
+        //check diagonal. If center is not target, not possible to form diag win.
         if(target != board[1].charAt(1)){
             return false;
         }
-		
+
         boolean diagonal1 = target == board[0].charAt(0);
-		//only proceed if the first letter match. Otherwise might get false positive
+        //only proceed if the first letter match. Otherwise might get false positive
         if(diagonal1){
             if(target == board[2].charAt(2)){
                 return true;
             }
         }
-        
+
         boolean diagonal2 = target == board[0].charAt(2);
         if(diagonal2){
             if(target == board[2].charAt(0)){
