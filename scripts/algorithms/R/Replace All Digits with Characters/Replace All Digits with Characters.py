35b4843bac5731fa0d1c180384651458f42a1f1a
AnasImloul <pydev27@gmail.com>
2022-09-03 15:11:13 +0100

    Runtime: 73 ms (Top 5.29%) | Memory: 13.8 MB (Top 58.42%)

--- scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.py
+++ scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.py
@@ -1,3 +1,4 @@
+# Runtime: 73 ms (Top 5.29%) | Memory: 13.8 MB (Top 58.42%)
 class Solution(object):
     def replaceDigits(self, s):
         """
@@ -5,11 +6,11 @@ class Solution(object):
         :rtype: str
         """
         res = []
-        
+
         for i in range(len(s)):
             if i % 2 == 0:
                 res.append(s[i])
             if i % 2 == 1:
                 res.append( chr(ord(s[i-1]) + int(s[i])) )
-                
-        return ''.join(res) 
+
+        return ''.join(res)
