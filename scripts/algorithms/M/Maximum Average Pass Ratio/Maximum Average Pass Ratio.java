6c938911b66047683dde09f7f2f0e136471225e7
AnasImloul <pydev27@gmail.com>
2022-09-04 00:07:53 +0100

    Runtime: 575 ms (Top 86.89%) | Memory: 100.8 MB (Top 90.16%)

--- scripts/algorithms/M/Maximum Average Pass Ratio/Maximum Average Pass Ratio.java
+++ scripts/algorithms/M/Maximum Average Pass Ratio/Maximum Average Pass Ratio.java
@@ -1,27 +1,28 @@
+// Runtime: 575 ms (Top 86.89%) | Memory: 100.8 MB (Top 90.16%)
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
         PriorityQueue<double[]> pq = new PriorityQueue<>(new Comparator<double[]>(){
             public int compare(double[] a, double[] b){
-                double adiff = (a[0]+1)/(a[1]+1) - (a[0]/a[1]);                
+                double adiff = (a[0]+1)/(a[1]+1) - (a[0]/a[1]);
                 double bdiff = (b[0]+1)/(b[1]+1) - (b[0]/b[1]);
                 if(adiff==bdiff) return 0;
                 return adiff>bdiff? -1:1;
             }
         });
-        
+
         for(int[] c:classes) pq.add(new double[]{c[0],c[1]});
-        
+
         for(int i =0;i<extraStudents;i++){
             double[] curr = pq.poll();
             pq.add(new double[]{curr[0]+1,curr[1]+1});
         }
-        
+
         double sum = 0;
         while(!pq.isEmpty()){
             double[] curr = pq.poll();
             sum+=curr[0]/curr[1];
         }
-        
+
         return sum/classes.length;
     }
 }
