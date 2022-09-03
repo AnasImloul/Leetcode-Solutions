a67665c1f70562134ea7c6e97690f109fd9d8bed
AnasImloul <pydev27@gmail.com>
2022-09-03 18:55:47 +0100

    Runtime: 11 ms (Top 17.31%) | Memory: 12.1 MB (Top 44.85%)

--- scripts/algorithms/B/Binary Tree Right Side View/Binary Tree Right Side View.cpp
+++ scripts/algorithms/B/Binary Tree Right Side View/Binary Tree Right Side View.cpp
@@ -1,45 +1,46 @@
+// Runtime: 11 ms (Top 17.31%) | Memory: 12.1 MB (Top 44.85%)
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-	vector<int> rightSideView(TreeNode* root) {
+    vector<int> rightSideView(TreeNode* root) {
 
-		vector<int> ans;
-		if(root == NULL){
-			return ans;
-		}
+        vector<int> ans;
+        if(root == NULL){
+            return ans;
+        }
 
-		queue<TreeNode* > q;
-		q.push(root);
-		while(!q.empty()){
-			int n = q.size();
-			for(int i = 0 ; i < n ; i++){
+        queue<TreeNode* > q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            for(int i = 0 ; i < n ; i++){
 
-				TreeNode* curr = q.front();
-				q.pop();
+                TreeNode* curr = q.front();
+                q.pop();
 
-				if(i == n - 1){
-					ans.push_back(curr -> val);
-				}
+                if(i == n - 1){
+                    ans.push_back(curr -> val);
+                }
 
-				if(curr -> left != nullptr){
-					q.push(curr -> left);
-				}
+                if(curr -> left != nullptr){
+                    q.push(curr -> left);
+                }
 
-				if(curr -> right != nullptr){
-					q.push(curr -> right);
-				}
-			}
-		}
-		return ans;
-	}
+                if(curr -> right != nullptr){
+                    q.push(curr -> right);
+                }
+            }
+        }
+        return ans;
+    }
 };
