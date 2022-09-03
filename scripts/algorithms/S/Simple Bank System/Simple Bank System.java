f16d1eee9c7156d06fa286aed22fe969ef552dc8
AnasImloul <pydev27@gmail.com>
2022-09-04 00:07:58 +0100

    Runtime: 265 ms (Top 12.10%) | Memory: 117.3 MB (Top 39.86%)

--- scripts/algorithms/S/Simple Bank System/Simple Bank System.java
+++ scripts/algorithms/S/Simple Bank System/Simple Bank System.java
@@ -1,3 +1,4 @@
+// Runtime: 265 ms (Top 12.10%) | Memory: 117.3 MB (Top 39.86%)
 class Bank {
 
     int N;
@@ -6,7 +7,7 @@ class Bank {
         this.N = balance.length;
         this.balance = balance;
     }
-    
+
     public boolean transfer(int account1, int account2, long money) {
         if(account1 < 1 || account1 > N || account2 < 1 || account2 > N || balance[account1 - 1] < money)
             return false;
@@ -14,14 +15,14 @@ class Bank {
         balance[account2 - 1] += money;
         return true;
     }
-    
+
     public boolean deposit(int account, long money) {
         if(account < 1 || account > N)
             return false;
         balance[account - 1] += money;
         return true;
     }
-    
+
     public boolean withdraw(int account, long money) {
         if(account < 1 || account > N || balance[account - 1] < money)
             return false;
