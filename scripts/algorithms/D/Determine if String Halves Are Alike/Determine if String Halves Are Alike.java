473a5f7ce8257f559d9a425dc3292c9f8fbd2c29
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:33 +0100

    Runtime: 10 ms (Top 23.59%) | Memory: 42.5 MB (Top 46.22%)

--- scripts/algorithms/D/Determine if String Halves Are Alike/Determine if String Halves Are Alike.java
+++ scripts/algorithms/D/Determine if String Halves Are Alike/Determine if String Halves Are Alike.java
@@ -1,7 +1,8 @@
+// Runtime: 10 ms (Top 23.59%) | Memory: 42.5 MB (Top 46.22%)
 class Solution {
     public boolean halvesAreAlike(String s) {
         //add vowels to the set
-		Set<Character> set = new HashSet<>();
+        Set<Character> set = new HashSet<>();
         set.add('a');
         set.add('e');
         set.add('i');
@@ -12,14 +13,14 @@ class Solution {
         set.add('I');
         set.add('O');
         set.add('U');
-        
-		//find the mid
+
+        //find the mid
         int mid = s.length() / 2;
         int count = 0;
         //increment the count for left half, decrement count for the second half if its a vowel
-		for (int i = 0; i < s.length(); i++)
-            count += (set.contains(s.charAt(i))) ? ((i < mid) ? 1 : -1) : 0; 
+        for (int i = 0; i < s.length(); i++)
+            count += (set.contains(s.charAt(i))) ? ((i < mid) ? 1 : -1) : 0;
         //finally count should be 0 to match left and right half
-		return count == 0;
+        return count == 0;
     }
 }
