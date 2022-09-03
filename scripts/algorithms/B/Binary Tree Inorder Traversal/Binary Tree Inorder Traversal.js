609e320722ba9eb363bea232a8a0f10ef3889fde
AnasImloul <pydev27@gmail.com>
2022-09-03 15:03:45 +0100

    Runtime: 86 ms (Top 54.59%) | Memory: 41.8 MB (Top 88.92%)

--- scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.js
+++ scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.js
@@ -1,5 +1,6 @@
+// Runtime: 86 ms (Top 54.59%) | Memory: 41.8 MB (Top 88.92%)
 var inorderTraversal = function(root) {
-    let result = [];    
+    let result = [];
     function traverse(node) {
         if(!node) {
             return;
@@ -9,5 +10,5 @@ var inorderTraversal = function(root) {
         traverse(node.right);
     }
     traverse(root);
-    return result;    
+    return result;
 };
