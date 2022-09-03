f168c8df437ed57403f92af853788636f0fe4c53
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:12 +0100

    Runtime: 2 ms (Top 16.64%) | Memory: 42.4 MB (Top 52.37%)

--- scripts/algorithms/F/Find the Middle Index in Array/Find the Middle Index in Array.java
+++ scripts/algorithms/F/Find the Middle Index in Array/Find the Middle Index in Array.java
@@ -1,23 +1,22 @@
+// Runtime: 2 ms (Top 16.64%) | Memory: 42.4 MB (Top 52.37%)
 
 class Solution {
     public int findMiddleIndex(int[] nums) {
-        
+
         for(int i=0;i<nums.length;i++)
         {
             int rsum=0;
             int lsum=0;
-            
+
             for(int k=0;k<i;k++) lsum+=nums[k];
             for(int k=i+1;k<nums.length;k++) rsum+=nums[k];
-            
-            
+
             if(lsum==rsum){
                 return i;
             }
         }
-        
+
         return -1;
-        
+
     }
 }
-
