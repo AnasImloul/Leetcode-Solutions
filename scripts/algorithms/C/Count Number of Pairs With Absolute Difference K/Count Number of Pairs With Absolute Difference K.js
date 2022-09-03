7ecf4441d96b8c92efed300ab6232ef0d864e511
AnasImloul <pydev27@gmail.com>
2022-09-03 15:03:47 +0100

    Runtime: 120 ms (Top 24.96%) | Memory: 43.9 MB (Top 40.35%)

--- scripts/algorithms/C/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.js
+++ scripts/algorithms/C/Count Number of Pairs With Absolute Difference K/Count Number of Pairs With Absolute Difference K.js
@@ -1,10 +1,11 @@
+// Runtime: 120 ms (Top 24.96%) | Memory: 43.9 MB (Top 40.35%)
 var countKDifference = function(nums, k) {
     nums = nums.sort((b,a) => b- a)
    let count = 0;
-    
+
     for(let i = 0; i< nums.length; i++) {
          for(let j = i + 1; j< nums.length; j++) {
-            if(Math.abs(nums[i] - nums[j]) == k) { 
+            if(Math.abs(nums[i] - nums[j]) == k) {
                 count++
             }
           }
