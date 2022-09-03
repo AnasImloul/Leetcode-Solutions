f3e49eaaf749598879e51cd9d72212166d583a94
AnasImloul <pydev27@gmail.com>
2022-09-03 15:03:43 +0100

    Runtime: 91 ms (Top 100.00%) | Memory: 47.1 MB (Top 69.23%)

--- scripts/algorithms/N/Number of Ways to Split a String/Number of Ways to Split a String.js
+++ scripts/algorithms/N/Number of Ways to Split a String/Number of Ways to Split a String.js
@@ -1,3 +1,4 @@
+// Runtime: 91 ms (Top 100.00%) | Memory: 47.1 MB (Top 69.23%)
 var numWays = function(s) {
     let one = 0;
     let list = [];
@@ -5,7 +6,7 @@ var numWays = function(s) {
         if(s[i]==="1") one++, list.push(i);
     }
     if(one%3!==0) return 0;
-    if(one===0) return ((s.length-1)*(s.length-2)/2) % 1000000007;  
+    if(one===0) return ((s.length-1)*(s.length-2)/2) % 1000000007;
     one/=3;
     return ((list[one]-list[one-1])*(list[2*one]-list[2*one-1])) % 1000000007;
 };
