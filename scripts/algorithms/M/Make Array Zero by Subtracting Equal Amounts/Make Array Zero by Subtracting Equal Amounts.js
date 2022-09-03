42ddd2186e727d89caccc63bf035da2371790813
AnasImloul <pydev27@gmail.com>
2022-09-03 18:37:35 +0100

    Runtime: 108 ms (Top 26.95%) | Memory: 41.6 MB (Top 95.74%)

--- scripts/algorithms/M/Make Array Zero by Subtracting Equal Amounts/Make Array Zero by Subtracting Equal Amounts.js
+++ scripts/algorithms/M/Make Array Zero by Subtracting Equal Amounts/Make Array Zero by Subtracting Equal Amounts.js
@@ -1,4 +1,5 @@
+// Runtime: 108 ms (Top 26.95%) | Memory: 41.6 MB (Top 95.74%)
 var minimumOperations = function(nums) {
-    let k = new Set(nums) // convert array to set; [...nums] is destructuring syntax 
+    let k = new Set(nums) // convert array to set; [...nums] is destructuring syntax
     return k.has(0) ? k.size-1 : k.size; // we dont need 0, hence if zero exists return size-1
 };
