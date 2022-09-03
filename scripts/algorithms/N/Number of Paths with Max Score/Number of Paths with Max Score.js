6878bcef9d6a9f25b40b3acfb5d367d55ccbbe2b
AnasImloul <pydev27@gmail.com>
2022-09-03 18:37:44 +0100

    Runtime: 273 ms (Top 14.29%) | Memory: 62 MB (Top 14.29%)

--- scripts/algorithms/N/Number of Paths with Max Score/Number of Paths with Max Score.js
+++ scripts/algorithms/N/Number of Paths with Max Score/Number of Paths with Max Score.js
@@ -1,12 +1,13 @@
+// Runtime: 273 ms (Top 14.29%) | Memory: 62 MB (Top 14.29%)
 var pathsWithMaxScore = function(board) {
   let n = board.length, dp = Array(n + 1).fill(0).map(() => Array(n + 1).fill(0).map(() => [-Infinity, 0]));
   let mod = 10 ** 9 + 7;
   dp[n - 1][n - 1] = [0, 1]; // [max score, number of paths]
-  
+
   for (let i = n - 1; i >= 0; i--) {
     for (let j = n - 1; j >= 0; j--) {
       if (board[i][j] === 'X' || board[i][j] === 'S') continue;
-      
+
       let paths = [dp[i][j + 1], dp[i + 1][j + 1], dp[i + 1][j]];
       for (let [maxScore, numPaths] of paths) {
         if (dp[i][j][0] < maxScore) {
