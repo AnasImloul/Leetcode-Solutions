00de6174ba23a09c0f4765dd70c27d23697ff6b7
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:36 +0100

    Runtime: 16 ms (Top 39.53%) | Memory: 44.7 MB (Top 53.62%)

--- scripts/algorithms/P/Palindrome Number/Palindrome Number.java
+++ scripts/algorithms/P/Palindrome Number/Palindrome Number.java
@@ -1,13 +1,14 @@
+// Runtime: 16 ms (Top 39.53%) | Memory: 44.7 MB (Top 53.62%)
 class Solution {
     public boolean isPalindrome(int x) {
         int sum = 0;
         int X = x;
-        
+
         while(x > 0){
             sum = 10 * sum + x % 10;
             x /= 10;
         }
-        
+
         return sum == X;
     }
 }
