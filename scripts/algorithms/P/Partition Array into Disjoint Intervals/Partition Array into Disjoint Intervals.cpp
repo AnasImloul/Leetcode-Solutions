dada6e9c0d0a9ffd3e7c5ef82f7b230c361b1185
AnasImloul <pydev27@gmail.com>
2022-09-04 00:24:02 +0100

    Runtime: 241 ms (Top 56.54%) | Memory: 102.3 MB (Top 15.65%)

--- scripts/algorithms/P/Partition Array into Disjoint Intervals/Partition Array into Disjoint Intervals.cpp
+++ scripts/algorithms/P/Partition Array into Disjoint Intervals/Partition Array into Disjoint Intervals.cpp
@@ -1,3 +1,4 @@
+// Runtime: 241 ms (Top 56.54%) | Memory: 102.3 MB (Top 15.65%)
 class Solution {
 public:
     vector<int> tree;
@@ -6,7 +7,7 @@ public:
         for(int i=0 ; i<nums.size(); i++) tree[i+n]=nums[i];
         for(int i=n-1 ; i>0 ; i--) tree[i] = min(tree[i<<1],tree[i<<1|1]);
     }
-    
+
     int query(int l, int r, int n) {
         l+=n,r+=n;
         int ans = INT_MAX;
@@ -17,7 +18,7 @@ public:
         }
         return ans;
     }
-    
+
     int partitionDisjoint(vector<int>& nums) {
         int n=nums.size();
         int mx=-1 ;
