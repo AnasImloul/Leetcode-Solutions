c2d53b40370a01bc0d4451065e0a9cee18a1b5e1
AnasImloul <pydev27@gmail.com>
2022-09-04 00:07:44 +0100

    Runtime: 183 ms (Top 63.97%) | Memory: 124.7 MB (Top 57.27%)

--- scripts/algorithms/F/Find Median from Data Stream/Find Median from Data Stream.java
+++ scripts/algorithms/F/Find Median from Data Stream/Find Median from Data Stream.java
@@ -1,15 +1,16 @@
+// Runtime: 183 ms (Top 63.97%) | Memory: 124.7 MB (Top 57.27%)
 class MedianFinder {
- 
+
     PriorityQueue maxHeap;
     PriorityQueue minHeap;
-    
+
     public MedianFinder() {
         maxHeap= new PriorityQueue<Integer>((a,b)->b-a);
         minHeap= new PriorityQueue<Integer>();
     }
-    
+
     public void addNum(int num) {
-        
+
         //Pushing
         if ( maxHeap.isEmpty() || ((int)maxHeap.peek() > num) ){
             maxHeap.offer(num);
@@ -17,8 +18,8 @@ class MedianFinder {
         else{
             minHeap.offer(num);
         }
-        
-        //Balancing 
+
+        //Balancing
         if ( maxHeap.size() > minHeap.size()+ 1){
              minHeap.offer(maxHeap.peek());
              maxHeap.poll();
@@ -27,20 +28,20 @@ class MedianFinder {
              maxHeap.offer(minHeap.peek());
              minHeap.poll();
         }
-        
+
     }
-    
+
     public double findMedian() {
-        
-        //Evaluating Median 
-        if ( maxHeap.size() ==  minHeap.size() ){ // Even Number 
+
+        //Evaluating Median
+        if ( maxHeap.size() == minHeap.size() ){ // Even Number
             return ((int)maxHeap.peek()+ (int)minHeap.peek())/2.0;
         }
-        else{ //Odd Number 
+        else{ //Odd Number
              if ( maxHeap.size() > minHeap.size()){
                  return (int)maxHeap.peek()+ 0.0;
              }
-            else{ // minHeap.size() > maxHeap.size() 
+            else{ // minHeap.size() > maxHeap.size()
                  return (int)minHeap.peek()+ 0.0;
             }
         }
