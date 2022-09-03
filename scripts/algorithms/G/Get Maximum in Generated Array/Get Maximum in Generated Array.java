40b1eb2710a2ffd0833ac96615e6dfdf0764b30c
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:09 +0100

    Runtime: 1 ms (Top 71.82%) | Memory: 41.2 MB (Top 34.16%)

--- scripts/algorithms/G/Get Maximum in Generated Array/Get Maximum in Generated Array.java
+++ scripts/algorithms/G/Get Maximum in Generated Array/Get Maximum in Generated Array.java
@@ -1,13 +1,14 @@
+// Runtime: 1 ms (Top 71.82%) | Memory: 41.2 MB (Top 34.16%)
 class Solution {
     public int getMaximumGenerated(int n) {
         if(n==0 || n==1) return n;
-        
+
         int nums[]=new int [n+1];
-        
+
         nums[0]=0;
         nums[1]=1;
         int max=Integer.MIN_VALUE;
-        
+
         for(int i=2;i<=n;i++){
             if(i%2==0){
                 nums[i]=nums[i/2];
