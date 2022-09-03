76d92feafdf27030859eb062d3bdefec7cbc2bef
AnasImloul <pydev27@gmail.com>
2022-09-03 18:55:50 +0100

    Runtime: 730 ms (Top 38.78%) | Memory: 147.5 MB (Top 81.13%)

--- scripts/algorithms/S/Seat Reservation Manager/Seat Reservation Manager.cpp
+++ scripts/algorithms/S/Seat Reservation Manager/Seat Reservation Manager.cpp
@@ -1,15 +1,16 @@
+// Runtime: 730 ms (Top 38.78%) | Memory: 147.5 MB (Top 81.13%)
 class SeatManager {
 public:
     priority_queue<int, vector<int>, greater<int> > pq;
     SeatManager(int n) {
         for(int i = 1; i <= n; i++) pq.push(i);
     }
-    
+
     int reserve() {
         int top = pq.top(); pq.pop();
         return top;
     }
-    
+
     void unreserve(int seatNumber) {
         pq.push(seatNumber);
     }
