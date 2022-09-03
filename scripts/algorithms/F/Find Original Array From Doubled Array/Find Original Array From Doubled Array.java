97e1fbbd591551d95f59d9ad19d07f3f3f8508d7
AnasImloul <pydev27@gmail.com>
2022-09-03 21:39:58 +0100

    Runtime: 93 ms (Top 85.10%) | Memory: 128.6 MB (Top 72.29%)

--- scripts/algorithms/F/Find Original Array From Doubled Array/Find Original Array From Doubled Array.java
+++ scripts/algorithms/F/Find Original Array From Doubled Array/Find Original Array From Doubled Array.java
@@ -1,21 +1,22 @@
+// Runtime: 93 ms (Top 85.10%) | Memory: 128.6 MB (Top 72.29%)
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        
+
         Arrays.sort(changed);
-            
+
         if(changed.length%2!=0) return new int[0];
-        
+
         int mid = changed.length/2;
-        
+
         int[] res = new int[mid];
-        
+
         int[] freq = new int[100001];
-        
+
         for(int no : changed)
             freq[no]++;
-        
+
         int idx=0;
-        
+
         for(int no: changed){
             if(freq[no] > 0 && no*2 <= 100000 && freq[no*2]>0){
                 freq[no]--;
@@ -23,13 +24,12 @@ class Solution {
                 res[idx++] = no;
             }
         }
-        
+
         for(int i=0; i<freq.length; i++){
             if(freq[i]!=0) return new int[0];
         }
-        
+
         return res;
-        
 
     }
 }
