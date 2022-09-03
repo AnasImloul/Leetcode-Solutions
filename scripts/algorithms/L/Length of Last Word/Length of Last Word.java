7941f235fd3a9d00cff3151824b403b886f555d6
AnasImloul <pydev27@gmail.com>
2022-09-03 18:46:56 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.9 MB (Top 78.55%)

--- scripts/algorithms/L/Length of Last Word/Length of Last Word.java
+++ scripts/algorithms/L/Length of Last Word/Length of Last Word.java
@@ -1,4 +1,4 @@
-// Runtime: 0 ms (Top 100.00%) | Memory: 41.7 MB (Top 83.09%)
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.9 MB (Top 78.55%)
 class Solution {
     public int lengthOfLastWord(String s) {
         int j=s.length()-1,len=0; boolean flag=true;
