8574b9ca88cb1bb2e494f54f551678f0cb0771d8
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:06 +0100

    Runtime: 2 ms (Top 82.01%) | Memory: 50 MB (Top 21.41%)

--- scripts/algorithms/J/Jump Game II/Jump Game II.java
+++ scripts/algorithms/J/Jump Game II/Jump Game II.java
@@ -1,26 +1,27 @@
+// Runtime: 2 ms (Top 82.01%) | Memory: 50 MB (Top 21.41%)
 class Solution {
-    
+
     public int jump(int[] nums) {
-        
+
         int result = 0;
-        
+
         int L = 0;
         int R = 0;
-        
+
         while (R < nums.length - 1) {
-            
+
             int localMaxRight = 0;
-            
+
             for (int i=L; i<=R; i++) {
-                
+
                 localMaxRight = Math.max(i + nums[i], localMaxRight);
             }
-            
+
             L = R + 1;
             R = localMaxRight;
-            result++;         
+            result++;
         }
-        
+
         return result;
     }
 }
