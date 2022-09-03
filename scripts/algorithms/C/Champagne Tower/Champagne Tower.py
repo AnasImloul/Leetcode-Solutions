6c9051efb33bfd91267421cc8839937c7ca692fc
AnasImloul <pydev27@gmail.com>
2022-09-03 18:31:55 +0100

    Runtime: 392 ms (Top 9.30%) | Memory: 14.4 MB (Top 11.38%)

--- scripts/algorithms/C/Champagne Tower/Champagne Tower.py
+++ scripts/algorithms/C/Champagne Tower/Champagne Tower.py
@@ -1,3 +1,4 @@
+# Runtime: 392 ms (Top 9.30%) | Memory: 14.4 MB (Top 11.38%)
 class Solution:
     def champagneTower(self, poured: int, r: int, c: int) -> float:
         quantity=defaultdict(int)
@@ -6,7 +7,7 @@ class Solution:
             flag=False
             for j in range(i+1):
                 prev_flow=quantity[(i,j)]-1
-                if prev_flow<=0: 
+                if prev_flow<=0:
                     continue
                 flag=True
                 quantity[(i,j)]=1
@@ -14,7 +15,3 @@ class Solution:
                 quantity[(i+1,j+1)]+=prev_flow/2
             if not flag: break
         return quantity[(r,c)]
-                
-                
-                
-        
