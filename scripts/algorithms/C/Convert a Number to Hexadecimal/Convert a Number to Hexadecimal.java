9f6e240135eaf0a7433b1078b0655c3683630be2
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:03 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 60.13%)

--- scripts/algorithms/C/Convert a Number to Hexadecimal/Convert a Number to Hexadecimal.java
+++ scripts/algorithms/C/Convert a Number to Hexadecimal/Convert a Number to Hexadecimal.java
@@ -1,18 +1,19 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 60.13%)
 class Solution {
     public String toHex(int num) {
         if(num == 0) return "0";
-        
+
         boolean start = true;
-        
+
         StringBuilder sb = new StringBuilder();
-        
+
         for(int i = 28; i >= 0; i -= 4) {
-            int digit = (num >> i)  & 15;
+            int digit = (num >> i) & 15;
             if(digit > 9) {
                 char curr = (char)(digit%10 + 'a');
                 sb.append(curr);
                 start = false;
-            }  else if(digit != 0) {
+            } else if(digit != 0) {
                 char curr = (char)(digit + '0');
                 sb.append(curr);
                 start = false;
@@ -22,7 +23,7 @@ class Solution {
                 }
             }
 
-        }    
+        }
         return sb.toString();
     }
 }
