e37e31e39a5fce43931763df3e937e3d917d5544
AnasImloul <pydev27@gmail.com>
2022-09-03 18:55:50 +0100

    Runtime: 3 ms (Top 94.98%) | Memory: 7.5 MB (Top 61.16%)

--- scripts/algorithms/F/Find Minimum Time to Finish All Jobs/Find Minimum Time to Finish All Jobs.cpp
+++ scripts/algorithms/F/Find Minimum Time to Finish All Jobs/Find Minimum Time to Finish All Jobs.cpp
@@ -1,3 +1,4 @@
+// Runtime: 3 ms (Top 94.98%) | Memory: 7.5 MB (Top 61.16%)
 class Solution {
 public:
     int minimumTimeRequired(vector<int>& jobs, int k) {
@@ -20,7 +21,7 @@ public:
         if(step>=jobs.size())
             return true;
         int cur = jobs[step];
-        // assign cur to worker i 
+        // assign cur to worker i
         for(int i=0;i<worker.size();i++){
             if(worker[i] + cur <= target){
                 worker[i] += cur;
