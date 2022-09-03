299de95aa1c1d342b1f5c87d7708856c497737d4
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:15 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 43.8 MB (Top 78.96%)

--- scripts/algorithms/P/Print Binary Tree/Print Binary Tree.java
+++ scripts/algorithms/P/Print Binary Tree/Print Binary Tree.java
@@ -1,11 +1,12 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 43.8 MB (Top 78.96%)
 class Solution {
     public List<List<String>> printTree(TreeNode root) {
         List<List<String>> res = new ArrayList();
-        
+
         int height = getHeight(root);
         int row = height + 1;
         int column = (int) Math.pow(2, height+1) - 1;
-        
+
         for(int k=0; k<row; k++){
             List<String> list = new ArrayList();
             for(int i=0; i<column; i++){
@@ -13,19 +14,19 @@ class Solution {
             }
             res.add(list);
         }
-        
+
         int left = 0;
         int right = column-1;
         int level=0;
         print(res, left, right, level, root);
-   
+
         return res;
     }
     public void print(List<List<String>> res, int left, int right, int level, TreeNode root){
         if(root == null) return;
         int mid = left+(right-left)/2;
         res.get(level).set(mid, String.valueOf(root.val));
-        
+
         print(res, left, mid-1, level+1, root.left);
         print(res, mid+1, right, level+1, root.right);
     }
@@ -33,7 +34,7 @@ class Solution {
         if (root==null) return -1;
         int left = getHeight(root.left);
         int right = getHeight(root.right);
-        
+
         return Math.max(left, right)+1;
     }
 }
