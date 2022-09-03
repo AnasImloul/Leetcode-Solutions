55573eff855d25e7f2a0c33b1cbb90279a5578a3
AnasImloul <pydev27@gmail.com>
2022-09-03 18:46:59 +0100

    Runtime: 1 ms (Top 77.34%) | Memory: 41.9 MB (Top 35.55%)

--- scripts/algorithms/R/Root Equals Sum of Children/Root Equals Sum of Children.java
+++ scripts/algorithms/R/Root Equals Sum of Children/Root Equals Sum of Children.java
@@ -1,7 +1,8 @@
+// Runtime: 1 ms (Top 77.34%) | Memory: 41.9 MB (Top 35.55%)
 class Solution
 {
     public boolean checkTree(TreeNode root)
-	{
-        return root.val == root.left.val + root.right.val;  // O(1)
+    {
+        return root.val == root.left.val + root.right.val; // O(1)
     }
 }
