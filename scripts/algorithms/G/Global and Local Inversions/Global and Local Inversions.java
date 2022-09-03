51a7201360f5869166cc5a2be9467ddcba59908f
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:09 +0100

    Runtime: 4 ms (Top 34.63%) | Memory: 77.1 MB (Top 70.64%)

--- scripts/algorithms/G/Global and Local Inversions/Global and Local Inversions.java
+++ scripts/algorithms/G/Global and Local Inversions/Global and Local Inversions.java
@@ -1,10 +1,11 @@
+// Runtime: 4 ms (Top 34.63%) | Memory: 77.1 MB (Top 70.64%)
 class Solution {
     public boolean isIdealPermutation(int[] nums) {
-        
+
         for(int i=0;i<nums.length;i++){
             if(Math.abs(nums[i]-i)>1) return false;
         }
-        
+
         return true;
     }
 }
