7a7c9fdb5e6bb2ba6f7f2a442e636ed7f1e008ed
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:22 +0100

    Runtime: 2 ms (Top 99.90%) | Memory: 54.7 MB (Top 23.03%)

--- scripts/algorithms/M/Maximum Product of Three Numbers/Maximum Product of Three Numbers.java
+++ scripts/algorithms/M/Maximum Product of Three Numbers/Maximum Product of Three Numbers.java
@@ -1,3 +1,4 @@
+// Runtime: 2 ms (Top 99.90%) | Memory: 54.7 MB (Top 23.03%)
 class Solution {
     public int maximumProduct(int[] nums) {
         int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
@@ -6,17 +7,17 @@ class Solution {
             if (n <= min1) {
                 min2 = min1;
                 min1 = n;
-            } else if (n <= min2) {     // n lies between min1 and min2
+            } else if (n <= min2) { // n lies between min1 and min2
                 min2 = n;
             }
-            if (n >= max1) {            // n is greater than max1, max2 and max3
+            if (n >= max1) { // n is greater than max1, max2 and max3
                 max3 = max2;
                 max2 = max1;
                 max1 = n;
-            } else if (n >= max2) {     // n lies betweeen max1 and max2
+            } else if (n >= max2) { // n lies betweeen max1 and max2
                 max3 = max2;
                 max2 = n;
-            } else if (n >= max3) {     // n lies betwen max2 and max3
+            } else if (n >= max3) { // n lies betwen max2 and max3
                 max3 = n;
             }
         }
