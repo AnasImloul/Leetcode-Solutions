07ef75195ded19b4434c05f46ebb54bef8b95161
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:04 +0100

    Runtime: 2 ms (Top 8.87%) | Memory: 43.1 MB (Top 80.91%)

--- scripts/algorithms/M/Minimum Distance to the Target Element/Minimum Distance to the Target Element.java
+++ scripts/algorithms/M/Minimum Distance to the Target Element/Minimum Distance to the Target Element.java
@@ -1,3 +1,4 @@
+// Runtime: 2 ms (Top 8.87%) | Memory: 43.1 MB (Top 80.91%)
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
         int ans = Integer.MAX_VALUE;
