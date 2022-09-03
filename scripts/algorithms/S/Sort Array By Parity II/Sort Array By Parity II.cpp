8af4cac912e7cb57f9673fe30131e0375adf93b2
AnasImloul <pydev27@gmail.com>
2022-09-03 18:55:46 +0100

    Runtime: 28 ms (Top 76.96%) | Memory: 21.4 MB (Top 66.53%)

--- scripts/algorithms/S/Sort Array By Parity II/Sort Array By Parity II.cpp
+++ scripts/algorithms/S/Sort Array By Parity II/Sort Array By Parity II.cpp
@@ -1,20 +1,21 @@
+// Runtime: 28 ms (Top 76.96%) | Memory: 21.4 MB (Top 66.53%)
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        
+
         vector<int>ans(nums.size());
-        
+
         int even_idx=0;
         int odd_idx=1;
-        
+
         for(int i=0;i<nums.size();i++)
         {
-            if((nums[i]%2)==0)  //the num is even
+            if((nums[i]%2)==0) //the num is even
             {
                 ans[even_idx]=nums[i];
                 even_idx+=2;
             }
-            else               //the num is odd
+            else //the num is odd
             {
                 ans[odd_idx]=nums[i];
                 odd_idx+=2;
