99eb52177ff60b038258d5258a68adb914758cd4
AnasImloul <pydev27@gmail.com>
2022-09-04 00:07:54 +0100

    Runtime: 1 ms (Top 81.71%) | Memory: 41.5 MB (Top 17.69%)

--- scripts/algorithms/M/Minimum Bit Flips to Convert Number/Minimum Bit Flips to Convert Number.java
+++ scripts/algorithms/M/Minimum Bit Flips to Convert Number/Minimum Bit Flips to Convert Number.java
@@ -1,3 +1,4 @@
+// Runtime: 1 ms (Top 81.71%) | Memory: 41.5 MB (Top 17.69%)
 class Solution {
     public int minBitFlips(int start, int goal) {
         int xor =start ^ goal;
@@ -7,6 +8,6 @@ class Solution {
             xor=xor & (xor-1);
         }
         return count;
-        
+
     }
 }
