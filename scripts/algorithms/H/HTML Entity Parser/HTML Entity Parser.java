a724a8e50206d15c078a24f350e1715113b89c23
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:39 +0100

    Runtime: 41 ms (Top 70.19%) | Memory: 56.3 MB (Top 73.08%)

--- scripts/algorithms/H/HTML Entity Parser/HTML Entity Parser.java
+++ scripts/algorithms/H/HTML Entity Parser/HTML Entity Parser.java
@@ -1,3 +1,4 @@
+// Runtime: 41 ms (Top 70.19%) | Memory: 56.3 MB (Top 73.08%)
 class Solution {
     public String entityParser(String text) {
         return text.replace("&quot;","\"").replace("&apos;","'").replace("&gt;",">").replace("&lt;","<").replace("&frasl;","/").replace("&amp;","&");
