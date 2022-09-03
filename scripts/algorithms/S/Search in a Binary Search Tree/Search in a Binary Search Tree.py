4b98519b2251d7824c8e504caa3c8c1f45356a79
AnasImloul <pydev27@gmail.com>
2022-09-03 18:31:52 +0100

    Runtime: 87 ms (Top 85.77%) | Memory: 16.7 MB (Top 24.56%)

--- scripts/algorithms/S/Search in a Binary Search Tree/Search in a Binary Search Tree.py
+++ scripts/algorithms/S/Search in a Binary Search Tree/Search in a Binary Search Tree.py
@@ -1,9 +1,10 @@
+# Runtime: 87 ms (Top 85.77%) | Memory: 16.7 MB (Top 24.56%)
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         def search(root):
