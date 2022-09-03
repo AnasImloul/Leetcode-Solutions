beb33e73f4ed460aa87836a6d72f9cb6b5c5becd
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:29 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 42.5 MB (Top 17.04%)

--- scripts/algorithms/R/Rotate String/Rotate String.java
+++ scripts/algorithms/R/Rotate String/Rotate String.java
@@ -1,14 +1,15 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 42.5 MB (Top 17.04%)
 class Solution {
     public boolean rotateString(String s, String goal) {
         int n = s.length(), m = goal.length();
         if (m != n) return false;
-        
+
         for (int offset = 0; offset < n; offset++) {
             if (isMatch(s, goal, offset)) return true;
         }
         return false;
     }
-    
+
     private boolean isMatch(String s, String g, int offset) {
         int n = s.length();
         for (int si = 0; si < n; si++) {
