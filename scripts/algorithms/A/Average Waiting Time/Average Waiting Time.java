a437e870c58357d2b9f46a7722ec7a622d5b0cd4
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:24 +0100

    Runtime: 16 ms (Top 7.49%) | Memory: 101.1 MB (Top 30.84%)

--- scripts/algorithms/A/Average Waiting Time/Average Waiting Time.java
+++ scripts/algorithms/A/Average Waiting Time/Average Waiting Time.java
@@ -1,14 +1,15 @@
+// Runtime: 16 ms (Top 7.49%) | Memory: 101.1 MB (Top 30.84%)
 class Solution {
     public double averageWaitingTime(int[][] customers) {
         double time = 0;
         double waitingTime = 0;
-        
+
         for(int[] cust : customers){
-            time = Math.max(cust[0],time);            
+            time = Math.max(cust[0],time);
             time = time + cust[1];
             waitingTime += (time - cust[0]);
         }
-                
+
         return waitingTime/customers.length;
     }
 }
