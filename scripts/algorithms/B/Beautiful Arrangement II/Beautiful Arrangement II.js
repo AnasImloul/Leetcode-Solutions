17ef68fa1098eb1040149e5b020127f2067d7f34
AnasImloul <pydev27@gmail.com>
2022-09-03 18:37:30 +0100

    Runtime: 74 ms (Top 97.37%) | Memory: 44.7 MB (Top 10.53%)

--- scripts/algorithms/B/Beautiful Arrangement II/Beautiful Arrangement II.js
+++ scripts/algorithms/B/Beautiful Arrangement II/Beautiful Arrangement II.js
@@ -1,17 +1,18 @@
+// Runtime: 74 ms (Top 97.37%) | Memory: 44.7 MB (Top 10.53%)
 var constructArray = function(n, k) {
-	const result = [];
-	let left = 1;
-	let right = n;
+    const result = [];
+    let left = 1;
+    let right = n;
 
-	for (let index = 0; index < n; index++) {
-		if (k === 1) {
-			result.push(left++);
-			continue;
-		}
-		const num = k & 1 ? left++ : right--;
-		result.push(num);
-		k -= 1;
-	}
+    for (let index = 0; index < n; index++) {
+        if (k === 1) {
+            result.push(left++);
+            continue;
+        }
+        const num = k & 1 ? left++ : right--;
+        result.push(num);
+        k -= 1;
+    }
 
-	return result;
+    return result;
 };
