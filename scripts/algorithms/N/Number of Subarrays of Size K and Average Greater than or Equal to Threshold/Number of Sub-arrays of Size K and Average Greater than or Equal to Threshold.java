e4cc60943d4da4ecec93443d6501d9326e50f2a5
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:10 +0100

    Runtime: 3 ms (Top 95.20%) | Memory: 53.3 MB (Top 91.54%)

---
+++ scripts/algorithms/N/Number of Subarrays of Size K and Average Greater than or Equal to Threshold/Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java
@@ -0,0 +1,16 @@
+// Runtime: 3 ms (Top 95.20%) | Memory: 53.3 MB (Top 91.54%)
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int average=0,count=0,start=0,sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(i>=k-1){
+                average=sum/k;
+                if(average>=threshold) count++;
+                sum-=arr[start];
+                start++;
+            }
+        }
+        return count;
+    }
+}
