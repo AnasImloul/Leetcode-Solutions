ed108a537e4ea4600cb57acfa858848bd20e7514
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:02 +0100

    Runtime: 64 ms (Top 66.67%) | Memory: 56.7 MB (Top 16.67%)

--- scripts/algorithms/M/Minimum Area Rectangle II/Minimum Area Rectangle II.java
+++ scripts/algorithms/M/Minimum Area Rectangle II/Minimum Area Rectangle II.java
@@ -1,7 +1,8 @@
+// Runtime: 64 ms (Top 66.67%) | Memory: 56.7 MB (Top 16.67%)
 class Solution {
     public double minAreaFreeRect(int[][] points) {
         Map<Pair<Double, Double>, Map<Double, List<int[][]>>> map = new HashMap<>();
-        
+
         double res = Double.MAX_VALUE;
         for (int i = 0; i < points.length; i++) {
             int[] p1 = points[i];
@@ -15,10 +16,10 @@ class Solution {
                 double dist2 = dist2(p1, p2);
                 if (!map.get(pm).containsKey(dist2))
                     map.get(pm).put(dist2, new ArrayList<>());
-                
+
                 // calculate area for each pair of p3/p4 and check min
-				// Worst case is each pair has same mid point with same length
-				// At worst case, below operation costs O(N)
+                // Worst case is each pair has same mid point with same length
+                // At worst case, below operation costs O(N)
                 for (int[][] ps : map.get(pm).get(dist2)) {
                     double d1 = dist2(p1, ps[0]);
                     double d2 = dist2(p1, ps[1]);
@@ -27,10 +28,10 @@ class Solution {
                 map.get(pm).get(dist2).add(new int[][]{p1, p2});
             }
         }
-        
+
         return res == Double.MAX_VALUE ? 0 : res;
     }
-    
+
     private double dist2(int[] p1, int[] p2) {
         return (p2[0]-p1[0])*(p2[0]-p1[0]) + (p2[1]-p1[1])*(p2[1]-p1[1]);
     }
