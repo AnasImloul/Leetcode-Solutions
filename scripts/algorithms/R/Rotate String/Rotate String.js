dc0599eae43b020e8fe2f6ce82f8d9e76ccffa6b
AnasImloul <pydev27@gmail.com>
2022-09-03 20:41:34 +0100

    Runtime: 96 ms (Top 39.69%) | Memory: 41.8 MB (Top 87.19%)

--- scripts/algorithms/R/Rotate String/Rotate String.js
+++ scripts/algorithms/R/Rotate String/Rotate String.js
@@ -1,8 +1,9 @@
+// Runtime: 96 ms (Top 39.69%) | Memory: 41.8 MB (Top 87.19%)
 var rotateString = function(s, goal) {
-   const n = s.length; 
+   const n = s.length;
    for(let i = 0; i < n; i++) {
       s = s.substring(1) + s[0];
-      if(s === goal) return true; 
+      if(s === goal) return true;
    }
-   return false; 
+   return false;
 };
