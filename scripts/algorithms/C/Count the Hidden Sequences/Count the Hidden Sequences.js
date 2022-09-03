d7ac96ee0d65d5c14ee5aef52c5b935fdfda626d
AnasImloul <pydev27@gmail.com>
2022-09-03 15:03:46 +0100

    Runtime: 212 ms (Top 12.50%) | Memory: 69.6 MB (Top 12.50%)

--- scripts/algorithms/C/Count the Hidden Sequences/Count the Hidden Sequences.js
+++ scripts/algorithms/C/Count the Hidden Sequences/Count the Hidden Sequences.js
@@ -1,16 +1,17 @@
+// Runtime: 212 ms (Top 12.50%) | Memory: 69.6 MB (Top 12.50%)
 var numberOfArrays = function(differences, lower, upper) {
     let temp = 0;
     let res = 0;
     let n = lower;
-    
+
     for (let i = 0; i < differences.length; i++) {
         temp += differences[i];
         differences[i] = temp;
     }
-    
+
     const min = Math.min(...differences);
     const max = Math.max(...differences);
-    
+
     while (n <= upper) {
         if (n + min >= lower && n + max <= upper) res++;
         n++;
