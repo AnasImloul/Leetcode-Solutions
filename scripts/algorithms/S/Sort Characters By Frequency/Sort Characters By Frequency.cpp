42d8436ed11935089c154537d1d5921d0c1ae988
AnasImloul <pydev27@gmail.com>
2022-09-03 20:16:40 +0100

    Runtime: 22 ms (Top 36.06%) | Memory: 8.5 MB (Top 65.22%)

--- scripts/algorithms/S/Sort Characters By Frequency/Sort Characters By Frequency.cpp
+++ scripts/algorithms/S/Sort Characters By Frequency/Sort Characters By Frequency.cpp
@@ -1,14 +1,15 @@
+// Runtime: 22 ms (Top 36.06%) | Memory: 8.5 MB (Top 65.22%)
 class Solution {
 public:
-    string frequencySort(string s) 
+    string frequencySort(string s)
     {
         unordered_map<char,int>mp;
-        for(int i=0;i<s.length();i++)   //get the frequency of every char of the string 
+        for(int i=0;i<s.length();i++) //get the frequency of every char of the string
         {
             mp[s[i]]++;
         }
-        
-        priority_queue<pair<int,char>>pq; //store the freq and char pair in max heap 
+
+        priority_queue<pair<int,char>>pq; //store the freq and char pair in max heap
         for(auto it=mp.begin();it!=mp.end();it++)
         {
             pq.push({it->second,it->first});
@@ -17,7 +18,7 @@ public:
         int val;
         while(!pq.empty())
         {
-            val=pq.top().first;          //append the char frequency times
+            val=pq.top().first; //append the char frequency times
             while(val--)
             {
                 str.push_back(pq.top().second);
@@ -25,7 +26,6 @@ public:
             pq.pop();
         }
         return str;
-        
-    }
-};  
 
+    }
+};
