7b097ac996630633d5a5ea9fa4309e3f0d42f841
AnasImloul <pydev27@gmail.com>
2022-09-03 18:46:52 +0100

    Runtime: 1 ms (Top 99.90%) | Memory: 44.3 MB (Top 29.92%)

--- scripts/algorithms/I/Intersection of Two Arrays/Intersection of Two Arrays.java
+++ scripts/algorithms/I/Intersection of Two Arrays/Intersection of Two Arrays.java
@@ -1,3 +1,4 @@
+// Runtime: 1 ms (Top 99.90%) | Memory: 44.3 MB (Top 29.92%)
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         int[] dp = new int[1000];
@@ -5,8 +6,8 @@ class Solution {
             dp[i]++;
         }
         int[] ans = new int[1000];
-		
-		//declaring ptr to track ans array index
+
+        //declaring ptr to track ans array index
         int ptr = 0;
         for(int i:nums2){
             if(dp[i] != 0){
