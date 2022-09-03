88441c042d7b23991f4e3114c5ddfc4884764f1d
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:35 +0100

    Runtime: 17 ms (Top 37.52%) | Memory: 44.4 MB (Top 28.50%)

--- scripts/algorithms/M/Multiply Strings/Multiply Strings.java
+++ scripts/algorithms/M/Multiply Strings/Multiply Strings.java
@@ -1,9 +1,10 @@
+// Runtime: 17 ms (Top 37.52%) | Memory: 44.4 MB (Top 28.50%)
 class Solution {
     public String multiply(String num1, String num2) {
         if(num1.equals("0") || num2.equals("0"))
             return "0";
         int[] arr=new int[num1.length()+num2.length()];
-        
+
         int index=0;
         for(int i=num1.length()-1;i>=0;i--)
         {
