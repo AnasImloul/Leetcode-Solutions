522655a62430d0ecdd7ec3161462eb591e2acee2
AnasImloul <pydev27@gmail.com>
2022-09-04 00:33:39 +0100

    Runtime: 33 ms (Top 14.78%) | Memory: 23.8 MB (Top 20.27%)

--- scripts/algorithms/B/Binary Tree Cameras/Binary Tree Cameras.cpp
+++ scripts/algorithms/B/Binary Tree Cameras/Binary Tree Cameras.cpp
@@ -1,20 +1,20 @@
+// Runtime: 33 ms (Top 14.78%) | Memory: 23.8 MB (Top 20.27%)
 class Solution {
 public:
     map<TreeNode*, int> mpr;
     int dp[1009][3];
-    int minCameraCover(TreeNode* root) 
+    int minCameraCover(TreeNode* root)
     {
         int num = 0;
         adres(root, num);
         memset(dp, -1, sizeof(dp));
-        
+
         int t1 = dp_fun(root, 0), t2 = dp_fun(root, 1), t3 = dp_fun(root, 2);
-        
-        
+
         return min({t1, t3});
 
     }
-    
+
     int dp_fun(TreeNode* cur, int st)
     {
         int nd = mpr[cur];
@@ -26,7 +26,7 @@ public:
                 {
                     return 1e8;
                 }
-                else 
+                else
                 {
                     return 0;
                 }
@@ -39,14 +39,14 @@ public:
             {
                 dp[nd][st] = min({dp_fun(cur->left, 0) + dp_fun(cur->right, 0), dp_fun(cur->left, 0) + dp_fun(cur->right, 2), dp_fun(cur->left, 2) + dp_fun(cur->right, 0), dp_fun(cur->left, 2) + dp_fun(cur->right, 2)});
             }
-            else 
+            else
             {
                 dp[nd][st] = min({dp_fun(cur, 2), dp_fun(cur->left, 2) + dp_fun(cur->right, 0), dp_fun(cur->left, 0) + dp_fun(cur->right, 2), dp_fun(cur->left, 2) + dp_fun(cur->right, 2)});
             }
         }
         return dp[nd][st];
     }
-    
+
     void adres(TreeNode* cur, int &cnt)
     {
         if(cur == NULL)
@@ -55,7 +55,7 @@ public:
         }
         mpr[cur] = cnt;
         cnt++;
-        
+
         adres(cur->left, cnt);
         adres(cur->right, cnt);
     }
