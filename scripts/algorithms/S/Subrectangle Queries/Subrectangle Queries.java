d16e9a855671c7f33a6817835c554b8d80339d0d
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:16 +0100

    Runtime: 53 ms (Top 20.07%) | Memory: 56.2 MB (Top 15.31%)

--- scripts/algorithms/S/Subrectangle Queries/Subrectangle Queries.java
+++ scripts/algorithms/S/Subrectangle Queries/Subrectangle Queries.java
@@ -1,9 +1,10 @@
+// Runtime: 53 ms (Top 20.07%) | Memory: 56.2 MB (Top 15.31%)
 class SubrectangleQueries {
     int[][] rectangle;
     public SubrectangleQueries(int[][] rectangle) {
         this.rectangle = rectangle;
     }
-    
+
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
         for(int i=row1;i<=row2;i++){
             for(int j=col1;j<=col2;j++){
@@ -11,7 +12,7 @@ class SubrectangleQueries {
             }
         }
     }
-    
+
     public int getValue(int row, int col) {
         return this.rectangle[row][col];
     }
