a1f50b6b9a186e9ccd7281fbf0a9420d55a75c55
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:44 +0100

    Runtime: 9 ms (Top 31.54%) | Memory: 43.4 MB (Top 14.91%)

--- scripts/algorithms/M/Maximize Sum Of Array After K Negations/Maximize Sum Of Array After K Negations.java
+++ scripts/algorithms/M/Maximize Sum Of Array After K Negations/Maximize Sum Of Array After K Negations.java
@@ -1,16 +1,17 @@
+// Runtime: 9 ms (Top 31.54%) | Memory: 43.4 MB (Top 14.91%)
 class Solution {
     public int largestSumAfterKNegations(int[] nums, int k) {
-        
+
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for(int val : nums) minHeap.add(val);
-        
+
         while(k > 0){
-            
+
             int curr = minHeap.poll();
             minHeap.add(-curr);
             k--;
         }
-        
+
         int sum = 0;
         while(!minHeap.isEmpty()){
             sum += minHeap.poll();
