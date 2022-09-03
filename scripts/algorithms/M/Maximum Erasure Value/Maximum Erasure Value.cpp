39acb579b589df122c4291c847c48a706d844547
AnasImloul <pydev27@gmail.com>
2022-09-04 00:24:14 +0100

    Runtime: 461 ms (Top 64.36%) | Memory: 127.1 MB (Top 40.21%)

--- scripts/algorithms/M/Maximum Erasure Value/Maximum Erasure Value.cpp
+++ scripts/algorithms/M/Maximum Erasure Value/Maximum Erasure Value.cpp
@@ -1,27 +1,28 @@
+// Runtime: 461 ms (Top 64.36%) | Memory: 127.1 MB (Top 40.21%)
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int curr_sum=0, res=0;
-		
-		//set to store the elements
+
+        //set to store the elements
         unordered_set<int> st;
-        
+
         int i=0,j=0;
         while(j<nums.size()) {
             while(st.count(nums[j])>0) {
-				//Removing the ith element untill we reach the repeating element
+                //Removing the ith element untill we reach the repeating element
                 st.erase(nums[i]);
                 curr_sum-=nums[i];
                 i++;
             }
-			//Add the current element to set and curr_sum value
+            //Add the current element to set and curr_sum value
             curr_sum+=nums[j];
             st.insert(nums[j++]);
-			
-			//res variable to keep track of largest curr_sum encountered till now...
+
+            //res variable to keep track of largest curr_sum encountered till now...
             res = max(res, curr_sum);
         }
-        
+
         return res;
     }
 };
