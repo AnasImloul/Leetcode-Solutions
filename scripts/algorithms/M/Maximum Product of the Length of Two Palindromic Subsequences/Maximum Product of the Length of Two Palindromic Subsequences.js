7640c18f7310d1ca6c65ff1ae22dd0795fe46212
AnasImloul <pydev27@gmail.com>
2022-09-03 20:41:58 +0100

    Runtime: 1058 ms (Top 35.29%) | Memory: 48.5 MB (Top 52.94%)

--- scripts/algorithms/M/Maximum Product of the Length of Two Palindromic Subsequences/Maximum Product of the Length of Two Palindromic Subsequences.js
+++ scripts/algorithms/M/Maximum Product of the Length of Two Palindromic Subsequences/Maximum Product of the Length of Two Palindromic Subsequences.js
@@ -1,3 +1,4 @@
+// Runtime: 1058 ms (Top 35.29%) | Memory: 48.5 MB (Top 52.94%)
 /**
  * @param {string} s
  * @return {number}
@@ -6,7 +7,7 @@ var maxProduct = function(s) {
     const n = s.length;
     let map = new Map();
     let res = 0;
-    
+
     for(let mask = 1; mask < 2 ** n;mask++){
         let str = "";
         for(let i = 0; i < n;i++){
@@ -19,22 +20,22 @@ var maxProduct = function(s) {
             map.set(mask,length);
         }
     }
-    
+
     map.forEach((l1,m1) => {
         map.forEach((l2,m2) => {
             if((m1 & m2) == 0){
                 res = Math.max(res,l1 * l2);
             }
-        })    
+        })
     })
-    
+
     return res;
 };
-    
+
 function isPalindrom(str){
     let l = 0;
     let r = str.length - 1;
-    
+
     while(l < r){
         if(str.charAt(l) != str.charAt(r)){
             return false;
@@ -42,6 +43,6 @@ function isPalindrom(str){
         l++;
         r--;
     }
-    
+
     return true;
 }
