a315e8a3994a65df44da6b78f1e20b0a03645e6e
AnasImloul <pydev27@gmail.com>
2022-09-03 15:11:13 +0100

    Runtime: 463 ms (Top 64.44%) | Memory: 20.3 MB (Top 40.95%)

--- scripts/algorithms/G/Greatest Sum Divisible by Three/Greatest Sum Divisible by Three.py
+++ scripts/algorithms/G/Greatest Sum Divisible by Three/Greatest Sum Divisible by Three.py
@@ -1,10 +1,11 @@
+# Runtime: 463 ms (Top 64.44%) | Memory: 20.3 MB (Top 40.95%)
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        dp = [0, float('-inf'), float('-inf')] 
+        dp = [0, float('-inf'), float('-inf')]
         for x in nums:
             dp_cp = dp[:]
             for left in range(3):
                 right = (left + x) % 3
                 dp[right] = max(dp_cp[right], dp_cp[left] + x)
-            
+
         return dp[0]
