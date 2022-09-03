6f33b19bce15ea3763471ed7f0c8348875b6d97a
AnasImloul <pydev27@gmail.com>
2022-09-03 18:46:54 +0100

    Runtime: 50 ms (Top 49.30%) | Memory: 48.5 MB (Top 48.18%)

--- scripts/algorithms/L/Longest Ideal Subsequence/Longest Ideal Subsequence.java
+++ scripts/algorithms/L/Longest Ideal Subsequence/Longest Ideal Subsequence.java
@@ -1,18 +1,19 @@
+// Runtime: 50 ms (Top 49.30%) | Memory: 48.5 MB (Top 48.18%)
 class Solution {
     public int longestIdealString(String s, int k) {
         int DP[] = new int[26], ans = 1;
-        
+
         for (int ch = 0, n = s.length(); ch < n; ch++) {
             int i = s.charAt(ch) - 'a';
             DP[i] = DP[i] + 1;
-            
+
             for (int j = Math.max(0, i - k); j <= Math.min(25, i + k); j++)
                 if (j != i)
                     DP[i] = Math.max(DP[i], DP[j] + 1);
-            
+
             ans = Math.max(ans, DP[i]);
         }
-        
+
         return ans;
     }
 }
