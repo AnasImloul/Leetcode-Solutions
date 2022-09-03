ad8d4a9519ca33d04f806df2ed72b918c7c227f2
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:05 +0100

    Runtime: 2 ms (Top 76.85%) | Memory: 44.7 MB (Top 48.41%)

--- scripts/algorithms/P/Permutations/Permutations.java
+++ scripts/algorithms/P/Permutations/Permutations.java
@@ -1,14 +1,15 @@
+// Runtime: 2 ms (Top 76.85%) | Memory: 44.7 MB (Top 48.41%)
 class Solution {
     List<List<Integer>> res = new LinkedList<>();
-    
+
     public List<List<Integer>> permute(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-    
+
         backTrack(nums, list, visited);
         return res;
     }
-    
+
     private void backTrack(int[] nums, ArrayList<Integer> list, boolean[] visited){
         if(list.size() == nums.length){
             res.add(new ArrayList(list));
