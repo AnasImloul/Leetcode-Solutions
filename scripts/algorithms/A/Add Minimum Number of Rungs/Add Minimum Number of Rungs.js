012c3eeda0abdb3fe79136bd2887fe60bc9d6606
AnasImloul <pydev27@gmail.com>
2022-09-03 20:41:38 +0100

    Runtime: 129 ms (Top 42.86%) | Memory: 49.9 MB (Top 85.71%)

--- scripts/algorithms/A/Add Minimum Number of Rungs/Add Minimum Number of Rungs.js
+++ scripts/algorithms/A/Add Minimum Number of Rungs/Add Minimum Number of Rungs.js
@@ -1,10 +1,10 @@
+// Runtime: 129 ms (Top 42.86%) | Memory: 49.9 MB (Top 85.71%)
 var addRungs = function(rungs, dist) {
     let res = 0;
     let prev = 0;
     for ( let i = 0; i < rungs.length; i++ ){
-        res += Math.floor(( rungs[i] - prev - 1 ) / dist ); 
+        res += Math.floor(( rungs[i] - prev - 1 ) / dist );
         prev = rungs[i];
     }
     return res;
 };
-
