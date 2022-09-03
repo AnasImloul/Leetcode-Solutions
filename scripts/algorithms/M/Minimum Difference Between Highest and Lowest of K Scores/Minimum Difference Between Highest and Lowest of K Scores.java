f9d7c9dcd0c90c0b8862158f778ffb3973edfb30
AnasImloul <pydev27@gmail.com>
2022-09-03 14:56:24 +0100

    Runtime: 6 ms (Top 73.44%) | Memory: 47.5 MB (Top 27.33%)

--- scripts/algorithms/M/Minimum Difference Between Highest and Lowest of K Scores/Minimum Difference Between Highest and Lowest of K Scores.java
+++ scripts/algorithms/M/Minimum Difference Between Highest and Lowest of K Scores/Minimum Difference Between Highest and Lowest of K Scores.java
@@ -1,15 +1,16 @@
+// Runtime: 6 ms (Top 73.44%) | Memory: 47.5 MB (Top 27.33%)
 class Solution {
     public int minimumDifference(int[] nums, int k) {
         if(k == 1)return 0;
         int i = 0,j = k-1,res = Integer.MAX_VALUE;
-		
+
         Arrays.sort(nums);
         while(j < nums.length){
             res = Math.min(res,nums[j] - nums[i]);
             j++;
             i++;
         }
-		
+
         return res;
     }
 }
