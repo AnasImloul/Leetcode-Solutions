865dc65fdb7b038f447e70e94d56860e01a638a0
AnasImloul <pydev27@gmail.com>
2022-09-03 15:11:11 +0100

    Runtime: 1302 ms (Top 13.95%) | Memory: 35.7 MB (Top 6.05%)

--- scripts/algorithms/P/Palindrome Partitioning/Palindrome Partitioning.py
+++ scripts/algorithms/P/Palindrome Partitioning/Palindrome Partitioning.py
@@ -1,3 +1,4 @@
+# Runtime: 1302 ms (Top 13.95%) | Memory: 35.7 MB (Top 6.05%)
 """
 we can approach this problem using manacher's algorithm with backtracking and recursion
 """
@@ -7,19 +8,19 @@ class Solution:
         def lps(s):
             if s in lookup:
                 return lookup[s]
-            
+
             final_res = []
             result_set = set()
             for k in range(len(s)):
                 i, j = k, k
-                
+
                 # check for odd length palindromes
                 while i>= 0 and j < len(s) and s[i] == s[j]:
                     # palindrome found
                     res = []
                     for partition in lps(s[:i]):
                         res.append(partition + [s[i:j+1]])
-                    for partition in res:    
+                    for partition in res:
                         for part in lps(s[j+1:]):
                             temp = partition + part
                             if tuple(temp) not in result_set:
@@ -27,7 +28,7 @@ class Solution:
                                 final_res.append(temp)
                     i-=1
                     j+=1
-                
+
                 # check for even length palindromes
                 i, j = k, k+1
                 while i >= 0 and j < len(s) and s[i] == s[j]:
@@ -35,7 +36,7 @@ class Solution:
                     res = []
                     for partition in lps(s[:i]):
                         res.append(partition + [s[i:j+1]])
-                    for partition in res:    
+                    for partition in res:
                         for part in lps(s[j+1:]):
                             temp = partition + part
                             if tuple(temp) not in result_set:
