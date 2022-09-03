b1a237ce7f3423c5aebf8bcb8ff5651b7fb84179
AnasImloul <pydev27@gmail.com>
2022-09-04 00:24:16 +0100

    Runtime: 84 ms (Top 92.95%) | Memory: 32.7 MB (Top 97.44%)

--- scripts/algorithms/V/Vowel Spellchecker/Vowel Spellchecker.cpp
+++ scripts/algorithms/V/Vowel Spellchecker/Vowel Spellchecker.cpp
@@ -1,63 +1,64 @@
+// Runtime: 84 ms (Top 92.95%) | Memory: 32.7 MB (Top 97.44%)
 class Solution {
 public:
     vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
         unordered_map<string, vector<int>> umap;
-        
+
         // step 1: add info in umap;
         for(int curr = 0; curr < wordlist.size(); curr++){
             // case 1: add same;
             umap[wordlist[curr]].push_back({curr});
             // notice that the lowercase may appear;
-            
+
             // case 2: add lowercase;
             string tmp = wordlist[curr];
             transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
-            
-            if(umap.find(tmp) == umap.end() && tmp != wordlist[curr]) umap[tmp].push_back({curr});      
-            
+
+            if(umap.find(tmp) == umap.end() && tmp != wordlist[curr]) umap[tmp].push_back({curr});
+
             // case 3: add vowel errors;
             // convert aeiou to _;
             for(int c_index = 0; c_index < tmp.size(); c_index++){
                 char c = tmp[c_index];
                 if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') tmp[c_index] = '_';
             }
-            
+
             if(umap.find(tmp) == umap.end()) umap[tmp].push_back({curr});
         }
-        
+
         // step 2: convert queries;
         for(int curr = 0; curr < queries.size(); curr++){
             string tmp = queries[curr];
             transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
-            
+
             // case 1: check same;
             if(umap.find(queries[curr]) != umap.end()){
                 queries[curr] = (umap[queries[curr]].size() == 1) ? wordlist[umap[queries[curr]][0]] : wordlist[umap[queries[curr]][1]];
                 continue;
             }
-            
+
             // case 2: check lowercase;
             if(umap.find(tmp) != umap.end() && tmp != queries[curr]){
                 queries[curr] = wordlist[umap[tmp][0]];
                 continue;
             }
-            
+
             // case 3: check vowel errors;
             // convert aeiou to _;
             for(int c_index = 0; c_index < tmp.size(); c_index++){
                 char c = tmp[c_index];
                 if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') tmp[c_index] = '_';
             }
-                  
+
             if(umap.find(tmp) != umap.end()){
                 queries[curr] = wordlist[umap[tmp][0]];
                 continue;
             }
-            
+
             // case 4: not found;
             queries[curr] = "";
         }
-        
+
         return queries;
     }
 };
