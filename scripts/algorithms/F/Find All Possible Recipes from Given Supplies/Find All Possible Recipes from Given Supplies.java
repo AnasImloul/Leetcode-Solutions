9536ee3a60dab00d16d280975f093899fe83b3a0
AnasImloul <pydev27@gmail.com>
2022-09-03 20:10:31 +0100

    Runtime: 63 ms (Top 70.73%) | Memory: 73 MB (Top 78.21%)

--- scripts/algorithms/F/Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.java
+++ scripts/algorithms/F/Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.java
@@ -1,42 +1,43 @@
+// Runtime: 63 ms (Top 70.73%) | Memory: 73 MB (Top 78.21%)
 class Solution {
     private static final int NOT_VISITED = 0;
     private static final int VISITING = 1;
     private static final int VISITED = 2;
-    
+
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         Map<String, Integer> status = new HashMap<>();
         Map<String, List<String>> prereqs = new HashMap<>();
-        
+
         for (int i = 0; i < recipes.length; ++ i) {
             status.put(recipes[i], NOT_VISITED);
             prereqs.put(recipes[i], ingredients.get(i));
         }
-        
+
         for (String s: supplies) {
             status.put(s, VISITED);
         }
-        
+
         List<String> output = new ArrayList<>();
         for (String s: recipes) {
             dfs (s, prereqs, status, output);
         }
-        
+
         return output;
     }
-    
+
     public boolean dfs(String s, Map<String, List<String>> prereqs, Map<String, Integer> status, List<String> output) {
         if (!status.containsKey(s)) {
             return false;
         }
-        
+
         if (status.get(s) == VISITING) {
             return false;
         }
-        
+
         if (status.get(s) == VISITED) {
             return true;
         }
-        
+
         status.put(s, VISITING);
         for (String p: prereqs.get(s)) {
             if (!dfs(p, prereqs, status, output)) {
@@ -45,7 +46,7 @@ class Solution {
         }
         status.put(s, VISITED);
         output.add(s);
-        
+
         return true;
     }
 }
