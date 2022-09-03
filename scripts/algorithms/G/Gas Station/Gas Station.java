2b85fa97f94677850d5d7081856e348deebe1d9a
AnasImloul <pydev27@gmail.com>
2022-09-03 18:46:48 +0100

    Runtime: 1 ms (Top 100.00%) | Memory: 62.5 MB (Top 91.09%)

--- scripts/algorithms/G/Gas Station/Gas Station.java
+++ scripts/algorithms/G/Gas Station/Gas Station.java
@@ -1,8 +1,9 @@
+// Runtime: 1 ms (Top 100.00%) | Memory: 62.5 MB (Top 91.09%)
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-	   //    *Upvote will be appreciated* 
+       // *Upvote will be appreciated*
         int totalFuel = 0;
-        int totalCost  = 0;
+        int totalCost = 0;
         int n = gas.length;
         for(int i = 0; i < n; i++) {
             totalFuel += gas[i];
@@ -14,9 +15,9 @@ class Solution {
         if(totalFuel < totalCost) {
             return -1;
         }
-        
+
         // It is greather then There may be an Answer
-        int start  = 0;
+        int start = 0;
         int currFuel = 0;
         for(int i = 0; i < n; i++) {
             currFuel += (gas[i]-cost[i]);
