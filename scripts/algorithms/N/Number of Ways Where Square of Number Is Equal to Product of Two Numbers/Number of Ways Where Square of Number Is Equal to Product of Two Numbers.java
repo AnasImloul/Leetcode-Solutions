30576de8f7631d8d5cc17f8d0bd2c441f38d8ec8
AnasImloul <pydev27@gmail.com>
2022-09-04 00:08:21 +0100

    Runtime: 19 ms (Top 92.55%) | Memory: 42.4 MB (Top 88.30%)

--- scripts/algorithms/N/Number of Ways Where Square of Number Is Equal to Product of Two Numbers/Number of Ways Where Square of Number Is Equal to Product of Two Numbers.java
+++ scripts/algorithms/N/Number of Ways Where Square of Number Is Equal to Product of Two Numbers/Number of Ways Where Square of Number Is Equal to Product of Two Numbers.java
@@ -1,10 +1,11 @@
+// Runtime: 19 ms (Top 92.55%) | Memory: 42.4 MB (Top 88.30%)
 class Solution {
     public int numTriplets(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         return count(nums1 , nums2) + count(nums2 , nums1);
     }
-    
+
     public int count(int a[] , int b[]){
         int n = a.length;
         int m = b.length;
@@ -22,7 +23,7 @@ class Solution {
                 else if(b[j] != b[k]){
                     int jNew = j;
                     int kNew = k;
-                    
+
                     while(b[j] == b[jNew])
                         jNew++;
                     while(b[k] == b[kNew])
@@ -40,5 +41,5 @@ class Solution {
         }
         return count;
     }
-    
+
 }
