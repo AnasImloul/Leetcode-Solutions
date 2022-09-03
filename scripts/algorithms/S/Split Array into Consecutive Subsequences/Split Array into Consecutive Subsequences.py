defe939a1e61653785446eff1984fef6f677c8bd
AnasImloul <pydev27@gmail.com>
2022-09-03 18:32:02 +0100

    Runtime: 574 ms (Top 89.21%) | Memory: 15.3 MB (Top 66.59%)

--- scripts/algorithms/S/Split Array into Consecutive Subsequences/Split Array into Consecutive Subsequences.py
+++ scripts/algorithms/S/Split Array into Consecutive Subsequences/Split Array into Consecutive Subsequences.py
@@ -1,16 +1,17 @@
+# Runtime: 574 ms (Top 89.21%) | Memory: 15.3 MB (Top 66.59%)
 
 import collections
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        
+
         counter = collections.Counter(nums)
         end = collections.defaultdict(int)
 
         for num in nums:
-            
+
             if counter[num]:
                 counter[num]-=1
-                
+
                 if end[num-1]:
                     end[num-1]-=1
                     end[num]+=1
