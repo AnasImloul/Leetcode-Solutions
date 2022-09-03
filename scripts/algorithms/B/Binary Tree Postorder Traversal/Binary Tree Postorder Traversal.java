a74df02c6af865ba7594180ffc56148033f90abd
AnasImloul <pydev27@gmail.com>
2022-09-03 21:40:10 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 42.3 MB (Top 40.74%)

--- scripts/algorithms/B/Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.java
+++ scripts/algorithms/B/Binary Tree Postorder Traversal/Binary Tree Postorder Traversal.java
@@ -1,16 +1,17 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 42.3 MB (Top 40.74%)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
@@ -19,7 +20,7 @@ class Solution {
         traversal(root);
         return res;
     }
-    
+
     public void traversal(TreeNode root){
         if(root == null)
             return;
