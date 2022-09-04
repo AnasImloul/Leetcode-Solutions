15885353c3c6554ae1e5f79c2cc9befa3a06cee7
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:36 +0100

    Runtime: 579 ms (Top 22.58%) | Memory: 89.6 MB (Top 70.97%)

--- scripts/algorithms/D/Diagonal Traverse II/Diagonal Traverse II.js
+++ scripts/algorithms/D/Diagonal Traverse II/Diagonal Traverse II.js
@@ -1,3 +1,4 @@
+// Runtime: 579 ms (Top 22.58%) | Memory: 89.6 MB (Top 70.97%)
 var findDiagonalOrder = function(nums) {
   let vals = []; // [row, diagonal value, actual value]
   for (let i = 0; i < nums.length; i++) {
