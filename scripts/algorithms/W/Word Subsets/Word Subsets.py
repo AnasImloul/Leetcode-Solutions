6a8112edeacf1dec2a7314d6bb5d1792cc18772c
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:18 +0100

    Runtime: 1721 ms (Top 30.52%) | Memory: 18.6 MB (Top 42.04%)

--- scripts/algorithms/W/Word Subsets/Word Subsets.py
+++ scripts/algorithms/W/Word Subsets/Word Subsets.py
@@ -1,3 +1,4 @@
+# Runtime: 1721 ms (Top 30.52%) | Memory: 18.6 MB (Top 42.04%)
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         result = []
@@ -5,7 +6,7 @@ class Solution:
         for w in words2:
             tempDict |= Counter(w)
         print(tempDict)
-        
+
         for w in words1:
             if not tempDict - Counter(w):
                 result.append(w)
