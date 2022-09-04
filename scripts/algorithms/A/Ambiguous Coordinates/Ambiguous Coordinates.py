12570d41f31eed8d89196d84354a41de2f2d3b45
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:03 +0100

    Runtime: 129 ms (Top 6.42%) | Memory: 13.9 MB (Top 92.66%)

--- scripts/algorithms/A/Ambiguous Coordinates/Ambiguous Coordinates.py
+++ scripts/algorithms/A/Ambiguous Coordinates/Ambiguous Coordinates.py
@@ -1,3 +1,4 @@
+# Runtime: 129 ms (Top 6.42%) | Memory: 13.9 MB (Top 92.66%)
 class Solution(object):
     def ambiguousCoordinates(self, s):
         """
@@ -6,18 +7,18 @@ class Solution(object):
         """
         def _isValidSplit(s):
             return False if len(s)>1 and re.match('/^[0]+$/',s) else True
-            
+
         def _isValidNum(ipart,fpart):
             return False if (len(ipart)>1 and ipart[0]=='0') or (fpart and fpart[-1]=='0') else True
-                             
+
         def _splitToNums(s):
             rets=[]
             if len(s)==1:return [s]
             for i in range(1,len(s)+1):
                 a,b=s[:i],s[i:]
                 if _isValidNum(a,b):rets.append("%s.%s"%(a,b) if b else "%s"%(a))
-            return rets               
-        
+            return rets
+
         ans,s=[],s[1:-1]
         for i in range(1,len(s)):
             a,b=s[:i],s[i:]
