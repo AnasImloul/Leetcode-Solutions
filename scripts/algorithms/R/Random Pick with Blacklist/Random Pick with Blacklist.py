50b59e06568db671797181745bb46948a7b9be73
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:29 +0100

    Runtime: 841 ms (Top 10.38%) | Memory: 24.8 MB (Top 56.73%)

--- scripts/algorithms/R/Random Pick with Blacklist/Random Pick with Blacklist.py
+++ scripts/algorithms/R/Random Pick with Blacklist/Random Pick with Blacklist.py
@@ -1,3 +1,4 @@
+# Runtime: 841 ms (Top 10.38%) | Memory: 24.8 MB (Top 56.73%)
 class Solution:
 
     def __init__(self, n: int, blacklist: List[int]):
@@ -7,17 +8,15 @@ class Solution:
         self.length=n-len(blacklist)
         flag=n-1
         for b in blacklist:
-            if b<self.length: 
+            if b<self.length:
                 while flag in self.hashmap:
                     flag-=1
                 self.hashmap[b]=flag
                 flag-=1
-            
+
     def pick(self) -> int:
         seed=random.randrange(self.length)
         return self.hashmap.get(seed,seed)
-        
-
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(n, blacklist)
