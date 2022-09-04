bcaa22e5f5db798071e4dfe2dc865c72555cf287
AnasImloul <pydev27@gmail.com>
2022-09-04 12:05:23 +0100

    Runtime: 2993 ms (Top 5.08%) | Memory: 7 MB (Top 43.65%)

--- scripts/algorithms/F/Fair Distribution of Cookies/Fair Distribution of Cookies.cpp
+++ scripts/algorithms/F/Fair Distribution of Cookies/Fair Distribution of Cookies.cpp
@@ -1,6 +1,7 @@
+// Runtime: 2993 ms (Top 5.08%) | Memory: 7 MB (Top 43.65%)
 class Solution {
 public:
-     
+
    int maxi=INT_MAX;
     void explore(vector<int>&cookies,int n,int k,vector<int>&vec)
     {
@@ -10,7 +11,7 @@ public:
           for(int i=0;i<k;i++)
           {
               m=max(m,vec[i]);
-              
+
           }
           maxi=min(maxi,m);
           return;
@@ -22,13 +23,12 @@ public:
             vec[i]-=cookies[n];
         }
     }
-    
-       
+
     int distributeCookies(vector<int>& cookies, int k) {
-      
+
         vector<int> vec(10,0);
-          explore(cookies,0,k,vec); 
+          explore(cookies,0,k,vec);
         return maxi;
-        
+
     }
 };
