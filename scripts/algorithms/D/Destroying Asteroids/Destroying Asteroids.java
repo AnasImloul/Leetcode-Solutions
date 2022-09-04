49f8966114aa75a5bda2090f39e0693fd09e7e3a
AnasImloul <pydev27@gmail.com>
2022-09-04 16:21:28 +0100

    Runtime: 476 ms (Top 5.23%) | Memory: 135.2 MB (Top 5.23%)

--- scripts/algorithms/D/Destroying Asteroids/Destroying Asteroids.java
+++ scripts/algorithms/D/Destroying Asteroids/Destroying Asteroids.java
@@ -1,16 +1,17 @@
+// Runtime: 476 ms (Top 5.23%) | Memory: 135.2 MB (Top 5.23%)
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
+
         for(int val:asteroids)
             maxHeap.add(val);
-        
+
         long bigMass = mass;
-        
+
         while(maxHeap.size()>0){
             int curr = maxHeap.poll();
-            
+
             if(bigMass>=curr){
                 bigMass+=curr;
                 while(minHeap.size()>0 && bigMass>=minHeap.peek()){
@@ -21,7 +22,7 @@ class Solution {
                 minHeap.add(curr);
             }
         }
-        
+
         return minHeap.size()==0 && maxHeap.size()==0;
     }
 }
