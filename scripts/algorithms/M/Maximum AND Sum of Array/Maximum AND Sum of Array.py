f90d328d42a42b95255bc4e54ed1579719be3c79
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:43 +0100

    Runtime: 1881 ms (Top 9.44%) | Memory: 23.9 MB (Top 80.86%)

--- scripts/algorithms/M/Maximum AND Sum of Array/Maximum AND Sum of Array.py
+++ scripts/algorithms/M/Maximum AND Sum of Array/Maximum AND Sum of Array.py
@@ -1,3 +1,4 @@
+# Runtime: 1881 ms (Top 9.44%) | Memory: 23.9 MB (Top 80.86%)
 from functools import lru_cache, cache
 
 class Solution:
@@ -8,9 +9,9 @@ class Solution:
                 return 0
             ans = 0
             for s in range(numSlots):
-                if m2 & (1 << s) == 0: # i.e. 0b0?, implying the slot is not full 
+                if m2 & (1 << s) == 0: # i.e. 0b0?, implying the slot is not full
                     if m1 & (1 << s) == 0: # 0b00 + 1 => 0b01
-                        nm1 = m1 | (1 << s);  nm2 = m2
+                        nm1 = m1 | (1 << s); nm2 = m2
                     else: # 0b01 + 1 => 0b10
                         nm1 = m1 & ~(1 << s); nm2 = m2 | (1 << s)
                     ans = max(ans, dp(i + 1, nm1, nm2) + ((s + 1) & nums[i])) # s + 1 is the actual slot no.
