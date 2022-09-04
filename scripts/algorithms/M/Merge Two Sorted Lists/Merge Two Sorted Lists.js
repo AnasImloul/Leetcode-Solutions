085b5baeec325dbc7a4c7e175415abac90247bc5
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:24 +0100

    Runtime: 127 ms (Top 16.18%) | Memory: 43.7 MB (Top 91.75%)

--- scripts/algorithms/M/Merge Two Sorted Lists/Merge Two Sorted Lists.js
+++ scripts/algorithms/M/Merge Two Sorted Lists/Merge Two Sorted Lists.js
@@ -1,8 +1,9 @@
+// Runtime: 127 ms (Top 16.18%) | Memory: 43.7 MB (Top 91.75%)
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * this.val = (val===undefined ? 0 : val)
+ * this.next = (next===undefined ? null : next)
  * }
  */
 /**
@@ -11,17 +12,17 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
+
     if(!list1) return list2
     if(!list2) return list1
-    
+
     let mergedList = new ListNode(0);
     let ptr = mergedList;
     let curr1 = list1;
     let curr2 = list2;
-    
+
     while(curr1 && curr2){
-    
+
         let newNode = new ListNode();
         if(!curr2 || curr1.val < curr2.val){
             newNode.val = curr1.val;
@@ -34,19 +35,19 @@ var mergeTwoLists = function(list1, list2) {
             ptr.next = newNode
             curr2 =curr2.next;
         }
-        
+
         ptr = ptr.next
-    }  
-    
+    }
+
     if(curr1 !== null){
         ptr.next = curr1;
         curr1 = curr1.next;
     }
-    
+
     if(curr2 !== null){
         ptr.next = curr2;
         curr2 = curr2.next;
     }
-    
+
     return mergedList.next
 };
