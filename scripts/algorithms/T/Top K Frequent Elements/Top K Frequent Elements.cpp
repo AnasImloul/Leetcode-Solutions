06eaff22d6d9f46d544a4444772c3f4c2f21146b
AnasImloul <pydev27@gmail.com>
2022-09-04 16:13:58 +0100

    Runtime: 12 ms (Top 97.44%) | Memory: 13.7 MB (Top 73.00%)

--- scripts/algorithms/T/Top K Frequent Elements/Top K Frequent Elements.cpp
+++ scripts/algorithms/T/Top K Frequent Elements/Top K Frequent Elements.cpp
@@ -1,20 +1,21 @@
+// Runtime: 12 ms (Top 97.44%) | Memory: 13.7 MB (Top 73.00%)
 class Solution {
 public:
-	vector<int> topKFrequent(vector<int>& nums, int k) {
-		unordered_map<int,int> up;
-		priority_queue<pair<int,int>> pq;
-		vector<int> a;
-		for(int i=0;i<nums.size();i++){
-			up[nums[i]]++;
-		}
-		for(auto& i:up){
-			pq.push({i.second,i.first});
-		}
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> up;
+        priority_queue<pair<int,int>> pq;
+        vector<int> a;
+        for(int i=0;i<nums.size();i++){
+            up[nums[i]]++;
+        }
+        for(auto& i:up){
+            pq.push({i.second,i.first});
+        }
 
-		while(k--){
-			a.push_back(pq.top().second);
-				pq.pop();
-		}
-		return a;
-	}
+        while(k--){
+            a.push_back(pq.top().second);
+                pq.pop();
+        }
+        return a;
+    }
 };
