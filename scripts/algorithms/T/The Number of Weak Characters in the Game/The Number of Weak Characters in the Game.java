69e09cf7c3d63a2d3e32e5dac74cdf36bac6ebc8
AnasImloul <pydev27@gmail.com>
2022-09-04 16:21:38 +0100

    Runtime: 23 ms (Top 90.90%) | Memory: 138 MB (Top 23.61%)

--- scripts/algorithms/T/The Number of Weak Characters in the Game/The Number of Weak Characters in the Game.java
+++ scripts/algorithms/T/The Number of Weak Characters in the Game/The Number of Weak Characters in the Game.java
@@ -1,3 +1,4 @@
+// Runtime: 23 ms (Top 90.90%) | Memory: 138 MB (Top 23.61%)
 class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
         int[] maxH = new int[100002];
@@ -8,7 +9,7 @@ class Solution {
         for(int i=100000;i>=0;i--){
             maxH[i] = Math.max(maxH[i+1],maxH[i]);
         }
-        
+
         for(int[] point:properties){
             if(point[1]<maxH[point[0]+1])
                 count++;
