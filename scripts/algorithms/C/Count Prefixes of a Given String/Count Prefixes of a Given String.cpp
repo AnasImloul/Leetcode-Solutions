f1caa49300e48a47aa44de2b83a890c0ebcfb880
AnasImloul <pydev27@gmail.com>
2022-09-04 14:45:41 +0100

    Runtime: 15 ms (Top 52.29%) | Memory: 11.9 MB (Top 92.97%)

--- scripts/algorithms/C/Count Prefixes of a Given String/Count Prefixes of a Given String.cpp
+++ scripts/algorithms/C/Count Prefixes of a Given String/Count Prefixes of a Given String.cpp
@@ -1,15 +1,16 @@
+// Runtime: 15 ms (Top 52.29%) | Memory: 11.9 MB (Top 92.97%)
 class Solution {
 public:
     //iterate over each words[i] and take the sub-string str from s of size equals to words[i].
     //and compare str with words[i] if both are equal increase count by 1.
-    int countPrefixes(vector<string>& words, string s) 
+    int countPrefixes(vector<string>& words, string s)
     {
         int count=0;
         for(auto it: words)
         {
             string str = s.substr(0, it.size());
-            if(str == it) count++;            
+            if(str == it) count++;
         }
-        return count;        
+        return count;
     }
 };
