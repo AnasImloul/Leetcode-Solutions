d652e75b43f93f04d6895de6c913ab1b715bf113
AnasImloul <pydev27@gmail.com>
2022-09-04 12:05:18 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.4 MB (Top 13.82%)

--- scripts/algorithms/M/Moving Stones Until Consecutive/Moving Stones Until Consecutive.cpp
+++ scripts/algorithms/M/Moving Stones Until Consecutive/Moving Stones Until Consecutive.cpp
@@ -1,16 +1,17 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.4 MB (Top 13.82%)
 class Solution {
 public:
-    
+
     vector<int> numMovesStones(int a, int b, int c) {
-        
+
         vector<int> arr = {a, b, c};
-        
+
         sort(arr.begin(), arr.end());
-        
+
         // find minimum moves
-        
+
         int mini = 0;
-        
+
         if(arr[1] - arr[0] == 1 && arr[2] - arr[1] == 1)
         {
             mini = 0;
@@ -23,11 +24,11 @@ public:
         {
             mini = 2;
         }
-        
+
         // find maximum moves
-        
+
         int maxi = (arr[1] - arr[0] - 1) + (arr[2] - arr[1] - 1);
-       
+
         return {mini, maxi};
     }
 };
