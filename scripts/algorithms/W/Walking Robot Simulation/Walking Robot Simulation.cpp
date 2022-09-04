3d063b5ea3dc33d4034b7baa0b6125dabccf0eaf
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:01 +0100

    Runtime: 386 ms (Top 8.00%) | Memory: 45.9 MB (Top 26.80%)

--- scripts/algorithms/W/Walking Robot Simulation/Walking Robot Simulation.cpp
+++ scripts/algorithms/W/Walking Robot Simulation/Walking Robot Simulation.cpp
@@ -1,25 +1,26 @@
+// Runtime: 386 ms (Top 8.00%) | Memory: 45.9 MB (Top 26.80%)
 class Solution {
 public:
-    
+
     //N--> left(-2):W, right(-1):E
     //S--> left:E, right:W
     //E--> left:N, right:S
     //W--> left:S, right:N
-    
+
     vector<vector<int>> change= { //for direction change
         {3,2},
         {2,3},
         {0,1},
         {1,0}
     };
-    
+
     vector<vector<int>> sign = { //signs for x and y coordinates movement
         {0,1},//North
         {0,-1},//south
         {1,0},//east
         {-1,0}//west
     };
-    
+
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         set<vector<int>> s;
         for(int i = 0;i<obstacles.size();i++){
