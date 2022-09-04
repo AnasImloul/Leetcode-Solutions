4625e410a8b4bdfeacf6cdefdcc3084973f96d0a
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:06 +0100

    Runtime: 201 ms (Top 18.42%) | Memory: 15.8 MB (Top 36.40%)

--- scripts/algorithms/B/Basic Calculator II/Basic Calculator II.py
+++ scripts/algorithms/B/Basic Calculator II/Basic Calculator II.py
@@ -1,3 +1,4 @@
+# Runtime: 201 ms (Top 18.42%) | Memory: 15.8 MB (Top 36.40%)
 class Solution:
     def calculate(self, s: str) -> int:
         stack = []
@@ -8,22 +9,21 @@ class Solution:
             ch = s[i]
             if ch.isdigit():
                 currentNumber = currentNumber * 10 + int(ch)
-            
+
             if ch in operations or i == len(s) - 1:
                 if operator == '+':
                     stack.append(currentNumber)
-                
+
                 elif operator == '-':
                     stack.append(-currentNumber)
-                
+
                 elif operator == '*':
                     stack.append(stack.pop() * currentNumber)
-                    
+
                 elif operator == '/':
                     stack.append(int(stack.pop()/currentNumber))
-                
+
                 currentNumber =0
                 operator = ch
-        
+
         return sum(stack)
-                
