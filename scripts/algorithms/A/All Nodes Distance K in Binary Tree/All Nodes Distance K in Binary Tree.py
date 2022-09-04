7340e92ae6a308146b378d2bb1a2421bd4791a76
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:25 +0100

    Runtime: 87 ms (Top 5.40%) | Memory: 14.2 MB (Top 98.06%)

--- scripts/algorithms/A/All Nodes Distance K in Binary Tree/All Nodes Distance K in Binary Tree.py
+++ scripts/algorithms/A/All Nodes Distance K in Binary Tree/All Nodes Distance K in Binary Tree.py
@@ -1,26 +1,27 @@
+# Runtime: 87 ms (Top 5.40%) | Memory: 14.2 MB (Top 98.06%)
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        
+
         # DFS to make the adj List
         adjList = defaultdict(list)
         def dfs(node):
             if not node:
                 return
-            
+
             if node.left:
                 adjList[node].append(node.left)
                 adjList[node.left].append(node)
-            
+
             if node.right:
                 adjList[node].append(node.right)
                 adjList[node.right].append(node)
             dfs(node.left)
             dfs(node.right)
-        
+
         dfs(root)
-        
+
         # bfs to find the nodes with k distance
-        
+
         q = deque([(target, 0)])
         visit = set()
         visit.add(target)
@@ -31,7 +32,7 @@ class Solution:
 
                 if dist == k:
                     res.append(node.val)
-                
+
                 if dist > k:
                     break
                 for nei in adjList[node]:
@@ -39,4 +40,3 @@ class Solution:
                         visit.add(nei)
                         q.append((nei, dist + 1))
         return res
-                    
