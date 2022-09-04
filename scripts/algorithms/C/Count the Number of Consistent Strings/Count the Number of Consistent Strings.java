8b80547ad6b18270004e1540fd0fa3bab2108a96
AnasImloul <pydev27@gmail.com>
2022-09-04 16:21:35 +0100

    Runtime: 15 ms (Top 73.10%) | Memory: 54.7 MB (Top 46.20%)

--- scripts/algorithms/C/Count the Number of Consistent Strings/Count the Number of Consistent Strings.java
+++ scripts/algorithms/C/Count the Number of Consistent Strings/Count the Number of Consistent Strings.java
@@ -1,21 +1,22 @@
+// Runtime: 15 ms (Top 73.10%) | Memory: 54.7 MB (Top 46.20%)
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
         Set<Character> allowedSet = new HashSet<>();
         for(int i=0;i<allowed.length();i++){
             allowedSet.add(allowed.charAt(i));
         }
-        
+
         int count = 0;
         for(String word: words){
             if(isConsistent(allowedSet, word)) count++;
         }
-        
+
         return count;
     }
-    
+
     boolean isConsistent(Set<Character> allowedSet, String word){
         for (int i = 0; i < word.length(); i++){
-            if(!allowedSet.contains(word.charAt(i))) return false;   
+            if(!allowedSet.contains(word.charAt(i))) return false;
         }
         return true;
     }
