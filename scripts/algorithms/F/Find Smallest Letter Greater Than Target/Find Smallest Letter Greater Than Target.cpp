17dbaebc96fab3bae85bfe3df3bb818d5767d71b
AnasImloul <pydev27@gmail.com>
2022-09-04 14:45:45 +0100

    Runtime: 24 ms (Top 57.17%) | Memory: 15.9 MB (Top 25.00%)

--- scripts/algorithms/F/Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.cpp
+++ scripts/algorithms/F/Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.cpp
@@ -1,14 +1,15 @@
+// Runtime: 24 ms (Top 57.17%) | Memory: 15.9 MB (Top 25.00%)
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        
+
         int siz = letters.size();
         bool isPresent = false;
         char ans;
         char temp = target;
-        
+
         if (target == letters[siz-1]) return letters[0];
-        
+
         for(int i=0; i<siz-1; i++) {
             if (target == letters[i] && target != letters[i+1]) {
                 ans = letters[i+1];
@@ -16,16 +17,16 @@ public:
                 break;
             }
         }
-        
+
         // if target not in letters
         while (!isPresent) {
-            temp = temp +1; 
-            isPresent = binary_search(letters.begin(), letters.end(), temp);   // used STL !! 
+            temp = temp +1;
+            isPresent = binary_search(letters.begin(), letters.end(), temp); // used STL !!
             if(isPresent) {
-                ans=temp; 
+                ans=temp;
                 break;
             }
-        }        
+        }
         return ans;
     }
 };
