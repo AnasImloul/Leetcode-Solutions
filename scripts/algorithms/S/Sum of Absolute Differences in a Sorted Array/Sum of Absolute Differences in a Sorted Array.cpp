8a65c3774a7e61b661b6f6d143a30a865190807c
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:11 +0100

    Runtime: 154 ms (Top 81.76%) | Memory: 83.2 MB (Top 78.51%)

--- scripts/algorithms/S/Sum of Absolute Differences in a Sorted Array/Sum of Absolute Differences in a Sorted Array.cpp
+++ scripts/algorithms/S/Sum of Absolute Differences in a Sorted Array/Sum of Absolute Differences in a Sorted Array.cpp
@@ -1,13 +1,14 @@
+// Runtime: 154 ms (Top 81.76%) | Memory: 83.2 MB (Top 78.51%)
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         vector<int>ans(nums.size(),0);
         for(int i = 1;i<nums.size();i++)
             ans[0]+=(nums[i]-nums[0]);
-        
+
         for(int j = 1;j<nums.size();j++)
             ans[j] = ans[j-1]+(nums[j]-nums[j-1])*j-(nums[j]-nums[j-1])*(nums.size()-j);
-        
+
         return ans;
     }
 };
