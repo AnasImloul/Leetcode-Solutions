6cd8cde4abadb61abbac6e4ede149b3bd8e798e2
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:04 +0100

    Runtime: 255 ms (Top 49.82%) | Memory: 34.9 MB (Top 21.03%)

--- scripts/algorithms/N/Number of Ways Where Square of Number Is Equal to Product of Two Numbers/Number of Ways Where Square of Number Is Equal to Product of Two Numbers.cpp
+++ scripts/algorithms/N/Number of Ways Where Square of Number Is Equal to Product of Two Numbers/Number of Ways Where Square of Number Is Equal to Product of Two Numbers.cpp
@@ -1,13 +1,14 @@
+// Runtime: 255 ms (Top 49.82%) | Memory: 34.9 MB (Top 21.03%)
 class Solution {
 public:
     int numTriplets(vector<int>& nums1, vector<int>& nums2) {
-        
+
         unordered_map<long, int> m1, m2;
         for(int i : nums1) m1[(long long)i*i]++;
         for(int i : nums2) m2[(long long)i*i]++;
-        
+
         int ans = 0;
-        
+
         for(int i=0; i<nums2.size()-1; i++){
             for(int j=i+1; j<nums2.size(); j++){
                 if(m1[(long long)nums2[i] * nums2[j]]){
@@ -22,6 +23,6 @@ public:
                 }
             }
         }
-        return ans; 
+        return ans;
     }
 };
