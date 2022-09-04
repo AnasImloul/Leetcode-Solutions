a941cb88d695e966578eeeec380f380122e558ca
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:10 +0100

    Runtime: 3 ms (Top 68.35%) | Memory: 6.5 MB (Top 93.46%)

--- scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.cpp
+++ scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.cpp
@@ -1,10 +1,11 @@
+// Runtime: 3 ms (Top 68.35%) | Memory: 6.5 MB (Top 93.46%)
 class Solution {
 public:
     int numDifferentIntegers(string word) {
-        unordered_map<string, int> hmap;  
+        unordered_map<string, int> hmap;
         for (int i = 0; i < word.size(); i++) {
             if (isdigit(word[i])) {
-			    string str;
+                string str;
                 while (word[i] == '0')
                     i++;
                 while (isdigit(word[i]))
