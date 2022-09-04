123057cdc3e782b00f1eb8e44a7e57c3a21e5cd8
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:17 +0100

    Runtime: 1276 ms (Top 58.39%) | Memory: 15.2 MB (Top 22.81%)

--- scripts/algorithms/Q/Query Kth Smallest Trimmed Number/Query Kth Smallest Trimmed Number.py
+++ scripts/algorithms/Q/Query Kth Smallest Trimmed Number/Query Kth Smallest Trimmed Number.py
@@ -1,3 +1,4 @@
+# Runtime: 1276 ms (Top 58.39%) | Memory: 15.2 MB (Top 22.81%)
 from collections import defaultdict
 
 class Solution:
@@ -5,15 +6,14 @@ class Solution:
         sl = len(nums[0])
         len_to_sorted = defaultdict(list)
         ans = [0] * len(queries)
-        
+
         for i, (k_smallest, trim_len) in enumerate(queries):
             if trim_len not in len_to_sorted:
                 # have to trim
                 for ni, num in enumerate(nums):
                     len_to_sorted[trim_len].append( (int(num[sl - trim_len:]), ni) )
-					
+
                 len_to_sorted[trim_len] = sorted(len_to_sorted[trim_len])
             ans[i] = len_to_sorted[trim_len][k_smallest -1][1]
-            
-            
+
         return ans
