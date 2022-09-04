e40aba70b6bfb0ecc6f206db54489d34244eb192
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:32 +0100

    Runtime: 78 ms (Top 84.71%) | Memory: 42.5 MB (Top 77.27%)

--- scripts/algorithms/C/Consecutive Characters/Consecutive Characters.js
+++ scripts/algorithms/C/Consecutive Characters/Consecutive Characters.js
@@ -1,11 +1,12 @@
+// Runtime: 78 ms (Top 84.71%) | Memory: 42.5 MB (Top 77.27%)
 var maxPower = function(s) {
     let count = 1;
     let maxNum =1;
-    
+
     for(let i=0;i<s.length;i++){
        (s[i]===s[Number(i)+1]) ? count++ : count=1;
         maxNum = Math.max(count,maxNum);
     }
-    
-    return  maxNum;
+
+    return maxNum;
 };
