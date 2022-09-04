73540ea1d40169bddb51e6cc33dc69901c9693ff
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:21 +0100

    Runtime: 48 ms (Top 80.79%) | Memory: 13.9 MB (Top 16.98%)

--- scripts/algorithms/K/Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies.py
+++ scripts/algorithms/K/Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies.py
@@ -1,3 +1,4 @@
+# Runtime: 48 ms (Top 80.79%) | Memory: 13.9 MB (Top 16.98%)
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         """
@@ -5,17 +6,15 @@ class Solution(object):
         :type extraCandies: int
         :rtype: List[bool]
         """
-        
+
         maxCandies = 0
         for candy in candies :
             if candy >= maxCandies:
                 maxCandies = candy
-        
+
         for i, candy in enumerate(candies):
             if candy + extraCandies >= maxCandies:
                 candies[i] = True
             else:
-                candies[i]  = False
+                candies[i] = False
         return candies
-                
-                
