d279036f8df27bfe315cb3792b4e84789741278f
AnasImloul <pydev27@gmail.com>
2022-09-04 14:52:16 +0100

    Runtime: 3 ms (Top 54.99%) | Memory: 51.8 MB (Top 39.25%)

--- scripts/algorithms/S/Surrounded Regions/Surrounded Regions.java
+++ scripts/algorithms/S/Surrounded Regions/Surrounded Regions.java
@@ -1,10 +1,11 @@
+// Runtime: 3 ms (Top 54.99%) | Memory: 51.8 MB (Top 39.25%)
 class Solution {
         boolean isClosed = true;
-    
+
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        
+
         // To identify all those O which are adjacent and unbounded by 'X', we put a temporary value
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -13,7 +14,7 @@ class Solution {
                 }
             }
         }
-        
+
         // revert the temperoray value and also replace remaining O with X
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -21,14 +22,14 @@ class Solution {
                 else if(board[i][j] == 'O') board[i][j] = 'X';
             }
         }
-        
+
     }
-    
+
     public void dfs(char[][] board, int i, int j){
         if(i<0 || j<0 || i>= board.length || j>=board[0].length || board[i][j] != 'O') return;
-        
+
         board[i][j] = 'T'; // to put a temperory mark/ to mark as visited
-        
+
         dfs(board, i, j+1); // Top
         dfs(board, i, j-1); // Bottom
         dfs(board, i+1, j); // Right
