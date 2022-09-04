ba4ab768bdf04f1c0ac2349b1f46efd355977afd
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:03 +0100

    Runtime: 104 ms (Top 51.21%) | Memory: 44.5 MB (Top 57.00%)

--- scripts/algorithms/S/Symmetric Tree/Symmetric Tree.js
+++ scripts/algorithms/S/Symmetric Tree/Symmetric Tree.js
@@ -1,11 +1,10 @@
+// Runtime: 104 ms (Top 51.21%) | Memory: 44.5 MB (Top 57.00%)
 var isSymmetric = function(root) {
     return helper(root.left, root.right);
-    
+
     function helper(left, right){
         if(!left && !right) return true;
         if((!left && right) || (left && !right) || (left.val !== right.val)) return false;
         return helper(left.left, right.right) && helper(left.right, right.left)
     }
 };
-
-
