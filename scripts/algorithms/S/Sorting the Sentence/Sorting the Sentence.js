99fff1f6ebae6e2a5e3226c2a96ae7c7daff00dd
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:04 +0100

    Runtime: 63 ms (Top 92.34%) | Memory: 42.5 MB (Top 5.22%)

--- scripts/algorithms/S/Sorting the Sentence/Sorting the Sentence.js
+++ scripts/algorithms/S/Sorting the Sentence/Sorting the Sentence.js
@@ -1,3 +1,4 @@
+// Runtime: 63 ms (Top 92.34%) | Memory: 42.5 MB (Top 5.22%)
 var sortSentence = function(s) {
     let sortingS = s.split(' ').sort((a,b) => a.substr(-1) - b.substr(-1));
     slicingS = sortingS.map(word => word.slice(0, -1));
