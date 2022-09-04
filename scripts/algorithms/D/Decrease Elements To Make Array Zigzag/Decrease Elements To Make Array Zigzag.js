0a8bfd7e7f2ff0c91b20663c6bf6e918c8809099
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:08 +0100

    Runtime: 112 ms (Top 7.14%) | Memory: 41.7 MB (Top 57.14%)

--- scripts/algorithms/D/Decrease Elements To Make Array Zigzag/Decrease Elements To Make Array Zigzag.js
+++ scripts/algorithms/D/Decrease Elements To Make Array Zigzag/Decrease Elements To Make Array Zigzag.js
@@ -1,3 +1,4 @@
+// Runtime: 112 ms (Top 7.14%) | Memory: 41.7 MB (Top 57.14%)
 var movesToMakeZigzag = function(nums) {
     const n = nums.length;
 
@@ -9,7 +10,7 @@ var movesToMakeZigzag = function(nums) {
 
     for (let i = 0; i < n; i++) {
         const currNum = nums[i];
-        const nextNum = i < n - 1 ? nums[i + 1] : Number.MIN_SAFE_INTEGER;        
+        const nextNum = i < n - 1 ? nums[i + 1] : Number.MIN_SAFE_INTEGER;
 
         if (i % 2 === 0) {
             if (lastEvenRight >= currNum) evenMoves += lastEvenRight - currNum + 1;
@@ -17,7 +18,7 @@ var movesToMakeZigzag = function(nums) {
             if (currNum <= nextNum) {
                 evenMoves += nextNum - currNum + 1;
             }
-            
+
             lastEvenRight = Math.min(currNum - 1, nextNum);
         }
         else {
@@ -26,10 +27,10 @@ var movesToMakeZigzag = function(nums) {
             if (currNum <= nextNum) {
                 oddMoves += nextNum - currNum + 1;
             }
-            
+
             lastOddRight = Math.min(currNum - 1, nextNum);
         }
-    } 
+    }
 
     return Math.min(oddMoves, evenMoves);
 };
