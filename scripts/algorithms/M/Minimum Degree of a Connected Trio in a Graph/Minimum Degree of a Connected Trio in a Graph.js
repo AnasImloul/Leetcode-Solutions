00218dbdfe715a6ad0c7784d91a86d0a3e9af2dd
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:12 +0100

    Runtime: 1504 ms (Top 45.95%) | Memory: 65.2 MB (Top 54.05%)

--- scripts/algorithms/M/Minimum Degree of a Connected Trio in a Graph/Minimum Degree of a Connected Trio in a Graph.js
+++ scripts/algorithms/M/Minimum Degree of a Connected Trio in a Graph/Minimum Degree of a Connected Trio in a Graph.js
@@ -1,3 +1,4 @@
+// Runtime: 1504 ms (Top 45.95%) | Memory: 65.2 MB (Top 54.05%)
 var minTrioDegree = function(n, edges) {
     // create an adjacency list of all the edges;
     const adjacencyList = new Array(n + 1).fill(0).map(() => new Set());
@@ -5,24 +6,24 @@ var minTrioDegree = function(n, edges) {
         adjacencyList[x].add(y);
         adjacencyList[y].add(x);
     }
-    
+
     let minimumDegree = Infinity;
-    
+
     // Find all the combinations of 3 vertices that connect
     // and if they connect calculate the degree
     for (let i = 1; i <= n; i++) {
         for (let j = i + 1; j <= n; j++) {
             for (let k = j + 1; k <= n; k++) {
                 if (adjacencyList[i].has(j) && adjacencyList[i].has(k) && adjacencyList[j].has(k)) {
-                    // We minus 6 because we have 3 vertices and each vertices has 2 edges 
+                    // We minus 6 because we have 3 vertices and each vertices has 2 edges
                     // going out to the 3 connecting nodes
                     const degree = adjacencyList[i].size + adjacencyList[j].size + adjacencyList[k].size - 6;
-                    minimumDegree = 
+                    minimumDegree =
                         Math.min(minimumDegree, degree);
                 }
             }
         }
     }
-    
+
     return minimumDegree === Infinity ? -1 : minimumDegree;
 };
