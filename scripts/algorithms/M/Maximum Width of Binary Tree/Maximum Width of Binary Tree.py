1217e7e3e05a401e10f0b5d027f05db99c7e1f78
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:40 +0100

    Runtime: 46 ms (Top 93.81%) | Memory: 14.7 MB (Top 75.82%)

--- scripts/algorithms/M/Maximum Width of Binary Tree/Maximum Width of Binary Tree.py
+++ scripts/algorithms/M/Maximum Width of Binary Tree/Maximum Width of Binary Tree.py
@@ -1,22 +1,23 @@
+# Runtime: 46 ms (Top 93.81%) | Memory: 14.7 MB (Top 75.82%)
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         q = deque([(root, 0)])
         res = 0
-        
+
         while q:
             res = max(res, q[-1][1] - q[0][1] + 1)
             for _ in range(len(q)):
                 curr, pos = q.popleft()
-                
+
                 if curr.left:
                     q.append((curr.left, pos*2))
                 if curr.right:
                     q.append((curr.right, pos*2 + 1))
-        
+
         return res
