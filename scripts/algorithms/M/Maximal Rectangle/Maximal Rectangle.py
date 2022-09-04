1fbb79d386a0cdce97634875c2969cf8ea16c526
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:33 +0100

    Runtime: 1058 ms (Top 6.78%) | Memory: 15.2 MB (Top 89.63%)

--- scripts/algorithms/M/Maximal Rectangle/Maximal Rectangle.py
+++ scripts/algorithms/M/Maximal Rectangle/Maximal Rectangle.py
@@ -1,7 +1,8 @@
+# Runtime: 1058 ms (Top 6.78%) | Memory: 15.2 MB (Top 89.63%)
 class Solution:
     def maxArea(self,arr: List[List[int]]) -> int:
         maxA = 0 #local max variable to return max area of a 1D array
-        stack = [-1] #initializing with minimum value so we can avoid using loop for remaining element in the stack after whole traversing  
+        stack = [-1] #initializing with minimum value so we can avoid using loop for remaining element in the stack after whole traversing
         arr.append(-1) # value for stack index -1 and one extra iteration to empty the stack
         for i in range(len(arr)): # traversing for stack from left to right
             arr[i] = int(arr[i]) # we are getting str value from matrix
@@ -14,12 +15,12 @@ class Solution:
     """Below is the code for adding each row one by one and passing it to above function to get the max area for each row"""
     # r1 - 1 0 1 0 0
     # r2 - 1 0 1 1 1
-    # r  = 2 0 2 1 1  This is for else condition
-    
+    # r = 2 0 2 1 1 This is for else condition
+
     # r1 - 1 1 1 0 1
     # r2 - 1 0 1 1 0
-    # r  = 2 0 2 1 0  This is for if condition
-    
+    # r = 2 0 2 1 0 This is for if condition
+
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         result = [0] * len(matrix[0])
         maxReact = 0
@@ -33,4 +34,3 @@ class Solution:
                     result[j] = result[j] + matrix[i][j]
             maxReact = max(maxReact,self.maxArea(result)) #after getting max area for 1D arr we are getting max value from those 1D arr for the 2D arr
         return maxReact
-                
