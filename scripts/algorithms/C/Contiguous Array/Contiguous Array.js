21c25bfc078eab322b876e24ac56631d5af26f1d
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:30 +0100

    Runtime: 142 ms (Top 79.05%) | Memory: 50.5 MB (Top 60.95%)

--- scripts/algorithms/C/Contiguous Array/Contiguous Array.js
+++ scripts/algorithms/C/Contiguous Array/Contiguous Array.js
@@ -1,13 +1,14 @@
+// Runtime: 142 ms (Top 79.05%) | Memory: 50.5 MB (Top 60.95%)
 var findMaxLength = function(nums) {
-	const hash = new Map([[0, -1]]);
-	let result = sum = 0;
+    const hash = new Map([[0, -1]]);
+    let result = sum = 0;
 
-	nums.forEach((num, index) => {
-		sum += num === 0 ? -1 : 1;
-		hash.has(sum) 
-			? result = Math.max(result, index - hash.get(sum))
-			: hash.set(sum, index);
-	});
+    nums.forEach((num, index) => {
+        sum += num === 0 ? -1 : 1;
+        hash.has(sum)
+            ? result = Math.max(result, index - hash.get(sum))
+            : hash.set(sum, index);
+    });
 
-	return result;
+    return result;
 };
