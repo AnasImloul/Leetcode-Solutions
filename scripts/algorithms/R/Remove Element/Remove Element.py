56c8fb7ad9b234d7c2cf9de0ec99e4215393d502
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:05 +0100

    Runtime: 74 ms (Top 6.53%) | Memory: 13.9 MB (Top 63.16%)

--- scripts/algorithms/R/Remove Element/Remove Element.py
+++ scripts/algorithms/R/Remove Element/Remove Element.py
@@ -1,3 +1,4 @@
+# Runtime: 74 ms (Top 6.53%) | Memory: 13.9 MB (Top 63.16%)
 class Solution(object):
     def removeElement(self, nums, val):
         """
@@ -5,7 +6,7 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        step  = 0
+        step = 0
         while step < len(nums):
             if nums[step] == val:
                 nums.pop(step)
