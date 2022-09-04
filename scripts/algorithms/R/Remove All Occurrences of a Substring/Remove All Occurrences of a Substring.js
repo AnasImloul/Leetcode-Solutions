1c566052df0fbef1156bc6f6def52b6983eb0841
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:07 +0100

    Runtime: 121 ms (Top 20.93%) | Memory: 48 MB (Top 8.53%)

--- scripts/algorithms/R/Remove All Occurrences of a Substring/Remove All Occurrences of a Substring.js
+++ scripts/algorithms/R/Remove All Occurrences of a Substring/Remove All Occurrences of a Substring.js
@@ -1,3 +1,4 @@
+// Runtime: 121 ms (Top 20.93%) | Memory: 48 MB (Top 8.53%)
 var removeOccurrences = function(s, part) {
     let n =part.length
     const reduceString = (s)=>{
@@ -8,7 +9,7 @@ var removeOccurrences = function(s, part) {
         s.splice(searchPart, n)
         s=s.join('')
         return reduceString(s)
-        
+
     }
     return reduceString(s)
 };
