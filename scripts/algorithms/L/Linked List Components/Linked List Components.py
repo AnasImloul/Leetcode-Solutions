e3add53136e6f243f0c2525841bb62a99bdf16e4
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:27 +0100

    Runtime: 171 ms (Top 62.36%) | Memory: 19.1 MB (Top 64.07%)

--- scripts/algorithms/L/Linked List Components/Linked List Components.py
+++ scripts/algorithms/L/Linked List Components/Linked List Components.py
@@ -1,9 +1,10 @@
+# Runtime: 171 ms (Top 62.36%) | Memory: 19.1 MB (Top 64.07%)
 class Solution:
     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
         d,count={},0
         for num in nums:
             d[num] = 0
-        
+
         while head:
             if head.val in d:
                 head = head.next
