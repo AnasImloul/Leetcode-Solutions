b61a45c4d3bfb14a7b7c94d1988e2ecdf7f8913e
AnasImloul <pydev27@gmail.com>
2022-09-04 12:05:16 +0100

    Runtime: 25 ms (Top 60.54%) | Memory: 14.6 MB (Top 67.48%)

--- scripts/algorithms/R/Replace Elements with Greatest Element on Right Side/Replace Elements with Greatest Element on Right Side.cpp
+++ scripts/algorithms/R/Replace Elements with Greatest Element on Right Side/Replace Elements with Greatest Element on Right Side.cpp
@@ -1,25 +1,26 @@
+// Runtime: 25 ms (Top 60.54%) | Memory: 14.6 MB (Top 67.48%)
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int n=arr.size();
-        
-		//taking last index as greatest for now
+
+        //taking last index as greatest for now
         int g=arr[n-1];
-		//setting last index as -1
+        //setting last index as -1
         arr[n-1]=-1;
         for(int i=n-2;i>=0;i--)
         {
-		//storing last index value to be changed to comapare with the greatest value till now
+        //storing last index value to be changed to comapare with the greatest value till now
             int h=arr[i];
-			//assigning greatest till now from right
+            //assigning greatest till now from right
             arr[i]=g;
-			//checking if current is greater than the previous indices
+            //checking if current is greater than the previous indices
             if(h>g)
             {
                 g=h;
             }
         }
                      //returning the value
-     return arr;   
+     return arr;
     }
 };
