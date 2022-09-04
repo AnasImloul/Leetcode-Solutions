ff9183976aac475c57a442ae562a2e1678401998
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:06 +0100

    Runtime: 74 ms (Top 94.68%) | Memory: 44 MB (Top 93.79%)

--- scripts/algorithms/E/Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.js
+++ scripts/algorithms/E/Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.js
@@ -1,3 +1,4 @@
+// Runtime: 74 ms (Top 94.68%) | Memory: 44 MB (Top 93.79%)
 const ops = new Set(['+', '-', '*', '/']);
 
 /**
@@ -5,8 +6,8 @@ const ops = new Set(['+', '-', '*', '/']);
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    const stack = [];    
-    
+    const stack = [];
+
     for (const token of tokens) {
         if (!ops.has(token)) {
             stack.push(Number(token));
@@ -16,26 +17,25 @@ var evalRPN = function(tokens) {
         const val1 = stack.pop();
         const val2 = stack.pop();
         let toPush;
-       
+
         if (token === '+') {
             toPush = val1 + val2;
         }
-        
+
         if (token === '-') {
             toPush = val2 - val1;
         }
-        
+
         if (token === '*') {
             toPush = val1 * val2;
         }
-        
+
         if (token === '/') {
             toPush = Math.trunc(val2 / val1);
         }
-        
+
         stack.push(toPush);
     }
-    
+
     return stack[0] || 0;
 };
-
