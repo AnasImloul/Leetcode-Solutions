7accb77862ae93685397108bcc4e7aeff0aaac2a
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:09 +0100

    Runtime: 2444 ms (Top 20.45%) | Memory: 28.6 MB (Top 5.68%)

--- scripts/algorithms/M/Minimum Number of People to Teach/Minimum Number of People to Teach.py
+++ scripts/algorithms/M/Minimum Number of People to Teach/Minimum Number of People to Teach.py
@@ -1,14 +1,15 @@
+# Runtime: 2444 ms (Top 20.45%) | Memory: 28.6 MB (Top 5.68%)
 class Solution:
     def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
         """
         1. Find out users who need to be taught
         2. If no user needs to be taught, return 0
         3. For all users who need to be taught a language, find the most popular language among them
-        4. Teach that language to rest of the users who need to be taught. 
+        4. Teach that language to rest of the users who need to be taught.
         """
         need_to_be_taught = set()
         languages = [set(language) for language in languages]
-        
+
         # 1. Find out users who needs to be taught
         for user1, user2 in friendships:
             # Adjust the 1 based indexing to 0 based indexing
@@ -16,24 +17,20 @@ class Solution:
             user2 = user2 - 1
             if not (languages[user1] & languages[user2]):
                 need_to_be_taught.update([user1, user2])
-                
+
         # 2. If no user needs to be taught, return 0
         if not need_to_be_taught:
             return 0
-        
+
         # 3. For all users who needs to be taught a language, find the most popular language among them
         language_spoken_by = collections.defaultdict(int)
         for user in need_to_be_taught:
             # for each user increment the count of languages he can speak
             for language in languages[user]:
                 language_spoken_by[language] += 1
-    
+
         # find occurrence of language spoken by maximum users who can't communicate with each other
         popular_language_count = max(language_spoken_by.values())
-        
-        # 4. Teach that language to rest of the users who need to be taught. 
+
+        # 4. Teach that language to rest of the users who need to be taught.
         return len(need_to_be_taught)- popular_language_count
-        
-        
-        
-                
