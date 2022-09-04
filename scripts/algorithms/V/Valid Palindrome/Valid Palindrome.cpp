a2a87c19fa0240fb30236a346e8785cfb008cd39
AnasImloul <pydev27@gmail.com>
2022-09-04 12:05:19 +0100

    Runtime: 8 ms (Top 58.78%) | Memory: 7.4 MB (Top 54.16%)

--- scripts/algorithms/V/Valid Palindrome/Valid Palindrome.cpp
+++ scripts/algorithms/V/Valid Palindrome/Valid Palindrome.cpp
@@ -1,21 +1,22 @@
+// Runtime: 8 ms (Top 58.78%) | Memory: 7.4 MB (Top 54.16%)
 class Solution {
 public:
     bool isPalindrome(string s) {
         auto it = remove_if(s.begin(), s.end(), [](char const &c) {
             return !isalnum(c);
         });
- 
+
         s.erase(it, s.end());
         transform(s.begin(), s.end(), s.begin(), ::tolower);
-        
+
         int i = 0;
         int j = s.size()-1;
-        
+
         while(i <= j) {
             if(s[i] != s[j]) return false;
             i++; j--;
         }
-        
+
         return true;
     }
 };
