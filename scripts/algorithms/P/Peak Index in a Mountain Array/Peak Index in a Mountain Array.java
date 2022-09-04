07f7aee948a766234a4c7a5dc5621d0df2d8d1a3
AnasImloul <pydev27@gmail.com>
2022-09-04 14:52:08 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 59.7 MB (Top 83.58%)

--- scripts/algorithms/P/Peak Index in a Mountain Array/Peak Index in a Mountain Array.java
+++ scripts/algorithms/P/Peak Index in a Mountain Array/Peak Index in a Mountain Array.java
@@ -1,9 +1,10 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 59.7 MB (Top 83.58%)
 class Solution {
 public int peakIndexInMountainArray(int[] arr) {
-     
+
     int start = 0;
     int end = arr.length - 1;
-    
+
     while( start < end){
         int mid = start + (end - start)/2;
         // if mid < mid next
@@ -16,5 +17,5 @@ public int peakIndexInMountainArray(int[] arr) {
         }
     }
     return start; // or we can return end also, bcz both will be on same value at the time, that's why loop breaks here.
-	}
+    }
 }
