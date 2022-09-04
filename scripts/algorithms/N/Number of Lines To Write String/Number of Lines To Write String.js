98ae5b635944da7a4c9d858ef0e088d60c8012e8
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:33 +0100

    Runtime: 110 ms (Top 27.73%) | Memory: 43.1 MB (Top 24.37%)

--- scripts/algorithms/N/Number of Lines To Write String/Number of Lines To Write String.js
+++ scripts/algorithms/N/Number of Lines To Write String/Number of Lines To Write String.js
@@ -1,13 +1,14 @@
+// Runtime: 110 ms (Top 27.73%) | Memory: 43.1 MB (Top 24.37%)
 var numberOfLines = function(widths, s) {
     let pixel=100, line=1;
     for(let i=0; i<s.length; i++){
         if(pixel>=widths[s[i].charCodeAt()-97]){
             pixel-=widths[s[i].charCodeAt()-97];
         }else{
-			// this word should be written in NEXT line, so it CANNOT count. 
+            // this word should be written in NEXT line, so it CANNOT count.
             i--; line++; pixel=100;
         }
     }
-	// 100-pixel = space used in this line.
+    // 100-pixel = space used in this line.
     return [line, 100-pixel];
 };
