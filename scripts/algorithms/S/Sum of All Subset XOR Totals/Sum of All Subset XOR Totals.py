7958cc7f0d38a64b9a105fea8c107e1e2863c2d0
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:17 +0100

    Runtime: 60 ms (Top 83.44%) | Memory: 13.8 MB (Top 70.65%)

--- scripts/algorithms/S/Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.py
+++ scripts/algorithms/S/Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.py
@@ -1,8 +1,9 @@
+# Runtime: 60 ms (Top 83.44%) | Memory: 13.8 MB (Top 70.65%)
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         def sums(term, idx):
             if idx == len(nums):
-                return term            
+                return term
             return sums(term, idx + 1) + sums(term ^ nums[idx], idx + 1)
-        
+
         return sums(0, 0)
