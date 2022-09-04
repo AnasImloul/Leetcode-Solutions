662949ac210aac3e3558f0105fb09fa63538e14f
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:37 +0100

    Runtime: 61 ms (Top 15.28%) | Memory: 13.8 MB (Top 62.83%)

--- scripts/algorithms/M/Maximum Repeating Substring/Maximum Repeating Substring.py
+++ scripts/algorithms/M/Maximum Repeating Substring/Maximum Repeating Substring.py
@@ -1,3 +1,4 @@
+# Runtime: 61 ms (Top 15.28%) | Memory: 13.8 MB (Top 62.83%)
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
         if word not in sequence:
@@ -10,6 +11,6 @@ class Solution:
             if word * mid in sequence:
                 left = mid + 1
             else:
-                right = mid - 1 
-                
+                right = mid - 1
+
         return left - 1
