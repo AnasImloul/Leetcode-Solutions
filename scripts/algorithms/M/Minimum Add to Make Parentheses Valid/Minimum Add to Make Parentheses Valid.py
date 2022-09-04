8a3aed04087422f1e6affa544d943bc9dd148660
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:10 +0100

    Runtime: 53 ms (Top 37.79%) | Memory: 13.8 MB (Top 96.01%)

--- scripts/algorithms/M/Minimum Add to Make Parentheses Valid/Minimum Add to Make Parentheses Valid.py
+++ scripts/algorithms/M/Minimum Add to Make Parentheses Valid/Minimum Add to Make Parentheses Valid.py
@@ -1,16 +1,17 @@
+# Runtime: 53 ms (Top 37.79%) | Memory: 13.8 MB (Top 96.01%)
 class Solution:
-	def minAddToMakeValid(self, s: str) -> int:
-		stack = []
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
 
-		for parenthese in s:
-			if parenthese == "(":
-				stack.append("(")
+        for parenthese in s:
+            if parenthese == "(":
+                stack.append("(")
 
-			else:
-				if not stack or stack[-1] == ")":
-					stack.append(")")
+            else:
+                if not stack or stack[-1] == ")":
+                    stack.append(")")
 
-				if stack and stack[-1] == "(":
-					stack.pop()
+                if stack and stack[-1] == "(":
+                    stack.pop()
 
-		return len(stack)
+        return len(stack)
