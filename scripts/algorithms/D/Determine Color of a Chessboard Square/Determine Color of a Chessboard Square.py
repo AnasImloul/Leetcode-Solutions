5b75dae1da8c7123d850498257e4445791d04970
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:42 +0100

    Runtime: 62 ms (Top 10.11%) | Memory: 13.8 MB (Top 56.37%)

--- scripts/algorithms/D/Determine Color of a Chessboard Square/Determine Color of a Chessboard Square.py
+++ scripts/algorithms/D/Determine Color of a Chessboard Square/Determine Color of a Chessboard Square.py
@@ -1,3 +1,4 @@
+# Runtime: 62 ms (Top 10.11%) | Memory: 13.8 MB (Top 56.37%)
 class Solution:
     def squareIsWhite(self, coordinates: str) -> bool:
         return (ord(coordinates[0])+ord(coordinates[1]))%2
