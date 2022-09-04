0005d55ebf697689599eaa00030dde1451819beb
AnasImloul <pydev27@gmail.com>
2022-09-04 14:52:15 +0100

    Runtime: 1 ms (Top 72.17%) | Memory: 42.2 MB (Top 52.18%)

--- scripts/algorithms/R/Remove Nth Node From End of List/Remove Nth Node From End of List.java
+++ scripts/algorithms/R/Remove Nth Node From End of List/Remove Nth Node From End of List.java
@@ -1,26 +1,27 @@
+// Runtime: 1 ms (Top 72.17%) | Memory: 42.2 MB (Top 52.18%)
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode temp = head;
         int len=0;
-    
+
         if(head==null || head.next==null)
             return null;
-        
+
         while(temp!=null){
             temp=temp.next;
             len++;
         }
-        
+
         if(len==n)
             return head.next;
-        
+
         int frontlen = len-n-1;
-        
+
         ListNode first=head.next;
         ListNode second = head;
-        
+
         int count=0;
-        
+
         while(first!=null){
             if(count==frontlen){
                 second.next=first.next;
@@ -31,7 +32,7 @@ class Solution {
                 count++;
             }
         }
-        
+
         return head;
     }
 }
