e74793c5fa7d36f03170759b99772338b066cd1f
AnasImloul <pydev27@gmail.com>
2022-09-04 15:07:43 +0100

    Runtime: 71 ms (Top 18.71%) | Memory: 15.5 MB (Top 28.33%)

--- scripts/algorithms/R/Reverse Linked List/Reverse Linked List.py
+++ scripts/algorithms/R/Reverse Linked List/Reverse Linked List.py
@@ -1,8 +1,9 @@
+# Runtime: 71 ms (Top 18.71%) | Memory: 15.5 MB (Top 28.33%)
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# def __init__(self, val=0, next=None):
+# self.val = val
+# self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head==None or head.next==None:
@@ -14,4 +15,3 @@ class Solution:
             p = head
             head = temp
         return p
-        
