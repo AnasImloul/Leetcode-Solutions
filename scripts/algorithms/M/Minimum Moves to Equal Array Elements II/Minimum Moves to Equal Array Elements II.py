02c82ef642174f9e485adf35b1ea87177b1eddee
AnasImloul <pydev27@gmail.com>
2022-09-04 12:39:19 +0100

    Runtime: 177 ms (Top 17.25%) | Memory: 15.5 MB (Top 9.55%)

--- scripts/algorithms/M/Minimum Moves to Equal Array Elements II/Minimum Moves to Equal Array Elements II.py
+++ scripts/algorithms/M/Minimum Moves to Equal Array Elements II/Minimum Moves to Equal Array Elements II.py
@@ -1,11 +1,12 @@
+# Runtime: 177 ms (Top 17.25%) | Memory: 15.5 MB (Top 9.55%)
 class Solution:
-	def minMoves2(self, nums: List[int]) -> int:
-		nums.sort()
-		n = len(nums)
-		m = n//2
-		median = nums[m]
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        m = n//2
+        median = nums[m]
 
-		ans = 0
-		for num in nums:
-			ans += abs(num-median)
-		return ans
+        ans = 0
+        for num in nums:
+            ans += abs(num-median)
+        return ans
