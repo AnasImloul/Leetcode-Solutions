27b3178ebdef42c92e5acb3113abdc7f348be2e5
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:12 +0100

    Runtime: 3 ms (Top 82.44%) | Memory: 7.8 MB (Top 19.11%)

--- scripts/algorithms/C/Cells in a Range on an Excel Sheet/Cells in a Range on an Excel Sheet.cpp
+++ scripts/algorithms/C/Cells in a Range on an Excel Sheet/Cells in a Range on an Excel Sheet.cpp
@@ -1,9 +1,10 @@
+// Runtime: 3 ms (Top 82.44%) | Memory: 7.8 MB (Top 19.11%)
 class Solution {
 public:
     vector<string> cellsInRange(string s) {
-        
+
         vector<string>ans;
-        
+
         for(char ch=s[0];ch<=s[3];ch++)
         {
             for(int i=s[1]-'0';i<=s[4]-'0';i++)
@@ -12,7 +13,7 @@ public:
                 res+=ch;
                 res+=to_string(i);
                 ans.push_back(res);
-               
+
             }
         }
         return ans;
