66690d01d29cd3e24b2624c66cafdcda77c52bea
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:09 +0100

    Runtime: 633 ms (Top 77.58%) | Memory: 145.9 MB (Top 23.71%)

--- scripts/algorithms/L/Length of Longest Fibonacci Subsequence/Length of Longest Fibonacci Subsequence.cpp
+++ scripts/algorithms/L/Length of Longest Fibonacci Subsequence/Length of Longest Fibonacci Subsequence.cpp
@@ -1,33 +1,34 @@
+// Runtime: 633 ms (Top 77.58%) | Memory: 145.9 MB (Top 23.71%)
 class Solution {
 public:
-    
+
     int help(int i,int j, unordered_map<int,int> &mp,vector<int> &nums , vector<vector<int>> &dp){
-        
+
         if(dp[i][j]!=-1)
             return dp[i][j];
-        
+
         int x=nums[i]+nums[j];
-        
+
         if(mp.find(x)!=mp.end())
         {
             dp[i][j]=help(j,mp[x],mp,nums,dp);
            return 1+dp[i][j];
         }
-        
+
         return 1;
     }
-    
-   int solve(int idx ,int n,  unordered_map<int,int> &mp,vector<int> &nums , vector<vector<int>> &dp)
+
+   int solve(int idx ,int n, unordered_map<int,int> &mp,vector<int> &nums , vector<vector<int>> &dp)
     {
         int maxi=INT_MIN;
-        
+
         for(int i=idx;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)  
+            for(int j=i+1;j<n;j++)
             {
-                
+
                 int x=nums[i]+nums[j];
-             
+
                 if(mp.find(x)!=mp.end())
                 {
                  maxi=max(maxi,2+help(j,mp[x],mp,nums,dp));
@@ -37,18 +38,15 @@ public:
         return (maxi==INT_MIN?0:maxi);
     }
     int lenLongestFibSubseq(vector<int>& arr) {
-        
+
         unordered_map<int,int> mp;
         int n=arr.size();
         vector<vector<int>> dp(n,vector<int> (n,-1));
-        
-        
+
         for(int i=0;i<arr.size();i++)
             mp[arr[i]]=i;
-        
-        
+
        return solve(0,n,mp,arr,dp);
-        
-        
+
     }
 };
