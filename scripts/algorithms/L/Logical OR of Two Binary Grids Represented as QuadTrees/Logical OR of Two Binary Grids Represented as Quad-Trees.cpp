76099e2f60fc9bd0accad1d45b7d35976e8c2d4a
AnasImloul <pydev27@gmail.com>
2022-09-04 16:14:05 +0100

    Runtime: 44 ms (Top 33.83%) | Memory: 16.4 MB (Top 45.86%)

---
+++ scripts/algorithms/L/Logical OR of Two Binary Grids Represented as QuadTrees/Logical OR of Two Binary Grids Represented as Quad-Trees.cpp
@@ -0,0 +1,36 @@
+// Runtime: 44 ms (Top 33.83%) | Memory: 16.4 MB (Top 45.86%)
+class Solution {
+public:
+    Node* intersect(Node* quadTree1, Node* quadTree2) {
+        if (quadTree1->isLeaf && quadTree2->isLeaf) {
+            return new Node(quadTree1->val || quadTree2->val, true);
+        }
+        if (quadTree1->isLeaf) {
+            if (quadTree1->val) return quadTree1;
+            return quadTree2;
+        }
+        if (quadTree2->isLeaf) {
+            if (quadTree2->val) return quadTree2;
+            return quadTree1;
+        }
+
+        Node *topLeft = intersect(quadTree1->topLeft, quadTree2->topLeft);
+        Node *topRight = intersect(quadTree1->topRight, quadTree2->topRight);
+        Node *bottomLeft = intersect(quadTree1->bottomLeft, quadTree2->bottomLeft);
+        Node *bottomRight = intersect(quadTree1->bottomRight, quadTree2->bottomRight);
+
+        Node *newnode = new Node();
+
+        if (topLeft->isLeaf && topRight->isLeaf && bottomLeft->isLeaf && bottomRight->isLeaf && topLeft->val == topRight->val && bottomLeft->val == bottomRight->val && topLeft->val == bottomRight->val) {
+            newnode->isLeaf = true;
+            newnode->val = topLeft->val;
+        } else {
+            newnode->topLeft = topLeft;
+            newnode->topRight = topRight;
+            newnode->bottomLeft = bottomLeft;
+            newnode->bottomRight = bottomRight;
+        }
+
+        return newnode;
+    }
+};
