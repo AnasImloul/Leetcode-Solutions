ea165e1841775e4590584e9cfaeca4ea975ec76a
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:30 +0100

    Runtime: 34 ms (Top 93.54%) | Memory: 13.8 MB (Top 81.83%)

--- scripts/algorithms/F/Find and Replace Pattern/Find and Replace Pattern.py
+++ scripts/algorithms/F/Find and Replace Pattern/Find and Replace Pattern.py
@@ -1,20 +1,21 @@
+# Runtime: 34 ms (Top 93.54%) | Memory: 13.8 MB (Top 81.83%)
 class Solution:
-	def findAndReplacePattern(self, words: List[str], p: str) -> List[str]:
-		def find(w): # function thats calculate the numeric pattern
-			l = []
-			m = defaultdict(int)
-			i = 0
-			for c in w:
-				if c in m:
-					l.append(m[c])
-				else:
-					i+=1
-					m[c]=i
-					l.append(m[c])
-			return l
-		ans = []
-		pfind = find(p)
-		for w in words:
-			wfind = find(w)
-			if wfind == pfind: ans.append(w) #check if numeric pattern of pattern is equal to pattern of word 
-		return ans
+    def findAndReplacePattern(self, words: List[str], p: str) -> List[str]:
+        def find(w): # function thats calculate the numeric pattern
+            l = []
+            m = defaultdict(int)
+            i = 0
+            for c in w:
+                if c in m:
+                    l.append(m[c])
+                else:
+                    i+=1
+                    m[c]=i
+                    l.append(m[c])
+            return l
+        ans = []
+        pfind = find(p)
+        for w in words:
+            wfind = find(w)
+            if wfind == pfind: ans.append(w) #check if numeric pattern of pattern is equal to pattern of word
+        return ans
