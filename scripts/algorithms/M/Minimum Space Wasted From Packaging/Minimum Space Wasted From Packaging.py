b1d926eeade0138b71b8b68160b727de969315c9
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:20 +0100

    Runtime: 2545 ms (Top 38.89%) | Memory: 37.8 MB (Top 95.24%)

--- scripts/algorithms/M/Minimum Space Wasted From Packaging/Minimum Space Wasted From Packaging.py
+++ scripts/algorithms/M/Minimum Space Wasted From Packaging/Minimum Space Wasted From Packaging.py
@@ -1,3 +1,4 @@
+# Runtime: 2545 ms (Top 38.89%) | Memory: 37.8 MB (Top 95.24%)
 class Solution:
     def minWastedSpace(self, packages: List[int], boxes: List[List[int]]) -> int:
         # prefix sum to save time
@@ -9,20 +10,20 @@ class Solution:
             tmp = 0
             # deal with smallest box first
             box.sort()
-            
+
             # record number of packages already dealt with
             start = 0
-            
+
             for b in box:
                 loc = bisect.bisect(packages, b)
                 if loc == 0: continue
                 tmp += b * (loc - start) - (acc[loc] - acc[start])
-                
+
                 # all are packaged
                 if loc == len(packages):
                     ans = min(ans, tmp)
                     break
-                    
+
                 start = loc
-        
+
         return ans % (10 **9+7) if ans != float('inf') else -1
