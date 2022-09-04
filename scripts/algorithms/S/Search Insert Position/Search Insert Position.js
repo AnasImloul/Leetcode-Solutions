e50e8fe88a7d0f7fdf1feade0c7f3a66a011d041
AnasImloul <pydev27@gmail.com>
2022-09-04 14:59:20 +0100

    Runtime: 71 ms (Top 81.32%) | Memory: 42.7 MB (Top 14.71%)

--- scripts/algorithms/S/Search Insert Position/Search Insert Position.js
+++ scripts/algorithms/S/Search Insert Position/Search Insert Position.js
@@ -1,23 +1,24 @@
+// Runtime: 71 ms (Top 81.32%) | Memory: 42.7 MB (Top 14.71%)
 var searchInsert = function(nums, target) {
     let start=0;
     let end= nums.length-1;
-    
+
     while(start <= end) {
         const mid = Math.trunc((start+end)/2);
         if(nums[mid] === target) {
             return mid;
-        }  
+        }
         if(nums[mid] < target) {
-            start =  mid+1;
+            start = mid+1;
         } else {
             end = mid-1
         }
         if(nums[end]< target){
           return end+1;
         }
-        
+
         if(nums[start] > target){
           return start;
         }
-    } 
+    }
 };
