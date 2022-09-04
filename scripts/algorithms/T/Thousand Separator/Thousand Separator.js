4ee0bdf150188d953a51034439da56688c8be1bd
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:10 +0100

    Runtime: 63 ms (Top 94.17%) | Memory: 41.7 MB (Top 95.00%)

--- scripts/algorithms/T/Thousand Separator/Thousand Separator.js
+++ scripts/algorithms/T/Thousand Separator/Thousand Separator.js
@@ -1,11 +1,12 @@
+// Runtime: 63 ms (Top 94.17%) | Memory: 41.7 MB (Top 95.00%)
 var thousandSeparator = function(n) {
     let ans = "";
-        
+
     if(n >= 1000){
     const arr = String(n).split('');
         for(let i=0;i<arr.length;i++){
             let temp = arr.length - i;
-            if(temp === 3  && arr.length > temp || temp === 6   && arr.length > temp ||  temp === 9  && arr.length > temp ||  temp ===  12 && arr.length > temp){
+            if(temp === 3 && arr.length > temp || temp === 6 && arr.length > temp || temp === 9 && arr.length > temp || temp === 12 && arr.length > temp){
                 ans += '.';
             }
         ans += arr[i];
@@ -13,6 +14,6 @@ var thousandSeparator = function(n) {
     }else{
         ans += n;
     }
-    
+
     return ans;
 };
