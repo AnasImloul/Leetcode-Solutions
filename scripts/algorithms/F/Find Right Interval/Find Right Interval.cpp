80f18a0caf8bd49cba37c34d42b2bdd20c421b2f
AnasImloul <pydev27@gmail.com>
2022-09-04 12:05:21 +0100

    Runtime: 175 ms (Top 20.59%) | Memory: 28.4 MB (Top 34.44%)

--- scripts/algorithms/F/Find Right Interval/Find Right Interval.cpp
+++ scripts/algorithms/F/Find Right Interval/Find Right Interval.cpp
@@ -1,20 +1,21 @@
+// Runtime: 175 ms (Top 20.59%) | Memory: 28.4 MB (Top 34.44%)
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-    
+
         map<int, int> mp;
         int n = intervals.size();
-        
+
         for(int i = 0; i < n; i++)
             mp[intervals[i][0]] = i;
-        
+
         vector<int> ans;
         for(auto &i : intervals)
         {
             auto it = mp.lower_bound(i[1]);
-            ans.push_back(it == mp.end() ? -1 : it->second);           
+            ans.push_back(it == mp.end() ? -1 : it->second);
         }
-        
-        return ans;        
+
+        return ans;
     }
 };
