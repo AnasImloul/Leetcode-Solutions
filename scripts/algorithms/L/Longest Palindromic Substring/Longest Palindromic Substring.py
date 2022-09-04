07ece6de8482f439b643b531a87bcefd5a4af06a
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:30 +0100

    Runtime: 643 ms (Top 86.54%) | Memory: 14 MB (Top 58.84%)

--- scripts/algorithms/L/Longest Palindromic Substring/Longest Palindromic Substring.py
+++ scripts/algorithms/L/Longest Palindromic Substring/Longest Palindromic Substring.py
@@ -1,14 +1,15 @@
+# Runtime: 643 ms (Top 86.54%) | Memory: 14 MB (Top 58.84%)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ""
         for i in range(len(s)):
             left, right = i - 1, i + 1
-            
+
             while (right < len(s) and s[right] == s[i]):
                 right += 1
-                
+
             while (0 <= left < right < len(s) and s[left] == s[right]):
                 left, right = left - 1, right + 1
-                
+
             res = s[left+1:right] if right - left-1 > len(res) else res
         return res
