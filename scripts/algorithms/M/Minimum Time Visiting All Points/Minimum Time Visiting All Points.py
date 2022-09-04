e8524c53a081e3d92137b1fba196102d7910953f
AnasImloul <pydev27@gmail.com>
2022-09-04 16:35:26 +0100

    Runtime: 150 ms (Top 9.30%) | Memory: 13.9 MB (Top 38.82%)

--- scripts/algorithms/M/Minimum Time Visiting All Points/Minimum Time Visiting All Points.py
+++ scripts/algorithms/M/Minimum Time Visiting All Points/Minimum Time Visiting All Points.py
@@ -1,3 +1,4 @@
+# Runtime: 150 ms (Top 9.30%) | Memory: 13.9 MB (Top 38.82%)
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
         prev_x, prev_y = points[0]
