b40f30b9319178ae6065c0cd6348a01a644b12e1
AnasImloul <pydev27@gmail.com>
2022-09-04 16:27:07 +0100

    Runtime: 118 ms (Top 50.00%) | Memory: 44.6 MB (Top 73.90%)

--- scripts/algorithms/N/Number of Closed Islands/Number of Closed Islands.js
+++ scripts/algorithms/N/Number of Closed Islands/Number of Closed Islands.js
@@ -1,3 +1,4 @@
+// Runtime: 118 ms (Top 50.00%) | Memory: 44.6 MB (Top 73.90%)
 /**
  * @param {number[][]} grid
  * @return {number}
@@ -6,10 +7,10 @@ var closedIsland = function(grid) {
     let rows = grid.length;
     let cols = grid[0].length;
     let islandCount = 0; // Initial island count
-    
+
     // For Quick Response
     if (rows <= 2 || cols <= 2) return islandCount;
-  
+
     for (let i = 0; i <= rows - 1; i++) {
         for (let j = 0; j <= cols - 1; j++) {
             /*
@@ -21,7 +22,7 @@ var closedIsland = function(grid) {
             }
         }
     }
-    
+
     // If land is found, increase the count and walk around land(adjacent indexes) to mark them as visited.
     for (let i = 1; i <= rows - 1; i++) {
         for (let j = 1; j <= cols - 1; j++) {
@@ -31,19 +32,19 @@ var closedIsland = function(grid) {
             }
         }
     }
-    
+
     // To walk around land and mark it as visited
     function dfs(x, y) {
         if (x < 0 || y < 0 || x >= rows || y >= cols) return;
         if (grid[x][y] !== 0) return;
-        
+
         grid[x][y] = 2;
-        
+
         dfs(x + 1, y);
         dfs(x, y + 1);
         dfs(x - 1, y);
         dfs(x, y - 1);
     }
-    
+
     return islandCount;
 };
