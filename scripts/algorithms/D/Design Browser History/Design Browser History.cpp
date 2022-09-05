aae4b4c77e2929d640117fb18758617eab71cad9
AnasImloul <pydev27@gmail.com>
2022-09-05 13:26:14 +0100

    Runtime: 265 ms (Top 35.36%) | Memory: 60.7 MB (Top 39.67%)

--- scripts/algorithms/D/Design Browser History/Design Browser History.cpp
+++ scripts/algorithms/D/Design Browser History/Design Browser History.cpp
@@ -1,5 +1,6 @@
+// Runtime: 265 ms (Top 35.36%) | Memory: 60.7 MB (Top 39.67%)
 class Node{
-public: 
+public:
     string url;
     Node* next;
     Node* back;
@@ -12,23 +13,23 @@ public:
 
 class BrowserHistory {
     Node* head;
-public:    
+public:
     BrowserHistory(string homepage) {
         head = new Node(homepage);
     }
-    
+
     void visit(string url) {
         Node* ptr = new Node(url);
         head->next = ptr;
         ptr->back = head;
         head = ptr;
     }
-    
+
     string back(int steps) {
         while(head->back && steps--){ head = head->back; }
         return head->url;
     }
-    
+
     string forward(int steps) {
         while(head->next && steps--){ head = head->next; }
         return head->url;
