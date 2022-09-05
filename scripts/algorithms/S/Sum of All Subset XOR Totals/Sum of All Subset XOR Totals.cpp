12a65b8e1e8fce2a18b6287f611cd042ec90a397
AnasImloul <pydev27@gmail.com>
2022-09-05 13:26:12 +0100

    Runtime: 6 ms (Top 60.42%) | Memory: 7.2 MB (Top 56.55%)

--- scripts/algorithms/S/Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.cpp
+++ scripts/algorithms/S/Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.cpp
@@ -1,8 +1,8 @@
+// Runtime: 6 ms (Top 60.42%) | Memory: 7.2 MB (Top 56.55%)
 class Solution {
 public:
-    
-    
-    int subsetXORSum(vector<int>& nums) 
+
+    int subsetXORSum(vector<int>& nums)
     {
         int ans=0;
         for(int i=0; i<32; i++)
