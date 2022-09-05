833d9799041c39e4350bd82aa92b48db6ace083b
AnasImloul <pydev27@gmail.com>
2022-09-05 13:26:27 +0100

    Runtime: 4 ms (Top 26.49%) | Memory: 5.8 MB (Top 90.19%)

---
+++ scripts/algorithms/F/Find the KBeauty of a Number/Find the K-Beauty of a Number.cpp
@@ -0,0 +1,36 @@
+// Runtime: 4 ms (Top 26.49%) | Memory: 5.8 MB (Top 90.19%)
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        string str = to_string(num);
+        int i = 0, j = 0, n = str.length();
+        int ind = 0;
+
+        while(j < n)
+        {
+            if(j - i + 1 < k)
+            {
+            // increment j till we get the window size
+                ++j;
+            }
+            else if(j - i + 1 == k)
+            {
+            // on hiting the window size
+            // extract window string and convert to int
+            // check if it follows the given condition
+                string s = str.substr(i,k);
+                int n = stoi(s);
+                if(n != 0 && num % n == 0 )
+                    ++ind;
+
+                // shift the window by ++j;
+                // remove previous calculation by ++i
+                ++i;
+                ++j;
+            }
+
+        }
+
+        return ind;
+    }
+};
