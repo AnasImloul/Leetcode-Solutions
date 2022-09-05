4352a2fcdd9db88b98c67cfab0dbc14a7d112744
AnasImloul <pydev27@gmail.com>
2022-09-05 13:30:45 +0100

    Runtime: 2445 ms (Top 18.75%) | Memory: 52 MB (Top 18.75%)

--- scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.js
+++ scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.js
@@ -1,7 +1,8 @@
-var lastSubstring = function(s) { 
+// Runtime: 2445 ms (Top 18.75%) | Memory: 52 MB (Top 18.75%)
+var lastSubstring = function(s) {
     let res = 0;
     for(let i = 1; i < s.length; i++){
-        if(s.slice(i) > s.slice(res)) res = i;     
+        if(s.slice(i) > s.slice(res)) res = i;
     };
     return s.slice(res);
 };
