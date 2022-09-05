31b18daea918084ec95f60c2a3bd0fcb2a376f00
AnasImloul <pydev27@gmail.com>
2022-09-05 13:26:23 +0100

    Runtime: 6 ms (Top 24.99%) | Memory: 5.8 MB (Top 76.00%)

--- scripts/algorithms/R/Reverse Integer/Reverse Integer.cpp
+++ scripts/algorithms/R/Reverse Integer/Reverse Integer.cpp
@@ -1,15 +1,16 @@
+// Runtime: 6 ms (Top 24.99%) | Memory: 5.8 MB (Top 76.00%)
 class Solution {
 public:
     int reverse(int x) {
-		long res = 0;
-		while (abs(x) > 0) {
-			res = (res + x % 10) * 10;
-			x /= 10;
-		}
-		x < 0 ? res = res / 10 * -1 : res = res / 10;
-		if (res < INT32_MIN || res > INT32_MAX) {
-			res = 0;
-		}
-		return res;
+        long res = 0;
+        while (abs(x) > 0) {
+            res = (res + x % 10) * 10;
+            x /= 10;
+        }
+        x < 0 ? res = res / 10 * -1 : res = res / 10;
+        if (res < INT32_MIN || res > INT32_MAX) {
+            res = 0;
+        }
+        return res;
     }
 };
