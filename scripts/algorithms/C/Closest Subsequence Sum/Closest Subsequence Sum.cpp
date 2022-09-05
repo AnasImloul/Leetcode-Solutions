2dcc30b6b15e093093784cd4d82bb995f0e9dea8
AnasImloul <pydev27@gmail.com>
2022-09-05 13:26:26 +0100

    Runtime: 1696 ms (Top 38.97%) | Memory: 63.6 MB (Top 61.56%)

--- scripts/algorithms/C/Closest Subsequence Sum/Closest Subsequence Sum.cpp
+++ scripts/algorithms/C/Closest Subsequence Sum/Closest Subsequence Sum.cpp
@@ -1,3 +1,4 @@
+// Runtime: 1696 ms (Top 38.97%) | Memory: 63.6 MB (Top 61.56%)
 class Solution {
 public:
     void find(vector<int>&v, int i, int e, int sum, vector<int>&sumv){
@@ -8,32 +9,31 @@ public:
         find(v,i+1,e,sum+v[i],sumv);
         find(v,i+1,e,sum,sumv);
     }
-        
-    
+
     int minAbsDifference(vector<int>& nums, int goal) {
         int n=nums.size();
-        
+
         //Step 1: Divide nums into 2 subarrays of size n/2 and n-n/2
-        
+
         vector<int>A,B;
         for(int i=0;i<n/2;i++)
             A.push_back(nums[i]);
         for(int i=n/2;i<n;i++)
             B.push_back(nums[i]);
-        
+
         //Step 2: Find all possible subset sums of A and B
-        
+
         vector<int>sumA,sumB;
         find(A,0,A.size(),0,sumA);
         find(B,0,B.size(),0,sumB);
-        
+
         sort(sumA.begin(),sumA.end());
         sort(sumB.begin(),sumB.end());
-        
+
         //Step 3: Find combinations from sumA & sumB such that abs(sum-goal) is minimized
-        
+
         int ans=INT_MAX;
-        
+
         for(int i=0;i<sumA.size();i++){
             int s=sumA[i];
             int l=0;
@@ -49,10 +49,10 @@ public:
                 }
                 else{
                     l=mid+1;
-                }    
+                }
             }
         }
-        
+
         return ans;
     }
 };
