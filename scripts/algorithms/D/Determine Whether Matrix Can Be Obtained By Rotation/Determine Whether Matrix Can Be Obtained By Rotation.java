6bae6384e98c198ff71a31acd289b3a946d88708
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:37 +0100

    Runtime: 5 ms (Top 6.49%) | Memory: 42.9 MB (Top 12.04%)

--- scripts/algorithms/D/Determine Whether Matrix Can Be Obtained By Rotation/Determine Whether Matrix Can Be Obtained By Rotation.java
+++ scripts/algorithms/D/Determine Whether Matrix Can Be Obtained By Rotation/Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -1,6 +1,7 @@
-class Solution {  
+// Runtime: 5 ms (Top 6.49%) | Memory: 42.9 MB (Top 12.04%)
+class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-		if (mat == target)  return true;
+        if (mat == target) return true;
         int n = mat.length;
         int[] res[] = new int[n][n];
         for (int i = 0; i < n; i++) { //clockwise 90
@@ -8,23 +9,23 @@ class Solution {
                 res[i][j] = mat[n - 1 - j][i];
             }
         }
-        
+
         int[] res2[] = new int[n][n];
         for (int i = 0; i < n; i++) { //clockwise 180
             for (int j = 0; j < n; j++) {
                 res2[i][j] = res[n - 1 - j][i];
             }
         }
-       
+
         int[] res3[] = new int[n][n];
         for (int i = 0; i < n; i++) { //clockwise 270
             for (int j = 0; j < n; j++) {
                 res3[i][j] = res2[n - 1 - j][i];
             }
         }
-        
-		//compare to 90,180,270 and itself
-        if(Arrays.deepEquals(target, res) || Arrays.deepEquals(target, res2) || Arrays.deepEquals(target, res3) || Arrays.deepEquals(target, mat) ){ 
+
+        //compare to 90,180,270 and itself
+        if(Arrays.deepEquals(target, res) || Arrays.deepEquals(target, res2) || Arrays.deepEquals(target, res3) || Arrays.deepEquals(target, mat) ){
             return true;
         }
         return false;
