c5b77889b02601daf979e5b1e9f9442122909463
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:33 +0100

    Runtime: 669 ms (Top 21.17%) | Memory: 13.8 MB (Top 63.98%)

--- scripts/algorithms/S/Sum of Square Numbers/Sum of Square Numbers.py
+++ scripts/algorithms/S/Sum of Square Numbers/Sum of Square Numbers.py
@@ -1,16 +1,17 @@
+# Runtime: 669 ms (Top 21.17%) | Memory: 13.8 MB (Top 63.98%)
 import math
 
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        
+
         a = 0
-        
+
         while a ** 2 <= c:
             b = math.sqrt(c - a ** 2)
-            
+
             if b.is_integer():
                 return True
-            
+
             a += 1
-        
+
         return False
