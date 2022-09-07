8157c83c21c015a90ae6a98e7f1dd8c0d4fe7024
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:32 +0100

    Runtime: 119 ms (Top 50.88%) | Memory: 45 MB (Top 5.70%)

--- scripts/algorithms/D/Divide Two Integers/Divide Two Integers.js
+++ scripts/algorithms/D/Divide Two Integers/Divide Two Integers.js
@@ -1,3 +1,4 @@
+// Runtime: 119 ms (Top 50.88%) | Memory: 45 MB (Top 5.70%)
 var divide = function(dividend, divisor) {
     if (dividend == -2147483648 && divisor == -1) return 2147483647;
     let subdividend = Math.abs(dividend)
@@ -15,4 +16,4 @@ var divide = function(dividend, divisor) {
     }
     if((dividend >0 && divisor <0) || (dividend <0 && divisor >0)) return 0-ans
     return ans
-};  
+};
