c0f43d1e3fcd4c448d4cc5d3f574d3a2540b258f
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:20 +0100

    Runtime: 408 ms (Top 21.69%) | Memory: 15.6 MB (Top 65.11%)

--- scripts/algorithms/M/Move Zeroes/Move Zeroes.py
+++ scripts/algorithms/M/Move Zeroes/Move Zeroes.py
@@ -1,12 +1,13 @@
+# Runtime: 408 ms (Top 21.69%) | Memory: 15.6 MB (Top 65.11%)
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        
+
         """
         notzero = 0
         zero = 0
-        
+
         while notzero < len(nums):
             if nums[notzero] != 0:
                 nums[zero] , nums[notzero] = nums[notzero], nums[zero]
