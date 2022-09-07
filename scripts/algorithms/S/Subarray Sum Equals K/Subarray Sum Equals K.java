9f952adad2079d87f1ed70521717849b6a8a7e83
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:52 +0100

    Runtime: 80 ms (Top 15.87%) | Memory: 68.2 MB (Top 25.50%)

--- scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.java
+++ scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.java
@@ -1,15 +1,16 @@
+// Runtime: 80 ms (Top 15.87%) | Memory: 68.2 MB (Top 25.50%)
 /*
 Runtime: 21 ms, faster than 98.97% of Java online submissions for Subarray Sum Equals K.
 Memory Usage: 47.1 MB, less than 85.93% of Java online submissions for Subarray Sum Equals K.
 */
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        
+
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0,1);
         int count = 0;
         int sum = 0;
-        
+
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
             if(map.containsKey(sum - k)){
