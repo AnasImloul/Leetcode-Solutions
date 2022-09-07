aeaa15c28530806548cd31d2a91bc2382876a50e
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:18 +0100

    Runtime: 64 ms (Top 18.00%) | Memory: 13.8 MB (Top 71.33%)

--- scripts/algorithms/P/Path In Zigzag Labelled Binary Tree/Path In Zigzag Labelled Binary Tree.py
+++ scripts/algorithms/P/Path In Zigzag Labelled Binary Tree/Path In Zigzag Labelled Binary Tree.py
@@ -1,25 +1,26 @@
+# Runtime: 64 ms (Top 18.00%) | Memory: 13.8 MB (Top 71.33%)
 class Solution:
     def pathInZigZagTree(self, label: int) -> List[int]:
         result = [label]
-        
-        #determine level of label 
+
+        #determine level of label
         #O(log n)
         level, nodeCount = 1, 1
         while label >= nodeCount * 2:
             nodeCount *= 2
             level += 1
-        
-        #Olog(n) time 
+
+        #Olog(n) time
         while label > 1:
             #compute max and min node
             maxNode, minNode = 2**level-1, 2**(level-1)
-            
+
             parent = ((maxNode + minNode) - label) // 2
-            
+
             #slightly costly operation
-            result = [parent] + result 
-            
+            result = [parent] + result
+
             label = parent
             level -= 1
-            
-        return result 
+
+        return result
