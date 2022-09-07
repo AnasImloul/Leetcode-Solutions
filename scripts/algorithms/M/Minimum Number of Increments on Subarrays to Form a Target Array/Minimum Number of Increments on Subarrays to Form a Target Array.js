d0e37037ccf23b99a00c1e672e064eeac3422e8f
AnasImloul <pydev27@gmail.com>
2022-09-07 19:48:00 +0100

    Runtime: 140 ms (Top 40.00%) | Memory: 50.8 MB (Top 90.00%)

--- scripts/algorithms/M/Minimum Number of Increments on Subarrays to Form a Target Array/Minimum Number of Increments on Subarrays to Form a Target Array.js
+++ scripts/algorithms/M/Minimum Number of Increments on Subarrays to Form a Target Array/Minimum Number of Increments on Subarrays to Form a Target Array.js
@@ -1,10 +1,11 @@
+// Runtime: 140 ms (Top 40.00%) | Memory: 50.8 MB (Top 90.00%)
 var minNumberOperations = function(target) {
     let res = target[0];
     for(let g=1; g<target.length; g++){
-        if(target[g] > target[g-1]){ 
+        if(target[g] > target[g-1]){
             res = res + (target[g] - target[g-1]);
         }
-        // for target[g] < target[g-1] we need not worry as its already been taken care by previous iterations 
+        // for target[g] < target[g-1] we need not worry as its already been taken care by previous iterations
     }
     return res;
 };
