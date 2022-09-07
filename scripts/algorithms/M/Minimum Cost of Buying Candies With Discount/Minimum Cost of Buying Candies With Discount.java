fb916f4fc7f78b3789e2976482aeba82519a9c40
AnasImloul <pydev27@gmail.com>
2022-09-07 18:38:40 +0100

    Runtime: 3 ms (Top 83.40%) | Memory: 42.5 MB (Top 73.57%)

--- scripts/algorithms/M/Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.java
+++ scripts/algorithms/M/Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.java
@@ -1,10 +1,11 @@
+// Runtime: 3 ms (Top 83.40%) | Memory: 42.5 MB (Top 73.57%)
 class Solution {
     /** Algorithm
-     *  1. Sort the cost array. 
-     *  2. In a loop, start from the back and buy items n, n-1 to get n-2 for free.
-     *  3. Decrement the position by 3 and continue. stop when you reach 1.
-     *  4. From 1, add the remaining 1 or 2 items.
-     * 
+     * 1. Sort the cost array.
+     * 2. In a loop, start from the back and buy items n, n-1 to get n-2 for free.
+     * 3. Decrement the position by 3 and continue. stop when you reach 1.
+     * 4. From 1, add the remaining 1 or 2 items.
+     *
      */
     public int minimumCost(int[] cost) {
         int minCost = 0;
