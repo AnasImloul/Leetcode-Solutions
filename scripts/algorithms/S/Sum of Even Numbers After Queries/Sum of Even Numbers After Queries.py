8d161f310c4ce7f4b8af8ea2099ea901d0319d56
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:15 +0100

    Runtime: 1565 ms (Top 5.26%) | Memory: 20.6 MB (Top 46.36%)

--- scripts/algorithms/S/Sum of Even Numbers After Queries/Sum of Even Numbers After Queries.py
+++ scripts/algorithms/S/Sum of Even Numbers After Queries/Sum of Even Numbers After Queries.py
@@ -1,24 +1,21 @@
+# Runtime: 1565 ms (Top 5.26%) | Memory: 20.6 MB (Top 46.36%)
 class Solution:
-#     O(n) || O(1)
+# O(n) || O(1)
 # Runtime: 583ms 72.40% || memory: 20.5mb 37.69%
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        totalEvenNumSum = sum([num  for num in nums if num % 2 == 0])
+        totalEvenNumSum = sum([num for num in nums if num % 2 == 0])
         result = []
 
         for val, idx in queries:
             oldVal = nums[idx]
             nums[idx] += val
-            
-            
+
             if oldVal % 2 == 0:
                 totalEvenNumSum -= oldVal
-            
+
             if nums[idx] % 2 == 0:
                 totalEvenNumSum += nums[idx]
-                
-        
-            result.append(totalEvenNumSum)            
-        
+
+            result.append(totalEvenNumSum)
+
         return result
-            
-                
