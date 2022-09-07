746f13b0e0083bf0b24a2d5e7eccb5cf6b7fd27a
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:32 +0100

    Runtime: 1132 ms (Top 16.52%) | Memory: 15.2 MB (Top 81.46%)

--- scripts/algorithms/J/Jump Game/Jump Game.py
+++ scripts/algorithms/J/Jump Game/Jump Game.py
@@ -1,13 +1,14 @@
+# Runtime: 1132 ms (Top 16.52%) | Memory: 15.2 MB (Top 81.46%)
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         """
         # Memoization + DFS Solution
         # TLE as we have as much as n decisions depending on nums[i] which could be
         # 10^5 as an uppercase according to problem constraints
-        # better off with a greedy approach 
-        
+        # better off with a greedy approach
+
         cache = {} # i : bool
-        
+
         def dfs(i):
             if i == len(nums) -1:
                 return True
@@ -22,24 +23,24 @@ class Solution:
                     return cache[i]
             cache[i] = False
             return cache[i]
-        
+
         return dfs(0)
         """
         # Greedy Solution
         # Key with greedy is to find a local and gobal optimum
         # here we find the furthest distance we can travel with each index
-        
+
         # futhest index reachable
         reachable = 0
-        
+
         # iterate through all indexes and if the current index is futher than what we can travel return fasle
         for i in range(len(nums)):
             if i > reachable:
                 return False
-            
+
             reachable = max(reachable, nums[i] + i)
             # if the futherest distance we can jump to is greater or equal than the last index break
             if reachable >= len(nums) - 1:
                 break
-        
-        return True 
+
+        return True
