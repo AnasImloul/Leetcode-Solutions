a37df161b56cbb5dd8757615745a1332aec4c352
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:30 +0100

    Runtime: 4322 ms (Top 46.43%) | Memory: 18 MB (Top 47.62%)

--- scripts/algorithms/C/Count Number of Special Subsequences/Count Number of Special Subsequences.py
+++ scripts/algorithms/C/Count Number of Special Subsequences/Count Number of Special Subsequences.py
@@ -1,12 +1,13 @@
+# Runtime: 4322 ms (Top 46.43%) | Memory: 18 MB (Top 47.62%)
 class Solution:
     def countSpecialSubsequences(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
+
         last_0 = 0
         last_1 = 0
         last_2 = 0
-        
+
         for i in nums:
             if i == 0:
                 last_0 = (2*last_0 + 1)% 1000000007
