5e2e2ddcef601485af58569d841076dd4fc44673
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:36 +0100

    Runtime: 2282 ms (Top 70.42%) | Memory: 24.8 MB (Top 77.26%)

--- scripts/algorithms/L/Largest Combination With Bitwise AND Greater Than Zero/Largest Combination With Bitwise AND Greater Than Zero.py
+++ scripts/algorithms/L/Largest Combination With Bitwise AND Greater Than Zero/Largest Combination With Bitwise AND Greater Than Zero.py
@@ -1,3 +1,4 @@
+# Runtime: 2282 ms (Top 70.42%) | Memory: 24.8 MB (Top 77.26%)
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
         return max(sum(n & (1 << i) > 0 for n in candidates) for i in range(0, 24))
