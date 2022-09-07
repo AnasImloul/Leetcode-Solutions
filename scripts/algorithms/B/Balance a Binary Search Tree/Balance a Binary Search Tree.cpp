1fd8f2b376570c69d579d7e0bb7f1563a02f9eef
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:26 +0100

    Runtime: 305 ms (Top 25.60%) | Memory: 63.3 MB (Top 34.89%)

--- scripts/algorithms/B/Balance a Binary Search Tree/Balance a Binary Search Tree.cpp
+++ scripts/algorithms/B/Balance a Binary Search Tree/Balance a Binary Search Tree.cpp
@@ -1,3 +1,4 @@
+// Runtime: 305 ms (Top 25.60%) | Memory: 63.3 MB (Top 34.89%)
 class Solution {
 public:
     vector<int> nums ;
@@ -8,18 +9,18 @@ public:
         traverse(root->right) ;
         return ;
     }
-    
+
     TreeNode * makeTree(int s , int e){
         if(s > e) return nullptr ;
         int m = (s + e) / 2 ;
         TreeNode * root = new TreeNode(nums[m]) ;
-        
+
         root->left = makeTree(s,m - 1) ;
         root->right = makeTree(m + 1,e) ;
-        
+
         return root ;
     }
-    
+
     TreeNode* balanceBST(TreeNode* root) {
         traverse(root) ;
         return makeTree(0,size(nums) - 1) ;
