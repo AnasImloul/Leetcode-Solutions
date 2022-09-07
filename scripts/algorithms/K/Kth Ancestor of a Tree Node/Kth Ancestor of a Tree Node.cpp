3493a206b9f3819f57fda47b4de823b56f5cf3a2
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:15 +0100

    Runtime: 867 ms (Top 5.20%) | Memory: 119.6 MB (Top 40.27%)

--- scripts/algorithms/K/Kth Ancestor of a Tree Node/Kth Ancestor of a Tree Node.cpp
+++ scripts/algorithms/K/Kth Ancestor of a Tree Node/Kth Ancestor of a Tree Node.cpp
@@ -1,12 +1,13 @@
+// Runtime: 867 ms (Top 5.20%) | Memory: 119.6 MB (Top 40.27%)
 class TreeAncestor {
 public:
-    //go up by only powers of two 
+    //go up by only powers of two
     vector<vector<int>> lift ;
     TreeAncestor(int n, vector<int>& parent) {
         lift.resize(n,vector<int>(21,-1)) ;
-        //every node's first ancestor is parent itself 
+        //every node's first ancestor is parent itself
         for(int i = 0 ; i < n ; ++i ) lift[i][0] = parent[i] ;
-        
+
         for(int i = 0 ; i < n ; ++i ){
             for(int j = 1 ; j <= 20 ; ++j ){
                 if(lift[i][j-1] == -1) continue ;
@@ -14,9 +15,9 @@ public:
             }
         }
     }
-    
+
     int getKthAncestor(int node, int k) {
-        
+
         for(int i = 0 ; i <= 20 ; ++i ){
             if(k & (1 << i)){
                 node = lift[node][i] ;
