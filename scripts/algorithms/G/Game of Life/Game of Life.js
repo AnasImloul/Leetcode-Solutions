3649f063a73d750e54751cd327ce234f3bb0b649
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:27 +0100

    Runtime: 101 ms (Top 32.25%) | Memory: 42 MB (Top 79.17%)

--- scripts/algorithms/G/Game of Life/Game of Life.js
+++ scripts/algorithms/G/Game of Life/Game of Life.js
@@ -1,3 +1,4 @@
+// Runtime: 101 ms (Top 32.25%) | Memory: 42 MB (Top 79.17%)
 /**
  * @param {number[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
@@ -5,7 +6,7 @@
 var gameOfLife = function(board) {
     const m = board.length, n = board[0].length;
     let copy = JSON.parse(JSON.stringify(board));
-    
+
     const getNeighbor = (row, col) => {
         let radius = [-1, 0, 1], count = 0;
         for(let i = 0; i < 3; i++) {
@@ -16,17 +17,17 @@ var gameOfLife = function(board) {
                         count++;
                     }
                 }
-            } 
+            }
         }
         return count;
     }
-    
+
     for(let i = 0; i < m; i++) {
         for(let j = 0; j < n; j++) {
             const count = getNeighbor(i, j);
             if(copy[i][j] == 1) {
                 if(count < 2 || count > 3) {
-                    board[i][j] = 0;   
+                    board[i][j] = 0;
                 }
             } else {
                 if(count == 3) {
