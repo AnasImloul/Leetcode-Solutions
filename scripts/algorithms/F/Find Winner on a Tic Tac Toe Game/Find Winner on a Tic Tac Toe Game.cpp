a2f4691964ff6f1dae2da9dd14ffdbddcc708d19
AnasImloul <pydev27@gmail.com>
2022-09-08 00:39:26 +0100

    Runtime: 2 ms (Top 71.74%) | Memory: 8.3 MB (Top 83.23%)

--- scripts/algorithms/F/Find Winner on a Tic Tac Toe Game/Find Winner on a Tic Tac Toe Game.cpp
+++ scripts/algorithms/F/Find Winner on a Tic Tac Toe Game/Find Winner on a Tic Tac Toe Game.cpp
@@ -1,13 +1,14 @@
+// Runtime: 2 ms (Top 71.74%) | Memory: 8.3 MB (Top 83.23%)
 class Solution {
 public:
-    string tictactoe(vector<vector<int>>& moves) 
+    string tictactoe(vector<vector<int>>& moves)
     {
         vector<vector<char>> grid(3,vector<char>(3));
         char val='x';
         for(auto &p:moves)
         {
             grid[p[0]][p[1]]=val;
-            
+
             val=val=='x'?'o':'x';
         }
         for (int i = 0; i < 3; i++){
@@ -19,18 +20,18 @@ public:
             if (grid[0][i] == 'x' && grid[1][i] == 'x' && grid[2][i] == 'x')return "A";
             if (grid[0][i] == 'o' && grid[1][i] == 'o' && grid[2][i] == 'o')return "B";
         }
-        //check diagonal 
+        //check diagonal
         if (grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x')return "A";
         if (grid[0][2] == 'x' && grid[1][1] == 'x' && grid[2][0] == 'x')return "A";
         if (grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o')return "B";
         if (grid[0][2] == 'o' && grid[1][1] == 'o' && grid[2][0] == 'o')return "B";
-        
+
         if(moves.size()==9)
         {
             return "Draw";
         }
         return "Pending";
-        
+
     }
 };
 //if you like the solution plz upvote.
