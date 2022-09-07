05bbc5e94ae9be2016c18b16758a6543534131e4
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:30 +0100

    Runtime: 76 ms (Top 5.14%) | Memory: 13.9 MB (Top 11.51%)

--- scripts/algorithms/S/Student Attendance Record I/Student Attendance Record I.py
+++ scripts/algorithms/S/Student Attendance Record I/Student Attendance Record I.py
@@ -1,16 +1,17 @@
+# Runtime: 76 ms (Top 5.14%) | Memory: 13.9 MB (Top 11.51%)
 class Solution:
     def checkRecord(self, s: str) -> bool:
         eligible = True
-        
+
         for i in range(0, len(s)-2):
             if s[i:i+3] == "LLL":
                 eligible = False
         absent = 0
         for i in range(len(s)):
             if s[i] == "A":
-                absent +=1 
-                
+                absent +=1
+
         if absent>=2:
             eligible = False
-            
+
         return(eligible)
