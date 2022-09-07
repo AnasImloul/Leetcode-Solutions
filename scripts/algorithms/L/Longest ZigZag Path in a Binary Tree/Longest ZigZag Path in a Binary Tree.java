1c9fe36806204bf1e21b8cbb8108c20f29a3a16f
AnasImloul <pydev27@gmail.com>
2022-09-07 11:17:52 +0100

    Runtime: 8 ms (Top 84.82%) | Memory: 54.1 MB (Top 87.52%)

--- scripts/algorithms/L/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree.java
+++ scripts/algorithms/L/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree.java
@@ -1,16 +1,17 @@
+// Runtime: 8 ms (Top 84.82%) | Memory: 54.1 MB (Top 87.52%)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
@@ -23,21 +24,20 @@ class Solution {
         Pair ans=longestZigZag_(root);
         return ans.maxLen;
     }
-    
+
     public Pair longestZigZag_(TreeNode root) {
         if(root==null)
             return new Pair();
         Pair l=longestZigZag_(root.left);
         Pair r=longestZigZag_(root.right);
-    
+
         Pair myAns=new Pair();
         myAns.left=l.right+1;
         myAns.right=r.left+1;
         int max=Math.max(myAns.left,myAns.right);
         myAns.maxLen=Math.max(max,Math.max(l.maxLen,r.maxLen));
         return myAns;
-        
+
     }
-    
-    
+
 }
