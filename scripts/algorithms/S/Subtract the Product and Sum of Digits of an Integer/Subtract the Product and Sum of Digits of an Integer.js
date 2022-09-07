9eeca670663dbd92ac3cdc4f47a47fae7cd28c87
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:12 +0100

    Runtime: 76 ms (Top 70.55%) | Memory: 42 MB (Top 76.90%)

--- scripts/algorithms/S/Subtract the Product and Sum of Digits of an Integer/Subtract the Product and Sum of Digits of an Integer.js
+++ scripts/algorithms/S/Subtract the Product and Sum of Digits of an Integer/Subtract the Product and Sum of Digits of an Integer.js
@@ -1,12 +1,12 @@
+// Runtime: 76 ms (Top 70.55%) | Memory: 42 MB (Top 76.90%)
 
 var subtractProductAndSum = function(n) {
   let product=1,sum=0
   n=n.toString().split('')
   n.forEach((x)=>{
    product *=parseInt(x)
-   sum +=parseInt(x)   
+   sum +=parseInt(x)
   }
   )
     return product-sum
   };
-  
