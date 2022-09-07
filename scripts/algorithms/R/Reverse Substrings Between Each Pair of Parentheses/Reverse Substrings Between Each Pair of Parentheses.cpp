86406d336c147903cb36bef1e657ad45f7e6aacf
AnasImloul <pydev27@gmail.com>
2022-09-07 18:22:44 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.2 MB (Top 81.63%)

--- scripts/algorithms/R/Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses.cpp
+++ scripts/algorithms/R/Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses.cpp
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.2 MB (Top 81.63%)
 class Solution {
 private:
     void revString(string& s, int start, int end) {
@@ -12,25 +13,25 @@ public:
         stack<int> startIdx;
         string ans = "";
         int n = s.length();
-        
+
         for (int i = 0; i < n; i++) {
-            
+
             if (s[i] == '(') {
                 startIdx.push(i);
             }
-            
+
             if (s[i] == ')') {
                 revString(s, startIdx.top(), i);
                 startIdx.pop();
             }
         }
-        
+
         for (auto ch : s) {
             if (ch != '(' && ch != ')') {
                 ans += ch;
             }
         }
-        
+
         return ans;
     }
 };
