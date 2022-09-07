ee2f2a743cb7d2e5e4be47f3f8c1476ebabc747d
AnasImloul <pydev27@gmail.com>
2022-09-07 21:32:12 +0100

    Runtime: 1 ms (Top 34.13%) | Memory: 42.2 MB (Top 19.29%)

--- scripts/algorithms/I/Implement Stack using Queues/Implement Stack using Queues.java
+++ scripts/algorithms/I/Implement Stack using Queues/Implement Stack using Queues.java
@@ -1,32 +1,33 @@
+// Runtime: 1 ms (Top 34.13%) | Memory: 42.2 MB (Top 19.29%)
 class MyStack {
-    
+
     Queue<Integer> queue = null;
-    
+
     public MyStack() {
-        queue =  new LinkedList<>();
+        queue = new LinkedList<>();
     }
-    
+
     public void push(int x) {
-        
-        Queue<Integer> tempQueue = new LinkedList<>();  
-        tempQueue.add(x);  
-        
+
+        Queue<Integer> tempQueue = new LinkedList<>();
+        tempQueue.add(x);
+
         while(!queue.isEmpty()){
          tempQueue.add(queue.remove());
         }
-        
+
         queue = tempQueue;
-        
+
     }
-    
+
     public int pop() {
        return queue.remove();
     }
-    
+
     public int top() {
-        return  queue.peek();
+        return queue.peek();
     }
-    
+
     public boolean empty() {
         return queue.isEmpty();
     }
