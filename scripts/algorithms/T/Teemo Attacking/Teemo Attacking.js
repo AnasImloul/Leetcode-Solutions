99cc17479c836c40b276fc215d9d0f52285a8069
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:26 +0100

    Runtime: 100 ms (Top 50.69%) | Memory: 45.4 MB (Top 78.13%)

--- scripts/algorithms/T/Teemo Attacking/Teemo Attacking.js
+++ scripts/algorithms/T/Teemo Attacking/Teemo Attacking.js
@@ -1,10 +1,11 @@
+// Runtime: 100 ms (Top 50.69%) | Memory: 45.4 MB (Top 78.13%)
 var findPoisonedDuration = function(timeSeries, duration) {
     let totalTime=duration
-    
+
     for(let i=0;i+1<timeSeries.length;i++){
         let diff=timeSeries[i+1]-timeSeries[i]
-        totalTime+= diff>duration ? duration : diff 
+        totalTime+= diff>duration ? duration : diff
     }
     return totalTime
-    
+
 };
