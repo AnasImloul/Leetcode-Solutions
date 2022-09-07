18159abf9980eb18b7d2161f9741be9ee80e58a2
AnasImloul <pydev27@gmail.com>
2022-09-08 00:10:08 +0100

    Runtime: 472 ms (Top 21.43%) | Memory: 73.8 MB (Top 80.95%)

--- scripts/algorithms/M/Minimum Number of Arrows to Burst Balloons/Minimum Number of Arrows to Burst Balloons.js
+++ scripts/algorithms/M/Minimum Number of Arrows to Burst Balloons/Minimum Number of Arrows to Burst Balloons.js
@@ -1,15 +1,16 @@
+// Runtime: 472 ms (Top 21.43%) | Memory: 73.8 MB (Top 80.95%)
 var findMinArrowShots = function(points) {
     points.sort((a, b) => a[0] - b[0])
-    
+
     let merged = [points[0]];
-    
+
     let earliestEnd = points[0][1];
-    
+
     for (let i = 1; i < points.length; i++) {
         let lastEnd = merged[merged.length - 1][1];
         let currStart = points[i][0];
         let currEnd = points[i][1];
-        
+
         if (lastEnd >= currStart && currStart <= earliestEnd) {
             merged[merged.length - 1][1] = Math.max(lastEnd, currEnd)
             earliestEnd = Math.min(earliestEnd, points[i][1]);
@@ -19,6 +20,6 @@ var findMinArrowShots = function(points) {
             earliestEnd = points[i][1]
         }
     }
-    
+
     return merged.length
 };
