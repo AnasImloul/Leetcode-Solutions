8dd81c38f621dda593054960616b7c244f5d3122
AnasImloul <pydev27@gmail.com>
2022-09-08 00:39:28 +0100

    Runtime: 560 ms (Top 5.11%) | Memory: 54 MB (Top 5.84%)

--- scripts/algorithms/N/Number of Valid Words in a Sentence/Number of Valid Words in a Sentence.cpp
+++ scripts/algorithms/N/Number of Valid Words in a Sentence/Number of Valid Words in a Sentence.cpp
@@ -1,25 +1,26 @@
+// Runtime: 560 ms (Top 5.11%) | Memory: 54 MB (Top 5.84%)
 #include <regex>
 
 class Solution {
 public:
-	int countValidWords(string sentence) {
+    int countValidWords(string sentence) {
 
-		int count = 0;
-		
-		// Defining the regex pattern
-		regex valid_word("[a-z]*([a-z]-[a-z])?[a-z]*[!,.]?");
+        int count = 0;
 
-		// splitting the sentence to words
-		stringstream s(sentence);
-		string word;
-		while(getline(s, word, ' ')) {
-		
-			// Checking if the word matches the regex pattern
-			if(word != "" && regex_match(word, valid_word)){
-				++count;
-			} 
-		}
+        // Defining the regex pattern
+        regex valid_word("[a-z]*([a-z]-[a-z])?[a-z]*[!,.]?");
 
-		return count;
-	}  
+        // splitting the sentence to words
+        stringstream s(sentence);
+        string word;
+        while(getline(s, word, ' ')) {
+
+            // Checking if the word matches the regex pattern
+            if(word != "" && regex_match(word, valid_word)){
+                ++count;
+            }
+        }
+
+        return count;
+    }
 };
