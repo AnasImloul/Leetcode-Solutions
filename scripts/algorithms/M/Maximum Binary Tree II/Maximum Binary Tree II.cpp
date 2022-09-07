2b840f92a92bafc0f05ad2a35b3df838170cbfb1
AnasImloul <pydev27@gmail.com>
2022-09-08 00:39:29 +0100

    Runtime: 13 ms (Top 20.00%) | Memory: 13.5 MB (Top 62.44%)

--- scripts/algorithms/M/Maximum Binary Tree II/Maximum Binary Tree II.cpp
+++ scripts/algorithms/M/Maximum Binary Tree II/Maximum Binary Tree II.cpp
@@ -1,45 +1,42 @@
+// Runtime: 13 ms (Top 20.00%) | Memory: 13.5 MB (Top 62.44%)
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    
+
     void insertIntoMaxTreeRec(TreeNode* root,TreeNode* newNode)//o(logn)
     {
-       
+
         if(!root)
             return;
         if(root->right&&root->right->val<newNode->val)
         {
             newNode->left=root->right;
             root->right=newNode;
-           
+
             return;
-            
+
         }
         if(!root->right)
         {root->right=newNode;
-        
+
         return;}
-        
+
          insertIntoMaxTreeRec( root->right, newNode);
-        
-        
-        
-        
-        
+
     }
     TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
-         TreeNode* curr=new  TreeNode(val);
-        
+         TreeNode* curr=new TreeNode(val);
+
         if(root->val<val)
         {
             curr->left=root;
@@ -47,10 +44,9 @@ public:
         }else
         {
               insertIntoMaxTreeRec( root, curr);
-        
-        
+
         }
         return root;
-        
+
     }
 };
