1bc03bab257c87250895fc03c9c37dcab48853bf
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:48 +0100

    Runtime: 3 ms (Top 51.95%) | Memory: 40.9 MB (Top 44.38%)

--- scripts/algorithms/P/Perfect Number/Perfect Number.java
+++ scripts/algorithms/P/Perfect Number/Perfect Number.java
@@ -1,12 +1,13 @@
+// Runtime: 3 ms (Top 51.95%) | Memory: 40.9 MB (Top 44.38%)
 class Solution {
     public boolean checkPerfectNumber(int num) {
         if(num==1)
         return false;
-        
+
         int sum = 1;
         for(int i=2; i<Math.sqrt(num); i++){
             if(num%i==0){
-            sum += i + num / i;    
+            sum += i + num / i;
             }
         }
         if(sum==num){
@@ -17,4 +18,3 @@ class Solution {
         }
     }
 }
-
