9c4402922840f47d6b51ca96526fba77e43ad10b
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:17 +0100

    Runtime: 13 ms (Top 80.40%) | Memory: 5.8 MB (Top 81.91%)

--- scripts/algorithms/P/Prime Palindrome/Prime Palindrome.cpp
+++ scripts/algorithms/P/Prime Palindrome/Prime Palindrome.cpp
@@ -1,5 +1,6 @@
+// Runtime: 13 ms (Top 80.40%) | Memory: 5.8 MB (Top 81.91%)
 class Solution {
-    
+
 public:
     bool isPrime(int N) {
     if (N < 2) return false;
@@ -24,7 +25,7 @@ public:
             if (n == reverse(n) && isPrime(n))
                 return n;
             n++;
-            
+
             // Any even length palindrome must be divisble by 11
             // so we will skip numbers N = [10,000,000, 99,999,999]
             if (10000000 < n && n < 100000000)
