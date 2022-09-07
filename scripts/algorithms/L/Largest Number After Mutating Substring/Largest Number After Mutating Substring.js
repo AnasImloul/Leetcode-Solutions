d45a4fd8271111df9c5ac685d57c24c2bc106749
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:17 +0100

    Runtime: 263 ms (Top 13.33%) | Memory: 66.8 MB (Top 46.67%)

--- scripts/algorithms/L/Largest Number After Mutating Substring/Largest Number After Mutating Substring.js
+++ scripts/algorithms/L/Largest Number After Mutating Substring/Largest Number After Mutating Substring.js
@@ -1,12 +1,13 @@
+// Runtime: 263 ms (Top 13.33%) | Memory: 66.8 MB (Top 46.67%)
 var maximumNumber = function(num, change) {
     const digits = num.split("");
-    
+
     let started = false;
-    
+
     for (let i = 0; i < digits.length; ++i) {
         const origDig = digits[i];
         const changeDig = change[origDig];
-        
+
         if (changeDig > origDig) {
             started = true;
             digits[i] = changeDig;
@@ -15,6 +16,6 @@ var maximumNumber = function(num, change) {
             break;
         }
     }
-    
-    return digits.join(""); 
+
+    return digits.join("");
 };
