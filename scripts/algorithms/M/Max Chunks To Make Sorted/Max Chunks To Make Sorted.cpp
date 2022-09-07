56f4e2ba6852dc3da40d1ec3980f11a573865a8e
AnasImloul <pydev27@gmail.com>
2022-09-07 21:35:40 +0100

    Runtime: 6 ms (Top 11.71%) | Memory: 7.3 MB (Top 29.81%)

--- scripts/algorithms/M/Max Chunks To Make Sorted/Max Chunks To Make Sorted.cpp
+++ scripts/algorithms/M/Max Chunks To Make Sorted/Max Chunks To Make Sorted.cpp
@@ -1,23 +1,23 @@
+// Runtime: 6 ms (Top 11.71%) | Memory: 7.3 MB (Top 29.81%)
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         // using chaining technique
         int maxi=INT_MIN;
         int ans =0;
-        
+
         for(int i=0;i<size(arr);i++)
-            
+
         {
             maxi = max(maxi,arr[i]);
-            
+
             if(maxi==i)
             {
                 //.. found partition
                 ans++;
-                
+
             }
         }
         return ans;
     }
 };
-
