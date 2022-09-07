a40ddb58a67ea99e31cd50b79be22287512a38be
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:20 +0100

    Runtime: 5 ms (Top 35.84%) | Memory: 7 MB (Top 15.75%)

--- scripts/algorithms/D/Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.cpp
+++ scripts/algorithms/D/Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.cpp
@@ -1,3 +1,4 @@
+// Runtime: 5 ms (Top 35.84%) | Memory: 7 MB (Top 15.75%)
 class Solution {
 public:
     vector<string> divideString(string s, int k, char fill) {
@@ -5,17 +6,17 @@ public:
         for(int i=0;i<s.size();i=i+k)
         {
             string t=s.substr(i,k); // make substring of size atmost k
-           
-            if(t.size()==k)  // if size if k then push
+
+            if(t.size()==k) // if size if k then push
             {
                 v.push_back(t);
                 continue;
             }
-			
-            int l=t.size();  // if it is the last group and size if less than k
-            for(int j=0;j<(k-l);j++)  // add fill char to t to make size k
+
+            int l=t.size(); // if it is the last group and size if less than k
+            for(int j=0;j<(k-l);j++) // add fill char to t to make size k
                 t+=fill;
-				
+
             v.push_back(t);
         }
         return v;
