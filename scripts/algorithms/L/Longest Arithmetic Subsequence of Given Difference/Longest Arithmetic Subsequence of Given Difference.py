26fd9936f8b932547afe3e5f86c1c9f1730df821
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:27 +0100

    Runtime: 548 ms (Top 98.07%) | Memory: 27.7 MB (Top 73.31%)

--- scripts/algorithms/L/Longest Arithmetic Subsequence of Given Difference/Longest Arithmetic Subsequence of Given Difference.py
+++ scripts/algorithms/L/Longest Arithmetic Subsequence of Given Difference/Longest Arithmetic Subsequence of Given Difference.py
@@ -1,10 +1,10 @@
+# Runtime: 548 ms (Top 98.07%) | Memory: 27.7 MB (Top 73.31%)
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         d = defaultdict(int)
         for num in arr:
             if num - difference in d:
-                d[num] =  d[num - difference] + 1
+                d[num] = d[num - difference] + 1
             else:
                 d[num] = 1
         return max((d[x] for x in d))
-    
