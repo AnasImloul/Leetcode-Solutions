59713699ee969d62fc3d86da5ce1e7fe8a67dfd0
AnasImloul <pydev27@gmail.com>
2022-09-07 21:32:11 +0100

    Runtime: 24 ms (Top 22.67%) | Memory: 61.2 MB (Top 10.43%)

--- scripts/algorithms/L/Longest Increasing Path in a Matrix/Longest Increasing Path in a Matrix.java
+++ scripts/algorithms/L/Longest Increasing Path in a Matrix/Longest Increasing Path in a Matrix.java
@@ -1,3 +1,4 @@
+// Runtime: 24 ms (Top 22.67%) | Memory: 61.2 MB (Top 10.43%)
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int[][] memo = new int[matrix.length][matrix[0].length];
@@ -10,9 +11,9 @@ class Solution {
         }
         return longestPath;
     }
-    
+
     private void dfs(int[][] matrix, int i, int j, int[][] memo) {
-		if (memo[i][j] != 0)
+        if (memo[i][j] != 0)
             return;
         int[][] dirs = {{-1,0}, {0,1}, {1,0}, {0,-1}};
         int max = 0;
@@ -29,7 +30,7 @@ class Solution {
         }
         memo[i][j] = 1 + max;
     }
-    
+
     private boolean isValid(int[][] matrix, int i, int j) {
         if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length)
             return false;
