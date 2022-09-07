8d457863dbfdf48f0d584f672266df82a724ad4f
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:16 +0100

    Runtime: 53 ms (Top 99.20%) | Memory: 42 MB (Top 50.52%)

--- scripts/algorithms/A/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.js
+++ scripts/algorithms/A/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.js
@@ -1,3 +1,4 @@
+// Runtime: 53 ms (Top 99.20%) | Memory: 42 MB (Top 50.52%)
 var average = function(salary) {
     let max = salary[0], min = salary[salary.length-1], sum = 0;
     for(let i = 0; i < salary.length; i++) {
