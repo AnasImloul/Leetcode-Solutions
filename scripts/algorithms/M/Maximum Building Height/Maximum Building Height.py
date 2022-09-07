24e2de4e3cf9fa2e2052ceae2c843c61aa198b29
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:28 +0100

    Runtime: 4560 ms (Top 6.25%) | Memory: 52.1 MB (Top 56.25%)

--- scripts/algorithms/M/Maximum Building Height/Maximum Building Height.py
+++ scripts/algorithms/M/Maximum Building Height/Maximum Building Height.py
@@ -1,3 +1,4 @@
+# Runtime: 4560 ms (Top 6.25%) | Memory: 52.1 MB (Top 56.25%)
 class Solution:
     def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
         arr = restrictions
@@ -10,7 +11,7 @@ class Solution:
             arr[i][1] = min(arr[i][1], arr[i+1][1]+arr[i+1][0]-arr[i][0])
         res = 0
         for i in range(1,n):
-			#position where height can be the highest between arr[i-1][0] and arr[i][0]
+            #position where height can be the highest between arr[i-1][0] and arr[i][0]
             k = (arr[i][1]-arr[i-1][1]+arr[i][0]+arr[i-1][0])//2
             res = max(res, arr[i-1][1]+k-arr[i-1][0])
         return res
