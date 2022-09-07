d6849cf5ed1553b2adb23ecd9eb34f3a54b0081c
AnasImloul <pydev27@gmail.com>
2022-09-07 21:32:21 +0100

    Runtime: 996 ms (Top 5.13%) | Memory: 131.3 MB (Top 81.29%)

--- scripts/algorithms/S/Sequentially Ordinal Rank Tracker/Sequentially Ordinal Rank Tracker.java
+++ scripts/algorithms/S/Sequentially Ordinal Rank Tracker/Sequentially Ordinal Rank Tracker.java
@@ -1,8 +1,9 @@
+// Runtime: 996 ms (Top 5.13%) | Memory: 131.3 MB (Top 81.29%)
 class SORTracker {
 
     private TreeMap<Integer, List<String>> map;
     private int queryNum;
-    
+
     // Find suitable position for name in the list
     private int getIndex(String name, List<String> list) {
         int l = 0, r = list.size() - 1, m = 0;
@@ -16,19 +17,19 @@ class SORTracker {
         }
         return name.compareTo(list.get(l)) > 0 ? l+1 : l;
     }
-    
+
     public SORTracker() {
         map = new TreeMap<>((a,b) -> (b-a));
         queryNum = 0;
     }
-    
+
     public void add(String name, int score) {
         List<String> list = map.getOrDefault(score, new ArrayList<>());
         int index = (list.size() == 0) ? 0 : getIndex(name, list);
         list.add(index, name);
         map.put(score, list);
     }
-    
+
     public String get() {
         int index = queryNum;
         for (int score: map.keySet()) {
