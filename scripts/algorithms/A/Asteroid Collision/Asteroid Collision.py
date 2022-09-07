f6536164d387107f70bfb08ed6580ceea7464576
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:31 +0100

    Runtime: 217 ms (Top 12.45%) | Memory: 15.1 MB (Top 94.18%)

--- scripts/algorithms/A/Asteroid Collision/Asteroid Collision.py
+++ scripts/algorithms/A/Asteroid Collision/Asteroid Collision.py
@@ -1,20 +1,21 @@
+# Runtime: 217 ms (Top 12.45%) | Memory: 15.1 MB (Top 94.18%)
 class Solution:
-	def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
 
-		res = []
+        res = []
 
-		for asteroid in asteroids:
+        for asteroid in asteroids:
 
-			while len(res) and asteroid < 0 and res[-1] > 0:
+            while len(res) and asteroid < 0 and res[-1] > 0:
 
-				if res[-1] == -asteroid: 
-					res.pop()
-					break
-				elif res[-1] < -asteroid:
-					res.pop()
-					continue
-				elif res[-1] > -asteroid:
-					break
-			else:
-				res.append(asteroid)
-		return res
+                if res[-1] == -asteroid:
+                    res.pop()
+                    break
+                elif res[-1] < -asteroid:
+                    res.pop()
+                    continue
+                elif res[-1] > -asteroid:
+                    break
+            else:
+                res.append(asteroid)
+        return res
