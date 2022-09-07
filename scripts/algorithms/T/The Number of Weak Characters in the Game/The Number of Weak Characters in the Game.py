5881d023fb3620f4ec62b9d66b1d63cbc4692490
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:53 +0100

    Runtime: 4914 ms (Top 7.28%) | Memory: 66.6 MB (Top 91.64%)

--- scripts/algorithms/T/The Number of Weak Characters in the Game/The Number of Weak Characters in the Game.py
+++ scripts/algorithms/T/The Number of Weak Characters in the Game/The Number of Weak Characters in the Game.py
@@ -1,11 +1,12 @@
+# Runtime: 4914 ms (Top 7.28%) | Memory: 66.6 MB (Top 91.64%)
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
-        
+
         properties.sort(key=lambda x: (-x[0],x[1]))
-        
+
         ans = 0
         curr_max = 0
-        
+
         for _, d in properties:
             if d < curr_max:
                 ans += 1
