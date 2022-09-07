0adddf1a654dfd332d20ef5797c514acc1dac297
AnasImloul <pydev27@gmail.com>
2022-09-07 20:03:43 +0100

    Runtime: 19 ms (Top 29.95%) | Memory: 42.8 MB (Top 82.92%)

--- scripts/algorithms/A/Add Strings/Add Strings.java
+++ scripts/algorithms/A/Add Strings/Add Strings.java
@@ -1,6 +1,7 @@
+// Runtime: 19 ms (Top 29.95%) | Memory: 42.8 MB (Top 82.92%)
 import java.math.BigInteger;
 class Solution {
-	public String addStrings(String num1, String num2) {
-		return new BigInteger(num1).add(new BigInteger(num2)).toString();        
-	}
+    public String addStrings(String num1, String num2) {
+        return new BigInteger(num1).add(new BigInteger(num2)).toString();
+    }
 }
