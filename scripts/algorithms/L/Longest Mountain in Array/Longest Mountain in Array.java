26f0d1737c3402fe9fdc630c5f33edbe381a4869
AnasImloul <pydev27@gmail.com>
2022-09-07 20:03:45 +0100

    Runtime: 3 ms (Top 81.75%) | Memory: 52.8 MB (Top 24.19%)

--- scripts/algorithms/L/Longest Mountain in Array/Longest Mountain in Array.java
+++ scripts/algorithms/L/Longest Mountain in Array/Longest Mountain in Array.java
@@ -1,3 +1,4 @@
+// Runtime: 3 ms (Top 81.75%) | Memory: 52.8 MB (Top 24.19%)
 class Solution {
     public int longestMountain(int[] arr) {
         if (arr.length < 3)
@@ -12,21 +13,21 @@ class Solution {
         }
         return max;
     }
-    
+
     public int left(int[] arr, int i) {
         int j = i - 1;
         while (j >= 0 && arr[j] < arr[j + 1])
             j--;
         return i - (j + 1);
     }
-    
+
     public int right(int[] arr, int i) {
         int j = i + 1;
         while (j < arr.length && arr[j - 1] > arr[j])
             j++;
         return j - (i + 1);
     }
-    
+
     public boolean isPeak(int[] arr, int i) {
         return i - 1 >= 0 && i + 1 < arr.length && arr[i - 1] < arr[i] && arr[i + 1] < arr[i];
     }
