7208b7a2feabce6c512689dc5452690c439080ae
AnasImloul <pydev27@gmail.com>
2022-09-07 22:44:06 +0100

    Runtime: 73 ms (Top 80.85%) | Memory: 42.2 MB (Top 59.57%)

--- scripts/algorithms/M/Minimum Moves to Convert String/Minimum Moves to Convert String.js
+++ scripts/algorithms/M/Minimum Moves to Convert String/Minimum Moves to Convert String.js
@@ -1,13 +1,14 @@
+// Runtime: 73 ms (Top 80.85%) | Memory: 42.2 MB (Top 59.57%)
 var minimumMoves = function(s) {
     let move = 0;
     let i = 0;
     while(i<s.length){
         let char = s[i];
-		// incrementing the index if we already have 'O'
+        // incrementing the index if we already have 'O'
         if(char== 'O'){
             i++;
         }
-		// incrementing the move and index by 3 (Per move =  3 characters)
+        // incrementing the move and index by 3 (Per move = 3 characters)
         if(char== 'X'){
             i=i+3;
             move++;
