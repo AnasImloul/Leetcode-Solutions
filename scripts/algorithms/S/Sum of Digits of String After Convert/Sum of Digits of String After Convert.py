a067887cab9e4e8012b9d9cb62f385e8f1737b24
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:57 +0100

    Runtime: 78 ms (Top 10.28%) | Memory: 14 MB (Top 20.56%)

--- scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.py
+++ scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.py
@@ -1,7 +1,7 @@
+# Runtime: 78 ms (Top 10.28%) | Memory: 14 MB (Top 20.56%)
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         nums = [str(ord(c) - ord('a') + 1) for c in s]
         for _ in range(k):
             nums = str(sum(int(digit) for num in nums for digit in num))
         return nums
-
