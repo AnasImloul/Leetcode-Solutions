f22e6ea724f5802d4f4e7f12635cc5d754df41f6
AnasImloul <pydev27@gmail.com>
2022-09-07 18:22:56 +0100

    Runtime: 295 ms (Top 5.06%) | Memory: 57.9 MB (Top 75.87%)

--- scripts/algorithms/M/Maximum Length of Subarray With Positive Product/Maximum Length of Subarray With Positive Product.cpp
+++ scripts/algorithms/M/Maximum Length of Subarray With Positive Product/Maximum Length of Subarray With Positive Product.cpp
@@ -1,3 +1,4 @@
+// Runtime: 295 ms (Top 5.06%) | Memory: 57.9 MB (Top 75.87%)
 class Solution {
 public:
     int getMaxLen(vector<int>& nums) {
@@ -6,7 +7,7 @@ public:
         int llen = 0, rlen = 0;
         int n = nums.size();
         for(int i = 0 ; i < n ; i++){
-            lprod *= nums[i] != 0 ? nums[i]/abs(nums[i]) : 0; 
+            lprod *= nums[i] != 0 ? nums[i]/abs(nums[i]) : 0;
             rprod *= nums[n-1-i] != 0 ? nums[n-1-i]/abs(nums[n-1-i]) : 0;
             if(lprod != 0) llen ++;
             if(rprod != 0) rlen ++;
