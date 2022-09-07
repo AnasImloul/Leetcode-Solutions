4b96dfd97f59314ccace4c6951dc2ffc6f1a2e05
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:21 +0100

    Runtime: 75 ms (Top 36.49%) | Memory: 14 MB (Top 43.32%)

--- scripts/algorithms/U/Unique Paths II/Unique Paths II.py
+++ scripts/algorithms/U/Unique Paths II/Unique Paths II.py
@@ -1,11 +1,12 @@
+# Runtime: 75 ms (Top 36.49%) | Memory: 14 MB (Top 43.32%)
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         def valid(r,c,matrix):
             return r >= 0 and c >= 0 and r < len(matrix) and c < len(matrix[0])
-        
+
         dp = [[0] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
         dp[0][0] = 1 ^ obstacleGrid[0][0]
-        
+
         for r in range(len(obstacleGrid)):
             for c in range(len(obstacleGrid[0])):
                 if obstacleGrid[r][c] != 1:
@@ -13,5 +14,5 @@ class Solution:
                         dp[r][c] += dp[r-1][c]
                     if valid(r, c-1, dp) and obstacleGrid[r][c-1] != 1:
                         dp[r][c] += dp[r][c-1]
-                        
+
         return dp[-1][-1]
