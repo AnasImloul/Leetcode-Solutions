c1836d260123e60e409bf38f0587f1f263afecdc
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:14 +0100

    Runtime: 154 ms (Top 50.21%) | Memory: 45.2 MB (Top 88.94%)

--- scripts/algorithms/L/Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.js
+++ scripts/algorithms/L/Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.js
@@ -1,3 +1,4 @@
+// Runtime: 154 ms (Top 50.21%) | Memory: 45.2 MB (Top 88.94%)
 var lengthOfLongestSubstring = function(s) {
     // keeps track of the most recent index of each letter.
     const seen = new Map();
@@ -5,7 +6,7 @@ var lengthOfLongestSubstring = function(s) {
     let start = 0;
     // keeps track of the maximum substring length.
     let maxLen = 0;
-    
+
     for(let i = 0; i < s.length; i++) {
         // if the current char was seen, move the start to (1 + the last index of this char)
         // max prevents moving backward, 'start' can only move forward
@@ -13,7 +14,7 @@ var lengthOfLongestSubstring = function(s) {
         seen.set(s[i], i);
         // maximum of the current substring length and maxLen
         maxLen = Math.max(i - start + 1, maxLen);
-    } 
-    
-    return maxLen;  
+    }
+
+    return maxLen;
 };
