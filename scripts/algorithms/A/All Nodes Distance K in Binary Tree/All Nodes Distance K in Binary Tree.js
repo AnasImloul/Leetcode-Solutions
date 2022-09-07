0565c936da8226045534597278fa4b77d1f4d23c
AnasImloul <pydev27@gmail.com>
2022-09-07 19:48:02 +0100

    Runtime: 139 ms (Top 9.47%) | Memory: 44.8 MB (Top 45.88%)

--- scripts/algorithms/A/All Nodes Distance K in Binary Tree/All Nodes Distance K in Binary Tree.js
+++ scripts/algorithms/A/All Nodes Distance K in Binary Tree/All Nodes Distance K in Binary Tree.js
@@ -1,8 +1,9 @@
+// Runtime: 139 ms (Top 9.47%) | Memory: 44.8 MB (Top 45.88%)
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * this.val = val;
+ * this.left = this.right = null;
  * }
  */
 /**
@@ -14,57 +15,56 @@
 var distanceK = function(root, target, k) {
     if(k === 0)
         return [target.val];
-    
+
     let res = [];
-    
+
     const helper = (node, k, bool) => {
         if(node === null)
             return [false, 0];
-        
+
         if(k === 0 && node.val !== target.val) {
             res.push(node.val);
             return null;
         }
-        
+
         if(bool) {
             helper(node.left, k - 1, bool);
             helper(node.right, k - 1, bool);
             return null;
         }
-        
+
         if(node.val === target.val) {
-		   //Here nodes of k distance are at bottom
+           //Here nodes of k distance are at bottom
             helper(node.left, k - 1, true);
             helper(node.right, k - 1, true);
             return [true, 1];
         } else {
-		   
+
             let [l, d] = helper(node.left, k, false);
-            
-			 //Found target node on left side, now get nodes of k distance from right side as well
+
+             //Found target node on left side, now get nodes of k distance from right side as well
             if(l === true) {
                 if(k - d === 0) //for current node
                     res.push(node.val);
                 helper(node.right, k - d - 1, true);
                 return [true, d + 1];
-            } 
-            
+            }
+
             let [r, d1] = helper(node.right, k, false);
-                
+
             if(r === true) {
                 if(k - d1 === 0)
                     res.push(node.val);
                 helper(node.left, k - d1 - 1, true);
                 return [true, d1 + 1];
-            } 
-            
+            }
+
             return [false, 0];
         }
-        
+
     }
-    
+
     helper(root, k, false);
-    
+
     return res;
 };
-
