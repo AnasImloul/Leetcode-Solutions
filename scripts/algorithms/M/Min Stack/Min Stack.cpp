ed4a06aff5411549ba48a01801fa1e56791d38d1
AnasImloul <pydev27@gmail.com>
2022-09-07 20:09:57 +0100

    Runtime: 29 ms (Top 73.41%) | Memory: 16.5 MB (Top 10.27%)

--- scripts/algorithms/M/Min Stack/Min Stack.cpp
+++ scripts/algorithms/M/Min Stack/Min Stack.cpp
@@ -1,16 +1,17 @@
+// Runtime: 29 ms (Top 73.41%) | Memory: 16.5 MB (Top 10.27%)
 class MinStack {
 public:
     stack<int> stk;
     stack<int> minstk;
-    
+
     MinStack() {
       //leave empty because we don't need to initialise an object while it being created
     }
-    
+
     void push(int val) {
-		//---------------Push in original stack------------------------------
+        //---------------Push in original stack------------------------------
         stk.push(val);
-		//-------Put always minimum element to MIN stack--------------
+        //-------Put always minimum element to MIN stack--------------
         if(minstk.size()==0)
             minstk.push(val);
         else{
@@ -20,18 +21,17 @@ public:
                 minstk.push(val);
         }
     }
-    
+
     void pop() {
         stk.pop();
         minstk.pop();
     }
-    
+
     int top() {
         return stk.top();
     }
-    
+
     int getMin() {
         return minstk.top();
     }
 };
-
