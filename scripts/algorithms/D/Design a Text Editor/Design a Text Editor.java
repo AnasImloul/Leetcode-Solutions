60aece392f8f15e01bd588fea78aec2f3e41acc1
AnasImloul <pydev27@gmail.com>
2022-09-07 20:03:44 +0100

    Runtime: 380 ms (Top 41.73%) | Memory: 138.9 MB (Top 59.95%)

--- scripts/algorithms/D/Design a Text Editor/Design a Text Editor.java
+++ scripts/algorithms/D/Design a Text Editor/Design a Text Editor.java
@@ -1,16 +1,17 @@
+// Runtime: 380 ms (Top 41.73%) | Memory: 138.9 MB (Top 59.95%)
 class TextEditor {
     StringBuilder res;
     int pos=0;
-    
+
     public TextEditor() {
         res = new StringBuilder();
     }
-    
+
     public void addText(String text) {
         res.insert(pos,text);
         pos += text.length();
     }
-    
+
     public int deleteText(int k) {
         int tmp = pos;
         pos -= k;
@@ -18,14 +19,14 @@ class TextEditor {
         res.delete(pos,tmp);
         return tmp-pos;
     }
-    
+
     public String cursorLeft(int k) {
         pos-=k;
         if(pos<0) pos = 0;
         if(pos<10) return res.substring(0,pos);
         return res.substring(pos-10,pos);
     }
-    
+
     public String cursorRight(int k) {
         pos+=k;
         if(pos>res.length()) pos = res.length();
@@ -33,4 +34,3 @@ class TextEditor {
         return res.substring(pos-10,pos);
     }
 }
-
