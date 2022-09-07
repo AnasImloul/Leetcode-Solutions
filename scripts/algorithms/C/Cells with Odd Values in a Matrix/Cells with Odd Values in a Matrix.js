209ba5657b1c0cfbda3d19236e4781a6e7bd82b7
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:32 +0100

    Runtime: 131 ms (Top 21.83%) | Memory: 44.6 MB (Top 30.28%)

--- scripts/algorithms/C/Cells with Odd Values in a Matrix/Cells with Odd Values in a Matrix.js
+++ scripts/algorithms/C/Cells with Odd Values in a Matrix/Cells with Odd Values in a Matrix.js
@@ -1,16 +1,17 @@
+// Runtime: 131 ms (Top 21.83%) | Memory: 44.6 MB (Top 30.28%)
 var oddCells = function(m, n, indices) {
     const matrix = Array.from(Array(m), () => Array(n).fill(0));
-    
+
     let res = 0;
     for (const [r, c] of indices) {
         for (let i = 0; i < n; i++) {
             // toggle 0/1 for even/odd
-			// another method: matrix[r][i] = 1 - matrix[r][i]
-			//             or: matrix[r][i] = +!matrix[r][i]
+            // another method: matrix[r][i] = 1 - matrix[r][i]
+            // or: matrix[r][i] = +!matrix[r][i]
             matrix[r][i] ^= 1;
             if (matrix[r][i]) res++; else res--;
         }
-		
+
         for (let i = 0; i < m; i++) {
             matrix[i][c] ^= 1;
             if (matrix[i][c]) res++; else res--;
