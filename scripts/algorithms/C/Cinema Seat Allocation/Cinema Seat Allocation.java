6b7f7bbab9c5235d1ce5042561f86f117001021b
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:49 +0100

    Runtime: 37 ms (Top 66.38%) | Memory: 70.8 MB (Top 57.25%)

--- scripts/algorithms/C/Cinema Seat Allocation/Cinema Seat Allocation.java
+++ scripts/algorithms/C/Cinema Seat Allocation/Cinema Seat Allocation.java
@@ -1,13 +1,14 @@
+// Runtime: 37 ms (Top 66.38%) | Memory: 70.8 MB (Top 57.25%)
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         Map<Integer, int[]> seats = new HashMap<>();
         int availableSlots = 2 * n; // max available slots since each empty row could fit at max 2 slots
-        
+
         for (int[] seat: reservedSeats) {
             int row = seat[0];
             int col = seat[1];
             int[] slots = seats.getOrDefault(row, new int[3]);
-            
+
             if (col >= 2 && col <= 5) { // left slot
                 slots[0] = 1;
             }
@@ -17,13 +18,13 @@ class Solution {
             if (col >= 6 && col <= 9) { // right slot
                 slots[2] = 1;
             }
-            
+
             seats.put(seat[0], slots);
         }
-        
+
         for (int[] slots: seats.values()) {
             int taken = slots[0] + slots[2];
-            
+
             if (taken == 2) { // both slots at either ends are taken
                 if (slots[1] == 0) { // check if middle slot not taken
                     availableSlots--; // reduce availableslots by 1 since middle slot is available
@@ -36,7 +37,7 @@ class Solution {
                 continue; // entire row is available - no need to reduce the available slots
             }
         }
-        
+
         return availableSlots;
     }
 }
