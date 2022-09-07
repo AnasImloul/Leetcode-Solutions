f88711c3cfe017c7487d16b6c6877c3fe7d89bb0
AnasImloul <pydev27@gmail.com>
2022-09-07 21:32:15 +0100

    Runtime: 2 ms (Top 77.73%) | Memory: 42.8 MB (Top 23.55%)

--- scripts/algorithms/M/Minimum Number of Operations to Convert Time/Minimum Number of Operations to Convert Time.java
+++ scripts/algorithms/M/Minimum Number of Operations to Convert Time/Minimum Number of Operations to Convert Time.java
@@ -1,9 +1,10 @@
+// Runtime: 2 ms (Top 77.73%) | Memory: 42.8 MB (Top 23.55%)
 class Solution {
-    public int  HHMMToMinutes(String s){
+    public int HHMMToMinutes(String s){
         return Integer.parseInt(s.substring(0,2))*60 + Integer.parseInt(s.substring(3,5)) ;
     }
     public int convertTime(String current, String correct) {
-        int diff =  HHMMToMinutes(correct) -  HHMMToMinutes(current);
+        int diff = HHMMToMinutes(correct) - HHMMToMinutes(current);
         int[] order = {60,15,5,1};
         int i = 0;
         int ops = 0;
