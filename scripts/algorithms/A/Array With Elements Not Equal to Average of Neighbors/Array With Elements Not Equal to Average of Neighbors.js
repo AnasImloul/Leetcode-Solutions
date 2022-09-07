95f64d2a3117d12d3097f7e078cd2ecd58d03246
AnasImloul <pydev27@gmail.com>
2022-09-07 22:44:10 +0100

    Runtime: 671 ms (Top 72.22%) | Memory: 86.2 MB (Top 94.44%)

--- scripts/algorithms/A/Array With Elements Not Equal to Average of Neighbors/Array With Elements Not Equal to Average of Neighbors.js
+++ scripts/algorithms/A/Array With Elements Not Equal to Average of Neighbors/Array With Elements Not Equal to Average of Neighbors.js
@@ -1,7 +1,8 @@
+// Runtime: 671 ms (Top 72.22%) | Memory: 86.2 MB (Top 94.44%)
 var rearrangeArray = function(nums) {
     let arr=[], res=[]
     for(let q of nums) arr[q]= 1
-    
+
     let l=0, r=arr.length-1
     while(l<=r){
         while(!arr[l])l++
