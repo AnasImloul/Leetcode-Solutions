f1ab7c5f1ff7105b8896c3c17d43dbee15ba4dc3
AnasImloul <pydev27@gmail.com>
2022-09-07 18:43:48 +0100

    Runtime: 4533 ms (Top 17.14%) | Memory: 13.9 MB (Top 48.57%)

--- scripts/algorithms/M/Maximum Number of Achievable Transfer Requests/Maximum Number of Achievable Transfer Requests.py
+++ scripts/algorithms/M/Maximum Number of Achievable Transfer Requests/Maximum Number of Achievable Transfer Requests.py
@@ -1,15 +1,16 @@
+# Runtime: 4533 ms (Top 17.14%) | Memory: 13.9 MB (Top 48.57%)
 class Solution:
     def maximumRequests(self, n: int, r: List[List[int]]) -> int:
         k=len(r)
         deg=[0 for i in range(n)]
-        
+
         def check(i,res,s):
             if i==k:
                 #print(deg,s)
                 if max(deg)==min(deg)==0:
                     res[0]=max(res[0],s)
-                return 
-            
+                return
+
             u,v=r[i]
             deg[u]-=1
             deg[v]+=1
@@ -20,8 +21,3 @@ class Solution:
         res=[0]
         check(0,res,0)
         return res[0]
-            
-            
-            
-    
-        
