bab95bbc1db3eac1810dd0bd263af90517612773
AnasImloul <pydev27@gmail.com>
2022-09-08 00:10:10 +0100

    Runtime: 377 ms (Top 95.35%) | Memory: 84 MB (Top 73.26%)

--- scripts/algorithms/F/Find All Lonely Numbers in the Array/Find All Lonely Numbers in the Array.js
+++ scripts/algorithms/F/Find All Lonely Numbers in the Array/Find All Lonely Numbers in the Array.js
@@ -1,3 +1,4 @@
+// Runtime: 377 ms (Top 95.35%) | Memory: 84 MB (Top 73.26%)
 var findLonely = function(nums) {
     let countMap = new Map();
     let result = [];
@@ -6,10 +7,10 @@ var findLonely = function(nums) {
     }
     for (let num of nums) {
         if (!countMap.has(num - 1) && !countMap.has(num + 1) && countMap.get(num) === 1) {
-            
+
             result.push(num);
         }
-        
+
     }
     return result;
 };
