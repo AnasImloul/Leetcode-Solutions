f9543ce16a3ac38cddba5c7b5281a93ead66f63e
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:23 +0100

    Runtime: 1262 ms (Top 24.62%) | Memory: 179.7 MB (Top 55.22%)

--- scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.cpp
+++ scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.cpp
@@ -1,7 +1,8 @@
+// Runtime: 1262 ms (Top 24.62%) | Memory: 179.7 MB (Top 55.22%)
 class Solution {
 public:
     vector<vector<int>>ans;
-    
+
     void dfs(vector<vector<int>>&graph , int node , int parent , vector<bool>&vis , int& ct , vector<int>&time , vector<int>&low){
         vis[node] = 1;
         time[node] = ct;
@@ -11,7 +12,7 @@ public:
             if(!vis[value]){
                 dfs(graph , value , node , vis , ct , time , low);
                 low[node] = min(low[node] , low[value]);
-                
+
                 if(low[value] > time[node]){
                     ans.push_back({node , value});
                 }
@@ -20,19 +21,19 @@ public:
             }
         }
     }
-    
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         ans.clear();
         vector<vector<int>>graph(n);
-        
+
         for(int i = 0 ; i < connections.size() ; i += 1){
             int a = connections[i][0];
             int b = connections[i][1];
-            
+
             graph[a].push_back(b);
             graph[b].push_back(a);
         }
-        
+
         vector<bool>vis(n , false);
         vector<int>time(n , -1);
         vector<int>low(n , -1);
@@ -42,7 +43,7 @@ public:
                 dfs(graph , i , -1 , vis , ct , time , low);
             }
         }
-        
+
         return ans;
     }
 };
