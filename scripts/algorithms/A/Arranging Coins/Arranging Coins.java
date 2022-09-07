1877939eb72e840072664bab3042bf395172b68b
AnasImloul <pydev27@gmail.com>
2022-09-07 20:03:46 +0100

    Runtime: 5 ms (Top 44.14%) | Memory: 40.7 MB (Top 83.77%)

--- scripts/algorithms/A/Arranging Coins/Arranging Coins.java
+++ scripts/algorithms/A/Arranging Coins/Arranging Coins.java
@@ -1,8 +1,9 @@
+// Runtime: 5 ms (Top 44.14%) | Memory: 40.7 MB (Top 83.77%)
 class Solution {
     public int arrangeCoins(int n) {
-        long s =0; long e = n;        
+        long s =0; long e = n;
         while (s <= e) {
-         long mid  = s +(e-s)/2; 
+         long mid = s +(e-s)/2;
             long coin = mid *( mid +1)/2;
             if(coin > n){
                  e = mid -1;
@@ -10,6 +11,6 @@ class Solution {
                 s = mid +1;
             } else return (int) mid;
         }
-        return (int)e;        
+        return (int)e;
     }
 }
