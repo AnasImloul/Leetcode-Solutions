8f5395ef2163212c7d30015411ff70034a9750fc
AnasImloul <pydev27@gmail.com>
2022-09-07 18:38:51 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 8.35%)

--- scripts/algorithms/W/Water Bottles/Water Bottles.java
+++ scripts/algorithms/W/Water Bottles/Water Bottles.java
@@ -1,15 +1,16 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.6 MB (Top 8.35%)
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int drinkedBottles = numBottles;
         int emptyBottles = numBottles;
-        
+
         while(emptyBottles >= numExchange){
             int gainedBottles = emptyBottles / numExchange;
-            
+
             drinkedBottles += gainedBottles;
-            
+
             int unusedEmptyBottles = emptyBottles % numExchange;
-            
+
             emptyBottles = gainedBottles + unusedEmptyBottles;
         }
         return drinkedBottles;
