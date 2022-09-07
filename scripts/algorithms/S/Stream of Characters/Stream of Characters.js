73e4336e21b9b1244c95813a3ecb8d18b6803477
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:30 +0100

    Runtime: 727 ms (Top 21.67%) | Memory: 101.6 MB (Top 6.67%)

--- scripts/algorithms/S/Stream of Characters/Stream of Characters.js
+++ scripts/algorithms/S/Stream of Characters/Stream of Characters.js
@@ -1,3 +1,4 @@
+// Runtime: 727 ms (Top 21.67%) | Memory: 101.6 MB (Top 6.67%)
 var StreamChecker = function(words) {
     function Trie() {
         this.suffixLink = null; //this is where it will fallback to when a letter can't be matched
@@ -6,8 +7,8 @@ var StreamChecker = function(words) {
     }
     this.root = new Trie();
     this.uniqueIds = 0; //used to count all the unique words discovered and as part of the Trie id system
-    
-	//standard trie traversal but keeping track of new words via id system
+
+    //standard trie traversal but keeping track of new words via id system
     for (const word of words) {
         let ptr = this.root;
         for (const c of word) {
@@ -17,46 +18,46 @@ var StreamChecker = function(words) {
             ptr = ptr.next.get(c);
         }
         if (ptr.id === -1) {
-            ptr.id = this.uniqueIds++; 
+            ptr.id = this.uniqueIds++;
         }
     }
-    
-	//BFS traversal to build the automaton
+
+    //BFS traversal to build the automaton
     const q = [];
     for (const [c, node] of this.root.next) {
-		//all first level children should point back to the root when a match to a character fails
+        //all first level children should point back to the root when a match to a character fails
         node.suffixLink = this.root;
         q.push(node);
     }
-    
+
     while (q.length) {
         const curr = q.shift();
         for (const [c, node] of curr.next) {
-            
-			let ptr = curr.suffixLink;
+
+            let ptr = curr.suffixLink;
             while (ptr !== this.root && !ptr.next.has(c)) {
                 ptr = ptr.suffixLink;
             }
-			//find the next suffixLink if it matches the current character or fallback to the root
+            //find the next suffixLink if it matches the current character or fallback to the root
             node.suffixLink = ptr.next.get(c) ?? this.root;
-			
-			//if the current suffixLink happens to also be a word we should store its id to make it quick to find
+
+            //if the current suffixLink happens to also be a word we should store its id to make it quick to find
             if (node.suffixLink.id !== -1) {
                 node.id = node.suffixLink.id;
             }
             q.push(node);
         }
     }
-	//the query ptr will now track every new streamed character and use it to match
+    //the query ptr will now track every new streamed character and use it to match
     this.queryPtr = this.root;
 };
 
 StreamChecker.prototype.query = function(letter) {
-	//the query ptr will now track every new streamed character and can be used to quickly find words
+    //the query ptr will now track every new streamed character and can be used to quickly find words
     while (this.queryPtr !== this.root && !this.queryPtr.next.has(letter)) {
         this.queryPtr = this.queryPtr.suffixLink;
     }
     this.queryPtr = this.queryPtr.next.get(letter) ?? this.root;
-	//if any word is found it will have an id that isn't -1
+    //if any word is found it will have an id that isn't -1
     return this.queryPtr.id !== -1;
 };
