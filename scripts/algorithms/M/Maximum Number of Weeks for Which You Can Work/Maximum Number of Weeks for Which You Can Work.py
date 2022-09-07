b72954cb498235a39c9d27cac7736c4d626310d9
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:25 +0100

    Runtime: 1032 ms (Top 59.60%) | Memory: 25.7 MB (Top 80.23%)

--- scripts/algorithms/M/Maximum Number of Weeks for Which You Can Work/Maximum Number of Weeks for Which You Can Work.py
+++ scripts/algorithms/M/Maximum Number of Weeks for Which You Can Work/Maximum Number of Weeks for Which You Can Work.py
@@ -1,3 +1,4 @@
+# Runtime: 1032 ms (Top 59.60%) | Memory: 25.7 MB (Top 80.23%)
 class Solution:
     def numberOfWeeks(self, m: List[int]) -> int:
         return min(sum(m), 2 * (sum(m) - max(m)) + 1)
