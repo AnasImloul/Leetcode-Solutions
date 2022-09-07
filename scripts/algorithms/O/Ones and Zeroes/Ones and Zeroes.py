12ee46e7865d808dd7866f887220b48da874c04f
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:51 +0100

    Runtime: 7928 ms (Top 10.31%) | Memory: 14.1 MB (Top 82.43%)

--- scripts/algorithms/O/Ones and Zeroes/Ones and Zeroes.py
+++ scripts/algorithms/O/Ones and Zeroes/Ones and Zeroes.py
@@ -1,3 +1,4 @@
+# Runtime: 7928 ms (Top 10.31%) | Memory: 14.1 MB (Top 82.43%)
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         dp = [[0 for _ in range(m+1)] for _ in range(n + 1)]
