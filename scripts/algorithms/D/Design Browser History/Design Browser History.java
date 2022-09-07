b91ae150382115d76dc70b261ed291952847120e
AnasImloul <pydev27@gmail.com>
2022-09-07 18:39:01 +0100

    Runtime: 122 ms (Top 39.97%) | Memory: 81.7 MB (Top 80.48%)

--- scripts/algorithms/D/Design Browser History/Design Browser History.java
+++ scripts/algorithms/D/Design Browser History/Design Browser History.java
@@ -1,12 +1,13 @@
+// Runtime: 122 ms (Top 39.97%) | Memory: 81.7 MB (Top 80.48%)
 class BrowserHistory {
     int current;
     ArrayList<String> history;
-    public BrowserHistory(String homepage) {    
+    public BrowserHistory(String homepage) {
         this.history = new ArrayList<>();
         history.add(homepage);
-		this.current = 0;
+        this.current = 0;
     }
-    
+
     public void visit(String url) {
         while (history.size()-1 > current) {//delete forward history
             history.remove(history.size()-1);//which means delete everything beyond our current website
@@ -14,13 +15,13 @@ class BrowserHistory {
         history.add(url);
         ++current;
     }
-    
+
     public String back(int steps) {
         if (steps>current) current = 0;//if we can't get enough back, we return first thing in our history
         else current -= steps;//if there will be no arrayindexoutofrange error, go back
         return history.get(current);//return current webpage
     }
-    
+
     public String forward(int steps) {
         //if we are going to move more than our arraylist, then we will return the last element
         if (steps+current>=history.size()) current = history.size() - 1;
