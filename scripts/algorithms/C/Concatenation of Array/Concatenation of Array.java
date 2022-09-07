191f8dcfe997d7b5c73dc1151231f813c905eb2a
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:50 +0100

    Runtime: 6 ms (Top 7.82%) | Memory: 50.1 MB (Top 50.44%)

--- scripts/algorithms/C/Concatenation of Array/Concatenation of Array.java
+++ scripts/algorithms/C/Concatenation of Array/Concatenation of Array.java
@@ -1,10 +1,11 @@
+// Runtime: 6 ms (Top 7.82%) | Memory: 50.1 MB (Top 50.44%)
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int[] ans = new int[2 * nums.length];
         for(int i = 0; i < nums.length; i++){
             ans[i] = ans[i + nums.length] = nums[i];
         }
-        
+
         return ans;
     }
 }
