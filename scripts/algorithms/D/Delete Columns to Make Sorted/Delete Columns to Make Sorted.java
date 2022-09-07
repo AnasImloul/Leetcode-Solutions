1f97c66a960c0f7baf68b30bac30d711e4d3d7e7
AnasImloul <pydev27@gmail.com>
2022-09-07 11:17:44 +0100

    Runtime: 9 ms (Top 91.77%) | Memory: 42.4 MB (Top 94.00%)

--- scripts/algorithms/D/Delete Columns to Make Sorted/Delete Columns to Make Sorted.java
+++ scripts/algorithms/D/Delete Columns to Make Sorted/Delete Columns to Make Sorted.java
@@ -1,7 +1,8 @@
+// Runtime: 9 ms (Top 91.77%) | Memory: 42.4 MB (Top 94.00%)
 class Solution {
     public int minDeletionSize(String[] strs) {
         int count = 0;
-        for(int i = 0; i < strs[0].length(); i++){  //strs[0].length() is used to find the length of the column
+        for(int i = 0; i < strs[0].length(); i++){ //strs[0].length() is used to find the length of the column
             for(int j = 0; j < strs.length-1; j++){
                 if((int) strs[j].charAt(i) <= (int) strs[j+1].charAt(i)){
                     continue;
