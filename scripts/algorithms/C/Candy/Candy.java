09dee4b1a81e59c3c8cc8ec6c3b43c35e30c969d
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:32 +0100

    Runtime: 16 ms (Top 14.09%) | Memory: 53.1 MB (Top 8.48%)

--- scripts/algorithms/C/Candy/Candy.java
+++ scripts/algorithms/C/Candy/Candy.java
@@ -1,18 +1,19 @@
+// Runtime: 16 ms (Top 14.09%) | Memory: 53.1 MB (Top 8.48%)
 class Solution {
     public int candy(int[] ratings) {
-       
+
         int[] left = new int[ratings.length];
         Arrays.fill(left, 1);
-       
+
         // we are checking from left to right that if the element next to our current element has greater rating, if yes then we are increasing their candy
         for(int i = 0; i<ratings.length-1; i++){
             if(ratings[i] < ratings[i+1])
                 left[i+1] = left[i]+1;
         }
-        
+
         int[] right = new int[ratings.length];
         Arrays.fill(right, 1);
-        
+
         //we are checking from right to left if the element after than our current element is greater or not , if yes then we are also checking their candies if greater rating has less number of candies then increasing their candy
         for(int i = ratings.length -2; i>=0; i--){
             if(ratings[i+1] < ratings[i] && right[i] <= right[i+1])
@@ -22,6 +23,6 @@ class Solution {
         for(int i = 0; i<right.length; i++){
             sum += Math.max(right[i], left[i]);
         }
-    
+
     return sum;}
 }
