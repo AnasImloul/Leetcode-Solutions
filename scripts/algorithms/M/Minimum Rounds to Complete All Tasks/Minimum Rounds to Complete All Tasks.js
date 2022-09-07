541e828827efee5a50e7e334dff716917d81aff8
AnasImloul <pydev27@gmail.com>
2022-09-08 00:10:10 +0100

    Runtime: 171 ms (Top 86.61%) | Memory: 55.3 MB (Top 97.32%)

--- scripts/algorithms/M/Minimum Rounds to Complete All Tasks/Minimum Rounds to Complete All Tasks.js
+++ scripts/algorithms/M/Minimum Rounds to Complete All Tasks/Minimum Rounds to Complete All Tasks.js
@@ -1,15 +1,16 @@
+// Runtime: 171 ms (Top 86.61%) | Memory: 55.3 MB (Top 97.32%)
 var minimumRounds = function(tasks) {
     const hash = {};
     let minRounds = 0;
-    
+
     for (const task of tasks) {
         hash[task] = hash[task] + 1 || 1;
     }
-    
+
     for (const count of Object.values(hash)) {
         if (count < 2) return -1;
         minRounds += Math.ceil(count / 3);
     }
-    
+
     return minRounds;
 };
