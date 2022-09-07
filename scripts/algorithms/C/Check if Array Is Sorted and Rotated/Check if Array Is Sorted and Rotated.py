9c83442f821ecc777ea126fbf1b48cf3b9c80c3e
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:52 +0100

    Runtime: 62 ms (Top 35.34%) | Memory: 13.7 MB (Top 99.68%)

--- scripts/algorithms/C/Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.py
+++ scripts/algorithms/C/Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.py
@@ -1,3 +1,4 @@
+# Runtime: 62 ms (Top 35.34%) | Memory: 13.7 MB (Top 99.68%)
 class Solution:
     def check(self, nums: List[int]) -> bool:
         count = 0
@@ -5,8 +6,8 @@ class Solution:
         for i in range(length):
             if nums[i] > nums[i + 1]:
                 count+=1
-    
+
         if count > 1 or (count == 1 and nums[0] < nums[length]):
             return False
-        
+
         return True
