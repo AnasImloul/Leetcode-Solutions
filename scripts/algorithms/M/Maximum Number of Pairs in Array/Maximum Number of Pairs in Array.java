d94e21b8a3ce75ec76aa553144214a4b561f123d
AnasImloul <pydev27@gmail.com>
2022-09-07 18:39:00 +0100

    Runtime: 4 ms (Top 21.09%) | Memory: 42.4 MB (Top 76.38%)

--- scripts/algorithms/M/Maximum Number of Pairs in Array/Maximum Number of Pairs in Array.java
+++ scripts/algorithms/M/Maximum Number of Pairs in Array/Maximum Number of Pairs in Array.java
@@ -1,21 +1,22 @@
+// Runtime: 4 ms (Top 21.09%) | Memory: 42.4 MB (Top 76.38%)
 class Solution {
     public int[] numberOfPairs(int[] nums) {
-        
+
         if(nums.length == 1)
         return new int[]{0,1};
-        
+
         HashSet<Integer> set = new HashSet<>();
-        
+
         int pairs=0;
         for(int i : nums){
             if(!set.contains(i)){
-               set.add(i);            // No pair present 
+               set.add(i); // No pair present
             }else{
-              set.remove(i);          // Pair found 
-              pairs++;                                              
+              set.remove(i); // Pair found
+              pairs++;
             }
         }
-        
-        return new int[]{pairs,set.size()};             
+
+        return new int[]{pairs,set.size()};
     }
 }
