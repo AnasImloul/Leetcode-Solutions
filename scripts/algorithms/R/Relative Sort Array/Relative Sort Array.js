884b5e04132eb430a85732e061c8645935339bfb
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:26 +0100

    Runtime: 92 ms (Top 51.55%) | Memory: 42.4 MB (Top 64.95%)

--- scripts/algorithms/R/Relative Sort Array/Relative Sort Array.js
+++ scripts/algorithms/R/Relative Sort Array/Relative Sort Array.js
@@ -1,3 +1,4 @@
+// Runtime: 92 ms (Top 51.55%) | Memory: 42.4 MB (Top 64.95%)
 /**
  * @param {number[]} arr1
  * @param {number[]} arr2
@@ -6,7 +7,7 @@
 var relativeSortArray = function(arr1, arr2) {
   const indices = new Map();
   arr2.forEach(indices.set, indices);
-  
+
   return arr1.sort((a, b) => {
     if (indices.has(a)) {
       return indices.has(b) ? indices.get(a) - indices.get(b) : -1;
