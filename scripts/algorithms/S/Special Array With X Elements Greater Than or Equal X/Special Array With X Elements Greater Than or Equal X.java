00d1ce86f86d16bbaaf63980ce8250f16b65924b
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:33 +0100

    Runtime: 1 ms (Top 88.84%) | Memory: 42.3 MB (Top 22.19%)

--- scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.java
+++ scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.java
@@ -1,21 +1,22 @@
+// Runtime: 1 ms (Top 88.84%) | Memory: 42.3 MB (Top 22.19%)
 class Solution {
     public int specialArray(int[] nums) {
         int x = nums.length;
         int[] counts = new int[x+1];
-        
+
         for(int elem : nums)
             if(elem >= x)
                 counts[x]++;
             else
                 counts[elem]++;
-        
+
         int res = 0;
         for(int i = counts.length-1; i > 0; i--) {
             res += counts[i];
             if(res == i)
                 return i;
         }
-        
+
         return -1;
     }
 }
