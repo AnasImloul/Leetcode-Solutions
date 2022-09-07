d8f1bd02103809e92d64fe4a9b105d8afe49a40b
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:55 +0100

    Runtime: 8 ms (Top 86.11%) | Memory: 41.5 MB (Top 82.95%)

--- scripts/algorithms/R/Race Car/Race Car.java
+++ scripts/algorithms/R/Race Car/Race Car.java
@@ -1,9 +1,10 @@
+// Runtime: 8 ms (Top 86.11%) | Memory: 41.5 MB (Top 82.95%)
 class Solution {
     public int racecar(int target) {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 1, 0});
         Set<String> visited = new HashSet<>();
-        
+
         while(!queue.isEmpty()){
             int[] item = queue.poll();
             int currPos = item[0];
@@ -17,7 +18,7 @@ class Solution {
             int nextPos = currPos + currSpeed;
             int nextSpeed = currSpeed * 2;
             String posSpeed = new StringBuilder().append(nextPos).append(",").append(nextSpeed).toString();
-			
+
             // If the particular state (position & speed) is not encountered earlier then we explore that state
             // And we also check if the nextPos is not beyond twice the size of target, then there is no point in exploring that route
             if(!visited.contains(posSpeed) && Math.abs(nextPos) < 2 * target){
@@ -26,11 +27,11 @@ class Solution {
             }
 
             // Choosing R
-			// We go in reverse only when we are moving away from the target in the positive or in the negative direction
+            // We go in reverse only when we are moving away from the target in the positive or in the negative direction
             if((currPos + currSpeed > target && currSpeed > 0) || (currPos + currSpeed < target && currSpeed < 0)) {
                 nextSpeed = currSpeed > 0 ? -1 : 1;
                 posSpeed = new StringBuilder().append(currPos).append(",").append(nextSpeed).toString();
-				
+
                 if(!visited.contains(posSpeed) && Math.abs(currPos) < 2 * target){
                     visited.add(posSpeed);
                     queue.add(new int[]{currPos, nextSpeed, distance + 1});
