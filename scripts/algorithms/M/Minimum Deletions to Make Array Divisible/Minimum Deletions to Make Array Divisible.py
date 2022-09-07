b3988d850dad07201d8b70490c0fd60b33382806
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:15 +0100

    Runtime: 1147 ms (Top 45.35%) | Memory: 25.8 MB (Top 79.68%)

--- scripts/algorithms/M/Minimum Deletions to Make Array Divisible/Minimum Deletions to Make Array Divisible.py
+++ scripts/algorithms/M/Minimum Deletions to Make Array Divisible/Minimum Deletions to Make Array Divisible.py
@@ -1,3 +1,4 @@
+# Runtime: 1147 ms (Top 45.35%) | Memory: 25.8 MB (Top 79.68%)
 class Solution:
     def minOperations(self, nums: List[int], divs: List[int]) -> int:
         div = reduce(gcd, divs)
