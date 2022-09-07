a951a08e646aad2d8a1e7a52050a40c95b292501
AnasImloul <pydev27@gmail.com>
2022-09-07 20:09:56 +0100

    Runtime: 72 ms (Top 16.98%) | Memory: 17.6 MB (Top 40.12%)

--- scripts/algorithms/M/Merge Similar Items/Merge Similar Items.cpp
+++ scripts/algorithms/M/Merge Similar Items/Merge Similar Items.cpp
@@ -1,14 +1,15 @@
+// Runtime: 72 ms (Top 16.98%) | Memory: 17.6 MB (Top 40.12%)
 class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
         map<int, int> m;
-        
+
         for(auto v : items1) m[v[0]] += v[1];
         for(auto v : items2) m[v[0]] += v[1];
-        
+
         vector<vector<int>> ans;
         for(auto i : m) ans.push_back({i.first, i.second});
-        
+
         return ans;
     }
 };
