857f095c3c1d20cca9915d720c5b437fca8d8887
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:56 +0100

    Runtime: 1384 ms (Top 56.07%) | Memory: 30.3 MB (Top 20.56%)

--- scripts/algorithms/F/Find XOR Sum of All Pairs Bitwise AND/Find XOR Sum of All Pairs Bitwise AND.py
+++ scripts/algorithms/F/Find XOR Sum of All Pairs Bitwise AND/Find XOR Sum of All Pairs Bitwise AND.py
@@ -1,4 +1,5 @@
+# Runtime: 1384 ms (Top 56.07%) | Memory: 30.3 MB (Top 20.56%)
 class Solution:
     def getXORSum(self, arr1: List[int], arr2: List[int]) -> int:
-        def xor_lis(lis): return functools.reduce(lambda a,b : a^b,lis)            
+        def xor_lis(lis): return functools.reduce(lambda a,b : a^b,lis)
         return xor_lis(arr1) & xor_lis(arr2)
