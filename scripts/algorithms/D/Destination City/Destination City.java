2c2c59c999d719edbbeb1f4c6e2cae859a7c969f
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:57 +0100

    Runtime: 4 ms (Top 58.98%) | Memory: 44 MB (Top 40.73%)

--- scripts/algorithms/D/Destination City/Destination City.java
+++ scripts/algorithms/D/Destination City/Destination City.java
@@ -1,7 +1,8 @@
+// Runtime: 4 ms (Top 58.98%) | Memory: 44 MB (Top 40.73%)
 class Solution {
     public String destCity(List<List<String>> paths) {
         HashSet<String> set1 = new HashSet<>();
-        
+
         for (int i = 0; i < paths.size(); ++i) {
             set1.add(paths.get(i).get(0));
         }
