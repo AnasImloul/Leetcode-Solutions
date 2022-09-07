1d5760fdcda51160ecef5f1727313edcd142fc49
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:00 +0100

    Runtime: 218 ms (Top 37.41%) | Memory: 52 MB (Top 63.08%)

--- scripts/algorithms/E/Equal Row and Column Pairs/Equal Row and Column Pairs.js
+++ scripts/algorithms/E/Equal Row and Column Pairs/Equal Row and Column Pairs.js
@@ -1,16 +1,17 @@
+// Runtime: 218 ms (Top 37.41%) | Memory: 52 MB (Top 63.08%)
 var equalPairs = function(grid) {
     let n = grid.length
     let count = 0;
-    
+
     let map = new Map()
-    
+
     //making rowArray
     for(let row = 0; row < n; row++){
         let temp = []
         for(let col = 0; col < n; col++){
             temp.push(grid[row][col])
         }
-        
+
         temp = temp.join()
 
         if(map.has(temp)){
@@ -21,20 +22,19 @@ var equalPairs = function(grid) {
             map.set(temp, 1)
         }
     }
-    
-    
+
     for(let col = 0; col < n; col++){
         let temp = []
         for(let row = 0; row < n; row++){
             temp.push(grid[row][col])
         }
-        
+
         temp = temp.join()
 
         if(map.has(temp)){
             count += map.get(temp)
         }
     }
-    
+
     return count;
 };
