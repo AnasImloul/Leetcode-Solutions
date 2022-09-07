cbd192cd0b717552ca1e4e4702d69e293f915257
AnasImloul <pydev27@gmail.com>
2022-09-07 22:44:11 +0100

    Runtime: 99 ms (Top 29.63%) | Memory: 41.6 MB (Top 91.36%)

--- scripts/algorithms/M/Maximum Ascending Subarray Sum/Maximum Ascending Subarray Sum.js
+++ scripts/algorithms/M/Maximum Ascending Subarray Sum/Maximum Ascending Subarray Sum.js
@@ -1,8 +1,9 @@
+// Runtime: 99 ms (Top 29.63%) | Memory: 41.6 MB (Top 91.36%)
 var maxAscendingSum = function(nums) {
-	const subarray = nums.reduce((acc, curr, index) => {
-		curr > nums[index - 1] ? acc[acc.length - 1] += curr : acc.push(curr);
-		return acc;
-	}, []);
+    const subarray = nums.reduce((acc, curr, index) => {
+        curr > nums[index - 1] ? acc[acc.length - 1] += curr : acc.push(curr);
+        return acc;
+    }, []);
 
-	return Math.max(...subarray);
+    return Math.max(...subarray);
 };
