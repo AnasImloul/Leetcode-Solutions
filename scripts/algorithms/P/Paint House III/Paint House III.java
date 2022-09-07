1abe8abce1033135e293008195d7d5ab2b7522c5
AnasImloul <pydev27@gmail.com>
2022-09-07 11:17:48 +0100

    Runtime: 70 ms (Top 34.94%) | Memory: 52.8 MB (Top 34.74%)

--- scripts/algorithms/P/Paint House III/Paint House III.java
+++ scripts/algorithms/P/Paint House III/Paint House III.java
@@ -1,3 +1,4 @@
+// Runtime: 70 ms (Top 34.94%) | Memory: 52.8 MB (Top 34.74%)
 class Solution {
     public int helper(int idx, int[] houses, int[][] cost,int target, int prevColor,int neigh,Integer[][][] dp)
     {
@@ -10,22 +11,22 @@ class Solution {
         if(dp[idx][prevColor][neigh]!=null)
             return dp[idx][prevColor][neigh];
         int minCost = Integer.MAX_VALUE;
-        
+
         if(houses[idx]==0)
         {
             for(int j = 0;j<cost[idx].length;j++)
             {
                 int minCostHere = Integer.MAX_VALUE;
-				
+
                 if(j+1==prevColor) // Painting the house with the same colour as that of the previous one.
                     minCostHere = helper(idx+1,houses,cost,target,prevColor,neigh,dp);
-					
+
                 else // Painting the house with a different color and incrementing the neighbour count.
                     minCostHere = helper(idx+1,houses,cost,target,j+1,neigh+1,dp);
-                
+
                 if(minCostHere!=Integer.MAX_VALUE)
                     minCostHere+=cost[idx][j];
-                
+
                 minCost = Math.min(minCostHere,minCost);
             }
         }
@@ -36,12 +37,12 @@ class Solution {
             else
                 minCost = helper(idx+1,houses,cost,target,houses[idx],neigh+1,dp);
         }
-        
+
         return dp[idx][prevColor][neigh] = minCost;
-        
+
     }
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        
+
         Integer[][][] dp = new Integer[m][n+1][target+1];
         int ans = helper(0,houses,cost,target,0,0,dp);
         return ans==Integer.MAX_VALUE?-1:ans;
