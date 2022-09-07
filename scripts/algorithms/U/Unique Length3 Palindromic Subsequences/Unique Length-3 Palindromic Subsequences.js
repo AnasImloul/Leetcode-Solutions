af440c878d1caab2b31b355dc3b2690d74e2224a
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:02 +0100

    Runtime: 206 ms (Top 90.48%) | Memory: 54.1 MB (Top 47.62%)

---
+++ scripts/algorithms/U/Unique Length3 Palindromic Subsequences/Unique Length-3 Palindromic Subsequences.js
@@ -0,0 +1,38 @@
+// Runtime: 206 ms (Top 90.48%) | Memory: 54.1 MB (Top 47.62%)
+var countPalindromicSubsequence = function(s) {
+    const charToIndices = {};
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charToIndices[char]) {
+            charToIndices[char].push(i);
+        } else {
+            charToIndices[char] = [i];
+        }
+    }
+
+    let count = 0;
+    for (const currChar in charToIndices) {
+        if (charToIndices[currChar].length < 2) continue;
+        const currCharIndices = charToIndices[currChar];
+        const firstIndex = currCharIndices[0];
+        const lastIndex = currCharIndices[currCharIndices.length - 1];
+
+        for (const char in charToIndices) {
+            const indices = charToIndices[char];
+            let lo = 0;
+            let hi = indices.length;
+            while (lo < hi) {
+                const mid = (lo + hi) >> 1;
+                if (indices[mid] <= firstIndex) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+            if (indices[lo] && indices[lo] < lastIndex) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
