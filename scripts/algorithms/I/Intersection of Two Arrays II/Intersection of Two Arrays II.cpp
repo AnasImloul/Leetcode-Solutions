4c63b4324babea3b268df7db953de0a4d2659fa4
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:21 +0100

    Runtime: 3 ms (Top 96.48%) | Memory: 10.1 MB (Top 81.91%)

--- scripts/algorithms/I/Intersection of Two Arrays II/Intersection of Two Arrays II.cpp
+++ scripts/algorithms/I/Intersection of Two Arrays II/Intersection of Two Arrays II.cpp
@@ -1,15 +1,16 @@
+// Runtime: 3 ms (Top 96.48%) | Memory: 10.1 MB (Top 81.91%)
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
      sort(nums1.begin(),nums1.end());
     sort(nums2.begin(),nums2.end());
-    
+
     int a = nums1.size();
     int b = nums2.size();
-    
+
     int i=0,j=0;
     vector<int>res;
-    
+
     while(i<a && j<b)
     {
         if(nums1[i] > nums2[j])
@@ -27,7 +28,7 @@ public:
             j++;
         }
     }
-    
+
     return res;
 }
 };
