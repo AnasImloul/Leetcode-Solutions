81379dc7c3ddf4fb7fc4c7fe332d62dc43fa33d5
AnasImloul <pydev27@gmail.com>
2022-09-07 21:32:17 +0100

    Runtime: 1 ms (Top 66.41%) | Memory: 42.3 MB (Top 45.67%)

--- scripts/algorithms/C/Construct Binary Search Tree from Preorder Traversal/Construct Binary Search Tree from Preorder Traversal.java
+++ scripts/algorithms/C/Construct Binary Search Tree from Preorder Traversal/Construct Binary Search Tree from Preorder Traversal.java
@@ -1,17 +1,18 @@
+// Runtime: 1 ms (Top 66.41%) | Memory: 42.3 MB (Top 45.67%)
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
         return bst(preorder, 0, preorder.length-1);
     }
-    
+
     public TreeNode bst(int[] preorder, int start, int end){
         if(start > end) return null;
-        
+
         TreeNode root = new TreeNode(preorder[start]);
         int breakPoint = start+1;
         while(breakPoint <= end && preorder[breakPoint] < preorder[start]){
             breakPoint++;
         }
-             
+
         root.left = bst(preorder, start+1, breakPoint-1);
         root.right = bst(preorder, breakPoint, end);
         return root;
