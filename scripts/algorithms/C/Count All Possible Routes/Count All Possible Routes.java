bf0e88400ec7eedd98551ded3d149312e6f90c56
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:29 +0100

    Runtime: 81 ms (Top 78.44%) | Memory: 41.8 MB (Top 95.21%)

--- scripts/algorithms/C/Count All Possible Routes/Count All Possible Routes.java
+++ scripts/algorithms/C/Count All Possible Routes/Count All Possible Routes.java
@@ -1,3 +1,4 @@
+// Runtime: 81 ms (Top 78.44%) | Memory: 41.8 MB (Top 95.21%)
 
 /*
 
@@ -10,52 +11,50 @@ Using DFS and Memo :
 
 */
 
-
 class Solution {
-    
+
     int mod = (int)Math.pow(10,9) + 7 ;
     int[][] dp ;
-    
+
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
-        
+
         dp = new int[locations.length][fuel+1] ;
-        
+
         for(int[] row : dp){
             Arrays.fill(row , -1) ;
         }
-        
+
         return dfs(locations , start , finish , fuel);
     }
-    
+
     public int dfs(int[] locations , int cur_location , int finish , int fuel){
-        
+
         if(fuel < 0){
             return 0 ;
         }
-        
+
         if(dp[cur_location][fuel] != -1){
             return dp[cur_location][fuel] ;
         }
-        
+
         int result = 0 ;
-        
+
         if(cur_location == finish){
             result++ ;
         }
-        
+
         for(int i=0 ; i<locations.length ; i++){
-            
+
             if(i == cur_location) continue ;
-            
-            int fuel_cost =  Math.abs(locations[i] - locations[cur_location]);
+
+            int fuel_cost = Math.abs(locations[i] - locations[cur_location]);
             int next_trip = dfs(locations , i , finish , fuel-fuel_cost);
             result += next_trip ;
             result %= mod ;
         }
-        
+
         dp[cur_location][fuel] = result ;
-        
+
         return dp[cur_location][fuel] ;
     }
 }
-
