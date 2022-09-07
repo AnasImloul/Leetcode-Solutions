d67a064b513710d6d1c41fa4fdcd7a07f4316c94
AnasImloul <pydev27@gmail.com>
2022-09-07 22:44:07 +0100

    Runtime: 121 ms (Top 5.08%) | Memory: 41.2 MB (Top 98.87%)

--- scripts/algorithms/S/Spiral Matrix/Spiral Matrix.js
+++ scripts/algorithms/S/Spiral Matrix/Spiral Matrix.js
@@ -1,16 +1,16 @@
+// Runtime: 121 ms (Top 5.08%) | Memory: 41.2 MB (Top 98.87%)
 /**
  * @param {number[][]} matrix
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
     let [top, bot, left, right, ans] = [0, matrix.length - 1, 0, matrix[0].length - 1,[]]
-    
-    
+
     while ((top <= bot) && (left <= right)) {
         for (let j = left; j <= right; j++) {
             ans.push(matrix[top][j])
         }
-        top ++; 
+        top ++;
         for (let i = top; i <= bot; i++) {
             ans.push(matrix[i][right])
         }
