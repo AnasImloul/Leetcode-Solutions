ae3f9bd22704a8bc7e03d971ce3decae5a3ed200
AnasImloul <pydev27@gmail.com>
2022-09-07 22:44:05 +0100

    Runtime: 93 ms (Top 42.36%) | Memory: 42 MB (Top 77.09%)

--- scripts/algorithms/C/Combination Sum III/Combination Sum III.js
+++ scripts/algorithms/C/Combination Sum III/Combination Sum III.js
@@ -1,3 +1,4 @@
+// Runtime: 93 ms (Top 42.36%) | Memory: 42 MB (Top 77.09%)
 /**
  * @param {number} k
  * @param {number} n
@@ -6,7 +7,7 @@
 var combinationSum3 = function(k, n) {
     const ans = [];
     const st = [];
-    
+
     function dfs(start, t) {
         if (t === 0 && st.length === k) {
             ans.push(Array.from(st));
@@ -19,8 +20,8 @@ var combinationSum3 = function(k, n) {
             st.pop();
         }
     }
-    
+
     dfs(1, n);
-    
+
     return ans;
 };
