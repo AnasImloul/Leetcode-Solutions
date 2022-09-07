b8b1880514057bc20166f26daa5161ff68bcffd1
AnasImloul <pydev27@gmail.com>
2022-09-07 18:43:53 +0100

    Runtime: 1817 ms (Top 5.05%) | Memory: 26.8 MB (Top 41.50%)

--- scripts/algorithms/M/Maximum Split of Positive Even Integers/Maximum Split of Positive Even Integers.py
+++ scripts/algorithms/M/Maximum Split of Positive Even Integers/Maximum Split of Positive Even Integers.py
@@ -1,3 +1,4 @@
+# Runtime: 1817 ms (Top 5.05%) | Memory: 26.8 MB (Top 41.50%)
 class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
         l=[]
@@ -5,13 +6,13 @@ class Solution:
             return l
         else:
             s=0
-            i=2                       # even pointer 2, 4, 6, 8, 10, 12...........
+            i=2 # even pointer 2, 4, 6, 8, 10, 12...........
             while(s<finalSum):
-                s+=i                #sum 
-                l.append(i)      # append the i in list
+                s+=i #sum
+                l.append(i) # append the i in list
                 i+=2
-            if s==finalSum:  #if sum s is equal to finalSum then no modidfication required
+            if s==finalSum: #if sum s is equal to finalSum then no modidfication required
                 return l
             else:
-                l.pop(l.index(s-finalSum))  #Deleting the element which makes s greater than finalSum
+                l.pop(l.index(s-finalSum)) #Deleting the element which makes s greater than finalSum
             return l
