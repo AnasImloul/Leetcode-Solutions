39b3c6bad49f2f7cf0667ae90d3d446318c6418c
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:25 +0100

    Runtime: 2922 ms (Top 60.17%) | Memory: 79.1 MB (Top 76.18%)

--- scripts/algorithms/M/Min Cost to Connect All Points/Min Cost to Connect All Points.py
+++ scripts/algorithms/M/Min Cost to Connect All Points/Min Cost to Connect All Points.py
@@ -1,23 +1,24 @@
+# Runtime: 2922 ms (Top 60.17%) | Memory: 79.1 MB (Top 76.18%)
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        
+
         cost = 0
         heap = []
-        
+
         #set to store past points to prevent cycle
         visited = set([0])
-        
+
         #i == the index of current point
         i = 0
-        
+
         while len(visited) < len(points):
-            #Add all costs from current point to unreached points to min heap 
+            #Add all costs from current point to unreached points to min heap
             for j in range(len(points)):
                 if j == i or j in visited:
                     continue
                 distance = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
                 heapq.heappush(heap, (distance, j))
-            
+
             #Add new min cost edge
             while True:
                 dist, point = heapq.heappop(heap)
@@ -28,5 +29,5 @@ class Solution:
                     #Update point
                     i = point
                     break
-                
+
         return cost
