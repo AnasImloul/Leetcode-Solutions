c016c5a9e8d66e418fa54786fa5c385601eb1e9f
AnasImloul <pydev27@gmail.com>
2022-09-07 18:38:47 +0100

    Runtime: 7 ms (Top 24.69%) | Memory: 88.4 MB (Top 55.38%)

--- scripts/algorithms/N/Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.java
+++ scripts/algorithms/N/Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.java
@@ -1,3 +1,4 @@
+// Runtime: 7 ms (Top 24.69%) | Memory: 88.4 MB (Top 55.38%)
 class Solution {
     public long getDescentPeriods(int[] prices) {
         int i=0;
@@ -5,12 +6,12 @@ class Solution {
         long ans=1;
         while(j<prices.length){
             if( prices[j-1]-prices[j]==1){
-			//It means that j(current element) can be part of previous subarrays (j-i) 
-			//and can also start a subarray from me (+1). So add (j-i+1) in total Subarrays
+            //It means that j(current element) can be part of previous subarrays (j-i)
+            //and can also start a subarray from me (+1). So add (j-i+1) in total Subarrays
                 int count=j-i+1;
                 ans+=count;
             }else{
-			//It means that j cannot be part of previous subarrays but can start subarray from me. So, ans+=1
+            //It means that j cannot be part of previous subarrays but can start subarray from me. So, ans+=1
                 i=j;
                 ans+=1;
             }
