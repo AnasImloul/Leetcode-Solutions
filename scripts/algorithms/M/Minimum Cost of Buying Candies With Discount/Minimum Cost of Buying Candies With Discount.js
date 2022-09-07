2530f4c4a7a4691a00a29b62a7f82df88f0d9991
AnasImloul <pydev27@gmail.com>
2022-09-08 00:10:09 +0100

    Runtime: 85 ms (Top 68.97%) | Memory: 44.1 MB (Top 21.26%)

--- scripts/algorithms/M/Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.js
+++ scripts/algorithms/M/Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.js
@@ -1,12 +1,13 @@
+// Runtime: 85 ms (Top 68.97%) | Memory: 44.1 MB (Top 21.26%)
 var minimumCost = function(cost) {
     if (cost.length < 3) {
         return cost.reduce((prev, cur) => prev + cur);
     }
-    
+
     cost.sort((a, b) => b - a);
     let count = 0;
     let sum = 0;
-    
+
     for (const num of cost) {
         if (count === 2) {
             count = 0;
@@ -15,6 +16,6 @@ var minimumCost = function(cost) {
         sum += num;
         count++;
     }
-    
+
     return sum;
 };
