2849f32b95f4eeb4e9926e6158ee3c0bd64ce8d6
AnasImloul <pydev27@gmail.com>
2022-09-07 18:30:15 +0100

    Runtime: 911 ms (Top 31.82%) | Memory: 75.8 MB (Top 9.09%)

---
+++ scripts/algorithms/K/KSimilar Strings/K-Similar Strings.js
@@ -0,0 +1,51 @@
+// Runtime: 911 ms (Top 31.82%) | Memory: 75.8 MB (Top 9.09%)
+var kSimilarity = function(s1, s2) {
+    // abc --> bca
+    // swap from 0: a !== b, find next b, swap(0,1) --> bac
+    // swap from 1: a !== c, find next c, swap(1,2) --> bca
+    return bfs(s1, s2);
+};
+
+const bfs = (a,b)=>{
+    if(a===b)
+        return 0;
+    const visited = new Set();
+    const queue = [];
+    queue.push([a,0,0]); // str, idx, swapCount
+    while(queue.length>0)
+    {
+        let [s, idx, cnt] = queue.shift();
+        while(s[idx]===b[idx])
+        {
+                idx++;
+        }
+        for(let j = idx+1; j<s.length; j++)
+        {
+            if(s[j]===b[idx]) {
+                s = swap(s, idx, j);
+
+                if(s===b) {
+                    return cnt+1;
+                }
+
+                if(!visited.has(s))
+                {
+                    queue.push([s.slice(), idx, cnt+1]);
+                    visited.add(s.slice());
+                }
+
+                // swap back for later index
+                s = swap(s, idx, j);
+            }
+        }
+    }
+    return -1;
+}
+
+const swap = (s, i, j)=>{
+  let arr = s.split('');
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+  return arr.join('');
+}
