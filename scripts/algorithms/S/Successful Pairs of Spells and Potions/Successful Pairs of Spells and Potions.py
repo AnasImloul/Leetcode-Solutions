93b3d9c0a53a312d81a5cf46150a67dc1915fcd8
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:29 +0100

    Runtime: 3070 ms (Top 29.46%) | Memory: 37.4 MB (Top 40.00%)

--- scripts/algorithms/S/Successful Pairs of Spells and Potions/Successful Pairs of Spells and Potions.py
+++ scripts/algorithms/S/Successful Pairs of Spells and Potions/Successful Pairs of Spells and Potions.py
@@ -1,8 +1,9 @@
+# Runtime: 3070 ms (Top 29.46%) | Memory: 37.4 MB (Top 40.00%)
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         result = self.function(spells,potions,success)
         return result
-    
+
     def function(self,arr1,arr2,success):
         n2 = len(arr2)
         arr2.sort() #Sorting Enables Us To Do Binary Search
@@ -10,6 +11,6 @@ class Solution:
         for i in arr1:
             val = math.ceil(success/i) #Finding the Value Of Portion With Least Strength So That It Can Be Greater Than Success
             idx = bisect.bisect_left(arr2,val) #Finding The Left Most Index So That The Value Can Be Inserted
-            res = n2-idx+1  #Calculating the remaining numbers after finding the suitable index
+            res = n2-idx+1 #Calculating the remaining numbers after finding the suitable index
             ans.append(res-1)
         return ans
