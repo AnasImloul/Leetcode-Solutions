274bcc0032645004f39c597350996e3d4e50f841
AnasImloul <pydev27@gmail.com>
2022-09-07 19:47:56 +0100

    Runtime: 629 ms (Top 35.29%) | Memory: 80.4 MB (Top 80.39%)

--- scripts/algorithms/K/K Radius Subarray Averages/K Radius Subarray Averages.js
+++ scripts/algorithms/K/K Radius Subarray Averages/K Radius Subarray Averages.js
@@ -1,7 +1,8 @@
+// Runtime: 629 ms (Top 35.29%) | Memory: 80.4 MB (Top 80.39%)
 var getAverages = function(nums, k) {
     const twoK = 2 * k;
     const windowSize = twoK + 1;
-    
+
     const result = [...nums].fill(-1);
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
