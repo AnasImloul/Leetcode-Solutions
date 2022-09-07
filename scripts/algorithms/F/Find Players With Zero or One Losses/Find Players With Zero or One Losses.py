11b2a9da66ac0eb7c3c2fa9c22c1de17a2d899a4
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:52 +0100

    Runtime: 5248 ms (Top 5.11%) | Memory: 68.3 MB (Top 89.11%)

--- scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.py
+++ scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.py
@@ -1,10 +1,11 @@
+# Runtime: 5248 ms (Top 5.11%) | Memory: 68.3 MB (Top 89.11%)
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         winners, losers, table = [], [], {}
         for winner, loser in matches:
             # map[key] = map.get(key, 0) + change . This format ensures that KEY NOT FOUND error is always prevented.
             # map.get(key, 0) returns map[key] if key exists and 0 if it does not.
-            table[winner] = table.get(winner, 0)  # Winner
+            table[winner] = table.get(winner, 0) # Winner
             table[loser] = table.get(loser, 0) + 1
         for k, v in table.items(): # Player k with losses v
             if v == 0:
