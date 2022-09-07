1bbb45f7ee3204ebbb10a4b2edc03404e9d75429
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:13 +0100

    Runtime: 1864 ms (Top 94.44%) | Memory: 14.6 MB (Top 87.65%)

--- scripts/algorithms/S/String Compression II/String Compression II.py
+++ scripts/algorithms/S/String Compression II/String Compression II.py
@@ -1,17 +1,18 @@
+# Runtime: 1864 ms (Top 94.44%) | Memory: 14.6 MB (Top 87.65%)
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-        # Find min lenth of the code starting from group ind, if there are res_k characters to delete and 
-		#       group ind needs to be increased by carry_over additional characters
-        def FindMinLen(ind, res_k, carry_over=0): 
-            
+        # Find min lenth of the code starting from group ind, if there are res_k characters to delete and
+        # group ind needs to be increased by carry_over additional characters
+        def FindMinLen(ind, res_k, carry_over=0):
+
             # If we already found the min length - just retrieve it (-1 means we did not calculate it)
             if carry_over == 0 and dynamic[ind][res_k] != -1:
                 return dynamic[ind][res_k]
-            
+
             # Number of character occurences that we need to code. Includes carry-over.
             cur_count = carry_over + frequency[ind]
 
-            # Min code length if the group ind stays intact. The code accounts for single-character "s0" vs. "s"  situation.
+            # Min code length if the group ind stays intact. The code accounts for single-character "s0" vs. "s" situation.
             min_len = 1 + min(len(str(cur_count)), cur_count - 1) + FindMinLen(ind+1,res_k)
 
             # Min length if we keep only 0, 1, 9, or 99 characters in the group - delete the rest, if feasible
@@ -28,7 +29,7 @@ class Solution:
             # If there was no carry-over, store the result
             if carry_over == 0: dynamic[ind][res_k] = min_len
             return min_len
-        
+
         # Two auxiliary lists - character groups (drop repeated) and number of characters in the group
         frequency, chars = [], ""
         for char in s:
@@ -36,8 +37,8 @@ class Solution:
                 frequency.append(0)
                 chars = chars + char
             frequency[-1] += 1
-        
-        # Table with the results. Number of character groups by number of available deletions.  
+
+        # Table with the results. Number of character groups by number of available deletions.
         dynamic = [[-1] * (k + 1) for i in range(len(frequency))] + [[0]*(k + 1)]
-        
+
         return FindMinLen(0, k)
