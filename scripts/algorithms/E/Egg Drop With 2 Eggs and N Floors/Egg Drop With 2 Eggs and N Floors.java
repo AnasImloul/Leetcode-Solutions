aa83bd2f2a6274d70b9fa8f90b67d9e90fef3d89
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:31 +0100

    Runtime: 74 ms (Top 32.31%) | Memory: 42.5 MB (Top 12.73%)

--- scripts/algorithms/E/Egg Drop With 2 Eggs and N Floors/Egg Drop With 2 Eggs and N Floors.java
+++ scripts/algorithms/E/Egg Drop With 2 Eggs and N Floors/Egg Drop With 2 Eggs and N Floors.java
@@ -1,10 +1,11 @@
+// Runtime: 74 ms (Top 32.31%) | Memory: 42.5 MB (Top 12.73%)
 class Solution {
     public int twoEggDrop(int n) {
         int egg = 2; // hard coded to 2 eggs for this problem
         int[][] dp = new int[n+1][egg+1];
         return eggDrop(n, egg, dp);
     }
-    
+
     int eggDrop(int n, int egg, int[][] dp) {
         if(n <= 2 || egg == 1) return n;
         if(dp[n][egg] != 0) return dp[n][egg];
