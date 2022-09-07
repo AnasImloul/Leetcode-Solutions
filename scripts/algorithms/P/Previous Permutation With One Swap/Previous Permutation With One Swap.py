15d1bcb95ffea28e5689e0af317b5c82e72cab99
AnasImloul <pydev27@gmail.com>
2022-09-08 00:20:58 +0100

    Runtime: 486 ms (Top 14.08%) | Memory: 15.3 MB (Top 47.61%)

--- scripts/algorithms/P/Previous Permutation With One Swap/Previous Permutation With One Swap.py
+++ scripts/algorithms/P/Previous Permutation With One Swap/Previous Permutation With One Swap.py
@@ -1,3 +1,4 @@
+# Runtime: 486 ms (Top 14.08%) | Memory: 15.3 MB (Top 47.61%)
 class Solution:
 
     def find_max(self, i, a, n):
@@ -14,7 +15,7 @@ class Solution:
         n = len(arr)
         for i in range(n-1, 0, -1):
             if(arr[i] < arr[i-1]):
-                # sending the first descending element  from right to max_function
+                # sending the first descending element from right to max_function
                 arr = self.find_max(i-1, arr, n)
                 break
         return arr
