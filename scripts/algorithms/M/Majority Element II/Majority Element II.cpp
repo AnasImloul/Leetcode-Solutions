755c01fdfccc638b7947cde4509703341378d63a
AnasImloul <pydev27@gmail.com>
2022-09-07 18:22:49 +0100

    Runtime: 38 ms (Top 5.64%) | Memory: 15.9 MB (Top 55.41%)

--- scripts/algorithms/M/Majority Element II/Majority Element II.cpp
+++ scripts/algorithms/M/Majority Element II/Majority Element II.cpp
@@ -1,15 +1,16 @@
+// Runtime: 38 ms (Top 5.64%) | Memory: 15.9 MB (Top 55.41%)
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
+
         int n = nums.size();
-        
+
         vector<int> result;
-        
+
         int num1 = -1 , num2 = -1 , count1 = 0 , count2 = 0;
-        
+
         for(auto it : nums){
-            
+
             if(num1 == it) ++count1;
             else if(num2 == it) ++count2;
             else if(count1 == 0){
@@ -24,23 +25,23 @@ public:
                 --count1;
                 --count2;
             }
-            
+
         }
-        
+
         count1 = 0;
         count2 = 0;
-        
+
         for(auto it : nums){
-            
+
             if(it == num1) ++count1;
             else if(it == num2) ++count2;
-            
+
         }
-        
+
         if(count1>(n/3)) result.push_back(num1);
         if(count2>(n/3)) result.push_back(num2);
-        
+
         return result;
-        
+
     }
 };
