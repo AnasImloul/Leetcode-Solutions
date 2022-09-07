65b7bd6b5772a0fdf369e9f9402af7d4413de682
AnasImloul <pydev27@gmail.com>
2022-09-07 21:14:30 +0100

    Runtime: 113 ms (Top 12.98%) | Memory: 43.5 MB (Top 27.62%)

--- scripts/algorithms/I/Increasing Order Search Tree/Increasing Order Search Tree.js
+++ scripts/algorithms/I/Increasing Order Search Tree/Increasing Order Search Tree.js
@@ -1,20 +1,21 @@
+// Runtime: 113 ms (Top 12.98%) | Memory: 43.5 MB (Top 27.62%)
 var increasingBST = function(root) {
     let newRoot = new TreeNode(-1);
     const newTree = newRoot;
-    
+
     const dfs = (node) => {
         if (!node) return null;
-        
+
         if (node.left) dfs(node.left);
-        
+
         const newNode = new TreeNode(node.val);
         newRoot.right = newNode;
         newRoot.left = null;
         newRoot = newRoot.right;
-        
+
         if (node.right) dfs(node.right);
     }
     dfs(root);
-    
+
     return newTree.right;
 };
