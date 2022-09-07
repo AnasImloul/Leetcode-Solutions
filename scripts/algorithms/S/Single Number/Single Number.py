50efdc40c0116d3874769ccd3f22a407d1db5246
AnasImloul <pydev27@gmail.com>
2022-09-07 22:52:29 +0100

    Runtime: 301 ms (Top 20.49%) | Memory: 16.1 MB (Top 98.97%)

--- scripts/algorithms/S/Single Number/Single Number.py
+++ scripts/algorithms/S/Single Number/Single Number.py
@@ -1,3 +1,4 @@
+# Runtime: 301 ms (Top 20.49%) | Memory: 16.1 MB (Top 98.97%)
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         nums.sort()
@@ -7,6 +8,5 @@ class Solution:
                 i+=2
             else:
                 return nums[i]
-            
-        
+
         return nums[-1]
