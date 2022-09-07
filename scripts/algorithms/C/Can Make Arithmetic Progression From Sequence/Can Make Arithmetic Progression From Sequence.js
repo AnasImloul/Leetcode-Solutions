26cd6a6c460164df434c8b13bde19b57f440ef65
AnasImloul <pydev27@gmail.com>
2022-09-07 18:29:50 +0100

    Runtime: 117 ms (Top 17.41%) | Memory: 42.3 MB (Top 65.96%)

--- scripts/algorithms/C/Can Make Arithmetic Progression From Sequence/Can Make Arithmetic Progression From Sequence.js
+++ scripts/algorithms/C/Can Make Arithmetic Progression From Sequence/Can Make Arithmetic Progression From Sequence.js
@@ -1,3 +1,4 @@
+// Runtime: 117 ms (Top 17.41%) | Memory: 42.3 MB (Top 65.96%)
 var canMakeArithmeticProgression = function(arr) {
     arr.sort(function(a,b){return a-b});
     var dif = arr[1] - arr[0];
