5d6e161c13b00dbd4425f5ebf5bc1f1801250a5e
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:29 +0100

    Runtime: 285 ms (Top 92.79%) | Memory: 15.1 MB (Top 75.68%)

--- scripts/algorithms/C/Can Convert String in K Moves/Can Convert String in K Moves.py
+++ scripts/algorithms/C/Can Convert String in K Moves/Can Convert String in K Moves.py
@@ -1,8 +1,9 @@
+# Runtime: 285 ms (Top 92.79%) | Memory: 15.1 MB (Top 75.68%)
 class Solution:
     def canConvertString(self, s: str, t: str, k: int) -> bool:
         if len(s) != len(t):
             return False
-        
+
         cycles, extra = divmod(k, 26)
         shifts = [cycles + (shift <= extra) for shift in range(26)]
 
@@ -13,5 +14,5 @@ class Solution:
             if not shifts[shift]:
                 return False
             shifts[shift] -= 1
-        
+
         return True
