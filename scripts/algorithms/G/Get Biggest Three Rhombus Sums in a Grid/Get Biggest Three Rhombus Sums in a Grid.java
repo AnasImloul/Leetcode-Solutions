196b49c907c5ec269a12c3818719f73545fafe17
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:36 +0100

    Runtime: 38 ms (Top 93.09%) | Memory: 52.8 MB (Top 78.80%)

--- scripts/algorithms/G/Get Biggest Three Rhombus Sums in a Grid/Get Biggest Three Rhombus Sums in a Grid.java
+++ scripts/algorithms/G/Get Biggest Three Rhombus Sums in a Grid/Get Biggest Three Rhombus Sums in a Grid.java
@@ -1,3 +1,4 @@
+// Runtime: 38 ms (Top 93.09%) | Memory: 52.8 MB (Top 78.80%)
 class Solution {
     public int[] getBiggestThree(int[][] grid) {
         int end = Math.min(grid.length, grid[0].length);
@@ -5,26 +6,25 @@ class Solution {
         for(int length=0; length<end; length++){
             searchBigThree(grid, maxThree, length);
         }
-        
+
         Arrays.sort(maxThree);
-        
+
         // If there are less than three distinct values, return all of them.
         if(maxThree[0] == 0){
             if(maxThree[1] == 0){
-                return new int[]{maxThree[2]}; 
+                return new int[]{maxThree[2]};
             }
-            return new int[]{maxThree[2],maxThree[1]}; 
+            return new int[]{maxThree[2],maxThree[1]};
         }
-        
+
         // reverse array
         maxThree[0] = maxThree[0]^maxThree[2];
         maxThree[2] = maxThree[0]^maxThree[2];
         maxThree[0] = maxThree[0]^maxThree[2];
-        
-        
+
         return maxThree;
     }
-    
+
     void searchBigThree(int[][] grid, int[] maxThree, int length){
         int end = grid.length-(length==0?0: 2*length);
         int end1 = grid[0].length-(length);
@@ -36,55 +36,55 @@ class Solution {
             }
         }
     }
-    
+
     /*
     get sum of edges of rhombus abcd
             a
            / \
-          d   b
+          d b
            \ /
             c
-    
+
     */
     int getSum(int[][] grid, int i, int j, int length){
         if(length == 0){
             return grid[i][j];
         }
-        
+
         int sum = 0;
         // edge ab
         for(int it=0; it<=length; it++){
             sum = sum + grid[i+it][j+it];
         }
-        
+
         // edge ad
         for(int it=1; it<=length; it++){
             sum = sum + grid[i+it][j-it];
         }
-        
+
         // edge dc
         for(int it=1; it<=length; it++){
             sum = sum + grid[i+length+it][j-length+it];
         }
-        
+
         // edge bc
         for(int it=1; it<length; it++){
             sum = sum + grid[i+length+it][j+length-it];
         }
-        
+
         return sum;
     }
-    
+
     void addToMaxThree(int[] maxThree, int num){
         // Do not add duplicate entry
         if(maxThree[0] == num || maxThree[1] == num || maxThree[2] == num ){
             return;
         }
-        
+
         Arrays.sort(maxThree);
-        
+
         if(maxThree[0] < num){
             maxThree[0] = num;
         }
-    } 
+    }
 }
