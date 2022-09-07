6677a76395cb7cc90913d114db8842a71cc6856c
AnasImloul <pydev27@gmail.com>
2022-09-07 23:01:49 +0100

    Runtime: 2 ms (Top 43.86%) | Memory: 42.6 MB (Top 27.39%)

--- scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.java
+++ scripts/algorithms/R/Replace All Digits with Characters/Replace All Digits with Characters.java
@@ -1,7 +1,8 @@
+// Runtime: 2 ms (Top 43.86%) | Memory: 42.6 MB (Top 27.39%)
 class Solution {
     public String replaceDigits(String s) {
         char[] str = s.toCharArray();
-        
+
         for(int i=0;i<str.length;i++){
             if(Character.isDigit(str[i])){
               str[i] = shift(str[i-1],str[i]);
@@ -9,8 +10,7 @@ class Solution {
         }
         return String.valueOf(str);
     }
-    
-    
+
     char shift(char letter, char number){
         int a = Integer.parseInt(String.valueOf(number));
         int asci = (int)letter;
