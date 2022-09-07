79d7e3fab914d2b63e719bb3ef0f12d71276d424
AnasImloul <pydev27@gmail.com>
2022-09-07 23:11:24 +0100

    Runtime: 51 ms (Top 25.06%) | Memory: 13.8 MB (Top 67.00%)

--- scripts/algorithms/N/Number of Provinces/Number of Provinces.cpp
+++ scripts/algorithms/N/Number of Provinces/Number of Provinces.cpp
@@ -1,33 +1,34 @@
+// Runtime: 51 ms (Top 25.06%) | Memory: 13.8 MB (Top 67.00%)
 class Solution {
 private:
-	void dfs(int node,vector<vector<int>> &graph,int n,vector<bool> &vis){
+    void dfs(int node,vector<vector<int>> &graph,int n,vector<bool> &vis){
 
-		vis[node] = true;
+        vis[node] = true;
 
-		for(int j = 0; j < graph[node].size(); j++){
-			if(graph[node][j] == 1 and !vis[j]){
-				dfs(j,graph,n,vis);
-			}
-		}
+        for(int j = 0; j < graph[node].size(); j++){
+            if(graph[node][j] == 1 and !vis[j]){
+                dfs(j,graph,n,vis);
+            }
+        }
 
-	}
+    }
 public:
-	int findCircleNum(vector<vector<int>>& isConnected) {
+    int findCircleNum(vector<vector<int>>& isConnected) {
 
-		int n = isConnected.size();
+        int n = isConnected.size();
 
-		vector<bool> vis(n,false);
+        vector<bool> vis(n,false);
 
-		int ans = 0;
+        int ans = 0;
 
-		for(int i = 0; i < n; i++){
-			if(!vis[i]){
-				ans++;
-				dfs(i,isConnected,n,vis);
-			}
-		}
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                ans++;
+                dfs(i,isConnected,n,vis);
+            }
+        }
 
-		return ans;
+        return ans;
 
-	}
+    }
 };
