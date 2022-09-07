23b656af8056f704b0938e20e1d7421c5bfcd302
AnasImloul <pydev27@gmail.com>
2022-09-07 18:29:45 +0100

    Runtime: 108 ms (Top 35.29%) | Memory: 42.1 MB (Top 52.94%)

--- scripts/algorithms/T/The Number of Full Rounds You Have Played/The Number of Full Rounds You Have Played.js
+++ scripts/algorithms/T/The Number of Full Rounds You Have Played/The Number of Full Rounds You Have Played.js
@@ -1,3 +1,4 @@
+// Runtime: 108 ms (Top 35.29%) | Memory: 42.1 MB (Top 52.94%)
 var numberOfRounds = function(loginTime, logoutTime) {
     const start = toMins(loginTime);
     const end = toMins(logoutTime);
@@ -6,13 +7,12 @@ var numberOfRounds = function(loginTime, logoutTime) {
     let roundEnd = Math.floor(end / 15);
 
     if (start < end) {
-        return Math.max(0, roundEnd - roundStart); 
-    } 
+        return Math.max(0, roundEnd - roundStart);
+    }
     else {
         roundEnd += 96;
         return roundEnd - roundStart;
     }
-
 
     function toMins(timeStr) {
         const [hh, mm] = timeStr.split(":");
