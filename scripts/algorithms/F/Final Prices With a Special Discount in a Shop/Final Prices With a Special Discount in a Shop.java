41db4f2d6f9d9efbf6bc096fc1f5e57438d882dd
AnasImloul <pydev27@gmail.com>
2022-09-07 20:03:44 +0100

    Runtime: 4 ms (Top 50.03%) | Memory: 44.5 MB (Top 32.12%)

--- scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.java
+++ scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.java
@@ -1,3 +1,4 @@
+// Runtime: 4 ms (Top 50.03%) | Memory: 44.5 MB (Top 32.12%)
 class Solution {
     public int[] finalPrices(int[] prices) {
         for(int i = 0; i < prices.length; i++){
@@ -6,7 +7,7 @@ class Solution {
                     prices[i] -= prices[j];
                     break;
                 }
-            }            
+            }
         }
         return prices;
     }
