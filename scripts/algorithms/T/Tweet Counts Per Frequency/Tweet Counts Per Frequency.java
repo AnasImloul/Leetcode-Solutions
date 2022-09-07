cb057abc69da010c5d0e514be8fc8b7ea57fd222
AnasImloul <pydev27@gmail.com>
2022-09-08 00:30:30 +0100

    Runtime: 351 ms (Top 16.84%) | Memory: 93.3 MB (Top 14.74%)

--- scripts/algorithms/T/Tweet Counts Per Frequency/Tweet Counts Per Frequency.java
+++ scripts/algorithms/T/Tweet Counts Per Frequency/Tweet Counts Per Frequency.java
@@ -1,13 +1,14 @@
+// Runtime: 351 ms (Top 16.84%) | Memory: 93.3 MB (Top 14.74%)
 class TweetCounts {
     Map<String, List<Integer>> map;
     public TweetCounts() {
         map = new HashMap<>();
     }
-    
+
     public void recordTweet(String tweetName, int time) {
         map.computeIfAbsent(tweetName, v->new ArrayList<>()).add(time);
     }
-    
+
     public List<Integer> getTweetCountsPerFrequency(String freq, String tweetName, int startTime, int endTime) {
         List<Integer> res = new ArrayList<>();
         if(map.containsKey(tweetName)) {
@@ -17,11 +18,11 @@ class TweetCounts {
                 int end = Math.min(startTime+interval-1, endTime); // need this to handle the last interval
                 res.add(getFreq(map.get(tweetName), startTime, end));
                 startTime=end+1; // ex: for minute, the interval is 60 so our end is 59. The next startTime is end+1
-            }   
+            }
         }
         return res;
     }
-    
+
     public int getFreq(List<Integer> list, int start, int end) {
         int st = Collections.binarySearch(list, start);
         if(st<0) {
@@ -29,9 +30,9 @@ class TweetCounts {
         }
         int en = Collections.binarySearch(list, end);
         if(en<0) {
-            en = (en+2)*-1;  // our exact end time might not be in the list, to get the last timestamp just smaller than end
+            en = (en+2)*-1; // our exact end time might not be in the list, to get the last timestamp just smaller than end
         }
-        
+
         return en-st+1; // the freq count
     }
 }
@@ -42,12 +43,11 @@ class TweetCounts {
     Freq(int val) {
         map.put(this, val);
     }
-     
-    public  int getVal() {
+
+    public int getVal() {
          return map.get(this);
      }
-     
-     
+
 }
 
 /**
@@ -56,4 +56,3 @@ class TweetCounts {
  * obj.recordTweet(tweetName,time);
  * List<Integer> param_2 = obj.getTweetCountsPerFrequency(freq,tweetName,startTime,endTime);
  */
-
