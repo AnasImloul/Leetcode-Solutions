eea4786a02d9ace5902e7762106cb5c841a85048
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:34 +0100

    Runtime: 170 ms (Top 20.15%) | Memory: 15 MB (Top 44.78%)

--- scripts/algorithms/S/Super Washing Machines/Super Washing Machines.py
+++ scripts/algorithms/S/Super Washing Machines/Super Washing Machines.py
@@ -1,3 +1,4 @@
+# Runtime: 170 ms (Top 20.15%) | Memory: 15 MB (Top 44.78%)
 from itertools import accumulate
 class Solution:
     def findMinMoves(self, machines: List[int]) -> int:
@@ -9,8 +10,7 @@ class Solution:
         left = list(accumulate(machines))
         result = 0
         for i in range(n):
-            move_to_right = max(left[i] - (i+1)*avg, 0) 
+            move_to_right = max(left[i] - (i+1)*avg, 0)
             move_to_left = max(left[-1]-(left[i-1] if i!=0 else 0) - (n-i)*avg, 0)
             result = max(result, move_to_right + move_to_left)
         return result
-                
