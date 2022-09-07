edb98f5a18845b04056c86cff775f09767b12b8a
AnasImloul <pydev27@gmail.com>
2022-09-07 18:29:57 +0100

    Runtime: 144 ms (Top 34.85%) | Memory: 45.3 MB (Top 50.00%)

--- scripts/algorithms/R/Redistribute Characters to Make All Strings Equal/Redistribute Characters to Make All Strings Equal.js
+++ scripts/algorithms/R/Redistribute Characters to Make All Strings Equal/Redistribute Characters to Make All Strings Equal.js
@@ -1,23 +1,23 @@
+// Runtime: 144 ms (Top 34.85%) | Memory: 45.3 MB (Top 50.00%)
 /**
  * @param {string[]} words
  * @return {boolean}
  */
 var makeEqual = function(words) {
-    
-    
+
     let length = words.length
-    
+
     let map = {}
     for( let word of words ) {
         for( let ch of word ) {
-            map[ch] = ( map[ch] || 0 )  + 1 
+            map[ch] = ( map[ch] || 0 ) + 1
         }
     }
-    
+
     for( let key of Object.keys(map)) {
         if( map[key] % length !=0 ) return false
     }
-    
+
     return true
-    
+
 };
