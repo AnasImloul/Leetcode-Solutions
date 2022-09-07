c5ca27aa464ff4b6eb6b3f8d425b58f9b3ecf7f4
AnasImloul <pydev27@gmail.com>
2022-09-07 19:54:31 +0100

    Runtime: 1072 ms (Top 44.44%) | Memory: 42.9 MB (Top 86.75%)

--- scripts/algorithms/T/Tuple with Same Product/Tuple with Same Product.py
+++ scripts/algorithms/T/Tuple with Same Product/Tuple with Same Product.py
@@ -1,15 +1,16 @@
+# Runtime: 1072 ms (Top 44.44%) | Memory: 42.9 MB (Top 86.75%)
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        
+
         from itertools import combinations
         mydict=defaultdict(int)
         ans=0
-        
+
         for a,b in combinations(nums,2):
             mydict[a*b]+=1
-        
+
         for i,j in mydict.items():
             if j>1:
                 ans+=(j*(j-1)//2)*8
-        
+
         return ans
