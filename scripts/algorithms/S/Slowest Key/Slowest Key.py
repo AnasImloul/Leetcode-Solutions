6e87e94200d3017a3f2ecdaf9c91160abd5183b8
AnasImloul <pydev27@gmail.com>
2022-09-07 21:21:16 +0100

    Runtime: 111 ms (Top 25.49%) | Memory: 14.1 MB (Top 45.43%)

--- scripts/algorithms/S/Slowest Key/Slowest Key.py
+++ scripts/algorithms/S/Slowest Key/Slowest Key.py
@@ -1,13 +1,14 @@
+# Runtime: 111 ms (Top 25.49%) | Memory: 14.1 MB (Top 45.43%)
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
         max_dur = releaseTimes[0]
         max_key = keysPressed[0]
-        
+
         for i in range(1, len(releaseTimes)):
             if releaseTimes[i] - releaseTimes[i-1] > max_dur:
                 max_dur = releaseTimes[i] - releaseTimes[i-1]
                 max_key = keysPressed[i]
             elif releaseTimes[i] - releaseTimes[i-1] == max_dur and max_key < keysPressed[i]:
                  max_key = keysPressed[i]
-                    
-        return max_key 
+
+        return max_key
