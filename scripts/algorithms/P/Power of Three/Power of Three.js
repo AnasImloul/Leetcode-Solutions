5cbee4ede25f540d9ba1a85d5afa1b43fba04515
AnasImloul <pydev27@gmail.com>
2022-09-09 00:30:58 +0100

    Runtime: 211 ms (Top 95.18%) | Memory: 51.5 MB (Top 23.97%)

--- scripts/algorithms/P/Power of Three/Power of Three.js
+++ scripts/algorithms/P/Power of Three/Power of Three.js
@@ -1,9 +1,10 @@
+// Runtime: 211 ms (Top 95.18%) | Memory: 51.5 MB (Top 23.97%)
 var isPowerOfThree = function(n) {
     if(n === 1){return true;}
     if(n === 0){return false;}
-    
+
     n/=3;
-    
+
     if(n%3 != 0 && n != 1){
         return false;
     }else{
