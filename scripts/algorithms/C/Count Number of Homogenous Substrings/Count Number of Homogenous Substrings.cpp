6cc099cfde38204473cf696bab8b25d140e3032a
AnasImloul <pydev27@gmail.com>
2022-09-08 23:25:29 +0100

    Runtime: 84 ms (Top 7.54%) | Memory: 11.8 MB (Top 13.97%)

--- scripts/algorithms/C/Count Number of Homogenous Substrings/Count Number of Homogenous Substrings.cpp
+++ scripts/algorithms/C/Count Number of Homogenous Substrings/Count Number of Homogenous Substrings.cpp
@@ -1,26 +1,27 @@
+// Runtime: 84 ms (Top 7.54%) | Memory: 11.8 MB (Top 13.97%)
 class Solution {
 public:
     int countHomogenous(string s) {
-        
+
         int n = s.length();
         long long count = 0;
         long long len = 0;
         int MOD = 1000000007;
-        
-        int i = 0;    
+
+        int i = 0;
         while(i<n)
         {
             len = 1;
-            
+
             // get length of same char substring
             while(i+1<n && s[i]==s[i+1])i++,len++;
-            
+
             // calculate number of substring from same char substring and then sum
             count = (count + ((len * (len+1)) /2 )%MOD ) % MOD;
-            
+
             i++;
         }
-        
+
         return count;
     }
 };
