7a7ac1b3f6f0c880bddc621ab4a7f793f61b995f
AnasImloul <pydev27@gmail.com>
2022-09-08 23:25:28 +0100

    Runtime: 38 ms (Top 52.14%) | Memory: 6.9 MB (Top 75.00%)

--- scripts/algorithms/F/Find All Good Strings/Find All Good Strings.cpp
+++ scripts/algorithms/F/Find All Good Strings/Find All Good Strings.cpp
@@ -1,3 +1,4 @@
+// Runtime: 38 ms (Top 52.14%) | Memory: 6.9 MB (Top 75.00%)
 class Solution {
 private:
     string s1,s2,evil;
@@ -9,7 +10,7 @@ private:
         if(!dp[pos][evilpos][s1equal][s2equal]){
             for(auto c='a'; c<='z';c++){
                 int curpos=evilpos;
-                while(c!=evil[curpos] && curpos>0) 
+                while(c!=evil[curpos] && curpos>0)
                     curpos=KMP[curpos-1];
                 if(c==evil[curpos]) curpos++;
                 if((s1equal && c<s1[pos]) || (s2equal && c>s2[pos]) || (curpos==evil.size()))
@@ -23,7 +24,7 @@ private:
         return dp[pos][evilpos][s1equal][s2equal];
     }
 public:
-    int findGoodStrings(int n, string s1, string s2, string evil) {   
+    int findGoodStrings(int n, string s1, string s2, string evil) {
         this->s1=s1;
         this->s2=s2;
         this->evil=evil;
@@ -36,6 +37,6 @@ public:
             if(evil[i]==evil[k]) KMP[i]=++k;
         }
         return calculate(0,0,1,1);
-     }   
-    
+     }
+
 };
