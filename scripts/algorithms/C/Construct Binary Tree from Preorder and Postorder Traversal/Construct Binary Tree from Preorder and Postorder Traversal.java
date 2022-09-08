e4617e6e8061eb5b7853c1c6ab1bca579a49da8c
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:05 +0100

    Runtime: 2 ms (Top 63.11%) | Memory: 44.6 MB (Top 8.84%)

--- scripts/algorithms/C/Construct Binary Tree from Preorder and Postorder Traversal/Construct Binary Tree from Preorder and Postorder Traversal.java
+++ scripts/algorithms/C/Construct Binary Tree from Preorder and Postorder Traversal/Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -1,15 +1,16 @@
-class Solution 
+// Runtime: 2 ms (Top 63.11%) | Memory: 44.6 MB (Top 8.84%)
+class Solution
 {
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder)
     {
         // O(n) time | O(h) space
-        if(preorder == null || postorder == null || preorder.length == 0 || postorder.length == 0)  return null;
-        
+        if(preorder == null || postorder == null || preorder.length == 0 || postorder.length == 0) return null;
+
         TreeNode root = new TreeNode(preorder[0]);
         int mid = 0;
-        
-        if(preorder.length == 1)    return root;
-        
+
+        if(preorder.length == 1) return root;
+
         // update mid
         for(int i = 0; i < postorder.length; i++)
         {
@@ -19,11 +20,11 @@ class Solution
                 break;
             }
         }
-        
+
         root.left = constructFromPrePost(
                                             Arrays.copyOfRange(preorder, 1, 1 + mid + 1),
                                             Arrays.copyOfRange(postorder, 0, mid + 1));
-        
+
         root.right = constructFromPrePost(
                                             Arrays.copyOfRange(preorder, 1 + mid + 1, preorder.length),
                                             Arrays.copyOfRange(postorder, mid + 1, postorder.length - 1));
