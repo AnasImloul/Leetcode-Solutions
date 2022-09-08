43420c5c487e41f82e468223c5d3364407c035d2
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:34 +0100

    Runtime: 92 ms (Top 15.46%) | Memory: 13.9 MB (Top 77.11%)

--- scripts/algorithms/M/Maximum Population Year/Maximum Population Year.py
+++ scripts/algorithms/M/Maximum Population Year/Maximum Population Year.py
@@ -1,10 +1,11 @@
+# Runtime: 92 ms (Top 15.46%) | Memory: 13.9 MB (Top 77.11%)
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         dates = []
         for birth, death in logs:
             dates.append((birth, 1))
             dates.append((death, -1))
-            
+
         dates.sort()
 
         population = max_population = max_year = 0
@@ -13,5 +14,5 @@ class Solution:
             if population > max_population:
                 max_population = population
                 max_year = year
-        
+
         return max_year
