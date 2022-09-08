405036d85a3d29dbfb08a163176f98cb2d57cc65
AnasImloul <pydev27@gmail.com>
2022-09-09 00:30:56 +0100

    Runtime: 107 ms (Top 61.22%) | Memory: 46.9 MB (Top 57.14%)

--- scripts/algorithms/L/Lowest Common Ancestor of Deepest Leaves/Lowest Common Ancestor of Deepest Leaves.js
+++ scripts/algorithms/L/Lowest Common Ancestor of Deepest Leaves/Lowest Common Ancestor of Deepest Leaves.js
@@ -1,3 +1,4 @@
+// Runtime: 107 ms (Top 61.22%) | Memory: 46.9 MB (Top 57.14%)
 var lcaDeepestLeaves = function(root) {
     if(!root) return root;
     // keep track of max depth if node have both deepest node
@@ -7,14 +8,14 @@ var lcaDeepestLeaves = function(root) {
             md = Math.max(md, d);
             return d;
         }
-        
+
         const ld = compute(r.left, d + 1);
         const rd = compute(r.right, d + 1);
-        
+
         if(ld == rd && ld == md) {
             ans = r;
         }
-        
+
         return Math.max(ld, rd);
     }
     compute();
