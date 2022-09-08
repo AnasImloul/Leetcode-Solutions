8fb02868bcb988ba28ca679048f400554364b6c8
AnasImloul <pydev27@gmail.com>
2022-09-08 23:01:30 +0100

    Runtime: 165 ms (Top 69.47%) | Memory: 62.9 MB (Top 100.00%)

--- scripts/algorithms/F/Find the Winner of an Array Game/Find the Winner of an Array Game.cpp
+++ scripts/algorithms/F/Find the Winner of an Array Game/Find the Winner of an Array Game.cpp
@@ -1,3 +1,4 @@
+// Runtime: 165 ms (Top 69.47%) | Memory: 62.9 MB (Top 100.00%)
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
@@ -5,13 +6,13 @@ public:
         int wins = 0;
         for (int i=1; i<arr.size();i++){
             if(winner > arr[i])
-                wins+=1;  //increment wins count   
+                wins+=1; //increment wins count
             else{
-                wins = 1;    //new winner
-                winner = arr[i];               
+                wins = 1; //new winner
+                winner = arr[i];
             }
             if(wins == k)
-                break;    //if wins count is k, then return winner       
+                break; //if wins count is k, then return winner
         }
         return winner;
     }
