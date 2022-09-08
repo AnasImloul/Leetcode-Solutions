19b63de8a15cc588683ea0886b76541f5163a8a3
AnasImloul <pydev27@gmail.com>
2022-09-08 23:40:58 +0100

    Runtime: 3 ms (Top 14.46%) | Memory: 41.2 MB (Top 58.53%)

--- scripts/algorithms/P/Power of Two/Power of Two.java
+++ scripts/algorithms/P/Power of Two/Power of Two.java
@@ -1,7 +1,8 @@
+// Runtime: 3 ms (Top 14.46%) | Memory: 41.2 MB (Top 58.53%)
 class Solution {
-    public boolean isPowerOfTwo(int n) { 
+    public boolean isPowerOfTwo(int n) {
         return power2(0,n);
-        
+
     }
     public boolean power2(int index,int n){
         if(Math.pow(2,index)==n)
