a777fd7096ad0825a49ff66c4cea85de215703df
AnasImloul <pydev27@gmail.com>
2022-09-09 00:31:04 +0100

    Runtime: 63 ms (Top 92.39%) | Memory: 41.7 MB (Top 76.57%)

--- scripts/algorithms/W/Word Pattern/Word Pattern.js
+++ scripts/algorithms/W/Word Pattern/Word Pattern.js
@@ -1,22 +1,23 @@
+// Runtime: 63 ms (Top 92.39%) | Memory: 41.7 MB (Top 76.57%)
 var wordPattern = function(pattern, s) {
     let wordArray = s.split(" ");
     if(wordArray.length !== pattern.length) return false;
-    
+
     let hm = {};
     let hs = new Set();
-    
+
     for(let index in pattern) {
         let word = wordArray[index];
         let char = pattern[index];
-        
+
         if(hm[char] !== undefined) {
-            if(hm[char] !== word) return false;    
+            if(hm[char] !== word) return false;
         } else {
             if(hs.has(word)) return false; // Duplicate Occurence of word on first occurrence of a char
             hm[char] = word;
             hs.add(word);
         }
     }
-    
+
     return true;
 }
