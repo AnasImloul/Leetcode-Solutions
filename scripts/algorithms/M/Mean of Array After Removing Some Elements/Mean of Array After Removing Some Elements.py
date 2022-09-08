30a7d59db011574ec93e87aadba03d6eec98cea8
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:46 +0100

    Runtime: 137 ms (Top 11.43%) | Memory: 14.1 MB (Top 36.03%)

--- scripts/algorithms/M/Mean of Array After Removing Some Elements/Mean of Array After Removing Some Elements.py
+++ scripts/algorithms/M/Mean of Array After Removing Some Elements/Mean of Array After Removing Some Elements.py
@@ -1,6 +1,6 @@
+# Runtime: 137 ms (Top 11.43%) | Memory: 14.1 MB (Top 36.03%)
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
         arr.sort()
 
         return statistics.mean(arr[int(len(arr)*5/100):len(arr)-int(len(arr)*5/100)])
-        
