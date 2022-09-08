c791b2effe7ad80366f836e8b9b2655b7b9eef8b
AnasImloul <pydev27@gmail.com>
2022-09-08 23:40:58 +0100

    Runtime: 7 ms (Top 22.65%) | Memory: 42.7 MB (Top 99.36%)

--- scripts/algorithms/N/Number of Provinces/Number of Provinces.java
+++ scripts/algorithms/N/Number of Provinces/Number of Provinces.java
@@ -1,20 +1,21 @@
+// Runtime: 7 ms (Top 22.65%) | Memory: 42.7 MB (Top 99.36%)
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int size = isConnected.length;
         boolean[] isCheck = new boolean[size+1];
         int ans = 0;
-        
+
         for(int i=1; i<=size; i++){
-            
-            if(!isCheck[i]){                               // Doing BFS if it's false in isCheck[]
+
+            if(!isCheck[i]){ // Doing BFS if it's false in isCheck[]
                 Queue<Integer> q = new LinkedList<>();
                 q.add(i);
-                ans++;                                     // No. of queue = No. of Graphs 
-                
+                ans++; // No. of queue = No. of Graphs
+
                 while(!q.isEmpty()){
                     int temp = q.remove();
                     isCheck[temp] = true;
-                    
+
                     for(int j=0; j<size; j++){
                         if(isConnected[temp-1][j]==1 && !isCheck[j+1])
                             q.add(j+1);
