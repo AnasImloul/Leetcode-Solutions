b06cb3b5824d95488686ba4c5f6baaa7c2387904
AnasImloul <pydev27@gmail.com>
2022-09-08 23:01:30 +0100

    Runtime: 2231 ms (Top 12.50%) | Memory: 55 MB (Top 76.79%)

--- scripts/algorithms/P/People Whose List of Favorite Companies Is Not a Subset of Another List/People Whose List of Favorite Companies Is Not a Subset of Another List.cpp
+++ scripts/algorithms/P/People Whose List of Favorite Companies Is Not a Subset of Another List/People Whose List of Favorite Companies Is Not a Subset of Another List.cpp
@@ -1,3 +1,4 @@
+// Runtime: 2231 ms (Top 12.50%) | Memory: 55 MB (Top 76.79%)
 /*
  * author: deytulsi18
  * problem: https://leetcode.com/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/
@@ -13,17 +14,17 @@ public:
                   b.begin(), b.end()));
     }
     vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) {
-        
+
         int n = favoriteCompanies.size();
         vector<int> res;
-        
+
         for (auto &i : favoriteCompanies)
-		sort(begin(i), end(i));
-        
+        sort(begin(i), end(i));
+
         for (int i = 0; i < n; i++)
         {
             bool isValid = true;
-            
+
             for (int j = 0; j < n; j++)
                 if (i != j)
                     if (isSubset(favoriteCompanies[i], favoriteCompanies[j]))
@@ -31,12 +32,11 @@ public:
                         isValid = false;
                         break;
                     }
-            
+
             if (isValid)
-                res.emplace_back(i); 
+                res.emplace_back(i);
         }
-                
+
         return res;
     }
 };;
-
