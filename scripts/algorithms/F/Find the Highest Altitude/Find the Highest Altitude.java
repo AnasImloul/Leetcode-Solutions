ce0f985aa06851f8915602c4eaeaff9f2751e594
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:05 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 39.7 MB (Top 97.89%)

--- scripts/algorithms/F/Find the Highest Altitude/Find the Highest Altitude.java
+++ scripts/algorithms/F/Find the Highest Altitude/Find the Highest Altitude.java
@@ -1,9 +1,10 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 39.7 MB (Top 97.89%)
 class Solution {
     public int largestAltitude(int[] gain) {
          int max_alt=0;
         int curr_alt=0;
         for(int i=0;i<gain.length;i++){
-            curr_alt+=gain[i]; 
+            curr_alt+=gain[i];
             max_alt=Math.max(curr_alt, max_alt);
         }
         return max_alt;
