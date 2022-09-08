2a1df4ed1fed94b6c5b67ae04b79e32e2037d832
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:02 +0100

    Runtime: 2 ms (Top 56.88%) | Memory: 44 MB (Top 9.63%)

--- scripts/algorithms/Q/Queens That Can Attack the King/Queens That Can Attack the King.java
+++ scripts/algorithms/Q/Queens That Can Attack the King/Queens That Can Attack the King.java
@@ -1,3 +1,4 @@
+// Runtime: 2 ms (Top 56.88%) | Memory: 44 MB (Top 9.63%)
 class Solution {
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
         List<List<Integer>> res = new ArrayList<>();
@@ -5,17 +6,17 @@ class Solution {
         int l=king[1];
         int [][]board =new int[8][8];
         int n=8;
-        
-//         8 cases;
-//         moving upward
-//         moving downward
-//         moving right
-//         moving left
-//         moving upper right diagonal
-//         moving upper left diagonal'
-//         moving lower left diagonal
-//         moving lower right diagonal        
-        
+
+// 8 cases;
+// moving upward
+// moving downward
+// moving right
+// moving left
+// moving upper right diagonal
+// moving upper left diagonal'
+// moving lower left diagonal
+// moving lower right diagonal
+
         int i,j;
         for(i=0;i<queens.length;i++)
         {
@@ -25,15 +26,15 @@ class Solution {
                 int column=queens[i][j+1];
                 board[row][column]=1;
             }
-            
+
         }
         // for(i=0;i<8;i++)
         // {
-        //     for(j=0;j<8;j++)
-        //     {
-        //         System.out.println(board[i][j]);
-        //     }
-        //        System.out.println();
+        // for(j=0;j<8;j++)
+        // {
+        // System.out.println(board[i][j]);
+        // }
+        // System.out.println();
         // }
         for(j=l;j<n;j++)
         {
@@ -57,7 +58,7 @@ class Solution {
             {
                  res.add(new ArrayList<>(Arrays.asList(i, l)));
                  break;
-                
+
             }
         }
         for(j=l;j>=0;j--)
@@ -66,23 +67,23 @@ class Solution {
             {
                  res.add(new ArrayList<>(Arrays.asList(r, j)));
                  break;
-                
+
             }
         }
         for(i=r,j=l;i>=0 && j>=0;j--,i--)
         {
-            if(board[i][j]==1) 
+            if(board[i][j]==1)
             {
                  res.add(new ArrayList<>(Arrays.asList(i, j)));
-                 break;    
+                 break;
             }
-        }    
+        }
         for(i=r,j=l;j<n && i>=0;j++,i--)
         {
             if(board[i][j]==1)
             {
                 res.add(new ArrayList<>(Arrays.asList(i, j)));
-                break;    
+                break;
             }
         }
         for(i=r,j=l;i<n && j<n;j++,i++)
@@ -90,15 +91,15 @@ class Solution {
             if(board[i][j]==1)
             {
                  res.add(new ArrayList<>(Arrays.asList(i, j)));
-                 break;    
+                 break;
             }
-        }    
+        }
         for(i=r,j=l;j>=0 && i<n;j--,i++)
         {
             if(board[i][j]==1)
             {
                  res.add(new ArrayList<>(Arrays.asList(i, j)));
-                 break;    
+                 break;
             }
         }
         return res;
