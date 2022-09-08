9bd72f9199c0810e17ecd521700c535a0b1bd034
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:45 +0100

    Runtime: 996 ms (Top 33.98%) | Memory: 14.1 MB (Top 94.23%)

--- scripts/algorithms/F/Find Substring With Given Hash Value/Find Substring With Given Hash Value.py
+++ scripts/algorithms/F/Find Substring With Given Hash Value/Find Substring With Given Hash Value.py
@@ -1,3 +1,4 @@
+# Runtime: 996 ms (Top 33.98%) | Memory: 14.1 MB (Top 94.23%)
 class Solution:
     def subStrHash(self, s: str, power: int, mod: int, k: int, hashValue: int) -> str:
         val = lambda ch : ord(ch) - ord("a") + 1
