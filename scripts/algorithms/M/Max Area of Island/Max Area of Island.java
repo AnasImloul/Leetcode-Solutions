56144df0d9d9dfab188c38dffe1331e145d0a74e
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:00 +0100

    Runtime: 93 ms (Top 5.05%) | Memory: 72.8 MB (Top 5.08%)

--- scripts/algorithms/M/Max Area of Island/Max Area of Island.java
+++ scripts/algorithms/M/Max Area of Island/Max Area of Island.java
@@ -1,36 +1,37 @@
+// Runtime: 93 ms (Top 5.05%) | Memory: 72.8 MB (Top 5.08%)
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-      
+
       final int rows=grid.length;
       final int cols=grid[0].length;
       final int[][] dirrections=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-      Map<String,List<int[]>> adj=new HashMap<>(); 
+      Map<String,List<int[]>> adj=new HashMap<>();
       boolean[][] visited=new boolean[rows][cols];
       Queue<String> queue=new LinkedList<>();
       int res=0;
-      
+
       for(int i=0;i<grid.length;i++)
       {
         for(int j=0;j<grid[i].length;j++)
         {
-          
+
           List<int[]> list=new ArrayList<>();
           for(int[] dirrection:dirrections)
           {
             int newRow=dirrection[0]+i;
             int newCol=dirrection[1]+j;
-            
+
             boolean isInBoard=newRow>=rows||newRow<0||newCol>=cols||newCol<0;
             if(!isInBoard)
             {
               list.add(new int[]{newRow,newCol,grid[newRow][newCol]});
             }
           }
-         
+
           adj.put(getNodeStringFormat(i,j,grid[i][j]),list);
         }
-      } 
-        
+      }
+
       for(int i=0;i<rows;i++)
       {
         for(int j=0;j<cols;j++)
@@ -43,10 +44,10 @@ class Solution {
           {
             String currentStr=queue.poll();
             String[] current=currentStr.split(",");
-            
+
             int row=Integer.valueOf(current[0]);
             int col=Integer.valueOf(current[1]);
-            int isLand=Integer.valueOf(current[2]); 
+            int isLand=Integer.valueOf(current[2]);
             if(!adj.containsKey(currentStr))
                 continue;
             if(visited[row][col])
@@ -61,14 +62,14 @@ class Solution {
               int newIsLand=item[2];
               if(!visited[newRow][newCol] && newIsLand==1 && isLand==1)
                   queue.add(getNodeStringFormat(newRow,newCol,newIsLand));
-            } 
+            }
           }
           res=Math.max(res,count);
         }
       }
-      
+
       return res;
-        
+
     }
   private String getNodeStringFormat(int row,int col,int isLand)
   {
