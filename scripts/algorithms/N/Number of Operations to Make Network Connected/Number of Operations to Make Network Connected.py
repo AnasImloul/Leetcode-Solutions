c48f4443ce0832c2a397fdff600b402d4897e6ef
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:44 +0100

    Runtime: 772 ms (Top 55.10%) | Memory: 34.3 MB (Top 72.08%)

--- scripts/algorithms/N/Number of Operations to Make Network Connected/Number of Operations to Make Network Connected.py
+++ scripts/algorithms/N/Number of Operations to Make Network Connected/Number of Operations to Make Network Connected.py
@@ -1,8 +1,9 @@
+# Runtime: 772 ms (Top 55.10%) | Memory: 34.3 MB (Top 72.08%)
 class Solution(object):
     def __init__(self):
         self.parents = []
         self.count = []
-        
+
     def makeConnected(self, n, connections):
         """
         :type n: int
@@ -10,14 +11,14 @@ class Solution(object):
         :rtype: int
         """
         if len(connections) < n-1:
-            return -1  
+            return -1
         self.parents = [i for i in range(n)]
         self.count = [1 for _ in range(n)]
         for connection in connections:
             a, b = connection[0], connection[1]
             self.union(a, b)
         return len({self.find(i) for i in range(n)}) - 1
-            
+
     def find(self, node):
         """
         :type node: int
@@ -26,7 +27,7 @@ class Solution(object):
         while(node != self.parents[node]):
             node = self.parents[node];
         return node
-    
+
     def union(self, a, b):
         """
         :type a: int
@@ -35,7 +36,7 @@ class Solution(object):
         """
         a_parent, b_parent = self.find(a), self.find(b)
         a_size, b_size = self.count[a_parent], self.count[b_parent]
-        
+
         if a_parent != b_parent:
             if a_size < b_size:
                 self.parents[a_parent] = b_parent
