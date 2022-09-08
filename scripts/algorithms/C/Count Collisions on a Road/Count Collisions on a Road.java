c30bbbdeb51b74d3601b10589ccf92805e19405c
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:03 +0100

    Runtime: 22 ms (Top 59.94%) | Memory: 54.1 MB (Top 76.37%)

--- scripts/algorithms/C/Count Collisions on a Road/Count Collisions on a Road.java
+++ scripts/algorithms/C/Count Collisions on a Road/Count Collisions on a Road.java
@@ -1,3 +1,4 @@
+// Runtime: 22 ms (Top 59.94%) | Memory: 54.1 MB (Top 76.37%)
 /*
 cars on left side which are moving in left direction are never going to collide,
 Similarly, cars on right side which are moving right side are never going to collide.
@@ -8,23 +9,23 @@ In between them every car is going to collide.
 class Solution {
     public int countCollisions(String directions) {
         int left = 0, right = directions.length() - 1;
-        
+
         while (left < directions.length() && directions.charAt(left) == 'L') {
             left++;
         }
-        
+
         while (right >= 0 && directions.charAt(right) == 'R') {
             right--;
         }
-        
+
         int count = 0;
         for (int i = left; i <= right; i++) {
             if (directions.charAt(i) != 'S') {
                 count++;
             }
         }
-		//combining these three loops - TC : O(N).
-        
+        //combining these three loops - TC : O(N).
+
         return count;
     }
 }
