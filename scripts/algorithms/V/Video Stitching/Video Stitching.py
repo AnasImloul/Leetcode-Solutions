df682ab4ef1caa949023016dd3515c3b58d01f90
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:49 +0100

    Runtime: 76 ms (Top 9.80%) | Memory: 14.1 MB (Top 5.65%)

--- scripts/algorithms/V/Video Stitching/Video Stitching.py
+++ scripts/algorithms/V/Video Stitching/Video Stitching.py
@@ -1,29 +1,29 @@
+# Runtime: 76 ms (Top 9.80%) | Memory: 14.1 MB (Top 5.65%)
 INF = float("inf")
 
 class Solution:
     def videoStitching(self, clips, time ) :
         clips.sort()
-        
+
         @lru_cache
         def dp(ci, reeled):
-            
-            
+
             if ci > len(clips) - 1:
-                return  INF#
-            
+                return INF#
+
             if clips[ci][0] > reeled:
                 return INF # cantt take this
-            
+
             if clips[ci][1] >= time:
                 return 1
             # takee
             a1 = 1 + dp(ci+1, clips[ci][1])
-            
+
             # not take
             a2 = dp(ci+1, reeled)
-            
+
             return min(a1, a2)
-            
+
         res = dp(0, 0)
         if res == INF:
             return -1
