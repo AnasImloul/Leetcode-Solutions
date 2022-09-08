d47506ce444e4221ed103e6c41a6734ad64142ed
AnasImloul <pydev27@gmail.com>
2022-09-09 00:31:08 +0100

    Runtime: 109 ms (Top 52.36%) | Memory: 47.7 MB (Top 20.94%)

--- scripts/algorithms/H/House Robber III/House Robber III.js
+++ scripts/algorithms/H/House Robber III/House Robber III.js
@@ -1,14 +1,15 @@
+// Runtime: 109 ms (Top 52.36%) | Memory: 47.7 MB (Top 20.94%)
 var rob = function(root) {
-	const dfs = (node = root) => {
-		if (!node || node.val === null) return [0, 0];
-		const { val, left, right } = node;
-		const [robL, notRobL] = dfs(left);
-		const [robR, notRobR] = dfs(right);
-		const rob = val + notRobL + notRobR;
-		const notRob = Math.max(robL, notRobL) + Math.max(robR, notRobR);
+    const dfs = (node = root) => {
+        if (!node || node.val === null) return [0, 0];
+        const { val, left, right } = node;
+        const [robL, notRobL] = dfs(left);
+        const [robR, notRobR] = dfs(right);
+        const rob = val + notRobL + notRobR;
+        const notRob = Math.max(robL, notRobL) + Math.max(robR, notRobR);
 
-		return [rob, notRob];
-	};
+        return [rob, notRob];
+    };
 
-	return Math.max(...dfs());
+    return Math.max(...dfs());
 };
