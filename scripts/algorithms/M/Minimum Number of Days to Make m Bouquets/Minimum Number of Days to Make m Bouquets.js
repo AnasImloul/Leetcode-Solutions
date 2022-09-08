422276163d1b013093d8e0c0df3dabe333bf788d
AnasImloul <pydev27@gmail.com>
2022-09-09 00:31:03 +0100

    Runtime: 139 ms (Top 86.32%) | Memory: 51.9 MB (Top 82.05%)

--- scripts/algorithms/M/Minimum Number of Days to Make m Bouquets/Minimum Number of Days to Make m Bouquets.js
+++ scripts/algorithms/M/Minimum Number of Days to Make m Bouquets/Minimum Number of Days to Make m Bouquets.js
@@ -1,3 +1,4 @@
+// Runtime: 139 ms (Top 86.32%) | Memory: 51.9 MB (Top 82.05%)
 var minDays = function(bloomDay, m, k) {
     if (m * k > bloomDay.length) {
         return -1;
@@ -5,27 +6,27 @@ var minDays = function(bloomDay, m, k) {
 
     let left = 0;
     let right = 0;
-    
+
     for (const day of bloomDay) {
         left = Math.min(day, left);
         right = Math.max(day, right);
     }
 
     let ans = right;
-    
+
     while (left < right) {
         const day = Math.floor((left + right) / 2);
-        
+
         let count = 0;
         let current = 0;
-        
+
         for (let j = 0; j < bloomDay.length; j++) {
             if (bloomDay[j] <= day) {
                 current++;
             } else {
                 current = 0;
             }
-            
+
             if (current === k) {
                 count++;
                 current = 0;
@@ -42,6 +43,6 @@ var minDays = function(bloomDay, m, k) {
             right = day;
         }
     }
-    
+
     return ans;
 };
