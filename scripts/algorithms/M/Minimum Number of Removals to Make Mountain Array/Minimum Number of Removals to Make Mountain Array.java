c44c6ef9d19693407cf6d5c86737f0617bb4cbb4
AnasImloul <pydev27@gmail.com>
2022-09-08 23:40:57 +0100

    Runtime: 103 ms (Top 13.29%) | Memory: 45.9 MB (Top 81.01%)

--- scripts/algorithms/M/Minimum Number of Removals to Make Mountain Array/Minimum Number of Removals to Make Mountain Array.java
+++ scripts/algorithms/M/Minimum Number of Removals to Make Mountain Array/Minimum Number of Removals to Make Mountain Array.java
@@ -1,10 +1,11 @@
+// Runtime: 103 ms (Top 13.29%) | Memory: 45.9 MB (Top 81.01%)
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
-        
+
         int n = nums.length;
         int[] LIS = new int[n];
         int[] LDS = new int[n];
-    
+
         Arrays.fill(LIS, 1);
         Arrays.fill(LDS, 1);
         // calculate the longest increase subsequence (LIS) for every index i
@@ -16,8 +17,8 @@ class Solution {
                     LIS[i] = LIS[j]+1;
             }
         }
-        
-		// calculate the longest decreasing subsequence(LDS) for every index i and keep track of the maximum of LIS+LDS
+
+        // calculate the longest decreasing subsequence(LDS) for every index i and keep track of the maximum of LIS+LDS
         int max = 0;
         for(int i=n-2 ; i >= 0 ; i--)
         {
@@ -26,8 +27,8 @@ class Solution {
                 if(nums[i] > nums[j] && LDS[j]+1 > LDS[i])
                     LDS[i] = LDS[j]+1;
             }
-            
-            if(LIS[i] > 1 && LDS[i] > 1) 
+
+            if(LIS[i] > 1 && LDS[i] > 1)
                 max = Math.max(LIS[i]+LDS[i]-1, max);
         }
         return n - max;
