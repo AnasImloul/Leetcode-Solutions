fd1f3280ee18568b7e237e8ca51b644b2bd8ecbe
AnasImloul <pydev27@gmail.com>
2022-09-08 23:32:47 +0100

    Runtime: 676 ms (Top 17.97%) | Memory: 19.4 MB (Top 70.89%)

--- scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.py
+++ scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.py
@@ -1,5 +1,6 @@
+# Runtime: 676 ms (Top 17.97%) | Memory: 19.4 MB (Top 70.89%)
 class RecentCounter:
-	# Here we use list to store ping details.
+    # Here we use list to store ping details.
     def __init__(self):
         self.store = []
 
@@ -8,9 +9,9 @@ class RecentCounter:
         # So here we append every t. Now in loop how many t from left side < t-3000, we just pop them
         # and return the length of the list, which'd contain elements in range(t-3000, t).
         # And since every t is going to greater than previous, we don't need to think about duplicates.
-	
+
         self.store.append(t)
-        
+
         while self.store[0] < t-3000:
             self.store.pop(0)
 
