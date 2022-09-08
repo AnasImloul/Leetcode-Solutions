64d7ee95e4dc6db04659429dce78f44b7112d116
AnasImloul <pydev27@gmail.com>
2022-09-08 23:41:01 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 42.3 MB (Top 19.55%)

--- scripts/algorithms/R/Remove Palindromic Subsequences/Remove Palindromic Subsequences.java
+++ scripts/algorithms/R/Remove Palindromic Subsequences/Remove Palindromic Subsequences.java
@@ -1,8 +1,9 @@
-class Solution 
+// Runtime: 0 ms (Top 100.00%) | Memory: 42.3 MB (Top 19.55%)
+class Solution
 {
-    public int removePalindromeSub(String s) 
+    public int removePalindromeSub(String s)
     {
-        int left = 0 , right = s.length() - 1 ;       
+        int left = 0 , right = s.length() - 1 ;
         while( left < right )
         {
             if( s.charAt(left++) != s.charAt(right--) )
