ff3262350400e355607d3610cad0fc707cfa063d
AnasImloul <pydev27@gmail.com>
2022-09-09 00:30:59 +0100

    Runtime: 361 ms (Top 36.36%) | Memory: 57.3 MB (Top 63.64%)

--- scripts/algorithms/N/Number of Ways to Arrive at Destination/Number of Ways to Arrive at Destination.js
+++ scripts/algorithms/N/Number of Ways to Arrive at Destination/Number of Ways to Arrive at Destination.js
@@ -1,3 +1,4 @@
+// Runtime: 361 ms (Top 36.36%) | Memory: 57.3 MB (Top 63.64%)
 /**
  * @param {number} n
  * @param {number[][]} roads
@@ -5,7 +6,7 @@
  */
 var countPaths = function(n, roads) {
     let adj = {}, dist = Array(n).fill(Infinity), minHeap = new MinHeap(), count = Array(n).fill(1);
-    
+
     //CREATE ADJ MATRIX
     for(let [from, to , weight] of roads){
         adj[from] = adj[from] || []
@@ -13,21 +14,21 @@ var countPaths = function(n, roads) {
         adj[to] = adj[to] || []
         adj[to].push([from, weight]);
     }
-    
+
     //PUT START IN QUEUE
     dist[n-1] = 0;
     minHeap.enqueue(n-1, 0);
-    
+
     while(minHeap.getSize()){
         let [node, curDist] = minHeap.dequeue();
         let children = adj[node] || [];
         for(let [childNode, childWeight] of children){
-		
-			//IF PATH ALREADY FOUND WITH THIS VALUE THEN ADD COUNT REACHED UNTIL NOW
+
+            //IF PATH ALREADY FOUND WITH THIS VALUE THEN ADD COUNT REACHED UNTIL NOW
             if(curDist + childWeight === dist[childNode]){
-                count[childNode] = (count[childNode] +count[node])  % 1000000007;
+                count[childNode] = (count[childNode] +count[node]) % 1000000007;
             } //IF NOT PATH FOUND YET THEN ADD COUNT OF WAYS TO THE NODE WE CAME FROM
-			else if(curDist + childWeight < dist[childNode]){
+            else if(curDist + childWeight < dist[childNode]){
                 count[childNode] = count[node];
                 dist[childNode] = curDist + childWeight;
                 minHeap.enqueue(childNode, dist[childNode]);
@@ -43,16 +44,16 @@ class MinHeap{
         this.list = list;
         this.build();
     }
-    
+
     build(){
         for(let [val,priority] of this.list)
             this.enqueue(val,priority);
     }
-    
+
     swap(pos1, pos2){
         [this.tree[pos1], this.tree[pos2]] = [this.tree[pos2],this.tree[pos1]]
     }
-    
+
     enqueue(val, priority){
         this.tree[this.tree.length] = [val, priority];
         let i = this.tree.length - 1, parent = ~~(i/2);
@@ -63,7 +64,7 @@ class MinHeap{
             parent = ~~(i/2);
         }
     }
-    
+
     dequeue(){
         let size = this.tree.length - 1, pos = 1;
         if(!size) return;
@@ -73,9 +74,9 @@ class MinHeap{
         this.heapify(pos);
         return deleted;
     }
-    
+
     heapify(pos){
-    
+
         if(pos > this.tree.length) return;
         let leftPos = 2*pos, rightPos = 2*pos +1;
         let left = this.tree[leftPos] ? this.tree[leftPos][1] : Infinity;
@@ -92,11 +93,11 @@ class MinHeap{
             this.heapify(minIndex);
         }
     }
-    
+
     getTree(){
         console.log(this.tree.slice(1));
     }
-    
+
     getSize(){
         return this.tree.length - 1;
     }
