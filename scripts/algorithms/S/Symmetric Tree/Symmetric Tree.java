f7136a102870c10215ac3b5e6f075ac80e15cf83
AnasImloul <pydev27@gmail.com>
2022-09-08 23:40:59 +0100

    Runtime: 1 ms (Top 64.02%) | Memory: 42.7 MB (Top 25.40%)

--- scripts/algorithms/S/Symmetric Tree/Symmetric Tree.java
+++ scripts/algorithms/S/Symmetric Tree/Symmetric Tree.java
@@ -1,8 +1,9 @@
+// Runtime: 1 ms (Top 64.02%) | Memory: 42.7 MB (Top 25.40%)
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return isSymmetric(root.left,root.right);
     }
-    
+
     public boolean isSymmetric(TreeNode rootLeft, TreeNode rootRight) {
         if(rootLeft == null && rootRight == null) {return true;}
         if(rootLeft == null || rootRight == null) {return false;}
