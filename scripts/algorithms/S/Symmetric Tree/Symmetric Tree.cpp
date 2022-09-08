9142ce70390af8ed549ace50fccc7c3a86394a64
AnasImloul <pydev27@gmail.com>
2022-09-08 23:25:31 +0100

    Runtime: 3 ms (Top 94.24%) | Memory: 16.5 MB (Top 47.31%)

--- scripts/algorithms/S/Symmetric Tree/Symmetric Tree.cpp
+++ scripts/algorithms/S/Symmetric Tree/Symmetric Tree.cpp
@@ -1,18 +1,19 @@
+// Runtime: 3 ms (Top 94.24%) | Memory: 16.5 MB (Top 47.31%)
 class Solution {
 public:
-    
+
     bool mirror(TreeNode* root1, TreeNode* root2){
         if(root1==NULL and root2==NULL) return true;
         if(root1==NULL or root2==NULL) return false;
-        
+
         if(root1->val!=root2->val) return false;
-        
+
         return mirror(root1->left, root2->right) and mirror(root1->right,root2->left);
     }
-    
+
     bool isSymmetric(TreeNode* root) {
         if(root==NULL) return true;
-        
+
         return mirror(root->left,root->right);
     }
 };
