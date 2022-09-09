a8e57d7e6374823289a1e27b1d6fd475f492250b
AnasImloul <pydev27@gmail.com>
2022-09-09 15:02:20 +0100

    Runtime: 450 ms (Top 11.11%) | Memory: 53.8 MB (Top 66.67%)

--- scripts/algorithms/S/Sum of Subsequence Widths/Sum of Subsequence Widths.js
+++ scripts/algorithms/S/Sum of Subsequence Widths/Sum of Subsequence Widths.js
@@ -1,3 +1,4 @@
+// Runtime: 450 ms (Top 11.11%) | Memory: 53.8 MB (Top 66.67%)
 var sumSubseqWidths = function(nums) {
     const mod = 1000000007;
     nums.sort((a, b) => a - b), total = 0, power = 1;
@@ -5,12 +6,12 @@ var sumSubseqWidths = function(nums) {
         total = (total + nums[i] * power) % mod;
         power = (power * 2) % mod;
     }
-    
+
     power = 1;
     for(let i = nums.length - 1; i >= 0; i--) {
         total = (total - nums[i] * power + mod) % mod;
         power = (power * 2) % mod;
     }
-    
+
     return (total + mod) % mod
 };
