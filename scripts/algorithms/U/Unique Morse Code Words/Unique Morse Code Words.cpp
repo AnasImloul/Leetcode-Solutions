ddd3be1d6b3429c8eaa6b922abea683f48fed826
AnasImloul <pydev27@gmail.com>
2022-09-09 14:40:21 +0100

    Runtime: 3 ms (Top 85.58%) | Memory: 8.5 MB (Top 62.02%)

--- scripts/algorithms/U/Unique Morse Code Words/Unique Morse Code Words.cpp
+++ scripts/algorithms/U/Unique Morse Code Words/Unique Morse Code Words.cpp
@@ -1,6 +1,7 @@
+// Runtime: 3 ms (Top 85.58%) | Memory: 8.5 MB (Top 62.02%)
 class Solution {
 public:
-  
+
   string convert(string st)
   {
      string s1[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
@@ -8,23 +9,20 @@ public:
     for(char a:st)
     {
       s+=s1[a - 'a'];
-      
+
     }
     return s;
-    
+
   }
     int uniqueMorseRepresentations(vector<string>& words) {
-      
-     set<string>st; 
+
+     set<string>st;
       for(int i=0;i<words.size();i++)
       {
       st.insert(convert(words[i]));
-      
-               
-      
+
       }
-      
-      
+
       return st.size();
     }
 };
