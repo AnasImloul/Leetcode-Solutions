84066ccb749924e29f4d951c8efdfde47ab01d52
AnasImloul <pydev27@gmail.com>
2022-09-09 13:14:18 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 7.2 MB (Top 42.17%)

--- scripts/algorithms/A/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ scripts/algorithms/A/Average Salary Excluding the Minimum and Maximum Salary/Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 7.2 MB (Top 42.17%)
 class Solution {
 public:
     double average(vector<int>& salary) {
@@ -7,7 +8,7 @@ public:
         {
             sum += salary[i];
         }
-        
+
         return sum/(salary.size()-2);
     }
 };
