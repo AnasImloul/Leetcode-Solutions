8022e5a50c54cbb5c31f86f41be7615be544b023
AnasImloul <pydev27@gmail.com>
2022-09-09 14:56:20 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 66.34%)

--- scripts/algorithms/I/Invert Binary Tree/Invert Binary Tree.java
+++ scripts/algorithms/I/Invert Binary Tree/Invert Binary Tree.java
@@ -1,18 +1,19 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.4 MB (Top 66.34%)
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        
+
         swap(root);
         return root;
     }
-	
+
     private static void swap(TreeNode current) {
         if (current == null) {
             return;
         }
-        
+
         swap(current.left);
         swap(current.right);
-        
+
         TreeNode temp = current.left;
         current.left = current.right;
         current.right = temp;
