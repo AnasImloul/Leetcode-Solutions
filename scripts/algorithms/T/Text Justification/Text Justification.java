f215381fdf82c02cda28c122d235196f7567c026
AnasImloul <pydev27@gmail.com>
2022-09-09 14:56:19 +0100

    Runtime: 4 ms (Top 15.97%) | Memory: 42.1 MB (Top 76.75%)

--- scripts/algorithms/T/Text Justification/Text Justification.java
+++ scripts/algorithms/T/Text Justification/Text Justification.java
@@ -1,13 +1,14 @@
+// Runtime: 4 ms (Top 15.97%) | Memory: 42.1 MB (Top 76.75%)
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> unBalanced = new ArrayList<>();
         List<String> balanced = new ArrayList<>();
         int numSpaces = 0;
-        
+
         StringBuffer sb = new StringBuffer();
-		//Following code creates a list of unbalanced lines by appending words and 1 space between them
+        //Following code creates a list of unbalanced lines by appending words and 1 space between them
         for(String word : words){
-            
+
             if(sb.length() == 0){
                 sb.append(word);
             }else{
@@ -18,18 +19,18 @@ class Solution {
                     sb = new StringBuffer(word);
                 }
             }
-            
+
         }
-        
+
         if(sb.length() >0){
             unBalanced.add(sb.toString());
         }
-        
+
         for(int j = 0; j < unBalanced.size(); j++){
             String line = unBalanced.get(j);
             numSpaces = maxWidth - line.length();
             StringBuffer lineB = new StringBuffer(line);
-			//This if block handles either last line or the scenario where in there's only one word in any sentence and hence no spaces
+            //This if block handles either last line or the scenario where in there's only one word in any sentence and hence no spaces
             if(j == unBalanced.size()-1 || !line.contains(" ")){
                 int tempSpaces = maxWidth - lineB.length();
                 while(tempSpaces > 0){
@@ -40,7 +41,7 @@ class Solution {
                 continue;
             };
             // The following block checks for each character and appends 1 space during each loop
-			//If there are still spaces left at the end of the String, again start from beggining and append spaces after each word
+            //If there are still spaces left at the end of the String, again start from beggining and append spaces after each word
             while(numSpaces > 0){
                 int i = 0;
                 while(i < lineB.length() - 1){
@@ -55,7 +56,7 @@ class Solution {
             }
             balanced.add(lineB.toString());
         }
-        
+
         return balanced;
     }
 }
