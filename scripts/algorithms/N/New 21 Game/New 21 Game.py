98a86f82745e52aafe9e9fb0372615229ee7195e
AnasImloul <pydev27@gmail.com>
2022-09-09 13:49:05 +0100

    Runtime: 110 ms (Top 69.47%) | Memory: 14.5 MB (Top 12.98%)

--- scripts/algorithms/N/New 21 Game/New 21 Game.py
+++ scripts/algorithms/N/New 21 Game/New 21 Game.py
@@ -1,3 +1,4 @@
+# Runtime: 110 ms (Top 69.47%) | Memory: 14.5 MB (Top 12.98%)
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
         dp = collections.deque([float(i <= n) for i in range(k, k + maxPts)])
@@ -5,5 +6,5 @@ class Solution:
         for i in range(k):
             dp.appendleft(s / maxPts)
             s += dp[0] - dp.pop()
-        
+
         return dp[0]
