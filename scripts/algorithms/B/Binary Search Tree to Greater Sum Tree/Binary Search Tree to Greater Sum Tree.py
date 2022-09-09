f88e6f0a896d7226e939250ee14bccaf9faa8c2c
AnasImloul <pydev27@gmail.com>
2022-09-09 13:49:03 +0100

    Runtime: 69 ms (Top 6.93%) | Memory: 14.1 MB (Top 33.83%)

--- scripts/algorithms/B/Binary Search Tree to Greater Sum Tree/Binary Search Tree to Greater Sum Tree.py
+++ scripts/algorithms/B/Binary Search Tree to Greater Sum Tree/Binary Search Tree to Greater Sum Tree.py
@@ -1,3 +1,4 @@
+# Runtime: 69 ms (Top 6.93%) | Memory: 14.1 MB (Top 33.83%)
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
         '''
@@ -5,35 +6,35 @@ class Solution:
         Then find prefix sum of the sorted array from the right side - O(n)
         Then do binary search on inorder array and get the prefix sum - O(nlogn) since we do binary search for each node
         '''
-		
-		#find the inorder traversal
+
+        #find the inorder traversal
         def inorder(node,arr):
             if not node: return
-            
+
             inorder(node.left,arr)
             arr.append(node.val)
             inorder(node.right,arr)
-            
+
         arr = []
         inorder(root,arr)
         n = len(arr)
-        
-		#find the prefix sum
+
+        #find the prefix sum
         prefix = [num for num in arr]
         i = n-2
         while i>=0:
             prefix[i] += prefix[i+1]
             i-=1
-            
-		#do binary search and fill in the prefix sum for each node
+
+        #do binary search and fill in the prefix sum for each node
         def fill(node):
             if not node: return
-            
+
             i = bisect_left(arr,node.val)
             node.val = prefix[i]
-            
+
             fill(node.right)
             fill(node.left)
-            
+
         fill(root)
         return root
