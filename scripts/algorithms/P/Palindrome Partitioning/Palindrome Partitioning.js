b7a48605317ae6b2a7e198fe7a68d6a070d8114f
AnasImloul <pydev27@gmail.com>
2022-09-09 15:02:21 +0100

    Runtime: 615 ms (Top 6.03%) | Memory: 86.7 MB (Top 24.65%)

--- scripts/algorithms/P/Palindrome Partitioning/Palindrome Partitioning.js
+++ scripts/algorithms/P/Palindrome Partitioning/Palindrome Partitioning.js
@@ -1,3 +1,4 @@
+// Runtime: 615 ms (Top 6.03%) | Memory: 86.7 MB (Top 24.65%)
 var partition = function(s) {
     let result = []
     backtrack(0, [], s, result)
@@ -19,7 +20,7 @@ function backtrack(i, partition, s, result){
         }
     }
 }
-    
+
 function isPal(str){
     return JSON.stringify(str.split('').reverse()) === JSON.stringify(str.split(''))
 }
