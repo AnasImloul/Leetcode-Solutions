fca9b7b2a1f89d3d2cd536b8f682805a8abac24c
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:19 +0100

    Runtime: 111 ms (Top 29.35%) | Memory: 42.3 MB (Top 82.36%)

--- scripts/algorithms/M/Maximum Product Subarray/Maximum Product Subarray.js
+++ scripts/algorithms/M/Maximum Product Subarray/Maximum Product Subarray.js
@@ -1,3 +1,4 @@
+// Runtime: 111 ms (Top 29.35%) | Memory: 42.3 MB (Top 82.36%)
 /**
  * @param {number[]} nums
  * @return {number}
@@ -6,12 +7,12 @@ var maxProduct = function(nums) {
     const n = nums.length - 1;
     let ans = nums[0];
     let l = 1, r = 1;
-    
+
     for (let i = 0; i < nums.length; i++) {
         l = (l ? l : 1) * nums[i];
         r = (r ? r : 1) * nums[n - i];
         ans = Math.max(ans, Math.max(l, r));
     }
-    
+
     return ans;
 };
