6265b74124c11a9ef43536a268587a98340434f2
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:54 +0100

    Runtime: 793 ms (Top 10.96%) | Memory: 22.9 MB (Top 23.08%)

--- scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.py
+++ scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.py
@@ -1,3 +1,4 @@
+# Runtime: 793 ms (Top 10.96%) | Memory: 22.9 MB (Top 23.08%)
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
         return [k for k,v in Counter(nums).items() if v>=2][::-1]
