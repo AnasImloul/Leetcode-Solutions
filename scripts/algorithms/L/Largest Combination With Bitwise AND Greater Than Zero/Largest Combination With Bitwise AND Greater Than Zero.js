4438839e6278f1be0bf7ff7f96107db4149808ac
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:15 +0100

    Runtime: 174 ms (Top 64.29%) | Memory: 51.5 MB (Top 53.57%)

--- scripts/algorithms/L/Largest Combination With Bitwise AND Greater Than Zero/Largest Combination With Bitwise AND Greater Than Zero.js
+++ scripts/algorithms/L/Largest Combination With Bitwise AND Greater Than Zero/Largest Combination With Bitwise AND Greater Than Zero.js
@@ -1,14 +1,15 @@
+// Runtime: 174 ms (Top 64.29%) | Memory: 51.5 MB (Top 53.57%)
 var largestCombination = function(candidates) {
   const indexArr=Array(24).fill(0)
-   
-  for(let candidate of candidates){  
-      let index =0 
+
+  for(let candidate of candidates){
+      let index =0
       while(candidate>0){
           if((candidate&1)===1)indexArr[index]+=1
           candidate>>>=1
           index++
       }
   }
-  
+
   return Math.max(...indexArr)
 };
