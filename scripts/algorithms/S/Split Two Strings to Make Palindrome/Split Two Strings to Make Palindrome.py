d149300748f09e84547cc97310527366f1e75b9e
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:56 +0100

    Runtime: 271 ms (Top 15.83%) | Memory: 15.4 MB (Top 41.73%)

--- scripts/algorithms/S/Split Two Strings to Make Palindrome/Split Two Strings to Make Palindrome.py
+++ scripts/algorithms/S/Split Two Strings to Make Palindrome/Split Two Strings to Make Palindrome.py
@@ -1,3 +1,4 @@
+# Runtime: 271 ms (Top 15.83%) | Memory: 15.4 MB (Top 41.73%)
 class Solution:
     def checkPalindromeFormation(self, a: str, b: str) -> bool:
         def pal(x):
@@ -8,7 +9,7 @@ class Solution:
         inb = 0
         outa = 0
         outb = len(b)-1
-        
+
         while a[ina] == b[inb]:
             ina -= 1
             inb += 1
@@ -16,16 +17,15 @@ class Solution:
                 return True # short circuit found break point
         # jump into each string now!?
         # is a or b a palindrome in this portion from inb to ina
-        if pal(a[inb:ina+1]) or pal(b[inb:ina+1]): 
+        if pal(a[inb:ina+1]) or pal(b[inb:ina+1]):
             return True # either one is breakpoint, so check remainder is palindrome
 
-        
         while a[outa] == b[outb]:
             outa += 1
             outb -= 1
             if outa >= outb:
                 return True
-        if pal(a[outa:outb+1]) or pal(b[outa:outb+1]): 
+        if pal(a[outa:outb+1]) or pal(b[outa:outb+1]):
             return True # either one is breakpoint, so check remainder
-        
+
         return False
