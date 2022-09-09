96664233ecede20049a2bf19a1945674b3466cde
AnasImloul <pydev27@gmail.com>
2022-09-09 13:29:33 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.8 MB (Top 57.76%)

--- scripts/algorithms/S/Sum of Left Leaves/Sum of Left Leaves.java
+++ scripts/algorithms/S/Sum of Left Leaves/Sum of Left Leaves.java
@@ -1,15 +1,15 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.8 MB (Top 57.76%)
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        
+
         if(root==null)
             return 0;
-        
+
         if(root.left!=null && root.left.left==null && root.left.right==null){
             return root.left.val + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
         }else{
             return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
         }
-        
-        
+
     }
 }
