df90571afabed1322758b0696234d945ba60e3ce
AnasImloul <pydev27@gmail.com>
2022-09-09 14:40:20 +0100

    Runtime: 436 ms (Top 21.75%) | Memory: 68.2 MB (Top 42.59%)

--- scripts/algorithms/F/Find Center of Star Graph/Find Center of Star Graph.cpp
+++ scripts/algorithms/F/Find Center of Star Graph/Find Center of Star Graph.cpp
@@ -1,10 +1,11 @@
+// Runtime: 436 ms (Top 21.75%) | Memory: 68.2 MB (Top 42.59%)
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
         //the center of a graph will be connected to all the edges
         //so keep a track of number of edges for every node
-        //if number of edges is n then it is the center 
-        
+        //if number of edges is n then it is the center
+
             //to store the number of edges for every node
         int n=edges.size();
         vector<int> mp(n+2,0);
@@ -18,6 +19,6 @@ public:
                 return edges[i][1];
         }
         return 0;
-        
+
     }
 };
