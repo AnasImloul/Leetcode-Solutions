c9715f6cfa36e46efb51defdabee99c96ad4e682
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:55 +0100

    Runtime: 68 ms (Top 5.88%) | Memory: 13.7 MB (Top 96.86%)

--- scripts/algorithms/S/Sorting the Sentence/Sorting the Sentence.py
+++ scripts/algorithms/S/Sorting the Sentence/Sorting the Sentence.py
@@ -1,6 +1,7 @@
+# Runtime: 68 ms (Top 5.88%) | Memory: 13.7 MB (Top 96.86%)
 class Solution:
     def sortSentence(self, s: str) -> str:
-        
+
         x = s.split()
         dic = {}
         for i in x :
