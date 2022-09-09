c9342892fb3b53c563ff7d3dc3bcda2500e6c613
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:11 +0100

    Runtime: 251 ms (Top 38.18%) | Memory: 58.2 MB (Top 43.64%)

--- scripts/algorithms/C/Check if Word Can Be Placed In Crossword/Check if Word Can Be Placed In Crossword.js
+++ scripts/algorithms/C/Check if Word Can Be Placed In Crossword/Check if Word Can Be Placed In Crossword.js
@@ -1,3 +1,4 @@
+// Runtime: 251 ms (Top 38.18%) | Memory: 58.2 MB (Top 43.64%)
 /**
  * @param {character[][]} board
  * @param {string} word
@@ -19,14 +20,14 @@ var placeWordInCrossword = function(board, word) {
         }
     }
     return false;
-    
+
     function getFill(x, y, goRight) {
         if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && board[x][y] !== '#') {
             return goRight ? board[x][y] + getFill(x, y + 1, goRight) : board[x][y] + getFill(x + 1, y, goRight);
         }
         return '';
     }
-    
+
     function match(str) {
         if (str.length !== word.length) {
             return false;
