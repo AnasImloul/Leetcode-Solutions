c8a7c26b1bee96d752fb625d8aa901d93ff827a0
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:22 +0100

    Runtime: 109 ms (Top 37.41%) | Memory: 45 MB (Top 18.53%)

--- scripts/algorithms/S/Single Number II/Single Number II.js
+++ scripts/algorithms/S/Single Number II/Single Number II.js
@@ -1,15 +1,16 @@
+// Runtime: 109 ms (Top 37.41%) | Memory: 45 MB (Top 18.53%)
  /**
  * @param {number[]} nums
  * @return {number}
  */
 var singleNumber = function(nums) {
     if(nums.length === 1) return nums[0];
-    
+
     const objNums = {};
     for(var indexI=0; indexI<nums.length; indexI++){
        if(objNums[nums[indexI]] === 2) delete objNums[nums[indexI]];
        else objNums[nums[indexI]] = (objNums[nums[indexI]] || 0) + 1;
     }
-    
+
     return Object.keys(objNums)[0];
 };
