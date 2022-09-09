1fb93a6f67da65fd1beabe1ed2a709407013df1d
AnasImloul <pydev27@gmail.com>
2022-09-09 15:02:21 +0100

    Runtime: 125 ms (Top 32.14%) | Memory: 42.9 MB (Top 73.21%)

--- scripts/algorithms/S/Swap Adjacent in LR String/Swap Adjacent in LR String.js
+++ scripts/algorithms/S/Swap Adjacent in LR String/Swap Adjacent in LR String.js
@@ -1,3 +1,4 @@
+// Runtime: 125 ms (Top 32.14%) | Memory: 42.9 MB (Top 73.21%)
 /**
  * @param {string} start
  * @param {string} end
@@ -6,30 +7,30 @@
 var canTransform = function(start, end) {
     let i = 0;
     let j = 0;
-    
+
     while (i < start.length || j < end.length) {
         if (start[i] === 'X') {
             i++;
             continue;
         }
-        
+
         if (end[j] === 'X') {
             j++;
             continue;
         }
-        
-		// Breaking (1)
+
+        // Breaking (1)
         if (start[i] !== end[j]) return false;
-        
-		// Breaking (2)
+
+        // Breaking (2)
         if (start[i] === 'R' && i > j) return false;
-		
-		// Breaking (3)
+
+        // Breaking (3)
         if (start[i] === 'L' && j > i) return false;
-        
+
         i++;
         j++;
     }
-    
+
     return true;
 };
