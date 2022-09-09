f1c3fe3c3811541cc32d5210d694444fdc315f78
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:21 +0100

    Runtime: 167 ms (Top 96.67%) | Memory: 72.6 MB (Top 50.00%)

--- scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.js
+++ scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.js
@@ -1,19 +1,20 @@
+// Runtime: 167 ms (Top 96.67%) | Memory: 72.6 MB (Top 50.00%)
 var mergeTriplets = function(triplets, target) {
-    
+
     let fst = false, snd = false, thrd = false;
-    
+
     const [t1, t2, t3] = target;
-    
+
     for(let i = 0; i < triplets.length; i++) {
-        
+
         const [a, b, c] = triplets[i];
-        
+
         if(a === t1 && b <= t2 && c <= t3) fst = true;
-        
+
         if(b === t2 && a <= t1 && c <= t3) snd = true;
-        
+
         if(c === t3 && a <= t1 && b <= t2) thrd = true;
-        
+
         if(fst && snd && thrd) return true;
     }
     return false;
