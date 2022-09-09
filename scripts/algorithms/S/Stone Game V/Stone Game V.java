ad70306e0551dc5cebf5633d18765e7f16ac642d
AnasImloul <pydev27@gmail.com>
2022-09-09 14:56:17 +0100

    Runtime: 299 ms (Top 83.10%) | Memory: 42.5 MB (Top 97.18%)

--- scripts/algorithms/S/Stone Game V/Stone Game V.java
+++ scripts/algorithms/S/Stone Game V/Stone Game V.java
@@ -1,3 +1,4 @@
+// Runtime: 299 ms (Top 83.10%) | Memory: 42.5 MB (Top 97.18%)
 class Solution {
     int dp[][];
     public int fnc(int a[], int i, int j, int sum){
@@ -13,10 +14,10 @@ class Solution {
         }
         if(dp[i][j]!=0)
             return dp[i][j];
-        
+
    int temp=0;
         int ans=Integer.MIN_VALUE;
-       
+
         for(int index=i;index<=j;index++){
             temp+=a[index];
             if(temp>sum-temp){
@@ -38,6 +39,6 @@ class Solution {
             sum+=ele;
         dp= new int[n][n];
         return fnc(stoneValue,0,n-1,sum);
-        
+
     }
 }
