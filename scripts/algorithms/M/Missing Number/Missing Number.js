05cebb8cef00f1df11e342f6a31b5b796c23c856
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:09 +0100

    Runtime: 90 ms (Top 77.79%) | Memory: 44.2 MB (Top 70.06%)

--- scripts/algorithms/M/Missing Number/Missing Number.js
+++ scripts/algorithms/M/Missing Number/Missing Number.js
@@ -1,3 +1,4 @@
+// Runtime: 90 ms (Top 77.79%) | Memory: 44.2 MB (Top 70.06%)
 var missingNumber = function(nums) {
     // Initialize res as zero...
     let res = 0;
@@ -6,5 +7,5 @@ var missingNumber = function(nums) {
         // Calculation process by helping of Bit Manipulation...
         res ^= nums[i] ^ (i + 1);
     }
-    return res;     // Return the result
+    return res; // Return the result
 };
