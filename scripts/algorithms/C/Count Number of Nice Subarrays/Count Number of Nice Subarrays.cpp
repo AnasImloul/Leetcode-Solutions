d69b86bc764e43118eda38551913abba741be03b
AnasImloul <pydev27@gmail.com>
2022-09-09 14:40:18 +0100

    Runtime: 196 ms (Top 75.30%) | Memory: 70.9 MB (Top 52.97%)

--- scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.cpp
+++ scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.cpp
@@ -1,26 +1,27 @@
+// Runtime: 196 ms (Top 75.30%) | Memory: 70.9 MB (Top 52.97%)
 class Solution {
 public:
-	vector<int> nums;
-	int solve(int k){
-		int low = 0, high = 0, cnt = 0, res = 0;
+    vector<int> nums;
+    int solve(int k){
+        int low = 0, high = 0, cnt = 0, res = 0;
 
-		while(high < nums.size()){
-			if(nums[high] & 1){
-				cnt++;
+        while(high < nums.size()){
+            if(nums[high] & 1){
+                cnt++;
 
-				while(cnt > k){
-					if(nums[low] & 1) cnt--;
-					low++;
-				}
-			}
-			high++;
-			res += high - low;
-		}
+                while(cnt > k){
+                    if(nums[low] & 1) cnt--;
+                    low++;
+                }
+            }
+            high++;
+            res += high - low;
+        }
 
-		return res;
-	}
-	int numberOfSubarrays(vector<int>& nums, int k) {
-		this -> nums = nums;
-		return solve(k) - solve(k - 1);
-	}
+        return res;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        this -> nums = nums;
+        return solve(k) - solve(k - 1);
+    }
 };
