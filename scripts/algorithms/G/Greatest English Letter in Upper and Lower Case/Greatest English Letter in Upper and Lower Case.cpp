38a69af1b0fd9fab0bd518be72c6300195f486bf
AnasImloul <pydev27@gmail.com>
2022-09-09 13:14:23 +0100

    Runtime: 15 ms (Top 22.24%) | Memory: 6.7 MB (Top 73.98%)

--- scripts/algorithms/G/Greatest English Letter in Upper and Lower Case/Greatest English Letter in Upper and Lower Case.cpp
+++ scripts/algorithms/G/Greatest English Letter in Upper and Lower Case/Greatest English Letter in Upper and Lower Case.cpp
@@ -1,11 +1,12 @@
+// Runtime: 15 ms (Top 22.24%) | Memory: 6.7 MB (Top 73.98%)
 class Solution {
 public:
-    
-    string greatestLetter(string s) 
+
+    string greatestLetter(string s)
     {
         vector<int> low(26), upp(26); //storing occurences of lower and upper case letters
         string res = "";
-    
+
         for(auto it : s) //iterate over each char and mark it in respective vector
         {
             if(it-'A'>=0 && it-'A'<26)
@@ -13,16 +14,16 @@ public:
             else
                 low[it-'a']++;
         }
-        
-        for(int i=25; i>=0; i--) //start from greater char 
+
+        for(int i=25; i>=0; i--) //start from greater char
         {
             if(low[i] && upp[i]) //if char found in upp and low that will be the result
             {
-                res += 'A'+i; 
+                res += 'A'+i;
                 break;
             }
-                
+
         }
-        return res;  
+        return res;
     }
 };
