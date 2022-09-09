34b2c809dc838bec09f2fbf5e9481f22c21b0ed1
AnasImloul <pydev27@gmail.com>
2022-09-09 13:49:00 +0100

    Runtime: 65 ms (Top 7.05%) | Memory: 13.8 MB (Top 68.36%)

--- scripts/algorithms/N/Number of Digit One/Number of Digit One.py
+++ scripts/algorithms/N/Number of Digit One/Number of Digit One.py
@@ -1,13 +1,14 @@
+# Runtime: 65 ms (Top 7.05%) | Memory: 13.8 MB (Top 68.36%)
 class Solution:
     def countDigitOne(self, n: int) -> int:
         num = str(n)[::-1]
         count = 0
         for i in range(len(num)-1, -1, -1):
-            pv = 10**i  # placevalue
+            pv = 10**i # placevalue
             # mulitplicity of current digit (how many times it will be repeated)
             mul = n//(pv*10)
-            rem = n % pv  # remainder of current place value
-            count += mul * pv  # count for number of times 1 occurs in this place when the current digit is considered to be less than 1
+            rem = n % pv # remainder of current place value
+            count += mul * pv # count for number of times 1 occurs in this place when the current digit is considered to be less than 1
             # if the current digit is greater than 1 then additional number of 1's are added to the count (equivalent to the place value)
             if num[i] > '1':
                 count += pv
@@ -15,4 +16,3 @@ class Solution:
             if num[i] == '1':
                 count += rem + 1
         return count
-
