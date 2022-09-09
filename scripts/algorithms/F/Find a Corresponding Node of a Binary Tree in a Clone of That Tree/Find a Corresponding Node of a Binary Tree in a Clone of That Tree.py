c16caaa3a1859aca2f6c62b3b70e810d6aedf97d
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:59 +0100

    Runtime: 1109 ms (Top 30.14%) | Memory: 24.2 MB (Top 19.53%)

--- scripts/algorithms/F/Find a Corresponding Node of a Binary Tree in a Clone of That Tree/Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py
+++ scripts/algorithms/F/Find a Corresponding Node of a Binary Tree in a Clone of That Tree/Find a Corresponding Node of a Binary Tree in a Clone of That Tree.py
@@ -1,3 +1,4 @@
+# Runtime: 1109 ms (Top 30.14%) | Memory: 24.2 MB (Top 19.53%)
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         def DFS(node1,node2):
@@ -5,7 +6,7 @@ class Solution:
                 return node2
             if node1 and node1.left is None and node1.right is None:
                 return
-            
+
             res1 = DFS(node1.left,node2.left) if node1 else None
             if res1 is not None:
                 return res1
