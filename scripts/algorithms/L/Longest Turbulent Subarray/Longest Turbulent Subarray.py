399837d27f1062b3c3d460fc30166e83dd23e281
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:57 +0100

    Runtime: 1152 ms (Top 13.10%) | Memory: 18.7 MB (Top 31.75%)

--- scripts/algorithms/L/Longest Turbulent Subarray/Longest Turbulent Subarray.py
+++ scripts/algorithms/L/Longest Turbulent Subarray/Longest Turbulent Subarray.py
@@ -1,17 +1,18 @@
+# Runtime: 1152 ms (Top 13.10%) | Memory: 18.7 MB (Top 31.75%)
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
         def cmp(a,b):
             if a == b: return 0
             if a > b : return 1
             return -1
-        
+
         n = len(arr)
         ans = 1
         prev = 0
         for i in range(1,n):
             c = cmp(arr[i-1],arr[i])
             if c == 0:
-                # we shift prev to i 
+                # we shift prev to i
                 prev = i
             elif i == n-1 or c * cmp(arr[i],arr[i+1]) != -1:
                 ans = ans if ans > i - prev + 1 else i - prev + 1
