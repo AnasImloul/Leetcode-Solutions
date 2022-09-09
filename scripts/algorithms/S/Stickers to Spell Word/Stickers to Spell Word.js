ef4f0f66f78268126103215375e79b26a18fb980
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:23 +0100

    Runtime: 210 ms (Top 53.57%) | Memory: 46 MB (Top 85.71%)

--- scripts/algorithms/S/Stickers to Spell Word/Stickers to Spell Word.js
+++ scripts/algorithms/S/Stickers to Spell Word/Stickers to Spell Word.js
@@ -1,3 +1,4 @@
+// Runtime: 210 ms (Top 53.57%) | Memory: 46 MB (Top 85.71%)
 /**
  * @param {string[]} stickers
  * @param {string} target
@@ -8,7 +9,7 @@
 var minStickers = function(stickers, target) {
     //* Inits *//
     let dp = new Map();
-    
+
     //* Helpers *//
     const getStringDiff = (str1, str2) => {
         for(let c of str2) {
@@ -17,13 +18,13 @@ var minStickers = function(stickers, target) {
         return str1;
     }
     dp.set('', 0);
-    
-    //* Main *// 
+
+    //* Main *//
     function calcStickers(restStr) {
         // if(restStr === "") return 0; <-- without memoization
         if (dp.has(restStr)) return dp.get(restStr);
         let res = Infinity;
-        
+
         for (let s of stickers.filter(s => s.includes(restStr[0]))) { // if current sticker does not contain the FIRST char, continue
             let str = getStringDiff(restStr, s); //aabbc - bc = aab
             res = Math.min(res, 1 + calcStickers(str)); // RECURSE.... calculate min stickers for the remaining letters.. try combination for all strings and get min
