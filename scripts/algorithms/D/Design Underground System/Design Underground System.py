ca706d075fcd2095a8a61bd8ec8242a005ec501f
AnasImloul <pydev27@gmail.com>
2022-09-09 15:12:27 +0100

    Runtime: 441 ms (Top 30.19%) | Memory: 23.9 MB (Top 87.19%)

--- scripts/algorithms/D/Design Underground System/Design Underground System.py
+++ scripts/algorithms/D/Design Underground System/Design Underground System.py
@@ -1,3 +1,4 @@
+# Runtime: 441 ms (Top 30.19%) | Memory: 23.9 MB (Top 87.19%)
 from collections import defaultdict
 class UndergroundSystem:
     def __init__(self):
@@ -6,7 +7,6 @@ class UndergroundSystem:
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.checkin[id] = (stationName, t)
-        
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         startStation, startTime = self.checkin[id]
@@ -14,6 +14,6 @@ class UndergroundSystem:
         if stationName not in self.traveltime[startStation]:
             self.traveltime[startStation][stationName] = []
         self.traveltime[startStation][stationName].append(t-startTime)
-        
+
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         return sum(self.traveltime[startStation][endStation])/len(self.traveltime[startStation][endStation])
