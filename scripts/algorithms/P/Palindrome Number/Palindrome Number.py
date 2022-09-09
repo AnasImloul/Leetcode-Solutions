1e7832e3790a3a96197abf92efeaacaf1eb1e774
AnasImloul <pydev27@gmail.com>
2022-09-09 13:49:03 +0100

    Runtime: 61 ms (Top 94.40%) | Memory: 13.8 MB (Top 59.53%)

--- scripts/algorithms/P/Palindrome Number/Palindrome Number.py
+++ scripts/algorithms/P/Palindrome Number/Palindrome Number.py
@@ -1,3 +1,4 @@
+# Runtime: 61 ms (Top 94.40%) | Memory: 13.8 MB (Top 59.53%)
 class Solution(object):
    def isPalindrome(self,x):
-	   return str(x) == str(x)[::-1]
+       return str(x) == str(x)[::-1]
