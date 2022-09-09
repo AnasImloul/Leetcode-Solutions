6f461259b43bfb7dbbff81f1c96b1783338c7d1f
AnasImloul <pydev27@gmail.com>
2022-09-09 14:56:22 +0100

    Runtime: 23 ms (Top 33.08%) | Memory: 50 MB (Top 44.62%)

--- scripts/algorithms/V/Vowels of All Substrings/Vowels of All Substrings.java
+++ scripts/algorithms/V/Vowels of All Substrings/Vowels of All Substrings.java
@@ -1,19 +1,20 @@
+// Runtime: 23 ms (Top 33.08%) | Memory: 50 MB (Top 44.62%)
 class Solution {
-    
+
     boolean isVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
-    
+
     public long countVowels(String word) {
         long count = 0;
         int len = word.length();
-        
+
         for(int pos = 0; pos < len; pos++) {
             if(isVowel(word.charAt(pos))) {
                 count += (long)(len - pos) * (long)(pos + 1);
             }
         }
-        
+
         return count;
     }
 }
