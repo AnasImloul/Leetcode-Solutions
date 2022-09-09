e289aab8ce3c327c6aa1b50d23ae5b69e9cb8178
AnasImloul <pydev27@gmail.com>
2022-09-09 14:56:18 +0100

    Runtime: 28 ms (Top 55.58%) | Memory: 124.3 MB (Top 45.11%)

--- scripts/algorithms/M/Maximum Total Importance of Roads/Maximum Total Importance of Roads.java
+++ scripts/algorithms/M/Maximum Total Importance of Roads/Maximum Total Importance of Roads.java
@@ -1,13 +1,14 @@
+// Runtime: 28 ms (Top 55.58%) | Memory: 124.3 MB (Top 45.11%)
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
         long ans = 0, x = 1;
-		long degree[] = new long[n];
+        long degree[] = new long[n];
         for(int road[] : roads){
-            degree[road[0]]++; 
-			degree[road[1]]++;
+            degree[road[0]]++;
+            degree[road[1]]++;
         }
         Arrays.sort(degree);
-        for(long i : degree) ans +=  i * (x++) ;
+        for(long i : degree) ans += i * (x++) ;
         return ans;
     }
 }
