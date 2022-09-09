1be7ce7c04b534d2aea5594767112efd81901fbb
AnasImloul <pydev27@gmail.com>
2022-09-09 15:12:26 +0100

    Runtime: 50 ms (Top 42.69%) | Memory: 13.9 MB (Top 55.15%)

--- scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.py
+++ scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.py
@@ -1,14 +1,14 @@
+# Runtime: 50 ms (Top 42.69%) | Memory: 13.9 MB (Top 55.15%)
 class Solution(object):
     def bitwiseComplement(self, n):
         m = n
         mask = 0
-        
+
         if n==0:
             return 1
         while m:
             mask = (mask << 1) | 1
             m = m>>1
-        
+
         ans = mask & (~n)
         return ans
-        
