6ffcf9b7a48c62315ae62e475870ee23f0925a95
AnasImloul <pydev27@gmail.com>
2022-09-09 15:02:19 +0100

    Runtime: 109 ms (Top 17.20%) | Memory: 41.9 MB (Top 54.94%)

--- scripts/algorithms/F/First Bad Version/First Bad Version.js
+++ scripts/algorithms/F/First Bad Version/First Bad Version.js
@@ -1,10 +1,11 @@
+// Runtime: 109 ms (Top 17.20%) | Memory: 41.9 MB (Top 54.94%)
 /**
  * Definition for isBadVersion()
- * 
+ *
  * @param {integer} version number
  * @return {boolean} whether the version is bad
  * isBadVersion = function(version) {
- *     ...
+ * ...
  * };
  */
 
@@ -17,23 +18,23 @@ var solution = function(isBadVersion) {
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
-    return function(n) {        
+    return function(n) {
         let ceiling = n
         let floor = 1
         let firstBadVersion = -1
-        
+
         while (floor <= ceiling) {
             const middle = Math.floor((ceiling + floor) / 2)
-            
+
             if (isBadVersion(middle)) {
-                
+
                 firstBadVersion = middle
                 ceiling = middle - 1
-            } else  {                
+            } else {
                 floor = middle + 1
             }
         }
-        
+
         return firstBadVersion
     };
 };
