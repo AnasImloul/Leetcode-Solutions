af1a80d4e089e68ca5301c0eb48ace534031ac10
AnasImloul <pydev27@gmail.com>
2022-09-09 16:08:25 +0100

    Runtime: 344 ms (Top 99.56%) | Memory: 108.2 MB (Top 83.84%)

--- scripts/algorithms/M/Map of Highest Peak/Map of Highest Peak.cpp
+++ scripts/algorithms/M/Map of Highest Peak/Map of Highest Peak.cpp
@@ -1,3 +1,4 @@
+// Runtime: 344 ms (Top 99.56%) | Memory: 108.2 MB (Top 83.84%)
 class Solution {
 public:
 
@@ -5,7 +6,7 @@ public:
         int r = isWater.size();
         int c = isWater[0].size();
         queue <pair<int,int>> curr;
-        
+
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 //set the land cell to -1 (not visited)
@@ -19,18 +20,18 @@ public:
                 }
             }
         }
-        
+
         int hill = 0;
         while (!curr.empty()) {
             int len = curr.size();
-            
+
             for (int k = 0; k < len; k++) {
-                
+
                 //for each cell check its 4 boundary cells
                 //if it is not visited, increase its hill by 1
                 pair <int, int> fnt = curr.front(); curr.pop();
                 int i = fnt.first, j = fnt.second;
-                
+
                 //top cell
                 if (i > 0 && isWater[i - 1][j] == -1) {
                     isWater[i - 1][j] = hill + 1;
@@ -52,11 +53,11 @@ public:
                     curr.push({i, j+1});
                 }
             }
-            
+
             //after 1 complete round increase the height of the hill
             hill += 1;
         }
-        
+
         return isWater;
     }
 };
