f4d2492f2ad0b509fb79c99dfcddc8b7f3a03d90
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:12 +0100

    Runtime: 89 ms (Top 60.33%) | Memory: 42.1 MB (Top 68.87%)

--- scripts/algorithms/R/Robot Bounded In Circle/Robot Bounded In Circle.js
+++ scripts/algorithms/R/Robot Bounded In Circle/Robot Bounded In Circle.js
@@ -1,3 +1,4 @@
+// Runtime: 89 ms (Top 60.33%) | Memory: 42.1 MB (Top 68.87%)
 var isRobotBounded = function(instructions) {
         // north = 0, east = 1, south = 2, west = 3
         let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
@@ -6,7 +7,7 @@ var isRobotBounded = function(instructions) {
         // facing north
         let idx = 0;
         let movements = [...instructions];
-        
+
         for (let move of movements) {
             if (move == 'L')
                 idx = (idx + 3) % 4;
@@ -14,10 +15,10 @@ var isRobotBounded = function(instructions) {
                 idx = (idx + 1) % 4;
             else {
                 x += directions[idx][0];
-                y += directions[idx][1];   
-            }    
+                y += directions[idx][1];
+            }
         }
-        
+
         // after one cycle:
         // robot returns into initial position
         // or robot doesn't face north
