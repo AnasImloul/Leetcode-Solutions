b0f3b99059a5dfee3f61dadb5a457ae19326234c
AnasImloul <pydev27@gmail.com>
2022-09-09 13:49:00 +0100

    Runtime: 3346 ms (Top 89.52%) | Memory: 46.9 MB (Top 30.64%)

--- scripts/algorithms/M/Maximum Number of Ways to Partition an Array/Maximum Number of Ways to Partition an Array.py
+++ scripts/algorithms/M/Maximum Number of Ways to Partition an Array/Maximum Number of Ways to Partition an Array.py
@@ -1,16 +1,17 @@
+# Runtime: 3346 ms (Top 89.52%) | Memory: 46.9 MB (Top 30.64%)
 class Solution:
     def waysToPartition(self, nums: List[int], k: int) -> int:
         prefix_sums = list(accumulate(nums))
         total_sum = prefix_sums[-1]
         best = 0
         if total_sum % 2 == 0:
-            best = prefix_sums[:-1].count(total_sum // 2)  # If no change
+            best = prefix_sums[:-1].count(total_sum // 2) # If no change
 
         after_counts = Counter(total_sum - 2 * prefix_sum
                                for prefix_sum in prefix_sums[:-1])
         before_counts = Counter()
 
-        best = max(best, after_counts[k - nums[0]])  # If we change first num
+        best = max(best, after_counts[k - nums[0]]) # If we change first num
 
         for prefix, x in zip(prefix_sums, nums[1:]):
             gap = total_sum - 2 * prefix
