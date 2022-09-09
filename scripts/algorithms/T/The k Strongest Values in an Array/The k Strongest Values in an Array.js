6990ec049734009125dc3c5a00ceb3544057dcd4
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:10 +0100

    Runtime: 645 ms (Top 8.33%) | Memory: 65.4 MB (Top 83.33%)

--- scripts/algorithms/T/The k Strongest Values in an Array/The k Strongest Values in an Array.js
+++ scripts/algorithms/T/The k Strongest Values in an Array/The k Strongest Values in an Array.js
@@ -1,12 +1,13 @@
-var getStrongest = function(arr, k) {    
-	// sort array so we can easily find median
+// Runtime: 645 ms (Top 8.33%) | Memory: 65.4 MB (Top 83.33%)
+var getStrongest = function(arr, k) {
+    // sort array so we can easily find median
     const sorted = arr.sort((a,b) => a-b)
-	// get index of median
-	const medianIndex = Math.floor(((sorted.length-1)/2))
-	// get median
+    // get index of median
+    const medianIndex = Math.floor(((sorted.length-1)/2))
+    // get median
     const median = sorted[medianIndex]
 
-	// custom sort function following the parameters given us in the description
+    // custom sort function following the parameters given us in the description
     const compareFunction = (a, b) => {
         if (Math.abs(a-median) > Math.abs(b-median)) {
             return 1
@@ -17,9 +18,9 @@ var getStrongest = function(arr, k) {
             return -1
         }
     }
-    
-	// sort array using our custom sort function
+
+    // sort array using our custom sort function
     const strongest = arr.sort(compareFunction).reverse().slice(0,k);
-    
+
     return strongest;
 };
