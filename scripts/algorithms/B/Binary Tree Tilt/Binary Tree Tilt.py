63fd2ccde8d7208987ad7027f60a123978b5c683
AnasImloul <pydev27@gmail.com>
2022-09-09 13:48:59 +0100

    Runtime: 66 ms (Top 84.95%) | Memory: 16.4 MB (Top 43.30%)

--- scripts/algorithms/B/Binary Tree Tilt/Binary Tree Tilt.py
+++ scripts/algorithms/B/Binary Tree Tilt/Binary Tree Tilt.py
@@ -1,16 +1,17 @@
+# Runtime: 66 ms (Top 84.95%) | Memory: 16.4 MB (Top 43.30%)
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
     def findTilt(self, root: Optional[TreeNode]) -> int:
         res = [0]
         def tilt_helper(root,res):
-            if not root: 
+            if not root:
                 return 0
-                
+
             left = tilt_helper(root.left,res)
             right = tilt_helper(root.right,res)
 
