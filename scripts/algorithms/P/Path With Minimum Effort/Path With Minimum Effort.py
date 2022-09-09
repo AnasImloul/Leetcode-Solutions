5b72190086d158a9e4c08364f330884f09784d0b
AnasImloul <pydev27@gmail.com>
2022-09-09 15:12:27 +0100

    Runtime: 1804 ms (Top 36.30%) | Memory: 15.4 MB (Top 76.73%)

--- scripts/algorithms/P/Path With Minimum Effort/Path With Minimum Effort.py
+++ scripts/algorithms/P/Path With Minimum Effort/Path With Minimum Effort.py
@@ -1,3 +1,4 @@
+# Runtime: 1804 ms (Top 36.30%) | Memory: 15.4 MB (Top 76.73%)
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         di = (0, 1, 0, -1)
@@ -11,7 +12,7 @@ class Solution:
                 continue
             visited[i][j] = True
             if i + 1 == m and j + 1 == n:
-                return effort  ## have reached the (m-1, n-1) cell
+                return effort ## have reached the (m-1, n-1) cell
             for k in range(4):
                 ii, jj = i + di[k], j + dj[k]
                 if 0 <= ii < m and 0 <= jj < n and not visited[ii][jj]:
