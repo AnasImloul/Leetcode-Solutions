4ba683e8846599d7f5f659c560408c86d0ebbf20
AnasImloul <pydev27@gmail.com>
2022-09-09 16:08:18 +0100

    Runtime: 10 ms (Top 20.79%) | Memory: 10.1 MB (Top 71.20%)

--- scripts/algorithms/F/Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.cpp
+++ scripts/algorithms/F/Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.cpp
@@ -1,9 +1,10 @@
+// Runtime: 10 ms (Top 20.79%) | Memory: 10.1 MB (Top 71.20%)
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int result = -1;
         int first = nums[0];
-        
+
         // check the array for rotated point when it is obtained break the loop and assign result as rotation point
         for(int i = 1; i<nums.size();i++){
             if(nums[i-1]>nums[i]){
