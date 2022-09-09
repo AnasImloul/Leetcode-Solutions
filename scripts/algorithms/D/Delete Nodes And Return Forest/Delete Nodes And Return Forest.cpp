fa0e1840e8acc760919e1c6e135c76ee9562a332
AnasImloul <pydev27@gmail.com>
2022-09-09 16:08:22 +0100

    Runtime: 53 ms (Top 21.57%) | Memory: 25.2 MB (Top 76.05%)

--- scripts/algorithms/D/Delete Nodes And Return Forest/Delete Nodes And Return Forest.cpp
+++ scripts/algorithms/D/Delete Nodes And Return Forest/Delete Nodes And Return Forest.cpp
@@ -1,13 +1,14 @@
+// Runtime: 53 ms (Top 21.57%) | Memory: 25.2 MB (Top 76.05%)
 class Solution {
     void solve(TreeNode* root,TreeNode* prev,unordered_map<int,int>& mp,int left,vector<TreeNode*>& ans){
         if(root==NULL)
             return;
-        
+
         int f=0;
-        
+
         solve(root->left,root,mp,1,ans);
         solve(root->right,root,mp,0,ans);
-        
+
          if(mp.find(root->val)!=mp.end()){
             f=1;
             if(root->right)
@@ -17,7 +18,7 @@ class Solution {
                 ans.push_back(root->left);
                 // cout<<root->right->val<<" ";}
         }
-        
+
         if(f==1){
             if(left)
                 prev->left=NULL;
@@ -25,26 +26,26 @@ class Solution {
                 prev->right=NULL;
         }
     }
-    
+
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         if(root==NULL)
             return {};
-        
+
         unordered_map<int,int> mp;
         for(int i=0;i<to_delete.size();i++)
             mp[to_delete[i]]++;
-        
+
        vector<TreeNode*> ans;
-        
+
         TreeNode* dummy=new TreeNode(-1);
         dummy->left=root;
-        
+
         solve(dummy->left,dummy,mp,1,ans);
         if(dummy->left)
         ans.push_back(root);
-               
+
         return ans;
     }
 };
-		```
+        ```
