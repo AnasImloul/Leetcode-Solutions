7f7c431200f5a87d24283e4c48edc37e21a0951e
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:21 +0100

    Runtime: 196 ms (Top 48.11%) | Memory: 50.6 MB (Top 47.62%)

--- scripts/algorithms/L/Longest Increasing Path in a Matrix/Longest Increasing Path in a Matrix.js
+++ scripts/algorithms/L/Longest Increasing Path in a Matrix/Longest Increasing Path in a Matrix.js
@@ -1,15 +1,16 @@
+// Runtime: 196 ms (Top 48.11%) | Memory: 50.6 MB (Top 47.62%)
 var longestIncreasingPath = function(matrix) {
     const m = matrix.length, n = matrix[0].length;
     const dp = new Array(m).fill(0).map(() => {
         return new Array(n).fill(-1);
     });
-    
+
     let ans = 0;
     const dir = [0, -1, 0, 1, 0];
-    
+
     const dfs = (x, y) => {
         if(dp[x][y] != -1) return dp[x][y];
-        
+
         for(let i = 1; i <= 4; i++) {
             const [nx, ny] = [x + dir[i], y + dir[i - 1]];
             if(
@@ -20,17 +21,17 @@ var longestIncreasingPath = function(matrix) {
                 dp[x][y] = Math.max(dp[x][y], 1 + dfs(nx, ny));
             }
         }
-        
+
         if(dp[x][y] == -1) dp[x][y] = 1;
         ans = Math.max(ans, dp[x][y]);
         return dp[x][y];
     }
-    
+
     for(let i = 0; i < m; i++) {
         for(let j = 0; j < n; j++) {
             if(dp[i][j] == -1) dfs(i, j);
         }
     }
-    
+
     return ans;
 };
