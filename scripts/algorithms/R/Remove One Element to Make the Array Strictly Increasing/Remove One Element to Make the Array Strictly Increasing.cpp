6201ef8d670dad69af3ee70a68c43330fdc9d841
AnasImloul <pydev27@gmail.com>
2022-09-09 14:40:18 +0100

    Runtime: 3 ms (Top 93.99%) | Memory: 10 MB (Top 62.81%)

--- scripts/algorithms/R/Remove One Element to Make the Array Strictly Increasing/Remove One Element to Make the Array Strictly Increasing.cpp
+++ scripts/algorithms/R/Remove One Element to Make the Array Strictly Increasing/Remove One Element to Make the Array Strictly Increasing.cpp
@@ -1,16 +1,17 @@
+// Runtime: 3 ms (Top 93.99%) | Memory: 10 MB (Top 62.81%)
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
 int count = 0;
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] <= nums[i - 1]) {
-                if (count == 1) 
+                if (count == 1)
                     return false;
                 count++;
                 if (i > 1 && nums[i] <= nums[i - 2] )
                     nums[i] = nums[i - 1];
             }
         }
-        return true;   
+        return true;
     }
 };
