45800d95da8c43ab08fec9608a87c84c0f27dbfd
AnasImloul <pydev27@gmail.com>
2022-09-09 13:39:19 +0100

    Runtime: 330 ms (Top 57.14%) | Memory: 59.4 MB (Top 77.14%)

--- scripts/algorithms/D/Divide Array in Sets of K Consecutive Numbers/Divide Array in Sets of K Consecutive Numbers.js
+++ scripts/algorithms/D/Divide Array in Sets of K Consecutive Numbers/Divide Array in Sets of K Consecutive Numbers.js
@@ -1,14 +1,15 @@
+// Runtime: 330 ms (Top 57.14%) | Memory: 59.4 MB (Top 77.14%)
 var isPossibleDivide = function(nums, k) {
     if(nums.length % k) {
         return false;
     }
-    
+
     nums.sort((a, b) => a - b);
-    
+
     let numberOfArrays = nums.length / k, index = 0, dp = Array(numberOfArrays).fill(null).map(() => []);
-    
+
     dp[0].push(nums[0]);
-    
+
     for(let i = 1; i < nums.length; i++) {
         if(nums[i] === nums[i - 1]) {
             if(index === numberOfArrays - 1) {
@@ -27,6 +28,6 @@ var isPossibleDivide = function(nums, k) {
         }
         dp[index].push(nums[i])
     }
-    
+
     return true;
 };
