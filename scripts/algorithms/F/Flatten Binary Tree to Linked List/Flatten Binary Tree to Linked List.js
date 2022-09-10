50caa16d56991d7733c770df4b1fbf150ca0c08c
AnasImloul <pydev27@gmail.com>
2022-09-10 14:29:01 +0100

    Runtime: 134 ms (Top 14.04%) | Memory: 45 MB (Top 18.98%)

--- scripts/algorithms/F/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List.js
+++ scripts/algorithms/F/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List.js
@@ -1,9 +1,10 @@
+// Runtime: 134 ms (Top 14.04%) | Memory: 45 MB (Top 18.98%)
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -13,23 +14,23 @@
 var flatten = function(root) {
     const dfs = (node) => {
         if (!node) return
-        
+
         if (!node.left && !node.right) return node
-        
+
         const leftNode = node.left
         const rightNode = node.right
-        
+
         const leftTree = dfs(leftNode)
         const rightTree = dfs(rightNode)
-        
+
         if (leftTree) leftTree.right = rightNode
-        
+
         node.left = null
         node.right = leftNode || rightNode
-        
+
         return rightTree || leftTree
     }
-    
+
     dfs(root)
     return root
 };
