2cd252426c0bdb134494a0b18f2e593b6c52962a
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:40 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 87.06%)

--- scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.cpp
+++ scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.cpp
@@ -1,22 +1,23 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 5.8 MB (Top 87.06%)
 class Solution {
 public:
-	bool isPerfectSquare(int num) {
+    bool isPerfectSquare(int num) {
 
-		if (num == 1)return true;
-		long long l = 1, h = num / 2;
+        if (num == 1)return true;
+        long long l = 1, h = num / 2;
 
-		while (l <= h) {
-			long long mid = l + (h - l) / 2;
-			long long midSqr = mid * mid;
+        while (l <= h) {
+            long long mid = l + (h - l) / 2;
+            long long midSqr = mid * mid;
 
-			if (midSqr == num) return true;
+            if (midSqr == num) return true;
 
-			if (num < midSqr) {
-				h = mid - 1;
-			} else {
-				l = mid + 1;
-			}
-		}
-		return false;
-	}
+            if (num < midSqr) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return false;
+    }
 };
