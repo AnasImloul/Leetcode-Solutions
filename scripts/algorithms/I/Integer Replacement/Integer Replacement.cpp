55a1579ce009d7c821de2a511d12aaf8ae603eec
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:38 +0100

    Runtime: 11 ms (Top 9.31%) | Memory: 5.9 MB (Top 81.51%)

--- scripts/algorithms/I/Integer Replacement/Integer Replacement.cpp
+++ scripts/algorithms/I/Integer Replacement/Integer Replacement.cpp
@@ -1,7 +1,8 @@
+// Runtime: 11 ms (Top 9.31%) | Memory: 5.9 MB (Top 81.51%)
 class Solution {
 public:
     int integerReplacement(int n) {
-        
+
         return helper(n);
     }
 
@@ -9,10 +10,10 @@ public:
     {
         if(n == 1)
             return 0;
-        
+
         if(n % 2)
             return 1 + min(helper(n - 1), helper(n + 1));
-        
+
         return 1 + helper(n / 2);
     }
 };
