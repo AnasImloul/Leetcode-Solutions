62b7c9f31d6e1773626b53f05568a4d140a9cfdf
AnasImloul <pydev27@gmail.com>
2022-09-10 14:29:01 +0100

    Runtime: 2912 ms (Top 12.50%) | Memory: 51.4 MB (Top 50.00%)

--- scripts/algorithms/S/Stone Game V/Stone Game V.js
+++ scripts/algorithms/S/Stone Game V/Stone Game V.js
@@ -1,16 +1,17 @@
+// Runtime: 2912 ms (Top 12.50%) | Memory: 51.4 MB (Top 50.00%)
 var stoneGameV = function(stoneValue) {
     // Find the stoneValue array's prefix sum
     let prefix = Array(stoneValue.length).fill(0);
     for (let i = 0; i < stoneValue.length; i++) {
         prefix[i] = stoneValue[i] + (prefix[i - 1] || 0);
     }
-    
+
     let dp = Array(stoneValue.length).fill().map(() => Array(stoneValue.length).fill(0));
-    
+
     function game(start, end) {
         if (dp[start][end]) return dp[start][end];
         if (start === end) return 0;
-        
+
         let max = 0;
         for (let i = start + 1; i <= end; i++) {
             let sumL = prefix[i - 1] - (prefix[start - 1] || 0);
@@ -27,6 +28,6 @@ var stoneGameV = function(stoneValue) {
             }
         } return dp[start][end] = max;
     }
-    
+
     return game(0, stoneValue.length - 1);
 };
