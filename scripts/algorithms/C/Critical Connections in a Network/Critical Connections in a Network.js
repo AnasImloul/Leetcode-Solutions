55f743df89edf3cb663425bcabfba7968c0e32bf
AnasImloul <pydev27@gmail.com>
2022-09-10 14:29:05 +0100

    Runtime: 731 ms (Top 93.55%) | Memory: 113.8 MB (Top 87.10%)

--- scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.js
+++ scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.js
@@ -1,29 +1,30 @@
+// Runtime: 731 ms (Top 93.55%) | Memory: 113.8 MB (Top 87.10%)
 var criticalConnections = function(n, connections) {
     // Graph Formation
     const graph = new Map();
     connections.forEach(([a, b]) => {
         const aconn = graph.get(a) || [];
         const bconn = graph.get(b) || [];
-        
+
         aconn.push(b), bconn.push(a);
         graph.set(a, aconn);
         graph.set(b, bconn);
     });
-    
+
     // Find Bridges
     const bridges = [];
     const visited = new Array(n).fill(false);
-    const tin     = new Array(n).fill(-1);
-    const dis     = new Array(n).fill(-1);
-    
-    let time      = 0;
-    
+    const tin = new Array(n).fill(-1);
+    const dis = new Array(n).fill(-1);
+
+    let time = 0;
+
     const dfs = (node, parent = -1) => {
         visited[node] = true;
         ++time;
         tin[node] = time;
         dis[node] = time;
-        
+
         const connections = graph.get(node);
         for(let conn of connections) {
             if(conn == parent) {
@@ -40,8 +41,8 @@ var criticalConnections = function(n, connections) {
             }
         };
     }
-    
+
     dfs(0);
-    
+
     return bridges;
 };
