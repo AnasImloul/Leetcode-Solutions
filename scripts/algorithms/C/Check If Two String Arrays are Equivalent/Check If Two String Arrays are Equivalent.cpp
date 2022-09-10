149ff203bb5a2cea3ec633199c805b001b010b42
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:53 +0100

    Runtime: 18 ms (Top 5.29%) | Memory: 11.7 MB (Top 29.17%)

--- scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.cpp
+++ scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.cpp
@@ -1,3 +1,4 @@
+// Runtime: 18 ms (Top 5.29%) | Memory: 11.7 MB (Top 29.17%)
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
@@ -6,10 +7,10 @@ public:
             a += x;
         for(auto x:word2)
             b += x;
-       
+
         if(a.compare(b)==0)
             return 1;
-        
+
         return 0;
     }
 };
