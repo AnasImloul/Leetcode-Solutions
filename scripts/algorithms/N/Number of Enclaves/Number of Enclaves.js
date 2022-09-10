40018ac88c57bd9a1f605ac04bac490867bfaa1a
AnasImloul <pydev27@gmail.com>
2022-09-10 14:28:53 +0100

    Runtime: 170 ms (Top 26.04%) | Memory: 54.3 MB (Top 16.57%)

--- scripts/algorithms/N/Number of Enclaves/Number of Enclaves.js
+++ scripts/algorithms/N/Number of Enclaves/Number of Enclaves.js
@@ -1,11 +1,12 @@
+// Runtime: 170 ms (Top 26.04%) | Memory: 54.3 MB (Top 16.57%)
 /**
  * @param {number[][]} grid
  * @return {number}
  */
 var numEnclaves = function(grid) {
-    
+
     let count = 0, rowLength = grid.length, colLength = grid[0].length
-    
+
     const updateBoundaryLand = (row,col) => {
         if(grid?.[row]?.[col]){
             grid[row][col] = 0
@@ -16,29 +17,27 @@ var numEnclaves = function(grid) {
 
         }
     }
-  
-   
-    
+
     for(let i=0;i<rowLength;i++){
         if(grid[i][0] || grid[i][colLength-1]){
             updateBoundaryLand(i,0)
             updateBoundaryLand(i,colLength-1)
         }
     }
-    
+
     for(let j=0;j<colLength;j++){
         if(grid[0][j] || grid[rowLength-1][j]){
             updateBoundaryLand(0,j)
             updateBoundaryLand(rowLength-1,j)
         }
     }
-    
+
     for(let i=0;i<rowLength;i++){
         for(let j=0;j<colLength;j++){
             if(grid[i][j]) count++
         }
     }
-    
+
     return count
 
 };
