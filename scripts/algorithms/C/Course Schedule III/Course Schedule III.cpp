d357e4a13a1b993a3cf428f3808309948e56c3d1
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:29 +0100

    Runtime: 280 ms (Top 87.39%) | Memory: 56.3 MB (Top 66.94%)

--- scripts/algorithms/C/Course Schedule III/Course Schedule III.cpp
+++ scripts/algorithms/C/Course Schedule III/Course Schedule III.cpp
@@ -1,3 +1,4 @@
+// Runtime: 280 ms (Top 87.39%) | Memory: 56.3 MB (Top 66.94%)
 class Solution {
 public:
     bool static cmp(vector<int> &a,vector<int>&b) {
@@ -8,11 +9,11 @@ public:
          int sm = 0;
          priority_queue<int> pq;
          for(int i=0; i<courses.size(); i++) {
-               pq.push(courses[i][0]);  
+               pq.push(courses[i][0]);
                sm+=courses[i][0]; // sum of duration of courses
                // when there exists a course duration that is invalid and can't be completed on that day!
-               if(sm>courses[i][1]) { 
-                    sm-=pq.top();  // remove the biggest invalid course duration!
+               if(sm>courses[i][1]) {
+                    sm-=pq.top(); // remove the biggest invalid course duration!
                     pq.pop();
                }
          }
