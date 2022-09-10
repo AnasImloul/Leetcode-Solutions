e62ae0b4d41edd393938fee8c976dbcc3f15799d
AnasImloul <pydev27@gmail.com>
2022-09-10 14:53:46 +0100

    Runtime: 47 ms (Top 54.94%) | Memory: 13.9 MB (Top 58.35%)

--- scripts/algorithms/L/Longest Uncommon Subsequence I/Longest Uncommon Subsequence I.py
+++ scripts/algorithms/L/Longest Uncommon Subsequence I/Longest Uncommon Subsequence I.py
@@ -1,7 +1,8 @@
+# Runtime: 47 ms (Top 54.94%) | Memory: 13.9 MB (Top 58.35%)
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
         if a == b:
             return -1
-        
+
         else:
             return max(len(a), len(b))
