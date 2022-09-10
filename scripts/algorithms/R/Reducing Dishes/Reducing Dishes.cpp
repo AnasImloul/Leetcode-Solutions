d0fe9e4b045a506e4a1374df9a85a9c0eab780f4
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:42 +0100

    Runtime: 10 ms (Top 77.23%) | Memory: 8.1 MB (Top 73.16%)

--- scripts/algorithms/R/Reducing Dishes/Reducing Dishes.cpp
+++ scripts/algorithms/R/Reducing Dishes/Reducing Dishes.cpp
@@ -1,8 +1,9 @@
+// Runtime: 10 ms (Top 77.23%) | Memory: 8.1 MB (Top 73.16%)
 class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end(), greater <int> () );
-        if (satisfaction[0] <=  0) return 0; 
+        if (satisfaction[0] <= 0) return 0;
         int ans = 0;
         int total = 0;
         for (int i = 0; i < satisfaction.size(); i++) {
@@ -10,8 +11,8 @@ public:
                 ans = ans + satisfaction[i];
                 total = total + ans;
                 continue;
-                
-            } 
+
+            }
             return total;
         }
         return total;
