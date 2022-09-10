3b37f324d6107277bdbaf787b6aa60c655c6184c
AnasImloul <pydev27@gmail.com>
2022-09-10 14:28:55 +0100

    Runtime: 94 ms (Top 40.52%) | Memory: 41.3 MB (Top 100.00%)

--- scripts/algorithms/N/Number of Days Between Two Dates/Number of Days Between Two Dates.js
+++ scripts/algorithms/N/Number of Days Between Two Dates/Number of Days Between Two Dates.js
@@ -1,3 +1,4 @@
+// Runtime: 94 ms (Top 40.52%) | Memory: 41.3 MB (Top 100.00%)
 var daysBetweenDates = function(date1, date2) {
      let miliSecondInaDay = 24*60*60*1000;
      if(date1>date2) return (new Date(date1) - new Date(date2)) / miliSecondInaDay
