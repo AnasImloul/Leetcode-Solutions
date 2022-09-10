174d5171100f734737d1d68b9e3e4ae2ee654232
AnasImloul <pydev27@gmail.com>
2022-09-10 14:53:41 +0100

    Runtime: 437 ms (Top 8.80%) | Memory: 15.9 MB (Top 13.77%)

--- scripts/algorithms/A/Assign Cookies/Assign Cookies.py
+++ scripts/algorithms/A/Assign Cookies/Assign Cookies.py
@@ -1,15 +1,16 @@
+# Runtime: 437 ms (Top 8.80%) | Memory: 15.9 MB (Top 13.77%)
 class Solution:
-	def findContentChildren(self, g: List[int], s: List[int]) -> int:
-		g.sort()
-		s.sort()
-		cont = 0
-		c = 0
-		k = 0
-		while  k< len(s) and c < len(g):
-			if s[k] >= g[c]:
-				c+=1
-				k+=1
-				cont+=1
-			else:
-				k+=1
-		return cont
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        cont = 0
+        c = 0
+        k = 0
+        while k< len(s) and c < len(g):
+            if s[k] >= g[c]:
+                c+=1
+                k+=1
+                cont+=1
+            else:
+                k+=1
+        return cont
