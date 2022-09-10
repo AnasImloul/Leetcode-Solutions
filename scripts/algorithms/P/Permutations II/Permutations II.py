c7083a67dd4f1f7fd105b508b7af1fcc3e09326c
AnasImloul <pydev27@gmail.com>
2022-09-10 14:53:16 +0100

    Runtime: 136 ms (Top 31.20%) | Memory: 14.2 MB (Top 62.53%)

--- scripts/algorithms/P/Permutations II/Permutations II.py
+++ scripts/algorithms/P/Permutations II/Permutations II.py
@@ -1,3 +1,4 @@
+# Runtime: 136 ms (Top 31.20%) | Memory: 14.2 MB (Top 62.53%)
 class Solution(object):
     def permuteUnique(self, nums):
         """
@@ -6,7 +7,7 @@ class Solution(object):
         """
         if len(nums) == 1:
             return [[nums[0]]]
-        
+
         res = self.permuteUnique(nums[1:])
 
         for i in range(len(res)-1, -1 , -1):
@@ -18,7 +19,7 @@ class Solution(object):
                 lst.insert(j, nums[0])
                 res.append(lst)
                 j += 1
-				
+
             res[i].insert(j,nums[0])
-                
+
         return res
