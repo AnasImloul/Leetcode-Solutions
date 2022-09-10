ba362a221129324267d50b79fe453e34f73c2d78
AnasImloul <pydev27@gmail.com>
2022-09-10 14:53:39 +0100

    Runtime: 8217 ms (Top 5.03%) | Memory: 52.2 MB (Top 99.70%)

--- scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.py
+++ scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.py
@@ -1,6 +1,7 @@
+# Runtime: 8217 ms (Top 5.03%) | Memory: 52.2 MB (Top 99.70%)
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        
+
         i=0
         while True:
             if i==len(triplets):
@@ -11,11 +12,11 @@ class Solution:
                     i-=1
                     break
             i+=1
-        
+
         a = [x[0] for x in triplets]
         b = [x[1] for x in triplets]
         c = [x[2] for x in triplets]
-        
+
         if target[0] not in a:
             return False
         if target[1] not in b:
