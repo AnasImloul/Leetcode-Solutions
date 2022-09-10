5830f3a39e9e75ac90be91ae3e4b7a9aa6bd5286
AnasImloul <pydev27@gmail.com>
2022-09-10 14:28:54 +0100

    Runtime: 120 ms (Top 95.62%) | Memory: 45.3 MB (Top 37.23%)

--- scripts/algorithms/E/Execution of All Suffix Instructions Staying in a Grid/Execution of All Suffix Instructions Staying in a Grid.js
+++ scripts/algorithms/E/Execution of All Suffix Instructions Staying in a Grid/Execution of All Suffix Instructions Staying in a Grid.js
@@ -1,10 +1,11 @@
-//     Time: O(n^2)
+// Runtime: 120 ms (Top 95.62%) | Memory: 45.3 MB (Top 37.23%)
+// Time: O(n^2)
 var executeInstructions = function(n, startPos, s) {
-    let answers = []; 
-    for (i = 0; i < s.length; i++) { 
+    let answers = [];
+    for (i = 0; i < s.length; i++) {
         let movement = 0;
         let [row, col] = startPos;
-        for (j = i; j < s.length; j++) { 
+        for (j = i; j < s.length; j++) {
             if (s[j] == "R") col++;
             else if (s[j] == "L") col--;
             else if (s[j] == "D") row++;
