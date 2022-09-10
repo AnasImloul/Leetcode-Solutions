378f8cb4eb180abeff39d4b939ae62c0901995b0
AnasImloul <pydev27@gmail.com>
2022-09-10 14:28:50 +0100

    Runtime: 51 ms (Top 100.00%) | Memory: 43.2 MB (Top 29.03%)

--- scripts/algorithms/S/Splitting a String Into Descending Consecutive Values/Splitting a String Into Descending Consecutive Values.js
+++ scripts/algorithms/S/Splitting a String Into Descending Consecutive Values/Splitting a String Into Descending Consecutive Values.js
@@ -1,16 +1,17 @@
+// Runtime: 51 ms (Top 100.00%) | Memory: 43.2 MB (Top 29.03%)
 /**
  * @param {string} s
  * @return {boolean}
  */
 var splitString = function(s) {
-    
+
     const backtracking = (index, prevStringValue) => {
         if(index === s.length) {
             return true;
         }
-        for(let i = index; i < s.length; i++) {    
+        for(let i = index; i < s.length; i++) {
             const currStringValue = s.slice(index ,i + 1);
-            
+
             if(parseInt(prevStringValue, 10) === parseInt(currStringValue, 10) + 1) {
                 if(backtracking(i + 1, currStringValue)) {
                     return true;
