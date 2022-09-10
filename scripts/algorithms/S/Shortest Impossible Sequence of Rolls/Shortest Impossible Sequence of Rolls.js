0dca735787aef30f3775730ffeed88a04c3d02f5
AnasImloul <pydev27@gmail.com>
2022-09-10 14:29:09 +0100

    Runtime: 217 ms (Top 10.77%) | Memory: 58.9 MB (Top 49.23%)

--- scripts/algorithms/S/Shortest Impossible Sequence of Rolls/Shortest Impossible Sequence of Rolls.js
+++ scripts/algorithms/S/Shortest Impossible Sequence of Rolls/Shortest Impossible Sequence of Rolls.js
@@ -1,8 +1,9 @@
-var shortestSequence = function(rolls, k) 
+// Runtime: 217 ms (Top 10.77%) | Memory: 58.9 MB (Top 49.23%)
+var shortestSequence = function(rolls, k)
 {
     let ans=1;
     let sett=new Set();
-    
+
     for(let i of rolls)
         {
             sett.add(i);
