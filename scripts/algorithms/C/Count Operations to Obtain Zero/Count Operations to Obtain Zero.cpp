b5126a548bc62710d44333f72e958d5e205805a4
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:49 +0100

    Runtime: 8 ms (Top 8.75%) | Memory: 7.5 MB (Top 7.30%)

--- scripts/algorithms/C/Count Operations to Obtain Zero/Count Operations to Obtain Zero.cpp
+++ scripts/algorithms/C/Count Operations to Obtain Zero/Count Operations to Obtain Zero.cpp
@@ -1,9 +1,10 @@
+// Runtime: 8 ms (Top 8.75%) | Memory: 7.5 MB (Top 7.30%)
 class Solution {
 public:
     int countOperations(int num1, int num2) {
         if(num1==0 || num2==0) return 0;
-        if(num1>=num2)   num1=num1-num2;
-        else if(num2>num1)   num2=num2-num1;
+        if(num1>=num2) num1=num1-num2;
+        else if(num2>num1) num2=num2-num1;
         return 1+countOperations(num1,num2);
     }
 };
