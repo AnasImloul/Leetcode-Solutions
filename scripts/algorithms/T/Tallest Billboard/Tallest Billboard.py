b7596bf0c3af8aeecfbc53b694bf13f23601938b
AnasImloul <pydev27@gmail.com>
2022-09-10 14:53:35 +0100

    Runtime: 760 ms (Top 68.15%) | Memory: 14.5 MB (Top 56.05%)

--- scripts/algorithms/T/Tallest Billboard/Tallest Billboard.py
+++ scripts/algorithms/T/Tallest Billboard/Tallest Billboard.py
@@ -1,3 +1,4 @@
+# Runtime: 760 ms (Top 68.15%) | Memory: 14.5 MB (Top 56.05%)
 
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
@@ -6,15 +7,15 @@ class Solution:
         for x in rods:
             nxt = dp.copy()
             for d, y in dp.items():
-                # init state 
-                # ------|----- d -----|      # tall side 
-                # - y --|                    # low  side
+                # init state
+                # ------|----- d -----| # tall side
+                # - y --| # low side
 
-                # put x to tall side 
+                # put x to tall side
                 # ------|----- d -----|---- x --|
                 # - y --|
                 nxt[d + x] = max(nxt.get(x + d, 0), y)
-               
+
                 nxt[abs(d - x)] = max(nxt.get(abs(d - x), 0), y + min(d, x))
             dp = nxt
         return dp[0]
