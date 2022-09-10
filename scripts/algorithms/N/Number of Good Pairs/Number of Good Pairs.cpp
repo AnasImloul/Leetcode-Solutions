bdbf0ba46767e1a8abd0424b9418ea34b0e3fff8
AnasImloul <pydev27@gmail.com>
2022-09-10 15:04:44 +0100

    Runtime: 8 ms (Top 7.86%) | Memory: 7.2 MB (Top 60.31%)

--- scripts/algorithms/N/Number of Good Pairs/Number of Good Pairs.cpp
+++ scripts/algorithms/N/Number of Good Pairs/Number of Good Pairs.cpp
@@ -1,16 +1,17 @@
+// Runtime: 8 ms (Top 7.86%) | Memory: 7.2 MB (Top 60.31%)
 class Solution {
 public:
-	int numIdenticalPairs(vector<int>& nums) {
+    int numIdenticalPairs(vector<int>& nums) {
 
-		int cnt = 0;
-		for(int i=0 ; i<nums.size() ; i++){
-			for(int j=i+1 ; j<nums.size() ; j++){
-				if(nums[i] == nums[j]){
-					cnt++;
-				}
-			}
-		}
+        int cnt = 0;
+        for(int i=0 ; i<nums.size() ; i++){
+            for(int j=i+1 ; j<nums.size() ; j++){
+                if(nums[i] == nums[j]){
+                    cnt++;
+                }
+            }
+        }
 
-		return cnt;
-	}
+        return cnt;
+    }
 };
