615cf3ec79e23f5d570dd3022da7e3d2f117d796
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:53 +0100

    Runtime: 3520 ms (Top 6.01%) | Memory: 173.8 MB (Top 5.23%)

--- scripts/algorithms/D/Design HashSet/Design HashSet.py
+++ scripts/algorithms/D/Design HashSet/Design HashSet.py
@@ -1,16 +1,14 @@
+# Runtime: 3520 ms (Top 6.01%) | Memory: 173.8 MB (Top 5.23%)
 class MyHashSet:
 
     def __init__(self):
         self.hash_list = [0]*10000000
-        
 
     def add(self, key: int) -> None:
         self.hash_list[key]+=1
-        
 
     def remove(self, key: int) -> None:
         self.hash_list[key] = 0
-        
 
     def contains(self, key: int) -> bool:
         if self.hash_list[key] > 0:
