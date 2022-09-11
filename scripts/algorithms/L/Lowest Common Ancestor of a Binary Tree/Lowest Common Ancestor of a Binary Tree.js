66293595a268b23804ac29abe20dad623d389fca
AnasImloul <pydev27@gmail.com>
2022-09-11 14:00:20 +0100

    Runtime: 139 ms (Top 26.90%) | Memory: 51.6 MB (Top 71.61%)

--- scripts/algorithms/L/Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.js
+++ scripts/algorithms/L/Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.js
@@ -1,6 +1,7 @@
+// Runtime: 139 ms (Top 26.90%) | Memory: 51.6 MB (Top 71.61%)
 var lowestCommonAncestor = function(root, p, q) {
     if(!root || root.val == p.val || root.val == q.val) return root;
-    
+
     let left = lowestCommonAncestor(root.left, p, q);
     let right = lowestCommonAncestor(root.right, p, q);
 
