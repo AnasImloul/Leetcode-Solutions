02626121d0c3b40b8c4d223f4544da8eac467af1
AnasImloul <pydev27@gmail.com>
2022-09-11 14:00:16 +0100

    Runtime: 115 ms (Top 13.51%) | Memory: 41.8 MB (Top 91.22%)

--- scripts/algorithms/R/Remove Digit From Number to Maximize Result/Remove Digit From Number to Maximize Result.js
+++ scripts/algorithms/R/Remove Digit From Number to Maximize Result/Remove Digit From Number to Maximize Result.js
@@ -1,10 +1,11 @@
+// Runtime: 115 ms (Top 13.51%) | Memory: 41.8 MB (Top 91.22%)
 /**
  * @param {string} number
  * @param {character} digit
  * @return {string}
  */
 var removeDigit = function(number, digit) {
-    
+
     let str = [];
     let flag = 0;
     for (let i = 0; i < number.length; i++) {
@@ -13,7 +14,7 @@ var removeDigit = function(number, digit) {
             str.push(temp);
         }
     }
-    
+
     str.sort();
     return str[str.length-1];
 };
