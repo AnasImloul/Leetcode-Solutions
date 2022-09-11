c649ee3f7cbcf19a22e81a27cc3a8a74b17b7f38
AnasImloul <pydev27@gmail.com>
2022-09-11 13:22:47 +0100

    Runtime: 2 ms (Top 99.78%) | Memory: 41.7 MB (Top 99.11%)

--- scripts/algorithms/M/Minimum Number of Moves to Seat Everyone/Minimum Number of Moves to Seat Everyone.java
+++ scripts/algorithms/M/Minimum Number of Moves to Seat Everyone/Minimum Number of Moves to Seat Everyone.java
@@ -1,10 +1,11 @@
+// Runtime: 2 ms (Top 99.78%) | Memory: 41.7 MB (Top 99.11%)
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
         Arrays.sort(seats);
         Arrays.sort(students);
         int diff = 0;
         for(int i=0; i<seats.length; i++){
-            diff +=  Math.abs(students[i]-seats[i]);
+            diff += Math.abs(students[i]-seats[i]);
         }
         return diff;
     }
