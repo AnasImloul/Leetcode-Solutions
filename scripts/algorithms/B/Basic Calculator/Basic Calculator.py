e504968ec9b5d03897f328492297a2027c8e8901
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:54 +0100

    Runtime: 137 ms (Top 57.95%) | Memory: 15.5 MB (Top 50.78%)

--- scripts/algorithms/B/Basic Calculator/Basic Calculator.py
+++ scripts/algorithms/B/Basic Calculator/Basic Calculator.py
@@ -1,29 +1,30 @@
+# Runtime: 137 ms (Top 57.95%) | Memory: 15.5 MB (Top 50.78%)
 
 ```class Solution:
     def calculate(self, s: str) -> int:
         curr,output,sign,stack = 0,0,1,[]
-        
+
         for ch in s:
             if ch.isdigit():
                 curr = curr * 10 + int(ch)
-            
-            elif ch == '+':                
-                output += sign * curr              
-                sign = 1 
+
+            elif ch == '+':
+                output += sign * curr
+                sign = 1
                 curr = 0
-                
+
             elif ch == '-':
                 output += sign * curr
                 sign = -1
-                curr  = 0
-            
+                curr = 0
+
             elif ch == '(':
                 #push the result and then the sign
                 stack.append(output)
                 stack.append(sign)
                 sign = 1
                 output = 0
-            
+
             elif ch == ')':
                 output += sign * curr
                 output *= stack.pop()
