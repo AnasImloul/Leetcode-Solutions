98c303c6427f7f2c1471f147108ba0b8105b4b99
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:52 +0100

    Runtime: 274 ms (Top 86.33%) | Memory: 16.5 MB (Top 16.14%)

--- scripts/algorithms/D/Degree of an Array/Degree of an Array.py
+++ scripts/algorithms/D/Degree of an Array/Degree of an Array.py
@@ -1,10 +1,11 @@
+# Runtime: 274 ms (Top 86.33%) | Memory: 16.5 MB (Top 16.14%)
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         degree = {num: [] for num in nums}
-        
+
         for i, num in enumerate(nums):
             degree[num].append(i)
-        
+
         indices= sorted(degree.values(), key = lambda x: len(x), reverse = True)
         max_len = len(indices[0])
         min_subarray = len(nums)
