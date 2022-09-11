3dbb74aff1902d22a3c957ccbc56df9355be406a
AnasImloul <pydev27@gmail.com>
2022-09-11 13:22:41 +0100

    Runtime: 2 ms (Top 55.13%) | Memory: 42.3 MB (Top 47.48%)

--- scripts/algorithms/B/Best Time to Buy and Sell Stock with Cooldown/Best Time to Buy and Sell Stock with Cooldown.java
+++ scripts/algorithms/B/Best Time to Buy and Sell Stock with Cooldown/Best Time to Buy and Sell Stock with Cooldown.java
@@ -1,19 +1,20 @@
+// Runtime: 2 ms (Top 55.13%) | Memory: 42.3 MB (Top 47.48%)
 class Solution {
     public int maxProfit(int[] prices) {
-        
+
         int n = prices.length;
-        
+
         int[][] dp = new int[n+2][2];
-        
+
         for(int index = n-1; index>=0; index--){
             for(int buy = 0; buy<=1; buy++){
-                
+
                 int profit = 0;
-                
-                if(buy == 0){   // buy stocks
+
+                if(buy == 0){ // buy stocks
                     profit = Math.max(-prices[index] + dp[index+1][1], 0 + dp[index+1][0]);
                 }
-                if(buy == 1){  // we can sell stocks
+                if(buy == 1){ // we can sell stocks
                     profit = Math.max(prices[index] + dp[index+2][0], 0 + dp[index+1][1]);
                 }
                 dp[index][buy] = profit;
