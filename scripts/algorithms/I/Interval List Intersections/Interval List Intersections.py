12ce61810fa654fcb3e6106ac6bb62724e5ba7f1
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:57 +0100

    Runtime: 325 ms (Top 13.56%) | Memory: 15.2 MB (Top 15.52%)

--- scripts/algorithms/I/Interval List Intersections/Interval List Intersections.py
+++ scripts/algorithms/I/Interval List Intersections/Interval List Intersections.py
@@ -1,21 +1,22 @@
+# Runtime: 325 ms (Top 13.56%) | Memory: 15.2 MB (Top 15.52%)
 from collections import deque
 
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        
+
         answer = []
-        
+
         if len(firstList) == 0 or len(secondList) == 0:
             return answer
-    
+
         first_queue= deque(firstList)
-        second_queue  = deque(secondList ) 
-        
+        second_queue = deque(secondList )
+
         first = first_queue.popleft()
         second = second_queue.popleft()
-        
+
         while first_queue or second_queue:
-            
+
             if first[1] < second[0]:
                 if len(first_queue):
                     first = first_queue.popleft()
@@ -26,7 +27,7 @@ class Solution:
                     second = second_queue.popleft()
                     continue
                 break
-                
+
             if first[0] <= second[0] and second[0] <= first[1]:
                 if first[1] <= second[1]:
                     answer.append([second[0], first[1]])
@@ -34,7 +35,7 @@ class Solution:
                         first = first_queue.popleft()
                         continue
                     break
-                    
+
                 else:
                     answer.append(second)
                     if len(second_queue) > 0:
@@ -48,20 +49,19 @@ class Solution:
                         second = second_queue.popleft()
                         continue
                     break
-                    
+
                 else:
                     answer.append(first)
                     if len(first_queue) > 0:
                         first = first_queue.popleft()
                         continue
                     break
-                    
 
         if first[0] <= second[0] and second[0] <= first[1]:
             if first[1] <= second[1]:
-                
+
                 if len(answer) > 0:
-                
+
                     if answer[-1] != [second[0], first[1]]:
                         answer.append([second[0], first[1]])
                 elif not answer:
@@ -84,7 +84,7 @@ class Solution:
                     if answer[-1] != first:
                         answer.append(first)
                 elif not answer:
-                        
+
                     answer.append(first)
 
         return answer
