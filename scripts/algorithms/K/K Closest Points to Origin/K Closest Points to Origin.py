49fe32b8166619e65eda1dabc18eb6fb50031859
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:58 +0100

    Runtime: 2003 ms (Top 6.82%) | Memory: 20.5 MB (Top 21.33%)

--- scripts/algorithms/K/K Closest Points to Origin/K Closest Points to Origin.py
+++ scripts/algorithms/K/K Closest Points to Origin/K Closest Points to Origin.py
@@ -1,3 +1,4 @@
+# Runtime: 2003 ms (Top 6.82%) | Memory: 20.5 MB (Top 21.33%)
 
 def cal(ele):
     return ele[0]**2 +ele[1]**2
@@ -13,28 +14,28 @@ def partition(arr,start,end):
         if cal(arr[j]) <= pivot_dis:
             i+=1
             arr[j],arr[i] = arr[i],arr[j]
-            
+
     arr[i+1],arr[end] = arr[end],arr[i+1]
     return i+1
 def qSelect(arr,kth,start,end):
     if start < end:
         pv= partition(arr,start,end)
-        #  _______________________
-        # |   Left   |ele|    Right|
-        #  ------------------------
-        #             pv
+        # _______________________
+        # | Left |ele| Right|
+        # ------------------------
+        # pv
         # after partition function call, pv is the index that sacrify:
         # all elements in Left will smaller than ele
-        # all elements in Right side will greater than ele 
+        # all elements in Right side will greater than ele
         if pv == kth:#
             return
         if kth < pv:
             return qSelect(arr,kth,start,pv-1)
         else:
             return qSelect(arr,kth,pv+1,end)
-# Space  O (logn) because of using recursion
+# Space O (logn) because of using recursion
 # Time: Average case: O(N)
-#       Worst case:   O(N**2)
+# Worst case: O(N**2)
 class Solution:
     def kClosest(self, points, k):
         # print(points)
