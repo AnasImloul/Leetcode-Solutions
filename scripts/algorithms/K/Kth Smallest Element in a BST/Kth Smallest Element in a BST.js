d605db9fd63ce0e659e0aa3a657e9523ad0123df
AnasImloul <pydev27@gmail.com>
2022-09-11 14:00:18 +0100

    Runtime: 75 ms (Top 95.24%) | Memory: 48.7 MB (Top 25.31%)

--- scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.js
+++ scripts/algorithms/K/Kth Smallest Element in a BST/Kth Smallest Element in a BST.js
@@ -1,9 +1,10 @@
+// Runtime: 75 ms (Top 95.24%) | Memory: 48.7 MB (Top 25.31%)
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -16,13 +17,12 @@ var kthSmallest = function(root, k) {
     while(root != null || stack.length > 0) {
         while(root != null){
             stack.push(root);
-            root = root.left  
+            root = root.left
         }
         root = stack.pop();
         if(--k == 0) break;
         root = root.right;
     };
-    
+
     return root.val;
 };
-
