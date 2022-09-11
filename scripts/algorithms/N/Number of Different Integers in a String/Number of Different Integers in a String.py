1aeee80f8218eabe8fd0b1a4f0d0f46b6fbeecee
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:58 +0100

    Runtime: 55 ms (Top 37.91%) | Memory: 13.8 MB (Top 61.66%)

--- scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.py
+++ scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.py
@@ -1,7 +1,8 @@
+# Runtime: 55 ms (Top 37.91%) | Memory: 13.8 MB (Top 61.66%)
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        
+
         word = re.findall('(\d+)', word)
         numbers = [int(i) for i in word]
-        
+
         return len(set(numbers))
