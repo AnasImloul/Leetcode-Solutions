7f0ea131ace4e1ec3bbb957cecec71168ade6dcc
AnasImloul <pydev27@gmail.com>
2022-09-11 13:22:43 +0100

    Runtime: 1 ms (Top 45.46%) | Memory: 42.8 MB (Top 25.20%)

--- scripts/algorithms/F/Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.java
+++ scripts/algorithms/F/Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.java
@@ -1,9 +1,9 @@
+// Runtime: 1 ms (Top 45.46%) | Memory: 42.8 MB (Top 25.20%)
 class Solution {
     public int findMin(int[] nums)
     {
         int min=nums[0];
-        
-        
+
         for(int i=0;i<nums.length;i++)
         {
             if(min>nums[i])
