02482e76b3415badaa0027497161cf34fa310c4e
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:50 +0100

    Runtime: 87 ms (Top 42.12%) | Memory: 14.3 MB (Top 6.17%)

--- scripts/algorithms/F/Find Common Characters/Find Common Characters.py
+++ scripts/algorithms/F/Find Common Characters/Find Common Characters.py
@@ -1,3 +1,4 @@
+# Runtime: 87 ms (Top 42.12%) | Memory: 14.3 MB (Top 6.17%)
 class Solution:
     def _get_char_counts(self, s: str) -> dict[str]:
         """builds a dict of letters : count"""
@@ -5,23 +6,23 @@ class Solution:
         for i in s:
             d[i] = d.get(i,0)+1
         return d
-    
+
     def commonChars(self, words: list[str]) -> list[str]:
         """returns a string of letters common between a list of words (including duplicates)"""
         if not words:
             return
-			
+
         # O(n^2)
         words = [self._get_char_counts(word) for word in words]
-        
-		# O(nm), set intersection
+
+        # O(nm), set intersection
         common = words[0].keys()
         for other in words[1:]:
             common &= other.keys()
-        
-		# O(nm), number of common characters across the number of words
+
+        # O(nm), number of common characters across the number of words
         result = []
         for c in common:
             result += [c] * min(count[c] for count in words)
-        
+
         return result
