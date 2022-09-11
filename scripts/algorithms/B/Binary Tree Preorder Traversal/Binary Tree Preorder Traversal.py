bc76397302c7b94c3c095a875621227211226510
AnasImloul <pydev27@gmail.com>
2022-09-11 15:47:55 +0100

    Runtime: 58 ms (Top 23.56%) | Memory: 13.9 MB (Top 60.28%)

--- scripts/algorithms/B/Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.py
+++ scripts/algorithms/B/Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.py
@@ -1,44 +1,43 @@
+# Runtime: 58 ms (Top 23.56%) | Memory: 13.9 MB (Top 60.28%)
 from collections import deque
 from typing import List, Optional
 
+class Solution:
+    """
+    Time: O(n)
+    Memory: O(n)
+    """
+
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        queue = deque([root])
+        preorder = []
+
+        while queue:
+            node = queue.pop()
+            preorder.append(node.val)
+
+            if node.right is not None:
+                queue.append(node.right)
+            if node.left is not None:
+                queue.append(node.left)
+
+        return preorder
 
 class Solution:
-	"""
-	Time:   O(n)
-	Memory: O(n)
-	"""
+    """
+    Time: O(n)
+    Memory: O(n)
+    """
 
-	def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-		if root is None:
-			return []
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return list(self.preorder_generator(root))
 
-		queue = deque([root])
-		preorder = []
-
-		while queue:
-			node = queue.pop()
-			preorder.append(node.val)
-
-			if node.right is not None:
-				queue.append(node.right)
-			if node.left is not None:
-				queue.append(node.left)
-
-		return preorder
-
-
-class Solution:
-	"""
-	Time:   O(n)
-	Memory: O(n)
-	"""
-
-	def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-		return list(self.preorder_generator(root))
-
-	@classmethod
-	def preorder_generator(cls, tree: Optional[TreeNode]):
-		if tree is not None:
-			yield tree.val
-			yield from cls.preorder_generator(tree.left)
-			yield from cls.preorder_generator(tree.right)
+    @classmethod
+    def preorder_generator(cls, tree: Optional[TreeNode]):
+        if tree is not None:
+            yield tree.val
+            yield from cls.preorder_generator(tree.left)
+            yield from cls.preorder_generator(tree.right)
