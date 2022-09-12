bf92abf85d8d238c4b31ddc79322c122d989a4be
AnasImloul <pydev27@gmail.com>
2022-09-12 14:41:28 +0100

    Runtime: 3 ms (Top 59.11%) | Memory: 5.7 MB (Top 87.39%)

--- scripts/algorithms/U/Ugly Number/Ugly Number.cpp
+++ scripts/algorithms/U/Ugly Number/Ugly Number.cpp
@@ -1,17 +1,18 @@
+// Runtime: 3 ms (Top 59.11%) | Memory: 5.7 MB (Top 87.39%)
 class Solution {
 public:
-   bool isUgly(int n) 
+   bool isUgly(int n)
   {
-	 if (n <= 0) return false;
-	 int n1 = 0;
-	 while(n != n1)
-	 {
-	 n1 = n;
-	 if ((n % 2) == 0) n /= 2;
-	 if ((n % 3) == 0) n /= 3;
-	 if ((n % 5) == 0) n /= 5;
-	 }
-	 if (n < 7) return true;
-	 return false;
+     if (n <= 0) return false;
+     int n1 = 0;
+     while(n != n1)
+     {
+     n1 = n;
+     if ((n % 2) == 0) n /= 2;
+     if ((n % 3) == 0) n /= 3;
+     if ((n % 5) == 0) n /= 5;
+     }
+     if (n < 7) return true;
+     return false;
   }
 };
