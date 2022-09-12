3959f9c64c1f603df2579b35936e1a038365c683
AnasImloul <pydev27@gmail.com>
2022-09-12 14:41:25 +0100

    Runtime: 11 ms (Top 6.33%) | Memory: 7.5 MB (Top 92.11%)

--- scripts/algorithms/S/Swap Nodes in Pairs/Swap Nodes in Pairs.cpp
+++ scripts/algorithms/S/Swap Nodes in Pairs/Swap Nodes in Pairs.cpp
@@ -1,18 +1,19 @@
+// Runtime: 11 ms (Top 6.33%) | Memory: 7.5 MB (Top 92.11%)
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        // if head is NULL OR just having a single node, then no need to change anything 
-        if(head == NULL || head -> next == NULL) 
+        // if head is NULL OR just having a single node, then no need to change anything
+        if(head == NULL || head -> next == NULL)
         {
             return head;
         }
-            
+
         ListNode* temp; // temporary pointer to store head -> next
         temp = head->next; // give temp what he want
-        
+
         head->next = swapPairs(head->next->next); // changing links
         temp->next = head; // put temp -> next to head
-        
+
         return temp; // now after changing links, temp act as our head
     }
 };
