c8d6b0531a1120580da8ab8e5584a6ca5cda42da
AnasImloul <pydev27@gmail.com>
2022-09-12 14:41:22 +0100

    Runtime: 125 ms (Top 23.98%) | Memory: 42.5 MB (Top 47.00%)

--- scripts/algorithms/M/Maximum Points You Can Obtain from Cards/Maximum Points You Can Obtain from Cards.cpp
+++ scripts/algorithms/M/Maximum Points You Can Obtain from Cards/Maximum Points You Can Obtain from Cards.cpp
@@ -1,15 +1,16 @@
+// Runtime: 125 ms (Top 23.98%) | Memory: 42.5 MB (Top 47.00%)
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-	
+
         int max_s = 0, left =0, right = 0, n = cardPoints.size();
-		
-		//getting sum of k right elements
+
+        //getting sum of k right elements
         for(int i = 0; i<k; i++){
             right += cardPoints[n-i-1];
         }
-		
-		// Assumming max as sum of k right elements
+
+        // Assumming max as sum of k right elements
         max_s = right;
         for(int i = 0; i<k; i++){
             left += cardPoints[i];
