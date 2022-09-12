3f2664210f4f75ed8f729f6407156b6319baed69
AnasImloul <pydev27@gmail.com>
2022-09-12 14:23:45 +0100

    Runtime: 1523 ms (Top 9.74%) | Memory: 34 MB (Top 8.51%)

--- scripts/algorithms/F/Find the Duplicate Number/Find the Duplicate Number.py
+++ scripts/algorithms/F/Find the Duplicate Number/Find the Duplicate Number.py
@@ -1,11 +1,11 @@
+# Runtime: 1523 ms (Top 9.74%) | Memory: 34 MB (Top 8.51%)
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        
+
         dictx = {}
-        
+
         for each in nums:
             if each not in dictx:
                 dictx[each] = 1
             else:
                 return each
-        
