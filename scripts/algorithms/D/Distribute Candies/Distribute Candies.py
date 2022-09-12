edd03c8fda6139809dee8afc7f6d591179393b66
AnasImloul <pydev27@gmail.com>
2022-09-12 14:23:39 +0100

    Runtime: 1728 ms (Top 12.88%) | Memory: 16.4 MB (Top 8.29%)

--- scripts/algorithms/D/Distribute Candies/Distribute Candies.py
+++ scripts/algorithms/D/Distribute Candies/Distribute Candies.py
@@ -1,3 +1,4 @@
+# Runtime: 1728 ms (Top 12.88%) | Memory: 16.4 MB (Top 8.29%)
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         return min(len(set(candyType)), (len(candyType)//2))
