2c5c97bb3247606ffd2a89873d4b237709b56d3b
AnasImloul <pydev27@gmail.com>
2022-09-12 14:30:28 +0100

    Runtime: 248 ms (Top 56.51%) | Memory: 95.8 MB (Top 75.37%)

--- scripts/algorithms/R/Range Sum of BST/Range Sum of BST.js
+++ scripts/algorithms/R/Range Sum of BST/Range Sum of BST.js
@@ -1,17 +1,18 @@
+// Runtime: 248 ms (Top 56.51%) | Memory: 95.8 MB (Top 75.37%)
 var rangeSumBST = function(root, low, high) {
     let sum = 0;
     const summer = (root) => {
         if(!root) {
             return;
         }
-		
+
         sum = root.val >= low && root.val <= high ? root.val + sum : sum;
-        
+
         summer(root.left)
         summer(root.right)
     }
-    
+
     summer(root);
-	
+
     return sum;
 };
