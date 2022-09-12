0047a7e2b431c197a73ab316b53d6191a3f4b96a
AnasImloul <pydev27@gmail.com>
2022-09-12 14:30:33 +0100

    Runtime: 101 ms (Top 73.64%) | Memory: 42.7 MB (Top 97.01%)

--- scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.js
+++ scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.js
@@ -1,19 +1,20 @@
+// Runtime: 101 ms (Top 73.64%) | Memory: 42.7 MB (Top 97.01%)
 var findMinDifference = function(timePoints) {
-	const DAY_MINUTES = 24 * 60;
-	const set = new Set();
+    const DAY_MINUTES = 24 * 60;
+    const set = new Set();
 
-	for (let index = 0; index < timePoints.length; index++) {
-		const time = timePoints[index];
-		const [hours, minutes] = time.split(':');
-		const totalMinutes = hours * 60 + +minutes;
-		if (set.has(totalMinutes)) return 0;
-		set.add(totalMinutes);
-	}
+    for (let index = 0; index < timePoints.length; index++) {
+        const time = timePoints[index];
+        const [hours, minutes] = time.split(':');
+        const totalMinutes = hours * 60 + +minutes;
+        if (set.has(totalMinutes)) return 0;
+        set.add(totalMinutes);
+    }
 
-	const timeMinutes = [...set].sort((a, b) => a - b);
-	return timeMinutes.reduce((min, time, index) => {
-		const next = timeMinutes[index + 1];
-		const diff = next ? next - time : DAY_MINUTES - time + timeMinutes[0];
-		return Math.min(min, diff);
-	}, Infinity);
+    const timeMinutes = [...set].sort((a, b) => a - b);
+    return timeMinutes.reduce((min, time, index) => {
+        const next = timeMinutes[index + 1];
+        const diff = next ? next - time : DAY_MINUTES - time + timeMinutes[0];
+        return Math.min(min, diff);
+    }, Infinity);
 };
