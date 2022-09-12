4b1a22b9522bf60b53b08c5f45f05259f6d0a800
AnasImloul <pydev27@gmail.com>
2022-09-12 14:30:34 +0100

    Runtime: 136 ms (Top 56.52%) | Memory: 53.8 MB (Top 73.91%)

--- scripts/algorithms/W/Watering Plants II/Watering Plants II.js
+++ scripts/algorithms/W/Watering Plants II/Watering Plants II.js
@@ -1,35 +1,36 @@
+// Runtime: 136 ms (Top 56.52%) | Memory: 53.8 MB (Top 73.91%)
 var minimumRefill = function(plants, capacityA, capacityB) {
     const n = plants.length;
-    
+
     let left = 0;
     let right = n - 1;
-    
+
     let remA = capacityA;
     let remB = capacityB;
-    
+
     let refills = 0;
-   
+
     while (left < right) {
         const leftAmount = plants[left++];
         const rightAmount = plants[right--];
-        
+
         if (leftAmount > remA) {
             ++refills;
             remA = capacityA;
         }
         remA -= leftAmount;
-        
+
         if (rightAmount > remB) {
             ++refills;
             remB = capacityB;
         }
         remB -= rightAmount;
-        
+
     }
-    
+
     if (left === right) {
         const midAmount = plants[left];
-        
+
         if (remB > remA) {
             if (remB < midAmount) ++refills;
         }
@@ -37,6 +38,6 @@ var minimumRefill = function(plants, capacityA, capacityB) {
             if (remA < midAmount) ++refills;
         }
     }
-    
+
     return refills;
 };
