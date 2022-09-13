5c0ad84f3abe34d6fb775c040573e35a82e42c21
AnasImloul <pydev27@gmail.com>
2022-09-13 10:57:49 +0100

    Runtime: 2577 ms (Top 35.48%) | Memory: 14.1 MB (Top 20.97%)

--- scripts/algorithms/C/Count Subtrees With Max Distance Between Cities/Count Subtrees With Max Distance Between Cities.py
+++ scripts/algorithms/C/Count Subtrees With Max Distance Between Cities/Count Subtrees With Max Distance Between Cities.py
@@ -1,3 +1,4 @@
+# Runtime: 2577 ms (Top 35.48%) | Memory: 14.1 MB (Top 20.97%)
 from collections import defaultdict
 class Solution:
     def countSubgraphsForEachDiameter(self, n: int, edges: List[List[int]]) -> List[int]:
@@ -13,33 +14,33 @@ class Solution:
                     for j in range(n):
                         D[i][j] = min(D[i][j],D[i][k]+D[k][j])
             return D
-        
+
         G = defaultdict(list)
         for v, w in edges:
             G[v-1].append(w-1)
             G[w-1].append(v-1)
-        
+
         def dfs(G, v, V, visited):
             if v in visited: return
             visited.add(v)
             for w in G[v]:
                 if w in V:
                     dfs(G, w, V, visited)
-                    
+
         def check_connected(G, V):
             if not V: return False
             root = next(iter(V))
             visited = set()
             dfs(G, root, V, visited)
             return visited == V
-        
+
         def max_distance(D, V):
             res = 0
             for v in V:
                 for w in V:
                     res = max(res, D[v][w])
             return res
-        
+
         def solve(include, idx, ans, G, D):
             if idx == n:
                 V = set(v for v in range(n) if include[v])
@@ -52,14 +53,9 @@ class Solution:
                 include[idx] = True
                 solve(include, idx+1, ans, G, D)
                 include[idx] = False
-                
+
         D = floydwarshall(n, edges)
         include = [False] * n
         ans = [0]*(n-1)
         solve(include, 0, ans, G, D)
         return ans
-        
-                        
-            
-            
-            
