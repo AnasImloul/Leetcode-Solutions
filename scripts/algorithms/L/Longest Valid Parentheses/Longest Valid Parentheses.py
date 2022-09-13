c0db571f9fff3e1e658cb65f39e9d560745623dc
AnasImloul <pydev27@gmail.com>
2022-09-13 10:57:50 +0100

    Runtime: 74 ms (Top 48.14%) | Memory: 14.8 MB (Top 24.60%)

--- scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.py
+++ scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.py
@@ -1,14 +1,15 @@
+# Runtime: 74 ms (Top 48.14%) | Memory: 14.8 MB (Top 24.60%)
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        
+
         maxi = 0
         stack = [-1]
-        
+
         for i in range(len(s)) :
             if s[i] == "(" : stack.append(i)
             else :
                 stack.pop()
                 if len(stack) == 0 : stack.append(i)
                 else : maxi = max(maxi, i - stack[-1])
-        
+
         return maxi
