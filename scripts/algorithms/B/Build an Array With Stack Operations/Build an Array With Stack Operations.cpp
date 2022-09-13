7994fb68db28db3663ebdcdf1925377ece80a9c2
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:39 +0100

    Runtime: 3 ms (Top 65.19%) | Memory: 7.9 MB (Top 10.48%)

--- scripts/algorithms/B/Build an Array With Stack Operations/Build an Array With Stack Operations.cpp
+++ scripts/algorithms/B/Build an Array With Stack Operations/Build an Array With Stack Operations.cpp
@@ -1,10 +1,10 @@
+// Runtime: 3 ms (Top 65.19%) | Memory: 7.9 MB (Top 10.48%)
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string>ans;         
-        int l=target.size(), count=0,ind=0;    // ind is index of the target array
-        
-        
+        vector<string>ans;
+        int l=target.size(), count=0,ind=0; // ind is index of the target array
+
         for(int i=1;i<=n;i++){
             if(count==l) break;
             if(target[ind]!=i){
