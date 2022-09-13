334288d374094ddbb95fe2997366a70b2da63971
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:41 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 8.2 MB (Top 56.00%)

--- scripts/algorithms/B/Binary Search Tree to Greater Sum Tree/Binary Search Tree to Greater Sum Tree.cpp
+++ scripts/algorithms/B/Binary Search Tree to Greater Sum Tree/Binary Search Tree to Greater Sum Tree.cpp
@@ -1,19 +1,20 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 8.2 MB (Top 56.00%)
 class Solution {
 public:
-    
+
     int s = 0;
-    
+
     void solve(TreeNode* root){
         if(!root) return;
         solve(root->right);
-        
+
         root->val = s + root->val;
         s = root->val;
-        
+
         solve(root->left);
         return;
     }
-    
+
     TreeNode* bstToGst(TreeNode* root) {
         if(!root) return NULL;
         solve(root);
