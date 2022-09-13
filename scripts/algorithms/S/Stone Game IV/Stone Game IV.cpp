5410c8f985d41e06146a4d91de51fbee65b676b0
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:47 +0100

    Runtime: 50 ms (Top 83.42%) | Memory: 7.9 MB (Top 52.59%)

--- scripts/algorithms/S/Stone Game IV/Stone Game IV.cpp
+++ scripts/algorithms/S/Stone Game IV/Stone Game IV.cpp
@@ -1,9 +1,10 @@
+// Runtime: 50 ms (Top 83.42%) | Memory: 7.9 MB (Top 52.59%)
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        
+
         vector<int> dp(n+1,false);
-        
+
         for(int i=1;i<=n;i++){
             for(int j=sqrt(i);j>=1;j--){
                 if(dp[i-j*j] == false){
@@ -12,7 +13,7 @@ public:
                 }
             }
         }
-        
+
         return dp[n];
     }
 };
