6dc36e5aa6e81d468f6bce78fbd19b57bd8ab702
AnasImloul <pydev27@gmail.com>
2022-09-13 10:57:47 +0100

    Runtime: 2948 ms (Top 45.50%) | Memory: 60.7 MB (Top 40.45%)

--- scripts/algorithms/D/Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.py
+++ scripts/algorithms/D/Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.py
@@ -1,13 +1,14 @@
+# Runtime: 2948 ms (Top 45.50%) | Memory: 60.7 MB (Top 40.45%)
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# def __init__(self, val=0, next=None):
+# self.val = val
+# self.next = next
 class Solution:
   def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
     if not head: return head
     if head and not head.next: return None
-    
+
     prev = ListNode(0, head)
     slow = fast = head
     while fast and fast.next:
@@ -16,4 +17,4 @@ class Solution:
       fast = fast.next.next
 
     prev.next = slow.next
-    return head      
+    return head
