316022231a4276de2471f0ffdf600b4d244e0745
AnasImloul <pydev27@gmail.com>
2022-09-13 10:57:41 +0100

    Runtime: 3421 ms (Top 39.22%) | Memory: 51.8 MB (Top 96.08%)

--- scripts/algorithms/S/Smallest Missing Genetic Value in Each Subtree/Smallest Missing Genetic Value in Each Subtree.py
+++ scripts/algorithms/S/Smallest Missing Genetic Value in Each Subtree/Smallest Missing Genetic Value in Each Subtree.py
@@ -1,25 +1,26 @@
+# Runtime: 3421 ms (Top 39.22%) | Memory: 51.8 MB (Top 96.08%)
 class Solution:
     def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
         ans = [1] * len(parents)
-        if 1 in nums: 
+        if 1 in nums:
             tree = {}
-            for i, x in enumerate(parents): 
+            for i, x in enumerate(parents):
                 tree.setdefault(x, []).append(i)
-                
+
             k = nums.index(1)
             val = 1
             seen = set()
-            
-            while k != -1: 
+
+            while k != -1:
                 stack = [k]
-                while stack: 
+                while stack:
                     x = stack.pop()
                     seen.add(nums[x])
-                    for xx in tree.get(x, []): 
-                        if nums[xx] not in seen: 
+                    for xx in tree.get(x, []):
+                        if nums[xx] not in seen:
                             stack.append(xx)
                             seen.add(nums[xx])
                 while val in seen: val += 1
                 ans[k] = val
                 k = parents[k]
-        return ans 
+        return ans
