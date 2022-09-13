2e60a28d03fbc8cdde51f8c9f3c1fa06b3119731
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:52 +0100

    Runtime: 62 ms (Top 15.48%) | Memory: 19.7 MB (Top 56.49%)

--- scripts/algorithms/F/Find Closest Number to Zero/Find Closest Number to Zero.cpp
+++ scripts/algorithms/F/Find Closest Number to Zero/Find Closest Number to Zero.cpp
@@ -1,12 +1,13 @@
+// Runtime: 62 ms (Top 15.48%) | Memory: 19.7 MB (Top 56.49%)
 class Solution {
 public:
     int findClosestNumber(vector<int>& nums) {
-//         setting the ans to maximum value of int
+// setting the ans to maximum value of int
         int ans = INT_MAX ;
-        for(int i :  nums){
+        for(int i : nums){
         // checking if each value of nums is less than the max value
             if(abs(i) < abs(ans)){
-                ans =  i ; //check for the lesser value
+                ans = i ; //check for the lesser value
             }
             else if(abs(i) == abs(ans)){
                 ans = max (ans,i) ; // return the maximum in cases there are multiple answers
