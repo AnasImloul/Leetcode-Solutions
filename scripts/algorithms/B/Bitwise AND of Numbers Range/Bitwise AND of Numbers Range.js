afcec061de012e3494116a39fda5192a2a7aa941
AnasImloul <pydev27@gmail.com>
2022-09-13 11:51:14 +0100

    Runtime: 261 ms (Top 34.83%) | Memory: 49.1 MB (Top 40.45%)

--- scripts/algorithms/B/Bitwise AND of Numbers Range/Bitwise AND of Numbers Range.js
+++ scripts/algorithms/B/Bitwise AND of Numbers Range/Bitwise AND of Numbers Range.js
@@ -1,18 +1,19 @@
+// Runtime: 261 ms (Top 34.83%) | Memory: 49.1 MB (Top 40.45%)
 var rangeBitwiseAnd = function(left, right) {
     const a = left.toString(2);
     const b = right.toString(2);
-    
+
     if (a.length !== b.length) {
         return 0;
     }
 
     let match = 0;
-    
+
     for (let i = 0; i < a.length; i++) {
         if (a[i] !== b[i]) {
             break;
         }
-        
+
         match++;
     }
 
