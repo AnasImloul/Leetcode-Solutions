ce3d868191052a581c976a3e50eff24ecbd82eb2
AnasImloul <pydev27@gmail.com>
2022-09-13 11:20:22 +0100

    Runtime: 7 ms (Top 87.86%) | Memory: 47.2 MB (Top 79.05%)

--- scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.java
+++ scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.java
@@ -1,3 +1,4 @@
+// Runtime: 7 ms (Top 87.86%) | Memory: 47.2 MB (Top 79.05%)
 public class Codec {
 
     // Encodes a tree to a single string.
@@ -6,7 +7,7 @@ public class Codec {
         traverse(root, string);
         return string.toString();
     }
-    
+
     private void traverse(TreeNode root, StringBuilder string){
         if(root == null) return;
         string.append(root.val).append(",");
@@ -17,19 +18,19 @@ public class Codec {
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if(data.isEmpty()) return null;
-        
+
         String[] dataArr = data.split(",");
-        
+
         TreeNode root = new TreeNode(Integer.parseInt(dataArr[0]));
-        
+
         for(int i = 1; i < dataArr.length; i++)
             insert(Integer.parseInt(dataArr[i]), root);
-        
+
         return root;
     }
-    
+
     private void insert(int digit, TreeNode root){
-        
+
         if(digit > root.val){
             if(root.right != null)
                 insert(digit, root.right);
@@ -38,7 +39,7 @@ public class Codec {
         } else {
             if(root.left != null)
                 insert(digit, root.left);
-            else 
+            else
                 root.left = new TreeNode(digit);
         }
 
