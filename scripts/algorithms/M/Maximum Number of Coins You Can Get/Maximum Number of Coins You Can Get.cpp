afa53ba1428c8817bcff97e148d7550988202064
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:40 +0100

    Runtime: 294 ms (Top 6.88%) | Memory: 53.5 MB (Top 9.97%)

--- scripts/algorithms/M/Maximum Number of Coins You Can Get/Maximum Number of Coins You Can Get.cpp
+++ scripts/algorithms/M/Maximum Number of Coins You Can Get/Maximum Number of Coins You Can Get.cpp
@@ -1,3 +1,4 @@
+// Runtime: 294 ms (Top 6.88%) | Memory: 53.5 MB (Top 9.97%)
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
@@ -8,7 +9,7 @@ public:
         int i=1;
         while(myPilesCount<(PilesSize/3))
         {
-           
+
             myPilesSum+=piles[i];
             myPilesCount++;
             i+=2;
