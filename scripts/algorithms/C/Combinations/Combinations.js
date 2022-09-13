a7a1c4ea1d35b4786aa83e4becb3e1729070e73a
AnasImloul <pydev27@gmail.com>
2022-09-13 11:51:10 +0100

    Runtime: 164 ms (Top 64.86%) | Memory: 47.8 MB (Top 82.84%)

--- scripts/algorithms/C/Combinations/Combinations.js
+++ scripts/algorithms/C/Combinations/Combinations.js
@@ -1,21 +1,22 @@
+// Runtime: 164 ms (Top 64.86%) | Memory: 47.8 MB (Top 82.84%)
 var combine = function(n, k) {
-    
+
     function helper (start, end, combo, subset, answer) {
-		if (combo==0) {
-			answer.push([...subset])
+        if (combo==0) {
+            answer.push([...subset])
             return;
         }
         if (end - start + 1 < combo) {
             return;
         }
-		if (start > end) {
-			return;
+        if (start > end) {
+            return;
         }
-		subset.push(start)
-		helper(start+1, end, combo - 1, subset, answer)
+        subset.push(start)
+        helper(start+1, end, combo - 1, subset, answer)
 
-		subset.pop()
-		helper(start+1, end, combo, subset, answer)
+        subset.pop()
+        helper(start+1, end, combo, subset, answer)
     }
 
     const answer = []
