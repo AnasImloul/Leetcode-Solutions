64feb343e3d556147348beffc0bb58ae0ce3ee90
AnasImloul <pydev27@gmail.com>
2022-09-13 11:51:12 +0100

    Runtime: 114 ms (Top 41.92%) | Memory: 45.1 MB (Top 19.07%)

--- scripts/algorithms/A/Add Strings/Add Strings.js
+++ scripts/algorithms/A/Add Strings/Add Strings.js
@@ -1,3 +1,4 @@
+// Runtime: 114 ms (Top 41.92%) | Memory: 45.1 MB (Top 19.07%)
 var addStrings = function(num1, num2) {
     let longer = num1.length >= num2.length ? num1 : num2;
     let shorter = num1.length < num2.length ? num1 : num2;
@@ -5,10 +6,10 @@ var addStrings = function(num1, num2) {
     for (let i = 0; i < diff; i++) {
       shorter = '0' + shorter;
     }
-    
+
     let str = '';
     let remainder = 0;
-    
+
     for (let i = (longer.length -1); i >= 0; i--) {
         let num = remainder > 0
             ? remainder + parseInt(longer[i]) + parseInt(shorter[i])
