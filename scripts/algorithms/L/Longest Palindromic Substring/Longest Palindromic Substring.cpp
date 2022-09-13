fb2c7a378ee336222c996894ea8714aa0327b697
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:45 +0100

    Runtime: 168 ms (Top 52.00%) | Memory: 173.7 MB (Top 19.81%)

--- scripts/algorithms/L/Longest Palindromic Substring/Longest Palindromic Substring.cpp
+++ scripts/algorithms/L/Longest Palindromic Substring/Longest Palindromic Substring.cpp
@@ -1,23 +1,24 @@
+// Runtime: 168 ms (Top 52.00%) | Memory: 173.7 MB (Top 19.81%)
 class Solution {
 public:
     string longestPalindrome(string s) {
         if(s.size() <= 1) return s;
-        
+
         string longest = "";
-        
+
         for (int i = 0; i < s.size(); i++) {
             string sub1 = expand(s, i, i+1);
             string sub2 = expand(s, i, i);
-            
+
             string sub3 = sub1.size() > sub2.size() ? sub1 : sub2;
-            
+
             if(sub3.size() > longest.size()) {
                 longest = sub3;
             }
         }
         return longest;
     }
-    
+
     string expand(string s, int i, int j) {
         while(j < s.size() && i >= 0 && s[i] == s[j]) {
             i--;
