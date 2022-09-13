de74f73c22edeed72c8243be51fdd717b8936fba
AnasImloul <pydev27@gmail.com>
2022-09-13 11:34:46 +0100

    Runtime: 23 ms (Top 13.24%) | Memory: 14 MB (Top 57.66%)

--- scripts/algorithms/F/Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.cpp
+++ scripts/algorithms/F/Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.cpp
@@ -1,6 +1,7 @@
+// Runtime: 23 ms (Top 13.24%) | Memory: 14 MB (Top 57.66%)
 class Solution {
 public:
-    int finalValueAfterOperations(vector<string>& operations) 
+    int finalValueAfterOperations(vector<string>& operations)
     {
         int X = 0;
         for(int i=0;i<operations.size();i++)
