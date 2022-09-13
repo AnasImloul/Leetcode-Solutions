aa9ccf2d969293b50f36a1b9de57165a9f667e52
AnasImloul <pydev27@gmail.com>
2022-09-13 10:57:51 +0100

    Runtime: 33 ms (Top 88.95%) | Memory: 13.8 MB (Top 62.28%)

--- scripts/algorithms/C/Convert a Number to Hexadecimal/Convert a Number to Hexadecimal.py
+++ scripts/algorithms/C/Convert a Number to Hexadecimal/Convert a Number to Hexadecimal.py
@@ -1,15 +1,16 @@
+# Runtime: 33 ms (Top 88.95%) | Memory: 13.8 MB (Top 62.28%)
 class Solution:
-	def toHex(self, num: int) -> str:
-		ret = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-		ans = ""
+    def toHex(self, num: int) -> str:
+        ret = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+        ans = ""
 
-		if num < 0:
-			num = pow(2,32) +num
+        if num < 0:
+            num = pow(2,32) +num
 
-		if num == 0:
-			return "0"
-		while num > 0:
-			ans = ret[num%16] +ans
-			num = num//16
+        if num == 0:
+            return "0"
+        while num > 0:
+            ans = ret[num%16] +ans
+            num = num//16
 
-		return ans
+        return ans
