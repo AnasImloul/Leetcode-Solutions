409c91d7c8a9043d908c9abe943023d8eb1fd2f7
AnasImloul <pydev27@gmail.com>
2022-09-13 11:51:12 +0100

    Runtime: 88 ms (Top 77.00%) | Memory: 44.4 MB (Top 78.92%)

--- scripts/algorithms/T/Transpose Matrix/Transpose Matrix.js
+++ scripts/algorithms/T/Transpose Matrix/Transpose Matrix.js
@@ -1,11 +1,12 @@
+// Runtime: 88 ms (Top 77.00%) | Memory: 44.4 MB (Top 78.92%)
 
 var transpose = function(matrix){
     let result = []
     for(let i=0;i<matrix[0].length;i++){
-       
+
         let col = []
-        
-        for(let  j= 0;j<matrix.length;j++){   
+
+        for(let j= 0;j<matrix.length;j++){
             col.push(matrix[j][i])
         }
         result.push(col)
@@ -13,4 +14,4 @@ var transpose = function(matrix){
     return result
 };
 
-	console.log(transpose( [ [1 , 2 , 3] , [ 4 , 5 , 6 ]  , [ 7 , 8 , 9 ] ] ) )
+    console.log(transpose( [ [1 , 2 , 3] , [ 4 , 5 , 6 ] , [ 7 , 8 , 9 ] ] ) )
