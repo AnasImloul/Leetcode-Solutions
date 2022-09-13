5978e544d13f219d3cd347d1dffc29b23b2c6cb6
AnasImloul <pydev27@gmail.com>
2022-09-13 11:51:09 +0100

    Runtime: 80 ms (Top 65.60%) | Memory: 42.1 MB (Top 90.67%)

--- scripts/algorithms/U/Univalued Binary Tree/Univalued Binary Tree.js
+++ scripts/algorithms/U/Univalued Binary Tree/Univalued Binary Tree.js
@@ -1,11 +1,12 @@
+// Runtime: 80 ms (Top 65.60%) | Memory: 42.1 MB (Top 90.67%)
 var isUnivalTree = function(root) {
     if (!root) {
         return false;
     }
-    
+
     const prev = root.val;
     const nodes = [root];
-    
+
     for (const node of nodes) {
         if (node.val !== prev) {
             return false;
@@ -22,4 +23,4 @@ var isUnivalTree = function(root) {
 
     return true;
 };
-	```
+    ```
