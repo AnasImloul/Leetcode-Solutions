5a52b0da5ba3274ba9472f652ebcfcecfd66a5ee
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:27 +0100

    Runtime: 347 ms (Top 94.76%) | Memory: 22.7 MB (Top 60.86%)

--- scripts/algorithms/M/Maximum Frequency Stack/Maximum Frequency Stack.py
+++ scripts/algorithms/M/Maximum Frequency Stack/Maximum Frequency Stack.py
@@ -1,11 +1,10 @@
+# Runtime: 347 ms (Top 94.76%) | Memory: 22.7 MB (Top 60.86%)
 class FreqStack:
 
     def __init__(self):
         self.cnt = {}
         self.maxcount = 0
         self.stack = {}
-        
-        
 
     def push(self, val: int) -> None:
         count = self.cnt.get(val,0)+1
@@ -14,15 +13,13 @@ class FreqStack:
             self.maxcount = count
             self.stack[count] = []
         self.stack[count].append(val)
-        
+
     def pop(self) -> int:
         res = self.stack[self.maxcount].pop()
         self.cnt[res]-=1
         if not self.stack[self.maxcount]:
             self.maxcount-=1
         return res
-        
-
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
