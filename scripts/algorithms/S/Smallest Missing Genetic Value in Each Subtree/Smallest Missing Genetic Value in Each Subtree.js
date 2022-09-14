c7a58ef2fcdfca98c86683871ac53f3681ee7e38
AnasImloul <pydev27@gmail.com>
2022-09-14 19:08:13 +0100

    Runtime: 689 ms (Top 100.00%) | Memory: 160 MB (Top 83.33%)

--- scripts/algorithms/S/Smallest Missing Genetic Value in Each Subtree/Smallest Missing Genetic Value in Each Subtree.js
+++ scripts/algorithms/S/Smallest Missing Genetic Value in Each Subtree/Smallest Missing Genetic Value in Each Subtree.js
@@ -1,13 +1,14 @@
+// Runtime: 689 ms (Top 100.00%) | Memory: 160 MB (Top 83.33%)
 var smallestMissingValueSubtree = function(parents, nums) {
     let n=parents.length,next=[...Array(n)].map(d=>[]),used={}
     for(let i=1;i<n;i++)
         next[parents[i]].push(i)
     let dfs=(node)=>{
         if(used[nums[node]])
-			return
-		used[nums[node]]=true
-		for(let child of next[node])
-			dfs(child)
+            return
+        used[nums[node]]=true
+        for(let child of next[node])
+            dfs(child)
     }
     let cur=nums.indexOf(1),leftAt=1,res=[...Array(n)].map(d=>1)
     while(cur!==-1){
