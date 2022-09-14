ccc75c5963a4455cbc88c66a36f54da686541fba
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:32 +0100

    Runtime: 81 ms (Top 17.97%) | Memory: 13.9 MB (Top 73.17%)

--- scripts/algorithms/P/Positions of Large Groups/Positions of Large Groups.py
+++ scripts/algorithms/P/Positions of Large Groups/Positions of Large Groups.py
@@ -1,6 +1,7 @@
+# Runtime: 81 ms (Top 17.97%) | Memory: 13.9 MB (Top 73.17%)
 class Solution:
     def largeGroupPositions(self, s: str) -> List[List[int]]:
-        
+
         i=0
         c=1
         prev=""
@@ -13,9 +14,8 @@ class Solution:
                     ans.append([i+1-c,i])
             else:
                 if c>=3:
-                    ans.append([i-c,i-1]) 
-                c=1 
+                    ans.append([i-c,i-1])
+                c=1
             prev=s[i]
             i+=1
         return ans
-        
