653f508f7742edb02d0f6ec8169b4eb8fadeb165
AnasImloul <pydev27@gmail.com>
2022-09-14 19:08:15 +0100

    Runtime: 92 ms (Top 87.91%) | Memory: 45.1 MB (Top 70.11%)

--- scripts/algorithms/C/Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.js
+++ scripts/algorithms/C/Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.js
@@ -1,9 +1,10 @@
+// Runtime: 92 ms (Top 87.91%) | Memory: 45.1 MB (Top 70.11%)
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -13,19 +14,19 @@
  */
 var buildTree = function(inorder, postorder) {
     let postIndex = postorder.length - 1
-    
+
     const dfs = (left, right) => {
         if (left > right) return null
-        
+
         const val = postorder[postIndex--]
         const mid = inorder.findIndex(e => e === val)
         const root = new TreeNode(val)
 
         root.right = dfs(mid + 1, right)
         root.left = dfs(left, mid - 1)
-        
+
         return root
     }
-    
+
     return dfs(0, inorder.length - 1)
 };
