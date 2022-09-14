f9c233c9689d0e8d7a7670adb983cac0ab3e5130
AnasImloul <pydev27@gmail.com>
2022-09-14 19:08:12 +0100

    Runtime: 192 ms (Top 23.33%) | Memory: 55.3 MB (Top 40.00%)

--- scripts/algorithms/M/Maximum Number of Weeks for Which You Can Work/Maximum Number of Weeks for Which You Can Work.js
+++ scripts/algorithms/M/Maximum Number of Weeks for Which You Can Work/Maximum Number of Weeks for Which You Can Work.js
@@ -1,16 +1,17 @@
+// Runtime: 192 ms (Top 23.33%) | Memory: 55.3 MB (Top 40.00%)
 
 var numberOfWeeks = function(milestones) {
     let maxElement = 0,arraySum = 0;
-    
+
     for(let milestone of milestones){
         arraySum += milestone;
         maxElement = Math.max(maxElement,milestone);
     }
-    
+
     let rest = arraySum - maxElement;
     let difference = maxElement - rest;
-    
+
     if(difference <= 1) return arraySum;
-    
+
     return (arraySum - difference) + 1;
 };
