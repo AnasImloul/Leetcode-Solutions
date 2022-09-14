0060e9e7ba11ce1882eb2f32e62e89835dea4227
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:38 +0100

    Runtime: 170 ms (Top 85.91%) | Memory: 15.7 MB (Top 23.94%)

--- scripts/algorithms/M/Minimum Number of Days to Disconnect Island/Minimum Number of Days to Disconnect Island.py
+++ scripts/algorithms/M/Minimum Number of Days to Disconnect Island/Minimum Number of Days to Disconnect Island.py
@@ -1,3 +1,4 @@
+# Runtime: 170 ms (Top 85.91%) | Memory: 15.7 MB (Top 23.94%)
 class Solution:
     def minDays(self, grid: List[List[int]]) -> int:
         cnt = 0
@@ -6,20 +7,20 @@ class Solution:
                 if grid[i][j]:
                     cnt += 1 # count the number of elements
                     root = (i, j) # assign the root node for the graph
-                    
-        if cnt <= 1 :  return cnt # no elements in the map
-        
+
+        if cnt <= 1 : return cnt # no elements in the map
+
         vis, low, time, res = {root}, {}, {}, []
-        
+
         # find whether articulation points are present in the matrix
-        def articulation_points(curr, parent): 
+        def articulation_points(curr, parent):
             low[curr] = time[curr] = len(vis)
             children = 0
             i, j = curr
-            
+
             for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
                 if (x, y) == parent : continue
-                    
+
                 if 0<=x<len(grid) and 0<=y<len(grid[0]) and grid[x][y]:
                     if (x, y) not in vis:
                         vis.add((x,y))
@@ -30,7 +31,7 @@ class Solution:
                             res.append([i, j])
                     else:
                         low[curr] = min(low[curr], time[(x, y)])
-                        
+
                 if parent == (-1, -1) and children > 1:
                     res.append([x, y])
 
@@ -42,4 +43,3 @@ class Solution:
             return 1
         else: # worst case, no articulation points
             return 2
-            
