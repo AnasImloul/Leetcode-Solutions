0dc65b8c248d641f5e24a957e267ae6549fbe71e
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:23 +0100

    Runtime: 56 ms (Top 29.40%) | Memory: 14 MB (Top 18.11%)

---
+++ scripts/algorithms/K/Kth Symbol in Grammar/K-th Symbol in Grammar.py
@@ -0,0 +1,16 @@
+# Runtime: 56 ms (Top 29.40%) | Memory: 14 MB (Top 18.11%)
+class Solution:
+  def solve(self,n,k):
+    if n==1 and k==1:
+      return 0
+    mid = pow(2,n-1)//2
+    if k<=mid:
+      return self.solve(n-1,k)
+
+    return not self.solve(n-1,k-mid)
+
+  def kthGrammar(self,n,k):
+    if self.solve(n,k):
+      return 1
+    else:
+      return 0
