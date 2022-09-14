f97bec8cda9186e653d54e07656ca54fedc40a87
AnasImloul <pydev27@gmail.com>
2022-09-14 19:08:14 +0100

    Runtime: 201 ms (Top 19.95%) | Memory: 46.9 MB (Top 27.05%)

--- scripts/algorithms/F/First Unique Character in a String/First Unique Character in a String.js
+++ scripts/algorithms/F/First Unique Character in a String/First Unique Character in a String.js
@@ -1,8 +1,8 @@
+// Runtime: 201 ms (Top 19.95%) | Memory: 46.9 MB (Top 27.05%)
 /**
  * @param {string} s
  * @return {number}
  */
-
 
 var firstUniqChar = function(s) {
     let dec = {};
@@ -12,19 +12,17 @@ var firstUniqChar = function(s) {
             dec[s[i]]['counter']++;
         }else{
             dec[s[i]] = {},
-            dec[s[i]]['counter'] =  1;
+            dec[s[i]]['counter'] = 1;
             dec[s[i]]['index'] = i;
         }
     }
-    
+
     for(let i=0; i < s.length ; i++){
         if(dec[s[i]]['counter'] == 1) {
             return dec[s[i]]['index'];
             break;
         }
     }
-    
+
     return -1;
 };
-
-
