c0aac440be8b6a6336d1d428a61e1d9db3451255
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:25 +0100

    Runtime: 85 ms (Top 14.83%) | Memory: 14 MB (Top 28.83%)

--- scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.py
+++ scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.py
@@ -1,9 +1,10 @@
+# Runtime: 85 ms (Top 14.83%) | Memory: 14 MB (Top 28.83%)
 class Solution:
     def prisonAfterNDays(self, cells: List[int], n: int) -> List[int]:
         patternMatch=defaultdict(int) # pattern match
         totalPrisons=8 # totalPrisons
-        cells=  [ str(c)  for c in (cells)] # into char type
-        for  d in range(1,n+1):
+        cells= [ str(c) for c in (cells)] # into char type
+        for d in range(1,n+1):
             tempCell=[]
             tempCell.append('0') # left corner case
             for c in range(1,totalPrisons-1):
@@ -12,12 +13,12 @@ class Solution:
                 else:
                     tempCell.append('0') # otherwise 0
             tempCell.append('0') # right corner case
-            cells=tempCell # update cells 
+            cells=tempCell # update cells
             pattern= ''.join(tempCell) # insert pattern in hashtable
-            if pattern in patternMatch: # if there is a match 
+            if pattern in patternMatch: # if there is a match
                 day=patternMatch[pattern]
-                remainder= (n%(d-1))-1 # take modulo 
+                remainder= (n%(d-1))-1 # take modulo
                 match= list(patternMatch.keys())[remainder] # find key
-                return   [ int(m) for m in match] # return 
-            patternMatch[pattern]=d # assign  day 
-        return [ int(c)  for c in (cells)] # return 
+                return [ int(m) for m in match] # return
+            patternMatch[pattern]=d # assign day
+        return [ int(c) for c in (cells)] # return
