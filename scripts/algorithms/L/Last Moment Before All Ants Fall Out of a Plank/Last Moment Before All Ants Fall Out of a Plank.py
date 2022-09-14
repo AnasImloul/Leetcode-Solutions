7b1ada45d9c508d57d15ebb180e87430fd50f133
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:27 +0100

    Runtime: 316 ms (Top 23.17%) | Memory: 15 MB (Top 18.90%)

--- scripts/algorithms/L/Last Moment Before All Ants Fall Out of a Plank/Last Moment Before All Ants Fall Out of a Plank.py
+++ scripts/algorithms/L/Last Moment Before All Ants Fall Out of a Plank/Last Moment Before All Ants Fall Out of a Plank.py
@@ -1,7 +1,8 @@
+# Runtime: 316 ms (Top 23.17%) | Memory: 15 MB (Top 18.90%)
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-		# make sure left and right are not empty without changing the answer
+        # make sure left and right are not empty without changing the answer
         left.append(0)
-        right.append(n)        
-        
+        right.append(n)
+
         return max(max(left), n - min(right))
