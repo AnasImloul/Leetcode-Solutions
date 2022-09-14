8fccb7bd3803c19a1e34ebd50899575a02e32a49
AnasImloul <pydev27@gmail.com>
2022-09-14 19:08:17 +0100

    Runtime: 113 ms (Top 76.66%) | Memory: 45.7 MB (Top 51.16%)

--- scripts/algorithms/C/Combination Sum/Combination Sum.js
+++ scripts/algorithms/C/Combination Sum/Combination Sum.js
@@ -1,3 +1,4 @@
+// Runtime: 113 ms (Top 76.66%) | Memory: 45.7 MB (Top 51.16%)
 function recursion(index, list, target, res, arr){
     if(index == arr.length){
         if(target == 0){
@@ -5,19 +6,19 @@ function recursion(index, list, target, res, arr){
         }
         return;
     }
-    
+
     if(arr[index] <= target){
-        
+
          list.push(arr[index]);
-        
+
          recursion(index, list, target - arr[index], res, arr);
-        
+
          list.pop();
-        
+
     }
-    
+
     recursion(index + 1, list, target, res, arr);
-   
+
 }
 var combinationSum = function(candidates, target) {
     let res = [];
