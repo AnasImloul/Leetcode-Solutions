670d80b84b6c6e0edcf2506b5747fccc368047c4
AnasImloul <pydev27@gmail.com>
2022-09-14 18:49:47 +0100

    Runtime: 4 ms (Top 39.06%) | Memory: 7.6 MB (Top 77.62%)

--- scripts/algorithms/L/Last Stone Weight/Last Stone Weight.cpp
+++ scripts/algorithms/L/Last Stone Weight/Last Stone Weight.cpp
@@ -1,7 +1,8 @@
+// Runtime: 4 ms (Top 39.06%) | Memory: 7.6 MB (Top 77.62%)
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        while(stones.size()>1){        
+        while(stones.size()>1){
             sort(stones.begin(), stones.end(), greater<int>());
             stones[1] = (stones[0]-stones[1]);
             stones.erase(stones.begin());
