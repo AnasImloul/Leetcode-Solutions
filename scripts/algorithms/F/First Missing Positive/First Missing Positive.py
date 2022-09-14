5a35c7f965c4f7cd3e75188f76e8c317bb29a7c3
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:22 +0100

    Runtime: 295 ms (Top 90.42%) | Memory: 28 MB (Top 72.11%)

--- scripts/algorithms/F/First Missing Positive/First Missing Positive.py
+++ scripts/algorithms/F/First Missing Positive/First Missing Positive.py
@@ -1,9 +1,10 @@
+# Runtime: 295 ms (Top 90.42%) | Memory: 28 MB (Top 72.11%)
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         mn = float('inf')
         mx = 0
         numsSet = set()
-        
+
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] > 0:
                 if nums[i] < mn:
@@ -12,7 +13,7 @@ class Solution:
                     mx = nums[i]
                 numsSet.add(nums[i])
             del nums[i]
-        
+
         if mn >= 2:
             return 1
         if len(numsSet) == mx:
