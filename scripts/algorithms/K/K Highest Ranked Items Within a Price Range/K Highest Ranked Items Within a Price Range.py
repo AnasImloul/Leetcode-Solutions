b5e5acfd69c72c950a86e0d63430db0111c9b2c2
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:35 +0100

    Runtime: 3258 ms (Top 98.66%) | Memory: 66 MB (Top 18.12%)

--- scripts/algorithms/K/K Highest Ranked Items Within a Price Range/K Highest Ranked Items Within a Price Range.py
+++ scripts/algorithms/K/K Highest Ranked Items Within a Price Range/K Highest Ranked Items Within a Price Range.py
@@ -1,3 +1,4 @@
+# Runtime: 3258 ms (Top 98.66%) | Memory: 66 MB (Top 18.12%)
 class Solution:
     def highestRankedKItems(self, G, pricing, start, k):
         m, n = len(G), len(G[0])
@@ -7,17 +8,17 @@ class Solution:
         visited.add((row, col))
         d = deque([node])
         ans = []
-        
+
         while d:
             dist, cost, row, col = d.popleft()
             if pricing[0] <= cost <= pricing[1]:
                 ans += [(dist, cost, row, col)]
-            
+
             for x, y in (row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1):
                 if 0 <= x <= m-1 and 0 <= y <= n-1 and (x, y) not in visited and G[x][y] != 0:
                     d.append((dist + 1, G[x][y], x, y))
                     visited.add((x, y))
-        
+
         ans = sorted(ans)
-        
+
         return [[x, y] for _, _, x, y in ans[:k]]
