1bb383aad60bc9c79a0d56019bd78250e89d76cc
AnasImloul <pydev27@gmail.com>
2022-09-14 12:44:36 +0100

    Runtime: 73 ms (Top 74.11%) | Memory: 15.7 MB (Top 42.55%)

--- scripts/algorithms/U/Unique Binary Search Trees II/Unique Binary Search Trees II.py
+++ scripts/algorithms/U/Unique Binary Search Trees II/Unique Binary Search Trees II.py
@@ -1,16 +1,17 @@
+# Runtime: 73 ms (Top 74.11%) | Memory: 15.7 MB (Top 42.55%)
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
 
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
         # define a sorted list of the numbers, for each num in that list , leftvalues
 # are left tree and right val are rightree, then for each number create a tree
 # assign the left and right to that root and append the root to the ans
-        nums = list(range(1,n+1))  
+        nums = list(range(1,n+1))
         def dfs(nums):
             if not nums:
                 return [None]
@@ -18,7 +19,7 @@ class Solution:
             for i in range(len(nums)):
                 leftTrees = dfs(nums[:i])
                 rightTrees = dfs(nums[i+1:])
-                
+
                 for l in leftTrees:
                     for r in rightTrees:
                         root = TreeNode(nums[i])
@@ -28,5 +29,3 @@ class Solution:
             return ans
 
         return dfs(nums)
-    
-    
