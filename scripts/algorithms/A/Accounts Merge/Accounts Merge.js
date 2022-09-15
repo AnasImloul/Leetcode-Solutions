95f4bb7215124ff655a7b0d9567fdf4f16f19621
AnasImloul <pydev27@gmail.com>
2022-09-15 11:41:29 +0100

    Runtime: 280 ms (Top 40.08%) | Memory: 78.1 MB (Top 13.99%)

--- scripts/algorithms/A/Accounts Merge/Accounts Merge.js
+++ scripts/algorithms/A/Accounts Merge/Accounts Merge.js
@@ -1,13 +1,14 @@
+// Runtime: 280 ms (Top 40.08%) | Memory: 78.1 MB (Top 13.99%)
 /**
  * @param {string[][]} accounts
  * @return {string[][]}
  */
 
-var accountsMerge = function(accounts) {  
-    
+var accountsMerge = function(accounts) {
+
     let graph = {};
     let nameDict = {};
-    
+
     for (let acc of accounts) {
         let name = acc[0];
         nameDict[acc[1]] = name;
@@ -20,10 +21,10 @@ var accountsMerge = function(accounts) {
             }
         }
     }
-    
+
     let res = [];
     let visited = new Set();
-    
+
     let dfs = function (key) {
         visited.add(key);
         let emails = [key];
@@ -32,10 +33,10 @@ var accountsMerge = function(accounts) {
                 emails.push(...dfs(e));
             }
         })
-            
+
         return emails;
     }
-    
+
     for (let key in graph) {
         if (!visited.has(key)) {
             let temp = dfs(key);
@@ -44,6 +45,6 @@ var accountsMerge = function(accounts) {
             res.push(temp);
         }
     }
-    
+
     return res;
 };
