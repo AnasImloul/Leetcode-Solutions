0645334aebdb152070b9c99bf6d8097e1d6248a2
AnasImloul <pydev27@gmail.com>
2022-09-15 11:37:58 +0100

    Runtime: 522 ms (Top 24.95%) | Memory: 104.9 MB (Top 77.23%)

--- scripts/algorithms/S/Shortest Subarray with Sum at Least K/Shortest Subarray with Sum at Least K.cpp
+++ scripts/algorithms/S/Shortest Subarray with Sum at Least K/Shortest Subarray with Sum at Least K.cpp
@@ -1,3 +1,4 @@
+// Runtime: 522 ms (Top 24.95%) | Memory: 104.9 MB (Top 77.23%)
 class Solution {
 public:
     //2-pointer doesnt work on neg elements
@@ -7,24 +8,23 @@ public:
         int minsize = INT_MAX;
         vector<long> prefixsum(n,0);
         deque<int> dq;
-        
+
         prefixsum[0] = nums[0];
         for(int i = 1;i<n;i++) prefixsum[i] = prefixsum[i-1] + nums[i];
 
-
         for(int i = 0;i<n;i++){
-            if(prefixsum[i]>=k) 
+            if(prefixsum[i]>=k)
                 minsize = min(minsize,i+1);
-            
+
             while(!dq.empty() && prefixsum[i]-prefixsum[dq.front()]>=k){
                 minsize = min(minsize,i-dq.front());
                 dq.pop_front();
             }
-            
+
             while(!dq.empty() && prefixsum[i]<=prefixsum[dq.back()]){
                 dq.pop_back();
             }
-            
+
             dq.push_back(i);
         }
         return (minsize==INT_MAX) ? -1 : minsize;
