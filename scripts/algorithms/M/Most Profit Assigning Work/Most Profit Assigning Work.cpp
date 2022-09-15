89d65300393ec5878c1cb08d07293a9c402cf96d
AnasImloul <pydev27@gmail.com>
2022-09-15 11:38:01 +0100

    Runtime: 157 ms (Top 36.41%) | Memory: 37.7 MB (Top 62.44%)

--- scripts/algorithms/M/Most Profit Assigning Work/Most Profit Assigning Work.cpp
+++ scripts/algorithms/M/Most Profit Assigning Work/Most Profit Assigning Work.cpp
@@ -1,10 +1,11 @@
+// Runtime: 157 ms (Top 36.41%) | Memory: 37.7 MB (Top 62.44%)
 class Solution {
 public:
     static bool cmp(pair<int,int>a, pair<int,int>b){
         if(a.first == b.first){
             return a.second<b.second;
         }
-        
+
         return a.first>b.first;
     }
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
@@ -15,10 +16,10 @@ public:
         }
         sort(vp.begin(), vp.end(), cmp);
         sort(worker.begin(), worker.end(), greater<int>());
-        
+
         //i is for traversing the vp and j is for traversing the worker
         int i = 0, j = 0;
-        
+
         while(i<vp.size() and j<worker.size()){
             if(worker[j]>=vp[i].second){
                 ans+=vp[i].first;
@@ -28,7 +29,7 @@ public:
                 i++;
             }
         }
-        
+
         return ans;
     }
 };
