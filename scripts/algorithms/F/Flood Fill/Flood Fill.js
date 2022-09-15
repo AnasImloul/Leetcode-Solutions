f697bffb15a474ef72df5f4df26f80f7b2abc597
AnasImloul <pydev27@gmail.com>
2022-09-15 11:41:30 +0100

    Runtime: 149 ms (Top 9.09%) | Memory: 44.8 MB (Top 18.89%)

--- scripts/algorithms/F/Flood Fill/Flood Fill.js
+++ scripts/algorithms/F/Flood Fill/Flood Fill.js
@@ -1,3 +1,4 @@
+// Runtime: 149 ms (Top 9.09%) | Memory: 44.8 MB (Top 18.89%)
 /**
  * @param {number[][]} image
  * @param {number} sr
@@ -10,32 +11,32 @@ var floodFill = function(image, sr, sc, color) {
     const startingPixelColor = image[sr][sc]
     const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
     const seenPixels = new Set()
-    
+
     if (
         startingPixelColor === undefined ||
         startingPixelColor === color
     ) return image
-    
+
     for (const pixel of pixelsToCheck) {
         const currentPixelColor = image[pixel[0]]?.[pixel[1]]
-        
+
         if (
             currentPixelColor === undefined ||
             startingPixelColor !== currentPixelColor
         ) continue
-            
+
         image[pixel[0]][pixel[1]] = color
-        
+
         for (const direction of directions) {
             const newPixel = [pixel[0] + direction[0], pixel[1] + direction[1]]
             const pixelString = newPixel.join()
-            
+
             if (seenPixels.has(pixelString)) continue
-            
+
             pixelsToCheck.push(newPixel)
             seenPixels.add(pixelString)
         }
     }
-    
+
     return image
 };
