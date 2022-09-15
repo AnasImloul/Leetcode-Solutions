7247804c5e6f934ab29f720fae7e258617c0b40a
AnasImloul <pydev27@gmail.com>
2022-09-15 11:37:59 +0100

    Runtime: 323 ms (Top 7.91%) | Memory: 93.3 MB (Top 89.21%)

--- scripts/algorithms/B/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
+++ scripts/algorithms/B/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
@@ -1,10 +1,11 @@
+// Runtime: 323 ms (Top 7.91%) | Memory: 93.3 MB (Top 89.21%)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int lsf = INT_MAX;
         int op = 0;
         int pist = 0;
-        
+
         for(int i = 0; i < prices.size(); i++){
             if(prices[i] < lsf){
                 lsf = prices[i];
