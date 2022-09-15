445bb446b4db1d01998e10bd4a2342c6c1a82fca
AnasImloul <pydev27@gmail.com>
2022-09-15 09:24:46 +0100

    Runtime: 59 ms (Top 27.86%) | Memory: 13.9 MB (Top 62.57%)

--- scripts/algorithms/L/Last Stone Weight/Last Stone Weight.py
+++ scripts/algorithms/L/Last Stone Weight/Last Stone Weight.py
@@ -1,3 +1,4 @@
+# Runtime: 59 ms (Top 27.86%) | Memory: 13.9 MB (Top 62.57%)
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         stones = [-x for x in stones]
@@ -12,4 +13,3 @@ class Solution:
         if len(stones):
             return -heapq.heappop(stones)
         return 0
-
