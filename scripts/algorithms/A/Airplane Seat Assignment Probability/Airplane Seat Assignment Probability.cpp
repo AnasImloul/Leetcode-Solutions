b7725abc1702abde1a96a38233fd7cfdefb0e5c9
AnasImloul <pydev27@gmail.com>
2022-09-15 11:37:57 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6 MB (Top 31.29%)

--- scripts/algorithms/A/Airplane Seat Assignment Probability/Airplane Seat Assignment Probability.cpp
+++ scripts/algorithms/A/Airplane Seat Assignment Probability/Airplane Seat Assignment Probability.cpp
@@ -1,7 +1,8 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6 MB (Top 31.29%)
 class Solution {
 public:
-	double nthPersonGetsNthSeat(int n) {
-		if(n==1)return (double)1;
-		return (double)1/2;
-	}
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1)return (double)1;
+        return (double)1/2;
+    }
 };
