88518a6379af84cbdee23fae9763bd716f73b2c3
AnasImloul <pydev27@gmail.com>
2022-09-15 11:47:31 +0100

    Runtime: 68 ms (Top 73.46%) | Memory: 43.2 MB (Top 88.65%)

--- scripts/algorithms/S/Substring with Concatenation of All Words/Substring with Concatenation of All Words.java
+++ scripts/algorithms/S/Substring with Concatenation of All Words/Substring with Concatenation of All Words.java
@@ -1,6 +1,7 @@
+// Runtime: 68 ms (Top 73.46%) | Memory: 43.2 MB (Top 88.65%)
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        
+
         HashMap<String, Integer> input = new HashMap<>();
         int ID = 1;
         HashMap<Integer, Integer> count = new HashMap<>();
@@ -9,17 +10,17 @@ class Solution {
                 input.put(word, ID++);
             int id = input.get(word);
             count.put(id,count.getOrDefault(id,0)+1);
-            
+
         }
         int len = s.length();
         int wordLen = words[0].length();
         int numWords = words.length;
         int windowLen = wordLen*numWords;
         int lastIndex = s.length()-windowLen;
-        
+
         int curWordId[] = new int[len];
         String cur = " "+s.substring(0,wordLen-1);
-        
+
         //Change to int array
         for(int i = 0; i< (len-wordLen+1); i++) {
             cur = cur.substring(1, cur.length())+s.charAt(i+wordLen-1);
@@ -30,15 +31,15 @@ class Solution {
             }
         }
         List<Integer> res = new ArrayList<>();
-        
+
         //compare using int make it faster 30 times in each comparison
         for(int i = 0; i<= lastIndex; i++) {
-            
+
             HashMap<Integer, Integer> winMap = new HashMap<>();
             for(int j = 0; j < windowLen && curWordId[i] != -1; j+=wordLen) {
-                
+
                 int candidate = curWordId[j+i];
-                
+
                 if(!count.containsKey(candidate))
                     break;
                 else{
@@ -46,16 +47,15 @@ class Solution {
                 }
                 if(winMap.get(candidate) > count.get(candidate))
                     break;
-                
-                
+
                 if(j == (windowLen - wordLen) && winMap.size() == count.size()){
                     res.add(i);
-                    
+
                 }
-                
+
             }
         }
-        
+
         return res;
     }
 }
