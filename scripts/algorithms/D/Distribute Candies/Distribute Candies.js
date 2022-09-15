a8c669d9da8655adcde7df852bcf00ee11b43764
AnasImloul <pydev27@gmail.com>
2022-09-15 11:41:26 +0100

    Runtime: 222 ms (Top 37.27%) | Memory: 56.5 MB (Top 32.38%)

--- scripts/algorithms/D/Distribute Candies/Distribute Candies.js
+++ scripts/algorithms/D/Distribute Candies/Distribute Candies.js
@@ -1,12 +1,12 @@
+// Runtime: 222 ms (Top 37.27%) | Memory: 56.5 MB (Top 32.38%)
 /**
  * @param {number[]} candyType
  * @return {number}
  */
 var distributeCandies = function(candyType) {
-    
+
     const set = new Set();
     candyType.map(e => set.add(e));
     return candyType.length/2 > set.size ? set.size : candyType.length/2
-    
-};
 
+};
