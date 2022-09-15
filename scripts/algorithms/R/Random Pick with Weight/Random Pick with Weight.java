2b3c216e362e785ee45961dbddf59d99945217fd
AnasImloul <pydev27@gmail.com>
2022-09-15 11:47:33 +0100

    Runtime: 50 ms (Top 42.43%) | Memory: 56.3 MB (Top 78.21%)

--- scripts/algorithms/R/Random Pick with Weight/Random Pick with Weight.java
+++ scripts/algorithms/R/Random Pick with Weight/Random Pick with Weight.java
@@ -1,5 +1,6 @@
+// Runtime: 50 ms (Top 42.43%) | Memory: 56.3 MB (Top 78.21%)
 class Solution {
-    
+
     private int[] prefixSum;
     private Random random;
 
@@ -9,17 +10,17 @@ class Solution {
         prefixSum = w;
         random = new Random();
     }
-    
+
     public int pickIndex() {
         int num = 1 + random.nextInt(prefixSum[prefixSum.length - 1]); // Generate random number between 1 and total sum of weights
         int left = 0;
         int right = prefixSum.length - 1;
-        
+
         while (left < right) {
             int mid = (left + right) / 2;
             if (num == prefixSum[mid])
                 return mid;
-            else if (num < prefixSum[mid]) 
+            else if (num < prefixSum[mid])
                 right = mid;
             else
                 left = mid + 1;
