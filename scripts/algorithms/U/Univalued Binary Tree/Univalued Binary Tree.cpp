c1efefe30f6602d9a2f6d3b8fccb9fba0a4d3a78
AnasImloul <pydev27@gmail.com>
2022-09-15 11:38:00 +0100

    Runtime: 8 ms (Top 19.98%) | Memory: 9.9 MB (Top 77.87%)

--- scripts/algorithms/U/Univalued Binary Tree/Univalued Binary Tree.cpp
+++ scripts/algorithms/U/Univalued Binary Tree/Univalued Binary Tree.cpp
@@ -1,3 +1,4 @@
+// Runtime: 8 ms (Top 19.98%) | Memory: 9.9 MB (Top 77.87%)
 class Solution {
 public:
     bool recur(TreeNode* root, int value){
@@ -7,17 +8,13 @@ public:
             return false;
         }
         return recur(root->left,value) &&recur(root->right,value);
-        
+
     }
     bool isUnivalTree(TreeNode* root) {
         if(root==NULL)
             return true;
         int value=root->val;
         return recur(root,value);
-        
-        
+
     }
 };
-
-
-
