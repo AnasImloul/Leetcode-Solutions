cbae679effad246b5edf2fcba505dfc4b775d483
AnasImloul <pydev27@gmail.com>
2022-09-15 11:47:29 +0100

    Runtime: 2 ms (Top 85.89%) | Memory: 41.5 MB (Top 67.63%)

--- scripts/algorithms/C/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.java
+++ scripts/algorithms/C/Count Triplets That Can Form Two Arrays of Equal XOR/Count Triplets That Can Form Two Arrays of Equal XOR.java
@@ -1,7 +1,8 @@
+// Runtime: 2 ms (Top 85.89%) | Memory: 41.5 MB (Top 67.63%)
 class Solution {
     public int countTriplets(int[] arr) {
         int count=0;
-        
+
         for(int i=0;i<arr.length;i++){
             int val=0;
             val=val^arr[i];
