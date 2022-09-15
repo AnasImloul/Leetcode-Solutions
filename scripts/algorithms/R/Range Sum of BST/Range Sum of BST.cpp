5f86a0b3e2bc13a9e0e60dec19ab2637618c8f57
AnasImloul <pydev27@gmail.com>
2022-09-15 11:38:03 +0100

    Runtime: 235 ms (Top 36.09%) | Memory: 64.6 MB (Top 92.35%)

--- scripts/algorithms/R/Range Sum of BST/Range Sum of BST.cpp
+++ scripts/algorithms/R/Range Sum of BST/Range Sum of BST.cpp
@@ -1,21 +1,21 @@
+// Runtime: 235 ms (Top 36.09%) | Memory: 64.6 MB (Top 92.35%)
 class Solution {
 public:
 
-	int solve(TreeNode* root, int low, int high){
-		if(root == NULL)
-			return 0;
+    int solve(TreeNode* root, int low, int high){
+        if(root == NULL)
+            return 0;
 
-		int sum = 0;
-		if(low <= root->val && root->val <= high){
-			sum = root->val;
-		}
+        int sum = 0;
+        if(low <= root->val && root->val <= high){
+            sum = root->val;
+        }
 
-		return sum + solve(root->left, low, high) + solve(root->right, low, high);
-	}
+        return sum + solve(root->left, low, high) + solve(root->right, low, high);
+    }
 
+    int rangeSumBST(TreeNode* root, int low, int high) {
 
-	int rangeSumBST(TreeNode* root, int low, int high) {
-
-		return solve(root, low, high);
-	}
+        return solve(root, low, high);
+    }
 };
