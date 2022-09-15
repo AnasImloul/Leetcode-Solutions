7ae1e67574648ecf2b87337e5c8bca08829709e7
AnasImloul <pydev27@gmail.com>
2022-09-15 11:47:25 +0100

    Runtime: 1 ms (Top 36.84%) | Memory: 41.2 MB (Top 58.44%)

--- scripts/algorithms/E/Excel Sheet Column Title/Excel Sheet Column Title.java
+++ scripts/algorithms/E/Excel Sheet Column Title/Excel Sheet Column Title.java
@@ -1,9 +1,10 @@
+// Runtime: 1 ms (Top 36.84%) | Memory: 41.2 MB (Top 58.44%)
 class Solution {
     public String convertToTitle(int columnNumber) {
         String ans = "";
         while(columnNumber > 0){
             columnNumber--;
-            ans = String.valueOf((char)('A' + (int)((26 + (long)columnNumber)  % 26))) + ans;
+            ans = String.valueOf((char)('A' + (int)((26 + (long)columnNumber) % 26))) + ans;
             columnNumber /= 26;
         }
         return ans;
