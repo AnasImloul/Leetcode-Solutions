d1eea6686f5c8b6b8b28671affc4a3d92a7660fa
AnasImloul <pydev27@gmail.com>
2022-09-16 17:45:53 +0100

    Runtime: 319 ms (Top 23.25%) | Memory: 57.2 MB (Top 92.90%)

--- scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.cpp
+++ scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.cpp
@@ -1,9 +1,10 @@
+// Runtime: 319 ms (Top 23.25%) | Memory: 57.2 MB (Top 92.90%)
 class RecentCounter {
 public:
     queue<int> q;
     RecentCounter() {
     }
-    
+
     int ping(int t) {
         q.push(t);
         int x = q.front();
