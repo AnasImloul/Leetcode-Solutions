07fc6b89548f14f90a63f46c5dda247da24ab1ff
AnasImloul <pydev27@gmail.com>
2022-09-16 17:31:03 +0100

    Runtime: 199 ms (Top 50.13%) | Memory: 64.1 MB (Top 50.22%)

--- scripts/algorithms/C/Contains Duplicate II/Contains Duplicate II.js
+++ scripts/algorithms/C/Contains Duplicate II/Contains Duplicate II.js
@@ -1,3 +1,4 @@
+// Runtime: 199 ms (Top 50.13%) | Memory: 64.1 MB (Top 50.22%)
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -7,12 +8,12 @@ var containsNearbyDuplicate = function(nums, k) {
     const duplicateCheck = {};
     let isValid = false;
     for(var indexI=0; indexI<nums.length;indexI++){
-         if(duplicateCheck[nums[indexI]] > -1)  {
+         if(duplicateCheck[nums[indexI]] > -1) {
             if(Math.abs(duplicateCheck[nums[indexI]] - indexI) <= k) {
               isValid = true;
               break;
             }
-            
+
         }
         duplicateCheck[nums[indexI]] = indexI;
     }
