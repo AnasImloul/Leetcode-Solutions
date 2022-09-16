a9a2ef19d9c84f5a4c519d3ee15c36ace519c499
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:11 +0100

    Runtime: 2 ms (Top 91.14%) | Memory: 57.8 MB (Top 74.85%)

--- scripts/algorithms/F/First Missing Positive/First Missing Positive.java
+++ scripts/algorithms/F/First Missing Positive/First Missing Positive.java
@@ -1,22 +1,23 @@
+// Runtime: 2 ms (Top 91.14%) | Memory: 57.8 MB (Top 74.85%)
 class Solution {
    public int firstMissingPositive(int[] nums) {
-       //cyclic sort 
+       //cyclic sort
         int i = 0;
         while (i<nums.length){
-            int correct  = nums[i]-1;
+            int correct = nums[i]-1;
             if(nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[correct]){
                 swap(nums,i,correct);
             }else{
                 i++;
             }
         }
-       //linear search to find the missing number 
+       //linear search to find the missing number
         for(int index=0;index<nums.length;index++){
             if (nums[index] != index+1) {
                 return index+1;
             }
         }
-       //if array has all the elements match to its index then 1st missing num will be 
+       //if array has all the elements match to its index then 1st missing num will be
        //nums.length+1
         return nums.length+1;
     }
