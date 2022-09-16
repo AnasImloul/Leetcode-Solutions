d6b7cbf04eeee97a42a63aea2470e3f4e07d3d08
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:17 +0100

    Runtime: 6 ms (Top 52.91%) | Memory: 42.5 MB (Top 42.22%)

--- scripts/algorithms/R/Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses.java
+++ scripts/algorithms/R/Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses.java
@@ -1,7 +1,8 @@
+// Runtime: 6 ms (Top 52.91%) | Memory: 42.5 MB (Top 42.22%)
 class Solution {
     public String reverseParentheses(String s) {
         Stack<String> stack = new Stack<>();
-        
+
         int j = 0;
         while(j < s.length()){
             /*
@@ -10,15 +11,15 @@ class Solution {
             */
             if(s.charAt(j) != ')')
                 stack.push(s.charAt(j)+"");
-            
+
             /*
                 Now that we have encountered an ')', its time
                 to start popping from top of stack unless we find an opening
                 parenthesis
-                
+
                 then we just need to reverse the string formed by popping
-                and put it back on stack. 
-                
+                and put it back on stack.
+
                 Try dry running and it will all make sense
             */
             else{
@@ -26,13 +27,13 @@ class Solution {
                 while(!stack.isEmpty() && !stack.peek().equals("(")){
                     sb.append(stack.pop());
                 }
-                
+
                 stack.pop();
                 stack.push(sb.reverse().toString());
             }
             j++;
         }
-        
+
         /*
             We have our result string in the stack now,
             we just need to pop it and return the reverse of it.
@@ -40,7 +41,7 @@ class Solution {
         StringBuilder res = new StringBuilder();
         while(!stack.isEmpty())
             res.append(stack.pop());
-        
+
         return res.reverse().toString();
     }
 }
