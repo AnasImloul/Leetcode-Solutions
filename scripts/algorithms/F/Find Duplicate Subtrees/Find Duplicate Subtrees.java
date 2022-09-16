b6f25455f902850dff9ee7a518439988d5267bce
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:21 +0100

    Runtime: 1783 ms (Top 5.02%) | Memory: 64.5 MB (Top 11.80%)

--- scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.java
+++ scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.java
@@ -1,16 +1,17 @@
+// Runtime: 1783 ms (Top 5.02%) | Memory: 64.5 MB (Top 11.80%)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
@@ -19,17 +20,16 @@ class Solution {
         HashSet<String> hashes = new HashSet<String>();
         HashSet<String> added = new HashSet<String>();
 
-        
         // for each node, perform a dfs traversal and generate a hash
-        // check if the hash already exists in a set, 
+        // check if the hash already exists in a set,
         // if it does, get the treenode and add it to the list
         Stack<TreeNode> s = new Stack<TreeNode>();
-        
+
         s.add(root);
         while(!s.isEmpty()){
             TreeNode tmp = s.pop();
             dfs(tmp, "", tmp, list, hashes, added);
-            
+
             if(tmp.left != null){
                 s.add(tmp.left);
             }
@@ -37,22 +37,21 @@ class Solution {
                 s.add(tmp.right);
             }
         }
-        
+
         return list;
-        
+
     }
-    
+
     public void dfs(TreeNode parent, String hash, TreeNode root, List<TreeNode> list, HashSet<String> set, HashSet<String> added){
-        
+
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        
+
         stack.add(root);
         //String hash = "";
         hash += root.val + "ROOT,";
         while(!stack.isEmpty()){
             TreeNode tmp = stack.pop();
             //hash += tmp.val + ",";
-            
 
             if(tmp.left != null){
                 hash += tmp.left.val + "L,";
@@ -80,7 +79,7 @@ class Solution {
                 }
                 return;
             }
-            
+
         }
     }
 }
