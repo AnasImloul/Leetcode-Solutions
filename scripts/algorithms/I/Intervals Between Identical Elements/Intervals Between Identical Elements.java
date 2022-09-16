76532d9c1af9f1e7eb46f98a7a69fe08ac177671
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:20 +0100

    Runtime: 544 ms (Top 6.59%) | Memory: 244.1 MB (Top 25.15%)

--- scripts/algorithms/I/Intervals Between Identical Elements/Intervals Between Identical Elements.java
+++ scripts/algorithms/I/Intervals Between Identical Elements/Intervals Between Identical Elements.java
@@ -1,3 +1,4 @@
+// Runtime: 544 ms (Top 6.59%) | Memory: 244.1 MB (Top 25.15%)
 class Solution {
     public long[] getDistances(int[] arr) {
         long[] output = new long[arr.length];
@@ -8,12 +9,12 @@ class Solution {
                 sumMap.put(arr[i], 0l);
                 countMap.put(arr[i], 0);
             }
-            
+
             output[i] += i * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
             sumMap.put(arr[i], sumMap.get(arr[i]) + i);
             countMap.put(arr[i], countMap.get(arr[i]) + 1);
         }
-        
+
         sumMap = new HashMap<>();
         countMap = new HashMap<>();
         int len = arr.length;
@@ -22,12 +23,12 @@ class Solution {
                 sumMap.put(arr[i], 0l);
                 countMap.put(arr[i], 0);
             }
-            
+
             output[i] += (len - i - 1) * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
             sumMap.put(arr[i], sumMap.get(arr[i]) + (len - i - 1));
             countMap.put(arr[i], countMap.get(arr[i]) + 1);
         }
-        
+
         return output;
     }
 }
