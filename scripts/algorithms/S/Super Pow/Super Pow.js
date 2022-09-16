c86708e539ffd4a1805a9985c67f74dff2b838b6
AnasImloul <pydev27@gmail.com>
2022-09-16 17:30:56 +0100

    Runtime: 100 ms (Top 64.52%) | Memory: 42.9 MB (Top 64.52%)

--- scripts/algorithms/S/Super Pow/Super Pow.js
+++ scripts/algorithms/S/Super Pow/Super Pow.js
@@ -1,18 +1,19 @@
+// Runtime: 100 ms (Top 64.52%) | Memory: 42.9 MB (Top 64.52%)
 var superPow = function(a, b) {
-	const MOD = 1337;
-	const pow = (num, n) => {
-		let result = 1;
-		for (let index = 0; index < n; index++) {
-			result = result * num % MOD;
-		}
-		return result;
-	};
+    const MOD = 1337;
+    const pow = (num, n) => {
+        let result = 1;
+        for (let index = 0; index < n; index++) {
+            result = result * num % MOD;
+        }
+        return result;
+    };
 
-	return b.reduceRight((result, n) => {
-		a %= MOD;
-		const powNum = result * pow(a, n) % MOD;
+    return b.reduceRight((result, n) => {
+        a %= MOD;
+        const powNum = result * pow(a, n) % MOD;
 
-		a = pow(a, 10);
-		return powNum;
-	}, 1);
+        a = pow(a, 10);
+        return powNum;
+    }, 1);
 };
