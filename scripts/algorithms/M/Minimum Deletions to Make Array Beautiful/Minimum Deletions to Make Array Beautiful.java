8ebadea973f803e2833e7711cdc51dcf0d8fc2e5
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:19 +0100

    Runtime: 8 ms (Top 50.94%) | Memory: 58.2 MB (Top 89.06%)

--- scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.java
+++ scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.java
@@ -1,12 +1,13 @@
+// Runtime: 8 ms (Top 50.94%) | Memory: 58.2 MB (Top 89.06%)
 class Solution {
     public int minDeletion(int[] nums) {
-        
+
         int deletion = 0, n = nums.length;
-        
+
         for (int i=0; i<n-1; ) {
             int newIndex = i-deletion;
-            if ((newIndex % 2 == 0) && nums[i] == nums[i+1])   deletion++;
-            else    i++;
+            if ((newIndex % 2 == 0) && nums[i] == nums[i+1]) deletion++;
+            else i++;
         }
         return ((n-deletion) % 2 == 0) ? deletion : deletion+1;
     }
