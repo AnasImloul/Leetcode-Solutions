9f6a97066f1094239a82cdb1647f3f82935592f1
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:16 +0100

    Runtime: 88 ms (Top 26.15%) | Memory: 43.2 MB (Top 92.07%)

--- scripts/algorithms/M/Matrix Block Sum/Matrix Block Sum.java
+++ scripts/algorithms/M/Matrix Block Sum/Matrix Block Sum.java
@@ -1,3 +1,4 @@
+// Runtime: 88 ms (Top 26.15%) | Memory: 43.2 MB (Top 92.07%)
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int m = mat.length,n = mat[0].length;
@@ -5,15 +6,15 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int val = 0;
-				// take new variables to get starting index of mat[r][c]
+                // take new variables to get starting index of mat[r][c]
                 int p = i-k,q=j-k;
-				//make sure p and q are atleast 0 (i.e. valid)
+                //make sure p and q are atleast 0 (i.e. valid)
                 while(p<0)
                     p++;
                 while(q<0)
                     q++;
-				//traverse again in the matrix with starting at p,q and ending at i+k and j+k
-				//add conditions to make sure the indices dont cross the values of m and n
+                //traverse again in the matrix with starting at p,q and ending at i+k and j+k
+                //add conditions to make sure the indices dont cross the values of m and n
                 for(int x = p;x<=i+k && x<m;x++){
                     for(int y = q;y<=j+k && y<n;y++){
                         val += mat[x][y];
