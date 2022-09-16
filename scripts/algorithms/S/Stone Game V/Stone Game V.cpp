28698eabad68d62aca6b6ddf5fcf0312fef4a95f
AnasImloul <pydev27@gmail.com>
2022-09-16 17:45:50 +0100

    Runtime: 1664 ms (Top 43.66%) | Memory: 10.6 MB (Top 82.68%)

--- scripts/algorithms/S/Stone Game V/Stone Game V.cpp
+++ scripts/algorithms/S/Stone Game V/Stone Game V.cpp
@@ -1,16 +1,17 @@
+// Runtime: 1664 ms (Top 43.66%) | Memory: 10.6 MB (Top 82.68%)
 class Solution {
 public:
     int dp[501][501];
-    
+
     int f(vector<int> &v,int i,int j){
-        
+
         if(i>=j) return 0;
-        
+
         if(dp[i][j]!=-1) return dp[i][j];
-        
+
         int r=0;
         for(int k=i;k<=j;k++) r+=v[k];
-        
+
         int l=0,ans=0;
         for(int k=i;k<=j;k++){
             l+=v[k];
@@ -21,11 +22,11 @@ public:
         }
         return dp[i][j]=ans;
     }
-    
+
     int stoneGameV(vector<int>& stoneValue) {
-        
+
         memset(dp,-1,sizeof(dp));
         return f(stoneValue,0,stoneValue.size()-1);
-        
+
     }
 };
