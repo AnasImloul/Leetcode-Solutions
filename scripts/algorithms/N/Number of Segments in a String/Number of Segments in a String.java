c1fbc98c6793ca901f1a0c6a4f6b446797b58ad9
AnasImloul <pydev27@gmail.com>
2022-09-16 17:34:15 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 41.9 MB (Top 35.26%)

--- scripts/algorithms/N/Number of Segments in a String/Number of Segments in a String.java
+++ scripts/algorithms/N/Number of Segments in a String/Number of Segments in a String.java
@@ -1,8 +1,9 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 41.9 MB (Top 35.26%)
 class Solution {
     public int countSegments(String s) {
         int length = 0;
         boolean flag = false;
-        
+
         for(Character c : s.toCharArray()) {
             if(c == ' ' && flag) {
                 length++;
@@ -11,7 +12,7 @@ class Solution {
                 flag = true;
             }
         }
-        
+
         return flag ? length + 1 : length;
     }
 }
