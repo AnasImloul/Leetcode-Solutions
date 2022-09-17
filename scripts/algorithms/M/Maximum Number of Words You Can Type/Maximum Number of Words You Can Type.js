c112600b9d3fe6a0c603d4712b2a9d167dd9e1b6
AnasImloul <pydev27@gmail.com>
2022-09-17 16:34:31 +0100

    Runtime: 108 ms (Top 32.13%) | Memory: 44.5 MB (Top 16.74%)

--- scripts/algorithms/M/Maximum Number of Words You Can Type/Maximum Number of Words You Can Type.js
+++ scripts/algorithms/M/Maximum Number of Words You Can Type/Maximum Number of Words You Can Type.js
@@ -1,9 +1,10 @@
+// Runtime: 108 ms (Top 32.13%) | Memory: 44.5 MB (Top 16.74%)
 var canBeTypedWords = function(text, brokenLetters) {
     let regexp="["+brokenLetters+"]\+"
     let word=text.split(" "), count=0;
     for(let i=0; i<word.length; i++){
         let work=true;
-		// if matches, means word[i] contains malfunction letters.
+        // if matches, means word[i] contains malfunction letters.
         if(word[i].match(regexp)){work=false};
         if(work){count++};
     }
