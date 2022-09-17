3e31045bfa50679a4f6d6a536d7ff26651e1a2d3
AnasImloul <pydev27@gmail.com>
2022-09-17 16:34:34 +0100

    Runtime: 516 ms (Top 87.84%) | Memory: 88.6 MB (Top 39.19%)

--- scripts/algorithms/C/Create Binary Tree From Descriptions/Create Binary Tree From Descriptions.js
+++ scripts/algorithms/C/Create Binary Tree From Descriptions/Create Binary Tree From Descriptions.js
@@ -1,15 +1,16 @@
+// Runtime: 516 ms (Top 87.84%) | Memory: 88.6 MB (Top 39.19%)
 var createBinaryTree = function(descriptions) {
   let nodes = new Map(), children = new Set();
   for (let [parent, child, isLeft] of descriptions) {
     let parentNode = nodes.get(parent) || new TreeNode(parent);
     if (!nodes.has(parent)) nodes.set(parent, parentNode);
-    
+
     let childNode = nodes.get(child) || new TreeNode(child);
     if (!nodes.has(child)) nodes.set(child, childNode);
-    
+
     if (isLeft) parentNode.left = childNode;
     else parentNode.right = childNode;
-    
+
     children.add(child);
   }
 
