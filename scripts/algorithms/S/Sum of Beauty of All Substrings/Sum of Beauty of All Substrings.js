f6ae077e86a9e145181816b7a3d9ba9a42401b09
AnasImloul <pydev27@gmail.com>
2022-09-17 16:34:33 +0100

    Runtime: 667 ms (Top 81.08%) | Memory: 48.8 MB (Top 62.16%)

--- scripts/algorithms/S/Sum of Beauty of All Substrings/Sum of Beauty of All Substrings.js
+++ scripts/algorithms/S/Sum of Beauty of All Substrings/Sum of Beauty of All Substrings.js
@@ -1,9 +1,10 @@
+// Runtime: 667 ms (Top 81.08%) | Memory: 48.8 MB (Top 62.16%)
 var beautySum = function(s) {
     const len = s.length;
-    
+
     let ans = 0;
     const freq = new Array(26).fill(0);
-    
+
     for(let i = 0; i < len; i++) {
         freq[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
         for(let j = i + 1; j < len; j++) {
@@ -12,11 +13,11 @@ var beautySum = function(s) {
         }
         freq.fill(0);
     }
-    
+
     function minMaxDiff(freq) {
         freq = freq.filter(f => f != 0);
         return Math.max(...freq) - Math.min(...freq);
     }
-    
+
     return ans;
 };
