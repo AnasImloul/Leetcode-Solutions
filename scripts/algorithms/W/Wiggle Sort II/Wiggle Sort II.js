4b8ab77faa05bcab59f5200616af43605b12bcd7
AnasImloul <pydev27@gmail.com>
2022-09-17 16:34:38 +0100

    Runtime: 133 ms (Top 67.12%) | Memory: 47 MB (Top 71.23%)

--- scripts/algorithms/W/Wiggle Sort II/Wiggle Sort II.js
+++ scripts/algorithms/W/Wiggle Sort II/Wiggle Sort II.js
@@ -1,11 +1,12 @@
+// Runtime: 133 ms (Top 67.12%) | Memory: 47 MB (Top 71.23%)
 
 var wiggleSort = function(nums) {
     nums.sort((a,b) => a - b);
-    
+
     let temp = [...nums];
-    
+
     let low = Math.floor((nums.length-1)/2), high = nums.length-1;
-    
+
     for(let i=0; i < nums.length; i++){
         if(i % 2 === 0){
             nums[i] = temp[low];
@@ -15,6 +16,6 @@ var wiggleSort = function(nums) {
             high--;
         }
     }
-    
+
     return nums;
 };
