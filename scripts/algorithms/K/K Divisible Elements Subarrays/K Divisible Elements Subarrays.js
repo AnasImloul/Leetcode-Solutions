7d0dd972489e6af5cccc942cd0e7f8a06a2f81cb
AnasImloul <pydev27@gmail.com>
2022-09-17 16:34:39 +0100

    Runtime: 3190 ms (Top 8.57%) | Memory: 86.8 MB (Top 45.72%)

--- scripts/algorithms/K/K Divisible Elements Subarrays/K Divisible Elements Subarrays.js
+++ scripts/algorithms/K/K Divisible Elements Subarrays/K Divisible Elements Subarrays.js
@@ -1,19 +1,20 @@
+// Runtime: 3190 ms (Top 8.57%) | Memory: 86.8 MB (Top 45.72%)
 var countDistinct = function(nums, k, p) {
     let ans = [];
-    
+
     let rec = (index,k,p,nums,ans,curr) => {
         let val = nums[index];
         let check = val%p;
         let isdiv = false;
         if(check == 0) isdiv=true;
-    
+
         if(index == nums.length) {
             if(curr.length>0){
                 ans.push(curr.join(","));
             }
             return;
         }
-        
+
         //take conditions
         if(isdiv && k==0){
           ans.push(curr.join(","));
@@ -26,18 +27,17 @@ var countDistinct = function(nums, k, p) {
              rec(index+1,k,p,nums,ans,curr);
              curr.pop()
         }
-        
-        
+
         //non take conditions
         if(curr.length == 0){
              rec(index+1,k,p,nums,ans,curr);
         } else {
             ans.push(curr.join(","));
         }
-   
+
     }
     rec(0,k,p,nums,ans,[]);
     let set = new Set(ans);
-   
+
     return set.size
 };
