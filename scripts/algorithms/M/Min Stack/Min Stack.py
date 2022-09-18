e116fe27f8ebe795d136024104a4de6edd7251c7
AnasImloul <pydev27@gmail.com>
2022-09-18 17:31:32 +0100

    Runtime: 117 ms (Top 35.13%) | Memory: 18.5 MB (Top 10.03%)

--- scripts/algorithms/M/Min Stack/Min Stack.py
+++ scripts/algorithms/M/Min Stack/Min Stack.py
@@ -1,26 +1,27 @@
+# Runtime: 117 ms (Top 35.13%) | Memory: 18.5 MB (Top 10.03%)
 class MinStack:
 
-	def __init__(self):
-		self.stack = []
+    def __init__(self):
+        self.stack = []
 
-	def push(self, val: int) -> None:
-		if not self.stack:
-			self.stack.append((val, val))
-		else:
-			self.stack.append((val, min(val, self.stack[-1][1])))
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
 
-	def pop(self) -> None:
-		if self.stack:
-			self.stack.pop()
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
 
-	def top(self) -> int:
-		if self.stack:
-			return self.stack[-1][0]
-		else:
-			return None
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+        else:
+            return None
 
-	def getMin(self) -> int:
-		if self.stack:
-			return self.stack[-1][1]
-		else:
-			return None
+    def getMin(self) -> int:
+        if self.stack:
+            return self.stack[-1][1]
+        else:
+            return None
