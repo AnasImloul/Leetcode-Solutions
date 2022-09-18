5438ac5845aa837639633e1fcb4026977363e2de
AnasImloul <pydev27@gmail.com>
2022-09-18 18:10:18 +0100

    Runtime: 122 ms (Top 14.09%) | Memory: 44.5 MB (Top 14.09%)

--- scripts/algorithms/V/Valid Parenthesis String/Valid Parenthesis String.js
+++ scripts/algorithms/V/Valid Parenthesis String/Valid Parenthesis String.js
@@ -1,3 +1,4 @@
+// Runtime: 122 ms (Top 14.09%) | Memory: 44.5 MB (Top 14.09%)
 /**
  * @param {string} s
  * @return {boolean}
@@ -7,26 +8,25 @@ var checkValidString = function(s) {
     return check(s,0,0,map);
 };
 
-
 function check(s,index,open,map){
     if(index == s.length){
         return open == 0;
     }
-    
+
     if(open < 0){
         return false;
     }
-    let string = index.toString()  + "##" + open.toString();
+    let string = index.toString() + "##" + open.toString();
     if(string in map){
         return map[string]
     }
-    
+
     if(s[index] == '('){
         let l = check(s,index+1,open+1,map)
         map[string] = l
         return l
     }else if (s[index] == ')'){
-        let r = check(s,index+1,open-1,map) 
+        let r = check(s,index+1,open-1,map)
         map[string] = r;
         return r
     }else {
