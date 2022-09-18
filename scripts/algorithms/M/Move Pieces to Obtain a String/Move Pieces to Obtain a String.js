036bca558faeb3fda88ca989fec19b241777753c
AnasImloul <pydev27@gmail.com>
2022-09-18 18:10:19 +0100

    Runtime: 151 ms (Top 47.54%) | Memory: 47.4 MB (Top 60.66%)

--- scripts/algorithms/M/Move Pieces to Obtain a String/Move Pieces to Obtain a String.js
+++ scripts/algorithms/M/Move Pieces to Obtain a String/Move Pieces to Obtain a String.js
@@ -1,3 +1,4 @@
+// Runtime: 151 ms (Top 47.54%) | Memory: 47.4 MB (Top 60.66%)
 /**
  * @param {string} start
  * @param {string} target
@@ -7,21 +8,20 @@ var canChange = function(start, target) {
     let i = 0
     let j = 0
     const len = start.length
-    
+
     while(i <= len && j <= len) {
-        while(i < len  && start[i] === '_') i++
+        while(i < len && start[i] === '_') i++
         while(j < len && target[j] === '_') j++
-        
+
         if(i === len && j === len) return true
         if(i === len || j === len) return false
-        
+
         if(start[i] !== target[j]) return false
         if(start[i] === 'L' && j > i) return false
         if(start[i] === 'R' && j < i) return false
         i++
         j++
     }
-    
+
     return false
 };
-
