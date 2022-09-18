6f419c16eb1854c82a787687546e2c896f1ab35b
AnasImloul <pydev27@gmail.com>
2022-09-18 18:10:21 +0100

    Runtime: 217 ms (Top 98.62%) | Memory: 52.7 MB (Top 26.90%)

--- scripts/algorithms/M/Maximum Number of Coins You Can Get/Maximum Number of Coins You Can Get.js
+++ scripts/algorithms/M/Maximum Number of Coins You Can Get/Maximum Number of Coins You Can Get.js
@@ -1,12 +1,13 @@
+// Runtime: 217 ms (Top 98.62%) | Memory: 52.7 MB (Top 26.90%)
 var maxCoins = function(piles) {
     let count = 0, i = 0, j = piles.length - 1;
     piles.sort((a, b) => b - a);
-    
+
     while(i < j) {
         count += piles[i + 1];
         i += 2;
         j--;
     }
-    
+
     return count;
 };
