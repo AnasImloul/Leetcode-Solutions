4b11a990e59ef8cc13760b906d47dd8de096c85f
AnasImloul <pydev27@gmail.com>
2022-09-18 16:56:24 +0100

    Runtime: 168 ms (Top 19.82%) | Memory: 126.2 MB (Top 22.91%)

--- scripts/algorithms/E/Earliest Possible Day of Full Bloom/Earliest Possible Day of Full Bloom.java
+++ scripts/algorithms/E/Earliest Possible Day of Full Bloom/Earliest Possible Day of Full Bloom.java
@@ -1,3 +1,4 @@
+// Runtime: 168 ms (Top 19.82%) | Memory: 126.2 MB (Top 22.91%)
 class Solution {
     public int earliestFullBloom(int[] plantTime, int[] growTime) {
         int n = plantTime.length;
@@ -7,15 +8,15 @@ class Solution {
             plants[i] = new int[]{plantTime[i], growTime[i]};
             totalPlantTime += plantTime[i];
         }
-		// Sort plants by grow time. 
-		// The plant with fewer grow time can be planted at the end.
+        // Sort plants by grow time.
+        // The plant with fewer grow time can be planted at the end.
         Arrays.sort(plants, (a, b) -> a[1] - b[1]);
-		// Max grow + plant time
+        // Max grow + plant time
         int maxTime = 0;
         for (int[] plant : plants) {
             maxTime = Math.max(maxTime, totalPlantTime + plant[1]);
-			// After putting this plant at the end of the chain,
-			// we can take the current plant time out of the total plant time 
+            // After putting this plant at the end of the chain,
+            // we can take the current plant time out of the total plant time
             totalPlantTime -= plant[0];
         }
         return maxTime;
