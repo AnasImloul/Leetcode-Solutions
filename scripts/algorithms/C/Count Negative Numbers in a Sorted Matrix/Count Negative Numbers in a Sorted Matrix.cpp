22611f86bb3fa93562c4d8d41ad25c40e769ed5b
AnasImloul <pydev27@gmail.com>
2022-09-18 17:37:19 +0100

    Runtime: 29 ms (Top 41.34%) | Memory: 11 MB (Top 7.78%)

--- scripts/algorithms/C/Count Negative Numbers in a Sorted Matrix/Count Negative Numbers in a Sorted Matrix.cpp
+++ scripts/algorithms/C/Count Negative Numbers in a Sorted Matrix/Count Negative Numbers in a Sorted Matrix.cpp
@@ -1,6 +1,7 @@
+// Runtime: 29 ms (Top 41.34%) | Memory: 11 MB (Top 7.78%)
 class Solution {
 public:
-	int countNegatives(vector<vector<int>>& grid) {
+    int countNegatives(vector<vector<int>>& grid) {
         int ans = 0; for (auto hehe : grid) for (int i : hehe) if (i < 0) ans++; return ans;
     }
 };
