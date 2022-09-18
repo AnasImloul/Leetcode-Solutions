b24a58b46faef67d59020dfb3427956794d56ef7
AnasImloul <pydev27@gmail.com>
2022-09-18 17:31:41 +0100

    Runtime: 766 ms (Top 22.98%) | Memory: 17.7 MB (Top 39.36%)

--- scripts/algorithms/H/Heaters/Heaters.py
+++ scripts/algorithms/H/Heaters/Heaters.py
@@ -1,28 +1,27 @@
+# Runtime: 766 ms (Top 22.98%) | Memory: 17.7 MB (Top 39.36%)
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         """
-            
+
         """
-        
-        
+
         houses.sort()
         heaters.sort()
-        
-        
+
         max_radius = -inf
-        
-        for house in houses: 
+
+        for house in houses:
             i = bisect_left(heaters, house)
-            
+
             if i == len(heaters):
                 max_radius = max(max_radius, house - heaters[-1])
-            elif i == 0: 
-                max_radius =  max(max_radius, heaters[i] - house)
+            elif i == 0:
+                max_radius = max(max_radius, heaters[i] - house)
             else:
                 curr = heaters[i]
                 prev = heaters[i-1]
                 max_radius = max(max_radius,min(abs(house - curr), abs(house-prev)))
-            
+
         return max_radius
-    
+
     # O(NLOGN)
