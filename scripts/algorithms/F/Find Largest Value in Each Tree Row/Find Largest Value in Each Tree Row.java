362a58a5a37540860073abc9f405cdaa26885e0a
AnasImloul <pydev27@gmail.com>
2022-09-18 16:56:31 +0100

    Runtime: 6 ms (Top 12.42%) | Memory: 45.3 MB (Top 32.64%)

--- scripts/algorithms/F/Find Largest Value in Each Tree Row/Find Largest Value in Each Tree Row.java
+++ scripts/algorithms/F/Find Largest Value in Each Tree Row/Find Largest Value in Each Tree Row.java
@@ -1,25 +1,26 @@
+// Runtime: 6 ms (Top 12.42%) | Memory: 45.3 MB (Top 32.64%)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     private List<Integer> li=new ArrayList<>();
-    
+
     public List<Integer> largestValues(TreeNode root) {
-        
+
         if(root==null) return li; //if root is NULL
-        
+
         //using bfs(level-order)
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
@@ -30,9 +31,9 @@ class Solution {
                 TreeNode temp=q.poll();
                 if(temp.left!=null) q.add(temp.left);
                 if(temp.right!=null) q.add(temp.right);
-                res =Math.max(res,temp.val); //comparing every node in each level to get max 
+                res =Math.max(res,temp.val); //comparing every node in each level to get max
             }
-            li.add(res); //adding each level Max value to the list 
+            li.add(res); //adding each level Max value to the list
         }
         return li;
     }
