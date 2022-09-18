733e40ffc142794a17e0bbc1d7614125fbf9a6a0
AnasImloul <pydev27@gmail.com>
2022-09-18 17:37:25 +0100

    Runtime: 195 ms (Top 40.35%) | Memory: 37.6 MB (Top 80.95%)

--- scripts/algorithms/M/My Calendar I/My Calendar I.cpp
+++ scripts/algorithms/M/My Calendar I/My Calendar I.cpp
@@ -1,16 +1,17 @@
+// Runtime: 195 ms (Top 40.35%) | Memory: 37.6 MB (Top 80.95%)
 class MyCalendar {
 public:
-	vector<pair<int,int>>cal;
-	MyCalendar() {
+    vector<pair<int,int>>cal;
+    MyCalendar() {
 
-	}
+    }
 
-	bool book(int start, int end) {
-		for(int i=0;i<cal.size();i++){
-			if(end<=cal[i].first || start>=cal[i].second)continue;
-			else return false;
-		}
-		cal.push_back({start,end});
-		return true;
-	}
+    bool book(int start, int end) {
+        for(int i=0;i<cal.size();i++){
+            if(end<=cal[i].first || start>=cal[i].second)continue;
+            else return false;
+        }
+        cal.push_back({start,end});
+        return true;
+    }
 };
