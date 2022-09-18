8258a749e2e9e78f3f424d57d45b738774408369
AnasImloul <pydev27@gmail.com>
2022-09-18 17:37:24 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 5.9 MB (Top 73.12%)

--- scripts/algorithms/M/Maximum Value at a Given Index in a Bounded Array/Maximum Value at a Given Index in a Bounded Array.cpp
+++ scripts/algorithms/M/Maximum Value at a Given Index in a Bounded Array/Maximum Value at a Given Index in a Bounded Array.cpp
@@ -1,3 +1,4 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 5.9 MB (Top 73.12%)
 class Solution {
 public:
     int maxValue(int n, int index, int maxSum) {
@@ -9,18 +10,18 @@ public:
             int mid = lo + (hi- lo)/2;
             long sum = 0;
             long rs = 0, ls = 0, m = mid -1;
-            if(r<=m) 
-				rs = m*(m+1)/2 - (m-r)*(m-r+1)/2; // if we have to calculate 9+8+7+6, sum = 9*(9+1)/2 - (9-4)(9-4+1)/2
-            else 
-				rs = m*(m+1)/2 + r - m;  // if we encounter the case in which array right to index is 3,2,1,1,1, sum = 3*(3+1)/2 + 5-2
-            if(l<=m) 
-				ls = m*(m+1)/2 - (m-l)*(m-l+1)/2; // similar to above.
-            else 
-				ls = m*(m+1)/2 + l-m;
-            
+            if(r<=m)
+                rs = m*(m+1)/2 - (m-r)*(m-r+1)/2; // if we have to calculate 9+8+7+6, sum = 9*(9+1)/2 - (9-4)(9-4+1)/2
+            else
+                rs = m*(m+1)/2 + r - m; // if we encounter the case in which array right to index is 3,2,1,1,1, sum = 3*(3+1)/2 + 5-2
+            if(l<=m)
+                ls = m*(m+1)/2 - (m-l)*(m-l+1)/2; // similar to above.
+            else
+                ls = m*(m+1)/2 + l-m;
+
             sum = mid + ls + rs;
-            
-			if(sum <= maxSum) 
+
+            if(sum <= maxSum)
                 res = mid,lo = mid+1;
             else hi = mid -1;
         }
