3c459ab75bdc039ca7b4e77dd453f62e8ae33e6c
AnasImloul <pydev27@gmail.com>
2022-09-18 16:56:29 +0100

    Runtime: 55 ms (Top 48.30%) | Memory: 81.3 MB (Top 78.98%)

--- scripts/algorithms/T/The k Strongest Values in an Array/The k Strongest Values in an Array.java
+++ scripts/algorithms/T/The k Strongest Values in an Array/The k Strongest Values in an Array.java
@@ -1,3 +1,4 @@
+// Runtime: 55 ms (Top 48.30%) | Memory: 81.3 MB (Top 78.98%)
 class Solution {
     public int[] getStrongest(int[] arr, int k) {
         int[] result = new int[k];
@@ -7,14 +8,14 @@ class Solution {
         while (left <= right) {
             int diff_l = Math.abs(arr[left] - median);
             int diff_r = Math.abs(arr[right] - median);
-            
+
             if (diff_r > diff_l)
                 result[idx++] = arr[right--];
             else if (diff_l > diff_r)
                 result[idx++] = arr[left++];
             else if (arr[right] > arr[left])
                 result[idx++] = arr[right--];
-            else 
+            else
                 result[idx++] = arr[left++];
             if (idx == k)
                 break;
