7e1b17bf12a0d616e42f069afea3ae7e9c4f63dd
AnasImloul <pydev27@gmail.com>
2022-09-19 12:07:27 +0100

    Runtime: 88 ms (Top 54.97%) | Memory: 42.1 MB (Top 16.56%)

--- scripts/algorithms/S/Sum of Digits in Base K/Sum of Digits in Base K.js
+++ scripts/algorithms/S/Sum of Digits in Base K/Sum of Digits in Base K.js
@@ -1,8 +1,9 @@
+// Runtime: 88 ms (Top 54.97%) | Memory: 42.1 MB (Top 16.56%)
 /**
  * @param {number} n
  * @param {number} k
  * @return {number}
  */
 var sumBase = function(n, k) {
-    return n.toString(k).split("").reduce((acc, cur) => +acc + +cur) 
+    return n.toString(k).split("").reduce((acc, cur) => +acc + +cur)
 };
