76b5d13c0a45068e81b1c09e96b8bb3623d283f3
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:12 +0100

    Runtime: 249 ms (Top 29.29%) | Memory: 59.2 MB (Top 22.22%)

--- scripts/algorithms/F/Furthest Building You Can Reach/Furthest Building You Can Reach.js
+++ scripts/algorithms/F/Furthest Building You Can Reach/Furthest Building You Can Reach.js
@@ -1,18 +1,19 @@
+// Runtime: 249 ms (Top 29.29%) | Memory: 59.2 MB (Top 22.22%)
 var furthestBuilding = function(heights, bricks, ladders) {
     const heap = new MaxPriorityQueue({priority: x => x});
-    
+
     let i;
-    
+
     for(i = 0; i < heights.length-1; i++) {
         if(heights[i] >= heights[i+1]) continue;
-        
+
         const diff = heights[i+1] - heights[i];
-        
+
         // if not enough bricks are left
-        if(diff > bricks) { 
+        if(diff > bricks) {
             // if not enough ladders are left
             if(ladders === 0) break;
-            
+
             // if the max bricks used at once > current jump
             // replace the max bricks with a ladder and use those bricks for current jump
             // else use ladder for the current jump
