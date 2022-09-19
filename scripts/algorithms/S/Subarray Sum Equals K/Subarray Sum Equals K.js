58c977f51f2fb3c7ba55aee85e343447416f51f7
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:13 +0100

    Runtime: 203 ms (Top 18.14%) | Memory: 53.8 MB (Top 33.52%)

--- scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.js
+++ scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.js
@@ -1,16 +1,17 @@
-	var subarraySum = function(nums, k) {
-		const obj = {};
-		let res = 0;
-		let sum = 0;
+// Runtime: 203 ms (Top 18.14%) | Memory: 53.8 MB (Top 33.52%)
+    var subarraySum = function(nums, k) {
+        const obj = {};
+        let res = 0;
+        let sum = 0;
 
-		for (let i = 0; i < nums.length; i++) {
+        for (let i = 0; i < nums.length; i++) {
 
-			sum += nums[i];
-			if (sum == k) res++;
+            sum += nums[i];
+            if (sum == k) res++;
 
-			if (obj[sum - k]) res += obj[sum - k];
+            if (obj[sum - k]) res += obj[sum - k];
 
-			obj[sum] ? obj[sum] += 1 : obj[sum] = 1;
-		}
-		return res;
-	};
+            obj[sum] ? obj[sum] += 1 : obj[sum] = 1;
+        }
+        return res;
+    };
