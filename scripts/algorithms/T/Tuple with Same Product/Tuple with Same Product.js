264d462c6030745349690d5b19bf39508b75f494
AnasImloul <pydev27@gmail.com>
2022-09-19 12:07:28 +0100

    Runtime: 713 ms (Top 56.41%) | Memory: 88 MB (Top 92.31%)

--- scripts/algorithms/T/Tuple with Same Product/Tuple with Same Product.js
+++ scripts/algorithms/T/Tuple with Same Product/Tuple with Same Product.js
@@ -1,8 +1,9 @@
+// Runtime: 713 ms (Top 56.41%) | Memory: 88 MB (Top 92.31%)
 var tupleSameProduct = function(nums) {
     let tupleCount = 0;
     let products = {}; // we'll keep track of how many times we've seen a given product before
     for (let a = 0; a < nums.length; a++) {
-        for (let b = a + 1; b < nums.length; b++) {        
+        for (let b = a + 1; b < nums.length; b++) {
             let product = nums[a] * nums[b];
             if (products[product]) { // we've seen at least one other pair of numbers with the same product already
                 tupleCount += 8 * products[product]; // multiply by 8 because for any 4 numbers there are 8 permutations
