fcb3c80c6d1ab639f5491dc7c0daa6e377bf7047
AnasImloul <pydev27@gmail.com>
2022-09-19 10:32:41 +0100

    Runtime: 684 ms (Top 14.03%) | Memory: 119 MB (Top 52.52%)

--- scripts/algorithms/F/Find the Longest Valid Obstacle Course at Each Position/Find the Longest Valid Obstacle Course at Each Position.cpp
+++ scripts/algorithms/F/Find the Longest Valid Obstacle Course at Each Position/Find the Longest Valid Obstacle Course at Each Position.cpp
@@ -1,3 +1,4 @@
+// Runtime: 684 ms (Top 14.03%) | Memory: 119 MB (Top 52.52%)
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
@@ -5,7 +6,7 @@ public:
         int result=1;
         vector<int> b(obstacles.size()+1, INT_MAX);
         b[0]=INT_MIN;
-       
+
         for(int i=0; i<obstacles.size(); i++){
             auto k = upper_bound(b.begin(), b.end(), obstacles[i]) - b.begin();
             b[k] = obstacles[i];
