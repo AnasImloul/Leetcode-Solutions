cea5bfbde503e43abaa48b6977e0815cce91b171
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:04 +0100

    Runtime: 116 ms (Top 23.96%) | Memory: 44.6 MB (Top 21.88%)

--- scripts/algorithms/C/Calculate Amount Paid in Taxes/Calculate Amount Paid in Taxes.js
+++ scripts/algorithms/C/Calculate Amount Paid in Taxes/Calculate Amount Paid in Taxes.js
@@ -1,11 +1,12 @@
+// Runtime: 116 ms (Top 23.96%) | Memory: 44.6 MB (Top 21.88%)
 var calculateTax = function(brackets, income) {
     return brackets.reduce(([tax, prev], [upper, percent]) => {
         let curr = Math.min(income, upper - prev);
         tax += curr * (percent / 100);
-		
+
         income -= curr;
         if (income <= 0) brackets.length = 0;
-		
+
         return [tax, upper];
-    }, [0, 0])[0];    
+    }, [0, 0])[0];
 };
