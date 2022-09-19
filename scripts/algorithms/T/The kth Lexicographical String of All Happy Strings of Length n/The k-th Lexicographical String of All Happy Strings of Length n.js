44d19e8c87fdd213c524d2fa939fc42ef98bf4ea
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:08 +0100

    Runtime: 183 ms (Top 11.54%) | Memory: 49.7 MB (Top 7.69%)

---
+++ scripts/algorithms/T/The kth Lexicographical String of All Happy Strings of Length n/The k-th Lexicographical String of All Happy Strings of Length n.js
@@ -0,0 +1,19 @@
+// Runtime: 183 ms (Top 11.54%) | Memory: 49.7 MB (Top 7.69%)
+var getHappyString = function(n, k) {
+    const arr = ['a','b','c'], finalArr = ['a','b','c'];
+    let chr = '', str = '';
+    if(finalArr[finalArr.length-1].length === n && finalArr[0].length === n ) {
+        return finalArr[k-1] && finalArr[k-1].length === n ? finalArr[k-1] : '';
+    }
+    for(; finalArr.length < k || finalArr[0].length <= n;) {
+        str = finalArr.shift();
+        for(let index2 = 0; index2 < 3; index2++){
+            chr = str[str.length-1];
+            if(chr !== arr[index2]) {
+                finalArr.push(str+arr[index2]);
+            }
+        }
+        if(finalArr[finalArr.length-1].length === n && finalArr[0].length === n ) break;
+    }
+    return finalArr[k-1] ? finalArr[k-1] : '';
+};
