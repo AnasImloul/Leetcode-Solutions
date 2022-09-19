354c7effef41afb78d056c585b4a37e097aa1b58
AnasImloul <pydev27@gmail.com>
2022-09-19 10:49:07 +0100

    Runtime: 161 ms (Top 18.27%) | Memory: 63.5 MB (Top 16.98%)

--- scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.java
+++ scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.java
@@ -1,5 +1,6 @@
+// Runtime: 161 ms (Top 18.27%) | Memory: 63.5 MB (Top 16.98%)
 class Solution {
-     public  int calculateSetBits(String s){
+     public int calculateSetBits(String s){
         int count=0;
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i)=='1') count++;
@@ -7,16 +8,16 @@ class Solution {
         return count;
     }
 
-    public  boolean isPrime(int n){
+    public boolean isPrime(int n){
         if (n==0 || n==1) return false;
         for (int i = 2; i <= n/2; i++) {
             if(n%i ==0 ) return false;
         }
-//        System.out.println(n+" - ");
+// System.out.println(n+" - ");
         return true;
     }
 
-    public  int countPrimeSetBits(int left, int right) {
+    public int countPrimeSetBits(int left, int right) {
         int count=0;
         for(int i=left;i<=right;i++){
             String b= Integer.toBinaryString(i);
