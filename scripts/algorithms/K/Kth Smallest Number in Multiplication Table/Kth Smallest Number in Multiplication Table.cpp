9863a463f0f929e87fc75dd93a350443078c5c31
AnasImloul <pydev27@gmail.com>
2022-09-19 10:32:36 +0100

    Runtime: 27 ms (Top 26.50%) | Memory: 6 MB (Top 28.37%)

--- scripts/algorithms/K/Kth Smallest Number in Multiplication Table/Kth Smallest Number in Multiplication Table.cpp
+++ scripts/algorithms/K/Kth Smallest Number in Multiplication Table/Kth Smallest Number in Multiplication Table.cpp
@@ -1,21 +1,22 @@
+// Runtime: 27 ms (Top 26.50%) | Memory: 6 MB (Top 28.37%)
 class Solution {
 public:
-   
+
     int findKthNumber(int m, int n, int k) {
         int high=m*n ,low=1;
-        
+
         int mid=0, ans=1e9;
         while(low<=high)
         {
             mid=low+(high-low)/2;
             int temp=0;
-            
-			// for each i find the max value ,less than or equal to n , such that 
-			// i*j<=mid 
-			// add j to answer
+
+            // for each i find the max value ,less than or equal to n , such that
+            // i*j<=mid
+            // add j to answer
            for(int i=1;i<=m;i++)
             temp+=min(mid/i,n);
-          
+
             if(temp>=k)
             {
                 ans=min(ans,mid);
@@ -23,7 +24,7 @@ public:
             }
             else
                 low=mid+1;
-				
+
         }
         return ans;
     }
