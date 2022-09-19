ebe29994ea8d11ad20f26a94e7fd32fffdfb3911
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:10 +0100

    Runtime: 111 ms (Top 16.67%) | Memory: 42.1 MB (Top 37.04%)

--- scripts/algorithms/V/Validate IP Address/Validate IP Address.js
+++ scripts/algorithms/V/Validate IP Address/Validate IP Address.js
@@ -1,29 +1,30 @@
+// Runtime: 111 ms (Top 16.67%) | Memory: 42.1 MB (Top 37.04%)
 var validIPAddress = function(queryIP) {
-	const iPv4 = () => {
-		const address = queryIP.split('.');
-		if (address.length !== 4) return null;
+    const iPv4 = () => {
+        const address = queryIP.split('.');
+        if (address.length !== 4) return null;
 
-		for (const str of address) {
-			const ip = parseInt(str);
-			if (ip < 0 || ip > 255) return null;
-			if (ip.toString() !== str) return null;
-		}
-		return 'IPv4';
-	};
+        for (const str of address) {
+            const ip = parseInt(str);
+            if (ip < 0 || ip > 255) return null;
+            if (ip.toString() !== str) return null;
+        }
+        return 'IPv4';
+    };
 
-	const iPv6 = () => {
-		const address = queryIP.split(':');
-		if (address.length !== 8) return null;
-		const config = '0123456789abcdefABCDEF';
-		const check = address.every(str => {
-			if (str === '' || str.length > 4) return false;
-			for (const s of str) {
-				if (!config.includes(s)) return false;
-			}
-			return true;
-		});
-		return check ? 'IPv6' : null; 
-	};
+    const iPv6 = () => {
+        const address = queryIP.split(':');
+        if (address.length !== 8) return null;
+        const config = '0123456789abcdefABCDEF';
+        const check = address.every(str => {
+            if (str === '' || str.length > 4) return false;
+            for (const s of str) {
+                if (!config.includes(s)) return false;
+            }
+            return true;
+        });
+        return check ? 'IPv6' : null;
+    };
 
-	return iPv4() ?? iPv6() ?? 'Neither';
+    return iPv4() ?? iPv6() ?? 'Neither';
 };
