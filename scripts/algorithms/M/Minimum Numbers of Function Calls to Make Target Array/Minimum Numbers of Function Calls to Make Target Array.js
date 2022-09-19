20244c7ad63996f2ce654260735c795198cb3dc7
AnasImloul <pydev27@gmail.com>
2022-09-19 12:07:22 +0100

    Runtime: 156 ms (Top 53.33%) | Memory: 45.2 MB (Top 100.00%)

--- scripts/algorithms/M/Minimum Numbers of Function Calls to Make Target Array/Minimum Numbers of Function Calls to Make Target Array.js
+++ scripts/algorithms/M/Minimum Numbers of Function Calls to Make Target Array/Minimum Numbers of Function Calls to Make Target Array.js
@@ -1,3 +1,4 @@
+// Runtime: 156 ms (Top 53.33%) | Memory: 45.2 MB (Top 100.00%)
 var minOperations = function(nums) {
     let maxpow = 0, ans = 0, pow, val
     for (let i = 0; i < nums.length; i++) {
