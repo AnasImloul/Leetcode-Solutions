493892b012d02c1de2839e439c4f3b84ce57b144
AnasImloul <pydev27@gmail.com>
2022-09-19 10:32:45 +0100

    Runtime: 128 ms (Top 82.18%) | Memory: 9.5 MB (Top 29.42%)

--- scripts/algorithms/C/Count Number of Teams/Count Number of Teams.cpp
+++ scripts/algorithms/C/Count Number of Teams/Count Number of Teams.cpp
@@ -1,7 +1,8 @@
+// Runtime: 128 ms (Top 82.18%) | Memory: 9.5 MB (Top 29.42%)
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        
+
         int i, j, n = rating.size(), ans = 0;
         vector<int> grt(n, 0), les(n, 0);
         for(i=0;i<n;i++)
@@ -14,7 +15,7 @@ public:
                     les[i] += 1;
             }
         }
-        
+
         for(i=0;i<n;i++)
         {
             for(j=i+1;j<n;j++)
