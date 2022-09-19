7d272816db85694d44da9409bb35e44dcb7eaa46
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:09 +0100

    Runtime: 120 ms (Top 50.89%) | Memory: 44.6 MB (Top 76.49%)

--- scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.js
+++ scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.js
@@ -1,5 +1,6 @@
+// Runtime: 120 ms (Top 50.89%) | Memory: 44.6 MB (Top 76.49%)
 var repeatedSubstringPattern = function(s) {
-    let repeatStr = s.repeat(2)  //first duplicate the string with repeat function
-    let sliceStr = repeatStr.slice(1,-1)  // slice string first and last string word
-    return sliceStr.includes(s)  // now check if the main string(s) is included by sliced string
+    let repeatStr = s.repeat(2) //first duplicate the string with repeat function
+    let sliceStr = repeatStr.slice(1,-1) // slice string first and last string word
+    return sliceStr.includes(s) // now check if the main string(s) is included by sliced string
 }
