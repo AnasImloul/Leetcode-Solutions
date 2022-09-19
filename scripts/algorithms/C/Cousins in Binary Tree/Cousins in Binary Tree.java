bdfebfb101f0259169a41fc5acefd45441e4c569
AnasImloul <pydev27@gmail.com>
2022-09-19 10:49:11 +0100

    Runtime: 2 ms (Top 20.27%) | Memory: 42 MB (Top 40.06%)

--- scripts/algorithms/C/Cousins in Binary Tree/Cousins in Binary Tree.java
+++ scripts/algorithms/C/Cousins in Binary Tree/Cousins in Binary Tree.java
@@ -1,15 +1,16 @@
+// Runtime: 2 ms (Top 20.27%) | Memory: 42 MB (Top 40.06%)
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
         Set<TreeNode> parentSet = new HashSet<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        
+
         while (!q.isEmpty()) {
             int len = q.size();
-            
+
             for (int i = 0; i < len; i++) {
                 TreeNode parent = q.remove();
-                
+
                 for (TreeNode child : new TreeNode[]{parent.left, parent.right}) {
                     if(child != null) {
                         q.add(child);
@@ -19,7 +20,7 @@ class Solution {
                 }
             }
             if (parentSet.size() > 0)
-                return parentSet.size() == 2;             //if same parent -> set size wil be 1
+                return parentSet.size() == 2; //if same parent -> set size wil be 1
         }
         return false;
     }
