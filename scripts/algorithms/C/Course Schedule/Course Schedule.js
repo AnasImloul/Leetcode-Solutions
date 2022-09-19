930f30f8f638f9828f5e5fae11a80e0d4b241b96
AnasImloul <pydev27@gmail.com>
2022-09-19 10:40:09 +0100

    Runtime: 135 ms (Top 41.22%) | Memory: 46.8 MB (Top 77.04%)

--- scripts/algorithms/C/Course Schedule/Course Schedule.js
+++ scripts/algorithms/C/Course Schedule/Course Schedule.js
@@ -1,3 +1,4 @@
+// Runtime: 135 ms (Top 41.22%) | Memory: 46.8 MB (Top 77.04%)
 var canFinish = function(numCourses, prerequisites) {
     const adjList = []
     const visit = []
@@ -6,22 +7,22 @@ var canFinish = function(numCourses, prerequisites) {
         if (!dfs(i)) return false
     }
     return true
-    
+
     function dfs(i) {
         // base case
         if (visit[i]) return false
         if (visit[i] === false) return true
-        
+
         visit[i] = true
-        
+
         for (const nei of adjList[i] ?? []) {
             if (!dfs(nei)) return false
         }
-        
+
         visit[i] = false
         return true
     }
-    
+
     function construAdj() {
         for (const pre of prerequisites) {
             if (!adjList[pre[0]]) adjList[pre[0]] = []
