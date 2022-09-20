9ed72a5f6132f2e6c02110b3f5f72d47727179ce
AnasImloul <pydev27@gmail.com>
2022-09-20 13:32:07 +0100

    Runtime: 5941 ms (Top 24.26%) | Memory: 59.9 MB (Top 86.39%)

--- scripts/algorithms/M/Maximum Fruits Harvested After at Most K Steps/Maximum Fruits Harvested After at Most K Steps.py
+++ scripts/algorithms/M/Maximum Fruits Harvested After at Most K Steps/Maximum Fruits Harvested After at Most K Steps.py
@@ -1,3 +1,4 @@
+# Runtime: 5941 ms (Top 24.26%) | Memory: 59.9 MB (Top 86.39%)
 class Solution:
     def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
         arr = [0 for _ in range(2*k+1)]
@@ -5,15 +6,15 @@ class Solution:
             if pos < startPos-k or pos > startPos+k:
                 continue
             arr[pos-(startPos-k)] += numOfFruit
-        
+
         left, right = sum(arr[:k+1]), sum(arr[k:])
         maxSeen = max(left, right)
-        
-        turn = 1                            # turning point
+
+        turn = 1 # turning point
         for i in range(2, k+1, 2):
             left = left-arr[i-2]-arr[i-1]+arr[k+turn]
             right = right-arr[~(i-2)]-arr[~(i-1)]+arr[k-turn]
             maxSeen = max(maxSeen, left, right)
             turn += 1
-        
+
         return maxSeen
