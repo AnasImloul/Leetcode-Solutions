9237a7639923b8c40cd1a6b4b5a428dfbe2c6125
AnasImloul <pydev27@gmail.com>
2022-09-20 02:56:35 +0100

    Runtime: 76 ms (Top 58.82%) | Memory: 42 MB (Top 61.76%)

--- scripts/algorithms/D/Decoded String at Index/Decoded String at Index.js
+++ scripts/algorithms/D/Decoded String at Index/Decoded String at Index.js
@@ -1,7 +1,8 @@
+// Runtime: 76 ms (Top 58.82%) | Memory: 42 MB (Top 61.76%)
 var decodeAtIndex = function(s, k) {
     let len=0;
     let isDigit=false
-    
+
     for(let v of s){
         if(v>='0'&&v<='9'){
             len*=+v
@@ -13,7 +14,7 @@ var decodeAtIndex = function(s, k) {
             }
         }
     }
-    
+
     for(let i=s.length-1;i>=0;i--){
         const v=s[i]
         if(v>='0'&&v<='9'){
