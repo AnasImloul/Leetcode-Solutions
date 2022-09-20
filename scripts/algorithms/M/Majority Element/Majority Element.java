c040614d3aaeb214bc33dc6a14675850303dec28
AnasImloul <pydev27@gmail.com>
2022-09-20 13:54:57 +0100

    Runtime: 14 ms (Top 32.93%) | Memory: 56.8 MB (Top 19.67%)

--- scripts/algorithms/M/Majority Element/Majority Element.java
+++ scripts/algorithms/M/Majority Element/Majority Element.java
@@ -1,13 +1,14 @@
+// Runtime: 14 ms (Top 32.93%) | Memory: 56.8 MB (Top 19.67%)
 class Solution {
     public int majorityElement(int[] nums) {
         // Base case...
         if (nums.length == 1) {
-		    return nums[0];
-	    }
+            return nums[0];
+        }
         // Sort nums array...
         Arrays.sort(nums);
         // Since the majority element appears more than n / 2 times...
         // The n/2 -th element in the sorted nums must be the majority element...
-	    return nums[nums.length / 2];
+        return nums[nums.length / 2];
     }
 }
