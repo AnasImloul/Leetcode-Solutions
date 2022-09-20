7658a640518ead00456a20fd95acfeb3ff369de9
AnasImloul <pydev27@gmail.com>
2022-09-20 13:32:05 +0100

    Runtime: 93 ms (Top 33.43%) | Memory: 14.1 MB (Top 44.38%)

--- scripts/algorithms/W/Watering Plants/Watering Plants.py
+++ scripts/algorithms/W/Watering Plants/Watering Plants.py
@@ -1,15 +1,16 @@
+# Runtime: 93 ms (Top 33.43%) | Memory: 14.1 MB (Top 44.38%)
 class Solution:
-	def wateringPlants(self, plants: List[int], capacity: int) -> int:
-		result = 0
-		curCap = capacity
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        result = 0
+        curCap = capacity
 
-		for i in range(len(plants)):
-			if curCap >= plants[i]:
-				curCap -= plants[i]
-				result += 1
+        for i in range(len(plants)):
+            if curCap >= plants[i]:
+                curCap -= plants[i]
+                result += 1
 
-			else:
-				result += i * 2 + 1
-				curCap = capacity - plants[i]
+            else:
+                result += i * 2 + 1
+                curCap = capacity - plants[i]
 
-		return result
+        return result
