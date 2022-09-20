98f9a4f03f9b4affcc1bde41a03afed4a8619458
AnasImloul <pydev27@gmail.com>
2022-09-20 02:56:40 +0100

    Runtime: 272 ms (Top 14.02%) | Memory: 62.9 MB (Top 24.30%)

--- scripts/algorithms/A/All Possible Full Binary Trees/All Possible Full Binary Trees.js
+++ scripts/algorithms/A/All Possible Full Binary Trees/All Possible Full Binary Trees.js
@@ -1,8 +1,9 @@
+// Runtime: 272 ms (Top 14.02%) | Memory: 62.9 MB (Top 24.30%)
 var allPossibleFBT = function(n) {
     if(n % 2 == 0) return [];
     if( n == 1 ) return [new TreeNode(0)];
     const ans = [];
-    
+
     const generate = (x) => {
         if(x == 1) return [new TreeNode(0)];
         const ans = [];
@@ -20,7 +21,7 @@ var allPossibleFBT = function(n) {
         }
         return ans;
     }
-    
+
     for(let l = 1; l <= n - 2; l += 2) {
         const left = generate(l);
         const right = generate(n - 1 - l);
@@ -33,6 +34,6 @@ var allPossibleFBT = function(n) {
             });
         });
     }
-    
+
     return ans;
 };
