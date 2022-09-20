9f04ecdfa440b7dadfa9f5836d65294b0a21fdc2
AnasImloul <pydev27@gmail.com>
2022-09-20 02:56:43 +0100

    Runtime: 161 ms (Top 59.39%) | Memory: 48.4 MB (Top 72.05%)

--- scripts/algorithms/F/Find the Difference of Two Arrays/Find the Difference of Two Arrays.js
+++ scripts/algorithms/F/Find the Difference of Two Arrays/Find the Difference of Two Arrays.js
@@ -1,9 +1,10 @@
+// Runtime: 161 ms (Top 59.39%) | Memory: 48.4 MB (Top 72.05%)
 var findDifference = function(nums1, nums2) {
     const s1 = new Set(nums1);
     const s2 = new Set(nums2);
-    
+
     const a1 = [...s1].filter(x => !s2.has(x));
     const a2 = [...s2].filter(x => !s1.has(x));
-    
+
     return [a1, a2];
 };
