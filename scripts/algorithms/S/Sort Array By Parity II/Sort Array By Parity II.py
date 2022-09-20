2a64aa4c3e308a937508ae89af89a302d429f29d
AnasImloul <pydev27@gmail.com>
2022-09-20 13:32:06 +0100

    Runtime: 608 ms (Top 6.36%) | Memory: 16.7 MB (Top 25.83%)

--- scripts/algorithms/S/Sort Array By Parity II/Sort Array By Parity II.py
+++ scripts/algorithms/S/Sort Array By Parity II/Sort Array By Parity II.py
@@ -1,7 +1,8 @@
+# Runtime: 608 ms (Top 6.36%) | Memory: 16.7 MB (Top 25.83%)
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
         arr = [None]*len(nums)
-        even,odd = 0,1    
+        even,odd = 0,1
         for i in(nums):
             if i % 2 == 0:
                 arr[even] = i
