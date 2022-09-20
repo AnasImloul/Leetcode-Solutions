52ed7b07f6e542d8d0072514527e3a266f66bfae
AnasImloul <pydev27@gmail.com>
2022-09-20 13:32:10 +0100

    Runtime: 81 ms (Top 68.03%) | Memory: 13.9 MB (Top 63.45%)

--- scripts/algorithms/S/Shuffle String/Shuffle String.py
+++ scripts/algorithms/S/Shuffle String/Shuffle String.py
@@ -1,3 +1,4 @@
+# Runtime: 81 ms (Top 68.03%) | Memory: 13.9 MB (Top 63.45%)
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         dec = {}
@@ -6,8 +7,8 @@ class Solution:
         for i in indices:
             dec[i] = s[c]
             c += 1
-		# dec = {"4":"c","5":"o","6":"d","7":"e","0":"l","2":"e","1":"e","3":"t"}
+        # dec = {"4":"c","5":"o","6":"d","7":"e","0":"l","2":"e","1":"e","3":"t"}
         for x in range(len(indices)):
             res += dec[x]
-			# x in range 0, 1, 2,....... len *indices or s* 
+            # x in range 0, 1, 2,....... len *indices or s*
         return res
