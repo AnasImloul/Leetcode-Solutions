24ae00dacd48c888a0ccb289f37b34e14ebfe197
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:55 +0100

    Runtime: 131 ms (Top 48.09%) | Memory: 46.3 MB (Top 65.46%)

--- scripts/algorithms/J/Jump Game/Jump Game.js
+++ scripts/algorithms/J/Jump Game/Jump Game.js
@@ -1,17 +1,18 @@
+// Runtime: 131 ms (Top 48.09%) | Memory: 46.3 MB (Top 65.46%)
 
 var canJump = function(nums) {
     let target = nums.length-1;
     let max = 0,index = 0;
-    
+
     while(index <= target){
         max = Math.max(max,index + nums[index]);
-        
+
         if(max >= target) return true;
-        
+
         if(index >= max && nums[index] === 0) return false;
-        
+
         index++;
     }
-    
+
     return false;
 };
