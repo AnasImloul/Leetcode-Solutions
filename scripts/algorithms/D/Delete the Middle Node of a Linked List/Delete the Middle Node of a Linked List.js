4c0c9d8ea25b76c4dc186bfae05d581cf61e1b6e
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:53 +0100

    Runtime: 1265 ms (Top 18.42%) | Memory: 126.5 MB (Top 69.59%)

--- scripts/algorithms/D/Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.js
+++ scripts/algorithms/D/Delete the Middle Node of a Linked List/Delete the Middle Node of a Linked List.js
@@ -1,8 +1,9 @@
+// Runtime: 1265 ms (Top 18.42%) | Memory: 126.5 MB (Top 69.59%)
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * this.val = (val===undefined ? 0 : val)
+ * this.next = (next===undefined ? null : next)
  * }
  */
 /**
@@ -12,21 +13,21 @@
 // Two Pointer: Fast & Slow | O(N) | O(1)
 var deleteMiddle = function(head) {
   if (!head.next) return null;
-  
+
   let prev = head;
-  let slow = head;  
+  let slow = head;
   let fast = head.next;
   let size = 2;
-  
+
   while (fast && fast.next) {
     prev = slow;
     slow = slow.next;
     fast = fast.next.next;
     size += fast ? 2 : 1;
   }
-  
+
   if (size % 2 === 0) slow.next = slow.next.next;
   else prev.next = slow.next;
-  
+
   return head;
 };
