5addeb4e3ddb45a9d2c3b443289ddd54b7e32fed
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:58 +0100

    Runtime: 89 ms (Top 56.57%) | Memory: 41.7 MB (Top 89.01%)

--- scripts/algorithms/M/Maximum 69 Number/Maximum 69 Number.js
+++ scripts/algorithms/M/Maximum 69 Number/Maximum 69 Number.js
@@ -1,4 +1,5 @@
-var maximum69Number  = function(num) {
+// Runtime: 89 ms (Top 56.57%) | Memory: 41.7 MB (Top 89.01%)
+var maximum69Number = function(num) {
   let flag=true
    num= num.toString().split('').map((x)=>{
         if(x!=='9'&&flag){
