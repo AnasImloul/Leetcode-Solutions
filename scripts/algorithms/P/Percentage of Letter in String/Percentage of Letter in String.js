2675d9650310cc20d9584d46d214a9852123a8be
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:57 +0100

    Runtime: 99 ms (Top 27.52%) | Memory: 42.1 MB (Top 51.50%)

--- scripts/algorithms/P/Percentage of Letter in String/Percentage of Letter in String.js
+++ scripts/algorithms/P/Percentage of Letter in String/Percentage of Letter in String.js
@@ -1,3 +1,4 @@
+// Runtime: 99 ms (Top 27.52%) | Memory: 42.1 MB (Top 51.50%)
 var percentageLetter = function(s, letter) {
     let count = 0;
     for (let i of s) { // count how many letters are in s
