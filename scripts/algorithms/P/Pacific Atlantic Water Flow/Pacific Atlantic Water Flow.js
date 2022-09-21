8db01057827ba744fa750de9534b78127932cfc4
AnasImloul <pydev27@gmail.com>
2022-09-21 16:56:00 +0100

    Runtime: 243 ms (Top 35.79%) | Memory: 50.5 MB (Top 78.35%)

--- scripts/algorithms/P/Pacific Atlantic Water Flow/Pacific Atlantic Water Flow.js
+++ scripts/algorithms/P/Pacific Atlantic Water Flow/Pacific Atlantic Water Flow.js
@@ -1,3 +1,4 @@
+// Runtime: 243 ms (Top 35.79%) | Memory: 50.5 MB (Top 78.35%)
 `/**
  * @param {number[][]} heights
  * @return {number[][]}
@@ -7,17 +8,17 @@ var pacificAtlantic = function(heights) {
     let pacific = new Set();
     let rows = heights.length;
     let cols = heights[0].length;
-    
+
     for (let c = 0; c < cols; c++) {
         explore(heights, 0, c, pacific, heights[0][c]); // dfs from top row
         explore(heights, rows - 1, c, atlantic, heights[rows - 1][c]); // dfs from bottom row
     }
-    
+
     for (let r = 0; r < rows; r++) {
         explore(heights, r, 0, pacific, heights[r][0]); // dfs from left most column
         explore(heights, r, cols - 1, atlantic, heights[r][cols - 1]); // dfs from right most column
     }
-    
+
     // check if water can flow to both atlantic and pacific ocean.
     let res = [];
     for (let r = 0; r < rows; r++) {
@@ -28,7 +29,7 @@ var pacificAtlantic = function(heights) {
             }
         }
     }
-    
+
     return res;
 };
 
@@ -36,16 +37,15 @@ function explore(heights, r, c, visited, prevHeight) {
     let rowInbound = 0 <= r && r < heights.length;
     let colInbound = 0 <= c && c < heights[0].length;
     if (!rowInbound || !colInbound) return;
-    
-    // height must be higher than prev height. water can only flow downwards not upwards. duh. 
+
+    // height must be higher than prev height. water can only flow downwards not upwards. duh.
     // if it's the first value then it's just the same value so it's not less than so it will not return.
     if (heights[r][c] < prevHeight) return;
-    
+
     let pos = r + ',' + c;
     if (visited.has(pos)) return;
     visited.add(pos)
-    
-    
+
     explore(heights, r + 1, c, visited, heights[r][c]) // below
     explore(heights, r - 1, c, visited, heights[r][c]) // above
     explore(heights, r, c + 1, visited, heights[r][c]) // right
