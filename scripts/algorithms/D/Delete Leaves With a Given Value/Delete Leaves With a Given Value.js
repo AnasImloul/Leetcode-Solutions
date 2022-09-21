18824661df9c02cebbb4f916bb341d24e5477d46
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:59 +0100

    Runtime: 148 ms (Top 30.20%) | Memory: 47.4 MB (Top 67.11%)

--- scripts/algorithms/D/Delete Leaves With a Given Value/Delete Leaves With a Given Value.js
+++ scripts/algorithms/D/Delete Leaves With a Given Value/Delete Leaves With a Given Value.js
@@ -1,6 +1,7 @@
+// Runtime: 148 ms (Top 30.20%) | Memory: 47.4 MB (Top 67.11%)
 var removeLeafNodes = function(root, target) {
     const parent = new TreeNode(-1, root, null);
-    
+
     const traverse = (r = root, p = parent, child = -1) => {
         if(!r) return null;
         traverse(r.left, r, -1);
