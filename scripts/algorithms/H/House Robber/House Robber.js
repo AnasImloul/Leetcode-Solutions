bfde1504218c6452061a0db61544cc8fc780a921
AnasImloul <pydev27@gmail.com>
2022-09-21 16:55:56 +0100

    Runtime: 81 ms (Top 64.99%) | Memory: 42 MB (Top 44.71%)

--- scripts/algorithms/H/House Robber/House Robber.js
+++ scripts/algorithms/H/House Robber/House Robber.js
@@ -1,10 +1,11 @@
+// Runtime: 81 ms (Top 64.99%) | Memory: 42 MB (Top 44.71%)
 const max=(x,y)=>x>y?x:y
 var rob = function(nums) {
      if(nums.length==1) return(nums[0]);
     let temp=[]
     temp[0]=nums[0];
         temp[1]=max(nums[0],nums[1]);
-        
+
         for(let i =2;i<nums.length;i++){
             temp[i] = max(temp[i-1],nums[i]+temp[i-2]);
         }
