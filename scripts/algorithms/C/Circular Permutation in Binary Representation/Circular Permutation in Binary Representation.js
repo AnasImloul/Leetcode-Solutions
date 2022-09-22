339b10dec8d473ed0ba38c90106eb47a6f366dfc
AnasImloul <pydev27@gmail.com>
2022-09-22 12:32:51 +0100

    Runtime: 152 ms (Top 85.71%) | Memory: 56.7 MB (Top 28.57%)

--- scripts/algorithms/C/Circular Permutation in Binary Representation/Circular Permutation in Binary Representation.js
+++ scripts/algorithms/C/Circular Permutation in Binary Representation/Circular Permutation in Binary Representation.js
@@ -1,23 +1,24 @@
+// Runtime: 152 ms (Top 85.71%) | Memory: 56.7 MB (Top 28.57%)
 var circularPermutation = function(n, start) {
     const grayCodes = [];
-    
+
     let startIdx = -1;
-    
+
     for (let i = 0; i <= 2**n - 1; i++) {
         grayCodes[i] = i ^ (i >> 1);
-        
+
         if (grayCodes[i] == start) startIdx = i;
     }
-    
+
     const res = [];
-    
+
     for (let i = 0; i <= 2**n - 1; i++) {
         res[i] = grayCodes[startIdx];
-        
+
         startIdx++;
-        
+
         if (startIdx == grayCodes.length) startIdx = 0;
     }
-   
+
     return res;
 };
