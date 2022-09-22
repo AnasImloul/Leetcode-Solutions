0b09d13706b00eb466d10635dcc35725f928921f
AnasImloul <pydev27@gmail.com>
2022-09-22 12:00:29 +0100

    Runtime: 10 ms (Top 59.80%) | Memory: 10.6 MB (Top 29.82%)

--- scripts/algorithms/S/Simplify Path/Simplify Path.cpp
+++ scripts/algorithms/S/Simplify Path/Simplify Path.cpp
@@ -1,47 +1,48 @@
-					// 😉😉😉😉Please upvote if it helps 😉😉😉😉
+// Runtime: 10 ms (Top 59.80%) | Memory: 10.6 MB (Top 29.82%)
+                    // Please upvote if it helps
 class Solution {
 public:
     string simplifyPath(string path) {
-        
+
         stack<string> st;
         string res;
-        
-        for(int i = 0;  i<path.size(); ++i)
+
+        for(int i = 0; i<path.size(); ++i)
         {
-            if(path[i] == '/')    
+            if(path[i] == '/')
                 continue;
             string temp;
-			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
+            // iterate till we doesn't traverse the whole string and doesn't encounter the last /
             while(i < path.size() && path[i] != '/')
             {
-				// add path to temp string
+                // add path to temp string
                 temp += path[i];
                 ++i;
             }
             if(temp == ".")
                 continue;
-			// pop the top element from stack if exists
+            // pop the top element from stack if exists
             else if(temp == "..")
             {
                 if(!st.empty())
                     st.pop();
             }
             else
-			// push the directory file name to stack
+            // push the directory file name to stack
                 st.push(temp);
         }
-        
-		// adding all the stack elements to res
+
+        // adding all the stack elements to res
         while(!st.empty())
         {
             res = "/" + st.top() + res;
             st.pop();
         }
-        
-		// if no directory or file is present
+
+        // if no directory or file is present
         if(res.size() == 0)
             return "/";
-        
+
         return res;
     }
 };
