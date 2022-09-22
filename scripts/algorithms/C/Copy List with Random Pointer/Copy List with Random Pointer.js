77b3dc0fbd06e735f447fc038aea985101c48aa4
AnasImloul <pydev27@gmail.com>
2022-09-22 12:32:48 +0100

    Runtime: 68 ms (Top 90.10%) | Memory: 44 MB (Top 53.93%)

--- scripts/algorithms/C/Copy List with Random Pointer/Copy List with Random Pointer.js
+++ scripts/algorithms/C/Copy List with Random Pointer/Copy List with Random Pointer.js
@@ -1,7 +1,8 @@
+// Runtime: 68 ms (Top 90.10%) | Memory: 44 MB (Top 53.93%)
 var copyRandomList = function(head) {
     if(!head) return head
     let curr = head;
-    
+
     // manipulate the Linked List and store a copy node at every nod's next
     while(curr != null){
         let next = curr.next;
@@ -10,7 +11,7 @@ var copyRandomList = function(head) {
         curr = next
     }
     // console.log(head)
-    
+
     // Setting random point of copied node according to original node
     curr = head;
     while(curr != null && curr.next != null){
@@ -22,16 +23,16 @@ var copyRandomList = function(head) {
         curr = curr.next.next
     }
     // console.log(head.next.next.next)
-    
+
     // break the like made for linking copy node with original node
     curr = head;
     let ans = head.next;
-    
+
     while(curr != null){
         let temp = curr.next;
         if(temp){
         curr.next = temp.next;
-        curr = curr.next; 
+        curr = curr.next;
         }
         if(curr){
         temp.next = curr.next;
