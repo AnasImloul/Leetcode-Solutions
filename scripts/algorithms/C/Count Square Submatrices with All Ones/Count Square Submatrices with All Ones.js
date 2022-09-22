b7e3325259e08921cb1e9936188c2f941ece9781
AnasImloul <pydev27@gmail.com>
2022-09-22 12:32:49 +0100

    Runtime: 150 ms (Top 40.00%) | Memory: 45.7 MB (Top 65.81%)

--- scripts/algorithms/C/Count Square Submatrices with All Ones/Count Square Submatrices with All Ones.js
+++ scripts/algorithms/C/Count Square Submatrices with All Ones/Count Square Submatrices with All Ones.js
@@ -1,3 +1,4 @@
+// Runtime: 150 ms (Top 40.00%) | Memory: 45.7 MB (Top 65.81%)
 var countSquares = function(matrix) {
     let m = matrix.length;
     let n = matrix[0].length;
@@ -9,9 +10,9 @@ var countSquares = function(matrix) {
             for(let j = 0; j < n; j++){
                 preCount += matrix[i][j];
                 if(i + 1 < m && j + 1 < n){
-                    if(matrix[i][j] != 0 
-                       &&matrix[i][j] == matrix[i + 1][j] 
-                       && matrix[i][j] == matrix[i][j + 1] 
+                    if(matrix[i][j] != 0
+                       &&matrix[i][j] == matrix[i + 1][j]
+                       && matrix[i][j] == matrix[i][j + 1]
                        && matrix[i][j] == matrix[i + 1][j + 1]) matrix[i][j]++;
                 }
                 count += matrix[i][j];
