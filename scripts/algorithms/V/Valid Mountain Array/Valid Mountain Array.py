438035ce53f1595651f516e58c8b4e5c78103fbd
AnasImloul <pydev27@gmail.com>
2022-09-22 11:52:50 +0100

    Runtime: 391 ms (Top 35.19%) | Memory: 15.5 MB (Top 21.40%)

--- scripts/algorithms/V/Valid Mountain Array/Valid Mountain Array.py
+++ scripts/algorithms/V/Valid Mountain Array/Valid Mountain Array.py
@@ -1,3 +1,4 @@
+# Runtime: 391 ms (Top 35.19%) | Memory: 15.5 MB (Top 21.40%)
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         if len(arr) < 3:
@@ -7,7 +8,7 @@ class Solution:
                 if i==1:
                     return False
                 break
-            
+
         for j in range(i,len(arr)):
             if arr[j] >= arr[j-1]:
                 return False
