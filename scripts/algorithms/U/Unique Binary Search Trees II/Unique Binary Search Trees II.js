72558d07dec83a167f323130eddca4a045bfeee8
AnasImloul <pydev27@gmail.com>
2022-09-22 12:32:54 +0100

    Runtime: 81 ms (Top 98.19%) | Memory: 47.9 MB (Top 80.14%)

--- scripts/algorithms/U/Unique Binary Search Trees II/Unique Binary Search Trees II.js
+++ scripts/algorithms/U/Unique Binary Search Trees II/Unique Binary Search Trees II.js
@@ -1,31 +1,32 @@
+// Runtime: 81 ms (Top 98.19%) | Memory: 47.9 MB (Top 80.14%)
 var generateTrees = function(n) {
     if(n <= 0){
         return [];
     }
     return generateRec(1, n);
-    
+
 };
 
 function generateRec(start, end){
     let result = [];
-    
+
     if(start > end){
         result.push(null);
         return result;
     }
-    
+
     for(let i = start; i <end+1; i++){
         let left = generateRec(start, i-1);
         let right = generateRec(i+1, end);
-        
-        for(let l = 0;  l < left.length; l++){
+
+        for(let l = 0; l < left.length; l++){
             for(let r = 0; r < right.length; r++){
                 let root = new TreeNode(i,left[l], right[r]);
                 result.push(root);
             }
         }
-        
+
     }
     return result;
-   
+
 }
