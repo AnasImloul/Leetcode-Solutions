82dcb66b6452bd02dd2f6979934999462aa69b86
AnasImloul <pydev27@gmail.com>
2022-09-22 11:48:50 +0100

    Runtime: 22 ms (Top 16.67%) | Memory: 44.5 MB (Top 21.67%)

--- scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.java
+++ scripts/algorithms/N/Number of Different Integers in a String/Number of Different Integers in a String.java
@@ -1,13 +1,14 @@
+// Runtime: 22 ms (Top 16.67%) | Memory: 44.5 MB (Top 21.67%)
 class Solution {
     public int numDifferentIntegers(String word) {
         String[] arr = word.replaceAll("[a-zA-Z]", " ").split("\\s+");
         Set<String> set = new HashSet<String>();
-            
+
         for (String str : arr) {
             if (!str.isEmpty())
                 set.add(String.valueOf(str.replaceAll("^0*","")));
         }
-                
+
         return set.size();
     }
 }
