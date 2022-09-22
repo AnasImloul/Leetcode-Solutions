bc750b289f367c18d529c1ca90a22473b9ee1307
AnasImloul <pydev27@gmail.com>
2022-09-22 12:00:27 +0100

    Runtime: 6 ms (Top 19.66%) | Memory: 7 MB (Top 71.58%)

--- scripts/algorithms/M/Middle of the Linked List/Middle of the Linked List.cpp
+++ scripts/algorithms/M/Middle of the Linked List/Middle of the Linked List.cpp
@@ -1,11 +1,12 @@
+// Runtime: 6 ms (Top 19.66%) | Memory: 7 MB (Top 71.58%)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
