fdf8c1df5bf592d47651a7cf2f934009c7a89a9d
AnasImloul <pydev27@gmail.com>
2022-09-22 11:48:51 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 40 MB (Top 85.42%)

--- scripts/algorithms/F/Fibonacci Number/Fibonacci Number.java
+++ scripts/algorithms/F/Fibonacci Number/Fibonacci Number.java
@@ -1,16 +1,17 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 40 MB (Top 85.42%)
 class Solution {
     public int fib(int n) {
-        
+
         int[] fiboArray = new int[n+2];
-        
+
         fiboArray[0] = 0;
         fiboArray[1] = 1;
-        
+
         for(int i=2; i<=n; i++) {
             fiboArray[i] = fiboArray[i-1] + fiboArray[i-2];
         }
-        
+
         return fiboArray[n];
-        
+
     }
 }
