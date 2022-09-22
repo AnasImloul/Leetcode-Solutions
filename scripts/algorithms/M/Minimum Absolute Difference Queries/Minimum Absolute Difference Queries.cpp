9745d446127fe9c986bb159b9feffe65127cbf03
AnasImloul <pydev27@gmail.com>
2022-09-22 12:00:28 +0100

    Runtime: 390 ms (Top 96.30%) | Memory: 112.1 MB (Top 64.81%)

--- scripts/algorithms/M/Minimum Absolute Difference Queries/Minimum Absolute Difference Queries.cpp
+++ scripts/algorithms/M/Minimum Absolute Difference Queries/Minimum Absolute Difference Queries.cpp
@@ -1,22 +1,23 @@
+// Runtime: 390 ms (Top 96.30%) | Memory: 112.1 MB (Top 64.81%)
 class Solution {
 public:
     vector<bitset<101>> tree;
     vector<int> minDifference(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        
+
         tree = vector<bitset<101>>(4*(n+1) + 1);
         buildtree(1, 0, n-1, nums);
-        
+
         vector<int> ans;
         for(auto &e: queries){
             auto finalOnesRepresentations = query(1, 0, n-1, e[0], e[1]);
-            
+
             // find the first 1
             int i = 0;
             while(i < 101 and finalOnesRepresentations[i] != 1){
                 i++;
             }
-            
+
             int gap = INT_MAX;
             int prev = i;
             i++;
@@ -27,34 +28,34 @@ public:
                     prev = i;
                 }
             }
-            ans.push_back(gap == INT_MAX ? -1 : gap);            
+            ans.push_back(gap == INT_MAX ? -1 : gap);
         }
         return ans;
     }
-    
+
     bitset<101> query(int index, int s, int e, int qs, int qe){
-        
+
         if(s > e) return bitset<101>();
         if(e < qs || s > qe) return bitset<101>();
         if(qs <= s and e <= qe) return tree[index];
-        
+
         int mid = (s+e)/2;
         auto left = query(index*2, s, mid, qs, qe);
         auto right = query(index*2+1, mid+1, e, qs , qe);
         return left | right;
     }
-    
+
     void buildtree(int index, int s, int e, vector<int>& a){
-        
+
         if(s > e)return;
-        
+
         if(s==e){
             bitset<101> b;
             b[a[s]] = 1;
             tree[index] = b;
             return;
         }
-        
+
         int mid = (s+e)/2;
         buildtree(index*2, s, mid, a);
         buildtree(index*2+1, mid+1, e, a);
