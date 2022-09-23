889338b400e8745d7e490b1483119ab28428c050
AnasImloul <pydev27@gmail.com>
2022-09-23 15:18:08 +0100

    Runtime: 15 ms (Top 24.35%) | Memory: 8.4 MB (Top 28.20%)

--- scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.cpp
+++ scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.cpp
@@ -1,3 +1,4 @@
+// Runtime: 15 ms (Top 24.35%) | Memory: 8.4 MB (Top 28.20%)
 class Solution {
 public:
     //Similar to Matrix Chain Multiplication optimization
@@ -5,19 +6,19 @@ public:
     int solve(vector<int>& stones, int k, int left, int right, vector<vector<int>>& dp){
         if(right == left) return 0;
         if(dp[left][right] != -1) return dp[left][right];
-        
+
         int res = 1e9;
         for(int i = left; i < right; i=i+k-1){
             res = min(res, solve(stones, k, left, i, dp) + solve(stones, k, i+1, right, dp));
         }
-        
+
         //this cumulative sum evaluation separately, as that of MCX
         if((right - left)%(k-1) ==0){
             res += presum[right+1] - presum[left];
         }
         return dp[left][right] = res;
     }
-           
+
     int mergeStones(vector<int>& stones, int k) {
         if((stones.size() - 1)%(k-1) != 0) return -1;
         presum.push_back(0);
