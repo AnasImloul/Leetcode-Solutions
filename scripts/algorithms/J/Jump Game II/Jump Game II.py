0946fde5fd0089a72ce0aa8f31a058d402720b4e
AnasImloul <pydev27@gmail.com>
2022-09-23 17:42:08 +0100

    Runtime: 305 ms (Top 40.77%) | Memory: 15 MB (Top 90.04%)

--- scripts/algorithms/J/Jump Game II/Jump Game II.py
+++ scripts/algorithms/J/Jump Game II/Jump Game II.py
@@ -1,15 +1,16 @@
+# Runtime: 305 ms (Top 40.77%) | Memory: 15 MB (Top 90.04%)
 class Solution(object):
     def jump(self, nums):
         ans = l = r = 0
-        
-        while r < len(nums) -  1: 
+
+        while r < len(nums) - 1:
             farthestJump = 0
-			
+
             for i in range(l, r + 1):
                 farthestJump = max(farthestJump, i + nums[i])
-            
+
             l = r + 1
             r = farthestJump
             ans += 1
-            
+
         return ans
