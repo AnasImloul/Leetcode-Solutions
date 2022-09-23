8b702b8603b124bed7ad9d044068bdd69ccba1a0
AnasImloul <pydev27@gmail.com>
2022-09-23 15:18:10 +0100

    Runtime: 2599 ms (Top 5.05%) | Memory: 326.4 MB (Top 5.74%)

--- scripts/algorithms/F/Find Kth Bit in Nth Binary String/Find Kth Bit in Nth Binary String.cpp
+++ scripts/algorithms/F/Find Kth Bit in Nth Binary String/Find Kth Bit in Nth Binary String.cpp
@@ -1,3 +1,4 @@
+// Runtime: 2599 ms (Top 5.05%) | Memory: 326.4 MB (Top 5.74%)
 class Solution {
 public:
     string Reverse(string s){
@@ -6,7 +7,7 @@ public:
         }
         return s;
     }
-    
+
     string invert(string s){
         for(int i=0;i<s.length();i++){
             if(s[i]=='1') s[i]='0';
@@ -14,14 +15,14 @@ public:
         }
         return s;
     }
-    
+
     string S(int n) {
         if(n==1){
             return "0";
         }
         return S(n-1) + "1" + Reverse(invert(S(n-1)));
     }
-    
+
     char findKthBit(int n, int k) {
         if(n==1) return '0';
         string s = S(n-1) + "1" + Reverse(invert(S(n-1)));
