14e8c3ef4292e0bf9bd9f774e32838aae3e9f9b0
AnasImloul <pydev27@gmail.com>
2022-09-23 17:42:20 +0100

    Runtime: 111 ms (Top 6.54%) | Memory: 14.1 MB (Top 35.32%)

--- scripts/algorithms/S/Satisfiability of Equality Equations/Satisfiability of Equality Equations.py
+++ scripts/algorithms/S/Satisfiability of Equality Equations/Satisfiability of Equality Equations.py
@@ -1,19 +1,20 @@
+# Runtime: 111 ms (Top 6.54%) | Memory: 14.1 MB (Top 35.32%)
 class UnionFind:
     def __init__(self,n):
         self.tot_components = n
         self.parent = [-1] * n
         self.rank = [0] * n
-        
+
     def find(self,node):
         if self.parent[node] == -1:
             return node
         self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
-    
+
     def union(self,node1,node2):
         node1_p = self.find(node1)
         node2_p = self.find(node2)
-        
+
         if node1_p == node2_p:
             return True
         else:
@@ -26,16 +27,16 @@ class UnionFind:
                 self.rank[node2_p] += 1
             self.tot_components -= 1
             return False
-        
+
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        
+
         uf = UnionFind(26)
-        
+
         for eqn in equations:
             if eqn[1] == "=":
                 uf.union(ord(eqn[0])-ord("a"),ord(eqn[3])-ord("a"))
-        
+
         for eqn in equations:
             if eqn[1] == "!":
                 if uf.find(ord(eqn[0])-ord("a")) == uf.find(ord(eqn[3])-ord("a")):
