8650534138c12da2b7c1fef56f052defe98930f4
AnasImloul <pydev27@gmail.com>
2022-09-23 15:18:12 +0100

    Runtime: 71 ms (Top 26.41%) | Memory: 19.4 MB (Top 72.64%)

--- scripts/algorithms/L/Largest Multiple of Three/Largest Multiple of Three.cpp
+++ scripts/algorithms/L/Largest Multiple of Three/Largest Multiple of Three.cpp
@@ -1,10 +1,11 @@
+// Runtime: 71 ms (Top 26.41%) | Memory: 19.4 MB (Top 72.64%)
 class Solution {
 public:
-    
+
     vector<int> ans;
-    
+
     void calculate(vector<int>& count,int no,vector<int>& temp){
-        
+
         if(no==0){
             int flag=0,sum1=0,sum2=0,validSum=0;
             for(int j=1;j<10;j++){
@@ -15,13 +16,13 @@ public:
             if(validSum%3!=0){
                 return ;
             }
-            
+
             if(sum2>sum1)
                 return;
             else if(sum1>sum2){
              for(int i=1;i<10;i++){
-              ans[i]=temp[i];    
-             } 
+              ans[i]=temp[i];
+             }
                 return ;
             }
             int j=9;
@@ -36,31 +37,30 @@ public:
             }
             if (flag==1){
              for(int i=1;i<10;i++){
-              ans[i]=temp[i];    
-             }   
+              ans[i]=temp[i];
+             }
             }
             return ;
         }
-        
+
         int targetCount=count[no]-2;
-        
+
         if(targetCount<0)
             targetCount=0;
-        
+
         int co=count[no];
-       
+
         do{
             temp[no]=co;
-            
+
             calculate(count,no-1,temp);
-                
+
             co--;
         }
         while(co>=targetCount);
-                
-        
+
     }
-    
+
     string largestMultipleOfThree(vector<int>& digits) {
         vector<int> count(10,0);
         int n=digits.size();
@@ -70,7 +70,7 @@ public:
         vector<int> temp(10,0);
         ans.resize(10,0);
         calculate(count,9,temp);
-        
+
         string res="";
         ans[0]=count[0];
         for(int i=9;i>=0;i--){
