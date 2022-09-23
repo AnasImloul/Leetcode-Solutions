75afd504cf9f05b7c181fe7fe319f697410ae3a4
AnasImloul <pydev27@gmail.com>
2022-09-23 15:18:14 +0100

    Runtime: 47 ms (Top 39.17%) | Memory: 19.3 MB (Top 13.84%)

--- scripts/algorithms/M/Move Zeroes/Move Zeroes.cpp
+++ scripts/algorithms/M/Move Zeroes/Move Zeroes.cpp
@@ -1,9 +1,10 @@
+// Runtime: 47 ms (Top 39.17%) | Memory: 19.3 MB (Top 13.84%)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int nums_size = nums.size();
         int cntr=0;
-        
+
         for(int i=0;i<nums_size;i++)
         {
             if(nums[i] != 0)
