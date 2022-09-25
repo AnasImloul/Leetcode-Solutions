5367c99dfa2054ea24176bd5263f21be89804a5e
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:16 +0100

    Runtime: 138 ms (Top 25.43%) | Memory: 42.6 MB (Top 72.74%)

--- scripts/algorithms/I/Isomorphic Strings/Isomorphic Strings.js
+++ scripts/algorithms/I/Isomorphic Strings/Isomorphic Strings.js
@@ -1,3 +1,4 @@
+// Runtime: 138 ms (Top 25.43%) | Memory: 42.6 MB (Top 72.74%)
 /**
  * @param {string} s
  * @param {string} t
@@ -7,19 +8,19 @@ var isIsomorphic = function(s, t) {
     const obj = {};
     const setValues = new Set();
     let isIso = true;
-    
-    for(var indexI=0; indexI<s.length;indexI++) {  
-       if(obj[s[indexI]] || setValues.has(t[indexI])) {         
-            if (obj[s[indexI]] ===  t[indexI]) continue; 
-           
+
+    for(var indexI=0; indexI<s.length;indexI++) {
+       if(obj[s[indexI]] || setValues.has(t[indexI])) {
+            if (obj[s[indexI]] === t[indexI]) continue;
+
             isIso= false;
-            break;        
-       }       
-       else { 
-       	obj[s[indexI]] = t[indexI];
+            break;
+       }
+       else {
+           obj[s[indexI]] = t[indexI];
         setValues.add(t[indexI]);
        }
     }
-    
+
     return isIso;
 };
