4fd74818441c7cd31f654d9ca249229e00694dfa
AnasImloul <pydev27@gmail.com>
2022-09-25 16:43:59 +0100

    Runtime: 105 ms (Top 40.54%) | Memory: 42.2 MB (Top 81.08%)

--- scripts/algorithms/M/Minimum Cost to Set Cooking Time/Minimum Cost to Set Cooking Time.js
+++ scripts/algorithms/M/Minimum Cost to Set Cooking Time/Minimum Cost to Set Cooking Time.js
@@ -1,3 +1,4 @@
+// Runtime: 105 ms (Top 40.54%) | Memory: 42.2 MB (Top 81.08%)
 /**
  * @param {number} startAt
  * @param {number} moveCost
@@ -7,22 +8,22 @@
  */
 var minCostSetTime = function(startAt, moveCost, pushCost, targetSeconds) {
     let cost = Infinity;
-    
+
     let maxMinutes = Math.floor(targetSeconds / 60);
-    
+
     for(let min = 0; min <= maxMinutes; min++) {
         let secs = targetSeconds - min * 60;
-        
+
         if (secs > 99 || min > 99) continue;
-        
+
         let buttons = String(100 * min + secs);
         let prev = Number(buttons[0]);
-        
+
         let sum = 0;
-        
+
         //start index will vary according to startAt pointer
         let start = 0;
-        
+
         //If startAt is equal to first button, we need to add pushCost fatigue
         if(prev === startAt) {
             sum += pushCost;
@@ -31,21 +32,21 @@ var minCostSetTime = function(startAt, moveCost, pushCost, targetSeconds) {
             //Else we need to add moveCost fatigue
             sum += moveCost;
          }
-        
+
         for(let i = start; i < buttons.length; i++) {
             let button = Number(buttons[i]);
-            
+
             if(button !== prev) {
                 sum += moveCost + pushCost;
             } else {
                 sum += pushCost;
             }
-            
+
             prev = button;
         }
-        
+
         cost = Math.min(cost, sum)
     }
-    
+
     return cost;
 };
