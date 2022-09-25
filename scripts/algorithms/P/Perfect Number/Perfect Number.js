1cbb09c43847f1ec670ddbbbdb7321e36f69d12c
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:14 +0100

    Runtime: 4252 ms (Top 41.99%) | Memory: 42.1 MB (Top 50.71%)

--- scripts/algorithms/P/Perfect Number/Perfect Number.js
+++ scripts/algorithms/P/Perfect Number/Perfect Number.js
@@ -1,14 +1,15 @@
-	/**
+// Runtime: 4252 ms (Top 41.99%) | Memory: 42.1 MB (Top 50.71%)
+    /**
  * @param {number} num
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-	let total = 0
+    let total = 0
 
-	for(let i = 1 ; i < num;i++){
-		if(num % i == 0){
-			total += i
-		}
-	}
-	return total == num ? true : false
+    for(let i = 1 ; i < num;i++){
+        if(num % i == 0){
+            total += i
+        }
+    }
+    return total == num ? true : false
 };
