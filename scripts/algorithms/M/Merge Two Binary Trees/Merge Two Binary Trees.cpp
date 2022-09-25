a201684baaaecf554eb94918e8b1807361086378
AnasImloul <pydev27@gmail.com>
2022-09-25 17:01:43 +0100

    Runtime: 83 ms (Top 22.08%) | Memory: 32.4 MB (Top 61.23%)

--- scripts/algorithms/M/Merge Two Binary Trees/Merge Two Binary Trees.cpp
+++ scripts/algorithms/M/Merge Two Binary Trees/Merge Two Binary Trees.cpp
@@ -1,12 +1,13 @@
+// Runtime: 83 ms (Top 22.08%) | Memory: 32.4 MB (Top 61.23%)
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
