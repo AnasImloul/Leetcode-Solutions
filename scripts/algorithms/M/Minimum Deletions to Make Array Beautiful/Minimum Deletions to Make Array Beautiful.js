9fd0d967d8e0abc88a41101c95c15da378d41c18
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:15 +0100

    Runtime: 296 ms (Top 10.64%) | Memory: 60.3 MB (Top 8.51%)

--- scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
+++ scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.js
@@ -1,16 +1,17 @@
+// Runtime: 296 ms (Top 10.64%) | Memory: 60.3 MB (Top 8.51%)
 var minDeletion = function(nums) {
     const n = nums.length;
     const res = [];
-    
+
     for (let i = 0; i < n; ++i) {
         const num = nums[i];
-        
+
         if (res.length % 2 === 0 || res.at(-1) != num) {
             res.push(num);
         }
     }
-    
+
     if (res.length % 2 === 1) res.pop();
-    
+
     return n - res.length;
 };
