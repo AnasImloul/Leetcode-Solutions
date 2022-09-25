73ce4a586299f3a1364d390a313aba4902ea932a
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:00 +0100

    Runtime: 154 ms (Top 25.58%) | Memory: 52.6 MB (Top 62.79%)

--- scripts/algorithms/N/Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.js
+++ scripts/algorithms/N/Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.js
@@ -1,23 +1,24 @@
+// Runtime: 154 ms (Top 25.58%) | Memory: 52.6 MB (Top 62.79%)
 var getDescentPeriods = function(prices) {
     const n = prices.length;
-    
+
     let left = 0;
     let totRes = 0;
-    
+
     while (left < n) {
         let count = 1;
-        
+
         let right = left + 1;
-        
+
         while (right < n && prices[right] + 1 === prices[right - 1]) {
             count += (right - left + 1);
             ++right;
         }
-       
+
         totRes += count;
-        
+
         left = right;
     }
-    
+
     return totRes;
 };
