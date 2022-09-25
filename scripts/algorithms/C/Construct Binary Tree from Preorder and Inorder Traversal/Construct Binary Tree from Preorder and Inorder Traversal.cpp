80a47275657bfa2bb17402c92cf4a887090c69d2
AnasImloul <pydev27@gmail.com>
2022-09-25 17:01:36 +0100

    Runtime: 37 ms (Top 52.12%) | Memory: 26.5 MB (Top 17.39%)

--- scripts/algorithms/C/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ scripts/algorithms/C/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,18 +1,19 @@
+// Runtime: 37 ms (Top 52.12%) | Memory: 26.5 MB (Top 17.39%)
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     typedef vector<int>::iterator vecIt;
-    
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, TreeNode* retNode, vecIt startIt, vecIt endIt)
     {
         if (startIt >= endIt)
@@ -20,7 +21,7 @@ public:
         vecIt rootIt;
         vecIt midIt;
         int rootVal;
-        
+
         rootIt = preorder.begin();
         rootVal = *rootIt;
         preorder.erase(rootIt);
@@ -32,7 +33,7 @@ public:
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         TreeNode* retNode;
-        
+
         return (buildTree(preorder, inorder, retNode, inorder.begin(), inorder.end()));
     }
 };
