5e19517e1e54395bef18b0ea3975ff52a6ca2ac6
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:01 +0100

    Runtime: 287 ms (Top 21.05%) | Memory: 51.7 MB (Top 84.21%)

--- scripts/algorithms/M/Minimum Limit of Balls in a Bag/Minimum Limit of Balls in a Bag.js
+++ scripts/algorithms/M/Minimum Limit of Balls in a Bag/Minimum Limit of Balls in a Bag.js
@@ -1,12 +1,13 @@
+// Runtime: 287 ms (Top 21.05%) | Memory: 51.7 MB (Top 84.21%)
 var minimumSize = function(nums, maxOperations) {
   let i = 1
   let j = Math.max(...nums)
-  
+
   while (i <= j) {
     let mid = Math.floor((j-i)/2 + i)
     let count = 0
     nums.forEach(n => count += Math.floor((n-1)/mid))
-    
+
     if (count <= maxOperations) {
       j = mid - 1
     } else {
