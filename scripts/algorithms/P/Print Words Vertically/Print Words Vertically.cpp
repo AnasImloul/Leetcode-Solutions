bbfd9442bf01cf9763741e62b84882757563b36f
AnasImloul <pydev27@gmail.com>
2022-09-25 17:01:38 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 6.4 MB (Top 78.82%)

--- scripts/algorithms/P/Print Words Vertically/Print Words Vertically.cpp
+++ scripts/algorithms/P/Print Words Vertically/Print Words Vertically.cpp
@@ -1,22 +1,23 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 6.4 MB (Top 78.82%)
 class Solution {
 public:
     vector<string> printVertically(string s) {
-        
+
         int row = 0, col = 0;
-        
+
         stringstream sso(s);
         string buffer;
-        
+
         while (sso >> buffer)
         {
             row++;
             col = max((int)buffer.size(),col);
         }
-        
+
         vector<vector<char>> matrix(row, vector<char>(col,' '));
-        
+
         sso = stringstream(s);
-        
+
         int i = 0;
         while (sso >> buffer)
         {
@@ -24,14 +25,14 @@ public:
                 matrix[i][j] = buffer[j];
             i++;
         }
-        
+
         vector<string> res;
         for (int j = 0; j < col; j++)
         {
             string item;
             for (int i = 0; i < row; i++)
                 item.push_back(matrix[i][j]);
-            
+
             for (int i = item.size()-1; i >= 0; i--)
             {
                 if (item[i] != ' ')
@@ -42,7 +43,7 @@ public:
             }
             res.push_back(item);
         }
-            
+
         return res;
     }
 };
