6f4d8e144538cfd591506cecb3aed9122cbe2768
AnasImloul <pydev27@gmail.com>
2022-09-25 16:44:09 +0100

    Runtime: 119 ms (Top 5.56%) | Memory: 42.6 MB (Top 69.44%)

--- scripts/algorithms/E/Escape The Ghosts/Escape The Ghosts.js
+++ scripts/algorithms/E/Escape The Ghosts/Escape The Ghosts.js
@@ -1,8 +1,9 @@
+// Runtime: 119 ms (Top 5.56%) | Memory: 42.6 MB (Top 69.44%)
 var escapeGhosts = function(ghosts, target) {
     const getDistance = (target, source = [0, 0]) => {
         return (
             Math.abs(target[0] - source[0])
-            + 
+            +
             Math.abs(target[1] - source[1])
         );
     }
