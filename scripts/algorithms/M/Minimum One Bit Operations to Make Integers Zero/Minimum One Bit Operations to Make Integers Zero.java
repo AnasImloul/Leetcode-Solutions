e4c94deb83b7cd756b973fb6f97dd80e5a3e09b7
AnasImloul <pydev27@gmail.com>
2022-09-25 16:55:22 +0100

    Runtime: 52 ms (Top 5.71%) | Memory: 41.9 MB (Top 21.90%)

--- scripts/algorithms/M/Minimum One Bit Operations to Make Integers Zero/Minimum One Bit Operations to Make Integers Zero.java
+++ scripts/algorithms/M/Minimum One Bit Operations to Make Integers Zero/Minimum One Bit Operations to Make Integers Zero.java
@@ -1,15 +1,16 @@
+// Runtime: 52 ms (Top 5.71%) | Memory: 41.9 MB (Top 21.90%)
 class Solution {
     public int minimumOneBitOperations(int n) {
-       
-    int inv = 0; 
-      
-        //  xor until n becomes zero 
-        for ( ; n != 0 ; n = n >> 1){ 
-            
+
+    int inv = 0;
+
+        // xor until n becomes zero
+        for ( ; n != 0 ; n = n >> 1){
+
             inv ^= n;
             System.out.println(inv+" "+n);
         }
-      
-        return inv; 
+
+        return inv;
     }
 }
