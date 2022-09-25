5ed3b4f9dfb37d73067b275a7e3352e4e29d9f83
AnasImloul <pydev27@gmail.com>
2022-09-25 16:55:36 +0100

    Runtime: 324 ms (Top 17.74%) | Memory: 165.9 MB (Top 16.84%)

--- scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.java
+++ scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.java
@@ -1,10 +1,11 @@
+// Runtime: 324 ms (Top 17.74%) | Memory: 165.9 MB (Top 16.84%)
 class Solution {
-    
+
   public boolean repeatedSubstringPattern(String s) {
        String temp="";
         for(int i=0 ;i<s.length()/2 ;i++){
             temp+=s.charAt(i);
-            
+
             if(s.length()%temp.length()==0) {
                 int times_repeat= s.length()/temp.length();
                 StringBuilder str = new StringBuilder();
