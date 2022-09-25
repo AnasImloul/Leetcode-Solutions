b603ddb61f764ae3967f45b8b0e2b3c96fa08266
AnasImloul <pydev27@gmail.com>
2022-09-25 16:35:01 +0100

    Runtime: 202 ms (Top 7.96%) | Memory: 17.3 MB (Top 91.79%)

--- scripts/algorithms/I/Insert Interval/Insert Interval.py
+++ scripts/algorithms/I/Insert Interval/Insert Interval.py
@@ -1,14 +1,15 @@
+# Runtime: 202 ms (Top 7.96%) | Memory: 17.3 MB (Top 91.79%)
 class Solution:
-	def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
 
-		intervals.append(newInterval)
-		intervals.sort(key=lambda x: x[0])
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
 
-		result = []
-		for interval in intervals:
-			if not result or result[-1][1] < interval[0]:
-				result.append(interval)
-			else:
-				result[-1][1] = max(result[-1][1],interval[1])
+        result = []
+        for interval in intervals:
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1],interval[1])
 
-		return result
+        return result
