e634929738398cc07a33f54a579b7ac2030ce4f7
AnasImloul <pydev27@gmail.com>
2022-09-25 16:35:03 +0100

    Runtime: 3864 ms (Top 51.07%) | Memory: 53.5 MB (Top 68.62%)

--- scripts/algorithms/R/Reverse Nodes in Even Length Groups/Reverse Nodes in Even Length Groups.py
+++ scripts/algorithms/R/Reverse Nodes in Even Length Groups/Reverse Nodes in Even Length Groups.py
@@ -1,3 +1,4 @@
+# Runtime: 3864 ms (Top 51.07%) | Memory: 53.5 MB (Top 68.62%)
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
         group = 2
@@ -9,7 +10,7 @@ class Solution:
                 cur = cur.next
                 cnt += 1
             pre, cur = tail, tail.next
-            if cnt % 2 == 0: # if group size is even 
+            if cnt % 2 == 0: # if group size is even
                 while cnt and cur:
                     nxt = cur.next
                     cur.next = pre
