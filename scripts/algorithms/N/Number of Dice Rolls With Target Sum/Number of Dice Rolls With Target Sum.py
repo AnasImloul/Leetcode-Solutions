f594eb230a7756933949d60d71ec738513bf65b3
AnasImloul <pydev27@gmail.com>
2022-09-25 16:35:05 +0100

    Runtime: 782 ms (Top 50.50%) | Memory: 18.9 MB (Top 42.22%)

--- scripts/algorithms/N/Number of Dice Rolls With Target Sum/Number of Dice Rolls With Target Sum.py
+++ scripts/algorithms/N/Number of Dice Rolls With Target Sum/Number of Dice Rolls With Target Sum.py
@@ -1,3 +1,4 @@
+# Runtime: 782 ms (Top 50.50%) | Memory: 18.9 MB (Top 42.22%)
 
 class Solution(object):
     def numRollsToTarget(self, n, k, target):
@@ -8,34 +9,27 @@ class Solution(object):
         :rtype: int
         """
 
-        
         mem = {}
-        
-        
-        
+
         def dfs(i,currSum):
-            
-            
-            
+
             if currSum > target:
                 return 0
-            
+
             if i == n:
                 if currSum == target:
                     return 1
                 return 0
-                
-            
+
             if (i,currSum) in mem:
-                return mem[(i,currSum)]   
-            
+                return mem[(i,currSum)]
+
             ans = 0
             for dicenumber in range(1,k+1):
                 ans += dfs(i+1,currSum+dicenumber)
-                
+
             mem[(i,currSum)] = ans
-            
+
             return mem[(i,currSum)]
-        
+
         return dfs(0,0) % (10**9 + 7)
-                
