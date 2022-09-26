2cee4eed106dc55914c255e2ec17d1f8322df47a
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:33 +0100

    Runtime: 260 ms (Top 32.64%) | Memory: 13.9 MB (Top 58.72%)

--- scripts/algorithms/S/Super Pow/Super Pow.py
+++ scripts/algorithms/S/Super Pow/Super Pow.py
@@ -1,9 +1,10 @@
+# Runtime: 260 ms (Top 32.64%) | Memory: 13.9 MB (Top 58.72%)
 class Solution:
-	def superPow(self, a: int, b: List[int]) -> int:
-		mod = 1337
-		ans = 1
+    def superPow(self, a: int, b: List[int]) -> int:
+        mod = 1337
+        ans = 1
 
-		for power in b:
-			ans  = ((pow(ans,10)%mod)*(pow(a,power)%mod))%mod
+        for power in b:
+            ans = ((pow(ans,10)%mod)*(pow(a,power)%mod))%mod
 
-		return ans
+        return ans
