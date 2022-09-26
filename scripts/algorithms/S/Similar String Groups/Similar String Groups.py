96dd78cb0fb46dbc7faccedb462cf5493eec81bf
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:28 +0100

    Runtime: 5068 ms (Top 15.23%) | Memory: 14.3 MB (Top 64.62%)

--- scripts/algorithms/S/Similar String Groups/Similar String Groups.py
+++ scripts/algorithms/S/Similar String Groups/Similar String Groups.py
@@ -1,3 +1,4 @@
+# Runtime: 5068 ms (Top 15.23%) | Memory: 14.3 MB (Top 64.62%)
 class Solution: #839. Similar String Groups
     def numSimilarGroups(self, strs: List[str]) -> int:
         #memo
@@ -5,12 +6,12 @@ class Solution: #839. Similar String Groups
         count = 0
         for i in range(len(strs)):
             if i not in visited:
-                #dfs 
+                #dfs
                 self.dfs(strs, i, visited)
                 #add a new connected area
                 count += 1
         return count
-    
+
     #dfs to search the similar string from 0 to n-1
     def dfs(self, strs, i, visited):
         #add current string to memo
@@ -18,8 +19,8 @@ class Solution: #839. Similar String Groups
         for j in range(len(strs)):
             if self.isSimilar(strs[i], strs[j]) and j not in visited:
                 self.dfs(strs, j , visited)
-                
-    # calculate the similarity of two strings            
+
+    # calculate the similarity of two strings
     def isSimilar(self, str1, str2):
         diff_count = 0
         for i in range(len(str1)):
