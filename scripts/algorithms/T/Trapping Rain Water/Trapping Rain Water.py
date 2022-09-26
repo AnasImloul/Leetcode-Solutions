ac7365d9a78562e85acedc706458074d8a5c6cb7
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:34 +0100

    Runtime: 254 ms (Top 43.79%) | Memory: 16.1 MB (Top 46.67%)

--- scripts/algorithms/T/Trapping Rain Water/Trapping Rain Water.py
+++ scripts/algorithms/T/Trapping Rain Water/Trapping Rain Water.py
@@ -1,3 +1,4 @@
+# Runtime: 254 ms (Top 43.79%) | Memory: 16.1 MB (Top 46.67%)
 class Solution:
     def trap(self, a: List[int]) -> int:
         l=0
@@ -5,18 +6,14 @@ class Solution:
         maxl=0
         maxr=0
         res=0
-        
+
         while (l<=r):
             if a[l]<=a[r]:
-                if a[l]>=maxl: maxl=a[l]  #update maxl if a[l] is >=
-                else: res+=maxl-a[l]      #adding captured water when maxl>a[l]
+                if a[l]>=maxl: maxl=a[l] #update maxl if a[l] is >=
+                else: res+=maxl-a[l] #adding captured water when maxl>a[l]
                 l+=1
             else:
                 if a[r]>=maxr: maxr=a[r]
                 else: res+=maxr-a[r]
                 r-=1
         return res
-                
-                
-                
-            
