e0d5bb3b6f6dc87d13f8fe83e1af763ac2de821e
AnasImloul <pydev27@gmail.com>
2022-09-26 17:33:16 +0100

    Runtime: 177 ms (Top 10.70%) | Memory: 46.2 MB (Top 31.77%)

--- scripts/algorithms/S/Shortest Unsorted Continuous Subarray/Shortest Unsorted Continuous Subarray.js
+++ scripts/algorithms/S/Shortest Unsorted Continuous Subarray/Shortest Unsorted Continuous Subarray.js
@@ -1,11 +1,12 @@
+// Runtime: 177 ms (Top 10.70%) | Memory: 46.2 MB (Top 31.77%)
 var findUnsortedSubarray = function(nums) {
-	let left = 0;
-	let right = nums.length - 1;
-	const target = [...nums].sort((a, b) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
+    const target = [...nums].sort((a, b) => a - b);
 
-	while (left < right && nums[left] === target[left]) left += 1;
-	while (right > left && nums[right] === target[right]) right -= 1;
-	const result = right - left;
+    while (left < right && nums[left] === target[left]) left += 1;
+    while (right > left && nums[right] === target[right]) right -= 1;
+    const result = right - left;
 
-	return result === 0 ? 0 : result + 1;
+    return result === 0 ? 0 : result + 1;
 };
