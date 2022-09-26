20ddb53ffe41ea98c0e690809a2a28e820402f4b
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:36 +0100

    Runtime: 112 ms (Top 18.89%) | Memory: 16.6 MB (Top 49.70%)

--- scripts/algorithms/A/Average of Levels in Binary Tree/Average of Levels in Binary Tree.py
+++ scripts/algorithms/A/Average of Levels in Binary Tree/Average of Levels in Binary Tree.py
@@ -1,14 +1,15 @@
+# Runtime: 112 ms (Top 18.89%) | Memory: 16.6 MB (Top 49.70%)
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# def __init__(self, val=0, left=None, right=None):
+# self.val = val
+# self.left = left
+# self.right = right
 class Solution:
 # O(n) || O(h) where h is the height of the tree
 # Runtime: 70ms 63.75% || Memory: 16.5mb 87.89%
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        
+
         queue = deque([root])
         result = []
 
@@ -25,10 +26,9 @@ class Solution:
                     queue.append(currNode.left)
                 if currNode.right:
                     queue.append(currNode.right)
-                    
+
             secretFormula = (runningSum/newLevelSize)
 
             result.append(secretFormula)
-
 
         return result
