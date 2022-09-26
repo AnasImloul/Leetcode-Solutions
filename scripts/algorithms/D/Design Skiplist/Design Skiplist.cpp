4a70d8cf8eb42155e4057435bd98d3e85008c6e0
AnasImloul <pydev27@gmail.com>
2022-09-26 17:55:59 +0100

    Runtime: 84 ms (Top 77.47%) | Memory: 29 MB (Top 86.97%)

--- scripts/algorithms/D/Design Skiplist/Design Skiplist.cpp
+++ scripts/algorithms/D/Design Skiplist/Design Skiplist.cpp
@@ -1,21 +1,22 @@
+// Runtime: 84 ms (Top 77.47%) | Memory: 29 MB (Top 86.97%)
 class Skiplist {
 public:
     map<int,int>s;
     Skiplist() {
-        
+
     }
-    
+
     bool search(int target) {
         if(s.find(target)!=s.end())
             return true;
-        else 
+        else
             return false;
     }
-    
+
     void add(int num) {
         s[num]++;
     }
-    
+
     bool erase(int num) {
         if(s.find(num)==s.end())
             return false;
