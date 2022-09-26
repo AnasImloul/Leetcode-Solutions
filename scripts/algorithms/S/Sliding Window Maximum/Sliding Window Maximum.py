0f4800e3c95dc916fede28844c56e69608fb91e7
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:31 +0100

    Runtime: 4951 ms (Top 7.51%) | Memory: 39.4 MB (Top 5.02%)

--- scripts/algorithms/S/Sliding Window Maximum/Sliding Window Maximum.py
+++ scripts/algorithms/S/Sliding Window Maximum/Sliding Window Maximum.py
@@ -1,15 +1,16 @@
+# Runtime: 4951 ms (Top 7.51%) | Memory: 39.4 MB (Top 5.02%)
 class Solution:
-	def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-		ans = []
-		pq = []
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        ans = []
+        pq = []
 
-		for i in range(k): heapq.heappush(pq,(-nums[i],i))
+        for i in range(k): heapq.heappush(pq,(-nums[i],i))
 
-		ans.append(-pq[0][0])
+        ans.append(-pq[0][0])
 
-		for i in range(k,len(nums)):
-			heapq.heappush(pq,(-nums[i],i))
-			while pq and pq[0][1] < i-k+1 : heapq.heappop(pq)
-			ans.append(-pq[0][0])
+        for i in range(k,len(nums)):
+            heapq.heappush(pq,(-nums[i],i))
+            while pq and pq[0][1] < i-k+1 : heapq.heappop(pq)
+            ans.append(-pq[0][0])
 
-		return ans
+        return ans
