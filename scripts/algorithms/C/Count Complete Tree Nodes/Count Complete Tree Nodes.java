957c625855b12aaa2b8c5f5c6da2d1a077395f86
AnasImloul <pydev27@gmail.com>
2022-09-26 18:11:36 +0100

    Runtime: 0 ms (Top 100.00%) | Memory: 50.7 MB (Top 10.84%)

--- scripts/algorithms/C/Count Complete Tree Nodes/Count Complete Tree Nodes.java
+++ scripts/algorithms/C/Count Complete Tree Nodes/Count Complete Tree Nodes.java
@@ -1,22 +1,23 @@
+// Runtime: 0 ms (Top 100.00%) | Memory: 50.7 MB (Top 10.84%)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    
-    static int count = 0; 
-    
+
+    static int count = 0;
+
     static void Postorder(TreeNode root){
         if(root == null){
             return;
@@ -25,7 +26,7 @@ class Solution {
         Postorder(root.right);
         count++;
     }
-    
+
     public int countNodes(TreeNode root) {
         count = 0;
         Postorder(root);
