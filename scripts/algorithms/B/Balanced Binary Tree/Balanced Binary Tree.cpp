c7568f4b81822d4888b75c22c6cf496a0e45db17
AnasImloul <pydev27@gmail.com>
2022-09-26 17:55:58 +0100

    Runtime: 8 ms (Top 96.99%) | Memory: 20.9 MB (Top 54.47%)

--- scripts/algorithms/B/Balanced Binary Tree/Balanced Binary Tree.cpp
+++ scripts/algorithms/B/Balanced Binary Tree/Balanced Binary Tree.cpp
@@ -1,3 +1,4 @@
+// Runtime: 8 ms (Top 96.99%) | Memory: 20.9 MB (Top 54.47%)
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
@@ -6,20 +7,20 @@ public:
     int height(TreeNode* root){
         if(!root)
             return 1;
-        
+
         int left = height(root->left);
-        
+
         if(left == -1)
             return -1;
-        
+
         int right = height(root->right);
-        
+
         if(right == -1)
             return -1;
-        
+
         if(abs(left - right) > 1)
             return -1;
-        
+
         return 1 + max(left, right);
     }
 };
