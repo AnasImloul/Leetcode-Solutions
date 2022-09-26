90f8153e0e151f0a7279b6c2dc0b79f8aeb45472
AnasImloul <pydev27@gmail.com>
2022-09-26 17:23:40 +0100

    Runtime: 66 ms (Top 35.29%) | Memory: 13.8 MB (Top 55.50%)

--- scripts/algorithms/F/Fibonacci Number/Fibonacci Number.py
+++ scripts/algorithms/F/Fibonacci Number/Fibonacci Number.py
@@ -1,8 +1,9 @@
+# Runtime: 66 ms (Top 35.29%) | Memory: 13.8 MB (Top 55.50%)
 class Solution:
     def fib(self, n: int) -> int:
         fa = [0, 1]
-        
+
         for i in range(2, n + 1):
             fa.append(fa[i-2] + fa[i-1])
-        
+
         return fa[n]
