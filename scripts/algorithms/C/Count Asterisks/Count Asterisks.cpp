2c6f419f24b4779e8e59eb21d9909b7fec2984bc
AnasImloul <pydev27@gmail.com>
2022-09-26 17:55:59 +0100

    Runtime: 3 ms (Top 64.27%) | Memory: 6.2 MB (Top 96.08%)

--- scripts/algorithms/C/Count Asterisks/Count Asterisks.cpp
+++ scripts/algorithms/C/Count Asterisks/Count Asterisks.cpp
@@ -1,18 +1,19 @@
+// Runtime: 3 ms (Top 64.27%) | Memory: 6.2 MB (Top 96.08%)
 /*
 1.) Maintain a counter to store the '|'
-2.) After every even no. of '|' count the asteriks. 
+2.) After every even no. of '|' count the asteriks.
 */
 class Solution {
 public:
     int countAsterisks(string s) {
-	
-	 int ans=0;
-	 
-	// count of no. of '|'
+
+     int ans=0;
+
+    // count of no. of '|'
         int cnt=0;
-		
+
         for(int i=0;i<s.size() ;i++) {
-		 //if count is even then count the asteriks
+         //if count is even then count the asteriks
               if(cnt%2==0) {
                   if(s[i]=='*') ans++;
               }
@@ -21,4 +22,3 @@ public:
         return ans;
     }
 };
-
