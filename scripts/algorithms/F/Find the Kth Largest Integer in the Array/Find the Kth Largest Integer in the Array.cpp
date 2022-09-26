6781028bd0fe46a034a1f0ce14c6db5b00bf17b9
AnasImloul <pydev27@gmail.com>
2022-09-26 17:56:00 +0100

    Runtime: 513 ms (Top 42.13%) | Memory: 55.3 MB (Top 76.34%)

--- scripts/algorithms/F/Find the Kth Largest Integer in the Array/Find the Kth Largest Integer in the Array.cpp
+++ scripts/algorithms/F/Find the Kth Largest Integer in the Array/Find the Kth Largest Integer in the Array.cpp
@@ -1,3 +1,4 @@
+// Runtime: 513 ms (Top 42.13%) | Memory: 55.3 MB (Top 76.34%)
 class Solution {
 public:
     static bool cmp(string &a,string &b)
@@ -8,7 +9,7 @@ public:
         }
         return a.size()<b.size();
     }
-    string kthLargestNumber(vector<string>& nums, int k) 
+    string kthLargestNumber(vector<string>& nums, int k)
     {
         sort(nums.begin(),nums.end(),cmp);
         int n=nums.size();
