c8446894469054a6b50a17176f66f5d180a3b361
AnasImloul <pydev27@gmail.com>
2022-10-14 16:18:36 +0100

    Runtime: 96 ms (Top 19.45%) | Memory: 30.9 MB (Top 22.86%)

--- scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.cpp
+++ scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.cpp
@@ -1,46 +1,47 @@
-class Codec 
+// Runtime: 96 ms (Top 19.45%) | Memory: 30.9 MB (Top 22.86%)
+class Codec
 {
     public:
-    
+
     string pre(TreeNode * root)
     {
         if(root==NULL)
             return "";
-        
+
         int temp = root->val;
         string tempz = to_string(temp);
-        
+
         string l = pre(root->left);
         string r = pre(root->right);
-        
+
         return (tempz + "," + l + "," + r);
     }
-    
+
     vector<int> parse_string(string data)
     {
         vector<int> ans;
         string temp = "";
         int n=data.size();
-        
+
         for(int i=0;i<n; ++i)
         {
             if(data[i]==',')
             {
                 if(temp.size()==0)
                     continue;
-                else 
+                else
                 {
                     int val = stoi(temp);
                     ans.push_back(val);
                     temp="";
                 }
             }
-            else 
+            else
                 temp += data[i];
         }
         return ans;
     }
-    
+
     TreeNode * make_tree(vector<int>& preorder, int min_val, int max_val, int& preorder_idx, int n)
     {
         if(preorder_idx == n)
@@ -58,26 +59,25 @@ class Codec
             return NULL;
     }
 
-    string serialize(TreeNode* root) 
+    string serialize(TreeNode* root)
     {
         string traversal = pre(root);
         //cout<<traversal<<endl;
         return traversal;
     }
 
-    TreeNode* deserialize(string data) 
+    TreeNode* deserialize(string data)
     {
         if(data.size()==0)
             return NULL;
         vector<int> traversal = parse_string(data);
-        
+
         // for(auto x:traversal)
-        //     cout<<x<<" ";
-        
+        // cout<<x<<" ";
+
         int preorder_idx = 0;
         TreeNode * root = make_tree(traversal, INT_MIN, INT_MAX, preorder_idx, traversal.size());
-        
+
         return root;
     }
 };
-
