fb44983f39e264085a630b53c2bad2dacb7025fb
Ihssaneabousshal <ihssaneabousahl@gmail.com>
2022-11-01 13:32:28 +0100

    Runtime: 109 ms (Top 73.24%) | Memory: 34.7 MB (Top 28.82%)

--- scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.cpp
+++ scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.cpp
@@ -1,15 +1,16 @@
+// Runtime: 109 ms (Top 73.24%) | Memory: 34.7 MB (Top 28.82%)
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         int n=nums.size();
         vector<int> a;
         vector<int> arr(n+1,0);
-        for(int i=0;i<nums.size();i++) 
+        for(int i=0;i<nums.size();i++)
             arr[nums[i]]++;
-        
-        for(int j=0;j<=n;j++) 
+
+        for(int j=0;j<=n;j++)
             if(arr[j]==2) a.push_back(j);
-        
+
         return a;
     }
 };
