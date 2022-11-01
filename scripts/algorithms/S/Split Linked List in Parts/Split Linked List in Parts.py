3c13d09908f0a3f191fba4acb423458ddf60161c
Ihssaneabousshal <ihssaneabousahl@gmail.com>
2022-11-01 13:36:54 +0100

    Runtime: 50 ms (Top 81.38%) | Memory: 14.5 MB (Top 12.03%)

--- scripts/algorithms/S/Split Linked List in Parts/Split Linked List in Parts.py
+++ scripts/algorithms/S/Split Linked List in Parts/Split Linked List in Parts.py
@@ -1,8 +1,9 @@
+# Runtime: 50 ms (Top 81.38%) | Memory: 14.5 MB (Top 12.03%)
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# def __init__(self, val=0, next=None):
+# self.val = val
+# self.next = next
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
         length = 0
@@ -12,18 +13,18 @@ class Solution:
             cur = cur.next
         # DON'T do following since this makes head become null
         # while head:
-        #     length += 1
-        #     head = head.next
-        
+        # length += 1
+        # head = head.next
+
         # calculate the base size and the number of parts contain extra number
         size, extra = length // k, length % k
-        
+
         # create empty list to store split parts
         res = [[] for _ in range(k)]
-        
+
         # use two ptrs to split parts
         prev, cur = None, head
-        
+
         for i in range(k):
             res[i] = cur
             # if this part contains extra number, it has (size+1) number
@@ -31,5 +32,5 @@ class Solution:
                 prev, cur = cur, cur.next
             if prev: prev.next = None
             extra -= 1
-        
+
         return res
