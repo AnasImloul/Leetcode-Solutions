a572ee0a6027a0793d97af5326d3b5cb0592e54d
Ihssaneabousshal <ihssaneabousahl@gmail.com>
2022-11-01 13:36:54 +0100

    Runtime: 68 ms (Top 17.05%) | Memory: 13.9 MB (Top 69.77%)

--- scripts/algorithms/S/Smallest Value of the Rearranged Number/Smallest Value of the Rearranged Number.py
+++ scripts/algorithms/S/Smallest Value of the Rearranged Number/Smallest Value of the Rearranged Number.py
@@ -1,3 +1,4 @@
+# Runtime: 68 ms (Top 17.05%) | Memory: 13.9 MB (Top 69.77%)
 class Solution:
     def smallestNumber(self, num: int) -> int:
         lst=[i for i in str(num)]
@@ -8,7 +9,7 @@ class Solution:
             itr=0
             while itr<len(lst) and lst[itr]=='0':
                 itr+=1
-            if itr==len(lst):       #All zeroes
+            if itr==len(lst): #All zeroes
                 return ''.join(lst)
             return ''.join([lst[itr]]+lst[:itr]+lst[itr+1:])
         return ''.join(lst)
