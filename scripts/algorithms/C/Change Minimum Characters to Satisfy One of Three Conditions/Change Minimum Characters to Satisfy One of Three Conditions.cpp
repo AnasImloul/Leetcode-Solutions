361d82a1c789cd6197fd77dad888f57e1ef4e552
Ihssaneabousshal <ihssaneabousahl@gmail.com>
2022-11-01 13:32:33 +0100

    Runtime: 560 ms (Top 5.43%) | Memory: 14.7 MB (Top 97.67%)

--- scripts/algorithms/C/Change Minimum Characters to Satisfy One of Three Conditions/Change Minimum Characters to Satisfy One of Three Conditions.cpp
+++ scripts/algorithms/C/Change Minimum Characters to Satisfy One of Three Conditions/Change Minimum Characters to Satisfy One of Three Conditions.cpp
@@ -1,3 +1,4 @@
+// Runtime: 560 ms (Top 5.43%) | Memory: 14.7 MB (Top 97.67%)
 class Solution {
 public:
     int count(string &a,string &b)
@@ -15,13 +16,13 @@ public:
     int minCharacters(string a, string b) {
         int x=count(a,b),y=count(b,a);
         int fans=INT_MAX;
-        
+
         for(char c='a';c<='z';c++)
         {
             int cnt=0;
             for(char it:a) cnt+=(it!=c);
             for(char it:b) cnt+=(it!=c);
-            fans=min(fans,cnt);  
+            fans=min(fans,cnt);
         }
         return min({x,y,fans});
     }
