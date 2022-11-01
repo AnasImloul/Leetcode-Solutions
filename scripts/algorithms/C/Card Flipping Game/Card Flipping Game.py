8532b0882b5c1a170086929a646a501c458286a1
Ihssaneabousshal <ihssaneabousahl@gmail.com>
2022-11-01 13:37:03 +0100

    Runtime: 7624 ms (Top 5.13%) | Memory: 14.1 MB (Top 95.38%)

--- scripts/algorithms/C/Card Flipping Game/Card Flipping Game.py
+++ scripts/algorithms/C/Card Flipping Game/Card Flipping Game.py
@@ -1,3 +1,4 @@
+# Runtime: 7624 ms (Top 5.13%) | Memory: 14.1 MB (Top 95.38%)
 class Solution:
     def flipgame(self, fronts: List[int], backs: List[int]) -> int:
         return min([v for v in fronts + backs if v not in set([i for i, j in zip(fronts, backs) if i == j])] or [0])
