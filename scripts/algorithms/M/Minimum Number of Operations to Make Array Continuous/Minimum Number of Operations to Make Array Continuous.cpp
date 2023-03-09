d43aa30796576cba0e7036d838031a4a8347b2d6
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-09 07:00:47 +0100

    Runtime 203 ms (Top 83.67%) | Memory 64.0 MB (Top 95.51%)

--- scripts/algorithms/M/Minimum Number of Operations to Make Array Continuous/Minimum Number of Operations to Make Array Continuous.cpp
+++ scripts/algorithms/M/Minimum Number of Operations to Make Array Continuous/Minimum Number of Operations to Make Array Continuous.cpp
@@ -1,22 +1,20 @@
-// Runtime: 308 ms (Top 76.57%) | Memory: 64.2 MB (Top 76.40%)
-
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int k = nums.size() - 1; // max size of sliding window is the target gap between min ani max elements
+        int k = nums.size() - 1;  // max size of sliding window is the target gap between min and max elements
         sort(nums.begin(), nums.end()); // sorting in O(nlogn) time so that we can remove duplicates and slide over the array
-        int newLen = unique(nums.begin(), nums.end()) - nums.begin(); // remove adjacent duplicates with STL unique
-        int l = 0, r = 0, fin = 1; // initialize left and right pointers and the minimum elements in the window
-        while (r < newLen) { // iterate over the new unique array
+        int newLen = unique(nums.begin(), nums.end()) - nums.begin();  // remove adjacent duplicates with STL unique
+        int l = 0, r = 0, fin = 1; //  initialize left and right pointers and the minimum elements in the window
+        while (r < newLen) {  // iterate over the new unique array
             if (l == r)
-                r++; // if the window is closed, open it ;)
+                r++;  //  if the window is closed, open it ;)
             else if (nums[r] - nums[l] > k)
-                l++; // if window becomes bigger than allowed size, shrink it
+                l++;  //  if window becomes bigger than allowed size, shrink it
             else {
-                fin = max(fin, r - l + 1); // if window is in allowed size, maximize the number of elements in the window
-                r++; // slide the right side
+                fin = max(fin, r - l + 1);  //  if window is in allowed size, maximize the number of elements in the window
+                r++;  // slide the right side 
             }
-        } // at this point, we have maximized the number of elements at any time inside the window
-        return k - fin + 1; // return the missing elements in that window
+        }  //  at this point, we have maximized the number of elements at any time inside the window
+        return k - fin + 1;   //  return the missing elements in that window
     }
 };
