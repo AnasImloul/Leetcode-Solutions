bbbd6e381465b97acc4d2da6c48076cd046c2307
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-09 07:16:12 +0100

    Runtime 10 ms (Top 76.1%) | Memory 10.0 MB (Top 31.42%)

--- scripts/algorithms/C/Counting Words With a Given Prefix/Counting Words With a Given Prefix.cpp
+++ scripts/algorithms/C/Counting Words With a Given Prefix/Counting Words With a Given Prefix.cpp
@@ -1,23 +1,20 @@
-// The easiest solution to this problem is:
-// Step 1: Calculate length of "pref"
-// Step 2: find substring of "words" from 0 to length of "pref"
-// if both match increment count by 1;
-
-
-
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        
-        int count=0;
-        int preflen=pref.size();        //step 1
-        
-        for(auto i:words){
-            if(i.substr(0,preflen) == pref)     //step 2
-                count++;                        //if both matches then increment count by 1
-            
+        int c=0;
+        for(auto word:words){
+            int b=0;
+            for(int i=0;i<pref.size();i++){
+                if(word[i]!=pref[i]){
+                    break;
+                }else{
+                    b++;
+                }
+            }
+            if(b==pref.size()){
+                c++;
+            }
         }
-        return count;   //return count
-        
+        return c;
     }
 };
