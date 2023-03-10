c18785bb0ef83389a02ab8c8fa6f405c64774331
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 00:30:43 +0100

    Runtime 0 ms (Top 100.0%) | Memory 5.0 MB (Top 94.6%)

--- scripts/algorithms/S/Smallest Integer Divisible by K/Smallest Integer Divisible by K.cpp
+++ scripts/algorithms/S/Smallest Integer Divisible by K/Smallest Integer Divisible by K.cpp
@@ -1,9 +1,17 @@
 class Solution {
-public: 
-    int smallestRepunitDivByK(int K) {
-        for (int r = 0, N = 1; N <= K; ++N)
-            if ((r = (r * 10 + 1) % K) == 0)
-                return N;
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k&1==0)return -1;
+        long long val=0;
+
+        for(int i=1; i<=k;i++)
+        {
+            // val=((val*10)+1);
+            if((val=(val*10+1)%k)==0)return i;
+        }
         return -1;
+        
+        
+        
     }
 };
