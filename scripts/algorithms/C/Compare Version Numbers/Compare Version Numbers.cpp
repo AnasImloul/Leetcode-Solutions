6c51a6695db80c7a3d72eb1ec9424c8d3d919ffa
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 16:39:57 +0100

    Runtime 4 ms (Top 31.56%) | Memory 6.0 MB (Top 41.35%)

--- scripts/algorithms/C/Compare Version Numbers/Compare Version Numbers.cpp
+++ scripts/algorithms/C/Compare Version Numbers/Compare Version Numbers.cpp
@@ -1,53 +1,38 @@
 class Solution {
-private:
-     int countDots(string &s){
-        int ans=0;
-        for(char c:s){
-            if(c=='.'){
-                ans++;
-            }
-        }
-        return ans;
-    }
-    //adds more revisions to the shorter one
-    void elongate(string &s,int remainingDots){
-        while(remainingDots--){
-            s+=".0";
-        }
-    }
-    //makes both the versions have equal number of revisions
-    void equate(string &version1,string &version2){
-        int dot1=countDots(version1);
-        int dot2=countDots(version2);
-        if(dot1>dot2){
-            elongate(version2,dot1-dot2);
-        } else if(dot2>dot1){
-            elongate(version1,dot2-dot1);
-        }
-    }
 public:
-    int compareVersion(string version1, string version2) {
-        equate(version1,version2);
-        int s1=0,s2=0;
-        while(s1<version1.size() and s2<version2.size()){
-            int e1=s1,e2=s2;
-            while(e1<version1.size() and version1[e1]!='.'){
-                e1++;
+    int compareVersion(string v1, string v2) {
+        int ans = 0;
+        int n1 = v1.size(), n2 = v2.size();
+        
+        for(int i=0,j=0; i<n1 || j<n2; ++i, ++j) {
+            
+            string s1 = "", s2 = "";
+
+            while(i<n1 && v1[i] != '.') {
+                if(s1.size() == 0 && v1[i] == '0') {
+                    ++i; continue;
+                }
+                
+                s1 += v1[i++];
             }
-            while(e2<version2.size() and version2[e2]!='.'){
-                e2++;
+            
+            while(j<n2 && v2[j] != '.') {
+                if(s2.size() == 0 && v2[j] == '0') {
+                    ++j; continue;
+                }
+                
+                s2 += v2[j++];
             }
-            int first=stoi(version1.substr(s1,e1-s1));
-            int second=stoi(version2.substr(s2,e2-s2));
-            if(first<second){
-                return -1;
-            } else if(first>second){
-                return 1;
-            } else {
-                s2=e2+1;
-                s1=e1+1;
-            }
+            
+            if(s1.size() < s2.size()) return -1;
+            else if(s2.size() < s1.size()) return 1;
+            
+            ans = s1.compare(s2);
+            if(ans < 0) return -1;
+            else if(ans > 0) return 1;
         }
+        
         return 0;
+        
     }
 };
