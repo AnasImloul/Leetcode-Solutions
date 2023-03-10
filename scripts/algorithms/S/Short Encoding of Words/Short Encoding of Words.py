ecefb65a7c83607f5ae930d2005e7430d87e11ff
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:13:34 +0100

    Runtime 163 ms (Top 65.19%) | Memory 16.0 MB (Top 33.54%)

--- scripts/algorithms/S/Short Encoding of Words/Short Encoding of Words.py
+++ scripts/algorithms/S/Short Encoding of Words/Short Encoding of Words.py
@@ -1,25 +1,36 @@
-# Runtime: 161 ms (Top 85.25%) | Memory: 14.5 MB (Top 85.26%)
 class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int:
-        """
-        "time", "me", "bell" reverse() =>
-        "emit", "em", "lleb" sort(reverse) =>
-        "lleb"
-        "emit"
-        "em"
-        """
+    def minimumLengthEncoding(self, words):
+        
+        
+        # root of suffix trie
+        trie_root = dict()
+        
+        # helper function to judge leaf node
+        isLeafNode = lambda node: len(node) == 0
+        
+        # collection of tail nodes
+        tail_nodes = []
+        
+        # set of unique words
+        unique_words = set(words)
+        
+        # scan each word
+        for word in unique_words:
+            
+            # build suffix trie from root node
+            cur = trie_root
+            
+            # scan each character in reversed order
+            for char in reversed(word):
+                
+                # update trie
+                cur[char] = cur.get(char, dict() )
+                
+                # go to next level
+                cur = cur[char]
+            
+            # save tail nodes with corresponding word length, +1 is for '#' symbol
+            tail_nodes.append( (cur, len(word)+1) )
 
-        words = [w[::-1] for w in words]
-
-        words.sort(reverse=True)
-
-        res = 0
-        comparable = ""
-        prev_max_len = 0
-
-        for w in words:
-            if not comparable.startswith(w):
-                res += len(w) + 1
-                comparable = w
-
-        return res
+        # summation of the length with all tail node which is also a leaf node
+        return sum( suffix_length for node, suffix_length in tail_nodes if isLeafNode(node) )
