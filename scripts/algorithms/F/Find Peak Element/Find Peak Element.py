498a0e45a1b05bf27323b72ac35ce549ae685eae
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:26:27 +0100

    Runtime 51 ms (Top 40.68%) | Memory 14.0 MB (Top 34.71%)

--- scripts/algorithms/F/Find Peak Element/Find Peak Element.py
+++ scripts/algorithms/F/Find Peak Element/Find Peak Element.py
@@ -1,29 +1,19 @@
-class Solution:
-    """
-    we can solve this problem using binary search
-    """
-    def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        def search(low, high, nums):
-            mid = (low + high) // 2
-            if mid == 0: 
-                if nums[mid] < nums[mid+1]:
-                    return search(mid+1, high, nums)
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = [-2**32]+nums+[-2**32]
+        l,r = 0,len(nums)-1
+        while l <=r:
+            m = (l+r)//2
+			# we find the target:
+            if nums[m] > nums[m-1] and nums[m] > nums[m+1]:
+                return m -1
+            else:
+                if nums[m] <nums[m+1]:
+                    l = m + 1
                 else:
-                    return mid
-                
-            elif mid == len(nums)-1:
-                if nums[mid] < nums[mid-1]:
-                    # goto left
-                    return search(low, mid-1, nums)
-                else:
-                    return mid
-                
-            elif nums[mid] < nums[mid-1]:
-                # goto left
-                return search(low, mid-1, nums)
-            elif nums[mid] < nums[mid+1]:
-                # goto right
-                return search(mid+1, high, nums)
-            return mid
+                    r = m - 1
+        return -1
