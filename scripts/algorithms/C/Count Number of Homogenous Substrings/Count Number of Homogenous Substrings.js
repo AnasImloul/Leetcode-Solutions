01948bd51f42b90871743c12bef095c8450f03d9
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:33:14 +0100

    Runtime 71 ms (Top 100.0%) | Memory 45.0 MB (Top 58.33%)

--- scripts/algorithms/C/Count Number of Homogenous Substrings/Count Number of Homogenous Substrings.js
+++ scripts/algorithms/C/Count Number of Homogenous Substrings/Count Number of Homogenous Substrings.js
@@ -1,15 +1,18 @@
 var countHomogenous = function(s) {
-    const MOD = 1e9 + 7;
-    let res = 1;
-    let count = 1;
+    let mod = 1e9 + 7
+    let n = s.length
+    let j=0, res = 0
     
-    for (let i = 1; i < s.length; i++) {
-        const curr = s.charAt(i);
-        const prev = s.charAt(i - 1);
-        
-        count = curr == prev ? count + 1 : 1;
-        res = (res + count) % MOD;
+    for(let i=0; i<n; i++){
+        if(i>0 && s[i-1] != s[i]){
+            let x = i-j
+            res += x*(x+1) / 2
+            j = i
+        }
     }
     
-    return res;
+    let x = n-j
+    res += x*(x+1) / 2
+
+    return res%mod
 };
