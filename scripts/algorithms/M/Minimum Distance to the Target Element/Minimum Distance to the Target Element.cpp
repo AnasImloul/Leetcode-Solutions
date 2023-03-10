491068d42c12ac66bb92b530086e2834f3fc7ab7
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:18:42 +0100

    Runtime 3 ms (Top 93.27%) | Memory 9.0 MB (Top 79.81%)

--- scripts/algorithms/M/Minimum Distance to the Target Element/Minimum Distance to the Target Element.cpp
+++ scripts/algorithms/M/Minimum Distance to the Target Element/Minimum Distance to the Target Element.cpp
@@ -1,10 +1,14 @@
-class Solution {
+class Solution{
 public:
-    int getMinDistance(vector<int>& nums, int target, int start) {
-        int ans=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                ans=min(ans,abs(i-start));
+    int getMinDistance(vector<int>& nums, int target, int start){
+        int ans = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            int temp = 0;
+            if (nums[i] == target){
+                temp = abs(i - start);
+                if (temp < ans){
+                    ans = temp;
+                }
             }
         }
         return ans;
