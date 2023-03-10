3d7a408803e53f0e23432f022db830abe717924c
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:52:07 +0100

    Runtime 111 ms (Top 58.24%) | Memory 53.0 MB (Top 27.47%)

--- scripts/algorithms/N/Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.js
+++ scripts/algorithms/N/Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.js
@@ -1,21 +1,7 @@
-// Runtime: 151 ms (Top 59.59%) | Memory: 49.8 MB (Top 86.99%)
 var numberOfBeams = function(bank) {
-    let before = 0,after = 0,total = 0;
-    for(i=0;i<bank.length;i++){
-        for(j=0;j<bank[i].length;j++){
-            if(bank[i][j] === '1'){
-                after ++;
-            }
-        }
-        if(after > 0){
-            if(before === 0){
-                before = after;
-            }else{
-                total += before*after;
-                before = after;
-            }
-        }
-        after = 0;
-    }
-    return total
+    let totalBeams = 0;
+    const maximumSecurityDevicePerRow = bank.map(row => (row.match(/1/g) || []).length).filter(Boolean)
+    for (let index = 0; index < maximumSecurityDevicePerRow.length - 1; index++) 
+        totalBeams+= maximumSecurityDevicePerRow[index] * maximumSecurityDevicePerRow[index + 1];
+    return totalBeams;
 };
