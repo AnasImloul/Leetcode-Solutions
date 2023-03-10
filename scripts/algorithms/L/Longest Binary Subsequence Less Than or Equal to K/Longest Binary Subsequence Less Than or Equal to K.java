a2ab17d12b85481c106913691828361cc2e38c3d
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:48:13 +0100

    Runtime 3 ms (Top 46.37%) | Memory 41.0 MB (Top 30.17%)

--- scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
+++ scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
@@ -1,26 +1,25 @@
-// Runtime: 2 ms (Top 83.60%) | Memory: 42.5 MB (Top 46.76%)
 class Solution {
     public int longestSubsequence(String s, int k) {
-        int z=0;
-        //count zero
-        for(int i=0;i<s.length();i++)if(s.charAt(i)=='0')z++;
-
-        int num=0,base=1,len=0;
-        //take as many ones from right as possible until they do not make the num>k
-        for(int i=s.length()-1;i>=0;i--){
-
-            if(num+base>k)break;
-            if(s.charAt(i)=='1'){
-               num+=base;
+        int numOfZeros = 0;
+        int numOfOnes = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '0'){
+                numOfZeros++;
             }
-            else {
-            //remove already taken zeros from zeros count
-                z--;
-            }
-            base*=2;
-            len++;
         }
-
-        return len+z;
+        int sum = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            char ch = s.charAt(i);
+            if(ch == '1'){
+                double val = Math.pow(2, s.length() - i - 1);
+                sum += val;
+                if(sum <= k){
+                    numOfOnes++;
+                }
+            }
+        }
+        return numOfZeros + numOfOnes;
     }
 }
