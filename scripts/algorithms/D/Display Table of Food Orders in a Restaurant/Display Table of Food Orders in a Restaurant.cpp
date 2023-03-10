649b0c24173180dc7d2d4fb01ca26daed944c831
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 16:59:18 +0100

    Runtime 318 ms (Top 36.48%) | Memory 104.0 MB (Top 15.2%)

--- scripts/algorithms/D/Display Table of Food Orders in a Restaurant/Display Table of Food Orders in a Restaurant.cpp
+++ scripts/algorithms/D/Display Table of Food Orders in a Restaurant/Display Table of Food Orders in a Restaurant.cpp
@@ -1,60 +1,40 @@
-class Solution {
-public:
-    vector<vector<string>> displayTable(vector<vector<string>>& orders) {
-        
-        set<int> tables;
-        set<string> foodItems;
-        
-        for (const auto& order : orders)
-        {
-            tables.insert(stoi(order[1]));
-            foodItems.insert(order[2]);
-        }
-        
-        int pos = 1;
-        unordered_map<int,int> t_pos;
-        for (const auto& t : tables)
-        {
-            t_pos[t] = pos;
-            pos++;
-        }
-        
-        pos = 1;
-        unordered_map<string,int> f_pos;
-        for (const auto& f : foodItems)
-        {
-            f_pos[f] = pos;
-            pos++;
-        }
-        
-        vector<vector<string>> displayTable(tables.size()+1, vector<string>(foodItems.size()+1,"0"));
-        
-        displayTable[0][0] = "Table";
-        
-        auto it = tables.begin();
-        for (int x = 1; x < displayTable.size(); x++)
-        {
-            displayTable[x][0] = to_string(*it);
-            it++;
-        }
-        
-        auto it_ = foodItems.begin();
-        for (int y = 1; y < displayTable[0].size(); y++)
-        {
-            displayTable[0][y] = *it_;
-            it_++;
-        }
-        
-        for (const auto& order : orders)
-        {
-            int t = t_pos[stoi(order[1])];
-            int f = f_pos[order[2]];
-            
-            int dec = stoi(displayTable[t][f]);
-            dec++;
-            displayTable[t][f] = to_string(dec);
-        }
-        
-        return displayTable;
-    }
+class Solution {
+public:
+    vector<vector<string>> displayTable(vector<vector<string>>& orders) {
+
+    vector<vector<string>>ans;
+    map<int,map<string,int>>m;
+    set<string>s; //Sets are useful as they dont contain duplicates as well arranges the strings in order.
+    for(auto row:orders)
+    {
+        s.insert(row[2]);
+        m[stoi(row[1])][row[2]]++;
+    }
+
+    vector<string>dem;
+    dem.push_back("Table");
+    for(auto a:s)
+    {
+        dem.push_back(a);
+    }//For the  first row only
+
+    ans.push_back(dem);
+    for(auto it:m)
+    {
+        vector<string>row;
+        row.push_back(to_string(it.first));
+        auto dummy=it.second;
+        for(auto st:s)//we use set here as it has food names stored in asc order.
+        {
+            row.push_back(to_string(dummy[st]));//we access the number of orders.
+        }
+
+        ans.push_back(row);
+
+    }
+
+    return ans;
+
+
+    }
 };
