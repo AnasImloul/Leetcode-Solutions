388e5539ede27aaa720e1fa2aeec2eddafa7a724
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:25:19 +0100

    Runtime 134 ms (Top 76.28%) | Memory 17.0 MB (Top 6.64%)

--- scripts/algorithms/I/Insert into a Binary Search Tree/Insert into a Binary Search Tree.py
+++ scripts/algorithms/I/Insert into a Binary Search Tree/Insert into a Binary Search Tree.py
@@ -1,24 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root: return TreeNode(val)
-        def search(root):
-            if val>root.val:
-                if root.right:
-                    search(root.right)
-                else:
-                    root.right=TreeNode(val)
-                    return 
-            else:
-                if root.left:
-                    search(root.left)
-                else:
-                    root.left=TreeNode(val)
-                    return
-        search(root)
+    def insertIntoBST(self, root, val):
+        if not root:
+            return TreeNode(val)
+      
+        if val<root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+  
         return root
