9ec2b5d3b7c3c6a1d6994f57f8057d205a880092
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:49:06 +0100

    Runtime 0 ms (Top 100.0%) | Memory 6.0 MB (Top 24.63%)

--- scripts/algorithms/R/Reformat Date/Reformat Date.cpp
+++ scripts/algorithms/R/Reformat Date/Reformat Date.cpp
@@ -1,8 +1,45 @@
 class Solution {
-    inline static unordered_map<string, string> u{{"Jan", "01"}, {"Feb","02"}, {"Mar","03"}, {"Apr","04"}, {"May","05"}, {"Jun","06"}, {"Jul","07"}, {"Aug","08"}, {"Sep","09"}, {"Oct","10"}, {"Nov","11"}, {"Dec","12"}};
 public:
     string reformatDate(string date) {
-        if (!isdigit(date[1])) date.insert(0, "0");
-        return date.substr(9) + '-' + u[date.substr(5, 3)] + '-' + date.substr(0,2);
+        map<string,int>m;
+        m["Jan"] =1;
+        m["Feb"] =2;
+        m["Mar"] =3;
+        m["Apr"] =4;
+        m["May"] =5;
+        m["Jun"] =6;
+        m["Jul"] =7;
+        m["Aug"] =8;
+        m["Sep"] =9;
+        m["Oct"] =10;
+        m["Nov"] =11;
+        m["Dec"] =12;
+        string ans;
+        for(int i=date.length()-4; i<date.length(); i++){
+            ans+=date[i];
+        }
+        ans += "-";
+        string month;
+        for(int i=date.length()-8; i<=date.length()-6; i++){
+            month+=date[i];
+        }
+
+        int yes = m[month];
+
+        if(yes<=9){
+            ans += '0';
+        }
+        ans = ans + to_string(yes);
+        ans += "-";
+        int i = 0;
+        if(date[1]=='t' || date[1]=='s' || date[1]=='n' || date[1]=='r'){
+            ans+='0';
+            ans+=date[0];
+        }
+        else{
+            ans+=date[0];
+            ans+=date[1];
+        }
+        return ans;
     }
 };
