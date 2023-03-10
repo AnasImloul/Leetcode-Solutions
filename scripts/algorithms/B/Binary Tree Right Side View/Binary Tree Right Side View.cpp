f0899f852aa343ee5468730be9f3b57b8e81fdd5
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:07:27 +0100

    Runtime 11 ms (Top 5.8%) | Memory 11.0 MB (Top 74.1%)

--- scripts/algorithms/B/Binary Tree Right Side View/Binary Tree Right Side View.cpp
+++ scripts/algorithms/B/Binary Tree Right Side View/Binary Tree Right Side View.cpp
@@ -1,46 +1,39 @@
-// Runtime: 11 ms (Top 17.31%) | Memory: 12.1 MB (Top 44.85%)
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- * int val;
- * TreeNode *left;
- * TreeNode *right;
- * TreeNode() : val(0), left(nullptr), right(nullptr) {}
- * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    void solve(TreeNode* root, vector<int> &ans, int level)
+    {
+        // base case
+        if (root == NULL)
+        {
+            return ; 
+        }
+
+        // we entered a new level 
+        if (level == ans.size())
+        {
+            ans.push_back(root -> val) ; 
+        }
+
+        solve(root -> right, ans, level + 1) ; 
+        solve(root -> left, ans, level + 1 ) ; 
+    }
     vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans ; 
+        int level = 0 ; 
 
-        vector<int> ans;
-        if(root == NULL){
-            return ans;
-        }
-
-        queue<TreeNode* > q;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            for(int i = 0 ; i < n ; i++){
-
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if(i == n - 1){
-                    ans.push_back(curr -> val);
-                }
-
-                if(curr -> left != nullptr){
-                    q.push(curr -> left);
-                }
-
-                if(curr -> right != nullptr){
-                    q.push(curr -> right);
-                }
-            }
-        }
-        return ans;
+        solve(root, ans, level) ;
+        
+        return ans ; 
     }
 };
