7456f13e7c780dce3520b8ae7670da0a44715a11
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 00:25:56 +0100

    Runtime 0 ms (Top 100.0%) | Memory 6.0 MB (Top 58.1%)

--- scripts/algorithms/M/Maximum Score After Splitting a String/Maximum Score After Splitting a String.cpp
+++ scripts/algorithms/M/Maximum Score After Splitting a String/Maximum Score After Splitting a String.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     int maxScore(string s) {
-        vector<int>z(size(s)),o(size(s));
-        z[0]=s[0]=='0';
-        o[size(s)-1]=s[size(s)-1]=='1';
-        int ans=0;
-        for(int i=1,j=size(s)-2;i<s.size();++i,--j){
-            z[i]=z[i-1];
-            o[j]=o[j+1];
-            if(s[i]=='0')++z[i];
-            if(s[j]=='1')++o[j];
-        }
-        for(int i=0;i<s.size()-1;++i) ans=max(ans,z[i]+o[i+1]);
-        return ans;
-    }
-};
+        int ones = 0;
+        int res = -s.length();
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s[i] == '1') ones += 2;
+            if (i - ones > res) res = i - ones;
+        };
 
-class Solution {
-public:
-    int maxScore(string s) {
-        int o=0,z=0,ans=0;
-        for(auto&i:s)o+=i=='1';
-        for(int i=0;i<size(s)-1;++i){
-            s[i]=='0'?++z:--o;
-            ans=max(ans,o+z);
-        }
-        return ans;
+        ones >>= 1;
+        if (s[s.length() - 1] == '1') ones++;
+
+        return ones + res + 1;
     }
 };
