0a9c3ef153210fb5332f8d170df477c715366014
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:02:56 +0100

    Runtime 0 ms (Top 100.0%) | Memory 39.0 MB (Top 26.44%)

--- scripts/algorithms/H/Hamming Distance/Hamming Distance.java
+++ scripts/algorithms/H/Hamming Distance/Hamming Distance.java
@@ -1,14 +1,12 @@
-// Runtime: 1 ms (Top 45.03%) | Memory: 40.9 MB (Top 59.95%)
 class Solution {
     public int hammingDistance(int x, int y) {
-        int n = x^y;
-        int ans = 0;
-
-// Run loop till n does not contain any 1
-        while(n != 0){
-            ans = ans + (n &1);
-            n >>>= 1;
+        int ans=x^y;
+        int count=0;
+        while(ans>0){
+            count+=ans&1;
+            ans>>=1;
         }
-        return ans;
+        
+        return count;
     }
 }
