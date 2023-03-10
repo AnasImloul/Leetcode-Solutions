212cbd61a8bd75f819fbcf601a740f6599ab939c
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:00:41 +0100

    Runtime 47 ms (Top 18.98%) | Memory 9.0 MB (Top 53.51%)

--- scripts/algorithms/W/Word Ladder II/Word Ladder II.cpp
+++ scripts/algorithms/W/Word Ladder II/Word Ladder II.cpp
@@ -1,56 +1,101 @@
-// Runtime: 23 ms (Top 90.82%) | Memory: 9.9 MB (Top 40.33%)
+// BFS gives TLE if we store path while traversing because whenever we find a better visit time for a word, we have to clear/make a new path vector everytime. 
+// The idea is to first use BFS to search from beginWord to endWord and generate the word-to-children mapping at the same time. 
+// Then, use DFS (backtracking) to generate the transformation sequences according to the mapping. 
+// The reverse DFS allows us to only make the shortest paths, never having to clear a whole sequence when we encounter better result in BFS
+// No string operations are done, by dealing with indices instead.
+
+
+
 class Solution {
 public:
-    map<string,vector<string>> parent;
-    unordered_map<string,int> dist;
-    vector<vector<string>> ans;
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-         for(string node : wordList) dist[node] = INT_MAX , parent[node] = {};
-         parent[beginWord] = {"root"};
-         queue<string> q;
-         dist[beginWord] = 1;
-         q.push(beginWord);
-         while(!q.empty()) {
-             string par = q.front();
-             q.pop();
-             if(par==endWord) break;
-             for(int i=0; i<par.size(); i++) {
-                 string pref = "" , suff = "";
-                 if(i) pref = par.substr(0,i);
-                 if(i<par.size()-1) suff = par.substr(i+1);
-                 string child = pref+'$'+suff;
-                 int pos = pref.size();
-                 for(char c='a'; c<='z'; c++) {
-                       if(c!=par[i]) {
-                            child[pos]=c;
-                            if(dist.count(child)) {
-                                 if(dist[child] > 1 + dist[par]) {
-                                     dist[child] = 1 + dist[par];
-                                     parent[child].clear(); // remove all parents , a better parent exists (shorter path from src to child)
-                                     parent[child].push_back(par); // add that parent
-                                     q.push(child);
-                                 }else if(dist[child] == 1 + dist[par]) {
-                                     parent[child].push_back(par); // add all parents of the best current distance
-                                 }
-                            }
-                       }
-                 }
-             }
-         }
-         vector<string> path;
-         pathfinder(endWord,path);
-         for(int i=0; i<ans.size(); i++) reverse(ans[i].begin(),ans[i].end());
-         return ans;
+bool able(string s,string t){
+    int c=0;
+    for(int i=0;i<s.length();i++)
+        c+=(s[i]!=t[i]);
+    return c==1;
+}
+void bfs(vector<vector<int>> &g,vector<int> parent[],int n,int start,int end){
+    vector <int> dist(n,1005);
+    queue <int> q;
+    q.push(start);
+    parent[start]={-1};
+    dist[start]=0;
+    while(!q.empty()){
+        int x=q.front();
+        q.pop();
+        for(int u:g[x]){
+            if(dist[u]>dist[x]+1){
+                dist[u]=dist[x]+1;
+                q.push(u);
+                parent[u].clear();
+                parent[u].push_back(x);
+            }
+            else if(dist[u]==dist[x]+1)
+                parent[u].push_back(x);
+        }
     }
-    void pathfinder(string par,vector<string>&path) {
-         if(par=="root") {
-              ans.push_back(path); // no parent exists of the root node , add the path to answer
-              return;
-         }
-         for(string node : parent[par]) {
-              path.push_back(par);
-              pathfinder(node,path);
-              path.pop_back();
-         }
+}
+void shortestPaths(vector<vector<int>> &Paths, vector<int> &path, vector<int> parent[],int node){
+    if(node==-1){
+        // as parent of start was -1, we've completed the backtrack
+        Paths.push_back(path);
+        return ;
     }
-};
+    for(auto u:parent[node]){
+        path.push_back(u);
+        shortestPaths(Paths,path,parent,u);
+        path.pop_back();
+    }
+}
+vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+    // start and end are indices of beginWord and endWord
+    int n=wordList.size(),start=-1,end=-1;
+    vector<vector<string>> ANS;
+    for(int i=0;i<n;i++){
+        if(wordList[i]==beginWord)
+            start=i;
+        if(wordList[i]==endWord)
+            end=i;
+    }
+    
+    // if endWord doesn't exist, return empty list
+    if(end==-1)
+        return ANS;
+    
+    // if beginWord doesn't exist, add it in start of WordList
+    if(start==-1){
+        wordList.emplace(wordList.begin(),beginWord);
+        start=0;
+        end++;
+        n++;
+    }
+    // for each word, we're making adjency list of neighbour words (words that can be made with one letter change)
+    // Paths will store all the shortest paths (formed later by backtracking)
+    vector<vector<int>> g(n,vector<int>()),Paths;
+    
+    // storing possible parents for each word (to backtrack later), path is the current sequence (while backtracking)
+    vector<int> parent[n],path;
+    
+    // creating adjency list for each pair of words in the wordList (including beginword)
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(able(wordList[i],wordList[j])){
+                g[i].push_back(j);
+                g[j].push_back(i);
+            }
+    
+    bfs(g,parent,n,start,end); 
+    
+    // backtracking to make shortestpaths
+    shortestPaths(Paths,path,parent,end);
+    for(auto u:Paths){
+        vector <string> now;
+        for(int i=0;i<u.size()-1;i++)
+            now.push_back(wordList[u[i]]);
+        reverse(now.begin(),now.end());
+        now.push_back(wordList[end]);
+        ANS.push_back(now);
+    }
+    return ANS;
+}
+}; 
