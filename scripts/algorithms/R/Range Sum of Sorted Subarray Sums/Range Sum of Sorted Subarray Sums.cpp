2897a6db1b8500e8f24628222da1ab1ee112be32
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:15:32 +0100

    Runtime 146 ms (Top 81.3%) | Memory 15.0 MB (Top 86.95%)

--- scripts/algorithms/R/Range Sum of Sorted Subarray Sums/Range Sum of Sorted Subarray Sums.cpp
+++ scripts/algorithms/R/Range Sum of Sorted Subarray Sums/Range Sum of Sorted Subarray Sums.cpp
@@ -1,21 +1,27 @@
-// Runtime: 189 ms (Top 50.81%) | Memory: 23.9 MB (Top 34.25%)
-class Solution {
+class Solution 
+{
 public:
-    int mod = 1e9 + 7;
-    int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> ans;
-        for(int i = 0; i < n; i++){
-            int psum = 0;
-            for(int j = i; j < n; j++){
-                psum += nums[j];
-                ans.push_back(psum);
+    int rangeSum(vector<int>& nums, int n, int left, int right) 
+    {
+        const int m= 1e9+7; // To return ans % m
+        int ans=0;  // Final Answer
+        int k=1;  // For 1 based indexing
+        int size= (n*(n+1))/2;  // We can form n(n+1)/2 subarrays for an array of size n
+        vector<int> subsum(size+1); 
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum+=nums[j];     // Sum of the subarray
+                subsum[k++]=sum;   // Inserting the prefix sum at the index
             }
         }
-        sort(ans.begin(), ans.end());
-        int res_sum = 0;
-        for(int i = left - 1; i < right; i++){
-            res_sum = (res_sum + ans[i]) % mod;
+        sort(subsum.begin(),subsum.end()); // Sorting the array
+        for(int i=left; i<=right; i++)
+        {
+            ans=(ans+subsum[i])%m;   // ans modulo 10^9 +7
         }
-        return res_sum;
+        return ans;
     }
 };
