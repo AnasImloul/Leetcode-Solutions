58763e5fc51bf7fe1c9bc67b5e71926f16211032
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:47:12 +0100

    Runtime 6 ms (Top 42.86%) | Memory 42.0 MB (Top 17.74%)

--- scripts/algorithms/R/Remove Covered Intervals/Remove Covered Intervals.java
+++ scripts/algorithms/R/Remove Covered Intervals/Remove Covered Intervals.java
@@ -1,21 +1,17 @@
-// Runtime: 4 ms (Top 98.71%) | Memory: 45.6 MB (Top 85.46%)
 class Solution {
-    public int removeCoveredIntervals(int[][] intervals)
-    {
-        int count=0;
-
-        for(int i=0;i<intervals.length;i++)
-        {
-            for(int j=0;j<intervals.length;j++)
-            {
-                if(intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1] && i!=j)
-                {
-                    count++;
-                    break;
-                }
-
+    public int removeCoveredIntervals(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (i1,i2) -> (i1[0]==i2[0]?i2[1]-i1[1]:i1[0]-i2[0]));
+        int c = intervals[0][0], d = intervals[0][1];
+        int ans = intervals.length;
+        for(int i=1;i<intervals.length;i++){
+            int a = intervals[i][0], b = intervals[i][1];
+            if(c<=a && b<=d) ans--; 
+            else {
+                c = a;
+                d = b;
             }
         }
-        return intervals.length-count;
+        return ans;
     }
 }
