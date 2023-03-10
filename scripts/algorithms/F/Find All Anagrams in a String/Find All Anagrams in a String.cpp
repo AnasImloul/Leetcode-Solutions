15dec8ad2942935c66d8b50a025be0a979c0fe22
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:03:38 +0100

    Runtime 8 ms (Top 94.25%) | Memory 8.0 MB (Top 41.97%)

--- scripts/algorithms/F/Find All Anagrams in a String/Find All Anagrams in a String.cpp
+++ scripts/algorithms/F/Find All Anagrams in a String/Find All Anagrams in a String.cpp
@@ -1,41 +1,39 @@
+//easy to understand
 class Solution {
 public:
-    bool areAnagram(string str1, string str2)
-    {
-        sort(str1.begin(), str1.end());
-        for (int i=0;i<str1.size();i++)
-        {
-            if (str1[i]!=str2[i])
-            {
+    bool allZeros(vector<int> &count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) 
                 return false;
-            }
         }
         return true;
     }
     vector<int> findAnagrams(string s, string p) {
-        vector<string> v;
+        string s1 = p, s2 = s;
+        int n = s1.length(); 
+        int m = s2.length();
+        if (n > m) 
+            return {};
+
         vector<int> ans;
-        string k="";
-        for(int i=0;i<s.size();i++)
-        {
-            for(int j=i;j<i+p.size() && j<s.size();j++)
-            {
-                k+=s[j];
-            }
-            if(k.size()==p.size())
-            {
-                v.push_back(k);   
-            }
-            k="";
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[s1[i] - 'a']++;
+            count[s2[i] - 'a']--;
         }
-        sort(p.begin(),p.end());
-        for(int i=0;i<v.size();i++)
-        {
-            if(areAnagram(v[i],p))
-            {
-                ans.push_back(i);
-            }
+        //it will check for s1 = abcd, s2 = cdab
+        if (allZeros(count)) {
+            ans.push_back(0);
         }
+
+        for (int i = n; i < m; i++) {
+            count[s2[i] - 'a']--;
+            count[s2[i - n] - 'a']++;
+            if (allZeros(count)) 
+                ans.push_back(i-n+1);
+        }
+
         return ans;
     }
 };
