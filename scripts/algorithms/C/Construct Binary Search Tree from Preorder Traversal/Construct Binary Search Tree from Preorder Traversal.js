cde9b1168556223c55940ffd9cc497cd4665da4c
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:55:00 +0100

    Runtime 68 ms (Top 63.8%) | Memory 44.0 MB (Top 13.85%)

--- scripts/algorithms/C/Construct Binary Search Tree from Preorder Traversal/Construct Binary Search Tree from Preorder Traversal.js
+++ scripts/algorithms/C/Construct Binary Search Tree from Preorder Traversal/Construct Binary Search Tree from Preorder Traversal.js
@@ -1,43 +1,15 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {number[]} preorder
- * @return {TreeNode}
- */
-
-function helper(root, val) {
-    var current = root;
-    var newNode = new TreeNode(val);
-
-     while (true) {
-        if (val === current.val) return undefined;
-        if (val < current.val) {
-          if (current.left === null) {
-            current.left = newNode;
-            return val;
-          } else {
-            current = current.left;
-          }
-        } else if (val > current.val) {
-          if (current.right === null) {
-            current.right = newNode;
-            return val;
-          } else {
-            current = current.right;
-          }
-        };
-      };
-}
-
 var bstFromPreorder = function(preorder) {
-    let root = new TreeNode(preorder[0]);
-    preorder.shift()
-    preorder.forEach(v => helper(root, v))
-    return root;
+  let head = new TreeNode(preorder[0]);
+  for (let i = 1, curr; i<preorder.length; i++) {
+     curr = head;
+     while (1) {
+        if (preorder[i]>curr.val) 
+          if (curr.right !=null) { curr = curr.right; }
+          else { curr.right = new TreeNode(preorder[i]); break; }
+        else
+          if (curr.left !=null) { curr = curr.left; }
+          else { curr.left = new TreeNode(preorder[i]); break; }
+     } 
+  } 
+  return head;  
 };
