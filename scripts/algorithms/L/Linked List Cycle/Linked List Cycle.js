8321e7249c608fdf6dd8fc434ee45cd5f87e8e66
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:39:46 +0100

    Runtime 75 ms (Top 74.0%) | Memory 45.0 MB (Top 17.15%)

--- scripts/algorithms/L/Linked List Cycle/Linked List Cycle.js
+++ scripts/algorithms/L/Linked List Cycle/Linked List Cycle.js
@@ -1,14 +1,28 @@
-var hasCycle = function(head) {
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function hasCycle(head){
     
-    let slow = fast = head;
-    
-    while(fast && fast.next){
-        
-        slow = slow.next;
-        fast = fast.next.next;
-        
-         if(fast == slow)
-            return true;
-    }
-    return false;
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+   //head and first pointers value in 
+  //each iteration with head=[3,2,0,-4], pos = 1
+  //1st iteration: 3 3
+  //2nd iteration: 2 0
+  //3rd iteration: 0 2
+  //final iteration: -4 -4
+  return false;
 };
