30c5cf2115543a08ddb51a7c688213ffb3b1c3a5
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:21:26 +0100

    Runtime 68 ms (Top 48.36%) | Memory 18.0 MB (Top 99.59%)

--- scripts/algorithms/M/Move Pieces to Obtain a String/Move Pieces to Obtain a String.cpp
+++ scripts/algorithms/M/Move Pieces to Obtain a String/Move Pieces to Obtain a String.cpp
@@ -1,42 +1,47 @@
+/*
+    https://leetcode.com/problems/move-pieces-to-obtain-a-string/
+    
+    Thing to note here is the direction in which L and R can move. '_' can
+    help them move. We just need to check that each L and R in start is in
+    such a position from where it can match the corresponding L/R in target.
+    
+    Since L moves left, the position of start L should be >= target L
+    Since R moves right, position of start R should be <= target R
+    TC: O(m + n)
+    SC: O(1)
+*/
 class Solution {
 public:
-    
-
     bool canChange(string start, string target) {
+        int m = start.size(), n = target.size();
+        int i = 0, j = 0;
         
-        
-        vector<pair<char,int >> v;
-        for(int i =0 ;i  < start.size();i++)
-        {
-            if(start[i] == 'L' || start[i] == 'R')
-            {
-                v.push_back({start[i],i});
-            }
+        while(i < m && j < n) {
+            // skip all the _ chars
+            while(i < m && start[i] == '_')
+                ++i;
+            while(j < n && target[j] == '_')
+                ++j;
+            // Either or both have reached the end
+            if(i == m || j == n)
+                break;
+            // if the current chars are diff, not possible
+            if(start[i] != target[j])
+                return false;
+            // L can only move left, if it is already behind it can't reach target L ahead
+            if(start[i] == 'L' && i < j)
+                return false;
+            // R can only move right, if it alraedy ahead, it can't reach target R behind
+            if(start[i] == 'R' && i > j)
+                return false;
+            ++i, ++j;
         }
-        
-        int i = 0;
-        for(int j = 0;j < target.size();j++)
-        {
-            if(target[j] == 'L' || target[j] == 'R') // lets find its corresponding Element in start
-            {
-                if(i >= v.size() || v[i].first != target[j]) // Not found or Different found
-                {
-                    return false;
-                }else 
-                {
-                   
-                    if(v[i].first == 'L' && v[i].second < j) // Condition a unsatisfied
-                        return false;
-                     if(v[i].first == 'R' && v[i].second > j) // Condition b unsatisfied
-                        return false;
-                     else 
-                        i++; // all good we move ahead in start;
-                }
-               
-                
-            }
-        }
-        
-        return i == v.size();// all elements accounted for in start
+        // In case some _ are left, skip them
+        while(i < m && start[i] == '_')
+            ++i;
+        while(j < n && target[j] == '_')
+            ++j;
+        // Both the strings should not be pointing to any L or R
+        return (i == m && j == n);
     }
 };
