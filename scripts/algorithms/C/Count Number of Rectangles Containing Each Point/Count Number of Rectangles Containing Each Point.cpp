168f259cb0486c3c786ba23359151a80ca70d127
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 00:32:28 +0100

    Runtime 825 ms (Top 32.71%) | Memory 95.0 MB (Top 18.52%)

--- scripts/algorithms/C/Count Number of Rectangles Containing Each Point/Count Number of Rectangles Containing Each Point.cpp
+++ scripts/algorithms/C/Count Number of Rectangles Containing Each Point/Count Number of Rectangles Containing Each Point.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
-        vector<vector<int>> umap(101);
-        for (auto &r: rectangles) {
-            umap[r[1]].push_back(r[0]);
-        }
         
-        for (int i = 0; i < 101; i++) {
-            sort(begin(umap[i]), end(umap[i]));
-        }
+        int i, count, ind, x, y, n = rectangles.size();
         
-        vector<int> res;
-        for (auto &p: points) {
-            int sum = 0;
-            for (int i = p[1]; i < 101; i++) {
-                auto lb = lower_bound(begin(umap[i]), end(umap[i]), p[0]);
-                sum += end(umap[i]) - lb;
+        vector<int> ans;
+        vector<vector<int>> heights(101);
+        
+        for(auto rect : rectangles)
+            heights[rect[1]].push_back(rect[0]);
+        
+        for(i=0;i<101;i++)
+            sort(heights[i].begin(), heights[i].end());
+        
+        for(auto point : points)
+        {
+            count = 0;
+            x = point[0];
+            y = point[1];
+            for(i=y;i<101;i++)
+            {
+                ind = lower_bound(heights[i].begin(), heights[i].end(), x) - heights[i].begin();
+                count += (heights[i].size() - ind);
             }
-            res.push_back(sum);
+            ans.push_back(count);
         }
-        return res;
+        return ans;
     }
 };
