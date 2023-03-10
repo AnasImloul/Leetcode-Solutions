306b4df6ea18cc92f8e6f4df7849cc0c3cf9eee5
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:59:24 +0100

    Runtime 0 ms (Top 100.0%) | Memory 39.0 MB (Top 80.93%)

--- scripts/algorithms/M/Minimum Moves to Reach Target Score/Minimum Moves to Reach Target Score.java
+++ scripts/algorithms/M/Minimum Moves to Reach Target Score/Minimum Moves to Reach Target Score.java
@@ -1,19 +1,17 @@
 class Solution {
     public int minMoves(int target, int maxDoubles) {
-        int steps = 0;
-        while(target > 1) {
-            if (target % 2 == 0 && maxDoubles > 0) {
-                target /= 2;
-                maxDoubles--;
-            } else if (maxDoubles == 0) {
-                steps += target - 1;
-                break;
-            } else {
-                target--;
+        int ans = 0;
+        for(int i=0;i<maxDoubles;i++){
+            if(target==1)break;
+            
+            if(target%2==0){
+                ans+=1;
+                target=(target)/2;
+            }else{
+                 ans+=2;
+                target=(target-1)/2;
             }
-            steps++;
         }
-        
-        return steps;
+        return ans+target-1;
     }
 }
