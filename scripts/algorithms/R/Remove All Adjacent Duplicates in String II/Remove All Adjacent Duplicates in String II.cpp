d9d0f428f79e18f302fdaa940beee1004a4f1857
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:41:19 +0100

    Runtime 15 ms (Top 88.55%) | Memory 10.0 MB (Top 82.33%)

--- scripts/algorithms/R/Remove All Adjacent Duplicates in String II/Remove All Adjacent Duplicates in String II.cpp
+++ scripts/algorithms/R/Remove All Adjacent Duplicates in String II/Remove All Adjacent Duplicates in String II.cpp
@@ -1,33 +1,66 @@
-// Runtime: 78 ms (Top 20.22%) | Memory: 10.8 MB (Top 48.34%)
+#define pp pair< int , char > 
+
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack <char> Stack;
-        stack <pair <char, int>> freqMap;
-        for (char &ch: s) {
-            if (!Stack.empty() && Stack.top() == ch) {
-                int freq = freqMap.top().second;
-                if (freq + 1 == k) {
-                    while (freq--) {
-                        Stack.pop();
-                    }
-                    freqMap.pop();
-                } else {
-                    freqMap.pop();
-                    freqMap.push({ch, freq + 1});
-                    Stack.push(ch);
-                }
-            } else {
-                Stack.push(ch);
-                freqMap.push({ch, 1});
+        
+        int n=s.size();
+        
+        stack< pp > stk;
+        
+        int i=0;
+        
+        while(i<n)
+        {
+            int count=1;
+            
+            char ch=s[i];
+            
+            while((i+1)<n && s[i]==s[i+1])
+            {
+                i++;
+                count++;
             }
+         
+            int c=0;
+            
+            if(!stk.empty() && stk.top().second==ch)
+            {
+                c+=stk.top().first;
+                stk.pop();
+            }
+            
+            count+=c;
+            
+            count=count%k;
+            
+            if(count!=0)
+            {
+                stk.push({count , ch});
+            }
+            
+            i++;
+            
+            
         }
-        string cleanedString = "";
-        while (!Stack.empty()) {
-            cleanedString += Stack.top();
-            Stack.pop();
+        
+        string str="";
+        
+        while(!stk.empty())
+        {
+            int count=stk.top().first;
+            
+            while(count--)
+            {
+                str.push_back(stk.top().second);
+            }
+            
+            stk.pop();
         }
-        reverse(cleanedString.begin(), cleanedString.end());
-        return cleanedString;
+        
+        reverse(str.begin() , str.end());
+        
+        return str;
+        
     }
 };
