5da715ef5081df5841660aa73c60e12855755449
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:48:16 +0100

    Runtime 7 ms (Top 99.24%) | Memory 26.0 MB (Top 78.82%)

--- scripts/algorithms/C/Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ scripts/algorithms/C/Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -1,31 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    typedef vector<int>::iterator vecIt;
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder, vecIt start, vecIt end)
-    {
-        if (start >= end)
-            return NULL;
-        vecIt rootIt = postorder.end() - 1;
-        int rootVal = *rootIt;
-        postorder.erase(rootIt);
-        TreeNode* newNode = new TreeNode(rootVal);
-        vecIt midIt = find(start, end, rootVal);
-        newNode->right = buildTree(inorder, postorder, midIt + 1, end);
-        newNode->left = buildTree(inorder, postorder, start, midIt);
-        return (newNode);
+    TreeNode* buildTree(vector<int>& ino, vector<int>& post) {
+    int i1 = post.size()-1;
+        return solve(i1,ino,post,0,ino.size()-1);
     }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return (buildTree(inorder, postorder, inorder.begin(), inorder.end()));
+    TreeNode* solve(int &i,vector<int> &in,vector<int> &post,int l,int r){
+        if(l>r)return NULL;
+        int x = r;
+        while(post[i] != in[x]){
+            x--;
+        }
+        i--;
+        // cout<<in[x]<<" ";
+        TreeNode* root = new TreeNode(in[x]);
+        root->right = solve(i,in,post,x+1,r);
+        root->left = solve(i,in,post,l,x-1);
+        return root;
     }
 };
