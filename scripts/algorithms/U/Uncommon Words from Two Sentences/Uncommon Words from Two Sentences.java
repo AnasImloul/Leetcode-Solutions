1041561e15d99d8e929a182e4765a773683befdd
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:45:11 +0100

    Runtime 2 ms (Top 97.2%) | Memory 42.0 MB (Top 36.87%)

--- scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.java
+++ scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.java
@@ -1,20 +1,23 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-       HashMap<String,Integer> hmap=new HashMap<>();
-       for(String s:s1.split(" "))
-            hmap.put(s,hmap.getOrDefault(s,0)+1);
-        for(String s:s2.split(" "))
-            hmap.put(s,hmap.getOrDefault(s,0)+1);
-        StringBuilder sb = new StringBuilder();
-        for(String s:hmap.keySet())
-        {
-            if(hmap.get(s)==1)
-                sb.append(s+" ");
+        List<String> list=new LinkedList<>();
+        Map<String, Integer> map=new HashMap<>();
+        String[] arr1=s1.split(" ");
+        String[] arr2=s2.split(" ");
+        for(int i=0;i<arr1.length;i++){
+            map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);
         }
-        
-        if(sb.toString().equals(""))
-            return new String[0];
-        
-        return sb.toString().trim().split(" ");
+        for(int i=0;i<arr2.length;i++){
+            map.put(arr2[i],map.getOrDefault(arr2[i],0)+1);
+        }
+        for(Map.Entry<String, Integer> entry:map.entrySet()){
+            if(entry.getValue()==1)
+                list.add(entry.getKey());
+        }
+        String[] res=new String[list.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=list.get(i);
+        }
+        return res;
     }
 }
