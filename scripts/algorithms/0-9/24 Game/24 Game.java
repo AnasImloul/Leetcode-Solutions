f256eae4019d1f6d04e42c47c22b30bed0ed75be
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 18:03:19 +0100

    Runtime 1 ms (Top 99.0%) | Memory 39.0 MB (Top 97.99%)

--- scripts/algorithms/0-9/24 Game/24 Game.java
+++ scripts/algorithms/0-9/24 Game/24 Game.java
@@ -1,49 +1,36 @@
+// 0 ms. 100%
 class Solution {
-    public boolean judgePoint24(int[] cards) {
-        List<Double> in=new ArrayList<>();
-        
-        for(int i : cards){
-            in.add((double)i);
-        }
-        
-        return dfs(in);
-    }
-    
-    
-    public boolean dfs(List<Double> cards){
-        if(cards.size()==1){
-            if(Math.abs(cards.get(0)-24.0)<0.001) return true;
-            return false;
-        }
-        
-        for(int i=0;i<cards.size();i++){
-            for(int j=i+1;j<cards.size();j++){
-                List<Double> possibleCombinations=generatePossibleResults(cards.get(i),cards.get(j));
-                
-                for(double c:possibleCombinations){
-                    List<Double> next=new ArrayList<>();
-                    next.add(c);
-                    
-                    for(int k=0;k<cards.size();k++){
-                        if(k!=i && k!=j) next.add(cards.get(k));
-                    }                    
-                    if(dfs(next)) return true;
-                } 
-                
+    private static final double EPS = 1e-6;
+    private boolean backtrack(double[] A, int n) {
+        if(n == 1) return Math.abs(A[0] - 24) < EPS;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                double a = A[i], b = A[j];
+                A[j] = A[n-1];
+                A[i] = a + b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a - b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = b - a;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a * b;
+                if(backtrack(A, n - 1)) return true;
+                if(Math.abs(b) > EPS) {
+                    A[i] = a / b;
+                    if(backtrack(A, n - 1)) return true;
+                }
+                if(Math.abs(a) > EPS) {
+                    A[i] = b / a;
+                    if(backtrack(A, n - 1)) return true;
+                }
+                A[i] = a; A[j] = b;
             }
         }
         return false;
-    
     }
-
-    private List<Double> generatePossibleResults(double a, double b) {
-        List<Double> res = new ArrayList<>();
-        res.add(a + b);
-        res.add(a - b);
-        res.add(b - a);
-        res.add(a * b);
-        res.add(a / b);
-        res.add(b / a);
-        return res;
+    public boolean judgePoint24(int[] nums) {
+        double[] A = new double[nums.length];
+        for(int i = 0; i < nums.length; i++) A[i] = nums[i];
+        return backtrack(A, A.length);
     }
 }
