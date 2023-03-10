988b91b05af4e382cd7ee7c29ca07b28ec265889
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 16:41:46 +0100

    Runtime 32 ms (Top 76.88%) | Memory 58.0 MB (Top 28.77%)

--- scripts/algorithms/R/Recover Binary Search Tree/Recover Binary Search Tree.cpp
+++ scripts/algorithms/R/Recover Binary Search Tree/Recover Binary Search Tree.cpp
@@ -1,41 +1,23 @@
-// Runtime: 37 ms (Top 87.11%) | Memory: 57.7 MB (Top 98.52%)
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- * int val;
- * TreeNode *left;
- * TreeNode *right;
- * TreeNode() : val(0), left(nullptr), right(nullptr) {}
- * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void fixBST(TreeNode* root, TreeNode* & prev ,TreeNode* & first ,TreeNode* &second){
-        if(root == NULL){
-            return ;
-        }
-// inorder traversal
-        fixBST(root->left ,prev,first,second);
-
-        if(prev != NULL and prev->val > root->val){
-// this is violtaing
-            if(first == NULL){
-                first = prev;
-            }
-            second = root;
-        }
-        prev = root;
-
-        fixBST(root->right,prev,first,second);
+    vector<int> v;
+    int i=0;
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
+    void check(TreeNode* root){
+        if(!root) return;
+        check(root->left);
+        if(v[i]!=root->val) swap(v[i],root->val);
+        i++;
+        check(root->right);
     }
     void recoverTree(TreeNode* root) {
-        TreeNode* first = NULL;
-        TreeNode* second = NULL;
-        TreeNode* prev = NULL;
-        fixBST(root,prev,first,second);
-        swap(first->val , second->val);
+        inorder(root);
+        sort(v.begin(),v.end());
+        check(root);
     }
 };
