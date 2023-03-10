2a189ea41685229e0a6bb266a132bbc7c6230cbe
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:28:28 +0100

    Runtime 14 ms (Top 91.59%) | Memory 8.0 MB (Top 62.76%)

--- scripts/algorithms/N/Number of Good Ways to Split a String/Number of Good Ways to Split a String.cpp
+++ scripts/algorithms/N/Number of Good Ways to Split a String/Number of Good Ways to Split a String.cpp
@@ -1,42 +1,23 @@
-// Runtime: 24 ms (Top 88.67%) | Memory: 17.6 MB (Top 8.95%)
 class Solution {
 public:
     int numSplits(string s) {
-        int dist = 0;
-        vector<int> v1;
-        vector<int> v2;
-        unordered_set<char> se;
-        unordered_set<char> b;
-
-        for(int i=0; i<s.size(); i++)
-        {
-            if(se.find(s[i]) == se.end())
-            {
-                se.insert(s[i]);
-                dist++;
-            }
-            v1.push_back(dist);
+        int n=s.size();
+        vector<int>left(26,0);
+        vector<int>right(26,0);
+        int left_count=0,right_count=0;
+        int splits=0;
+        for(auto &it:s){
+            right[it-'a']++;
+            if(right[it-'a']==1)right_count++;
         }
-
-        dist = 0;
-
-        for(int i=s.size()-1; i>=0; i--)
-        {
-            if(b.find(s[i]) == b.end())
-            {
-                b.insert(s[i]);
-                dist++;
-            }
-            v2.push_back(dist);
+        for(auto &it:s){
+            left[it-'a']++;
+            right[it-'a']--;
+            if(left[it-'a']==1)left_count++;
+            if(right[it-'a']==0)right_count--;
+            if(left_count==right_count)
+                splits++;
         }
-
-        dist = 0;
-
-        for(int i=0; i<v2.size()-1; i++)
-        {
-            if(v1[i] == v2[s.size()-i-2]) dist++;
-        }
-
-        return dist;
+        return splits;
     }
 };
