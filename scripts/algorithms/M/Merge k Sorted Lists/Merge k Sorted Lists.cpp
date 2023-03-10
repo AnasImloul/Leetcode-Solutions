a0dd408c942a1127aec79ae0e9be3118a2987b55
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:20:36 +0100

    Runtime 26 ms (Top 64.39%) | Memory 14.0 MB (Top 16.21%)

--- scripts/algorithms/M/Merge k Sorted Lists/Merge k Sorted Lists.cpp
+++ scripts/algorithms/M/Merge k Sorted Lists/Merge k Sorted Lists.cpp
@@ -1,49 +1,28 @@
-// To compare the elements of node
-class Comparator{
-    
-public:
-    bool operator()(ListNode* n1, ListNode* n2){
-        return n2->val < n1->val;
-    }
-};
-
 class Solution {
-public: 
-    
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        int k=lists.size();
-        
-        if(k==0) return NULL;
-        
-        list<ListNode*> ll;
-                
-        priority_queue<ListNode*, vector<ListNode*>, Comparator> minheap;
-        
-//         push first element of every subarray in the minheap
-        for(int i=0; i<k; i++){
-            if(lists[i]) minheap.push(lists[i]);
-        }        
-        
-//          push remaining elements 
-        while(!minheap.empty()){
-//             take out top element
-            ListNode* n =minheap.top(); 
-            minheap.pop();
-//             push the next element of its same array if exists
-            if(n && n->next) minheap.push(n->next);
-            
-//             minimum elements of the remaining will always be at top
-            if(!ll.empty()){
-                ll.back()->next=n;
-                ll.push_back(n);
-            }
-            else{
-                ll.push_back(n);
-            }
+public:
+    struct compare
+    {
+        bool operator()(ListNode* &a,ListNode* &b)
+        {
+            return a->val>b->val;
         }
-        
-        return ll.front();
-        
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,compare>minh;
+        for(int i=0;i<lists.size();i++)
+        {
+           if(lists[i]!=NULL) minh.push(lists[i]);
+        }
+        ListNode* head=new ListNode(0);
+        ListNode* temp=head;
+        while(minh.size()>0)
+        {
+            ListNode* p=minh.top();
+            minh.pop();
+            temp->next=new ListNode(p->val);
+            temp=temp->next;
+            if(p->next!=NULL) minh.push(p->next);
+        }
+        return head->next;
     }
 };
