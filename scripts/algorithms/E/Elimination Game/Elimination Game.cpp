22a0572545a9130d7ccc425aad26a154be62fbd2
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:02:45 +0100

    Runtime 8 ms (Top 26.77%) | Memory 5.0 MB (Top 57.38%)

--- scripts/algorithms/E/Elimination Game/Elimination Game.cpp
+++ scripts/algorithms/E/Elimination Game/Elimination Game.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        int beg = 1, d = 1, len = n;
-        bool fromleft = true;
-
-        while(len != 1 ) {
-            if(fromleft || (len&1))
-                beg += d;
-            len >>= 1;
-            d <<= 1;
-            fromleft = !fromleft;
+        bool left=true;
+        int head=1,step=1;//step is the difference between adjacent elements.
+        while(n>1){
+            if(left || (n&1)){//(n&1)->odd
+                head=head+step;
+            }
+            step=step*2;
+            n=n/2;
+            left=!left;
         }
-
-        return beg;
+        return head;
     }
 };
