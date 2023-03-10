a2567f9bf9e233cac5c40402e8e128c7601cffcd
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:52:43 +0100

    Runtime 75 ms (Top 93.85%) | Memory 46.0 MB (Top 38.46%)

--- scripts/algorithms/C/Count Square Submatrices with All Ones/Count Square Submatrices with All Ones.js
+++ scripts/algorithms/C/Count Square Submatrices with All Ones/Count Square Submatrices with All Ones.js
@@ -1,23 +1,17 @@
-// Runtime: 150 ms (Top 40.00%) | Memory: 45.7 MB (Top 65.81%)
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
 var countSquares = function(matrix) {
-    let m = matrix.length;
-    let n = matrix[0].length;
-
-    while(matrix){
-        let preCount = 0;
-        let count = 0;
-        for(let i = 0; i < m; i++){
-            for(let j = 0; j < n; j++){
-                preCount += matrix[i][j];
-                if(i + 1 < m && j + 1 < n){
-                    if(matrix[i][j] != 0
-                       &&matrix[i][j] == matrix[i + 1][j]
-                       && matrix[i][j] == matrix[i][j + 1]
-                       && matrix[i][j] == matrix[i + 1][j + 1]) matrix[i][j]++;
-                }
-                count += matrix[i][j];
-            }
-        }
-        if(preCount == count) return count;
+  let count = 0;
+  for (let i = 0; i < matrix.length; ++i) {
+    for (let j = 0; j < matrix[0].length; ++j) {
+      if (matrix[i][j] === 0) continue;
+      if (i > 0 && j > 0) {
+        matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]);
+      }
+      count += matrix[i][j];
     }
+  }
+  return count; 
 };
