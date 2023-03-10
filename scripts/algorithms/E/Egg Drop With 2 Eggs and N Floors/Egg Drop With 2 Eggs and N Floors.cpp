24f278968494b0073e69a528af86c7251a84ab04
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-10 17:37:37 +0100

    Runtime 27 ms (Top 52.6%) | Memory 9.0 MB (Top 7.66%)

--- scripts/algorithms/E/Egg Drop With 2 Eggs and N Floors/Egg Drop With 2 Eggs and N Floors.cpp
+++ scripts/algorithms/E/Egg Drop With 2 Eggs and N Floors/Egg Drop With 2 Eggs and N Floors.cpp
@@ -1,10 +1,30 @@
-int dp[1001] = {};
+int dp[1001][1001];
 class Solution {
 public:
-int twoEggDrop(int n) {
-	if (dp[n] == 0)
-		for (int i = 1; i <= n; ++i)
-			dp[n] = min(dp[n] == 0 ? n : dp[n], 1 + max(i - 1, twoEggDrop(n - i)));
-	return dp[n];
-}
+    int solve(int e, int f){
+        if(f == 0 || f == 1){
+            return f;
+        }
+        if(e == 1){
+            return f;
+        }
+        if(dp[e][f] != -1) return dp[e][f];
+        int mn = INT_MAX;
+        int left = 1, right = f;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            int left_result = solve(e-1,mid-1);
+            int right_result = solve(e,f-mid);
+            mn = min(mn,1+max(left_result, right_result));
+            if(left_result<right_result)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return dp[e][f] = mn;
+    }
+    int twoEggDrop(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(2,n);
+    }
 };
