9415935c420701e60a79600db4bc03fbacec17ca
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 01:40:25 +0100

    Runtime 63 ms (Top 78.86%) | Memory 102.0 MB (Top 30.8%)

--- scripts/algorithms/M/Most Beautiful Item for Each Query/Most Beautiful Item for Each Query.java
+++ scripts/algorithms/M/Most Beautiful Item for Each Query/Most Beautiful Item for Each Query.java
@@ -1,24 +1,35 @@
-// Runtime: 65 ms (Top 93.33%) | Memory: 101.6 MB (Top 93.33%)
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        int n = queries.length, m = items.length;
-        Arrays.sort(items, Comparator.comparingInt(o -> o[0]));
-        int[] ans = new int[n];
-
-        for (int i = 0; i < m; i++)
-            items[i][1] = Math.max(items[i][1], items[i > 0? i - 1 : 0][1]);
-
-        int j = 0;
-        for (int q : queries){
-            int lo = -1, hi = m - 1; //pad lo = -1 to mark no result
-            while(lo < hi){
-                int mid = lo + (hi - lo) / 2 + 1;
-                if (q >= items[mid][0]) lo = mid;
-                else hi = mid - 1;
-            }
-            ans[j++] = lo == -1? 0 : items[lo][1];
+        int[] ans = new int[queries.length];
+        Arrays.sort(items, (a, b) -> (a[0] - b[0]));
+        int maxBeautySoFar = Integer.MIN_VALUE;
+        int[] maxBeauty = new int[items.length];
+        
+        for(int i = 0; i < items.length; i++) {
+            if(maxBeautySoFar < items[i][1]) maxBeautySoFar = items[i][1];
+            maxBeauty[i] = maxBeautySoFar;
         }
-
+        
+        for(int i = 0; i < queries.length; i++) {
+            int idx = findLargestIdxWithPriceLessThan(items, queries[i]);
+            if(idx != Integer.MIN_VALUE) ans[i] = maxBeauty[idx];
+        }
         return ans;
+    }
+    
+    public int findLargestIdxWithPriceLessThan(int[][] items, int price) {
+        int l = 0;
+        int r = items.length - 1;
+        int maxIdxLessThanEqualToPrice = Integer.MIN_VALUE; 
+        while(l <= r) {
+            int mid = (l + r)/2;
+            if(items[mid][0] > price) {
+                r = mid - 1;
+            } else {
+                maxIdxLessThanEqualToPrice = Math.max(maxIdxLessThanEqualToPrice, mid);
+                l = mid + 1;
+            }
+        }
+        return maxIdxLessThanEqualToPrice;
     }
 }
