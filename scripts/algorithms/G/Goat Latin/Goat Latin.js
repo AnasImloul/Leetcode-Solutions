b4cbb305e1c8f30b21ab0e2cd3570155af2e630b
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 01:26:57 +0100

    Runtime 59 ms (Top 81.28%) | Memory 42.0 MB (Top 13.37%)

--- scripts/algorithms/G/Goat Latin/Goat Latin.js
+++ scripts/algorithms/G/Goat Latin/Goat Latin.js
@@ -1,16 +1,31 @@
-// Runtime: 94 ms (Top 44.83%) | Memory: 42.2 MB (Top 73.56%)
-ar toGoatLatin = function(sentence) {
-    let arr = sentence.split(' ')
-    let vowels = ['a', 'e', 'i', 'o', 'u']
-    for (let i=0; i< arr.length; i++) {
-        if (vowels.includes(arr[i][0].toLowerCase())) {
-            arr[i] = arr[i] + 'ma'
-        } else {
-            arr[i] = arr[i].substring(1) + arr[i].substring(0,1) + 'ma'
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var toGoatLatin = function(S) {
+    let re = /[aeiou]/gi
+    let word = S.split(' ')
+    let add = 0
+    let arr = []
+    
+    for(let i= 0; i < word.length; i++) {
+        if(!word[i].substring(0,1).match(re)) {
+            arr = word[i].split('')
+            let letter = arr.shift()
+            arr.push(letter)
+            word[i] = arr.join('')
         }
-
-        arr[i] = arr[i] + 'a'.repeat(i+1)
-     }
-
-    return arr.join(' ')
+        
+        // have to append 'ma' regardless if it starts with a vowel
+        word[i] += 'ma'
+        
+        // append 'a' number of times as the index + 1
+        add = i+1
+        while(add >0) {
+            word[i] += 'a'
+            add--
+        }
+    }
+    
+    return word.join(' ')
 };
