c475cc47f8390cb94f47fdf338e83e9b6d448dc7
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 21:41:38 +0100

    Runtime 60 ms (Top 93.0%) | Memory 44.0 MB (Top 17.91%)

--- scripts/algorithms/A/Add Strings/Add Strings.js
+++ scripts/algorithms/A/Add Strings/Add Strings.js
@@ -1,26 +1,28 @@
-// Runtime: 114 ms (Top 41.92%) | Memory: 45.1 MB (Top 19.07%)
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
 var addStrings = function(num1, num2) {
-    let longer = num1.length >= num2.length ? num1 : num2;
-    let shorter = num1.length < num2.length ? num1 : num2;
-    let diff = (longer.length - shorter.length)
-    for (let i = 0; i < diff; i++) {
-      shorter = '0' + shorter;
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let sum = 0, carry = 0, x, y;
+    let result = "";
+    while(i>=0 || j>=0){
+        x = i < 0 ? 0 : num1.charAt(i) - "0";
+        y = j < 0 ? 0 : num2.charAt(j) - "0";
+        sum = (x + y + carry) % 10;
+        carry = Math.floor((x + y + carry) /10);
+        result =  sum + result;
+        
+        i--;
+        j--;
     }
-
-    let str = '';
-    let remainder = 0;
-
-    for (let i = (longer.length -1); i >= 0; i--) {
-        let num = remainder > 0
-            ? remainder + parseInt(longer[i]) + parseInt(shorter[i])
-            : parseInt(longer[i]) + parseInt(shorter[i]);
-        if (num > 9) {
-            remainder = 1;
-            num = num - 10;
-        } else {
-            remainder = 0;
-        }
-        str = num.toFixed() + str;
+    
+    if(carry!=0){
+        result =  carry + result;
     }
-    return !!remainder ? remainder + str : str;
+    
+    return result;
+   
 };
