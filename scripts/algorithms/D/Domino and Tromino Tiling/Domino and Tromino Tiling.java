9130d547a4738a546994ad3bd225bb0d215713fe
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:35:53 +0100

    Runtime 0 ms (Top 100.0%) | Memory 39.0 MB (Top 29.3%)

--- scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.java
+++ scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.java
@@ -1,19 +1,9 @@
-// Runtime: 3 ms (Top 24.92%) | Memory: 40.4 MB (Top 83.54%)
 class Solution {
     public int numTilings(int n) {
-        int[]dp=new int[n+1];
-        if(n==1){
-            return 1;
+        long[] dp = new long[n + 3]; dp[0] = 1; dp[1] = 2; dp[2] = 5;
+        for (int i = 3; i < n; i ++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007;
         }
-        if(n==2){
-            return 2;
-        }
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=5;
-        for(int i=4;i<=n;i++){
-            dp[i]=(((2*dp[i-1])%1000000007)+dp[i-3])%1000000007;
-        }
-        return dp[n];
+        return (int)dp[n - 1];
     }
 }
