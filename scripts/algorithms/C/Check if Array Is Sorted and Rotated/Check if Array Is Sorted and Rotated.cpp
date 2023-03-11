e2effe84ba15954df2c83f9003508bdb631b5a4f
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:44:54 +0100

    Runtime 2 ms (Top 56.2%) | Memory 8.0 MB (Top 10.84%)

--- scripts/algorithms/C/Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.cpp
+++ scripts/algorithms/C/Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        
-        int count=0;
-        int n=nums.size();
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>nums[(i+1)%n])count++;
-        }
-        
-        return count<=1;
+        int n = size(nums);
+        vector<int> og = nums;
+        sort(og.begin(),og.end());
+         for(int x = 0;x<n;x++){
+             int cnt = 0;
+             for(int j = 0;j<n;j++){
+                 if(og[j]==nums[(j+x)%n]) cnt++;
+             }
+             if(cnt==n) return true;
+         }
+         return false;
+      
     }
 };
