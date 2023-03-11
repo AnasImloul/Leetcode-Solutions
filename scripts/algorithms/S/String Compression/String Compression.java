cd9e042a6e5940a009e1e76b4f0b4dcf5529633a
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 01:37:45 +0100

    Runtime 1 ms (Top 95.98%) | Memory 42.0 MB (Top 44.31%)

--- scripts/algorithms/S/String Compression/String Compression.java
+++ scripts/algorithms/S/String Compression/String Compression.java
@@ -1,27 +1,30 @@
 class Solution {
     public int compress(char[] chars) {
-        int pt1=0,pt2=1; // pt1 is slow pointer to update chars & pt2 is fast pointer to calculate the count for pt1;
-        int count=1; // since every element will occur atleast once.
-        for(;pt2<chars.length;pt2++){
-            if(chars[pt2]==chars[pt2-1]){
-                count++;
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+            int j = i;
+
+            while (j < chars.length && chars[j] == chars[i]) {
+                j++;
             }
-            else {
-                chars[pt1] = chars[pt2-1]; // change pt1 to last same character
-                pt1 = setGroupCount(count,chars,++pt1); // if (count > 1) then update chars with count
-                count=1; // reset to 1 for new element
+
+            chars[index++] = chars[i];
+
+            if (j - i > 1) {
+                String count = j - i + "";
+
+                for (char c : count.toCharArray()) {
+                    chars[index++] = c;
+                }
             }
+
+            i = j;
         }
-        chars[pt1] = chars[pt2-1];          // same as above else block for last element
-        return setGroupCount(count, chars, ++pt1); 
-    }
-    
-    private int setGroupCount(int count,char[] chars, int pt){
-        if(count>1){
-            String str = String.valueOf(count);
-            for(int i=0;i<str.length();i++)
-                chars[pt++]=str.charAt(i);
-            }
-        return pt;
+
+        return index;
     }
 }
+
+// TC: O(n), SC: O(1)
