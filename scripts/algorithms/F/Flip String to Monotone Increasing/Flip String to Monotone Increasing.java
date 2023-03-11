a4989deb92835433bf1ede999a6e64bb9c95c5ff
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:31:11 +0100

    Runtime 10 ms (Top 69.37%) | Memory 43.0 MB (Top 7.75%)

--- scripts/algorithms/F/Flip String to Monotone Increasing/Flip String to Monotone Increasing.java
+++ scripts/algorithms/F/Flip String to Monotone Increasing/Flip String to Monotone Increasing.java
@@ -1,22 +1,16 @@
-// Runtime: 12 ms (Top 74.68%) | Memory: 51.9 MB (Top 43.97%)
 class Solution {
     public int minFlipsMonoIncr(String s) {
-        int count1 =0;
-        int delete_count =0;
-        for(char ch: s.toCharArray()){
-            if(ch=='1'){
-                if(count1<=0){
-                    count1=1;
-                }
-                else{
-                    count1++;
-                }
-            }
-            else if(count1>0){
-                delete_count++;
-                count1--;
-            }
+        int n = s.length();
+        int zeroToOne =0;
+        int countOfOnes=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='0')
+                zeroToOne=Math.min(zeroToOne+1,countOfOnes);
+            else
+                countOfOnes=countOfOnes+1;
         }
-        return delete_count;
+        return zeroToOne;
+        
     }
 }
