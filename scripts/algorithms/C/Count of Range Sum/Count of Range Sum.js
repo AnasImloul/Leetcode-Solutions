9f2ef80994f3a45c6ac137916e6ae3fb184abe03
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 21:40:21 +0100

    Runtime 1959 ms (Top 22.0%) | Memory 73.0 MB (Top 28.0%)

--- scripts/algorithms/C/Count of Range Sum/Count of Range Sum.js
+++ scripts/algorithms/C/Count of Range Sum/Count of Range Sum.js
@@ -1,54 +1,52 @@
-function SegmentTreeNode(tl, tr) {
-    this.sum = 0;
-    this.left = null;
-    this.right = null;
-    this.tl = tl; // tree left boundary
-    this.tr = tr; // tree right boundary
-}
+var countRangeSum = function(nums, lower, upper) {
+  let preSum = Array(nums.length + 1).fill(0);
+  let count = 0;
 
-const build = (a, tl, tr) => {
-    if (tl > tr) return null;
-    let stn = new SegmentTreeNode(a[tl], a[tr]);
-    if (tl == tr) return stn;
-    let mid = tl + tr >> 1;
-    stn.left = build(a, tl, mid);
-    stn.right = build(a, mid + 1, tr);
-    return stn;
-};
+  // create preSum array, use preSum to check the sum range
+  for (let i = 0; i < nums.length; i++) {
+      preSum[i + 1] = preSum[i] + nums[i];
+  }
 
-const update = (stn, newVal) => {
-    if (stn == null) return;
-    if (newVal >= stn.tl && newVal <= stn.tr) {
-        stn.sum++;
-        update(stn.left, newVal);
-        update(stn.right, newVal);
-    }
-};
+  const sort = function(preSum) {
+      if (preSum.length === 1) return preSum;
 
-const sumUtil = (stn, l, r) => {
-    if (stn == null) return 0;
-    if (l <= stn.tl && r >= stn.tr) return stn.sum;
-    if (r < stn.tl || l > stn.tr) return 0;
-    return sumUtil(stn.left, l, r) + sumUtil(stn.right, l, r);
-};
+      let mid = Math.floor(preSum.length / 2);
+      let left = sort(preSum.slice(0, mid));
+      let right = sort(preSum.slice(mid))
 
-const countRangeSum = (a, lower, upper) => {
-    let an = a.length;
-    let pre = new Set();
-    let sum = 0;
-    for (const e of a) {
-        sum += e;
-        pre.add(sum);
-    }
-    let u = [...pre];
-    let un = u.length;
-    u.sort((x, y) => x - y);
-    let root = build(u, 0, un - 1);
-    let res = 0;
-    for (let i = an - 1; ~i; i--) {
-        update(root, sum);
-        sum -= a[i];
-        res += sumUtil(root, lower + sum, upper + sum);
-    }
-    return res;
+      return merge(left, right);
+  }
+
+  const merge = function(left, right) {
+      let start = 0;
+      let end = 0;
+
+      for (let i = 0; i < left.length; i++) {
+          // all elements before start index, after subtracting left[i] are less than lower, which means all elements after start index are bigger or equal than lower
+          while (start < right.length && right[start] - left[i] < lower) {
+              start++;
+          }
+          // similarly, all elements before end index are less or euqal then upper
+          while (end < right.length && right[end] - left[i] <= upper) {
+              end++;
+          }
+
+          // since the initial values of start and end are the same, and upper >= lower, so end will >= start too, which means the rest of the end minus start element difference will fall between [lower, upper].
+          count += end - start;
+      }
+
+      let sort = [];
+      while (left.length && right.length) {
+          if (left[0] <= right[0]) {
+              sort.push(left.shift());
+          } else {
+              sort.push(right.shift());
+          }
+      }
+
+      return [...sort, ...left, ...right];
+  }
+
+  sort(preSum);
+  return count;
 };
