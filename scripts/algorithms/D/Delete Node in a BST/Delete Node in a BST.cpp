1d9a0c5171177da18ab603e55d1980b669d00df8
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:43:53 +0100

    Runtime 39 ms (Top 56.53%) | Memory 32.0 MB (Top 98.43%)

--- scripts/algorithms/D/Delete Node in a BST/Delete Node in a BST.cpp
+++ scripts/algorithms/D/Delete Node in a BST/Delete Node in a BST.cpp
@@ -1,158 +1,19 @@
-// Runtime: 73 ms (Top 21.98%) | Memory: 32.7 MB (Top 81.48%)
 class Solution {
 public:
-
-    TreeNode *loc;
-    TreeNode *par;
-
-    TreeNode *smallestbst(TreeNode *root)
-    {
-        if (root == NULL)
-            return NULL;
-
-        TreeNode *next = smallestbst(root->left);
-        if (next == NULL)
-            return root;
-        else if (root->val < next->val)
-            return root;
-        return next;
-    }
-
-    TreeNode *largestbst(TreeNode *root)
-    {
-        if (root == NULL)
-            return NULL;
-
-        TreeNode *next = largestbst(root->right);
-        if (next == NULL)
-            return root;
-        else if (root->val > next->val)
-            return root;
-        return next;
-    }
-
-    void searchanodebst(TreeNode *root, int item)
-    {
-        TreeNode *trav = root;
-        TreeNode *trail = NULL;
-        while (true)
-        {
-            if ((trav == NULL) || (trav->val == item))
-            {
-                if (trav == NULL)
-                    loc = NULL;
-                break;
-            }
-            trail = trav;
-            if (item < trav->val)
-                trav = trav->left;
-            else
-                trav = trav->right;
-        }
-        loc = trav;
-        par = trail;
-    }
-
-    TreeNode *deleteanodebst(TreeNode *root, int item)
-    {
-        searchanodebst(root, item);
-        // cout << " " << loc->val << "-" << par->val << " ";
-        if (par == NULL)
-        {
-            if (root->right == NULL)
-                return root->left;
-            if (root->left == NULL)
-                return root->right;
-
-            TreeNode *tmp = largestbst(root->left);
-            tmp->right = root->right;
-            tmp = root->left;
-            root->left = NULL;
-            root->right = NULL;
-            return tmp;
-        }
-
-        if (loc->left == NULL && loc->right == NULL)
-        {
-            if (par->right == loc)
-                par->right = NULL;
-            else
-                par->left = NULL;
-
-            return root;
-        }
-
-        if (loc->left == NULL)
-        {
-            if (par->right == loc)
-            {
-                par->right = loc->right;
-                loc->right = NULL;
-            }
-            else
-            {
-                par->left = loc->right;
-                loc->right = NULL;
-            }
-            return root;
-        }
-
-        if (loc->right == NULL)
-        {
-            if (par->right == loc)
-            {
-                par->right = loc->left;
-                loc->left = NULL;
-            }
-            else
-            {
-                par->left = loc->left;
-                loc->left = NULL;
-            }
-            return root;
-        }
-
-        // cout << " DEBUG 01 ";
-
-        if (par->right == loc)
-        {
-            par->right = loc->left;
-            TreeNode *tmp = largestbst(loc->left);
-            // cout << " '" << tmp->val << "' ";
-            tmp->right = loc->right;
-            loc->left = NULL;
-            loc->right = NULL;
-            return root;
-        }
-
-        par->left = loc->left;
-        TreeNode *tmp = largestbst(loc->left);
-        // cout << " '" << tmp->val << "' ";
-        tmp->right = loc->right;
-        loc->left = NULL;
-        loc->right = NULL;
-        return root;
-    }
-
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL) return NULL;
-        while(true)
-        {
-            loc = NULL;
-            par = NULL;
-            searchanodebst(root, key);
-            if(loc != NULL)
-            {
-                loc = NULL;
-                par = NULL;
-                root = deleteanodebst(root, key);
+        if(root) 
+            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
+            else if(key > root->val) root->right = deleteNode(root->right, key);       
+            else{
+                if(!root->left && !root->right) return NULL;          //No child condition
+                if (!root->left || !root->right)
+                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
+					                                                //Two child condition   
+                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
+                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
+                root->val = temp->val;                            //       root->val = temp->val;
+                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
             }
-            else
-            {
-                break;
-            }
-        }
-
         return root;
-    }
+    }   
 };
