87b498f95c24d755e17b48b7ae300b1dd2684f98
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:45:56 +0100

    Runtime 0 ms (Top 100.0%) | Memory 6.0 MB (Top 84.93%)

--- scripts/algorithms/0-9/2 Keys Keyboard/2 Keys Keyboard.cpp
+++ scripts/algorithms/0-9/2 Keys Keyboard/2 Keys Keyboard.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        for (int j = n/2; j >= 1; j--) {
-            if (n % j == 0) {
-                return minSteps(j) + n/j;
+//See the solution for this explanation 
+    int byPrimeFactorization(int n) {
+        if(n == 1)
+            return 0;
+        if(n == 2)
+            return 2;
+        int factor = 2, ans = 0;
+        while(n > 1) {
+            while(n % factor == 0) {
+                ans += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        return ans;
+    }
+   
+    int byDp(int n) {
+        vector<int> dp(1001, INT_MAX);
+        dp[0] = dp[1] = 0;
+        dp[2] = 2, dp[3] = 3;
+        for(int i = 4; i <= n; i++) {
+            dp[i] = i; //maximum number of operations required will be i
+            for(int j = 2; j <= i / 2; j++) { //we copy and paste j A's till we have i A's
+                int x = i - j; //we already have j A's in our stream, so remaining = i - j
+                if(x % j == 0) { //if remaining number of A's is a multiple of J
+                    dp[i] = min(dp[i], dp[j] + 1 + (x / j)); //1 operation to copy, x / j to paste, dp[j] for getting j A's
+                }
             }
         }
-        return 0;
+        return dp[n];
+    }
+    
+    int minSteps(int n) {
+        return byPrimeFactorization(n);
     }
 };
