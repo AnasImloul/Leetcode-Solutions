87362030435c381b734413ca47aaa2f33f018d72
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:48:39 +0100

    Runtime 2 ms (Top 42.13%) | Memory 6.0 MB (Top 93.87%)

--- scripts/algorithms/S/String Without AAA or BBB/String Without AAA or BBB.cpp
+++ scripts/algorithms/S/String Without AAA or BBB/String Without AAA or BBB.cpp
@@ -2,69 +2,14 @@ class Solution {
 public:
     string strWithout3a3b(int a, int b) {
         string ans="";
-        int i=0,j=0;
-        bool flag = false;
-        if(a > b){
-            for(j=0;j<b;j++,i+=2){
-                if(a-i == b-j){ // If this condition satisfies means from this point our count of remaining a and b become same so we need to print them alternatively.
-                    flag = true;
-                    break;
-                }
-                ans.push_back('a');
-                ans.push_back('a');
-                ans.push_back('b');
-                
-            }
-            
-            if(flag == true){
-                //Printing a and b alternately as count of remaining a and b is same.
-                for(i;i<a;i++){
-                   ans.push_back('a');
-                   ans.push_back('b');
-               } 
-            }
-            else{       // If count does not become same then we print 2 a and 1 b in upper for loop and still some a remains we print them in below loop.
-                while(i<a){
-                    ans.push_back('a');
-                    i++;
-                }
-            }
+        while(a and b){
+            if(a > b) ans += "aab", a--;
+            else if(b > a) ans +="bba", b--;
+            else ans += "ab";
+            a--, b--;
         }
-        
-        
-        else if( b> a){
-           for(i=0;i<a;i++,j+=2){
-               if(a-i == b-j){
-                    flag = true;
-                    break;
-                }
-               ans.push_back('b');
-               ans.push_back('b');
-               ans.push_back('a');
-               
-           } 
-             if(flag == true){
-                 
-                for(i;i<a;i++){
-                   ans.push_back('a');
-                   ans.push_back('b');
-               } 
-            }
-            else{
-                while(j<b){
-                    ans.push_back('b');
-                    j++;
-                } 
-            }
-        }
-        
-        
-        else{
-           for(i=0;i<a;i++){
-               ans.push_back('a');
-               ans.push_back('b');
-           } 
-        }
+        while(a) ans +='a' , a--;
+        while(b) ans +='b' , b--;
         return ans;
     }
 };
