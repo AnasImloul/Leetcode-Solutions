256fbbfbea7206bfa3d23a703097b8f72c799f25
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 01:31:57 +0100

    Runtime 70 ms (Top 73.37%) | Memory 44.0 MB (Top 31.37%)

--- scripts/algorithms/M/Missing Number/Missing Number.js
+++ scripts/algorithms/M/Missing Number/Missing Number.js
@@ -1,11 +1,3 @@
-// Runtime: 90 ms (Top 77.79%) | Memory: 44.2 MB (Top 70.06%)
 var missingNumber = function(nums) {
-    // Initialize res as zero...
-    let res = 0;
-    // Traverse all the element through the loop...
-    for (let i = 0; i < nums.length; ++i) {
-        // Calculation process by helping of Bit Manipulation...
-        res ^= nums[i] ^ (i + 1);
-    }
-    return res; // Return the result
+    return ((1 + nums.length)*nums.length/2) - nums.reduce((a,b) => a+b)
 };
