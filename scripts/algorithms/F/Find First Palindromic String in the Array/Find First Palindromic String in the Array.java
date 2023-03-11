a808cbfad1cb210ad0af52f993ddf7b04976c872
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 20:45:23 +0100

    Runtime 4 ms (Top 55.11%) | Memory 42.0 MB (Top 30.91%)

--- scripts/algorithms/F/Find First Palindromic String in the Array/Find First Palindromic String in the Array.java
+++ scripts/algorithms/F/Find First Palindromic String in the Array/Find First Palindromic String in the Array.java
@@ -1,13 +1,9 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-
-        for(int i=0; i<words.length;i++){
-            StringBuilder sb = new StringBuilder();
-            sb.append(words[i]);
-            sb.reverse();
-            // System.out.println(words[i]+" "+sb);
-            if(words[i].equals(sb. toString())){
-                return words[i];
+        for (String s : words) {
+            StringBuilder sb = new StringBuilder(s);
+            if (s.equals(sb.reverse().toString())) {
+                return s;
             }
         }
         return "";
