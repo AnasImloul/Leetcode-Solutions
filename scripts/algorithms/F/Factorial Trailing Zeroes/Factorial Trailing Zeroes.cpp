e6171ca183121d279fc3b9fd43e663dcb1e02e1b
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:41:29 +0100

    Runtime 3 ms (Top 39.5%) | Memory 6.0 MB (Top 64.23%)

--- scripts/algorithms/F/Factorial Trailing Zeroes/Factorial Trailing Zeroes.cpp
+++ scripts/algorithms/F/Factorial Trailing Zeroes/Factorial Trailing Zeroes.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int cnt = 0;
-        for(int i = 5; i <= n; i *= 5)
-            cnt += n/i;
-        return cnt;
+        int ni=0, mi=0;
+        for (int i=1; i<=n; i++){
+            int x=i;
+            while (x%2==0){
+                x= x>>1;
+                ni++;
+            }
+            while (x%5==0){
+                x= x/5;
+                mi++;
+            }
+        }
+        return min(mi,ni);
     }
 };
