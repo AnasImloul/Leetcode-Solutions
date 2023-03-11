56a0925daca44b638f2bc015a090041ae74413e6
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:32:57 +0100

    Runtime 1 ms (Top 100.0%) | Memory 40.0 MB (Top 16.6%)

--- scripts/algorithms/D/Divide Two Integers/Divide Two Integers.java
+++ scripts/algorithms/D/Divide Two Integers/Divide Two Integers.java
@@ -1,16 +1,22 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend==Integer.MIN_VALUE && divisor==-1) return Integer.MAX_VALUE;
-        int sign = (dividend>=0) ^ (divisor>=0) ? -1:1;
+        if(dividend == 1<<31 && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        boolean sign = (dividend >= 0) == (divisor >= 0) ? true : false;
+        
         dividend = Math.abs(dividend);
         divisor = Math.abs(divisor);
-        int res=0;
-        while (dividend-divisor>=0){
-            int x=0;
-            while (dividend - (divisor<<1<<x) >=0) x++;
-            res+=(1<<x);
-            dividend-=(divisor<<x);
+        
+        int result = 0;
+        while(dividend - divisor >= 0){
+            int count = 0;
+            while(dividend - (divisor << 1 << count) >= 0){
+                count++;
+            }
+            result += 1 <<count;
+            dividend -= divisor << count;
         }
-        return res*sign;
+        return sign ? result : -result;
     }
 }
