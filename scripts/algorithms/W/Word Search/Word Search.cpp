c315bab21efe64c237d28ddb5fe3216adf937c3d
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:47:44 +0100

    Runtime 445 ms (Top 76.22%) | Memory 8.0 MB (Top 61.91%)

--- scripts/algorithms/W/Word Search/Word Search.cpp
+++ scripts/algorithms/W/Word Search/Word Search.cpp
@@ -1,46 +1,27 @@
-// Runtime: 465 ms (Top 65.47%) | Memory: 8.2 MB (Top 18.19%)
 class Solution {
 public:
-    bool flag=false;
-    void exist(int k,int i ,int j,string& word,vector<vector<char>>& board,vector<vector<int>>& vis,int n,int m){
-        if(k==word.size()){
-            flag=true;
-            return;
+    bool solve(int i,int j,int &m,int &n,vector<vector<char>> &board,string &str,int s){
+        if(s>=str.length()){
+            return true;
         }
-// Down
-        if(!flag && i+1<m && !vis[i+1][j] && board[i+1][j]==word[k]){
-            vis[i][j]=1;
-            exist(k+1,i+1,j,word,board,vis,n,m);
-            vis[i][j]=0;
+        if(i<0||j<0||i>=m||j>=n||board[i][j]=='#'){
+            return false;
         }
-// Left
-        if(!flag && j-1>=0 && !vis[i][j-1] && board[i][j-1]==word[k]){
-            vis[i][j]=1;
-            exist(k+1,i,j-1,word,board,vis,n,m);
-            vis[i][j]=0;
-        }
-// Right
-        if(!flag && j+1<n && !vis[i][j+1] && board[i][j+1]==word[k]){
-            vis[i][j]=1;
-            exist(k+1,i,j+1,word,board,vis,n,m);
-            vis[i][j]=0;
-        }
-// up
-        if(!flag && i-1>=0 && !vis[i-1][j] && board[i-1][j]==word[k]){
-            vis[i][j]=1;
-            exist(k+1,i-1,j,word,board,vis,n,m);
-            vis[i][j]=0;
-        }
+        char c = board[i][j];
+        board[i][j] = '#';
+        bool a = false;
+        if(c==str[s])
+        a = solve(i+1,j,m,n,board,str,s+1)||solve(i-1,j,m,n,board,str,s+1)||solve(i,j-1,m,n,board,str,s+1) ||  solve(i,j+1,m,n,board,str,s+1);
+        board[i][j] = c;
+        return a;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(word[0]==board[i][j]) exist(1,i,j,word,board,vis,n,m);
-                if(flag) return flag;
+        int i,j,m=board.size(),n=board[0].size();
+        for(i = 0; i < m; i++){
+            for(j = 0; j < n; j++){
+                if(board[i][j]==word[0] && solve(i,j,m,n,board,word,0)){
+                    return true;
+                }
             }
         }
         return false;
