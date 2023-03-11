ad1521dc96edb1dee9c6c939a8a240043ae95f94
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 20:44:10 +0100

    Runtime 2 ms (Top 45.67%) | Memory 42.0 MB (Top 12.27%)

--- scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.java
+++ scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.java
@@ -1,4 +1,21 @@
-	class Solution {
-
-public int mincostTickets(int[] days, int[] costs) {
- return recursive(days, costs,0);
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int day: days) set.add(day);
+        int n = days[days.length - 1];
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++){
+            if(!set.contains(i)){
+                dp[i] = dp[i - 1];
+                continue;
+            }
+            int a = dp[Math.max(0, i - 1)] + costs[0];
+            int b = dp[Math.max(0, i - 7)] + costs[1];
+            int c = dp[Math.max(0, i - 30)] + costs[2];
+            dp[i] = Math.min(a, Math.min(b, c));
+        }
+        return dp[n];
+    }
+}
