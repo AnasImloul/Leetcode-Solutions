268f1d72a0234d6c04dd25f7f65f82557bdb2b6d
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 04:36:52 +0100

    Runtime 1 ms (Top 93.61%) | Memory 41.0 MB (Top 20.98%)

--- scripts/algorithms/M/Maximum Repeating Substring/Maximum Repeating Substring.java
+++ scripts/algorithms/M/Maximum Repeating Substring/Maximum Repeating Substring.java
@@ -1,7 +1,13 @@
 class Solution {
-    public int maxRepeating(String sequence, String word) {
-        String find="";
-        while(sequence.contains(find)) find += word;
-        return (find.length()-word.length())/word.length();
+    public int maxRepeating(String s, String w) {
+        if(w.length()>s.length()) return 0;
+        int ans=0;
+        StringBuilder sb=new StringBuilder("");
+        while(sb.length()<=s.length()){
+            sb.append(w);
+            if(s.contains(sb)) ans++;
+            else break;
+        }
+        return ans;
     }
 }
