961b467dda576a9c88421157f650f36529e82c79
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-11 21:21:17 +0100

    Runtime 766 ms (Top 36.36%) | Memory 17.0 MB (Top 27.83%)

--- scripts/algorithms/0-9/01 Matrix/01 Matrix.py
+++ scripts/algorithms/0-9/01 Matrix/01 Matrix.py
@@ -1,21 +1,25 @@
-# Runtime: 1146 ms (Top 40.37%) | Memory: 16.2 MB (Top 99.65%)
+from collections import deque
+from typing import List
+
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows = len(mat)
-        cols = len(mat[0])
-
-        for i in range(rows):
-            for j in range(cols):
-                if mat[i][j] != 0:
-                    top = mat[i-1][j] if i>0 else float('inf')
-                    left = mat[i][j-1] if j>0 else float('inf')
-                    mat[i][j] = 1 + min(top, left)
-
-        for i in range(rows-1, -1, -1):
-            for j in range(cols-1, -1, -1):
-                if mat[i][j] != 0:
-                    right = mat[i][j+1] if j<cols-1 else float('inf')
-                    bottom = mat[i+1][j] if i<rows-1 else float('inf')
-                    mat[i][j] = min(min(right, bottom)+1, mat[i][j])
-
-        return mat
+        # Initialize a queue and a 2D array to store the distances
+        queue = deque()
+        dist = [[float('inf') for _ in range(len(mat[0]))] for _ in range(len(mat))]
+        
+        # Add all the 0s in the matrix to the queue and set their distance to 0
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+                    dist[i][j] = 0
+        
+        # Perform breadth-first search
+        while queue:
+            i, j = queue.popleft()
+            for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if 0 <= x < len(mat) and 0 <= y < len(mat[0]) and dist[x][y] > dist[i][j] + 1:
+                    dist[x][y] = dist[i][j] + 1
+                    queue.append((x, y))
+        
+        return dist
