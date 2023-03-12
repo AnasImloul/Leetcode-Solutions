68aa15f8c70a7637910d8578f911854844e9702b
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:23:20 +0100

    Runtime 6840 ms (Top 6.84%) | Memory 42.0 MB (Top 13.7%)

--- scripts/algorithms/R/Remove Boxes/Remove Boxes.py
+++ scripts/algorithms/R/Remove Boxes/Remove Boxes.py
@@ -1,36 +1,11 @@
-from functools import cache
-
 class Solution:
-    def removeBoxes(self, boxes: List[int]) -> int:
-        colors, consecutive = [], []
-        cnt = 0
-        prev_color = None
-        for color in boxes:
-            if prev_color is None:
-                cnt = 1
-            elif color != prev_color:
-                colors.append(prev_color)
-                consecutive.append(cnt)
-                cnt = 1
-            else:
-                cnt += 1
-            prev_color = color
-        colors.append(prev_color)
-        consecutive.append(cnt)
-                
-        @cache
-        def dp(i, j, carry_over):
-            if i == j:
-                return 0
-            
-            # option 1: remove i and calculate rest of score
-            best = (consecutive[i] + carry_over) ** 2 + dp(i+1, j, 0)
-            
-            # option 2: Remove all boxes in between i and a similar colored box x,
-			# so that we can remove i+x.
-            for x in range(i+1, j):
-                if colors[i] == colors[x]:
-                    best = max(best, dp(i+1, x, 0) + dp(x, j, consecutive[i] + carry_over))
-            return best
+    def removeBoxes(self, B):
         
-        return dp(0, len(colors), 0)
+        @lru_cache(None)
+        def dp(i, j, k):
+            if i > j: return 0
+            indx = [m for m in range(i+1, j+1) if B[m] == B[i]]
+            ans = (k+1)**2 + dp(i+1, j, 0)
+            return max([ans] + [dp(i+1, m-1, 0) + dp(m, j, k+1) for m in indx])   
+            
+        return dp(0, len(B)-1, 0)
