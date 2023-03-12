238002e71c52a02de00c632bb8197d5b163a716b
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 02:49:14 +0100

    Runtime 11 ms (Top 47.56%) | Memory 6.0 MB (Top 69.41%)

--- scripts/algorithms/F/Fibonacci Number/Fibonacci Number.cpp
+++ scripts/algorithms/F/Fibonacci Number/Fibonacci Number.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        double sqrt5 = 2.23606797749979;
-        double expr1 = 1.61803398874989;
-        double expr2 = -0.618033988749895;
-        double res = (pow(expr1,n)-pow(expr2,n))/sqrt5;
-        int result = (int)round(res);
-        return result;
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
