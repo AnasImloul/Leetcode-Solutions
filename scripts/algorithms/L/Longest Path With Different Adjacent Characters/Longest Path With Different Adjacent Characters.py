1daacb0ff9c2f7aa7a43e5b4a6b33f28fb4b9146
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:25:08 +0100

    Runtime 3047 ms (Top 5.2%) | Memory 264.0 MB (Top 5.2%)

--- scripts/algorithms/L/Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.py
+++ scripts/algorithms/L/Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.py
@@ -1,13 +1,13 @@
-# Runtime: 5213 ms (Top 8.25%) | Memory: 84.5 MB (Top 93.36%)
-from graphlib import TopologicalSorter
-
 class Solution:
-    def longestPath(self, parent: List[int], s: str) -> int:
-        def lengths():
-            downs = [[0] for _ in s]
-            ts = TopologicalSorter(dict(enumerate(zip(parent))))
-            for node in [*ts.static_order()][:0:-1]:
-                yield 1 + sum(nlargest(2, downs[node]))
-                if s[node] != s[parent[node]]:
-                    downs[parent[node]].append(1 + max(downs[node]))
-        return max(lengths())
+    def longestPath(self, parent: list[int], s: str) -> int:
+        def l_path_and_chain(tree: dict[int, list[int]], s: str, root: int) -> tuple[int, int]:
+            lp = lc1 = lc2 = 0
+            for child, path, chain in ((c, *l_path_and_chain(tree, s, c)) for c in tree[root]):
+                lp = max(lp, path)
+                if s[child] != s[root]: *_, lc2, lc1 = sorted((chain, lc2, lc1))
+
+            return max(lp, lc1 + lc2 + 1), lc1 + 1
+
+        t = defaultdict(list)
+        for c, p in enumerate(parent): t[p].append(c)
+        return l_path_and_chain(t, s, 0)[0]
