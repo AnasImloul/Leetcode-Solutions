319d31cc41f435b5c114d8e710e8d0762cf2dafe
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:34:31 +0100

    Runtime 62 ms (Top 91.18%) | Memory 42.0 MB (Top 97.6%)

--- scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.js
+++ scripts/algorithms/S/Sum of Mutated Array Closest to Target/Sum of Mutated Array Closest to Target.js
@@ -1,45 +1,11 @@
-// Runtime: 114 ms (Top 45.10%) | Memory: 43.1 MB (Top 78.43%)
-var findBestValue = function(arr, target) {
-    const n = arr.length;
-    const max = Math.max(...arr);
-
-    let left = 0;
-    let right = max;
-
-    let prevSum = Number.MIN_SAFE_INTEGER;
-    let prevVal = -1;
-
-    while (left <= right) {
-        const mid = left + Math.floor((right - left) / 2);
-
-        const currSum = changeSum(mid);
-
-        if (currSum === target) return mid;
-
-        const currDiff = Math.abs(target - currSum);
-        const prevDiff = Math.abs(target - prevSum);
-
-        if (currDiff < prevDiff || (currDiff === prevDiff && mid < prevVal)) {
-            prevSum = currSum;
-            prevVal = mid;
-        }
-
-        if (currSum < target) left = mid + 1;
-        else right = mid - 1;
-    }
-
-    return prevVal;
-
-    function changeSum(value) {
-        let sum = 0;
-
-        for (let i = 0; i < n; i++) {
-            const num = arr[i];
-
-            if (num > value) sum += value;
-            else sum += num;
-        }
-
-        return sum;
-    }
+var findBestValue = function (arr, target) {
+	let low = 1, mid, high = Math.max(...arr);
+	const getSumDif = val =>
+		arr.reduce((acc, cur) => acc + (cur < val ? cur : val), 0) - target;
+	while (low < high) {
+		mid = (low + high) >>> 1;
+		getSumDif(mid) < 0 ? (low = mid + 1) : (high = mid);
+	}
+	Math.abs(getSumDif(low - 1)) <= Math.abs(getSumDif(low)) && low--;
+	return low;
 };
