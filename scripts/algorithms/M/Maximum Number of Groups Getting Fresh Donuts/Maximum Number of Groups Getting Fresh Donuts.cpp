32c940f6dd27559a15385a9625b9c5ce64ee8a2a
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:31:07 +0100

    Runtime 8 ms (Top 80.82%) | Memory 8.0 MB (Top 94.52%)

--- scripts/algorithms/M/Maximum Number of Groups Getting Fresh Donuts/Maximum Number of Groups Getting Fresh Donuts.cpp
+++ scripts/algorithms/M/Maximum Number of Groups Getting Fresh Donuts/Maximum Number of Groups Getting Fresh Donuts.cpp
@@ -1,1 +1,42 @@
-O(n!)
+class Solution{
+typedef long long ll;
+#define vi(x) vector<x>
+public:
+    unordered_map<string, ll>dp;
+    string s;
+    ll func(ll rem){
+        if(dp.find(s)==dp.end()){
+            dp[s]=-1;
+        }
+        ll&ans=dp[s];
+        if(ans==-1){
+            ans=0;
+            ll bs=s.size();
+            for(ll i=1;i<bs;++i){
+                if(s[i]>'0'){
+                    --s[i];
+                    ans=max(ans, func((bs+rem-i)%bs)+!rem);
+                    ++s[i];
+                }
+            }
+        }
+        return ans;
+    }
+    int maxHappyGroups(int bs, vector<int>&v) {
+        s.assign(bs, '0');
+        ll ans=0;
+        for(ll it:v){
+            if(it%bs==0){
+                ++ans;
+            }
+            else if(s[bs-it%bs]>'0'){
+                ++ans;
+                --s[bs-it%bs];
+            }
+            else{
+                ++s[it%bs];
+            }
+        }
+        return ans+func(0);
+    }
+};
