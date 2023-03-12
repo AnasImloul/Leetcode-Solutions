2e5f4af393dab238e7c518a1ecf175f906e854a4
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 22:42:37 +0100

    Runtime 62 ms (Top 8.89%) | Memory 14.0 MB (Top 29.77%)

--- scripts/algorithms/S/Search Insert Position/Search Insert Position.py
+++ scripts/algorithms/S/Search Insert Position/Search Insert Position.py
@@ -1,14 +1,6 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        low = 0
-        high = len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                low = mid + 1
-        return low
-               
+    def searchInsert(self, nums, target):
+        for i, num in enumerate(nums):
+            if num >= target:
+                return i
+        return len(nums)
