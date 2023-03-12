55647dea607f56f76f2235ddbd6ed43ee0d8bfbb
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 03:28:46 +0100

    Runtime 31 ms (Top 78.95%) | Memory 13.0 MB (Top 60.23%)

--- scripts/algorithms/M/Max Difference You Can Get From Changing an Integer/Max Difference You Can Get From Changing an Integer.py
+++ scripts/algorithms/M/Max Difference You Can Get From Changing an Integer/Max Difference You Can Get From Changing an Integer.py
@@ -1,23 +1,41 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
-        tmp = num
-        xA, xB = 9, 1
-        while tmp:
-            digit = tmp % 10
-            if digit != 9:
-                xA = digit
-            if digit > 1:
-                xB = digit  
-            tmp //= 10
+        i=0
+        while i<len(str(num)):
+            change = (str(num)[i])
+            if change!='9':
+                break
+            i+=1
         
-        yA, yB = 9, 0 if digit == 1 and xB != 1 else 1
-        digitPlace = 1
-        a = b = 0
-        while num:
-            digit = num % 10
-            a += digitPlace * (digit if digit != xA else yA)
-            b += digitPlace * (digit if digit != xB else yB)
-            num //= 10
-            digitPlace *= 10
-        
-        return a - b
+        i=0
+        flag = False
+        while i<len(str(num)):
+            sc = (str(num)[i])
+            if sc!='1' and sc!='0':
+                if i>0:
+                    flag =True
+                break
+            i+=1
+        small = str(num)
+        num = str(num)
+        m=''
+        for i in range(len(str(num))):
+            if num[i]==change:
+                m+='9'
+            else:
+                m+=num[i]
+        m = int(m)
+        s =''
+        for i in range(len(num)):
+            if small[i]==sc:
+                if flag:
+                    s+='0'
+                else:
+                    if small[i]=="0":
+                        s+='0'
+                    else:
+                        s+='1'
+            else:
+                s+=small[i]
+        small = int(s)
+        return m - small
