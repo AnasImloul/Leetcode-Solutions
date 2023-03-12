abbc75248e67d7c73658ce4cdc902f3c9825f087
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 22:38:56 +0100

    Runtime 835 ms (Top 37.25%) | Memory 20.0 MB (Top 15.69%)

--- scripts/algorithms/M/Minimum Possible Integer After at Most K Adjacent Swaps On Digits/Minimum Possible Integer After at Most K Adjacent Swaps On Digits.py
+++ scripts/algorithms/M/Minimum Possible Integer After at Most K Adjacent Swaps On Digits/Minimum Possible Integer After at Most K Adjacent Swaps On Digits.py
@@ -1,47 +1,20 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def minInteger(self, num: str, k: int) -> str:
-        n=len(num)
-        ft=[0]*(n+1)
-        D=defaultdict(lambda : deque())
-        for idx,val in enumerate(num):
-            D[int(val)].append(idx+1)
-            
-        def update(i,no):
-            while i<=n:
-                ft[i]+=no
-                i += i & -i
-                
-        def query(i):
-            res=0
-            while i>0:
-                res+=ft[i]
-                i -= i & -i
-            return res
-        
-        ans=""
-        t=0
-        while k>0:
-            flag=False
-            for i in range(10):
-                if len(D[i])>0:
-                    x=D[i][0]-query(D[i][0])-1
-                    if k>=x:
-                        k-=x
-                        flag=True
-                        ans+=str(i)
-                        update(D[i][0],1)
-                        D[i].popleft()
-                        break
-            if not flag:
+        sz, window = len(num), SortedList()
+        remainedIndices, poppedIndices = SortedList(range(sz)), []
+        while k > 0:
+            while len(window) < k + 1 and len(window) < len(remainedIndices):
+                idx = remainedIndices[len(window)]
+                window.add((num[idx], idx))
+            if not window:
                 break
-        temp=[]
-        for key in D:
-            for val in D[key]:
-                temp.append((val,key))
-        temp.sort()
-        for idx,no in temp:
-            ans+=str(no)
-        return ans
-        
-                
-        
+            index = window.pop(0)[1]
+            k -= remainedIndices.bisect_left(index)
+            remainedIndices.remove(index)
+            poppedIndices.append(index)
+            for idx in remainedIndices[k + 1: len(window)]:
+                window.remove((num[idx], idx))
+        poppedSet = set(poppedIndices)
+        return "".join(num[idx] for idx in poppedIndices) + "".join(num[idx] for idx in range(sz) if idx not in poppedSet)
