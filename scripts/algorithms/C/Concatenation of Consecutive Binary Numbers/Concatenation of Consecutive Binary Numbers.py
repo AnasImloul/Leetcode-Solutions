144cb5cf7f0e7a1ef8c2c44d9245b12e7abd3779
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 03:31:55 +0100

    Runtime 1204 ms (Top 90.49%) | Memory 13.0 MB (Top 96.72%)

--- scripts/algorithms/C/Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
+++ scripts/algorithms/C/Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
@@ -1,7 +1,12 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        s = 0
+        modulo = 10 ** 9 + 7
+        shift = 0 # tracking power of 2
+        res = 0
+        
         for i in range(1, n+1):
-            s = (s << i.bit_length() | i) % 1000000007
-            
-        return s
+            if i & (i - 1) == 0: # see if num reaches a greater power of 2
+                shift += 1
+            res = ((res << shift) + i) % modulo # do the simulation
+        
+        return res
