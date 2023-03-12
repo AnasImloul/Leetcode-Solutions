9ddba59042d9414d831053fa8a7a89717ee8f1d6
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:24:12 +0100

    Runtime 38 ms (Top 37.65%) | Memory 13.0 MB (Top 92.67%)

--- scripts/algorithms/U/Ugly Number/Ugly Number.py
+++ scripts/algorithms/U/Ugly Number/Ugly Number.py
@@ -1,10 +1,12 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n <= 0: return False
         
-        while True:
-            if n == 1: return True
-            elif not n % 2: n /= 2
-            elif not n % 3: n /= 3
-            elif not n % 5: n /= 5
-            else: return False
+        prime = [2, 3, 5] # prime factors list provided in question againt which we have to check the provided number. 
+        
+        if n == 0: # as we dont have factors for 0
+            return False 
+        
+        for p in prime: # traversing prime numbers from given prime number list.
+            while n % p == 0: # here well check if the number is having the factor or not. For instance 6%2==0 is true implies 2 is a factor of 6.
+                n //= p # num = num//p # in this well be having 3(6/2), 1(3/3). Doing this division to update our number
+        return n == 1 # at last well always have 1, if the number would have factors from the provided list 
