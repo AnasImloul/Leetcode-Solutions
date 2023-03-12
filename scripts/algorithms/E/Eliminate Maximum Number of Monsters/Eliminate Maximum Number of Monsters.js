4851d816c42bdf480c17a6d50df3402629718733
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 03:43:46 +0100

    Runtime 161 ms (Top 89.58%) | Memory 60.0 MB (Top 56.25%)

--- scripts/algorithms/E/Eliminate Maximum Number of Monsters/Eliminate Maximum Number of Monsters.js
+++ scripts/algorithms/E/Eliminate Maximum Number of Monsters/Eliminate Maximum Number of Monsters.js
@@ -4,16 +4,35 @@
  * @return {number}
  */
 var eliminateMaximum = function(dist, speed) {
-      let t = new Array(dist.legnth)
-    for(let i = 0; i< dist.length; i++){
-      let m =  Math.ceil(dist[i]/ speed[i])
-      t[i] = m
+    let res = 0;
+    let len = dist.length;
+    let map = new Map();
+    for(let i=0; i<len; i++){
+        // the last time to eliminate
+        let a = Math.ceil(dist[i] / speed[i]);
+        if(map.has(a)){
+            let c = map.get(a);
+            c ++;
+            map.set(a, c);
+        }else{
+            map.set(a, 1);
+        }
     }
-   t = t.sort((a,b) => a-b)
-    let c = 0
-     for(let i = 0; i< t.length; i++){
-        if(t[i]-i > 0) c++
-       else if(t[i]-i <= 0) return c
+
+    let keys = Array.from(map.keys());
+    keys.sort((a, b) => a-b);
+    // time to eliminate
+    let t = 0;
+    for(let i=0; i<keys.length; i++){
+        let c = map.get(keys[i]);
+        if(c > keys[i]-t){
+            res += keys[i]-t;
+            break;
+        }else{
+            res += c;
+            t += c;
+        }
     }
-    return c
+
+    return res;
 };
