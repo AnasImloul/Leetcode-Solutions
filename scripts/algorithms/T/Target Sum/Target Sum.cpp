cce97873e42995b823df536401eb277ef91ae883
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:30:01 +0100

    Runtime 623 ms (Top 27.99%) | Memory 89.0 MB (Top 5.7%)

--- scripts/algorithms/T/Target Sum/Target Sum.cpp
+++ scripts/algorithms/T/Target Sum/Target Sum.cpp
@@ -1,16 +1,26 @@
-class Solution {
+class Solution { // YAA
 public:
-	int solve(int i,vector<int>& nums,int target){
-		if(i==nums.size()){
-			if(target==0)return 1;
-			return 0;
-		} 
-		int pick= solve(i+1,nums,target-nums[i]);
-		int notpick= solve(i+1,nums,target+nums[i]);
-		return pick+notpick;
-	}
-
-	int findTargetSumWays(vector<int>& nums, int target) {
-		return solve(0,nums,target);  
-	}
+    
+    int solve(vector<int>& nums, int target, int idx, unordered_map<string, int> &dp) {
+        if(idx == nums.size()) {
+            if(target == 0) {
+                return 1;
+            }
+            return 0;
+        }
+        string key = to_string(idx) + " " + to_string(target);
+        if(dp.find(key) != dp.end()) {
+            return dp[key];
+        }
+        // +
+        int x = solve(nums, target - nums[idx], idx+1, dp);
+        // -
+        int y = solve(nums, target + nums[idx], idx+1, dp);
+        // sum
+        return dp[key] = x+y;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<string, int> dp;
+        return solve(nums, target, 0, dp);
+    }
 };
