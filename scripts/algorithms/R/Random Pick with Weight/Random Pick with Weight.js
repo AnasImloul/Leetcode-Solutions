5dd2f8df348313a047075f8dcfc676bda056c3fa
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 22:54:26 +0100

    Runtime 682 ms (Top 6.58%) | Memory 55.0 MB (Top 5.92%)

--- scripts/algorithms/R/Random Pick with Weight/Random Pick with Weight.js
+++ scripts/algorithms/R/Random Pick with Weight/Random Pick with Weight.js
@@ -1,23 +1,34 @@
-var Solution = function(w) {
-    this.w = [];
-    this.total = w.reduce((collector, el)=>collector+el,0)
-    let start = 0
-    for (const el of w){
-        this.w.push([start, start+el])
-        start = start+el 
+class Solution {
+    constructor(nums) {
+        this.map = new Map();
+        this.sum = 0;
+        
+        nums.forEach((num, i) => {
+            this.sum += num;
+            this.map.set(this.sum, i);
+        })
     }
-};
-
-/**
- * @return {number}
- */
-Solution.prototype.pickIndex = function() {
-    const rand = Math.floor(Math.random()*this.total)
-    for (let i = 0; i < this.w.length ; i++){
-        const pair = this.w[i];
-        if (pair[0] <= rand && rand < pair[1]){
-            return i;
+    pickIndex = function() {
+        const random_sum = Math.floor(Math.random() * this.sum);
+        return this.fetchIndexUsingBS(random_sum);
+    }
+    fetchIndexUsingBS = function(target) {
+        const sums = Array.from(this.map.keys());
+        let lo = 0,
+            hi = sums.length - 1,
+            mid;
+        
+        while(lo <= hi) {
+            mid = Math.floor((hi - lo)/2) + lo;
+            // if((mid === 0 || sums[mid - 1] < target) && sums[mid] >= target) {
+            //     return this.map.get(sums[mid]);
+            // }
+            if(sums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
         }
+        return lo;
     }
-    
-};
+}
