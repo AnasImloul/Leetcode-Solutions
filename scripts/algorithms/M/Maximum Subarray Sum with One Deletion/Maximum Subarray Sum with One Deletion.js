07930482b574780c1b4126dd4eb4989d082ab2e1
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 22:52:17 +0100

    Runtime 69 ms (Top 81.82%) | Memory 45.0 MB (Top 81.82%)

--- scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.js
+++ scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.js
@@ -1,36 +1,31 @@
-// Runtime: 103 ms (Top 59.09%) | Memory: 49.7 MB (Top 31.82%)
 /**
  * @param {number[]} arr
  * @return {number}
  */
+ 
+
+// var maximumSum = function(arr) {
+//     const len = arr.length;
+//     const dp = new Array(len).fill(() => [null, null]);
+//     dp[0][0] = arr[0];
+//     dp[0][1] = 0;
+//     let result = arr[0];
+//     for(let i = 1; i < len; i++) {
+//         dp[i][1] = Math.max(dp[i-1][1] + arr[i], dp[i-1][0]);
+//         dp[i][0] = Math.max(dp[i-1][0] + arr[i], arr[i]);
+//         result = Math.max(result, Math.max(dp[i][0], dp[i][1]));  
+//     }
+    
+//     return result;
+// };
+
+// optimization
 var maximumSum = function(arr) {
-    let n = arr.length
-     let fw = [];
-    let bw = [];
-    let sum = arr[0]
-    let max = arr[0];
-
-    fw[0] = arr[0];
-
-    for(let i = 1; i < n; i++){
-        sum = Math.max(arr[i], sum + arr[i]);
-        max = Math.max(max, sum);
-
-        fw[i] = sum;
+    let noDelete = arr[0], oneDelete = 0, max = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        oneDelete = Math.max(oneDelete + arr[i], noDelete);
+        noDelete = Math.max(noDelete + arr[i], arr[i]);
+        max = Math.max(max, Math.max(noDelete, oneDelete));
     }
-
-    sum = arr[n-1]
-    max = arr[n-1]
-    bw[n - 1] = arr[n - 1];
-     for(let i = n - 2; i >= 0; i--){
-        sum = Math.max(arr[i], sum + arr[i]);
-        max = Math.max(max, sum);
-         bw[i] = sum;
-    }
-
-    let fans = max;
-    for(let i = 1; i < n - 1; i++)
-        fans = Math.max(fans, fw[i - 1] +
-                              bw[i + 1]);
-    return fans;
+    return max;
 };
