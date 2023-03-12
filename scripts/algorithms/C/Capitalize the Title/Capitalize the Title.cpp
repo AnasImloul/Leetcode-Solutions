a2147e142230e6b9106c11ea3da7f680cb8521e4
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 13:28:13 +0100

    Runtime 5 ms (Top 33.4%) | Memory 6.0 MB (Top 52.78%)

--- scripts/algorithms/C/Capitalize the Title/Capitalize the Title.cpp
+++ scripts/algorithms/C/Capitalize the Title/Capitalize the Title.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-	string capitalizeTitle(string title) {
-
-		int len = title.length();
-
-		for(int i = 0; i < len; ++i) {
-
-			int firstIndex = i; // store the first index of the word
-
-			while(i < len && title[i]  != ' ') {
-				title[i] = tolower(title[i]); // converting the character at ith index to lower case ony by one
-				++i;
-			}
-			
-			// if word is of length greater than 2, then turn the first character of the word to upper case
-			if(i - firstIndex > 2) {
-				title[firstIndex] = toupper(title[firstIndex]); // converting the first character of the word to upper case
-			}
-		}
-
-		return title; // return the final result
-	}
+    string capitalize(string s){
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        if(s.length() <= 2){
+            return s;
+        }
+        s[0] = s[0] - 'a' + 'A';
+        return s;
+    }
+    string capitalizeTitle(string title) {
+        string str = "";
+        string ans = "";
+        for(int i = 0; i < title.length(); i++){
+            if(title[i] != ' '){
+                str.push_back(title[i]);
+            }
+            else{
+                str = capitalize(str);
+                ans += str + " ";
+                str = "";
+            }
+        }
+        str = capitalize(str);
+        ans += str;
+        return ans;
+    }
 };
