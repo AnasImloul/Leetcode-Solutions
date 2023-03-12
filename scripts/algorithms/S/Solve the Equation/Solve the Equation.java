d9026c6aaef55c4c8e21bf9dc4fa7aaf7379a177
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-12 22:36:08 +0100

    Runtime 3 ms (Top 82.19%) | Memory 40.0 MB (Top 51.37%)

--- scripts/algorithms/S/Solve the Equation/Solve the Equation.java
+++ scripts/algorithms/S/Solve the Equation/Solve the Equation.java
@@ -1,17 +1,44 @@
-    def solveEquation(self, equation: str) -> str:
-    """ O(N)TS """
-    x, y, p = 0, 0, 1
-    for i in re.finditer(r"=|[+-]?\d*x|[+-]?\d+", equation):
-        g = i.group()
-        if g == '=':
-            p = -1
-        elif g[-1] == 'x':
-            x += p * int(g.replace('x', '1' if len(g) == 1 or not g[-2].isdigit() else ''))
-        else:
-            y += -p * int(g)
-
-    if x == 0 == y:
-        return 'Infinite solutions'
-    elif x == 0:
-        return "No solution"
-    return f'x={y // x}'
+class Solution {
+    public int[] simplifyEqn(String eqn){
+        int prevSign = 1;
+        int sumX = 0;
+        int sumNums = 0;
+        for(int i=0;i<eqn.length();){
+            int coEff = 0;
+            int j = i;
+            while(j<eqn.length() && Character.isDigit(eqn.charAt(j))){
+                coEff = coEff*10 + (eqn.charAt(j)-'0');
+                j++;
+            }
+            if(j<eqn.length() && eqn.charAt(j)=='x'){
+                if(i==j)
+                    coEff = 1;
+                sumX += prevSign*coEff;
+                j++;
+            }
+            else{
+                sumNums += prevSign*coEff; 
+            }
+            if(j<eqn.length() && eqn.charAt(j)=='+')
+                prevSign = 1;
+            else if(j<eqn.length() && eqn.charAt(j)=='-')
+                prevSign = -1;
+            i=++j;
+        }
+        return new int[] {sumX, sumNums};
+    }
+    public String solveEquation(String equation) {
+        String[] leftNRight = equation.split("=");
+        String left = leftNRight[0], right = leftNRight[1];
+        int[] leftEqn = simplifyEqn(left);
+        int[] rightEqn = simplifyEqn(right);
+        int x = leftEqn[0]-rightEqn[0];
+        int num = rightEqn[1]-leftEqn[1];
+        if(x==0)
+            if(num==0)
+                return "Infinite solutions";
+            else
+                return "No solution";
+        return "x="+num/x;
+    }
+}
