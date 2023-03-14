d515f3f71881b38dcb76e9fd672226452ca1c88c
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-14 15:52:37 +0100

    Runtime 0 ms (Top 100.0%) | Memory 6.0 MB (Top 9.4%)

--- scripts/algorithms/A/Add Digits/Add Digits.cpp
+++ scripts/algorithms/A/Add Digits/Add Digits.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int addDigits(int num) {
-      int sum=0;
-        while(num)
-        {
-            sum+=(num%10);
-            num/=10;
-        }
-        if(sum<10)
-            return sum;
-        else
-            return addDigits(sum);
+    int addDigits(int n) {
+        return n < 10 ? n : addDigits(n / 10 + n % 10);
     }
 };
