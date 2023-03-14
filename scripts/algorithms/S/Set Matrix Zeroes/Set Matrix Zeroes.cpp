30b8c9b1f7a11426c5e121c0a52d58af8d884c3c
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-14 15:56:28 +0100

    Runtime 12 ms (Top 83.72%) | Memory 13.0 MB (Top 18.74%)

--- scripts/algorithms/S/Set Matrix Zeroes/Set Matrix Zeroes.cpp
+++ scripts/algorithms/S/Set Matrix Zeroes/Set Matrix Zeroes.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size(), cols = matrix[0].size();
-        int rowFlag = 0, colFlag = 0;
-        
-        for(int i = 0; i < rows; i++){
-            if(matrix[i][0] == 0){
-                rowFlag = 1;
-                break;
-            }
-        }
-        
-        for(int i = 0; i < cols; i++){
-            if(matrix[0][i] == 0){
-                colFlag = 1;
-                break;
-            }
-        }
-        
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        unordered_set<int> r,c;
+        int i,j,m = matrix.size(),n = matrix[0].size();
+        for(i = 0; i < m; i++){
+            for(j = 0; j < n; j++){
+                if(!matrix[i][j]){
+                    r.insert(i);
+                    c.insert(j);
                 }
             }
         }
-        
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < cols; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
+        for(auto &i: r){
+            for(j = 0; j < n; j++){
+                matrix[i][j] = 0;
             }
-        }
-        
-        if(rowFlag){
-            for(int i = 0; i < rows; i++){
-                matrix[i][0] = 0;
-            }
-        }
-        
-        if(colFlag){
-            for(int i = 0; i < cols; i++){
-                matrix[0][i] = 0;
+        }for(auto &i: c){
+            for(j = 0; j < m; j++){
+                matrix[j][i] = 0;
             }
         }
     }
