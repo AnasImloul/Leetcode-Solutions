829ee18f719e2fa9137b0cfec412617589c486fd
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-14 16:21:35 +0100

    Runtime 11 ms (Top 38.81%) | Memory 11.0 MB (Top 8.44%)

--- scripts/algorithms/R/Rotate List/Rotate List.cpp
+++ scripts/algorithms/R/Rotate List/Rotate List.cpp
@@ -1,28 +1,38 @@
-// Runtime: 20 ms (Top 13.46%) | Memory: 11.7 MB (Top 88.59%)
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+// like if it is useful to you
     ListNode* rotateRight(ListNode* head, int k) {
-
-        if(head == nullptr || head->next == nullptr || k == 0) return head;
-
-        ListNode* node = head;
-        int s = 1;
-
-        while(node->next != nullptr){
-            s++;
-            node = node->next;
-        }
-
-        node->next = head;
-        k = k%s;
-
-        while(--s >= k){
-            node = node->next;
-        }
-
-        ListNode* res = node->next;
-        node->next = nullptr;
-        return res;
+       if(head == NULL){
+           return head;
+       }
+       vector<int> nums;
+       ListNode *temp = head;
+       while(temp != NULL){
+           nums.push_back(temp->val);
+           temp = temp->next;
+       }
+// if k greater than size;
+        k = k%nums.size();
+// rotating vector
+     reverse(nums.begin(),nums.end());
+     reverse(nums.begin(),nums.begin()+k);
+     reverse(nums.begin()+k,nums.end());
+// replace value of list    
+ temp = head;
+     for(int i = 0; i<nums.size();i++){
+         temp->val = nums[i];
+         temp = temp->next;
+     }
+     return head;
     }
 };
