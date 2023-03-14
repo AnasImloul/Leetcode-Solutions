23aa5d68302aeda028b1138f7ee774d3a9f863e6
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-14 15:54:23 +0100

    Runtime 533 ms (Top 34.15%) | Memory 6.0 MB (Top 92.68%)

--- scripts/algorithms/T/The Earliest and Latest Rounds Where Players Compete/The Earliest and Latest Rounds Where Players Compete.cpp
+++ scripts/algorithms/T/The Earliest and Latest Rounds Where Players Compete/The Earliest and Latest Rounds Where Players Compete.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    vector<int> earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
-        firstPlayer -= 1, secondPlayer -= 1; 
+    vector<int> earliestAndLatest(int n, int first, int second) {
+        int minRound = INT_MAX, maxRound = INT_MIN;
+
+        function<void(int,int,int,int)> dfs = 
+            [&](int deadMask,int i,int j, int curRound) {
+            
+            while (i < j and deadMask & (1<<i)) // 'i' is dead warrior, try next
+                i += 1;
         
-        map<int, vector<int>> memo; 
-        function<vector<int>(int, int, int, int)> fn = [&](int r, int mask, int i, int j) {
-            if (memo.find(mask) == memo.end()) {
-                if (i >= j) return fn(r+1, mask, 0, n-1); 
-                if (!(mask & (1 << i))) return fn(r, mask, i+1, j); 
-                if (!(mask & (1 << j))) return fn(r, mask, i, j-1); 
-                if ((i == firstPlayer && j == secondPlayer) || (i == secondPlayer && j == firstPlayer)) return vector<int>(2, r); 
-                if (i == firstPlayer || i == secondPlayer) return fn(r, mask^(1<<j), i+1, j-1); 
-                if (j == firstPlayer || j == secondPlayer) return fn(r, mask^(1<<i), i+1, j-1); 
-                else {
-                    vector<int> x = fn(r, mask^(1<<j), i+1, j-1); 
-                    vector<int> y = fn(r, mask^(1<<i), i+1, j-1); 
-                    memo[mask] = {min(x[0], y[0]), max(x[1], y[1])}; 
-                }
+            while (i < j and deadMask & (1<<j)) // 'j' is dead warrior, try next
+                j -= 1;
+
+            if (i >= j) // end of round, no more fights possible
+                dfs(deadMask, 1, n, curRound + 1);
+
+            else if (i == first and j == second) // BATTLE OF THE IMMORTALS
+                minRound = min(curRound,minRound),
+                maxRound = max(curRound,maxRound);
+            
+            else{ // BATTLE includes a mortal  
+                if (i != first and i != second) // 'i' is MORTAL, he may die
+                    dfs(deadMask | (1<<i), i+1, j-1, curRound);
+                if (j != first and j != second) // 'j' is MORTAL, he may die
+                    dfs(deadMask | (1<<j), i+1, j-1, curRound);
+
             }
-            return memo[mask]; 
         };
-        
-        return fn(1, (1<<n)-1, 0, n-1); 
+        dfs(0,1,n,1);
+        return {minRound, maxRound};
     }
 };
