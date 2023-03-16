05bc642377e2cacd13782f9c66337a3ad23e1446
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-17 00:17:47 +0100

    Runtime 667 ms (Top 71.31%) | Memory 13.0 MB (Top 11.81%)

--- scripts/algorithms/N/Number of Ways to Buy Pens and Pencils/Number of Ways to Buy Pens and Pencils.py
+++ scripts/algorithms/N/Number of Ways to Buy Pens and Pencils/Number of Ways to Buy Pens and Pencils.py
@@ -1,10 +1,3 @@
 class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
-        ways = 0;
-        penscost = 0;
-        while penscost <= total:
-            remainingAmount = total - penscost;
-            pencils = remainingAmount//cost2 + 1;
-            ways += pencils;
-            penscost += cost1;
-        return ways
+        return sum((total - pens*cost1) // cost2 + 1 for pens in range(total // cost1 + 1))
