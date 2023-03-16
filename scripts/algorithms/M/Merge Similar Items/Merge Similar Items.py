ca21c6743c5ae6bcc57690f5b88d1cc451c0a1a9
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-17 00:16:47 +0100

    Runtime 128 ms (Top 80.23%) | Memory 14.0 MB (Top 74.21%)

--- scripts/algorithms/M/Merge Similar Items/Merge Similar Items.py
+++ scripts/algorithms/M/Merge Similar Items/Merge Similar Items.py
@@ -1,19 +1,13 @@
 class Solution:
-    from collections import defaultdict
-    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-		
-		# The idea here was to create a hashmap of value to weight mapping
-        # iterate through items1 and populate the hashmap
-		value_weight = defaultdict(int)
-        for i, j in items1:
-            value_weight[i] = j
-		
-        # compare values with hashmap and update the weights
-		for i, j in items2:
-            value_weight[i] += j
-			
-		# return the hashmap in list format
-        ans = []
-        for value in sorted(value_weight.keys()):
-            ans.append([value, value_weight[value]])
-        return ans
+	"""
+	Time:   O((n+m)*log(n+m))
+	Memory: O(n+m)
+	"""
+
+	def mergeSimilarItems(self, first: List[List[int]], second: List[List[int]]) -> List[List[int]]:
+		merged = defaultdict(int)
+
+		for value, weight in first + second:
+			merged[value] += weight
+
+		return sorted(merged.items())
