16fb549e8ef485bbb410351bd2b35ba4432e4329
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-17 00:15:40 +0100

    Runtime 2826 ms (Top 93.23%) | Memory 67.0 MB (Top 73.16%)

--- scripts/algorithms/C/Count Sub Islands/Count Sub Islands.py
+++ scripts/algorithms/C/Count Sub Islands/Count Sub Islands.py
@@ -1,30 +1,28 @@
-class Solution:
-def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-    
-    m=len(grid1)
-    n=len(grid1[0])
-    
-    def dfs(i,j):
-        if i<0 or i>=m or j<0 or j>=n or grid2[i][j]==0:
-            return
+class Solution(object):
+    def countSubIslands(self, grid1, grid2):
+        m=len(grid1)
+        n=len(grid1[0])
         
-        grid2[i][j]=0
-        dfs(i+1,j)
-        dfs(i,j+1)
-        dfs(i,j-1)
-        dfs(i-1,j)
-        
-    # removing all the non-common sub-islands
-    for i in range(m):
-        for j in range(n):
-            if grid2[i][j]==1 and grid1[i][j]==0:
-                dfs(i,j)
-    
-    c=0
-	# counting sub-islands
-    for i in range(m):
-        for j in range(n):
-            if grid2[i][j]==1:
-                dfs(i,j)
-                c+=1
-    return c
+        def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid2[i][j]==0:
+                return 
+            grid2[i][j]=0
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        # here we remove the unnecesaary islands by seeing the point that if for a land in grid2 and water in grid1 it cannot be a subisland and hence island in which this land resides should be removed 
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j]==1 and grid1[i][j]==0:
+                    dfs(i,j)
+		#now we just need to count the islands left over 			
+        count=0
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j]==1:
+                    
+                    dfs(i,j)
+                    count+=1
+        return count 
+		
