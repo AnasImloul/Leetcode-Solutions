fd11cc1ec50c345055bc27a1a633254528df07d5
Anas Imloul <76872415+AnasImloul@users.noreply.github.com>
2023-03-17 00:14:49 +0100

    Runtime 37 ms (Top 48.12%) | Memory 14.0 MB (Top 72.81%)

--- scripts/algorithms/R/Reverse Words in a String/Reverse Words in a String.py
+++ scripts/algorithms/R/Reverse Words in a String/Reverse Words in a String.py
@@ -1,5 +1,40 @@
-class Solution(object):
-    def reverseWords(self, s):
-        tmp = s.split()
-        tmp.reverse()
-        return ' '.join(tmp)
+class Solution:
+    def split(self, s: str, delimiter=" ") -> List[str]:
+        start, end = 0, 0
+
+        res = []
+        for ch in s:
+            if ch == delimiter:
+                if start == end:
+                    start += 1
+                else:
+                    res.append(s[start:end])
+                    start  = end + 1
+            
+            end += 1
+        
+        if start != end:
+            res.append(s[start:end])
+
+        return res
+
+    def reverse_list(self, ll: List[str]) -> List[str]:
+        l, r = 0, len(ll) - 1
+
+        while l < r:
+            ll[l], ll[r] = ll[r], ll[l]
+            l += 1
+            r -= 1
+        
+        return ll
+
+    def reverseWords(self, s: str) -> str:
+
+        # split first
+        splitted_str_list = self.split(s)
+
+        # reverse splitted list
+        reversed_str_list = self.reverse_list(splitted_str_list)
+
+        # join an return
+        return " ".join(reversed_str_list)
