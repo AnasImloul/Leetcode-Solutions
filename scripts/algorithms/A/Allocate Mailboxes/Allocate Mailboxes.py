4fa5216c548efc290f237dcc94b83a21b7ed953e
AnasImloul <pydev27@gmail.com>
2023-04-14 01:11:42 +0000

    Runtime 2663 ms (Top 10.38%) | Memory 14.0 MB (Top 83.77%)

--- scripts/algorithms/A/Allocate Mailboxes/Allocate Mailboxes.py
+++ scripts/algorithms/A/Allocate Mailboxes/Allocate Mailboxes.py
@@ -1,24 +1,27 @@
-from functools import lru_cache
+from statistics import median
+
 class Solution:
     def minDistance(self, houses: List[int], k: int) -> int:
         houses.sort()
-        n = len(houses)
-        @lru_cache(None)
-        def cost(i,j):
-            cost = 0
-            while i < j:
-                cost += houses[j]-houses[i]
-                i += 1
-                j -= 1
-            return cost
-        @lru_cache(None)
-        def dfs(i,k):
-            if i == n:
-                return 0
-            if k == 0:
-                return float('inf')
-            res = float('inf')
-            for j in range(i,n):
-                res = min(res,cost(i,j)+dfs(j+1,k-1))
-            return res
-        return dfs(0,k)
+
+        # cost to serve specific houses with one mailbox
+        def cost_one_box(houses):
+            box = int(median(houses))
+            return sum(abs(house - box) for house in houses)
+
+        # C[i][j] = cost of serving houses 0...i with mailboxes 0...j
+        C = [[0 for _ in range(k)] for _ in range(len(houses))]
+        for j in range(k):
+            C[0][j] = 0
+        for i in range(len(houses)):
+            C[i][0] = cost_one_box(houses[:i+1])
+        
+        # C[i][j] = min([C[i'][j-1] + cost of serving houses i'+1...i with mailbox j] for i' in [0 (or j-1)...i-1]) 
+        for i in range(1, len(houses)):
+            for j in range(1, k):
+                C[i][j] = min(C[_i][j-1] + cost_one_box(houses[_i+1:i+1]) for _i in range(min(i-1,j-1), i))
+                
+        return C[-1][-1]
+                
+
+         
