747d0f0a9318666012e27da92a915685f3ac534d
AnasImloul <pydev27@gmail.com>
2023-04-14 01:07:14 +0000

    Runtime 997 ms (Top 38.52%) | Memory 14.0 MB (Top 66.67%)

--- scripts/algorithms/V/Verbal Arithmetic Puzzle/Verbal Arithmetic Puzzle.py
+++ scripts/algorithms/V/Verbal Arithmetic Puzzle/Verbal Arithmetic Puzzle.py
@@ -1,68 +1,69 @@
-# Runtime: 4515 ms (Top 5.02%) | Memory: 38.7 MB (Top 5.74%)
-from itertools import permutations
 class Solution:
     def isSolvable(self, words: List[str], result: str) -> bool:
-        # we need begins set to track start letters if word is longer than one to not have them as 0
-        begins = {result[0]} if len(result) > 1 else set()
-
-        # the idea to have counter is to sum repeted letters based on where they are in the word
-
-        c = Counter()
-        for w in words:
-            if len(w) > 1:
-                begins.add(w[0])
-            for i,l in enumerate(w[::-1]):
-                c[l] += 10**i
-
-        # we subtract "result" letters from the counter
-        for i,l in enumerate(result[::-1]):
-            c[l] -= 10**i
-
-        # we get at the end something like that Counter({'S': 1000, 'E': 91, 'R': 10, 'D': 1, 'Y': -1, 'N': -90, 'O': -900, 'M': -9000})
-
-        # lets split problem into 2 let's check positive letters and negative
-        pos = []
-        neg = []
-        for l, v in c.items():
-            if v > 0:
-                pos.append( (l,v) )
-            elif v < 0:
-                neg.append( (l,-v) )
-
-        if not neg and not pos:
-            return True
-
-        if not neg or not pos:
-            # False if begins else True this one is needed for words with one letter like words = ["A","B"], result = "A"
-            return False if begins else True
-
-        NN = len(neg)
-        NP = len(pos)
-
-        # we need this one to process permutaion with the smaller number first
-        # that would optimize speed
-        if NN > NP:
-            neg,pos = pos,neg
-            NN,NP = NP, NN
-
-        # we create dict with list of sets of permutations
-        # keys sum of digit for letter * coefficient
-        nd = defaultdict(list)
-        for used in permutations(list(range(10)), NN):
-            if 0 in used and neg[used.index(0)][0] in begins:
-                continue
-            nd[sum(v * used[i] for i,(l,v) in enumerate(neg))].append(set(used))
-
-        # same for the other part
-        # but while we are doing that we check if overlap of digits is missing
-        for used in permutations(list(range(10)), NP):
-            if 0 in used and pos[used.index(0)][0] in begins:
-                continue
-            s = sum(v * used[i] for i,(l,v) in enumerate(pos))
-            s1 = set(used)
-            if s in nd:
-                for s2 in nd[s]:
-                    if not s1 & s2:
-                        return True
-
-        return False
+        
+        # reverse words
+        words = [i[::-1] for i in words]
+        result = result[::-1]
+        allWords = words + [result]
+        
+        # chars that can not be 0
+        nonZero = set()
+        for word in allWords:
+            if len(word) > 1:
+                nonZero.add(word[-1])
+        
+        # numbers selected in backtracking
+        selected = set()
+        # char to Int map
+        charToInt = dict()
+        mxLen = max([len(i) for i in allWords])
+        
+        def res(i = 0, c = 0, sm = 0):
+            if c == mxLen:
+                return 1 if sm == 0 else 0
+            elif i == len(words):
+                num = sm % 10
+                carry = sm // 10
+                if c >= len(result):
+                    if num == 0:
+                        return res(0, c+1, carry)
+                    else:
+                        return 0
+                # result[c] should be mapped to num if a mapping exists
+                if result[c] in charToInt:
+                    if charToInt[result[c]] != num:
+                        return 0
+                    else:
+                        return res(0, c+1, carry)
+                elif num in selected:
+                    return 0
+                # if mapping does not exist, create a mapping
+                elif (num == 0 and result[c] not in nonZero) or num > 0:
+                    selected.add(num)
+                    charToInt[result[c]] = num
+                    ret = res(0, c + 1, carry)
+                    del charToInt[result[c]]
+                    selected.remove(num)
+                    return ret
+                else:
+                    return 0
+            else:
+                word = words[i]
+                if c >= len(word):
+                    return res(i+1, c, sm)
+                elif word[c] in charToInt:
+                    return res(i+1, c, sm + charToInt[word[c]])
+                else:
+                    ret = 0
+                    # possibilities for word[c]
+                    for j in range(10):
+                        if (j == 0 and word[c] not in nonZero) or j > 0:
+                            if j not in selected:
+                                selected.add(j)
+                                charToInt[word[c]] = j
+                                ret += res(i + 1, c, sm + j)
+                                del charToInt[word[c]]
+                                selected.remove(j)
+                    return ret
+        
+        return res() > 0
