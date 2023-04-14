44e16806c33b8565cd0ce51e75ef4c1f75187aec
AnasImloul <pydev27@gmail.com>
2023-04-14 01:09:18 +0000

    Runtime 353 ms (Top 12.5%) | Memory 26.0 MB (Top 46.91%)

--- scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.py
+++ scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.py
@@ -1,22 +1,38 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
-        
-        if len(arr) == 1:
+        n = len(arr)
+        if n ==1:
             return arr[0]
+        dpLeft = [-10**4-1 for _ in range(n)]
+        dpLeft[0] = arr[0]
+        i = 1 
+        while i < n :
+            dpLeft[i] = max(arr[i],dpLeft[i-1]+arr[i])
+            i += 1
+        dpRight = [-10**4-1 for _ in range(n)]
+        dpRight[-1] = arr[-1]
+        j = n-2 
+        while j >= 0:
+            dpRight[j] = max(arr[j],dpRight[j+1]+arr[j])
+            j -= 1
+        k = 0
+        maxi = -10**4-1
+        while k < n:
+            # if we take it 
+            curr_maxi_with = dpRight[k] + dpLeft[k] - arr[k]
+            
+            if k==0:
+                curr_maxi_without = dpRight[k+1]
+            elif k==n-1:
+                curr_maxi_without = dpLeft[k-1]
+            else:
+                if dpLeft[k-1]>=0 and dpRight[k+1]>=0:
+                    curr_maxi_without = dpRight[k+1] + dpLeft[k-1]
+                else:
+                    curr_maxi_without = max(dpLeft[k-1],dpRight[k+1])
+            
+            maxi= max(maxi,curr_maxi_without, curr_maxi_with)
+            k += 1
         
-		# max sum of subarray that includes current and is continuous
-        on_0, on_1 = arr[0], max(0, arr[0]) + arr[1] 
-		# max sum of subarray that includes current and isn't noncontinuous
-        off_0, off_1 = float('-inf'), float('-inf') 
-		
-        ans = max(on_0, on_1, off_0, off_1) # intial ans
-		
-        p = 2 
-        while p < len(arr):
-            
-            on_0, on_1, off_0, off_1 = on_1, max(on_1 + arr[p], arr[p]), off_1, max(on_0 + arr[p], off_1 + arr[p])
-            ans = max(ans, on_1, off_1) # update ans
-            
-            p += 1
+        return maxi 
 
-        return  ans
