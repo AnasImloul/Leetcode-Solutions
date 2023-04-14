6808d60d56d9a4d8dfb49426c195be3261825942
AnasImloul <pydev27@gmail.com>
2023-04-14 15:10:12 +0000

    Runtime 397 ms (Top 6.9%) | Memory 42.0 MB (Top 70.68%)

--- scripts/algorithms/S/Sum of Subarray Minimums/Sum of Subarray Minimums.cpp
+++ scripts/algorithms/S/Sum of Subarray Minimums/Sum of Subarray Minimums.cpp
@@ -1,95 +1,21 @@
+typedef long long ll;
+
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        
-        int n = arr.size();
-        
-        long long mod = 1e9 + 7;
-        
-        vector<int> left_smaller(n, -1);
-        
-        vector<int> right_smaller(n, n);
-        
-        stack<int> st;
-        
-        // fill left_smaller array
-        
-        // find next smaller element on left side
-        
-        for(int i = 0; i < n; i++)
-        {
-            // remove the index with greater element than arr[i]
-            
-            while(!st.empty() && arr[st.top()] >= arr[i])
-            {
-                st.pop();
-            }
-          
-            // if stack is empty
-           
-            if(st.empty())
-            {
-                left_smaller[i] = -1;
-            }
-            else
-            {
-                left_smaller[i] = st.top();
-            }
-            
-            // push the current index into stack
-            
-            st.push(i);
+        const int modval = 1e9 + 7;
+        int n = arr.size(), ans = 0;
+        vector<int> stck;
+        vector<ll> res(n);
+        for (int i = 0; i < n; i++) {
+            while (not stck.empty() and arr[i] <= arr[stck.back()]) stck.pop_back();
+            res[i] = arr[i] * (i - ((stck.empty()) ? -1 : stck.back()));
+            if (not stck.empty()) res[i] += res[stck.back()];
+            res[i] %= modval;
+            stck.push_back(i);
+            cout << res[i] << "\n";
         }
-        
-        while(!st.empty())
-        {
-            st.pop();
-        }
-        
-        // fill the right_smaller array
-        
-        // find the next smaller element on right side
-        
-        for(int i = n - 1; i >= 0; i--)
-        {
-            // remove the index with greater element than arr[i]
-            
-            while(!st.empty() && arr[st.top()] > arr[i])
-            {
-                st.pop();
-            }
-            
-            // if stack is empty
-            
-            if(st.empty())
-            {
-                right_smaller[i] = n;
-            }
-            else
-            {
-                right_smaller[i] = st.top();
-            }
-            
-            // push the current index into stack
-            
-            st.push(i);
-        }
-        
-        // calculate the sum
-        
-        long long sum = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            // sum of total subarrays which has arr[i] as smallest element
-            
-            long long curr = ((right_smaller[i] - i) % mod * (i - left_smaller[i]) % mod * arr[i] % mod) % mod;
-            
-            // update the sum
-            
-            sum = (sum % mod + curr % mod) % mod;
-        }
-        
-        return sum;
+        for (ll i : res) ans = (ans + i) % modval;
+        return ans;
     }
 };
