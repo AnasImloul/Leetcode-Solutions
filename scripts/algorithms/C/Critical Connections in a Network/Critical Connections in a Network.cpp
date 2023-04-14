8330ffb6dd6b7702fce0245c5f1051914c9f5e63
AnasImloul <pydev27@gmail.com>
2023-04-14 01:25:09 +0000

    Runtime 549 ms (Top 97.42%) | Memory 161.0 MB (Top 93.6%)

--- scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.cpp
+++ scripts/algorithms/C/Critical Connections in a Network/Critical Connections in a Network.cpp
@@ -1,49 +1,37 @@
-// Runtime: 1262 ms (Top 24.62%) | Memory: 179.7 MB (Top 55.22%)
 class Solution {
+    int timer = 1;
 public:
-    vector<vector<int>>ans;
-
-    void dfs(vector<vector<int>>&graph , int node , int parent , vector<bool>&vis , int& ct , vector<int>&time , vector<int>&low){
-        vis[node] = 1;
-        time[node] = ct;
-        low[node] = ct++;
-        for(auto value : graph[node]){
-            if(value == parent)continue;
-            if(!vis[value]){
-                dfs(graph , value , node , vis , ct , time , low);
-                low[node] = min(low[node] , low[value]);
-
-                if(low[value] > time[node]){
-                    ans.push_back({node , value});
-                }
-            }else{
-                low[node] = min(low[node] , time[value]);
-            }
+void dfs(vector<int>adj[] , int node , int parent , vector<int>&tin , vector<int>&low , vector<int>&vis , vector<vector<int>>&ans)
+{
+    vis[node] = 1;
+    tin[node] = low[node] = timer;
+    timer++;
+    for(auto it:adj[node])
+    {
+        if(it == parent) continue;
+        if(vis[it] == 0)
+        {
+            dfs(adj , it , node , tin , low , vis , ans);
+            low[node] = min(low[node] , low[it]);
+            if(tin[node] < low[it])
+            ans.push_back({node , it});
+        }
+        else
+        {
+            low[node] = min(low[node] , low[it]);
         }
     }
-
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        ans.clear();
-        vector<vector<int>>graph(n);
-
-        for(int i = 0 ; i < connections.size() ; i += 1){
-            int a = connections[i][0];
-            int b = connections[i][1];
-
-            graph[a].push_back(b);
-            graph[b].push_back(a);
+}
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& con) {
+        vector<int>adj[n];
+        for(int i =  0;i<con.size();i++)
+        {
+            adj[con[i][0]].push_back(con[i][1]);
+            adj[con[i][1]].push_back(con[i][0]);
         }
-
-        vector<bool>vis(n , false);
-        vector<int>time(n , -1);
-        vector<int>low(n , -1);
-        int ct = 0;
-        for(int i = 0 ; i < n ; i += 1){
-            if(!vis[i]){
-                dfs(graph , i , -1 , vis , ct , time , low);
-            }
-        }
-
+        vector<vector<int>>ans;
+        vector<int>vis(n , 0) , tin(n , 0) , low(n , 0);
+        dfs(adj ,  0 , -1 , tin , low , vis , ans);
         return ans;
     }
 };
