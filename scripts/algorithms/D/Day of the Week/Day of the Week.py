2ab6370b07df71e5725bbc0b608367b5eb85bc1c
AnasImloul <pydev27@gmail.com>
2023-04-14 01:05:34 +0000

    Runtime 26 ms (Top 87.8%) | Memory 14.0 MB (Top 18.7%)

--- scripts/algorithms/D/Day of the Week/Day of the Week.py
+++ scripts/algorithms/D/Day of the Week/Day of the Week.py
@@ -1,20 +1,25 @@
-# Runtime: 67 ms (Top 7.61%) | Memory: 13.9 MB (Top 27.96%)
 class Solution:
     def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        dic1={2:"Sunday", 3:"Monday", 4:"Tuesday", 5:"Wednesday", 6:"Thursday", 0:"Friday", 1:"Saturday"}
-        dic2={1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
-        y=year-1971
-        lp=0
-        for i in range(1971,year):
-            if i%4==0:
-                lp+=1
-        dy=y*365+lp
-        dm=0
-        for i in range(1,month):
-            if i==2 and year%4==0 and year!=2100:
-                dm+=1
-            dm+=dic2[i]
-        dd=day
-        td=dd+dm+dy
-        print(td)
-        return dic1[(td-1)%7]
+        LOWEST_DAY, LOWEST_MONTH, LOWEST_YEAR, DAY = 1, 1, 1971, 5
+        DAYS = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+        difference = self.daysBetweenDates((LOWEST_DAY, LOWEST_MONTH, LOWEST_YEAR), (day, month, year))
+        return DAYS[(difference + DAY) % 7]
+
+    def daysBetweenDates(self, date1: tuple, date2: tuple) -> int:
+        LOWEST_YEAR = 1971
+
+        def daysSinceLowest(date: tuple) -> int:
+            day, month, year = date
+
+            isLeapYear = lambda x: 1 if (x % 4 == 0 and x % 100 != 0) or x % 400 == 0 else 0
+
+            days: int = 0
+            # days between the LOWEST_YEAR and year
+            days += 365 * (year - LOWEST_YEAR) + sum(map(isLeapYear, range(LOWEST_YEAR, year)))
+            # days between year and exact date
+            daysInMonth = (31, 28 + isLeapYear(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+            days += sum(daysInMonth[:month - 1]) + day
+            return days
+
+        return abs(daysSinceLowest(date1) - daysSinceLowest(date2))
