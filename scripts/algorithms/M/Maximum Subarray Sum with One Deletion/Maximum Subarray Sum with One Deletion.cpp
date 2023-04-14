dffb1fc3a9c08d1da8a1cfc71bfed723daeeee6e
AnasImloul <pydev27@gmail.com>
2023-04-14 15:11:39 +0000

    Runtime 27 ms (Top 93.1%) | Memory 24.0 MB (Top 24.48%)

--- scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.cpp
+++ scripts/algorithms/M/Maximum Subarray Sum with One Deletion/Maximum Subarray Sum with One Deletion.cpp
@@ -1,17 +1,30 @@
-// Runtime: 73 ms (Top 25.99%) | Memory: 23.3 MB (Top 89.10%)
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-         int n=arr.size();
-        int ans=arr[0];
-        int no_del=arr[0];
-        int del=0;
-        for(int i=1;i<n;i++)
-        {
-            del=max(no_del,del+arr[i]);
-            no_del=max(arr[i],no_del+arr[i]);
-            ans=max(ans,max(del,no_del));
+        int n = arr.size(), curr, maxi = INT_MIN;
+        vector<int> fw(n + 1, 0), bw(n + 1, 0);
+        fw[0] = arr[0];
+        maxi = max(maxi, fw[0]);
+        
+		// ith element denotes the maximum subarray sum with ith element as the last element
+        for(int i = 1; i < n; ++i) {
+            curr = max(arr[i], fw[i - 1] + arr[i]);
+            maxi = max(maxi, curr);
+            fw[i] = curr;
         }
-        return ans;
+        
+		// similar to fw array, but in the backward direction
+        bw[n - 1] = curr = arr[n - 1];
+        for(int i = n - 2; i >= 0; --i) {
+            curr = max(arr[i], bw[i + 1] + arr[i]);
+            maxi = max(maxi, curr);
+            bw[i] = curr;
+        }
+        
+        int res = INT_MIN;
+        for(int i = 1; i < n - 1; ++i) {
+            res = max(res, fw[i - 1] + bw[i + 1]);
+        }
+        return max(res, maxi);
     }
 };
