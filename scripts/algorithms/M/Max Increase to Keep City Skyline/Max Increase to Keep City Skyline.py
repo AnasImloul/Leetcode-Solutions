a1ac3745735a77d9dfdf1028cd7616d88e64037c
AnasImloul <pydev27@gmail.com>
2023-04-14 01:05:58 +0000

    Runtime 80 ms (Top 44.98%) | Memory 14.0 MB (Top 41.48%)

--- scripts/algorithms/M/Max Increase to Keep City Skyline/Max Increase to Keep City Skyline.py
+++ scripts/algorithms/M/Max Increase to Keep City Skyline/Max Increase to Keep City Skyline.py
@@ -1,16 +1,26 @@
-class Solution(object):
-    def maxIncreaseKeepingSkyline(self, grid):
-        rows = cols = len(grid)
-        maxRows, maxCols = [0]*rows, [0]*cols
-        ans = 0
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        Rows_max = {}
+        Cols_max = {}
+        for i in range(n):
+            Rows_max[i] = max(grid[i])
+            Max = 0
+            for j in range(n):
+                Max = max(grid[j][i], Max)
+            
+            Cols_max[i] = Max
+
+
+        count = 0
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] == Rows_max[r] or grid[r][c] == Cols_max[c]:
+                    continue
+                
+                height_to_add = min(Rows_max[r], Cols_max[c])
+                count += height_to_add - grid[r][c]
+                
+        return count
+            
         
-        for i in range(rows):
-            for j in range(cols):
-                maxRows[i] = max(grid[i][j], maxRows[i])
-                maxCols[j] = max(grid[i][j], maxCols[j])
-                
-        for r in range(rows):
-            for c in range(cols):
-                ans += min(maxRows[r], maxCols[c]) - grid[r][c]
-                
-        return ans
