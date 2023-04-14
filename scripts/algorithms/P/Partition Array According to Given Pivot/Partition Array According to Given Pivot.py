14269d9db812caea0aa31a836cc3fa459e88035f
AnasImloul <pydev27@gmail.com>
2023-04-14 01:08:53 +0000

    Runtime 1444 ms (Top 53.91%) | Memory 31.0 MB (Top 38.8%)

--- scripts/algorithms/P/Partition Array According to Given Pivot/Partition Array According to Given Pivot.py
+++ scripts/algorithms/P/Partition Array According to Given Pivot/Partition Array According to Given Pivot.py
@@ -1,3 +1,13 @@
 class Solution:
-    def pivotArray(self, nums: List[int], p: int) -> List[int]:
-        return [n for n in nums if n < p] + [n for n in nums if n == p] + [n for n in nums if n > p]
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left, mid, right=[], [], []
+
+        for itr in nums:
+            if itr<pivot:
+                left.append(itr)
+            elif itr>pivot:
+                right.append(itr)                
+            else:
+                mid.append(itr)
+        
+        return left+mid+right
