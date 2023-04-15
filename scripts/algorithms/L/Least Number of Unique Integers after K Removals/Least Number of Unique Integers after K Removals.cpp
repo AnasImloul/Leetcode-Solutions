4696116fdf101ad0a888260a1f2bd21f27bf5f82
AnasImloul <pydev27@gmail.com>
2023-04-15 14:55:47 +0000

    Runtime 178 ms (Top 79.7%) | Memory 62.0 MB (Top 66.4%)

--- scripts/algorithms/L/Least Number of Unique Integers after K Removals/Least Number of Unique Integers after K Removals.cpp
+++ scripts/algorithms/L/Least Number of Unique Integers after K Removals/Least Number of Unique Integers after K Removals.cpp
@@ -1,33 +1,23 @@
-class Solution {
-public:
-	int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-
-		int n = arr.size();
-
-		unordered_map<int,int> m;
-
-		for(auto it : arr) m[it]++;
-
-		vector<int> v;
-		// storing the frequency of number
-		for(auto it : m) v.push_back(it.second);
-
-		// sorting to least to most frequent
-		sort(v.begin(),v.end());
-
-		for(int i = 0; i < v.size(); i++){
-			if(k <= 0) break;
-			int mn = min(k,v[i]);
-			k -= mn;
-			v[i] -= mn;
-		}
-
-		int ans = 0;
-
-		for(int i = 0; i < v.size(); i++){
-			if(v[i] > 0) ans++;
-		}
-
-		return ans;
-	}
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        int ans;
+        unordered_map<int,int> mp;
+        for(int i=0; i<arr.size(); i++){
+            mp[arr[i]]++;
+        }
+
+        priority_queue< int, vector<int>, greater<int>> pq;
+        for(auto it : mp){
+            pq.push(it.second);
+        }
+
+        while(k>0){
+           k-= pq.top();   
+           if(k>=0){
+              pq.pop(); 
+           }
+        }
+        return pq.size();
+    }
 };
