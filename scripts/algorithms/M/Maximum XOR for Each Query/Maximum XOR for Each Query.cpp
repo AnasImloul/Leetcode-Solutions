223272216efdeffee32dcb9076fd3485921f4e24
AnasImloul <pydev27@gmail.com>
2023-04-15 14:56:15 +0000

    Runtime 208 ms (Top 16.59%) | Memory 97.0 MB (Top 22.87%)

--- scripts/algorithms/M/Maximum XOR for Each Query/Maximum XOR for Each Query.cpp
+++ scripts/algorithms/M/Maximum XOR for Each Query/Maximum XOR for Each Query.cpp
@@ -1,20 +1,23 @@
-// Runtime: 297 ms (Top 34.32%) | Memory: 95.6 MB (Top 61.05%)
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int n=nums.size();
-        vector<int> arr(n,0);
-        arr[0]=nums[0];
-        for(int i=1;i<n;i++){
-            arr[i]=arr[i-1]^nums[i];
+        int n = nums.size(), xoR=0, prevXOR=0;  
+        vector<int> ans;
+        for(auto it:nums)
+            xoR^=it;
+        for(int j=0;j<n;j++){
+            xoR^=prevXOR;
+            int k=0;
+            for(int i=0;i<maximumBit;i++){
+                if((xoR & (1<<i)))
+                    continue;
+                else
+                    k^=(1<<i);
+            }
+            ans.push_back(k);
+            prevXOR=nums.back();
+            nums.pop_back();
         }
-        int idx=0;
-        int k = (1<<maximumBit) - 1;
-
-        for(int i=n-1;i>=0;i--){
-            int curr=k & ~(arr[i]); //invertBits(arr[i]);
-            nums[idx++]=curr;
-        }
-        return nums;
+        return ans;
     }
 };
