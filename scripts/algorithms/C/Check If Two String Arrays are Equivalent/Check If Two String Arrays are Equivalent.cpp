cc063c553db7bec7bed70fb4d67754a4c661ed24
AnasImloul <pydev27@gmail.com>
2023-04-15 14:55:14 +0000

    Runtime 7 ms (Top 39.79%) | Memory 11.0 MB (Top 85.15%)

--- scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.cpp
+++ scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.cpp
@@ -1,16 +1,34 @@
-// Runtime: 18 ms (Top 5.29%) | Memory: 11.7 MB (Top 29.17%)
 class Solution {
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string a,b;
-        for(auto x:word1)
-            a += x;
-        for(auto x:word2)
-            b += x;
-
-        if(a.compare(b)==0)
-            return 1;
-
-        return 0;
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        int wordIdx1 = 0, wordIdx2 = 0, chIdx1 = 0, chIdx2 = 0;
+        while(true)
+        {
+            char ch1 = word1[wordIdx1][chIdx1];
+            char ch2 = word2[wordIdx2][chIdx2];
+            if (ch1 != ch2) return false;
+            
+            chIdx1++; //incrementing the character index of current word from "word1"
+            chIdx2++; //incrementing the character index of current word from "word2";
+            //=========================================================
+            if (chIdx1 == word1[wordIdx1].size()) //if current word from "word1" is over
+            { 
+                wordIdx1++;  //move to next word in "word1"
+                chIdx1 = 0;  //reset character index to 0
+            }
+            if (chIdx2 == word2[wordIdx2].size()) //if  current word from "word2" is over
+            { 
+                wordIdx2++;  //move to next word  in "word2"
+                chIdx2 = 0; //reset character index to 0
+            }
+            //=================================================================
+            if (wordIdx1 == word1.size() && wordIdx2 == word2.size()) break; // words in both arrays are finished
+            
+            if (wordIdx1 == word1.size() || wordIdx2 == word2.size()) return false;
+            //if words in any onr of the arrays are finished and other still has some words in it
+            //then there is no way same string could be formed on concatenation
+        }
+        return true; 
     }
 };
