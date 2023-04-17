10748d2e3f41bd5464c2ed75736c7fbaabcb0abb
AnasImloul <pydev27@gmail.com>
2023-04-17 10:23:18 +0000

    Runtime 111 ms (Top 58.8%) | Memory 36.0 MB (Top 43.74%)

--- scripts/algorithms/M/Minimum Deletions to Make String Balanced/Minimum Deletions to Make String Balanced.cpp
+++ scripts/algorithms/M/Minimum Deletions to Make String Balanced/Minimum Deletions to Make String Balanced.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    //Idea is to traverse from right to left & use stack to keep track if 'b' comes before 'a' in string
-    //then we need to pop & need to delete that character
-    int minimumDeletions(string s) {
-        stack<char> st;
-        int n =s.size();
-        int res = 0;
+    int minimumDeletions(string s){
+
+        vector<int> deletions(s.size()+1, 0);
+        int b_count = 0;
         
-        for(int i = n-1; i >=0; i--){
-            char c = s[i];
-            if(!st.empty() && st.top() < c){
-                res++;
-                st.pop();
-            }else{
-                st.push(c);
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='a'){
+                // Either Delete this 'a' or Delete all previous 'b's.
+                deletions[i+1] = min(deletions[i]+1, b_count);
+            } else{
+                deletions[i+1] = deletions[i];
+                b_count++;
             }
         }
-        
-        return res;
+
+        return deletions[s.size()];
+
+        // The code is contributed by Mufaddal Saifuddin
     }
 };
