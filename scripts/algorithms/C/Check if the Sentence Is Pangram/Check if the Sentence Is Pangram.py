f0a9621fc6e9d759959df1f91692184f2ff18b6d
AnasImloul <pydev27@gmail.com>
2023-04-17 10:41:39 +0000

    Runtime 40 ms (Top 12.41%) | Memory 13.0 MB (Top 5.31%)

--- scripts/algorithms/C/Check if the Sentence Is Pangram/Check if the Sentence Is Pangram.py
+++ scripts/algorithms/C/Check if the Sentence Is Pangram/Check if the Sentence Is Pangram.py
@@ -1,4 +1,3 @@
-# Runtime: 48 ms (Top 54.68%) | Memory: 13.9 MB (Top 56.46%)
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        return len(set(sentence)) == 26
+        return len(set(sentence))==26
