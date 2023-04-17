d11c4c9c0707794856311fc49dca154222ed2e57
AnasImloul <pydev27@gmail.com>
2023-04-17 10:42:43 +0000

    Runtime 87 ms (Top 21.23%) | Memory 16.0 MB (Top 85.41%)

--- scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.py
+++ scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.py
@@ -1,20 +1,22 @@
 class Solution:
-    def findMinDifference(self, timePoints: List[str]) -> int:
-        def str2seconds(x):
-            h, m = x.split(':')
-            return int(h) * 60 + int(m)
-        
-        def getDifference(a, b):
-            
-            return min(b - a, 24 * 60 - b + a)
-        
-        timePoints = map(str2seconds, timePoints)
-        timePoints = sorted(timePoints)
-        
-        n = len(timePoints)
-        result = 2 ** 31
-        for i in range(n - 1):
-            result = min(result, getDifference(timePoints[i], timePoints[i + 1]))
-        result = min(result, getDifference(timePoints[0], timePoints[-1]))
-        
-        return result
+	def findMinDifference(self, timePoints: List[str]) -> int:
+		timePoints.sort()
+
+		def getTimeDiff(timeString1, timeString2):
+			time1 = int(timeString1[:2]) * 60 + int(timeString1[3:])
+			time2 = int(timeString2[:2]) * 60 + int(timeString2[3:])
+
+			minDiff = abs(time1 - time2)
+
+			return min(minDiff, 1440 - minDiff)
+
+		result = getTimeDiff(timePoints[0], timePoints[-1]) # edge case, we need to check minDiff of first and last time after sorting
+
+		for i in range(len(timePoints)-1):
+			curMin = getTimeDiff(timePoints[i], timePoints[i+1])
+
+			if curMin < result:
+				result = curMin
+
+		return result
+		
