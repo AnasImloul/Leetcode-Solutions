a34df7a4f3ef7f919214aa2649657fa5c5fe05d9
AnasImloul <pydev27@gmail.com>
2023-04-17 10:20:32 +0000

    Runtime 34 ms (Top 38.68%) | Memory 13.0 MB (Top 56.1%)

--- scripts/algorithms/C/Complex Number Multiplication/Complex Number Multiplication.py
+++ scripts/algorithms/C/Complex Number Multiplication/Complex Number Multiplication.py
@@ -1,8 +1,11 @@
 class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
-        a1, b1 = num1[:-1].split("+")
-        a2, b2 = num2[:-1].split("+")
-        a1, b1, a2, b2 = int(a1), int(b1), int(a2), int(b2)
-        real = a1*a2-b1*b2
-        imaginary = a1*b2+a2*b1
-        return str(real)+"+"+str(imaginary)+"i"
+        i1=num1.index('+')
+        i2=num2.index('+')
+        a=int(num1[0:i1])
+        x=int(num2[0:i2])
+        b=int(num1[i1+1:len(num1)-1])
+        y=int(num2[i2+1:len(num2)-1])
+        ans1=a*x+(-1)*b*y
+        ans2=a*y+b*x
+        return str(ans1)+'+'+(str(ans2)+'i')
