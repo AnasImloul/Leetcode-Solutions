e6c739c20ece581aa751bcef3440885357f6bc4f
AnasImloul <pydev27@gmail.com>
2023-04-17 11:34:30 +0000

    Runtime 161 ms (Top 90.13%) | Memory 14.0 MB (Top 14.91%)

--- scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.py
+++ scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.py
@@ -1,14 +1,4 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        isPrime = [True] * 33
-        isPrime[0], isPrime[1] = False, False
-        for i in range(33):
-            if isPrime[i]:
-                for j in range(2, 1 + 32 // i):
-                    isPrime[i * j] = False
-        ctr = 0
-        for i in range(left, right + 1):
-            curr = "{:b}".format(i).count("1")
-            if isPrime[curr]:
-                ctr += 1
-        return ctr
+        primes = {2, 3, 5, 7, 11, 13, 17, 19}
+        return sum(bin(n).count('1') in primes for n in range(left, right + 1))
