d3ad1a743bcf8839d45f4c2ee533c8fdd8c1e98d
AnasImloul <pydev27@gmail.com>
2023-04-17 10:43:46 +0000

    Runtime 80 ms (Top 67.73%) | Memory 17.0 MB (Top 13.6%)

--- scripts/algorithms/C/Convert BST to Greater Tree/Convert BST to Greater Tree.py
+++ scripts/algorithms/C/Convert BST to Greater Tree/Convert BST to Greater Tree.py
@@ -1,12 +1,14 @@
 class Solution:
-    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(node,x):
-            if node is None:
-                return x
-            x = dfs(node.right,x)
-            x += node.val
-            node.val = x
-            x = dfs(node.left,x)
-            return x
-        dfs(root,0)
-        return root
+	def convertBST(self, root: TreeNode) -> TreeNode:
+		sum = 0
+		
+		def sol(root: TreeNode) -> TreeNode:
+			nonlocal sum
+			if root:
+				sol(root.right)
+				root.val += sum
+				sum = root.val
+				sol(root.left)
+			return root
+		
+		return sol(root)
