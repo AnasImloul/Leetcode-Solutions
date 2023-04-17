7846c6b5755a76c82dedbc4af501d22875dacdf3
AnasImloul <pydev27@gmail.com>
2023-04-17 10:41:10 +0000

    Runtime 474 ms (Top 57.29%) | Memory 18.0 MB (Top 55.47%)

--- scripts/algorithms/S/Sum of Subarray Minimums/Sum of Subarray Minimums.py
+++ scripts/algorithms/S/Sum of Subarray Minimums/Sum of Subarray Minimums.py
@@ -1,24 +1,16 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        n=len(arr)
-        stack=[]
-        left,right=[-1]*n,[n]*n
-        mod=10**9+7
-        ans=0
-        for i in range(n-1,-1,-1):
-            while stack and arr[stack[-1]]>arr[i]:
-                stack.pop()
-            if stack:
-                right[i]=stack[-1]
-            stack.append(i)
-        stack=[]
+        n = len(arr)
+        small_before = [-1]*n
+        stack = []
         for i in range(n):
-            while stack and arr[stack[-1]]>=arr[i]:
+            while stack and arr[stack[-1]] >= arr[i]:
                 stack.pop()
-            if stack:
-                left[i]=stack[-1]
+            if stack:small_before[i] = stack[-1]
             stack.append(i)
-            ans += (right[i]-i)*(i-left[i])*arr[i]
-            ans%=mod
-        return ans
-                
+        best = [0]*(n+1)
+        ans = 0
+        for i in range(n):
+            best[i] = best[small_before[i]] + (i - small_before[i])*arr[i]
+            ans += best[i]
+        return ans%1000000007
