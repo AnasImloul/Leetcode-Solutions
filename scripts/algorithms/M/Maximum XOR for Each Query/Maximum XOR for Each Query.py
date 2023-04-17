0fd1384aac6bca8ddfc219c72bf24be592dd42c4
AnasImloul <pydev27@gmail.com>
2023-04-17 11:30:34 +0000

    Runtime 951 ms (Top 98.17%) | Memory 30.0 MB (Top 92.2%)

--- scripts/algorithms/M/Maximum XOR for Each Query/Maximum XOR for Each Query.py
+++ scripts/algorithms/M/Maximum XOR for Each Query/Maximum XOR for Each Query.py
@@ -1,3 +1,22 @@
 class Solution:
-    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:        
-        return list(accumulate([nums[0] ^ 2 ** maximumBit - 1] + nums[1:], ixor))[::-1]
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        m,prefix_xor = 2**maximumBit - 1 , list(accumulate(nums,xor))[::-1]
+        return (i^m for i in prefix_xor)
+        
+        """
+          2    3    4    7  ( Given )
+        010  011  100  111  (given in binary)
+        010  001  101  010  (since constrain is high && we have to repeatively calculate xor
+                            we should use prefix-xor)
+       ^  k    k    k    k  (k'th val to xor to get all set bit i.e 111)
+        111  111  111  111  ( this is result and we have to find val of k for each query)
+    
+        
+        NOTE: i ^ k = res, then i ^ res = k
+        So take the prefix-xor and xor every ele with all set bit.
+        all set-bit = pow(2,maximumBit) - 1
+        prefix-xor = accumulate(nums,xor)
+        
+        make sure to use prefix-xor in reverse order because
+        in question we have asked xor of complete list in first query
+        """
