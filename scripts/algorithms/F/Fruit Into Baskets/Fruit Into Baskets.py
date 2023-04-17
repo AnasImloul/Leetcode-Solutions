7030052bcb3174bc0138b057d4de7e57d2b73174
AnasImloul <pydev27@gmail.com>
2023-04-17 11:32:33 +0000

    Runtime 915 ms (Top 76.5%) | Memory 20.0 MB (Top 83.27%)

--- scripts/algorithms/F/Fruit Into Baskets/Fruit Into Baskets.py
+++ scripts/algorithms/F/Fruit Into Baskets/Fruit Into Baskets.py
@@ -1,40 +1,29 @@
 class Solution:
-	def totalFruit(self, nums: List[int]) -> int:
-		"""
-		This problem asks us to find the maximum length of a subarray, 
-		which has maximum 2 unique numbers.
-		I changed the input "fruit -> nums" for easy typing.
-		"""
-		#Define the budget k we can spend
-		k = 2
-		left = 0
-		hashmap = {}
+    def totalFruit(self, fruits: List[int]) -> int:
+        ans=0
+        fruitdict=defaultdict()
+        stack=[]
+        i,j=0,0
 
-		for right in range(len(nums)):
+        while j<len(fruits):
+            if fruits[j] not in fruitdict and len(fruitdict)<2:
+                stack.append(fruits[j])
+                fruitdict[fruits[j]]=j
+                j+=1
 
-			# when we meet a new unique number, 
-			# add it to the hashmap and lower the budget
-			if nums[right] not in hashmap:
-				k -= 1
-				hashmap[nums[right]] = 1
+            elif  fruits[j] in fruitdict:
+                fruitdict[fruits[j]]=j
+                j+=1
 
-			# when we meet a number in our window
-			elif nums[right] in hashmap:
-				hashmap[nums[right]] += 1
-
-			# when we exceed our budget
-			if k < 0:
-				# when we meet a number that can help us to save the budget
-				if hashmap[nums[left]] == 1:
-					del hashmap[nums[left]]
-					k += 1
-				# otherwise we just reduce the freq
-				else:
-					hashmap[nums[left]] -= 1
-				left += 1
-
-		"""
-		We never shrink our window, we just expand the window when it satisfies the condition
-		So, finally the window size is the maximum
-		"""        
-		return right - left + 1
+            else:  
+                if fruitdict[stack[0]]>fruitdict[stack[1]]  :
+                    i = fruitdict[stack[1]]+1
+                    del fruitdict[stack[1]]
+                    stack.pop()
+                else:
+                    i = fruitdict[stack[0]]+1
+                    del fruitdict[stack[0]] 
+                    stack.pop(0)              
+            
+            ans=max(ans,j-i)
+        return ans
