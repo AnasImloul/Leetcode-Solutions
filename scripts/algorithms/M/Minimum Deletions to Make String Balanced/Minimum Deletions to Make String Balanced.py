919f5435491a93bd9dac1a5edd780aea70b0ff7c
AnasImloul <pydev27@gmail.com>
2023-04-17 10:18:34 +0000

    Runtime 1934 ms (Top 5.22%) | Memory 21.0 MB (Top 10.7%)

--- scripts/algorithms/M/Minimum Deletions to Make String Balanced/Minimum Deletions to Make String Balanced.py
+++ scripts/algorithms/M/Minimum Deletions to Make String Balanced/Minimum Deletions to Make String Balanced.py
@@ -1,14 +1,24 @@
 class Solution:
-    def minimumDeletions(self, s: str) -> int:
-        # track the minimum number of deletions to make the current string balanced ending with 'a', 'b'
-        end_a, end_b = 0,0 
-        for val in s:
-            if val == 'a':
-                # to end with 'a', nothing to do with previous ending with 'a'
-                # to end with 'b', need to delete the current 'a' from previous ending with 'b'
-                end_b += 1
-            else:
-                # to end with 'a', need to delete the current 'b' from previous ending with 'a'
-                # to end with 'b', nothing to do, so just pick smaller of end_a, end_b
-                end_a, end_b = end_a+1, min(end_a, end_b)
-        return min(end_a, end_b)
+	def minimumDeletions(self, s: str) -> int:
+		preSum = [0] * (len(s) + 1)
+		sufSum = [0] * (len(s) + 1)
+
+		for i in range(len(s)):
+			if s[i] == "a":
+				preSum[i] += 1 + preSum[i-1]
+
+			else:
+				preSum[i] = preSum[i-1]
+
+			if s[len(s)-i-1] == "b":
+				sufSum[len(s)-i-1] += 1 + sufSum[len(s)-i]
+
+			else:
+				sufSum[len(s)-i-1] += sufSum[len(s)-i]
+
+		maxStringLength = 0
+		for i in range(len(s)):
+			if preSum[i] + sufSum[i] > maxStringLength:
+				maxStringLength = preSum[i] + sufSum[i]
+
+		return len(s) - maxStringLength
