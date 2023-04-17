ba5ca74043bca37064052488097a49d337615de0
AnasImloul <pydev27@gmail.com>
2023-04-17 10:42:14 +0000

    Runtime 923 ms (Top 5.9%) | Memory 14.0 MB (Top 6.54%)

--- scripts/algorithms/M/Max Consecutive Ones III/Max Consecutive Ones III.py
+++ scripts/algorithms/M/Max Consecutive Ones III/Max Consecutive Ones III.py
@@ -1,21 +1,17 @@
+# TC : O(N)
+# SC : O(1)
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        
+        z_count = 0 #count zeors in nums
+        mx_ones = 0
         j = 0
-        
-        ans = 0
-        
-        result = -999999999
-        
         for i in range(len(nums)):
-            if nums[i]==0:
-                ans+=1
-            elif ans>k:
-                result = max(result,j-i)
-
-            while ans>k:
-                if nums[j]==0:
-                    ans-=1
+            if nums[i] == 0: 
+                z_count+=1
+            while z_count>k:#if zeros count cross k decrease count
+                if nums[j] == 0:
+                    z_count-=1
                 j+=1
-
-        return max(result,i-j+1)
+            print(i,j)
+            mx_ones = max(mx_ones, i-j+1)
+        return mx_ones
