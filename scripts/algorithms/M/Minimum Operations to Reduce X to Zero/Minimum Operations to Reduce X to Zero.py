d684a4260850f8fda49a6844df4fff080ff68e07
AnasImloul <pydev27@gmail.com>
2023-04-19 15:32:49 +0000

    Runtime 1153 ms (Top 84.59%) | Memory 27.0 MB (Top 71.48%)

--- scripts/algorithms/M/Minimum Operations to Reduce X to Zero/Minimum Operations to Reduce X to Zero.py
+++ scripts/algorithms/M/Minimum Operations to Reduce X to Zero/Minimum Operations to Reduce X to Zero.py
@@ -1,31 +1,24 @@
-class Solution(object):
-    def minOperations(self, nums, x):
-        """
-        :type nums: List[int]
-        :type x: int
-        :rtype: int
-        """
-        maxLen = total = windowStart = 0
-        targetSum = sum(nums)
-        
-        # edge cases
-        if targetSum < x: return -1
-        if targetSum == x: return len(nums)
-        
-        # opposite of sliding window but still do sliding window
-        # so instead of find continous subarray,
-        # we need to find outer points that is not included in continous subarray
-        k = targetSum - x # this is for continous subarray
-        
-        for windowEnd in range(len(nums)):
-            total += nums[windowEnd]
-            
-            while total > k:
-                total -= nums[windowStart]
-                windowStart += 1
-            
-            if total == k:
-                maxLen = max(maxLen, windowEnd - windowStart + 1)
-    
-        
-        return len(nums) - maxLen if maxLen != 0 else -1
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        start =0
+        end = len(nums)-1
+        totalsum = sum(nums)
+        check = 0
+        windowlen =0
+        s = totalsum - x # s in case1 = 11-5 =6
+        if totalsum<x:
+            return -1
+        if sum(nums) ==x:
+            return len(nums)
+
+        for end in range(len(nums)):
+            check += nums[end]
+            while check > s:
+                check -= nums[start]
+                start +=1
+            if check ==s:
+                windowlen = max(windowlen,end-start+1)
+        if windowlen !=0:
+            return len(nums)-windowlen
+        else:
+            return -1        
