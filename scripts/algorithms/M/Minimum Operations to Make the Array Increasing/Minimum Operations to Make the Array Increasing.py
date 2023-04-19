4ddaf95d4a4d2f86510fad3173d95f8314523e13
AnasImloul <pydev27@gmail.com>
2023-04-19 15:35:29 +0000

    Runtime 121 ms (Top 90.49%) | Memory 14.0 MB (Top 99.36%)

--- scripts/algorithms/M/Minimum Operations to Make the Array Increasing/Minimum Operations to Make the Array Increasing.py
+++ scripts/algorithms/M/Minimum Operations to Make the Array Increasing/Minimum Operations to Make the Array Increasing.py
@@ -1,9 +1,13 @@
-# Runtime: 357 ms (Top 5.07%) | Memory: 14.8 MB (Top 8.90%)
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(len(nums)-1):
-            if nums[i] >= nums[i+1]:
-                count += (nums[i]+1)-nums[i+1]
-                nums[i+1] = nums[i]+1
-        return count
+        sol = 0
+        last = nums[0]
+
+        for i in range(len(nums) - 1):
+            if last >= nums[i + 1]:
+                sol += last - nums[i + 1] + 1
+                last = last + 1
+            else:
+                last = nums[i + 1]
+        
+        return sol
