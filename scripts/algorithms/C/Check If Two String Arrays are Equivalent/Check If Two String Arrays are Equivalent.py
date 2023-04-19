1545254c6c4b6525df4aeda4c0d740dfe29699de
AnasImloul <pydev27@gmail.com>
2023-04-19 15:04:25 +0000

    Runtime 41 ms (Top 12.2%) | Memory 13.0 MB (Top 62.33%)

--- scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.py
+++ scripts/algorithms/C/Check If Two String Arrays are Equivalent/Check If Two String Arrays are Equivalent.py
@@ -1,5 +1,3 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        word1 = "".join(word1)
-        word2 = "".join(word2)
-        return word1 == word2
+        return True if "".join(word1) == "".join(word2) else False
