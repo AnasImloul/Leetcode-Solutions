d9a42dcbe2a21b62130e2eee189251200221329a
AnasImloul <pydev27@gmail.com>
2023-04-19 15:33:19 +0000

    Runtime 620 ms (Top 25.92%) | Memory 35.0 MB (Top 23.6%)

--- scripts/algorithms/L/Least Number of Unique Integers after K Removals/Least Number of Unique Integers after K Removals.py
+++ scripts/algorithms/L/Least Number of Unique Integers after K Removals/Least Number of Unique Integers after K Removals.py
@@ -1,12 +1,19 @@
-from heapq import heapify, heappop
-class Solution:
-    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        freqs = Counter(arr)
-        counts = list(freqs.values())
-        heapify(counts)
-        while k > 0:
-            if counts[0] <= k:
-                k -= heappop(counts)
-            else:
-                break
-        return len(counts)
+from heapq import heappop, heapify
+
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        counter = collections.Counter(arr)
+        min_heap = [(count, num) for num, count in counter.items()]
+
+        heapify(min_heap)
+
+        while k > 0:
+            count, num = min_heap[0]
+
+            if count > k:
+                break
+
+            heappop(min_heap)
+            k -= count
+
+        return len(min_heap)
