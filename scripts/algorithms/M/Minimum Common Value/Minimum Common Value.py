746f209ff6baff5c27c3f6b8c9568f4eb46c9769
AnasImloul <pydev27@gmail.com>
2023-04-19 14:58:49 +0000

    Runtime 463 ms (Top 59.73%) | Memory 32.0 MB (Top 99.33%)

---
+++ scripts/algorithms/M/Minimum Common Value/Minimum Common Value.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            if nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return -1
