4cdca8589a0cbe4345371637e8df39b2da1d5ea5
AnasImloul <pydev27@gmail.com>
2023-04-19 15:31:14 +0000

    Runtime 33 ms (Top 44.0%) | Memory 13.0 MB (Top 91.27%)

--- scripts/algorithms/M/Minimum Sum of Four Digit Number After Splitting Digits/Minimum Sum of Four Digit Number After Splitting Digits.py
+++ scripts/algorithms/M/Minimum Sum of Four Digit Number After Splitting Digits/Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -1,13 +1,5 @@
 class Solution:
     def minimumSum(self, num: int) -> int:
-
-
-        nums=[int(ch) for ch in str(num)]
-
-        nums.sort()
-
-        val1=int(str(nums[0]) + str(nums[-1]))
-        val2=int(str(nums[1]) + str(nums[2]))
-
-        return val1+val2
-        
+        s=list(str(num))
+        s.sort()
+        return int(s[0]+s[2])+int(s[1]+s[3])
