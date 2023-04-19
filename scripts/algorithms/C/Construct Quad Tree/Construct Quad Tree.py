ecc7fab48a8b4d8c6fec2be71a4dc7beaed47ad8
AnasImloul <pydev27@gmail.com>
2023-04-19 15:00:59 +0000

    Runtime 115 ms (Top 56.25%) | Memory 14.0 MB (Top 37.11%)

--- scripts/algorithms/C/Construct Quad Tree/Construct Quad Tree.py
+++ scripts/algorithms/C/Construct Quad Tree/Construct Quad Tree.py
@@ -11,23 +11,31 @@ class Node:
 """
 
 class Solution:
-    def construct(self, grid: List[List[int]]) -> 'Node': # grid is a square
-        end = len(grid)
-        gridSum = sum([sum(row) for row in grid])
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def is_grid_a_leaf(mylist, n):
+            x = mylist[0][0]
+            for i in range(n):
+                for j in range(n):
+                    if mylist[i][j] != x:
+                        return (0,0)
+            return (1,x)
         
-        if gridSum == end**2:
-            return Node(1, True)
-        
-        elif gridSum == 0:
-            return Node(0, True)
-        
-        else:
-            node = Node(None, False)
-            end //= 2
-            node.topLeft = self.construct([row[:end] for row in grid[:end]])
-            node.bottomLeft = self.construct([row[:end] for row in grid[end:]])
-            node.topRight = self.construct([row[end:] for row in grid[:end]])
-            node.bottomRight = self.construct([row[end:] for row in grid[end:]])
+        def tree_builder(currlist) -> 'Node':
+            node = Node()
+            n = len(currlist)
+            node.isLeaf, node.val = is_grid_a_leaf(currlist, n)
             
-        return node
-
+            if node.isLeaf:
+                node.topLeft = node.topRight = node.bottomLeft = node.bottomRight = None
+            else:
+                top_left = [[row[i] for i in range(n//2)] for row in currlist[0:n//2]]
+                top_right= [[row[i] for i in range(n//2,n)] for row in currlist[0:n//2]]
+                bot_left = [[row[i] for i in range(n//2)] for row in currlist[n//2:n]]
+                bot_right= [[row[i] for i in range(n//2,n)] for row in currlist[n//2:n]]
+                node.topLeft = tree_builder(top_left)
+                node.topRight = tree_builder(top_right)
+                node.bottomLeft = tree_builder(bot_left)
+                node.bottomRight = tree_builder(bot_right)
+            return node
+        
+        return tree_builder(grid)
