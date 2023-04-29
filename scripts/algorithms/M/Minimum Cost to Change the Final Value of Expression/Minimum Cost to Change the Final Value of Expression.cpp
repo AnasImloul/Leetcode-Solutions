298e2cdba1e2b8478b1e8f9616a0901746ff1036
AnasImloul <pydev27@gmail.com>
2023-04-29 16:40:24 +0100

    Runtime 104 ms (Top 35.9%) | Memory 16.0 MB (Top 82.5%)

--- scripts/algorithms/M/Minimum Cost to Change the Final Value of Expression/Minimum Cost to Change the Final Value of Expression.cpp
+++ scripts/algorithms/M/Minimum Cost to Change the Final Value of Expression/Minimum Cost to Change the Final Value of Expression.cpp
@@ -1,112 +1,68 @@
 class Solution {
 public:
-    int minOperationsToFlip(string expression) {
-        expression = "(" + expression + ")";
-        splitParantheses(expression);
-        sort(p.begin(), p.end(), [](Parantheses &a, Parantheses &b){
-            return a.order > b.order;
-        });
-        for (int i = 0; i < p.size(); i++)
-            solveParantheses(expression, i);
-        return evaluations[0].toFlip;
-    }
-private:
-    class Parantheses{
-    public:
-        int open;
-        int close;
-        int eval;
-        int toFlip;
-        int order;
-    };
-    unordered_map<int, Parantheses> evaluations; // information about Paratheses stored by index of open bracket.
-    vector<Parantheses> p;
-    void splitParantheses(string &e){
-        int open = 0;
-        stack<int> opened;
-        for (int i = 0; i < e.size(); i++)
-            switch (e[i]){
-                case '(':
-                    open++;
-                    opened.push(i);
-                    break;
-                case ')':
-                    open--;
-                    Parantheses para;
-                    para.open = opened.top();
-                    opened.pop();
-                    para.close = i;
-                    para.order = open;
-                    p.push_back(para);
-                    break;
-            }
-    }
-    void solveParantheses(string &e, int index){
-        int open = p[index].open;
-        int close = p[index].close;
-        int poz = open + 1;
-        int eval = -1; // evaluation of the expression so far (-1 because we need to read a value first)
-        int toFlipEval; // number of flips needed to change current evaluation.
-        for (int i = open + 1; i < close; i++){
-            if (e[i] == '&' || e[i] == '|'){
-                op = e[i];
-                continue;
-            }
-            int val; // value of th ecurrent term
-            int toFlipVal; // number of flips needed to change current term.
-            if (evaluations.count(i)){
-                val = evaluations[i].eval;
-                toFlipVal = evaluations[i].toFlip;
-                i = evaluations[i].close;
-            }
-            else if (e[i] == '0'){
-                val = 0;
-                toFlipVal = 1;
-            }
-            else if (e[i] == '1'){
-                val = 1;
-                toFlipVal = 1;
+    int minOperationsToFlip(string exp) {
+        stack<pair<char,int>> st;   //exp val,cost
+
+        pair<char,int> p;
+
+        for(auto x:exp){
+            if(x=='(' || x=='&' || x=='|'){
+                st.push({x,0});   //till now no looking need to change exp so mark cost as 0 or we can add cost of these while solving exp
             }
             else{
-                printf("ERROR, e[i] = %c\n", e[i]);
-            }
-            
-            if (eval == -1){
-                eval = val;
-                toFlipEval = toFlipVal;
-            } else {
-                if (op == '&'){
-                    if (eval == 1 && val == 1){
-                        toFlipEval = min(toFlipEval, toFlipVal);
-                    } else{
-                        int changeJustTerm, changeOperatorAndTerm;
-                        changeJustTerm = !eval * toFlipEval + !val * toFlipVal;
-                        if (eval != val)
-                            changeOperatorAndTerm = 1;
-                        else
-                            changeOperatorAndTerm = min(toFlipEval, toFlipVal) + 1;
-                        toFlipEval = min(changeJustTerm, changeOperatorAndTerm);
-                        eval = 0;
-                        
+                if(x==')'){
+                    p=st.top();
+                    st.pop();
+                    st.pop();   //this pop for '(' 
+                }
+                else{
+                    //if number or we can say '0' or '1'
+                    p={x,1};    //cost going to be one becoz x if either '0' or '1'
+                }
+                while(!st.empty() && (st.top().first=='&' || st.top().first=='|')){
+                    char op=st.top().first;
+                    st.pop();
+                    char val2=p.first;
+                    int cost2=p.second;
+                    char val1=st.top().first;
+                    int cost1=st.top().second;
+                    st.pop();
+
+                    if(op=='&'){
+                        if(val1=='1' && val2=='1'){
+                            p={'1',min(cost1,cost2)};
+                        }
+                        if(val1=='1' && val2=='0'){
+                            p={'0',1};
+                        }
+                        if(val1=='0' && val2=='1'){
+                            p={'0',1};
+                        }
+                        if(val1=='0' && val2=='0'){
+                            p={'0',min(1+cost1,1+cost2)};
+                        }
                     }
-                } else{
-                    if (eval == 0 && val == 0){
-                        toFlipEval = min(toFlipEval, toFlipVal);
-                    } else{
-                        int changeJustTerm, changeOperatorAndTerm;
-                        changeJustTerm = eval * toFlipEval + val * toFlipVal;
-                        if (eval != val)
-                            changeOperatorAndTerm = 1;
-                        else
-                            changeOperatorAndTerm = min(toFlipEval, toFlipVal) + 1;
-                        toFlipEval = min(changeJustTerm, changeOperatorAndTerm);
-                        eval = 1;
+
+                    if(op=='|'){
+                        if(val1=='1' && val2=='1'){
+                            p={'1',1+min(cost1,cost2)};
+                        }
+                        if(val1=='1' && val2=='0'){
+                            p={'1',1};
+                        }
+                        if(val1=='0' && val2=='1'){
+                            p={'1',1};
+                        }
+                        if(val1=='0' && val2=='0'){
+                            p={'0',min(cost1,cost2)};
+                        }
                     }
                 }
+                st.push(p);
+
             }
         }
-        p[index].eval = eval;
-        p[index].toFlip = toFlipEval;
-        evaluations[open] = p[index];
+        return st.top().second;
     }
 };
+//code by sachin
