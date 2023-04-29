45f8730eb03ef013f38fecdec71d253e5558c80f
AnasImloul <pydev27@gmail.com>
2023-04-29 17:27:42 +0100

    Runtime 0 ms (Top 100.0%) | Memory 5.0 MB (Top 98.78%)

--- scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.cpp
+++ scripts/algorithms/D/Domino and Tromino Tiling/Domino and Tromino Tiling.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-	#define ll long long
-	const int mod = 1e9 + 7;
-	int numTilings(int n) {
-		vector<vector<ll>> dp(n+1,vector<ll>(4,0));
-
-		dp[0][3]=1;
-
-		for(int i=1; i<=n; i++){
-			dp[i][0] += dp[i-1][3];
-
-			dp[i][1] += (dp[i-1][2] + dp[i-1][0]) % mod;            
-
-			dp[i][2] += (dp[i-1][1] + dp[i-1][0]) % mod;
-
-			dp[i][3] += (dp[i-1][3] + dp[i-1][1] + dp[i-1][2] + dp[i-1][0]) % mod;
-		}
-
-		return dp[n][3];
-	}
+    int numTilings(int n) {
+        long dp[n+1];
+        dp[0]=1;
+        for(int i=1; i<=n; i++){
+            if(i<3)
+                dp[i]=i;
+            else
+                dp[i] = (dp[i-1]*2+dp[i-3])%1000000007;
+        }
+        return (int)dp[n];
+    }
 };
