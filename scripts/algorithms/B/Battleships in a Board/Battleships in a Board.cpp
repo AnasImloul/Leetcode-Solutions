e3c71f2abba4143eb3375f83737b58efc5a532db
AnasImloul <pydev27@gmail.com>
2023-04-29 17:29:26 +0100

    Runtime 7 ms (Top 77.62%) | Memory 9.0 MB (Top 5.66%)

--- scripts/algorithms/B/Battleships in a Board/Battleships in a Board.cpp
+++ scripts/algorithms/B/Battleships in a Board/Battleships in a Board.cpp
@@ -1,29 +1,105 @@
 class Solution {
-    int m, n;
-    vector<int> dx{-1, 0, 1, 0};
-    vector<int> dy{0, 1, 0, -1};
 public:
-    int countBattleships(vector<vector<char>>& board) {
-        
-        int cnt = 0;
-        m = board.size(), n = board[0].size();
-        
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if(board[i][j] == 'X')
-                    cnt++, helper(board, i, j);
-        
-        return cnt;
-    }
-    
-    void helper(vector<vector<char>> &board, int i, int j)
-    {
-        board[i][j] = '.';
-        for(int k = 0; k < dx.size(); k++)
-        {
-            int x = i + dx[k], y = j + dy[k];
-            if(x >= 0 && y >= 0 && x < m && y < n && board[x][y] == 'X')
-                helper(board, x, y);
+
+    bool isSafe(int i,int j,int r,int c,vector<vector<char>> &arr,vector<vector<bool>> &visited){
+           if(i<0 || i>r || j<0 || j>c){
+             return false;
+         }
+       //  cout<<arr[i][j]<<" ";
+        if(arr[i][j]!='X'){
+            return false;
         }
+        if(arr[i][j] == 'X' && visited[i][j] == true){
+            return false;
+        }
+
+        if(arr[i][j] == 'X' && visited[i][j] == false){
+            return true;
+        }
+         return false;
+    
+    }
+
+    void solve(vector<vector<char>> &arr,vector<vector<bool>> &visited,int &r,int &c,int i,int j){
+
+        if((i>r && j>c) && (i<0 && j<0)){
+            return;
+        }
+
+        // down (i+1,j)
+        if(isSafe(i+1,j,r,c,arr,visited)){
+             visited[i+1][j] = true;
+             solve(arr,visited,r,c,i+1,j);
+        }else{
+            // if(arr[i][j] == 'X'){
+            //      visited[i][j] = true;
+            // }
+        }
+        // // right (i,j+1)
+
+        if(isSafe(i,j+1,r,c,arr,visited)){
+             visited[i][j+1] = true;
+             solve(arr,visited,r,c,i,j+1);
+        }else{
+            // if(arr[i][j] == 'X'){
+            //      visited[i][j] = true;
+            // }
+        }
+        // left (i,j-1);
+        if(isSafe(i,j-1,r,c,arr,visited)){
+            visited[i][j-1] = true;
+             solve(arr,visited,r,c,i,j-1);
+        }else{
+            // if(arr[i][j] == 'X'){
+            //      visited[i][j] = true;
+            // }
+        }
+        // up   (i-1,j)
+        if(isSafe(i-1,j,r,c,arr,visited)){
+             visited[i-1][j] = true;
+             solve(arr,visited,r,c,i-1,j);
+        }else{
+            // if(arr[i][j] == 'X'){
+            //      visited[i][j] = true;
+            // }
+        }
+
+    }
+    int countBattleships(vector<vector<char>>& board) {
+        int row = board.size()-1;
+        int col = board[0].size()-1;
+        vector<vector<bool>> isVisited(row+1,vector<bool>(col+1,false));
+        vector<vector<int>> toStart;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j] == 'X'){
+                    vector<int> temp(2);
+                    temp[0] = i;
+                    temp[1] = j;
+                  //  cout<<i<<" "<<j<<endl;
+                    toStart.push_back(temp);
+                }
+            }
+        }
+        int ans = 0;
+         for(int i=0;i<toStart.size();i++){
+            if(isVisited[toStart[i][0]][toStart[i][1]]){
+
+            }else{
+            isVisited[toStart[i][0]][toStart[i][1]] = true;
+             solve(board,isVisited,row,col,toStart[i][0],toStart[i][1]);
+             ans++;
+            }
+             
+         }
+
+        //  for(auto cc:isVisited){
+        //      for(auto c:cc){
+        //          cout<<c<<" ";
+        //      }
+        //      cout<<endl;
+        //  }
+
+        return ans;
     }
 };
