c52f032cc682561bbea3db9a9ce5cbbed6134f39
AnasImloul <pydev27@gmail.com>
2023-04-29 18:18:12 +0100

    Runtime 163 ms (Top 90.2%) | Memory 97.0 MB (Top 95.51%)

--- scripts/algorithms/F/Find Closest Node to Given Two Nodes/Find Closest Node to Given Two Nodes.cpp
+++ scripts/algorithms/F/Find Closest Node to Given Two Nodes/Find Closest Node to Given Two Nodes.cpp
@@ -1,59 +1,52 @@
-// Runtime: 308 ms (Top 59.56%) | Memory: 97.7 MB (Top 72.24%)
 class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        
+        // special case, if node1 and node2 are same the answer will be itself
+        if(node1 == node2)
+            return node1;
 
         int n = edges.size();
+        // distance vectors for node1 and node2
+        vector<int> m1(n, -1);
+        vector<int> m2(n, -1);
+        int ans = -1;
+        int maxD = n + 1;
 
-        // 2 distance vectors to store the distance of all the nodes from node1 and node2
-        vector<int> dist1(n, -1);
-        vector<int> dist2(n, -1);
-
-        // 2 visited vectors to keep a track of all the nodes visited from node1 and node2
-        // this is done to deal with the possible cycles in the graph
-        vector<bool> vis1(n, false);
-        vector<bool> vis2(n, false);
-
-        int curNode = node1;
-        int curDist = 0;
-
-        // iterate over the edges and keep track of the distance between node1 and current node
-        // if curNode == -1, end of path reached.
-        // if vis[curNode] == true, cycle detected.
-
-        while(curNode!=-1 && vis1[curNode] == false) {
-            vis1[curNode] = true;
-            dist1[curNode] = curDist;
-            curNode = edges[curNode];
-            curDist++;
+        int node = node1;
+        int dist = 0;
+        // calculate distance from node1       
+        while(node != -1 && m1[node] == -1)
+        {
+            m1[node] = dist;
+            dist++;
+            node = edges[node];   
         }
 
-        // repeat above steps for node2
-        curNode = node2;
-        curDist = 0;
-        while(curNode!=-1 && vis2[curNode] == false) {
-            vis2[curNode] = true;
-            dist2[curNode] = curDist;
-            curNode = edges[curNode];
-            curDist++;
-        }
-
-        // iterate over the distance vectors and check if the current node has path from node1 and node 2
-        // the distance from both nodes to the current node will be saved in the distance vectors
-
-        int minDist = INT_MAX;
-        int index = -1;
-        for(int i=0;i<n;i++) {
-            if(dist1[i] != -1 && dist2[i]!=-1) {
-                int maxDist = max(dist1[i], dist2[i]);
-                if(maxDist < minDist) {
-                    minDist = maxDist;
-                    index = i;
+        node = node2;
+        dist = 0;
+        // calculate distance from node2 while checking for the same node in distance vector for node1
+        while(node != -1 && m2[node] == -1)
+        {
+            if(m1[node] != -1)
+            {
+                if(maxD > max(dist, m1[node]))
+                {
+                    ans = node;
+                    maxD = max(dist, m1[node]);
+                }
+                else if(maxD == max(dist, m1[node]))
+                {
+                    if(ans > node)
+                        ans = node;
                 }
             }
+
+            m2[node] = dist;
+            dist++;
+            node = edges[node];
         }
 
-        return index;
-
+        return ans;
     }
 };
