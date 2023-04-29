a682308899edee9adbbab03907be4b8b70c181b8
AnasImloul <pydev27@gmail.com>
2023-04-29 18:13:39 +0100

    Runtime 7 ms (Top 96.1%) | Memory 20.0 MB (Top 96.16%)

--- scripts/algorithms/B/Balanced Binary Tree/Balanced Binary Tree.cpp
+++ scripts/algorithms/B/Balanced Binary Tree/Balanced Binary Tree.cpp
@@ -1,26 +1,23 @@
-// Runtime: 8 ms (Top 96.99%) | Memory: 20.9 MB (Top 54.47%)
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
-    }
-    int height(TreeNode* root){
-        if(!root)
-            return 1;
-
-        int left = height(root->left);
-
-        if(left == -1)
-            return -1;
-
-        int right = height(root->right);
-
-        if(right == -1)
-            return -1;
-
-        if(abs(left - right) > 1)
-            return -1;
-
-        return 1 + max(left, right);
+        // If the tree is empty, we can say it’s balanced...
+        if (root == NULL)  return true;
+        // Height Function will return -1, when it’s an unbalanced tree...
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+    // Create a function to return the “height” of a current subtree using recursion...
+	int Height(TreeNode* root) {
+        // Base case...
+		if (root == NULL)  return 0;
+        // Height of left subtree...
+		int leftHeight = Height(root->left);
+        // Height of height subtree...
+		int rightHight = Height(root->right);
+        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
+		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
+        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
+		return max(leftHeight, rightHight) + 1;
     }
 };
