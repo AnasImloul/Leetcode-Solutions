8a1eae2a6f81036e7c417c7e0289f191c801eeee
AnasImloul <pydev27@gmail.com>
2023-04-29 17:30:43 +0100

    Runtime 13 ms (Top 81.71%) | Memory 17.0 MB (Top 70.86%)

--- scripts/algorithms/X/X of a Kind in a Deck of Cards/X of a Kind in a Deck of Cards.cpp
+++ scripts/algorithms/X/X of a Kind in a Deck of Cards/X of a Kind in a Deck of Cards.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    int gcd(int a,int b)
+    {
+        while(a>0 && b>0)
+        {
+            if(a>b) a=a%b;
+            else b=b%a;
+        }
+        return (a==0? b:a);
+    }
     bool hasGroupsSizeX(vector<int>& deck) {
         unordered_map<int,int> mp;
-        for(auto it: deck){
-            mp[it]++;
+        vector<int> v;
+        for(auto i:deck)
+        {
+            mp[i]++;
         }
-        int fin=0;
-        for(auto it: mp){
-            fin=__gcd(fin,it.second);
+        for(auto it:mp)
+        {
+            v.push_back(it.second);
         }
-        return fin>=2;
+        int g=-1;
+        for(int i=0;i<v.size();i++)
+        {
+            if(g==-1)
+                g=v[i];
+            else 
+                g=gcd(g,v[i]);
+        }
+        return g>1;
     }
 };
