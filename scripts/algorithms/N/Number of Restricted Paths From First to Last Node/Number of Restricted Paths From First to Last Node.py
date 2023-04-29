f58f5f50bfae9a904dc616251dc2c761f47b83ae
AnasImloul <pydev27@gmail.com>
2023-04-29 16:23:00 +0100

    Runtime 2915 ms (Top 5.64%) | Memory 43.0 MB (Top 83.8%)

--- scripts/algorithms/N/Number of Restricted Paths From First to Last Node/Number of Restricted Paths From First to Last Node.py
+++ scripts/algorithms/N/Number of Restricted Paths From First to Last Node/Number of Restricted Paths From First to Last Node.py
@@ -1,32 +1,57 @@
-# Runtime: 5534 ms (Top 5.35%) | Memory: 55.9 MB (Top 70.78%)
 class Solution:
     def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
-        graph = collections.defaultdict(list) # build graph
-        for a, b, w in edges:
-            graph[a].append((w, b))
-            graph[b].append((w, a))
-        heap = graph[n]
-        heapq.heapify(heap)
-        d = {n: 0}
-        while heap: # Dijkstra from node `n` to other nodes, record shortest distance to each node
-            cur_w, cur = heapq.heappop(heap)
-            if cur in d: continue
-            d[cur] = cur_w
-            for w, nei in graph[cur]:
-                heapq.heappush(heap, (w+cur_w, nei))
-        graph = collections.defaultdict(list)
-        for a, b, w in edges: # pruning based on `restricted` condition, make undirected graph to directed-acyclic graph
-            if d[a] > d[b]:
-                graph[a].append(b)
-            elif d[a] < d[b]:
-                graph[b].append(a)
-        ans, mod = 0, int(1e9+7)
-        @cache
-        def dfs(node): # use DFS to find total number of paths
-            if node == n:
-                return 1
-            cur = 0
-            for nei in graph[node]:
-                cur = (cur + dfs(nei)) % mod
-            return cur
-        return dfs(1)
+        
+        dct_nd    = {}
+        dist_to_n = {}
+        queue     = deque() # from n-node to 1-node
+        visited   = set()
+        
+        # 1 step: create dictionary with nodes and nodes' distances to n
+
+        # create dictionary with format (weight, node_to)
+        # heap will automatically sort weight and node_to in ascending order
+        for l, r, w in edges:
+            dct_nd[l] = dct_nd.get(l, []) + [(w, r)]
+            dct_nd[r] = dct_nd.get(r, []) + [(w, l)]
+        
+        dist_to_n[n] = 0
+        queue.append(n)
+        visited.add(n)
+        
+        hpf = dct_nd[n].copy()   # without '.copy()' hpf will be only pointer and dct_nd[n] could change
+        heapify(hpf)
+        while hpf:
+            el_w, el_nd = heappop(hpf)
+            if el_nd in visited:   continue
+            
+            dist_to_n[el_nd] = el_w
+            visited.add(el_nd)
+            queue.append(el_nd)
+
+            if el_nd == 1:         break  # you don't need to traverse more if you've reached 1-node
+                                          # other distances will be more than distance of 1-node
+            for (i_w, i_nd) in dct_nd[el_nd]:
+                if i_nd not in visited:
+                    heappush(hpf, (el_w + i_w, i_nd))
+        # step 1: end
+
+        # add to dictionary one more field: number of routes to n
+        dist_to_n = {k: [v, 0]   for k, v in dist_to_n.items()}
+        dist_to_n[n] = [dist_to_n[n][0], 1]  # for n-node number of routes = 1
+        
+        # step 2: Dynamic Programming
+        visited.clear()
+        while queue:
+            # start from n and traverse futher and futher
+            nd_prv = queue.popleft()
+            visited.add(nd_prv)
+            for (w_cur, nd_cur) in dct_nd[nd_prv]:
+                if nd_cur not in visited           and   \
+                   nd_cur     in dist_to_n.keys()  and dist_to_n[nd_cur][0] > dist_to_n[nd_prv][0]:
+                    # to current node add number of routes from previous node
+                    dist_to_n[nd_cur][1] += dist_to_n[nd_prv][1]
+                    # !!! careful !!! you need to add modulo operation (as said in the task)
+                    dist_to_n[nd_cur][1]  = int(dist_to_n[nd_cur][1] % (1e9+7))
+        # step 2: End
+        
+        return dist_to_n[1][1] # return number of routes for 1-node
