ad41408e24f5ef31a4304b8047895ca318e1e49c
AnasImloul <pydev27@gmail.com>
2023-04-29 16:09:45 +0100

    Runtime 47 ms (Top 9.58%) | Memory 16.0 MB (Top 8.13%)

--- scripts/algorithms/M/Minimum Bit Flips to Convert Number/Minimum Bit Flips to Convert Number.py
+++ scripts/algorithms/M/Minimum Bit Flips to Convert Number/Minimum Bit Flips to Convert Number.py
@@ -1,3 +1,7 @@
 class Solution:
-    def minBitFlips(self, start: int, goal: int) -> int:
-        return (bin(start^goal).count("1"))
+    def minBitFlips(self, s: int, g: int) -> int:
+        count = 0 
+        while s or g:
+            if s%2 != g%2: count+=1
+            s, g = s//2, g//2
+        return count
