0c842357ed3bc08ca2c0edf7bb168f7c23d3cd80
AnasImloul <pydev27@gmail.com>
2023-04-29 16:16:46 +0100

    Runtime 33 ms (Top 44.87%) | Memory 16.0 MB (Top 12.99%)

--- scripts/algorithms/D/Decode String/Decode String.py
+++ scripts/algorithms/D/Decode String/Decode String.py
@@ -1,26 +1,24 @@
-# Runtime: 67 ms (Top 5.09%) | Memory: 13.9 MB (Top 20.12%)
 class Solution:
     def decodeString(self, s: str) -> str:
-        def helper(index, encodedStr):
-            k = 0
-            string = ""
-
-            # len(s) will be diffrent depend of subString
-            while index < len(encodedStr):
-                if encodedStr[index] == "[": # we found opening bracket - call helper to find sub string
-                    new_index , SubStr = helper(index+1, encodedStr)
-                    # add SubStr to string
-                    string += (k*SubStr)
-                    k , index = 0 , new_index
-                elif encodedStr[index].isdigit():
-                    k = k*10 + int(encodedStr[index])
-                elif encodedStr[index] == "]": # we found closing bracket - return sub string
-                    return index, string
-                else:
-                    string += encodedStr[index] # add curr char to sting
-
-                index +=1
-            return index, string
-
-        _ , decodedStr = helper(0,s)
-        return decodedStr
+        stk = []
+        
+        for i in range(len(s)):
+            
+            if s[i] != ']':
+                stk.append(s[i])  
+            else:
+                strr = ''
+                while stk[-1] != '[':
+                    strr = stk.pop() + strr
+                stk.pop()
+                
+                num = ''
+                while stk and stk[-1].isdigit():
+                    num = stk.pop() + num
+                    
+                stk.append(int(num) * strr)
+        
+        return ''.join(stk)
+                    
+                    
+                    
