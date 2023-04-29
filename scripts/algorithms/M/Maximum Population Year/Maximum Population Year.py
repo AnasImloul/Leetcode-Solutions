0ec358e4efae5a2b23b2b28bdb099be2c183bacf
AnasImloul <pydev27@gmail.com>
2023-04-29 18:23:13 +0100

    Runtime 58 ms (Top 9.59%) | Memory 16.0 MB (Top 18.85%)

--- scripts/algorithms/M/Maximum Population Year/Maximum Population Year.py
+++ scripts/algorithms/M/Maximum Population Year/Maximum Population Year.py
@@ -1,18 +1,24 @@
-# Runtime: 92 ms (Top 15.46%) | Memory: 13.9 MB (Top 77.11%)
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        dates = []
-        for birth, death in logs:
-            dates.append((birth, 1))
-            dates.append((death, -1))
+        logs.sort(key=lambda x: x[0])
+        print(logs)
+        living = 0
+        max_living = 0
+        year = 0
 
-        dates.sort()
+        for ind, (start, stop) in enumerate(logs):
+            born = ind+1
+            dead = 0
+            for i in range(ind):
+                if logs[i][1] <= start:
+                    dead += 1
+            
+            living = born - dead
+            # print(born, dead, living, max_living)
+            if living > max_living:
+                max_living = living
+                year = start
 
-        population = max_population = max_year = 0
-        for year, change in dates:
-            population += change
-            if population > max_population:
-                max_population = population
-                max_year = year
-
-        return max_year
+        
+        
+        return year
