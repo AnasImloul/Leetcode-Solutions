726c147c2b2866d6cbe1ab7fa79cf1a43ce02e59
AnasImloul <pydev27@gmail.com>
2023-04-29 16:17:03 +0100

    Runtime 48 ms (Top 7.91%) | Memory 16.0 MB (Top 10.37%)

--- scripts/algorithms/E/Evaluate Division/Evaluate Division.py
+++ scripts/algorithms/E/Evaluate Division/Evaluate Division.py
@@ -1,26 +1,35 @@
- class Solution:
+class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = defaultdict(list)
-        for equation, value in zip(equations, values):
-            adj[equation[0]].append((equation[1], value))
-            adj[equation[1]].append((equation[0], 1 / value))
-        ans = []
-        for query in queries:
-            self.bfs(query, adj, ans)
-        return ans
-    
-    def bfs(self, query, adj, ans):
-        x, y = query
-        queue = deque([(x, 1)])
-        visited = set([x])
-        while queue:
-            for _ in range(len(queue)):
-                node, r = queue.popleft()
-                for neighbor, v in adj[node]:
-                    if neighbor == y:
-                        ans.append(r * v)
-                        return
-                    if neighbor not in visited:
-                        queue.append((neighbor, r * v))
-                        visited.add(neighbor)
-        ans.append(-1.0)        
+        graph = dict()
+        
+        for (n, d), v in zip(equations, values):
+            if n not in graph:
+                graph[n] = []
+            if d not in graph:
+                graph[d] = []
+                
+            graph[n].append((d, v))
+            graph[d].append((n, 1/v))
+        
+        def dfs(node, target, product, visited):
+            if n not in graph or d not in graph:
+                return -1
+            
+            if node == target:
+                return product
+            
+            visited.add(node)
+            
+            for neighbor, quotient in graph[node]:
+                if neighbor not in visited:
+                    soln = dfs(neighbor, target, product * quotient, visited)
+                    if soln != -1:
+                        return soln
+                    
+            return -1
+        
+        solns = []
+        for n, d in queries:
+            solns.append(dfs(n, d, 1, set()))
+            
+        return solns
