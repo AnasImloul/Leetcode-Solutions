378c321462471a5b9479d1dbdc9a4b4ee4f93936
AnasImloul <pydev27@gmail.com>
2023-04-29 16:17:45 +0100

    Runtime 7901 ms (Top 42.13%) | Memory 43.0 MB (Top 5.1%)

--- scripts/algorithms/B/Burst Balloons/Burst Balloons.py
+++ scripts/algorithms/B/Burst Balloons/Burst Balloons.py
@@ -1,42 +1,18 @@
+from functools import cache
+
 class Solution:
-    
-    def memoization(self, i, j, nums, dp):
-        
-        if i > j:
-            return 0
-        
-        if dp[i][j] != -1:
-            return dp[i][j]
-        
-        maxi = float('-inf')
-        
-        for ind in range(i, j+1):
-            cost = nums[i-1] * nums[ind] * nums[j+1] + self.memoization(i, ind-1, nums, dp) + self.memoization(ind+1, j, nums, dp)
-            maxi = max(maxi, cost)
-            #dp[i][j] = maxi
-        
-        dp[i][j] = maxi
-        return maxi
-    
     def maxCoins(self, nums: List[int]) -> int:
-        c = len(nums)
-        nums.insert(0, 1)
-        nums.append(1)
-        n = len(nums)
-        mem_dp = [[-1 for _ in range(c+1)] for _ in range(c+1)]
+        nums = [1] + nums + [1]
+        length = len(nums)
+        dp = [[None]*(length+1) for i in range(length+1)]
         
-        #return self.memoization(1, c, nums, mem_dp)
-        
-        dp = [[0 for _ in range(c+2)] for _ in range(c+2)]
-        
-        for i in range(c, 0, -1):
-            for j in range(1, c+1):
-                if i > j:
-                    continue
-                maxi = float('-inf')
-                for ind in range(i, j+1):
-                    cost = nums[i-1] * nums[ind] * nums[j+1] + dp[i][ind-1] + dp[ind+1][j]
-                    maxi = max(maxi, cost)
-                    dp[i][j] = maxi
-        
-        return dp[1][c]
+        @cache
+        def dfs(l,r):
+            if l>r: return 0
+            if dp[l][r] is not None: return dp[l][r]
+            dp[l][r] = 0
+            for i in range(l,r+1):
+                coins = dfs(l, i-1) + dfs(i+1, r) + nums[l-1]*nums[i]*nums[r+1]
+                dp[l][r] = max(dp[l][r], coins)
+            return dp[l][r]
+        return dfs(1, length-2)
