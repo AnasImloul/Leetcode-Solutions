bc00ce34889548135068d42a0415a197321f37a4
AnasImloul <pydev27@gmail.com>
2023-04-29 16:42:39 +0100

    Runtime 150 ms (Top 86.96%) | Memory 10.0 MB (Top 98.32%)

--- scripts/algorithms/M/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.cpp
+++ scripts/algorithms/M/Maximum Length of Repeated Subarray/Maximum Length of Repeated Subarray.cpp
@@ -2,24 +2,57 @@ class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<vector<int>>dp(nums1.size()+1,vector<int>(nums2.size()+1,0));
         
-        int ans=0;
-        for(int i=1;i<(nums1.size()+1);i++)
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        //moving num2 on num1
+        
+        int ptr2 = 0;
+        
+        int cnt = 0;
+        
+        int largest  = INT_MIN;
+        
+        for(int i=0;i<n1;i++)
         {
-            for(int j=1;j<(nums2.size()+1);j++)
+            cnt = 0;
+            for(int j=i,ptr2=0;j<n1 && ptr2<n2;j++,ptr2++)
             {
-                if(nums1[i-1]==nums2[j-1])
+                if(nums1[j]==nums2[ptr2])
                 {
-                    dp[i][j]=1+dp[i-1][j-1];
-                    ans=max(ans,dp[i][j]);
+                    cnt++;
+                    
                 }
                 else
                 {
-                    dp[i][j]=0;
+                    cnt = 0;
                 }
+                largest = max(largest,cnt);
             }
         }
-        return ans;
+        
+        //moving num1 on num2
+        ptr2 = 0;
+        cnt = 0;
+        for(int i=0;i<n2;i++)
+        {
+            cnt = 0;
+            for(int j=i,ptr2=0;j<n2 && ptr2<n1;j++,ptr2++)
+            {
+                if(nums2[j]==nums1[ptr2])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt = 0;
+                }
+                largest = max(largest,cnt);
+            }
+        }
+        
+        return largest;
+        
     }
 };
