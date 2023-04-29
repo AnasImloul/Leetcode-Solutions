3cfd65748074cd058f3b9a32fdbeced843f941c4
AnasImloul <pydev27@gmail.com>
2023-04-29 18:42:12 +0100

    Runtime 11 ms (Top 67.94%) | Memory 12.0 MB (Top 45.73%)

--- scripts/algorithms/F/Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.cpp
+++ scripts/algorithms/F/Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    vector<string> removeAnagrams(vector<string>& words) {        
-        vector<string> resultant_array;
-        
-		// Keeping the first pointer at index 0 and the second pointer at index 1
-        int ptr1 = 0;
-        int ptr2 = 1;
-        
-        while(ptr2<=words.size()){
-            string s1 = words[ptr1];
-            sort(s1.begin(),s1.end());
-            
-            resultant_array.push_back(words[ptr1]);
-            
-            while(ptr2<words.size()){
-                string s2 = words[ptr2];
-                sort(s2.begin(), s2.end());
-                
-				// Remember to compare the sorted arrays to check for anagrams
-                if(s1!=s2){
-                    break;
-                }
-                else{
-                    ptr2++;
-                }
+    vector<string> removeAnagrams(vector<string>& words) {
+        string a, b;
+        for(int i=1; i<words.size(); i++) {
+            a = words[i];
+            b = words[i-1];
+            sort(a.begin(), a.end());
+            sort(b.begin(), b.end());
+            if(a==b) {
+                words.erase(words.begin()+i);
+                i--;
+                continue;
             }
-            
-            ptr1 = ptr2;
-            ptr2++;
         }
-        
-        return resultant_array;
+        return words;
     }
 };
