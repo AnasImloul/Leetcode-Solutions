09794ca91708cdd6c95a727c533d730429365962
AnasImloul <pydev27@gmail.com>
2023-04-29 16:15:28 +0100

    Runtime 41 ms (Top 8.8%) | Memory 16.0 MB (Top 7.26%)

--- scripts/algorithms/C/Climbing Stairs/Climbing Stairs.py
+++ scripts/algorithms/C/Climbing Stairs/Climbing Stairs.py
@@ -4,10 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-         def climb(n):
-             if n==1: #only one step option is availble
-                 return 1
-             if n ==2: # two options are possible : to take two 1-stpes or to only take one 2-steps
-                 return 2
-             return climb(n-1) + climb(n-2)
-         return climb(n)
+        memo ={}
+        memo[1] = 1
+        memo[2] = 2
+        
+        def climb(n):
+            if n in memo: # if the recurssion already done before first take a look-up in the look-up table
+                return memo[n]
+            else:   # Store the recurssion function in the look-up table and reuturn the stored look-up table function
+                memo[n] =  climb(n-1) + climb(n-2)
+                return memo[n]
+        
+        return climb(n)
