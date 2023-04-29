8f2dc22a399a1c547f48ec912699595d346f9f70
AnasImloul <pydev27@gmail.com>
2023-04-29 19:12:54 +0100

    Runtime 36 ms (Top 17.59%) | Memory 7.0 MB (Top 8.91%)

--- scripts/algorithms/D/Day of the Year/Day of the Year.cpp
+++ scripts/algorithms/D/Day of the Year/Day of the Year.cpp
@@ -1,19 +1,59 @@
 class Solution {
 public:
-    int dayOfYear(string date) {
-        unordered_map<int, int>months{{1,0}, {2,31}, {3,59}, {4,90}, {5,120}, {6,151}, {7,181}, {8,212}, {9,243}, {10,273}, {11,304}, {12,334}};
-        int year = getInt(date, 0, 3);
-        int mon = getInt(date, 5, 6);
-        int day = getInt(date, 8, 9);
-        int res = months[mon] + day;
-        return (year%4==0 && (year%400 == 0 || year%100 != 0)) && mon > 2 ? res + 1 : res;
+    bool leapyear(int year){
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
     }
-    
-    int getInt(string &date, int l, int r)
-    {
-        int num = 0;
-        for(int i = l; i<=r; i++)
-            num = num*10 + (date[i]-'0');
-        return num;
+        
+    int dayOfYear(string date) {
+        vector<int> v;
+        int ans = 0;
+        int n = date.length();
+        for (int i = 0; i < n; i++) {
+            if (date[i] >= '0' && date[i] <= '9') {
+                ans = ans * 10 + (date[i] - '0'); // subtract '0' to get the integer value
+            } else {
+                v.push_back(ans);
+                ans = 0;
+            }
+        }
+        v.push_back(ans); // add the last value to the vector
+        if (v.size() != 3) return -1; // error handling for invalid input
+        int year = v[0];
+        int month = v[1];
+        int day = v[2];
+        if (month == 1) return day;
+        if (month == 2) return 31 + day;
+       
+        if (month == 3) return leapyear(year) ? 60 + day : 59 + day;
+         if (month == 4) return leapyear(year) ? 91 + day : 90 + day;
+         if (month == 5) return leapyear(year) ? 121 + day : 120 + day;
+         if (month == 6) return leapyear(year) ? 152 + day : 151 + day;
+         if (month == 7) return leapyear(year) ? 182 + day : 181 + day;
+         if (month == 8) return leapyear(year) ? 213 + day : 212 + day;
+         if (month == 9) return leapyear(year) ? 244 + day : 243 + day;
+         if (month == 10) return leapyear(year) ? 274+ day : 273 + day;
+         if (month == 11) return leapyear(year) ? 305 + day : 304 + day;
+         if (month == 12) return leapyear(year) ? 335 + day : 334 + day;
+        return -1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
