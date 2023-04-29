d001339a34036428b07133cb7a1552cf6eca89db
AnasImloul <pydev27@gmail.com>
2023-04-29 17:24:59 +0100

    Runtime 175 ms (Top 15.5%) | Memory 18.0 MB (Top 6.2%)

--- scripts/algorithms/S/Set Intersection Size At Least Two/Set Intersection Size At Least Two.py
+++ scripts/algorithms/S/Set Intersection Size At Least Two/Set Intersection Size At Least Two.py
@@ -1,16 +1,8 @@
 class Solution:
-    def intersectionSizeTwo(self, A: List[List[int]]) -> int:
-        A.sort(key=lambda x:(x[1],-x[0]))
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
         ans = []
-        ans.append(A[0][1]-1)
-        ans.append(A[0][1])
-        n = len(A)
-        # print(A)
-        for i in range(1,n):
-            if A[i][0]>ans[-1]:
-                ans.append(A[i][1]-1)
-                ans.append(A[i][1])
-            elif A[i][0]>ans[-2]:
-                ans.append(A[i][1])
-        # print(ans)
+        for x, y in sorted(intervals, key=lambda x: (x[1], -x[0])): 
+            if not ans or ans[-2] < x: 
+                if ans and x <= ans[-1]: ans.append(y)
+                else: ans.extend([y-1, y])
         return len(ans)
