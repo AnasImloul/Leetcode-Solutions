d6a6216ca443200c5da268936f2cf3a6ed8e5d07
AnasImloul <pydev27@gmail.com>
2023-04-29 18:19:55 +0100

    Runtime 7 ms (Top 88.47%) | Memory 5.0 MB (Top 92.72%)

--- scripts/algorithms/R/Roman to Integer/Roman to Integer.cpp
+++ scripts/algorithms/R/Roman to Integer/Roman to Integer.cpp
@@ -1,39 +1,107 @@
 class Solution {
 public:
-int getRoman(char c){
-    switch (c) {
-        case 'I':
-            return 1;
-        case 'V':
-            return 5;
-        case 'X':
-            return 10;
-        case 'L' :
-            return 50;
-        case 'C':
-            return 100;
-        case 'D':
-            return 500;
-        case 'M':
-            return 1000;
-        default:
-            return 0;
+    int romanToInt(string s) 
+    {
+
+    int count=0,ans=0;
+        
+    for(int i=0; s[i]!='\0';i++)
+    {
+        count++;
     }
-}
-int romanToInt(string s){
-    int result = 0;
-    for(int i = 0; i<s.size();i++){
-        int num = getRoman(s[i]);
-        int numNext = getRoman(s[i+1]);
-        if(num>=numNext){
-            result = result+num;
+        
+    int s1[count+1];
+        
+    for(int i=0; i<count; i++)
+        {
+            if(s[i]=='M')
+            s1[i]=1000;
+            else if(s[i]=='V')
+            s1[i]=5;
+            else if(s[i]=='X')
+            s1[i]=10;
+            else if(s[i]=='L')
+            s1[i]=50;
+            else if(s[i]=='C')
+            s1[i]=100;
+            else if(s[i]=='D')
+            s1[i]=500;
+            else 
+            s1[i]=1;
+        }  
+        
+    s1[count]=0;
+
+    for(int j=0; j<=count; j++)
+        {
+            if (j!=count)
+            {          
+                 if(s1[j]>=s1[j+1])
+                {
+                    switch(s1[j])
+                    {
+                      case 1: ans = ans+1;
+                      break;
+                      case 5: ans = ans+5;
+                      break;
+                      case 10: ans = ans+10;
+                      break;
+                      case 50: ans = ans+50;
+                      break;
+                      case 100: ans = ans+100;
+                      break;
+                      case 500: ans = ans+500;
+                      break;
+                      case 1000: ans = ans+1000;
+                      break;
+                    }        
+                }      
+
+                 else if(s1[j]<s1[j+1])
+                {
+                    switch(s1[j])
+                    {
+                    case 1: ans = ans-1;
+                    break;
+                    case 5: ans = ans-5;
+                    break;
+                    case 10: ans = ans-10;
+                    break;
+                    case 50: ans = ans-50;
+                    break;
+                    case 100: ans = ans-100;
+                    break;
+                    case 500: ans = ans-500;
+                    break;
+                    case 1000: ans = ans-1000;
+                    break;
+                    }           
+                }
+            }
+
+            else if(j==count)
+            {
+                switch(s1[j])
+                {
+                  case 1: ans = ans+1;
+                  break;
+                  case 5: ans = ans+5;
+                  break;
+                  case 10: ans = ans+10;
+                  break;
+                  case 50: ans = ans+50;
+                  break;
+                  case 100: ans = ans+100;
+                  break;
+                  case 500: ans = ans+500;
+                  break;
+                  case 1000: ans = ans+1000;
+                  break;
+                }
+            }
         }
-        if(num<numNext){
-            numNext = numNext-num;
-            result = result+numNext;
-            i++;
-        }
+
+    return ans;
+        
     }
-    return result;
-}
 };
