c8189b16b4c1eec44eb80c7563cd439486d140f1
AnasImloul <pydev27@gmail.com>
2023-04-29 18:47:45 +0100

    Runtime 42 ms (Top 5.0%) | Memory 16.0 MB (Top 10.6%)

--- scripts/algorithms/O/Occurrences After Bigram/Occurrences After Bigram.py
+++ scripts/algorithms/O/Occurrences After Bigram/Occurrences After Bigram.py
@@ -1,15 +1,5 @@
 class Solution:
     def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
-        l=text.split()
-        result=[]
-        size=len(l)
-        cur, nxt=0, 1
-        while(nxt<size):
-            if l[cur]==first and l[nxt]==second:
-                if nxt+1<size:
-                    result.append(l[nxt+1])
-                else:
-                    break
-            cur+=1
-            nxt+=1
-        return result
+        pattern = r"(?<=\b" + first +" " + second + r" )[a-z]*"
+        txt = re.findall(pattern,text)
+        return txt
