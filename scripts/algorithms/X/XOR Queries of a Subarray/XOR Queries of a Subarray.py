d9bf0a26160d5227d4b12133369aced3e744a9b0
AnasImloul <pydev27@gmail.com>
2023-04-29 14:43:37 +0100

    Runtime 399 ms (Top 22.1%) | Memory 31.0 MB (Top 11.6%)

--- scripts/algorithms/X/XOR Queries of a Subarray/XOR Queries of a Subarray.py
+++ scripts/algorithms/X/XOR Queries of a Subarray/XOR Queries of a Subarray.py
@@ -1,10 +1,31 @@
-class Solution(object):
-    def xorQueries(self, arr, queries):
-        """
-        :type arr: List[int]
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        xors=[0]*len(arr)
-        for i in range(len(arr)): xors[i]=xors[i-1]^arr[i] if i>0 else arr[0]
-        return [xors[r]^xors[l-1] if l>0 else xors[r] for l,r in queries]
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+
+
+       """
+
+       arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+
+       find pref xor of arr
+
+       pref = [x,x,x,x]
+
+       for each query find the left and right indices
+       the xor for range (l, r) would be pref[r] xor pref[l-1]
+       
+       """     
+       n, m = len(queries), len(arr)
+
+       answer = [1]*n
+
+       pref = [1]*m
+       pref[0] = arr[0]
+       if m > 1:
+           for i in range(1,m):
+               pref[i] = pref[i-1] ^ arr[i]
+
+       for (i, (l,r)) in enumerate(queries):
+           if l == 0: answer[i] = pref[r]          
+           else: answer[i] = pref[r] ^ pref[l-1]
+
+       return answer
