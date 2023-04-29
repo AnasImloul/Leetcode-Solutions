ea14ddcc8280906a19b867e86718ed0286db4ff6
AnasImloul <pydev27@gmail.com>
2023-04-29 16:22:39 +0100

    Runtime 55 ms (Top 5.11%) | Memory 16.0 MB (Top 39.68%)

--- scripts/algorithms/C/Count of Matches in Tournament/Count of Matches in Tournament.py
+++ scripts/algorithms/C/Count of Matches in Tournament/Count of Matches in Tournament.py
@@ -1,4 +1,3 @@
-# Runtime: 57 ms (Top 45.10%) | Memory: 13.9 MB (Top 51.38%)
 class Solution:
-    def numberOfMatches(self, n: int) -> int:
-        return n-1
+	def numberOfMatches(self, n: int) -> int:
+		return n - 1
