5ae82253fd410933e699d252e92193455660e6d6
AnasImloul <pydev27@gmail.com>
2023-04-29 16:29:22 +0100

    Runtime 55 ms (Top 11.45%) | Memory 16.0 MB (Top 5.34%)

--- scripts/algorithms/S/Satisfiability of Equality Equations/Satisfiability of Equality Equations.py
+++ scripts/algorithms/S/Satisfiability of Equality Equations/Satisfiability of Equality Equations.py
@@ -1,44 +1,30 @@
-# Runtime: 111 ms (Top 6.54%) | Memory: 14.1 MB (Top 35.32%)
-class UnionFind:
-    def __init__(self,n):
-        self.tot_components = n
-        self.parent = [-1] * n
-        self.rank = [0] * n
-
-    def find(self,node):
-        if self.parent[node] == -1:
-            return node
-        self.parent[node] = self.find(self.parent[node])
-        return self.parent[node]
-
-    def union(self,node1,node2):
-        node1_p = self.find(node1)
-        node2_p = self.find(node2)
-
-        if node1_p == node2_p:
-            return True
-        else:
-            if self.rank[node1_p] < self.rank[node1_p]:
-                self.parent[node1_p] = node2_p
-            elif self.rank[node1_p] > self.rank[node1_p]:
-                self.parent[node2_p] = node1_p
-            else:
-                self.parent[node1_p] = node2_p
-                self.rank[node2_p] += 1
-            self.tot_components -= 1
-            return False
-
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
+        from collections import defaultdict
+        g = defaultdict(list)
+        for e in equations:
+            if e[1] == '=':
+                x = e[0]
+                y = e[3]
+                g[x].append(y)
+                g[y].append(x)
+        
+        # marked the connected components as 0,1,2,...,25
+        ccs = defaultdict(lambda: -1) # -1 means unmarked or unseen
 
-        uf = UnionFind(26)
-
-        for eqn in equations:
-            if eqn[1] == "=":
-                uf.union(ord(eqn[0])-ord("a"),ord(eqn[3])-ord("a"))
-
-        for eqn in equations:
-            if eqn[1] == "!":
-                if uf.find(ord(eqn[0])-ord("a")) == uf.find(ord(eqn[3])-ord("a")):
+        def dfs(node, cc):
+            if node not in ccs:
+                ccs[node] = cc
+                for neighbour in g[node]:
+                    dfs(neighbour, cc)
+        
+        for i in range(26):
+            dfs(chr(i+97), i)
+        
+        for e in equations:
+            if e[1] == '!':
+                x = e[0]
+                y = e[3]
+                if ccs[x] == ccs[y]:
                     return False
         return True
