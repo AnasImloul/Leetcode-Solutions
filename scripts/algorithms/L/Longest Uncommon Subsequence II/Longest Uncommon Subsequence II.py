190b0dd80b0d9cb92772b273602a06f0a93bb749
AnasImloul <pydev27@gmail.com>
2023-04-29 16:18:17 +0100

    Runtime 52 ms (Top 6.37%) | Memory 16.0 MB (Top 14.71%)

--- scripts/algorithms/L/Longest Uncommon Subsequence II/Longest Uncommon Subsequence II.py
+++ scripts/algorithms/L/Longest Uncommon Subsequence II/Longest Uncommon Subsequence II.py
@@ -1,23 +1,55 @@
 class Solution:
-    def isSub(self, s1, s2):
-        n = len(s1)
-        i = 0
-        for c in s2:
-            if i >= n:
-                break
-            if s1[i] == c:
-                i += 1
-        return i == n
-    
-    def findLUSlength(self, strs: List[str]) -> int:
-        n = len(strs)
-        strs.sort(key = lambda s: -len(s))
-        for i in range(n):
-            valid = True
-            for j in range(n):
-                if i != j and self.isSub(strs[i], strs[j]):
-                    valid = False
-                    break
-            if valid:
-                return len(strs[i])
+    def findLUSlength(self, s: List[str]) -> int:
+        def lcs(X, Y):
+            m = len(X)
+            n = len(Y)
+            L = [[None]*(n + 1) for i in range(m + 1)]
+            for i in range(m + 1):
+                for j in range(n + 1):
+                    if i == 0 or j == 0 :
+                        L[i][j] = 0
+                    elif X[i-1] == Y[j-1]:
+                        L[i][j] = L[i-1][j-1]+1
+                    else:
+                        L[i][j] = max(L[i-1][j], L[i][j-1])
+            return L[m][n]
+            
+        k = 0 
+        mp = {}
+        for i in s:
+            if len(i) not in mp:
+                mp[len(i)] = [i]
+            else:
+                mp[len(i)].append(i)
+            k = max(k , len(i))
+        
+        t = k
+        while(k):
+            if k not in mp:
+              k -= 1
+            else:
+                curr = mp[k]
+                n = len(curr)
+                for i in range(n):
+                    f = 0
+                    for j in range(n):
+                        if(i != j and curr[i] == curr[j]):
+                            f = 1
+                            break
+                    if(not(f)):
+                        ff = 0 
+                        for j in range(k+1 , t+1):
+                            if(ff):break
+                            if(j not in mp): continue
+                            for jj in mp[j]:
+                                if(lcs(curr[i] , jj) == len(curr[i])):
+                                    ff = 1
+                                    break
+                        if(not(ff)):return len(curr[i])
+                k-=1
+                
         return -1
+
+        
+
+        
