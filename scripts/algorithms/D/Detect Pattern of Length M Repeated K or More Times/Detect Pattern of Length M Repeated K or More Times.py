6f4a36793afd5d898a456f76c31bd046eb11ae46
AnasImloul <pydev27@gmail.com>
2023-04-29 18:43:31 +0100

    Runtime 45 ms (Top 16.74%) | Memory 16.0 MB (Top 15.38%)

--- scripts/algorithms/D/Detect Pattern of Length M Repeated K or More Times/Detect Pattern of Length M Repeated K or More Times.py
+++ scripts/algorithms/D/Detect Pattern of Length M Repeated K or More Times/Detect Pattern of Length M Repeated K or More Times.py
@@ -1,27 +1,17 @@
-class Solution(object):
-    def containsPattern(self, arr, m, k):
-        """
-        :type arr: List[int]
-        :type m: int
-        :type k: int
-        :rtype: bool
-        """
-        w_start=0
-        w_end=w_start+m
-        n=len(arr)
-        count=1
-        while (w_end < n):
-            if (arr[w_start:w_end] == arr[w_end:w_end+m]): 
-                count+=1
-                if count >= k:
-                    return True
-                w_start=w_end
-                w_end+=m
-                #if found the matching window, 
-                #increments both by the size of window(m)
-                #otherwise increment by 1
-            else:
-                count = 1
-                w_start+=1
-                w_end+=1
-        return False
+class Solution:
+    def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
+        if len(arr) < k*m:
+            return False
+        
+        n = len(arr)
+        pattern = arr[0:m]
+        repeats = 1
+        for i in range(m, n - m + 1, m):
+            if arr[i:i+m] != pattern:
+                break
+            
+            repeats += 1
+            if repeats >= k:
+                return True
+            
+        return self.containsPattern(arr[1:], m, k)
