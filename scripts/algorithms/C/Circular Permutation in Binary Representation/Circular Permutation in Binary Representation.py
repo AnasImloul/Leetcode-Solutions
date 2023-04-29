9bfea793a5bb58f5d5230daa783de7eaba74704e
AnasImloul <pydev27@gmail.com>
2023-04-29 16:20:35 +0100

    Runtime 202 ms (Top 44.64%) | Memory 24.0 MB (Top 24.11%)

--- scripts/algorithms/C/Circular Permutation in Binary Representation/Circular Permutation in Binary Representation.py
+++ scripts/algorithms/C/Circular Permutation in Binary Representation/Circular Permutation in Binary Representation.py
@@ -1,27 +1,5 @@
 class Solution:
-    def circularPermutation(self, n: int, s: int) -> List[int]:
-        
-        def findbin(n):
-            if n==1:
-                return [0,1]
-            
-            l=findbin(n-1)
-            nl=[]
-            for x in l:
-                nl.append(x)
-            mask=(1<<(n-1))
-            for i in range(len(l)-1,-1,-1):
-                nl.append(l[i]|mask)
-            return nl
-        
-        l=findbin(n)
-        index=0
-        
-        for i in range(len(l)):
-            
-            if l[i]==s:
-                index=i
-                break
-        return l[i:]+l[:i]
-    
-            
+    def circularPermutation(self, n: int, start: int) -> List[int]:
+        gray_code = [x ^ (x >> 1) for x in range(2 ** n)]
+        start_i = gray_code.index(start)
+        return gray_code[start_i:] + gray_code[:start_i]
