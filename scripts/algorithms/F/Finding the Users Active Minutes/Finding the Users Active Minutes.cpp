1431f2186d712779e3b0f01afc1089f9c9241687
AnasImloul <pydev27@gmail.com>
2023-04-29 16:44:11 +0100

    Runtime 304 ms (Top 36.68%) | Memory 99.0 MB (Top 21.85%)

--- scripts/algorithms/F/Finding the Users Active Minutes/Finding the Users Active Minutes.cpp
+++ scripts/algorithms/F/Finding the Users Active Minutes/Finding the Users Active Minutes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-        vector<int> res(k);
-        unordered_map<int,unordered_set<int>> m;
-        for(int i=0;i<logs.size();i++){
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) 
+    {
+        vector<int>ans(k,0);
+        unordered_map<int,unordered_set<int>>m;
+
+        for(int i = 0; i<logs.size(); i++)
+        {
             m[logs[i][0]].insert(logs[i][1]);
         }
-        unordered_map<int,int> uma;
-        for(int i=0;i<logs.size();i++){
-            uma[logs[i][0]] = m[logs[i][0]].size();
+        for(auto x : m)
+        {
+            int t = x.second.size();
+            ans[t-1]++;
         }
-        for(auto p : uma){
-            res[p.second - 1]++;
-            
-        }
-        return res;
+        return ans;
     }
 };
