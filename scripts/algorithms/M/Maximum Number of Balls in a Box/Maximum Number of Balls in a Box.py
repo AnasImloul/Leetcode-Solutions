56450358fc2d722de772e63a73abf80ed5458e9d
AnasImloul <pydev27@gmail.com>
2023-04-29 18:48:35 +0100

    Runtime 647 ms (Top 49.55%) | Memory 16.0 MB (Top 6.3%)

--- scripts/algorithms/M/Maximum Number of Balls in a Box/Maximum Number of Balls in a Box.py
+++ scripts/algorithms/M/Maximum Number of Balls in a Box/Maximum Number of Balls in a Box.py
@@ -1,31 +1,12 @@
-
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        boxes = [0] * 100
         
-        # Function which will take integer number as an input and return it's sum
-        # if input is 123 then it'll return 6 (1+2+3)
+        for i in range(lowLimit, highLimit + 1):
+			
+			# For the current number "i", convert it into a list of its digits.
+			# Compute its sum and increment the count in the frequency table.
+			
+            boxes[sum([int(j) for j in str(i)])] += 1
         
-        def numberSum(number:int)->int:
-            sum1 = 0
-            while number:
-                sum1 += number%10
-                number = number //10
-            return sum1
-        
-        # create a hashmap having key as box number and value as box count
-        hashMap = defaultdict(int)
-        
-        for i in range(lowLimit, highLimit+1):
-            # pass i to numberSum function inorder to find it's sum1
-            # Once sum1 is found, we'll use this sum1 as our hash_val
-            # If box has alreary one ball avaiable then increment it by one
-            hash_val = numberSum(i)
-            hashMap[hash_val] += 1
-        
-        # Check balls in every box
-        # hashMap.values() will create an array of balls count in each box.
-        values = hashMap.values()
-        
-        # return maximum balls in boxes
-        return max(values)
-
+        return max(boxes)
