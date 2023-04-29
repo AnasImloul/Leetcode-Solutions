2274055b06efe6106b6f352ebea6c385300eccbb
AnasImloul <pydev27@gmail.com>
2023-04-29 16:17:20 +0100

    Runtime 59 ms (Top 5.86%) | Memory 16.0 MB (Top 5.41%)

--- scripts/algorithms/W/Word Break/Word Break.py
+++ scripts/algorithms/W/Word Break/Word Break.py
@@ -1,10 +1,21 @@
-from functools import cache
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @cache
-        def dfs(i):
-            if i == len(s): return True
-            for word in wordDict:
-                if s.startswith(word, i) and dfs(i+len(word)): return True
+        memo = {}
+
+
+        def can_construct(target, strings_bank, memo):    
+            if target in memo:
+                return memo[target]
+            if target == "":
+                return True
+            for element in strings_bank: # for every element in our dict we check if we can start constructing the string "s"
+                if element == target[0:len(element)]: # the remaining of the string "s" (which is the suffix) is the new target 
+                    suffix = target[len(element):]
+                    if can_construct(suffix, strings_bank, memo):
+                        memo[target] = True
+                        return True
+            memo[target] = False
             return False
-        return dfs(0)
+
+
+        return can_construct(s, wordDict, memo)
