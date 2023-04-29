2a20b2b8996f2964dd109e8d86a9e73873393b18
AnasImloul <pydev27@gmail.com>
2023-04-29 19:23:45 +0100

    Runtime 1097 ms (Top 34.94%) | Memory 25.0 MB (Top 38.25%)

--- scripts/algorithms/C/Count Lattice Points Inside a Circle/Count Lattice Points Inside a Circle.cpp
+++ scripts/algorithms/C/Count Lattice Points Inside a Circle/Count Lattice Points Inside a Circle.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int countLatticePoints(vector<vector<int>>& C) {
-      set<pair<int,int>> S;
-        for(int i=0;i<C.size();i++){
-            int a=C[i][0],b=C[i][1],x=C[i][2],l=a-x,d=b-x;
-            for(int j=l;j<=x+a;j++)
-            for(int k=d;k<=x+b;k++)
-                if(pow(a-j,2)+pow(b-k,2)<=x*x) 
-                S.insert({j,k}); 
+    bool circle(int x , int y , int c1 , int c2, int r){
+        if((x-c1)*(x-c1) + (y-c2)*(y-c2) <= r*r) 
+            return true ;
+        return false ;
+    }
+    
+    int countLatticePoints(vector<vector<int>>& circles) {
+        int n = circles.size() , ans = 0 ;
+        set<pair<int,int>> set ;
+        for(auto v : circles){
+            int r = v[2] , x = v[0] , y = v[1]; 
+            for(int i = x-r ; i <= x+r ; i++)
+                for(int j = y-r ; j <= y+r ; j++)
+                    if(circle(i,j,x,y,r)){
+                        pair<int,int> p(i,j) ;
+                        set.insert(p) ;
+                    }
         }
-        return S.size();  
+        return set.size() ;
     }
 };
