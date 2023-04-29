d75309a199dd7680891c59b73c70f2965990d58c
AnasImloul <pydev27@gmail.com>
2023-04-29 16:21:15 +0100

    Runtime 542 ms (Top 75.41%) | Memory 28.0 MB (Top 54.1%)

--- scripts/algorithms/D/Decode the Slanted Ciphertext/Decode the Slanted Ciphertext.py
+++ scripts/algorithms/D/Decode the Slanted Ciphertext/Decode the Slanted Ciphertext.py
@@ -1,7 +1,12 @@
 class Solution:
     def decodeCiphertext(self, encodedText: str, rows: int) -> str:
-        cols, res = len(encodedText) // rows, ""
+        n = len(encodedText)
+        cols = n // rows
+        step = cols + 1
+        res = ""
+        
         for i in range(cols):
-            for j in range(i, len(encodedText), cols + 1):
+            for j in range(i, n, step):
                 res += encodedText[j]
+            
         return res.rstrip()
