0520c85ae64d2d71902f656cb945b5f9ba6e2187
AnasImloul <pydev27@gmail.com>
2023-04-29 19:14:34 +0100

    Runtime 3 ms (Top 83.55%) | Memory 5.0 MB (Top 78.55%)

--- scripts/algorithms/M/Maximum Score From Removing Stones/Maximum Score From Removing Stones.cpp
+++ scripts/algorithms/M/Maximum Score From Removing Stones/Maximum Score From Removing Stones.cpp
@@ -1,25 +1,6 @@
-// Runtime: 165 ms (Top 38.49%) | Memory: 6.1 MB (Top 38.40%)
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        priority_queue<int> pq;
-        pq.push(a);
-        pq.push(b);
-        pq.push(c);
-        int f=1;
-        int ans=0;
-        while(f)
-        {
-            int f=pq.top(); pq.pop();
-            int s=pq.top(); pq.pop();
-            if(f==0 || s==0)
-                break;
-            ans++;
-            f--; s--;
-            pq.push(f);
-            pq.push(s);
-        }
-        return ans;
-
+        return min((a + b + c) / 2, a + b + c - max({a, b, c}));
     }
 };
