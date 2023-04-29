017fa0e3f31bbd20614533eb056df6c9ff611db0
AnasImloul <pydev27@gmail.com>
2023-04-29 16:40:44 +0100

    Runtime 0 ms (Top 100.0%) | Memory 7.0 MB (Top 90.38%)

--- scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.cpp
+++ scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.cpp
@@ -1,29 +1,24 @@
-// Runtime: 15 ms (Top 24.35%) | Memory: 8.4 MB (Top 28.20%)
 class Solution {
 public:
-    //Similar to Matrix Chain Multiplication optimization
-    vector<int> presum;
-    int solve(vector<int>& stones, int k, int left, int right, vector<vector<int>>& dp){
-        if(right == left) return 0;
-        if(dp[left][right] != -1) return dp[left][right];
-
-        int res = 1e9;
-        for(int i = left; i < right; i=i+k-1){
-            res = min(res, solve(stones, k, left, i, dp) + solve(stones, k, i+1, right, dp));
-        }
-
-        //this cumulative sum evaluation separately, as that of MCX
-        if((right - left)%(k-1) ==0){
-            res += presum[right+1] - presum[left];
-        }
-        return dp[left][right] = res;
+    #define LIM 32
+    vector<int> prefixSum = {0};
+    int dp[LIM][LIM][LIM];
+    int solve(vector<int> &stones, int start, int end, int k){
+        // Base Case: Invalid Index or For single element answer is always 0
+        if(start >= end) return 0;
+        int cost = INT_MAX;
+        if(dp[start][end][k]!=-1) return dp[start][end][k];
+        for(int i = start; i < end; i = i+k-1)
+            cost = min(cost, solve(stones, start, i, k) + solve(stones, i+1, end, k));
+        if((end-start)%(k-1) == 0)
+            cost += prefixSum[end+1]-prefixSum[start];
+        return dp[start][end][k]=cost;
     }
-
-    int mergeStones(vector<int>& stones, int k) {
-        if((stones.size() - 1)%(k-1) != 0) return -1;
-        presum.push_back(0);
-        for(auto s: stones) presum.push_back(s + presum.back());
-        vector<vector<int>> dp(32, vector<int> (32, -1));
-        return solve(stones, k, 0, stones.size()-1, dp);
+    int mergeStones(vector<int>& stones, int k){
+        int n = stones.size();
+        if((n-1)%(k-1) != 0) return -1;
+        for(int i : stones) prefixSum.push_back(prefixSum.back()+i);
+        for(int i=0; i<LIM; i++) for(int j=0; j<LIM; j++) for(int k=0; k<LIM; k++) dp[i][j][k]=-1;
+        return solve(stones, 0, n-1, k);
     }
 };
