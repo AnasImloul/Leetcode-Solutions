4a6ed522a624f34aad441bddf0ab2d4c0ba88654
AnasImloul <pydev27@gmail.com>
2023-04-29 17:24:04 +0100

    Runtime 85 ms (Top 75.42%) | Memory 16.0 MB (Top 13.33%)

--- scripts/algorithms/S/Surface Area of 3D Shapes/Surface Area of 3D Shapes.py
+++ scripts/algorithms/S/Surface Area of 3D Shapes/Surface Area of 3D Shapes.py
@@ -1,20 +1,21 @@
 class Solution:
-    def surfaceArea(self, grid: List[List[int]]) -> int:
-        ans = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if i == 0:
-                    ans += grid[i][j]
-                if i == len(grid) - 1:
-                    ans += grid[i][j]
-                if j == 0:
-                    ans += grid[i][j]
-                if j == len(grid[0]) - 1:
-                    ans += grid[i][j]
-                if i < len(grid) - 1:
-                    ans += abs(grid[i + 1][j] - grid[i][j])
-                if j < len(grid[0]) - 1:
-                    ans += abs(grid[i][j + 1] - grid[i][j])
-                if grid[i][j] > 0:
-                    ans += 2
-        return ans
+    def surfaceArea(self, grid: List[List[int]]) -> int:        
+        m, n = len(grid), len(grid[0])
+        
+        area = 0
+        for r in range(m):               
+            for c in range(n):
+                if grid[r][c] != 0:
+                    area += 2
+                
+                if r == 0 or r == m - 1:
+                    area += grid[r][c] if m != 1 else 2*grid[r][c]
+                if r != m - 1:                    
+                    area += abs(grid[r][c] - grid[r+1][c])
+                    
+                if c == 0 or c == n - 1:
+                    area += grid[r][c] if n != 1 else 2*grid[r][c]
+                if c != n - 1:                    
+                    area += abs(grid[r][c] - grid[r][c+1])                    
+                
+        return area
