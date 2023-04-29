e4638aa4b552eed268dd7e6cda7c9ad694a55242
AnasImloul <pydev27@gmail.com>
2023-04-29 19:14:53 +0100

    Runtime 1202 ms (Top 16.95%) | Memory 12.0 MB (Top 66.1%)

--- scripts/algorithms/C/Cat and Mouse II/Cat and Mouse II.cpp
+++ scripts/algorithms/C/Cat and Mouse II/Cat and Mouse II.cpp
@@ -1,59 +1,80 @@
-int f[8][8][8][8][67];
-
 class Solution {
-public:
-    
-    int n,m,cj,mj;   //cj means catJump, mj means mouseJump
-    vector<string> g;
-    int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-    
-	//dp[cat_x][cat_y][mouse_x][mouse_y][steps], 0 = cat wins, 1 = mouse wins.
-    int dp(int cx, int cy, int mx, int my, int k){ 
-        if(k >= 67) return 0;    //when steps >= 67, cat wins
-        int& v = f[cx][cy][mx][my][k];
-        if(v != -1) return v;  
-        
-        if(k & 1){    //step for cat
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j <= cj; j++){
-                    int x = cx + dx[i]*j, y = cy + dy[i]*j;
-                    if(x < 0 or x >= n or y < 0 or y >= m or g[x][y] == '#') break;
-					 //If Cat occupies the same position as Mouse, Cat wins.
-                    if(x == mx and y == my) return v = 0;  
-					//If Cat reaches the food first, Cat wins.
-                    if(g[x][y] == 'F') return v = 0;
-                    if(!dp(x,y,mx,my,k+1)) return v = 0;
-                }
-            }
-            return v = 1;
-        }
-        else{  //step for mouse
-            for(int i = 0; i < 4; i++){
-                for(int j = 0; j <= mj; j++){
-                    int x = mx + dx[i]*j, y = my + dy[i]*j;
-                    if(x < 0 or x >= n or y < 0 or y >= m or g[x][y] == '#') break;
-					//mouse will not reach cat's position
-                    if(x == cx and y == cy) continue;
-					//If Mouse reaches the food first, Mouse wins.
-                    if(g[x][y] == 'F') return v = 1;
-                    if(dp(cx,cy,x,y,k+1)) return v = 1;
-                }
-            }
-            return v = 0;
-        }
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    int dp[2][81][8][8][8][8];
+    inline bool canGo(vector<string>& grid, int i , int j) {
+        return i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size()
+        && grid[i][j] != '#';
     }
 
-    
+public:
+    int solve(vector<string>& grid, int catJump, int mouseJump, int turn,
+     int mouseSteps, int mousePosX, int mousePosY, int catPosX, int catPosY) {
+        if (mouseSteps > 80) {
+            return 0;
+        }
+        if (grid[mousePosX][mousePosY] == 'F') {
+            return 1;
+        }
+        if (mousePosX == catPosX && mousePosY == catPosY) {
+            return 0;
+        }
+        if (grid[catPosX][catPosY] == 'F') {
+            return 0;
+        }
+
+        int &ans = dp[turn][mouseSteps][mousePosX][mousePosY][catPosX][catPosY];
+        if (ans != -1) {
+            return ans;
+        }
+
+        bool mouseCanWin = 0;
+        if (turn) {
+            mouseCanWin = 1;
+            for (int k = 0; k < 4; k++) {
+                for (int jump = 0; jump <= catJump; jump++) {
+                    int newCatPosX = catPosX + dx[k] * jump;
+                    int newCatPosY = catPosY + dy[k] * jump;
+                    if (canGo(grid, newCatPosX, newCatPosY)) {
+                        mouseCanWin &= solve(grid, catJump, mouseJump, 0, mouseSteps, mousePosX, mousePosY, newCatPosX, newCatPosY);
+                    if (!mouseCanWin) break;
+                    } else break; // cannot jump over the wall
+                }
+            }
+        } else {
+            mouseCanWin = 0;
+            for (int k = 0; k < 4; k++) {
+                for (int jump = 0; jump <= mouseJump; jump++) {
+                    int newMousePosX = mousePosX + dx[k] * jump;
+                    int newMousePosY = mousePosY + dy[k] * jump;
+                    if (canGo(grid, newMousePosX, newMousePosY)) {
+                        mouseCanWin |= solve(grid, catJump, mouseJump, 1, mouseSteps + 1, newMousePosX, newMousePosY, catPosX, catPosY);
+                        if (mouseCanWin) break;
+                    } else break; // cannot jump over the wall               
+                }
+            }
+        }
+        return ans = mouseCanWin;
+    }
+
     bool canMouseWin(vector<string>& grid, int catJump, int mouseJump) {
-        g = grid;
-        n = grid.size(), m = g[0].size(), cj = catJump, mj = mouseJump;
-        //positions of cat and mouse
-        int cx,cy,mx,my;
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                if(g[i][j] == 'C') cx = i, cy = j;
-                else if(g[i][j] == 'M') mx = i, my = j;
-        memset(f, -1, sizeof f);
-        return dp(cx, cy, mx, my, 0);
+        memset(dp, -1, sizeof(dp));
+        int mousePosX = 0;
+        int mousePosY = 0;
+        int catPosX = 0;
+        int catPosY = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 'C') {
+                    catPosX = i;
+                    catPosY = j;
+                }
+                if (grid[i][j] == 'M') {
+                    mousePosX = i;
+                    mousePosY = j;
+                }
+            }
+        }
+        return solve(grid, catJump, mouseJump, 0, 0, mousePosX, mousePosY, catPosX, catPosY);
     }
 };
