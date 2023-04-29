b78b07e105575063c8361743a0882b45a13db8dc
AnasImloul <pydev27@gmail.com>
2023-04-29 17:31:58 +0100

    Runtime 16 ms (Top 45.24%) | Memory 5.0 MB (Top 92.86%)

--- scripts/algorithms/L/Least Operators to Express Number/Least Operators to Express Number.cpp
+++ scripts/algorithms/L/Least Operators to Express Number/Least Operators to Express Number.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int leastOpsExpressTarget(int x, int target) {
-        unordered_map<int, int> memo; 
+        // base case:
+        // if target is less then x then we can only have values like either
+        // x - (x/x)-(x/x).... or (x/x)+(x/x)+(x/x)...
+        if(target<x){
+            return min((x-target)*2, target*2-1);
+        }
         
-        function<int(int)> fn = [&](int val) {
-            if (val < x) return min(2*val-1, 2*(x-val)); 
-            if (memo.find(val) == memo.end()) {
-                int k = log(val)/log(x); 
-                memo[val] = k + fn(val - pow(x, k)); 
-                if (pow(x, k+1) < 2*val) 
-                    memo[val] = min(memo[val], k+1+fn(pow(x, k+1) - val)); 
-            }
-            return memo[val]; 
-        }; 
+        if(x==target) return 0;
         
-        return fn(target); 
+        long long int sum=x;
+        int ans=0;
+        
+        //greedly get to the closest number such that x>target and then perform either (x/x)+(x/x)+...+(x/x) or
+        // x-(x/x)-(x/x)... etc
+        
+        while(sum<target){
+             ans++;
+            sum =sum*x;
+           
+        }
+        
+        // at this point sum is either greater than target or equal to target
+        if(sum==target)
+            return ans;
+        
+        // Now we have two choice to reduce the number either we make new number
+        // ---> target = sum-target (but this can only work if target>sum-target)
+        // ---> target-sum/x
+        int op1 = INT_MAX, op2 = INT_MAX;
+        if(sum-target<target){
+            op1 = leastOpsExpressTarget(x, sum-target) + ans;
+        }
+        op2 = leastOpsExpressTarget(x, target-sum/x) + ans-1;
+        
+        return min(op1,op2)+1;
     }
 };
