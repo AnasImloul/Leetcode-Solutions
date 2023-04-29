0b95f79340633a22a609fb1d67a35556262364dc
AnasImloul <pydev27@gmail.com>
2023-04-29 16:26:49 +0100

    Runtime 360 ms (Top 61.88%) | Memory 25.0 MB (Top 6.33%)

--- scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.py
+++ scripts/algorithms/F/Find All Duplicates in an Array/Find All Duplicates in an Array.py
@@ -1,4 +1,15 @@
-# Runtime: 793 ms (Top 10.96%) | Memory: 22.9 MB (Top 23.08%)
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        return [k for k,v in Counter(nums).items() if v>=2][::-1]
+        res = []
+        hm = {}
+        # adding entries in hashmap to check frequency
+        for i, v in enumerate(nums):
+            if v not in hm:
+                hm[v] = 1
+            else:
+                hm[v] += 1
+        # checking frequency of item and adding output to an array
+        for key, value in hm.items():
+            if value > 1:
+                res.append(key)
+        return res
