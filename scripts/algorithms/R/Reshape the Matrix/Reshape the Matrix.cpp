8266ba839672fd51fac0b2cf5a3703c21b44ead2
AnasImloul <pydev27@gmail.com>
2023-04-29 16:43:35 +0100

    Runtime 13 ms (Top 46.3%) | Memory 10.0 MB (Top 95.2%)

--- scripts/algorithms/R/Reshape the Matrix/Reshape the Matrix.cpp
+++ scripts/algorithms/R/Reshape the Matrix/Reshape the Matrix.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        int m = mat.size(), n = mat[0].size();
-        if(m*n != r*c)
+        if(mat.size()* mat[0].size()!= r * c) {
             return mat;
-        vector<vector<int>> res(r, vector<int>(c));
-        
-        int k = 0, l = 0;
-        for(int i = 0; i < m; i++) 
-            for(int j = 0; j < n; j++) {
-                res[k][l++] = mat[i][j]; 
-                if(l == c) k++,l = 0;
+        } 
+        vector<vector<int>>v(r,vector<int>(c));
+        int k = 0;
+        int l = 0;
+
+        for(int i = 0; i < mat.size(); i++) {
+            for(int j = 0; j < mat[0].size(); j++) {
+                if(l == v[0].size()) {
+                    l = 0;
+                    k++;
+                }
+
+                v[k][l++] = mat[i][j];
             }
-                
-        return res;
+        }
+        return v;
     }
 };
