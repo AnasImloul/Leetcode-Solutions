fec0432e6cce164aed38dae96cd365bbbc09eeab
AnasImloul <pydev27@gmail.com>
2023-04-29 16:22:23 +0100

    Runtime 534 ms (Top 71.15%) | Memory 17.0 MB (Top 5.77%)

--- scripts/algorithms/S/Split Array into Consecutive Subsequences/Split Array into Consecutive Subsequences.py
+++ scripts/algorithms/S/Split Array into Consecutive Subsequences/Split Array into Consecutive Subsequences.py
@@ -1,24 +1,44 @@
-# Runtime: 574 ms (Top 89.21%) | Memory: 15.3 MB (Top 66.59%)
-
-import collections
 class Solution:
-    def isPossible(self, nums: List[int]) -> bool:
 
-        counter = collections.Counter(nums)
-        end = collections.defaultdict(int)
+    from collections import defaultdict
 
+    def isPossible(self, nums):
+        # If the length of the array is less than 3, it's not possible to create subsequences of length 3 or more.
+        if len(nums) < 3:
+            return False
+
+        # 'count' dictionary stores the frequency of each number in the input array.
+        count = defaultdict(int)
+
+        # 'tails' dictionary stores the number of subsequences that end at a certain number.
+        tails = defaultdict(int)
+
+        # Populate the 'count' dictionary with the frequency of each number.
         for num in nums:
+            count[num] += 1
 
-            if counter[num]:
-                counter[num]-=1
+        # Iterate through the input array.
+        for num in nums:
+            # If the count of the current number is 0, it means this number has already been used in a subsequence.
+            if count[num] == 0:
+                continue
+            # If there is a subsequence that ends with the current number minus 1,
+            # it means we can extend that subsequence by adding the current number.
+            elif tails[num - 1] > 0:
+                tails[num - 1] -= 1  # Decrease the count of the tails that end with the current number minus 1.
+                tails[num] += 1  # Increase the count of the tails that end with the current number.
+            # If there are enough numbers after the current number to form a subsequence,
+            # create a new subsequence starting with the current number.
+            elif count[num + 1] > 0 and count[num + 2] > 0:
+                count[num + 1] -= 1  # Decrease the count of the next number.
+                count[num + 2] -= 1  # Decrease the count of the number after the next number.
+                tails[num + 2] += 1  # Increase the count of the tails that end with the number after the next number.
+            else:
+                # If we can't extend an existing subsequence or start a new one, return False.
+                return False
 
-                if end[num-1]:
-                    end[num-1]-=1
-                    end[num]+=1
-                elif counter[num+1] and counter[num+2]:
-                    counter[num+1]-=1
-                    counter[num+2]-=1
-                    end[num+2] +=1
-                else:
-                    return False
+            # Decrease the count of the current number since it's used in a subsequence.
+            count[num] -= 1
+
+        # If the function successfully iterates through the entire array, return True.
         return True
