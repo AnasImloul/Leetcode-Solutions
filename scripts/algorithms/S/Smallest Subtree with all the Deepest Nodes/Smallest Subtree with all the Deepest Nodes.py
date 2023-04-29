9f14b555fce55b7535f227c64693e46cda4d3f45
AnasImloul <pydev27@gmail.com>
2023-04-29 18:48:17 +0100

    Runtime 55 ms (Top 6.59%) | Memory 16.0 MB (Top 8.24%)

--- scripts/algorithms/S/Smallest Subtree with all the Deepest Nodes/Smallest Subtree with all the Deepest Nodes.py
+++ scripts/algorithms/S/Smallest Subtree with all the Deepest Nodes/Smallest Subtree with all the Deepest Nodes.py
@@ -1,22 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-                                # dfs traverses  the node's subtree and returns the max
-                                # level in the subtree if dfs(node.left) == dfs(node.right) 
-                                # then node is a candidate for the answer. The dict d 
-                                # stores the the most recent candidate node by key = maxLevel
-                                # for the subtree. The root returns the actual max level,
-                                # so the answer is d[dfs(root)]
+
+        # find a set of deepest nodes first
+        deepest_nodes = [0]
+        self.find_deepest(root, 0, deepest_nodes)
         
-        def dfs(node = root, level = 0):
-            if not node:
-                return level-1
-            
-            l, r = dfs(node.left, level+1), dfs(node.right, level+1)
-            
-            if l == r:
-                d[l] = node
-                
-            return max(l,r)
+        # extract the depth and also make a set out of the values
+        targets = set(deepest_nodes[1:])
+
+        # get the subtree
+        return self.find_merge(root, targets)[0]
+
+    def find_deepest(self, node, current_depth, deepest_nodes):
+
+        # make a check
+        if not node:
+            return
         
-        d = {}
-        return d[dfs()]
+        # make a check if we are a deep node
+        if current_depth > deepest_nodes[0]:
+            deepest_nodes.clear()
+            deepest_nodes.append(current_depth)
+            deepest_nodes.append(node.val)
+        elif current_depth == deepest_nodes[0]:
+            deepest_nodes.append(node.val)
+        
+        # go deeper
+        self.find_deepest(node.left, current_depth+1, deepest_nodes)
+        self.find_deepest(node.right, current_depth+1, deepest_nodes)
+    
+    def find_merge(self, node, targets):
+
+        # make a check
+        if not node:
+            return None, set()
+
+        # check whether we are a target
+        found = set()
+        if node.val in targets:
+            found.add(node.val)
+
+        # go deeper and get result nodes
+        nleft, left = self.find_merge(node.left, targets)
+        if nleft is not None:
+            return nleft, set()
+        nright, right = self.find_merge(node.right, targets)
+        if nright is not None:
+            return nright, set()
+
+        # merge the found set
+        found = found | left | right
+
+        # check whether we found all
+        if not (targets - found):
+            return node, set()
+        else:
+            return None, found
