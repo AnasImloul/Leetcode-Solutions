aa3e271e0fbba31f9bd128264f96a516dd953479
AnasImloul <pydev27@gmail.com>
2023-04-29 18:37:26 +0100

    Runtime 5 ms (Top 87.59%) | Memory 6.0 MB (Top 27.13%)

--- scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.cpp
+++ scripts/algorithms/P/Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
-    int solve(int n){
-        
-    int cnt=0;
-    int v=0;
-    while (n > 0) {
-        v= n % 2;
-        if(v==1)
-            cnt++;
-        n = n / 2;
-        
-    }
-
-   return cnt;
-        
-}
-    bool isprime(int n){
-         if (n <= 1)
-        return false;
- 
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
- 
-    return true;
-    }
-    
- 
     int countPrimeSetBits(int left, int right) {
-        int count = 0;
-        for(int i=left;i<=right;i++){
-            if(isprime(solve(i))){
-                count++;
-            }
+        int ans=0;
+        while(left<=right) {
+            int cnt=__builtin_popcount(left);
+            if(cnt==2 || cnt==3 || cnt==5 || cnt==7 || cnt==11 || cnt==13 || cnt==17 || cnt==19)
+                ++ans;
+            ++left;
         }
-        return count;
+        return ans;
     }
 };
