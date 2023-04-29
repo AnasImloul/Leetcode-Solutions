2d2fa474cb53c2f0a027d13973ced1b5476222b3
AnasImloul <pydev27@gmail.com>
2023-04-29 17:23:48 +0100

    Runtime 36 ms (Top 36.71%) | Memory 17.0 MB (Top 6.8%)

--- scripts/algorithms/F/Find in Mountain Array/Find in Mountain Array.py
+++ scripts/algorithms/F/Find in Mountain Array/Find in Mountain Array.py
@@ -1,38 +1,54 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        n = mountain_arr.length()
-        #edge case
-        if n == 1:
-            if mountain_arr.get(0) == target:
-                return 0
-            return -1
-		
-		start = 0
-        end = n - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if mid == 0:
-                start = 1
-                continue
-            if mountain_arr.get(mid-1) > mountain_arr.get(mid) or mountain_arr.get(mid) > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        if mountain_arr.get(end) == target:
-            return end
-			
-        start = 0
-        end = n - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if mountain_arr.get(mid) == target:
-                return mid
-            if mid == 0:
-                start = 1
-                continue
-            if mountain_arr.get(mid-1) < mountain_arr.get(mid) or mountain_arr.get(mid) > target:
-                start = mid + 1
-            else:
-                end = mid - 1
+        min_index = -1
+        peak = self.findpeak(mountain_arr)
         
+        min_index = self.binary_search(0, peak, mountain_arr, target, 1)
+        if min_index == -1:
+            min_index = self.binary_search(peak+1, mountain_arr.length() - 1, mountain_arr, target, -1)
+        return min_index
+
+    def findpeak(self, mountain_arr):
+        start = 0
+        end = mountain_arr.length() - 1
+        while start < end:
+            mid = start + int((end - start)/2)
+            if mountain_arr.get(mid) < mountain_arr.get(mid + 1):
+                start = mid + 1
+            else:
+                end = mid
+        
+        return start
+    
+    def binary_search(self, start, end, mountain_arr, target, asc):
+        while start <= end:
+            mid = start + int((end - start)/2)
+            mountain_arr_get_mid = mountain_arr.get(mid)
+            if target == mountain_arr_get_mid:
+                return mid
+            if asc == 1:
+                if target < mountain_arr_get_mid:
+                    end = mid - 1
+                elif target > mountain_arr_get_mid:
+                    start = mid + 1
+            else:
+                if target < mountain_arr_get_mid:
+                    start = mid + 1
+                elif target > mountain_arr_get_mid:
+                    end = mid - 1
+            
         return -1
+
+
+
+
+
+
