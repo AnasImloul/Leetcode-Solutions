b567cff2f16eca63dfdd978ac424d5711fd985d2
AnasImloul <pydev27@gmail.com>
2023-04-29 17:23:01 +0100

    Runtime 744 ms (Top 99.21%) | Memory 23.0 MB (Top 10.16%)

--- scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.py
+++ scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.py
@@ -1,25 +1,22 @@
-class Solution:
-	def numberOfSubarrays(self, arr, k):
-		# the difference gives the exact K elements count
-		return self.atMostKElements(arr, k) - self.atMostKElements(arr, k-1)
-
-	def atMostKElements(self, arr, k):
-		i, j = 0, 0
-		res, curr_window_count = 0, 0
-		for j in range(len(arr)):
-			# the {condition} which increases the curr_window_count. Different for different problems
-			if arr[j]%2 != 0: curr_window_count += 1
-
-			# check if the window is valid. If its invalid, make it valid by shortening the left pointer.
-			# Valid is when curr_window_count <= k (at most K elements)
-			if curr_window_count > k:
-				while curr_window_count > k:
-					# this condition will be specific to the problem
-					if arr[i]%2 != 0: curr_window_count -= 1
-					i += 1
-
-			# total nos of elements in the valid window is added to the res. 
-			# eg: [1, 2, 3], if this is the valid window, then [3], [2,3], [1,2,3] are all valid for solving at most K elements problem.
-			res += j-i+1
-			j += 1
-		return res
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        start = tempodd = count = 0
+        first = -1
+        for i, num in enumerate(nums):
+            # If odd, count the number of odds we got
+            # Also lets keep track of first odd we encounter
+            if num % 2:
+                tempodd += 1
+                if first == -1:
+                    first = i
+            # If we get k odds or more, we should count the subarrays
+            if tempodd >= k:
+                # We got one more than k counts, Just remove the first odd
+                if tempodd > k:
+                    first = start = first + 1
+                    while nums[first] % 2 == 0:
+                        first = first + 1
+                    tempodd -= 1
+                # If we get exact k odds, we should count the subarrays
+                count += first - start + 1
+        return count
