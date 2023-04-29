d2a2ad36200d7001f1c39a284a59d668648858fd
AnasImloul <pydev27@gmail.com>
2023-04-29 18:43:15 +0100

    Runtime 533 ms (Top 8.72%) | Memory 16.0 MB (Top 5.24%)

--- scripts/algorithms/C/Count Vowel Substrings of a String/Count Vowel Substrings of a String.py
+++ scripts/algorithms/C/Count Vowel Substrings of a String/Count Vowel Substrings of a String.py
@@ -1,3 +1,36 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-        return sum(set(word[i:j+1]) == set('aeiou') for i in range(len(word)) for j in range(i+1, len(word)))
+        vowels = {'a','e','i','o','u'}
+        pointer = 0
+        res = 0
+        if len(word) <= 4:
+            return 0
+        while pointer != len(word)-4:
+            # if set(list(word[pointer:pointer+5])) == vowels:
+            #     temp = 1
+            #     res += 1
+            #     while set(list(word[pointer:pointer+temp+5])) == vowels and pointer+temp+4 != len(word):
+            #         res += 1
+            #         temp += 1
+            # elif word[pointer] in vowels:
+            #     temp = 1
+            #     while set(list(word[pointer:pointer+5+temp])) != vowels:
+            #         temp += 1
+            #     res += 1
+            # pointer += 1
+            temp = 0
+            if word[pointer] in vowels:
+                while temp+pointer != len(word)-4:
+                    test_1 = set(list(word[pointer:pointer+temp+5]))
+                    test_2 = word[pointer:pointer+temp+5]
+                    if set(list(word[pointer:pointer+temp+5])).issubset(vowels): 
+                        if set(list(word[pointer:pointer+temp+5])) == vowels:
+                            res += 1
+                        temp+=1
+                    else:
+                        break
+                    
+            pointer += 1
+        return res
+
+            
