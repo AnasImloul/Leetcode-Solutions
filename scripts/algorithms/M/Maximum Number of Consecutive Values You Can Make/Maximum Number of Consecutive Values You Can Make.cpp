c42f953f60c7bf362dc5bb62922fff9cc202a83f
AnasImloul <pydev27@gmail.com>
2023-04-29 16:43:16 +0100

    Runtime 167 ms (Top 9.82%) | Memory 66.0 MB (Top 61.82%)

--- scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.cpp
+++ scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int getMaximumConsecutive(vector<int>& coins) {
-        // support variables
-        int res = 1;
-        // preparing coins
-        sort(begin(coins), end(coins));
-        for (int coin: coins) {
-            // checking if we could have reached coin without gaps
-            if (res >= coin) res += coin;
-            else break;
+        sort(coins.begin(), coins.end());
+        int n = coins.size();
+        int ans = 1;
+        for(int i = 0; i < n; i++) {
+            if(coins[i] > ans) return ans;
+            ans += coins[i]; 
         }
-        return res;
+        return ans;
     }
 };
