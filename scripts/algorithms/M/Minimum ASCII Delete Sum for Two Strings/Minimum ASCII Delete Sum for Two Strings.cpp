6d4350f77b039b93ede5c6bac050f3ba76fb40e2
AnasImloul <pydev27@gmail.com>
2023-04-29 19:25:01 +0100

    Runtime 75 ms (Top 18.17%) | Memory 15.0 MB (Top 16.26%)

--- scripts/algorithms/M/Minimum ASCII Delete Sum for Two Strings/Minimum ASCII Delete Sum for Two Strings.cpp
+++ scripts/algorithms/M/Minimum ASCII Delete Sum for Two Strings/Minimum ASCII Delete Sum for Two Strings.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    /*
-Logic:This question is just a variation of lcs as there we need to find out the length,
-here we need to find out sum of their ascii values, just a slight change is there 
-i.e we need to add the ascii value of the character which is equal to that we can 
-memorize its value and at last we have our ans which is basically the sum of ascii value
-of the longest common subsequence. So we need to find lowest ASCII sum of deleted characters to make two strings equal so we will 
-calculate the sum of both the string's ascii values and substract our ans from it.
-    */
-    int minimumDeleteSum(string s1, string s2) {
-        int n=s1.size(),m=s2.size(),sum1=0,sum2=0;
-        int dp[n+1][m+1];
-        //calculate sum of ascii values of s1.
-        for(int i=0;i<n;i++)
-            sum1+=s1[i];
-        //calculate sum of ascii values of s2.
-        for(int i=0;i<m;i++)
-            sum2+=s2[i];
-        //apply lcs base condition.
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0||j==0)
-                    dp[i][j]=0;
-            }
+   long long int solve(int i,int j,string &s,string &t,vector<vector<int>> &dp)
+    {
+        if(i<0) {
+            long long ans=0;
+           for(int k=0;k<j+1;k++)
+           {
+              int x=int(t[k]);
+              ans+=x;
+           }
+           return ans;
         }
-        //apply lcs...
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s1[i-1]==s2[j-1])
-                    dp[i][j]=s1[i-1]+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+        if(j<0)
+        { 
+            long long ans=0;
+           for(int k=0;k<i+1;k++)
+           {
+              int x=int(s[k]);
+              ans+=x;
+           }
+           return ans;
         }
-        int ans=dp[n][m];
-        //final_ans = sum1-ans+sum2-ans=>sum1+sum2-2*ans
-        return sum1+sum2-2*ans;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]= solve(i-1,j-1,s,t,dp);
+        else{
+           long long x=int(s[i]) +solve(i-1,j,s,t,dp);
+             long long y=int(t[j])+solve(i,j-1,s,t,dp);
+             return dp[i][j]=min(x,y);
+        }
+    }
+    long long int minimumDeleteSum(string s1, string s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n-1,m-1,s1,s2,dp);
     }
 };
