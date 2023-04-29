35fb8d89e4932cdc46b5ad1719baaec993a06c97
AnasImloul <pydev27@gmail.com>
2023-04-29 19:15:11 +0100

    Runtime 185 ms (Top 30.79%) | Memory 97.0 MB (Top 9.68%)

--- scripts/algorithms/S/Sum of Beauty in the Array/Sum of Beauty in the Array.cpp
+++ scripts/algorithms/S/Sum of Beauty in the Array/Sum of Beauty in the Array.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
-        int sum=0;
-        vector<int> minV,maxV;
-        int maximum=0;
-        int minimum=INT_MAX;
-        for(int i=0;i<nums.size();i++)
-        {
-            maximum=max(maximum,nums[i]);
-            maxV.push_back(maximum);
+        int n=nums.size();
+        vector<int>right;
+        vector<int>left;
+        int low=nums[0];
+        for(int i=0;i<nums.size();i++){
+            
+            left.push_back(low);
+            low=max(low,nums[i]);
         }
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-              minimum=min(minimum,nums[i]);
-            minV.push_back(minimum);
+        low=nums[n-1];
+        for(int i=n-1;i>=0;i--){
+            
+            right.push_back(low);
+            low=min(low,nums[i]);
         }
-        for(int i=1;i<nums.size()-1;i++)
-        {
-           if((maxV[i-1]<nums[i] && minV[minV.size()-i-2]>nums[i]))
-              sum+=2;
-           else if(nums[i-1]<nums[i] && nums[i+1]>nums[i])
-               sum+=1;
+        reverse(right.begin(),right.end());
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]>left[i] && nums[i]<right[i]){
+                ans+=2;
+            }
+            else if(nums[i]>nums[i-1] && nums[i]<nums[i+1]){
+                ans+=1;
+            }
+            else{
+                ans+=0;
+            }
         }
-      return sum;  
+        return ans;
+        
     }
 };
