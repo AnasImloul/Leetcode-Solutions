926430585f312280327dfa2814508f6eaeabdada
AnasImloul <pydev27@gmail.com>
2023-04-29 18:45:41 +0100

    Runtime 1446 ms (Top 99.58%) | Memory 26.0 MB (Top 6.53%)

--- scripts/algorithms/F/Find Missing Observations/Find Missing Observations.py
+++ scripts/algorithms/F/Find Missing Observations/Find Missing Observations.py
@@ -1,5 +1,3 @@
-# Runtime: 3172 ms (Top 17.12%) | Memory: 25 MB (Top 17.51%)
-
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
         missing_val, rem = divmod(mean * (len(rolls) + n) - sum(rolls), n)
