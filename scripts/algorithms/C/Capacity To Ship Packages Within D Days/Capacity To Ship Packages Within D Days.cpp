2b4be9217b2b20cee18cf80240d4866878e5773e
AnasImloul <pydev27@gmail.com>
2023-04-29 17:32:55 +0100

    Runtime 64 ms (Top 73.51%) | Memory 31.0 MB (Top 96.31%)

--- scripts/algorithms/C/Capacity To Ship Packages Within D Days/Capacity To Ship Packages Within D Days.cpp
+++ scripts/algorithms/C/Capacity To Ship Packages Within D Days/Capacity To Ship Packages Within D Days.cpp
@@ -1,40 +1,58 @@
 class Solution {
 public:
-    bool isPossible(vector<int> weights, int mid, int days){
-        int day=1, sum=0; //minimum day and weight
-        for(int i=0; i<weights.size(); i++){
-            sum += weights[i];
-            if(sum > mid){
-                day++; sum = weights[i];
+    // function to check if it is possible to ship all the items within the given number of days using a given weight capacity
+    bool isPossible(int cp, vector<int>& weights, int days)
+    {
+        int d = 1;
+        int cw = 0;
+        // iterate through the weights and check if it is possible to ship all the items
+        for (auto x : weights) {
+            if (x + cw <= cp) {
+                // if the current weight can be added to the current capacity, add it
+                cw += x;
+            } else {
+                // if not, increment the number of days required for shipping and start a new shipment
+                d++;
+                cw = x;
+                if (x > cp) {
+                    // if the current weight is greater than the current capacity, it is impossible to ship all the items
+                    return false;
+                }
             }
         }
-        
-        //for mid(weight) how much day it is taking? at next step we reduce wt if this is possible
-        return day <= days;
+        // check if it is possible to ship all the items within the given number of days
+        return d <= days;
     }
     
-    int shipWithinDays(vector<int>& weights, int days) {
-        int max_weight = *max_element(weights.begin(), weights.end()),
-            total_weight = accumulate(weights.begin(), weights.end(), 0),
-            least_weight;// final Answer
+    // function to find the minimum weight capacity required to ship all the items within the given number of days
+    int shipWithinDays(vector<int>& weights, int days)
+    {
+        // initialize the search range for the weight capacity
+        int s = 1;
+        int e = 1E9;
+        int ans = -1;
         
-        //base case
-        if(weights.size() == days) return max_weight;
-        
-        //search for minimum weight
-        while(max_weight <= total_weight){
-            //weight to be shipped at a time (candidate)
-            int mid = max_weight + (total_weight - max_weight)/2;
+        // perform binary search on the weight capacity range
+        while (s <= e) {
+            // set the mid-point of the range as the current weight capacity
+            int mid = (s + e) / 2;
             
-            //function to know if we can allocate this weight(mid) to satisfy condition?
-            if(isPossible(weights, mid, days)){
-                least_weight = mid;
-                
-                //to find minimum weight ever after we find a possible candidate
-                total_weight = mid -1;
+            // check if it is possible to ship all the items within the given number of days using the current weight capacity
+            bool ok = isPossible(mid, weights, days);
+            
+            // if it is possible to ship all the items within the given number of days using the current weight capacity,
+            // set the current weight capacity as the new answer and reduce the upper bound of the weight capacity range
+            if (ok) {
+                ans = mid;
+                e = mid - 1;
+            } else {
+                // if it is not possible to ship all the items within the given number of days using the current weight capacity,
+                // increase the lower bound of the weight capacity range
+                s = mid + 1;
             }
-            else max_weight = mid +1; 
         }
-        return least_weight;
+        
+        // return the minimum weight capacity required to ship all the items within the given number of days
+        return ans;
     }
 };
