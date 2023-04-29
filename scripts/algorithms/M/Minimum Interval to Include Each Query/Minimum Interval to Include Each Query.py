df785a49a01f2d617635e2c5b21f757b32343d24
AnasImloul <pydev27@gmail.com>
2023-04-29 16:14:25 +0100

    Runtime 1906 ms (Top 16.3%) | Memory 61.0 MB (Top 13.41%)

--- scripts/algorithms/M/Minimum Interval to Include Each Query/Minimum Interval to Include Each Query.py
+++ scripts/algorithms/M/Minimum Interval to Include Each Query/Minimum Interval to Include Each Query.py
@@ -1,10 +1,36 @@
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        
+        # sort queries from small to large
+        q = deque(sorted([(x, i) for i, x in enumerate(queries)]))
+        
+        # answer to queries, initial state set to -1
         ans = [-1] * len(queries)
-        intervals.sort(key = lambda i: i[1] - i[0])
-        queries = sorted([q, i] for i, q in enumerate(queries))
-        for left, right in intervals:
-            idx = bisect.bisect(queries, [left])
-            while idx < len(queries) and queries[idx][0] <= right:
-                ans[queries.pop(idx)[1]] = right - left + 1
+
+        # sort intervals by low, high and size
+        ivals = deque(sorted([(a, b, b - a + 1) for a, b in intervals]))
+        
+        # available intervals
+        cands = []
+
+        
+        while q:
+            x, i = q.popleft()
+            
+            # if lower bound of intervals on the top of stack <= current query
+            while ivals and x >= ivals[0][0]:
+                a, b, c = ivals.popleft()
+                # if higher bound of intervals also meets the requirements
+                # if not then discard the interval
+                if x <= b:
+                    heappush(cands, (c, b, a))
+            
+            # udpate available intervals by removing old ones which no longer has a eligible higher bound
+            while cands:
+                c, b, a = heappop(cands)
+                if x <= b:
+                    ans[i] = c
+                    heappush(cands, (c, b, a))
+                    break
+
         return ans
