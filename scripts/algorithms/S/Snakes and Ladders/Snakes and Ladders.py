4a423290285ee81ecb60bc99a3031a3c7af94b5f
AnasImloul <pydev27@gmail.com>
2023-04-29 17:25:33 +0100

    Runtime 126 ms (Top 47.73%) | Memory 16.0 MB (Top 7.87%)

--- scripts/algorithms/S/Snakes and Ladders/Snakes and Ladders.py
+++ scripts/algorithms/S/Snakes and Ladders/Snakes and Ladders.py
@@ -1,34 +1,35 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n=len(board)
-        vis=[0]*(n**2+1)
-        board.reverse()
         
-        # code for reversing board
-        for i in range(len(board)):
-            if i%2!=0:
-                x=board[i]
-                x.reverse()
-                board[i]=x
-				
-        board=sum(board,[]) # this line will convert 2d-array into 1d-array
+        # creating a borad map to loop-up the square value
+        board_map = {}
+        i = 1
+        b_rev = board[::-1]
+        for d, r in enumerate(b_rev):
+			# reverse for even rows - here d is taken as direction 
+            if d%2 != 0: r = r[::-1] 
+            for s in r:
+                board_map[i] = s
+                i += 1
         
-        for i in range(len(board)):
-            if board[i]!=-1:
-                vis[i+1]=board[i]
-                
-        q=collections.deque()
-        q.append([1,0])
-        seen=set()
+        # BFS Algorithm
+        q = [(1, 0)] # (curr, moves)
+        v = set()
+        goal = len(board) * len(board) # end square
+        
         while q:
-            idx,moves=q.popleft()
-            for i in range(1,7):
-                next_idx=idx+i
-                if vis[next_idx]!=0:
-                    next_idx=vis[next_idx]
-                if next_idx==(n**2):
-                    return (moves+1)
-                if next_idx not in seen:
-                    seen.add(next_idx)
-                    q.append([next_idx,moves+1])
+            curr, moves = q.pop(0)
+            # win situation
+            if curr == goal: return moves
+            # BFS on next 6 places (rolling a die)
+            for i in range(1, 7):
+                # skip square outside board
+                if curr+i > goal: continue
+                # get value from mapping
+                next_pos = curr+i if board_map[curr+i] == -1 else board_map[curr+i]
+                if next_pos not in v:
+                    v.add(next_pos)
+                    q.append((next_pos, moves+1))
+        
         return -1
+ 
