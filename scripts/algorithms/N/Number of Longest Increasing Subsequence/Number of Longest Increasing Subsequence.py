738f380ed933763715f83a5eb5abad45f79062c3
AnasImloul <pydev27@gmail.com>
2023-04-29 18:42:58 +0100

    Runtime 147 ms (Top 91.8%) | Memory 16.0 MB (Top 5.28%)

--- scripts/algorithms/N/Number of Longest Increasing Subsequence/Number of Longest Increasing Subsequence.py
+++ scripts/algorithms/N/Number of Longest Increasing Subsequence/Number of Longest Increasing Subsequence.py
@@ -1,21 +1,33 @@
 class Solution:
-    def findNumberOfLIS(self, nums) -> int:
-        cnt=[1 for _ in range(len(nums))]
-        dp=[1 for _ in range(len(nums))]
-        maximum=0
-        maxLst=[]
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[j]<nums[i]:
-                    if (dp[j]+1)>dp[i]:
-                        dp[i]=dp[j]+1
-                        cnt[i]=cnt[j]
-                    elif (dp[j]+1)==dp[i]:
-                        cnt[i]+=cnt[j]
-            if dp[i]>maximum:
-                maximum=dp[i]
-                maxLst=[i]
-            elif dp[i]==maximum:
-                maxLst.append(i)
-        ans=sum([cnt[i] for i in maxLst])
-        return ans
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        
+        if not nums or len(nums) == 0:
+            return 0
+        
+        def find_pos(sub, val):
+            left, right = 0, len(sub) - 1
+            while left < right:
+                mid = (left + right) >> 1
+                if sub[mid] >= val: 
+                    right = mid
+                else:
+                    left = mid + 1
+            return left 
+        
+    
+        sub_list = []
+        
+        for val in nums:
+            if len(sub_list) == 0 or val > sub_list[-1][-1][0]:
+                # should append a new element at the end
+                cur_count = sum([x[1] for x in sub_list[-1] if val > x[0]]) if len(sub_list) != 0 else 1
+                sub_list.append([(val, cur_count)])
+            else:
+                # get the last number to turn it back to a LIS problem
+                cur_sub = [array[-1][0] for array in sub_list]
+                pos = find_pos(cur_sub, val)
+                # if pos == 0, means it is smallest, no need to look the previous level and set it to be 1
+                cur_count = sum([x[1] for x in sub_list[pos - 1] if val > x[0]]) if pos > 0 else 1
+                sub_list[pos].append((val, cur_count))
+      
+        return sum([x[1] for x in sub_list[-1]])
