d9e4fe64650f7ec6918ae3c764da78b7a8471bc9
AnasImloul <pydev27@gmail.com>
2023-04-29 18:38:59 +0100

    Runtime 6 ms (Top 48.39%) | Memory 8.0 MB (Top 9.53%)

--- scripts/algorithms/R/Richest Customer Wealth/Richest Customer Wealth.cpp
+++ scripts/algorithms/R/Richest Customer Wealth/Richest Customer Wealth.cpp
@@ -1,15 +1,14 @@
-// Runtime: 9 ms (Top 48.51%) | Memory: 7.7 MB (Top 99.02%)
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int res = 0;
-        for(int i =0;i<accounts.size();i++){
-            int temp = 0;
-            for(int j = 0;j<accounts[i].size();j++){
-                temp+=accounts[i][j];
-            }
-            res = max(res,temp);
+           int maxWealth = 0;
+        for (auto account : accounts) {
+            int currentSum = 0;
+            for (int x : account) currentSum += x;
+            if (currentSum > maxWealth) maxWealth = currentSum;
         }
-        return res;
+        return maxWealth;
     }
 };
+
+
