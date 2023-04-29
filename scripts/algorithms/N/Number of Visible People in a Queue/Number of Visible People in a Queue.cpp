2aaf036e3628111f16061f20dd929aa7f369d5c9
AnasImloul <pydev27@gmail.com>
2023-04-29 17:27:06 +0100

    Runtime 215 ms (Top 16.96%) | Memory 88.0 MB (Top 13.6%)

--- scripts/algorithms/N/Number of Visible People in a Queue/Number of Visible People in a Queue.cpp
+++ scripts/algorithms/N/Number of Visible People in a Queue/Number of Visible People in a Queue.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    vector<int> canSeePersonsCount(vector<int>& heights) {
-        vector<int>st;
-        int n=heights.size();
-		
-		// Yes my friend, we can use vector just like stack
-        vector<int>ans(n,0);
-        st.push_back(heights[n-1]);
-        for(int i=n-2;i>=0;i--)
-        {
-        
-            std::vector<int>::iterator up;
-            up = std::upper_bound(st.begin(), st.end(), heights[i], std::greater<int>());
-            
-            int ele = up-st.begin();
-           
-		   // This is just consequence of appling binary search in decreasing array
-		   if(ele!=0)
-                ele--;
-            ele = st.size()-ele;
-            
-            ans[i]=ele;
-			// Update Mono Stack 
-            while(st.size()>0&&st.back()<=heights[i])
-                st.pop_back();
-            st.push_back(heights[i]);
+    vector<int> canSeePersonsCount(vector<int>& h) {
+        vector<int>ans ;
+        stack<int>s ;
+        int a = 0;
+        for(int i = h.size()-1 ; i >= 0 ; i--){
+            if(s.empty()){
+                ans.push_back(a);
+                s.push(h[i]);a++;
+            }
+            else{
+                if(s.top() > h[i]){
+                    ans.push_back(1);
+                    s.push(h[i]);a++;
+                }
+                else{
+                int b = 0;
+                while(!s.empty() && s.top() < h[i]){
+                    s.pop() ; a--;b++;
+                }
+                if(!s.empty())b++;
+                ans.push_back(b);
+                s.push(h[i]); a++;
+                } 
+            }
         }
+        reverse(ans.begin() , ans.end());
         return ans;
     }
 };
