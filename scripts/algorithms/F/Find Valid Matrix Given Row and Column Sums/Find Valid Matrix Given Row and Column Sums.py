534c4fc85252c01a86a7f9b684b7b827ce107d1c
AnasImloul <pydev27@gmail.com>
2023-04-29 16:29:59 +0100

    Runtime 737 ms (Top 78.91%) | Memory 21.0 MB (Top 6.18%)

--- scripts/algorithms/F/Find Valid Matrix Given Row and Column Sums/Find Valid Matrix Given Row and Column Sums.py
+++ scripts/algorithms/F/Find Valid Matrix Given Row and Column Sums/Find Valid Matrix Given Row and Column Sums.py
@@ -1,13 +1,28 @@
-# Runtime: 2542 ms (Top 7.54%) | Memory: 19.1 MB (Top 61.04%)
 class Solution:
-    def restoreMatrix(self, rs: List[int], cs: List[int]) -> List[List[int]]:
-        row = len(rs)
-        col = len(cs)
-        ans = [[0 for i in range(col)] for j in range(row)]
-        for i in range(row):
-            for j in range(col):
-                el = min(rs[i], cs[j])
-                ans[i][j] = el
-                rs[i] -= el
-                cs[j] -= el
-        return ans
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        matrix = []
+        for row in range(len(rowSum)):
+            data, array = rowSum[row], []
+            for col in range(len(colSum)):
+                if data == 0 or colSum[col] == 0:
+                    array.append(0)
+                elif data > colSum[col]:
+                    data -= colSum[col]
+                    array.append(colSum[col])
+                    colSum[col] = 0
+                else:
+                    array.append(data)
+                    colSum[col] -= data
+                    data = 0
+            matrix.append(array)
+
+        return matrix
+                    
+                    
+
+
+
+
+
+
+
