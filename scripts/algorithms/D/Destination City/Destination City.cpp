28df40030c015115ef47e8f4819e0fc3ca79e9aa
AnasImloul <pydev27@gmail.com>
2023-04-29 19:20:55 +0100

    Runtime 7 ms (Top 98.28%) | Memory 10.0 MB (Top 79.46%)

--- scripts/algorithms/D/Destination City/Destination City.cpp
+++ scripts/algorithms/D/Destination City/Destination City.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        string ans="";
-        int flag=0;
-        for(int i=0;i<paths.size();i++){
-            for(int j=0;j<paths.size();j++){
-                if(paths[i][1]==paths[j][0])
-                    break;
-                else{
-                    
-                    if(j==paths.size()-1){
-                        ans+=paths[i][1];
-                        flag=1;
-                        break;
-                    }
-                }
-            }
-            if(flag==1)
-                break;
+        string ans;
+        unordered_map<string, int> m;
+        for(int i=0; i<paths.size(); i++){
+            m[paths[i][0]]++;
+            m[paths[i][1]]--;
         }
+        for(auto city : m){
+            if(city.second == -1) ans = city.first;
+        }
+
         return ans;
     }
 };
