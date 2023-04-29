b3199123daa727f0197e4ecc63c376d0431ae520
AnasImloul <pydev27@gmail.com>
2023-04-29 16:10:02 +0100

    Runtime 56 ms (Top 7.98%) | Memory 16.0 MB (Top 7.23%)

--- scripts/algorithms/C/Create Target Array in the Given Order/Create Target Array in the Given Order.py
+++ scripts/algorithms/C/Create Target Array in the Given Order/Create Target Array in the Given Order.py
@@ -1,13 +1,42 @@
 class Solution:
-    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+    def createTargetArray(self, nums, index):
         
-        out = [] 
-        for num, idx in zip(nums, index):
+        def merge(arr, low, mid, high):
+            L, R = arr[low:mid+1], arr[mid+1:high+1]
+            i = j = 0
+            k = low
             
-            if len(out) <= idx:
-                out.append(num)
+            while i < len(L) and j < len(R):
+                if L[i][0] + j >= R[j][0]:
+                    arr[k] = R[j]
+                    j += 1
+                else:
+                    L[i][0] += j
+                    arr[k] = L[i]
+                    i += 1
+                k += 1
+            
+            while i < len(L):
+                L[i][0] += j
+                arr[k] = L[i]
+                i += 1; k += 1
                 
-            else:
-                out.insert(idx, num)
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1; k += 1
+            
+        
+        def mergeSort(arr, low, high):
+            if low < high:
+                mid = (low + high) // 2
+                mergeSort(arr, low, mid)
+                mergeSort(arr, mid + 1, high)
+                merge(arr, low, mid, high)
                 
-        return out
+        arr = [[index[i], nums[i]] for i in range(len(nums))]
+        mergeSort(arr, 0, len(nums) - 1)
+        
+        for x in arr:
+            nums[x[0]] = x[1]
+        
+        return nums
