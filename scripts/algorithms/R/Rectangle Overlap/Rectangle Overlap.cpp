2fb1f9b07af80a391946451d8250cc22a91bbe54
AnasImloul <pydev27@gmail.com>
2023-04-29 18:41:54 +0100

    Runtime 0 ms (Top 100.0%) | Memory 7.0 MB (Top 74.6%)

--- scripts/algorithms/R/Rectangle Overlap/Rectangle Overlap.cpp
+++ scripts/algorithms/R/Rectangle Overlap/Rectangle Overlap.cpp
@@ -1,10 +1,26 @@
-// Runtime: 4 ms (Top 30.08%) | Memory: 7.8 MB (Top 65.04%)
-        class Solution {
-        public:
-            bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
-            {
-               if(rec2[1]>=rec1[3] || rec1[1]>=rec2[3] || rec1[0]>=rec2[2] || rec2[0]>=rec1[2])
-                   return false;
-                return true;
-            }
-        };
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        int ax1 = rec1[0];
+        int ay1 = rec1[1];
+        int ax2 = rec1[2];
+        int ay2 = rec1[3];
+ 
+        int bx1 = rec2[0];
+        int by1 = rec2[1];
+        int bx2 = rec2[2];
+        int by2 = rec2[3];
+
+        int x5 = max(ax1,bx1);
+        int y5 = max(ay1,by1);
+        int x6 = min(ax2,bx2);
+        int y6 = min(ay2,by2);
+        if(x5<x6 && y5<y6){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+};
