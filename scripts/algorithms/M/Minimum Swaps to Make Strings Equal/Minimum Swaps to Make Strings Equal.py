aa7e34734d598fcc15a11242e5ef42c73ccf2d46
AnasImloul <pydev27@gmail.com>
2023-04-29 16:14:59 +0100

    Runtime 48 ms (Top 8.44%) | Memory 16.0 MB (Top 15.58%)

--- scripts/algorithms/M/Minimum Swaps to Make Strings Equal/Minimum Swaps to Make Strings Equal.py
+++ scripts/algorithms/M/Minimum Swaps to Make Strings Equal/Minimum Swaps to Make Strings Equal.py
@@ -1,10 +1,15 @@
 class Solution:
-    def minimumSwap(self, s: str, t: str) -> int:
-        xy = yx = 0
-        for i in range(len(s)):
-            if s[i] != t[i]:
-                if s[i] == 'x': xy += 1
-                else: yx += 1
-        if (xy + yx) % 2: return -1
-
-        return xy//2 + yx//2 + 2*(xy%2)
+    def minimumSwap(self, s1: str, s2: str) -> int:
+        h = defaultdict(int)
+        count = 0    # variable to keep track of the number of mismatches; it is impossible to make strings equal if count is odd
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                count += 1
+                h[s1[i]] += 1
+        if count % 2 != 0:     
+            return -1
+        res, a, b = 0, h['x'], h['y']
+        res += a // 2 + b // 2
+        if a % 2 == 0:
+            return res
+        return res + 2
