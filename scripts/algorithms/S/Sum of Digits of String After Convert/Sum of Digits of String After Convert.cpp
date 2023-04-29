aab58fa96c98670d5d13028513c5fb3c66d5f112
AnasImloul <pydev27@gmail.com>
2023-04-29 18:39:18 +0100

    Runtime 3 ms (Top 59.36%) | Memory 6.0 MB (Top 29.35%)

--- scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.cpp
+++ scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        int n=s.length();
-        string ans="";
-        for(int i=0;i<n;i++){
-            ans+=to_string(int(s[i])-96);
+        
+        long long sum=0,d;
+        string convert;
+        
+        for(auto& ch: s)
+        {
+            convert+= to_string((ch-96));
         }
-        int temp=0;
-        while(k!=0){
-            for(int i=0;i<ans.length();i++){
-                temp+=(int(ans[i])-'0');
+
+        while(k--)
+        {
+            sum=0;
+            for(auto& ch: convert)
+            {
+               sum+= (ch-48);
             }
-            
-            ans=to_string(temp);
-            temp=0;
-            k--;
+            convert=to_string(sum);
         }
-        int fans=stoi(ans);
-        return fans;
+       
+        return sum;
     }
 };
