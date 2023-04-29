c06756c6d70b79e0e986a16a9fc2bbea169150d5
AnasImloul <pydev27@gmail.com>
2023-04-29 16:13:10 +0100

    Runtime 409 ms (Top 9.95%) | Memory 26.0 MB (Top 5.36%)

--- scripts/algorithms/R/Random Pick Index/Random Pick Index.py
+++ scripts/algorithms/R/Random Pick Index/Random Pick Index.py
@@ -1,9 +1,35 @@
 class Solution:
-    def __init__(self, nums: List[int]):
-        self.multimap = defaultdict(list)
-        for i, n in enumerate(nums):
-            self.multimap[n].append(i)
 
-    # O(1) time complexity
+    def __init__(self, nums: List[int]):
+        #self.nums = nums
+        #create a hash of values with their list of indices
+        self.map = defaultdict(list)
+        for i,v in enumerate(nums):
+            self.map[v].append(i)
+        
+
     def pick(self, target: int) -> int:
-        return random.choice(self.multimap[target])
+        return random.sample(self.map[target],1)[0]
+        '''
+        reservoir = 0
+        count = 0
+        for i in range(len(self.nums)):
+            if self.nums[i] == target:
+                count+=1
+                if random.random() < 1/count:
+                    reservoir = i
+        return reservoir
+
+        
+        samp = []
+        for i in range(len(self.nums)):
+            if self.nums[i] == target:
+                samp.append(i)
+        return (random.sample(samp,1))[0]
+        '''
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
