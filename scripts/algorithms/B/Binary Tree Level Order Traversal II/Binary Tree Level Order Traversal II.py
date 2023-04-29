d02c73f340bb6cdffbdc6312aa70ce2cac01c2fb
AnasImloul <pydev27@gmail.com>
2023-04-29 16:18:34 +0100

    Runtime 49 ms (Top 6.1%) | Memory 17.0 MB (Top 8.9%)

--- scripts/algorithms/B/Binary Tree Level Order Traversal II/Binary Tree Level Order Traversal II.py
+++ scripts/algorithms/B/Binary Tree Level Order Traversal II/Binary Tree Level Order Traversal II.py
@@ -1,15 +1,13 @@
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        level = [root] if root else []
-        res = []
-        while level:
-            nxt_level = []
-            res.append([])
-            level.reverse()
-            while level:
-                v = level.pop()
-                res[-1].append(v.val)
-                if v.left: nxt_level.append(v.left)
-                if v.right: nxt_level.append(v.right)
-            level = nxt_level
-        return res[::-1]
+        def dfs(node, level, result):
+            if not node:
+                return
+            if level >= len(result):
+                result.append([])
+            result[level].append(node.val)
+            dfs(node.left, level+1, result)
+            dfs(node.right, level+1, result)
+        result = []
+        dfs(root, 0, result)
+        return result[::-1]
