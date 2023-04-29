b26cb938cd3402b666403df97f9df697d6eaf9a8
AnasImloul <pydev27@gmail.com>
2023-04-29 18:38:01 +0100

    Runtime 12 ms (Top 52.72%) | Memory 9.0 MB (Top 41.4%)

--- scripts/algorithms/M/Matrix Block Sum/Matrix Block Sum.cpp
+++ scripts/algorithms/M/Matrix Block Sum/Matrix Block Sum.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n=mat.size(),m=mat[0].size();
-        
-        vector<vector<int>>grid(n,vector<int>(m));
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int minus=(i-1>=0 && j-1>=0)?grid[i-1][j-1]:0;
-                int add1=(i-1>=0)?grid[i-1][j]:0;
-                int add2=(j-1>=0)?grid[i][j-1]:0;
-                grid[i][j]=mat[i][j]+add1+add2-minus;
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> pref(n+1, vector<int>(m+1, 0));
+        // Calculating prefix sum
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                // note that while counting for [i-1][j] and [i][j-1];
+                // pref[i-1][j-1] will be added twice. So we reduce it once.
+                pref[i][j] = mat[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
             }
         }
-        
-        vector<vector<int>>ans(n,vector<int>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int maxRow=min(i+k,n-1),maxCol=min(j+k,m-1);
-                int base=grid[maxRow][maxCol];
-                base-=(i-k-1>=0)?grid[i-k-1][maxCol]:0;
-                base-=(j-k-1>=0)?grid[maxRow][j-k-1]:0;
-                base+=(i-k-1>=0 && j-k-1>=0)?grid[i-k-1][j-k-1]:0;
-                ans[i][j]=base;
+
+        // Find the sum of the elements specified in the K-block
+        vector<vector<int>> res(n, vector<int>(m, 0));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                // checking for all pairs to be in bounds.
+                int r1 = max(0, i-k);
+                int c1 = max(0, j-k);
+                int r2 = min(n-1, i+k);
+                int c2 = min(m-1, j+k);
+                // finding res[i][j] = bottom right - bottom left - top right + top left
+                res[i][j] = pref[r2+1][c2+1] - pref[r2+1][c1] - pref[r1][c2+1] + pref[r1][c1];
             }
         }
-        
-        return ans;
+
+        return res;
+
     }
 };
