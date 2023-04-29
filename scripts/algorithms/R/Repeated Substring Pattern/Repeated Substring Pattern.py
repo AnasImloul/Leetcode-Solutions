a8806a970649d9c675edd5e81b8671a3592d8e6e
AnasImloul <pydev27@gmail.com>
2023-04-29 16:12:18 +0100

    Runtime 143 ms (Top 27.61%) | Memory 16.0 MB (Top 5.18%)

--- scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.py
+++ scripts/algorithms/R/Repeated Substring Pattern/Repeated Substring Pattern.py
@@ -1,15 +1,6 @@
-# Runtime: 227 ms (Top 34.24%) | Memory: 14.1 MB (Top 15.73%)
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        i=0
-        ans=""
-        l=len(s)
-        while i <l:
-            ans+=s[i]
-            if l%(i+1)==0:
-                if ans*(l//(i+1)) == s:
-                    if i==l-1:
-                        return False
-                    return True
-            i+=1
+        for i in range(1, len(s)//2+1):
+            if s[:i] * (len(s)//i) == s:
+                return True
         return False
