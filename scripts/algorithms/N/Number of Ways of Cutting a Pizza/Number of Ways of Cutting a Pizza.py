469d211acc25326bb7d49280a5a537beae1ffcf3
AnasImloul <pydev27@gmail.com>
2023-04-29 16:24:55 +0100

    Runtime 181 ms (Top 95.39%) | Memory 18.0 MB (Top 19.44%)

--- scripts/algorithms/N/Number of Ways of Cutting a Pizza/Number of Ways of Cutting a Pizza.py
+++ scripts/algorithms/N/Number of Ways of Cutting a Pizza/Number of Ways of Cutting a Pizza.py
@@ -1,33 +1,50 @@
-# Runtime: 470 ms (Top 64.29%) | Memory: 16.1 MB (Top 38.57%)
 class Solution:
-    def ways(self, pizza: List[str], k: int) -> int:
-        m, n = len(pizza), len(pizza[0])
+    def ways(self, p: List[str], k: int) -> int:
+        m, n = len(p), len(p[0])
+        apples = [[0 for _ in range(n+1)] for _ in range(m+1)] 
+        # number of apples in (0,0) to (i,j)
+        apples[1][1] = 0 if p[0][0] == '.' else 1
+        for j in range(1,n):
+            apples[1][j+1] = apples[1][j] if p[0][j] == '.' else apples[1][j] + 1
+        for i in range(1,m):
+            cur_apple = 0 if p[i][0] == '.' else 1
+            apples[i+1][1] = apples[i][1] + cur_apple
+            for j in range(1,n):
+                if p[i][j] == 'A': cur_apple += 1
+                apples[i+1][j+1] = apples[i][j+1] + cur_apple
 
-        suffix = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m - 1, -1, -1):
-            for j in range(n):
-                suffix[i][j] = suffix[i + 1][j] + (pizza[i][j] == 'A')
-
-        for i in range(m):
-            for j in range(n - 1, -1, -1):
-                suffix[i][j] += suffix[i][j + 1]
-
-        MOD = 10 ** 9 + 7
+        def getApples(x,y,z,w):
+        # get the number of apples within range
+        # x,y: top-left coordinates of current pizza
+        # z,w: bottom-right
+            return apples[z+1][w+1] + apples[x][y] - apples[z+1][y] - apples[x][w+1]
 
         @lru_cache(None)
-        def helper(top, left, cuts):
-            if cuts == 0:
-                return 1 if suffix[top][left] else 0
+        def cut(c,x,y,z,w):
+        # c: number of cuts already cut
+        # x,y: top-left coordinates of current pizza
+        # z,w: bottom-right
+            if c == k - 1:
+                return 1 if getApples(x,y,z,w) > 0 else 0
 
-            res = 0
-            for i in range(top + 1, m):
-                if suffix[top][left] != suffix[i][left]:
-                    res = (res + helper(i, left, cuts - 1)) % MOD
+            r = 0
+            # horizontal cuts
+            i = x
+            while i < z:
+                if getApples(x,y,i,w) > 0: break
+                i += 1
+            while i < z:
+                r += cut(c+1,i+1,y,z,w)
+                i += 1
+            # vertical cuts
+            j = y
+            while j < w:
+                if getApples(x,y,z,j) > 0: break
+                j += 1
+            while j < w:
+                r += cut(c+1,x,j+1,z,w)
+                j += 1
 
-            for j in range(left + 1, n):
-                 if suffix[top][left] != suffix[top][j]:
-                    res = (res + helper(top, j, cuts - 1)) % MOD
+            return r
 
-            return res
-
-        return helper(0, 0, k - 1)
+        return cut(0,0,0,m-1,n-1)%(10**9+7)
