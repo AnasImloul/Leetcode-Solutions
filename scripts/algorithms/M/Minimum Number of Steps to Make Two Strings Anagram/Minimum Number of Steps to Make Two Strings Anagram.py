7fbc7c31ffaecdb9ecb4561c16fd331f3a59bef2
AnasImloul <pydev27@gmail.com>
2023-04-29 16:25:14 +0100

    Runtime 1507 ms (Top 5.11%) | Memory 17.0 MB (Top 5.32%)

--- scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.py
+++ scripts/algorithms/M/Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.py
@@ -1,37 +1,7 @@
-
-class Solution(object):
-    def minSteps(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
-        
-       
-        
-        
-        ans = 0
-        
-        shist = {}
-		thist = {}
-		
-        for i in range(len(s)):
-            shist[s[i]] = shist.get(s[i],0)+1
-			thist[t[i]] = thist.get(t[i],0)+1
-
-
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        for ch in s:
+		    # Find and replace only one occurence of this character in t
+            t = t.replace(ch, '', 1)
             
-            
-                    
-            
-        ans = 0
-        for ch in shist:
-            
-            if ch in thist:
-                if shist[ch] - thist[ch] >0:
-                    ans+= shist[ch] - thist[ch]
-            else:
-                ans+= shist[ch]
-                
-        return ans
-
+        return len(t)
