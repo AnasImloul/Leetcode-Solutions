0f353680203a3c80bb965922aaa88af72115375a
AnasImloul <pydev27@gmail.com>
2023-04-29 16:28:19 +0100

    Runtime 48 ms (Top 5.9%) | Memory 16.0 MB (Top 20.31%)

--- scripts/algorithms/N/Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.py
+++ scripts/algorithms/N/Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.py
@@ -1,3 +1,5 @@
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        return len([i for i in patterns if i in word])
+        return sum([pattern in word for pattern in patterns])
+
+        
