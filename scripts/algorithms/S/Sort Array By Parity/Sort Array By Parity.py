4a75a327ea78482268786377fa5e02acb5da14de
AnasImloul <pydev27@gmail.com>
2023-04-29 18:22:11 +0100

    Runtime 91 ms (Top 8.59%) | Memory 17.0 MB (Top 10.74%)

--- scripts/algorithms/S/Sort Array By Parity/Sort Array By Parity.py
+++ scripts/algorithms/S/Sort Array By Parity/Sort Array By Parity.py
@@ -1,12 +1,9 @@
-
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        beg=0
-        end = len(nums)-1
-        while beg <= end:
-            if nums[beg]%2 ==0:
-                beg +=1
-            else:
-                nums[beg], nums[end] = nums[end],nums[beg]
-                end -=1
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] % 2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+
         return nums
