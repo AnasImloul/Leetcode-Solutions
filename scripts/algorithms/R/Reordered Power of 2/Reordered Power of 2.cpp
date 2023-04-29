b9a2115b821a193c90ad3b3d4b1f712f56b95821
AnasImloul <pydev27@gmail.com>
2023-04-29 18:36:50 +0100

    Runtime 0 ms (Top 100.0%) | Memory 6.0 MB (Top 86.93%)

--- scripts/algorithms/R/Reordered Power of 2/Reordered Power of 2.cpp
+++ scripts/algorithms/R/Reordered Power of 2/Reordered Power of 2.cpp
@@ -1,54 +1,31 @@
 class Solution {
-    
 public:
-    
-    bool reorderedPowerOf2(int n) {
-        unordered_map<char,long long >m1;
-        int count =0;
-        string s= to_string(n);
-        for(auto x : s){
-            m1[x]++;
-            count++;
-        }
-        
-        vector<vector<string>>powerOftwo(11);
-        
-        long long temp = 1;
-        
-        while( temp <= 1e10 ){
-            string s = to_string(temp);
-            int t = (int)s.size();
-            // cout<<s<<endl;
-            powerOftwo[t].push_back(s);
-            temp *=2;
-            if(t==10){
-                break;
-            }
-        }
-        
-        for(int i = 0 ; i<powerOftwo[count].size();i++){
-            
-            string s = powerOftwo[count][i];
-            // cout<<s<<endl;
-            unordered_map<char,long long>temp = m1;
-            bool check = true;
-            for(auto x :s){
-                if(temp[x] == 0){
-                    check =false;
-                    break;
-                }
-                else{
-                    temp[x]--;
-                }
-                
-            }
-            if(check){
-                // cout<<s<<" :"<<endl;
-                return true;
-            }
-        }
-        
-        return false;
-    }
+    bool reorderedPowerOf2(int n) 
+{
+   string str = to_string(n);
+   sort(str.begin(),str.end());
+   long long int num;
+   int counter=0;
+   
+   while(true)
+   {
+      num = pow(2,counter++);
+      
+      string temp = to_string(num);
+      
+       
+      if(temp.length()>str.length())
+      {
+          //cout<<"temp="<<temp<<endl;
+          break;
+      }
+       
+      sort(temp.begin(),temp.end());
+         
+      if(str == temp)
+      return true;
+     
+   }     
+   return false;   
+}
 };
-
