5b40a89a3b6c5e25f4d441c9734839892aeddf80
AnasImloul <pydev27@gmail.com>
2023-04-29 17:24:41 +0100

    Runtime 1002 ms (Top 59.47%) | Memory 35.0 MB (Top 13.68%)

--- scripts/algorithms/F/Find Good Days to Rob the Bank/Find Good Days to Rob the Bank.py
+++ scripts/algorithms/F/Find Good Days to Rob the Bank/Find Good Days to Rob the Bank.py
@@ -1,12 +1,11 @@
 class Solution:
-    def goodDaysToRobBank(self, nums: List[int], time: int) -> List[int]:  
-        n=len(nums)
-        if time==0:return range(n)
-        res=[]
-        p1,p2=0,0
-        for i in range(1,n-time):
-            p1+=1 if nums[i-1]>=nums[i] else -p1
-            p2+=1 if nums[i+time-1]<=nums[i+time] else -p2
-            
-            if p1>=time and p2>=time:res.append(i)
-        return res
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        decreasing = [0] * len(security)
+        increasing = [0] * len(security)
+        for i in range(len(security)):
+            if i > 0 and security[i - 1] >= security[i]:
+                decreasing[i] = decreasing[i - 1] + 1
+        for i in reversed(range(len(security))):
+            if i < len(security) - 1 and security[i] <= security[i + 1]:
+                increasing[i] = increasing[i + 1] + 1
+        return [i for i in range(len(security)) if increasing[i] >= time and decreasing[i] >= time]
