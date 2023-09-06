35be015f0707a023846117bfef0b7ad77ad3b47a
anasimloul <pydev27@gmail.com>
2023-09-06 23:25:28 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 49.90 MB (Top 64.7%)

--- scripts/algorithms/S/Single Element in a Sorted Array/Single Element in a Sorted Array.java
+++ scripts/algorithms/S/Single Element in a Sorted Array/Single Element in a Sorted Array.java
@@ -1,27 +1,21 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 49.90 MB (Top 64.7%)
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int l = 0;
+        int h = nums.length-1;
         
-        int left = 0;
-        int right = nums.length-1;
-        
-        while(left < right){
+        while(l<h){
+            int mid = l+(h-l)/2;      // divide the array
             
-            int mid = (left+right)/2;
+            if(nums[mid]==nums[mid+1]) mid = mid-1;      //two same elements should be in same half
             
-            if(nums[mid] == nums[mid+1]){
-                mid = mid - 1;                   //two same elements should be in same half
-            }
+            if((mid-l+1)%2!=0) h = mid;            // checking the length of left half. If its is odd then update ur right pointer to mid
             
-            if((mid-left+1)%2 != 0){
-                right = mid;                     // checking the length of left half. 
-                                                //If its is odd then update ur right pointer to mid
-            }else{
-                left = mid + 1;                  // else your right half will be odd 
-                                                //then update your left pointer to mid+1
-
-            }
+            else l = mid+1;    // else your right half will be odd then update your left pointer to mid+1
         }
         
-        return nums[left];                  //left pointer will have the answer at last 
+        return nums[l];     //left pointer will have the answer at last
     }
 }
