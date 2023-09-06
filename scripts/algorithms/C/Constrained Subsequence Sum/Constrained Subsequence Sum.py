4c757a6d29752a5256edd9f41103de859742b9e2
anasimloul <pydev27@gmail.com>
2023-09-06 23:54:31 +0100

    Runtime: 1386 ms (Top 69.5%) | Memory: 30.16 MB (Top 58.7%)

--- scripts/algorithms/C/Constrained Subsequence Sum/Constrained Subsequence Sum.py
+++ scripts/algorithms/C/Constrained Subsequence Sum/Constrained Subsequence Sum.py
@@ -1,15 +1,22 @@
+# Runtime: 1386 ms (Top 69.5%) | Memory: 30.16 MB (Top 58.7%)
+
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        
-        res = nums[0]
-        hq = [(-nums[0], 0)] # (-dp[j],j) dp[j]: maximum sum until index j including nums[j]  
-        
-        for j, num in enumerate(nums[1:], start = 1):
-    
-            while hq and hq[0][1] < j-k: heapq.heappop(hq)
+        deque = []
+        for i, num in enumerate(nums):
                 
-            cur = max(-hq[0][0], 0) + num
-            heapq.heappush(hq,(-cur,j))
-            res = max(res,cur)
-
-        return res
+            while(deque and deque[0] < i - k): # delete that didn't end with a number in A[i-k:i]
+                deque.pop(0)
+                
+            if deque:  # compute the max sum we can get at index i
+                nums[i] = nums[deque[0]] + num
+            
+            while(deque and nums[deque[-1]] < nums[i]): 
+                # delet all the sequence that smaller than current sum, becaus there will never be
+                # considers ==> smaller than current sequence, and end before current sequence
+                deque.pop()
+                
+            if nums[i] > 0: # if nums[i] < 0, it can't be a useful prefix sum    
+            	deque.append(i)
+        
+        return max(nums)
