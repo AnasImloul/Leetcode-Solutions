e71018c406a45cb1c92e86922969c30815c88f82
anasimloul <pydev27@gmail.com>
2023-09-06 23:15:37 +0100

    Runtime: 5 ms (Top 99.0%) | Memory: 43.48 MB (Top 20.1%)

--- scripts/algorithms/K/Kth Distinct String in an Array/Kth Distinct String in an Array.java
+++ scripts/algorithms/K/Kth Distinct String in an Array/Kth Distinct String in an Array.java
@@ -1,3 +1,5 @@
+// Runtime: 5 ms (Top 99.0%) | Memory: 43.48 MB (Top 20.1%)
+
 class Solution {
     public String kthDistinct(String[] arr, int k) {
         Map<String,Integer> map=new HashMap<>();
