b11ba8ce950c0edf6babca14203a3e45db6d46f1
anasimloul <pydev27@gmail.com>
2023-09-06 23:17:21 +0100

    Runtime: 2 ms (Top 84.7%) | Memory: 40.80 MB (Top 15.0%)

--- scripts/algorithms/V/Valid Parentheses/Valid Parentheses.java
+++ scripts/algorithms/V/Valid Parentheses/Valid Parentheses.java
@@ -1,40 +1,22 @@
+// Runtime: 2 ms (Top 84.7%) | Memory: 40.80 MB (Top 15.0%)
+
 class Solution {
     public boolean isValid(String s) {
-        
-        Stack<Character> stack=new Stack<>();
-    
-        for( int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            
-            if(ch=='(' || ch == '{' || ch == '[' )
-            {
-                stack.push(ch);
-            }
-            else
-            {
-                if(stack.isEmpty())
-                {
-                    return false;
-                }
-                else
-                {
-                    
-                
-                if(ch==')' && stack.peek()=='(')
-                    stack.pop();
-                else if(ch==']' && stack.peek()=='[')
-                    stack.pop();
-                else if(ch=='}' && stack.peek()=='{')
-                    stack.pop();
-                else
-                    return false;
-                    
-                    
-                } 
-            }   
+        Stack<Character> stack = new Stack<Character>(); // create an empty stack
+        for (char c : s.toCharArray()) { // loop through each character in the string
+            if (c == '(') // if the character is an opening parenthesis
+                stack.push(')'); // push the corresponding closing parenthesis onto the stack
+            else if (c == '{') // if the character is an opening brace
+                stack.push('}'); // push the corresponding closing brace onto the stack
+            else if (c == '[') // if the character is an opening bracket
+                stack.push(']'); // push the corresponding closing bracket onto the stack
+            else if (stack.isEmpty() || stack.pop() != c) // if the character is a closing bracket
+                // if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
+                // does not match the closing bracket, the string is not valid, so return false
+                return false;
         }
-        
+        // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
         return stack.isEmpty();
     }
-}```
+}
