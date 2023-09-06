4ebe3512182da1574079ae3f3f78b85f8245f7ef
anasimloul <pydev27@gmail.com>
2023-09-07 00:04:35 +0100

    Runtime: 36 ms (Top 83.5%) | Memory: 16.25 MB (Top 70.5%)

--- scripts/algorithms/C/Check if One String Swap Can Make Strings Equal/Check if One String Swap Can Make Strings Equal.py
+++ scripts/algorithms/C/Check if One String Swap Can Make Strings Equal/Check if One String Swap Can Make Strings Equal.py
@@ -1,19 +1,6 @@
+# Runtime: 36 ms (Top 83.5%) | Memory: 16.25 MB (Top 70.5%)
+
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        bad = [] # list of bad indices
-        if s1 == s2 : return True
-        
-        for i in range(len(s1)):
-            if s1[i] != s2[i]:
-                bad.append(i)
-                
-        if  not (len(bad) == 2): return False 
-        
-        s1 = list(s1)
-        s1[bad[0]], s1[bad[1]] = s1[bad[1]], s1[bad[0]]
-        res = ""
-        
-        for char in s1:
-            res += char
-        
-        return (res == s2)
+        diff = [[x, y] for x, y in zip(s1, s2) if x != y]
+        return not diff or len(diff) == 2 and diff[0][::-1] == diff[1]
