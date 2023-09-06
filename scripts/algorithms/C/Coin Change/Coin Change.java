2c633c5cf9f14bde255e857247f62fca5b131443
anasimloul <pydev27@gmail.com>
2023-09-06 23:29:53 +0100

    Runtime: 19 ms (Top 47.6%) | Memory: 43.93 MB (Top 22.2%)

--- scripts/algorithms/C/Coin Change/Coin Change.java
+++ scripts/algorithms/C/Coin Change/Coin Change.java
@@ -1,1 +1,39 @@
-dfs(total)
+// Runtime: 19 ms (Top 47.6%) | Memory: 43.93 MB (Top 22.2%)
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int m=coins.length,n=amount;
+        int dp[][]=new int[m+1][n+1];
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+          for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+               int t1 = Integer.MAX_VALUE;
+        if ((i-1) == 0) {
+            if (j % coins[i-1] == 0) {
+                dp[i][j]= j / coins[i-1];
+            } else {
+                dp[i][j]= (int)1e9;
+            }
+        } 
+        else {
+            int t2 = dp[i-1][j];
+        if (coins[i-1] <= j) {
+            t1 = dp[i][j-coins[i-1]] + 1; 
+        }
+                dp[i][j]= Math.min(t1, t2);
+        }
+            }
+        }
+          if(dp[m][n]>=1e9)
+        return -1;
+        else
+        return dp[m][n];
+        }
+    }
+    
+  
