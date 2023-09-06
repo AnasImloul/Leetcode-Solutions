aae0e9750431bbb6db446dd3735b65e1fa38752b
anasimloul <pydev27@gmail.com>
2023-09-07 00:01:00 +0100

    Runtime: 308 ms (Top 92.4%) | Memory: 17.82 MB (Top 76.3%)

--- scripts/algorithms/K/Koko Eating Bananas/Koko Eating Bananas.py
+++ scripts/algorithms/K/Koko Eating Bananas/Koko Eating Bananas.py
@@ -1,19 +1,16 @@
-import math
-
+# Runtime: 308 ms (Top 92.4%) | Memory: 17.82 MB (Top 76.3%)
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        n = len(piles)
-        if n == h:
-            return max(piles)
-        piles.sort(reverse=True)
-        extra = h - n # each pile takes at least 1 hour
-        total_amount = 0
-        for i, pile in enumerate(piles):
-            if extra < i+1: 
-                return pile # 1 hour lowest resolution 
+        def check(x):
+            return sum(ceil(ele/x) for ele in piles) <= h
 
-            total_amount += pile
-            tmp_k = math.ceil(total_amount / (extra + i + 1))
-            if i+1 < n and tmp_k > piles[i+1] or i+1 == n:
-                return tmp_k # round up
+        l = 1
+        r = max(piles)
+        while l < r:
+            mid = (l+r) >> 1
+            if not check(mid):
+                l=mid+1
+            else:
+                r=mid
+        return l
