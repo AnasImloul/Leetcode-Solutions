7d8bebcbd9598c22efa12821882c2460e9ecdecd
anasimloul <pydev27@gmail.com>
2023-09-06 23:19:37 +0100

    Runtime: 1 ms (Top 85.5%) | Memory: 40.27 MB (Top 18.8%)

--- scripts/algorithms/N/Number of Good Pairs/Number of Good Pairs.java
+++ scripts/algorithms/N/Number of Good Pairs/Number of Good Pairs.java
@@ -1,13 +1,18 @@
+// Runtime: 1 ms (Top 85.5%) | Memory: 40.27 MB (Top 18.8%)
+
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for(int i=0; i< nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i] == nums [j] && i < j){
-                    count ++;
-                }
+        int len =nums.length;
+        int counter=0;
+        for (int i =0;i<len;i++){
+            for (int j=i+1;j<len;j++){
+                if(nums[i]==nums[j]){
+                    counter++; 
+                }           
             }
+            
         }
-        return count;
+        return counter;
+        
     }
 }
