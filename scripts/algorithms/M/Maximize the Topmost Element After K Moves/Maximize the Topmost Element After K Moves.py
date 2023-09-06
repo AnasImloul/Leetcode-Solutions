36f7f3cf35b8d845a7d5b72746e4860359d1f21c
anasimloul <pydev27@gmail.com>
2023-09-06 23:56:18 +0100

    Runtime: 498 ms (Top 63.4%) | Memory: 30.38 MB (Top 40.6%)

--- scripts/algorithms/M/Maximize the Topmost Element After K Moves/Maximize the Topmost Element After K Moves.py
+++ scripts/algorithms/M/Maximize the Topmost Element After K Moves/Maximize the Topmost Element After K Moves.py
@@ -1,16 +1,20 @@
+# Runtime: 498 ms (Top 63.4%) | Memory: 30.38 MB (Top 40.6%)
+
 class Solution:
     def maximumTop(self, nums: List[int], k: int) -> int:
-        if (len(nums) == 1):
-            if k % 2 == 0:
-                return nums[0]
-            else:
+        if len(nums) == 1:
+            if k%2 != 0:
                 return -1
-
-        sol = -1
-
-        for idx in range(min(k + 1, len(nums))):
-            if k == idx or k > (idx + 1):
-                sol = max(sol, nums[idx])
-
+            return nums[0]
         
-        return sol
+        if k == 0:
+            return nums[0]
+        if k == len(nums):
+            return max(nums[:-1])
+        if k > len(nums):
+            return max(nums)
+        if k == 1:
+            return nums[1]
+        m = max(nums[:k-1])
+        m = max(m, nums[k])
+        return m
