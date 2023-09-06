94e888293a0de29ba68c15456e79618f94af9067
anasimloul <pydev27@gmail.com>
2023-09-07 00:00:38 +0100

    Runtime: 44 ms (Top 29.7%) | Memory: 16.32 MB (Top 23.9%)

--- scripts/algorithms/T/Thousand Separator/Thousand Separator.py
+++ scripts/algorithms/T/Thousand Separator/Thousand Separator.py
@@ -1,16 +1,8 @@
+# Runtime: 44 ms (Top 29.7%) | Memory: 16.32 MB (Top 23.9%)
+
 class Solution:
-	def thousandSeparator(self, n: int) -> str:
-		nums = str(n)
-		count = 0
-		result = ""
-		i = len(nums)-1
-
-		while i >= 0:
-			result = nums[i] + result
-			count +=1
-
-			if count == 3 and i != 0:
-				count = 0
-				result = "." + result
-			i-=1
-		return result
+    def thousandSeparator(self, n: int) -> str:
+        s=str(n)
+        s=s[::-1]
+        res = '.'.join(s[i:i + 3] for i in range(0, len(s), 3))
+        return res[::-1]
