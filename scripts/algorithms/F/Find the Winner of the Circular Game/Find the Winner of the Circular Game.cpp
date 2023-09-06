0ef5f8bd9e30f0bc029032ad603c67bfaf6c4192
anasimloul <pydev27@gmail.com>
2023-09-06 22:42:18 +0100

    Runtime: 5 ms (Top 46.5%) | Memory: 6.40 MB (Top 33.1%)

--- scripts/algorithms/F/Find the Winner of the Circular Game/Find the Winner of the Circular Game.cpp
+++ scripts/algorithms/F/Find the Winner of the Circular Game/Find the Winner of the Circular Game.cpp
@@ -1,20 +1,19 @@
+// Runtime: 5 ms (Top 46.5%) | Memory: 6.40 MB (Top 33.1%)
+
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int> q;
-        for(int i=1;i<=n;i++){
-            q.push(i);
+        vector<int>temp;
+
+        for(int i=1;i<=n;i++) temp.push_back(i);
+
+        int i=0;
+
+        while(temp.size()>1){
+            int t=temp.size();
+            i=(i+k-1)%t;
+            temp.erase(temp.begin()+i);
         }
-        while(q.size()!=1){
-            int x=k;
-            while(x>1){
-                int r=q.front();
-                q.pop();
-                q.push(r);
-                x--;
-            }
-            q.pop();
-        }
-        return q.front();
+        return *temp.begin();
     }
 };
