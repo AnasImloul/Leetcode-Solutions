64eee2c79338e29c6601ae01e82a03de27eafe12
anasimloul <pydev27@gmail.com>
2023-09-06 23:57:01 +0100

    Runtime: 931 ms (Top 66.6%) | Memory: 45.16 MB (Top 22.2%)

--- scripts/algorithms/N/Number of Flowers in Full Bloom/Number of Flowers in Full Bloom.py
+++ scripts/algorithms/N/Number of Flowers in Full Bloom/Number of Flowers in Full Bloom.py
@@ -1,18 +1,27 @@
-# Runtime: 3214 ms (Top 11.47%) | Memory: 41 MB (Top 91.69%)
+# Runtime: 931 ms (Top 66.6%) | Memory: 45.16 MB (Top 22.2%)
+
 class Solution:
-    def fullBloomFlowers(self, flowers: List[List[int]], persons: List[int]) -> List[int]:
-        events = []
-        for s, e in flowers:
-            heapq.heappush(events, (s,1))
-            # Note here should be e+1 since the flowers still count at timepoint e.
-            heapq.heappush(events, (e+1,-1))
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        #we ADD flowers in the order in which they start, but we remove them in the order they 
+        #end.  For this reason, we sort the flowers by starting time but put them in a heap, 
+        #in which we remove them by ending time
+        starts = sorted(flowers) #keep flowers untouched in case array should be constant
+        blooming = [] #heap of active flowers
+        answer = [-1] * len(people) #output array
+        hours = [(people[i], i) for i in range(len(people))] #the people, ordered and indexed by their time
+        hours.sort()
 
-        flowers = 0
-        answer = {}
-        for t in sorted(persons):
-            while events and events[0][0] <= t:
-                _, f = heapq.heappop(events)
-                flowers += f
-            answer[t] = flowers
-
-        return [answer[i] for i in persons]
+        i = 0 #going through starts
+        for hour, person in hours:
+            #add all flowers that have started
+            while i < len(starts) and starts[i][0] <= hour:
+                heappush(blooming, (starts[i][1], starts[i][0]))
+                i += 1
+            #now remove all flowers that have ended.  Note that we only care about the absolute smallest, 
+            #and in python minheaps, that is always the first element - even if no other element's order 
+            #is guaranteed
+            while blooming and blooming[0][0] < hour: #as long as the soonest to end blooming flower hasn't already stopped
+                heappop(blooming)
+            answer[person] = len(blooming)
+        
+        return answer
