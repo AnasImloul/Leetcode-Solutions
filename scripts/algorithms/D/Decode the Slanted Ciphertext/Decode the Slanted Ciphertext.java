46f2451860fef2bbac86e7c06f7605da2bac4f20
anasimloul <pydev27@gmail.com>
2023-09-06 23:28:04 +0100

    Runtime: 57 ms (Top 15.7%) | Memory: 54.11 MB (Top 92.9%)

--- scripts/algorithms/D/Decode the Slanted Ciphertext/Decode the Slanted Ciphertext.java
+++ scripts/algorithms/D/Decode the Slanted Ciphertext/Decode the Slanted Ciphertext.java
@@ -1,10 +1,27 @@
+// Runtime: 57 ms (Top 15.7%) | Memory: 54.11 MB (Top 92.9%)
+
 class Solution {
-    public String decodeCiphertext(String encodedText, int rows) {
-    int sz = encodedText.length(), cols = sz / rows;
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < cols; ++i)
-        for (int j = i; j < sz; j += cols + 1)
-            sb.append(encodedText.charAt(j));
-    return sb.toString().stripTrailing();
+    public String decodeCiphertext(String str, int rows) {
+
+        //first find column size!!
+    	int cols=str.length()/rows;
+    	StringBuilder res=new StringBuilder(),new_res=new StringBuilder();;
+    	for(int i=0;i<cols;i++) {
+        
+            //iterating diagonally!!
+            for(int j=i;j<str.length();j+=cols+1)
+    			res.append(str.charAt(j));
+    	}
+        
+        //removing last spaces!!!
+        int fg=0;
+        for(int i=res.length()-1;i>=0;i--) {
+            
+            if(fg==0&&res.charAt(i)==' ')
+                continue;
+            fg=1;
+            new_res.append(res.charAt(i));
+        }
+        return new_res.reverse().toString();
+    }
 }
-}}
