8862d2a841e783a16128f63c63e1deba2f2e1468
anasimloul <pydev27@gmail.com>
2023-09-06 23:26:10 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 39.00 MB (Top 80.9%)

--- scripts/algorithms/C/Climbing Stairs/Climbing Stairs.java
+++ scripts/algorithms/C/Climbing Stairs/Climbing Stairs.java
@@ -1,18 +1,21 @@
-/**
- * Question   : Climbing Stairs
- * Complexity : Time: O(2^n) ; Space: O(n)
- */
+// Runtime: 0 ms (Top 100.0%) | Memory: 39.00 MB (Top 80.9%)
+
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 0;
+        int[] memo = new int[n + 1];
+        return calculateWays(n, memo);
+    }
+    
+    private int calculateWays(int n, int[] memo) {
+        if (n == 1 || n == 2) {
+            return n;
         }
-        if (n == 1) {
-            return 1;
+        
+        if (memo[n] != 0) {
+            return memo[n];
         }
-        if (n == 2) {
-            return 2;
-        }
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        
+        memo[n] = calculateWays(n - 1, memo) + calculateWays(n - 2, memo);
+        return memo[n];
     }
 }
