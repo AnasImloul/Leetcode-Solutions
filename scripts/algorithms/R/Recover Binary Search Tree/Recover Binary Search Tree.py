4da2b0eca108c597564a5a2cbab79ed8bb95c369
anasimloul <pydev27@gmail.com>
2023-09-06 23:55:24 +0100

    Runtime: 61 ms (Top 95.6%) | Memory: 16.68 MB (Top 75.1%)

--- scripts/algorithms/R/Recover Binary Search Tree/Recover Binary Search Tree.py
+++ scripts/algorithms/R/Recover Binary Search Tree/Recover Binary Search Tree.py
@@ -1,22 +1,53 @@
+# Runtime: 61 ms (Top 95.6%) | Memory: 16.68 MB (Top 75.1%)
+
 class Solution:
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
+    
+    """
+        Brute force kind of thing
+        -> Inorder Traversal returns sorted array
+        -> find a swap btwn numbers to make sorted
+        Make single swap to make array sorted
+        [1, 2, 3, 4, 10, 6, 9, 5, 10, 12]
+         x, x, x, x, x, No
+            prev number is mismatch -> 10 is cause
+        now go frm right to left
+        [1, 2, 3, 4, 10, 6, 9, 5, 11, 12]
+                            No x   x   x
+                        mismatch with next number -> 5 is the cause
+        swap 10, 5
+        
+        Eg: 2
+        [3, 2, 1]
+         x  No -> 3 is the cause
+        [3, 2, 1]
+         x  No -> 1 is the cause
+        swap values -> 1, 3
+    """
+    
+    def inorder(self, root, li):
+        if root is None:
+            return li
+        li = self.inorder(root.left, li)
+        li.append(root)
+        li = self.inorder(root.right, li)
+        return li
+    
+    def recoverTree(self, root: TreeNode) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        # strategy: 
-        #   - do in-order traversal (yielding node ptr)
-        #   - find two values out of order and fix
-        
-        # in-order traversal
-        def dfs(root):
-            if root.left is not None:
-                yield from dfs(root.left)
-            yield root
-            if root.right is not None:
-                yield from dfs(root.right)
-        
-        nodes = [node for node in dfs(root)]
-        sorted_vals = sorted([n.val for n in nodes])
-        
-        for n, val in zip(nodes, sorted_vals):
-            n.val = val
+        li = self.inorder(root, [])
+        n = len(li)
+        i, j = 1, n-2
+        a = li[0]
+        for i in range(1, n):
+            if li[i].val < li[i-1].val:
+                a = li[i-1]
+                break
+        b = li[-1]
+        for i in range(n-2, -1, -1):
+            if li[i].val > li[i+1].val:
+                b = li[i+1]
+                break
+
+        a.val,b.val = b.val, a.val
