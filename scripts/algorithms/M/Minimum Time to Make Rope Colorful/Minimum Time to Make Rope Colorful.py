85faff4ddbb68cf3ec97d4a85a6eaf89c353fe56
anasimloul <pydev27@gmail.com>
2023-09-07 00:03:37 +0100

    Runtime: 862 ms (Top 94.6%) | Memory: 27.26 MB (Top 71.4%)

--- scripts/algorithms/M/Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.py
+++ scripts/algorithms/M/Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.py
@@ -1,18 +1,11 @@
-class Solution:
-	def minCost(self, colors: str, neededTime: List[int]) -> int:
-		n=len(colors)
+# Runtime: 862 ms (Top 94.6%) | Memory: 27.26 MB (Top 71.4%)
 
-		cur_sum=0
-		i=0
-		while i<n-1:
-			if colors[i]==colors[i+1]:
-				max_val=float("-inf")
-				for j in range(i,n):
-					max_val=max(max_val,neededTime[j])
-					if j!=n-1 and colors[j]!=colors[j+1]:
-						break
-				cur_sum+=sum(neededTime[i:j+1])-max_val
-				i=j+1
-			else:
-				i+=1
-		return cur_sum
+class Solution:
+    def minCost(self, s: str, cost: List[int]) -> int:
+        ans = prev = 0 # index of previously retained letter 
+        for i in range(1, len(s)): 
+            if s[prev] != s[i]: prev = i
+            else: 
+                ans += min(cost[prev], cost[i])
+                if cost[prev] < cost[i]: prev = i
+        return ans 
