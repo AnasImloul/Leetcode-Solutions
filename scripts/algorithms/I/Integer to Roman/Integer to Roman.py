3d0e2c154aad79fc45ca0abe9ea35017a7ea795a
anasimloul <pydev27@gmail.com>
2023-09-07 00:08:56 +0100

    Runtime: 47 ms (Top 86.4%) | Memory: 16.42 MB (Top 14.9%)

--- scripts/algorithms/I/Integer to Roman/Integer to Roman.py
+++ scripts/algorithms/I/Integer to Roman/Integer to Roman.py
@@ -1,24 +1,25 @@
+# Runtime: 47 ms (Top 86.4%) | Memory: 16.42 MB (Top 14.9%)
+
 class Solution:
     def intToRoman(self, num: int) -> str:
-        alphabet = {
-            1000: 'M',
-            900: 'CM',
-            500: 'D',
-            400: 'CD',
-            100: 'C',
-            90: 'XC',
-            50: 'L',
-            40: 'XL',
-            10: 'X',
-            9: 'IX',
-            5: 'V',
-            4: 'IV',
-            1: 'I',
+        # Creating Dictionary for Lookup
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
         }
-        ans = ''
-        for x in alphabet:
-            if x <= num:
-                ans += (num // x) * alphabet[x]
-                num %= x
-        return ans
-                
+        
+        # Result Variable
+        r = ''
+        
+        
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            # If n in list then add the roman value to result variable
+            while n <= num:
+                r += num_map[n]
+                num-=n
+        return r
