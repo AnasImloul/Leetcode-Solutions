d27228ab157741d5c9a8111a9ae0f8e30507759c
anasimloul <pydev27@gmail.com>
2023-09-06 23:57:57 +0100

    Runtime: 2799 ms (Top 43.0%) | Memory: 16.80 MB (Top 93.0%)

--- scripts/algorithms/L/Longest Duplicate Substring/Longest Duplicate Substring.py
+++ scripts/algorithms/L/Longest Duplicate Substring/Longest Duplicate Substring.py
@@ -1,36 +1,18 @@
-# Runtime: 4205 ms (Top 48.47%) | Memory: 237.6 MB (Top 7.47%)
+# Runtime: 2799 ms (Top 43.0%) | Memory: 16.80 MB (Top 93.0%)
+
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
-        """
-        the length of substring will be between 0 and n
-        we do a binary search between lengths and check if we can find +2 occurances of a duplicate
-        we check using sliding window
-        """
-
-        def check_duplicate_count(window_len) -> str:
-
-            visited = set()
-            for i in range(0, len(s) - window_len):
-                sub = s[i : i + window_len + 1]
-                if sub in visited:
-                    return sub
-                visited.add(sub)
-
-            return None
-
-        l, r = 0, len(s) - 1
-        ln = float("-inf")
+        left = 0
+        right = 1
         res = ""
-        while l < r:
-            candid_len = l + (r - l) // 2
-            cand = check_duplicate_count(candid_len)
-            if cand and len(cand) > ln:
-                ln = len(cand)
-                res = cand
-            # try to make it larger if possible
-            if cand:
-                l = candid_len + 1
-            else:
-                r = candid_len
-
+        n = len(s)
+        while right<n:
+            if s[left:right] in s[left+1:]:
+                if right - left > len(res):
+                    res = s[left:right]
+                right+=1
+                continue
+            left+=1
+            if left == right:
+                right+=1
         return res
