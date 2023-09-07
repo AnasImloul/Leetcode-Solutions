ce12b6ed6e50c24bdf74781b2c745f5a4cad24ab
anasimloul <pydev27@gmail.com>
2023-09-07 16:23:00 +0100

    Runtime: 1436 ms (Top 61.0%) | Memory: 50.25 MB (Top 70.4%)

--- scripts/algorithms/M/Maximum Sum Obtained of Any Permutation/Maximum Sum Obtained of Any Permutation.py
+++ scripts/algorithms/M/Maximum Sum Obtained of Any Permutation/Maximum Sum Obtained of Any Permutation.py
@@ -1,11 +1,14 @@
-# Runtime: 6387 ms (Top 5.45%) | Memory: 65.4 MB (Top 5.45%)
-import numpy as np
+# Runtime: 1436 ms (Top 61.0%) | Memory: 50.25 MB (Top 70.4%)
+
 class Solution:
-    def maxSumRangeQuery(self, nums, requests) -> int:
-        idx_freq = np.zeros(len(nums), dtype=np.int64)
-        for start, end in requests:
-            idx_freq[start: end + 1] += 1
-        idx_freq.sort()
-        arr_nums = np.array(nums, dtype=np.int64)
-        arr_nums.sort()
-        return np.sum(idx_freq * arr_nums) % 1_000_000_007
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        count = [0] * len(nums)
+        for i, j in requests:
+            count[i] += 1
+            if j + 1 < len(count):
+                count[j+1] -= 1
+        cur = 0
+        for i in range(len(count)):
+            count[i] += cur
+            cur = count[i]
+        return sum(n * c for n, c in zip(sorted(nums, reverse=True), sorted(count, reverse=True))) % (10 ** 9 + 7)
