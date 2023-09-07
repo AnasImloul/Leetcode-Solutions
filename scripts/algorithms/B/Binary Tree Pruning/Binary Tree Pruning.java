bc85f8cf8ecee843c44640a9ee8cab4ae3a72623
anasimloul <pydev27@gmail.com>
2023-09-07 19:54:16 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 39.81 MB (Top 82.6%)

--- scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.java
+++ scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.java
@@ -1,15 +1,14 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 39.81 MB (Top 82.6%)
+
 class Solution {
-    
-   
     public TreeNode pruneTree(TreeNode root) {
-        if(root == null)
-            return null;
+        
+        if(root == null) return root;
+        
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
-        if(root.left == null && root.right == null && root.val == 0)
-        {
-            return null;
-        }
-        return root;
+    
+        if(root.left == null && root.right == null && root.val == 0) return null;
+        else return root;
     }
 }
