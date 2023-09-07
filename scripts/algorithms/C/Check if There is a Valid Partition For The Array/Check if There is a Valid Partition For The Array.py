4cc2535b89ac4e990285bef3915c555f8a6e8ab5
anasimloul <pydev27@gmail.com>
2023-09-07 17:44:34 +0100

    Runtime: 920 ms (Top 32.4%) | Memory: 30.14 MB (Top 95.0%)

--- scripts/algorithms/C/Check if There is a Valid Partition For The Array/Check if There is a Valid Partition For The Array.py
+++ scripts/algorithms/C/Check if There is a Valid Partition For The Array/Check if There is a Valid Partition For The Array.py
@@ -1,10 +1,22 @@
+# Runtime: 920 ms (Top 32.4%) | Memory: 30.14 MB (Top 95.0%)
+
 class Solution:
-    def validPartition(self, n: List[int]) -> bool:
-        @cache
-        def dfs(i: int) -> bool:
-            if i >= len(n) - 1:
-                return i == len(n)
-            two = n[i] == n[i + 1]
-            three = i < len(n) - 2 and ((two and n[i + 1] == n[i + 2]) or (n[i] + 1 == n[i + 1] and n[i] + 2 == n[i + 2]))
-            return (two and dfs(i + 2)) or (three and dfs(i + 3))
-        return dfs(0)
+    def validPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        dp = [False] * 3
+        dp[0] = True  # An empty partition is always valid
+
+        for i in range(2, n + 1):
+            ans = False
+
+            if nums[i - 1] == nums[i - 2]:
+                ans = ans or dp[(i - 2) % 3]
+            if i >= 3 and nums[i - 1] == nums[i - 2] == nums[i - 3]:
+                ans = ans or dp[(i - 3) % 3]
+            if i >= 3 and nums[i - 1] == nums[i - 2] + 1 == nums[i - 3] + 2:
+                ans = ans or dp[(i - 3) % 3]
+
+            dp[i % 3] = ans
+
+        return dp[n % 3]
