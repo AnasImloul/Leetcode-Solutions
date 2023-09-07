cf9d1f0d5476145ab7f99fef32d2e8c64c5ac980
anasimloul <pydev27@gmail.com>
2023-09-07 17:38:51 +0100

    Runtime: 640 ms (Top 64.4%) | Memory: 17.10 MB (Top 9.2%)

--- scripts/algorithms/C/Count Special Quadruplets/Count Special Quadruplets.py
+++ scripts/algorithms/C/Count Special Quadruplets/Count Special Quadruplets.py
@@ -1,16 +1,5 @@
+# Runtime: 640 ms (Top 64.4%) | Memory: 17.10 MB (Top 9.2%)
+
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        res = 0
-        l = len(nums)
-        
-        count = defaultdict(lambda: 0)
-        count[nums[l-1] - nums[l-2]] = 1
-        
-        for b in range(l - 3, 0, -1):
-            for a in range(b - 1, -1, -1):
-                res += count[nums[a] + nums[b]]
-            
-            for x in range(l - 1, b, -1):
-                count[nums[x] - nums[b]] += 1
-        
-        return res
+        return sum([1 for a, b, c, d in combinations(nums, 4) if a + b + c == d])
