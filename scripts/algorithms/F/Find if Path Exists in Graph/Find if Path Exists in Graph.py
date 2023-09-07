26d3cc146fdff8d8fe19f1eb74ab27575ac6f2b7
anasimloul <pydev27@gmail.com>
2023-09-07 17:38:23 +0100

    Runtime: 1647 ms (Top 68.5%) | Memory: 108.91 MB (Top 50.2%)

--- scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.py
+++ scripts/algorithms/F/Find if Path Exists in Graph/Find if Path Exists in Graph.py
@@ -1,30 +1,34 @@
-# Runtime: 4426 ms (Top 5.00%) | Memory: 313.2 MB (Top 8.29%)
+# Runtime: 1647 ms (Top 68.5%) | Memory: 108.91 MB (Top 50.2%)
 
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
-        graph = self.buildGraph(edges)
-        return self.hasPath(graph, start, end, set())
-
-    # function to convert list of edges to adjacency list graph
-    def buildGraph(self, edges):
-        graph = {}
-        for edge in edges:
-            a, b = edge
-            if a not in graph:
-                graph[a] = []
-            if b not in graph:
-                graph[b] = []
-            graph[a].append(b)
-            graph[b].append(a)
-        return graph
-
-    def hasPath(self, graph, src, dst, visited):
-        if src == dst:
-            return True
-        if src in visited:
-            return False
-        visited.add(src)
-        for neighbour in graph[src]:
-            if self.hasPath(graph, neighbour, dst, visited) == True:
+class Solution(object):
+    def validPath(self, n, edges, start, end):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        visited = [False]*n
+        d = {}
+		#store the undirected edges for both vertices
+        for i in edges:
+            if i[0] in d:
+                d[i[0]].append(i[1])
+            else:
+                d[i[0]] = [i[1]]
+                
+            if i[1] in d:
+                d[i[1]].append(i[0])
+            else:
+                d[i[1]] = [i[0]]
+        #create a queue as we will apply BFS
+        q = [start]
+        while q:
+            curr = q.pop(0)  #pop the first element as we do in queue
+            if curr == end:  #if its the end then we can return True
                 return True
-        return False
+            elif curr in d and not visited[curr]: #else if it is not the end then check whether its visited or not
+                q.extend(d[curr])  #add the adjacent vertices of the current node to the queue
+            visited[curr] = True  #mark this curr vertex as visited = True, so that we dont visit this vertex again
+        return False  #return False if the queue gets empty and we dont reach the end
