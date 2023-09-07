ab590c6d9efe582806efff4c406d6a49b78f6594
anasimloul <pydev27@gmail.com>
2023-09-07 19:59:43 +0100

    Runtime: 16 ms (Top 72.7%) | Memory: 45.86 MB (Top 86.3%)

--- scripts/algorithms/C/Count Ways to Make Array With Product/Count Ways to Make Array With Product.java
+++ scripts/algorithms/C/Count Ways to Make Array With Product/Count Ways to Make Array With Product.java
@@ -1,3 +1,5 @@
+// Runtime: 16 ms (Top 72.7%) | Memory: 45.86 MB (Top 86.3%)
+
 class Solution {
     // List of primes upto sqrt(10e4)
     int[] PRIMES = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
