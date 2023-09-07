c4fbdbd69330e0aa7cd2d0599620356e6a64150e
anasimloul <pydev27@gmail.com>
2023-09-07 19:57:02 +0100

    Runtime: 11 ms (Top 93.9%) | Memory: 44.11 MB (Top 57.7%)

--- scripts/algorithms/M/Maximum AND Sum of Array/Maximum AND Sum of Array.java
+++ scripts/algorithms/M/Maximum AND Sum of Array/Maximum AND Sum of Array.java
@@ -1,29 +1,41 @@
-class Solution {
+// Runtime: 11 ms (Top 93.9%) | Memory: 44.11 MB (Top 57.7%)
 
+class Solution {
+    int[] memo;
+    int[] nums;
+    int numSlots;
     public int maximumANDSum(int[] nums, int numSlots) {
-        int[] slotWt = new int[numSlots+1];
-        Map<Pair<Integer, String>, Integer> dp = new HashMap<>();
-        return helper(0, nums, slotWt, numSlots, dp);
+        this.memo=new int[1<<(2*numSlots)];
+        this.nums=nums;
+        this.numSlots=numSlots;
+        return helper(0,0);
     }
-    
-    public int helper(int idx, int[] nums, int[] slotWt, int numSlots, Map<Pair<Integer, String>, Integer> dp){
-        
-        if(idx==nums.length) return 0;
-        
-        Pair<Integer, String> key = new Pair<>(idx, Arrays.toString(slotWt));
-        if(dp.containsKey(key)) return dp.get(key);
-        
-        int maxi = Integer.MIN_VALUE;
-        for(int k=1; k<=numSlots; k++){
-            if(slotWt[k] < 2){
-                slotWt[k]++;
-                int ans = (nums[idx] & k) + helper(idx+1, nums, slotWt, numSlots, dp);
-                slotWt[k]--;
-                maxi = Math.max(maxi, ans);
-            }
+    int helper(int numIndex, int set) {
+        // Base case when we used all the numbers 
+        if(numIndex==nums.length) return 0;
+        // Set informs BOTH the slots used and the numIndex. If the later
+        // statement surprises you, think it like this: We must place all
+        // the numbers in a slot, so how many slots are used in numIndex=10?
+        // yes! 9 slots (because we will use the 10th now!), so the set will
+        // have 10 ones. No other numIndex will have 9 ones. So having memo
+        // with 2 dimentions would be redundant, as you would naver have a
+        // combination of numIndex 3 with sets 1, 2, 4, 6.. etc, only
+        // numIndex 2 will have those sets.
+        if(memo[set]>0) return memo[set]-1; // I use memo-1 so I dont have to fill it with -1
+        int max=0;
+        for(int i=0;i<numSlots;i++) {
+            int firstHalfSlot = (set&(1<<i))==0?i:-1; // -1 if it is used
+            int secondHalfSlot = (set&(1<<(i+numSlots)))==0?i+numSlots:-1; // -1 if it is used
+            int slotChosen = firstHalfSlot>-1?firstHalfSlot:secondHalfSlot;
+            if(slotChosen<0) continue; // both slots are used
+            int andSum=0;
+            if(slotChosen>=numSlots) andSum=((slotChosen-numSlots)+1)&nums[numIndex];
+            else andSum=(slotChosen+1)&nums[numIndex];
+            // By adjusting the set in the recursion signature
+            // I am backtracking in an elegant way.
+            max=Math.max(max, andSum+ helper(numIndex+1,set|1<<slotChosen));
         }
-        
-        dp.put(key, maxi);
-        return maxi;
+        memo[set]=max+1; // I use memo+1 so I dont have to fill it with -1
+        return max;
     }
 }
