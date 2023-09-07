b499ca1d7ef7ed20959da6b75477acced6cdc05d
anasimloul <pydev27@gmail.com>
2023-09-07 20:05:14 +0100

    Runtime: 1 ms (Top 98.8%) | Memory: 40.90 MB (Top 99.4%)

--- scripts/algorithms/P/Parsing A Boolean Expression/Parsing A Boolean Expression.java
+++ scripts/algorithms/P/Parsing A Boolean Expression/Parsing A Boolean Expression.java
@@ -1,51 +1,65 @@
+// Runtime: 1 ms (Top 98.8%) | Memory: 40.90 MB (Top 99.4%)
+
 class Solution {
-    public boolean parseBoolExpr(String expression) {
-        int n=expression.length();
-        
-        Stack<Character>operator=new Stack<>();
-        Stack<Character>operand=new Stack<>();
-        
-        for(int i=0;i<n;i++){
-            char c=expression.charAt(i);
-            if(c==','){
-                continue;
-            }
-            
-            if(c=='!' || c=='|' || c=='&'){
-                operator.push(c);
-            }else if(c=='(' || c=='f' || c=='t'){
-                operand.push(c);
-            }else{
-                char op=operator.pop();
-                char ans=operand.pop();
-                boolean flag=false;
-                while(!operand.isEmpty() && operand.peek()!='('){
-                    ans=evaluate(ans,operand.pop(),op);
-                    flag=true;
-                }
-                operand.pop();
-                if(!flag){
-                    if(op=='!'){
-                    operand.push(ans=='f'?'t':'f');    
-                    }else{
-                        operand.push(ans);
-                    }
-                    
-                }else{
-                operand.push(ans);    
-                }
-                
-            }
-        }
-        return operand.peek()=='t';
+    
+    int pos = 0;
+    
+    public boolean parseBoolExpr(String s) {
+        pos = 0;
+        return solve(s, '-');
     }
     
-    public char evaluate(char a, char b, char op){
-        if(op=='|'){
-            return (a=='t' || b=='t')?'t':'f';
-        }else if(op=='&'){
-            return (a=='t' && b=='t')?'t':'f';
+    public boolean solve(String s, char prev_sign) {
+       
+        boolean res = s.charAt(pos) == 'f' ? false : true;
+        char cur_sign = ' ';
+        int flag_res_init = 0;
+        while(pos < s.length()) {
+            
+            char cur_char = s.charAt(pos++);
+            
+            if(isExpr(cur_char)){
+                res = eval(cur_char == 't'?true:false, res , prev_sign);
+            }
+            else if(isSign(cur_char)){
+                cur_sign = cur_char;
+            }
+            else if(cur_char == '('){
+                if(flag_res_init == 1 || prev_sign == '!')
+                    res = eval(solve(s, cur_sign), res, prev_sign);
+                else {
+                    res = solve(s, cur_sign);
+                    flag_res_init = 1;
+                }
+            }
+            else if(cur_char == ')'){
+                 return res;
+            }
+            
         }
-        return 't';
+       return res;
+    }
+    
+    public boolean isExpr(char c){
+        return (c == 'f' || c == 't');
+    }
+    
+    public boolean isSign(char c){
+        return (c == '!' || c == '&' || c == '|');
+    }
+    
+    public boolean eval(boolean e1, boolean e2, char sign) {
+     
+        boolean res = false;
+        if(sign == '!')
+            res = !e1;
+        
+        else if(sign == '|')
+            res = e1 | e2;
+        
+        else if(sign == '&')
+            res = e1&e2;
+        
+        return res;
     }
 }
