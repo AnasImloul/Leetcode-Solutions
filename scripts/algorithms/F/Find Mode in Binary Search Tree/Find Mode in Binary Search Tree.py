d2ba50eddf7e73690fff6539444840c59e8899e6
anasimloul <pydev27@gmail.com>
2023-09-07 01:40:12 +0100

    Runtime: 30 ms (Top 89.5%) | Memory: 20.95 MB (Top 77.8%)

--- scripts/algorithms/F/Find Mode in Binary Search Tree/Find Mode in Binary Search Tree.py
+++ scripts/algorithms/F/Find Mode in Binary Search Tree/Find Mode in Binary Search Tree.py
@@ -1,24 +1,23 @@
-class Solution:
-    def __init__(self):
-        self.d = {}
-        self.m = 1
-        self.ans = []
-        
-    def dfs(self, x):
-        if not x:
-            return
-        if x.val not in self.d:
-            self.d[x.val] = 1    
-        else:
-            self.d[x.val] += 1
-            self.m = max(self.m, self.d[x.val])
-            
-        self.dfs(x.left)
-        self.dfs(x.right)
-    
-    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+# Runtime: 30 ms (Top 89.5%) | Memory: 20.95 MB (Top 77.8%)
+
+class Solution(object):
+    prev = None
+    max_count = 0
+    current_count = 0 
+    result = []
+
+    def findMode(self, root):
         self.dfs(root)
-        for x in self.d:
-            if self.d[x] == self.m:
-                self.ans.append(x)
-        return self.ans
+        return self.result
+
+    def dfs(self, node):
+        if not node: return
+        self.dfs(node.left)
+        self.current_count = 1 if node.val != self.prev else self.current_count + 1
+        if self.current_count == self.max_count:
+            self.result.append(node.val)
+        elif self.current_count > self.max_count:
+            self.result = [node.val]
+            self.max_count = self.current_count
+        self.prev = node.val
+        self.dfs(node.right)
