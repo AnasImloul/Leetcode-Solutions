85f68c652109a744d546be5e4b7a1e2aa2a98ad1
anasimloul <pydev27@gmail.com>
2023-09-07 19:53:42 +0100

    Runtime: 1 ms (Top 91.5%) | Memory: 38.98 MB (Top 97.6%)

--- scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.java
+++ scripts/algorithms/M/Monotone Increasing Digits/Monotone Increasing Digits.java
@@ -1,18 +1,30 @@
+// Runtime: 1 ms (Top 91.5%) | Memory: 38.98 MB (Top 97.6%)
+
 class Solution {
     public int monotoneIncreasingDigits(int n) {
-        char[] arr = String.valueOf(n).toCharArray();
-        int start = arr.length;
-        for (int i = arr.length - 2; i >= 0; i --) {
-            if (arr[i] > arr[i + 1]) {
-                start = i + 1;
-                arr[i] --;
+        int position;
+        int digitInTheNextPosition;
+        while ((position = getThePositionNotSatisfied(n)) != -1) {
+            digitInTheNextPosition = ((int) (n / Math.pow(10, position - 1))) % 10;
+            n -= Math.pow(10, position - 1) * (digitInTheNextPosition + 1);
+            n -= n % Math.pow(10, position);
+            n += Math.pow(10, position) - 1;
+        }
+        return n;
+    }
+
+    public int getThePositionNotSatisfied(int n) {
+        int k = 10;
+        int position = 0;
+        while (n > 0) {
+            if (k < n % 10) {
+                return position;
+            } else {
+                k = n % 10;
+                n /= 10;
+                position++;
             }
         }
-        
-        for (int i = start; i < arr.length; i ++) {
-            arr[i] = '9';
-        }
-        
-        return Integer.parseInt(String.valueOf(arr));
+        return -1;
     }
 }
