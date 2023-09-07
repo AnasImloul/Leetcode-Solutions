48143e849b9bb61b6739fd5a58d54fbc13934cdc
anasimloul <pydev27@gmail.com>
2023-09-07 16:20:04 +0100

    Runtime: 123 ms (Top 85.4%) | Memory: 16.81 MB (Top 58.4%)

--- scripts/algorithms/L/Longest String Chain/Longest String Chain.py
+++ scripts/algorithms/L/Longest String Chain/Longest String Chain.py
@@ -1,37 +1,20 @@
+# Runtime: 123 ms (Top 85.4%) | Memory: 16.81 MB (Top 58.4%)
+
 class Solution:
-	def find(self, word, refer):
-		# stack = [(word, level)]
-		stack = [(word, 1)]
-		# keep the maximum level(deepest) we have go through
-		max_level = 1
-		visited = set()
-		while stack:
-			char, level = stack.pop()
-			for i in range(len(char)+1):
-				# create all the possible transformation if we replace one character with *
-				# for example: ab -> a*, *b
-				possible = char[:i] + '*' + char[i:]
-				if possible in refer and possible not in visited:
-					visited.add(possible)
-					for w in refer[possible]:
-						max_level = max(level+1, max_level)
-						stack.append((w, level+1))
-
-		return max_level
-
-	def longestStrChain(self, words: List[str]) -> int:
-		map_dict = defaultdict(list)
-		res = 1
-		for w in words:
-			for i in range(len(w)):
-				# get all possible next char
-				# for example: a -> a*, *a; ab -> ab*, a*b, *ab
-				transform = w[:i] + '*' + w[i+1:]
-				map_dict[transform].append(w)
-				# map_dict: {'*a': ['a'], 'a*': ['a'] ...}
-
-		words = sorted(words, key=len)
-		for i, w in enumerate(words):
-			ans = self.find(w, map_dict)
-			res = max(res, ans)
-		return res
+    def longestStrChain(self, words: List[str]) -> int:
+        
+        words.sort(key=len)
+        dic = {}
+        
+        for i in words:
+            dic[ i ] = 1
+            
+            for j in range(len(i)):
+                
+                # creating words by deleting a letter
+                successor = i[:j] + i[j+1:]
+                if successor in dic:
+                    dic[ i ] = max (dic[i], 1 + dic[successor])
+        
+        res = max(dic.values())
+        return res
