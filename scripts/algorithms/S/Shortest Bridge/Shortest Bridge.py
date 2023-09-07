2968f2177d651f5166251bffc600dc2922a6fe2c
anasimloul <pydev27@gmail.com>
2023-09-07 17:40:13 +0100

    Runtime: 329 ms (Top 84.2%) | Memory: 17.67 MB (Top 84.2%)

--- scripts/algorithms/S/Shortest Bridge/Shortest Bridge.py
+++ scripts/algorithms/S/Shortest Bridge/Shortest Bridge.py
@@ -1,90 +1,32 @@
+# Runtime: 329 ms (Top 84.2%) | Memory: 17.67 MB (Top 84.2%)
+
 class Solution:
-    # O(I + W) Time | O(I + W) Space - Where "I" is the first island
-    # and "W" is the water to traverse.
-    def shortestBridge(self, grid: List[List[int]]) -> int:
-
-        start_row, start_col = self.__get_first_island(grid)
-
-        island_positions = self.__dfs(grid, start_row, start_col, [])
-
-        shortest_bridge = self.__bfs(grid, island_positions)
-
-        return shortest_bridge
-
-    # O(N) Time | O(1) Space
-    def __get_first_island(self, grid):
-        row_length = len(grid)
-        col_length = len(grid[0])
-
-        for row in range(row_length):
-            for col in range(col_length):
-                if grid[row][col] == 1:
-                    return row, col
-
-
-    # O(N) Time | O(N) Space
-    def __dfs(self, grid, row, col, neighbors):
-        if row < 0 or row >= len(grid):
-            return
-
-        if col < 0 or col >= len(grid[0]):
-            return
-
-        if grid[row][col] != 1:
-            return
-
-        grid[row][col] = 2
-        neighbors.append((row, col))
-
-        self.__dfs(grid, row + 1, col, neighbors)
-        self.__dfs(grid, row - 1, col, neighbors)
-        self.__dfs(grid, row, col + 1, neighbors)
-        self.__dfs(grid, row, col - 1, neighbors)
-
-        return neighbors
-
-    # O(N) Time | O(N) Space
-    def __bfs(self, grid, queue):
-        steps = 0
-
+    def shortestBridge(self, grid):
+        m, n = len(grid), len(grid[0])
+        start_i, start_j = next((i, j) for i in range(m) for j in range(n) if grid[i][j])
+        
+        
+        stack = [(start_i, start_j)]
+        visited = set(stack)
+        while stack:
+            i, j = stack.pop()
+            visited.add((i, j))  
+            for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j):
+                if 0 <= ii < m and 0 <= jj < n and grid[ii][jj] and (ii, jj) not in visited:
+                    stack.append((ii, jj))
+                    visited.add((ii, jj))
+        
+        
+        ans = 0
+        queue = list(visited)
         while queue:
-            level = []
-            for _ in range(len(queue)):
-                curr_row, curr_col = queue.pop(0)
-
-                neighbors = self.__get_neighbors(grid, curr_row, curr_col)
-                for n_row, n_col in neighbors:
-
-                    if grid[n_row][n_col] == 1:
-                        return steps
-
-                    elif grid[n_row][n_col] == 0:
-                        grid[n_row][n_col] = -1 # Mark as visited
-                        level.append((n_row, n_col))
-
-            queue = level
-            steps += 1
-
-        return steps
-
-    # O(1) Time | O(1) Space
-    def __get_neighbors(self, grid, row, col):
-        row_length = len(grid)
-        col_length = len(grid[0])
-
-        up = row - 1 >= 0
-        down = row + 1 < row_length
-        left = col - 1 >= 0
-        right = col + 1 < col_length
-
-        neighbors = []
-        if up:
-            neighbors.append((row - 1, col))
-        if down:
-            neighbors.append((row + 1, col))
-        if left:
-            neighbors.append((row, col - 1))
-        if right:
-            neighbors.append((row, col + 1))
-
-        return neighbors
+            new_queue = []
+            for i, j in queue:
+                for ii, jj in (i-1, j), (i, j-1), (i, j+1), (i+1, j):
+                    if 0 <= ii < m and 0 <= jj < n and (ii, jj) not in visited:
+                        if grid[ii][jj] == 1:
+                            return ans
+                        new_queue.append((ii, jj))
+                        visited.add((ii, jj))
+            queue = new_queue
+            ans += 1
