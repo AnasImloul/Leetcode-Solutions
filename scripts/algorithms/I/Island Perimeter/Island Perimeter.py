98d18edef2272729552f566c9b724816226de642
anasimloul <pydev27@gmail.com>
2023-09-07 17:35:00 +0100

    Runtime: 382 ms (Top 98.7%) | Memory: 16.57 MB (Top 93.9%)

--- scripts/algorithms/I/Island Perimeter/Island Perimeter.py
+++ scripts/algorithms/I/Island Perimeter/Island Perimeter.py
@@ -1,22 +1,16 @@
+# Runtime: 382 ms (Top 98.7%) | Memory: 16.57 MB (Top 93.9%)
+
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        
-        rows = len(grid)
-        cols=len(grid[0])
-        grid = [[0]*cols] + grid + [[0]*cols] #add rows of only zeros at top and bottom of grid  
-        for i in range(rows+2): 
-            grid[i] = [0]+grid[i]+[0] #add cols of only zeros at left and right of grid  
-        perimeter=0
-        #for each cell that is 1(land) check if its surrounded by 1s or 0s. Add up the number of edges with 0s.
-        for row in range(1,rows+1):
-            for col in range(1,cols+1):
-                if grid[row][col]==1:
-                    if grid[row-1][col]==0:
-                        perimeter+=1
-                    if grid[row+1][col]==0:
-                        perimeter+=1
-                    if grid[row][col-1]==0:
-                        perimeter+=1
-                    if grid[row][col+1]==0:
-                        perimeter+=1
+        perimeter = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i != 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    if j != 0 and grid[i][j-1] == 1:
+                        perimeter -= 2 
+            
         return perimeter
