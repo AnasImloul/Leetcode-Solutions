b2bd76fe2dcc090b819999669ec3e274b16f36fb
anasimloul <pydev27@gmail.com>
2023-09-07 01:10:36 +0100

    Runtime: 209 ms (Top 98.8%) | Memory: 16.64 MB (Top 51.7%)

--- scripts/algorithms/S/Subarray Sums Divisible by K/Subarray Sums Divisible by K.py
+++ scripts/algorithms/S/Subarray Sums Divisible by K/Subarray Sums Divisible by K.py
@@ -1,36 +1,21 @@
+# Runtime: 209 ms (Top 98.8%) | Memory: 16.64 MB (Top 51.7%)
+
 class Solution:
-    def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        #if two prefix sums produce equal remainder then we got a subarray
-        #if si%k==sj%k then (si-sj)%k=si%k-sj%k=0
-        #so we store remainders successively also check if this remainder already available if yes increase count of that remainder +1
-        #now be careful for -ve remainders as they may cause other result suppose 1 as remainder and -1 as remainder they are not equal according to you right but -1+k which is the positive remainder version of -1 may match a result so always make re``mainder+k if it is negative
-        #that's it
-        
-        prefsum=0
-        ans=0
-        d={0:1}
-        
+    def subarraysDivByK(self, nums, k):
+        n = len(nums)
+        prefix_mod = 0
+        result = 0
+
+        # There are k mod groups 0...k-1.
+        mod_groups = [0] * k
+        mod_groups[0] = 1
+
         for num in nums:
-            prefsum+=num
-            remainder=prefsum%k
-            
-            if remainder<0:
-                remainder=remainder+k #making positive version of negative remainder
-            
-            #check if remainder already exst we got a answer
-            if remainder in d:
-                ans+=d[remainder]
-            
-            #now time to store this remainder
-            if remainder not in d:
-                d[remainder]=1
-            else:
-                d[remainder]+=1
-            
-            #it took a lot of effort to understand solution from multiple sources
-            #lets run and check if it is accepted
-        
-        return ans
-    #yeah passed example test cases
-    #yoo passed
-	```
+            # Take modulo twice to avoid negative remainders.
+            prefix_mod = (prefix_mod + num % k + k) % k
+            # Add the count of subarrays that have the same remainder as the current
+            # one to cancel out the remainders.
+            result += mod_groups[prefix_mod]
+            mod_groups[prefix_mod] += 1
+
+        return result
