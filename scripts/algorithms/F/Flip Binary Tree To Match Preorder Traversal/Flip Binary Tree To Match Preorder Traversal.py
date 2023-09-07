ec4cbb415d4e7620e85eb4a4db5012adb93c17a5
anasimloul <pydev27@gmail.com>
2023-09-07 16:19:24 +0100

    Runtime: 50 ms (Top 21.2%) | Memory: 16.23 MB (Top 89.3%)

--- scripts/algorithms/F/Flip Binary Tree To Match Preorder Traversal/Flip Binary Tree To Match Preorder Traversal.py
+++ scripts/algorithms/F/Flip Binary Tree To Match Preorder Traversal/Flip Binary Tree To Match Preorder Traversal.py
@@ -1,59 +1,50 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Runtime: 50 ms (Top 21.2%) | Memory: 16.23 MB (Top 89.3%)
+
 class Solution:
-    def traverse(self,node,index,n):
-        res = []
-        if not node: 
-            index[0] -= 1
-            return True,[]
+    def flipMatchVoyage(self, root, voyage):
         
-        # when we reach the end of voyage but still node is present
-        if node and index[0] == n : return False,[]
+        # ------------------------------
         
-        #print('Here node = ',node.val, 'Index = ',index[0] , 'voyage val is',self.voyage[index[0]])
+        def dfs(root):
+            
+            if not root:
+                # base case aka stop condition
+				# empty node or empty tree
+                return True
+            
+            
+            ## general cases
+            if root.val != voyage[dfs.idx]:
+                
+                # current node mismatch, no chance to make correction by flip
+                return False
+            
+            # voyage index moves forward
+            dfs.idx += 1
+            
+            if root.left and (root.left.val != voyage[dfs.idx]):
+                
+                # left child mismatch, flip with right child if right child exists
+                root.right and result.append( root.val )
+                
+                # check subtree in preorder DFS with child node flip
+                return dfs(root.right) and dfs(root.left)
+                
+            else:
+                
+                # left child match, check subtree in preorder DFS
+                return dfs(root.left) and dfs(root.right)
+                
+      
+        # --------------------------
         
-        # if node.val is not same as voyage[index] in preorder traverse
-        if node.val != self.voyage[index[0]] : return False,[]
+        # flip sequence
+        result = []
         
-        if not node.left and not node.right : return True , []
+        # voyage index during dfs
+        dfs.idx = 0
         
-        # for traversing first check if node.left is voyage[index+1] or node.right or None
-        old = index[0]
+        # start checking from root node
+        good = dfs(root)
         
-        # try left and right
-        index[0] = index[0] + 1
-        a,l1 = self.traverse(node.left,index,n)
-        index[0] = index[0] + 1
-        b,l2 = self.traverse(node.right,index,n)
-        #print('node =',node.val,'  left1 =',a,' and right1 =',b)
-        if a and b :
-            res = []
-            for i in l1: res.append(i)
-            for i in l2: res.append(i)
-            return True , res
-        
-        #try right and left
-        index[0] = old
-        index[0] = index[0] + 1
-        a,l1 = self.traverse(node.right,index,n)
-        index[0] = index[0] + 1
-        b,l2 = self.traverse(node.left,index,n) 
-        #print('node =',node.val,'   left2 =',b,' and right2 =',a)
-        if a and b :
-            res = []
-            res.append(node.val)
-            for i in l1: res.append(i)
-            for i in l2: res.append(i)
-            return True , res
-        
-        return False,[]
-    
-    def flipMatchVoyage(self, root: Optional[TreeNode], voyage: List[int]) -> List[int]:
-        self.voyage = voyage
-        ans , lst = self.traverse(root,[0],len(voyage))
-        if ans == False : return [-1]
-        return lst
+        return result if good else [-1]
