0df78df8c37338bb1554b21167b72a4bf3fa6951
anasimloul <pydev27@gmail.com>
2023-09-07 01:04:01 +0100

    Runtime: 7 ms (Top 98.2%) | Memory: 13.31 MB (Top 45.5%)

--- scripts/algorithms/M/Merge Strings Alternately/Merge Strings Alternately.py
+++ scripts/algorithms/M/Merge Strings Alternately/Merge Strings Alternately.py
@@ -1,9 +1,19 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        
-        res=''
-        
-        for i in range(min(len(word1),len(word2))):
-            res += word1[i] + word2[i]
-            
-        return res + word1[i+1:] + word2[i+1:]
+# Runtime: 7 ms (Top 98.2%) | Memory: 13.31 MB (Top 45.5%)
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        i=0
+        j=0
+        st=[]
+        while i<len(word1) and j<len(word2):
+            st.append(word1[i])
+            st.append(word2[j])
+            i+=1
+            j+=1
+        while j<len(word2):
+            st.append(word2[j])
+            j+=1
+        while i<len(word1):
+            st.append(word1[i])
+            i+=1
+        return "".join(st)
