e3ca04fff89dd40cc089b3ad282f07f53486ec9c
anasimloul <pydev27@gmail.com>
2023-09-07 17:34:29 +0100

    Runtime: 137 ms (Top 70.4%) | Memory: 17.77 MB (Top 49.0%)

--- scripts/algorithms/D/Determine if Two Strings Are Close/Determine if Two Strings Are Close.py
+++ scripts/algorithms/D/Determine if Two Strings Are Close/Determine if Two Strings Are Close.py
@@ -1,16 +1,7 @@
+# Runtime: 137 ms (Top 70.4%) | Memory: 17.77 MB (Top 49.0%)
+
+from collections import Counter
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        lst1=[0]*26
-        lst2=[0]*26
-        for i in word1:
-            lst1[ord(i)-97]+=1
-        for i in word2:
-            lst2[ord(i)-97]+=1
-        for i in range(26):
-            if (lst1[i]>0 and lst2[i]==0) or (lst1[i]==0 and lst2[i]>0):
-                return False
-        lst1.sort()
-        lst2.sort()
-        if lst1[:]==lst2[:]:
-            return True
-        return False
+        return set(word1) == set(word2) and Counter(Counter(word1).values()) == Counter(Counter(word2).values())
