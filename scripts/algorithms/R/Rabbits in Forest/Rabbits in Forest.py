596fa6b4b5820d082304f526299c7320174df5f0
anasimloul <pydev27@gmail.com>
2023-09-07 16:14:09 +0100

    Runtime: 54 ms (Top 35.1%) | Memory: 16.38 MB (Top 76.8%)

--- scripts/algorithms/R/Rabbits in Forest/Rabbits in Forest.py
+++ scripts/algorithms/R/Rabbits in Forest/Rabbits in Forest.py
@@ -1,9 +1,25 @@
-# Runtime: 95 ms (Top 8.35%) | Memory: 14.1 MB (Top 29.67%)
+# Runtime: 54 ms (Top 35.1%) | Memory: 16.38 MB (Top 76.8%)
+
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        # store ans in ans_map
-        ans_map = collections.Counter(answers)
-        total = 0
-        for ans, count in ans_map.items():
-            total += (ans + 1) * ceil(count / (ans + 1))
-        return total
+        counts = {}
+        count = 0
+
+        for answer in answers:
+            if answer == 0:
+                # This must be the only rabbit of its color.
+                count += 1
+            elif answer not in counts or counts[answer] == 0:
+                # This is the first time the color appears.
+                counts[answer] = 1
+                # Add all rabbits having this new color.
+                count += answer + 1
+            else:
+                # Add one to how many times answer occurred.
+                counts[answer] += 1
+                if counts[answer] > answer:
+                    # If n+1 rabbits have said n,
+                    # this color group is complete.
+                    counts[answer] = 0
+        
+        return count
