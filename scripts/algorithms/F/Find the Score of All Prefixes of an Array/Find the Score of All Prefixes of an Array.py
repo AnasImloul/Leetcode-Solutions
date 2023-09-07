20d51b8829ea480f730632f5a1a517f941530ccd
anasimloul <pydev27@gmail.com>
2023-09-07 17:36:23 +0100

    Runtime: 434 ms (Top 63.6%) | Memory: 40.12 MB (Top 13.9%)

--- scripts/algorithms/F/Find the Score of All Prefixes of an Array/Find the Score of All Prefixes of an Array.py
+++ scripts/algorithms/F/Find the Score of All Prefixes of an Array/Find the Score of All Prefixes of an Array.py
@@ -1,3 +1,19 @@
+# Runtime: 434 ms (Top 63.6%) | Memory: 40.12 MB (Top 13.9%)
+
 class Solution:
     def findPrefixScore(self, nums: List[int]) -> List[int]:
-        return accumulate(map(add, nums, accumulate(nums, max)))
+        n = len(nums)
+        ans = [0] * n
+        max_val = float('-inf')
+        for i in range(n):
+            max_val = max(max_val, nums[i])
+            conver = nums[i] + max_val
+            ans[i] = conver
+        res=[]
+        s=0
+        for i in range(n):
+            s+=ans[i]            
+            res.append(s)
+            
+        return res
+        
