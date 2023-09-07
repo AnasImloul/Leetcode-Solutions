df4293eeb5198f53cf3d2ff68997c82054f728a7
anasimloul <pydev27@gmail.com>
2023-09-07 17:39:48 +0100

    Runtime: 637 ms (Top 91.1%) | Memory: 29.39 MB (Top 30.6%)

--- scripts/algorithms/N/Number of Subsequences That Satisfy the Given Sum Condition/Number of Subsequences That Satisfy the Given Sum Condition.py
+++ scripts/algorithms/N/Number of Subsequences That Satisfy the Given Sum Condition/Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -1,13 +1,17 @@
+# Runtime: 637 ms (Top 91.1%) | Memory: 29.39 MB (Top 30.6%)
+
 class Solution:
-    def numSubseq(self, nums, target):
+    def numSubseq(self, nums: List[int], target: int) -> int:
         nums.sort()
-        l, r = 0, len(nums) - 1
-        ans = 0
-        mod = int(1e9+7)
-        while l <= r:
-            if nums[l] + nums[r] > target:
-                r -= 1
+        left, right = 0, len(nums) - 1
+        count = 0
+        mod = 10 ** 9 + 7
+        
+        while left <= right:
+            if nums[left] + nums[right] > target:
+                right -= 1
             else:
-                ans += pow(2, r-l, mod)
-                l += 1
-        return ans % mod
+                count += pow(2, right - left, mod)
+                left += 1
+        
+        return count % mod
