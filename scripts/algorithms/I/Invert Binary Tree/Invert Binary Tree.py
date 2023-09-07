1105aaf15004e148fbdc875345d307bd2b175cc0
anasimloul <pydev27@gmail.com>
2023-09-07 01:56:17 +0100

    Runtime: 7 ms (Top 98.3%) | Memory: 13.26 MB (Top 67.3%)

--- scripts/algorithms/I/Invert Binary Tree/Invert Binary Tree.py
+++ scripts/algorithms/I/Invert Binary Tree/Invert Binary Tree.py
@@ -1,38 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
-        def dfs(root):
-            if not root:
-                return None
-            root.left,root.right = dfs(root.right), dfs(root.left)
-            return root
-        
-        return dfs(root)
-		
-		
-2nd Solution: 
+# Runtime: 7 ms (Top 98.3%) | Memory: 13.26 MB (Top 67.3%)
 
-class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
-        def helper(root):
-            if not root:
-                return None
-     
-            left = helper(root.left)
-            right =  helper(root.right)
-
-                
-            root.left = right
-            root.right = left
+class Solution(object):
+    def invertTree(self, root):
+        # Base case...
+        if root == None:
             return root
-        
-        helper(root)
-        
-        return root
+        # swapping process...
+        root.left, root.right = root.right, root.left
+        # Call the function recursively for the left subtree...
+        self.invertTree(root.left)
+        # Call the function recursively for the right subtree...
+        self.invertTree(root.right)
+        return root     # Return the root...
