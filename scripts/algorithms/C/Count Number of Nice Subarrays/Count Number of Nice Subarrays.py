d82d6e17b37a4254ecc354270417581c2fa06a50
anasimloul <pydev27@gmail.com>
2023-09-07 01:38:59 +0100

    Runtime: 628 ms (Top 69.2%) | Memory: 18.88 MB (Top 60.5%)

--- scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.py
+++ scripts/algorithms/C/Count Number of Nice Subarrays/Count Number of Nice Subarrays.py
@@ -1,22 +1,31 @@
-class Solution:
-    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        start = tempodd = count = 0
-        first = -1
-        for i, num in enumerate(nums):
-            # If odd, count the number of odds we got
-            # Also lets keep track of first odd we encounter
-            if num % 2:
-                tempodd += 1
-                if first == -1:
-                    first = i
-            # If we get k odds or more, we should count the subarrays
-            if tempodd >= k:
-                # We got one more than k counts, Just remove the first odd
-                if tempodd > k:
-                    first = start = first + 1
-                    while nums[first] % 2 == 0:
-                        first = first + 1
-                    tempodd -= 1
-                # If we get exact k odds, we should count the subarrays
-                count += first - start + 1
-        return count
+# Runtime: 628 ms (Top 69.2%) | Memory: 18.88 MB (Top 60.5%)
+
+class Solution(object):
+	def numberOfSubarrays(self, nums, k):
+		"""
+		e.g. k = 2
+		nums = [2, 2, 1, 2, 1, 2, 2]
+		index=  0  1  2  3  4  5  6
+		2 even numbers to left of first 1
+		2 even numbers to right of last 1
+		total number of subarrays = pick between 0-2 numbers on left, then, pick between 0-2 numbers on right
+		i.e (left+1)  (right+1)
+		Then slide window to next set of 2 odd numbers
+		"""
+
+		odds = []
+
+		for i in range(len(nums)):
+			if nums[i] & 1:
+				odds.append(i)                      #' Find index of all odd numbers '
+
+		odds = [-1] + odds + [len(nums)]            #' Handle edge cases '
+		nice = 0
+
+		for i in range(1, len(odds)-k):
+			left = odds[i] - odds[i-1] - 1          #' Number of 'left' even numbers '
+			right = odds[i+k] - odds[i+k-1] - 1     #' Number of 'right' even numbers '
+			nice += (left+1)*(right+1)              #' Total sub-arrays in current window '
+
+		return nice
+    
