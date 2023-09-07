64c8d4e82cea8cc55ffc9be5fe7f1ab016f138d0
anasimloul <pydev27@gmail.com>
2023-09-07 16:29:05 +0100

    Runtime: 41 ms (Top 45.3%) | Memory: 16.25 MB (Top 71.7%)

--- scripts/algorithms/T/The Number of Full Rounds You Have Played/The Number of Full Rounds You Have Played.py
+++ scripts/algorithms/T/The Number of Full Rounds You Have Played/The Number of Full Rounds You Have Played.py
@@ -1,29 +1,10 @@
+# Runtime: 41 ms (Top 45.3%) | Memory: 16.25 MB (Top 71.7%)
+
 class Solution:
     def numberOfRounds(self, startTime: str, finishTime: str) -> int:
-        sh, sm = startTime.split(":")
-        eh, em = finishTime.split(":")
-		
-        # sh - hour part of the start time
-		# sm - minute part of the start time
-		# eh - hour part of the finish time
-		# em = minute part of the finish time
-        sh, sm, eh, em = int(sh), int(sm), int(eh), int(em)
-        
-        if sh == eh:
-            if sm <= em:
-                return em//15 - (sm//15 + 1)
-            else:
-                val = 0
-                sm = 60 - sm
-                val += sm//15
-                val += em//15
-                return 23*4 + val 
-        else:
-            if eh < sh:
-                eh += 24
-            val = 0
-            sm = 60 - sm
-            val += sm//15
-            val += em//15
-            sh += 1
-            return (eh-sh)*4 + val
+        hs, ms = (int(x) for x in startTime.split(":"))
+        ts = 60 * hs + ms
+        hf, mf = (int(x) for x in finishTime.split(":"))
+        tf = 60 * hf + mf
+        if 0 <= tf - ts < 15: return 0 # edge case 
+        return tf//15 - (ts+14)//15 + (ts>tf)*96
