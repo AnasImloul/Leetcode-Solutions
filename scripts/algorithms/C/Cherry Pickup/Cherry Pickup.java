4209734ffc88b5640edf0fa1e3fe543a18496da5
anasimloul <pydev27@gmail.com>
2023-09-07 19:57:44 +0100

    Runtime: 12 ms (Top 87.2%) | Memory: 44.42 MB (Top 46.1%)

--- scripts/algorithms/C/Cherry Pickup/Cherry Pickup.java
+++ scripts/algorithms/C/Cherry Pickup/Cherry Pickup.java
@@ -1,36 +1,40 @@
+// Runtime: 12 ms (Top 87.2%) | Memory: 44.42 MB (Top 46.1%)
+
 class Solution {
-    int max = 0;
     public int cherryPickup(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if(m == 1 && n == 1)
-            return grid[0][0];
-        dfs(0, 0, m, n, grid, 0);
-        return max;
+        int m = grid.length, n = grid[0].length;
+        //For O(N^3) Dp sapce solution
+        dp2  = new Integer[m][n][m];
+        int ans=solve2(0,0,0,grid,0,m,n);
+        if(ans==Integer.MIN_VALUE) return 0;
+        return ans;
     }
-    public void dfs(int i, int j, int m, int n, int[][] grid, int ccsf){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == -1)
-            return;
-        if(i == m-1 && j == n-1){
-            helper(i, j, m, n, grid, ccsf);
+   
+    private Integer[][][] dp2;
+    private int solve2(int x1, int y1, int x2, int[][] g, int cpsf, int m, int n){
+        int y2 = x1+y1+-x2;
+        if(x1>=m||x2>=m||y1>=n||y2>=n||g[x1][y1]==-1||g[x2][y2]==-1) return Integer.MIN_VALUE;
+        if(x1==m-1&&y1==n-1) return g[x1][y1];
+        //If both p1 and p2 reach (m-1,n-1)
+        if(dp2[x1][y1][x2]!=null) return dp2[x1][y1][x2];
+        int cherries=0;
+        //If both p1 and p2 are at same position then we need to add the cherry only once.
+        if(x1==x2&&y1==y2){
+            cherries+=g[x1][y1];
         }
-        int cheeries = grid[i][j];
-        grid[i][j] = 0;
-        dfs(i+1, j, m, n, grid, ccsf+cheeries);
-        dfs(i, j+1, m, n, grid, ccsf+cheeries);
-        grid[i][j] = cheeries;
-    }
-    public void helper(int i, int j, int m, int n, int[][] grid, int ccsf){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == -1)
-            return;
-        if(i == 0 && j == 0){
-            max = Math.max(max, ccsf);
-            return;
+        //If p1 and p2 are at different positions then repective cherries can be added.
+        else{
+            cherries+=g[x1][y1]+g[x2][y2];
         }
-        int cheeries = grid[i][j];
-        grid[i][j] = 0;
-        helper(i-1, j, m, n, grid, ccsf+cheeries);
-        helper(i, j-1, m, n, grid, ccsf+cheeries);
-        grid[i][j] = cheeries;
+        //4 possibilites for p1 and p2 from each point
+        int dd=solve2(x1+1,y1,x2+1,g,cpsf+cherries,m,n); //both moves down
+        int dr=solve2(x1+1,y1,x2,g,cpsf+cherries,m,n);   //p1 moves down and p2 moves right
+        int rr=solve2(x1,y1+1,x2,g,cpsf+cherries,m,n);  //both moves right 
+        int rd=solve2(x1,y1+1,x2+1,g,cpsf+cherries,m,n); //p1 moves right and p2 moves down
+        
+        //We take maximum of 4 possiblities
+        int max=Math.max(Math.max(dd,dr), Math.max(rr,rd));
+        if(max==Integer.MIN_VALUE) return dp2[x1][y1][x2]=max;
+        return dp2[x1][y1][x2]=cherries+=max;
     }
 }
