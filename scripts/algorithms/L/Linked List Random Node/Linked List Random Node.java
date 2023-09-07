7cc611d7aac9428e1e0d222c174dc28554f8c839
anasimloul <pydev27@gmail.com>
2023-09-07 19:24:38 +0100

    Runtime: 12 ms (Top 57.7%) | Memory: 44.32 MB (Top 46.5%)

--- scripts/algorithms/L/Linked List Random Node/Linked List Random Node.java
+++ scripts/algorithms/L/Linked List Random Node/Linked List Random Node.java
@@ -1,16 +1,20 @@
+// Runtime: 12 ms (Top 57.7%) | Memory: 44.32 MB (Top 46.5%)
+
 class Solution {
-ArrayList<Integer> list=new ArrayList();
+    int N = 0;
+    ListNode head = null;
     public Solution(ListNode head) {
-        
-        while(head!=null)
-        {
-            list.add(head.val);
-            head=head.next;
-        }
+        this.head = head;
     }
     
     public int getRandom() {
-        int x=(int )(Math.random()*list.size());
-        return list.get(x);
+        ListNode p = this.head;
+        int i = 1, ans = 0;
+        while (p != null) {
+            if (Math.random() * i < 1) ans = p.val; // replace ans with i-th node.val with probability 1/i
+            p = p.next;
+            i ++;
+        }
+        return ans;
     }
 }
