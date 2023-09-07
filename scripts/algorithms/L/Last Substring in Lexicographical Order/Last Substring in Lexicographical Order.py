30251fcd417010ead5cabd2db706c39669a1cfca
anasimloul <pydev27@gmail.com>
2023-09-07 17:35:49 +0100

    Runtime: 273 ms (Top 82.1%) | Memory: 20.15 MB (Top 100.0%)

--- scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.py
+++ scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.py
@@ -1,16 +1,19 @@
-# Runtime: 993 ms (Top 30.00%) | Memory: 25.8 MB (Top 18.75%)
+# Runtime: 273 ms (Top 82.1%) | Memory: 20.15 MB (Top 100.0%)
+
 class Solution:
     def lastSubstring(self, s: str) -> str:
+        i = 0
+        j = 1
+        k = 0
         n = len(s)
-        cmax = max(s)
-        indexes = [ i for i,c in enumerate(s) if c == cmax ]
-        gap = 1
-        while len(indexes) > 1:
-            new_indexes = []
-            cmax = max(s[i+gap] for i in indexes if i+gap < n)
-            for i,st in enumerate(indexes):
-                if i > 0 and indexes[i-1] + gap == st: continue
-                if st + gap < n and s[st + gap] == cmax:new_indexes.append(st)
-            indexes = new_indexes
-            gap += 1
-        return s[indexes[0]:]
+        while j + k < n:
+            if s[i + k] == s[j + k]:
+                k += 1
+            elif s[i + k] > s[j + k]:
+                j += k + 1
+                k = 0
+            elif s[i + k] < s[j + k]:
+                i = max(i + k + 1, j)
+                j = i + 1
+                k = 0
+        return s[i:]
