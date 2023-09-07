ccfe76e28a77ec05ff0f081b921e412552cd8b7f
anasimloul <pydev27@gmail.com>
2023-09-07 01:41:11 +0100

    Runtime: 22 ms (Top 13.1%) | Memory: 13.44 MB (Top 40.5%)

--- scripts/algorithms/I/Increasing Order Search Tree/Increasing Order Search Tree.py
+++ scripts/algorithms/I/Increasing Order Search Tree/Increasing Order Search Tree.py
@@ -1,26 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def increasingBST(self, root: TreeNode) -> TreeNode:
-        def inorder(root,a):
-            if root:
-                inorder(root.left,a)
-                a.append(root.val)
-                inorder(root.right,a)
+# Runtime: 22 ms (Top 13.1%) | Memory: 13.44 MB (Top 40.5%)
+
+class Solution(object):
+    
+    def increasingBST(self, root):
         
-        a=[]
-        inorder(root,a)
-        p1=TreeNode()
-        tmp=p1
-        for i in range(len(a)):
-            if i==len(a)-1:
-                p1.val=a[i]
-            else:
-                p1.val=a[i]
-                p1.right=TreeNode()
-                p1=p1.right
-        return tmp
+        def sortBST(node):
+            if not node:    return []
+            
+            # return the in order BST nodes in list
+            return sortBST(node.left) + [node.val] + sortBST(node.right)
+            
+        # the in order sorted list of the tree nodes
+        sorted_list = sortBST(root)
+        
+        # generate new tree: temp for update, ans for return the root
+        ans = temp = TreeNode(sorted_list[0])
+        
+        # insert nodes to the right side of the new tree
+        for i in range(1, len(sorted_list)):
+            temp.right = TreeNode(sorted_list[i])
+            temp = temp.right
+            
+        return ans
