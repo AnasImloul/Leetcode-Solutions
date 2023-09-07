0f443b7e9350a1f41b0c0f1189b3dcfbe567a754
anasimloul <pydev27@gmail.com>
2023-09-07 17:46:33 +0100

    Runtime: 47 ms (Top 36.0%) | Memory: 16.40 MB (Top 44.2%)

--- scripts/algorithms/F/Find and Replace Pattern/Find and Replace Pattern.py
+++ scripts/algorithms/F/Find and Replace Pattern/Find and Replace Pattern.py
@@ -1,21 +1,27 @@
-# Runtime: 34 ms (Top 93.54%) | Memory: 13.8 MB (Top 81.83%)
+# Runtime: 47 ms (Top 36.0%) | Memory: 16.40 MB (Top 44.2%)
+
 class Solution:
-    def findAndReplacePattern(self, words: List[str], p: str) -> List[str]:
-        def find(w): # function thats calculate the numeric pattern
-            l = []
-            m = defaultdict(int)
-            i = 0
-            for c in w:
-                if c in m:
-                    l.append(m[c])
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        d={}
+        for i,v in enumerate(pattern):
+            if v in d:
+                d[v].append(i)
+            else:
+                d|={v:[i]}
+        #DICTIONARY CONTAINING LETTERS AND THEIR INDICES
+        ans=[]
+        for i in words:
+            e={}
+            for j,v in enumerate(i):
+                if v in e:
+                    e[v].append(j)
                 else:
-                    i+=1
-                    m[c]=i
-                    l.append(m[c])
-            return l
-        ans = []
-        pfind = find(p)
-        for w in words:
-            wfind = find(w)
-            if wfind == pfind: ans.append(w) #check if numeric pattern of pattern is equal to pattern of word
+                    e|={v:[j]}
+        #DICTIONARY CONTAINING LETTERS OF INDICES OF CURRENT WORD
+            for u,v in zip(d.values(),e.values()):
+                #COMPARING EACH VALUE
+                if u!=v:
+                    break
+            #IF SUCCESSFUL APPEND TO ANS
+            else:ans.append(i)
         return ans
