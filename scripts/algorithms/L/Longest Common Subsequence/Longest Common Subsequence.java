f6ce13f36f37429d05d64db52d10482756c7877b
anasimloul <pydev27@gmail.com>
2023-09-07 19:51:38 +0100

    Runtime: 21 ms (Top 78.9%) | Memory: 48.13 MB (Top 74.6%)

--- scripts/algorithms/L/Longest Common Subsequence/Longest Common Subsequence.java
+++ scripts/algorithms/L/Longest Common Subsequence/Longest Common Subsequence.java
@@ -1,24 +1,21 @@
-class Solution 
-{
-    public int longestCommonSubsequence(String text1, String text2)
-    {
-        // O(n * m) time | space
-        int n = text1.length();
-        int m = text2.length();
-        
-        // dp[i][j] represents that the length of longestCommonSubsequence at text1[i] && text[2]
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(text1.charAt(i) == text2.charAt(j))
-                    dp[i+1][j+1] = 1 + dp[i][j];
-                else
-                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+// Runtime: 21 ms (Top 78.9%) | Memory: 48.13 MB (Top 74.6%)
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
-        }      
-        return dp[n][m];
+        }
+
+        return dp[m][n];
     }
 }
