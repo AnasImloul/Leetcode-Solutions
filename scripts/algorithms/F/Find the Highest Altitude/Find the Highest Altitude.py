60557e1a4066e081cc944314ebebdff3da5f9934
anasimloul <pydev27@gmail.com>
2023-09-07 16:20:46 +0100

    Runtime: 50 ms (Top 25.7%) | Memory: 16.17 MB (Top 90.4%)

--- scripts/algorithms/F/Find the Highest Altitude/Find the Highest Altitude.py
+++ scripts/algorithms/F/Find the Highest Altitude/Find the Highest Altitude.py
@@ -1,10 +1,5 @@
+# Runtime: 50 ms (Top 25.7%) | Memory: 16.17 MB (Top 90.4%)
+
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        curr_alt=0
-        max_alt=0
-        for i in range(0,len(gain)):
-            curr_alt+=gain[i]
-            max_alt=max(max_alt,curr_alt)
-        return max_alt
-		
-//TC: O(n), SC: O(1)		
+        return max(accumulate([0]+gain))
