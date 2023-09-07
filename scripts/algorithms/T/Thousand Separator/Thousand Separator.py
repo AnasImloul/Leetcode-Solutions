416def807ba50e8a99c3baf26fd093e0290d1a03
anasimloul <pydev27@gmail.com>
2023-09-07 01:07:21 +0100

    Runtime: 17 ms (Top 24.0%) | Memory: 13.24 MB (Top 48.1%)

--- scripts/algorithms/T/Thousand Separator/Thousand Separator.py
+++ scripts/algorithms/T/Thousand Separator/Thousand Separator.py
@@ -1,8 +1,31 @@
-# Runtime: 44 ms (Top 29.7%) | Memory: 16.32 MB (Top 23.9%)
+# Runtime: 17 ms (Top 24.0%) | Memory: 13.24 MB (Top 48.1%)
 
-class Solution:
-    def thousandSeparator(self, n: int) -> str:
-        s=str(n)
-        s=s[::-1]
-        res = '.'.join(s[i:i + 3] for i in range(0, len(s), 3))
-        return res[::-1]
+class Solution(object):
+    def thousandSeparator(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        n = str(n)
+        if len(n) <= 3:
+            return str(n)
+        result = ""      
+        dot = '.'
+        index = 0
+        startPos = len(n) % 3 
+        if startPos == 0:
+            startPos += 3
+        val = -1
+        while index < len(n):
+            result += n[index]
+            if index == startPos - 1:
+                result += dot
+                val = 0
+            if val != -1:
+                val += 1
+                if val > 3 and (val - 1) % 3 == 0 and index != len(n) - 1:
+                    result += dot
+                    val = 1  
+            index += 1
+
+        return result
