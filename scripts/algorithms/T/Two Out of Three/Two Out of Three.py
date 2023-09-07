5355372f740b7e70c40bd7167c3231c7904e5c40
anasimloul <pydev27@gmail.com>
2023-09-07 16:27:18 +0100

    Runtime: 65 ms (Top 93.8%) | Memory: 16.30 MB (Top 83.7%)

--- scripts/algorithms/T/Two Out of Three/Two Out of Three.py
+++ scripts/algorithms/T/Two Out of Three/Two Out of Three.py
@@ -1,9 +1,14 @@
+# Runtime: 65 ms (Top 93.8%) | Memory: 16.30 MB (Top 83.7%)
+
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        ret = []
-
-        ret += set(nums1).intersection(set(nums2))
-        ret += set(nums1).intersection(set(nums3))
-        ret += set(nums2).intersection(set(nums3))
-
-        return set(ret)
+        output = []
+        for i in nums1:
+            if i in nums2 or i in nums3:
+                if i not in output:
+                    output.append(i)
+        for j in nums2:
+            if j in nums3 or j in nums1:
+                if j not in output:
+                    output.append(j)
+        return output
