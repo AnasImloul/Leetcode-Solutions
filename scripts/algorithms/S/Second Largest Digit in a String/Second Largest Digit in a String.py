08c72fd11454bebbca8775a92d2b7296b19015b0
anasimloul <pydev27@gmail.com>
2023-09-07 16:29:37 +0100

    Runtime: 80 ms (Top 5.3%) | Memory: 16.30 MB (Top 72.1%)

--- scripts/algorithms/S/Second Largest Digit in a String/Second Largest Digit in a String.py
+++ scripts/algorithms/S/Second Largest Digit in a String/Second Largest Digit in a String.py
@@ -1,11 +1,11 @@
+# Runtime: 80 ms (Top 5.3%) | Memory: 16.30 MB (Top 72.1%)
+
 class Solution:
     def secondHighest(self, s: str) -> int:
-        s=set(s)
-        a=[]
-        for i in s:
-            if i.isnumeric() :
-                a.append(int(i))
-        a.sort()
-        if len(a)<2:
-            return -1
-        return a[len(a)-2]
+        nums = []
+        for char in s:
+            if char.isdigit():
+                nums.append(int(char))
+        nums = [num for num in nums if num != max(nums)]
+        if len(nums) >= 1: return max(nums)
+        else: return -1
