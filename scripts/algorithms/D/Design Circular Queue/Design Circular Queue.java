8b2e6d59d28cb8fb3913c156d6eae99e85437853
anasimloul <pydev27@gmail.com>
2023-09-07 20:06:03 +0100

    Runtime: 5 ms (Top 38.7%) | Memory: 44.24 MB (Top 53.3%)

--- scripts/algorithms/D/Design Circular Queue/Design Circular Queue.java
+++ scripts/algorithms/D/Design Circular Queue/Design Circular Queue.java
@@ -1,81 +1,66 @@
+// Runtime: 5 ms (Top 38.7%) | Memory: 44.24 MB (Top 53.3%)
+
 class MyCircularQueue {
 
-    int head = -1;
-    int tail = -1;
-    int[] q;
-    int k;
+    private int front;
+    private int rear;
+    private int[] arr;
+    private int cap;
     
+    private int next(int i){ // to get next idx after i in circular queue
+        return (i+1)%cap;
+    }
+    private int prev(int i){ // to get prev idx before i in circular queue
+        return (i+cap-1)%cap;
+    }
+    
+	// rest is as simple as implmenting a normal queue using array.
     public MyCircularQueue(int k) {
-        q = new int[k];
-        this.k = k;
+        arr = new int[k];
+        cap=k;
+        front=-1;
+        rear=-1;
     }
     
     public boolean enQueue(int value) {
-        if(head == -1 && tail == -1) {
-            // enqueue when the queue is empty
-            head = tail = 0;
-            q[tail] = value;    // enQueue done
+        if(isFull())return false;
+        if(front==-1){
+            front=0;
+            rear=0;
+            arr[rear]=value;
             return true;
         }
-        if((tail == k-1 && head == 0) || tail+1 == head) {
-            // queue is full
-            return false;
-        }
-        tail++;
-        if(tail == k) // condition for circularity
-            tail = 0;
-        q[tail] = value;    // enQueue done
+        rear = next(rear);
+        arr[rear]=value;
         return true;
     }
     
     public boolean deQueue() {
-        if(head == -1) {
-            // check if q is already empty
-            return false;
-        } 
-        if(head == tail) {
-            // only 1 element is there and head,tail both points same index
-            head = tail = -1;   // deQueue done
+        if(isEmpty())return false;
+        if(front==rear){
+            front=-1;
+            rear=-1;
             return true;
         }
-        head++;       // deQueue done
-        if(head == k) // condition for circularity
-            head = 0;
+        front=next(front);
         return true;
     }
     
     public int Front() {
-        if(head == -1) 
-            return -1;
-        return q[head];
+        if(front==-1)return -1;
+        return arr[front];
     }
     
     public int Rear() {
-        if(tail == -1) 
-            return -1;
-        return q[tail];
+        if(rear==-1)return -1;
+        return arr[rear];
     }
     
     public boolean isEmpty() {
-        if(head == -1 && tail == -1) 
-            return true;
-        return false;
+        return front==-1;
     }
     
     public boolean isFull() {
-        if(tail == k-1 || tail+1 == head) 
-            return true;
-        return false;
+        return front!=-1 && next(rear)==front;
     }
 }
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue obj = new MyCircularQueue(k);
- * boolean param_1 = obj.enQueue(value);
- * boolean param_2 = obj.deQueue();
- * int param_3 = obj.Front();
- * int param_4 = obj.Rear();
- * boolean param_5 = obj.isEmpty();
- * boolean param_6 = obj.isFull();
- */
