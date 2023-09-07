f831eceb0ac10d7dae8a18556cea2bd198e36d7a
anasimloul <pydev27@gmail.com>
2023-09-07 17:45:04 +0100

    Runtime: 549 ms (Top 98.6%) | Memory: 23.43 MB (Top 9.7%)

--- scripts/algorithms/T/Triples with Bitwise AND Equal To Zero/Triples with Bitwise AND Equal To Zero.py
+++ scripts/algorithms/T/Triples with Bitwise AND Equal To Zero/Triples with Bitwise AND Equal To Zero.py
@@ -1,16 +1,17 @@
+# Runtime: 549 ms (Top 98.6%) | Memory: 23.43 MB (Top 9.7%)
+
 class Solution:
     def countTriplets(self, nums: List[int]) -> int:
-        ans = 0
-        mem = [0] * (1 << 16)
-        size = len(nums)
-
-        for i in range(size):
-            for j in range(i, size):
-                mem[nums[i]&nums[j]] += 2 if i != j else 1
+        freq = defaultdict(int)
+        for x in nums: 
+            for y in nums: 
+                freq[x&y] += 1
         
-        for ij in range(1 << 16):
-            if mem[ij] > 0:
-                for k in nums:
-                    if k & ij == 0:
-                        ans += mem[ij]
-        return ans
+        ans = 0
+        for x in nums: 
+            mask = x = x ^ 0xffff
+            while x: 
+                ans += freq[x]
+                x = mask & (x-1)
+            ans += freq[0]
+        return ans 
