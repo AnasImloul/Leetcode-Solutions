76d44138e2de70c6298355fe66b54499f6624c6e
anasimloul <pydev27@gmail.com>
2023-09-07 16:18:01 +0100

    Runtime: 133 ms (Top 63.3%) | Memory: 16.65 MB (Top 94.0%)

--- scripts/algorithms/C/Count Binary Substrings/Count Binary Substrings.py
+++ scripts/algorithms/C/Count Binary Substrings/Count Binary Substrings.py
@@ -1,18 +1,33 @@
-# Runtime: 609 ms (Top 8.10%) | Memory: 14.5 MB (Top 82.48%)
+# Runtime: 133 ms (Top 63.3%) | Memory: 16.65 MB (Top 94.0%)
+
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        n = len(s)
-        ctrs = []
-        i = 0
-        while i < n:
-            ctr = 1
-            while i < n - 1 and s[i] == s[i + 1]:
-                ctr += 1
-                i += 1
-            i += 1
-            ctrs.append(ctr)
-        res = 0
-        k = len(ctrs)
-        for i in range(k - 1):
-            res += min(ctrs[i], ctrs[i + 1])
-        return res
+        
+        # previous continuous occurrence, current continuous occurrence
+        pre_cont_occ, cur_cont_occ = 0, 1
+        
+        # counter for binary substrings with equal 0s and 1s
+        counter = 0
+        
+		# scan each character pair in s
+        for idx in range(1, len(s)):
+            
+            if s[idx] == s[idx-1]:
+                
+                # update current continuous occurrence
+                cur_cont_occ += 1
+            
+            else:
+                # update counter of binary substrings between prevous character group and current character group
+                counter += min(pre_cont_occ, cur_cont_occ)
+
+                # update previous as current's continuous occurrence
+                pre_cont_occ = cur_cont_occ
+                
+                # reset current continuous occurrence to 1
+                cur_cont_occ = 1
+        
+        # update for last time
+        counter += min(pre_cont_occ, cur_cont_occ)
+        
+        return counter
