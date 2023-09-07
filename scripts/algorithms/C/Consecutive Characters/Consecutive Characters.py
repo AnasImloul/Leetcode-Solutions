5bd7676b99560bbc79f650eb00c5f988b84e2eb9
anasimloul <pydev27@gmail.com>
2023-09-07 01:48:14 +0100

    Runtime: 30 ms (Top 32.1%) | Memory: 13.25 MB (Top 75.9%)

--- scripts/algorithms/C/Consecutive Characters/Consecutive Characters.py
+++ scripts/algorithms/C/Consecutive Characters/Consecutive Characters.py
@@ -1,10 +1,19 @@
-class Solution:
+# Runtime: 30 ms (Top 32.1%) | Memory: 13.25 MB (Top 75.9%)
+
+class Solution(object):
     def maxPower(self, s):
-        cnt = 0
-        m = 0
-        for i in range(1, len(s)):
-            if (s[i-1] == s[i]): 
-                cnt += 1
-                m = max(cnt, m)
-            else: cnt = 0
-        return m + 1
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack=[]
+        mxpow=0
+        for i in s:
+            if stack and stack[-1]!=i:
+                mxpow=max(mxpow,len(stack))
+                stack=[]
+                stack.append(i)
+            else:
+                stack.append(i)
+        mxpow=max(mxpow,len(stack))
+        return mxpow
