6b30366f454e9044ef553c69c2d81a5497b4889e
anasimloul <pydev27@gmail.com>
2023-09-07 17:47:24 +0100

    Runtime: 58 ms (Top 23.2%) | Memory: 16.30 MB (Top 28.6%)

--- scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.py
+++ scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.py
@@ -1,15 +1,13 @@
+# Runtime: 58 ms (Top 23.2%) | Memory: 16.30 MB (Top 28.6%)
+
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         nums.sort()
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            m = l + (r - l) // 2
-            x = len(nums) - m
-            if nums[m] >= x:
-                if m == 0 or nums[m - 1] < x:
-                    return x
-                else:
-                    r = m - 1
-            else:
-                l = m + 1
+        for i in range(max(nums)+1):
+            y=len(nums)-bisect.bisect_left(nums,i)
+            if y==i:
+                return i
         return -1
+
+        
+        
