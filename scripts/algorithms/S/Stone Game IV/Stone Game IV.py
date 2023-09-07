e414f3094001e4970a0c33705c62838133707b27
anasimloul <pydev27@gmail.com>
2023-09-07 17:37:56 +0100

    Runtime: 189 ms (Top 93.6%) | Memory: 21.09 MB (Top 49.0%)

--- scripts/algorithms/S/Stone Game IV/Stone Game IV.py
+++ scripts/algorithms/S/Stone Game IV/Stone Game IV.py
@@ -1,19 +1,12 @@
+# Runtime: 189 ms (Top 93.6%) | Memory: 21.09 MB (Top 49.0%)
+
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        dyp = [False] * (n + 1)
-        sqrs = []
-        currSqr = 1
-        for i in range(1, n + 1):
-            if i == currSqr * currSqr:
-                sqrs.append(i)
-                currSqr += 1
-                dyp[i] = True
-            else:
-                for sqr in sqrs:
-                    if not dyp[i - sqr]:
-                        dyp[i] = True
-                        break
-        return dyp[n]
-	```
-	
-	### Thank You!
+        squares = lambda x: (i * i for i in range(isqrt(x), 0, -1))
+        
+        @cache
+        def can_win(n: int) -> bool:
+            return n and not all(can_win(n - s) for s in squares(n))
+        
+        return can_win(n)
+
