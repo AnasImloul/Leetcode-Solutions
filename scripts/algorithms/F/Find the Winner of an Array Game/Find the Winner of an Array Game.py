13afd2f02c9421d30cfd6d0bedd7a1db540abba9
anasimloul <pydev27@gmail.com>
2023-09-07 17:41:40 +0100

    Runtime: 506 ms (Top 96.6%) | Memory: 30.16 MB (Top 47.7%)

--- scripts/algorithms/F/Find the Winner of an Array Game/Find the Winner of an Array Game.py
+++ scripts/algorithms/F/Find the Winner of an Array Game/Find the Winner of an Array Game.py
@@ -1,16 +1,14 @@
+# Runtime: 506 ms (Top 96.6%) | Memory: 30.16 MB (Top 47.7%)
+
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
-        if k == 1:
-            return max(arr[0], arr[1])
-        
-        i = wins = 0
-        for j in range(1, len(arr)):
-            if arr[i] > arr[j]:
-                wins += 1
-                if wins == k:
-                    return arr[i]
+        winner = arr[0]
+        wins = 0
+        for i in range(1,len(arr),1):
+            if(winner > arr[i]): wins+=1  # increment wins count   
             else:
-                i = j
-                wins = 1
-        
-        return max(arr)
+                wins = 1    # new winner
+                winner = arr[i]
+            if(wins == k): break    # if wins count is k, then return winner
+                
+        return winner
