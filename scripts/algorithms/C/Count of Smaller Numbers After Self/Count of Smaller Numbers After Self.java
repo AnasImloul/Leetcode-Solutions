7b99a0e814a79ebbb5b66838007fcf83b77cfa60
anasimloul <pydev27@gmail.com>
2023-09-07 19:50:39 +0100

    Runtime: 15 ms (Top 97.6%) | Memory: 57.69 MB (Top 69.7%)

--- scripts/algorithms/C/Count of Smaller Numbers After Self/Count of Smaller Numbers After Self.java
+++ scripts/algorithms/C/Count of Smaller Numbers After Self/Count of Smaller Numbers After Self.java
@@ -1,49 +1,33 @@
-class Solution {
-    public int[] tree = new int[400000];
-    
+// Runtime: 15 ms (Top 97.6%) | Memory: 57.69 MB (Top 69.7%)
+
+class Solution {    
     public List<Integer> countSmaller(int[] nums) {
-        int n = nums.length;
-        List<Integer> list =  new ArrayList<>();
-        for(int i = 0 ; i < n ; i++)
-            nums[i] = nums[i]+10000;
-        
-        for(int i = n-1 ; i >= 0 ; i--)
-        {
-            updateTree(0 , 0 , 20000 , nums[i]);
-            list.add(query(0 , 0 , 20000 , 0 , nums[i]-1));
+        int min = 20001;
+        int max = -1;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
         
-        Collections.reverse(list);
-        return list;
-    }
-    
-    public void updateTree(int ind , int low , int high , int val){
-        if(low == high){
-            tree[ind]++;
-            return;
+        min--;
+        int[] count = new int[max-min+1];
+        Integer[] result = new Integer[nums.length];
+        for (int i = nums.length-1; i >=0; i--) {
+            int k = nums[i]-min-1;
+            int c = 0;
+            do {
+                c += count[k];
+                k -= (-k&k);
+            } while (k > 0);
+            result[i] = c;
+            
+            k = nums[i]-min;
+            while (k < count.length) {
+                count[k]++;
+                k += (-k&k);
+            }
         }
         
-        int mid = (low+high)/2;
-        if(mid >= val)
-            updateTree(2*ind+1 , low , mid , val);
-        else
-            updateTree(2*ind+2 , mid+1 , high  , val);
-        
-        tree[ind] = tree[2*ind+1] + tree[2*ind+2];
-    }
-    
-    public int query(int ind , int low , int high , int left , int right){
-        if(low >= left && high <= right){
-            return tree[ind];
-        }
-        
-        if(high < left || low > right)
-            return 0;
-        
-        int mid = (low+high)/2;
-        int leftFind = query(2*ind+1 , low , mid , left , right);
-        int rightFind = query(2*ind+2 , mid+1 , high , left , right);
-        
-        return leftFind+rightFind;
+        return Arrays.asList(result);
     }
 }
