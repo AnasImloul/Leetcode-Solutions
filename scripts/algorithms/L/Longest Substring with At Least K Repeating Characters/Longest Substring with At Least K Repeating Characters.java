248cb4edef1c5c3bc1f65e038893c8be868bbdbe
anasimloul <pydev27@gmail.com>
2023-09-08 10:15:59 +0100

    Runtime: 1034 ms (Top 5.0%) | Memory: 40.34 MB (Top 79.2%)

--- scripts/algorithms/L/Longest Substring with At Least K Repeating Characters/Longest Substring with At Least K Repeating Characters.java
+++ scripts/algorithms/L/Longest Substring with At Least K Repeating Characters/Longest Substring with At Least K Repeating Characters.java
@@ -1,17 +1,44 @@
-class Solution {
-	public int longestSubstring(String s, int k) {
-		int n=s.length();
-		if(n==0 || k>n)return 0;
-		if(k<=1) return n;
-		HashMap<Character,Integer> map=new HashMap<>();
-		for(char x:s.toCharArray())
-			map.put(x,map.getOrDefault(x,0)+1);
-		int low=0;
-		while(low<n && map.get(s.charAt(low))>=k)low++;
-		if(low>=n-1)return low;
-		int left=longestSubstring(s.substring(0,low),k);
-		while(low<n && map.get(s.charAt(low))<k)low++;
-		 int right=(low < n) ? longestSubstring(s.substring(low), k) : 0;
-		return Math.max(left,right);
+// Runtime: 1034 ms (Top 5.0%) | Memory: 40.34 MB (Top 79.2%)
 
+// Solution 1: O(n^2) brute force
+// time O(n^2)
+// space O(1)
+class Solution {
+    public int longestSubstring(String s, int k) {
+        if (s == null || s.isEmpty() || k > s.length()) {
+            return 0;
+        }
+        
+        int[] map = new int[26]; // letter -> freq
+        int n = s.length();
+        int max = 0; // length of longest substring T so far
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(map, 0);
+            for (int j = i; j < n; j++) {
+                map[s.charAt(j) - 'a']++;                
+                if (isValid(s, i, j, k, map)) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        
+        return max;
+    }
+    
+    // return true if each distinct character in the substring s[left..right] appear >= k times
+    private boolean isValid(String s, int left, int right, int k, int[] map) {
+        int numLetters = 0; // number of distinct letters
+        int numLettersAtLeastK = 0;
+        for (int num : map) {
+            if (num > 0) {
+                numLetters++;
+            }
+            
+            if (num >= k) {
+                numLettersAtLeastK++;
+            }
+        }
+        
+        return numLettersAtLeastK == numLetters;
+    }
 }
