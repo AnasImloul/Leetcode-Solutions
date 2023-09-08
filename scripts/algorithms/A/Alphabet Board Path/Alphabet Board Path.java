fc7636b687f77ea8bc99f1813949feb31d825407
anasimloul <pydev27@gmail.com>
2023-09-08 19:29:26 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 40.07 MB (Top 98.6%)

--- scripts/algorithms/A/Alphabet Board Path/Alphabet Board Path.java
+++ scripts/algorithms/A/Alphabet Board Path/Alphabet Board Path.java
@@ -1,51 +1,19 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 40.07 MB (Top 98.6%)
+
 class Solution {
     public String alphabetBoardPath(String target) {
-        String ans = "";
-        String[] board = {"abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"};
-        int currR = 0 , currC=0;
-        for(int i=0;i<target.length();i++){
-            int positionR = 0 , positionC = 0;
-            char temp = target.charAt(i);
-            boolean b = false;
-            if(target.charAt(i)=='z' && board[currR].charAt(currC)!='z'){
-                temp = 'u';
-                b = true;
-            }
-            for(int j=0;j<board.length;j++){
-                for(int k=0;k<board[j].length();k++){
-                    if(board[j].charAt(k)==temp){
-                        positionR = j;
-                        positionC = k;
-                        break;
-                    }
-                }
-            }
-            if(board[currR].charAt(currC)=='z' && target.charAt(i)!='z'){
-                currR--;
-                ans+="U";
-            }
-            if(true){
-                if(positionR>currR){
-                    for(int j=1;j<=positionR-currR;j++)ans+="D";
-                }
-                else{
-                    for(int j=1;j<=currR-positionR;j++)ans+="U";
-                }
-            }
-            if(true){
-                if(positionC>currC){
-                    for(int j=1;j<=positionC-currC;j++)ans+="R";
-                }
-                else{
-                    for(int j=1;j<=currC-positionC;j++)ans+="L";
-                }
-            }
-            if(target.charAt(i)=='z' && board[currR].charAt(currC)!='z')ans+="D";
-            ans+="!";
-            if(b) positionR++;
-            currR = positionR;
-            currC = positionC;
+        int x = 0, y = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < target.length(); i++){
+            char ch = target.charAt(i);
+            int x1 = (ch - 'a') / 5;
+            int y1 = (ch - 'a') % 5;
+            while(x1 < x)   {x--; sb.append('U');}
+            while(y1 > y)   {y++; sb.append('R');}
+            while(y1 < y)   {y--; sb.append('L');}
+            while(x1 > x)   {x++; sb.append('D');}
+            sb.append('!');
         }
-        return ans;
+        return sb.toString();
     }
 }
