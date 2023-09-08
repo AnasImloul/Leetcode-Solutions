29bc17f9c415b17d5c9de7ae9eb9c73d06c532eb
anasimloul <pydev27@gmail.com>
2023-09-08 19:56:54 +0100

    Runtime: 115 ms (Top 52.9%) | Memory: 45.97 MB (Top 98.2%)

--- scripts/algorithms/R/Roman to Integer/Roman to Integer.js
+++ scripts/algorithms/R/Roman to Integer/Roman to Integer.js
@@ -1,31 +1,29 @@
+// Runtime: 115 ms (Top 52.9%) | Memory: 45.97 MB (Top 98.2%)
+
 var romanToInt = function(s) {
-    let v = [];
-    let romans = [{a: 'I', b: 1}, {a: 'V', b: 5}, {a: 'X', b: 10}, 
-                  {a: 'L', b: 50}, {a: 'C', b: 100}, {a: 'D', b: 500}, {a: 'M', b: 1000}];
-    for(let i=0; i<s.length; i++){
-        for(let j=0; j<romans.length; j++){
-            if(s[i] == romans[j].a){
-                v.push(romans[j].b);
-            }
+      const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const cur = sym[s[i]];
+        const next = sym[s[i + 1]];
+
+        if (cur < next) {
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
         }
     }
-    let n = 0;
-    for(let i=0; i<v.length; i++){
-        if(i+1 < v.length){
-            if(v[i+1] <= v[i]){
-                n += v[i];
-            }
-            else if(v[i+1] > v[i]){
-                n = n + v[i+1] - v[i]
-                i += 1;
-                if(i >= v.length){
-                    return n;
-                }
-            }
-        }
-        else{
-            n += v[i]; 
-        }
-    }
-    return n;
+
+    return result;
 };
