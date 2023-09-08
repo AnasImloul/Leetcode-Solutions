a1a63c45e774c716d21a60dd62d25412f3479dd4
anasimloul <pydev27@gmail.com>
2023-09-08 10:23:49 +0100

    Runtime: 1 ms (Top 99.9%) | Memory: 44.19 MB (Top 7.0%)

--- scripts/algorithms/S/Subsets II/Subsets II.java
+++ scripts/algorithms/S/Subsets II/Subsets II.java
@@ -1,33 +1,43 @@
+// Runtime: 1 ms (Top 99.9%) | Memory: 44.19 MB (Top 7.0%)
+
 class Solution {
-    
-    Set<List<Integer>> result = new HashSet<>();
-    int nums[];
-    
-    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // Sort the input array to handle duplicates properly
         Arrays.sort(nums);
-        
-        this.nums = nums;
-        traverse(new ArrayList(), 0);
-        
-        List<List<Integer>> list = new ArrayList();
-        for(List<Integer> i : result)
-            list.add(i);
-        return list;
-        
-        
+        // Start the recursion with an empty prefix list
+        return subset(new ArrayList<Integer>(), nums);
     }
     
-    void traverse(List<Integer> list, int index){
-        if(index >= nums.length){
-            result.add(new ArrayList(list));
-            return;
+    // Recursive function to generate subsets
+    public List<List<Integer>> subset(ArrayList<Integer> prefix, int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        // Base case: If there are no elements in nums, add the current prefix to result
+        if (nums.length == 0) {
+            result.add(new ArrayList<>(prefix));
+            return result;
         }
-        list.add(nums[index]);
-        traverse(list, index+1);
-        list.remove(Integer.valueOf(nums[index]));
-        traverse(list, index+1);
         
+        // Include the first element of nums in the prefix
+        ArrayList<Integer> withCurrent = new ArrayList<>(prefix);
+        withCurrent.add(nums[0]);
+        
+        // Recursive call with the first element included
+        List<List<Integer>> left = subset(withCurrent, Arrays.copyOfRange(nums, 1, nums.length));
+        
+        List<List<Integer>> right = new ArrayList<>();
+        
+        // Check for duplicates in the prefix and decide whether to include the first element again
+        if (prefix.size() > 0 && prefix.get(prefix.size() - 1) == nums[0]) {
+            // If the current element is a duplicate, don't include it in the prefix
+            // This avoids generating duplicate subsets
+        } else {
+            // If the current element is not a duplicate, include it in the prefix
+            right = subset(prefix, Arrays.copyOfRange(nums, 1, nums.length));
+        }
+        
+        // Combine the subsets with and without the current element
+        left.addAll(right);
+        return left;
     }
-    
 }
