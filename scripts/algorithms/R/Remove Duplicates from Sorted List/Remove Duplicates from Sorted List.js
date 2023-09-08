dac41601e89c0ff8ad9d04315160e5423b7cfde8
anasimloul <pydev27@gmail.com>
2023-09-08 20:07:20 +0100

    Runtime: 61 ms (Top 64.8%) | Memory: 44.06 MB (Top 61.3%)

--- scripts/algorithms/R/Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.js
+++ scripts/algorithms/R/Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.js
@@ -1,29 +1,24 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var deleteDuplicates = function(head) {
-    let prev = head;
+// Runtime: 61 ms (Top 64.8%) | Memory: 44.06 MB (Top 61.3%)
 
-    while(prev && prev.next){
-        let curr = prev;
-        
-        while(curr.next){
-            if(prev.val === curr.next.val){
-                curr.next = curr.next.next
-            }
-            else{
-                curr = curr.next
-            }
+var deleteDuplicates = function(head) {
+    // Special case...
+    if(head == null || head.next == null)
+        return head;
+    // Initialize a pointer curr with the address of head node...
+    let curr = head;
+    // Traverse all element through a while loop if curr node and the next node of curr node are present...
+    while( curr != null && curr.next != null){
+        // If the value of curr is equal to the value of prev...
+        // It means the value is present in the linked list...
+        if(curr.val == curr.next.val){
+            // Hence we do not need to include curr again in the linked list...
+            // So we increment the value of curr...
+            curr.next = curr.next.next;
         }
-        prev = prev.next
+        // Otherwise, we increment the curr pointer...
+        else{
+            curr = curr.next; 
+        }
     }
-    return head
+    return head;        // Return the sorted linked list without any duplicate element...
 };
