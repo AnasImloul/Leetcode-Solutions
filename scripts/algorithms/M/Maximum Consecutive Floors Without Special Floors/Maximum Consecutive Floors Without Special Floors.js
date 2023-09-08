f4bf7fbfbe420dc8ad479d637564816339bc1b8e
anasimloul <pydev27@gmail.com>
2023-09-08 19:54:59 +0100

    Runtime: 181 ms (Top 84.2%) | Memory: 56.68 MB (Top 15.7%)

--- scripts/algorithms/M/Maximum Consecutive Floors Without Special Floors/Maximum Consecutive Floors Without Special Floors.js
+++ scripts/algorithms/M/Maximum Consecutive Floors Without Special Floors/Maximum Consecutive Floors Without Special Floors.js
@@ -1,18 +1,18 @@
+// Runtime: 181 ms (Top 84.2%) | Memory: 56.68 MB (Top 15.7%)
+
+/**
+ * @param {number} bottom
+ * @param {number} top
+ * @param {number[]} special
+ * @return {number}
+ */
 var maxConsecutive = function(bottom, top, special) {
-    special.push(top + 1, bottom - 1);
+    special.push(top+1);
+    special.push(bottom-1);
     special.sort((a, b) => a - b);
-    
-    const n = special.length;
-    
-    let max = 0;
-    
-    for (let i = 1; i < n; ++i) {
-        const curr = special[i];
-        const prev = special[i - 1];
-        const between = curr - prev - 1;
-        
-        max = Math.max(max, between);
+    let specialMax = 0;
+    for (let i = 1; i < special.length; i++){
+        specialMax = Math.max(specialMax, special[i] - special[i-1] - 1)
     }
-    
-    return max;
+    return specialMax;
 };
