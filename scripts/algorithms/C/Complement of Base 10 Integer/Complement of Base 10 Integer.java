4097e121a2eb69fc9cb47e9c5c9215257ba1d54a
anasimloul <pydev27@gmail.com>
2023-09-08 11:38:55 +0100

    Runtime: 1 ms (Top 20.2%) | Memory: 39.34 MB (Top 43.8%)

--- scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.java
+++ scripts/algorithms/C/Complement of Base 10 Integer/Complement of Base 10 Integer.java
@@ -1,17 +1,16 @@
+// Runtime: 1 ms (Top 20.2%) | Memory: 39.34 MB (Top 43.8%)
+
 class Solution {
     public int bitwiseComplement(int n) {
-        
-        String conversion = Integer.toBinaryString(n);
-        String temp = "";
-        for(int i = 0; i < conversion.length(); i++){
-            if(conversion.charAt(i) == '0'){
-                temp += "1";
-            }
-            else{
-                temp += "0";
-            }
+        String bin = Integer.toBinaryString(n);
+        String res = "";
+        for(char c :bin.toCharArray())
+        {
+            if( c == '1')
+                res += "0";
+            else
+                res += "1";
         }
-        int complement = Integer.parseInt(temp, 2);
-        return complement;
+        return Integer.parseInt(res, 2);
     }
 }
