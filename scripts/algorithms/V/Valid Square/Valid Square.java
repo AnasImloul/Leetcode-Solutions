8fea6116160df68b65ca8317dfca57da245f2029
anasimloul <pydev27@gmail.com>
2023-09-08 11:41:36 +0100

    Runtime: 1 ms (Top 81.1%) | Memory: 40.42 MB (Top 51.8%)

--- scripts/algorithms/V/Valid Square/Valid Square.java
+++ scripts/algorithms/V/Valid Square/Valid Square.java
@@ -1,23 +1,24 @@
+// Runtime: 1 ms (Top 81.1%) | Memory: 40.42 MB (Top 51.8%)
+
 class Solution {
+    // This method returns true if the given 4 points form a square, false otherwise
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-
-        Set<Integer> distances = new HashSet<>();
-        distances.add(distance(p1, p2));
-        distances.add(distance(p1, p3));
-        distances.add(distance(p1, p4));
-
-        distances.add(distance(p2, p3));
-        distances.add(distance(p2, p4));
-
-        distances.add(distance(p3, p4));
-
-		// there should be only 2 unique distances (between neighbour points and apposite points)
-        return !distances.contains(0) && distances.size() == 2;
+        // We use a set to store the distances between the points
+        Set<Integer> set = new HashSet();
+        // Calculate the distances between all pairs of points and add them to the set
+        set.add(distanceSquare(p1,p2));
+        set.add(distanceSquare(p1,p3));
+        set.add(distanceSquare(p1,p4));
+        set.add(distanceSquare(p2,p3));
+        set.add(distanceSquare(p2,p4));
+        set.add(distanceSquare(p3,p4));
+        // A square must have 4 equal sides, so the set must contain 2 different values (the lengths of the sides and the diagonals)
+        // The set should not contain 0, as that would mean that two points have the same coordinates
+        return !set.contains(0) && set.size() == 2;
     }
-
-    private int distance(int[] p1, int[] p2) {
-
-        // distance = (x1 - x2) ^ 2 + (y1 - y2) ^ 2;
-        return ((p1[0] - p2[0]) * (p1[0] - p2[0])) + ((p1[1] - p2[1]) * (p1[1] - p2[1]));
+    // This method calculates the distance between two points and returns its square
+    private int distanceSquare(int[] a, int[] b){
+        // We use the Pythagorean theorem to calculate the distance between the points
+        return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
     }
 }
