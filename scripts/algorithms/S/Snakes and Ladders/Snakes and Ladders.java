1ea56bd1023428b9c812bde91235c8277b939d71
anasimloul <pydev27@gmail.com>
2023-09-08 19:38:43 +0100

    Runtime: 7 ms (Top 58.3%) | Memory: 43.50 MB (Top 67.5%)

--- scripts/algorithms/S/Snakes and Ladders/Snakes and Ladders.java
+++ scripts/algorithms/S/Snakes and Ladders/Snakes and Ladders.java
@@ -1,73 +1,89 @@
+// Runtime: 7 ms (Top 58.3%) | Memory: 43.50 MB (Top 67.5%)
+
 class Solution {
-    public int snakesAndLadders(int[][] board) {
-        if(board == null || board.length == 0) return 0; //Empty Input
-        int n = board.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        getSnakesAndLadders(board , map); //this function will get snake&ladder positions in grid
-        
-        int target = n*n;
-        boolean[] visited = new boolean[target + 1]; //assumeing 1based index so +1;
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
-        visited[1] = true;
-        
-        int steps = 0;
-        while(queue.isEmpty() == false){
-            int size = queue.size();
-            for(int i = 0; i < size; i++){
-                int current = queue.poll();
-                if(current == target){
-                    return steps;
-                }
-                //go for all possiblities
-                for(int dice = 1; dice <= 6; dice++){
-                    int jump = current + dice;
-                    if(jump > target){
-                        break;
-                    }
-                    //if this ladder or snake already used then don't use again
-                    if(map.containsKey(jump) && visited[map.get(jump)]) continue;
-                    
-                    //if there is ladder on place which not used already
-                    if(map.containsKey(jump) && visited[map.get(jump)] == false){
-                        visited[jump] = true; //start of ladder
-                        queue.add(map.get(jump));
-                        visited[map.get(jump)] = true; //end of ladder
-                    }else if(visited[jump] == false){
-                        queue.add(jump);
-                        visited[jump] = true;
+    //what if we have changed the dice number, or changing the starting index or changing the ending index
+    //so i have covered all possible ways in which this question can be asked
+
+//bfs tip:- for better bfs, we can use marking first and then inserting it in the queue which works faster then removing first and then checking
+    public int [] getans(int dice,HashMap<Integer,Integer> map,int si,int ei){
+        //if si==ei just directly return 
+        if(si==ei) return new int [] {0,0,0};
+        LinkedList<int[]> que = new LinkedList<>();
+        que.addLast(new int[] {si,0,0});
+        int level = 0;
+        //to stop visiting cells again
+        boolean [] vis = new boolean [ei+1];
+        vis[si]=true;
+        //starting bfs
+        while(que.size()!=0){
+            int size=que.size();
+            while(size-->0){
+                int [] rem = que.removeFirst();
+                int idx= rem[0];
+                int lad = rem[1];
+                int sna = rem[2];
+                for(int i=1;i<=dice;i++){
+                    int x =i+rem[0]; //checking all the steps
+                    if(x<=ei){ //valid points
+                        if(map.containsKey(x)){ //this means that we have encountered a snake or a ladder
+                            if(map.containsKey(x)){
+                                int val = map.get(x); 
+                                if(val==ei) return new int[] {level+1,lad+1,sna};
+                                if(!vis[val]){
+                                    vis[val]=true;
+                                    //if val>x this means we have a ladder and if less, then it is a snake
+                                    que.addLast(val>x? new int [] {val,lad+1,sna}:new int [] {val,lad,sna+1});
+                                }
+                            }
+                        }
+                        else{
+                            //if it is not present in map, then it is a normal cell, so just insert it directly
+                            if(x==ei) return new int [] {level+1,lad,sna};
+                            if(!vis[x]){
+                                vis[x]=true;
+                                que.addLast(new int [] {x,lad,sna});
+                            }
+                        }
                     }
                 }
             }
-            steps += 1;
+            level++;
         }
-        return -1;
+        return new int [] {-1,0,0};
     }
-    
-    //function to traverse spiraly and get all snake&ladder positions
-    public void getSnakesAndLadders(int[][] board , HashMap<Integer,Integer> map){
-        int startRow = 0;
-        int position = 1;
-        for(int row = board.length-1; row >= 0; row--){
-            if(startRow%2 == 0){
-                for(int col = 0; col < board.length; col++){
-                    if(board[row][col] != -1){
-                        int dest = board[row][col];
-                        map.put(position , dest);
+    public int snakesAndLadders(int[][] board) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count = 1;
+        int n = board.length;
+        boolean flag = true;
+        //traversing the board in the board game fashion and checking if the count that is representing the cell number, if we encounter something other then -1, then it can be a snake or it can be a ladder and mapping that cell index (i.e count to that number)
+        for(int i=n-1;i>=0;i--){
+            //traversing in the order of the board
+            if(flag){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]!=-1){
+                        map.put(count,board[i][j]);   
                     }
-                    position += 1;
-                }
-            }else{
-                for(int col = board.length-1; col >= 0; col--){
-                    if(board[row][col] != -1){
-                        int dest = board[row][col];
-                        map.put(position, dest);
-                    }
-                    position += 1;
+                    count++;
+                    flag=false;
                 }
             }
-            startRow += 1;
+            else{
+                //reversing the direction
+                for(int j=n-1;j>=0;j--){
+                    if(board[i][j]!=-1){
+                        map.put(count,board[i][j]);
+                    }
+                    flag=true;
+                    count++;
+                }
+            }
         }
+        //if snake on destination then just return -1;
+        if(board[0][0]!=-1) return -1;
+        //we only want the minimum steps, but for more conceptual approach for this question, {minm steps,ladders used, snakes used} 
+        int [] ans = getans(6,map,1,n*n);;
+        return ans[0];
+        
     }
 }
-
