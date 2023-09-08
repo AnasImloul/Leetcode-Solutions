c406806f06ded6c6395b7be38593186b509eb6d8
anasimloul <pydev27@gmail.com>
2023-09-08 11:34:22 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 40.83 MB (Top 42.1%)

--- scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.java
+++ scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.java
@@ -1,10 +1,32 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 40.83 MB (Top 42.1%)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<Integer> vals = new ArrayList<>();
+    List<Integer> li = new LinkedList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root==null) return vals;
-        inorderTraversal(root.left);
-        vals.add(root.val);
-        inorderTraversal(root.right);
-        return vals;
+     if(root == null){
+         List<Integer> li = new LinkedList<Integer>();
+         return li ;
+     }
+     inorderTraversal(root.left);     
+     li.add(root.val);     
+     inorderTraversal(root.right);
+     return li;
+
     }
+    
 }
