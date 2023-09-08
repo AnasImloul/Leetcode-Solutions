8c0fb51327317789eb6c5af64f7a65c83cdcc669
anasimloul <pydev27@gmail.com>
2023-09-08 20:01:10 +0100

    Runtime: 172 ms (Top 86.4%) | Memory: 104.29 MB (Top 45.7%)

--- scripts/algorithms/E/Even Odd Tree/Even Odd Tree.js
+++ scripts/algorithms/E/Even Odd Tree/Even Odd Tree.js
@@ -1,24 +1,40 @@
+// Runtime: 172 ms (Top 86.4%) | Memory: 104.29 MB (Top 45.7%)
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var isEvenOddTree = function(root) {
-    let stack = [root]
-    let level = 1
-    
-    while(stack.length>0) {
-        let prev = level % 2 === 0 ? Infinity : -Infinity
-        
-        for (let i=stack.length-1; i>=0; i--) {
-            let node = stack.shift()            
+    let queue = [root];
+    let steps = 0;
 
-            if (level % 2 === 0 && (node.val % 2 !== 0 || prev <= node.val)) return false
-            else if (level % 2 !== 0 && (node.val % 2 === 0 || prev >= node.val)) return false 
+    while(queue.length>0){
+        const currLength = queue.length;
+        let nextQueue=[];
 
-            if (node.left) stack.push(node.left)
-            if (node.right) stack.push(node.right)
-            
-            prev = node.val
+        for(let i=0; i<currLength;i++){
+            const node = queue[i];
+            if(steps%2 !== 0){
+                if((i<currLength-1 && node.val<=queue[i+1].val) || node.val%2 !== 0){
+                    return false;
+                }
+            }else if((i<currLength-1 && node.val>=queue[i+1].val)|| node.val%2 === 0){
+                return false;
+            }
+            if(node.left) nextQueue.push(node.left);
+            if(node.right) nextQueue.push(node.right);
         }
-        
-        level++
+        queue = nextQueue;
+        steps++;
     }
-    
-    return true 
+
+    return true;
 };
