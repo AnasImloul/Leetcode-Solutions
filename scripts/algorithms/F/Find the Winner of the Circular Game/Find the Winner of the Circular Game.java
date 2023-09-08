0e32a41ffb9c45c2a7a655a6446fad8f992a6bcb
anasimloul <pydev27@gmail.com>
2023-09-08 19:34:32 +0100

    Runtime: 43 ms (Top 26.7%) | Memory: 43.29 MB (Top 18.2%)

--- scripts/algorithms/F/Find the Winner of the Circular Game/Find the Winner of the Circular Game.java
+++ scripts/algorithms/F/Find the Winner of the Circular Game/Find the Winner of the Circular Game.java
@@ -1,17 +1,22 @@
+// Runtime: 43 ms (Top 26.7%) | Memory: 43.29 MB (Top 18.2%)
+
 class Solution {
     public int findTheWinner(int n, int k) {
-	    // We set the first node and keep it (to be able to set it as the follower of the last node we will create)
-        Node firstNode = new Node(1);
-		// This is just part of our loop
-        Node prevNode = firstNode;
-        for(int i=2; i<=n; i++) {
-            Node currentNode = new Node(i);
-            currentNode.prev = prevNode;
-            prevNode.next = currentNode;
-            prevNode = currentNode;
+	    // Initialisation of the LinkedList
+        LinkedList<Integer> participants = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+		    participants.add(i);
+		}
+		
+		int lastKilled = 0;
+		// Run the game
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k-1; j++) {
+			    participants.add(participants.poll());
+			}
+            lastKilled = participants.poll();
         }
-		// Then set the last and first nodes
-        firstNode.prev = prevNode;
-        prevNode.next = firstNode;
+        // Return the last one killed
+        return lastKilled;
     }
 }
