536afc547c74d6dc6a4862491ed8cf3157b89dd5
anasimloul <pydev27@gmail.com>
2023-09-08 11:40:11 +0100

    Runtime: 18 ms (Top 78.7%) | Memory: 55.94 MB (Top 56.9%)

--- scripts/algorithms/M/Minimum Absolute Difference/Minimum Absolute Difference.java
+++ scripts/algorithms/M/Minimum Absolute Difference/Minimum Absolute Difference.java
@@ -1,23 +1,26 @@
+// Runtime: 18 ms (Top 78.7%) | Memory: 55.94 MB (Top 56.9%)
+
 class Solution {
-
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(arr);
-        List<List<Integer>> result = new ArrayList<>();
-
-        int minDifference = Integer.MAX_VALUE;
-        for (int i = 1; i < arr.length; i++) {
-            int currentDifference = Math.abs(arr[i] - arr[i - 1]);
-            if (minDifference > currentDifference) {
-                minDifference = currentDifference;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1;i++){
+            int diff=Math.abs(arr[i]-arr[i+1]);
+            if(diff<min)
+            {
+            min=diff;
             }
         }
-
-        for (int i = 1; i < arr.length; i++) {
-            if (minDifference == Math.abs(arr[i] - arr[i - 1])) {
-                result.add(List.of(arr[i - 1], arr[i]));
+        for(int i=0;i<arr.length-1;i++){
+            int diff=Math.abs(arr[i]-arr[i+1]);
+            if(diff==min){
+                 List<Integer> pair=new ArrayList<>(2);
+                pair.add(arr[i]);
+                pair.add(arr[i+1]);
+                ans.add(pair);
             }
         }
-
-        return result;
+        return ans;
     }
 }
