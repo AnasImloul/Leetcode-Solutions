3e4214afe95f690d979d9640fadc806ffd2690f1
anasimloul <pydev27@gmail.com>
2023-09-08 20:06:34 +0100

    Runtime: 74 ms (Top 53.9%) | Memory: 45.22 MB (Top 51.1%)

--- scripts/algorithms/K/Koko Eating Bananas/Koko Eating Bananas.js
+++ scripts/algorithms/K/Koko Eating Bananas/Koko Eating Bananas.js
@@ -1,15 +1,28 @@
+// Runtime: 74 ms (Top 53.9%) | Memory: 45.22 MB (Top 51.1%)
+
 var minEatingSpeed = function(piles, h) {
-    let low = 1, high = Math.max(...piles)
-    let minBanana = Infinity
-    while(low <= high){
-        const banana = Math.floor((high - low) / 2) + low
-        const hours = piles.reduce((a, c) => a + Math.ceil(c / banana), 0)
-        if(hours <= h){
-            minBanana = banana
-            high = banana - 1
+    /*The range of bananas that Koko can eat is k = 1 to Max(piles)*/
+    let startk = 1;
+    let endk = Math.max(...piles);
+    
+    while(startk <= endk){
+        let midk = Math.floor(startk + (endk - startk)/2);
+        /*midk are the count of bananas that koko decide to eat. 
+        So how many hours she will take to finish the piles?*/
+        let hrs = 0;
+        for(let pile of piles){
+            /*pile is the num of bananas in piles*/
+            hrs += Math.ceil(pile/midk);
         }
-        else low = banana + 1
+        if(hrs > h){
+            /*Now if hrs > h she will not be to finish the pile so we have 
+            to increase the bananas by moving start.*/
+            startk = midk + 1;
+        }else{
+            /*If hrs <= h she will be eating too fast so we can reduce the bananas 
+            so she eats slowly. So decrement end.*/
+            endk = midk - 1;
+        }
     }
-    return minBanana
+    return startk;
 };
-// time O(nlog(max(banana)), space O(1) 
