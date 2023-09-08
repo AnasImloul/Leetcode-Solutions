4d3264951b829b786e31d577ef1524cb30cea067
anasimloul <pydev27@gmail.com>
2023-09-08 10:13:55 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 39.05 MB (Top 80.3%)

--- scripts/algorithms/C/Count All Valid Pickup and Delivery Options/Count All Valid Pickup and Delivery Options.java
+++ scripts/algorithms/C/Count All Valid Pickup and Delivery Options/Count All Valid Pickup and Delivery Options.java
@@ -1,11 +1,12 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 39.05 MB (Top 80.3%)
+
 class Solution {
     public int countOrders(int n) {
-        int mod = 1000000007;
-        long ans = 1, sum = 0;
-        for(int i=0;i<n;i++){
-            sum += (4*i+1);
-            ans = (ans*sum)%mod;
+        long res = 1;
+        long mod = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            res = res * (2 * i - 1) * i % mod;
         }
-        return (int)(ans%mod);
+        return (int)res;
     }
 }
