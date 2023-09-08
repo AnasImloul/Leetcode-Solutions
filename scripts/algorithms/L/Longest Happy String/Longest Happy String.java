c30cd5cf7240b9dcb3920400b65f41c91a332bac
anasimloul <pydev27@gmail.com>
2023-09-08 11:07:01 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 39.66 MB (Top 66.7%)

--- scripts/algorithms/L/Longest Happy String/Longest Happy String.java
+++ scripts/algorithms/L/Longest Happy String/Longest Happy String.java
@@ -1,64 +1,47 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 39.66 MB (Top 66.7%)
+
+/*
+The idea behid this problem
+1. Here we start by taking the size as the sum of a, b, c.
+2. Then we use 3 variables A, B, C to count the occurance of a, b, c.
+3. Now we iterate until the size, and 
+    -> Checks the largest number among a, b, c and whether the count < 2 or whther the count of other letters is 2 and there is still letters that can be added, then we append the letter, decrement from the total count of that particular letter and increase the occurance of that letter and set others back to zero.
+    
+4. Finally return the string.
+*/
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
+        int totalSize = a + b + c;
+        int A = 0;
+        int B = 0;
+        int C = 0;
         StringBuilder sb = new StringBuilder();
-        PriorityQueue<Pair> queue = new PriorityQueue<>((x, y) -> y.val - x.val);
-        
-        if (a > 0) {
-            queue.add(new Pair('a', a));
-        }
-        if (b > 0) {
-            queue.add(new Pair('b', b));
-        }
-        if (c > 0) {
-            queue.add(new Pair('c', c));
-        }
-        
-        sb.append("dd");
-        
-        while (!queue.isEmpty()) {
-            //get max count pair
-            Pair first = queue.remove();
-            
-            //check if last two chars in sb are same as first pair char
-            if (first.c == sb.charAt(sb.length() - 2) && first.c == sb.charAt(sb.length() - 1)) {
-                //if queue is empty we can't add anymore to sb as it will violate rule for max 2 same chars
-                if (queue.isEmpty()) {
-                    break;
-                }
-                
-                //we can add the next max count pair now
-                Pair second = queue.remove();
-                sb.append(second.c);
-                
-                // update second pair and add it back
-                if (second.val > 1) {
-                    second.val--;
-                    queue.add(second);
-                }
-                //add first pair (which we did not use) back to queue
-                queue.add(first);
-            } else { // this means we can add first pair char as it doesn't violate the max 2 same chars rule
-                sb.append(first.c);
-                
-                //update first pair and add it back
-                if (first.val > 1) {
-                    first.val--;
-                    queue.add(first);
-                }
+        for (int i=0; i<totalSize; i++) {
+            // checks a is largest and its count still < 2 or B and C =2 and there are still a that can be added 
+            if ((a>=b && a>=c && A<2) || (B==2 && a>0) || (C==2 && a>0)) {
+                sb.append("a");
+                a -= 1;
+                A += 1;
+                B = 0;
+                C = 0;
+            }
+            // check b is largest and its count still < 2 or A and C = 2 and there are still b that cam be added
+            else if ((b>=a && b>=c && B<2) || (A==2 && b>0) || (C==2 && b>0)) {
+                sb.append("b");
+                b -= 1;
+                B += 1;
+                A = 0;
+                C = 0;
+            }
+            // checks c is largest and its count still < 2 or B and A = 2 and there are still c that can be added
+            else if ((c>=a && c>=b && C<2) || (A==2 && c>0) || (B==2 && c>0)) {
+                sb.append("c");
+                c -= 1;
+                C += 1;
+                A = 0;
+                B = 0;
             }
         }
-        
-        //exclude "dd" from the result
-        return sb.substring(2);
-    }
-}
-
-class Pair {
-    char c;
-    int val;
-    
-    Pair(char c, int val) {
-        this.c = c;
-        this.val = val;
+        return sb.toString();
     }
 }
