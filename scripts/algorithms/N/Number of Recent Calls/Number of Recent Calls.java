0b0bf8bb090d820e4d28eec2789ed16aedd6e816
anasimloul <pydev27@gmail.com>
2023-09-08 10:15:19 +0100

    Runtime: 1373 ms (Top 10.1%) | Memory: 52.32 MB (Top 27.9%)

--- scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.java
+++ scripts/algorithms/N/Number of Recent Calls/Number of Recent Calls.java
@@ -1,21 +1,24 @@
-class RecentCounter {
+// Runtime: 1373 ms (Top 10.1%) | Memory: 52.32 MB (Top 27.9%)
 
-    LinkedList<Integer> ll = new LinkedList<>();
-    
-    
-    
+class RecentCounter {
+    ArrayList<Integer> calls ;
     public RecentCounter() {
-        ll.clear();
-    }
+        calls = new ArrayList<Integer>();
+        }
     
     public int ping(int t) {
-        ll.add(t);
-        
-        while(t-ll.peek() > 3000){
-            ll.remove();
+        calls.add(t);
+        int count = 0;
+        for(Integer call:calls){
+            if( t-call<=3000) count++;
         }
-            
-        return ll.size();
+        return count;
+        
     }
 }
 
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
