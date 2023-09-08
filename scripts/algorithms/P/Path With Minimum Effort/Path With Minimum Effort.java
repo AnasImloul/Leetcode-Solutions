d5dfaf397fc565fb252b84dd66f6486716591237
anasimloul <pydev27@gmail.com>
2023-09-08 10:09:43 +0100

    Runtime: 36 ms (Top 97.2%) | Memory: 44.02 MB (Top 69.2%)

--- scripts/algorithms/P/Path With Minimum Effort/Path With Minimum Effort.java
+++ scripts/algorithms/P/Path With Minimum Effort/Path With Minimum Effort.java
@@ -1,26 +1,140 @@
-// 36 ms. 97.51%
-class Solution {
-    private static final int[][] dir = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-    public int minimumEffortPath(int[][] heights) {
-        int m = heights.length, n = heights[0].length;
-        int[][] dist = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-        dist[0][0] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        pq.add(new int[] {0, 0, 0});
-        while(!pq.isEmpty()) {
-            int[] p = pq.poll();
-            int i = p[0], j = p[1];
-            if(i == m - 1 && j == n - 1) break;
-            for(int[] d: dir) {
-                int x = i + d[0], y = j + d[1];
-                if(x < 0 || x >= m || y < 0 || y >= n) continue;
-                int alt = Math.max(p[2], Math.abs(heights[i][j] - heights[x][y]));
-                if(alt < dist[x][y]) {
-                    pq.add(new int[] {x, y, dist[x][y] = alt});
-                }
-            }
-        }
-        return dist[m - 1][n - 1];
+// Runtime: 36 ms (Top 97.2%) | Memory: 44.02 MB (Top 69.2%)
+
+class Tuple {
+
+    int distance;
+
+    int row;
+
+    int col;
+
+    
+
+    Tuple(int distance, int row, int col) {
+
+        this.distance = distance;
+
+        this.row = row;
+
+        this.col = col;
+
     }
+
 }
+
+
+
+class Solution {
+
+    public int minimumEffortPath(int[][] heights) {
+
+        // Create a min heap based on the distance
+
+        PriorityQueue<Tuple> minHeap = new PriorityQueue<>((x, y) -> x.distance - y.distance);
+
+        
+
+        int rows = heights.length;
+
+        int cols = heights[0].length;
+
+        
+
+        // Create a 2D array to store the minimum effort to reach each cell
+
+        int effort[][] = new int[rows][cols];
+
+        
+
+        // Initialize all efforts to maximum initially
+
+        for (int i = 0; i < rows; i++) {
+
+            Arrays.fill(effort[i], Integer.MAX_VALUE);
+
+        }
+
+        
+
+        effort[0][0] = 0; // Initial effort at the starting cell
+
+        
+
+        // Add the starting cell to the min heap
+
+        minHeap.add(new Tuple(0, 0, 0));
+
+        
+
+        // Arrays to represent row and column changes for 4 directions
+
+        int dr[] = {-1, 0, 1, 0}; // Up, Right, Down, Left
+
+        int dc[] = {0, 1, 0, -1};
+
+        
+
+        while (!minHeap.isEmpty()) {
+
+            Tuple current = minHeap.poll(); // Get the cell with the minimum effort
+
+            int distance = current.distance;
+
+            int row = current.row;
+
+            int col = current.col;
+
+            
+
+            if (row == rows - 1 && col == cols - 1) {
+
+                return distance; // If reached the destination, return the effort
+
+            }
+
+            
+
+            // Explore each of the 4 possible directions
+
+            for (int i = 0; i < 4; i++) {
+
+                int newRow = row + dr[i]; // Calculate new row index
+
+                int newCol = col + dc[i]; // Calculate new column index
+
+                
+
+                // Check if the new cell is within bounds
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+
+                    
+
+                    // Calculate the new effort based on the maximum of height difference and current effort
+
+                    int newEffort = Math.max(Math.abs(heights[row][col] - heights[newRow][newCol]), distance);
+
+                    
+
+                    // If the new effort is less than the stored effort for the cell, update and add to heap
+
+                    if (newEffort < effort[newRow][newCol]) {
+
+                        effort[newRow][newCol] = newEffort;
+
+                        minHeap.add(new Tuple(newEffort, newRow, newCol)); // Add to heap for further exploration
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return 0; // This value should be replaced with the actual minimum effort
+
+    }
+
+}
+
