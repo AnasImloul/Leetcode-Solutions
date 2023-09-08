b071b737593a7cc6487e70c8fcc88701a925f701
anasimloul <pydev27@gmail.com>
2023-09-08 11:30:59 +0100

    Runtime: 5 ms (Top 28.3%) | Memory: 45.04 MB (Top 8.2%)

--- scripts/algorithms/M/Minimum Number of Refueling Stops/Minimum Number of Refueling Stops.java
+++ scripts/algorithms/M/Minimum Number of Refueling Stops/Minimum Number of Refueling Stops.java
@@ -1,23 +1,17 @@
+// Runtime: 5 ms (Top 28.3%) | Memory: 45.04 MB (Top 8.2%)
+
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>((i1,i2)->i2-i1);
-        int len=stations.length;
-        int pre=0 ,cur, fuel=startFuel;
-        int count=0;
-        for(int i=0;i<=len;++i){
-            cur=i==len?target:stations[i][0];
-            fuel -= cur-pre;
-            while(fuel<0 && !pq.isEmpty()){
-                fuel += pq.poll();
-                count++;
-            }
-            if(fuel<0) return -1;
-            
-            if(i!=len){
-                pq.add(stations[i][1]);
-                pre=cur;
+        if(startFuel >= target) return 0;
+        int[][] dp = new int[stations.length + 1][stations.length + 1];
+        for (int i = 0; i < dp.length; i++) dp[i][0] = startFuel;
+        for (int j = 1; j < dp.length; j++) {
+            for (int i = j; i < dp.length; i++) {
+                dp[i][j] = Math.max(dp[i-1][j], stations[i-1][0] > dp[i-1][j-1] ?
+                        Integer.MIN_VALUE : dp[i-1][j-1] + stations[i-1][1]);
+                if(dp[i][j] >= target) return j;
             }
         }
-        return count;
+        return -1;
     }
 }
