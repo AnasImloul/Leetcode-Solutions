ec9d36bff76578d32ec1df9d7fa7b794ad9c0787
anasimloul <pydev27@gmail.com>
2023-09-08 11:07:44 +0100

    Runtime: 29 ms (Top 44.9%) | Memory: 82.52 MB (Top 97.7%)

--- scripts/algorithms/R/Remove Max Number of Edges to Keep Graph Fully Traversable/Remove Max Number of Edges to Keep Graph Fully Traversable.java
+++ scripts/algorithms/R/Remove Max Number of Edges to Keep Graph Fully Traversable/Remove Max Number of Edges to Keep Graph Fully Traversable.java
@@ -1,81 +1,100 @@
+// Runtime: 29 ms (Top 44.9%) | Memory: 82.52 MB (Top 97.7%)
+
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) 
     {
-        Arrays.sort(edges , (a,b) -> b[0] - a[0]);
+        Arrays.sort(edges, (a, b)->{
+            return b[0]-a[0];
+        });//giving the priority to third type of edge or the edge which Bob and Alice both can access
         
-        int[] aparent = new int[n+1];
-        int[] bparent = new int[n+1];
+        //1-based indexing of nodes 
+        int []parentAlice= new int[n+1];//Graph 1 for Alice connectedness
+        int []parentBob= new int[n+1];//Graph 2 for Bob connectedness
         
-        int[] arank = new int[n+1];
-        int[] brank = new int[n+1];
-        
-        for(int i=0;i<n+1;i++){
-            aparent[i] = i;
-            bparent[i] = i;
-            arank[i] = 1;
-            brank[i] = 1;
+        for(int i= 0; i< n+1; i++){//every node is pointing to itself, at first no connection is considered all sets are independent(no dependency) 
+            parentAlice[i]= i;
+            parentBob[i]= i;
         }
         
-        int amerged = 1;
-        int bmerged = 1;
-        int remove = 0;
+        //number of merged unique node for Alice and Bob that are required to maintain the connectedness of Alice and Bob graph nodes//intialised with one because merging happens in pair 
+        int mergeAlice= 1;
+        int mergeBob= 1;
         
-        for(int[] edge : edges){
-            if(edge[0] == 3){
-                boolean aunion = union(edge[1] , edge[2] , aparent , arank);
-                boolean bunion = union(edge[1] , edge[2] , bparent , brank);
-                
-                if(aunion == true)
-                    amerged++;
-                if(bunion == true)
-                    bmerged++;
-                if(aunion == false && bunion == false)
-                    remove++;
-            }
-            else if(edge[0] == 1){
-                boolean aunion = union(edge[1] , edge[2] , aparent , arank);
-                if(aunion == true)
-                    amerged++;
-                else
-                    remove++;
-            }
-            else{
-                boolean bunion = union(edge[1] , edge[2] , bparent , brank);
-                if(bunion == true)
-                    bmerged++;
-                else
-                    remove++;
-            }
+        //number of cyclic or the non dependent node, that are not required for the connectedness of Alice and Bob nodes  
+        int removeEdge= 0;
+        
+        for(int []edge: edges)
+        {
+            int cat= edge[0];//category of edge 1)edge Alice can only access 2)edge Bob can only access 3)edge both Alice and Bob can access
+            int u= edge[1];
+            int v= edge[2];
             
-        }
-        if(amerged != n || bmerged != n)
-                return -1;
-            return remove;
-    }
-    
-    public int find(int x , int[] parent){
-        if(parent[x] == x)
-            return x;
-        int temp = find(parent[x] , parent);
-        parent[x] = temp;
-        return temp;
-    }
-    
-    public boolean union(int x , int y , int[] parent , int[] rank){
-        int lx = find(x , parent);
-        int ly = find(y , parent);
-        
-        if(lx != ly){
-            if(rank[lx] > rank[ly])
-                parent[ly] = lx;
-            else if(rank[ly] > rank[lx])
-                parent[lx] = ly;
-            else{
-                parent[ly] = lx;
-                rank[lx]++;
+            if(cat == 3){//edge both Alice and Bob an access
+                
+                //creating dependency of nodes in graph 1 and 2 
+                boolean tempAlice= union(u, v, parentAlice);
+                boolean tempBob= union(u, v, parentBob);
+                
+                if(tempAlice == true)
+                    mergeAlice+= 1;
+                
+                if(tempBob == true)
+                    mergeBob+= 1;
+                
+                if(tempAlice == false && tempBob == false)//retundant or the cyclic non-dependent edge//both Alice and Bob don't rquire it connection is already there between these pair of nodes
+                    removeEdge+= 1;
             }
-            return true;
+            else if(cat == 2){//edge Bob can only access 
+                
+                //creating dependency of nodes in graph 2
+                boolean tempBob= union(u, v, parentBob);
+                
+                if(tempBob == true)
+                    mergeBob+= 1;
+                else//no merging of set is done, that means that this edge is not required because it will form cycle or the dependency 
+                    removeEdge+= 1;
+            }
+            else{//edge Alice can only access 
+                
+                //creating dependency of nodes in graph 1
+                boolean tempAlice= union(u, v, parentAlice);
+                
+                if(tempAlice == true)
+                    mergeAlice+= 1; 
+                else//no merging of set is done, that means that this edge is not required because it will form cycle or the dependency 
+                    removeEdge+= 1;
+            }
         }
-        return false;
+        if(mergeAlice != n || mergeBob != n)//all node are not connected, connectedness is not maintained 
+            return -1;
+        return removeEdge;//number of edge removed by maintaining the connectedness 
     }
+    
+    public int find(int x, int[] parent)
+    {
+        if(parent[x] == x)//when we found the absolute root or the leader of the set 
+            return x;
+        
+        int temp= find(parent[x], parent);
+        
+        parent[x]= temp;//Path Compression//child pointing to the absolute root or the leader of the set, while backtracking
+        
+        return temp;//returning the absolute root 
+    }
+    
+    public boolean union(int x, int y, int[] parent)
+    {
+        int lx= find(x, parent);//leader of set x or the absolute root
+        int ly= find(y, parent);//leader of set y or the absolute root
+        
+        if(lx != ly){//belong to different set merging 
+            
+            //Rank Compression is not done, but you can do it 
+            parent[lx]= ly;
+            
+            return true;//union done, dependency created
+        }
+        else
+            return false;//no union done cycle is due to this edge 
+    }//Please do Upvote, it helps a lot 
 }
