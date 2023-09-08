96251cef44273db0531025237a7ff9bc4fa55f01
anasimloul <pydev27@gmail.com>
2023-09-08 11:28:23 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 39.76 MB (Top 13.5%)

--- scripts/algorithms/B/Binary Number with Alternating Bits/Binary Number with Alternating Bits.java
+++ scripts/algorithms/B/Binary Number with Alternating Bits/Binary Number with Alternating Bits.java
@@ -1,10 +1,14 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 39.76 MB (Top 13.5%)
+
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String l = Integer.toBinaryString(n);
-        for(int i = 0 ; i< l.length() -1 ; i++){
-            if(l.charAt(i) == l.charAt(i+1))
-                return false;
-        }
-        return true;
+        int flag = 1;
+        if(n % 2 == 0) flag = 0;
+        return bin(n / 2, flag);
+    }
+    public boolean bin(int n, int flag) {
+        if(flag == n % 2) return false;
+        if(n == 0) return true;
+        else return bin(n / 2, n % 2);
     }
 }
