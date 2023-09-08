d6d27053b4d1f54befc6889706c365250141bbb0
anasimloul <pydev27@gmail.com>
2023-09-08 10:07:49 +0100

    Runtime: 7 ms (Top 73.3%) | Memory: 44.18 MB (Top 74.0%)

--- scripts/algorithms/D/Design Front Middle Back Queue/Design Front Middle Back Queue.java
+++ scripts/algorithms/D/Design Front Middle Back Queue/Design Front Middle Back Queue.java
@@ -1,47 +1,52 @@
+// Runtime: 7 ms (Top 73.3%) | Memory: 44.18 MB (Top 74.0%)
+
 class FrontMiddleBackQueue {
-    LinkedList<Integer> list;
-    
-    public FrontMiddleBackQueue() {
-        list = new LinkedList();
+
+    Deque<Integer> dq1, dq2;
+    public FrontMiddleBackQueue() {        
+        dq1 = new ArrayDeque<Integer>();
+        dq2 = new ArrayDeque<Integer>();
     }
     
     public void pushFront(int val) {
-        list.add(0, val);
-    }
-    
-    public void pushMiddle(int val) {
-        list.add(list.size() / 2, val);
-    }
+        dq1.addFirst(val);
+    }    
     
     public void pushBack(int val) {
-        list.add(val);
-    }
+        dq2.addLast(val);
+    }   
+    
+    public void pushMiddle(int val) {
+        while(dq1.size() + 1 < dq2.size())
+            dq1.addLast(dq2.removeFirst());       
+        while(dq1.size() > dq2.size())
+            dq2.addFirst(dq1.removeLast());        
+        dq1.addLast(val);        
+    }     
     
     public int popFront() {
-        if(isEmpty()) return -1;
-       
-        return list.remove(0);
-    }
+        if(!dq1.isEmpty())
+            return dq1.removeFirst();
+        if(!dq2.isEmpty())
+            return dq2.removeFirst();
+        return -1;    
+    }  
     
     public int popMiddle() {
-        if(isEmpty()) return -1;
-        
-        return list.remove(findMiddleIndex());
-    }
+        if(dq1.isEmpty() && dq2.isEmpty())
+            return -1;        
+        while(dq1.size() < dq2.size())
+            dq1.addLast(dq2.removeFirst());     
+        while(dq1.size() > dq2.size() + 1)
+            dq2.addFirst(dq1.removeLast());
+        return !dq1.isEmpty() ? dq1.removeLast() : dq2.removeFirst();
+    }    
     
     public int popBack() {
-        if(isEmpty()) return -1;
-        
-        return list.remove(list.size() - 1);
-    }
-    
-    private boolean isEmpty(){
-        return list.isEmpty();
-    }
-    
-    private int findMiddleIndex(){
-        return (list.size() - 1) / 2;
-    }
-    
-
+        if(!dq2.isEmpty())
+            return dq2.removeLast();
+        if(!dq1.isEmpty())
+            return dq1.removeLast();
+        return -1;            
+    }    
 }
