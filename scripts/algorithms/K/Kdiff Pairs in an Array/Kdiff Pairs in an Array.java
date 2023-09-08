49806b668c7685cc113fbbd1671f8dc2a96e9265
anasimloul <pydev27@gmail.com>
2023-09-08 11:31:24 +0100

    Runtime: 11 ms (Top 50.6%) | Memory: 43.63 MB (Top 70.1%)

---
+++ scripts/algorithms/K/Kdiff Pairs in an Array/Kdiff Pairs in an Array.java
@@ -0,0 +1,17 @@
+// Runtime: 11 ms (Top 50.6%) | Memory: 43.63 MB (Top 70.1%)
+
+ // O(n) Time Solution
+
+ class Solution {
+ 		public int findPairs(int[] nums, int k) {
+ 			Map<Integer, Integer> map = new HashMap();
+ 			for (int num : nums)
+ 				map.put(num, map.getOrDefault(num, 0) + 1);
+
+ 			int result = 0;
+ 			for (int i : map.keySet())
+ 				if (k > 0 && map.containsKey(i + k) || k == 0 && map.get(i) > 1)
+ 					result++;
+ 			return result;
+ 		}
+ 	}
