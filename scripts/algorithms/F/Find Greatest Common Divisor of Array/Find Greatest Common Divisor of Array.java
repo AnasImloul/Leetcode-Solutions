b9e9aebf84e04c09f4e43560c1f598b7cc62f1d0
anasimloul <pydev27@gmail.com>
2023-09-08 19:28:44 +0100

    Runtime: 3 ms (Top 25.8%) | Memory: 42.56 MB (Top 95.7%)

--- scripts/algorithms/F/Find Greatest Common Divisor of Array/Find Greatest Common Divisor of Array.java
+++ scripts/algorithms/F/Find Greatest Common Divisor of Array/Find Greatest Common Divisor of Array.java
@@ -1,15 +1,16 @@
+// Runtime: 3 ms (Top 25.8%) | Memory: 42.56 MB (Top 95.7%)
+
 class Solution {
     public int findGCD(int[] nums) {
-        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-        for(int num : nums) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+        Arrays.sort(nums);
+        int n=nums[nums.length-1];
+        int result=nums[0];
+        while(result>0){
+            if(nums[0]%result==0 && n%result==0){
+                break;
+            }
+            result--;
         }
-        return gcd(min, max);
-    }
-    
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+        return result;
     }
 }
