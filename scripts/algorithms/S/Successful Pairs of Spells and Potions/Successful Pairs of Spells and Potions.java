969484c054ffe0a8c99f869f09cead97fbe8efbd
anasimloul <pydev27@gmail.com>
2023-09-08 11:29:13 +0100

    Runtime: 40 ms (Top 80.4%) | Memory: 58.00 MB (Top 19.8%)

---
+++ scripts/algorithms/S/Successful Pairs of Spells and Potions/Successful Pairs of Spells and Potions.java
@@ -0,0 +1,26 @@
+// Runtime: 40 ms (Top 80.4%) | Memory: 58.00 MB (Top 19.8%)
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] pairs = new int[n];
+        Arrays.sort(potions);
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int left = 0;
+            int right = m - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long product = (long) spell * potions[mid];
+                if (product >= success) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            pairs[i] = m - left;
+        }
+        return pairs;
+    }
+}
