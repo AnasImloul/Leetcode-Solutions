2b9772fe8379b73a31a84d7dd48b778a5499b30c
anasimloul <pydev27@gmail.com>
2023-09-08 11:06:32 +0100

    Runtime: 1 ms (Top 32.9%) | Memory: 40.38 MB (Top 71.0%)

---
+++ scripts/algorithms/F/Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,19 @@
+// Runtime: 1 ms (Top 32.9%) | Memory: 40.38 MB (Top 71.0%)
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int haylength=haystack.length();
+        int needlelength=needle.length();
+        if(haylength<needlelength)
+            return -1;
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            int j=0;
+            while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j==needle.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
