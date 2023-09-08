781b01d0204b23affe7b35f1ece5ce28d104dfce
anasimloul <pydev27@gmail.com>
2023-09-08 10:24:52 +0100

    Runtime: 3 ms (Top 55.1%) | Memory: 43.05 MB (Top 60.9%)

--- scripts/algorithms/M/Most Frequent Number Following Key In an Array/Most Frequent Number Following Key In an Array.java
+++ scripts/algorithms/M/Most Frequent Number Following Key In an Array/Most Frequent Number Following Key In an Array.java
@@ -1,25 +1,23 @@
+// Runtime: 3 ms (Top 55.1%) | Memory: 43.05 MB (Top 60.9%)
+
 class Solution {
     public int mostFrequent(int[] nums, int key) {
-        int freq[] = new int[1001];
-        
-        //count frequency of numbers which are followed by key
-        for (int i = 1; i < nums.length; i++) {
-            //if previous number is key
-            //increase frequency of the number
-            if (nums[i - 1] == key) { 
-                freq[nums[i]]++;
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==key){
+                int target=nums[i+1];
+                map.put(target,map.getOrDefault(target,0)+1);
             }
         }
-        
-        //check which number is most frequent
-        int max = 0;
-        for (int i = 1; i < freq.length; i++) {
-            if (freq[i] > freq[max]) {
-                max = i;
+        int max=0;
+        int re=0;
+        for(int x:map.keySet()){
+            if(map.get(x)>max){
+                re=x;
+                max=map.get(x);
             }
         }
-        
-        //return
-        return max;
+        return re;
     }
 }
