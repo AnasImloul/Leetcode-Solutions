d807c1015ded6795a6ed537121cc0796d31b75ae
anasimloul <pydev27@gmail.com>
2023-09-08 11:29:41 +0100

    Runtime: 135 ms (Top 50.7%) | Memory: 44.02 MB (Top 24.4%)

--- scripts/algorithms/M/Minimum Number of Operations to Move All Balls to Each Box/Minimum Number of Operations to Move All Balls to Each Box.java
+++ scripts/algorithms/M/Minimum Number of Operations to Move All Balls to Each Box/Minimum Number of Operations to Move All Balls to Each Box.java
@@ -1,13 +1,16 @@
-class Solution {
-    public int[] minOperations(String boxes) {
-        int len = boxes.length();
-        int[] ans = new int[len];
-        for(int i=0;i<len;i++){
-            int cost = 0;
-            for(int j=0;j<len;j++){
-                if(boxes.charAt(j)=='1') cost += Math.abs(i-j);
+// Runtime: 135 ms (Top 50.7%) | Memory: 44.02 MB (Top 24.4%)
+
+class Solution{
+    public int[] minOperations(String boxes){
+        int n = boxes.length();
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            int t = 0;
+            for(int j=0; j<n; j++){
+                char c = boxes.charAt(j);
+                if(c=='1') t += Math.abs(i-j);
             }
-            ans[i] = cost;
+            ans[i] = t;
         }
         return ans;
     }
