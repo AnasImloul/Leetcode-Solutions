1db967332086c9f13bbed882bea298011eea9740
anasimloul <pydev27@gmail.com>
2023-09-08 11:02:03 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 40.55 MB (Top 34.5%)

--- scripts/algorithms/P/Percentage of Letter in String/Percentage of Letter in String.java
+++ scripts/algorithms/P/Percentage of Letter in String/Percentage of Letter in String.java
@@ -1,25 +1,17 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 40.55 MB (Top 34.5%)
+
 class Solution {
-    public int percentageLetter(String s, char letter) {
-        int count=0; //count acts as a counter variabe to count the give character "letter" in the string 
-        
-        //Traverse the string s and each time the character int the string is equal to "letter"
-        //increment the count variable by 1.
-        for(char ch:s.toCharArray())
-        {
-            if(ch==letter)
-                count++;
+    public int percentageLetter(String str, char letter) {
+        int count=0;
+        int n=str.length();
+        for(int i=0;i<n;i++){
+            if(str.charAt(i)==letter){
+
+                count ++;
+            }
         }
+                 int per= (100*count)/n;
+         return per;   
         
-    //If the count is zero, then the percentage is also zero
-        if(count==0) 
-            return 0;
-        
-        
-     // to find the total characters in the strings or we can say to find the length of the string   
-        int total=s.length(); 
-    
-    // Calculate the total percentage using the standard percentage formula i.e (given*100)/total
-        int per=(count*100)/total;
-        return per;
     }
 }
