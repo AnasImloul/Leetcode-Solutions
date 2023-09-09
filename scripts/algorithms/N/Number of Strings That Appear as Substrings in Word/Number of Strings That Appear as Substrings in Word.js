c515797a533a5f923ffe5a4eb57b55ab76577d88
AnasImloul <pydev27@gmail.com>
2023-09-09 17:35:41 +0100

    Runtime: 45 ms (Top 93.3%) | Memory: 42.54 MB (Top 18.4%)

--- scripts/algorithms/N/Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.js
+++ scripts/algorithms/N/Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.js
@@ -1,12 +1,13 @@
-// Runtime: 75 ms (Top 77.95%) | Memory: 42 MB (Top 77.17%)
+// Runtime: 45 ms (Top 93.3%) | Memory: 42.54 MB (Top 18.4%)
+
 /**
  * @param {string[]} patterns
  * @param {string} word
  * @return {number}
  */
 var numOfStrings = function(patterns, word) {
-    let result = 0;
-
+    let result = 0; 
+    
     for(let char of patterns){
         if(word.includes(char)){
             result++;
@@ -14,6 +15,6 @@ var numOfStrings = function(patterns, word) {
                result += 0;
            }
     }
-
+    
     return result;
 };
