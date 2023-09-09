83466af03a508e1855249fdd2c410b27e76df7d5
anasimloul <pydev27@gmail.com>
2023-09-09 01:26:28 +0100

    Runtime: 39 ms (Top 92.6%) | Memory: 12.28 MB (Top 69.1%)

--- scripts/algorithms/R/Remove Boxes/Remove Boxes.cpp
+++ scripts/algorithms/R/Remove Boxes/Remove Boxes.cpp
@@ -1,29 +1,38 @@
+// Runtime: 39 ms (Top 92.6%) | Memory: 12.28 MB (Top 69.1%)
+
 class Solution {
-    vector<vector<vector<int>>> dp;
 public:
-    int removeBoxes(vector<int>& boxes) {
-        int n = boxes.size();
-        dp.resize(n+1, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
-        
-        return solve(boxes, 0, n-1, 0);
+    int dp[102][102][102];
+    int solve(int i, int j, int extra, vector<pair<int, int>> &groups)
+    {
+        if (i > j)
+            return 0;
+
+        if (dp[i][j][extra] != -1)
+            return dp[i][j][extra];
+
+        int ans = (groups[i].second + extra) * (groups[i].second + extra) + solve(i + 1, j, 0, groups);
+
+        for (int g = i + 1; g <= j; g++)
+            if (groups[g].first == groups[i].first)
+                ans = max(ans, solve(i + 1, g - 1, 0, groups) + solve(g, j, extra + groups[i].second, groups));
+
+        return dp[i][j][extra] = ans;
     }
-    
-    int solve(vector<int> &boxes, int i, int j, int cnt) {
-        if(i > j) return 0;
-        
-        if(dp[i][j][cnt] != -1) return dp[i][j][cnt];
-        
-        int i0 = i, cnt0 = cnt;
-        
-        while(i + 1 <= j && boxes[i] == boxes[i+1]) i++, cnt++;
-        
-        int ans = (cnt+1) * (cnt+1) + solve(boxes, i+1, j, 0);
-        
-        for(int k=i+1; k<=j; k++) {
-            if(boxes[k] == boxes[i]) {
-                ans = max(ans, solve(boxes, i+1, k-1, 0) + solve(boxes, k, j, cnt+1));
-            }
+    int removeBoxes(vector<int> &boxes)
+    {
+        int n = boxes.size();
+
+        vector<pair<int, int>> groups;
+        for (int i = 0; i < n; i++)
+        {
+            int j = i;
+            while (i + 1 < n and boxes[i + 1] == boxes[j])
+                i++;
+            groups.push_back({boxes[j], i - j + 1});
         }
-        return dp[i0][j][cnt0] = ans;
+
+        memset(dp, -1, sizeof(dp));
+        return solve(0, groups.size() - 1, 0, groups);
     }
 };
