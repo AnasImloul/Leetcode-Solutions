8b1eb0adb8465d4249733dfed3125ab5b3cb61ed
AnasImloul <pydev27@gmail.com>
2023-09-09 17:08:43 +0100

    Runtime: 124 ms (Top 40.7%) | Memory: 50.74 MB (Top 23.6%)

--- scripts/algorithms/M/Minimum Difference Between Largest and Smallest Value in Three Moves/Minimum Difference Between Largest and Smallest Value in Three Moves.js
+++ scripts/algorithms/M/Minimum Difference Between Largest and Smallest Value in Three Moves/Minimum Difference Between Largest and Smallest Value in Three Moves.js
@@ -1,23 +1,22 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// Runtime: 124 ms (Top 40.7%) | Memory: 50.74 MB (Top 23.6%)
+
 var minDifference = function(nums) {
-    if (nums.length <= 4) return 0
+    let len = nums.length;
+    if (len < 5) return 0;
+
     nums.sort((a,b) => a-b)
     
-    let i = 0;
-    let j = nums.length - 4
-    let min = Infinity
-    
-    while(i <= 3){
-        min = Math.min(nums[j] - nums[i], min)
-        i++
-        j++
-    }
-    
-    
-    return min
-    
-};
+    return Math.min(
+        
+    ( nums[len-1] - nums[3] ),  // 3 elements removed from start 0 from end
+    ( nums[len-4] - nums[0] ),  // 3 elements removed from end 0 from start
+    ( nums[len-2] - nums[2] ),  // 2 elements removed from start 1 from end
+    ( nums[len-3] - nums[1] ),  // 2 elements removed from end 1 from start
+   
 
+)
+    
+    
+    
+      
+};
