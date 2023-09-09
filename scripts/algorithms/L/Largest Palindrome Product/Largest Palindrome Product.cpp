475f254ea0a3286e9b1496f0c169cac1721fdead
AnasImloul <pydev27@gmail.com>
2023-09-09 18:08:00 +0100

    Runtime: 335 ms (Top 8.2%) | Memory: 7.03 MB (Top 6.6%)

--- scripts/algorithms/L/Largest Palindrome Product/Largest Palindrome Product.cpp
+++ scripts/algorithms/L/Largest Palindrome Product/Largest Palindrome Product.cpp
@@ -1,42 +1,31 @@
+// Runtime: 335 ms (Top 8.2%) | Memory: 7.03 MB (Top 6.6%)
+
 class Solution {
 public:
-    int flip(int n){
-        // given integer abc returns intger cba by going to a temporary string
-        auto str = to_string(n);
-        std::reverse(str.begin(), str.end());
-        return atoi(str.c_str());
-    }
-       
-    bool isInteger(double v){
-        // checks if the double v represents an integer value
-        double tmp;
-        return std::modf(v, &tmp) == 0.0;
-        // alternative less robust implementation
-        // return  static_cast<double>(static_cast<long>(v)) == v;
-    }
-    
     int largestPalindrome(int n) {
-        
-        if (n == 1) return 9;
-        
-        const long max = pow(10, n);
-        
-        for (int z = 2; z < max -1; z++){
-            const long left = max -z;
-            const long right = flip(left);
-            const double sqrt_term = z*z - 4*right;
-           
-		    // let's avoid generating nans applying the square root to negative numbers
-            if (sqrt_term < 0.0){ continue; }
-            
-            const double root1 = 0.5*(z + sqrt(sqrt_term));
-            const double root2 = 0.5*(z - sqrt(sqrt_term));
-            
-            if (isInteger(root1) || isInteger(root2)){
-                return (max*left + right) % 1337;
-            }
+        if(n==1)
+        {
+            return 9;
         }
-		// This should never be reached so we don't really care about what we return in here
-        return -1;
+        int hi=pow(10,n)-1;
+        int lo=pow(10,n-1);
+        int kk=1337;
+        for(int i=hi;i>=lo;i--)
+        {
+            string s=to_string(i);
+            string k=s;
+            reverse(k.begin(),k.end());
+            s+=k;
+            long long int ll=stol(s);
+            for(int j=hi;j>=sqrtl(ll);j--)
+            {
+                if(ll%j==0)
+                {
+                    return ll%kk;
+                }
+            }
+            
+        }
+        return 0;
     }
 };
