9c31a490459d8c3a5ebd2e7481bdd041e0997cb1
AnasImloul <pydev27@gmail.com>
2023-09-09 16:53:27 +0100

    Runtime: 81 ms (Top 52.6%) | Memory: 48.75 MB (Top 19.3%)

--- scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.js
+++ scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.js
@@ -1,18 +1,32 @@
+// Runtime: 81 ms (Top 52.6%) | Memory: 48.75 MB (Top 19.3%)
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
 var addOneRow = function(root, val, depth) {
-    if(depth == 1) {
-        return new TreeNode(val, root);
-    }
-    
-    const addRowHelper = (n = root, d = 1) => {
-        if(!n) return;
-        if(d + 1 == depth) {
-            n.left = new TreeNode(val, n.left, null);
-            n.right = new TreeNode(val, null, n.right);
-            return;
-        }
-        addRowHelper(n.left, d + 1);
-        addRowHelper(n.right, d + 1);
-    } 
-    addRowHelper();
-    return root;
+	if (depth === 1) return new TreeNode(val, root);
+
+	const refactor = (node = root, currentDep = 1) => {
+		if (!node) return;
+		if (currentDep === depth - 1) {
+			const { left, right } = node;
+			node.left = new TreeNode(val, left);
+			node.right = new TreeNode(val, null, right);
+		}
+		refactor(node.left, currentDep + 1);
+		refactor(node.right, currentDep + 1);
+	};
+	refactor();
+	return root;
 };
