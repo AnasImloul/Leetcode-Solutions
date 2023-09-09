9fabe8a733bffd0733d3d412735f0ef3e33b6127
AnasImloul <pydev27@gmail.com>
2023-09-09 18:04:29 +0100

    Runtime: 13 ms (Top 41.4%) | Memory: 11.78 MB (Top 90.0%)

--- scripts/algorithms/M/Minimum Moves to Equal Array Elements II/Minimum Moves to Equal Array Elements II.cpp
+++ scripts/algorithms/M/Minimum Moves to Equal Array Elements II/Minimum Moves to Equal Array Elements II.cpp
@@ -1,11 +1,14 @@
+// Runtime: 13 ms (Top 41.4%) | Memory: 11.78 MB (Top 90.0%)
+
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        int sum=0,n=nums.size();
-        sort(nums.begin(),nums.end());
-        int median=nums[n/2];
-        for(int i=0;i<n;i++)
-            sum+=abs(nums[i]-median);     
-    return sum; 
+        int result = 0, length = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < length; i++) {
+            int median = length / 2;
+            result += abs(nums[i] - nums[median]);
+        }
+        return result;
     }
 };
