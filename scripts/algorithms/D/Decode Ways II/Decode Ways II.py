f6fc7ecdc4ed5724ef2e573a7a2c15b68a277797
AnasImloul <pydev27@gmail.com>
2023-09-09 18:09:29 +0100

    Runtime: 180 ms (Top 97.4%) | Memory: 21.20 MB (Top 53.7%)

--- scripts/algorithms/D/Decode Ways II/Decode Ways II.py
+++ scripts/algorithms/D/Decode Ways II/Decode Ways II.py
@@ -1,48 +1,18 @@
+# Runtime: 180 ms (Top 97.4%) | Memory: 21.20 MB (Top 53.7%)
+
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        non_zero = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        first_incl, second_incl = 1, 0
-        first_excl, second_excl = 0, 0
+    def numDecodings(self, s: str):
+        if s[0] == '0': return 0
+        cMap = {'0':0, '*': 9, '**': 15, '1*': 9, '2*': 6} #{char(s) : multiplier} mapping
+        for i in range(1, 27): cMap[str(i)] = 1
+        for i in range(0, 7): cMap['*'+str(i)] = 2
+        for i in range(7, 10): cMap['*'+str(i)] = 1
         
-        if s[0] in non_zero:
-            second_incl = 1
-        if s[0] == '*':
-            second_incl = 9
-            
+        dp = [0]*(len(s)+1)
+        dp[0], dp[-1] = cMap[s[0]] , 1
+        
         for i in range(1, len(s)):
-            new_incl, new_excl = 0, 0
-            if s[i] == '*':
-                new_incl = 9 * (second_incl + second_excl)
-                
-                if s[i-1] == '1':
-                    # number is of type (1, *)
-                    new_excl = 9 * (first_incl + first_excl)
-                    
-                elif s[i-1] == '2':
-                    # number is of type (2, *)
-                    new_excl = 6 * (first_incl + first_excl)
-                    
-                elif s[i-1] == '*':
-                    # number is of type (*, *)
-                    new_excl = 15 * (first_incl + first_excl)
-            else:
-                if s[i] in non_zero:
-                    new_incl = second_incl + second_excl
-                        
-                if s[i-1] == '*':
-                    # number is of type (*,digit)
-                    if int(s[i]) <= 6:
-                        new_excl = 2 * (first_excl + first_incl)
-                    else:
-                        new_excl = first_incl + first_excl
-                        
-                else:
-                    # number is of type (digit,digit)
-                    val = int(s[i-1:i+1])
-                    if 10 <= val <= 26:
-                        new_excl = first_incl + first_excl
-                    else:
-                        new_excl = 0
-            first_incl, first_excl = second_incl, second_excl
-            second_incl, second_excl = new_incl, new_excl
-        return (second_incl + second_excl) % (10**9 + 7)
+            dp[i] += (cMap[s[i]]*dp[i-1] + cMap.get(s[i-1:i+1],0)*dp[i-2])%(10**9 + 7)
+            if not dp[i]: return 0
+            
+        return dp[-2]
