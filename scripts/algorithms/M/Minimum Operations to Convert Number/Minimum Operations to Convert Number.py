6cb2198b188c02beb7aa4d96776adce1b3602379
AnasImloul <pydev27@gmail.com>
2023-09-09 16:45:26 +0100

    Runtime: 4131 ms (Top 24.3%) | Memory: 185.60 MB (Top 35.5%)

--- scripts/algorithms/M/Minimum Operations to Convert Number/Minimum Operations to Convert Number.py
+++ scripts/algorithms/M/Minimum Operations to Convert Number/Minimum Operations to Convert Number.py
@@ -1,18 +1,19 @@
+# Runtime: 4131 ms (Top 24.3%) | Memory: 185.60 MB (Top 35.5%)
+
 class Solution:
-def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
-    if start==goal:
-        return 0
-    
-    q = [(start,0)]
-    seen = {start}
-    while q:
-        n,s = q.pop(0)
-        for num in nums:
-            for cand in [n+num,n-num,n^num]:
-                if cand==goal:
-                    return s+1
-                if 0<=cand<=1000 and cand not in seen:
-                    seen.add(cand)
-                    q.append((cand,s+1))
-    
-    return -1
+    def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
+        ans = 0
+        seen = {start}
+        queue = deque([start])
+        while queue: 
+            for _ in range(len(queue)): 
+                val = queue.popleft()
+                if val == goal: return ans 
+                if 0 <= val <= 1000: 
+                    for x in nums: 
+                        for op in (add, sub, xor): 
+                            if op(val, x) not in seen: 
+                                seen.add(op(val, x))
+                                queue.append(op(val, x))
+            ans += 1
+        return -1 
