2b5825d05952be31c381513add542fd46b23e3f8
AnasImloul <pydev27@gmail.com>
2023-09-09 18:16:50 +0100

    Runtime: 90 ms (Top 49.1%) | Memory: 16.67 MB (Top 9.2%)

--- scripts/algorithms/M/Most Frequent Number Following Key In an Array/Most Frequent Number Following Key In an Array.py
+++ scripts/algorithms/M/Most Frequent Number Following Key In an Array/Most Frequent Number Following Key In an Array.py
@@ -1,10 +1,6 @@
+# Runtime: 90 ms (Top 49.1%) | Memory: 16.67 MB (Top 9.2%)
+
 class Solution:
-    def mostFrequent(self, nums, key):
-        counts = {}
-        
-        for i in range(1,len(nums)):
-            if nums[i-1]==key:
-                if nums[i] not in counts: counts[nums[i]] = 1
-                else: counts[nums[i]] += 1
-        
-        return max(counts, key=counts.get)
+    def mostFrequent(self, nums: List[int], key: int) -> int:
+        l = [t for k,t in zip(nums, nums[1:]) if k == key]
+        return max(set(l), key = l.count)
