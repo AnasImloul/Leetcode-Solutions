b099a9818ee3337aed73afd31cb7e1174b223198
AnasImloul <pydev27@gmail.com>
2023-09-09 18:18:51 +0100

    Runtime: 45 ms (Top 36.3%) | Memory: 16.20 MB (Top 88.7%)

--- scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.py
+++ scripts/algorithms/V/Valid Perfect Square/Valid Perfect Square.py
@@ -1,13 +1,18 @@
+# Runtime: 45 ms (Top 36.3%) | Memory: 16.20 MB (Top 88.7%)
+
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        beg, end = 0, 65535
-        
-        while beg <= end:
-            mid = (beg+end)//2
-            if mid*mid == num:
+        if num == 1:
+            return True
+        lo = 2
+        hi = num // 2
+        while lo <= hi:
+            mid = lo + (hi - lo) //2
+            print(mid)
+            if mid * mid == num:
                 return True
-            elif mid*mid < num:
-                beg =  mid+1
+            if mid * mid > num:
+                hi = mid - 1
             else:
-                end = mid-1
+                lo = mid + 1
         return False
