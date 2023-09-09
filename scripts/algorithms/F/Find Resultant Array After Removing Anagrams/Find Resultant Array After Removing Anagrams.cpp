509690b8d260ab73564ac3620e2768762722941b
AnasImloul <pydev27@gmail.com>
2023-09-09 17:42:38 +0100

    Runtime: 15 ms (Top 33.9%) | Memory: 12.42 MB (Top 38.6%)

--- scripts/algorithms/F/Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.cpp
+++ scripts/algorithms/F/Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.cpp
@@ -1,16 +1,16 @@
+// Runtime: 15 ms (Top 33.9%) | Memory: 12.42 MB (Top 38.6%)
+
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        string a, b;
-        for(int i=1; i<words.size(); i++) {
-            a = words[i];
-            b = words[i-1];
-            sort(a.begin(), a.end());
-            sort(b.begin(), b.end());
-            if(a==b) {
-                words.erase(words.begin()+i);
+        for(int i = 1;i<words.size();i++){
+            string x = words[i];
+            sort(x.begin(),x.end());
+            string y = words[i-1];
+            sort(y.begin(),y.end());
+            if(x == y){
+                words.erase(words.begin() + i);
                 i--;
-                continue;
             }
         }
         return words;
