09bb1ef50e3aa0c6b02cee33550b8c95da63ca3f
AnasImloul <pydev27@gmail.com>
2023-09-09 17:03:54 +0100

    Runtime: 91 ms (Top 28.5%) | Memory: 71.62 MB (Top 8.5%)

--- scripts/algorithms/P/Palindromic Substrings/Palindromic Substrings.js
+++ scripts/algorithms/P/Palindromic Substrings/Palindromic Substrings.js
@@ -1,21 +1,31 @@
-int n = s.length();
-        int count = 0;
-        for(int i = 0;i<n;i++){
-            int l = i;
-            int r = l;
-            while(l>=0 && r<n && s[l]==s[r]){
-                count++;
-                l--;
-                r++;
-            }
+// Runtime: 91 ms (Top 28.5%) | Memory: 71.62 MB (Top 8.5%)
+
+var countSubstrings = function(s) {
+    const n = s.length;
+    const dp = [...Array(n)].map(() => Array(n).fill(false));
+    let count = 0;
+    
+    // Base case: single letter substrings
+    for(let i = 0; i < n; i++) {
+        dp[i][i] = true;
+        count++
+    }
+    
+    // Base case: double letter substrings
+    for(let i = 0; i < n-1; i++) {
+        dp[i][i+1] = (s[i] === s[i+1]);
+        dp[i][i+1] && count++;
+    }
+    
+    // substrings longer than 2 chars
+    for(let len = 3; len <= n; len++) {
+        let start = 0, end = start+len-1;
+        
+        while(end < n) {
+            dp[start][end] = (dp[start+1][end-1] && s[start] === s[end]);
+            dp[start][end] && count++;
+            start++; end++;
         }
-        for(int i = 0;i<n-1;i++){
-            int l =  i;
-            int r = l+1;
-            while(l>=0 && r<n && s[l]==s[r]){
-                count++;
-                l--;
-                r++;
-            }
-        }
-        return count;
+    }
+    return count;
+};
