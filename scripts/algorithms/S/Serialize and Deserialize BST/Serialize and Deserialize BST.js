cf42538d5cb5742dfccd4de39268d42db202efbd
AnasImloul <pydev27@gmail.com>
2023-09-09 17:25:43 +0100

    Runtime: 69 ms (Top 97.8%) | Memory: 50.68 MB (Top 26.8%)

--- scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.js
+++ scripts/algorithms/S/Serialize and Deserialize BST/Serialize and Deserialize BST.js
@@ -1,4 +1,6 @@
-/**
+// Runtime: 69 ms (Top 97.8%) | Memory: 50.68 MB (Top 26.8%)
+
+;/**
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -13,19 +15,28 @@
  * @return {string}
  */
 var serialize = function(root) {
-    if(!root) return "";
-    let result = "";
-    function DFS(root) {
-        result = result ? result + ',' + root.val : result + root.val;
-        if(root.left) {
-            DFS(root.left);
-        }
-        if(root.right) {
-            DFS(root.right);
-        }
+    // Using Preorder traversal to create a string of BST
+    // Preorder works in following way
+    // root -> left -> right
+    let preorder = [];
+    
+    function dfs(node) {
+        if (node === null) return;
+        // Get root value
+        preorder.push(node.val);
+
+        // Get All the Left values
+        dfs(node.left);
+
+        // Get all the right values
+        dfs(node.right);
     }
-    DFS(root);
-    return result;
+
+    // call it with root
+    dfs(root)
+
+    // Turn into string and return it
+    return preorder.join(',')
 };
 
 /**
@@ -35,22 +46,33 @@ var serialize = function(root) {
  * @return {TreeNode}
  */
 var deserialize = function(data) {
-    
-    if(data === '') return null;
-    preorder = data.split(',');
-    let i = 0;
-    function constructTree(lower, upper) {
-        if(i === preorder.length) return null;
-        if(Number(preorder[i]) > upper) return null;
-        if(Number(preorder[i]) < lower) return null;
+    if (data === "") return null;
 
-        let root = new TreeNode(preorder[i]);
-        i++;
-        root.left = constructTree(lower, root.val);
-        root.right = constructTree(root.val, upper);
+    // Get numbers array from a string
+    const preorder = data.split(',').map(Number);
+
+    // using -Infinity and +Infinity as placeholder check
+    function recur(lower = -Infinity, upper = Infinity) {
+        // This condition useful for when we are filling left side of tree it'll avoid all the values greater then then its upper value by putting null init.
+        if (preorder[0] < lower || preorder[0] > upper) return null;
+
+        // If preorder become empty
+        if (preorder.length === 0) return null;
+
+        // Create a root node [shift method will change the original array]
+        const root = new TreeNode(preorder.shift());
+
+        // Here for left side of tree, we are using current root node's value as 'upper bound' (so higher values ignored).
+        root.left = recur(lower, root.val);
+
+        // Same as above for right side we are using root node's values as 'lower bound' (so lower values ignored);
+        root.right = recur(root.val, upper);
+
         return root;
     }
-    return constructTree(-Infinity, Infinity);
+
+    // Final root will be out BST
+    return recur();
 };
 
 /**
