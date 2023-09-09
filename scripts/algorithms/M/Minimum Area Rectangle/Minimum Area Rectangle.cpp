10450c06b6a6f17b0fdd606641dce4634d30ab5e
AnasImloul <pydev27@gmail.com>
2023-09-09 18:05:47 +0100

    Runtime: 2865 ms (Top 5.1%) | Memory: 612.86 MB (Top 5.1%)

--- scripts/algorithms/M/Minimum Area Rectangle/Minimum Area Rectangle.cpp
+++ scripts/algorithms/M/Minimum Area Rectangle/Minimum Area Rectangle.cpp
@@ -1,59 +1,31 @@
-/*
-    https://leetcode.com/problems/minimum-area-rectangle/
-    
-    TC: O(n^2)
-    SC: O(n)
-    
-    The major challenge is to find 4 points that form a rectangle. 
-    Brute force approach is to have 4 loops each controlling a coordinate.
-    
-    To make this faster, we use hashing to store the y coordinates for each of the x coordinate.
-    Using 2 loops, fixing one coordinate each, we treat them as diagonals and check for the existence
-    of other two pts. If we find the other two pts, then compare the area and proceed.
-*/
+// Runtime: 2865 ms (Top 5.1%) | Memory: 612.86 MB (Top 5.1%)
+
 class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
-        // We need to find out 4 points that form a rectangle
-        // We can fix two points, but need an efficient way to confirm the other two
+        unordered_map<int, unordered_set<int>> pts;
+        for(auto p : points)
+        {
+            pts[p[0]].insert(p[1]);
+        }
         
-        // Store all the vertical pts for each of the x coordinates
-        // <x coordinate, Set(y coordinate)>
-        unordered_map<int, unordered_set<int> > vertical_pts;
-        for(auto point: points)
-            vertical_pts[point[0]].emplace(point[1]);
-        
-        int min_area = INT_MAX;
-        
-        // We need pt1 and pt2 to be diagonal points
-        for(int pt1 = 0; pt1 < points.size(); pt1++)
-            for(int pt2 = pt1 + 1; pt2 < points.size(); pt2++) {
-                int x1 = points[pt1][0], y1 = points[pt1][1];
-                int x2 = points[pt2][0], y2 = points[pt2][1];
-                
-                // check if the two points are either on the same horizontal or vertical line
-                if(x1 == x2 || y1 == y2)
-                    continue;
-                
-                // Once we know that the two points are not on the same vertical or horizontal line,
-                // We can treat them as diagonals. Now we check for the other two points.
-                /*
-                    Let the diagonal pts be C (pt1) and D (pt2)
-                    A-----B
-                    |     |
-                    C-----D
-                    
-                    Check if A and D exists. 
-                    NOTE: Position of C and B might be reversed or at A and D position as well.
-                    But below code will work since it checks for the other pts by creating the data from these diagonal pts
-                */
-                
-                // A and D exists
-                if(vertical_pts[x1].count(y2) && vertical_pts[x2].count(y1)) {
-                    min_area = min(min_area, (abs(x2 - x1) * abs(y2 - y1)));
+        int minArea = INT_MAX;
+        int n = points.size();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                auto p1 = points[i];
+                auto p2 = points[j];
+                if(pts[p1[0]].size()<2 || pts[p2[0]].size()<2) continue; // added to avoid extra loop
+                if(p1[0]!=p2[0] && p1[1]!=p2[1]){
+                    if(pts[p1[0]].count(p2[1])>0 && pts[p2[0]].count(p1[1])>0) {
+                        minArea = min(minArea, (abs(p1[0]-p2[0]) * abs(p1[1]-p2[1])) );
+                    }
                 }
+                
             }
-        
-        return min_area == INT_MAX ? 0 : min_area;
+        }
+        return minArea == INT_MAX ? 0 : minArea;
     }
 };
