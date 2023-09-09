d28d5b40dae8537a63d4b7e8c3d157059315f82e
AnasImloul <pydev27@gmail.com>
2023-09-09 18:22:01 +0100

    Runtime: 52 ms (Top 33.3%) | Memory: 16.43 MB (Top 7.2%)

--- scripts/algorithms/C/Cells in a Range on an Excel Sheet/Cells in a Range on an Excel Sheet.py
+++ scripts/algorithms/C/Cells in a Range on an Excel Sheet/Cells in a Range on an Excel Sheet.py
@@ -1,3 +1,14 @@
+# Runtime: 52 ms (Top 33.3%) | Memory: 16.43 MB (Top 7.2%)
+
 class Solution:
     def cellsInRange(self, s: str) -> List[str]:
-        return [chr(c) + chr(r) for c in range(ord(s[0]), ord(s[3]) + 1) for r in range(ord(s[1]), ord(s[4]) + 1)]
+        start, end = s.split(':')
+        start_letter, start_num = start[0], int(start[-1])
+        end_letter, end_num = end[0], int(end[1])
+        alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        alphabet_slice = \
+            alphabet[alphabet.index(start_letter):alphabet.index(end_letter) + 1]
+        res = list()
+        for el in alphabet_slice:
+            res += [el + str(num) for num in range(start_num, end_num + 1)]
+        return res
