195dfaa2e0df70b15258931cc02131cb62756d4c
AnasImloul <pydev27@gmail.com>
2023-09-09 18:01:15 +0100

    Runtime: 11 ms (Top 71.1%) | Memory: 13.70 MB (Top 5.9%)

--- scripts/algorithms/S/Set Matrix Zeroes/Set Matrix Zeroes.cpp
+++ scripts/algorithms/S/Set Matrix Zeroes/Set Matrix Zeroes.cpp
@@ -1,24 +1,28 @@
+// Runtime: 11 ms (Top 71.1%) | Memory: 13.70 MB (Top 5.9%)
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int> r,c;
-        int i,j,m = matrix.size(),n = matrix[0].size();
-        for(i = 0; i < m; i++){
-            for(j = 0; j < n; j++){
-                if(!matrix[i][j]){
-                    r.insert(i);
-                    c.insert(j);
+        unordered_map<int,int>ump;
+        unordered_map<int,int>mp;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    ump[i]=1;
+                    mp[j]=1;
                 }
             }
         }
-        for(auto &i: r){
-            for(j = 0; j < n; j++){
-                matrix[i][j] = 0;
-            }
-        }for(auto &i: c){
-            for(j = 0; j < m; j++){
-                matrix[j][i] = 0;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(ump[i] || mp[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
+
+
+        
+        
     }
 };
