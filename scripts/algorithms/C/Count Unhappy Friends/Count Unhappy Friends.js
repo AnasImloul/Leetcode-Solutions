785248c0cd8662e8a85e1abab0d28d8efef561ea
AnasImloul <pydev27@gmail.com>
2023-09-09 17:18:50 +0100

    Runtime: 60 ms (Top 83.3%) | Memory: 46.06 MB (Top 50.0%)

--- scripts/algorithms/C/Count Unhappy Friends/Count Unhappy Friends.js
+++ scripts/algorithms/C/Count Unhappy Friends/Count Unhappy Friends.js
@@ -1,28 +1,22 @@
-// Runtime: 94 ms (Top 73.77%) | Memory: 52 MB (Top 24.59%)
+// Runtime: 60 ms (Top 83.3%) | Memory: 46.06 MB (Top 50.0%)
+
 var unhappyFriends = function(n, preferences, pairs) {
-    let assigned = {}
-    let count = 0
-
-    for(let pair of pairs){
-        let [x, y] = pair
-        assigned[x] = y
-        assigned[y] = x
+    let happyMap = new Array(n);
+    for (let [i, j] of pairs) {
+        happyMap[i] = preferences[i].indexOf(j);
+        happyMap[j] = preferences[j].indexOf(i);
     }
-
-    for(let x=0; x<n; x++){
-        let xLimit = preferences[x].indexOf(assigned[x.toString()])
-        let xPrefs = preferences[x].slice(0, xLimit)
-
-        for(let u of xPrefs){
-            let uLimit = preferences[u].indexOf(assigned[u.toString()])
-            let uPrefs = preferences[u].slice(0, uLimit)
-
-            if(uPrefs.includes(x)){
-                count += 1
-                break
+    
+    let unhappy = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < happyMap[i]; j++) {
+            let partner = preferences[i][j];
+            if (preferences[partner].indexOf(i) < happyMap[partner]) {
+                unhappy++;
+                break;
             }
         }
     }
-
-    return count
+    
+    return unhappy;
 };
