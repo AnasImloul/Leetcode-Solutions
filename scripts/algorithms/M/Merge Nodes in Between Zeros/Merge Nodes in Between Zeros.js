47e65b68c87f3e1c215b4db3df61efd6970fcb6a
AnasImloul <pydev27@gmail.com>
2023-09-09 17:02:27 +0100

    Runtime: 405 ms (Top 56.3%) | Memory: 151.24 MB (Top 25.2%)

--- scripts/algorithms/M/Merge Nodes in Between Zeros/Merge Nodes in Between Zeros.js
+++ scripts/algorithms/M/Merge Nodes in Between Zeros/Merge Nodes in Between Zeros.js
@@ -1,22 +1,21 @@
+// Runtime: 405 ms (Top 56.3%) | Memory: 151.24 MB (Top 25.2%)
+
 var mergeNodes = function(head) {
-    let count = 0;
-    
-    let node = head; // start on first zero
-    
-    while(node.next) {
-        node.val += node.next.val; // add current value to node
-        node.next = node.next.next; // remove value between zeroes
-        
-        // remove last zero
-        if(!node.next.next) {
-            node.next = null;
-            break;
-        }
-        // traverse node to next 0
-        if(node.next.val === 0) {
-            node = node.next;
-        }
+    var res = new ListNode()
+    var ans = res ;
+    var current = head.next;
+    var sum = 0;
+    while(current){
+              
+            if(current.val != 0){
+                sum = sum + current.val;
+            }else{
+                res.next = new ListNode(sum);
+                sum = 0;
+                res = res.next;
+            }
+        current = current.next;
+            
     }
-    
-    return head;
-};```
+    return ans.next;
+};
