016cc2457a6aedeee84504816ca4c2ec9b5f1e3b
AnasImloul <pydev27@gmail.com>
2023-09-09 20:21:25 +0100

    Runtime: 218 ms (Top 90.0%) | Memory: 61.37 MB (Top 60.0%)

--- scripts/algorithms/R/Removing Minimum Number of Magic Beans/Removing Minimum Number of Magic Beans.js
+++ scripts/algorithms/R/Removing Minimum Number of Magic Beans/Removing Minimum Number of Magic Beans.js
@@ -1,20 +1,22 @@
-// Runtime: 419 ms (Top 42.86%) | Memory: 59 MB (Top 60.32%)
+// Runtime: 218 ms (Top 90.0%) | Memory: 61.37 MB (Top 60.0%)
+
 /**
  * @param {number[]} beans
  * @return {number}
  */
-var minimumRemoval = function(beans) {
-    const N = beans.length;
-    const sum = beans.reduce((acc, c) => c + acc, 0);
-
-    beans.sort((a,b) => a - b);
-
-    let ans = Infinity;
-    for(let i = 0 ; i < beans.length; i++) {
-        ans = Math.min(ans, sum - (N - i) * beans[i])
+ // time complexity -> O(NlogN) and Space is O(logN) due to sorting. 
+ var minimumRemoval = function(beans) {
+    beans.sort((a, b) => a - b);
+    let frontSum = beans.reduce((sum , a) => sum + a, 0);
+    let backSum = 0;
+    let done = 0;
+    let result = Number.MAX_SAFE_INTEGER;
+    for(let j = beans.length - 1; j >= 0; j--){
+        frontSum -= beans[j];
+        count = frontSum + (backSum - (beans[j] * done));
+        result = Math.min(result, count);
+        done++;
+        backSum += beans[j];
     }
-
-    return ans;
+    return result;
 };
-// TC: O(NlogN) for sorting
-// SC: O(1)
