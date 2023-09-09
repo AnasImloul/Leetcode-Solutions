8ce40bcda342ce86dcce581a5fe59828c1053175
AnasImloul <pydev27@gmail.com>
2023-09-09 17:43:48 +0100

    Runtime: 16 ms (Top 39.9%) | Memory: 7.79 MB (Top 70.0%)

--- scripts/algorithms/R/Roman to Integer/Roman to Integer.cpp
+++ scripts/algorithms/R/Roman to Integer/Roman to Integer.cpp
@@ -1,107 +1,20 @@
+// Runtime: 16 ms (Top 39.9%) | Memory: 7.79 MB (Top 70.0%)
+
 class Solution {
 public:
-    int romanToInt(string s) 
-    {
-
-    int count=0,ans=0;
-        
-    for(int i=0; s[i]!='\0';i++)
-    {
-        count++;
-    }
-        
-    int s1[count+1];
-        
-    for(int i=0; i<count; i++)
-        {
-            if(s[i]=='M')
-            s1[i]=1000;
-            else if(s[i]=='V')
-            s1[i]=5;
-            else if(s[i]=='X')
-            s1[i]=10;
-            else if(s[i]=='L')
-            s1[i]=50;
-            else if(s[i]=='C')
-            s1[i]=100;
-            else if(s[i]=='D')
-            s1[i]=500;
-            else 
-            s1[i]=1;
-        }  
-        
-    s1[count]=0;
-
-    for(int j=0; j<=count; j++)
-        {
-            if (j!=count)
-            {          
-                 if(s1[j]>=s1[j+1])
-                {
-                    switch(s1[j])
-                    {
-                      case 1: ans = ans+1;
-                      break;
-                      case 5: ans = ans+5;
-                      break;
-                      case 10: ans = ans+10;
-                      break;
-                      case 50: ans = ans+50;
-                      break;
-                      case 100: ans = ans+100;
-                      break;
-                      case 500: ans = ans+500;
-                      break;
-                      case 1000: ans = ans+1000;
-                      break;
-                    }        
-                }      
-
-                 else if(s1[j]<s1[j+1])
-                {
-                    switch(s1[j])
-                    {
-                    case 1: ans = ans-1;
-                    break;
-                    case 5: ans = ans-5;
-                    break;
-                    case 10: ans = ans-10;
-                    break;
-                    case 50: ans = ans-50;
-                    break;
-                    case 100: ans = ans-100;
-                    break;
-                    case 500: ans = ans-500;
-                    break;
-                    case 1000: ans = ans-1000;
-                    break;
-                    }           
-                }
-            }
-
-            else if(j==count)
-            {
-                switch(s1[j])
-                {
-                  case 1: ans = ans+1;
-                  break;
-                  case 5: ans = ans+5;
-                  break;
-                  case 10: ans = ans+10;
-                  break;
-                  case 50: ans = ans+50;
-                  break;
-                  case 100: ans = ans+100;
-                  break;
-                  case 500: ans = ans+500;
-                  break;
-                  case 1000: ans = ans+1000;
-                  break;
-                }
-            }
+    int romanToInt(string s){
+        unordered_map<char, int> T = { { 'I' , 1 },
+        { 'V' , 5 },
+        { 'X' , 10 },
+        { 'L' , 50 },
+        { 'C' , 100 },
+        { 'D' , 500 },
+        { 'M' , 1000 } };
+        int sum = T[s.back()];
+        for (int i = s.length() - 2; i >= 0; --i){
+            if (T[s[i]] < T[s[i + 1]]) sum -= T[s[i]];
+            else sum += T[s[i]];
         }
-
-    return ans;
-        
+        return sum;
     }
 };
