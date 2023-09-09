880f5e239b7ddd28f4268364a807c3db39ba5935
AnasImloul <pydev27@gmail.com>
2023-09-09 17:23:46 +0100

    Runtime: 51 ms (Top 67.8%) | Memory: 41.86 MB (Top 66.1%)

--- scripts/algorithms/T/Truncate Sentence/Truncate Sentence.js
+++ scripts/algorithms/T/Truncate Sentence/Truncate Sentence.js
@@ -1,3 +1,10 @@
+// Runtime: 51 ms (Top 67.8%) | Memory: 41.86 MB (Top 66.1%)
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
 var truncateSentence = function(s, k) {
-    return s.split(' ').splice(0, k).join(' ')
+   return s.split(" ").slice(0,k).join(" ")
 };
