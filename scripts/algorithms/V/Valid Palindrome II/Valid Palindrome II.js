4dbf22d4db823687dc633565830193e4fda8e7f5
AnasImloul <pydev27@gmail.com>
2023-09-09 17:10:45 +0100

    Runtime: 77 ms (Top 37.5%) | Memory: 48.02 MB (Top 54.6%)

--- scripts/algorithms/V/Valid Palindrome II/Valid Palindrome II.js
+++ scripts/algorithms/V/Valid Palindrome II/Valid Palindrome II.js
@@ -1,25 +1,30 @@
-var validPalindrome = function(s, count) {
-    let cursorStart = 0;
-    let cursorEnd = s.length - 1;
-    let countRemove = count || 0;
-    
-    while (cursorStart < cursorEnd) {
-        if (s[cursorStart] === s[cursorEnd]) {
-            cursorStart++;
-            cursorEnd--;
-        } else if (s[cursorStart] === s[cursorEnd - 1] && s[cursorStart + 1] === s[cursorEnd]) {
-            return validPalindrome(s.slice(cursorStart + 1, cursorEnd + 1), countRemove + 1) || validPalindrome(s.slice(cursorStart, cursorEnd), countRemove + 1) ;
-        } else if (s[cursorStart] === s[cursorEnd - 1]) {
-            cursorEnd--;
-            countRemove++;
-        } else if (s[cursorStart + 1] === s[cursorEnd]) {
-            cursorStart++;
-            countRemove++;
-        } else {
-            return false;
+// Runtime: 77 ms (Top 37.5%) | Memory: 48.02 MB (Top 54.6%)
+
+/*
+Solution:
+
+1. Use two pointers, one initialised to 0 and the other initialised to end of string. Check if characters at each index
+are the same. If they are the same, shrink both pointers. Else, we have two possibilities: one that neglects character
+at left pointer and the other that neglects character at right pointer. Hence, we check if s[low+1...right] is a palindrome
+or s[low...right-1] is a palindrome. If one of them is a palindrome, we know that we can form a palindrome with one deletion and return true. Else, we require more than one deletion, and hence we return false.
+*/
+var validPalindrome = function(s) {
+    let low = 0, high = s.length-1;
+    while (low < high) {
+        if (s[low] !== s[high]) {
+            return isPalindrome(s, low+1, high) || isPalindrome(s, low, high-1);
         }
-        if (countRemove > 1) return false;
+        low++, high--;
     }
-    
     return true;
+    // T.C: O(N)
+    // S.C: O(1)
 };
+
+function isPalindrome(str, low, high) {
+    while (low < high) {
+        if (str[low] !== str[high]) return false;
+        low++, high--;
+    }
+    return true;
+}
