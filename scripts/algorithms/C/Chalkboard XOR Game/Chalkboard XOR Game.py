21621855fecdceccbf605810af58007d6708bb43
AnasImloul <pydev27@gmail.com>
2023-09-09 18:20:59 +0100

    Runtime: 84 ms (Top 36.7%) | Memory: 16.24 MB (Top 100.0%)

--- scripts/algorithms/C/Chalkboard XOR Game/Chalkboard XOR Game.py
+++ scripts/algorithms/C/Chalkboard XOR Game/Chalkboard XOR Game.py
@@ -1,3 +1,14 @@
+# Runtime: 84 ms (Top 36.7%) | Memory: 16.24 MB (Top 100.0%)
+
+
+
+
+
+
+
+
+
+
 class Solution:
-    def xorGame(self, nums: List[int]) -> bool:
-        return not reduce(lambda a,b: a^b, nums) or not len(nums)%2
+  def xorGame(self, nums: List[int]) -> bool:
+    return functools.reduce(operator.xor, nums) == 0 or len(nums) % 2 == 0
