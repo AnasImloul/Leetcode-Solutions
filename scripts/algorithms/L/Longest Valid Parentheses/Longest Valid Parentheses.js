e5c2d353c85b93072bfeca304a05d56fd972efca
AnasImloul <pydev27@gmail.com>
2023-09-09 17:24:56 +0100

    Runtime: 60 ms (Top 57.3%) | Memory: 44.98 MB (Top 23.9%)

--- scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.js
+++ scripts/algorithms/L/Longest Valid Parentheses/Longest Valid Parentheses.js
@@ -1,42 +1,24 @@
+// Runtime: 60 ms (Top 57.3%) | Memory: 44.98 MB (Top 23.9%)
+
 var longestValidParentheses = function(s) {
-    let validity = 0, length = 0, max =0;
-    for(let index = 0; index < s.length; index++) { // front loop
-        if(s[index] == ')') {
-            if(validity == 0) {
-                max = max > length ? max : length;
-                length = 0;
+    let indexStack=[-1]
+    let characterStack=[];
+    let maxLength=0;
+    for(let i=0;i<s.length;i++){
+        if(s[i]=='('){
+            indexStack.push(i);
+            characterStack.push(s[i]);
+        }else{
+            if(characterStack[characterStack.length-1]=='('){
+                indexStack.pop();
+                characterStack.pop();
+                if(maxLength<i-indexStack[indexStack.length-1]){
+                    maxLength=i-indexStack[indexStack.length-1];
+                }
+            }else{
+                indexStack.push(i);
             }
-            else {
-                validity--;
-                length += 2;
-            }        
-        }
-        else if(s[index] == '(') {
-            validity++;
-        }
-        if(validity == 0) {
-            max = max > length ? max : length;
         }
     }
-    length = 0;
-    validity = 0;
-    for(let index = s.length-1; index >= 0; index--) { // back loop
-        if(s[index] == '(') {
-            if(validity == 0) {
-                max = max > length ? max : length;
-                length = 0;
-            }
-            else {
-                validity--;
-                length += 2;
-            }        
-        }
-        else if(s[index] == ')') {
-            validity++;
-        }
-        if(validity == 0) {
-            max = max > length ? max : length;
-        }
-    }
-    return max;
+    return maxLength;
 };
