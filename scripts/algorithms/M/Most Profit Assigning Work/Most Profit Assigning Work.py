5c24b34dd2f2563f53066c1c44c592754a467211
AnasImloul <pydev27@gmail.com>
2023-09-09 18:20:05 +0100

    Runtime: 489 ms (Top 13.7%) | Memory: 19.08 MB (Top 98.5%)

--- scripts/algorithms/M/Most Profit Assigning Work/Most Profit Assigning Work.py
+++ scripts/algorithms/M/Most Profit Assigning Work/Most Profit Assigning Work.py
@@ -1,26 +1,48 @@
-from bisect import bisect_right
-from collections import deque
-
+# Runtime: 489 ms (Top 13.7%) | Memory: 19.08 MB (Top 98.5%)
 
 class Solution:
-    def maxProfitAssignment(self, difficulty, profit, worker):
-        L = len(difficulty)
-
-        # create sorted difficulty and corresponding maximum profits
-        diffs = []
-        profs = deque([0])  # reduce profs emtpy judge when using max function
-
-        for diff, prof in sorted((difficulty[i], profit[i]) for i in range(L)):
-            diffs.append(diff)
-            profs.append(max(profs[-1], prof))
-
-        profs.popleft()  # remove helper dummy head
-
-        # binary search for capable work, and add up max possible profits of that work
-        total_profits = 0
-
+    #Time-Complexity: O(n + nlogn + n + mlog(n)) -> O((n+m) *logn)
+    #Space-Complexity: O(n)
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        #Approach: First of all, linearly traverse each and every corresponding index
+        #position of first two input arrays: difficulty and profit to group each
+        #item by 1-d array and put it in separate 2-d array. Then, sort the 2-d array
+        #by increasing difficulty of the job! Then, for each worker, perform binary
+        #search and consistently update the max profit the current worker can work and
+        #earn! Add this value to answer variable, which is cumulative for all workers!
+        #this will be the result returned at the end!
+        arr = []
+        for i in range(len(difficulty)):
+            arr.append([difficulty[i], profit[i]])
+        #sort by difficulty!
+        arr.sort(key = lambda x: x[0])
+        #then, I need to update the maximum profit up to each and every item!
+        maximum = float(-inf)
+        for j in range(len(arr)):
+            maximum = max(maximum, arr[j][1])
+            arr[j][1] = maximum
+        ans = 0
+        #iterate through each and every worker!
         for w in worker:
-            idx = bisect_right(diffs, w) - 1
-            if idx >= 0: total_profits += profs[idx]
-
-        return total_profits
+            bestProfit = 0
+            #define search space to perform binary search!
+            L, R = 0, len(arr) - 1
+            #as long as search space has at least one element to consider or one job,
+            #continue iterations of binary search!
+            while L <= R:
+                mid = (L + R) // 2
+                mid_e = arr[mid]
+                #check if current job has difficulty that is manageable!
+                if(mid_e[0] <= w):
+                    bestProfit = max(bestProfit, mid_e[1])
+                    #we still need to search right and try higher difficulty
+                    #jobs that might yield higher profit!
+                    L = mid + 1
+                    continue
+                else:
+                    R = mid - 1
+                    continue
+            #once we break from while loop and end binary search, we should have
+            #found bestProfit for current worker performing task that is manageable!
+            ans += bestProfit
+        return ans
