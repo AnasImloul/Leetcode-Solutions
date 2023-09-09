d0ed247d5234c57eb53476e0f78fa69481c9e71d
AnasImloul <pydev27@gmail.com>
2023-09-09 17:54:52 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 7.93 MB (Top 70.9%)

--- scripts/algorithms/C/Cells with Odd Values in a Matrix/Cells with Odd Values in a Matrix.cpp
+++ scripts/algorithms/C/Cells with Odd Values in a Matrix/Cells with Odd Values in a Matrix.cpp
@@ -1,18 +1,27 @@
-class Solution {
+// Runtime: 0 ms (Top 100.0%) | Memory: 7.93 MB (Top 70.9%)
+
+static int x = []() {
+std::ios::sync_with_stdio(false);
+cin.tie(nullptr);
+return 0; }();
+
+class Solution { // tc: O(n+m) & sc: O(n+m)
 public:
-    int oddCells(int m, int n, vector<vector<int>>& indices) {
-        vector<int> rows(m, 0);
-        vector<int> cols(n, 0);
-        for (auto idx : indices) {
-            rows[idx[0]]++;
-            cols[idx[1]]++;
+    int oddCells(int n, int m, vector<vector<int>>& indices) {
+        vector<bool> rows(n,false),cols(m,false);
+        for(auto index: indices){
+            rows[index[0]] = rows[index[0]] ^ true;
+            cols[index[1]] = cols[index[1]] ^ true;
         }
-        int counter = 0;
-        for (auto i : rows) {
-            for (auto j : cols) {
-                counter += (i + j) % 2 == 1 ? 1 : 0;
-            }
+        
+        int r(0),c(0);
+        for(int i(0);i<n;i++){
+            if(rows[i]) r++;
         }
-        return counter;
+        
+        for(int i(0);i<m;i++){
+            if(cols[i]) c++;
+        }
+        return r*(m-c) + c*(n-r); // (or) return (r*m + c*n - 2*r*c);
     }
 };
