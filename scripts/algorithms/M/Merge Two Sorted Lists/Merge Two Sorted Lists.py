a4279866b5c79f7f2f329363e434ac336ae1ff46
AnasImloul <pydev27@gmail.com>
2023-09-09 18:10:22 +0100

    Runtime: 43 ms (Top 67.2%) | Memory: 16.18 MB (Top 97.5%)

--- scripts/algorithms/M/Merge Two Sorted Lists/Merge Two Sorted Lists.py
+++ scripts/algorithms/M/Merge Two Sorted Lists/Merge Two Sorted Lists.py
@@ -1,13 +1,17 @@
+# Runtime: 43 ms (Top 67.2%) | Memory: 16.18 MB (Top 97.5%)
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        if list1.val<list2.val:
-            list1.next = self.mergeTwoLists(list1.next,list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list2.next,list1)
-        return list2
+        cur = dummy = ListNode()
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
