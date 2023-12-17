fe88fbd5b459dd5222b55058834b7d9354249605
AnasImloul <pydev27@gmail.com>
2023-12-17 14:27:03 +0100

    Runtime: 50 ms (Top 86.19%) | Memory: 43.00 MB (Top 14.55%)

--- scripts/algorithms/F/Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.js
+++ scripts/algorithms/F/Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.js
@@ -1,10 +1,16 @@
+// Runtime: 50 ms (Top 86.19%) | Memory: 43.00 MB (Top 14.55%)
+
 var sumZero = function(n) {
-    let arr = []
-    let sum = 0
-    for (let i=1; i < n; i++) {
-        arr.push(i)
-        sum = sum + i
-    }
-    arr.push(-sum)
-    return arr
-};
+  var num = Math.floor(n/2); 
+  var res = [];
+
+  for(var i=1;i<=num;i++){
+      res.push(i,-i)
+     } 
+
+  if(n%2!==0){
+    res.push(0)
+  }
+  
+  return res 
+}
