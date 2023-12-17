b2367c80c47a32dbe8abe4fed45af811e531a22c
AnasImloul <pydev27@gmail.com>
2023-12-17 13:19:13 +0100

    Runtime: 34 ms (Top 93.57%) | Memory: 16.20 MB (Top 87.8%)

---
+++ scripts/algorithms/S/String to Integer atoi/String to Integer atoi..py
@@ -0,0 +1,43 @@
+// Runtime: 34 ms (Top 93.57%) | Memory: 16.20 MB (Top 87.8%)
+
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        value, state, pos, sign = 0, 0, 0, 1
+
+        if len(str) == 0:
+            return 0
+
+        while pos < len(str):
+            current_char = str[pos]
+            if state == 0:
+                if current_char == " ":
+                    state = 0
+                elif current_char == "+" or current_char == "-":
+                    state = 1
+                    sign = 1 if current_char == "+" else -1
+                elif current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 1:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    return 0
+            elif state == 2:
+                if current_char.isdigit():
+                    state = 2
+                    value = value * 10 + int(current_char)
+                else:
+                    break
+            else:
+                return 0
+            pos += 1
+
+        value = sign * value
+        value = min(value, 2 ** 31 - 1)
+        value = max(-(2 ** 31), value)
+
+        return value
