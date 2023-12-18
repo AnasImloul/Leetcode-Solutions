78c27020dd01804eab48ca47e9e6c08c4f822f1e
AnasImloul <pydev27@gmail.com>
2023-12-18 17:09:52 +0100

    Runtime: 9 ms (Top 29.13%) | Memory: 9.60 MB (Top 91.68%)

--- scripts/algorithms/F/Find Subsequence of Length K With the Largest Sum/Find Subsequence of Length K With the Largest Sum.cpp
+++ scripts/algorithms/F/Find Subsequence of Length K With the Largest Sum/Find Subsequence of Length K With the Largest Sum.cpp
@@ -1,39 +1,19 @@
-typedef pair<int,int> p;
+// Runtime: 9 ms (Top 29.13%) | Memory: 9.60 MB (Top 91.68%)
+
+// OJ: https://leetcode.com/problems/find-subsequence-of-length-k-with-the-largest-sum/
+// Author: github.com/lzl124631x
+// Time: O(NlogN)
+// Space: O(N)
 class Solution {
 public:
-    vector<int> maxSubsequence(vector<int>& nums, int k) {
-        // Heap 'q' will store the k largest elements of the array 
-        // The following heap is of pair where first element of pair is the element and the second element is the corresponding index
-        // That's how in queue sorting will be done according to the array element
-        priority_queue<p,vector<p>,greater<p>>q;
-        for(int i=0;i<nums.size();i++)
-        {
-            q.push({nums[i],i});
-            if(q.size()>k)
-            {
-                q.pop();
-            }
-        }
-        
-        // heap 'aux' will sort the elements based on their index
-        // sorting according to the index is necessary in order get the original order of the elements 
-		// in the following heap the first element of the pair is the index and the second element is the array element itself
-        priority_queue<p,vector<p>,greater<p>>aux;
-        while(!q.empty())
-        {
-            aux.push({q.top().second,q.top().first});
-            q.pop();
-        }
-        
-        // once the elements are sorted according to their indices , push them in a vector
-        vector<int>ans;
-        while(!aux.empty())
-        {
-            ans.push_back(aux.top().second);
-            aux.pop();
-        }
-        
-        // finally return the answer
+    vector<int> maxSubsequence(vector<int>& A, int k) {
+        vector<int> id(A.size());
+        iota(begin(id), end(id), 0); // Index array 0, 1, 2, ...
+        sort(begin(id), end(id), [&](int a, int b) { return A[a] > A[b]; }); // Sort the indexes in descending order of their corresponding values in `A`
+        id.resize(k); // Only keep the first `k` indexes with the greatest `A` values
+        sort(begin(id), end(id)); // Sort indexes in ascending order
+        vector<int> ans;
+        for (int i : id) ans.push_back(A[i]);
         return ans;
     }
 };
