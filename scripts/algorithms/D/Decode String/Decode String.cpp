8374207d33aa1390d013d7e6e9686b08e43095df
AnasImloul <pydev27@gmail.com>
2023-12-18 11:02:37 +0100

    Runtime: 6 ms (Top 9.18%) | Memory: 11.30 MB (Top 8.61%)

--- scripts/algorithms/D/Decode String/Decode String.cpp
+++ scripts/algorithms/D/Decode String/Decode String.cpp
@@ -1,36 +1,46 @@
+// Runtime: 6 ms (Top 9.18%) | Memory: 11.30 MB (Top 8.61%)
+
 class Solution {
 public:
+
     string decodeString(string s) {
-        stack<string> strstk;
-        stack<int> freq;
-        string curstr;
-        int k=0;
-        for(char c:s)
-        {
-            if(isdigit(c))
-                k=k*10+(c-'0');
-            else if(isalpha(c))
-                curstr.push_back(c);
-            else if(c=='[')
-            {
-                freq.push(k);
-                strstk.push(curstr);
-                curstr.clear();
-                k=0;
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ']') {
+                st.push(s[i]);
             }
-            else if(c==']')
-            {
-                string temp=curstr;
-                curstr=strstk.top();
-                strstk.pop();
-                int fre=freq.top();
-                freq.pop();
-                while(fre--)
-                {
-                    curstr+=temp;
+            else{
+                string curr_str = "";
+                
+                while(st.top() != '['){
+                    curr_str = st.top() + curr_str ;
+                    st.pop();
+                }
+                
+                st.pop();   // for '['
+                string number = "";
+                
+                // for calculating number
+                
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+                int k_time = stoi(number);    // convert string to number
+                
+                while(k_time--){
+                    for(int p = 0; p < curr_str.size() ; p++)
+                        st.push(curr_str[p]);
                 }
             }
         }
-        return curstr;
+        
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
+        
     }
 };
