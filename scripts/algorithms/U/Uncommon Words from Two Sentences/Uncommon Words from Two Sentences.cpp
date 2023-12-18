6f9b55c2d7b5385751489c17bcfd3fc99e2417d5
AnasImloul <pydev27@gmail.com>
2023-12-18 10:58:17 +0100

    Runtime: 4 ms (Top 47.05%) | Memory: 7.20 MB (Top 56.72%)

--- scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.cpp
+++ scripts/algorithms/U/Uncommon Words from Two Sentences/Uncommon Words from Two Sentences.cpp
@@ -1,53 +1,27 @@
+// Runtime: 4 ms (Top 47.05%) | Memory: 7.20 MB (Top 56.72%)
+
 class Solution {
 public:
-    vector<string> uncommonFromSentences(string s1, string s2) {
-      int i=0,temp;
-      map<string,int> m;
-      while(i<s1.length())
-      {
-          temp=i;
-          while(i<s1.length() && s1[i]!=' ')
-          {
-              i++;
-          }
-          if(m.find(s1.substr(temp,i-temp))==m.end())
-          {
-          m[s1.substr(temp,i-temp)]=1;
-          }
-          else
-          {
-            m[s1.substr(temp,i-temp)]++;
-          }
-          i++;
-      }  
-    int j=0;
-    int t;
-    vector<string> ans;
-    while(j<s2.length())
-    {
-       t=j;
-       while(j<s2.length() && s2[j]!=' ')
-        {
-              j++;
+    void uncommon(string s,unordered_map<string,int>&mp) {
+        string word="";
+        for(int i=0; i<s.size(); i++) {
+                if(s[i]==' ') {
+                     mp[word]++;
+                     word="";
+                }else {
+                     word+=s[i];
+                }
         }
-      if(m.find(s2.substr(t,j-t))!=m.end())
-      {
-         m[s2.substr(t,j-t)]++;
-      }
-      else
-      {
-        m[s2.substr(t,j-t)]=1;
-      }
-      j++;
+        mp[word]++;
     }
-    map<string,int>::iterator itr;
-    for(itr=m.begin();itr!=m.end();itr++)
-    {
-      if(itr->second<2)
-      {
-      ans.push_back(itr->first);
-      }
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> ans;
+        unordered_map<string,int> mp;
+        uncommon(s1,mp);
+        uncommon(s2,mp);
+        for(pair<string,int> p : mp) {
+            if(p.second==1) ans.push_back(p.first);
+        }
+        return ans;
     }
-    return ans;
-}
 };
