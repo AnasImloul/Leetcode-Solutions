4d6cb37a60ff211912db5e16238e4dc0d7e928e3
AnasImloul <pydev27@gmail.com>
2023-12-18 15:23:25 +0100

    Runtime: 42 ms (Top 26.09%) | Memory: 16.30 MB (Top 30.04%)

--- scripts/algorithms/N/Next Greater Element III/Next Greater Element III.py
+++ scripts/algorithms/N/Next Greater Element III/Next Greater Element III.py
@@ -1,23 +1,20 @@
-# Runtime: 54 ms (Top 28.53%) | Memory: 13.9 MB (Top 25.25%)
+// Runtime: 42 ms (Top 26.09%) | Memory: 16.30 MB (Top 30.04%)
+
 class Solution:
-    def nextGreaterElement(self, n: int) -> int:
-        s=list(str(n))
-        ind=-1
-        for x in range(len(s)-1,0,-1):
-            if s[x]>s[x-1]:
-                ind=x-1
-                break
-        if ind==-1:
-            return -1
-        ind2=-1
-        for x in range(len(s)-1,ind,-1):
-            if s[x]>s[ind]:
-                ind2=x
-                break
-        s[ind],s[ind2]=s[ind2],s[ind]
-        s=s[:ind+1]+s[ind+1:][::-1]
-        n1=int(''.join(s))
-        if n==n1 or n1>(2**31-1):
-            return -1
-        else:
-            return n1
+    def nextGreaterElement(self, n):
+        digits = list(str(n))
+        i = len(digits) - 1
+        while i-1 >= 0 and digits[i] <= digits[i-1]:
+            i -= 1
+            
+        if i == 0: return -1
+        
+        j = i
+        while j+1 < len(digits) and digits[j+1] > digits[i-1]:
+            j += 1
+        
+        digits[i-1], digits[j] = digits[j], digits[i-1]
+        digits[i:] = digits[i:][::-1]
+        ret = int(''.join(digits))
+        
+        return ret if ret < 1<<31 else -1
