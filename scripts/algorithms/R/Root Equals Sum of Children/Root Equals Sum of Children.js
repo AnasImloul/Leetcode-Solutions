b13aebfac4d71258ec1dcf471839a8014a11eed7
AnasImloul <pydev27@gmail.com>
2023-12-18 16:20:15 +0100

    Runtime: 55 ms (Top 53.46%) | Memory: 42.60 MB (Top 83.59%)

--- scripts/algorithms/R/Root Equals Sum of Children/Root Equals Sum of Children.js
+++ scripts/algorithms/R/Root Equals Sum of Children/Root Equals Sum of Children.js
@@ -1,5 +1,5 @@
+// Runtime: 55 ms (Top 53.46%) | Memory: 42.60 MB (Top 83.59%)
+
 var checkTree = function(root) {
-    let value = root.left.val + root.right.val;
-    value = value === root.val ? true : false;
-    return value;
+    return root.val === root.left.val + root.right.val;
 };
