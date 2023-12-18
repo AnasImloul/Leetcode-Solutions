1c1763cd6185946bc75290808b5c4cec066ae4a9
AnasImloul <pydev27@gmail.com>
2023-12-18 15:26:55 +0100

    Runtime: 4890 ms (Top 15.42%) | Memory: 42.20 MB (Top 64.95%)

--- scripts/algorithms/M/Minimum Obstacle Removal to Reach Corner/Minimum Obstacle Removal to Reach Corner.py
+++ scripts/algorithms/M/Minimum Obstacle Removal to Reach Corner/Minimum Obstacle Removal to Reach Corner.py
@@ -1,35 +1,20 @@
-class Solution(object):
-    def minimumObstacles(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        R, C = len(grid), len(grid[0])
-        
-        d = [(1,0),(-1,0),(0,1),(0,-1)]
-        
-        distances = [[-1] * C for _ in range(R)]
-        
-        q = deque([(0, 0, 0)])
-        
-        while q:
-            for _ in range(len(q)):
-                dist, r, c = q.popleft()
-                
-                for dr, dc in d:
-                    rr, cc = r + dr, c + dc
-					#Check if the cell is inbounds and has not been visited before
-                    if 0 <= rr < R and 0 <= cc < C and distances[rr][cc] == -1:
-                        
-                        #If the cell is an obstacle - assign current distance + 1 to its value
-						if grid[rr][cc] == 1:
-                            distances[rr][cc] = dist + 1
-                            q.append((dist + 1, rr, cc))
-                            
-                        else:
-                            #Make sure we process cells with less obstacles first hence appendleft
-                            distances[rr][cc] = dist
-                            q.appendleft((dist, rr, cc))
-                            
-        return distances[R - 1][C - 1]
+// Runtime: 4890 ms (Top 15.42%) | Memory: 42.20 MB (Top 64.95%)
 
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        q = [(0, 0, 0)]
+        dist = [[float('inf') for _ in range(n)] for _ in range(m)]
+
+        while q:
+            size = len(q)
+            for _ in range(size):
+                obs, x, y = heapq.heappop(q)
+                if dist[x][y] < float('inf'): continue
+                obs += grid[x][y]
+                dist[x][y] = obs
+                if x + 1 < m: heapq.heappush(q, (obs, x + 1, y))
+                if x > 0: heapq.heappush(q, (obs, x - 1, y))
+                if y + 1 < n: heapq.heappush(q, (obs, x, y + 1))
+                if y > 0: heapq.heappush(q, (obs, x, y - 1))
+        return dist[m - 1][n - 1]
