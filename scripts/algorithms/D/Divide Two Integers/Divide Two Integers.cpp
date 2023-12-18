0c05185a1f9be0e5eca03c2140c76848a8a542c7
AnasImloul <pydev27@gmail.com>
2023-12-18 11:01:06 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.30 MB (Top 35.14%)

--- scripts/algorithms/D/Divide Two Integers/Divide Two Integers.cpp
+++ scripts/algorithms/D/Divide Two Integers/Divide Two Integers.cpp
@@ -1,8 +1,22 @@
-// Runtime: 10 ms (Top 11.29%) | Memory: 6 MB (Top 29.05%)
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.30 MB (Top 35.14%)
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if((dividend == INT_MIN)&&(divisor == -1)) return INT_MAX;
-        return dividend/divisor;
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
+            }
+            dvd -= temp;
+            ans += m;
+        }
+        return sign * ans;
     }
 };
