10f6e92f8d284bc81d77252e3ae8679cf76273a9
AnasImloul <pydev27@gmail.com>
2023-12-18 13:37:32 +0100

    Runtime: 151 ms (Top 22.99%) | Memory: 94.00 MB (Top 86.21%)

--- scripts/algorithms/L/Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.java
+++ scripts/algorithms/L/Longest Path With Different Adjacent Characters/Longest Path With Different Adjacent Characters.java
@@ -1,36 +1,41 @@
+// Runtime: 151 ms (Top 22.99%) | Memory: 94.00 MB (Top 86.21%)
+
 class Solution {
+    int longestPathValue = 1; // variable to store the length of the longest path
+
     public int longestPath(int[] parent, String s) {
-        ArrayList<ArrayList<Integer>> a=new ArrayList<>();
-        for(int i=0;i<parent.length;i++)a.add(new ArrayList<Integer>());
-        for(int i=1;i<parent.length;i++){
-            a.get(parent[i]).add(i);
+        // create an adjacency list representation of the tree
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i = 1; i < parent.length; i++){
+            int j = parent[i];
+            adj.putIfAbsent(j, new LinkedList<>());
+            adj.get(j).add(i);
         }
-        Pair p=dfs(0,a,s);
-        return p.maxCount;
+        // call dfs on the root of the tree
+        dfs(0, adj, s);
+        return longestPathValue;
     }
-    public Pair dfs(int root,ArrayList<ArrayList<Integer>> a,String s){
-        if(a.get(root).size()==0)return new Pair(1,1);
-        int maxCount=0,count1=0,count2=0;
-        for(int child:a.get(root)){
-            Pair pair=dfs(child,a,s);
-            maxCount=Math.max(maxCount,pair.maxCount);
-            if(s.charAt(root)!=s.charAt(child)){
-                if(count1<pair.count){
-                    count2=count1;
-                    count1=pair.count;
-                }else if(pair.count>count2){
-                    count2=pair.count;
-                }
+
+    public int dfs(int node, Map<Integer, List<Integer>> adj, String s){
+        // if the node is a leaf node, return 1
+        if(!adj.containsKey(node)) return 1;
+        int max = 0, secondMax = 0;
+        // for each neighbor of the node
+        for(int nbrNode : adj.get(node)){
+            int longestPathFromNbrNode = dfs(nbrNode , adj, s);
+            // if the characters at the current node and its neighbor are the same, ignore the neighbor
+            if(s.charAt(node) == s.charAt(nbrNode)) continue;
+            // update max and secondMax with the longest path from the neighbor node
+            if(longestPathFromNbrNode > max){
+                secondMax = max;
+                max = longestPathFromNbrNode;
+            }else if(longestPathFromNbrNode > secondMax){
+                secondMax = longestPathFromNbrNode;
             }
         }
-        maxCount=Math.max(maxCount,count1+count2+1);
-        return new Pair(count1+1,maxCount);
-    }
-    class Pair{
-        int count,maxCount;
-        Pair(int count,int maxCount){
-            this.count=count;
-            this.maxCount=maxCount;
-        }
+        // update longestPathValue with the longest path that includes the current node
+        longestPathValue = Math.max(longestPathValue, max+secondMax+1);
+        return max+1;
     }
 }
+
