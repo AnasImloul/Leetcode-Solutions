19192e1e65f8883303b507670d61257a8206101b
AnasImloul <pydev27@gmail.com>
2023-12-18 11:53:44 +0100

    Runtime: 49 ms (Top 8.88%) | Memory: 16.20 MB (Top 55.73%)

--- scripts/algorithms/S/Sort Colors/Sort Colors.py
+++ scripts/algorithms/S/Sort Colors/Sort Colors.py
@@ -1,15 +1,17 @@
+// Runtime: 49 ms (Top 8.88%) | Memory: 16.20 MB (Top 55.73%)
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        d = {0:0,1:0,2:0}
-        for i in nums:
-            if i in d:
-                d[i]+=1
-        
-        nums.clear()
-        for i in d:
-            while d[i]>0:
-                nums.append(i)
-                d[i]-=1
+
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
