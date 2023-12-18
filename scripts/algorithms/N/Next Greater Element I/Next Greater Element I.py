3d14acfa6a294d048942840bdeffbf97f892ba16
AnasImloul <pydev27@gmail.com>
2023-12-18 11:54:34 +0100

    Runtime: 50 ms (Top 80.48%) | Memory: 16.70 MB (Top 24.94%)

--- scripts/algorithms/N/Next Greater Element I/Next Greater Element I.py
+++ scripts/algorithms/N/Next Greater Element I/Next Greater Element I.py
@@ -1,41 +1,14 @@
+// Runtime: 50 ms (Top 80.48%) | Memory: 16.70 MB (Top 24.94%)
 
-class Solution(object):
+class Solution:
     def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-      """
+        dic, stack = {}, []
         
-        
-        
-      # index of the next greater for nums2
-        
-        stack = []
-        nextGreater = [-1]*len(nums2)
-    
-        for i in range(len(nums2)):
-
-            while (stack and nums2[stack[-1]] < nums2[i]):
-                nextGreater[stack.pop()] = i
-        
-            stack.append(i)
-      
-        
-        
-        
-        # map nums to its idx so we can map from nums1 to nums2 quickly
-        mapIdx2 = {}
-        for i,num in enumerate(nums2):
-            mapIdx2[num] = i
-        
-        
-    
-        ans = []
-        for num in nums1:
-            idx = nextGreater[mapIdx2[num]]
-            ans.append( nums2[idx] if idx != -1 else idx)
-        return ans
+        for num in nums2[::-1]:
+            while stack and num > stack[-1]:
+                stack.pop()
+            if stack:
+                dic[num] = stack[-1]
+            stack.append(num)
             
-    
-
+        return [dic.get(num, -1) for num in nums1]
