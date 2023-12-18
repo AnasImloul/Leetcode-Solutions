bf969f3fd4ff41422c964eb85101727030021156
AnasImloul <pydev27@gmail.com>
2023-12-18 11:55:33 +0100

    Runtime: 1085 ms (Top 46.04%) | Memory: 30.40 MB (Top 85.02%)

--- scripts/algorithms/D/Daily Temperatures/Daily Temperatures.py
+++ scripts/algorithms/D/Daily Temperatures/Daily Temperatures.py
@@ -1,13 +1,11 @@
+// Runtime: 1085 ms (Top 46.04%) | Memory: 30.40 MB (Top 85.02%)
+
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Next greater number
-        res = [0] * len(temperatures)
-		# store the index
-        stack = []
-        for i, temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temp:
-                res[stack[-1]] = i - stack[-1]
-                stack.pop()
-            
-            stack.append(i)
-        return res
+    def dailyTemperatures(self, T):
+        ans, s = [0]*len(T), deque()
+        for cur, cur_tmp in enumerate(T):
+            while s and cur_tmp > T[s[-1]]:
+                ans[s[-1]] = cur - s[-1]
+                s.pop()
+            s.append(cur)
+        return ans
