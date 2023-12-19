35616d93e142985dcd7862e5f8c0fdfa0624eb0d
AnasImloul <pydev27@gmail.com>
2023-12-19 09:00:22 +0100

    Runtime: 2593 ms (Top 36.11%) | Memory: 66.70 MB (Top 22.22%)

--- scripts/algorithms/D/Delivering Boxes from Storage to Ports/Delivering Boxes from Storage to Ports.py
+++ scripts/algorithms/D/Delivering Boxes from Storage to Ports/Delivering Boxes from Storage to Ports.py
@@ -1,24 +1,26 @@
+// Runtime: 2593 ms (Top 36.11%) | Memory: 66.70 MB (Top 22.22%)
+
+from sortedcontainers import SortedList as MonoQueue
 class Solution:
-    def boxDelivering(self, boxes: List[List[int]], portsCount: int, maxBoxes: int, maxWeight: int) -> int:
-        n = len(boxes)
-        que = collections.deque([(-1,0)])  # monotonic queue. item: (division_position, trip_cost)
-        pre = -1  # latest end of previous trip
-		ws = 0  # maximal weights of current trip
-        for i, (p, w) in enumerate(boxes):  # p: port; w: weight
-            # update the earliest possible start of current trip:
-            ws += w
-            while i - pre > maxBoxes or ws > maxWeight:
-                pre += 1
-                ws -= boxes[pre][1]
-            while que[0][0] < pre: que.popleft()  ## pop out the boxes out of range of current trip
-            
-            # min cost of current trip. front of monotonic queue is always the minimal cost that meets the limitation
-            mn = (2 if i+1<n and p==boxes[i+1][0] else 1) + que[0][1]
-            while que[-1][1] >= mn: que.pop()  # maintain the queue monotonic
-            que.append((i, mn))
-        
-        base_trip = 1  # calc base trip cost
-        for i in range(n-1):
-            if boxes[i][0] != boxes[i+1][0]: base_trip += 1
-        
-        return base_trip + que[-1][1]
+    def boxDelivering(self, A, __, B, W):
+        n = len(A)
+        def slidingWindow():
+            l=0
+            cW = 0
+            for r in range(n):
+                cW+=A[r][1]
+                while cW>W or r-l+1>B:
+                    cW-=A[l][1]
+                    l+=1
+                yield l,r
+        Seg=MonoQueue(key=lambda t:t[1])
+        addAll = 0
+        olddp = 0
+        for l,r in slidingWindow():
+            if r!=0:
+                addAll+= ( A[r][0]!=A[r-1][0] )
+            Seg.add((r, olddp-addAll+2))
+            while Seg[0][0]<l:
+                Seg.pop(0)
+            olddp = Seg[0][1]+addAll
+        return olddp
