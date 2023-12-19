d7913ec210df49fe35007294d6a3439377dc60b2
AnasImloul <pydev27@gmail.com>
2023-12-19 09:01:58 +0100

    Runtime: 74 ms (Top 67.79%) | Memory: 17.30 MB (Top 13.48%)

--- scripts/algorithms/C/Construct K Palindrome Strings/Construct K Palindrome Strings.py
+++ scripts/algorithms/C/Construct K Palindrome Strings/Construct K Palindrome Strings.py
@@ -1,12 +1,16 @@
-# Runtime: 334 ms (Top 5.05%) | Memory: 14.9 MB (Top 23.23%)
+// Runtime: 74 ms (Top 67.79%) | Memory: 17.30 MB (Top 13.48%)
+
+from collections import Counter
+
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        characters, numOfOdds = {}, 0
-        for char in s:
-            if char not in characters:
-                characters[char] = 0
-            characters[char] += 1
-        for key in characters:
-            if characters[key]%2 != 0:
-                numOfOdds += 1
-        return numOfOdds <= k and k <= len(s)
+        if k > len(s):
+            return False
+        h = Counter(s)
+        countOdd = 0
+        for value in h.values():
+            if value % 2:
+                countOdd += 1
+        if countOdd > k:
+            return False
+        return True
