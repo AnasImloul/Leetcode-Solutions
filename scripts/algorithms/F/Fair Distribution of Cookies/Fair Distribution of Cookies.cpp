58e8a5e845c599832056916cd1336e7bafdbceb4
AnasImloul <pydev27@gmail.com>
2023-12-19 09:26:48 +0100

    Runtime: 1339 ms (Top 11.66%) | Memory: 7.20 MB (Top 82.43%)

--- scripts/algorithms/F/Fair Distribution of Cookies/Fair Distribution of Cookies.cpp
+++ scripts/algorithms/F/Fair Distribution of Cookies/Fair Distribution of Cookies.cpp
@@ -1,34 +1,28 @@
-// Runtime: 2993 ms (Top 5.08%) | Memory: 7 MB (Top 43.65%)
+// Runtime: 1339 ms (Top 11.66%) | Memory: 7.20 MB (Top 82.43%)
+
 class Solution {
 public:
-
-   int maxi=INT_MAX;
-    void explore(vector<int>&cookies,int n,int k,vector<int>&vec)
-    {
-      if(n==cookies.size())
-      {
-          int m=0;
-          for(int i=0;i<k;i++)
-          {
-              m=max(m,vec[i]);
-
-          }
-          maxi=min(maxi,m);
-          return;
-      }
-        for(int i=0;i<k;i++)
-        {
-            vec[i]+=cookies[n];
-            explore(cookies,n+1,k,vec);
-            vec[i]-=cookies[n];
+    int ans = INT_MAX;
+    void solve(int start, vector<int>& nums, vector<int>& v, int k){
+        if(start==nums.size()){
+            int maxm = INT_MIN;
+            for(int i=0;i<k;i++){
+                maxm = max(maxm,v[i]);
+            }
+            ans = min(ans,maxm);
+            return;
+        }
+        for(int i=0;i<k;i++){
+            v[i] += nums[start];
+            solve(start+1,nums,v,k);
+            v[i] -= nums[start];
         }
     }
-
-    int distributeCookies(vector<int>& cookies, int k) {
-
-        vector<int> vec(10,0);
-          explore(cookies,0,k,vec);
-        return maxi;
-
+    
+    int distributeCookies(vector<int>& nums, int k) { // nums is the cookies vector
+        int n = nums.size();
+        vector<int> v(k,0); // v is to store each sum of the k subsets
+        solve(0,nums,v,k);
+        return ans;
     }
 };
