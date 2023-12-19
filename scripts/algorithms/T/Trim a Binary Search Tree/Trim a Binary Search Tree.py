2fc89ce1ae25ba61eb4f50a142fd1fc52f729d18
AnasImloul <pydev27@gmail.com>
2023-12-19 17:15:22 +0100

    Runtime: 58 ms (Top 27.96%) | Memory: 20.40 MB (Top 38.8%)

--- scripts/algorithms/T/Trim a Binary Search Tree/Trim a Binary Search Tree.py
+++ scripts/algorithms/T/Trim a Binary Search Tree/Trim a Binary Search Tree.py
@@ -1,14 +1,10 @@
-class Solution:
-    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
-        
-        def trim(node):
-			# While current node does not meet the (low, high) requirement trim it
-            while node and (node.val < low or node.val > high):
-                node = node.right if node.val < low else node.left
-            
-            if node:
-                node.left = trim(node.left)
-                node.right = trim(node.right)
-            return node
+// Runtime: 58 ms (Top 27.96%) | Memory: 20.40 MB (Top 38.8%)
 
-        return trim(root)
+class Solution:
+	def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+		if not root: return root
+		if root.val < low: return self.trimBST(root.right, low, high)
+		if root.val > high: return self.trimBST(root.left, low, high)
+		root.left = self.trimBST(root.left, low, high)
+		root.right = self.trimBST(root.right, low, high)
+		return root
