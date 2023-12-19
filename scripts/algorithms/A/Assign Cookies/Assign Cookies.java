98f807c307f17dcef2a07ea0e39677e55552d658
AnasImloul <pydev27@gmail.com>
2023-12-19 09:43:22 +0100

    Runtime: 8 ms (Top 98.76%) | Memory: 45.20 MB (Top 5.55%)

--- scripts/algorithms/A/Assign Cookies/Assign Cookies.java
+++ scripts/algorithms/A/Assign Cookies/Assign Cookies.java
@@ -1,12 +1,30 @@
+// Runtime: 8 ms (Top 98.76%) | Memory: 45.20 MB (Top 5.55%)
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        int i =0,j=0,c=0;
+        
         Arrays.sort(g);
         Arrays.sort(s);
-        int i =0;
-        for(int j =0; i < g.length && j < s.length; j++) {
-            if(g[i] <= s[j]) i++;
+        
+        
+        for(;i< g.length;i++)
+        {
+                // System.out.println(s[j]+" "+g[i]);
+ 
+            while(j<s.length)
+            {
+                if(s[j]>=g[i] )
+                {
+                    // System.out.println(s[j]+" "+g[i]);
+                    j++;c++;
+                    break;
+                }
+                j++;
+            }
         }
-        return i;
+       
+        return c;
+        
     }
-}.
-
+}
