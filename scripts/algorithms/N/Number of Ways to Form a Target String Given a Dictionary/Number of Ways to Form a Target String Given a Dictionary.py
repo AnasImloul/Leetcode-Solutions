c781bf7c4e396ae76d159b90357631d4616123cb
AnasImloul <pydev27@gmail.com>
2023-12-19 17:13:19 +0100

    Runtime: 1795 ms (Top 74.72%) | Memory: 380.50 MB (Top 27.21%)

--- scripts/algorithms/N/Number of Ways to Form a Target String Given a Dictionary/Number of Ways to Form a Target String Given a Dictionary.py
+++ scripts/algorithms/N/Number of Ways to Form a Target String Given a Dictionary/Number of Ways to Form a Target String Given a Dictionary.py
@@ -1,20 +1,25 @@
-from collections import defaultdict
-from functools import lru_cache
+// Runtime: 1795 ms (Top 74.72%) | Memory: 380.50 MB (Top 27.21%)
+
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        M = 10**9 + 7
-        dt = defaultdict(int)
-        m, n = len(words[-1]), len(target)
+        MOD = 10 ** 9 + 7
+        m, n = len(words[0]), len(target)
+        charAtIndexCnt = [[0] * m for _ in range(128)]
         for word in words:
             for i, c in enumerate(word):
-                dt[i,c] += 1
+                charAtIndexCnt[ord(c)][i] += 1  # Count the number of character `c` at index `i` of all words
+
         @lru_cache(None)
-        def dfs(i,j):
-            if j == n: return 1
-            if i == m: return 0
-            res = 0
-            if dt[i,target[j]]:
-                res = (res + dt[i,target[j]]*dfs(i+1,j+1))%M
-            res = (res + dfs(i+1,j))%M
-            return res
-        return dfs(0,0)
+        def dp(k, i):
+            if i == n:  # Formed a valid target
+                return 1
+            if k == m:  # Reached to length of words[x] but don't found any result
+                return 0
+            c = target[i]
+            ans = dp(k + 1, i)  # Skip k_th index of words
+            if charAtIndexCnt[ord(c)][k] > 0: # Take k_th index of words if found character `c` at index k_th
+                ans += dp(k + 1, i + 1) * charAtIndexCnt[ord(c)][k]
+                ans %= MOD
+            return ans
+
+        return dp(0, 0)
