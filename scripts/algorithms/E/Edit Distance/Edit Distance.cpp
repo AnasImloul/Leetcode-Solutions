682304a05805185edd51d7923a3981c915038c77
AnasImloul <pydev27@gmail.com>
2023-12-19 09:31:36 +0100

    Runtime: 13 ms (Top 45.48%) | Memory: 9.60 MB (Top 18.07%)

--- scripts/algorithms/E/Edit Distance/Edit Distance.cpp
+++ scripts/algorithms/E/Edit Distance/Edit Distance.cpp
@@ -1,22 +1,25 @@
+// Runtime: 13 ms (Top 45.48%) | Memory: 9.60 MB (Top 18.07%)
+
 class Solution {
 public:
-    int f(int i,int j,string &word1,string &word2,vector<vector<int>> &dp)
-    {
-        if(i<0)
-            return j+1;
-        if(j<0)
-            return i+1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(word1[i]==word2[j])
-            return dp[i][j]=f(i-1,j-1,word1,word2,dp);
-        else
-            return dp[i][j]=1+min(f(i-1,j-1,word1,word2,dp),min(f(i-1,j,word1,word2,dp),f(i,j-1,word1,word2,dp)));
-    }
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
