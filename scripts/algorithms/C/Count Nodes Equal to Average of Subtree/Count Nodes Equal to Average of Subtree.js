bb0de40ec1c04919c179e01b5ae9f13d6bd3edec
AnasImloul <pydev27@gmail.com>
2023-12-20 10:21:16 +0100

    Runtime: 73 ms (Top 20.02%) | Memory: 47.50 MB (Top 17.03%)

--- scripts/algorithms/C/Count Nodes Equal to Average of Subtree/Count Nodes Equal to Average of Subtree.js
+++ scripts/algorithms/C/Count Nodes Equal to Average of Subtree/Count Nodes Equal to Average of Subtree.js
@@ -1,24 +1,22 @@
-// Runtime: 153 ms (Top 26.67%) | Memory: 47 MB (Top 46.11%)
+// Runtime: 73 ms (Top 20.02%) | Memory: 47.50 MB (Top 17.03%)
 
 var averageOfSubtree = function(root) {
-    let count = 0;
-
-    function traverse(node) {
-        if (node === null) {
-            return [0, 0];
-        }
-
-        let [sumLeft, countLeft] = traverse(node.left);
-        let [sumRight, countRight] = traverse(node.right);
-
-        if (Math.floor((sumLeft + sumRight + node.val) / (countLeft + countRight + 1)) === node.val) {
-            count++;
-        }
-
-        return [sumLeft + sumRight + node.val, countLeft + countRight + 1];
-    }
-
+    let result = 0;
+    
+    const traverse = node => {
+        if (!node) return [0, 0];
+        
+        const [leftSum, leftCount] = traverse(node.left);
+        const [rightSum, rightCount] = traverse(node.right);
+        
+        const currSum = node.val + leftSum + rightSum;
+        const currCount = 1 + leftCount + rightCount;
+        
+        if (Math.floor(currSum / currCount) === node.val) result++;
+        
+        return [currSum, currCount];
+    };
+    
     traverse(root);
-
-    return count;
+    return result;
 };
