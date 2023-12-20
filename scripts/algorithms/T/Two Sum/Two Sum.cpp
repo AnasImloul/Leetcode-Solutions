1cc49c1917277d8786e87d148750ef77d5c83138
AnasImloul <pydev27@gmail.com>
2023-12-20 13:27:51 +0100

    Runtime: 245 ms (Top 44.51%) | Memory: 10.50 MB (Top 77.03%)

--- scripts/algorithms/T/Two Sum/Two Sum.cpp
+++ scripts/algorithms/T/Two Sum/Two Sum.cpp
@@ -1,21 +1,17 @@
-// Runtime: 415 ms (Top 35.30%) | Memory: 10 MB (Top 96.80%)
+// Runtime: 245 ms (Top 44.51%) | Memory: 10.50 MB (Top 77.03%)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Two for loops for selecting two numbers and check sum equal to target or not
-
-        for(int i = 0;i < nums.size();i++)
-        {
-            for(int j = i + 1;j < nums.size();j++)
-            {
-                // j = i + 1; no need to check back elements it covers in i;
-                if ((nums[i] + nums[j]) == target){
-                    // Check sum == target or not
-                    return{i,j};
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
                 }
-
             }
         }
-        return {};
+        return {}; // No solution found
     }
 };
+
