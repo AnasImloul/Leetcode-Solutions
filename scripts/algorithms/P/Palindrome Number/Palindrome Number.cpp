f8e65313a0ffe26d4b4c4cd0ce25c36b4395de60
AnasImloul <pydev27@gmail.com>
2023-12-21 18:11:54 +0100

    Runtime: 7 ms (Top 71.76%) | Memory: 6.40 MB (Top 21.28%)

--- scripts/algorithms/P/Palindrome Number/Palindrome Number.cpp
+++ scripts/algorithms/P/Palindrome Number/Palindrome Number.cpp
@@ -1,19 +1,22 @@
+// Runtime: 7 ms (Top 71.76%) | Memory: 6.40 MB (Top 21.28%)
+
 class Solution {
-    long long convert(int x){
-        //reversing can exceed the allowed INTEGER domain for 32-bit.
-        long long ans=0;
-        while(x>0){
-            ans = ans*10+x%10;
-            x=x/10;
-        }
-        return ans;
-    }
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        //getting the reverse number and checking whether  reversed_number==original_number
-        long long ans=convert(x);
-        //typeCasting for Checking
-        return ans==(long long)(x);
+        if (x < 0) {
+            return false;
+        }
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
     }
 };
+
