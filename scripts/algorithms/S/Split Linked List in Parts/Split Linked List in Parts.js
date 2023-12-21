5cfb76f54f7ae6dec5a20a299de6014457ccdbcf
AnasImloul <pydev27@gmail.com>
2023-12-21 17:28:16 +0100

    Runtime: 72 ms (Top 6.17%) | Memory: 45.00 MB (Top 9.88%)

--- scripts/algorithms/S/Split Linked List in Parts/Split Linked List in Parts.js
+++ scripts/algorithms/S/Split Linked List in Parts/Split Linked List in Parts.js
@@ -1,22 +1,40 @@
+// Runtime: 72 ms (Top 6.17%) | Memory: 45.00 MB (Top 9.88%)
+
 var splitListToParts = function(head, k) {
-    let arr=[];
-    while(head){arr.push(head.val); head=head.next;}
-    let n=arr.length;
-    let output=[];
-    let indx=0;
-    
-    while(k){
-        let len=Math.ceil(n/k);
-        n-=len; k--;
-        let newHead=new ListNode(-1);
-        let list=newHead;
-        for(let j=0; j<len; j++){
-            list.next=new ListNode(arr[indx]);
-            list=list.next;
-            indx++;
-        }
-        output.push(newHead.next);
+    let length = 0, current = head, parts = [];
+
+    while (current) {
+        length++;
+        current = current.next;
     }
-    
-    return output;
+
+    let base_size = Math.floor(length / k), extra = length % k;
+    current = head;
+
+    for (let i = 0; i < k; i++) {
+        let part_size = base_size + (extra > 0 ? 1 : 0);
+        let part_head = null, part_tail = null;
+
+        for (let j = 0; j < part_size; j++) {
+            if (!part_head) {
+                part_head = part_tail = current;
+            } else {
+                part_tail.next = current;
+                part_tail = part_tail.next;
+            }
+
+            if (current) {
+                current = current.next;
+            }
+        }
+
+        if (part_tail) {
+            part_tail.next = null;
+        }
+
+        parts.push(part_head);
+        extra = Math.max(extra - 1, 0);
+    }
+
+    return parts;
 };
