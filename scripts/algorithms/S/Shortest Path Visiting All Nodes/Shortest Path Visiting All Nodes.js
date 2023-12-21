90b92c92ce9f3a0742e6ef011a10f2dea47aa60a
AnasImloul <pydev27@gmail.com>
2023-12-21 17:31:29 +0100

    Runtime: 92 ms (Top 67.21%) | Memory: 50.60 MB (Top 77.05%)

--- scripts/algorithms/S/Shortest Path Visiting All Nodes/Shortest Path Visiting All Nodes.js
+++ scripts/algorithms/S/Shortest Path Visiting All Nodes/Shortest Path Visiting All Nodes.js
@@ -1,26 +1,37 @@
-var shortestPathLength = function(graph) {
-  let n = graph.length, fullBitmask = (1 << n) - 1;
-  let queue = [], visited = new Set();
-  for (var i = 0; i < n; i++) {
-    queue.push([i, 1 << i]);
-    visited.add(`${i},${1 << i}`);
-  }
+// Runtime: 92 ms (Top 67.21%) | Memory: 50.60 MB (Top 77.05%)
 
-  let steps = 0;
-  while (queue.length) {
-    let next = [];
-    while (queue.length) {
-      let [node, bitmask] = queue.pop();
-      if (bitmask === fullBitmask) return steps;
-      for (var neighbor of graph[node]) {
-        let newBitmask = bitmask | (1 << neighbor);
-        if (!visited.has(`${neighbor},${newBitmask}`)) {
-          next.push([neighbor, newBitmask]);
-          visited.add(`${neighbor},${newBitmask}`);
-        }
-      }
+/**
+ * @param {number[][]} graph
+ * @return {number}
+ */
+var shortestPathLength = function(graph) {
+    const n = graph.length;
+    const allVisited = (1 << n) - 1;
+    const queue = [];
+    const visited = new Set();
+
+    for (let i = 0; i < n; i++) {
+        queue.push([1 << i, i, 0]);
+        visited.add((1 << i) * 16 + i);
     }
-    queue = next;
-    steps++;
-  }
-};
+
+    while (queue.length > 0) {
+        const [mask, node, dist] = queue.shift();
+
+        if (mask === allVisited) {
+            return dist;
+        }
+
+        for (const neighbor of graph[node]) {
+            const newMask = mask | (1 << neighbor);
+            const hashValue = newMask * 16 + neighbor;
+
+            if (!visited.has(hashValue)) {
+                visited.add(hashValue);
+                queue.push([newMask, neighbor, dist + 1]);
+            }
+        }
+    }
+
+    return -1;
+}
