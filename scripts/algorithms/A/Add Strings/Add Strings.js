82a70a0bda1bd2245ad6889103d20f7ea6ac2575
AnasImloul <pydev27@gmail.com>
2023-12-21 17:25:15 +0100

    Runtime: 69 ms (Top 19.29%) | Memory: 44.40 MB (Top 55.25%)

--- scripts/algorithms/A/Add Strings/Add Strings.js
+++ scripts/algorithms/A/Add Strings/Add Strings.js
@@ -1,28 +1,18 @@
-/**
- * @param {string} num1
- * @param {string} num2
- * @return {string}
- */
+// Runtime: 69 ms (Top 19.29%) | Memory: 44.40 MB (Top 55.25%)
+
 var addStrings = function(num1, num2) {
     let i = num1.length - 1;
     let j = num2.length - 1;
-    let sum = 0, carry = 0, x, y;
-    let result = "";
-    while(i>=0 || j>=0){
-        x = i < 0 ? 0 : num1.charAt(i) - "0";
-        y = j < 0 ? 0 : num2.charAt(j) - "0";
-        sum = (x + y + carry) % 10;
-        carry = Math.floor((x + y + carry) /10);
-        result =  sum + result;
-        
-        i--;
-        j--;
+    let carry = 0;
+    let sum = '';
+     
+    for (;i >= 0 || j >= 0 || carry > 0;i--, j--) {
+        const digit1 = i < 0 ? 0 : num1.charAt(i) - '0';
+        const digit2 = j < 0 ? 0 : num2.charAt(j) - '0';
+        const digitsSum = digit1 + digit2 + carry;
+        sum = `${digitsSum % 10}${sum}`;
+        carry = Math.floor(digitsSum / 10);
     }
     
-    if(carry!=0){
-        result =  carry + result;
-    }
-    
-    return result;
-   
+    return sum;
 };
