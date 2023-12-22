63da4585eef084f6a06539c81fe87361f80510ae
AnasImloul <pydev27@gmail.com>
2023-12-22 19:57:05 +0100

    Runtime: 23 ms (Top 5.63%) | Memory: 11.70 MB (Top 10.19%)

--- scripts/algorithms/K/Keys and Rooms/Keys and Rooms.cpp
+++ scripts/algorithms/K/Keys and Rooms/Keys and Rooms.cpp
@@ -1,25 +1,17 @@
+// Runtime: 23 ms (Top 5.63%) | Memory: 11.70 MB (Top 10.19%)
+
 class Solution {
-public:
-    void dfs(int node, vector<vector<int>>& rooms,vector<int> &visited){
-        visited[node]=1;
-        
-        for(auto it: rooms[node]){
-            if(visited[it]==0)
-                dfs(it, rooms, visited);
-            else continue;
-        }
-        return;
+    void dfs(vector<vector<int>>& rooms, unordered_set<int> & keys, unordered_set<int> & visited, int curr) {
+        visited.insert(curr);
+        for (int k : rooms[curr]) keys.insert(k);
+        for (int k : keys) if (visited.find(k) == visited.end()) dfs(rooms, keys, visited, k);
     }
     
+public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<int> visited(n,0);
-        
-        dfs(0,rooms,visited);
-        
-        for(int i=0;i<n;i++){
-            if(visited[i]==0) return false;
-        }    
-        return true;
+        unordered_set<int> keys;
+        unordered_set<int> visited;
+        dfs(rooms, keys, visited, 0);
+        return visited.size() == rooms.size();
     }
 };
