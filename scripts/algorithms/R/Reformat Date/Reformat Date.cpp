aea6a2e237dca9de15b8e0a98becbca412ee6c1e
AnasImloul <pydev27@gmail.com>
2023-12-22 19:51:03 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.50 MB (Top 57.94%)

--- scripts/algorithms/R/Reformat Date/Reformat Date.cpp
+++ scripts/algorithms/R/Reformat Date/Reformat Date.cpp
@@ -1,45 +1,20 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.50 MB (Top 57.94%)
+
 class Solution {
 public:
     string reformatDate(string date) {
-        map<string,int>m;
-        m["Jan"] =1;
-        m["Feb"] =2;
-        m["Mar"] =3;
-        m["Apr"] =4;
-        m["May"] =5;
-        m["Jun"] =6;
-        m["Jul"] =7;
-        m["Aug"] =8;
-        m["Sep"] =9;
-        m["Oct"] =10;
-        m["Nov"] =11;
-        m["Dec"] =12;
-        string ans;
-        for(int i=date.length()-4; i<date.length(); i++){
-            ans+=date[i];
-        }
-        ans += "-";
-        string month;
-        for(int i=date.length()-8; i<=date.length()-6; i++){
-            month+=date[i];
-        }
-
-        int yes = m[month];
-
-        if(yes<=9){
-            ans += '0';
-        }
-        ans = ans + to_string(yes);
-        ans += "-";
-        int i = 0;
-        if(date[1]=='t' || date[1]=='s' || date[1]=='n' || date[1]=='r'){
-            ans+='0';
-            ans+=date[0];
-        }
-        else{
-            ans+=date[0];
-            ans+=date[1];
-        }
-        return ans;
+        // easiest way to get index with O(1)
+        unordered_map<string, string> months = {{"Jan", "01"}, {"Feb", "02"}, {"Mar", "03"},{"Apr", "04"}, {"May", "05"}, {"Jun", "06"}, {"Jul", "07"}, {"Aug", "08"}, {"Sep", "09"},{"Oct", "10"}, {"Nov", "11"}, {"Dec", "12"}};
+        istringstream s(date); 
+        // get day
+        string day; s >> day;
+        day.pop_back(); day.pop_back();
+        if (day.size() == 1)
+            day = "0" + day;
+        // get month
+        string month; s >> month;
+        // get year
+        string year; s >> year;
+        return year + "-" + months[month] + "-" + day;
     }
 };
