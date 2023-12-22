dc22aa1a793768574ca893accc4a1d947f616934
AnasImloul <pydev27@gmail.com>
2023-12-22 19:50:00 +0100

    Runtime: 4 ms (Top 33.36%) | Memory: 8.90 MB (Top 28.05%)

--- scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.cpp
+++ scripts/algorithms/B/Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.cpp
@@ -1,12 +1,20 @@
+// Runtime: 4 ms (Top 33.36%) | Memory: 8.90 MB (Top 28.05%)
+
 class Solution {
 public:
-    vector<int> v;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root!=NULL){
-            inorderTraversal(root->left);
-            v.push_back(root->val);
-            inorderTraversal(root->right);            
+        vector<int> nodes;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
         }
-        return v;
+        return nodes;
     }
 };
