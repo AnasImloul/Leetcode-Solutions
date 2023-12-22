204c1b0a7f8dbefec93f18d740488e9e9e712b75
AnasImloul <pydev27@gmail.com>
2023-12-22 19:53:46 +0100

    Runtime: 2 ms (Top 44.3%) | Memory: 6.40 MB (Top 48.64%)

--- scripts/algorithms/M/Maximum 69 Number/Maximum 69 Number.cpp
+++ scripts/algorithms/M/Maximum 69 Number/Maximum 69 Number.cpp
@@ -1,15 +1,24 @@
+// Runtime: 2 ms (Top 44.3%) | Memory: 6.40 MB (Top 48.64%)
+
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s=to_string(num);
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='6'){
-                 s[i]='9';
+        
+        string s = to_string(num);
+        
+        /* You can change at the most one digit,
+           so traverse from left and change the first
+           encountered 6  to  9  (to increase th value of num) */
+        
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(s[i] == '6')
+            {
+                s[i] = '9';
                 break;
             }
-               
         }
-        num=stoi(s);
-        return num;
+        
+        return stoi(s);
     }
 };
