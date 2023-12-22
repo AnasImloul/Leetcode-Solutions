80c31a278865baa678072971b0d8685ba58c3a8f
AnasImloul <pydev27@gmail.com>
2023-12-22 19:52:29 +0100

    Runtime: 3 ms (Top 41.4%) | Memory: 7.10 MB (Top 85.79%)

--- scripts/algorithms/I/Implement Stack using Queues/Implement Stack using Queues.cpp
+++ scripts/algorithms/I/Implement Stack using Queues/Implement Stack using Queues.cpp
@@ -1,63 +1,38 @@
-// Runtime: 8 ms (Top 6.60%) | Memory: 7 MB (Top 20.63%)
+// Runtime: 3 ms (Top 41.4%) | Memory: 7.10 MB (Top 85.79%)
+
 class MyStack {
 public:
+    /** Initialize your data structure here. */
     queue<int> q1;
-    queue<int> q2;
     MyStack() {
-
+        
     }
-
+    
+    /** Push element x onto stack. */
     void push(int x) {
+        int sz = q1.size();
         q1.push(x);
+        while(sz--){
+            q1.push(q1.front());
+            q1.pop();
+        }
     }
-
+    
+    /** Removes the element on top of the stack and returns that element. */
     int pop() {
-
-        while(q1.size() !=1){
-         int temp = q1.front();
-            q1.pop();
-            q2.push(temp);
-        }
-        int temp = q1.front();
+        int result = top();
         q1.pop();
-        while(!q2.empty()){
-            int tp = q2.front();
-            q2.pop();
-            q1.push(tp);
-        }
-        return temp;
+        return result;
     }
-
+    
+    /** Get the top element. */
     int top() {
-        while(q1.size() !=1){
-         int temp = q1.front();
-            q1.pop();
-            q2.push(temp);
-        }
-        int temp = q1.front();
-        q1.pop();
-        q2.push(temp);
-        while(!q2.empty()){
-            int tp = q2.front();
-            q2.pop();
-            q1.push(tp);
-        }
-        return temp;
+        return q1.front();
     }
-
+    
+    /** Returns whether the stack is empty. */
     bool empty() {
-        if(q1.empty()){
-            return true;
-        }
-        return false;
+        return q1.empty();
     }
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
