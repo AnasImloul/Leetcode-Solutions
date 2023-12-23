669e95e34345fda6c1a5ed1ca66236a913777f81
AnasImloul <pydev27@gmail.com>
2023-12-23 18:26:56 +0100

    Runtime: 593 ms (Top 63.06%) | Memory: 17.30 MB (Top 29.73%)

--- scripts/algorithms/K/Kth Smallest Number in Multiplication Table/Kth Smallest Number in Multiplication Table.py
+++ scripts/algorithms/K/Kth Smallest Number in Multiplication Table/Kth Smallest Number in Multiplication Table.py
@@ -1,18 +1,15 @@
+// Runtime: 593 ms (Top 63.06%) | Memory: 17.30 MB (Top 29.73%)
+
 class Solution:
-    def numSmaller(self, x, m, n):
-        res = 0
-        for i in range(1, m + 1):
-            res += min(x // i, n)
-        return res
-    
-    def findKthNumber(self, m: int, n: int, k: int) -> int:
-        beg = 1
-        end = m * n
-        while beg < end:
-            mid = (beg + end) // 2
-            curr = self.numSmaller(mid, m, n)
-            if curr < k:
-                beg = mid + 1
+    def findKthNumber(self, m, n, k):
+        def count(x):
+            return sum(min(x//i, n) for i in range(1,m+1))
+			
+        L, R, mid, ans = 0, m*n, 0, 0
+        while L <= R:
+            mid = (L + R) >> 1
+            if count(mid) < k:
+                L = mid + 1
             else:
-                end = mid
-        return beg
+                R, ans = mid - 1, mid
+        return ans
