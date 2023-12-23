232e42da8225467e77384770acbb4a3f39e07817
AnasImloul <pydev27@gmail.com>
2023-12-23 14:32:25 +0100

    Runtime: 39 ms (Top 87.58%) | Memory: 17.70 MB (Top 6.02%)

--- scripts/algorithms/D/Delete Node in a Linked List/Delete Node in a Linked List.py
+++ scripts/algorithms/D/Delete Node in a Linked List/Delete Node in a Linked List.py
@@ -1,15 +1,12 @@
-# Runtime: 84 ms (Top 11.90%) | Memory: 14.2 MB (Top 91.96%)
+// Runtime: 39 ms (Top 87.58%) | Memory: 17.70 MB (Top 6.02%)
+
 # Definition for singly-linked list.
 # class ListNode:
-# def __init__(self, x):
-# self.val = x
-# self.next = None
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
         node.val = node.next.val
         node.next = node.next.next
