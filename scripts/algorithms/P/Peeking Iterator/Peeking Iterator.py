3d60e5312bb9053a2f650bbf36c9b499ed24daf4
AnasImloul <pydev27@gmail.com>
2023-12-23 14:34:00 +0100

    Runtime: 44 ms (Top 25.31%) | Memory: 17.60 MB (Top 8.9%)

--- scripts/algorithms/P/Peeking Iterator/Peeking Iterator.py
+++ scripts/algorithms/P/Peeking Iterator/Peeking Iterator.py
@@ -1,18 +1,17 @@
+// Runtime: 44 ms (Top 25.31%) | Memory: 17.60 MB (Top 8.9%)
+
 class PeekingIterator:
-  def __init__ (self,iterator):
-    self.arr = iterator.v 
-    self._next = 0 
-    self.is_has_next = True 
-  
-  def next(self):
-    val = self.arr[self._next] 
-    self.next+=1 
-    if self.next >= len(self.arr):
-      self.is_has_next = False 
-    return val 
-  
-  def hasNext(self):
-    return self.is_has_next 
-  
-  def peek(self):
-    return self.arr[self._next]
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+        
+    def peek(self):
+        return self.buffer
+        
+    def next(self):
+        tmp = self.buffer
+        self.buffer = self.iterator.next() if self.iterator.hasNext() else None
+        return tmp
+        
+    def hasNext(self):
+        return self.buffer != None
