75094949ec7d16cefb816c0dc48d2aca4b6ca563
AnasImloul <pydev27@gmail.com>
2023-12-23 14:30:41 +0100

    Runtime: 670 ms (Top 99.03%) | Memory: 22.00 MB (Top 10.8%)

--- scripts/algorithms/L/Lemonade Change/Lemonade Change.py
+++ scripts/algorithms/L/Lemonade Change/Lemonade Change.py
@@ -1,24 +1,19 @@
-# Runtime: 1581 ms (Top 17.51%) | Memory: 18.1 MB (Top 19.63%)
+// Runtime: 670 ms (Top 99.03%) | Memory: 22.00 MB (Top 10.8%)
 
 class Solution:
-    def lemonadeChange(self, bills: List[int]) -> bool:
-
-        change = {5:0,10:0}
-        for i in bills:
-            if i==5:
-                change[5]+=1
-            elif i==10:
-                if change[5]>0:
-                    change[5]-=1
-                    change[10]+=1
-                else:
-                    return False
+    def lemonadeChange(self, bills):
+        five = ten = 0
+        for num in bills:
+            if num == 5:
+                five += 1
+            elif num == 10 and five:
+                ten += 1
+                five -= 1
+            elif num == 20 and five and ten:
+                five -= 1
+                ten -= 1
+            elif num == 20 and five >= 3:
+                five -= 3
             else:
-                if (change[10]>0) & (change[5]>0):
-                    change[10]-=1
-                    change[5]-=1
-                elif change[5]>=3:
-                    change[5]-=3
-                else:
-                    return False
+                return False
         return True
