198fe87dae68402ea3cc98c1d0948a98f7381894
AnasImloul <pydev27@gmail.com>
2023-12-23 18:30:00 +0100

    Runtime: 1248 ms (Top 52.16%) | Memory: 34.60 MB (Top 50.8%)

--- scripts/algorithms/L/Longest Palindromic Subsequence/Longest Palindromic Subsequence.py
+++ scripts/algorithms/L/Longest Palindromic Subsequence/Longest Palindromic Subsequence.py
@@ -1,13 +1,17 @@
+// Runtime: 1248 ms (Top 52.16%) | Memory: 34.60 MB (Top 50.8%)
+
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        n = len(s)
-        dp = [[0 for x in range(n)] for x in range(n)]
-        for i in range(n): dp[i][i] = 1 # Single length strings are palindrome
-        for chainLength in range(2, n+1):
-            for i in range(0, n-chainLength+1): # Discarding the lower triangle
-                j = i + chainLength - 1
-                if s[i] == s[j]: 
-                    if chainLength == 2: dp[i][j] = 2
-                    else: dp[i][j] = dp[i+1][j-1] + 2
-                else: dp[i][j] = max(dp[i+1][j], dp[i][j-1])
-        return dp[0][n-1]
+        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+        for k in range(1, len(s) + 1):
+            for i in range(len(s) - k + 1):
+                j = k + i - 1
+                if i == j:
+                    dp[i][j] = 1
+                elif i + 1 == j and s[i] == s[j]:
+                    dp[i][j] = 2
+                elif s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][-1]
