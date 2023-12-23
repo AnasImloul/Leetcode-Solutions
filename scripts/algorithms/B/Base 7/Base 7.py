261b577823899bc11dd26e5c11d348b82a1f22e4
AnasImloul <pydev27@gmail.com>
2023-12-23 18:32:02 +0100

    Runtime: 35 ms (Top 75.17%) | Memory: 17.30 MB (Top 6.4%)

--- scripts/algorithms/B/Base 7/Base 7.py
+++ scripts/algorithms/B/Base 7/Base 7.py
@@ -1,15 +1,17 @@
+// Runtime: 35 ms (Top 75.17%) | Memory: 17.30 MB (Top 6.4%)
+
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num==0: return '0'
-        flag = 1 if num>=0 else 0
-        output = ""
+        
+        if not num:
+            return '0'
+        
+        sign = num < 0
         num = abs(num)
-        pos = int(math.log(num, 7))
-        while(num>0):
-            cur = int(num/(math.pow(7, pos)))
-            output+=str(cur)
-            num -= cur*(math.pow(7, pos))
-            if pos>=1 and num==0:
-                output+="0"*pos
-            pos-=1
-        return output if flag else "-"+output
+        
+        stack = []
+        while num:
+            stack.append(str(num % 7))
+            num = num // 7
+        
+        return '-'*sign + ''.join(stack[::-1])
