94c8760c5cf2f577012c8046dd7f65fabeba69a6
AnasImloul <pydev27@gmail.com>
2023-12-23 18:35:02 +0100

    Runtime: 118 ms (Top 25.36%) | Memory: 17.90 MB (Top 5.07%)

--- scripts/algorithms/F/Frog Position After T Seconds/Frog Position After T Seconds.py
+++ scripts/algorithms/F/Frog Position After T Seconds/Frog Position After T Seconds.py
@@ -1,18 +1,21 @@
+// Runtime: 118 ms (Top 25.36%) | Memory: 17.90 MB (Top 5.07%)
+
 class Solution:
     def frogPosition(self, n: int, edges: List[List[int]], t: int, target: int) -> float:
-        adj = [[] for i in range(n)]
-        for u,v in edges:
-            adj[u-1].append(v-1)
-            adj[v-1].append(u-1)
-        def f(u,p,tt):
-            if(u==target-1): return tt==0 or len(adj[u])==(p>=0)
-            if(tt==0): return 0
-            res = 0
-            for v in adj[u]:
-                if(p==v): continue
-                res = max(res,(1/(len(adj[u])-(p!=-1)))*f(v,u,tt-1))
-            return res
-        return f(0,-1,t)
+        nei = collections.defaultdict(set)
+        for a, b in edges:
+            nei[a].add(b)
+            nei[b].add(a)
             
-            
-        
+        visited, res = set(), 0.
+        def dfs(leaf_id, p, time):
+            nonlocal res
+            if time >= t:
+                if leaf_id == target: res = p
+                return
+            visited.add(leaf_id)
+            neighbors = nei[leaf_id] - visited
+            for n in neighbors or [leaf_id]:
+                dfs(n, p / (len(neighbors) or 1), time + 1)
+        dfs(1, 1, 0)
+        return res
