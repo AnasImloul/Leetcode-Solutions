2c195baab3092e9ead674db76aa0bd8b6a950444
AnasImloul <pydev27@gmail.com>
2023-12-25 10:04:22 +0100

    Runtime: 149 ms (Top 85.0%) | Memory: 104.00 MB (Top 63.18%)

--- scripts/algorithms/M/Minimum Rounds to Complete All Tasks/Minimum Rounds to Complete All Tasks.cpp
+++ scripts/algorithms/M/Minimum Rounds to Complete All Tasks/Minimum Rounds to Complete All Tasks.cpp
@@ -1,22 +1,25 @@
+// Runtime: 149 ms (Top 85.0%) | Memory: 104.00 MB (Top 63.18%)
+
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        map<int,int> hm;
+        unordered_map<int,int> mp;
         for(int i=0;i<tasks.size();i++){
-            hm[tasks[i]]++;
+            mp[tasks[i]]++;
         }
-        int num,freq,ans=0;
-        for (auto i : hm){
-            freq = i.second;
-            if(freq==1)
+        
+        int output=0;
+        
+        for (auto freq : mp){
+            if(freq.second==1)
                 return -1;
-            if(freq%3==0){
-                ans += freq/3;
+            if(freq.second%3==0){
+                output += freq.second/3;
             }
             else{
-                ans += freq/3 + 1;
+                output += freq.second/3 + 1;
             }
         }
-        return ans;
+        return output;
     }
 };
