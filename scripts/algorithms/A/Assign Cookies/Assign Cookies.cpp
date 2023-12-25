7c5cf4cb0865d21667dcc0d4a41db37ec9ae1d3a
AnasImloul <pydev27@gmail.com>
2023-12-25 09:58:53 +0100

    Runtime: 28 ms (Top 16.33%) | Memory: 18.00 MB (Top 6.11%)

--- scripts/algorithms/A/Assign Cookies/Assign Cookies.cpp
+++ scripts/algorithms/A/Assign Cookies/Assign Cookies.cpp
@@ -1,18 +1,20 @@
+// Runtime: 28 ms (Top 16.33%) | Memory: 18.00 MB (Top 6.11%)
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n=g.size();
-        int m=s.size();
-        if(n==0 or m==0)return 0;
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int i=0,j=0;
-        while(i<m and j<n)
-        {
-            if(s[i]>=g[j])
-                j++;
-            i++;
+        sort(g.begin(), g.end()); // sort the children's greed factors in non-decreasing order
+        sort(s.begin(), s.end()); // sort the cookie sizes in non-decreasing order
+        int contentChildren = 0;
+        int i = 0; // pointer to the current child's greed factor
+        int j = 0; // pointer to the current cookie size
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) { // if the current cookie can satisfy the current child's greed factor
+                contentChildren++;
+                i++;
+            }
+            j++;
         }
-        return j;
+        return contentChildren;
     }
 };
