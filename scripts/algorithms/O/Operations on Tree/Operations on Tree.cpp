02872d95c1888e68035193d5bc26827b0ad05b64
AnasImloul <pydev27@gmail.com>
2023-12-25 09:56:46 +0100

    Runtime: 452 ms (Top 31.8%) | Memory: 134.50 MB (Top 43.59%)

--- scripts/algorithms/O/Operations on Tree/Operations on Tree.cpp
+++ scripts/algorithms/O/Operations on Tree/Operations on Tree.cpp
@@ -1,89 +1,91 @@
+// Runtime: 452 ms (Top 31.8%) | Memory: 134.50 MB (Top 43.59%)
+
 class LockingTree {
 public:
-    
-    unordered_map<int,int> locks_aquired;
-    vector<vector<int>> graph;
-    unordered_map<int,int> parents;
-    
-    LockingTree(vector<int>& parent)
-    {   
-        int n=parent.size();
-        graph =  vector<vector<int> >(n);
-        parents[0] = -1 ;   // since parent of node 0 is always -1 
-
-        for(int i=1; i<n; i++)
-        {
-            parents[i] = parent[i];  
-            graph[parent[i]].push_back(i);
-        }
-    }
-     
-    bool lock(int num, int user)    
-    {
-        if(locks_aquired.find(num) == locks_aquired.end())
-            { locks_aquired[num]=user; return 1;}
-        return 0;
-    }
-    
-    
-    bool unlock(int num, int user)      
-    {
-        if(locks_aquired.find(num) != locks_aquired.end() && locks_aquired[num]==user)
-            {   locks_aquired.erase(num); return 1;}
-        return 0;
-    }
-    
-    bool check_ans(int num)
-    {
-        while(num != -1)
-        {
-            if(locks_aquired.find(num) != locks_aquired.end()) return 0;
-            num = parents[num];   
-        }
-        return 1;
-    }
-    
-    bool dfs(int num)   // function to check if alteast one desendent is lock or not 
-    {
-        for(int i=0; i<graph[num].size(); i++)
-        {
-            int x = graph[num][i]; 
-            if( locks_aquired.find(x) != locks_aquired.end())
-                return 1;
-            if( dfs(x)) return 1;
-        }
-        return 0; 
-    }
-    
-    void unlock_descendents(int num)   
-    {
-        for(int i=0; i<graph[num].size(); i++)
-        {
-            int x = graph[num][i]; 
-            if( locks_aquired.find(x) != locks_aquired.end())
-                locks_aquired.erase(x);
-            dfs_des(x);
-        }
-    }
-    
-    bool upgrade(int num, int user) 
-    {   
-        if( locks_aquired.find(num) != locks_aquired.end()) return 0;     // if current node is locked can't lock it again 
+    unordered_map<int, vector<int>> descendents;
+    vector<vector<int>> Node;
+    /*
+        Node[i][0] = parent[i]
+        Node[i][1] = -1; (means unlocked)
+        Node[i][1] = x;  (means locked by user x)
+    */
+    int n;
+    LockingTree(vector<int>& parent) {
+        n = parent.size();
+        Node.resize(n, vector<int>(2, -1));
         
-        if( dfs(num) && check_ans(parents[num]))  
-        {
-            unlock_descendents(num); 
-            locks_aquired[num]=user;
-            return 1;
+        Node[0][0] = -1; //root has no parent
+        for(int i = 1; i<n; i++) {
+            Node[i][0] = parent[i];
+            descendents[parent[i]].push_back(i);            
         }
-        return 0;
+    }
+    
+    bool lock(int num, int user) {
+        if(Node[num][1] != -1) return false;
+        
+        Node[num][1] = user;
+        return true;
+    }
+    
+    bool unlock(int num, int user) {
+        if(Node[num][1] != user) return false;
+        
+        Node[num][1] = -1;
+        return true;
+    }
+    
+    //Condition-2 (Atleast one descendent should be locked)
+    void checkDescendents(int num, bool& atleastOne) {
+        if(descendents.count(num) == 0 || descendents[num].size() == 0)
+            return;
+        
+        for(int& x : descendents[num]) {
+            if(Node[x][1] != -1) {
+                atleastOne = true;
+                return;
+            }
+            checkDescendents(x, atleastOne);
+        }
+    }
+    
+    //Condition-3 (Check if any ancestor is locked)
+    bool IsAnyAncestorLocked(int& num) {
+        if(num == -1)
+            return false; //you reached end and found none locked
+        
+        return Node[num][1] != -1 || IsAnyAncestorLocked(Node[num][0]);
+    }
+    
+    void unlockDescendents(int num) {
+        if(descendents.count(num) == 0 || descendents[num].size() == 0)
+            return;
+        
+        for(int& x : descendents[num]) {
+            Node[x][1] = -1;
+            unlockDescendents(x);
+        }
+    }
+    
+    bool upgrade(int num, int user) {
+        //condition : 1
+        if(Node[num][1] != -1) return false;
+        
+        
+        //condition : 2
+        bool atleastOne = false;
+        checkDescendents(num, atleastOne);
+        //If no node was locked, return false
+        if(!atleastOne) return false;
+        
+        
+        //condition : 3
+        if(IsAnyAncestorLocked(Node[num][0])) return false;
+        
+        
+        //Do the rest
+        unlockDescendents(num);
+        Node[num][1] = user;
+        return true;
     }
 };
-
-/**
- * Your LockingTree object will be instantiated and called as such:
- * LockingTree* obj = new LockingTree(parent);
- * bool param_1 = obj->lock(num,user);
- * bool param_2 = obj->unlock(num,user);
- * bool param_3 = obj->upgrade(num,user);
- */
