6d9677bc34ff2edc50eb840f12ec6dd1b4332d3b
AnasImloul <pydev27@gmail.com>
2023-12-28 22:56:44 +0100

    Runtime: 118 ms (Top 7.93%) | Memory: 17.50 MB (Top 9.64%)

--- scripts/algorithms/M/Minimum Difference Between Highest and Lowest of K Scores/Minimum Difference Between Highest and Lowest of K Scores.py
+++ scripts/algorithms/M/Minimum Difference Between Highest and Lowest of K Scores/Minimum Difference Between Highest and Lowest of K Scores.py
@@ -1,6 +1,12 @@
+// Runtime: 118 ms (Top 7.93%) | Memory: 17.50 MB (Top 9.64%)
+
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         nums.sort()
-        cur = float('inf')
-        for i in range(len(nums)-k+1):
-            cur = min(cur, nums[i+k-1]-nums[i])
+        m,n=100001,len(nums)
+        i,j=0,k-1
+        while j<n:
+            m=min(m,nums[j]-nums[i])
+            i+=1
+            j+=1
+        return m
