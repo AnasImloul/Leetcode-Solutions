2c1ce5acdf57ea2768be54348ee1abf7275abbc9
AnasImloul <pydev27@gmail.com>
2023-12-28 22:00:28 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 11.80 MB (Top 48.44%)

--- scripts/algorithms/D/Determine if Two Events Have Conflict/Determine if Two Events Have Conflict.cpp
+++ scripts/algorithms/D/Determine if Two Events Have Conflict/Determine if Two Events Have Conflict.cpp
@@ -1,23 +1,8 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 11.80 MB (Top 48.44%)
+
 class Solution {
 public:
-    bool haveConflict(vector<string> &event1, vector<string> &event2) {
-        int start = stoi(event1[0].substr(3)) + stoi(event1[0].substr(0, 2)) * 60;
-        int end = stoi(event1[1].substr(3)) + stoi(event1[1].substr(0, 2)) * 60;
-        int start1 = stoi(event2[0].substr(3)) + stoi(event2[0].substr(0, 2)) * 60;
-        int end1 = stoi(event2[1].substr(3)) + stoi(event2[1].substr(0, 2)) * 60;
-        if (start >= start1 && start <= end1){
-            return true;
-        }
-        if (end >= start1 && end <= end1){
-            return true;
-        }
-        if (start1 >= start && start1 <= end){
-            return true;
-        }
-        if (end1 >= start && end1 <= end){
-            return true;
-        }
-        return false;
-
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        return !(event1[0] > event2[1] || event1[1] < event2[0]);
     }
 };
