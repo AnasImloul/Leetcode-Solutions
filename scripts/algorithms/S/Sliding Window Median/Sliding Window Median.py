b7d6d16e155cdefbf6c05ff889bba855f534f50a
AnasImloul <pydev27@gmail.com>
2023-12-28 22:34:26 +0100

    Runtime: 447 ms (Top 43.48%) | Memory: 30.40 MB (Top 31.32%)

--- scripts/algorithms/S/Sliding Window Median/Sliding Window Median.py
+++ scripts/algorithms/S/Sliding Window Median/Sliding Window Median.py
@@ -1,28 +1,34 @@
-import bisect
+// Runtime: 447 ms (Top 43.48%) | Memory: 30.40 MB (Top 31.32%)
 
+import heapq
+from collections import defaultdict
 class Solution:
-    
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        
-        half = int(k / 2)
-        
-        if k % 2 == 0:
-            i, j = half - 1, half + 1
-        else:
-            i, j = half, half + 1
-        
-        def median(l, i, j):
-            return sum(l[i:j])/len(l[i:j])
-        
-        digits = nums[:k]
-        digits.sort()
-        
-        result = [median(digits, i, j)]
-            
-        for q in range(k, len(nums)):
-            
-            digits.remove(nums[q - k])
-            bisect.insort(digits, nums[q])
-            result.append(median(digits, i, j))   
-            
-        return result
+        if not nums or not k:
+            return []
+        lo = [] # max heap
+        hi = [] # min heap
+        for i in range(k):
+            if len(lo) == len(hi):
+                heapq.heappush(hi, -heapq.heappushpop(lo, -nums[i]))
+            else:
+                heapq.heappush(lo, -heapq.heappushpop(hi, nums[i]))
+        ans = [float(hi[0])] if k & 1 else [(hi[0] - lo[0]) / 2.0]
+        to_remove = defaultdict(int)
+        for i in range(k, len(nums)): # right bound of window
+            heapq.heappush(lo, -heapq.heappushpop(hi, nums[i])) # always push to lo
+            out_num = nums[i-k]
+            if out_num > -lo[0]:
+                heapq.heappush(hi, -heapq.heappop(lo))
+            to_remove[out_num] += 1
+            while lo and to_remove[-lo[0]]:
+                to_remove[-lo[0]] -= 1
+                heapq.heappop(lo)
+            while to_remove[hi[0]]:
+                to_remove[hi[0]] -= 1
+                heapq.heappop(hi)
+            if k % 2:
+                ans.append(float(hi[0]))
+            else:
+                ans.append((hi[0] - lo[0]) / 2.0)
+        return ans
