158ca5adb7a514ac938033ba276e961c8711297b
AnasImloul <pydev27@gmail.com>
2023-12-28 23:22:26 +0100

    Runtime: 8 ms (Top 92.82%) | Memory: 7.90 MB (Top 76.07%)

--- scripts/algorithms/D/Delete Operation for Two Strings/Delete Operation for Two Strings.cpp
+++ scripts/algorithms/D/Delete Operation for Two Strings/Delete Operation for Two Strings.cpp
@@ -1,22 +1,25 @@
+// Runtime: 8 ms (Top 92.82%) | Memory: 7.90 MB (Top 76.07%)
+
 class Solution {
 public:
-    int tabulation(string s1, string s2){
-        vector<vector<int>> table(s1.length() + 1, vector<int>(s2.length() + 1));
-        for(int i=1; i<s1.length()+1; i++){
-            for(int j=1; j<s2.length()+1; j++){
-                if(s1[i-1] == s2[j-1]){
-                    table[i][j] = 1 + table[i-1][j-1];
-                }
-                else{
-                    table[i][j] = max(table[i-1][j], table[i][j-1]);
-                }
+    int lcs(string X, string Y,int m,int n){
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
             }
         }
-        return table[s1.length()][s2.length()];
+        
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(X[i-1]==Y[j-1]) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[m][n];
     }
-    
-    int minDistance(string word1, string word2) {
-        int lcs = tabulation(word1, word2);
-        return (word1.length() - lcs) + (word2.length() - lcs);
+    int minDistance(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        return m+n-2*lcs(s1,s2,m,n) ;
     }
 };
