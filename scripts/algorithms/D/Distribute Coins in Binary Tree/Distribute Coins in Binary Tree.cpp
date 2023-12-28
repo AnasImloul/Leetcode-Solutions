5d726a6a4613f7645cfb39adc9d77102017dc4d2
AnasImloul <pydev27@gmail.com>
2023-12-28 22:15:03 +0100

    Runtime: 10 ms (Top 18.75%) | Memory: 14.30 MB (Top 15.52%)

--- scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.cpp
+++ scripts/algorithms/D/Distribute Coins in Binary Tree/Distribute Coins in Binary Tree.cpp
@@ -1,19 +1,32 @@
-// Runtime: 8 ms (Top 63.16%) | Memory: 14 MB (Top 42.54%)
+// Runtime: 10 ms (Top 18.75%) | Memory: 14.30 MB (Top 15.52%)
 
 class Solution {
 public:
-    int ans = 0;
-    int recr(TreeNode *root)
+    
+    // calculate required moves in postorder manner
+    int traverse(TreeNode* root, int &moves)
     {
         if(!root) return 0;
-        int left = recr(root->left);
-        int right = recr(root->right);
-        int curr = root->val + left + right;
-        ans += abs(curr-1);
-        return curr-1;
+        
+        // left subtree excess amount coin
+        int left = traverse(root->left,moves);
+        
+        // right subtree excess amount coin
+        int right = traverse(root->right,moves);
+        
+        // moves needed to neutralize the excess amount of coins of left and right subtree
+        moves += abs(left) + abs(right);
+        
+        // current subtree including current root excess amount is 
+        // total coins in the subtree - total nodes in the subtree
+        // left and right subtree excess amount coin + root coin -1
+        return root->val + left + right -1;
     }
+    
     int distributeCoins(TreeNode* root) {
-        recr(root);
-        return ans;
+        
+        int moves = 0;
+        traverse(root,moves);
+        return moves;
     }
 };
