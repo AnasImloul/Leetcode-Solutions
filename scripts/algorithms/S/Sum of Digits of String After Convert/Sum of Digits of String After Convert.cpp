2bcf4f4df22020e2968f8d9fda1dc1370fe4c669
AnasImloul <pydev27@gmail.com>
2023-12-28 22:01:02 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.50 MB (Top 94.31%)

--- scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.cpp
+++ scripts/algorithms/S/Sum of Digits of String After Convert/Sum of Digits of String After Convert.cpp
@@ -1,25 +1,27 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.50 MB (Top 94.31%)
+
 class Solution {
 public:
+    int sumDigits(int num) {
+        int sum = 0;
+        while(num) {
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+    
     int getLucky(string s, int k) {
-        
-        long long sum=0,d;
-        string convert;
-        
-        for(auto& ch: s)
-        {
-            convert+= to_string((ch-96));
-        }
+        int sum = 0;
+        for(char &ch : s) {
+		    int val = ch-'a'+1;
+		    sum += val < 10 ? val : (val%10 + val/10);
+		}
 
-        while(k--)
-        {
-            sum=0;
-            for(auto& ch: convert)
-            {
-               sum+= (ch-48);
-            }
-            convert=to_string(sum);
-        }
-       
+        k -= 1; //Why ? We already did one transformation above (getting sum of character values)
+        while(k-- && sum >= 10) //sum >= 10 reduces unwanted Transform calls because if we are left with 1-digit,that will be our result
+            sum = sumDigits(sum);
+        
         return sum;
     }
 };
