eb2bd391b9636fc8dc1469ac1116d269959c4ae2
AnasImloul <pydev27@gmail.com>
2023-12-28 22:40:18 +0100

    Runtime: 43 ms (Top 21.7%) | Memory: 17.30 MB (Top 8.84%)

--- scripts/algorithms/S/Same Tree/Same Tree.py
+++ scripts/algorithms/S/Same Tree/Same Tree.py
@@ -1,8 +1,15 @@
+// Runtime: 43 ms (Top 21.7%) | Memory: 17.30 MB (Top 8.84%)
+
 class Solution:
-def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if not p and not q:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [[p,q]]
+        while stack:
+            p,q = stack.pop()
+            if not p and not q:					#(1)
+                continue
+            elif p and q and p.val == q.val:	#(2)
+                stack.append([p.left, q.left])
+                stack.append([p.right, q.right])
+            else:								#(3)
+                return False
         return True
-    elif not p or not q:
-        return False
-    else:
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
