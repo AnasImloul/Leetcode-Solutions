31915fe315145c93603a7638efa5b41059018f88
AnasImloul <pydev27@gmail.com>
2023-12-28 23:18:49 +0100

    Runtime: 165 ms (Top 79.54%) | Memory: 97.30 MB (Top 73.44%)

---
+++ scripts/algorithms/I/Insert Delete GetRandom O1/Insert Delete GetRandom O1.cpp
@@ -0,0 +1,37 @@
+// Runtime: 165 ms (Top 79.54%) | Memory: 97.30 MB (Top 73.44%)
+
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (m.find(val) != m.end()) return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (m.find(val) == m.end()) return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
+};
+
