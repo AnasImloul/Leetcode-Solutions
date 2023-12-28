825ae7dc0cbf92fd200ffdeace94912696ddc02e
AnasImloul <pydev27@gmail.com>
2023-12-28 16:29:59 +0100

    Runtime: 194 ms (Top 22.81%) | Memory: 17.70 MB (Top 6.09%)

--- scripts/algorithms/A/Arithmetic Subarrays/Arithmetic Subarrays.py
+++ scripts/algorithms/A/Arithmetic Subarrays/Arithmetic Subarrays.py
@@ -1,34 +1,21 @@
+// Runtime: 194 ms (Top 22.81%) | Memory: 17.70 MB (Top 6.09%)
+
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        out=[]
-        for i, j in zip(l, r):
-            out.append(self.canMakeArithmeticProgression(nums[i:j+1]))
-        return out
+        ans = []
         
-    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        minArr = min(arr)
-        maxArr = max(arr)
-		
-		# if difference between minArr and maxArr cannot be divided into equal differences, then return false
-        if (maxArr-minArr)%(len(arr)-1)!=0:
-            return False
-			
-		# consecutive difference in arithmetic progression
-        diff = int((maxArr-minArr)/(len(arr)-1))
-        if diff == 0:
-            if arr != [arr[0]]*len(arr):
-                return False
-            return True
-		
-		# array to check all numbers in A.P. are present in input array.
-		# A.P.[minArr, minArr+d, minArr+2d, . . . . . . . maxArr]
-        check = [1]*len(arr)
-        for num in arr:
-            if (num-minArr)%diff != 0:
-                return False
-            check[(num-minArr)//diff]=0
-		
-		# if 1 is still in check array it means at least one number from A.P. is missing from input array.
-        if 1 in check:
-            return False
-        return True
+        def find_diffs(arr):
+            
+            arr.sort()
+
+            dif = []
+            
+            for i in range(len(arr) - 1):
+                dif.append(arr[i] - arr[i + 1])
+            
+            return len(set(dif)) == 1
+        
+        for i , j in zip(l , r):
+            ans.append(find_diffs(nums[i:j + 1]))
+        
+        return ans
