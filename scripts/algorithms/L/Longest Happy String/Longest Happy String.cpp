1f522f828964a37398c28be9c2ef3467414f80cc
AnasImloul <pydev27@gmail.com>
2023-12-28 23:16:46 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.60 MB (Top 42.97%)

--- scripts/algorithms/L/Longest Happy String/Longest Happy String.cpp
+++ scripts/algorithms/L/Longest Happy String/Longest Happy String.cpp
@@ -1,32 +1,53 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.60 MB (Top 42.97%)
+
 class Solution {
 public:
-	#define f first    
-	#define s second
-	string longestDiverseString(int a, int b, int c) {
-		priority_queue<pair<int,char>> pq;
-		if(a>0)pq.push({a,'a'});        
-		if(b>0)pq.push({b,'b'});        
-		if(c>0)pq.push({c,'c'});
-
-		string ans  = "";
-		while(!pq.empty()){
-			auto t = pq.top(); pq.pop();
-			int c = t.f;
-			char ch = t.s;
-			if(ans.size() > 1 && ans[ans.size()-1]==ch && ch == ans[ans.size()-2]) {
-				if(pq.empty())break;
-				auto t = pq.top(); pq.pop();
-				int c2 = t.f;
-				char ch2 = t.s;
-				ans += ch2;
-				c2-=1;
-				if(c2)pq.push({c2,ch2});
-			}else{
-				ans += ch;
-				c-=1;
-			}
-			if(c)pq.push({c,ch});
-		}
-		return ans;
-	}
+    string longestDiverseString(int a, int b, int c) {
+        //using max heap
+        priority_queue<pair<int,char>>pq;
+        if(a)
+        pq.push({a,'a'});
+        if(b)
+        pq.push({b,'b'});
+        if(c)
+        pq.push({c,'c'});
+        string ans="";
+        while(pq.size()>1){
+            pair<int,char>one = pq.top();pq.pop();
+            pair<int,char>two = pq.top();pq.pop();
+            if(one.first>=2){
+                ans+=one.second;
+                ans+=one.second;
+                one.first-=2;
+            }
+            else{
+                ans+=one.second;
+                one.first-=1;
+            }
+            if(two.first>=2 && two.first>=one.first){
+                ans+=two.second;
+                ans+=two.second;
+                two.first-=2;
+            }
+            else{
+                ans+=two.second;
+                two.first-=1;
+            }
+            if(one.first>0)
+                pq.push(one);
+            if(two.first>0)
+                pq.push(two);
+        }
+        if(pq.empty())
+            return ans;
+        if(pq.top().first>=2){
+            ans+=pq.top().second;
+            ans+=pq.top().second;
+        }
+        else{
+            ans+=pq.top().second;
+        }
+        return ans;
+        
+    }
 };
