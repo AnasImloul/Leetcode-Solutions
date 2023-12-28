6130707011e9900f5d74a020395d1989e7d3d494
AnasImloul <pydev27@gmail.com>
2023-12-28 21:59:21 +0100

    Runtime: 138 ms (Top 5.04%) | Memory: 61.50 MB (Top 67.87%)

--- scripts/algorithms/S/Subarray Product Less Than K/Subarray Product Less Than K.cpp
+++ scripts/algorithms/S/Subarray Product Less Than K/Subarray Product Less Than K.cpp
@@ -1,19 +1,17 @@
+// Runtime: 138 ms (Top 5.04%) | Memory: 61.50 MB (Top 67.87%)
+
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int start = 0;
-        long prod = 1;
-        int count =0; // count of subarray prod  less than k
-        for(int end =0; end< nums.size(); end++){
-            prod *= nums[end];
-    
-           while(prod >= k && start < nums.size()){
-               prod = prod/nums[start];// divide product by nums at start pointer t reduce the prod
-                start++;//move start pointer because no longer nums at start can give us prod < k
-              }
-           if(prod < k)
-            count += end - start +1;
-      }
-    return count;
+        int n = nums.size(), ans = 0, prod = 1, j = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && i <= j) 
+                prod =  prod/nums[i-1];
+            else 
+                j = i;
+            while (j < n &&  prod*nums[j] < k) prod *= nums[j++];
+            ans += j-i;
+        }
+        return ans;
     }
 };
