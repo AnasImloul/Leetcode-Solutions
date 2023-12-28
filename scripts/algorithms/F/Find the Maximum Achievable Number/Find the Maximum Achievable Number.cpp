6f0a5431f5ad8c068229d9dd83f0d17c1464505b
AnasImloul <pydev27@gmail.com>
2023-12-28 23:20:17 +0100

    Runtime: 2 ms (Top 53.64%) | Memory: 6.20 MB (Top 75.08%)

---
+++ scripts/algorithms/F/Find the Maximum Achievable Number/Find the Maximum Achievable Number.cpp
@@ -0,0 +1,9 @@
+// Runtime: 2 ms (Top 53.64%) | Memory: 6.20 MB (Top 75.08%)
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        return num+(t*2);    
+    }
+};
