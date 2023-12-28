e9bb7ec9f1124ad7c2a4b9d977a49b6ffe408064
AnasImloul <pydev27@gmail.com>
2023-12-28 22:36:03 +0100

    Runtime: 35 ms (Top 80.71%) | Memory: 17.30 MB (Top 5.56%)

--- scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.py
+++ scripts/algorithms/B/Binary Tree Pruning/Binary Tree Pruning.py
@@ -1,24 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+// Runtime: 35 ms (Top 80.71%) | Memory: 17.30 MB (Top 5.56%)
+
 class Solution:
-    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
-        def dfs(node):
-            if not node:
-                return None
-            
-            left = dfs(node.left)
-            right = dfs(node.right)
-            if node.val == 0 and not left and not right:
-                return None
-            else:
-                node.left = left
-                node.right = right
-                return node
-        
-        return dfs(root)
-            
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root: return root
+        root.left, root.right = self.pruneTree(root.left), self.pruneTree(root.right)
+        return None if not root.val and not root.left and not root.right else root
