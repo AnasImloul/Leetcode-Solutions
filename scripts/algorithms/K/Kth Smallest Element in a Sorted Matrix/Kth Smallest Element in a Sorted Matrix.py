c45699b45b752d1368567fed151b4f0b1332cba1
AnasImloul <pydev27@gmail.com>
2023-12-28 22:49:53 +0100

    Runtime: 281 ms (Top 5.06%) | Memory: 22.00 MB (Top 9.58%)

--- scripts/algorithms/K/Kth Smallest Element in a Sorted Matrix/Kth Smallest Element in a Sorted Matrix.py
+++ scripts/algorithms/K/Kth Smallest Element in a Sorted Matrix/Kth Smallest Element in a Sorted Matrix.py
@@ -1,3 +1,21 @@
+// Runtime: 281 ms (Top 5.06%) | Memory: 22.00 MB (Top 9.58%)
+
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return sorted(list(sum(matrix, [])))[k-1]
+    def kthSmallest(self, matrix, k):
+        n, beg, end = len(matrix), matrix[0][0], matrix[-1][-1]
+        
+        def check(m):
+            i, j, cnt = 0, n-1, 0
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > m: j -= 1
+                cnt += (j + 1)
+            return cnt
+         
+        while beg < end:
+            mid = (beg + end)//2
+            if check(mid) < k:
+                beg = mid + 1
+            else:
+                end = mid
+                
+        return beg
