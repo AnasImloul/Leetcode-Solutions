cca2b02265aa240e885771ecfd363f83abe30492
AnasImloul <pydev27@gmail.com>
2023-12-28 22:37:38 +0100

    Runtime: 4822 ms (Top 24.41%) | Memory: 38.50 MB (Top 62.2%)

--- scripts/algorithms/M/Minimum Degree of a Connected Trio in a Graph/Minimum Degree of a Connected Trio in a Graph.py
+++ scripts/algorithms/M/Minimum Degree of a Connected Trio in a Graph/Minimum Degree of a Connected Trio in a Graph.py
@@ -1,24 +1,20 @@
-# Runtime: 1826 ms (Top 63.02%) | Memory: 50.7 MB (Top 5.73%)
+// Runtime: 4822 ms (Top 24.41%) | Memory: 38.50 MB (Top 62.2%)
+
 class Solution:
     def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
-        result = n ** 2 # placeholder value
-        adj = [set() for _ in range(n + 1)]
-        visited = set()
-        for edge in edges:
-            adj[edge[0]].add(edge[1])
-            adj[edge[1]].add(edge[0])
-        for first in range(1, n + 1): # iterate through every node
-            for second in adj[first]: # iterate through every neighbor of the first node
-                e1 = tuple(sorted((first, second)))
-                if e1 in visited:
-                    continue
-                visited.add(e1) # mark e1 (first -> second) as visited because we don't want to visit (second -> first) again
-                for third in adj[second]:
-                    if third == first: # skip if the third node is the first node (we need the first node to be its neighbor rather than itself!)
-                        continue
-                    e2 = tuple(sorted((first, third)))
-                    if first in adj[third]: # we don't check if e2 is in visited because the third node is not the current node being explored
-                        visited.add(e2) # we need to mark e2 as visited because one end of e2 is the first node and if we have checked (third -> first), we don't need to check (first -> third) again
-                        degree = len(adj[first]) - 2 + len(adj[second]) - 2 + len(adj[third]) - 2
-                        result = min(result, degree)
-        return -1 if result == n ** 2 else result
+        graph = [[False]*n for _ in range(n)]
+        degree = [0]*n
+        
+        for u, v in edges: 
+            graph[u-1][v-1] = graph[v-1][u-1] = True
+            degree[u-1] += 1
+            degree[v-1] += 1
+        
+        ans = inf
+        for i in range(n): 
+            for j in range(i+1, n):
+                if graph[i][j]: 
+                    for k in range(j+1, n):
+                        if graph[j][k] and graph[k][i]: 
+                            ans = min(ans, degree[i] + degree[j] + degree[k] - 6)
+        return ans if ans < inf else -1
