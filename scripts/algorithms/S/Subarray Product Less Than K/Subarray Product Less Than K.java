bc7db112d94ae7d933ed3877eda0b4ed13af5f32
AnasImloul <pydev27@gmail.com>
2023-12-28 15:54:40 +0100

    Runtime: 14 ms (Top 19.32%) | Memory: 47.80 MB (Top 5.49%)

--- scripts/algorithms/S/Subarray Product Less Than K/Subarray Product Less Than K.java
+++ scripts/algorithms/S/Subarray Product Less Than K/Subarray Product Less Than K.java
@@ -1,37 +1,21 @@
+// Runtime: 14 ms (Top 19.32%) | Memory: 47.80 MB (Top 5.49%)
+
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-		//if k=0 then ans will always be zero as we have positive integers array only.
-        if(k==0)
-            return 0;
-        
-        int length = 0;
-        long product = 1;
+        int n = nums.length;
+        long p = 1l;
         int i = 0;
         int j = 0;
-        int n = nums.length;
-        int ans = 0;
-        
-        while(j<n){
-            product*=nums[j];
-			//Add the number to current window if the product is less than k and calculate no of subarrays using length only.
-            if(product<k){
-                length+=1;
-                ans+=length;
+        int total = 0;
+        while(j < n){
+            p *= nums[j];
+            while(i <= j&&p >= k){
+                p /= nums[i];
+                i++;
             }
-            else{
-				//Remove element one by one till product becomes less than k.
-                while(i<=j && product>=k){
-                    product/=nums[i];
-                    i++;
-                }
-				//As we have added only 1 element to the window and this element can make subarray to j-i element along with itself.
-                ans+=(j-i)+1;
-				//Update the current subarray length.
-                length=j-i+1;
-            }
+            total += (j - i + 1);
             j++;
         }
-        
-        return ans;
+        return total;
     }
 }
