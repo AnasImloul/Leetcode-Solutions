5fe18d0086420982099a22c8268c45f39211f918
AnasImloul <pydev27@gmail.com>
2023-12-28 22:31:01 +0100

    Runtime: 2191 ms (Top 5.08%) | Memory: 62.60 MB (Top 7.94%)

--- scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.py
+++ scripts/algorithms/M/Merge Triplets to Form Target Triplet/Merge Triplets to Form Target Triplet.py
@@ -1,26 +1,12 @@
-# Runtime: 8217 ms (Top 5.03%) | Memory: 52.2 MB (Top 99.70%)
+// Runtime: 2191 ms (Top 5.08%) | Memory: 62.60 MB (Top 7.94%)
+
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-
-        i=0
-        while True:
-            if i==len(triplets):
-                break
-            for j in range(3):
-                if triplets[i][j]>target[j]:
-                    triplets.pop(i)
-                    i-=1
-                    break
-            i+=1
-
-        a = [x[0] for x in triplets]
-        b = [x[1] for x in triplets]
-        c = [x[2] for x in triplets]
-
-        if target[0] not in a:
-            return False
-        if target[1] not in b:
-            return False
-        if target[2] not in c:
-            return False
-        return True
+        
+        a, b, c = 0, 0, 0
+        for i, (x, y, z) in enumerate(triplets):
+                if not(  x > target[0] or y > target[1] or z > target[2]):
+                     a, b, c = max(a, x), max(b, y), max(c, z)
+                        
+        return [a, b, c] == target
+            
