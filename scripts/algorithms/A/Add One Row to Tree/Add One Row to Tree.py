c7698aca34a025d5eaa7c11c8c47c24d3a6680ad
AnasImloul <pydev27@gmail.com>
2023-12-28 16:26:20 +0100

    Runtime: 77 ms (Top 6.3%) | Memory: 18.50 MB (Top 98.9%)

--- scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.py
+++ scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.py
@@ -1,39 +1,12 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
+// Runtime: 77 ms (Top 6.3%) | Memory: 18.50 MB (Top 98.9%)
 
 class Solution:
-
-    def Solve(
-        self,
-        root,
-        val,
-        depth,
-        curr,
-        ):
-        if root == None:
-            return None
-        if depth == 1:
-            return TreeNode(val, root)
-        if curr == depth - 1:
-            (left, right) = (root.left, root.right)
-            (root.left, root.right) = (TreeNode(val, left),
-                    TreeNode(val, None, right))
-            return root
-        self.Solve(root.left, val, depth, curr + 1)
-        self.Solve(root.right, val, depth, curr + 1)
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        if d == 1: return TreeNode(v, root, None)
+        elif d == 2:
+            root.left = TreeNode(v, root.left, None)
+            root.right = TreeNode(v, None, root.right)
+        else:
+            if root.left: self.addOneRow(root.left, v, d-1)
+            if root.right: self.addOneRow(root.right, v, d-1)
         return root
-
-    def addOneRow(
-        self,
-        root,
-        val,
-        depth,
-        ):
-        return self.Solve(root, val, depth, 1)
