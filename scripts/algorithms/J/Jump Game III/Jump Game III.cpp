e2e355896c55e1c88420a931ed85c48cbec2ec16
AnasImloul <pydev27@gmail.com>
2023-12-28 22:07:43 +0100

    Runtime: 26 ms (Top 94.29%) | Memory: 47.00 MB (Top 27.2%)

--- scripts/algorithms/J/Jump Game III/Jump Game III.cpp
+++ scripts/algorithms/J/Jump Game III/Jump Game III.cpp
@@ -1,21 +1,17 @@
+// Runtime: 26 ms (Top 94.29%) | Memory: 47.00 MB (Top 27.2%)
+
 class Solution {
 public:
-    bool dfs(int curr, vector<int>& arr, vector<bool>& visited, int size) {
-		// edge cases - we can't go outside the array size and if the curr index is alredy visited it will repeat same recursion all over again so we can return false in that case too.
-        if(curr < 0 || curr >= size || visited[curr])
-            return false;
-		// If we reach zero we can return true
-        if(arr[curr] == 0)
-            return true;
-        visited[curr] = true;
-		// do dfs in left and right and return two if any of the two paths end up reaching to zero
-        return dfs(curr - arr[curr], arr, visited, size) || 
-            dfs(curr + arr[curr], arr, visited, size);
+    
+    bool ff(vector<int>& arr, vector<bool>& visited, int i) {
+        if (i < 0 || i >= arr.size() || visited[i]) return false;
+        if (arr[i] == 0) return true;
+        visited[i] = true;
+        return ff(arr, visited, i + arr[i]) || ff(arr, visited, i - arr[i]);
     }
     
     bool canReach(vector<int>& arr, int start) {
-        int size = arr.size();
-        vector<bool> visited(size, false);
-        return dfs(start, arr, visited, size);
+        vector<bool> visited(arr.size(), false);
+        return ff(arr, visited, start);
     }
 };
