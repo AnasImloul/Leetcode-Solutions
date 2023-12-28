58426d7855864d71a1236567c27a937de3032fe8
AnasImloul <pydev27@gmail.com>
2023-12-28 23:15:11 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 7.50 MB (Top 80.85%)

--- scripts/algorithms/D/Decrease Elements To Make Array Zigzag/Decrease Elements To Make Array Zigzag.cpp
+++ scripts/algorithms/D/Decrease Elements To Make Array Zigzag/Decrease Elements To Make Array Zigzag.cpp
@@ -1,19 +1,36 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 7.50 MB (Top 80.85%)
+
 class Solution {
 public:
     int movesToMakeZigzag(vector<int>& nums) {
-        int oddSum = 0, evenSum = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            if(i%2 == 0){
-                int left = i==0?INT_MAX:nums[i-1];
-                int right = i==n-1?INT_MAX:nums[i+1];
-                evenSum += max(nums[i]-min(left,right)+1,0);
-            }else{
-                int left = i==0?INT_MAX:nums[i-1];
-                int right = i==n-1?INT_MAX:nums[i+1];
-                oddSum += max(nums[i]-min(left,right)+1,0);
-            }
-        }
-        return min(evenSum,oddSum);
+        int even = 0, odd = 0;
+
+		if (nums.size() <= 1) return 0;
+
+        /* Even indices are smaller */
+        for (int i = 0; i < nums.size(); i+= 2) {
+			if (i == 0) {
+				if (nums[i] >= nums[i+1]) even += nums[i]-nums[i+1]+1;
+			} else if (i == nums.size() - 1) {
+				if (nums[i-1] <= nums[i]) even += nums[i]-nums[i-1]+1;
+			} else {
+				if (nums[i-1] <= nums[i] || nums[i+1] <= nums[i]) {
+					even += nums[i] - min(nums[i-1], nums[i+1]) + 1;
+				}
+			}
+		}
+        
+        /* Odd indices are smaller */
+		for (int i = 1; i < nums.size(); i += 2) {
+			if (i == nums.size() - 1) {
+				if (nums[i-1] <= nums[i]) odd += nums[i]-nums[i-1]+1;
+			} else {
+				if (nums[i-1] <= nums[i] || nums[i+1] <= nums[i]) {
+					odd += nums[i] - min(nums[i-1], nums[i+1]) + 1;
+				}
+			}
+		}
+
+		return min(even, odd);
     }
 };
