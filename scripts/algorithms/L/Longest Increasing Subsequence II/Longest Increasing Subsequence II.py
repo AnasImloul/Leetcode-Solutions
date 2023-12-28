b6f0f97d15dfae31d8883ece927b1a38eb4dea50
AnasImloul <pydev27@gmail.com>
2023-12-28 22:45:49 +0100

    Runtime: 1618 ms (Top 81.94%) | Memory: 27.50 MB (Top 50.93%)

---
+++ scripts/algorithms/L/Longest Increasing Subsequence II/Longest Increasing Subsequence II.py
@@ -0,0 +1,39 @@
+// Runtime: 1618 ms (Top 81.94%) | Memory: 27.50 MB (Top 50.93%)
+
+class SEG:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * 2 * self.n
+       
+    def query(self, l, r):
+        l += self.n
+        r += self.n
+        ans = 0
+        while l < r:
+            if l & 1:
+                ans = max(ans, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                ans = max(ans, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return ans
+    
+    def update(self, i, val):
+        i += self.n
+        self.tree[i] = val
+        while i > 1:
+            i >>= 1
+            self.tree[i] = max(self.tree[i * 2], self.tree[i * 2 + 1])
+
+class Solution:
+    def lengthOfLIS(self, A: List[int], k: int) -> int:
+        n, ans = max(A), 1
+        seg = SEG(n)
+        for a in A:
+            a -= 1
+            premax = seg.query(max(0, a - k), a)
+            ans = max(ans, premax + 1)
+            seg.update(a, premax + 1)
+        return ans
