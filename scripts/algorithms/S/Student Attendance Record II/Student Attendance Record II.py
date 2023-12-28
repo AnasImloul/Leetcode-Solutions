95790c5d3ed0021df94d07b0e47bbc3ffbb5168b
AnasImloul <pydev27@gmail.com>
2023-12-28 22:33:22 +0100

    Runtime: 228 ms (Top 90.78%) | Memory: 35.80 MB (Top 36.89%)

--- scripts/algorithms/S/Student Attendance Record II/Student Attendance Record II.py
+++ scripts/algorithms/S/Student Attendance Record II/Student Attendance Record II.py
@@ -1,44 +1,36 @@
+// Runtime: 228 ms (Top 90.78%) | Memory: 35.80 MB (Top 36.89%)
+
+import numpy as np
+
 class Solution:
+    
     def checkRecord(self, n: int) -> int:
-        if n == 1:
-            return 3
-        if n==2:
-            return 8
-        """
-        Keep track of last 2 digits
-        ways to get ll = last combinations ending with p,l (l,l not allowed)
-        ways to get lp = last combinations ending with l,p + p,l
-        and so on
-        
-        finally account for adding A by the usual loop.
-        """
-        
-        combos_l_l = 1
-        combos_l_p = 1
-        combos_p_l = 1
-        combos_p_p = 1
+        MODULUS = 10**9 + 7
 
-        MOD = 1000000007
-        
-        f = [0]*(n+1)
-        f[0] = 1
-        f[1] = 2
-        f[2] = 4
-        for i in range(2, n):
-            combos_l_l_new = combos_p_l
-            combos_l_p_new = combos_l_l + combos_p_l
-            combos_p_l_new = combos_l_p + combos_p_p 
-            combos_p_p_new = combos_l_p + combos_p_p
-            
-            combos_l_l, combos_l_p, combos_p_l, combos_p_p = combos_l_l_new%MOD, combos_l_p_new%MOD, combos_p_l_new%MOD, combos_p_p_new  %MOD
-            
-            f[i+1] = (combos_l_l + combos_l_p + combos_p_l + combos_p_p)%MOD
-            
+        initial_counts = np.array(
+            [1, 0, 0, 0, 0, 0], 
+            dtype=np.int64
+        )
 
-        total = f[-1]
-        # print(f)
-        for i in range(1,n+1):
-            total += (f[i-1]*f[n-i]) 
-            
-        return ( total ) % (MOD)
-            
+        adjacency_matrix = np.array([
+            [1, 1, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0],
+        ], dtype=np.int64)
+
+        def power(A, exp):
+            B = np.identity(len(A), dtype=np.int64)
+            for bit in reversed(bin(exp)[2:]):
+                if bit == '1':
+                    B = B @ A
+                    B %= MODULUS
+                A = A @ A
+                A %= MODULUS
+            return B
+
+        final_counts = power(adjacency_matrix, n) @ initial_counts
+
+        return sum(final_counts) % MODULUS
