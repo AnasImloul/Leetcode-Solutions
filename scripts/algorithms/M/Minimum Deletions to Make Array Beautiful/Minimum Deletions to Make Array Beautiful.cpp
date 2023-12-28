0a6f9949b5ad8de36312c6aa33b86aff219f278e
AnasImloul <pydev27@gmail.com>
2023-12-28 21:45:57 +0100

    Runtime: 145 ms (Top 30.03%) | Memory: 121.50 MB (Top 63.37%)

--- scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.cpp
+++ scripts/algorithms/M/Minimum Deletions to Make Array Beautiful/Minimum Deletions to Make Array Beautiful.cpp
@@ -1,32 +1,15 @@
-INTUITION
-1. since we have to find the minimum deletions, we dont have to 
-	actually delete the elements , we just have to count those elements.
-2. Now if we delete the element and shift all the elements towards left , it will
-	cause time limit exceeded.
-3. To handle above case we can  observe one thing that, if we delete some element
-	at a certain position then the indices of all the elements towards the right 
-	will get inverted means the odd index will become even and the even will become odd.
-4. In the end checking if the vector size if even or odd, if it is even simply return or else if it 
-	is odd then decrement result by 1 since we have to remove one element to have the vector size even.
+// Runtime: 145 ms (Top 30.03%) | Memory: 121.50 MB (Top 63.37%)
+
 class Solution {
-public:
-    int minDeletion(vector<int>& nums) {
-        int res=0,n=nums.size(),i;
-        int flag=1;
-            for(i=0;i<nums.size()-1;i++){
-                    if(i%2==0 and nums[i]==nums[i+1]  and flag){
-                        res++;
-                        flag=0;
-                    }
-                    else if(i%2==1 and nums[i]==nums[i+1] and flag==0){
-                        res++;
-                        flag=1;
-                    }
-                }
-        int x=n-res;
-        if(x%2==0){
-            return res;
+    public:
+        int minDeletion(vector < int > & nums) {
+            bool even = true;
+            int size = 0, c = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                while (i + 1 < nums.size() && even && nums[i] == nums[i + 1]) i++, c++;
+                size++;
+                even = !even;
+            }
+            return c + size % 2;
         }
-        return res+1;
-    }
 };
