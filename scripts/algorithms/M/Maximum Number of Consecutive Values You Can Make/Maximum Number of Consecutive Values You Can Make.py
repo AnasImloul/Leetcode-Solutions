03516925672b827a3d180c9a0b12065f9eba7b60
AnasImloul <pydev27@gmail.com>
2023-12-28 22:27:56 +0100

    Runtime: 576 ms (Top 97.83%) | Memory: 23.00 MB (Top 5.22%)

--- scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.py
+++ scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.py
@@ -1,11 +1,16 @@
+// Runtime: 576 ms (Top 97.83%) | Memory: 23.00 MB (Top 5.22%)
+
 class Solution:
     def getMaximumConsecutive(self, coins: List[int]) -> int:
+        cur_max = 0
         coins.sort()
-        
-        res = 1
-        
+      
         for coin in coins:
-            if (res >= coin):
-                res += coin
+            if coin == 1:
+                cur_max += 1
+            elif coin <= cur_max+1:
+                cur_max += coin
+            else: #coin > cur_max + 1
+                break
         
-        return res
+        return cur_max+1
