dc81cd3acd2b739f2519cc9ba987f7b9badfbbbb
AnasImloul <pydev27@gmail.com>
2023-12-28 23:23:41 +0100

    Runtime: 107 ms (Top 16.47%) | Memory: 41.90 MB (Top 63.1%)

--- scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.cpp
+++ scripts/algorithms/S/Subarray Sum Equals K/Subarray Sum Equals K.cpp
@@ -1,27 +1,38 @@
-
-/********************************* Solution Using HashMap / Prefix ********************/
+// Runtime: 107 ms (Top 16.47%) | Memory: 41.90 MB (Top 63.1%)
 
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-       
-        unordered_map<int, int> prefixSum {{0, 1}};
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size(); // take the size of the array
         
-        int sum = 0;
-        int numOfSubArr = 0;
-        int size = nums.size();
+        int prefix[n]; // make a prefix array to store prefix sum
         
+        prefix[0] = arr[0]; // for element at index at zero, it is same
         
-        for (int index = 0; index < size; index++){
-            
-            sum += nums[index];
-            if (prefixSum.find(sum-k) != prefixSum.end()) 
-                numOfSubArr += prefixSum[sum-k];
-            
-            prefixSum[sum]++;
+        // making our prefix array
+        for(int i = 1; i < n; i++)
+        {
+            prefix[i] = arr[i] + prefix[i - 1];
         }
         
-        return numOfSubArr;
+        unordered_map<int,int> mp; // declare an unordered map
+        
+        int ans = 0; // to store the number of our subarrays having sum as 'k'
+        
+        for(int i = 0; i < n; i++) // traverse from the prefix array
+        {
+            if(prefix[i] == k) // if it already becomes equal to k, then increment ans
+                ans++;
+            
+            // now, as we discussed find whether (prefix[i] - k) present in map or not
+            if(mp.find(prefix[i] - k) != mp.end())
+            {
+                ans += mp[prefix[i] - k]; // if yes, then add it our answer
+            }
+            
+            mp[prefix[i]]++; // put prefix sum into our map
+        }
+        
+        return ans; // and at last, return our answer
     }
 };
-
