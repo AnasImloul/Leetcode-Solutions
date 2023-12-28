f83b6303964944097042cc311b57b69dc0d6e8b9
AnasImloul <pydev27@gmail.com>
2023-12-28 22:55:40 +0100

    Runtime: 33 ms (Top 98.18%) | Memory: 17.40 MB (Top 5.01%)

--- scripts/algorithms/T/Two Furthest Houses With Different Colors/Two Furthest Houses With Different Colors.py
+++ scripts/algorithms/T/Two Furthest Houses With Different Colors/Two Furthest Houses With Different Colors.py
@@ -1,11 +1,26 @@
-# Runtime: 75 ms (Top 33.45%) | Memory: 13.7 MB (Top 99.77%)
+// Runtime: 33 ms (Top 98.18%) | Memory: 17.40 MB (Top 5.01%)
+
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
-        p, res = inf, 0
-        for i, c in enumerate(colors):
-            if (c != colors[0]):
-                res = i
-                p = min(p, i)
-            else:
-                res = max(res, i - p)
-        return res
+		#first pass
+        l, r = 0, len(colors)-1
+        dist = 0
+        
+        while r > l:
+            if colors[r] != colors[l]:
+                dist = r-l
+				#slight performance increase, break out if you find it 
+				#because it can't get bigger than this
+                break 
+            r -= 1
+			
+        #second pass, backwards
+        l, r = 0, len(colors)-1
+        while r > l:
+            if colors[r] != colors[l]:
+                dist = max(dist, r-l)
+                break
+            l += 1
+        
+        return dist
+	
