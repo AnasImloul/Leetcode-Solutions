d2882fe7821d56da6a3617a684bb30abd0c88140
AnasImloul <pydev27@gmail.com>
2023-12-28 23:18:01 +0100

    Runtime: 116 ms (Top 82.06%) | Memory: 66.60 MB (Top 59.64%)

--- scripts/algorithms/A/Append K Integers With Minimal Sum/Append K Integers With Minimal Sum.cpp
+++ scripts/algorithms/A/Append K Integers With Minimal Sum/Append K Integers With Minimal Sum.cpp
@@ -1,28 +1,59 @@
+// Runtime: 116 ms (Top 82.06%) | Memory: 66.60 MB (Top 59.64%)
+
 class Solution {
 public:
-    long long minimalKSum(vector<int>& nums, int k){
-        sort(nums.begin(), nums.end());
-        long long s = 0;
-        int i = 0;
-        int prev = 0;
-        while(k>0 && i<nums.size()){
-            int len = nums[i] - prev - 1;
-            if(len>k) len = k;
-            if(len>0){
-                long long int start = prev;
-                long long int end = start + len;
-                long long int sum = (end*(end+1))/2 - (start*(start+1))/2;
-                s += sum;
-                k -= len;
+    long long minimalKSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        // Sort array so that we can get smaller elements first
+        sort(begin(nums), end(nums));
+        
+        long long sum = 0;
+        
+        // Previous element that we encountered
+        int prev = 0, curr = 0;
+        
+        for(int i=0; i<n; i++) {
+            
+            curr = nums[i];
+            
+            // Get the difference b/w prev and current 
+            // So that we can append elements that are in between them
+            long long diff = (curr - prev - 1);
+            
+            // If prev and current are same then just skip
+            if(diff <= 0) {
+                prev = curr;
+                continue;
             }
-            prev = nums[i]; i++;
+            
+            // If there are more available elements b/w prev and current
+            // Then we just take closest k element from prev.
+            // And leave remaining as it is
+            if(diff > k) {
+                diff = k;
+                curr = prev + k + 1;
+            }
+            
+            // Get the sum of all elements b/w prev and current
+            // Since it is AP series, we can use direct formula
+            sum += (diff * 1LL * (curr + prev) / 2);
+            
+            // Update previous to current
+            prev = curr;
+            
+            // Update count of how many more element we need to append 
+            k -= diff;
+            
+            if(k == 0) break;
         }
-        if(k>0){
-            long long int start = prev;
-            long long int end = start + k;
-            long long int sum = (end*(end+1))/2 - (start*(start+1))/2;
-            s += sum;
+        
+        // Case : When we have reached the end of array 
+        // And still we have some more element left to append
+        if(k) {
+            sum += (k * 1LL * (2 * prev + k + 1) / 2);
         }
-        return s;
+        
+        return sum;
     }
 };
