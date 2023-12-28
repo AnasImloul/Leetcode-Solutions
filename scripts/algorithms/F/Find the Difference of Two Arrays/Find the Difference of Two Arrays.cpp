047c90f1fbe3eb8fa8790836c5a1f8a7c14751a7
AnasImloul <pydev27@gmail.com>
2023-12-28 22:13:54 +0100

    Runtime: 94 ms (Top 8.91%) | Memory: 32.60 MB (Top 77.65%)

--- scripts/algorithms/F/Find the Difference of Two Arrays/Find the Difference of Two Arrays.cpp
+++ scripts/algorithms/F/Find the Difference of Two Arrays/Find the Difference of Two Arrays.cpp
@@ -1,31 +1,20 @@
+// Runtime: 94 ms (Top 8.91%) | Memory: 32.60 MB (Top 77.65%)
+
+// OJ: https://leetcode.com/contest/weekly-contest-286/problems/find-the-difference-of-two-arrays/
+// Author: github.com/lzl124631x
+// Time: O(A + B)
+// Space: O(A + B)
 class Solution {
 public:
-    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-	//unordered_set is implemented using a hash table where keys are hashed into indices of a hash table
-	// all operations take O(1) on average
-        unordered_set<int> n1;
-        unordered_set<int> n2;
-        for(int i=0;i<nums1.size();i++)
-            n1.insert(nums1[i]);
-        
-        for(int i=0;i<nums2.size();i++)
-            n2.insert(nums2[i]);
-        
-        vector<int> ans1;
-		vector<vector<int>> ans;
-        for(int x:n1)
-        {
-            if(n2.find(x)==n2.end())
-                ans1.push_back(x);
+    vector<vector<int>> findDifference(vector<int>& A, vector<int>& B) {
+        unordered_set<int> sa(begin(A), end(A)), sb(begin(B), end(B));
+        vector<vector<int>> ans(2);
+        for (int n : sa) {
+            if (sb.count(n) == 0) ans[0].push_back(n);
         }
-		ans.push_back(ans1);
-		ans1.clear();
-        for(int x:n2)
-        {
-            if(n1.find(x)==n1.end())
-                ans1.push_back(x);
+        for (int n : sb) {
+            if (sa.count(n) == 0) ans[1].push_back(n);
         }
-        ans.push_back(ans1);
         return ans;
     }
 };
