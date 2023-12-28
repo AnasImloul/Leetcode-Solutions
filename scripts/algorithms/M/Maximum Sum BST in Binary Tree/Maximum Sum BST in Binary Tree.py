fdd3ff1a64f197cdbfa5488db4ef1ba4428c4626
AnasImloul <pydev27@gmail.com>
2023-12-28 22:42:19 +0100

    Runtime: 405 ms (Top 22.49%) | Memory: 35.50 MB (Top 99.31%)

--- scripts/algorithms/M/Maximum Sum BST in Binary Tree/Maximum Sum BST in Binary Tree.py
+++ scripts/algorithms/M/Maximum Sum BST in Binary Tree/Maximum Sum BST in Binary Tree.py
@@ -1,28 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+// Runtime: 405 ms (Top 22.49%) | Memory: 35.50 MB (Top 99.31%)
+
 class Solution:
-    def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        # declaire a variable maxSum to hold the max path sum
-        self.maxSum=0
-        def getMax(root):
-            # return max,min,val - if root is null, valid BST
-            if not root : return (float("-inf"),float("inf"),0)
-            # traverse left and right part
-            leftMax,leftMin,leftMaxSum=getMax(root.left)
-            rightMax,rightMin,rightMaxSum=getMax(root.right)
-            # if a valid BST
-            if root.val>leftMax and root.val<rightMin:
-                # update maxSum
-                self.maxSum=max(self.maxSum,root.val+leftMaxSum+rightMaxSum)
-                # return maximum and minimum node values starting from that node and pathSum
-                return max(root.val,rightMax),min(root.val,leftMin),root.val+leftMaxSum+rightMaxSum
-            # if not a BST - set an impossible condition such than the root is also returned as non-BST
-            return  (float("inf"),float("-inf"),0)
-        getMax(root)    
-        return self.maxSum
-        
+    def maxSumBST(self, root: TreeNode) -> int:
+        res = 0
+        def traverse(root):
+            '''return status_of_bst, size_of_bst, left_bound, right_bound'''
+            nonlocal res
+            if not root: return 1, 0, None, None # this subtree is empty
             
+            ls, l, ll, lr = traverse(root.left)
+            rs, r, rl, rr = traverse(root.right)
+            
+            if ((ls == 2 and lr < root.val) or ls == 1) and ((rs == 2 and rl > root.val) or rs == 1):
+		        # this subtree is a BST
+                size = root.val + l + r
+                res = max(res, size)
+                return 2, size, (ll if ll is not None else root.val), (rr if rr is not None else root.val)
+            return 0, None, None, None # this subtree is not a BST
+        
+        traverse(root)
+        return res
