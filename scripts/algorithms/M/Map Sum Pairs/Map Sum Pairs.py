acaab4626dbf007b7fe0aaab2941b3fddd8d1bbf
AnasImloul <pydev27@gmail.com>
2023-12-29 15:45:26 +0100

    Runtime: 29 ms (Top 97.14%) | Memory: 17.50 MB (Top 9.54%)

--- scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.py
+++ scripts/algorithms/M/Map Sum Pairs/Map Sum Pairs.py
@@ -1,16 +1,34 @@
-	class MapSum:
+// Runtime: 29 ms (Top 97.14%) | Memory: 17.50 MB (Top 9.54%)
 
-		def __init__(self):
-			self.cache = {}
+class MapSum:
 
-		def insert(self, key: str, val: int) -> None:
-			self.cache[key] = val
+    def __init__(self):
+        self.trie = {}
 
-		def sum(self, prefix: str) -> int:
-			len_prefix = len(prefix)
-			total = 0
-			for word, value in self.cache.items():
-				if word[: len_prefix] == prefix:
-					total += value
+    def insert(self, key: str, val: int) -> None:
+        node = self.trie
+        for ch in key:
+            node = node.setdefault(ch, {})
+        node['val'] = val
+        return
 
-			return total
+    def sum(self, prefix: str) -> int:
+        def traverse(node):
+            nonlocal res
+            if not node:
+                return
+            if 'val' in node:
+                res += node['val']
+            for child in node:
+                if child == 'val': continue
+                traverse(node[child])
+            return
+            
+        node = self.trie
+        for ch in prefix:
+            node = node.get(ch, {})
+            if not node:
+                return 0
+        res = 0
+        traverse(node)
+        return res
