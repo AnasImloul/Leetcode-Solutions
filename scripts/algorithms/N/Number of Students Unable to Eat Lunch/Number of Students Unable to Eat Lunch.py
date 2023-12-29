336105f94906e31ac656e7d38cc0608f4b66b534
AnasImloul <pydev27@gmail.com>
2023-12-29 15:46:44 +0100

    Runtime: 76 ms (Top 5.69%) | Memory: 17.30 MB (Top 7.83%)

--- scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.py
+++ scripts/algorithms/N/Number of Students Unable to Eat Lunch/Number of Students Unable to Eat Lunch.py
@@ -1,15 +1,17 @@
-class Solution(object):
-    def countStudents(self, students, sandwiches):
-        """
-        :type students: List[int]
-        :type sandwiches: List[int]
-        :rtype: int
-        """
-        while sandwiches:
-            if sandwiches[0] in students:
-                students.remove(sandwiches[0])
+// Runtime: 76 ms (Top 5.69%) | Memory: 17.30 MB (Top 7.83%)
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = 0
+        while len(students) > count:
+            if students[0] == sandwiches[0]:
                 sandwiches.pop(0)
+                count = 0
             else:
-                break
-        return len(sandwiches)
-            
+                students.append(students[0])
+                count+=1
+
+            students.pop(0)
+        return len(students)
+		
+#Upvote will be encouraging.
