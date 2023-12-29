cb44f461a2cba7cc1a5c88f0aee2febf1a856dff
AnasImloul <pydev27@gmail.com>
2023-12-29 15:39:16 +0100

    Runtime: 68 ms (Top 91.63%) | Memory: 20.60 MB (Top 5.75%)

--- scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.py
+++ scripts/algorithms/M/Minimum Time Difference/Minimum Time Difference.py
@@ -1,22 +1,21 @@
+// Runtime: 68 ms (Top 91.63%) | Memory: 20.60 MB (Top 5.75%)
+
 class Solution:
-	def findMinDifference(self, timePoints: List[str]) -> int:
-		timePoints.sort()
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        # Convert time points to minutes since midnight and sort the list
+        minutes = sorted([int(time[:2]) * 60 + int(time[3:]) for time in timePoints])
+        
+        # Calculate the minimum difference between adjacent time points
+        min_diff = float('inf')
+        for i in range(len(minutes) - 1):
+            diff = minutes[i+1] - minutes[i]
+            if diff < min_diff:
+                min_diff = diff
+        
+        # Calculate the difference between the first and last time points
+        diff = (24*60 - minutes[-1] + minutes[0]) % (24*60)
+        if diff < min_diff:
+            min_diff = diff
+        
+        return min_diff
 
-		def getTimeDiff(timeString1, timeString2):
-			time1 = int(timeString1[:2]) * 60 + int(timeString1[3:])
-			time2 = int(timeString2[:2]) * 60 + int(timeString2[3:])
-
-			minDiff = abs(time1 - time2)
-
-			return min(minDiff, 1440 - minDiff)
-
-		result = getTimeDiff(timePoints[0], timePoints[-1]) # edge case, we need to check minDiff of first and last time after sorting
-
-		for i in range(len(timePoints)-1):
-			curMin = getTimeDiff(timePoints[i], timePoints[i+1])
-
-			if curMin < result:
-				result = curMin
-
-		return result
-		
