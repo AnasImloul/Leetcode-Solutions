a4321a8e72a30328017ef00c9d6d2016a4c99c32
AnasImloul <pydev27@gmail.com>
2023-12-29 15:48:10 +0100

    Runtime: 717 ms (Top 99.99%) | Memory: 24.60 MB (Top 36.15%)

--- scripts/algorithms/R/Reduction Operations to Make the Array Elements Equal/Reduction Operations to Make the Array Elements Equal.py
+++ scripts/algorithms/R/Reduction Operations to Make the Array Elements Equal/Reduction Operations to Make the Array Elements Equal.py
@@ -1,3 +1,10 @@
+// Runtime: 717 ms (Top 99.99%) | Memory: 24.60 MB (Top 36.15%)
+
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        return sum(accumulate(c for _,c in sorted(Counter(nums).items(), reverse=True)[:-1]))
+        ans = val = 0
+        nums.sort()
+        for i in range(1, len(nums)): 
+            if nums[i-1] < nums[i]: val += 1
+            ans += val
+        return ans 
