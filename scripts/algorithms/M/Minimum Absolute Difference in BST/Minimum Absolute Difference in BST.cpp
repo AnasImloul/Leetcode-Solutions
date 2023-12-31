1947909dba0e14ad5590250c104d88392381b4fe
AnasImloul <pydev27@gmail.com>
2024-01-01 00:50:11 +0100

    Runtime: 7 ms (Top 96.14%) | Memory: 25.40 MB (Top 77.88%)

--- scripts/algorithms/M/Minimum Absolute Difference in BST/Minimum Absolute Difference in BST.cpp
+++ scripts/algorithms/M/Minimum Absolute Difference in BST/Minimum Absolute Difference in BST.cpp
@@ -1,19 +1,22 @@
-class Solution {
-    TreeNode* pre = nullptr;
-    int minimum = std::numeric_limits<int>:: max();
-public:
-    void inorder(TreeNode* node) {
-        if (!node) return;
-        inorder(node->left);
-        if(pre) {
-            minimum = min(node->val - pre->val, minimum);
-        }
-        pre = node;
-        inorder(node->right);
-    }
-    int getMinimumDifference(TreeNode* root) {
-        inorder(root);
-        return minimum;
-    }
+// Runtime: 7 ms (Top 96.14%) | Memory: 25.40 MB (Top 77.88%)
 
+class Solution {
+public:
+    int inorder(TreeNode* root, int& prevElement, int& ans){
+
+        if(root->left) inorder(root->left, prevElement, ans);
+        if(prevElement >=0 ) ans = min(ans, abs(prevElement-root->val));
+        prevElement = root->val;
+        if(root->right) inorder(root->right, prevElement, ans);
+
+        return ans;      
+
+
+    }
+    int getMinimumDifference(TreeNode* root){
+        int prevElement = -1;
+        int ans = INT_MAX;
+        return inorder(root, prevElement, ans);
+     
+    }
 };
