0d0bc1bf0988bbb5fe34d9bfeac36742a78d6633
AnasImloul <pydev27@gmail.com>
2024-01-01 00:57:05 +0100

    Runtime: 2 ms (Top 64.56%) | Memory: 6.40 MB (Top 55.7%)

--- scripts/algorithms/B/Building Boxes/Building Boxes.cpp
+++ scripts/algorithms/B/Building Boxes/Building Boxes.cpp
@@ -1,27 +1,45 @@
+// Runtime: 2 ms (Top 64.56%) | Memory: 6.40 MB (Top 55.7%)
+
 class Solution {
+private:
+    int getLayerSize(int x){
+        return x * (x+1) / 2;
+    }
+    int getCubeSize(int x){
+        long d = static_cast<long>(x);
+        long sz = (d * (d+1) * (d+2)) / 6;
+        return static_cast<int>(sz);
+    }
 public:
+
     int minimumBoxes(int n) {
-        
-        // Find the biggest perfect pyramid
-        uint32_t h = cbrt((long)6*n);
-        uint32_t pyramid_box_num = h*(h+1)/2*(h+2)/3;   // /6 is split to /2 and /3 to avoid long to save space
-        if ( pyramid_box_num > n ){
-            h--;
-            pyramid_box_num = h*(h+1)/2*(h+2)/3;            // /6 is split to /2 and /3 to avoid long to save space
+        // First Binary Search
+        int l(0), r(2000), m;
+        while(l <= r)
+        {
+            m = (l+r)/2;
+            int sz = getCubeSize(m);
+            if(sz <= n)
+                l = m + 1;
+            else
+                r = m - 1;
         }
+        int aSideOfPyramid = r;
+        int rest = n - getCubeSize(aSideOfPyramid);
         
-        // Calculate how many floor-touching boxes in the pyramid
-        int pyramid_base_num = (h)*(h+1)/2;
-        
-        // Get the number of boxes to be added to the perfect pyramid
-        n -= pyramid_box_num;
-        
-        // Find how many floor-touching boxes are added
-        int z = sqrt(2*n);
-        int max_box_added = z*(z+1)/2;
-        if ( max_box_added < n)
-            z++;
-        
-        return pyramid_base_num+z;
+		// Second Binary Search
+        l=0,r=2000;
+        while(l <= r)
+        {
+            m = (l+r)/2;
+            int sz = getLayerSize(m);
+            if(sz < rest)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        int addedFloorCubic = l;
+
+        return getLayerSize(aSideOfPyramid) + addedFloorCubic;
     }
 };
