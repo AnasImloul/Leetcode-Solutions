0f191c9c1bb9b742b27d84420b4b1525f0755acb
AnasImloul <pydev27@gmail.com>
2024-01-01 00:40:01 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 8.80 MB (Top 64.77%)

--- scripts/algorithms/O/Orderly Queue/Orderly Queue.cpp
+++ scripts/algorithms/O/Orderly Queue/Orderly Queue.cpp
@@ -1,22 +1,19 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 8.80 MB (Top 64.77%)
+
 class Solution {
 public:
-string orderlyQueue(string s, int k) {
- 
-if(k>1){
-       sort(s.begin(),s.end());
-       return s;
-    }
-    else{
-        string res=s;
-        
-        for(int i=0;i<s.length();i++){
-            s=s.substr(1)+s.substr(0,1);
-            res=min(res,s);
+    string orderlyQueue(string S, int K) {
+       // for k>1 we can make it fully sorted string after roation because here we are not bound to the roatate first char only.
+        if (K > 1) {
+            sort(S.begin(), S.end());
+            return S;
         }
-        return res;
+        // for k==1 we can rotate whole string any times like-  S="cba" we can get cba, bac,acb so in S+S ="cbacba" we need to find only lexicographically smallest string  of size n in S+S.
+            string tempr=S;
+            S= S+ S;
+            for(int i=1;i<tempr.size();i++){
+                tempr=min(tempr,S.substr(i,tempr.size()));
+            }
+            return tempr;
     }
-    
-    return s;
-    
-}
 };
