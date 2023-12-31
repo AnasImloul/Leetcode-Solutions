1c3803cbe41d5351d97b12137b1c42c30a9d8244
AnasImloul <pydev27@gmail.com>
2024-01-01 00:30:59 +0100

    Runtime: 215 ms (Top 30.21%) | Memory: 78.90 MB (Top 67.67%)

--- scripts/algorithms/C/Car Fleet/Car Fleet.cpp
+++ scripts/algorithms/C/Car Fleet/Car Fleet.cpp
@@ -1,37 +1,37 @@
+// Runtime: 215 ms (Top 30.21%) | Memory: 78.90 MB (Top 67.67%)
+
+class Car{
+public:
+    Car(int pos, int speed){
+        this->pos = pos;
+        this->speed= speed;
+    }
+    int pos;
+    int speed;
+};
+
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        
-        int n = position.size();
-        vector<pair<int,int>> cars;
-        
-        for(int i=0; i<n; i++)cars.push_back({position[i], speed[i]});
-        
-        sort(cars.begin(),cars.end());
-        
-        int start=0;
-        int mid=0;
-        int fleet = n;
-        
-        double times[n];
-        
-        for(int i=n-1; i>=0; i--){
-            times[i] =((double) (target-cars[i].first)/cars[i].second);
-            if(i<n-1)times[i] = max(times[i],times[i+1]);
+        vector<Car> cars;
+        int N = position.size();
+        for(int i = 0; i<N; i++){
+            cars.emplace_back(position.at(i), speed.at(i));
         }
         
-        while(mid<n){            
-            double timeA = times[start];
-            double timeB = times[mid];
-            //cout<<timeA<<" "<<timeB<<endl;
-            if(mid-start+1 == 2){
-                if(timeA<=timeB)fleet--;
-                start++;
+        sort(cars.begin(), cars.end(), [](const Car& a, const Car& b){
+            return a.pos<b.pos;
+        });
+        
+        stack<float> mono;
+        for(int i = 0; i<N; i++){
+            float time = 
+                (target-cars.at(i).pos)/(float)cars.at(i).speed;
+            while(!mono.empty() && time >= mono.top()){
+                mono.pop();
             }
-            mid++;
+            mono.push(time);
         }
-        
-        return fleet;
-        
+        return mono.size();
     }
 };
