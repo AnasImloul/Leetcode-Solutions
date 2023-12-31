ea18222e6a999f49bf77dde417b3fa69cdfcb619
AnasImloul <pydev27@gmail.com>
2024-01-01 00:35:04 +0100

    Runtime: 51 ms (Top 98.57%) | Memory: 38.10 MB (Top 95.53%)

--- scripts/algorithms/X/XOR Queries of a Subarray/XOR Queries of a Subarray.cpp
+++ scripts/algorithms/X/XOR Queries of a Subarray/XOR Queries of a Subarray.cpp
@@ -1,48 +1,25 @@
+// Runtime: 51 ms (Top 98.57%) | Memory: 38.10 MB (Top 95.53%)
+
 class Solution {
 public:
-    
-    // we know that (x ^ x)  = 0,
-    
-    // arr = [1,4,8,3,7,8],  let we have to calculate xor of subarray[2,4]
-    
-    // (1 ^ 4 ^ 8 ^ 3 ^ 7) ^ (1 ^ 4) = (8 ^ 3 ^ 7), this is nothing but prefix[right] ^ prefix[left - 1]
-    
-    // (1 ^ 1 = 0) and (4 ^ 4) = 0
-    
-    
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        
-        int n = queries.size();
-        
-        // find the prefix xor of arr
-        
-        for(int i = 1; i < arr.size(); i++)
-        {
-            arr[i] = (arr[i - 1] ^ arr[i]);
+        int n = arr.size();
+        int q = queries.size();
+        vector<int> answer(q, 0);
+        vector<int> prefXor(n, 0);
+
+        prefXor[0] = arr[0];
+        for(int i = 1; i < n; i++) {
+            prefXor[i] = prefXor[i - 1] ^ arr[i];
         }
-        
-        // calculate each query
-        
-        vector<int> res(n);
-        
-        for(int i = 0; i < n; i++)
-        {
+
+        for(int i = 0; i < q; i++) {
             int left = queries[i][0];
-            
             int right = queries[i][1];
-            
-            // find the xorr of the subarray
-            
-            int xorr = arr[right];
-            
-            if(left > 0)
-            {
-                xorr ^= arr[left - 1];
-            }
-            
-            res[i] = xorr;
+            if(left == 0) answer[i] = prefXor[right];
+            else answer[i] = prefXor[right] ^ prefXor[left - 1];
         }
-       
-        return res;
+
+        return answer;
     }
 };
