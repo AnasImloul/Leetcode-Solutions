8203c7c44b57bd916621712e9a529589e13df433
AnasImloul <pydev27@gmail.com>
2024-01-01 00:48:29 +0100

    Runtime: 70 ms (Top 83.06%) | Memory: 35.10 MB (Top 50.72%)

--- scripts/algorithms/A/Accounts Merge/Accounts Merge.cpp
+++ scripts/algorithms/A/Accounts Merge/Accounts Merge.cpp
@@ -1,82 +1,70 @@
+// Runtime: 70 ms (Top 83.06%) | Memory: 35.10 MB (Top 50.72%)
+
+// if you like this, please give me a upvote
 class Solution {
 public:
+    int parents[1001]; // why 1001? becoz it is max length of accounts, see constraints (1 <= accounts.length <= 1000)
+    
+    // function to find original parent
+    int findpar(int parents[], int to_find)
+    {
+        while(parents[to_find] != to_find)
+            to_find = parents[to_find];
+        
+        return to_find; 
+    }
+    
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        map<string,int>stop;
-        map<int,string>name;
-        int count=0;
-        vector<int>par(1005,0),sz(1005,1);
-        for(int i=1;i<=1000;i++)
-        {
-            par[i]=i;
-        }
-        function<int(int)> findpar = [&](int a) -> int {
-            if(a==par[a])return par[a];
-            return par[a]=findpar(par[a]);
-        };
-        function<void(int,int)> dounion =[&](int a,int b) ->void {
-            a=findpar(a);
-            b=findpar(b);
-            if(a!=b)
-            {
-                if(sz[a]<sz[b]) swap(a,b);
-                par[b]=a;
-                sz[a]+=sz[b];
-            }
-        };
+        // Link each email to their respective orginal parent, 
+        // we will do it by linking each email to its unique id
+        unordered_map<string, int> email_to_id;
         
-        for(auto it:accounts){
+        for(int i = 0; i < accounts.size(); i++) // traversing from each row
+        {
+            parents[i] = i; // intially we don't know anything, so every parent is unique id of itself
             
-            int n=it.size();
-            vector<int>curpar;
-            for(int i=1;i<n;i++)
+            // why we start from 1? becoz emails starting from this place
+            for(int j = 1; j < accounts[i].size(); j++)
             {
-                if(stop[it[i]])
-                    curpar.push_back(stop[it[i]]);
+                // we check if this particular email already got their unique id, if yes then we link it to its original parent unique id
+                if(email_to_id.find(accounts[i][j]) != email_to_id.end())
+                {
+                    int parent1 = findpar(parents, i);
+                    int parent2 = findpar(parents, email_to_id[accounts[i][j]]);
+                    
+                    parents[parent1] = parent2;
+                }
+                else // if not find, then just give it to exsiting parent
+                {
+                    email_to_id[accounts[i][j]] = parents[i];
+                }
             }
-            if(curpar.size()==0) // if no par present 
+        }
+            
+            // Now, we create a map that store all email id with their unique ids
+            //            id,  vector of all emails
+            unordered_map<int, vector<string>> resultant_map;
+            
+            for(auto it: email_to_id)
             {
-                count++;
-                for(int i=1;i<n;i++)
-                {
-                    stop[it[i]]=count;
-                }
-                name[count]=it[0];
+                int uniq_id = findpar(parents, it.second); 
+                resultant_map[uniq_id].push_back(it.first);
             }
-            else{ // if par present then do union
-                int first=curpar[0];
-                for(int i=1;i<n;i++) 
-                {
-                    stop[it[i]]=first;
-                }
+            
+            // uff tired, so now towards final ans
+            vector<vector<string>> ans;
+            for(auto it: resultant_map)
+            {
+                vector<string> email = it.second;
                 
-                for(int j=1;j<curpar.size();j++) // union for all child
-                {
-                    int id=curpar[j];
-                    dounion(first,id);
-                }
+                // question demand in sorted order
+                sort(email.begin(), email.end());
+                
+                // inserting name in starting with the help of unique id
+                email.insert(email.begin(), accounts[it.first][0]);
+                
+                ans.push_back(email);
             }
-        }
-        
-        vector<vector<string>>ans;
-        
-        for(int i=1;i<=1000;i++)
-        {
-            vector<string> cur;
-            if(findpar(i)==i)
-            {
-                for(auto it:stop)
-                {
-                    if(findpar(it.second)==i){
-                        cur.push_back(it.first);
-                    }
-                }
-            }
-            if(cur.size()>0)
-            {
-                cur.insert(cur.begin(),name[i]);
-                ans.push_back(cur);
-            }
-        }
         return ans;
     }
 };
