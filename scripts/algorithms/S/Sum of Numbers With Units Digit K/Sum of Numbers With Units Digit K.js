2b6b99aac1bc56090eb4d0753fb9680aa0b19d52
AnasImloul <pydev27@gmail.com>
2024-01-01 02:55:29 +0100

    Runtime: 57 ms (Top 47.06%) | Memory: 42.40 MB (Top 5.88%)

--- scripts/algorithms/S/Sum of Numbers With Units Digit K/Sum of Numbers With Units Digit K.js
+++ scripts/algorithms/S/Sum of Numbers With Units Digit K/Sum of Numbers With Units Digit K.js
@@ -1,7 +1,23 @@
+// Runtime: 57 ms (Top 47.06%) | Memory: 42.40 MB (Top 5.88%)
+
+/**
+ * @param {number} num
+ * @param {number} k
+ * @return {number}
+ */
 var minimumNumbers = function(num, k) {
-    if (num === 0) return 0;
-    for (let i = 1; i <= 10; i++) {
-        if (k*i % 10 === num % 10 && k*i <= num) return i;
-        if (k*i > num) return -1
-    } return -1;
+    if (num === 0) return 0
+    if (num === k) return 1
+    if (k === 0) return num % 10 !== 0 ? -1 : 1
+    
+    let x = Math.floor(num / k)
+    
+    while (x >= 0) {
+        if ((num - 10 * x) > 0 && (num - 10 * x) % k === 0) {
+            return (num - 10 * x) / k
+        }
+        x--
+    }
+    
+    return -1
 };
