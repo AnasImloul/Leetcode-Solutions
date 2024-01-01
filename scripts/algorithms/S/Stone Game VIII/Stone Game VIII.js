9936e8e9a6808e486fbbd34461f85bf7c3a8f2b7
AnasImloul <pydev27@gmail.com>
2024-01-01 03:02:54 +0100

    Runtime: 186 ms (Top 100.0%) | Memory: 63.30 MB (Top 50.0%)

--- scripts/algorithms/S/Stone Game VIII/Stone Game VIII.js
+++ scripts/algorithms/S/Stone Game VIII/Stone Game VIII.js
@@ -1,18 +1,13 @@
-var stoneGameVIII = function(stones) {
-    let prefix = Array(stones.length);
-    for (let i = 0; i < stones.length; i++) prefix[i] = stones[i] + (prefix[i - 1] || 0);
-    
-    let dp = Array(stones.length).fill(0);
-    function game(idx) {
-        if (dp[idx]) return dp[idx];
-        if (idx >= stones.length) return 0;
-        let skip = game(idx + 1);
-        let take = prefix[idx] - skip;
-        dp[idx] = take;
-        // Both Alice and Bob must maximize their scores, which would maximize the difference for Alice and minimize the difference for Bob
-        // Return the max score of either removing stones from the current idx or skipping to the next idx
-        return !skip ? take : Math.max(skip, take);
-    }
+// Runtime: 186 ms (Top 100.0%) | Memory: 63.30 MB (Top 50.0%)
 
-    return game(1);
+var stoneGameVIII = function(S) {  
+    let n=S.length, prefix=[0],dp=[...Array(n)]
+    for(let i=0;i<n;i++)
+        prefix.push(prefix[prefix.length-1]+S[i])
+    let bestRight=prefix[n],bestCurrent
+    for(let i=n-2;i>=0;i--)
+        bestCurrent=bestRight,
+		//update what you re going to use in the future
+        bestRight=Math.max(bestRight,prefix[i+1]-bestCurrent) 
+    return bestCurrent
 };
