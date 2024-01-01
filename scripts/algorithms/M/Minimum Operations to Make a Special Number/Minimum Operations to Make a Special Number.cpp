d4da65ebdfa86bb423eb3694640fb0ddd82e1914
AnasImloul <pydev27@gmail.com>
2024-01-01 01:14:26 +0100

    Runtime: 3 ms (Top 49.8%) | Memory: 6.60 MB (Top 50.6%)

---
+++ scripts/algorithms/M/Minimum Operations to Make a Special Number/Minimum Operations to Make a Special Number.cpp
@@ -0,0 +1,21 @@
+// Runtime: 3 ms (Top 49.8%) | Memory: 6.60 MB (Top 50.6%)
+
+class Solution {
+public:
+    int minimumOperations(string num) {
+        bool fivefound = false;
+        bool zerofound = false;
+        for (int i =num.size()-1; i >=0;i--) {
+            
+            if (zerofound && num[i]=='0') return num.size()-2-i;
+            if (zerofound && num[i]=='5') return num.size()-2-i;
+            if (fivefound && num[i]=='2') return num.size()-2-i;
+            if (fivefound && num[i]=='7') return num.size()-2-i;
+            if (num[i]=='5') fivefound = true;
+            if (num[i]=='0') zerofound = true;
+            
+        }
+        if (zerofound) return num.size()-1;
+        return num.size();
+    }
+};
