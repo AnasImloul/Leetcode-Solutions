4ae20aff99493fdcd19651dec7e7c9dccae58d8c
AnasImloul <pydev27@gmail.com>
2024-01-01 01:11:33 +0100

    Runtime: 71 ms (Top 84.38%) | Memory: 24.60 MB (Top 56.83%)

--- scripts/algorithms/S/Student Attendance Record II/Student Attendance Record II.cpp
+++ scripts/algorithms/S/Student Attendance Record II/Student Attendance Record II.cpp
@@ -1,42 +1,52 @@
+// Runtime: 71 ms (Top 84.38%) | Memory: 24.60 MB (Top 56.83%)
+
 class Solution {
 public:
-    int mod = 1e9+7;
-    int dp[100001][10][10];
-    int recur(int abs,int late,int n){
-        if(abs > 1){
-            return 0;
-        }
-        if(late >= 3){
-            return 0;
-        }
-        if(n == 0){
-            return 1;
-            
-        }
-        if(dp[n][late][abs] != -1) return dp[n][late][abs];
-        
-        int ans = 0;
-        ans = (ans%mod + recur(abs+1,0,n-1)%mod)%mod;
-        
-        ans = (ans%mod + recur(abs,late+1,n-1)%mod)%mod;
-        
-        ans = (ans%mod + recur(abs,0,n-1)%mod)%mod;
-       //This dp is running from n to 0 and is cut off if
-        //absent is greater than 1 or late of consecutive is greater than
-        //equal to 3.
-        //Here in the first recursion i.e line 19 & line 23 late is made 0
-        //to signify that the consecutive lates are stopped.***
-
-        return dp[n][late][abs] = ans;
+    const int mod = 1e9 + 7;
+    
+    int add(int a, int b){
+        return (a%mod + b%mod)%mod;
     }
+    
+    int sub(int a, int b){
+        return (a%mod - b%mod + mod)%mod;
+    }
+    
+    int mul(int a, int b){
+        return (a%mod * 1ll * b%mod)%mod;
+    }
+    
     int checkRecord(int n) {
-        for(int i=0;i<=100000;i++){
-            for(int j=0;j<10;j++){
-                for(int k=0;k<10;k++){
-                    dp[i][j][k] = -1;
-                }
-            }
+        if(n == 1) return 3;
+     
+        vector<int>PorL(n + 1); /*for strings ending with 'P' or 'L' and no 'A' is present*/
+        vector<int>P(n + 1); /* for strings ending with 'P' and no 'A' is present */
+
+        // for 0 length strings only 1 option can be there (base case)
+        P[0] = 1, PorL[0] = 1;
+        
+        /*for 1 length strings*/
+        P[1] = 1; /*can end with 'P' only */
+        PorL[1] = 2; /*can end with either 'P' or 'L' */
+
+        /*for 2 length strings*/
+        P[2] = PorL[1]; /*ending with 'P' so the previous string can end with 'P' or 'L' */
+        PorL[2] = P[2] + P[1] + P[0]; /*either current char can be 'P' or 'L' or last 2 chars can be 'L' i.e "LL" */
+        
+        for(int i = 3;i<=n;i++){
+            P[i] = PorL[i - 1] % mod;
+            PorL[i] = add(P[i], add(P[i - 1], P[i - 2]));
         }
-        return recur(0,0,n);
+        
+        int ans = PorL[n]; /*if we have no 'A'*/
+        /*since we can have only 1 'A' lets try to insert at each position */
+        for(int i = 1;i<=n;i++){
+            /*if we insert 'A' at ith position */
+            int leftLength = i - 1; /*the length of the string to the left of i*/
+            int rightLength = n - i; /*the length of the string to the right of i*/
+            ans = add(ans, mul(PorL[leftLength], PorL[rightLength]));
+        }
+        return ans;
+        
     }
 };
