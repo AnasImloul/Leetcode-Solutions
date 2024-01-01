45b832fa4abf7919117e8081de919addf6180193
AnasImloul <pydev27@gmail.com>
2024-01-01 02:40:38 +0100

    Runtime: 295 ms (Top 61.43%) | Memory: 73.80 MB (Top 71.43%)

--- scripts/algorithms/P/Product of the Last K Numbers/Product of the Last K Numbers.js
+++ scripts/algorithms/P/Product of the Last K Numbers/Product of the Last K Numbers.js
@@ -1,32 +1,21 @@
+// Runtime: 295 ms (Top 61.43%) | Memory: 73.80 MB (Top 71.43%)
 
 var ProductOfNumbers = function() {
-    this.nums = [];
-    this.len = 0;
+  this.products = [];
 };
 
-/** 
- * @param {number} num
- * @return {void}
- */
 ProductOfNumbers.prototype.add = function(num) {
-    this.nums.push(num);
-    this.len++;
+  if (num == 0) {
+    this.products = [];
+  } else {
+    const N = this.products.length;
+    this.products.push(N == 0 ? num : num * this.products[N - 1]);
+  }
 };
 
-/** 
- * @param {number} k
- * @return {number}
- */
 ProductOfNumbers.prototype.getProduct = function(k) {
-    let product = 1;
-    for (let i = 1; i <= k; i++)
-        product *= this.nums[this.len - i];
-    return product;
+  const N = this.products.length;
+  if (N - k < 0) return 0;
+  if (N == k) return this.products[N - 1];
+  return this.products[N - 1] / this.products[N - k - 1];
 };
-
-/** 
- * Your ProductOfNumbers object will be instantiated and called as such:
- * var obj = new ProductOfNumbers()
- * obj.add(num)
- * var param_2 = obj.getProduct(k)
- */
