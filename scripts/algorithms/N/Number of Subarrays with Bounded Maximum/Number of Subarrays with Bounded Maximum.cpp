2cb0bbf56183f1489fa2f1210306fb2511784681
AnasImloul <pydev27@gmail.com>
2024-01-01 01:17:44 +0100

    Runtime: 46 ms (Top 85.16%) | Memory: 52.70 MB (Top 42.49%)

--- scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.cpp
+++ scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.cpp
@@ -1,20 +1,25 @@
+// Runtime: 46 ms (Top 85.16%) | Memory: 52.70 MB (Top 42.49%)
+
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-        int ans = 0; // store ans
-        int j=-1; // starting window
-        int sub = 0; // if current element is less than left bound then count how may element before current element which is less than left and must be continues(it means any element which is greater than left bound reset the count to 0 )
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>right){
-                j = i;
-                sub = 0;
+        int n =  nums.size();
+        int si =0;//starting index
+        int ei = 0; //ending index
+        int result =0, prev_count =0;
+        while(ei < n){
+            if(left <= nums[ei]  &&  nums[ei]<= right){
+              prev_count = ei- si +1;
+              result += prev_count;
+            }else if (  nums[ei] < left){
+                 result += prev_count;
+            }else{
+                //right < nums[ei]
+                si = ei +1;
+                prev_count =0;
             }
-            else if(nums[i]<left){
-                sub++;
-            }
-            else sub = 0;
-            ans = ans + i - j - sub;
-        }
-        return ans;
+            ei++;
+       }
+      return result;
     }
 };
