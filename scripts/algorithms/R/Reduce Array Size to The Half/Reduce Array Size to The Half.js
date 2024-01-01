72b261f1789dbb2b8ffa984b318bfaee3c75d78f
AnasImloul <pydev27@gmail.com>
2024-01-01 03:00:10 +0100

    Runtime: 105 ms (Top 71.97%) | Memory: 57.90 MB (Top 93.94%)

--- scripts/algorithms/R/Reduce Array Size to The Half/Reduce Array Size to The Half.js
+++ scripts/algorithms/R/Reduce Array Size to The Half/Reduce Array Size to The Half.js
@@ -1,28 +1,15 @@
-var minSetSize = function(arr) {
-  let halfSize = arr.length / 2;
-  const numsCount = Object.values(getNumsCount(arr)).sort((a, b) => b - a); // get the frequencies in an array sorted in descending order
-  let setSize = 0;
-  if (numsCount[0] >= halfSize) return 1; // if the highest frequency is greater than or equal to half of the array size, then you already have your smallest set size of 1
-  for (let i = 0; i < numsCount.length; i++) {
-    let numCount = numsCount[i];
-    if (halfSize > 0) {
-      setSize++;
-      halfSize -= numCount;
-    } else {
-      return setSize;
-    };
-  };
-};
+// Runtime: 105 ms (Top 71.97%) | Memory: 57.90 MB (Top 93.94%)
 
-var getNumsCount = function (arr) {
-  let eleCounts = {};
-  for (let i = 0; i < arr.length; i++) {
-    let num = arr[i];
-    if (!eleCounts[num]) {
-      eleCounts[num] = 1;
-    } else {
-      eleCounts[num]++;
-    };
-  };
-  return eleCounts;
-};
+var minSetSize = function(arr) {
+  let half = arr.length / 2
+  if (half === 1) return 1
+  const counts = {}
+  for (let n of arr) {
+    if (counts[n] === undefined) counts[n] = 1
+    else if (++counts[n] >= half) return 1
+  }
+  const aggr = Object.values(counts).sort((a, b) => a - b)
+  let i = aggr.length;
+  while (half > 0) half -= aggr[--i]
+  return aggr.length - i
+}
