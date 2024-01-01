76d80e7a1b83bb9dca0fb25f5cdc029b1b9c7c33
AnasImloul <pydev27@gmail.com>
2024-01-01 02:47:20 +0100

    Runtime: 83 ms (Top 91.06%) | Memory: 47.80 MB (Top 54.47%)

--- scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.js
+++ scripts/algorithms/F/Find Duplicate Subtrees/Find Duplicate Subtrees.js
@@ -1,63 +1,17 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {TreeNode[]}
- */
+// Runtime: 83 ms (Top 91.06%) | Memory: 47.80 MB (Top 54.47%)
+
 var findDuplicateSubtrees = function(root) {
-    let stack = [];
-    let result = [];
-    let hashTable = {};
-    stack.push(root);
-    while (stack.length) {
-        let node = stack.pop();
-        let treeToArr = dfsArr(node);
-        if (node.right) {
-            stack.push(node.right);
-        }
-        if (node.left) {
-            stack.push(node.left);
-        }
-		// If sub tree has not been seen, set to one.
-		// If sub tree has been seen, increment number.
-        hashTable[treeToArr] = hashTable[treeToArr] ? 
-            { num: hashTable[treeToArr].num + 1, ref: node} :
-            { num: 1, ref: node};
-    }
-	// Builds result array with duplicate trees seen
-    for (let i in hashTable) {
-        if (hashTable[i].num > 1) {
-            result.push(hashTable[i].ref);
-        }
-    }
-    return result;
+  const map = new Map(), res = []
+  dfs(root, map, res)
+  return res
 };
 
-// Post order of subtree stored in array
-function dfsArr (node) {
-    let stack = [];
-    let result = [];
-    stack.push(node);
-    while (stack.length) {
-        let node = stack.pop();
-        node ? result.push(node.val) : result.push(null);
-        if (!node) continue;
-        if (node.right) {
-            stack.push(node.right);
-        } else {
-            stack.push(null);
-        }
-        if (node.left) {
-            stack.push(node.left);
-        } else {
-            stack.push(null);
-        }
-    }
-    return result;
+function dfs(root, map, res){
+  if(!root) return '#'
+  const subtree = `${root.val}.${dfs(root.left,map,res)}.${dfs(root.right, map,res)}`
+  map.set(subtree,(map.get(subtree)||0) + 1)
+  if(map.get(subtree) === 2){
+    res.push(root)
+  }
+  return subtree
 }
