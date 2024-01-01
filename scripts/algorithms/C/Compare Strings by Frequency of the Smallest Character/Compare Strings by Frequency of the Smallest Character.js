8e0a38f27bb48ce3bce147dedd0b2b99d99a22e4
AnasImloul <pydev27@gmail.com>
2024-01-01 02:42:23 +0100

    Runtime: 115 ms (Top 46.43%) | Memory: 49.20 MB (Top 30.36%)

--- scripts/algorithms/C/Compare Strings by Frequency of the Smallest Character/Compare Strings by Frequency of the Smallest Character.js
+++ scripts/algorithms/C/Compare Strings by Frequency of the Smallest Character/Compare Strings by Frequency of the Smallest Character.js
@@ -1,32 +1,11 @@
-/**
- * @param {string[]} queries
- * @param {string[]} words
- * @return {number[]}
- */
+// Runtime: 115 ms (Top 46.43%) | Memory: 49.20 MB (Top 30.36%)
+
 var numSmallerByFrequency = function(queries, words) {
-    let res=[]
-    queries=queries.map(q=>countFrequency(q))//[3,2]
-    words=words.map(w=>countFrequency(w))//[1,2,3,4]
-  words = words.sort((a, b) => a - b)
-    for(let i=0;i<queries.length;i++){
-      let start=0,end=words.length-1
-      while(start<=end){
-          let mid=start+Math.floor((end-start)/2)
-          if(queries[i]<words[mid]){
-              end=mid-1
-          }else{
-              start=mid+1
-          }
-      }
-      res.push(words.length-1-end)
-  }
-    return res
+    const wordsF = words.map(word => f(word));
+    return queries.map(str => f(str))
+        .map(strF => wordsF.reduce((count, wordF) => strF<wordF? count+1:count,0))
 };
 
-function countFrequency(str){
-    let freq={}
-    str.split('').forEach(key=>{
-        freq[key]=(freq[key]||0)+1
-    })
-    return freq[Object.keys(freq).sort()[0]]
+function f(str){
+    return str.split("").sort().reduce((sum, curr,i,arr) => curr === arr[0] ? sum+1: sum,0);
 }
