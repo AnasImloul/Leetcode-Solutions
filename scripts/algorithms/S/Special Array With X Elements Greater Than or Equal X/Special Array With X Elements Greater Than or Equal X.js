e721f98f81d4060440ef480ea290d9c368cb2f35
AnasImloul <pydev27@gmail.com>
2024-01-01 02:22:43 +0100

    Runtime: 57 ms (Top 37.81%) | Memory: 42.20 MB (Top 49.25%)

--- scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.js
+++ scripts/algorithms/S/Special Array With X Elements Greater Than or Equal X/Special Array With X Elements Greater Than or Equal X.js
@@ -1,12 +1,32 @@
+// Runtime: 57 ms (Top 37.81%) | Memory: 42.20 MB (Top 49.25%)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var specialArray = function(nums) {
-    const freq = new Array(1001).fill(0);
-    for(let n of nums)
-        freq[n]++;
-    
-    for(let i=1000, cnt=0; i>=0; i--){
-        cnt += freq[i];
-        if(i==cnt) return i;
+    // step 1: sort
+    nums = nums.sort((a,b) => a-b);
+    // step 2: search
+    for(let i=nums.length; i>=0; i--) {
+        // found:
+        if(bs(nums, i)) return i;
     }
-    
+    // not found:
     return -1;
 };
+
+function bs(array, target) {
+    let left = 0;
+    let right = array.length - 1;
+
+    while(left <= right){
+        const mid = Math.floor((left+right)/2);
+        if(array[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    const greaterThanTarget = array.length - left;
+
+    return greaterThanTarget === target;
+}
