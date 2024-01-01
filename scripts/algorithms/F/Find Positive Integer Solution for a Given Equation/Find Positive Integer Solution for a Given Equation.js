a3208320498f314092179043b1dcfd1dccbeb1dd
AnasImloul <pydev27@gmail.com>
2024-01-01 02:39:31 +0100

    Runtime: 58 ms (Top 64.29%) | Memory: 43.20 MB (Top 11.9%)

--- scripts/algorithms/F/Find Positive Integer Solution for a Given Equation/Find Positive Integer Solution for a Given Equation.js
+++ scripts/algorithms/F/Find Positive Integer Solution for a Given Equation/Find Positive Integer Solution for a Given Equation.js
@@ -1,12 +1,35 @@
+// Runtime: 58 ms (Top 64.29%) | Memory: 43.20 MB (Top 11.9%)
+
+/**
+ * // This is the CustomFunction's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function CustomFunction() {
+ *     @param {integer, integer} x, y
+ *     @return {integer}
+ *     this.f = function(x, y) {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {CustomFunction} customfunction
+ * @param {integer} z
+ * @return {integer[][]}
+ */
 var findSolution = function(customfunction, z) {
-    const result = [];
-    
-    for(let x = 1; x <= z; x++) {
-        for(let y = 1; y <= z; y++) {
-            const prod = customfunction.f(x, y);
-            if(prod === z) result.push([x, y]);
-            if(prod > z) break;
+    var ans = []
+    var x = 1, y = 1000;
+    while(x<=1000 && y>0){
+        if(customfunction.f(x,y)>z){
+            y--;
+        }else if(customfunction.f(x,y)<z){
+            x++;
+        }else{
+            ans.push([x,y]);
+            x++;
+            y--;
         }
     }
-    return result;
+    return ans;
 };
