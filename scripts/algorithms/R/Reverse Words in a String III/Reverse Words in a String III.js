3838c5148516e70c758df121e5ab04bda0ec3c51
AnasImloul <pydev27@gmail.com>
2024-01-01 02:26:36 +0100

    Runtime: 76 ms (Top 15.58%) | Memory: 49.70 MB (Top 6.97%)

--- scripts/algorithms/R/Reverse Words in a String III/Reverse Words in a String III.js
+++ scripts/algorithms/R/Reverse Words in a String III/Reverse Words in a String III.js
@@ -1,8 +1,20 @@
-var reverseWords = function(s) {
-    // make array of the words from s
-    let words = s.split(" ");
-    for (let i in words) {
-	    // replace words[i] with words[i] but reversed
-        words.splice(i, 1, words[i].split("").reverse().join(""))
-    } return words.join(" ");
-};
+// Runtime: 76 ms (Top 15.58%) | Memory: 49.70 MB (Top 6.97%)
+
+var reverseWords = function(s){
+        let chars = [...s];
+        let start = 0;
+
+        for (let i = 0; i < chars.length; i++) {
+            if (chars[i] === ' ' || i === chars.length - 1) {
+                let end = (i === chars.length - 1 && chars[i] !== ' ') ? i + 1 : i;
+                while (start < end) {
+                    [chars[start], chars[end - 1]] = [chars[end - 1], chars[start]];
+                    start++;
+                    end--;
+                }
+                start = i + 1;
+            }
+        }
+
+        return chars.join('');
+    }
