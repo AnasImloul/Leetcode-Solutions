61221459142c326432e195b39e08f49bf435ff76
AnasImloul <pydev27@gmail.com>
2024-01-01 02:51:53 +0100

    Runtime: 144 ms (Top 6.56%) | Memory: 44.10 MB (Top 88.52%)

--- scripts/algorithms/R/Remove Covered Intervals/Remove Covered Intervals.js
+++ scripts/algorithms/R/Remove Covered Intervals/Remove Covered Intervals.js
@@ -1,12 +1,13 @@
+// Runtime: 144 ms (Top 6.56%) | Memory: 44.10 MB (Top 88.52%)
+
 var removeCoveredIntervals = function(intervals) {
-  intervals.sort((a,b)=> a[0]-b[0] || b[1]-a[1]);
-  let overlap=0;
-  for (i=1,prev=0; i<intervals.length; i++)
-//    if ((intervals[prev][0] <= intervals[i][0]) //no need to check, already done in sort
-//      && (intervals[prev][1] >= intervals[i][1]))
-	if (intervals[prev][1] >= intervals[i][1]) // just look at 2nd index
-      overlap++  // add to skipped elements
-    else
-      prev=i; // didn't overlap, so we can advance our previous element
-  return intervals.length-overlap;
+    intervals.sort((a, b) => (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]))
+    let count = 0, cur = 0
+    for(let interval of intervals){
+        if(cur < interval[1]){
+            cur = interval[1]
+            count++
+        }
+    }
+    return count
 };
