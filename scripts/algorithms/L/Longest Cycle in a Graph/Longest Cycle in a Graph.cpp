890cd765fb20a643eeed58a39f9e88a8c14f3700
AnasImloul <pydev27@gmail.com>
2024-01-01 01:06:20 +0100

    Runtime: 137 ms (Top 83.42%) | Memory: 90.50 MB (Top 95.59%)

--- scripts/algorithms/L/Longest Cycle in a Graph/Longest Cycle in a Graph.cpp
+++ scripts/algorithms/L/Longest Cycle in a Graph/Longest Cycle in a Graph.cpp
@@ -1,48 +1,29 @@
+// Runtime: 137 ms (Top 83.42%) | Memory: 90.50 MB (Top 95.59%)
+
 class Solution {
-public:
-    
-    int maxLength = -1;
-    
-    void getcycle(vector<int> &edges,int si,vector<bool>& visit,vector<int>& store){
-        if(si == -1)return ;
-        if(visit[si]){
-            int count = -1;
-            for(int i =0;i<store.size();i++){
-                if(store[i]==si){
-                    count = i;
-                    break;
-                } }
-            
-            if(count==-1)return;
-            int size = (store.size()-count);
-            maxLength = max(maxLength,size);
-            return ;
-        }
-        
-        visit[si] = true;
-        store.push_back(si);
-        getcycle(edges,edges[si],visit,store);
-        
-        
-        return ;
-        
+ public:
+  int longestCycle(vector<int>& edges) {
+    int ans = -1; // Initialize the answer to -1
+    int time = 1; // Initialize the current time step to 1
+    vector<int> timeVisited(edges.size()); // Initialize a vector to store the time at which each node was first visited
+
+    // Iterate through each node in the graph
+    for (int i = 0; i < edges.size(); ++i) {
+      if (timeVisited[i]) // If the node has already been visited, skip it
+        continue;
+      const int startTime = time; // Record the start time of the current traversal
+      int u = i; // Initialize the current node to the ith node
+      // Traverse the graph until the end of the path is reached or a visited node is encountered
+      while (u != -1 && !timeVisited[u]) {
+        timeVisited[u] = time++; // Record the current time step and increment time
+        u = edges[u]; // Move to the next node in the path
+      }
+      // If a cycle is found that includes the current node, update the answer
+      if (u != -1 && timeVisited[u] >= startTime)
+        ans = max(ans, time - timeVisited[u]);
     }
-    
-    
-    
-    int longestCycle(vector<int>& edges) {
-        
-        vector<bool> visit(edges.size(),0);
-        
-        for(int i =0;i<edges.size();i++){
-            
-            if(visit[i])continue;
-            vector<int> store;
-            getcycle(edges,i,visit,store);
-     
-        }
-        
-        return maxLength;
-        
-    }
+
+    return ans; // Return the length of the longest cycle found
+  }
 };
+
