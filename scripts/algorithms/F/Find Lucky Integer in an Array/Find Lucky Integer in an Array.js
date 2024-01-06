5ba5d40bf6bdd77f720967e9b4a61cc40196b938
AnasImloul <pydev27@gmail.com>
2024-01-06 21:47:29 +0100

    Runtime: 47 ms (Top 95.84%) | Memory: 44.00 MB (Top 53.55%)

--- scripts/algorithms/F/Find Lucky Integer in an Array/Find Lucky Integer in an Array.js
+++ scripts/algorithms/F/Find Lucky Integer in an Array/Find Lucky Integer in an Array.js
@@ -1,18 +1,24 @@
-/**
- * @param {number[]} arr
- * @return {number}
- */
- 
+// Runtime: 47 ms (Top 95.84%) | Memory: 44.00 MB (Top 53.55%)
+
+// time O(n) space O(n)
 var findLucky = function(arr) {
-	let obj = {};
-	arr.map((num) => {
-		obj[num] = obj[num] + 1 || 1;
-	});
-
-	let answer = -1;
-	for (let key in obj) {
-		if (Number(key) === obj[key]) answer = Math.max(answer, Number(key));
-	}
-
-	return answer;
+    let max = -1
+    
+    const map = {}
+    
+    for(const number of arr) {
+        if(map[number]) {
+            map[number] += 1
+        } else {
+            map[number] = 1
+        }
+    }
+    
+    for(const key in map) {
+        if(Number(key) === map[key] && map[key] > max) {
+            max = map[key]
+        }
+    }
+    
+    return max
 };
