18a665d629f668bb70befd4a4741c83ae7622c56
AnasImloul <pydev27@gmail.com>
2024-01-07 00:06:38 +0100

    Runtime: 11 ms (Top 95.78%) | Memory: 23.70 MB (Top 32.89%)

--- scripts/algorithms/R/Reverse String/Reverse String.cpp
+++ scripts/algorithms/R/Reverse String/Reverse String.cpp
@@ -1,12 +1,17 @@
+// Runtime: 11 ms (Top 95.78%) | Memory: 23.70 MB (Top 32.89%)
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int i = -1, j = s.size();
-        while (++i < --j){
-            //Instead of using temp we can do the following
-            s[i] = s[j] + s[i];
-            s[j] = s[i] - s[j];
-            s[i] = s[i] - s[j];
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while(i <= j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
     }
 };
