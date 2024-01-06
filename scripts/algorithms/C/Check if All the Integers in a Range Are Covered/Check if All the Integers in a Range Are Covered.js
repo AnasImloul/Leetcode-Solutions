b832f98c1cc1d63d4b793fe920a5a921622e22be
AnasImloul <pydev27@gmail.com>
2024-01-06 22:02:05 +0100

    Runtime: 50 ms (Top 78.79%) | Memory: 42.60 MB (Top 57.58%)

--- scripts/algorithms/C/Check if All the Integers in a Range Are Covered/Check if All the Integers in a Range Are Covered.js
+++ scripts/algorithms/C/Check if All the Integers in a Range Are Covered/Check if All the Integers in a Range Are Covered.js
@@ -1,16 +1,32 @@
+// Runtime: 50 ms (Top 78.79%) | Memory: 42.60 MB (Top 57.58%)
+
+/**
+ * @param {number[][]} ranges
+ * @param {number} left
+ * @param {number} right
+ * @return {boolean}
+ */
 var isCovered = function(ranges, left, right) {
-  var map=new Map()
-  for(let i=left;i<=right;i++){
-      map.set(i,0)
-  }
-  for(let range of ranges){
-      for(let i=range[0];i<=range[1];i++){
-          map.set(i,1)
-      }
-  }
-    // console.log(map)
-  for(let key of map.keys()){
-      if(map.get(key)===0) return false
-  }
+    let z=0,f=0,e=[];
+    let a=ranges.flat(Infinity);
+
+    function abc(c,d){
+        for(let i=c;i<=d;i++)
+        if(e[i]==i)
+        e[i]=null;
+    }
+    for(let i=left;i<=right;i++)
+        e[i]=i;
+    for(let i=0;i<a.length;i=i+2){
+        if(a[i]<=left&&a[i+1]>=right)
+        return true;
+        else if(a[i]>right)
+        continue;
+        else abc(a[i],a[i+1])
+    }
+    for(let i=left;i<=right;i++)
+    if(e[i]!=null)
+    return false
+    
     return true
 };
