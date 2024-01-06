134a9785289bcbf8f45e422bdda5ef6e51ecde3e
AnasImloul <pydev27@gmail.com>
2024-01-06 23:35:40 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 10.20 MB (Top 89.83%)

--- scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.cpp
+++ scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.cpp
@@ -1,19 +1,21 @@
-// Runtime: 0 ms (Top 100.00%) | Memory: 10 MB (Top 18.49%)
+// Runtime: 0 ms (Top 100.0%) | Memory: 10.20 MB (Top 89.83%)
+
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int>sk;
-        for(int i=0;i<prices.size();i++){
-        int discount=prices[i];
-            for(int j=i+1;j<prices.size();j++){
-                if(prices[i]>=prices[j]){
-                    discount=prices[i]-prices[j];
+        vector<int> res;
+        for(int i=0;i<prices.size();i++)
+        {
+            res.push_back(prices[i]);
+            for(int j=i+1;j<prices.size();j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    res[i] = prices[i]-prices[j];
                     break;
                 }
-
             }
-            sk.push_back(discount);
         }
-        return sk;
+        return res;
     }
 };
