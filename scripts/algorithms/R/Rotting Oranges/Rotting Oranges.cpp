41f40566ff2756ac578858d042b2155ecf9a9b63
AnasImloul <pydev27@gmail.com>
2024-01-06 22:24:55 +0100

    Runtime: 5 ms (Top 64.41%) | Memory: 13.50 MB (Top 68.29%)

--- scripts/algorithms/R/Rotting Oranges/Rotting Oranges.cpp
+++ scripts/algorithms/R/Rotting Oranges/Rotting Oranges.cpp
@@ -1,44 +1,51 @@
+// Runtime: 5 ms (Top 64.41%) | Memory: 13.50 MB (Top 68.29%)
+
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int R = grid.size();
-        int C = grid[0].size();
-        queue<vector<int>> nextOrangesToRot;
-        for(int r = 0; r < R; ++r)
-            for(int c = 0; c < C; ++c)
-                if(grid[r][c] == 2)
-                    nextOrangesToRot.push({r, c});
+    int orangesRotting(vector<vector<int>>& grid) 
+    {
         
-        vector<int> xDir {-1, 0, 0, 1};
-        vector<int> yDir {0, 1, -1, 0};
-        int totalTime = 0;
-        while(!nextOrangesToRot.empty())
-        {
-            int size = nextOrangesToRot.size();
-            while(size--)
+        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
+        
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        queue<pair<int,int>> q;
+        int fresh=0; //To keep track of all fresh oranges left
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
             {
-                vector<int>& currOrangeCell =  nextOrangesToRot.front();
-                
-                for(int i = 0; i < 4; ++i)
-                {
-                    int nextR = currOrangeCell[0] + xDir[i];
-                    int nextC = currOrangeCell[1] + yDir[i];
-                    if(nextR >= 0 && nextR < R && 
-                       nextC >= 0 && nextC < C && grid[nextR][nextC] == 1)
-                    {
-                        grid[nextR][nextC] = 2; //rotted
-                        nextOrangesToRot.push({nextR, nextC});
-                    }
-                }
-                nextOrangesToRot.pop();
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                if(grid[i][j]==1)
+                    fresh++;
             }
-            if(nextOrangesToRot.size())
-                totalTime++;
+        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
+        while(!q.empty())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int> p=q.front();
+                q.pop();
+                for(int i=0;i<4;i++)
+                {
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
+                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
+                    {
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
+                    }
+                    
+                }
+            }
+            ans++; //incremented after each minute passes
         }
-        for(int r = 0; r < R; ++r)
-            for(int c = 0; c < C; ++c)
-                if(grid[r][c] == 1)
-                   return -1;
-        return totalTime;
+        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
+        return ans;
+        
     }
 };
