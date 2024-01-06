4beb8f40f19a100e1ded5e3cebdcc864747d9feb
AnasImloul <pydev27@gmail.com>
2024-01-07 00:05:36 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 7.10 MB (Top 30.21%)

--- scripts/algorithms/M/Maximum Number of Words You Can Type/Maximum Number of Words You Can Type.cpp
+++ scripts/algorithms/M/Maximum Number of Words You Can Type/Maximum Number of Words You Can Type.cpp
@@ -1,25 +1,31 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 7.10 MB (Top 30.21%)
+
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
         vector<int> ch(26,0);
-		// store the broken letters in ch vector
+        //store the broken letters
         for(char c: brokenLetters){
             ch[c-'a']=1;
         }
-        int cnt=0,ans=0;
-		//traversing the text string
-        for(int i=0;i<text.length();i++){
-		    //if char is ' ' means that we got a new word
-            if(text[i]==' '){
-			    // cnt remain 0 means that there is no broken letter in this word
-                if(cnt==0) ans++;
-                cnt=0; //reinitialize cnt to 0 as new word start from here
-            }else if(ch[text[i]-'a']==1){ //if char is present in ch then just increment the cnt
-                    cnt++;
+        // breaking text into word using string stream
+        stringstream s(text); // Used for breaking words
+        string word; // to store individual words
+  
+        int count = 0,flag=0;
+        while (s >> word){
+            flag=0;
+            for(char &c: word){
+                //if the word contains a broken letter mark flag=1
+                if(ch[c-'a']==1){
+                    flag=1;
+                    break;
+                }
             }
+            //if flag is 0 which means that there is no broken letter in word so increase the count
+            if(flag==0)
+                count++;
         }
-		//for last word in string
-        if(cnt==0) ans++;
-        return ans;
+        return count;
     }
 };
