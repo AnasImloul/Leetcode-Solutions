df605314c9d262c8b696edaa6d8532f675fe7dfa
AnasImloul <pydev27@gmail.com>
2024-01-06 23:28:01 +0100

    Runtime: 7 ms (Top 84.73%) | Memory: 8.30 MB (Top 65.52%)

--- scripts/algorithms/U/Unique Substrings in Wraparound String/Unique Substrings in Wraparound String.cpp
+++ scripts/algorithms/U/Unique Substrings in Wraparound String/Unique Substrings in Wraparound String.cpp
@@ -1,35 +1,21 @@
+// Runtime: 7 ms (Top 84.73%) | Memory: 8.30 MB (Top 65.52%)
+
 class Solution {
 public:
-
     int findSubstringInWraproundString(string p) {
-        unordered_map<char, int> mp;
-
-        // if the characters are not contiguous and also check whether after 'z' I am getting 'a'. If so, reset the streak to 1
-        // else streak++
-
-        int streak = 0;
-
-        for (int i = 0; i < p.size(); i++) {
-
-            // contiguous
-            if (i and (p[i] - p[i - 1] == 1)) streak++;
-
-            // z...a case
-            else if (i and (p[i] == 'a' and p[i - 1] == 'z')) streak++;
-
-            else streak = 1;
-
-            mp[p[i]] = max(mp[p[i]], streak);
+        int ans=1,prev_ans=1;
+        vector<int> arr(26,0);
+        arr[p[0]-'a']=1;
+        for (int i=1;i<p.size();i++){
+            if ((p[i-1]-'a'+1)%26==p[i]-'a') // Checking for continuation
+                prev_ans++;
+            else
+                prev_ans=1;
+            if (arr[p[i]-'a']<prev_ans){
+                ans+=prev_ans-arr[p[i]-'a'];
+                arr[p[i]-'a']=prev_ans;
+            }
         }
-
-        // why sum? cuz there might be multiple substrings when the streak's broken for one
-
-        int ans = 0;
-        for (auto x : mp) {
-            ans += x.second;
-        }
-
         return ans;
     }
 };
-
