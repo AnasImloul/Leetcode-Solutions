38fe97df27b32db18b08c9b02285d9acddb6eb84
AnasImloul <pydev27@gmail.com>
2024-01-06 22:12:58 +0100

    Runtime: 53 ms (Top 61.16%) | Memory: 45.00 MB (Top 12.4%)

--- scripts/algorithms/R/Remove All Occurrences of a Substring/Remove All Occurrences of a Substring.js
+++ scripts/algorithms/R/Remove All Occurrences of a Substring/Remove All Occurrences of a Substring.js
@@ -1,15 +1,26 @@
-// Runtime: 121 ms (Top 20.93%) | Memory: 48 MB (Top 8.53%)
-var removeOccurrences = function(s, part) {
-    let n =part.length
-    const reduceString = (s)=>{
-        let searchPart =s.indexOf(part)
-        if (searchPart<0)
-            return s
-        s= s.split('')
-        s.splice(searchPart, n)
-        s=s.join('')
-        return reduceString(s)
+// Runtime: 53 ms (Top 61.16%) | Memory: 45.00 MB (Top 12.4%)
 
+var removeOccurrences = function(s, part) {
+    let stack = [];
+    for(let c of s) {
+        stack.push(c);
+        if(stack.length >= part.length && stack[stack.length-1] == part[part.length-1]) {
+            let temp = [];
+            for(let i = 0; i < part.length; i++) {
+                temp.push(stack.pop());
+            }
+            temp.reverse();
+            if(temp.join('') != part) {
+                for(let c of temp) {
+                    stack.push(c);
+                }
+            }
+        }
     }
-    return reduceString(s)
+    let ans = [];
+    while(stack.length > 0) {
+        ans.push(stack.pop());
+    }
+    ans.reverse();
+    return ans.join('');
 };
