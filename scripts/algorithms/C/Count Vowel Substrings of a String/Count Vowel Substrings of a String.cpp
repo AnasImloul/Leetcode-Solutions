3531535115440cceba9865512083d6cfc5234f9f
AnasImloul <pydev27@gmail.com>
2024-01-06 23:30:29 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.90 MB (Top 63.9%)

--- scripts/algorithms/C/Count Vowel Substrings of a String/Count Vowel Substrings of a String.cpp
+++ scripts/algorithms/C/Count Vowel Substrings of a String/Count Vowel Substrings of a String.cpp
@@ -1,24 +1,34 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.90 MB (Top 63.9%)
+
 class Solution {
-public:
-    int countVowelSubstrings(string word) {
-        int n = word.size();
-        int res = 0;
+    public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    };
+    
+    int atMostK(string &s, int k) {
+        int res = 0, i=0, n = s.size();
+        unordered_map<char, int> mp;
         
-        unordered_set<char> vowels {'a', 'e', 'i', 'o', 'u'};
-        
-        for (int i = 0; i < n; i++)
-        {
-            unordered_set<char> letters;
-            // string temp = "";
-            for (int j = i; j < n; j++)
-            {
-                // temp += word[j];
-                letters.insert(word[j]);
-                if (letters == vowels)
-                res++;
+        for(int j=0; j<n; j++) {
+            if (!isVowel(s[j])) {
+                i = j + 1;
+                //Clear map as new substring will begin
+                mp.clear();
+                continue;
             }
+            mp[s[j]]++;
+            while(mp.size() > k){
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
+            }
+            res += j - i + 1;
         }
-        
         return res;
     }
-}; 
+    
+    int countVowelSubstrings(string str) {
+        return atMostK(str, 5) - atMostK(str, 4);
+    }
+};
