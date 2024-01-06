224a053bf65dfb09891583f87d546b917a1f4984
AnasImloul <pydev27@gmail.com>
2024-01-06 22:14:04 +0100

    Runtime: 86 ms (Top 65.0%) | Memory: 44.60 MB (Top 65.0%)

--- scripts/algorithms/S/Stone Game IV/Stone Game IV.js
+++ scripts/algorithms/S/Stone Game IV/Stone Game IV.js
@@ -1,22 +1,42 @@
+// Runtime: 86 ms (Top 65.0%) | Memory: 44.60 MB (Top 65.0%)
+
+// DP problem
 var winnerSquareGame = function(n) {
+
+//             //////////////////////////////////
+//     1. Define an array that Alice will win or loss (T/F)
+//             //////////////////////////////////
+
+    const res = new Array(n+1).fill(false);
+    let k = 1;
     
-    const map = new Map();
-    map.set(0, false);
-    map.set(1, true);
-    
-    const dfs = (num) => {
-        if( map.has(num) ) return map.get(num);
-        let sqRoot = Math.floor(Math.sqrt(num));
+    for (let i = 1; i <= n; i++) {
+
+//             //////////////////////////////////
+//     2. if it is square number, obviously, Alice win
+//             //////////////////////////////////
         
-        for(let g=1; g<=sqRoot; g++){
-            if( !dfs(num - (g*g)) ) {
-                map.set(num, true);
-                return true;
-            }    
+        if (k*k === i) {
+            res[i] = true;
+            k++;
+            continue;
         }
-        map.set(num, false);
-        return false;
+
+//             //////////////////////////////////
+//     3. after Alice removes j*j (1 <= j < k) stones, find whether res[i-j*j] is T/F (= Bob win/loss)
+//        if j exists that res[i-j*j] = false, it means that Bob will lose the game with the remaining i-j*j stones => Alice wins
+//        otherwise, Alice lose 
+//             //////////////////////////////////
+
+        let AliceWin = false;
+        for (let j = 1; j < k && j*j <= i; j++) {
+            if (!res[i-j*j]) {
+                AliceWin = true;
+                break;
+            }
+        }
+        res[i] = AliceWin;
     }
     
-    return dfs(n)
+    return res[n];
 };
