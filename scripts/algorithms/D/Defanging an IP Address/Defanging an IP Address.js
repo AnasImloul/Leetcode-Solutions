1c21bedb51e5841c66e9636aa43c60e5259220c9
AnasImloul <pydev27@gmail.com>
2024-01-06 22:08:03 +0100

    Runtime: 45 ms (Top 87.3%) | Memory: 41.80 MB (Top 53.19%)

--- scripts/algorithms/D/Defanging an IP Address/Defanging an IP Address.js
+++ scripts/algorithms/D/Defanging an IP Address/Defanging an IP Address.js
@@ -1,7 +1,18 @@
+// Runtime: 45 ms (Top 87.3%) | Memory: 41.80 MB (Top 53.19%)
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
 var defangIPaddr = function(address) {
-   return address.split("").map((x)=>{
-        if(x=='.')
-            return "[.]"
-        else return x
-    }).join("")
+    var result="";
+    
+    for(var i=0;i<address.length;i++) {
+        if(address[i] === ".")
+            result += "[.]";
+        else
+            result += address[i];
+    }
+    
+    return result;    
 };
