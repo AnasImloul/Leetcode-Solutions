9e9a355e466641eaa7ded369481002b55c0d615b
AnasImloul <pydev27@gmail.com>
2024-01-06 21:55:52 +0100

    Runtime: 47 ms (Top 78.6%) | Memory: 41.90 MB (Top 55.1%)

--- scripts/algorithms/S/Summary Ranges/Summary Ranges.js
+++ scripts/algorithms/S/Summary Ranges/Summary Ranges.js
@@ -1,19 +1,30 @@
+// Runtime: 47 ms (Top 78.6%) | Memory: 41.90 MB (Top 55.1%)
 
+// Runtime: 46 ms, faster than 86.21% of JavaScript online submissions for Summary Ranges.
+// Time Complexity : O(N)
+// Space Complexity : O(1)
 var summaryRanges = function(nums) {
-    let result = [];
-    let startIdx = 0; 
-    
-    for(let i = 0; i < nums.length; i++){
-        let peekRight = nums[i+1];
-        let peekLeft = nums[i-1];
-        if (peekRight !== nums[i] + 1 && peekLeft !== nums[i] - 1){
-           result.push(`${nums[i]}`);
-           startIdx++;
-        } else if ( peekRight !== nums[i] + 1){
-          result.push(`${nums[startIdx]}->${nums[i]}`);
-          startIdx = i+1;
-        }
-      
-    } 
-   return result;
+    // Create a list of string to store the output result...
+    const output = [];
+    // Start traversing the array from idx = 0 till idx < sizeofarray in a while loop.
+    let idx = 0;
+    while(idx < nums.length) {
+        // Initialize beg and last index for identifying the continuous element in the array...
+        let beg, last;
+        // Mark the number at current index as beginning element of the range...
+        beg = nums[idx];
+        // Traverse the array beggining from current index & find the last element whose difference from previous element is exactly 1, i.e. nums[idx + 1] == nums[idx] + 1...
+        while(idx+1 < nums.length && nums[idx+1] == nums[idx] + 1) 
+            idx++;
+        // Set this element as last element of the range...
+        last = nums[idx];
+        // If continuous element isn't present...
+        if(beg == last)
+            output.push(beg + "");
+        // If present...
+        else
+            output.push( beg + "->" + last );
+        idx++;          
+    }
+    return output;      // Return the output result list...
 };
