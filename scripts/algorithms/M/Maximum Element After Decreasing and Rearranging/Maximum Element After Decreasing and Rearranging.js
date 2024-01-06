e36b8ed751d32dacead30f1c0f493a79d4ce70a9
AnasImloul <pydev27@gmail.com>
2024-01-06 21:35:05 +0100

    Runtime: 60 ms (Top 97.33%) | Memory: 51.50 MB (Top 90.66%)

--- scripts/algorithms/M/Maximum Element After Decreasing and Rearranging/Maximum Element After Decreasing and Rearranging.js
+++ scripts/algorithms/M/Maximum Element After Decreasing and Rearranging/Maximum Element After Decreasing and Rearranging.js
@@ -1,9 +1,14 @@
+// Runtime: 60 ms (Top 97.33%) | Memory: 51.50 MB (Top 90.66%)
+
 var maximumElementAfterDecrementingAndRearranging = function(arr) {
-    if (!arr.length) return 0
-    arr.sort((a, b) => a - b)
-    arr[0] = 1
+    arr.sort((a, b) => a - b);
+    let maxVal = 1;
+
     for (let i = 1; i < arr.length; i++) {
-        if (Math.abs(arr[i] - arr[i - 1]) > 1) arr[i] = arr[i - 1] + 1
+        if (arr[i] > maxVal) {
+            maxVal += 1;
+        }
     }
-    return arr.at(-1)
+
+    return maxVal;    
 };
