e9ccb8a14c2f889bdc0d2018c2e623b56a0dc27b
AnasImloul <pydev27@gmail.com>
2024-01-06 23:48:06 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 8.50 MB (Top 25.14%)

--- scripts/algorithms/O/Optimal Division/Optimal Division.cpp
+++ scripts/algorithms/O/Optimal Division/Optimal Division.cpp
@@ -1,19 +1,31 @@
-// Runtime: 4 ms (Top 45.16%) | Memory: 8 MB (Top 87.37%)
+// Runtime: 0 ms (Top 100.0%) | Memory: 8.50 MB (Top 25.14%)
+
+// This code is using 2nd Approach.
+
 class Solution {
 public:
     string optimalDivision(vector<int>& nums) {
-        string s="";
-        if(nums.size()==1)
-            return to_string(nums[0]);
-        if(nums.size()==2)
-            return to_string(nums[0])+'/'+to_string(nums[1]);
-        for(int i=0;i<nums.size();i++)
-        {
-            s+=to_string(nums[i])+"/";
-            if(i==0)
-                s+="(";
+        int n=nums.size();
+        string ans;
+		
+		// check for size if its 1 then we can't use parantheses and 
+		// if its 2 then also we should not use it.
+		
+        if(n==1) return ans=to_string(nums[0]);
+        if(n==2) return ans=to_string(nums[0])+"/"+to_string(nums[1]);
+		
+		//  for size greater than 2 add paranthese after first number 
+		
+        ans=to_string(nums[0]);
+        ans.append("/(");
+        for(int i=1;i<n-1;i++){
+            ans.append(to_string(nums[i])+"/");
         }
-        s[s.size()-1]=')';
-        return s;
+        ans.append(to_string(nums[n-1]));
+        ans.append(")");
+		
+		// finally this becomes as a/(b/c/d/....) which is our answer
+		
+        return ans;
     }
 };
