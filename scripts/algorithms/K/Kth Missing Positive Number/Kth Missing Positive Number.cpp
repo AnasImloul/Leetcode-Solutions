5d54ec09592ac1172f2fe0ac221e8043158c5731
AnasImloul <pydev27@gmail.com>
2024-01-06 23:43:28 +0100

    Runtime: 3 ms (Top 79.43%) | Memory: 9.90 MB (Top 63.69%)

--- scripts/algorithms/K/Kth Missing Positive Number/Kth Missing Positive Number.cpp
+++ scripts/algorithms/K/Kth Missing Positive Number/Kth Missing Positive Number.cpp
@@ -1,25 +1,20 @@
+// Runtime: 3 ms (Top 79.43%) | Memory: 9.90 MB (Top 63.69%)
+
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
-
-    int n=arr.size();
-    if(arr[n-1]==n)
-        return n+k;
-    else
+    int findKthPositive(vector<int>& arr, int k) 
     {
-        int start=0;
-        int end=n-1;
-        int ans;
-        while(start<=end)
+        int n=arr.size();
+        int low=0,high=n-1;
+        while(low<=high)
         {
-            int mid=start+(end-start)/2;
-            if((arr[mid]-(mid+1))<k)
-                start=mid+1;
+            int mid=(high-low)/2+low;
+            int x=arr[mid]-mid-1;//will give no. of missing elements
+            if(x>=k)
+            high=mid-1;
             else
-                end=mid-1;
+            low=mid+1;
         }
-        return start+k;
-    }
-
+        return k+low;   
     }
 };
