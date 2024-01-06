c2ad6b2a2e6136ae03b16e80d5334232e8139ae5
AnasImloul <pydev27@gmail.com>
2024-01-06 23:40:12 +0100

    Runtime: 18 ms (Top 81.73%) | Memory: 25.20 MB (Top 23.35%)

--- scripts/algorithms/P/Previous Permutation With One Swap/Previous Permutation With One Swap.cpp
+++ scripts/algorithms/P/Previous Permutation With One Swap/Previous Permutation With One Swap.cpp
@@ -1,17 +1,17 @@
+// Runtime: 18 ms (Top 81.73%) | Memory: 25.20 MB (Top 23.35%)
+
 class Solution {
 public:
     vector<int> prevPermOpt1(vector<int>& arr) {
-        int i, p, mn = -1;
-        for(i=arr.size() - 2; i>=0; i--) {
-            if(arr[i] > arr[i + 1]) break;
-        }
-        if(i == -1) return arr;
-
-        for(int j=i + 1; j<arr.size(); j++) {
-            if(arr[j] > mn && arr[j] < arr[i]) mn = arr[j], p = j;
-    
-        }
-        swap(arr[i], arr[p]);
+        int n = arr.size();
+        int c1 = n-2, c2 = n-1;
+        while(c1>=0 && arr[c1]<=arr[c1+1])
+            c1--;
+        if(c1<0)
+            return arr;
+        while(arr[c2] >= arr[c1] || c2>0 && arr[c2]==arr[c2-1])
+            c2--;
+        swap(arr[c1], arr[c2]);
         return arr;
     }
 };
