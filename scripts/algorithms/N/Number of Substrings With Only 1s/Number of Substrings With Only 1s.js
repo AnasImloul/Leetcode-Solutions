b0f934144975f489ff52c62b8590acd69184e526
AnasImloul <pydev27@gmail.com>
2024-01-06 22:04:25 +0100

    Runtime: 58 ms (Top 68.6%) | Memory: 45.00 MB (Top 50.0%)

--- scripts/algorithms/N/Number of Substrings With Only 1s/Number of Substrings With Only 1s.js
+++ scripts/algorithms/N/Number of Substrings With Only 1s/Number of Substrings With Only 1s.js
@@ -1,18 +1,5 @@
+// Runtime: 58 ms (Top 68.6%) | Memory: 45.00 MB (Top 50.0%)
+
 var numSub = function(s) {
-    const mod = Math.pow(10, 9)+7;
-    let r = 0, tot = 0;
-    
-    while (r<s.length) {
-        if (s[r]==='1') {
-            let tmp = r;
-            while (tmp < s.length && s[tmp] === '1') {
-                tot += tmp - r + 1;
-                tot%=mod;
-                tmp++;
-            }
-            r = tmp;
-        }
-        r++;
-    }
-    return tot;
+    return s.split("0").map(a=>a.length*(a.length+1)/2).reduce((a,b)=>a+b,0)  % 1000000007;
 };
