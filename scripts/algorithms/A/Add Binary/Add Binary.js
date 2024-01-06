aba0a6b5fda61fc3a2e4c6bec351c6bbef8ba53b
AnasImloul <pydev27@gmail.com>
2024-01-06 21:33:59 +0100

    Runtime: 52 ms (Top 82.96%) | Memory: 41.90 MB (Top 95.02%)

--- scripts/algorithms/A/Add Binary/Add Binary.js
+++ scripts/algorithms/A/Add Binary/Add Binary.js
@@ -1,12 +1,5 @@
+// Runtime: 52 ms (Top 82.96%) | Memory: 41.90 MB (Top 95.02%)
+
 var addBinary = function(a, b) {
-    let i = a.length-1, j = b.length-1, carry = 0
-    let s =[]
-    while(i >= 0 || j >= 0){
-        let sum = carry
-        if(i >= 0)  sum += a[i--].charCodeAt() - 48
-        if(j >= 0)  sum += b[j--].charCodeAt() - 48
-        s.unshift(sum % 2)
-        carry = ~~(sum/2)
-    }
-    return carry > 0 ? "1" + s.join("") : s.join("")
-};
+    return (BigInt("0b"+a) + BigInt("0b"+b)).toString(2);
+}
