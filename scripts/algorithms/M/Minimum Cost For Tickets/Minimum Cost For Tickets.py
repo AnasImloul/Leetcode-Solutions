a94b6129a3d0e05593d619659d02811956d2f6c5
AnasImloul <pydev27@gmail.com>
2024-01-07 21:08:13 +0100

    Runtime: 44 ms (Top 83.24%) | Memory: 17.30 MB (Top 40.22%)

--- scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.py
+++ scripts/algorithms/M/Minimum Cost For Tickets/Minimum Cost For Tickets.py
@@ -1,29 +1,18 @@
+// Runtime: 44 ms (Top 83.24%) | Memory: 17.30 MB (Top 40.22%)
+
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        min_cost = float('inf')
-        que = deque()
-        # enqueue the possible state of day 1
-        que.append((days[0], costs[0]))
-        que.append((days[0]+7-1, costs[1]))
-        que.append((days[0]+30-1, costs[2]))
-        last_day = days[-1]
-        for i in range(1, len(days)):
-            for _ in range(len(que)):
-                node = que.popleft()
-                if node[0] < days[i]:
-                    # try taking all the three pass on that day and enqueue 
-                    que.append((days[i], node[1]+costs[0]))
-                    if days[i] + 7 - 1 >= last_day:
-                        min_cost = min(min_cost, node[1] + costs[1])
-                    else:
-                        que.append((days[i]+7-1, node[1]+costs[1]))
-                    if days[i] + 30 - 1 >= last_day: 
-                        min_cost = min(min_cost, node[1] + costs[2])
-                    else:
-                        que.append((days[i]+30-1, node[1]+costs[2]))
-                else:
-                    que.append(node)
-        for _ in range(len(que)):
-            node = que.popleft()
-            min_cost = min(min_cost, node[1])
-        return min_cost
+        cost = 0
+        last7 = deque()
+        last30 = deque()
+        for day in days:
+            while last7 and last7[0][0] + 7 <= day:
+                last7.popleft()
+            while last30 and last30[0][0] + 30 <= day:
+                last30.popleft()
+            last7.append((day, cost + costs[1]))
+            last30.append((day, cost + costs[2]))
+            cost = min(cost + costs[0],
+                      last7[0][1],
+                      last30[0][1])
+        return cost
