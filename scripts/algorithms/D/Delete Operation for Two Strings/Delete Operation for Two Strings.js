d969e34840b3c69e2bef4bfa98186f135d79a8f7
AnasImloul <pydev27@gmail.com>
2024-01-07 01:09:21 +0100

    Runtime: 80 ms (Top 69.7%) | Memory: 45.20 MB (Top 98.48%)

--- scripts/algorithms/D/Delete Operation for Two Strings/Delete Operation for Two Strings.js
+++ scripts/algorithms/D/Delete Operation for Two Strings/Delete Operation for Two Strings.js
@@ -1,15 +1,17 @@
-var minDistance = function(word1, word2) {
-	const WORD1_LEN = word1.length;
-	const WORD2_LEN = word2.length;
-	const dp = Array(WORD1_LEN + 1).fill('').map(() => Array(WORD2_LEN + 1).fill(0));
+// Runtime: 80 ms (Top 69.7%) | Memory: 45.20 MB (Top 98.48%)
 
-	for (let index = 1; index <= WORD1_LEN; index++) {
-		for (let j = 1; j <= WORD2_LEN; j++) {
-			dp[index][j] = word1[index - 1] === word2[j - 1]
-				? dp[index - 1][j - 1] + 1
-				: Math.max(dp[index - 1][j], dp[index][j - 1]);
-		}
-	}
-
-	return WORD1_LEN + WORD2_LEN - 2 * dp[WORD1_LEN][WORD2_LEN];
+var minDistance = function(W1, W2) {
+    let m = W1.length, n = W2.length
+    if (m < n) [W1, W2, m, n] = [W2, W1, n, m]
+    let WA1 = W1.split(""), WA2 = W2.split(""),
+        dpLast = new Uint16Array(n + 1),
+        dpCurr = new Uint16Array(n + 1)
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) 
+            dpCurr[j+1] = WA1[i] === WA2[j]
+                ? dpLast[j] + 1
+                : Math.max(dpCurr[j], dpLast[j+1]);
+        [dpLast, dpCurr] = [dpCurr, dpLast]
+    }
+    return m + n - 2 * dpLast[n] 
 };
