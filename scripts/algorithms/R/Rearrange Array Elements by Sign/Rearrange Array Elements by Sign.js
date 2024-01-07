29d26838a21d172f77a87286b3cefae5636caee8
AnasImloul <pydev27@gmail.com>
2024-01-07 01:31:37 +0100

    Runtime: 261 ms (Top 95.19%) | Memory: 96.70 MB (Top 54.92%)

--- scripts/algorithms/R/Rearrange Array Elements by Sign/Rearrange Array Elements by Sign.js
+++ scripts/algorithms/R/Rearrange Array Elements by Sign/Rearrange Array Elements by Sign.js
@@ -1,15 +1,25 @@
-var rearrangeArray = function(nums) {
-   let result = Array(nums.length).fill(0);
-    let posIdx = 0, negIdx = 1;
-   for(let i=0;i<nums.length;i++) {
-       if(nums[i]>0) {
-           result[posIdx] = nums[i]
-           posIdx +=2;
-       } else {
-            result[negIdx] = nums[i]
-           negIdx +=2;
-       }
-   }
-    return result;
- };
+// Runtime: 261 ms (Top 95.19%) | Memory: 96.70 MB (Top 54.92%)
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let result = [];
+    let l=0;
+    let r=0;
+    while(result.length !== nums.length){
+         while( nums[l]< 0){
+              l++
+          }
+        while( nums[r]> 0){
+              r++
+         }
+        
+        result.push(nums[l], nums[r]);
+        l++;
+        r++;
+    }
+    return result
+    
+};
