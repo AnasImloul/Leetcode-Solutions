29a4627d10f83d8bbb80d23d1efbe9328a26c863
AnasImloul <pydev27@gmail.com>
2024-01-07 21:07:00 +0100

    Runtime: 110 ms (Top 96.18%) | Memory: 17.60 MB (Top 51.23%)

--- scripts/algorithms/M/Minimum Number of Taps to Open to Water a Garden/Minimum Number of Taps to Open to Water a Garden.py
+++ scripts/algorithms/M/Minimum Number of Taps to Open to Water a Garden/Minimum Number of Taps to Open to Water a Garden.py
@@ -1,16 +1,20 @@
+// Runtime: 110 ms (Top 96.18%) | Memory: 17.60 MB (Top 51.23%)
+
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        maxRanges = [0]
-        for i in range(len(ranges)):
-            minIdx = max(i - ranges[i], 0)
-            maxIdx = min(i + ranges[i], n)
-            idx = bisect_left(maxRanges, minIdx)
-            if idx == len(maxRanges) or maxIdx <= maxRanges[idx]: continue
-            if idx == len(maxRanges) - 1:
-                maxRanges.append(maxIdx)
-            else:
-                maxRanges[idx + 1] = max(maxRanges[idx + 1], maxIdx)
-        if maxRanges[-1] < n:
-            return -1
-        else:
-            return len(maxRanges) - 1
+        max_range = [0] * (n + 1)
+        
+        for i, r in enumerate(ranges):
+            left, right = max(0, i - r), min(n, i + r)
+            max_range[left] = max(max_range[left], right - left)
+        
+		# it's a jump game now
+        start = end = step = 0
+        
+        while end < n:
+            step += 1
+            start, end = end, max(i + max_range[i] for i in range(start, end + 1))
+            if start == end:
+                return -1
+            
+        return step
