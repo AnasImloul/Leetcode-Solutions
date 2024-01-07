2d90b34ba6b353a1448483c2381d18ba4ebe7d60
AnasImloul <pydev27@gmail.com>
2024-01-07 01:37:44 +0100

    Runtime: 81 ms (Top 79.6%) | Memory: 49.10 MB (Top 75.12%)

--- scripts/algorithms/M/Maximum Binary Tree/Maximum Binary Tree.js
+++ scripts/algorithms/M/Maximum Binary Tree/Maximum Binary Tree.js
@@ -1,11 +1,10 @@
+// Runtime: 81 ms (Top 79.6%) | Memory: 49.10 MB (Top 75.12%)
+
 var constructMaximumBinaryTree = function(nums) {
-    if (nums.length === 0) return null;
-    let max = Math.max(...nums);
-    let index = nums.indexOf(max);
-    let left = nums.slice(0, index);
-    let right = nums.slice(index + 1);
-    let root = new TreeNode(max);
-    root.left = constructMaximumBinaryTree(left);
-    root.right = constructMaximumBinaryTree(right);
-    return root;
-};
+    max = Math.max(...nums)
+    var index = nums.indexOf(max)
+    var tree = new TreeNode(max)
+    if(index !==0) tree.left = constructMaximumBinaryTree(nums.slice(0,index));
+    if(index !==nums.length-1) tree.right=constructMaximumBinaryTree(nums.slice(index+1));
+    return tree
+}
