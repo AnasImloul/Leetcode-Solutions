a5176adecab91c49a00af9b76fe2edfa675d8094
AnasImloul <pydev27@gmail.com>
2024-01-07 21:09:46 +0100

    Runtime: 51 ms (Top 88.07%) | Memory: 17.40 MB (Top 31.65%)

--- scripts/algorithms/B/Bag of Tokens/Bag of Tokens.py
+++ scripts/algorithms/B/Bag of Tokens/Bag of Tokens.py
@@ -1,14 +1,24 @@
+// Runtime: 51 ms (Top 88.07%) | Memory: 17.40 MB (Top 31.65%)
+
 class Solution:
-    def bagOfTokensScore(self, tokens, power):
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         tokens.sort()
-        n = len(tokens)
-        i, j = 0, n
-        while i < j:
-            if tokens[i] <= power:
-                power -= tokens[i]
-                i += 1
-            elif i - (n - j) and j > i + 1:
-                j -= 1
-                power += tokens[j]
-            else: break
-        return i - (n - j)
+        i,j=0,len(tokens)-1
+        m=score=0
+        while i<j:
+            if tokens[i]>power and score==0:
+                break  
+            if tokens[i]<=power:
+                score+=1
+                power-=tokens[i]
+                m=max(m,score)
+                i+=1
+            else:
+                if score>0:
+                    score-=1
+                    power+=tokens[j]
+                    j-=1
+        if i<len(tokens) and tokens[i]<=power:
+            score+=1
+            m=max(m,score)
+        return m
