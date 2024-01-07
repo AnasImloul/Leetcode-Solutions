63633108e3e21b402d7ceef3919c83b2a8685da3
AnasImloul <pydev27@gmail.com>
2024-01-07 01:16:40 +0100

    Runtime: 83 ms (Top 70.37%) | Memory: 48.90 MB (Top 44.44%)

--- scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.js
+++ scripts/algorithms/L/Last Substring in Lexicographical Order/Last Substring in Lexicographical Order.js
@@ -1,8 +1,41 @@
-// Runtime: 2445 ms (Top 18.75%) | Memory: 52 MB (Top 18.75%)
+// Runtime: 83 ms (Top 70.37%) | Memory: 48.90 MB (Top 44.44%)
+
+// Second Solution
+// Efficient
+/*
+The substring starting with the largest character is the answer. Hence we advance j and try to find the biggest character 
+while we use i to keep track of the current maximum string.
+What if there is more than one largest character?
+- we will need to compare both substrings starting with the largest character.
+- (1) the substring with bigger next character is the answer.
+- (2) if the shorter substring is exactly the same to the longer substring up to its end, the longer substring is the answer.
+
+i is the starting index of the answer
+j is the pointer iterating through given string
+k is the offset to use when we find two substrings starting with the same character
+*/
 var lastSubstring = function(s) {
-    let res = 0;
-    for(let i = 1; i < s.length; i++){
-        if(s.slice(i) > s.slice(res)) res = i;
-    };
-    return s.slice(res);
+    let i = 0, j = 1, k = 0;
+    while (j + k < s.length) {
+        k = 0;
+        while (s[i+k] === s[j+k]) {
+            k += 1;
+            if (j + k === s.length) break;
+            // if the shorter substring is exactly the same to the longer substring up to its end, 
+            // the longer substring is the answer.
+            // hence, if j+k reaches the end, s.substring(i) will be the answer since we maintain i < j
+        }
+        if (s[i+k] > s[j+k]) {
+            j = j + 1 + k; // because there is no chance s[i+k] <= s[j+k]
+        } else if (s[i+k] < s[j+k]) {
+            i = i + 1 + k; // because there is no chance s[i+k] >= s[j+k]
+        }
+        // to ensure that i < j
+        if (i >= j) {
+            j = i + 1;
+        }
+    }
+    return s.substring(i);
+    // T.C: O(N)
+    // S.C: O(1)
 };
