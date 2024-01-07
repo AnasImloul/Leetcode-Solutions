93077e06b276436b1be103f48c99756a9e12479a
AnasImloul <pydev27@gmail.com>
2024-01-07 21:14:31 +0100

    Runtime: 623 ms (Top 88.48%) | Memory: 22.50 MB (Top 12.84%)

--- scripts/algorithms/F/Find the Town Judge/Find the Town Judge.py
+++ scripts/algorithms/F/Find the Town Judge/Find the Town Judge.py
@@ -1,16 +1,13 @@
+// Runtime: 623 ms (Top 88.48%) | Memory: 22.50 MB (Top 12.84%)
+
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        possible = set([i for i in range(1,n+1)])
-        in_edges = [0] * (n+1)
-        
-        for first, second in trust:
-            possible.discard(first)
-            in_edges[second] += 1
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        Trusted = [0] * (N+1)
+        for (a, b) in trust:
+            Trusted[a] -= 1
+            Trusted[b] += 1
             
-        if len(possible) != 1:
-            return -1
-        
-        i = possible.pop()
-        if in_edges[i] == n-1:
-            return i
+        for i in range(1, len(Trusted)):
+            if Trusted[i] == N-1:
+                return i
         return -1
