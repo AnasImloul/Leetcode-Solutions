9d67536645a36ded93c12f03fc95eeed5ab4804f
AnasImloul <pydev27@gmail.com>
2024-01-07 01:36:04 +0100

    Runtime: 215 ms (Top 66.67%) | Memory: 127.70 MB (Top 16.67%)

--- scripts/algorithms/C/Count Artifacts That Can Be Extracted/Count Artifacts That Can Be Extracted.js
+++ scripts/algorithms/C/Count Artifacts That Can Be Extracted/Count Artifacts That Can Be Extracted.js
@@ -1,35 +1,51 @@
+// Runtime: 215 ms (Top 66.67%) | Memory: 127.70 MB (Top 16.67%)
+
+/**
+ * @param {number} n
+ * @param {number[][]} artifacts
+ * @param {number[][]} dig
+ * @return {number}
+ */
 var digArtifacts = function(n, artifacts, dig) {
-    const set = new Set();
-    
-    for (const [row, col] of dig) {
-        set.add(getCoord(row, col));
-    }
-    
-    let count = 0;
-    
-    for (let i = 0; i < artifacts.length; ++i) {
-        const [startRow, startCol, endRow, endCol] = artifacts[i];
-        
-        let extracted = true;
-        
-        for (let row = startRow; row <= endRow; ++row) {
-            for (let col = startCol; col <= endCol; ++col) {
-                const coord = getCoord(row, col);        
-        
-                if (!set.has(coord)) {
-                    extracted = false;
-                    break;
-                }
-            }
+   // Step 1
+    const virtualMap = [] // 0 = null, n = artifact_n
+    for (let x = 0; x< n; x++){
+        const row = []
+        for(let y = 0; y<n; y++){
+            row.push(0)
         }
-        
-        if (extracted) ++count;
-        
+        virtualMap.push(row)
     }
-    
-    return count;
-    
-    function getCoord(row, col) {
-        return `${row}#${col}`;
+	
+	// Step 2
+    const artifactSize = []
+    for(let artifactIdx in artifacts){
+        let [artY1, artX1, artY2, artX2] = artifacts[artifactIdx]
+        artifactSize.push((artX2 + 1 - artX1) * (artY2 + 1 - artY1))
+        while(artY1 <= artY2){
+            while(artX1 <= artX2){
+                virtualMap[artY1][artX1] = Number(artifactIdx)+1
+                artX1++
+            }
+            artY1++
+            artX1 = artifacts[artifactIdx][1]
+        }
     }
+
+    // Step 3
+    for(let digComp of dig){
+        const [digY, digX] = digComp
+        const content = virtualMap[digY][digX]
+        if(content !== 0){
+            artifactSize[content-1] -= 1 
+        }
+    }
+
+    // Step 4
+    return artifactSize.reduce((acc, remainder) => {
+        if(remainder === 0){
+            acc++
+        }
+        return acc
+    }, 0)
 };
