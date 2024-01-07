52d8da84e6c778ee5306a417bfbc5b762cdc0154
AnasImloul <pydev27@gmail.com>
2024-01-07 21:23:21 +0100

    Runtime: 72 ms (Top 94.85%) | Memory: 47.70 MB (Top 62.89%)

--- scripts/algorithms/M/Minesweeper/Minesweeper.js
+++ scripts/algorithms/M/Minesweeper/Minesweeper.js
@@ -1,34 +1,42 @@
+// Runtime: 72 ms (Top 94.85%) | Memory: 47.70 MB (Top 62.89%)
+
 var updateBoard = function(board, click) {
-	const [clickR, clickC] = click;
-	const traverseAround = ({ currentRow, currentCol, fun }) => {
-		for (let row = -1; row <= 1; row++) {
-			for (let col = -1; col <= 1; col++) {
-				fun(currentRow + row, currentCol + col);
-			}
-		}
-	};
-	const getMinesCount = (currentRow, currentCol) => {
-		let result = 0;
+  const rows = board.length;
+  const cols = board[0].length;
+  dfs(click[0], click[1]);
+  return board;
 
-		function check(row, col) {
-			const value = board[row]?.[col];
-			if (value == 'M') result += 1;
-		}
-		traverseAround({ currentRow, currentCol, fun: check });
-		return result;
-	};
-	const dfs = (row = clickR, col = clickC) => {
-		const currnet = board[row]?.[col];
-		if (currnet !== 'E') return;
-		const minesCount = getMinesCount(row, col);
-		board[row][col] = minesCount === 0 ? 'B' : `${minesCount}`;
-		if (minesCount > 0) return;
+  function dfs(i, j) {
+    if (!board[i][j]) return;
+    if (board[i][j] === 'M') {
+      board[i][j] = 'X';
+      return;
+    }
+    if (board[i][j] !== 'E') return;
 
-		traverseAround({ currentRow: row, currentCol: col, fun: dfs });
-	};
+    const mines = checkForMine(i, j); // Check for mines
+	
+    if (mines) {
+      board[i][j] = mines.toString();
+      return;
+    } else {
+      // If we haven't got mines, check another cells
+      board[i][j] = 'B';
+      for (let x = Math.max(i - 1, 0); x < Math.min(i + 2, rows); x++) {
+        for (let y = Math.max(j - 1, 0); y < Math.min(j + 2, cols); y++) {
+          dfs(x, y);
+        }
+      }
+    }
+  }
 
-	board[clickR][clickC] === 'M'
-		? board[clickR][clickC] = 'X'
-		: dfs();
-	return board;
-};
+  function checkForMine(x, y) {
+    let mines = 0;
+    for (let i = Math.max(x - 1, 0); i < Math.min(x + 2, rows); i++) {
+      for (let j = Math.max(y - 1, 0); j < Math.min(y + 2, cols); j++) {
+        if (board[i][j] === 'M') mines++;
+      }
+    }
+    return mines;
+  }
+}
