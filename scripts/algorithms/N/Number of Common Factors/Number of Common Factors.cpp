6fb667955010f3aaf6f668718d0b49a72c522b50
AnasImloul <pydev27@gmail.com>
2024-01-08 01:31:52 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.30 MB (Top 37.62%)

--- scripts/algorithms/N/Number of Common Factors/Number of Common Factors.cpp
+++ scripts/algorithms/N/Number of Common Factors/Number of Common Factors.cpp
@@ -1,14 +1,14 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.30 MB (Top 37.62%)
+
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        int count=0;
-       for(int i=1;i<=min(a,b);i++)
-       {
-            if((a%i==0) and (b%i==0))
-            {
-              count++;	
-            }
-       }        
-       return count;
+        int cf = 0;
+        int m = min(a, b);
+        for(int i = 1; i <= m; i++) {
+            if(a % i == 0 && b % i == 0)
+                cf++;
+        }
+        return cf;
     }
 };
