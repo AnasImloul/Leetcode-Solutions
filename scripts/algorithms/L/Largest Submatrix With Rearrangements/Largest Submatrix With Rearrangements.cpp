1a32c867054d567d5cca16ba9e7c4983aa47e4dc
AnasImloul <pydev27@gmail.com>
2024-01-09 12:54:56 +0100

    Runtime: 145 ms (Top 80.13%) | Memory: 84.00 MB (Top 13.41%)

--- scripts/algorithms/L/Largest Submatrix With Rearrangements/Largest Submatrix With Rearrangements.cpp
+++ scripts/algorithms/L/Largest Submatrix With Rearrangements/Largest Submatrix With Rearrangements.cpp
@@ -1,27 +1,30 @@
+// Runtime: 145 ms (Top 80.13%) | Memory: 84.00 MB (Top 13.41%)
+
+#pragma GCC optimize("O3")
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        int ans = 0;
-        vector<int> height(n, 0);
-		
-		// view each row and its above as pillars 
-        for(int i = 0; i < m; ++i){
-			// calculate heights
-            for(int j = 0; j < n; ++j){
-                if(matrix[i][j] == 0) height[j] = 0;
-                else height[j] += 1;
+        int m=matrix.size(), n=matrix[0].size();
+        int area=count(matrix[0].begin(), matrix[0].end(), 1);
+        for(int i=1;  i<m; i++){
+            #pragma unroll
+            for(int j=0; j<n; j++){
+                if (matrix[i][j]!=0)
+                    matrix[i][j]+=matrix[i-1][j];
             }
-			
-			// sort pillars
-            vector<int> order_height = height;
-            sort(order_height.begin(), order_height.end());
-			
-			// iterate to get the maxium rectangle
-            for(int j = 0; j < n; ++j){
-                ans = max(ans, order_height[j] * (n - j));
-            }
+            auto row=matrix[i];
+            sort(row.begin(), row.end());
+            #pragma unroll
+            for(int i=0; i<n; i++)
+                area=max(area, row[i]*(n-1-i+1));
         }
-        return ans;
+        return area;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
