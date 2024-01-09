2cccf19220104e8a27ec13491171aa04dd24e601
AnasImloul <pydev27@gmail.com>
2024-01-09 03:54:47 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.70 MB (Top 37.86%)

--- scripts/algorithms/V/Valid Parentheses/Valid Parentheses.cpp
+++ scripts/algorithms/V/Valid Parentheses/Valid Parentheses.cpp
@@ -1,22 +1,18 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.70 MB (Top 37.86%)
+
 class Solution {
 public:
-// Ref:  balanced parenthesis video from LUV bhaiya YT channel 
-unordered_map<char,int>symbols={{'[',-1},{'{',-2},{'(',-3},{']',1},{'}',2},{')',3}};
-    stack<char>st;
     bool isValid(string s) {
-        for(auto &i:s){
-            if(symbols[i]<0){
-                st.push(i);
-            }
-            else{
-                if(st.empty()) return false;
-                char top=st.top();
-                st.pop();
-                if(symbols[i]+symbols[top]!=0) return false;
+        stack<char> st;  //taking stack for keep tracking the order of the brackets..
+        for(auto i:s)  //iterate over each and every elements
+        {
+            if(i=='(' or i=='{' or i=='[') st.push(i);  //if current element of the string will be opening bracket then we will just simply push it into the stack
+            else  //if control comes to else part, it means that current element is a closing bracket, so check two conditions  current element matches with top of the stack and the stack must not be empty...
+            {
+                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) return false;
+                st.pop();  //if control reaches to that line, it means we have got the right pair of brackets, so just pop it.
             }
         }
-    if(st.empty()) return true;
-    return false;
-        
+        return st.empty();  //at last, it may possible that we left something into the stack unpair so return checking stack is empty or not..
     }
 };
