c751c2cdfef66800f57029b366e935937c3e45a0
AnasImloul <pydev27@gmail.com>
2024-01-09 07:23:57 +0100

    Runtime: 7 ms (Top 81.4%) | Memory: 10.90 MB (Top 13.89%)

--- scripts/algorithms/C/Counting Words With a Given Prefix/Counting Words With a Given Prefix.cpp
+++ scripts/algorithms/C/Counting Words With a Given Prefix/Counting Words With a Given Prefix.cpp
@@ -1,20 +1,25 @@
+// Runtime: 7 ms (Top 81.4%) | Memory: 10.90 MB (Top 13.89%)
+
+// The easiest solution to this problem is:
+// Step 1: Calculate length of "pref"
+// Step 2: find substring of "words" from 0 to length of "pref"
+// if both match increment count by 1;
+
+
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int c=0;
-        for(auto word:words){
-            int b=0;
-            for(int i=0;i<pref.size();i++){
-                if(word[i]!=pref[i]){
-                    break;
-                }else{
-                    b++;
-                }
-            }
-            if(b==pref.size()){
-                c++;
-            }
+        
+        int count=0;
+        int preflen=pref.size();        //step 1
+        
+        for(auto i:words){
+            if(i.substr(0,preflen) == pref)     //step 2
+                count++;                        //if both matches then increment count by 1
+            
         }
-        return c;
+        return count;   //return count
+        
     }
 };
