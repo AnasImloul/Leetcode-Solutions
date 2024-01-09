520dccaf2a0792842d06ed5c53ce2fdf8df84d1c
AnasImloul <pydev27@gmail.com>
2024-01-09 13:52:30 +0100

    Runtime: 208 ms (Top 67.74%) | Memory: 64.90 MB (Top 80.65%)

--- scripts/algorithms/S/Shortest Path to Get All Keys/Shortest Path to Get All Keys.js
+++ scripts/algorithms/S/Shortest Path to Get All Keys/Shortest Path to Get All Keys.js
@@ -1,96 +1,154 @@
-var shortestPathAllKeys = function(grid) {
-    const n = grid.length;
-    const m = grid[0].length;
+// Runtime: 208 ms (Top 67.74%) | Memory: 64.90 MB (Top 80.65%)
 
-    function isKey(letter) {
-        return (
-            letter.charCodeAt(0) >= 'a'.charCodeAt(0) && letter.charCodeAt(0) <= 'z'.charCodeAt(0)
-        );
+/**
+ * @param {string[]} grid
+ * @return {number}
+ */
+var shortestPathAllKeys = function(grid) 
+{
+    const rows = grid.length, cols = grid[0].length, INF = Number.MAX_SAFE_INTEGER-1;
+
+    /* Find the locks and keys */
+    let keyChars = '', start;
+    for(let i=0; i<rows; i++)
+    {
+        for(let j=0; j<cols; j++)
+        {
+            let letter = grid[i][j];
+            if(letter === '@')
+            {
+                start = [i,j];
+                continue;
+            }
+            if(letter === '.' || letter === '#')
+                continue;
+            if(letter >= 'a')
+                keyChars += letter;
+        }
+    }
+    let lockChars = keyChars.toUpperCase();
+    const combos = Math.pow(2,keyChars.length);
+
+
+    let mask = 0x01, bitPos = {}, unlocks = {};
+    for(let i=0; i<keyChars.length; i++)
+    {
+        bitPos[keyChars[i]] = mask;
+        unlocks[lockChars[i]] = mask;
+        mask *= 2;
     }
 
-    function isLock(letter) {
-        return (
-            letter.charCodeAt(0) >= 'A'.charCodeAt(0) && letter.charCodeAt(0) <= 'Z'.charCodeAt(0)
-        );
+    let dp = Array(rows);
+    for(let i=0; i<rows; i++)
+    {
+        dp[i] = Array(cols);
+        for(let j=0; j<cols; j++)
+            dp[i][j] = Array(combos).fill(INF);
     }
+    dp[start[0]][start[1]][0] = 0;
+//console.log(dp);
+//console.log(bitPos, unlocks);
+    
+    const getNeighbors = function(r, c)
+    {
+        let neighbors = [];
+        if(r > 0)
+            neighbors.push([r-1,c]);
+        if(r < rows-1)
+            neighbors.push([r+1,c]);
+        if(c > 0)
+            neighbors.push([r,c-1]);
+        if(c < cols-1)
+            neighbors.push([r,c+1]);
 
-    const dns = [
-        [1, 0],
-        [0, 1],
-        [-1, 0],
-        [0, -1],
-    ];
+        return neighbors;
+    };
 
-    let start = { x: 0, y: 0, keys: new Set() };
-    let keyMap = new Map();
-
-    for (let y = 0; y < n; ++y) {
-        for (let x = 0; x < m; ++x) {
-            const cell = grid[y][x];
-            if (cell === '@') {
-                start.x = x;
-                start.y = y;
-            } else if (isKey(cell)) {
-                keyMap.set(cell, [x, y]);
+    /**** SMUSH functions to "smush" through the various terrain ****/
+    const smushWall = (r,c,prev) => false;
+    const smushSpace = function(r,c,prev)
+    {
+        let changed = false;
+        for(let i=0; i<prev.length; i++)
+        {
+            if(dp[r][c][i] > prev[i]+1)
+            {
+                dp[r][c][i] = prev[i]+1;
+                changed = true;
             }
         }
-    }
-
-    function attemptToGetKey(x, y, keys, kx, ky) {
-        let q = [[x, y, keys, 0]];
-        let v = new Set();
-        while (q.length) {
-            let nextQueue = [];
-            for (let i = 0; i < q.length; ++i) {
-                const [x, y, keys, steps] = q[i];
-                if (x === kx && ky === y) {
-                    keys.add(grid[ky][kx]);
-                    return { keysAcquired: keys, movesUsed: steps };
-                }
-                const locationHash = x + y * m;
-                if (v.has(locationHash)) continue;
-                v.add(locationHash);
-                const cell = grid[y][x];
-                if (cell !== '.') {
-                    if (isLock(cell) && !keys.has(cell.toLowerCase())) {
-                        continue;
-                    }
-                    if (isKey(cell)) {
-                        keys.add(cell);
-                    }
-                }
-                for (const [mx, my] of dns) {
-                    const nx = x + mx;
-                    const ny = y + my;
-                    const nextHash = ny * m + nx;
-                    if (grid[ny]?.[nx] != null && grid[ny]?.[nx] !== '#') {
-                        if (!v.has(nextHash)) {
-                            q.push([nx, ny, new Set(Array.from(keys)), steps + 1]);
-                        }
-                    }
+        return changed;
+    };
+    const smushKeyFunc = function(key)
+    {
+        let mask = bitPos[key];
+        const smushKey = function(r,c,prev)
+        {
+            let changed = false;
+            for(let i=0; i<prev.length; i++)
+            {
+                if(dp[r][c][i|mask] > prev[i]+1)
+                {
+                    dp[r][c][i|mask] = prev[i]+1;
+                    changed = true;
                 }
             }
-            q = nextQueue;
+            return changed;
         }
-        return { keysAcquired: null, movesUsed: 0 };
+        return smushKey;
+    };
+    const smushLockFunc = function(key)
+    {
+        let mask = unlocks[key];
+        const smushLock = function(r,c,prev)
+        {
+            let changed = false;
+            for(let i=0; i<prev.length; i++)
+            {
+                if((i & mask) !== mask)
+                    continue;
+                if(dp[r][c][i] > prev[i]+1)
+                {
+                    dp[r][c][i] = prev[i]+1;
+                    changed = true;
+                }
+            }
+            return changed;
+        }
+        return smushLock;
+    };
+    let smush = {'#':smushWall, '.':smushSpace, '@':smushSpace};
+    for(let i=0; i<keyChars.length; i++)
+    {
+        smush[keyChars[i]] = smushKeyFunc(keyChars[i]);
+    }
+    for(let i=0; i<lockChars.length; i++)
+    {
+        smush[lockChars[i]] = smushLockFunc(lockChars[i]);
     }
 
-    let q = new MinPriorityQueue();
-    q.enqueue([start.x, start.y, start.keys], 0);
-    while (q.size()) {
-        const { element, priority: steps } = q.dequeue();
-        const [x, y, keys] = element;
-        if (keys.size === keyMap.size) {
-            return steps;
-        }
-        for (const [key, [kx, ky]] of keyMap) {
-            if (!keys.has(key)) {
-                let { movesUsed, keysAcquired } = attemptToGetKey(x, y, keys, kx, ky);
-                if (movesUsed) {
-                    q.enqueue([kx, ky, keysAcquired], movesUsed + steps);
+    let minPath = INF,  buf  = [start];
+    while(buf.length > 0)
+    {
+        let next = new Set();
+        for(let [r,c] of buf)
+        {
+            let neighbors = getNeighbors(r,c);
+            for(let [nrow,ncol] of neighbors)
+            {
+                if(smush[grid[nrow][ncol]](nrow, ncol, dp[r][c]))
+                {
+                    if(dp[nrow][ncol][dp[nrow][ncol].length-1] < INF)
+                        minPath = Math.min(minPath, dp[nrow][ncol][dp[nrow][ncol].length-1]);
+                    else
+                        next.add([nrow,ncol]);
                 }
             }
         }
+        buf = [...next];
     }
-    return -1;
-}
+
+//console.log(bitPos);
+
+    return (minPath === INF) ? -1 : minPath;
+};
