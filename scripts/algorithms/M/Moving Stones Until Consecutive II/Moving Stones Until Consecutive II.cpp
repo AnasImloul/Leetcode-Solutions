15d05ac1a11c9a7ccb44718019ff4005b47d24c2
AnasImloul <pydev27@gmail.com>
2024-01-09 04:28:38 +0100

    Runtime: 14 ms (Top 88.14%) | Memory: 13.50 MB (Top 98.31%)

--- scripts/algorithms/M/Moving Stones Until Consecutive II/Moving Stones Until Consecutive II.cpp
+++ scripts/algorithms/M/Moving Stones Until Consecutive II/Moving Stones Until Consecutive II.cpp
@@ -1,14 +1,35 @@
+// Runtime: 14 ms (Top 88.14%) | Memory: 13.50 MB (Top 98.31%)
+
 class Solution {
 public:
-    vector<int> numMovesStonesII(vector<int>& stones) {
-        sort(begin(stones), end(stones)); 
-        int n = size(stones), ii = 0, low = INT_MAX, high = max(stones[n-2] - stones[0], stones[n-1] - stones[1]) - (n - 2); 
-        
-        for (int i = 0; i < n; ++i) {
-            while (stones[i] - stones[ii] >= n) ++ii; 
-            if (i - ii + 1 == n - 1 && stones[i] - stones[ii] == n - 2) low = min(low, 2); 
-            else low = min(low, n - (i - ii + 1)); 
-        }
-        return {low, high}; 
-    }
+	vector<int> numMovesStonesII(vector<int>& stones) {
+		sort(stones.begin(),stones.end());
+		int n=stones.size();
+		int MAX=0;
+		int MIN=INT_MAX;
+		int a=stones.back()-stones[1]+1-(n-1);
+		int b=stones[n-2]-stones[0]+1-(n-1);
+		int j=0;
+		MAX=max(a,b);
+		for(int i=0;i<n;i++){
+			while(j<n && stones[j]-stones[i]+1<n){
+				j++;
+			}
+			if(j>=n){
+				break;
+			}
+			int gap;
+			if(stones[j]-stones[i]+1==n){
+				gap=n-(j-i+1);
+			}
+			else if(j-1-i+1==n-1){
+				gap=2;
+			}
+			else{
+				gap=n-(j-1-i+1);
+			}
+			MIN=min(MIN,gap);
+		}
+		return {MIN,MAX};
+	}
 };
