b487eb77701ace1df5a89ba21208e63d72b98451
AnasImloul <pydev27@gmail.com>
2024-01-09 04:13:59 +0100

    Runtime: 7 ms (Top 85.26%) | Memory: 10.50 MB (Top 92.63%)

--- scripts/algorithms/B/Brace Expansion II/Brace Expansion II.cpp
+++ scripts/algorithms/B/Brace Expansion II/Brace Expansion II.cpp
@@ -1,87 +1,72 @@
+// Runtime: 7 ms (Top 85.26%) | Memory: 10.50 MB (Top 92.63%)
+
 class Solution {
 public:
-    vector<string> braceExpansionII(string expression) {
-        string ss;
-        int n = expression.size();
-        for(int i = 0; i < n; i++){
-            if(expression[i] == ','){
-                ss += '+';
-            }
-            else{
-                ss += expression[i];
-                if((isalpha(expression[i]) || expression[i] == '}') && i+1 < n && (isalpha(expression[i+1]) || expression[i+1] == '{')){
-                    ss += '*';
+    void getUnion(vector<string>& a, const vector<string>& b) {
+        if (!a.size()) {
+            a = b;
+            return;
+        }
+        for (const auto& str_b: b) {
+            bool found = false;
+            for (const auto& str_a: a) {
+                if (str_a == str_b) {
+                    found = true;
+                    break;
                 }
             }
+            if (!found) a.push_back(str_b);
         }
-        
-        stack<char>stk1;
-        vector<string>postfix;
-        for(char c:ss){
-            if(c == '{'){
-                stk1.push(c);
-            } else if(c == '}') {
-                while(stk1.top() != '{'){
-                    postfix.push_back(string(1, stk1.top()));
-                    stk1.pop();
-                }
-                stk1.pop();
-            } else if(c == '+'){
-                while(!stk1.empty() && (stk1.top() == '+' || stk1.top() == '*')){
-                    postfix.push_back(string(1, stk1.top()));
-                    stk1.pop();
-                }
-                stk1.push(c);
-            } else if(c == '*'){
-                while(!stk1.empty() && stk1.top() == '*'){
-                    postfix.push_back(string(1, stk1.top()));
-                    stk1.pop();
-                }
-                stk1.push(c);
-            } else {
-                postfix.push_back(string(1, c));
-            }
-        }
-        while(!stk1.empty()){
-            postfix.push_back(string(1, stk1.top()));
-            stk1.pop();
-        }
-        /*for(string sp:postfix){
-            cout << sp << " ";
-        }
-        cout << endl;*/
-        stack<vector<string>>cont;
-        for(string s:postfix){
-            if(isalpha(s[0])){
-                cont.push({s});
-            } else {
-                vector<string>second = cont.top();
-                cont.pop();
-                vector<string>first = cont.top();
-                cont.pop();
-                
-                if(s[0] == '+'){
-                    for(string sec:second){
-                        first.push_back(sec);
-                    }
-                    cont.push(first);
-                } else {
-                    vector<string>cartesian;
-                    for(string fst:first){
-                        for(string sec:second){
-                            cartesian.push_back(fst + sec);
-                        }
-                    }
-                    cont.push(cartesian);
-                }
-            }
-        }
-        set<string>sstr;
-        for(string sc:cont.top()){
-            sstr.insert(sc);
-        }
-        
-        vector<string>ret(sstr.begin(), sstr.end());
-        return ret;
     }
-};```
+    void getComb(vector<string>& a, const vector<string>& b) {
+        if (!a.size()) {
+            a = b;
+            return;
+        }
+        vector<string> tmp;
+        for (const auto& str_a: a) {
+            for (const auto& str_b: b) {
+                tmp.push_back(str_a + str_b);
+            }
+        }
+        a = tmp;
+    }
+    vector<string> braceExpansionII(string expression) {
+        if (expression.size() == 0) return {};
+        if (expression.size() == 1) return {expression};
+        int n = expression.size();
+        vector<string> ans;
+        vector<string> processing_str;
+        int in_brace = 0;
+        string partial_str{};
+
+        for (int i = 0; i < n; ++ i) {
+            if (expression[i] == '{') {
+                if (in_brace) partial_str += expression[i];
+                in_brace ++;
+            } else if (expression[i] == '}') {
+                in_brace --;
+                if (in_brace) partial_str += expression[i];
+                if (!in_brace) {
+                    getComb(processing_str, braceExpansionII(partial_str));
+                    partial_str.clear();
+                }
+            } else if (in_brace == 0 && expression[i] == ',') {
+                getUnion(ans, processing_str);
+                processing_str.clear();
+                partial_str.clear();
+            } else if (in_brace == 0) { 
+                std::string tmp = "";
+                while (i < n && expression[i] >= 'a' && expression[i] <= 'z') 
+                    tmp += expression[i++];
+                -- i;
+                getComb(processing_str, {tmp});
+            } else {
+                partial_str += expression[i];
+            }
+        }
+        getUnion(ans, processing_str);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
