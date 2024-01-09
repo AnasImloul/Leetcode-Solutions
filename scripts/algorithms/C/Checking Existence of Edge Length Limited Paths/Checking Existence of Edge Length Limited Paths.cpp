95f0c3d7243a42265a636c98a48d404567e759d6
AnasImloul <pydev27@gmail.com>
2024-01-09 12:43:25 +0100

    Runtime: 390 ms (Top 98.27%) | Memory: 111.20 MB (Top 59.25%)

--- scripts/algorithms/C/Checking Existence of Edge Length Limited Paths/Checking Existence of Edge Length Limited Paths.cpp
+++ scripts/algorithms/C/Checking Existence of Edge Length Limited Paths/Checking Existence of Edge Length Limited Paths.cpp
@@ -1,53 +1,53 @@
-class DSU {
-public:
-    vector<int>Parent;
-    vector<int>Rank;
-    DSU(int n) {
-        Parent.resize(n);
-        Rank.resize(n, 0);
-        for(int i = 0; i < n; i++) Parent[i] = i;
-    }
-
-    int Find(int x) {
-        return Parent[x] = Parent[x] == x? x : Find(Parent[x]);
-    }
-
-    void Union(int a, int b) {
-        a = Find(a);
-        b = Find(b);
-        if(Rank[a] == Rank[b]) Rank[a]++;
-        if(Rank[a] > Rank[b]){
-            Parent[b] = a;
-        }
-        else {
-            Parent[a] = b;
-        }
-    }
-};
+// Runtime: 390 ms (Top 98.27%) | Memory: 111.20 MB (Top 59.25%)
 
 class Solution {
 public:
-    vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& q) {
-        int nq = q.size();
-        for(int i = 0; i < nq; i++) {
-            q[i].push_back(i);
-        }
-        sort(begin(q), end(q), [&](auto const& a, auto const& b) {
-          return a[2] < b[2];
+    vector<bool> distanceLimitedPathsExist(int length, vector<vector<int>>& adjList, vector<vector<int>>& queries) {
+        vector<int> parent(length);
+        vector<int> rank(length);
+        vector<int> weight(length);
+        for (int i = 0; i < length ; i++) parent[i] = i;
+
+        sort(adjList.begin(), adjList.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[2] < b[2];
         });
-        sort(begin(edgeList), end(edgeList), [&](auto const& a, auto const& b) {
-          return a[2] < b[2];
-        });
-        vector<bool>ans(nq, 0);
-        int i = 0;
-        DSU dsu(n);
-        for(int j = 0; j < nq; j++) {
-          while(i < edgeList.size() && edgeList[i][2] < q[j][2]) {
-            dsu.Union(edgeList[i][0], edgeList[i][1]);
-            i++;
-          }
-          if(dsu.Find(q[j][0]) == dsu.Find(q[j][1])) ans[q[j][3]] = 1;
+        for (vector<int>& edge : adjList) unionByRank(edge[0], edge[1], edge[2], parent, rank, weight);
+
+        vector<bool> answer;
+        for (vector<int>& query : queries)
+            answer.push_back(isConnectedAndWithinLimit(query[0], query[1], query[2], parent, weight));
+
+        return answer;
+    }
+
+    bool isConnectedAndWithinLimit(int p, int q, int limit, vector<int>& parent, vector<int>& weight) {
+        return find(p, limit, parent, weight) == find(q, limit, parent, weight);
+    }
+
+    int find(int x, int limit, vector<int>& parent, vector<int>& weight) {
+        while (x != parent[x]) {
+            if (weight[x] >= limit) {
+                break;
+            }
+            x = parent[x];
         }
-        return ans;
+        return x;
+    }
+
+    void unionByRank(int x, int y, int limit, vector<int>& parent, vector<int>& rank, vector<int>& weight) {
+        int x_ref = find(x, INT_MAX, parent, weight);
+        int y_ref = find(y, INT_MAX, parent, weight);
+        if (x_ref != y_ref) {
+            if (rank[x_ref] < rank[y_ref]) {
+                parent[x_ref] = y_ref;
+                weight[x_ref] = limit;
+            } else {
+                parent[y_ref] = x_ref;
+                weight[y_ref] = limit;
+                if (rank[x_ref] == rank[y_ref]) {
+                    rank[x_ref]++;
+                }
+            }
+        }
     }
 };
