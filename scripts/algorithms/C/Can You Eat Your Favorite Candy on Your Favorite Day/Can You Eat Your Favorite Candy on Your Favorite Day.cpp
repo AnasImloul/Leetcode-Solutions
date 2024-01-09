492650ced5df27721b371ab8751acee250a1b7c1
AnasImloul <pydev27@gmail.com>
2024-01-09 06:55:55 +0100

    Runtime: 269 ms (Top 81.94%) | Memory: 121.60 MB (Top 45.83%)

--- scripts/algorithms/C/Can You Eat Your Favorite Candy on Your Favorite Day/Can You Eat Your Favorite Candy on Your Favorite Day.cpp
+++ scripts/algorithms/C/Can You Eat Your Favorite Candy on Your Favorite Day/Can You Eat Your Favorite Candy on Your Favorite Day.cpp
@@ -1,22 +1,32 @@
+// Runtime: 269 ms (Top 81.94%) | Memory: 121.60 MB (Top 45.83%)
+
 class Solution {
 public:
-    typedef long long ll;
     vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries) {
-        int n = candiesCount.size();
-        vector<ll> pre_sum(n+1, 0);
-        for (int i = 1; i <= n; ++i) {
-            pre_sum[i] = pre_sum[i-1]+candiesCount[i-1];
+
+        ///lower bound and higher bound 
+        // lower bound: eat one per day, on favourite day, the candy type i does not run out
+        // higher bound: eat dailycap, on favourite day, the cap reaches type i
+
+        // type + day + cap
+
+        vector <bool> outcome;
+
+        vector <long int> candyIndex(candiesCount.size());
+        long int x = 0;
+        for (int i =0; i< candiesCount.size();i++){
+            candyIndex[i] = x;
+            x += candiesCount[i];
         }
-        vector<bool> res(queries.size(), 0);
-        
-        for (int i = 0; i < queries.size(); ++i) {
-            auto &p = queries[i];
-            ll max = pre_sum[p[0]+1]-1;
-            ll min = pre_sum[p[0]] / p[2];
-            if (p[1] >= min && p[1] <= max)
-                res[i] = true;
+
+
+        for (int i =0;i< queries.size();i++){
+
+            long int highBound = (long int) queries[i][2]*(queries[i][1]+1), lowBound = queries[i][1]+1;
+
+            bool answer = candyIndex[queries[i][0]] < highBound && (candyIndex[queries[i][0]] + candiesCount[queries[i][0]]-1) >= (lowBound -1);
+            outcome.push_back(answer);
         }
-        
-        return res;
+        return outcome;
     }
 };
