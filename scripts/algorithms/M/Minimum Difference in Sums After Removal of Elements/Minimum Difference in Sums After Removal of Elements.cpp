1529e904b4759e2a7616e234911e07186d4927f2
AnasImloul <pydev27@gmail.com>
2024-01-09 13:00:37 +0100

    Runtime: 248 ms (Top 89.8%) | Memory: 136.40 MB (Top 85.71%)

--- scripts/algorithms/M/Minimum Difference in Sums After Removal of Elements/Minimum Difference in Sums After Removal of Elements.cpp
+++ scripts/algorithms/M/Minimum Difference in Sums After Removal of Elements/Minimum Difference in Sums After Removal of Elements.cpp
@@ -1,50 +1,41 @@
-// Runtime: 1038 ms (Top 20.34%) | Memory: 161 MB (Top 15.01%)
+// Runtime: 248 ms (Top 89.8%) | Memory: 136.40 MB (Top 85.71%)
+
 class Solution {
 public:
     long long minimumDifference(vector<int>& nums) {
-        int N = nums.size();
-        int n = N/3;
-        priority_queue<int> pq;
-        map<int, long long> m;
-        long long sum = 0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            pq.push(nums[i]);
+        long long ans = LLONG_MAX, suml = 0, sumr = 0, sumll, sumrr;
+        int n = nums.size()/3;
+        priority_queue<int> lpq;
+        priority_queue<int,vector<int>,greater<int>> rpq;
+        for(int i=0;i<n;i++) {
+            lpq.push(nums[i]), suml += nums[i];
+            rpq.push(nums[i+2*n]), sumr += nums[i+2*n];
         }
-        for(int i=0;i<=n;i++){
-            if(i == 0){
-                m[i] = sum;
+        sumll = suml;
+        sumrr = sumr;
+        vector<long long> leftdp(n), rightdp(n);
+        ans = min(ans,suml-sumr);
+        for(int i=n;i<2*n;i++){
+            if(lpq.top()>nums[i]){
+                suml -= lpq.top();
+                suml += nums[i];
+                lpq.pop();
+                lpq.push(nums[i]);
             }
-            else{
-                pq.push(nums[n+i-1]);
-                sum += nums[i+n-1];
-                sum -= pq.top();
-                pq.pop();
-                m[i] = sum;
+            leftdp[i-n] = suml;
+        }
+        for(int i=2*n-1;i>=n;i--){
+            if(rpq.top()<nums[i]){
+                sumr -= rpq.top();
+                sumr += nums[i];
+                rpq.pop();
+                rpq.push(nums[i]);
             }
+            rightdp[i-n] = sumr;
         }
-        sum = 0;
-        priority_queue<int, vector<int>, greater<int>> p;
-        for(int i=N-1;i>N-1-n;i--){
-            sum += nums[i];
-            p.push(nums[i]);
-        }
-        for(int i=0;i<=n;i++){
-            if(i == 0){
-                m[n-i] -= sum;
-            }
-            else{
-                p.push(nums[N-n-i]);
-                sum += nums[N-n-i];
-                sum -= p.top();
-                p.pop();
-                m[n-i] -= sum;
-            }
-        }
-        long long ans = 9223372036854775807;
-        for(auto it : m){
-            ans = min(ans, it.second);
-        }
+        for(int i=0;i+1<n;i++) ans = min(ans,leftdp[i]-rightdp[i+1]);
+        ans = min(ans,leftdp[n-1]-sumrr);
+        ans = min(ans,sumll-rightdp[0]);
         return ans;
     }
 };
