3e4cab1e5da77d17677ac73cee48c02e56c164a9
AnasImloul <pydev27@gmail.com>
2024-01-09 13:29:08 +0100

    Runtime: 4 ms (Top 94.57%) | Memory: 6.30 MB (Top 51.12%)

--- scripts/algorithms/P/Power of Three/Power of Three.cpp
+++ scripts/algorithms/P/Power of Three/Power of Three.cpp
@@ -1,8 +1,21 @@
+// Runtime: 4 ms (Top 94.57%) | Memory: 6.30 MB (Top 51.12%)
+
 class Solution {
 public:
-	bool isPowerOfThree(int n) {
-		if(n<=0){return false;}
-		if(n>pow(2, 31)-1 || n<pow(2, 31)*(-1)){return false;}
-		return 1162261467%n==0;
-	}
+    bool isPowerOfThree(int n)
+    {
+        if(n < 1)
+        {
+            return false;
+        }
+        if(n == 1)
+        {
+            return true;
+        }
+        if(n%3 != 0)
+        {
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
 };
