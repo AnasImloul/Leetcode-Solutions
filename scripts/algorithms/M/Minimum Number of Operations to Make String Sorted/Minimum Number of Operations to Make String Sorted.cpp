63403764916cac3c504cd9e8a0746bb23aecfd9b
AnasImloul <pydev27@gmail.com>
2024-01-09 12:57:02 +0100

    Runtime: 13 ms (Top 88.46%) | Memory: 7.40 MB (Top 76.92%)

--- scripts/algorithms/M/Minimum Number of Operations to Make String Sorted/Minimum Number of Operations to Make String Sorted.cpp
+++ scripts/algorithms/M/Minimum Number of Operations to Make String Sorted/Minimum Number of Operations to Make String Sorted.cpp
@@ -1,40 +1,55 @@
+// Runtime: 13 ms (Top 88.46%) | Memory: 7.40 MB (Top 76.92%)
+
+const int MOD = 1e9 + 7;
+const int N = 3010;
+
+long f[N];
+long g[N];
+
+long qmi(long a, int k) {
+    long res = 1;
+    while (k != 0) {
+        if ((k & 1) == 1) {
+            res = res * a % MOD;
+        }
+        k >>= 1;
+        a = a * a % MOD;
+    }
+    return res;
+}
+
+int init = []() {
+    f[0] = g[0] = 1;
+    for (int i = 1; i < N; ++i) {
+        f[i] = f[i - 1] * i % MOD;
+        g[i] = qmi(f[i], MOD - 2);
+    }
+    return 0;
+}();
+
 class Solution {
 public:
     int makeStringSorted(string s) {
-        int freq[26] = {}, n = size(s), MOD = 1'000'000'007; 
-        for (auto& c : s) ++freq[c - 'a']; 
-        
-        auto power = [](long x, int p, int m) {
-            long ans = 1; 
-            while (p) {
-                if (p&1) {
-                    ans = (ans * x) % m; 
-                    p -= 1; 
-                } else {
-                    x = (x * x) % m; 
-                    p /= 2; 
-                } 
-            }
-            return ans; 
-        };
-        
-        long ans = 0, fac = 1; 
-        vector<long> ifac(n+1, 1); // +1 buffer 
-        
-        for (int i = 1; i < n; ++i) {
-            fac = fac * i % MOD; 
-            ifac[i] = power(fac, MOD-2, MOD); // Fermat's little theorem
+        int cnt[26]{};
+        for (char& c : s) {
+            ++cnt[c - 'a'];
         }
-        
-        for (auto& x : freq) fac = fac * ifac[x] % MOD; 
-        
+        int n = s.size();
+        long ans = 0;
         for (int i = 0; i < n; ++i) {
-            for (int k = 0; k < s[i] - 'a'; ++k) 
-                ans = (ans + fac * freq[k]) % MOD; 
-            fac = fac * power(n-i-1, MOD-2, MOD) % MOD; 
-            fac = fac * freq[s[i] - 'a'] % MOD; 
-            --freq[s[i] - 'a']; 
+            int m = 0;
+            for (int j = 0; j < 26; ++j) {
+                if (j < s[i] - 'a') {
+                    m += cnt[j];
+                }
+            }
+            long t = m * f[n - i - 1] % MOD;
+            for (int j = 0; j < 26; ++j) {
+                t = t * g[cnt[j]] % MOD;
+            }
+            ans = (ans + t) % MOD;
+            --cnt[s[i] - 'a'];
         }
-        return ans; 
+        return ans;
     }
 };
