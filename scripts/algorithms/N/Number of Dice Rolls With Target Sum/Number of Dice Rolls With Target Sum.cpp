272cad382711cd77a0fd52e1a428d9ea62e4e06a
AnasImloul <pydev27@gmail.com>
2024-01-09 13:12:08 +0100

    Runtime: 11 ms (Top 87.19%) | Memory: 6.50 MB (Top 92.03%)

--- scripts/algorithms/N/Number of Dice Rolls With Target Sum/Number of Dice Rolls With Target Sum.cpp
+++ scripts/algorithms/N/Number of Dice Rolls With Target Sum/Number of Dice Rolls With Target Sum.cpp
@@ -1,24 +1,25 @@
+// Runtime: 11 ms (Top 87.19%) | Memory: 6.50 MB (Top 92.03%)
+
 class Solution {
 public:
-	int dp[31][1001];
-	Solution(){
-		memset(dp,-1,sizeof(dp));
-	}
-	int help(int dno,int &tdice,int &tf,int target){
-		if(target<0 or dno>tdice) return 0;
-		if(target==0 and dno==tdice) {
-			return 1;
-		}
-		if(dp[dno][target]!=-1) return dp[dno][target];
-		int ans=0;
-		for(int i=1;i<=tf;i++){
-			ans=(ans%1000000007+help(dno+1,tdice,tf,target-i)%1000000007)%1000000007;
-
-		}
-		return dp[dno][target]=ans;
-	}
-	int numRollsToTarget(int n, int k, int target) {
-	   return help(0,n,k,target);
-
-	}
+    #define MOD 1000000007
+    int dp[32][1002];
+    
+    int solve(int d, int f, int target){
+        if(d==0 && target==0) return 1;
+        if(d<=0 || target<=0) return 0;
+        if(dp[d][target] != -1) return dp[d][target];
+     
+        int sum=0;
+        for(int i=1;i<=f;i++){
+            sum = (sum % MOD + solve(d-1,f,target-i)%MOD)%MOD;
+        }
+        dp[d][target] = sum;
+        return dp[d][target];
+    }
+    
+    int numRollsToTarget(int d, int f, int target) {
+        memset(dp,-1,sizeof(dp));
+        return solve(d,f,target);
+    }
 };
