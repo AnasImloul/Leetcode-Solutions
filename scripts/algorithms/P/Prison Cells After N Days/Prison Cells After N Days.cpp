fcc15428bba2a0faf8312c85f983471aec473cf8
AnasImloul <pydev27@gmail.com>
2024-01-09 13:04:29 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 12.40 MB (Top 75.91%)

--- scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.cpp
+++ scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.cpp
@@ -1,47 +1,22 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 12.40 MB (Top 75.91%)
+
 class Solution {
+
 public:
-    vector<int> prisonAfterNDays(vector<int>& cells, int n) {
-        //since there are only a 2**6 number of states and n can go to 10**9
-        //this means that there is bound to be repetition of states
-        int state=0;
-        //making the initial state bitmask
-        for(int i=0;i<cells.size();i++){
-            if(cells[i]){
-                state^=(1<<(7-i));
-            }
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        if(N == 0) return cells;
+        prison(cells, 1);              //Prison cells for one day, The pattern repeats from day 1 - 14
+        prison(cells, (N - 1) % 14);         //Simply day 14 = day 1, Day 0 never repeats because of corners  
+        return cells;                               
+    }
+    void prison(vector<int>& cells, const int& N){
+        for(int i = 0; i < N; i++){
+            vector<int> v(8,0);
+        for(int i = 1; i < 7; i++)
+            if(cells[i - 1] == cells[i + 1])
+                v[i] = 1;
+            else v[i] = 0;
+        cells = v;
         }
-        //this array stores the various states encountered
-        vector<int>seen;
-        while(n--){
-            int next=0;
-            //transitioning to the next state
-            for(int pos=6;pos>0;pos--){
-                int right=pos+1,left=pos-1;
-                if(((state>>right)&1)==((state>>left)&1)){
-                    next|=(1<<pos);
-                }
-            }
-            //if the next state is equal to the initial state, this means that we have
-            //found the cycle. Let the length of the cycle be l=seen.size(). Therefore 
-            //in the remaining n days, n/l of those will have no effect on the 
-            //prison configuration. This means we can return the configuration of the 
-            //n%l day.
-            if(seen.size() and seen[0]==next){
-                int cnt=seen.size();
-                state=seen[n%cnt];
-                break;
-            } else {
-                seen.push_back(next);
-                state=next;
-            }
-        }
-        //translating the prison state from the bitmask to an array.
-        vector<int>ans(cells.size(),0);
-        for(int i=0;i<8;i++){
-            if((state>>i)&1){
-                ans[7-i]=1;
-            }
-        }
-        return ans;
     }
 };
