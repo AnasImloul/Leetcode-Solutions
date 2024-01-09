471b127d4d5982139743039f39ad07398a82d6b6
AnasImloul <pydev27@gmail.com>
2024-01-09 07:19:31 +0100

    Runtime: 20 ms (Top 71.39%) | Memory: 71.90 MB (Top 9.3%)

--- scripts/algorithms/A/Add Two Numbers/Add Two Numbers.cpp
+++ scripts/algorithms/A/Add Two Numbers/Add Two Numbers.cpp
@@ -1,50 +1,28 @@
-// Runtime: 55 ms (Top 55.51%) | Memory: 71 MB (Top 93.72%)
+// Runtime: 20 ms (Top 71.39%) | Memory: 71.90 MB (Top 9.3%)
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *head1, *head2, *head3, *temp;
-        head1=l1;
-        head2 = l2;
-        head3=NULL;
-        int carry = 0, res = 0;
-        while(head1 && head2){
-            res = head1->val +head2->val + carry;
-            carry = res/10;
-            head1->val = res%10;
-            if(!head3){
-                head3 = head1;
-                temp = head3;
+        ListNode *dummy = new ListNode(0);
+        ListNode *curr = dummy;
+        int carry = 0;
+        
+        while(l1 != NULL || l2 != NULL || carry == 1){
+            int sum = 0;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            else{
-                temp->next = head1;
-                temp = temp->next;
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
             }
-            head1 = head1->next;
-            head2 = head2->next;
+            sum += carry;
+            carry = sum/10;
+            ListNode *node = new ListNode(sum % 10);
+            curr->next = node;
+            curr = curr->next;
         }
-        while(head1){
-            res = head1->val + carry;
-            carry = res/10;
-            head1->val = res%10;
-            temp->next = head1;
-            temp = temp->next;
-            head1 = head1->next;
-        }
-        while(head2){
-            res = head2->val + carry;
-            carry = res/10;
-            head2->val = res%10;
-            temp->next = head2;
-            temp = temp->next;
-            head2 = head2->next;
-        }
-        if(carry){
-            ListNode* result = new ListNode();
-            result->val = carry;
-            result->next = NULL;
-            temp->next = result;
-            temp = temp->next;
-        }
-        return head3;
+        return dummy->next;
     }
 };
