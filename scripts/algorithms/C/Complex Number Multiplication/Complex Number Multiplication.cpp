642a64e830dea20722376a468e9d91a8585c439a
AnasImloul <pydev27@gmail.com>
2024-01-09 13:16:10 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 6.40 MB (Top 16.79%)

--- scripts/algorithms/C/Complex Number Multiplication/Complex Number Multiplication.cpp
+++ scripts/algorithms/C/Complex Number Multiplication/Complex Number Multiplication.cpp
@@ -1,23 +1,20 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 6.40 MB (Top 16.79%)
+
 class Solution {
-public: 
-    pair<int,int> seprateRealAndImg(string &s){
-        int i = 0;
-        string real,img;
-        while(s[i] != '+') real += s[i++];
-        while(s[++i] != 'i') img += s[i];
-        
-        return {stoi(real),stoi(img)};
+public:
+    pair<int, int> parse(string num) {
+        int i = num.find('+');
+        double real = stoi(num.substr(0, i));
+        double imaginary = stoi(num.substr(i+1, num.size()-i-2));
+        pair<int, int> res(real, imaginary);
+        return res;
     }
+    
     string complexNumberMultiply(string num1, string num2) {
-        pair<int,int> x = seprateRealAndImg(num1);
-        pair<int,int> y = seprateRealAndImg(num2);
+        pair<int, int> a = parse(num1), b = parse(num2);
+        int real_a = a.first, imag_a = a.second;
+        int real_b = b.first, imag_b = b.second;
         
-        int a1 = x.first,b1 = x.second;
-        int a2 = y.first,b2 = y.second;
-        
-        int real = (a1 * a2) - (b1*b2);
-        int img = (b1*a2) + (a1 * b2);
-        
-        return to_string(real) + "+" + to_string(img) + "i";
+        return to_string(real_a * real_b - imag_a * imag_b) + '+' + to_string(real_a * imag_b  + real_b * imag_a)+'i' ;
     }
 };
