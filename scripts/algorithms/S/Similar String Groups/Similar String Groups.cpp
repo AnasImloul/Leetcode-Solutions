8a521272582cc250329dd0f1e7f0377e9cf36d2a
AnasImloul <pydev27@gmail.com>
2024-01-09 04:21:48 +0100

    Runtime: 97 ms (Top 54.91%) | Memory: 10.70 MB (Top 62.14%)

--- scripts/algorithms/S/Similar String Groups/Similar String Groups.cpp
+++ scripts/algorithms/S/Similar String Groups/Similar String Groups.cpp
@@ -1,63 +1,43 @@
-// Runtime: 634 ms (Top 19.62%) | Memory: 166 MB (Top 12.21%)
+// Runtime: 97 ms (Top 54.91%) | Memory: 10.70 MB (Top 62.14%)
+
 class Solution {
 public:
-
-    bool similar(string s1,string s2)
-    {
-        int n=s1.size();
-        int i=0,j=n-1;
-       int missMatch=0;
-        for(i=0;i<n;i++)
-        {
-            if(s1[i]!=s2[i] and ++missMatch>2)return 0;
+    bool similar(string &s,string &t){
+        int dif=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]){
+                dif++;
+            }
         }
-        return 1;
+        return dif<=2;
     }
-
-    void dfs(string x,unordered_map<string,vector<string>>&adj,unordered_set<string>&visited)
-    {
-        visited.insert(x);
-        for(auto nbr:adj[x])
-        {
-            if(visited.find(nbr)==visited.end())
-            {
-
-                dfs(nbr,adj,visited);
+    void dfs(int node,vector<int>&vis,vector<vector<int>>&graph){
+        vis[node]=1;
+        for(auto&child:graph[node]){
+            if(!vis[child]){
+                dfs(child,vis,graph);
             }
         }
     }
-
     int numSimilarGroups(vector<string>& strs) {
-        int n = strs.size();
-        unordered_map<string,vector<string>>adj;
-        for(int i=0;i<n;i++)
-        {
-            string s1 = strs[i];
-            for(int j=i+1;j<n;j++)
-            {
-                string s2 = strs[j];
-                if(similar(s1,s2))
-                {
-                    adj[s1].push_back(s2);
-                    adj[s2].push_back(s1);
-
+        int n=strs.size();
+        vector<vector<int>>graph(strs.size());
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(similar(strs[i],strs[j])){
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
                 }
             }
-
         }
-
-        int count=0;
-        unordered_set<string>visited;
-        for(auto x:strs)
-        {
-            if(visited.find(x)==visited.end())
-            {
-                //cout<<x<<endl;
-                dfs(x,adj,visited);
-                count++;
+        int ans=0;
+        vector<int>vis(n);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,graph);
+                ans++;
             }
         }
-        return count;//(strs[0],strs[2]);
-
+        return ans;
     }
 };
