4ed3f167882539a1a69ec83b0f9e64d76a4019e0
AnasImloul <pydev27@gmail.com>
2024-01-09 06:54:41 +0100

    Runtime: 267 ms (Top 65.96%) | Memory: 13.40 MB (Top 32.62%)

--- scripts/algorithms/T/Triples with Bitwise AND Equal To Zero/Triples with Bitwise AND Equal To Zero.cpp
+++ scripts/algorithms/T/Triples with Bitwise AND Equal To Zero/Triples with Bitwise AND Equal To Zero.cpp
@@ -1,24 +1,26 @@
+// Runtime: 267 ms (Top 65.96%) | Memory: 13.40 MB (Top 32.62%)
+
 class Solution {
 public:
-    //we can make an O(n**3) algorithm by simply iterating 3 times across the array and 
-    //taking the bitwise AND. That would involve a lot of duplicate AND calculation.
-    //Instead we can iterate using two counters i and j, and store the count of every 
-    //bitwise AND we encounter in a hashmap. Then we will iterate the array and for every
-    //value in the hashmap, if it's bitwise AND with the current element is zero, then we
-    //add the number of times that AND appears in our array to our answer.
-    int countTriplets(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                mp[nums[i]&nums[j]]++;
+    int countTriplets(vector<int>& a) {
+        int  n = a.size();
+        unordered_map<int,int> mp;
+        for(int  i= 0;i<n;i++)
+        {
+            for(int  j= 0;j<n;j++)
+            {
+               mp[(a[i] & a[j])]++;
             }
         }
-        int ans=0;
-        for(int k=0;k<nums.size();k++){
-            for(unordered_map<int,int>::iterator it=mp.begin();it!=mp.end();it++){
-                if(!(nums[k]&it->first)){
-                    ans+=it->second;
-                }
+        int ans  = 0;
+        for(auto &op : mp)
+        {
+            for(int  i = 0;i<n;i++)
+            {
+                if(op.first & a[i])
+                {}
+                else
+                {ans+=op.second;}
             }
         }
         return ans;
