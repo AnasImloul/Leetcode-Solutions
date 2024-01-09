f23084b8ab185d058a1598abff01ad273922b7ba
AnasImloul <pydev27@gmail.com>
2024-01-09 13:37:55 +0100

    Runtime: 217 ms (Top 82.22%) | Memory: 125.90 MB (Top 26.67%)

--- scripts/algorithms/M/Maximum Number of Events That Can Be Attended II/Maximum Number of Events That Can Be Attended II.js
+++ scripts/algorithms/M/Maximum Number of Events That Can Be Attended II/Maximum Number of Events That Can Be Attended II.js
@@ -1,39 +1,30 @@
+// Runtime: 217 ms (Top 82.22%) | Memory: 125.90 MB (Top 26.67%)
+
 /**
  * @param {number[][]} events
  * @param {number} k
  * @return {number}
  */
-var maxValue = function(events, k) {
-    events = events.sort((a, b) => a[1] - b[1]);
-    const n = events.length;
-    var ans = 0;
-    var dp = Array.from(Array(k+1), () => new Array(n).fill(0));
+
+var f = function(ind,k,events,dp){
+    if(ind == events.length || k == 0) return 0;
     
-    for(let i = 0; i < n; i++) {
-        dp[1][i] = Math.max((i ? dp[1][i-1] :0), events[i][2]);
-        ans = Math.max(dp[1][i], ans);
-    }
-    
-    function binarySearch(l, r, target) {
-        var pos = -1;
-        while(l <= r) {
-            const mid = Math.floor((l+r)/2);
-            if (events[mid][1] < target) {
-                pos = mid;
-                l = mid+1;
-            }
-            else r = mid-1;
+    if(dp[ind][k] != -1) return dp[ind][k];
+    let take = events[ind][2];
+    for(let i = ind+1; i < events.length; ++i){
+        if(events[ind][1] < events[i][0]){
+            take += f(i,k-1,events,dp);
+            break;
         }
-        return pos;
     }
-    
-    for(let i = 0; i < n; i++) {
-        const j = binarySearch(0, i-1, events[i][0]);
-            for(let l = 2; l <= k; l++) {
-                dp[l][i] = Math.max(( j >= 0 ? dp[l-1][j] + events[i][2] : 0), (i ? dp[l][i-1] : 0));
-                ans = Math.max(dp[l][i], ans);
-            }
+    let notTake = f(ind+1,k,events,dp);
+    return dp[ind][k] = Math.max(take,notTake);
+}
+var maxValue = function(events, k) {
+    const dp = [];
+    events.sort((a,b)=>a[0]===b[0]?a[1]-b[1]:a[0]-b[0])
+    for(let i = 0; i < events.length; ++i){
+        dp[i] = new Array(k+1).fill(-1);
     }
-    
-    return ans;
+    return f(0,k,events,dp);
 };
