460c5b09cc34bb4f60756d32d5210fda48dbb490
AnasImloul <pydev27@gmail.com>
2024-01-09 04:43:39 +0100

    Runtime: 69 ms (Top 97.04%) | Memory: 32.80 MB (Top 50.74%)

--- scripts/algorithms/P/Perfect Rectangle/Perfect Rectangle.cpp
+++ scripts/algorithms/P/Perfect Rectangle/Perfect Rectangle.cpp
@@ -1,27 +1,26 @@
-class Solution {
- public:
-  bool isRectangleCover(vector<vector<int>>& rectangles) {
-    SegmentTree<STMax> st(2e5 + 1);
-    sort(rectangles.begin(), rectangles.end(), [](auto& lhs, auto& rhs) -> bool {
-      if (lhs[1] != rhs[1]) return lhs[1] < rhs[1];
-      return lhs[0] < rhs[0];
-    });
+// Runtime: 69 ms (Top 97.04%) | Memory: 32.80 MB (Top 50.74%)
 
-    int min_x = rectangles[0][0];
-    int min_y = rectangles[0][1];
-    int max_a = rectangles[0][2];
-    int max_b = rectangles[0][3];
-    long long sum = 1ll * (max_a - min_x) * (max_b - min_y);
-    st.update(min_x + 1e5 + 1, max_a + 1e5, max_b);
-    for (int i = 1; i < rectangles.size(); ++i) {
-      if (rectangles[i][0] < min_x) return false;
-      int max_height = st.query(rectangles[i][0] + 1e5 + 1, rectangles[i][2] + 1e5);
-      if (max_height > rectangles[i][1]) return false;
-      max_a = max(max_a, rectangles[i][2]);
-      max_b = max(max_b, rectangles[i][3]);
-      sum += 1ll * (rectangles[i][2] - rectangles[i][0]) * (rectangles[i][3] - rectangles[i][1]);
-      st.update(rectangles[i][0] + 1e5 + 1, rectangles[i][2] + 1e5, rectangles[i][3]);
+class Solution {
+public:
+    bool isRectangleCover(vector<vector<int>>& rectangles) {
+        unordered_map<long long, int> mark;
+        const long long N=1000000;
+        for(vector<int>& x:rectangles){
+            int x1=x[0];
+            int y1=x[1];
+            int x2=x[2];
+            int y2=x[3];
+            mark[x1*N+y1]++;
+            mark[x1*N+y2]--;
+            mark[x2*N+y1]--;
+            mark[x2*N+y2]++;
+        }
+        int n_mark=0;
+        for(auto ptr=mark.begin();ptr!=mark.end();ptr++)
+            if(ptr->second!=0){
+                if(abs(ptr->second)!=1) return false;
+                n_mark++;
+            }
+        return n_mark==4;
     }
-    return sum == 1ll * (max_a - min_x) * (max_b - min_y);
-  }
 };
