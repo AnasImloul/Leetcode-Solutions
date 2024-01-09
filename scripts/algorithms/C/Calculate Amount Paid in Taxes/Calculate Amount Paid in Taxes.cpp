e40461d3dea85296be3a6cf6a2daf92e2b14b3b2
AnasImloul <pydev27@gmail.com>
2024-01-09 04:30:09 +0100

    Runtime: 7 ms (Top 90.16%) | Memory: 14.00 MB (Top 9.45%)

--- scripts/algorithms/C/Calculate Amount Paid in Taxes/Calculate Amount Paid in Taxes.cpp
+++ scripts/algorithms/C/Calculate Amount Paid in Taxes/Calculate Amount Paid in Taxes.cpp
@@ -1,24 +1,17 @@
+// Runtime: 7 ms (Top 90.16%) | Memory: 14.00 MB (Top 9.45%)
+
 class Solution {
 public:
-    double calculateTax(vector<vector<int>>& brackets, int income) 
-    {    
-        double tax = 0; 
-        
-        if(brackets[0][0]<=income) tax=(brackets[0][0]*brackets[0][1]/100.0);         
-        else return (income*brackets[0][1]/100.0);
-            
-        for(int i=1; i<brackets.size(); i++)
-        {
-            if(brackets[i][0]<=income)
-                    tax += ((brackets[i][0]-brackets[i-1][0])*brackets[i][1]/100.0);
-            
-            else 
-            {
-                tax += ((income-brackets[i-1][0])*brackets[i][1]/100.0);
-                break;
-            }
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double ans =0;
+        int back=0;
+        for(auto x:brackets){
+            int upper = x[0], percent=x[1];
+            if(upper>income){
+                ans += (income-back)*percent; break;}
+            else ans += (upper-back)*percent;
+            back = upper;
         }
-        return tax;
-        
+        return (ans/100);
     }
 };
