427cacc8f45a9768ae80b37b501cd231f495cb2f
AnasImloul <pydev27@gmail.com>
2024-01-09 13:09:19 +0100

    Runtime: 31 ms (Top 87.54%) | Memory: 21.90 MB (Top 67.02%)

--- scripts/algorithms/B/Brick Wall/Brick Wall.cpp
+++ scripts/algorithms/B/Brick Wall/Brick Wall.cpp
@@ -1,31 +1,24 @@
+// Runtime: 31 ms (Top 87.54%) | Memory: 21.90 MB (Top 67.02%)
+
 class Solution {
 public:
-    int leastBricks(vector<vector<int>>& wall) {
-        vector<unordered_set<int>> v;
-        for(auto i : wall) {
-            int cs = 0;
-            unordered_set<int> st;
-            for(int j=0; j<i.size() - 1; j++) {
-                cs += i[j];
-                st.insert(cs);
+    int leastBricks(vector<vector<int>>& wall) 
+    {
+        unordered_map<int, int> edge_frequency;     //HashMap to store the number of common edges among the rows
+        int max_frequency = 0;         //Variable to store the frequency of most occuring edge
+        
+        for(int row=0; row<wall.size(); row++)        //Iterating through each row
+        {
+            int edge_postion = 0;       //Variable to store different edge postion
+            
+            for(int brick_no=0; brick_no< wall[row].size() -1; brick_no++)    //Iterating through each brick inside a row
+            { 
+                int current_brick_length = wall[row][brick_no];  //Length of the current brick
+                edge_postion = edge_postion + current_brick_length ;  //Next Edge Position = Previous Edge Position + Current Brick's Length
+                edge_frequency[edge_postion]++;  //Incrementing the Frequency of just calculated Edge Postion
+                max_frequency = max(edge_frequency[edge_postion],max_frequency);  //Comparing the "Frequency of just calculated Edge Postion" with "Max Frequency seen till now" & storing whichever is greater.
             }
-            v.push_back(st);
         }
-        
-        unordered_map<int, int> m;
-        for(auto i : v) {
-            for(auto j : i) m[j]++;
-        }
-        
-        int mxfr = INT_MIN, mxel;
-        for(auto i : m) {
-            if(i.second > mxfr) mxel = i.first, mxfr = i.second;
-        }
-        
-        int ans = 0;
-        for(auto s : v) {
-            if(s.find(mxel) == s.end()) ans++;
-        }
-        return ans;
+        return wall.size() - max_frequency; // returning (Number of Bricks Crossed by Line) i.e. (Number of Rows in Wall - Frequency of Most Occuring Edge) 
     }
 };
