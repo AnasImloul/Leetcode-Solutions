5e6a8dd521de356b26a93c3b79aa5db4eb563d36
AnasImloul <pydev27@gmail.com>
2024-01-10 20:55:44 +0100

    Runtime: 40 ms (Top 97.32%) | Memory: 41.70 MB (Top 60.47%)

--- scripts/algorithms/C/Climbing Stairs/Climbing Stairs.js
+++ scripts/algorithms/C/Climbing Stairs/Climbing Stairs.js
@@ -1,20 +1,10 @@
-/*
-DP
+// Runtime: 40 ms (Top 97.32%) | Memory: 41.70 MB (Top 60.47%)
 
-dp[i] represents the total number of different ways to take i steps
-So, we want to get dp[n].
-dp[n] = dp[n-1] + dp[n-2] because we can either take 1 or 2 steps.
-
-We have two base cases: dp[1] = 1 and dp[2] = 2 because
-there is one way to take 1 step and there are two ways to take 2 steps (1 step + 1 step OR 2 step)
-*/
 var climbStairs = function(n) {
-    let dp = new Array(n + 1);
-    dp[1] = 1, dp[2] = 2;
-    for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
+    if (n < 4) return n;
+    let fib = [1, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
-    return dp[n];
-    // T.C: O(N)
-    // S.C: O(N)
+    return fib[n];
 };
