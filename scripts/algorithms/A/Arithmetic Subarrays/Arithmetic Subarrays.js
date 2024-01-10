c731f4b20057832f1f6623a6a1eee6cc7492b656
AnasImloul <pydev27@gmail.com>
2024-01-10 20:50:42 +0100

    Runtime: 84 ms (Top 88.87%) | Memory: 49.20 MB (Top 18.22%)

--- scripts/algorithms/A/Arithmetic Subarrays/Arithmetic Subarrays.js
+++ scripts/algorithms/A/Arithmetic Subarrays/Arithmetic Subarrays.js
@@ -1,22 +1,47 @@
+// Runtime: 84 ms (Top 88.87%) | Memory: 49.20 MB (Top 18.22%)
+
 /**
  * @param {number[]} nums
  * @param {number[]} l
  * @param {number[]} r
  * @return {boolean[]}
  */
- var checkArithmeticSubarrays = function(nums, l, r) {
+var checkArithmeticSubarrays = function(nums, l, r) {
     let result = [];
-    for(let i=0;i<l.length;i++) {
-        let subNums = [...nums].slice(l[i], r[i]+1);
-        subNums = subNums.sort((a,b) => a-b);
-        let diff = subNums[1]-subNums[0];
-        let s = true
-        for(let j=0;j<subNums.length-1;j++) {
-            if(!(subNums[j+1]-subNums[j] == diff))
-                s = false
-        }
-        result.push(s)
+    for (let i = 0; i < l.length; i++) {
+        result.push(isArithmetic(nums.slice(l[i], r[i] + 1)));
     }
-    return result
+    return result;
 };
 
+function isArithmetic(arr) {
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+
+    if (min == max) {
+        return true;
+    }
+
+    let step = (max - min) / (arr.length - 1);
+
+    if (step != Math.floor(step)) {
+        return false;
+    }
+
+    let set = new Set();
+    for (let x of arr) {
+        if (set.has(x)) {
+            return false;
+        }
+
+        set.add(x);
+    }
+
+    for (let x = min; x <= max; x += step) {
+        if (!set.has(x)) {
+            return false;
+        }
+    }
+
+    return true;
+}
