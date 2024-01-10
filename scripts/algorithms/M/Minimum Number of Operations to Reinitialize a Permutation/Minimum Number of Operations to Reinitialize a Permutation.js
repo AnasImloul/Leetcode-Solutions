fa249122b4ac7a83cb02f24d68f3d5816abefaeb
AnasImloul <pydev27@gmail.com>
2024-01-10 21:09:18 +0100

    Runtime: 57 ms (Top 54.55%) | Memory: 41.60 MB (Top 90.91%)

--- scripts/algorithms/M/Minimum Number of Operations to Reinitialize a Permutation/Minimum Number of Operations to Reinitialize a Permutation.js
+++ scripts/algorithms/M/Minimum Number of Operations to Reinitialize a Permutation/Minimum Number of Operations to Reinitialize a Permutation.js
@@ -1,40 +1,15 @@
- * @param {number} n
- * @return {number}
- */
+// Runtime: 57 ms (Top 54.55%) | Memory: 41.60 MB (Top 90.91%)
+
 var reinitializePermutation = function(n) {
-  if (n < 2) return 0;
-  let prem = [];
-  let count = 0;
-  for (var i = 0; i < n; i++) {
-    prem[i] = i;
-  }
-  let newArr = [];
-  newArr = helper(prem, newArr);
+    const BASE_INDEX = 1;
+    let result = 1;
+    let index = n / 2 + (BASE_INDEX - 1) / 2;
 
-  const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-
-  if (equals(prem, newArr)) {
-    count++;
-    return count;
-  } else {
-    while (!equals(prem, newArr)) {
-      count++;
-      let temp = newArr;
-      newArr = [];
-      newArr = helper(temp, newArr);
+    while (index !== BASE_INDEX) {
+        index = index % 2
+            ? n / 2 + (index - 1) / 2
+            : index / 2;
+        result += 1;
     }
-  }
-  return count + 1;
-};
-
-var helper = function (prem, arr) {
-  let n = prem.length;
-  for (var i = 0; i < n; i++) {
-    if (i % 2 == 0) {
-      arr[i] = prem[i / 2];
-    } else {
-      arr[i] = prem[n / 2 + (i - 1) / 2];
-    }
-  }
-  return arr;
+    return result;
 };
