e5ed1190faa503e5a1e1d9816934ee28a212f912
AnasImloul <pydev27@gmail.com>
2024-01-10 21:22:11 +0100

    Runtime: 435 ms (Top 38.09%) | Memory: 65.90 MB (Top 50.0%)

--- scripts/algorithms/R/Remove Stones to Minimize the Total/Remove Stones to Minimize the Total.js
+++ scripts/algorithms/R/Remove Stones to Minimize the Total/Remove Stones to Minimize the Total.js
@@ -1,55 +1,100 @@
+// Runtime: 435 ms (Top 38.09%) | Memory: 65.90 MB (Top 50.0%)
+
 /**
  * @param {number[]} piles
  * @param {number} k
  * @return {number}
  */
-
 var minStoneSum = function(piles, k) {
-    var heapArr = [];
-    for (let i=0; i<piles.length; i++){
-        heapArr.push(piles[i]);
-        heapifyUp();
-    }
-
-    function heapifyUp(){
-        let currIndex = heapArr.length-1;
-        while (heapArr[currIndex] > heapArr[Math.floor((currIndex-1)/2)]){
-            swap(currIndex, Math.floor((currIndex-1)/2));
-            currIndex = Math.floor((currIndex-1)/2);
+    const heap = new Heap(piles);
+    
+    let i = 0;
+    while(i< k){
+	
+		//TAKE THE TOP MAX VALUE TO REDUCE
+        let top = heap.dequeue();
+        let updated = top - (~~(top/2));
+		
+		//IF AFTER  UPDATION VALUE IS NOT 0 THEN INSERT AGAIN
+        if(updated){
+            heap.enqueue(updated);
         }
+        i++;
     }
+    return  heap.getTree().reduce((acc,v)=> acc+v,0);
     
-    function heapifyDown(){
-        let currIndex = 0;
-        let bigger = currIndex;
-        while (heapArr[currIndex*2+1]){
-            if (heapArr[currIndex] < heapArr[currIndex*2+1]){
-                bigger = currIndex * 2 + 1;
-            }
-            
-            if (heapArr[bigger] < heapArr[currIndex*2+2]){
-                bigger = currIndex * 2 + 2;
-            }
-            
-            if (bigger === currIndex){
-                break;
-            }
-            swap(currIndex, bigger);
-            currIndex = bigger;
-        }
-    }
-    
-    function swap(currIndex, otherIndex){
-        let temp = heapArr[currIndex];
-        heapArr[currIndex] = heapArr[otherIndex];
-        heapArr[otherIndex] = temp;
-    }
-    
-    while (k > 0){
-        heapArr[0] -= Math.floor(heapArr[0]/2);
-        heapifyDown();
-        k--;
-    }
-    
-    return heapArr.reduce((a,num) => a+num, 0);
 };
+
+
+class Heap{
+    constructor(list = []){
+        this.tree = [null];
+        this.list = list;
+        this.build();
+    }
+    
+    build(){
+        for(let priority of this.list)
+            this.enqueue(priority);
+    }
+    
+    swap(pos1, pos2){
+        [this.tree[pos1], this.tree[pos2]] = [this.tree[pos2],this.tree[pos1]]
+    }
+    
+    enqueue(priority){
+        this.tree[this.tree.length] = priority;
+        let i = this.tree.length - 1, parent = ~~(i/2);
+        while(i > 1){
+            if(this.tree[parent] < this.tree[i])
+                this.swap(parent,i);
+            i = parent;
+            parent = ~~(i/2);
+        }
+    }
+    
+    dequeue(){
+     let size = this.tree.length - 1, pos = 1;
+    if(!size) return;
+
+
+    let last = this.tree.pop(), deleted = this.tree[pos];
+
+
+    if(!deleted && last) return last;
+
+    this.tree[pos] = last;
+    this.heapify(pos);
+    return deleted;
+    }
+    
+    heapify(pos){
+    
+        if(pos > this.tree.length) return;
+        let leftPos = 2*pos, rightPos = 2*pos +1;
+        
+        let left = this.tree[leftPos] ? this.tree[leftPos] : -Infinity;
+        let right = this.tree[rightPos] ? this.tree[rightPos] : -Infinity, minVal = null, minIndex = null;
+        
+        if(left > right){
+            minVal = left;
+            minIndex = leftPos;
+        }else{
+            minVal = right;
+            minIndex = rightPos
+        }
+        if(this.tree[pos] < minVal){
+            this.swap(pos,minIndex);
+            this.heapify(minIndex);
+        }
+        
+    }
+    
+    getTree(){
+        return this.tree.slice(1);
+    }
+    
+    getSize(){
+        return this.tree.length - 1;
+    }
+}
