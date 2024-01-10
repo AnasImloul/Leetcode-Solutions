d175d58bb989da999e7344ab5466ecdbbe5afc33
AnasImloul <pydev27@gmail.com>
2024-01-10 21:17:48 +0100

    Runtime: 92 ms (Top 80.92%) | Memory: 48.10 MB (Top 48.59%)

--- scripts/algorithms/T/Target Sum/Target Sum.js
+++ scripts/algorithms/T/Target Sum/Target Sum.js
@@ -1,11 +1,21 @@
-var findTargetSumWays = function(nums, target) {
-    const memo = new Map()
-    return backtrack(0, 0)
+// Runtime: 92 ms (Top 80.92%) | Memory: 48.10 MB (Top 48.59%)
+
+var findTargetSumWays = function(nums, S) {
+  let sums = new Map([[0, 1]]);
+  
+  for (let num of nums) {
+    const next = new Map();
     
-    function backtrack(i, cur){
-        const key = `${i} ${cur}`
-        if(i == nums.length) return cur == target ? 1 : 0
-        if(!memo.has(key)) memo.set(key, backtrack(i + 1, -nums[i] + cur) + backtrack(i + 1, nums[i] + cur))
-        return memo.get(key)
+    for (let [sum, amount] of sums) {
+      const plus = sum + num;
+      const minus = sum - num;
+
+      next.set(plus, next.has(plus) ? next.get(plus) + amount : amount);
+      next.set(minus, next.has(minus) ? next.get(minus) + amount : amount);
     }
+    
+    sums = next;
+  }
+  
+  return sums.has(S) ? sums.get(S) : 0;
 };
