164e83aa8ea3c047d49c06bafcb5f1c23a643802
AnasImloul <pydev27@gmail.com>
2024-01-10 20:47:33 +0100

    Runtime: 52 ms (Top 88.07%) | Memory: 45.00 MB (Top 7.49%)

--- scripts/algorithms/R/Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.js
+++ scripts/algorithms/R/Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.js
@@ -1,28 +1,19 @@
+// Runtime: 52 ms (Top 88.07%) | Memory: 45.00 MB (Top 7.49%)
+
 var deleteDuplicates = function(head) {
-    
-    let map = new Map()
-    let result = new ListNode(-1)
-    
-    let newCurr = result
-    let curr = head
-    
-    while(head){
-        if(map.has(head.val)){
-            map.set(head.val, 2)
-        }else{
-            newArr.push(head.val)
-            map.set(head.val, 1)
+    const dummy = new ListNode();
+    dummy.next = head;
+    let node = dummy;                                                  // the last known distinct node
+    while (node.next) {
+        if (node.next.next && node.next.val === node.next.next.val) {  // if the next two nodes are equal...
+            let nonValNode = node.next.next.next;
+            while (nonValNode && nonValNode.val === node.next.val) {   // ...find the first one that isn't...
+                nonValNode = nonValNode.next;
+            }
+            node.next = nonValNode;                                    // ...and glue it to the last known distinct node;...
+        } else {
+            node = node.next;                                          // ...otherwise the next node is distinct
         }
-        head = head.next
     }
-    
-    while(curr){
-        if(map.get(curr.val) !== 2){
-            newCurr.next = new ListNode(curr.val)
-            newCurr = newCurr.next
-        }
-        curr = curr.next
-    }
-    
-    return result.next
+    return dummy.next;
 };
