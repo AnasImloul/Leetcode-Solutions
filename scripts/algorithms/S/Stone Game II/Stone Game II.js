39b1d3d940f39ec0f7df66d74e4dbe1151e602e5
AnasImloul <pydev27@gmail.com>
2024-01-10 21:01:47 +0100

    Runtime: 76 ms (Top 76.19%) | Memory: 47.60 MB (Top 52.38%)

--- scripts/algorithms/S/Stone Game II/Stone Game II.js
+++ scripts/algorithms/S/Stone Game II/Stone Game II.js
@@ -1,23 +1,31 @@
-// Runtime: 247 ms (Top 9.09%) | Memory: 45 MB (Top 68.18%)
+// Runtime: 76 ms (Top 76.19%) | Memory: 47.60 MB (Top 52.38%)
 
+/**
+ * @param {number[]} piles
+ * @return {number}
+ */
 var stoneGameII = function(piles) {
-  const length = piles.length;
-  const dp = [...Array(length + 1).fill(null)].map((_) =>
-    Array(length + 1).fill(0)
-  );
-  const sufsum = new Array(length + 1).fill(0);
-  for (let i = length - 1; i >= 0; i--) {
-    sufsum[i] = sufsum[i + 1] + piles[i];
-  }
-  for (let i = 0; i <= length; i++) {
-    dp[i][length] = sufsum[i];
-  }
-  for (let i = length - 1; i >= 0; i--) {
-    for (let j = length - 1; j >= 1; j--) {
-      for (let X = 1; X <= 2 * j && i + X <= length; X++) {
-        dp[i][j] = Math.max(dp[i][j], sufsum[i] - dp[i + X][Math.max(j, X)]);
-      }
+    let sum = 0;
+    const dp = new Array(101).fill(0).map(() => new Array(201).fill(0));
+    for(let i = 0; i < piles.length; i++) {
+        sum += piles[i];
     }
-  }
-  return dp[0][1];
+    let diff = help(0, 1, piles, dp);
+    return (sum + diff) / 2;
 };
+
+function help(i, M, piles, dp) {
+    if(i >= piles.length)
+        return 0;
+    if(dp[i][M] != 0)
+        return dp[i][M];
+    let total = 0;
+    let ans = Number.MIN_SAFE_INTEGER;
+    for(let j = 0; j < 2 * M; j++) {
+        if(i + j < piles.length)
+            total += piles[i+j];
+        ans = Math.max(ans, total - help(i+j+1,Math.max(M,j+1),piles, dp));
+    }
+    dp[i][M] = ans;
+    return ans;
+}
