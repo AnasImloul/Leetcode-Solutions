78d908cddee747bd3d68dcf1d825e762869c8400
AnasImloul <pydev27@gmail.com>
2024-01-10 20:37:50 +0100

    Runtime: 59 ms (Top 81.36%) | Memory: 45.80 MB (Top 89.83%)

--- scripts/algorithms/B/Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.js
+++ scripts/algorithms/B/Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.js
@@ -1,17 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {boolean[]}
- */
-var prefixesDivBy5 = function(nums) {
-    const answer = new Array(nums.length).fill(false)
-    let cur = 0
-    for (let i = 0; i < nums.length; i++) {
-        cur = cur * 2 + nums[i]
-        if (cur % 5 === 0) 
-            answer[i] = true
-        cur %= 5
-    }
-    
-    return answer
-};
+// Runtime: 59 ms (Top 81.36%) | Memory: 45.80 MB (Top 89.83%)
 
+var prefixesDivBy5 = function(nums) {
+   let res = [];
+   let binary = 0;
+   for(let num of nums){
+       binary = (binary % 5) * 2 + num;
+       let checkBinary = (binary % 5 === 0);
+       res.push(checkBinary);
+   }    
+   return res;
+};
