2d5144767caf19ee1b8902eb66e15468d1e138dd
AnasImloul <pydev27@gmail.com>
2024-01-10 20:46:04 +0100

    Runtime: 497 ms (Top 62.5%) | Memory: 184.40 MB (Top 50.0%)

--- scripts/algorithms/T/The Time When the Network Becomes Idle/The Time When the Network Becomes Idle.js
+++ scripts/algorithms/T/The Time When the Network Becomes Idle/The Time When the Network Becomes Idle.js
@@ -1,52 +1,47 @@
-/**
- * @param {number[][]} edges
- * @param {number[]} patience
- * @return {number}
- */
+// Runtime: 497 ms (Top 62.5%) | Memory: 184.40 MB (Top 50.0%)
+
 var networkBecomesIdle = function(edges, patience) {
-  /*
-  Approach:
-  Lets call D is the distance from node to master
-  And last message sent from node is at T
-  Then last message will travel till D+T and network will be idal at D+T+1
-  */  
-    let edgesMap={},minDistanceFromMasterArr=[],ans=0,visited={};
-    for(let i=0;i<edges.length;i++){
-        if(edgesMap[edges[i][0]]===undefined){
-            edgesMap[edges[i][0]] = [];
-        }
-        edgesMap[edges[i][0]].push(edges[i][1]);
-        if(edgesMap[edges[i][1]]===undefined){
-            edgesMap[edges[i][1]] = [];
-        }
-        edgesMap[edges[i][1]].push(edges[i][0]);
+    let graph = {};
+    for (let [edge, neighbor] of edges) {
+        if (!(edge in graph)) graph[edge] = [];
+        graph[edge].push(neighbor);
+        if (!(neighbor in graph)) graph[neighbor] = [];
+        graph[neighbor].push(edge);
     }
-    
-    let queue=[],node,neighbour;
-    minDistanceFromMasterArr[0]=0;//Distance of source to source is 0 
-    queue.push(0);
-    while(queue[0]!==undefined){
-        node = queue.shift();
-        for(let i=0;i<edgesMap[node].length;i++){
-            neighbour = edgesMap[node][i];   
-            if(minDistanceFromMasterArr[neighbour]===undefined){
-                minDistanceFromMasterArr[neighbour] = minDistanceFromMasterArr[node] + 1;
-                queue.push(neighbour);
+
+    // Initialize queue with server 0
+    let q = [0];
+	let seconds = 0, complete = {};
+    while (q.length) {
+        let newQ = [];
+        while (q.length) {
+            let current = q.pop();
+            if (current in complete) continue;
+            complete[current] = seconds * 2;
+            for (let edge of graph[current]) {
+                if (complete[edge]) continue;
+                newQ.push(edge);
             }
         }
-    }
-    for(let i=1;i<patience.length;i++){
-        let responseWillBeReceivedAt = minDistanceFromMasterArr[i]*2;
-        let lastMessageSentAt;
-        if(patience[i]<responseWillBeReceivedAt){
-            lastMessageSentAt = Math.floor((responseWillBeReceivedAt-1)/patience[i])*patience[i];
-        }else{
-            lastMessageSentAt=0;
-        }
-        let lastMessageWillTravelTill = lastMessageSentAt + responseWillBeReceivedAt;
-        let firstIdleSecond = lastMessageWillTravelTill+1;
-        ans = Math.max(ans,firstIdleSecond);
+        q = newQ;
+        seconds++;
     }
     
-    return ans;
+    // Total servers
+    let servers = Object.keys(graph).length;
+    
+    let maxSeconds = 0;
+    for (let i = 1; i < servers; i++) {
+        let timeToComplete = complete[i];
+        let resendInterval = patience[i];
+        
+        // If timeToComplete == 4 and resendInterval == 2, the msg will be resent 1 time at second 2 and not second 4
+        // If timeToComplete == 5 and resendInterval == 2, the msg will be resent 2 times at second 2 and second 4
+        // Hence we use Math.ceil(...) - 1
+        let resendCount = Math.ceil(timeToComplete / resendInterval) - 1;
+        
+        let seconds = timeToComplete + (resendInterval * resendCount);
+        maxSeconds = Math.max(maxSeconds, seconds);
+    }
+    return maxSeconds + 1;
 };
