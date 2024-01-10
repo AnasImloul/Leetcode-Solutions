54cd19340648ebcf79ea1df0666390b6f55326bb
AnasImloul <pydev27@gmail.com>
2024-01-10 21:04:03 +0100

    Runtime: 354 ms (Top 80.0%) | Memory: 113.40 MB (Top 40.0%)

--- scripts/algorithms/K/Kth Ancestor of a Tree Node/Kth Ancestor of a Tree Node.js
+++ scripts/algorithms/K/Kth Ancestor of a Tree Node/Kth Ancestor of a Tree Node.js
@@ -1,39 +1,57 @@
+// Runtime: 354 ms (Top 80.0%) | Memory: 113.40 MB (Top 40.0%)
+
 /**
  * @param {number} n
  * @param {number[]} parent
  */
-var TreeAncestor = function(n, parent) {
-    this.n = n;
-    this.parent = parent;
-};
 
+
+let par;
+var TreeAncestor = function(n, parent) {
+  let p = new Array(n);
+    for(let i=0;i<n;i++){
+           p[i] = new Array(20);
+       }
+    
+        for(let i=0;i<n;i++){
+            p[i][0] = parent[i];
+        }
+    
+    for(let j=1;j<20;j++){
+        for(let i=0;i<n;i++){
+            if(p[i][j-1]==-1){
+                p[i][j]= -1;
+            }
+            else{
+                p[i][j] = p[p[i][j-1]][j-1];
+            }
+        
+        }
+    }
+    par = p;
+}
 /** 
  * @param {number} node 
  * @param {number} k
  * @return {number}
  */
-
-/*
-Qs:
-1. Is the given tree a binary tree or n-ary tree?
-2. Can k be greater than possible (the total number of ancestors of given node)?
-
-Every node has a parent except the root.
-1. Check if given node has a parent. If not, return -1.
-2. Set given node as the current ancestor and start a while loop which continues while k is greater than 0.
-At each iteration, we set `ancestor` to the parent of current `ancestor` and decrement k. If k is still greater
-than 0 but ancestor is -1, that means k is greater than possible. Hence, we return -1. Else, while loop will
-end when we are at the correct k-th ancestor. We return ancestor.
-*/
 TreeAncestor.prototype.getKthAncestor = function(node, k) {
-    // check if given node has a parent
-    if (this.parent[node] === -1) return -1;
-    let ancestor = node;
-    while (k > 0) {
-        if (ancestor === -1) return -1; // k is greater than total number of ancestors of given node
-        ancestor = this.parent[ancestor];
-        k--;
-    }
-    return ancestor;
-    // T.C: O(k)
+    
+        for(let i=0;i<20;i++){
+            if((k>>i)&1){
+                node = par[node][i];
+                if(node==-1){
+                    return -1;
+                }
+            }
+        }
+    return node;
+    
 };
+
+/** 
+ * Your TreeAncestor object will be instantiated and called as such:
+ * var obj = new TreeAncestor(n, parent)
+ * var param_1 = obj.getKthAncestor(node,k)
+ */
+
