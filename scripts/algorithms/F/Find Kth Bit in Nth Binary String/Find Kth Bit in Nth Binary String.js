22a02b818220a462fd2f43b5c29bd9676107ee18
AnasImloul <pydev27@gmail.com>
2024-01-10 20:53:00 +0100

    Runtime: 54 ms (Top 95.65%) | Memory: 41.70 MB (Top 91.3%)

--- scripts/algorithms/F/Find Kth Bit in Nth Binary String/Find Kth Bit in Nth Binary String.js
+++ scripts/algorithms/F/Find Kth Bit in Nth Binary String/Find Kth Bit in Nth Binary String.js
@@ -1,16 +1,26 @@
-// Runtime: 1452 ms (Top 22.22%) | Memory: 112 MB (Top 33.33%)
+// Runtime: 54 ms (Top 95.65%) | Memory: 41.70 MB (Top 91.3%)
+
 var findKthBit = function(n, k) {
-    return recursivelyGenerate(n-1)[k-1];
-}
+   if (k === 1)    return '0';
+   if (k === 2)    return '1';
+   
+   let binary = 2;
+   while (binary < k) {
+       binary *= 2;
+   }
+   
+   let count = 0;
+   while (binary > 2) {
+       if (k == binary/2) {
+//             The starting number is 1 if at position power of 2 (2^x)
+           return (count%2 == 0) ? '1' : '0';
+       }
+       if (k > binary/2) {
+           k = binary-k;
+           count++;
+       }
+       binary /= 2;
+   }
 
-function recursivelyGenerate(bitLength, memo=new Array(bitLength+1)){
-    if(bitLength===0) return "0";
-    if(memo[bitLength]) return memo[bitLength];
-    const save = recursivelyGenerate(bitLength-1);
-    memo[bitLength] = save + '1' + reverseInvert(save);
-    return memo[bitLength];
-}
-
-function reverseInvert(s){
-    return s.split("").map(a=>a==='0'?'1':'0').reverse().join("");
-}
+   return (count%2 == 0) ? '0' : '1';
+};
