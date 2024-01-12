b53658495374de42d2ff2d6747eed9df60895ce0
AnasImloul <pydev27@gmail.com>
2024-01-12 23:57:57 +0100

    Runtime: 35 ms (Top 98.98%) | Memory: 45.00 MB (Top 39.53%)

--- scripts/algorithms/M/Minimum Area Rectangle/Minimum Area Rectangle.java
+++ scripts/algorithms/M/Minimum Area Rectangle/Minimum Area Rectangle.java
@@ -1,39 +1,34 @@
-// Runtime: 70 ms, faster than 94.51% of Java submissions
+// Runtime: 35 ms (Top 98.98%) | Memory: 45.00 MB (Top 39.53%)
 
 class Solution {
-
     public int minAreaRect(int[][] points) {
-
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-
-		// Group the points by x coordinates
-        for (int[] point : points) {
-            if (!map.containsKey(point[0])) map.put(point[0], new HashSet<>());
-            map.get(point[0]).add(point[1]);
+        HashMap<Integer,Set<Integer>> hm = new HashMap<>();
+        int area=Integer.MAX_VALUE;
+        
+        for(int[] point: points)
+        {            
+            if(!hm.containsKey(point[0]))
+                hm.put(point[0],new HashSet());        
+            hm.get(point[0]).add(point[1]); // x-coordinate already exits, just add the y-coordinate to the set
         }
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < points.length - 1; i++) {
-
-            int x1 = points[i][0];
-            int y1 = points[i][1];
-            for (int j = i + 1; j < points.length; j++) {
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                if (x1 == x2 || y1 == y2) // We are looking for diagonal point, so if j is neighbour point, then continue
-                    continue;
-
-                // Note - We are calculating area first (before checking whether these points form the correct rectangle), because
-                // cost of checking rectangle is very higher than calculating area. So if area less than the prev area (min), then only 
-                // it makes sense to check rectangle and override min (if these points forms the correct rectangle)
-                int area = Math.abs(x1 - x2) * Math.abs(y1 - y2);
-                if (area < min) {
-                    boolean isRectangle = map.get(x1).contains(y2) && map.get(x2).contains(y1);
-                    if (isRectangle) min = area;
+        
+        for(int i=0;i<points.length-1;i++)
+        {
+            for(int j=i+1;j<points.length;j++)
+            {
+                int x1=points[i][0],x2=points[j][0],y1=points[i][1],y2=points[j][1]; // this step reduced the time from 368ms to 186ms
+                if(x1!=x2 && y1!=y2) //diagonal is not parallel to x or y axis
+                {
+                    if(area>Math.abs((x2-x1) * (y2-y1))) //pre-calulate the area to avoid unecessary lookup. This step reduced the time from 186ms to 57ms.
+                    {
+                        if(hm.get(x1).contains(y2) && hm.get(x2).contains(y1)) // learnt from other leetcoders. Thank you.
+                             area=Math.abs((x2-x1) * (y2-y1)); 
+                    }
                 }
+                
             }
         }
-
-        return min == Integer.MAX_VALUE ? 0 : min;
+        
+        return area<Integer.MAX_VALUE? area:0;
     }
 }
