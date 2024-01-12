a9c50c4a3c0bf2363659c0f5f0504c75a0d4e2d6
AnasImloul <pydev27@gmail.com>
2024-01-12 23:59:39 +0100

    Runtime: 24 ms (Top 72.47%) | Memory: 45.70 MB (Top 34.84%)

--- scripts/algorithms/R/Rearrange Words in a Sentence/Rearrange Words in a Sentence.java
+++ scripts/algorithms/R/Rearrange Words in a Sentence/Rearrange Words in a Sentence.java
@@ -1,11 +1,24 @@
+// Runtime: 24 ms (Top 72.47%) | Memory: 45.70 MB (Top 34.84%)
+
 class Solution {
     public String arrangeWords(String text) {
-        String[] words = text.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            words[i] = words[i].toLowerCase();
-        }
-        Arrays.sort(words, (s, t) -> s.length() - t.length());
-        words[0] = Character.toUpperCase(words[0].charAt(0)) + words[0].substring(1);
-        return String.join(" ", words);
+        text = text.replace(text.charAt(0)+"", (char)(text.charAt(0)+32)+"");
+
+        String[] arr = text.split(" ");
+        Arrays.sort(arr, new Comparator<String>(){
+            public int compare(String s1, String s2){
+                return Integer.compare(s1.length(), s2.length());
+            }
+        });
+
+        StringBuilder str = new StringBuilder(arr[0]);
+
+        for(int i = 1; i < arr.length; i++)
+            str.append(" "+arr[i]);
+
+        text = (char)(str.charAt(0)-32)+str.substring(1, str.length());
+
+        return text;
     }
 }
+// UP-VOTE IF HELPFUL
