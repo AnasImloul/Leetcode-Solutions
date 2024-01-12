74bf296b16995bea3e925ad8f13a53d23830e4d6
AnasImloul <pydev27@gmail.com>
2024-01-13 00:13:20 +0100

    Runtime: 3 ms (Top 100.0%) | Memory: 45.60 MB (Top 14.69%)

--- scripts/algorithms/S/Split Two Strings to Make Palindrome/Split Two Strings to Make Palindrome.java
+++ scripts/algorithms/S/Split Two Strings to Make Palindrome/Split Two Strings to Make Palindrome.java
@@ -1,24 +1,35 @@
+// Runtime: 3 ms (Top 100.0%) | Memory: 45.60 MB (Top 14.69%)
+
 class Solution {
     public boolean checkPalindromeFormation(String a, String b) {
-        return split(a, b) || split(b, a);
+        // either way of split should give us a palindrome
+        return cut(a, b) || cut(b, a);
     }
-    
-    private boolean split(String a, String b) {
-        int left = 0, right = a.length() - 1;
-        while (left < right && a.charAt(left) == b.charAt(right)) {
-            left++;
-            right--;
+
+    // method to match letters from both ends
+    private boolean cut(String a, String b) {
+        int i = 0, j = a.length() - 1;
+        // converge from both ends till we have same letters
+        while (i < j && a.charAt(i) == b.charAt(j)) {
+            i++; j--;
         }
-        if (left >= right) return true;
-        return isPalindrome(a, left, right) || isPalindrome(b, left, right);
+
+        // the case when we surpassed the mid point from both ends
+        if (i >= j) return true;
+        // the case when there is still a substring left in between
+        // or say we didn't reach the mid point
+        // we will check if that substring is a palindrome or not
+        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
     }
-    
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left <= right) {
-            if (s.charAt(left) != s.charAt(right)) return false;
-            left++;
-            right--;
+
+    // method to check if a string is palindrome
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
         }
+
         return true;
     }
 }
