33c32fbd5bf66046d69e35a0209645623395cc6a
AnasImloul <pydev27@gmail.com>
2024-01-13 00:01:57 +0100

    Runtime: 5 ms (Top 97.91%) | Memory: 58.00 MB (Top 9.21%)

--- scripts/algorithms/W/Ways to Make a Fair Array/Ways to Make a Fair Array.java
+++ scripts/algorithms/W/Ways to Make a Fair Array/Ways to Make a Fair Array.java
@@ -1,63 +1,29 @@
+// Runtime: 5 ms (Top 97.91%) | Memory: 58.00 MB (Top 9.21%)
+
 class Solution {
     public int waysToMakeFair(int[] nums) {
-        
-        /*
-        Idea - 
-        
-        have left (odd & even) & right (odd & even) odd & even sums separately
-        
-        as we move each element subtract & add appropriately
-        */
-        int count = 0;
-        
-        int evenLeft = 0;
-        int oddLeft = 0;
-        
-        int evenRight = 0;
-        int oddRight = 0;
-            
-        // calculate the right odd & even initially since we move from 0->n
-        for(int i=0;i<nums.length;i++)
-        {
-            if(i%2 == 0)
-            {
-                evenRight+=nums[i];
-            } else{
-                oddRight+= nums[i];
+        int esum = 0;  // Sum of even-indexed elements
+        int osum = 0;  // Sum of odd-indexed elements
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                osum += nums[i];
+            } else {
+                esum += nums[i];
             }
         }
-        
-        // move from 0 -> n
-        for(int i=0;i<nums.length;i++)
-        {
-            
-            // start from 1 since left at 0 index is 0 anyways & array bounds checked
-            if(i>0)
-            {
-                // add previous element to left count
-                if(i%2 == 0)
-                {
-                     oddLeft +=nums[i-1];
-                } else{
-                     evenLeft+=nums[i-1];
-                }
+        int count = 0;
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                osum = osum - nums[i] + prev;
+            } else {
+                esum = esum - nums[i] + prev;
             }
-            
-            // subtract current element value from right counts so we get right count excluding the current element
-            if(i%2 == 0)
-            {
-                evenRight-=nums[i];
-            } else{
-                oddRight-=nums[i];
+            if (esum == osum) {
+                count++;
             }
-            
-            // if at any point we have below condition true increment count
-            // notice here we are adding even & odd of opposite sides here since on excluding the current element the right counterpart sum switches
-            // i.e if it was odd before it becomes even, else if it was even becomes odd
-             if(evenLeft+oddRight == oddLeft+evenRight)
-                {
-                    count++;
-                }
+            prev = nums[i];
         }
         return count;
     }
