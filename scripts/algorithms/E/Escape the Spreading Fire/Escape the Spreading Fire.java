fa1f7c2a4caaf693b1114452bef444d356410ddf
AnasImloul <pydev27@gmail.com>
2024-01-13 00:20:40 +0100

    Runtime: 12 ms (Top 89.41%) | Memory: 45.50 MB (Top 60.0%)

--- scripts/algorithms/E/Escape the Spreading Fire/Escape the Spreading Fire.java
+++ scripts/algorithms/E/Escape the Spreading Fire/Escape the Spreading Fire.java
@@ -1,98 +1,101 @@
-
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+// Runtime: 12 ms (Top 89.41%) | Memory: 45.50 MB (Top 60.0%)
 
 class Solution {
-
-    public boolean ok(int[][] grid, int[][] dist, int wait_time) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        Queue<Pair<Integer, Integer, Integer>> Q = new LinkedList<>();
-        Q.add(new Pair<>(0, 0, wait_time));
-
-        int[][] visited = new int[n][m];
-        visited[0][0] = 1;
-
-        while (!Q.isEmpty()) {
-            Pair<Integer, Integer, Integer> at = Q.poll();
-            int[][] moves = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-            for (int[] to : moves) {
-                int ii = at.first + to[0];
-                int jj = at.second + to[1];
-                if (!inBounds(ii, jj, n, m) || visited[ii][jj] == 1 || grid[ii][jj] == 2) continue;
-                if (ii == n - 1 && jj == m - 1 && dist[ii][jj] >= at.third + 1) return true;
-                if (dist[ii][jj] <= at.third + 1) continue;
-                Q.add(new Pair<>(ii, jj, 1 + at.third));
-                visited[ii][jj] = 1;
-            }
-        }
-        return false;
-    }
-
-    public boolean inBounds(int i, int j, int n, int m) {
-        return (0 <= i && i < n && 0 <= j && j < m);
-    }
-
     public int maximumMinutes(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int[][] dist = new int[n][m];
-
-        for (int[] r : dist) Arrays.fill(r, Integer.MAX_VALUE);
-
-        Queue<Pair<Integer, Integer, Integer>> Q = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    Q.add(new Pair<>(i, j, 0));
-                    dist[i][j] = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[][] times = new int[m][n];
+        for(int[] row : times) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        
+        int[] dir = new int[]{-1, 0, 1, 0, -1};
+        
+		// Queue for running BFS
+        Deque<int[]> fires = new ArrayDeque<>();
+		
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if (grid[row][col] == 1) {
+                    times[row][col] = 0;
+                    fires.offer(new int[]{row, col});
                 }
             }
         }
-
-        while (!Q.isEmpty()) {
-            Pair<Integer, Integer, Integer> at = Q.poll();
-            int[][] moves = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-            for (int[] to : moves) {
-                int ii = at.first + to[0];
-                int jj = at.second + to[1];
-                if (!inBounds(ii, jj, n, m) || grid[ii][jj] == 2 || dist[ii][jj] <= at.third + 1) continue;
-                dist[ii][jj] = 1 + at.third;
-                Q.add(new Pair<>(ii, jj, 1 + at.third));
+        
+        int time = 1;
+        while(!fires.isEmpty()) {
+            int size = fires.size();
+            
+            for(int i = 0; i < size; i++) {
+                int[] pos = fires.poll();
+                
+                for(int j = 0; j < 4; j++) {
+                    int x = pos[0] + dir[j];
+                    int y = pos[1] + dir[j + 1];
+                    
+                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 0 && times[x][y] == Integer.MAX_VALUE) {
+                        times[x][y] = time;
+                        fires.offer(new int[]{x, y});
+                    }
+                }
             }
+            
+            time++;
         }
+        
+        fires.clear();
+        
+        int ans = Integer.MAX_VALUE;
+        fires.offer(new int[]{0, 0});
+        grid[0][0] = 2;
 
-        int left = 0;
-        int right = 1_000_000_000;
+        time = 1;
+        while(!fires.isEmpty() && grid[m - 1][n - 1] == 0) {
+            int size = fires.size();
+            
+            int t = Integer.MIN_VALUE;
+            
+            for(int i = 0; i < size && grid[m - 1][n - 1] == 0; i++) {
+                int[] pos = fires.poll();
+            
+                for(int j = 0; j < 4 && grid[m - 1][n - 1] == 0; j++) {
+                    
+                    int x = pos[0] + dir[j];
+                    int y = pos[1] + dir[j + 1];
 
-        int ans = -1;
+                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 0 && times[x][y] >= time) {
+                        if (x == m - 1 && y == n - 1) {
+                            t = times[x][y] - time;
+                            grid[x][y] = 2;
+                            break;
+                        }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (ok(grid, dist, mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else right = mid - 1;
+                        grid[x][y] = 2;
+                        fires.offer(new int[]{x, y});
+                        
+						// if times[x][y] == Integer.MAX_VALUE, fire will never reach this cell and it will contribute maximum wait time
+                        t = Math.max(t, times[x][y] == Integer.MAX_VALUE ? 1000000000 : times[x][y] - time - 1);
+                    }
+                }
+            }
+            
+            ans = Math.min(ans, t);
+            
+            time++;
         }
-
-        return ans;
+        
+		// You can never reach the safe house
+        if (grid[m - 1][n - 1] != 2) {
+            return -1;
+        }
+        
+		// you can reach the safe house but fire can not
+        if (times[m - 1][n - 1] == Integer.MAX_VALUE) {
+            return 1000000000;
+        }
+        
+        return ans == Integer.MAX_VALUE || ans == Integer.MIN_VALUE ? -1 : ans;
     }
-
-    static class Pair<T, K, L> {
-        T first;
-        K second;
-        L third;
-
-        public Pair(T first, K second, L third) {
-            this.first = first;
-            this.second = second;
-            this.third = third;
-        }
-    }
-
 }
