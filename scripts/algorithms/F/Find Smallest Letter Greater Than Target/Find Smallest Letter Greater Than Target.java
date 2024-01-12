32c235c0b276bf9a0e1f65b0635ea0081b2f9103
AnasImloul <pydev27@gmail.com>
2024-01-13 00:16:04 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 44.10 MB (Top 77.94%)

--- scripts/algorithms/F/Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.java
+++ scripts/algorithms/F/Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.java
@@ -1,14 +1,28 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 44.10 MB (Top 77.94%)
+
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int start=0,end=letters.length-1;
-    while(start<=end){
-      int mid=start+(end-start)/2;
-      if(letters[mid]>target){    //strictly greater is the solution we want
-        end = mid-1;
-      }else{
-        start=mid+1;
-      }
-    }
-    return letters[start % letters.length];    // this is the wrap around condition , we use modulo %
+        char res=letters[0];
+        int start=0;
+        int end=letters.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(letters[mid]==target)
+            {
+                start=mid+1;
+            }
+            else if(target>letters[mid])
+            {
+                start=mid+1;
+            }
+            else if(letters[mid]>target)
+            {
+                res=letters[mid];
+                end=mid-1;
+            }
+        }
+        return res;
+        
     }
 }
