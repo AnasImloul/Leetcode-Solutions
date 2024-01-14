6db4267f912b602fe16e99359f23a14b90786a13
AnasImloul <pydev27@gmail.com>
2024-01-14 15:11:31 +0100

    Runtime: 7 ms (Top 99.5%) | Memory: 45.00 MB (Top 40.87%)

--- scripts/algorithms/D/Design Parking System/Design Parking System.java
+++ scripts/algorithms/D/Design Parking System/Design Parking System.java
@@ -1,10 +1,20 @@
+// Runtime: 7 ms (Top 99.5%) | Memory: 45.00 MB (Top 40.87%)
+
 class ParkingSystem {
-    private int[] size;
+    private int slots[] = new int[3];
     public ParkingSystem(int big, int medium, int small) {
-        this.size = new int[]{big, medium, small};
+        slots[0] = big;
+        slots[1] = medium;
+        slots[2] = small;
     }
     
     public boolean addCar(int carType) {
-        return size[carType-1]-->0;
+        if(slots[carType-1]>0)
+        {   
+            slots[carType-1]--;
+            return true;
+            
+        }
+        return false;
     }
 }
