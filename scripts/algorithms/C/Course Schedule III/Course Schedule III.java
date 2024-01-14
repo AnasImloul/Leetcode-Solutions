c465b54a6a5aab5c8c564e05d497e6ed4f3cffc3
AnasImloul <pydev27@gmail.com>
2024-01-14 15:06:37 +0100

    Runtime: 32 ms (Top 83.06%) | Memory: 54.60 MB (Top 52.82%)

--- scripts/algorithms/C/Course Schedule III/Course Schedule III.java
+++ scripts/algorithms/C/Course Schedule III/Course Schedule III.java
@@ -1,13 +1,19 @@
+// Runtime: 32 ms (Top 83.06%) | Memory: 54.60 MB (Top 52.82%)
+
 class Solution {
-    public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a,b)->(a[1]-b[1]));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(b - a));
-        int time = 0;
-        for(int[] course: courses){
-            time += course[0];
-            pq.add(course[0]);
-            if(time > course[1])
-                time -= pq.poll();
+    public int scheduleCourse(int[][] C) {
+        Arrays.sort(C, (a,b) -> a[1] - b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        int total = 0;
+        for (int[] course : C) {
+            int dur = course[0], end = course[1];
+            if (dur + total <= end) {
+                total += dur;
+                pq.add(dur);
+            } else if (pq.size() > 0 && pq.peek() > dur) {
+                total += dur - pq.poll();
+                pq.add(dur);
+            }
         }
         return pq.size();
     }
