542bbefa7c4e6d3f27f6885598e191cfba449026
AnasImloul <pydev27@gmail.com>
2024-01-14 22:20:30 +0100

    Runtime: 658 ms (Top 96.88%) | Memory: 18.30 MB (Top 28.13%)

--- scripts/algorithms/F/Find Longest Awesome Substring/Find Longest Awesome Substring.py
+++ scripts/algorithms/F/Find Longest Awesome Substring/Find Longest Awesome Substring.py
@@ -1,16 +1,28 @@
-from collections import defaultdict
+// Runtime: 658 ms (Top 96.88%) | Memory: 18.30 MB (Top 28.13%)
+
 class Solution:
     def longestAwesome(self, s: str) -> int:
-        mask = 0
-        index = defaultdict(lambda:float('-inf'),{0:-1})
-        res = 0
-        for i,c in enumerate(s):
-            mask ^= (1<<(ord(c)-ord('0')))
-            if index[mask] == float('-inf'):
-                index[mask] = i
-            res = max(res, i-index[mask])
-			#when the palindrome has one odd numbers of digits
-            for j in range(10):
-                tmp_mask = mask^(1<<j)
-                res = max(res, i-index[tmp_mask] if index[tmp_mask] != float('-inf') else 0)
-        return res
+        # li = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+        li = [2**i for i in range(10)]
+        # checker = {0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512}
+        checker = set(li)
+        checker.add(0)
+        # di: k = prefix xor, v = the first idx I got a new prefix_xor_value.
+        di = collections.OrderedDict({0: -1})
+        maxLength = prefix_xor = 0
+        
+        for i in range(len(s)):
+            prefix_xor ^= li[int(s[i])]
+            # Found a new prefix_xor_value
+            if prefix_xor not in di:
+                di[prefix_xor] = i
+            
+            # XOR operation with previous prefix_xor_value
+            for key in di.keys():
+                if i - di[key] <= maxLength:
+                    break
+				# s[di[key] : i] is Awesome Substring
+                if key ^ prefix_xor in checker:
+                    maxLength = i - di[key]
+        return maxLength
+            
