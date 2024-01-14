29e2859d47fd1fc0992e90ee3ba7b63ea0a5c498
AnasImloul <pydev27@gmail.com>
2024-01-14 22:23:10 +0100

    Runtime: 32 ms (Top 94.64%) | Memory: 17.30 MB (Top 36.34%)

--- scripts/algorithms/D/Decode the Message/Decode the Message.py
+++ scripts/algorithms/D/Decode the Message/Decode the Message.py
@@ -1,16 +1,18 @@
+// Runtime: 32 ms (Top 94.64%) | Memory: 17.30 MB (Top 36.34%)
+
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-        i=0
-        d={}
-        for j in key:
-            if j!=" " and j not in d:
-                d[j]=alpha[i]
-                i+=1
-            if len(d)==26:
-                break
-        res=""
-        d[" "]=" "
-        for i in message:
-            res+=d[i]
+        mapping = {' ': ' '}
+        i = 0
+        res = ''
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        
+        for char in key:
+            if char not in mapping:
+                mapping[char] = letters[i]
+                i += 1
+        
+        for char in message:
+            res += mapping[char]
+                
         return res
