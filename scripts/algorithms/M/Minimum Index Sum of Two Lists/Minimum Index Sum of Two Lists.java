0217bafd3ac4bb19b9bfca6b9649e15161e8981f
AnasImloul <pydev27@gmail.com>
2024-01-15 15:33:01 +0100

    Runtime: 7 ms (Top 93.08%) | Memory: 45.40 MB (Top 19.13%)

--- scripts/algorithms/M/Minimum Index Sum of Two Lists/Minimum Index Sum of Two Lists.java
+++ scripts/algorithms/M/Minimum Index Sum of Two Lists/Minimum Index Sum of Two Lists.java
@@ -1,23 +1,32 @@
+// Runtime: 7 ms (Top 93.08%) | Memory: 45.40 MB (Top 19.13%)
+
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        List<String> l1 = Arrays.asList(list1);
-        int least = Integer.MAX_VALUE;
-        List<String> returnArray = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < list2.length; i++) {
-           if (l1.contains(list2[i])) {
-               map.put(list2[i], l1.indexOf(list2[i]) + i);
-           }
-        }
-        for (Map.Entry<String, Integer> entry: map.entrySet()){ 
-            if (entry.getValue() <= least) least = entry.getValue();
-        }
-        for (Map.Entry<String, Integer> entry: map.entrySet()){ 
-            if (entry.getValue() == least) returnArray.add(entry.getKey());
+        if (list1.length > list2.length) {
+            return findRestaurant(list2, list1);
         }
         
-        if (returnArray.size() > 1) return returnArray.toArray(String[]::new);
-        return new String[]{returnArray.get(0)};
+        Map<String, Integer> map1 = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            map1.put(list1[i], i);
+        }
+        
+        List<String> mins = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < list2.length; i++) {
+            String rest2 = list2[i];
+            if (map1.containsKey(rest2)) {
+                int sum = map1.get(rest2) + i;
+                if (sum < minSum) {
+                    mins = new ArrayList<>();
+                    minSum = sum;
+                }
+                if (sum == minSum) {
+                    mins.add(rest2);
+                }
+            }
+        }
+        
+        return mins.toArray(new String[mins.size()]);
     }
 }
