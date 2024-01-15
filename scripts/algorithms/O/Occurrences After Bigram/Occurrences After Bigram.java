c870b2265ab3a4ba0fc2614785b5663482486996
AnasImloul <pydev27@gmail.com>
2024-01-15 15:31:41 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 41.60 MB (Top 23.12%)

--- scripts/algorithms/O/Occurrences After Bigram/Occurrences After Bigram.java
+++ scripts/algorithms/O/Occurrences After Bigram/Occurrences After Bigram.java
@@ -1,17 +1,20 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 41.60 MB (Top 23.12%)
+
 class Solution {
     public String[] findOcurrences(String text, String first, String second) {
-        List<String> list = new ArrayList<>();
-        
-        String[] arr = text.split(" ");
-        
-        for(int i = 0; i < arr.length - 2; i++) {
-            if(arr[i].equals(first) && arr[i + 1].equals(second)) {
-                list.add(arr[i + 2]);
-            }
+        String[] st = text.split(" ");
+        List<String> l = new ArrayList<String>();
+        int  i =0,n = st.length;
+
+        while(i<n)
+        {
+            if(st[i].equals(first))
+                if(i+1<n-1 && st[i+1].equals(second))
+                    l.add(st[i+2]);
+            i++;
         }
-        
-        String[] result = list.toArray(new String[0]);
-        
-        return result;
+
+        return l.toArray(new String[0]);
     }
 }
+
