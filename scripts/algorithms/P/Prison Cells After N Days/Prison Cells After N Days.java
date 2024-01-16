79f60b3f83a0c53b5bc3173b98311c1da7651185
AnasImloul <pydev27@gmail.com>
2024-01-16 16:03:43 +0100

    Runtime: 1 ms (Top 89.57%) | Memory: 42.40 MB (Top 48.82%)

--- scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.java
+++ scripts/algorithms/P/Prison Cells After N Days/Prison Cells After N Days.java
@@ -1,25 +1,26 @@
-class Solution {
-    public int[] prisonAfterNDays(int[] cells, int n) {
-        // # Since we have 6 cells moving cells (two wil remain unchaged at anytime) 
-        // # the cycle will restart after 14 iteration
-        // # 1- The number of days if smaller than 14 -> you brute force O(13)
-        // # 2- The number is bigger than 14
-        // #                       - You do a first round of 14 iteration
-        // #                       - Than you do a second round of n%14 iteration
-        // #                       ===> O(27)
-        
-        n = n % 14 == 0 ? 14 : n%14;
-        int temp[] = new int[cells.length];
-        
-        while(n-- > 0)
-        {
-            for(int i=1; i <cells.length- 1; i++)
-            {
-              temp[i] = cells[i-1] == cells[i+1]? 1: 0;
-            }
-            cells = temp.clone();
-        }
+// Runtime: 1 ms (Top 89.57%) | Memory: 42.40 MB (Top 48.82%)
 
-        return cells;
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        if(N==0) return cells;
+        int[][] mem=new int[14][8];            // Repeat pattern after day 14, so Day 1 and Day 15 is equal
+        mem[0][0]=0;
+        mem[0][7]=0;
+        for(int i=1;i<7;i++){                    // calculating Day 1 and insert at 0th position in mem
+            if(cells[i-1]==cells[i+1])
+                mem[0][i]=1;
+            else
+                mem[0][i]=0;
+        }
+        
+        for(int j=1;j<14;j++){                    // calculating Day 2 to 14 and inserting at position 1 to 13 in mem.
+            for(int i=1;i<7;i++){
+                if(mem[j-1][i-1]==mem[j-1][i+1])
+                    mem[j][i]=1;
+                else
+                    mem[j][i]=0;
+            }
+        }
+        return mem[(N-1)%14];                //return the day modulo 14
     }
 }
