22ddecf9bf705134d767c6a769b30d60f2dd409c
AnasImloul <pydev27@gmail.com>
2024-01-16 15:50:29 +0100

    Runtime: 1 ms (Top 90.41%) | Memory: 42.90 MB (Top 8.98%)

--- scripts/algorithms/C/Check If String Is a Prefix of Array/Check If String Is a Prefix of Array.java
+++ scripts/algorithms/C/Check If String Is a Prefix of Array/Check If String Is a Prefix of Array.java
@@ -1,3 +1,5 @@
+// Runtime: 1 ms (Top 90.41%) | Memory: 42.90 MB (Top 8.98%)
+
 class Solution {
     public boolean isPrefixString(String s, String[] words) {
         StringBuilder res = new StringBuilder ("");
