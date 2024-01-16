5c5c9279e10e84b2d5127e08e6493369a801ddce
AnasImloul <pydev27@gmail.com>
2024-01-16 15:15:09 +0100

    Runtime: 184 ms (Top 100.0%) | Memory: 54.40 MB (Top 6.82%)

--- scripts/algorithms/M/Magnetic Force Between Two Balls/Magnetic Force Between Two Balls.js
+++ scripts/algorithms/M/Magnetic Force Between Two Balls/Magnetic Force Between Two Balls.js
@@ -1,36 +1,40 @@
+// Runtime: 184 ms (Top 100.0%) | Memory: 54.40 MB (Top 6.82%)
+
+/**
+ * @param {number[]} position
+ * @param {number} m
+ * @return {number}
+ */
 var maxDistance = function(position, m) {
-    position = position.sort((a, b) => a - b);
+    position.sort((a, b) => a - b);
 
-    function canDistribute(n) {
-        let count = 1;
-        let dist = 0;
-        
-        for(let i = 1; i < position.length; i++) {
-            dist += position[i] - position[i - 1];
-            
-            if (dist >= n) {
-                count++;
-                dist = 0;
-            }
-        }
-        
-        return count >= m;
-    }
-    
-    let left = 1;
-    let right = position[position.length - 1] - position[0];
-    let ans;
-    
+    let left = 1, right = Math.max(...position) - Math.min(...position);
+    max = -1;
     while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-
-        if (canDistribute(mid)) {
-            ans = mid;
-            left = mid + 1;
+        const p = Math.floor((left + right) / 2);
+        if (isValid(position, m, p)) {
+            max = p;
+            left = p + 1;
         } else {
-            right = mid - 1;
+            right = p - 1;
         }
     }
-    
-    return ans;
+
+    return max;   
 };
+ 
+
+const isValid = (sortedPosition, m, p) => {
+    let counter = 1;
+    let prev = sortedPosition[0];
+    for (let i = 1; i < sortedPosition.length; ++i) {
+        if (sortedPosition[i] - prev >= p) {
+            counter++;
+            prev = sortedPosition[i];
+        } 
+    }
+
+    return counter >= m;
+}
+
+
