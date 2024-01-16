cc0c1058b0c64248e0ca2c3f094f546f8763b18f
AnasImloul <pydev27@gmail.com>
2024-01-16 15:30:29 +0100

    Runtime: 26 ms (Top 92.47%) | Memory: 48.80 MB (Top 99.46%)

--- scripts/algorithms/S/Sender With Largest Word Count/Sender With Largest Word Count.java
+++ scripts/algorithms/S/Sender With Largest Word Count/Sender With Largest Word Count.java
@@ -1,25 +1,46 @@
+// Runtime: 26 ms (Top 92.47%) | Memory: 48.80 MB (Top 99.46%)
 
 class Solution {
-    public String largestWordCount(String[] messages, String[] senders) {
-        HashMap<String,Integer> hm=new HashMap<>();
-		
-        int max=0;
-        String name="";
-        for(int i=0;i<messages.length;i++){
-            String[] words=messages[i].split(" ");
+    public String largestWordCount(String[] messages, String[] senders) 
+    {
+        HashMap<String,Integer> map = new HashMap<>();
+        String res = "";int max =0;
+        
+        for(int i=0; i<messages.length;i++)
+        {
+            int words = get_count(messages[i]);
             
-            int freq=hm.getOrDefault(senders[i],0)+words.length;
-            hm.put(senders[i],freq);
-            
-            if(hm.get(senders[i])>max){
-                max=hm.get(senders[i]);
-                name=senders[i];
-            }
-            else if(hm.get(senders[i])==max && name.compareTo(senders[i])<0){
-                name=senders[i];
-            } 
+            if(!map.containsKey(senders[i]))
+               map.put(senders[i] , words);
+               
+            else
+               map.put(senders[i],map.get(senders[i]) + words);
         }
         
-        return name;
+        for(String s: map.keySet())
+        {
+            if(map.get(s) > max)
+            {
+                res = s;
+                max = map.get(s);
+            }
+            
+            if(map.get(s) == max && res.compareTo(s) < 0)
+                res = s;
+        }
+        return res;
+    }
+    
+    private int get_count(String s)
+    {
+        int spaces = 0;
+        
+        for(int i=0; i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == ' ')
+                spaces++;
+        }
+        return spaces+1;
     }
 }
