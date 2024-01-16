394192e0067870457b87fcdd7695c66ff2caa735
AnasImloul <pydev27@gmail.com>
2024-01-16 15:16:21 +0100

    Runtime: 68 ms (Top 96.23%) | Memory: 44.50 MB (Top 96.23%)

--- scripts/algorithms/M/Maximum Length of a Concatenated String with Unique Characters/Maximum Length of a Concatenated String with Unique Characters.js
+++ scripts/algorithms/M/Maximum Length of a Concatenated String with Unique Characters/Maximum Length of a Concatenated String with Unique Characters.js
@@ -1,41 +1,35 @@
+// Runtime: 68 ms (Top 96.23%) | Memory: 44.50 MB (Top 96.23%)
+
 var maxLength = function(arr) {
-    const bits = [];
-    for(let word of arr) {
-        let b = 0, flag = true;
-        for(let c of word) {
-            const idx = c.charCodeAt(0) - 'a'.charCodeAt(0);
-            const setBit = (1 << idx);
-            if((b & setBit) != 0) {
-                flag = false;
-                break;
-            }
-            b = (b ^ setBit);
+    const n = arr.length;
+    let maxLen = 0;
+    
+    backtrack(0, "");
+    
+    return maxLen;
+    
+    function backtrack(start, prevStr) {
+        maxLen = Math.max(maxLen, prevStr.length);
+        
+        if (start === n) return; 
+        
+        for (let i = start; i < n; i++) {
+            const word = arr[i];
+            
+            if (isUnique(prevStr + word)) backtrack(i + 1, prevStr + word);
         }
-        if(flag) bits.push(b);
     }
     
-    const len = bits.length;
-    const dp = new Map();
-    const solve = (i = 0, b = 0) => {
-        if(i == len) {
-            let c = 0;
-            while(b > 0) {
-                c += (b & 1);
-                b >>= 1;
-            }
-            return c;
+    function isUnique(str) {
+        let bits = 0 | 0;
+
+        for (const char of str) {
+            const index = char.charCodeAt(0) - 97;
+            
+            if (bits & (1 << index)) return false;
+            bits |= (1 << index);
         }
-        const key = [i, b].join(':');
-        if(dp.has(key)) return dp.get(key);
         
-        let ans = 0;
-        if((b & bits[i]) == 0) {
-            // take
-            ans = Math.max(ans, solve(i + 1, b | bits[i]));
-        }
-        ans = Math.max(ans, solve(i + 1, b));
-        dp.set(key, ans);
-        return ans;
+        return true;
     }
-    return solve();
 };
