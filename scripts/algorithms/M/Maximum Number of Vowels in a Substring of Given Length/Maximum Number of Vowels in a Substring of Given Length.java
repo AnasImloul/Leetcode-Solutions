efe276036c5ae5cd568908fb9c9cac02b1c3d082
AnasImloul <pydev27@gmail.com>
2024-01-16 16:07:30 +0100

    Runtime: 7 ms (Top 97.98%) | Memory: 45.20 MB (Top 9.52%)

--- scripts/algorithms/M/Maximum Number of Vowels in a Substring of Given Length/Maximum Number of Vowels in a Substring of Given Length.java
+++ scripts/algorithms/M/Maximum Number of Vowels in a Substring of Given Length/Maximum Number of Vowels in a Substring of Given Length.java
@@ -1,24 +1,32 @@
+// Runtime: 7 ms (Top 97.98%) | Memory: 45.20 MB (Top 9.52%)
+
 class Solution {
     public int maxVowels(String s, int k) {
-        int max=0,cnt=0;
-        for(int i=0,j=0;j<s.length();j++){
-            if(j-i+1<k){
-                if(s.charAt(j)=='a' ||s.charAt(j)=='e' || s.charAt(j)=='i' || s.charAt(j)=='o' || s.charAt(j)=='u' ){
-                    cnt++;
-                }
-            }
-            else{
-                if(s.charAt(j)=='a' ||s.charAt(j)=='e' || s.charAt(j)=='i' || s.charAt(j)=='o' || s.charAt(j)=='u'){
-                    cnt++;
-                }
-                max=Math.max(max,cnt);
-                if(s.charAt(i)=='a' ||s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
-                    cnt--;
-                }
-                i++;
-            }
-            
+        int n = s.length();
+        int maxVowels = 0;
+        int count = 0;
+
+        int[] vowels = new int[128];
+        vowels['a'] = 1;
+        vowels['e'] = 1;
+        vowels['i'] = 1;
+        vowels['o'] = 1;
+        vowels['u'] = 1;
+
+        for (int i = 0; i < k; i++) {
+            count += vowels[s.charAt(i)];
         }
-        return max;
+
+        maxVowels = count;
+        for (int i = k; i < n; i++) {
+            count += vowels[s.charAt(i)] - vowels[s.charAt(i - k)];
+            maxVowels = Math.max(maxVowels, count);
+            //System.out.println(Arrays.toString(vowels));
+            if (maxVowels == k) {
+                return maxVowels; 
+            }
+        }
+        return maxVowels;
     }
 }
+
