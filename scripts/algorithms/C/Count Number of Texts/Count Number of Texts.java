26588be6d60d609347793f58a3bce30888b434d6
AnasImloul <pydev27@gmail.com>
2024-01-16 16:13:02 +0100

    Runtime: 19 ms (Top 90.12%) | Memory: 45.20 MB (Top 64.2%)

--- scripts/algorithms/C/Count Number of Texts/Count Number of Texts.java
+++ scripts/algorithms/C/Count Number of Texts/Count Number of Texts.java
@@ -1,23 +1,24 @@
+// Runtime: 19 ms (Top 90.12%) | Memory: 45.20 MB (Top 64.2%)
+
 class Solution {
     int mod = (1000000007);
 
     public int countTexts(String pressedKeys) {
         int[] key = new int[] { 0, 0, 3, 3, 3, 3, 3, 4, 3, 4 };
         int n = pressedKeys.length();
-        return solve(0,pressedKeys,key);
-    }
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
 
-    public int solve(int ind, String s, int[] key) {
-        if (ind == s.length()) {
-            return 1;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int count = 0;
+            int num = pressedKeys.charAt(ind) - '0';
+            int rep = key[num];
+            for (int i = 0; i < rep && ind + i < pressedKeys.length() && pressedKeys.charAt(ind) == pressedKeys.charAt(ind + i); i++) {
+                count += dp[ind+i+1];
+                count %= mod;
+            }
+             dp[ind] = count;
         }
-        int count = 0;
-        int num = s.charAt(ind) - '0';
-        int rep = key[num];
-        for (int i = 0; i < rep && ind + i < s.length() && s.charAt(ind) == s.charAt(ind + i); i++) {
-            count += solve(ind + 1 + i, s, key);
-            count %= mod;
-        }
-        return  count;
+        return dp[0];
     }
 }
