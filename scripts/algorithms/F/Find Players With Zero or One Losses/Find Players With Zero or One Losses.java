a745548cb1d5a156c369902f892978a97f242ef8
AnasImloul <pydev27@gmail.com>
2024-01-16 15:33:50 +0100

    Runtime: 22 ms (Top 99.73%) | Memory: 91.90 MB (Top 61.98%)

--- scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.java
+++ scripts/algorithms/F/Find Players With Zero or One Losses/Find Players With Zero or One Losses.java
@@ -1,34 +1,40 @@
+// Runtime: 22 ms (Top 99.73%) | Memory: 91.90 MB (Top 61.98%)
+
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        int[] won = new int[100001];
-        int[] loss = new int[100001];
-        
-        for(int[] match : matches) {
-            won[match[0]]++;
-            loss[match[1]]++;
-        }
-        
-        // System.out.print(Arrays.toString(won));
-        // System.out.print(Arrays.toString(loss));
-        
-        List<List<Integer>> ans  = new ArrayList<>();
-        
-        List<Integer> wonAllMatches = new ArrayList<>();
-        List<Integer> lostOneMatch = new ArrayList<>();
-        
-        for(int i = 0; i < won.length; i++) {
-            if(won[i] > 0 && loss[i] == 0) { //for checking players that have not lost any match.
-                wonAllMatches.add(i);
-            }
-            
-            if(loss[i] == 1) {
-                lostOneMatch.add(i); //for checking players that have lost exactly one match.
+        int[] losses = new int[100001];
+
+        for (int i = 0; i < matches.length; i++) {
+            int win = matches[i][0];
+            int loss = matches[i][1];
+
+            if (losses[win] == 0) {
+                losses[win] = -1;
+            } 
+
+            if (losses[loss] == -1) {
+                losses[loss] = 1;
+            } else {
+                losses[loss]++;
             }
         }
-        
-        ans.add(wonAllMatches);
-        ans.add(lostOneMatch);
-        
-        return ans;
+
+        List<Integer> zeroLoss = new ArrayList<>();
+        List<Integer> oneLoss = new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < losses.length; i++) {
+            if (losses[i] == -1) {
+                zeroLoss.add(i);
+            } else if (losses[i] == 1) {
+                oneLoss.add(i);
+            }
+        }
+
+        result.add(zeroLoss);
+        result.add(oneLoss);
+
+        return result;
     }
 }
