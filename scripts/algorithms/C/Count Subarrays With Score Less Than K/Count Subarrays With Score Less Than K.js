b2fa7708c54dee70d19f4c82211b33ace92097fb
AnasImloul <pydev27@gmail.com>
2024-01-16 15:04:51 +0100

    Runtime: 81 ms (Top 78.57%) | Memory: 53.40 MB (Top 28.57%)

--- scripts/algorithms/C/Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.js
+++ scripts/algorithms/C/Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.js
@@ -1,19 +1,12 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var countSubarrays = function(nums, k) 
-{
-    let count=0;
-    let j=0;
-    let sum=0;
-    for(let i=0;i<nums.length;i++){
-        sum+=nums[i];
-        while(sum*(i-j+1)>=k){
-            sum-=nums[j++];
-        }
-        count+=(i-j+1);
-    }
-    return count;
+// Runtime: 81 ms (Top 78.57%) | Memory: 53.40 MB (Top 28.57%)
+
+var countSubarrays = function(nums, k) {
+  let curentRunningSum = 0, result = 0, windowEnd = 0, windowStart = 0;
+  while (windowEnd < nums.length) {
+    curentRunningSum += nums[windowEnd];
+    while (curentRunningSum * (windowEnd - windowStart + 1) >= k)  curentRunningSum -= nums[windowStart++];
+    result += windowEnd - windowStart + 1;
+    windowEnd++;
+  }
+  return result; 
 };
