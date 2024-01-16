885f7c76b959f4d7f8e9f17413938f8f4183f316
AnasImloul <pydev27@gmail.com>
2024-01-16 15:00:14 +0100

    Runtime: 53 ms (Top 94.39%) | Memory: 47.10 MB (Top 9.35%)

--- scripts/algorithms/P/Pairs of Songs With Total Durations Divisible by 60/Pairs of Songs With Total Durations Divisible by 60.js
+++ scripts/algorithms/P/Pairs of Songs With Total Durations Divisible by 60/Pairs of Songs With Total Durations Divisible by 60.js
@@ -1,34 +1,18 @@
-/**
- * @param {number[]} time
- * @return {number}
- */
+// Runtime: 53 ms (Top 94.39%) | Memory: 47.10 MB (Top 9.35%)
+
 var numPairsDivisibleBy60 = function(time) {
-    let result = 0;
-    let seen = new Map();
-    
-    
-    for(let t of time){
-        let target = 60 - (t % 60 );
+    let num=0;
+    let arr=new Array(60).fill(0);
+
+    for(let i of time){
+        let k=i%60;
+
+        if(k===0)
+            num+=arr[k];
+        else
+            num+=arr[60-k];
         
-		// check the count of the target time to make the current time[i] divisble by 60
-        // The catch here is to treat 60 as 0 in the given formula : target = 60 - (time % 60)
-        let targetCount = seen.get(target === 60 ? 0 : target);
-        
-		// if the target exists in the map, increment the result by the count
-        if(targetCount !== undefined){
-            result += targetCount;
-        }
-       
-		// below steps are simply to check if the time[i] % 60 exists in map or not 
-		// and increment the count 
-        let count = seen.get(t % 60);
-        
-        if(count === undefined){
-            seen.set(t % 60, 1);
-        } else {
-            seen.set(t % 60, count + 1);
-        }
+        arr[k]++;
     }
-    
-    return result;
+    return num;
 };
