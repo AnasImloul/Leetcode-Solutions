f547871ac439e179425210bde877fcdafcda4035
AnasImloul <pydev27@gmail.com>
2024-01-16 15:01:29 +0100

    Runtime: 53 ms (Top 100.0%) | Memory: 47.90 MB (Top 57.14%)

--- scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.js
+++ scripts/algorithms/N/Number of Subarrays with Bounded Maximum/Number of Subarrays with Bounded Maximum.js
@@ -1,24 +1,13 @@
-  var numSubarrayBoundedMax = function(nums, left, right) {
-        // si is start index
-        // ei is end index
-    let si=0, ei=0, finalCount=0, currentCount=0;
+// Runtime: 53 ms (Top 100.0%) | Memory: 47.90 MB (Top 57.14%)
 
-    while(ei<nums.length){ // moving ei till length of array
-        if(left<=nums[ei] && nums[ei]<=right) // considering case number falls in the range
-            {
-                currentCount= ei-si+1;  // to get the subarrays b/w start index and end index
-                finalCount+=currentCount; // add current count of subarrays to final count  which we will return at end
-            }
-        if(nums[ei]<left){
-              finalCount+=currentCount;  // in this case when num is smaller than left range we won't calculate subarrays but just include the current count in final count.
-        }
-         if(nums[ei]>right){
-              si=ei+1; // in this case when num is greater than right range we need to move our start index to end+1 because there won't be any subarrays that will fall in range.
-             currentCount=0; // we will set current count 0 in such case.
-        }
-
-        ei++;
-     }
-    return finalCount;
-
-    };
+var numSubarrayBoundedMax = function(nums, left, right) {
+    let ans = 0, low = 0, mid = 0
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i]
+        if (num > right) mid = 0
+        else ans += ++mid
+        if (num >= left) low = 0
+        else ans -= ++low
+    }
+    return ans
+};
