d8e43c453b2667d4bc6dfbed35e9c55d861c0e00
AnasImloul <pydev27@gmail.com>
2024-01-16 14:56:24 +0100

    Runtime: 98 ms (Top 67.73%) | Memory: 65.80 MB (Top 42.93%)

--- scripts/algorithms/C/Continuous Subarray Sum/Continuous Subarray Sum.js
+++ scripts/algorithms/C/Continuous Subarray Sum/Continuous Subarray Sum.js
@@ -1,15 +1,27 @@
+// Runtime: 98 ms (Top 67.73%) | Memory: 65.80 MB (Top 42.93%)
+
 var checkSubarraySum = function(nums, k) {
-	const hash = new Map([[0, -1]]);
-	let sum = 0;
-
-	for (let index = 0; index < nums.length; index++) {
-		sum += nums[index];
-		const r = sum % k;
-
-		if (hash.has(r)) {
-			if (index - hash.get(r) > 1) return true;
-		}
-		else hash.set(r, index);
-	}
-	return false;
-};
+    const n = nums.length;
+    if(n < 2) {
+        return false;
+    }
+    const map = new Map();
+	
+	// edge case - to handle sum % k === 0 and
+	// if the first element of array is a multiple of k, condition 0 - (-1) = 1 > 1 wil fail (min length of subarray must be 2)
+    map.set(0, -1);
+    let sum = 0;
+    
+    for(let i = 0; i < n; ++i) {
+        sum += nums[i];
+        sum = sum % k;
+		
+		// handles min length of subarray must be 2
+        if(map.has(sum) && i - map.get(sum) > 1) {
+            return true;
+        } else if(!map.has(sum)) {
+            map.set(sum, i);
+        }
+    }
+    return false;
+}
