ef32ee1d411e2b50dec70105fd5e061f924dffd7
AnasImloul <pydev27@gmail.com>
2024-01-16 16:15:21 +0100

    Runtime: 1 ms (Top 94.74%) | Memory: 41.50 MB (Top 35.64%)

--- scripts/algorithms/V/Validate IP Address/Validate IP Address.java
+++ scripts/algorithms/V/Validate IP Address/Validate IP Address.java
@@ -1,14 +1,63 @@
+// Runtime: 1 ms (Top 94.74%) | Memory: 41.50 MB (Top 35.64%)
+
 class Solution {
-    public String validIPAddress(String queryIP) {
-        String regexIpv4 = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+    public String validIPAddress(String IP) {
+        if(IP.length()==0) return "Neither";
         
-        String regexIpv6 = "((([0-9a-fA-F]){1,4})\\:){7}(([0-9a-fA-F]){1,4})";
+        if(IP.indexOf(".")>=0) return validateIPV4(IP);
         
-        if(queryIP.matches(regexIpv4))
-            return "IPv4";
-        else if(queryIP.matches(regexIpv6))
-            return "IPv6";
-        else
-            return "Neither";
+        if(IP.indexOf(":")>=0) return validateIPV6(IP);
+        
+        return "Neither";
     }
-}
+    
+    private  String validateIPV4(String ip){
+	   // step 1 
+        if(ip.charAt(0)=='.' || ip.charAt(ip.length()-1)=='.') return "Neither";
+           
+		  //step 2 
+          String[] component=ip.split("\\.");
+          
+		  //step 3
+           if(component.length!=4) return "Neither";
+           
+		   //step 4
+           for(String comp:component){
+             if(comp.length()==0 || comp.length()>3 || (comp.charAt(0)=='0' && comp.length()>1)){
+                 return "Neither";
+             }
+               
+			   //step5
+              for(char ch:comp.toCharArray()){
+                  if(ch<'0' || ch>'9') return "Neither";
+              }
+               
+			   //step6
+              int num=Integer.parseInt(comp);
+              if(num<0 || num>255) return "Neither";
+               
+           }
+           
+           return "IPv4";
+           }
+           
+   private String validateIPV6(String ip){
+     if(ip.charAt(0)==':' || ip.charAt(ip.length()-1)==':') return "Neither";
+       
+       String[] component=ip.split(":");
+       
+       if(component.length!=8) return "Neither";
+       
+       for(String comp:component){
+       if(comp.length()==0 || comp.length()>4) return "Neither";
+           
+           
+           for(char ch:comp.toLowerCase().toCharArray()){
+             if((ch<'0' || ch>'9') && (ch!='a' && ch!='b' && ch!='c' && ch!='d' && ch!='e' && ch!='f')){
+                 return "Neither";
+             }  
+           }
+       }
+       return "IPv6";
+     }
+ }
