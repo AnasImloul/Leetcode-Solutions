5a0178a44bf1929a7fb442860a6b10b605873914
AnasImloul <pydev27@gmail.com>
2024-01-16 14:45:25 +0100

    Runtime: 57 ms (Top 84.32%) | Memory: 44.70 MB (Top 40.04%)

--- scripts/algorithms/O/Odd Even Linked List/Odd Even Linked List.js
+++ scripts/algorithms/O/Odd Even Linked List/Odd Even Linked List.js
@@ -1,3 +1,5 @@
+// Runtime: 57 ms (Top 84.32%) | Memory: 44.70 MB (Top 40.04%)
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -10,19 +12,17 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    if (!head || !head.next) return head;
-    let oddNode = head;
-    const evenHead = head.next;
-    let evenNode = head.next;
     
-    while(evenNode?.next) {
-        oddNode.next = evenNode.next;
-        oddNode = oddNode.next;
-        evenNode.next = oddNode.next;
-        evenNode = evenNode.next;
-    }
-    
-    oddNode.next = evenHead;
-    
-    return head;
+  if (!head || !head.next) return head;
+
+  let odd = head, even = head.next, firstEven = even;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = firstEven;
+  return head;
+
 };
