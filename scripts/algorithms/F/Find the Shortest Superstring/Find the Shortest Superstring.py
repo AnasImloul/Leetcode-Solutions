6b5cee159f3ebdb88c519842f97739ca4f7b7dff
AnasImloul <pydev27@gmail.com>
2024-01-17 16:59:42 +0100

    Runtime: 551 ms (Top 76.67%) | Memory: 73.00 MB (Top 16.67%)

--- scripts/algorithms/F/Find the Shortest Superstring/Find the Shortest Superstring.py
+++ scripts/algorithms/F/Find the Shortest Superstring/Find the Shortest Superstring.py
@@ -1,14 +1,36 @@
+// Runtime: 551 ms (Top 76.67%) | Memory: 73.00 MB (Top 16.67%)
+
 class Solution:
-    def shortestSuperstring(self, A):
-        @lru_cache(None)
-        def suffix(i,j):
-            for k in range(min(len(A[i]),len(A[j])),0,-1):
-                if A[j].startswith(A[i][-k:]):
-                    return A[j][k:]
-            return A[j]
-        n = len(A)
-        @lru_cache(None)
-        def dfs(bitmask, i):
-            if bitmask + 1 == 1 << n: return ""
-            return min([suffix(i, j)+dfs(bitmask | 1<<j, j) for j in range(n) if not(bitmask & 1<<j)], key = len)
-        return min([A[i]+dfs(1<<i,i) for i in range(n)], key = len)
+    def shortestSuperstring(self, words: List[str]) -> str:
+        n = len(words)
+        graph = [[0]*n for _ in range(n)] # graph as adjacency matrix 
+        
+        for i in range(n):
+            for j in range(n): 
+                if i != j: 
+                    for k in range(len(words[j])): 
+                        if words[i].endswith(words[j][:k]): 
+                            graph[i][j] = len(words[j]) - k 
+                            
+        @cache
+        def fn(prev, mask): 
+            """Return length of shortest superstring & current choice of word."""
+            if mask == 0: return 0, None
+            vv, kk = inf, None
+            for k in range(n): 
+                if mask & 1<<k: 
+                    v, _ = fn(k, mask ^ 1<<k)
+                    offset = len(words[k]) if prev == -1 else graph[prev][k]
+                    if v + offset < vv: vv, kk = v + offset, k
+            return vv, kk
+        
+        ans = []
+        prev = -1 
+        mask = (1<<n) - 1
+        while mask: 
+            _, k = fn(prev, mask)
+            if ans: ans.append(words[k][-graph[prev][k]:])
+            else: ans.append(words[k])
+            prev = k
+            mask ^= 1<<k 
+        return "".join(ans)
