fbce4b4b9cbb71bb69106f8bc1191ade11b4431e
AnasImloul <pydev27@gmail.com>
2024-01-17 17:24:29 +0100

    Runtime: 825 ms (Top 92.58%) | Memory: 31.60 MB (Top 21.29%)

--- scripts/algorithms/M/Minimum Operations to Reduce X to Zero/Minimum Operations to Reduce X to Zero.py
+++ scripts/algorithms/M/Minimum Operations to Reduce X to Zero/Minimum Operations to Reduce X to Zero.py
@@ -1,24 +1,20 @@
+// Runtime: 825 ms (Top 92.58%) | Memory: 31.60 MB (Top 21.29%)
+
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        start =0
-        end = len(nums)-1
-        totalsum = sum(nums)
-        check = 0
-        windowlen =0
-        s = totalsum - x # s in case1 = 11-5 =6
-        if totalsum<x:
-            return -1
-        if sum(nums) ==x:
-            return len(nums)
-
-        for end in range(len(nums)):
-            check += nums[end]
-            while check > s:
-                check -= nums[start]
-                start +=1
-            if check ==s:
-                windowlen = max(windowlen,end-start+1)
-        if windowlen !=0:
-            return len(nums)-windowlen
-        else:
-            return -1        
+        target, n = sum(nums) - x, len(nums)
+        
+        if target == 0:
+            return n
+        
+        max_len = cur_sum = left = 0
+        
+        for right, val in enumerate(nums):
+            cur_sum += val
+            while left <= right and cur_sum > target:
+                cur_sum -= nums[left]
+                left += 1
+            if cur_sum == target:
+                max_len = max(max_len, right - left + 1)
+        
+        return n - max_len if max_len else -1
