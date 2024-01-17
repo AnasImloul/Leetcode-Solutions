bdafedc5c80bef9e44b7a462ceb2fdc933792379
AnasImloul <pydev27@gmail.com>
2024-01-17 17:13:59 +0100

    Runtime: 34 ms (Top 84.4%) | Memory: 17.50 MB (Top 12.64%)

--- scripts/algorithms/G/Get Maximum in Generated Array/Get Maximum in Generated Array.py
+++ scripts/algorithms/G/Get Maximum in Generated Array/Get Maximum in Generated Array.py
@@ -1,8 +1,18 @@
+// Runtime: 34 ms (Top 84.4%) | Memory: 17.50 MB (Top 12.64%)
+
+from queue import deque
+
 class Solution:
-    def getMaximumGenerated(self, n):
-        nums = [0]*(n+2)
-        nums[1] = 1
-        for i in range(2, n+1):
-            nums[i] = nums[i//2] + nums[(i//2)+1] * (i%2)
-    
-        return max(nums[:n+1])
+    def getMaximumGenerated(self, n: int) -> int:
+        
+        def arrgen(n):
+            yield 0
+            q = deque([1])
+            while True:
+                yield q[0]
+                q.append(q[0])
+                q.append(q[0]+q[1])
+                q.popleft()
+                
+        g = arrgen(n)
+        return max(next(g) for _ in range(n+1))
