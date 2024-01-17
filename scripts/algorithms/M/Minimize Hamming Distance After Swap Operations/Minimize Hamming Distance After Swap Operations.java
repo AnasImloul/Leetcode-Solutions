391f9c67ecaad7ef3e00e2a1dfb9b3c215e8c95d
AnasImloul <pydev27@gmail.com>
2024-01-17 17:55:51 +0100

    Runtime: 36 ms (Top 98.25%) | Memory: 87.50 MB (Top 31.58%)

--- scripts/algorithms/M/Minimize Hamming Distance After Swap Operations/Minimize Hamming Distance After Swap Operations.java
+++ scripts/algorithms/M/Minimize Hamming Distance After Swap Operations/Minimize Hamming Distance After Swap Operations.java
@@ -1,83 +1,55 @@
+// Runtime: 36 ms (Top 98.25%) | Memory: 87.50 MB (Top 31.58%)
+
 class Solution {
     public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
-        int minHamming = 0;
-        UnionFind uf = new UnionFind(source.length);
-        for (int [] swap : allowedSwaps) {
-            int firstIndex = swap[0];
-            int secondIndex = swap[1];
-           // int firstParent = uf.find(firstIndex);
-           // int secondParent = uf.find(secondIndex);
-           // if (firstParent != secondParent)
-           //     uf.parent[firstParent] = secondParent;
-		   uf.union(firstIndex, secondIndex);
+        int[] line = new int[source.length];
+        Arrays.fill(line, 100001);
+        for (int[] swap : allowedSwaps) {
+            int root0 = getRoot(line, swap[0]);
+            int root1 = getRoot(line, swap[1]);
+            int min = Integer.min(root0, root1);
+            line[root0] = min;
+            line[root1] = min;
+            // for speed up
+            line[swap[0]] = min;
+            line[swap[1]] = min;
         }
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
-        for (int i=0; i<source.length; i++) {
-            int num = source[i];
-            int root = uf.find(i);
-            map.putIfAbsent(root, new HashMap<>());
-            Map<Integer, Integer> store = map.get(root);
-            store.put(num, store.getOrDefault(num, 0) + 1);
+        HashMap<Integer, HashMap<Integer, Integer>> pool = new HashMap<>();
+        int different = 0;
+        int noSwap = 0;
+        for (int i = 0; i < line.length; i++) {
+            if (line[i] == 100001) {
+                if (source[i] != target[i]) {
+                    // not allow swap distance
+                    noSwap++;
+                }
+                continue;
+            }
+            Integer root = getRoot(line, i);
+            if (!pool.containsKey(root)) {
+                pool.put(root, new HashMap<>());
+            }
+            HashMap<Integer, Integer> freq = pool.get(root);
+            freq.put(source[i], freq.getOrDefault(source[i], 0)+1);
+            freq.put(target[i], freq.getOrDefault(target[i], 0)-1);
         }
-        for (int i=0; i<source.length; i++) {
-            int num = target[i];
-            int root = uf.find(i);
-            Map<Integer, Integer> store = map.get(root);
-            if (store.getOrDefault(num, 0) == 0)
-                minHamming += 1;
-            else
-                store.put(num, store.get(num) - 1);
+        for (HashMap<Integer, Integer> freq : pool.values()) {
+            for (Integer value : freq.values()) {
+                different += Math.abs(value);
+            }
         }
-        return minHamming;
-    }
-}
-
-class UnionFind {
-    int size;
-    int components;
-    int [] parent;
-    int [] rank;
-    UnionFind(int n) {
-        if (n <= 0) throw new IllegalArgumentException("Size <= 0 is not allowed");
-        size = n;
-        components = n;
-        parent = new int [n];
-        rank = new int [n];
-        for (int i=0; i<n; i++)
-            parent[i] = i;
+        // two different number mean one hamming distance
+        return different/2+noSwap;
     }
     
-    public int find(int p) {
-        while (p != parent[p]) {
-            parent[p] = parent[parent[p]];
-            p = parent[p];
+    private int getRoot(int[] line, int index) {
+        if (line[index] == 100001) {
+            line[index] = index;
+            return index;
         }
-        return p;
-    }
-    
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rank[rootQ] > rank[rootP]) {
-            parent[rootP] = rootQ;
+        if (line[index] == index) {
+            return index;
         }
-        else {
-            parent[rootQ] = rootP;
-            if (rank[rootQ] == rank[rootP])
-                rank[rootP] += 1;
-        }
-        components -= 1;
-    }
-    
-    public int size() {
-        return size;
-    }
-    
-    public boolean isConnected(int p, int q) {
-        return find(p) == find(q);
-    }
-    
-    public int numberComponents() {
-        return components;
+        return getRoot(line, line[index]);
     }
 }
