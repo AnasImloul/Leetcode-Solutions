37ef9635b0537c338d02f6bc14b7b2171b8c2dd9
AnasImloul <pydev27@gmail.com>
2024-01-17 17:57:31 +0100

    Runtime: 15 ms (Top 84.55%) | Memory: 49.70 MB (Top 5.99%)

--- scripts/algorithms/D/Degree of an Array/Degree of an Array.java
+++ scripts/algorithms/D/Degree of an Array/Degree of an Array.java
@@ -1,34 +1,47 @@
+// Runtime: 15 ms (Top 84.55%) | Memory: 49.70 MB (Top 5.99%)
+
 class Solution {
     public int findShortestSubArray(int[] nums) {
-       int ans = Integer.MAX_VALUE;
-        Map<Integer, Integer> count = new HashMap<>();
-        Map<Integer, Integer> startIndex = new HashMap<>();
-        Map<Integer, Integer> endIndex = new HashMap<>();
+        // The int is an array of [firstEncounter, lastEncounter, frequency]
+        HashMap<Integer, int[]> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
 
-        for (int i = 0; i < nums.length; i++) {
-            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
-        }
+            // If the key does not exist in the map, we put it with the first encounter and last encounter set to the current position, 'i', and the freqency 1
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], new int[]{i, i, 1});
+            } 
 
-        for (int i = 0; i < nums.length; i++) {
-            int no = nums[i];
-            if (!startIndex.containsKey(no)) {
-                startIndex.put(no, i);
-            }
-            endIndex.put(no, i);
-        }
-
-        int degree = Integer.MIN_VALUE;
-        for (Integer key : count.keySet()) {
-            degree = Math.max(degree, count.get(key));
-        }
-
-        for (Integer key : count.keySet()) {
-            if (count.get(key) == degree) {
-                int arraySize = endIndex.get(key) - startIndex.get(key) + 1;
-                ans = Math.min(ans, arraySize);
+            // If it does exist, we update the last encounter to the current position and we increase the frequency by 1
+            else {
+                int[] arr = map.get(nums[i]);
+                arr[1] = i;
+                arr[2]++;
+                map.put(nums[i], arr);
             }
         }
+        // Maximim frequency
+        int maxFreq = Integer.MIN_VALUE;
 
-        return ans; 
+        // Minimum distance
+        int minDist = Integer.MAX_VALUE;
+
+        // Going through all the values of the HashMap
+        for(int[] value : map.values()){
+            // value[0] = the first encounter index
+            // value[1] = the last encounter index
+            // value[2] = frequency
+
+            // If the frecuency is greater than the maxFreq, then we update it and also set the minDist
+            if(value[2] > maxFreq){
+                maxFreq = value[2];
+                minDist = value[1] - value[0] + 1;
+            } 
+
+            // If the frecuency is equal to the current max, we take the minimum between the exiting minDist and the minimum distance for the current value
+            else if(value[2] == maxFreq){
+                minDist = Math.min(minDist, value[1] - value[0] + 1);
+            }
+        }
+        return minDist;
     }
 }
