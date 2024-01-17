eba820e3143befd14c28f1f1c53ca0b0c4a885c8
AnasImloul <pydev27@gmail.com>
2024-01-17 17:45:41 +0100

    Runtime: 7 ms (Top 78.37%) | Memory: 45.40 MB (Top 34.13%)

--- scripts/algorithms/C/Count Submatrices With All Ones/Count Submatrices With All Ones.java
+++ scripts/algorithms/C/Count Submatrices With All Ones/Count Submatrices With All Ones.java
@@ -1,54 +1,39 @@
+// Runtime: 7 ms (Top 78.37%) | Memory: 45.40 MB (Top 34.13%)
+
 class Solution {
-	public int numSubmat(int[][] mat) {
-		int n = mat.length;
-		int m = mat[0].length;
-		//left[i][j] indicates how many consecutive 1s are on the left of i, j
-		//left[i][j] indicates how many consecutive 1s are above i, j
-		//dp[i][j] represents the number of submatrices with i, j as the lower right corner
-		int[][] left = new int[n][m];
-		int[][] up = new int[n][m];
-		int[][] dp = new int[n][m];
-		up[0][0] = mat[0][0];
-		left[0][0] = mat[0][0];
-		dp[0][0] = mat[0][0];
-		for(int i = 1; i < n; i++){
-			if(mat[i][0] == 0){
-				continue;
-			}
-			up[i][0] = mat[i][0];
-			left[i][0] = left[i - 1][0] + 1;
-			dp[i][0] = left[i][0];
-		}
+    private int n;
+    private int res = 0;
+    
+    public int numSubmat(int[][] mat) {
+        this.n = mat[0].length;
+        
+        // dp[j] : the height (number of consecutive '1's) of column j 
+        int[] dp = new int[n];
+        for (int i = 0; i < mat.length; i++) {
+            // calculating (updating) heights
+            for (int j = 0; j < n; j++) {
+                dp[j] = mat[i][j] == 1 ? dp[j] + 1 : 0;
+            }
+            enumerateRowByMinHeight(dp);
+        }
+        return res;
+    }
 
-		for(int j = 1; j < m; j++){
-			if(mat[0][j] == 0){
-				continue;
-			}
-			up[0][j] = up[0][j - 1] + 1;
-			left[0][j] = mat[0][j];
-			dp[0][j] = up[0][j];
-		}
+    public void enumerateRowByMinHeight(int[] dp) {
+        // monotonic stack storing indices : for index p < q in stack, dp[p] < dp[q]
+        Deque<Integer> stack = new LinkedList<>();
+        stack.offerLast(-1);
 
-		for(int i = 1; i < n; i++){
-			for(int j = 1; j < m; j++){
-				if(mat[i][j] == 0){
-					continue;
-				}
-				up[i][j] = up[i][j - 1] + 1;
-				left[i][j] = left[i - 1][j] + 1;
-				int minHeight = up[i][j];
-				for(int k = 0; k < left[i][j]; k++){
-					minHeight = Math.min(minHeight, up[i - k][j]);
-					dp[i][j] += minHeight;
-				}
-			}
-		}
-		int res = 0;
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				res += dp[i][j];
-			}
-		}
-		return res;
-	}
+        for (int j = 0; j < n; j++) {
+            while (stack.peekLast() != -1 && dp[stack.peekLast()] >= dp[j]) {
+                int idx = stack.pollLast();
+                res += dp[idx] * (idx - stack.peekLast()) * (j - idx);
+            }
+            stack.offerLast(j);
+        }
+        while (stack.peekLast() != -1) {
+            int idx = stack.pollLast();
+            res += dp[idx] * (idx - stack.peekLast()) * (n - idx);
+        }
+    }
 }
