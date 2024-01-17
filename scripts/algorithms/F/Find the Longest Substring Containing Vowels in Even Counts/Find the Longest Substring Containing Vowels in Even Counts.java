d52baa11dddfb6f2665930a99426c625a971bc1b
AnasImloul <pydev27@gmail.com>
2024-01-17 18:31:37 +0100

    Runtime: 12 ms (Top 87.39%) | Memory: 45.40 MB (Top 86.55%)

--- scripts/algorithms/F/Find the Longest Substring Containing Vowels in Even Counts/Find the Longest Substring Containing Vowels in Even Counts.java
+++ scripts/algorithms/F/Find the Longest Substring Containing Vowels in Even Counts/Find the Longest Substring Containing Vowels in Even Counts.java
@@ -1,16 +1,56 @@
+// Runtime: 12 ms (Top 87.39%) | Memory: 45.40 MB (Top 86.55%)
+
 class Solution {
-     public int findTheLongestSubstring(String s) {
-        int res = 0 , mask = 0, n = s.length();
-        HashMap<Integer, Integer> seen = new HashMap<>();// key--> Mask, value--> Index
-        seen.put(0, -1);
-        for (int i = 0; i < n; ++i) {
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){ // check only vowels and skip consonant
-                int c=s.charAt(i);
-                mask=mask ^ c;
-                seen.putIfAbsent(mask, i);
-            }            
-            res = Math.max(res, i - seen.get(mask));
+
+    // taken help as for my code TLE T.C = (n^2);
+
+/*    static boolean check(String s){
+        int n = s.length();
+
+        int arr[] = new int[26];
+
+        for(int i=0; i<n; i++){
+            int a = s.charAt(i)-97;
+            arr[a]++;
         }
-        return res;
+
+        if(arr[0]%2!=0 || arr[4]%2!=0 || arr[8]%2!=0 || arr[14]%2!=0 ||arr[20]%2!=0) return false;
+        else return true; 
+    }
+
+    static int helper(String str){
+        int ans = 0;
+        int n = str.length();
+        StringBuilder s = new StringBuilder(str);
+
+        for(int k=1; k<=n; k++){
+            for(int i=0; i<=n-k; i++){
+                String x = s.substring(i,i+k);
+                if(check(x)){
+                    ans = Math.max(ans,k);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    */
+    public int findTheLongestSubstring(String s) {
+
+        int[] map = new int[32];
+        Arrays.fill(map, -2);
+        map[0] = -1;
+        int n = s.length(), mask = 0, len = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') mask ^= 1 << (4 -
+                    (ch == 'a' ? 0 : ch == 'e' ? 1 : ch == 'i' ? 2 : ch == 'o' ? 3 : 4));
+            if (map[mask] == -2) map[mask] = i;
+            else len = Math.max(len, i - map[mask]);
+        }
+        return len;
     }
 }
+
+
