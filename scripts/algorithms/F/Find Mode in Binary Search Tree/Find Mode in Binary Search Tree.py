fd10538f8ff2d9f069af445ae18d8e0172aa580b
AnasImloul <pydev27@gmail.com>
2024-01-17 17:19:54 +0100

    Runtime: 31 ms (Top 99.88%) | Memory: 19.20 MB (Top 96.36%)

--- scripts/algorithms/F/Find Mode in Binary Search Tree/Find Mode in Binary Search Tree.py
+++ scripts/algorithms/F/Find Mode in Binary Search Tree/Find Mode in Binary Search Tree.py
@@ -1,23 +1,41 @@
-# Runtime: 30 ms (Top 89.5%) | Memory: 20.95 MB (Top 77.8%)
+// Runtime: 31 ms (Top 99.88%) | Memory: 19.20 MB (Top 96.36%)
 
-class Solution(object):
-    prev = None
-    max_count = 0
-    current_count = 0 
-    result = []
-
+class Solution:
     def findMode(self, root):
-        self.dfs(root)
-        return self.result
+        curr_node = root
+        result = []
+        curr_streak = 0
+        curr_num = float("inf")
+        max_streak = 0
 
-    def dfs(self, node):
-        if not node: return
-        self.dfs(node.left)
-        self.current_count = 1 if node.val != self.prev else self.current_count + 1
-        if self.current_count == self.max_count:
-            self.result.append(node.val)
-        elif self.current_count > self.max_count:
-            self.result = [node.val]
-            self.max_count = self.current_count
-        self.prev = node.val
-        self.dfs(node.right)
+        while curr_node:
+            if curr_node.left:
+                neighbor = curr_node.left
+                while neighbor.right is not None:
+                    neighbor = neighbor.right
+                neighbor.right = curr_node
+                
+                tmp = curr_node.left
+                curr_node.left = None
+                curr_node = tmp
+            else: 
+                # since we deleted the left pointer when we were done processing in the previous if-block, 
+                # we know that we only reach this else case 
+                # if we've already processed this node. 
+                # Therefore, do the value processing here 
+                # and move to the right neighbor afterward
+                if curr_node.val == curr_num:
+                    curr_streak += 1
+                else:
+                    curr_streak = 0
+                    curr_num = curr_node.val
+                
+                if curr_streak == max_streak:
+                    result.append(curr_num)
+                elif curr_streak > max_streak:
+                    max_streak = curr_streak
+                    result = [curr_num]
+        
+                curr_node = curr_node.right
+        
+        return result
