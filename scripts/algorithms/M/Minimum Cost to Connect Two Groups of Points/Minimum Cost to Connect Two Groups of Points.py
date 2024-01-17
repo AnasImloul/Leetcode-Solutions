a8243a369f08e8a8316265881f06e26c71ef5142
AnasImloul <pydev27@gmail.com>
2024-01-17 17:01:56 +0100

    Runtime: 345 ms (Top 92.68%) | Memory: 29.40 MB (Top 24.39%)

--- scripts/algorithms/M/Minimum Cost to Connect Two Groups of Points/Minimum Cost to Connect Two Groups of Points.py
+++ scripts/algorithms/M/Minimum Cost to Connect Two Groups of Points/Minimum Cost to Connect Two Groups of Points.py
@@ -1,20 +1,14 @@
+// Runtime: 345 ms (Top 92.68%) | Memory: 29.40 MB (Top 24.39%)
+
 class Solution:
     def connectTwoGroups(self, cost: List[List[int]]) -> int:
         m, n = len(cost), len(cost[0])
-        min_arr = [min(x) for x in zip(*cost)]
+        mn = [min(x) for x in zip(*cost)] # min cost of connecting points in 2nd group 
         
         @lru_cache(None)
-        def dp(i, mask):
-            if i == m:
-                ans = 0
-                for j in range(n):
-                    if not mask & (1 << j):
-                        ans += min_arr[j]
-                return ans
-            
-            ans = float('inf')
-            for j in range(n):
-                ans = min(ans, cost[i][j] + dp(i + 1, mask | (1 << j)))
-            return ans
-        
-        return dp(0, 0)
+        def fn(i, mask):
+            """Return min cost of connecting group1[i:] and group2 represented as mask."""
+            if i == m: return sum(mn[j] for j in range(n) if not (mask & (1<<j)))
+            return min(cost[i][j] + fn(i+1, mask | 1<<j) for j in range(n))
+                
+        return fn(0, 0)
