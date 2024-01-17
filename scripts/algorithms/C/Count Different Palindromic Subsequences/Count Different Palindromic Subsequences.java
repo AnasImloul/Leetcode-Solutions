f7a8e8f0efb9dd892822a9210b5ccf82d4504152
AnasImloul <pydev27@gmail.com>
2024-01-17 17:43:53 +0100

    Runtime: 33 ms (Top 61.21%) | Memory: 50.70 MB (Top 75.86%)

--- scripts/algorithms/C/Count Different Palindromic Subsequences/Count Different Palindromic Subsequences.java
+++ scripts/algorithms/C/Count Different Palindromic Subsequences/Count Different Palindromic Subsequences.java
@@ -1,36 +1,56 @@
+// Runtime: 33 ms (Top 61.21%) | Memory: 50.70 MB (Top 75.86%)
+
 class Solution {
-    public int countPalindromicSubsequences(String s) {
-        int n = s.length(), ans = 0, M = (int)1e9+7, E = 4;
-        int[][] next = new int[n][E];
-        int[][] prev = new int[n][E];
-        int[][][] dp = new int[n][n][E];
-        for (int i = 0; i < E; i++){ // find next letter in O(1) or you can use a treeset which will run in O(logn)
-            for (int j = 0, k = n - 1; j < n; j++, k--){
-                prev[j][i] = s.charAt(j) - 'a' == i? j : (j == 0?   -1 : prev[j-1][i]);
-                next[k][i] = s.charAt(k) - 'a' == i? k : (k == n-1? -1 : next[k+1][i]);
-            }
-        }
-        for (int i = n - 1; i >= 0; i--){
-            for (int j = i; j < n; j++){
-                if (s.charAt(i) != s.charAt(j)){
-                    continue;
-                }
-                int cur = s.charAt(i) - 'a';
-                dp[i][j][cur] += i == j? 1:2;
-                for (int k = 0; k < E && j-i >= 2; k++){
-                    if (next[i+1][k] != -1 && prev[j-1][k] != -1){ // expand from both sides
-                        dp[i][j][cur] += dp[next[i+1][k]][prev[j-1][k]][k];
-                        dp[i][j][cur] %= M;
-                    }
-                }
-            }
-        }
-        for (int k = 0; k < E; k++){ // get the sum of outer most pairs
-            if (next[0][k] != -1 && prev[n-1][k] != -1){
-                ans += dp[next[0][k]][prev[n-1][k]][k];
-                ans %= M;
-            }
-        }
-        return ans;
+    public int countPalindromicSubsequences(String str) {
+        int[] pre = new int[str.length()];
+		HashMap<Character, Integer> map = new HashMap<>();
+		int mod = 1000000007;
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(map.containsKey(ch))
+				pre[i] = map.get(ch);
+			else
+				pre[i] = -1;
+			map.put(ch, i);
+		}
+		
+		int[] next = new int[str.length()];
+		map = new HashMap<>();
+		for(int i = str.length() - 1; i >= 0; i--) {
+			char ch = str.charAt(i);
+			if(map.containsKey(ch))
+				next[i] = map.get(ch);
+			else
+				next[i] = Integer.MAX_VALUE;
+			map.put(ch, i);
+		}
+		
+		int[][] dp = new int[str.length()][str.length()];
+		
+		for(int g = 0; g < dp.length; g++) {
+			for(int i = 0, j = g; j < dp[0].length; i++, j++) {
+				if(g == 0)
+					dp[i][j] = 1;
+				else if(g == 1)
+					dp[i][j] = 2;
+				else {
+					if(str.charAt(i) == str.charAt(j)) {
+						int n = next[i];
+						int p = pre[j];
+						
+						if(n > p)
+							dp[i][j] = ((2 * dp[i+1][j-1]) + 2) % mod;
+						else if(n == p)
+							dp[i][j] = ((2 * dp[i+1][j-1]) + 1) % mod;
+						else
+							dp[i][j] = ((2 * dp[i+1][j-1]) - dp[n+1][p-1]) % mod;
+					}else
+						dp[i][j] = (dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1]) % mod;
+				}
+                if(dp[i][j] < 0)
+                    dp[i][j] += mod;
+			}
+		}
+		return dp[0][dp[0].length - 1] % mod;
     }
 }
