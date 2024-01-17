22361c4317a3e90c6b2ad845d947bbea71e8a56a
AnasImloul <pydev27@gmail.com>
2024-01-17 18:22:11 +0100

    Runtime: 1 ms (Top 97.04%) | Memory: 41.60 MB (Top 27.41%)

--- scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.java
+++ scripts/algorithms/M/Minimum Cost to Merge Stones/Minimum Cost to Merge Stones.java
@@ -1,68 +1,63 @@
+// Runtime: 1 ms (Top 97.04%) | Memory: 41.60 MB (Top 27.41%)
+
 class Solution {
+    int prefix[];
     public int mergeStones(int[] stones, int k) {
-        int n = stones.length;
-        int[] prefixSums = new int[n];
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            sum += stones[i];
-            prefixSums[i] = sum;
+        int n=stones.length;
+        if((n-1)%(k-1) != 0) return -1;
+        prefix=new int[stones.length+1];
+        prefix[0]=0;
+        int sum=0;
+        for(int i=0;i<stones.length;i++){
+            sum+=stones[i];
+            prefix[i+1]=sum;
         }
-        Integer[][] dp = new Integer[n][n];
-        return merge(0, n-1, stones, k, dp, prefixSums);
+        int dp[][]=new int[stones.length][stones.length];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return check(prefix,k,0,stones.length-1,dp);
     }
-    
-    private int merge(int start, int end, int[] stones, int k, Integer[][] dp, int[] prefixSums) {
-        if (dp[start][end] != null) {
-            // already computed
-            return dp[start][end];
-        }
-        if (start == end) {
-            // base case: single pile already
-            dp[start][end] = 0;
+    public int check(int[] prefix,int k,int i,int j,int dp[][]){
+        if(i>=j){
             return 0;
         }
-        if ((end - start) % (k-1) != 0) {
-            // end - start + 1 is how many piles to merge
-            // so end - start is how many piles we have to delete by merge
-            // and at each merge, we delete k-1 piles.
-            dp[start][end] = -1;
-            return -1;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        // ok so now have K or more piles to merge
-        // need to test all possible K-paritions using K-1 delimiters and recurse on them and min.
-        AtomicInteger minCostWrapper = new AtomicInteger(Integer.MAX_VALUE);
-        partition(start, end, stones, k, 0, k - 1, minCostWrapper, dp, prefixSums);
-        int min = minCostWrapper.get();
-        int rtn = min == Integer.MAX_VALUE? -1 : min;
-        rtn += prefixSums[end] - (start - 1 >=0 ? prefixSums[start-1] : 0);
-        dp[start][end] = rtn;
-        return rtn;
+        int min=Integer.MAX_VALUE;
+        for(int t=i;t<j;t=t+k-1){
+            int min1=check(prefix,k,i,t,dp)+check(prefix,k,t+1,j,dp);
+            min=Math.min(min,min1);
+        }
+        if((j-i)%(k-1)==0){
+            min+=prefix[j+1]-prefix[i];
+        }
+        dp[i][j]=min;
+        return min;
     }
-    
-    private void partition(int curr, 
-                          int end,
-                          int[] stones,
-                          int k,
-                          int costSoFar, 
-                          int delimiters,
-                          AtomicInteger minCost, 
-                          Integer[][] dp, 
-                           int[] prefixSums) {
-        if (delimiters == 0) {
-            // base case: no more delimiters
-            int cost = merge(curr, end, stones, k, dp, prefixSums);
-            cost += costSoFar;
-            if (cost != -1 && cost < minCost.get()) {
-                minCost.set(cost);
+}
+class Solution0 {
+    public int mergeStones(int[] stones, int K) {
+        int n = stones.length;
+        if ((n - 1) % (K - 1) > 0) return -1;
+
+        int[] prefix = new int[n+1];
+        for (int i = 0; i <  n; i++)
+            prefix[i + 1] = prefix[i] + stones[i];
+
+        int[][] dp = new int[n][n];
+        for (int m = K; m <= n; ++m)
+            for (int i = 0; i + m <= n; ++i) {
+                int j = i + m - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int mid = i; mid < j; mid += K - 1)
+                    dp[i][j] = Math.min(dp[i][j], dp[i][mid] + dp[mid + 1][j]);
+                if ((j - i) % (K - 1) == 0)
+                    dp[i][j] += prefix[j + 1] - prefix[i];
             }
-            return;
-        }
-        //for(int i=curr; i<end && (end - i - 1) >= delimiters - 1)
-        for(int i=curr; (i<end) && (end - i) >= delimiters; i++) {
-            int nextCost = merge(curr, i, stones, k, dp, prefixSums);
-            if (nextCost != -1) {
-                partition(i+1, end, stones, k, costSoFar + nextCost, delimiters - 1, minCost, dp, prefixSums);
-            }
-        }
+        return dp[0][n - 1];
     }
 }
