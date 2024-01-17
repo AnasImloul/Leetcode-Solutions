72377f23b0394807de13a5428bb9108e7eac0da8
AnasImloul <pydev27@gmail.com>
2024-01-17 17:03:25 +0100

    Runtime: 437 ms (Top 80.03%) | Memory: 18.00 MB (Top 70.21%)

--- scripts/algorithms/P/Partition Equal Subset Sum/Partition Equal Subset Sum.py
+++ scripts/algorithms/P/Partition Equal Subset Sum/Partition Equal Subset Sum.py
@@ -1,15 +1,14 @@
+// Runtime: 437 ms (Top 80.03%) | Memory: 18.00 MB (Top 70.21%)
+
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        if total % 2: return False
-        total //= 2
-        leng = len(nums)
-        dp = [[False] * (total + 1) for _ in range(leng + 1)]
-        
-        for i in range(leng + 1): dp[i][0] = True
-        for i in range(1, leng + 1):
-            for j in range(1, total + 1):
-                dp[i][j] = dp[i-1][j]
-                if j - nums[i-1] >= 0:
-                    dp[i][j] |= dp[i-1][j-nums[i-1]] 
-        return dp[leng][total]
+        dp, s = set([0]), sum(nums)
+        if s&1:
+            return False
+        for num in nums:
+            for curr in range(s>>1, num-1, -1):
+                if curr not in dp and curr-num in dp:
+                    if curr == s>>1:
+                        return True
+                    dp.add(curr)
+        return False
