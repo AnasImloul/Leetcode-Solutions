3878b394328c9230df548422b6952985ca5d8ec5
AnasImloul <pydev27@gmail.com>
2024-01-17 16:55:14 +0100

    Runtime: 53 ms (Top 95.6%) | Memory: 20.10 MB (Top 30.35%)

--- scripts/algorithms/L/Linked List Random Node/Linked List Random Node.py
+++ scripts/algorithms/L/Linked List Random Node/Linked List Random Node.py
@@ -1,10 +1,10 @@
+// Runtime: 53 ms (Top 95.6%) | Memory: 20.10 MB (Top 30.35%)
+
 class Solution:
-
     def __init__(self, head: Optional[ListNode]):
-        self.lst = []
+        self.ll=[]
         while head:
-            self.lst.append(head.val)
-            head = head.next
-
+            self.ll.append(head.val)
+            head=head.next
     def getRandom(self) -> int:
-        return random.choice(self.lst)
+        return self.ll[randint(0, len(self.ll)-1)]
