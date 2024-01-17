5a408b09a7ebb352e2ef5e874ccdcc18d482fba2
AnasImloul <pydev27@gmail.com>
2024-01-17 17:08:20 +0100

    Runtime: 44 ms (Top 99.57%) | Memory: 18.50 MB (Top 5.17%)

--- scripts/algorithms/C/Complete Binary Tree Inserter/Complete Binary Tree Inserter.py
+++ scripts/algorithms/C/Complete Binary Tree Inserter/Complete Binary Tree Inserter.py
@@ -1,34 +1,62 @@
+// Runtime: 44 ms (Top 99.57%) | Memory: 18.50 MB (Top 5.17%)
+
+from collections import deque
+
 class CBTInserter:
 
-    def __init__(self, root: Optional[TreeNode]):
+    
+    def __init__(self, root: TreeNode):
+        
         self.root = root
-        self.queue = Queue()
-        self.queue.put(self.root)
-        self.parent_of_last_inserted = None
         
-    def insert(self, val: int) -> int:
-        if self.parent_of_last_inserted is not None and self.parent_of_last_inserted.right is None:
-            self.parent_of_last_inserted.right = TreeNode(val)
-            self.queue.put(self.parent_of_last_inserted.right)
-            return self.parent_of_last_inserted.val
+        self.parent_keeper = deque([root])
+
+        while True:
+            
+            cur = self.parent_keeper[0]
+            
+            if cur:
+                
+                if cur.left:
+                    
+                    self.parent_keeper.append( cur.left )
+                    
+                    if cur.right:
+                        
+                        self.parent_keeper.append( cur.right )
+                        
+                        # cur is completed with two child, pop out
+                        self.parent_keeper.popleft()
+                    
+                    else:
+                        # parent of next insertion is found, stop
+                        break
+                
+                else:
+                    # parent of next insertion is found, stop
+                    break
         
-        while not self.queue.empty():
-            node = self.queue.get()
-            if node.left is None:
-                node.left = TreeNode(val)
-                self.queue.put(node.left)
-                self.parent_of_last_inserted = node
-                return node.val
-            else:
-                self.queue.put(node.left)
-            if node.right is None:
-                node.right = TreeNode(val)
-                self.queue.put(node.right)
-                self.parent_of_last_inserted = node
-                return node.val
-            else:
-                self.queue.put(node.right)
         
 
-    def get_root(self) -> Optional[TreeNode]:
+    def insert(self, v: int) -> int:
+        
+        parent = self.parent_keeper[0]
+        
+		# Insert with leftward compact, to meet the definition of complete binary tree
+		
+        if not parent.left:
+            parent.left = TreeNode( v )
+            self.parent_keeper.append( parent.left )
+        else:
+            parent.right = TreeNode( v )
+            self.parent_keeper.append( parent.right )
+            
+            # current parent is completed with two child now, pop parent from parent keeper on the head
+            self.parent_keeper.popleft()
+            
+        return parent.val
+        
+
+    def get_root(self) -> TreeNode:
+        
         return self.root
