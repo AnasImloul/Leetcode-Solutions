e3ff02159bc024ec0dac900e8d0bcb8cc601fb73
AnasImloul <pydev27@gmail.com>
2024-01-17 17:04:47 +0100

    Runtime: 106 ms (Top 94.04%) | Memory: 24.40 MB (Top 92.45%)

--- scripts/algorithms/R/Range Sum of BST/Range Sum of BST.py
+++ scripts/algorithms/R/Range Sum of BST/Range Sum of BST.py
@@ -1,21 +1,16 @@
+// Runtime: 106 ms (Top 94.04%) | Memory: 24.40 MB (Top 92.45%)
+
 class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-    
-        res = 0
+    def rangeSumBST(self, root, L, R):
+        if not root:
+            return 0
         
-        def dfs(node):
-            nonlocal res
-            
-            if not node:
-                return
-            
-            if node.val >= low and node.val <= high:
-                
-                res += node.val
-                
-            dfs(node.left)
-            dfs(node.right)
-            
-        dfs(root)
-        
-        return res
+        if L <= root.val <= R:
+            return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        else:
+            return self.rangeSumBST(root.left, L, R)
+
+
+
