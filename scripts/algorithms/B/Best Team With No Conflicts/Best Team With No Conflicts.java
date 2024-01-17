4daaa2371cdb74acbea9cca7b3d30e23ba4ac702
AnasImloul <pydev27@gmail.com>
2024-01-17 17:50:29 +0100

    Runtime: 27 ms (Top 92.02%) | Memory: 45.10 MB (Top 27.31%)

--- scripts/algorithms/B/Best Team With No Conflicts/Best Team With No Conflicts.java
+++ scripts/algorithms/B/Best Team With No Conflicts/Best Team With No Conflicts.java
@@ -1,38 +1,36 @@
+// Runtime: 27 ms (Top 92.02%) | Memory: 45.10 MB (Top 27.31%)
+
 class Solution {
+    int res = 0;
     public int bestTeamScore(int[] scores, int[] ages) {
-        int n = scores.length;
-        // combine the arrays for ease in processing
-        int[][] combined = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            combined[i][0] = ages[i];
-            combined[i][1] = scores[i];
+        int len = scores.length;
+        int[][] team = new int[len][2];
+        for (int i = 0; i < len; i++) {
+            team[i][0] = ages[i];
+            team[i][1] = scores[i]; // team is [age, score]
+        }
+		// double sort first by age then by score, then we can traverse from young to old
+        Arrays.sort(team, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        
+        int[] dp = new int[len];
+        // dp is the max sum for all sequences (not necessarily consecutive) ending in current idx
+        dp[0] = team[0][1];
+        for (int i = 1; i < len; i++) {
+            int max = team[i][1]; // At least it could start a new sequence by itself without extend
+			// for each current idx, go visit all previous index to grow the sequences
+            for (int j = 0; j < i; j++) {
+                if (team[i][1] >= team[j][1]) {
+                    max = Math.max(max, dp[j] + team[i][1]);
+                }
+            }
+            dp[i] = max;
+        }
+      
+        int res = 0;
+        for (int num : dp) {
+            res = Math.max(res, num);
         }
         
-        // sort the arrays by age and scores
-        Arrays.sort(combined, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        
-		// find max answer between all possible teams
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-            ans = Math.max(ans, recurse(combined, i));
-        
-        return ans;
-    }
-    
-    private int recurse(int[][] combined, int cur) {
-        // base case
-        if (cur == -1)
-            return 0;
-        
-        // try teaming player with others if no conflict found
-        int ans = 0;
-        for (int prev = 0; prev < cur; prev++) {
-            if (combined[cur][0] == combined[prev][0] || combined[cur][1] >= combined[prev][1])
-                ans = Math.max(ans, recurse(combined, prev));
-        }
-        
-        // add score of current player
-        ans += combined[cur][1];
-        return ans;
+        return res;
     }
 }
