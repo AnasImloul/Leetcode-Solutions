93519f245f4b1bb3f6a77b497a4a1ce0c61e46a6
AnasImloul <pydev27@gmail.com>
2024-01-21 15:55:51 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 2.00 MB (Top 96.55%)

---
+++ scripts/algorithms/F/Find the Maximum Achievable Number/Find the Maximum Achievable Number.rs
@@ -0,0 +1,8 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 2.00 MB (Top 96.55%)
+
+impl Solution {
+    pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+        num + (2*t)
+        
+    }
+}
