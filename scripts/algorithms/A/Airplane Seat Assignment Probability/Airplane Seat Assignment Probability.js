f078f1c740fd0e47ee4f8c8a136bf1b87f4e9fb6
AnasImloul <pydev27@gmail.com>
2024-01-22 16:31:28 +0100

    Runtime: 48 ms (Top 84.85%) | Memory: 48.40 MB (Top 9.09%)

---
+++ scripts/algorithms/A/Airplane Seat Assignment Probability/Airplane Seat Assignment Probability.js
@@ -0,0 +1,9 @@
+// Runtime: 48 ms (Top 84.85%) | Memory: 48.40 MB (Top 9.09%)
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthPersonGetsNthSeat = function(n) {
+    return n===1? 1 :0.5
+};
