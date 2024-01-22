c3276b9454cae61ff3f00c23422a29d35deeef53
AnasImloul <pydev27@gmail.com>
2024-01-22 16:42:47 +0100

    Runtime: 96 ms (Top 94.12%) | Memory: 59.50 MB (Top 11.76%)

--- scripts/algorithms/M/Minimum Path Cost in a Grid/Minimum Path Cost in a Grid.js
+++ scripts/algorithms/M/Minimum Path Cost in a Grid/Minimum Path Cost in a Grid.js
@@ -1,42 +1,27 @@
+// Runtime: 96 ms (Top 94.12%) | Memory: 59.50 MB (Top 11.76%)
+
 var minPathCost = function(grid, moveCost) {
-    const rows = grid.length;
-    const cols = grid[0].length;
 
-    const cache = [];
-    
-    for (let i = 0; i < rows; i++) {
-        cache.push(Array(cols).fill(null));
-    }
-    
-    function move(row, col) {
-        const val = grid[row][col];
+    const dfs=(r,c,dp)=>{
+        if(r===grid.length || c===grid[r].length) return 0;
+        if(dp[r][c]!==undefined) return dp[r][c];
+
+        // current cost
+        const cost=grid[r][c], nextRow=r+1;
+
+        if(nextRow===grid.length) return cost;
         
-        if (cache[row][col] !== null) {
-            return cache[row][col];
+        let nextMoveCost=Number.MAX_SAFE_INTEGER;
+        for(let nextCol=0;nextCol<grid[nextRow].length;nextCol++){
+            nextMoveCost=Math.min(nextMoveCost,dfs(nextRow,nextCol,dp)+moveCost[cost][nextCol]/*path cost*/);
         }
-        
-        if (row === rows - 1) {
-            return val;
-        }
-        
-        let ans = Number.MAX_SAFE_INTEGER;
+        return dp[r][c]=cost+(nextMoveCost===Number.MAX_SAFE_INTEGER?0:nextMoveCost);
+    };
 
-        for (let i = 0; i < cols; i++) {
-            const addCost = moveCost[val][i];
-
-            ans = Math.min(ans, move(row + 1, i) + val + addCost);
-        }
-        
-        cache[row][col] = ans;
-        
-        return ans;
+    let minCost=Number.MAX_SAFE_INTEGER;
+    const dp=Array(grid.length).fill().map(()=>Array(grid[0].length).fill());
+    for(let c=0;c<grid[0].length;c++){
+        minCost=Math.min(minCost,dfs(0,c,dp));
     }
-
-    let ans = Number.MAX_SAFE_INTEGER;
-    
-    for (let i = 0; i < cols; i++) {
-        ans = Math.min(ans, move(0, i));
-    }
-    
-    return ans;
+    return minCost;
 };
