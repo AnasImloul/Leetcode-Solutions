ef00a0685da69210cad689e963df049bf1706a79
AnasImloul <pydev27@gmail.com>
2024-01-23 13:15:28 +0100

    Runtime: 39 ms (Top 85.2%) | Memory: 16.70 MB (Top 56.78%)

--- scripts/algorithms/R/Running Sum of 1d Array/Running Sum of 1d Array.py
+++ scripts/algorithms/R/Running Sum of 1d Array/Running Sum of 1d Array.py
@@ -1,8 +1,16 @@
+// Runtime: 39 ms (Top 85.2%) | Memory: 16.70 MB (Top 56.78%)
+
 class Solution:
-    def runningSum(self, nums):
-        a=0
-        list=[]
-        for i in nums:
-            a=i+a
-            list.append(a)
-        return(list)
+    def runningSum(self, nums: List[int]) -> List[int]:
+        # The variable that will have the running sum
+        tot = 0
+        # The array that will hold the running su,
+        ans = []
+        # For loop
+        for ele in nums:
+            # Adding the element
+            tot += ele
+            # Appending this running sum to ans
+            ans.append(tot)
+        # Return ans
+        return ans
