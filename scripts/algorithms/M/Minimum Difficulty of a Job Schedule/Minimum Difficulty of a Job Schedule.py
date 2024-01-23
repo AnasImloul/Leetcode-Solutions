f4cc050f1718dd2e83accea592410527e4086b0f
AnasImloul <pydev27@gmail.com>
2024-01-23 13:06:47 +0100

    Runtime: 483 ms (Top 81.04%) | Memory: 16.60 MB (Top 99.3%)

--- scripts/algorithms/M/Minimum Difficulty of a Job Schedule/Minimum Difficulty of a Job Schedule.py
+++ scripts/algorithms/M/Minimum Difficulty of a Job Schedule/Minimum Difficulty of a Job Schedule.py
@@ -1,17 +1,36 @@
+// Runtime: 483 ms (Top 81.04%) | Memory: 16.60 MB (Top 99.3%)
+
 class Solution:
-    def solve(self, nums, index, d):
-        if index == len(nums) or d == 1:
-            #print(max(nums[index:]))
-            return max(nums[index:])
-        ans = float("inf")
-        for i in range(index, len(nums)-d+1):
-            curr = max(nums[index:i+1]) + self.solve(nums, i+1, d-1)
-            ans = min(ans, curr)
-        return ans
-    
-    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        ans = self.solve(jobDifficulty, 0, d)
-        if ans == float("inf"):
+    def minDifficulty(self, jobDifficulty, days):
+        length = len(jobDifficulty)
+        if days > length:
             return -1
-        else:
-            return ans
+
+        min_difficulties = [[float('inf')] * length for _ in range(days)]
+
+        max_diff = 0
+        i = 0
+        while i <= length - days:
+            max_diff = max(max_diff, jobDifficulty[i])
+            min_difficulties[0][i] = max_diff
+            i += 1
+
+        current_day = 1
+        while current_day < days:
+            to = current_day
+            while to <= length - days + current_day:
+                current_job_difficulty = jobDifficulty[to]
+                result = float('inf')
+                j = to - 1
+                while j >= current_day - 1:
+                    result = min(result, min_difficulties[current_day - 1][j] + current_job_difficulty)
+                    current_job_difficulty = max(current_job_difficulty, jobDifficulty[j])
+                    j -= 1
+                min_difficulties[current_day][to] = result
+                to += 1
+            current_day += 1
+
+        return min_difficulties[days - 1][length - 1]
+
+
+
