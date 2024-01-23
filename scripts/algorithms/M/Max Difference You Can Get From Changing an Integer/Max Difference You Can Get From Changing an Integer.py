de1aad5d4ee34d177e8df741903e33783f8c0007
AnasImloul <pydev27@gmail.com>
2024-01-23 13:42:07 +0100

    Runtime: 34 ms (Top 82.14%) | Memory: 16.40 MB (Top 63.78%)

--- scripts/algorithms/M/Max Difference You Can Get From Changing an Integer/Max Difference You Can Get From Changing an Integer.py
+++ scripts/algorithms/M/Max Difference You Can Get From Changing an Integer/Max Difference You Can Get From Changing an Integer.py
@@ -1,41 +1,15 @@
+// Runtime: 34 ms (Top 82.14%) | Memory: 16.40 MB (Top 63.78%)
+
 class Solution:
     def maxDiff(self, num: int) -> int:
-        i=0
-        while i<len(str(num)):
-            change = (str(num)[i])
-            if change!='9':
-                break
-            i+=1
-        
-        i=0
-        flag = False
-        while i<len(str(num)):
-            sc = (str(num)[i])
-            if sc!='1' and sc!='0':
-                if i>0:
-                    flag =True
-                break
-            i+=1
-        small = str(num)
         num = str(num)
-        m=''
-        for i in range(len(str(num))):
-            if num[i]==change:
-                m+='9'
-            else:
-                m+=num[i]
-        m = int(m)
-        s =''
-        for i in range(len(num)):
-            if small[i]==sc:
-                if flag:
-                    s+='0'
-                else:
-                    if small[i]=="0":
-                        s+='0'
-                    else:
-                        s+='1'
-            else:
-                s+=small[i]
-        small = int(s)
-        return m - small
+        
+        i = next((i for i in range(len(num)) if num[i] != "9"), -1) #first non-9 digit
+        hi = int(num.replace(num[i], "9"))
+        
+        if num[0] != "1": lo = int(num.replace(num[0], "1"))
+        else: 
+            i = next((i for i in range(len(num)) if num[i] not in "01"), -1)
+            lo = int(num.replace(num[i], "0") if i > 0 else num)
+            
+        return hi - lo
