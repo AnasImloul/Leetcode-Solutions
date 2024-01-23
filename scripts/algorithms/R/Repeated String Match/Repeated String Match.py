e61da234631a3f834255179c0fe9043886b9058f
AnasImloul <pydev27@gmail.com>
2024-01-23 13:40:10 +0100

    Runtime: 30 ms (Top 95.42%) | Memory: 16.60 MB (Top 60.78%)

--- scripts/algorithms/R/Repeated String Match/Repeated String Match.py
+++ scripts/algorithms/R/Repeated String Match/Repeated String Match.py
@@ -1,10 +1,13 @@
+// Runtime: 30 ms (Top 95.42%) | Memory: 16.60 MB (Top 60.78%)
+
 class Solution:
-    def repeatedStringMatch(self, a: str, b: str) -> int:
-        res=""
-        count=0
-        while len(res)<len(b)+len(a):
-            count+=1
-            res=a*count
-            if b in res:
-                return count
-        return -1
+    def repeatedStringMatch(self, A: str, B: str) -> int:
+        if len(A) >= len(B):
+            if B in A: return 1
+            elif B in A*2: return 2
+            else: return -1
+        prefix = max(0, B.find(A)) #prefix -- length of A1
+        repeat, postfix = divmod(len(B)-prefix, len(A)) #postfix -- length of A2
+        repeat += bool(prefix) + bool(postfix)
+        if B in A * repeat: return repeat
+        else: return -1 
