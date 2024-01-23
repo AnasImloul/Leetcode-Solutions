77c70734d51f6cf434ff5f380dc471daa5b43882
AnasImloul <pydev27@gmail.com>
2024-01-23 13:14:12 +0100

    Runtime: 34 ms (Top 84.25%) | Memory: 16.50 MB (Top 59.83%)

--- scripts/algorithms/S/Spiral Matrix II/Spiral Matrix II.py
+++ scripts/algorithms/S/Spiral Matrix II/Spiral Matrix II.py
@@ -1,42 +1,29 @@
-# Runtime: 61 ms (Top 26.67%) | Memory: 13.9 MB (Top 37.41%)
-class Solution(object):
-    def generateMatrix(self, n):
-        if n==1:
-            return [[1]]
-        matrix=[[0 for a in range(n)] for b in range(n)]
-        le_b=0
-        u_b=0
-        r_b=n-1
-        lo_b=n-1
-        ele=1
-        while ele<(n**2)+1:
-            i=u_b
-            j=le_b
-            while ele<(n**2)+1 and j<=r_b:
-                matrix[i][j]=ele
-                ele+=1
-                j+=1
-            u_b+=1
-            i=u_b
-            j=r_b
-            while ele<(n**2)+1 and i<=lo_b:
-                matrix[i][j]=ele
-                ele+=1
-                i+=1
-            r_b-=1
-            i=lo_b
-            j=r_b
-            while ele<(n**2)+1 and j>=le_b:
-                matrix[i][j]=ele
-                ele+=1
-                j-=1
-            lo_b-=1
-            i=lo_b
-            j=le_b
-            while ele<(n**2)+1 and i>=u_b:
-                matrix[i][j]=ele
-                ele+=1
-                i-=1
-            le_b+=1
+// Runtime: 34 ms (Top 84.25%) | Memory: 16.50 MB (Top 59.83%)
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        if not n:
+            return []
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        left, right, top, bottom, num = 0, n-1, 0, n-1, 1
+        while left <= right and top <= bottom:
+            for i in range(left, right+1):
+                matrix[top][i] = num 
+                num += 1
+            top += 1
+            for i in range(top, bottom+1):
+                matrix[i][right] = num
+                num += 1
+            right -= 1
+            if top <= bottom:
+                for i in range(right, left-1, -1):
+                    matrix[bottom][i] = num
+                    num += 1
+                bottom -= 1
+            if left <= right:
+                for i in range(bottom, top-1, -1):
+                    matrix[i][left] = num
+                    num += 1
+                left += 1
         return matrix
-        ```
+
