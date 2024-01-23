d5a429a53ea768406e1657385ac4cd8c64c5e763
AnasImloul <pydev27@gmail.com>
2024-01-23 13:22:29 +0100

    Runtime: 253 ms (Top 99.08%) | Memory: 17.50 MB (Top 42.72%)

--- scripts/algorithms/N/Number of Ways to Divide a Long Corridor/Number of Ways to Divide a Long Corridor.py
+++ scripts/algorithms/N/Number of Ways to Divide a Long Corridor/Number of Ways to Divide a Long Corridor.py
@@ -1,20 +1,17 @@
+// Runtime: 253 ms (Top 99.08%) | Memory: 17.50 MB (Top 42.72%)
+
 class Solution:
-    def numberOfWays(self, corridor: str) -> int:
-        numSeats = 0
-        numPlants = 0
-        dividers = 1
-        mod = 1000000007
-        
+    def numberOfWays(self, corridor):
+        seat, res, plant = 0, 1, 0
         for i in corridor:
-            if i == 'S':
-                numSeats += 1
-            if numSeats == 2 and i == 'P':
-                numPlants += 1
-            if numSeats == 3:
-                dividers *= (numPlants + 1)
-                dividers %= mod
-                numSeats = 1
-                numPlants = 0
-        
-        if numSeats < 2: return 0
-        return dividers
+            if i=='S':
+                seat += 1
+            else:
+                if seat == 2:
+                    plant += 1
+            if seat == 3:
+                res = res*(plant+1) % (10**9 + 7)
+                seat , plant = 1 , 0
+        if seat != 2:
+            return 0
+        return res
