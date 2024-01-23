6bd0a12ffe9be6a445376a21297bfcc0102f9e95
AnasImloul <pydev27@gmail.com>
2024-01-23 13:05:50 +0100

    Runtime: 33 ms (Top 98.51%) | Memory: 16.50 MB (Top 56.37%)

--- scripts/algorithms/S/Sum of All Odd Length Subarrays/Sum of All Odd Length Subarrays.py
+++ scripts/algorithms/S/Sum of All Odd Length Subarrays/Sum of All Odd Length Subarrays.py
@@ -1,9 +1,11 @@
-# Runtime: 68 ms (Top 74.29%) | Memory: 13.9 MB (Top 63.03%)
+// Runtime: 33 ms (Top 98.51%) | Memory: 16.50 MB (Top 56.37%)
+
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        n = len(arr)
+        
+        length = len(arr)
         ans = 0
-        for i in range(n):
-            total = (i+1) * (n-1-i+1)
-            ans = ans + (total//2 + total%2) * arr[i]
-        return ans
+        
+        for i in range(length) :
+            ans += ((i+1)*(length-i)+1)//2 * arr[i]
+        return ans;
