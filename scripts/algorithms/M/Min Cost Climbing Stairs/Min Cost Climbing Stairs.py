eeebd9e5fe8db04d57f29e74d07142e7c71793b1
AnasImloul <pydev27@gmail.com>
2024-01-23 13:37:10 +0100

    Runtime: 52 ms (Top 87.09%) | Memory: 16.80 MB (Top 65.44%)

--- scripts/algorithms/M/Min Cost Climbing Stairs/Min Cost Climbing Stairs.py
+++ scripts/algorithms/M/Min Cost Climbing Stairs/Min Cost Climbing Stairs.py
@@ -1,8 +1,15 @@
-# Runtime: 127 ms (Top 14.40%) | Memory: 14 MB (Top 75.22%)
+// Runtime: 52 ms (Top 87.09%) | Memory: 16.80 MB (Top 65.44%)
+
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = cost[:2] + [0]*(n-2)
-        for i in range(2, n):
-            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
-        return min(dp[-1], dp[-2])
+	def minCostClimbingStairs(self, cost: List[int]) -> int:
+		cur = 0 
+		dp0 = cost[0]
+		if len(cost) >= 2:
+			dp1 = cost[1]
+
+		for i in range(2, len(cost)):
+			cur = cost[i] + min(dp0, dp1)
+			dp0 = dp1
+			dp1 = cur
+
+		return min(dp0, dp1)
