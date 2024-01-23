4bf1cda510e9230db4c6fbd6d6cafa8d48b56cc9
AnasImloul <pydev27@gmail.com>
2024-01-23 13:39:11 +0100

    Runtime: 818 ms (Top 91.97%) | Memory: 26.40 MB (Top 28.47%)

--- scripts/algorithms/L/Largest Component Size by Common Factor/Largest Component Size by Common Factor.py
+++ scripts/algorithms/L/Largest Component Size by Common Factor/Largest Component Size by Common Factor.py
@@ -1,37 +1,40 @@
+// Runtime: 818 ms (Top 91.97%) | Memory: 26.40 MB (Top 28.47%)
+
+class UnionFind: 
+    
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1]*n
+        
+    def find(self, p): 
+        if p != self.parent[p]: 
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+    
+    def union(self, p, q): 
+        prt, qrt = self.find(p), self.find(q)
+        if prt == qrt: return False 
+        if self.rank[prt] > self.rank[qrt]: prt, qrt = qrt, prt
+        self.parent[prt] = qrt
+        self.rank[qrt] += self.rank[prt]
+        return True 
+
+
 class Solution:
-    def largestComponentSize(self, nums: List[int]) -> int:
+    def largestComponentSize(self, A: List[int]) -> int:
+        m = max(A)
+        uf = UnionFind(m+1)
+        seen = set(A)
         
-        def find(node):
-            if parent[node] == -1: return node
-            else:
-                parent[node] = find(parent[node])
-                return parent[node]
-        
-        def union(idx1,idx2):
-            par1,par2 = find(idx1),find(idx2)
-            if par1!=par2:
-                if rank[par1] > rank[par2]:
-                    parent[par2] = par1
-                elif rank[par2] > rank[par1]:
-                    parent[par1] = par2
-                else:
-                    parent[par2] = par1
-                    rank[par1] += 1
-        
-        n = len(nums)
-        parent = defaultdict(lambda:-1)
-        rank = defaultdict(lambda:0)
-        for i in range(n):
-            limit = int(nums[i]**0.5)
-            for j in range(2,limit+1):
-                if nums[i] % j == 0:
-                    union(nums[i],j)
-                    union(nums[i],nums[i]//j)
-        count = defaultdict(lambda:0)
-        best = -1
-        for num in nums:
-            par = find(num)
-            tmp = count[par] + 1
-            if tmp > best: best = tmp
-            count[par] = tmp
-        return best
+        # modified sieve of eratosthenes 
+        sieve = [1]*(m+1)
+        sieve[0] = sieve[1] = 0 
+        for k in range(m//2+1): 
+            if sieve[k]: 
+                prev = k if k in seen else 0
+                for x in range(2*k, m+1, k): 
+                    sieve[x] = 0
+                    if x in seen: 
+                        if prev: uf.union(prev, x)
+                        else: prev = x
+        return max(uf.rank)
