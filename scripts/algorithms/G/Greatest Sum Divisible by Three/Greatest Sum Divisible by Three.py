812e2bf921d224a7b82a8d99344073a254617c68
AnasImloul <pydev27@gmail.com>
2024-01-23 13:11:01 +0100

    Runtime: 190 ms (Top 92.53%) | Memory: 22.10 MB (Top 78.57%)

--- scripts/algorithms/G/Greatest Sum Divisible by Three/Greatest Sum Divisible by Three.py
+++ scripts/algorithms/G/Greatest Sum Divisible by Three/Greatest Sum Divisible by Three.py
@@ -1,11 +1,34 @@
-# Runtime: 463 ms (Top 64.44%) | Memory: 20.3 MB (Top 40.95%)
+// Runtime: 190 ms (Top 92.53%) | Memory: 22.10 MB (Top 78.57%)
+
+from math import inf
+
+
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        dp = [0, float('-inf'), float('-inf')]
-        for x in nums:
-            dp_cp = dp[:]
-            for left in range(3):
-                right = (left + x) % 3
-                dp[right] = max(dp_cp[right], dp_cp[left] + x)
+        res = 0
+        r1_min1 = inf
+        r1_min2 = inf
+        r2_min1 = inf
+        r2_min2 = inf
 
-        return dp[0]
+        for v in nums:
+            res += v
+            if v % 3 == 1:
+                if v < r1_min1:
+                    r1_min2 = r1_min1
+                    r1_min1 = v
+                elif v < r1_min2:
+                    r1_min2 = v
+            elif v % 3 == 2:
+                if v < r2_min1:
+                    r2_min2 = r2_min1
+                    r2_min1 = v
+                elif v < r2_min2:
+                    r2_min2 = v
+
+        if res % 3 == 1:
+            res -= min(r1_min1, r2_min1 + r2_min2)
+        elif res % 3 == 2:
+            res -= min(r2_min1, r1_min1 + r1_min2)
+
+        return res
