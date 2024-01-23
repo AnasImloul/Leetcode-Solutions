da132c69fc1f56ab96f406843ed9942566bfd17b
AnasImloul <pydev27@gmail.com>
2024-01-23 13:31:06 +0100

    Runtime: 40 ms (Top 89.61%) | Memory: 19.60 MB (Top 55.06%)

--- scripts/algorithms/R/Rearrange Words in a Sentence/Rearrange Words in a Sentence.py
+++ scripts/algorithms/R/Rearrange Words in a Sentence/Rearrange Words in a Sentence.py
@@ -1,6 +1,8 @@
+// Runtime: 40 ms (Top 89.61%) | Memory: 19.60 MB (Top 55.06%)
+
 class Solution:
     def arrangeWords(self, text: str) -> str:
-        l=list(text.split(" "))
-        l=sorted(l,key= lambda word: len(word))
-        l=' '.join(l)
-        return l.capitalize()
+        a = []
+        for x in text.split(" "):
+            a.append(x.lower())
+        return " ".join(sorted(a, key=len)).capitalize()
