86f12768764eb5f104be3eea21cbc6ee9dbd1262
AnasImloul <pydev27@gmail.com>
2024-01-23 13:13:11 +0100

    Runtime: 226 ms (Top 89.51%) | Memory: 16.80 MB (Top 56.81%)

--- scripts/algorithms/K/Knight Dialer/Knight Dialer.py
+++ scripts/algorithms/K/Knight Dialer/Knight Dialer.py
@@ -1,32 +1,33 @@
+// Runtime: 226 ms (Top 89.51%) | Memory: 16.80 MB (Top 56.81%)
+
 class Solution:
+    MOD = 10**9 + 7
+
     def knightDialer(self, n: int) -> int:
-        # Sum elements of matrix modulo mod subroutine
-        def sum_mat(matrix, mod):
-            return sum(sum(row) % mod for row in matrix) % mod
-        
-        # Matrix multiplication subroutine
-        def mult_mat(a, b):
-            return [[sum(a[i][k]*b[k][j] for k in range(10)) for j in range(10)] for i in range(10)]
-        
-        # Matrix exponentiation subroutine
-        def pow_mat(matrix, k, dp):
-            if k not in dp:
-                if k == 0:
-                    dp[k] = [[(1 if i == j else 0) for j in range(10)] for i in range(10)]
-                else:
-                    dp[k] = pow_mat(matrix, k//2, dp)
-                    dp[k] = mult_mat(dp[k], dp[k])
-                    if k % 2:
-                        dp[k] = mult_mat(dp[k], matrix)
-            return dp[k]
-       
-        # Create matrix
-        edges = [(1, 6), (1, 8), (2, 9), (2, 7), (3, 4), (3, 8), (4, 0), (4, 9), (6, 1), (6, 0), (6, 7)]
-        matrix =  [[0 for j in range(10)] for i in range(10)]
-        for i, j in edges:
-            matrix[i][j] = 1
-            matrix[j][i] = 1
-        
-		# Solve
-        mod = 10**9 + 7
-        return sum_mat(pow_mat(matrix, n-1, {}), mod)
+        # Initialize an array to store the possible positions of the knight on the phone pad
+        cur_pos = [1] * 10
+
+        # Loop through the number of jumps required
+        for jump in range(2, n + 1):
+            # Create a new list to store the updated positions after each jump
+            new_pos = [0] * 10
+
+            # Calculate the new positions based on the valid knight moves
+            new_pos[0] = (cur_pos[6] + cur_pos[4]) % self.MOD
+            new_pos[1] = (cur_pos[6] + cur_pos[8]) % self.MOD
+            new_pos[2] = (cur_pos[7] + cur_pos[9]) % self.MOD
+            new_pos[3] = (cur_pos[4] + cur_pos[8]) % self.MOD
+            new_pos[4] = (cur_pos[0] + cur_pos[3] + cur_pos[9]) % self.MOD
+            new_pos[5] = 0  # Knight cannot move to position 5
+            new_pos[6] = (cur_pos[0] + cur_pos[1] + cur_pos[7]) % self.MOD
+            new_pos[7] = (cur_pos[2] + cur_pos[6]) % self.MOD
+            new_pos[8] = (cur_pos[1] + cur_pos[3]) % self.MOD
+            new_pos[9] = (cur_pos[2] + cur_pos[4]) % self.MOD
+
+            # Update the current positions list for the next iteration
+            cur_pos = new_pos
+
+        # Calculate the total count of distinct phone numbers
+        total_count = sum(cur_pos) % self.MOD
+
+        return total_count
