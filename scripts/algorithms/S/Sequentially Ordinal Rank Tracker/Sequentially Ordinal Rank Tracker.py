21751c602335d2cbb63857599c7b534c1aab997f
AnasImloul <pydev27@gmail.com>
2024-01-23 13:01:31 +0100

    Runtime: 675 ms (Top 95.65%) | Memory: 40.70 MB (Top 63.04%)

--- scripts/algorithms/S/Sequentially Ordinal Rank Tracker/Sequentially Ordinal Rank Tracker.py
+++ scripts/algorithms/S/Sequentially Ordinal Rank Tracker/Sequentially Ordinal Rank Tracker.py
@@ -1,43 +1,21 @@
-# Runtime: 1341 ms (Top 80.70%) | Memory: 40.9 MB (Top 9.94%)
+// Runtime: 675 ms (Top 95.65%) | Memory: 40.70 MB (Top 63.04%)
 
-from heapq import *
-
-class MinHeapItem:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
-    def __lt__(self, other):
-        return self.score < other.score or \
-               (self.score == other.score and self.name > other.name)
-
-class MaxHeapItem:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
-    def __lt__(self, other):
-        return self.score > other.score or \
-               (self.score == other.score and self.name < other.name)
-
+from sortedcontainers import SortedList
 class SORTracker:
+
     def __init__(self):
-        self.min_heap = []
-        self.max_heap = []
-        self.i = 1
-
+        self.arr=SortedList()
+        self.index=-1
     def add(self, name: str, score: int) -> None:
-        current = MinHeapItem(name, score)
-        if len(self.min_heap) < self.i:
-            heappush(self.min_heap, current)
-        elif current > self.min_heap[0]:
-            temp = heapreplace(self.min_heap, current)
-            heappush(self.max_heap, MaxHeapItem(temp.name, temp.score))
-        else:
-            heappush(self.max_heap, MaxHeapItem(name, score))
-
+        self.arr.add([-score,name])
+        
     def get(self) -> str:
-        ans = self.min_heap[0].name
-        self.i += 1
-        if self.max_heap:
-            temp = heappop(self.max_heap)
-            heappush(self.min_heap, MinHeapItem(temp.name, temp.score))
-        return ans
+       
+        self.index+=1
+        return self.arr[self.index][1]
+
+
+# Your SORTracker object will be instantiated and called as such:
+# obj = SORTracker()
+# obj.add(name,score)
+# param_2 = obj.get()
