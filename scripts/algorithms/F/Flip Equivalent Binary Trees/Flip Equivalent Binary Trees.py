28833c3621d696eaa74da6449115033bb04b19e4
AnasImloul <pydev27@gmail.com>
2024-01-24 12:47:34 +0100

    Runtime: 32 ms (Top 89.57%) | Memory: 16.70 MB (Top 53.69%)

--- scripts/algorithms/F/Flip Equivalent Binary Trees/Flip Equivalent Binary Trees.py
+++ scripts/algorithms/F/Flip Equivalent Binary Trees/Flip Equivalent Binary Trees.py
@@ -1,18 +1,19 @@
+// Runtime: 32 ms (Top 89.57%) | Memory: 16.70 MB (Top 53.69%)
+
 class Solution:
-    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def swap(root):
-            if root.right is None or (root.left is not None and root.left.val > root.right.val):
-                root.left, root.right = root.right, root.left
-        
-        def equal(root1, root2):
-            if root1 is None or root2 is None:
-                return root1 is None and root2 is None
-            
-            swap(root1)
-            swap(root2)
-            
-            return (root1.val == root2.val and
-                    equal(root1.left, root2.left) and
-                    equal(root1.right, root2.right))
-        
-        return equal(root1, root2)
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        queue = deque([(root1, root2)])
+        while queue:
+            node1, node2 = queue.pop()
+            if (not node1) and (not node2):
+                continue
+            elif (not node1) or (not node2) or (node1.val != node2.val):
+                return False
+            L1, R1, L2, R2 = node1.left, node1.right, node2.left, node2.right
+            if (L1 and L2 and L1.val == L2.val) or (R1 and R2 and R1.val == R2.val):
+                queue.append((L1, L2))
+                queue.append((R1, R2))
+            else:
+                queue.append((L1, R2))
+                queue.append((L2, R1))
+        return True
