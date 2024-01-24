90aee5ee858b730f8dc7edef17149e7eee3b6ce2
AnasImloul <pydev27@gmail.com>
2024-01-24 13:22:26 +0100

    Runtime: 460 ms (Top 99.07%) | Memory: 53.10 MB (Top 90.93%)

--- scripts/algorithms/A/Adding Spaces to a String/Adding Spaces to a String.py
+++ scripts/algorithms/A/Adding Spaces to a String/Adding Spaces to a String.py
@@ -1,14 +1,13 @@
-#TLE SOLUTION
+// Runtime: 460 ms (Top 99.07%) | Memory: 53.10 MB (Top 90.93%)
+
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-		spaces_idx = len(spaces)-1
-
-		def addString(index,s):
-			s = s[:index]+" "+s[index:] 
-			#This changes the string at every instance hence taking so much time
-			return s
-			
-		while spaces_idx>=0:
-			s = addString(spaces[spaces_idx],s)
-			spaces_idx-=1
-		return s
+        
+        arr = []
+        prev = 0
+        for space in spaces:
+            arr.append(s[prev:space])
+            prev = space
+        arr.append(s[space:])
+       
+        return " ".join(arr)
