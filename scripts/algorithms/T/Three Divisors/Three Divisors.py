9308dbc6d50dea221df71514ce0b6e4a3828db32
AnasImloul <pydev27@gmail.com>
2024-01-24 12:53:22 +0100

    Runtime: 34 ms (Top 88.51%) | Memory: 16.40 MB (Top 53.4%)

--- scripts/algorithms/T/Three Divisors/Three Divisors.py
+++ scripts/algorithms/T/Three Divisors/Three Divisors.py
@@ -1,14 +1,5 @@
-# Runtime: 73 ms (Top 19.18%) | Memory: 13.9 MB (Top 56.58%)
-import math
+// Runtime: 34 ms (Top 88.51%) | Memory: 16.40 MB (Top 53.4%)
+
 class Solution:
     def isThree(self, n: int) -> bool:
-        primes = {3:1, 5:1, 7:1, 11:1, 13:1, 17:1, 19:1, 23:1, 29:1, 31:1, 37:1, 41:1, 43:1, 47:1, 53:1, 59:1, 61:1, 67:1, 71:1, 73:1, 79:1, 83:1, 89:1, 97:1}
-        if n == 4:
-            return True
-        else:
-            a = math.sqrt(n)
-
-            if primes.get(a,0):
-                return True
-            else:
-                return False
+        return sum(n%i == 0 for i in range(1, n+1)) == 3
