06b9d51db886788ed5819607407cf230d0a7a8a0
AnasImloul <pydev27@gmail.com>
2024-01-24 13:12:40 +0100

    Runtime: 47 ms (Top 83.49%) | Memory: 16.60 MB (Top 61.19%)

--- scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.py
+++ scripts/algorithms/F/Final Prices With a Special Discount in a Shop/Final Prices With a Special Discount in a Shop.py
@@ -1,14 +1,10 @@
+// Runtime: 47 ms (Top 83.49%) | Memory: 16.60 MB (Top 61.19%)
+
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        ## RC ##
-        ## APPROACH : STACK ##
-        ## LOGIC ## 
-        ## 1. Use Monotonic Increasing Stack Concept
-        ## 2. Main idea is to find the Next Smaller Element in O(N) using #1
-        stack = []
-        for i, num in enumerate(prices):
-            while(stack and prices[stack[-1]] >= num):
-                popIndex = stack.pop()
-                prices[popIndex] -= num
-            stack.append(i)
-        return prices
+        for i in range(len(prices)):
+          for j in range(i+1,len(prices)):
+            if prices[j]<=prices[i]:
+              prices[i]=prices[i]-prices[j]
+              break
+        return (prices)
