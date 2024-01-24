fc850bfe749de5a24a40db1e4e70935671a1b9ad
AnasImloul <pydev27@gmail.com>
2024-01-24 13:06:58 +0100

    Runtime: 147 ms (Top 96.67%) | Memory: 19.20 MB (Top 63.7%)

--- scripts/algorithms/M/Minimum Cost to Hire K Workers/Minimum Cost to Hire K Workers.py
+++ scripts/algorithms/M/Minimum Cost to Hire K Workers/Minimum Cost to Hire K Workers.py
@@ -1,16 +1,24 @@
-# Runtime: 198 ms (Top 98.00%) | Memory: 16.6 MB (Top 33.56%)
-from heapq import heappush, heappop
+// Runtime: 147 ms (Top 96.67%) | Memory: 19.20 MB (Top 63.7%)
+
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        quality_efficiency = sorted([(wage[i]/quality[i],i) for i in range(len(quality))])
-        e = quality_efficiency[k-1][0]
-        heap = [-quality[i] for _, i in quality_efficiency[:k]]
-        heapify(heap)
-        total = -sum(heap)
-        res = total*e
-        for e, i in quality_efficiency[k:]:
-            heappush(heap,-quality[i])
-            total += quality[i] + heappop(heap)
-            if total*e < res:
-                res = total*e
-        return res
+        n=len(wage)
+        arr=[[wage[i]/quality[i],quality[i]] for i in range(n)]
+        arr.sort(key=lambda x:x[0])
+        kSmallest=0
+        pq=[]
+        for i in range(k):
+            heapq.heappush(pq,-arr[i][1])
+            kSmallest+=arr[i][1]
+        minCost=arr[k-1][0]*kSmallest
+        for c in range(k,n):
+            if pq and abs(pq[0])>arr[c][1]:
+                qRem=-heappop(pq)
+                kSmallest-=qRem
+                kSmallest+=arr[c][1]
+                heappush(pq,-arr[c][1])
+            minCost=min(minCost,arr[c][0]*kSmallest)
+        return minCost
+            
+        
+        
