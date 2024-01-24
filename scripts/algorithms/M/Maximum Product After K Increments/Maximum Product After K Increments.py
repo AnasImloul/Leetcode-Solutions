c0b9b145dc41430e59ef201055ea19dd1e029664
AnasImloul <pydev27@gmail.com>
2024-01-24 13:20:20 +0100

    Runtime: 985 ms (Top 85.38%) | Memory: 27.10 MB (Top 71.7%)

--- scripts/algorithms/M/Maximum Product After K Increments/Maximum Product After K Increments.py
+++ scripts/algorithms/M/Maximum Product After K Increments/Maximum Product After K Increments.py
@@ -1,26 +1,14 @@
-import heapq
+// Runtime: 985 ms (Top 85.38%) | Memory: 27.10 MB (Top 71.7%)
 
 class Solution:
-    def maximumProduct(self, nums, k: int) -> int:
-        
-        # creating a heap
-        heap = []
-        for i in nums:
-            heapq.heappush (heap,i)
-            
-            
-        # basic idea here is keep on incrementing smallest number, then only multiplication of that number will be greater
-        # so basically till I have operations left I will increment my smallest number
-        while k :
-            current = heapq.heappop(heap)
-            heapq.heappush(heap, current+1)
-            k-=1
-            
-        result =1
-        
-        # Just Multiply all the numbers in heap and return the value
-        while len(heap)>0:
-            x= heapq.heappop(heap)
-            result =(result*x )% (10**9+7)
-            
-        return result
+    def maximumProduct(self, nums: List[int], k: int) -> int:
+        heap = nums.copy()
+        heapify(heap)
+        for i in range(k):
+            t = heappop(heap)
+            heappush(heap, t + 1)
+        ans = 1
+        mod = 1000000007
+        for i in heap:
+            ans = (ans*i) % mod
+        return ans
