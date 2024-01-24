662eb967fec50da3590dd99f8d8659b662ffeea7
AnasImloul <pydev27@gmail.com>
2024-01-24 12:48:44 +0100

    Runtime: 34 ms (Top 84.96%) | Memory: 16.60 MB (Top 62.78%)

--- scripts/algorithms/B/Binary String With Substrings Representing 1 To N/Binary String With Substrings Representing 1 To N.py
+++ scripts/algorithms/B/Binary String With Substrings Representing 1 To N/Binary String With Substrings Representing 1 To N.py
@@ -1,8 +1,10 @@
+// Runtime: 34 ms (Top 84.96%) | Memory: 16.60 MB (Top 62.78%)
+
 class Solution:
-    def queryString(self, s: str, n: int) -> bool:
-        
-        for i in range(1,n+1):
-            if bin(i)[2:] not in s:
-                return False
-        return True
-        
+    def queryString(self, S: str, N: int) -> bool:
+        ans = set()
+        for i in range(len(S)):
+            for ii in range(i, i + N.bit_length()): 
+                x = int(S[i:ii+1], 2)
+                if 1 <= x <= N: ans.add(x)
+        return len(ans) == N
