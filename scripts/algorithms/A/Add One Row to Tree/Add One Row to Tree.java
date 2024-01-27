7d31fda4f476e042d46cc7c25e3b06d5c0c271ce
AnasImloul <pydev27@gmail.com>
2024-01-27 21:22:25 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 44.40 MB (Top 37.52%)

--- scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.java
+++ scripts/algorithms/A/Add One Row to Tree/Add One Row to Tree.java
@@ -1,40 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        findAndAdd(root, val, depth, 1);
-        return root; 
-    }
+// Runtime: 0 ms (Top 100.0%) | Memory: 44.40 MB (Top 37.52%)
 
-    public void findAndAdd(TreeNode root, int val, int depth, int currDepth){
-        if(depth == 1 && currDepth == 1){
-            root.left = new TreeNode(root.val, root.left, root.right);
-            root.right = null;
-            root.val = val;
-            return;
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) return new TreeNode(v, root, null);
+        else if (d == 2) {
+            root.left = new TreeNode(v, root.left, null);
+            root.right = new TreeNode(v, null, root.right);
+        } else {
+            if (root.left != null) addOneRow(root.left, v, d-1);
+            if (root.right != null) addOneRow(root.right, v, d-1);
         }
-        if(root == null)
-            return;
-        if(currDepth == depth - 1){
-            root.left = new TreeNode(val, root.left, null);
-            root.right = new TreeNode(val, null, root.right);
-        }else{
-            findAndAdd(root.left, val, depth, currDepth + 1);
-            findAndAdd(root.right, val, depth, currDepth + 1);
-        }
-        return;
+        return root;
     }
 }
