26c4ae774d61bbefc03dedb7beaa712fca798090
AnasImloul <pydev27@gmail.com>
2024-01-27 21:10:18 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 41.60 MB (Top 23.65%)

--- scripts/algorithms/F/First Letter to Appear Twice/First Letter to Appear Twice.java
+++ scripts/algorithms/F/First Letter to Appear Twice/First Letter to Appear Twice.java
@@ -1,13 +1,12 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 41.60 MB (Top 23.65%)
+
 class Solution {
     public char repeatedCharacter(String s) {
-        HashSet<Character> hset = new HashSet<>();
-        for(char ch:s.toCharArray())
-        {
-            if(hset.contains(ch))
-                return ch;
-            else
-                hset.add(ch);
+        HashSet<Character> set = new HashSet<>();//Create a set of characters
+        for(int i = 0 ; i < s.length() ; i++){
+            if(set.contains(s.charAt(i))) return s.charAt(i);//If the set already contains the current character, then it is the required ans
+            set.add(s.charAt(i));
         }
-        return ' ';
+        return 'a';//As it is given in the question that there is at least one letter that appears twice, therefore it is certain that the ans will be found before we reach this statement. So, just adding any random return statement so that there is no error in the code.
     }
 }
