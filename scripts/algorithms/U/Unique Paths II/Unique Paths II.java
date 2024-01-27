125688bfb2bd9697d78bc89a6d00aad8bb976a13
AnasImloul <pydev27@gmail.com>
2024-01-27 21:38:45 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 41.40 MB (Top 60.59%)

--- scripts/algorithms/U/Unique Paths II/Unique Paths II.java
+++ scripts/algorithms/U/Unique Paths II/Unique Paths II.java
@@ -1,20 +1,15 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 41.40 MB (Top 60.59%)
+
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int[][] path = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-			//if there is an obstacle at the current block then you cannot get there
-                if(obstacleGrid[i][j]!=1){
-                    if(i==0 && j==0)
-                        path[i][j] = 1;
-                    if(i>0)
-                        path[i][j] += path[i-1][j];
-                    if(j>0)
-                        path[i][j] += path[i][j-1];
-                }
-            }
-        }
-        return path[--m][--n];
+    public int uniquePathsWithObstacles(int[][] OG) {
+        if (OG[0][0] == 1) return 0;
+        int m = OG.length, n = OG[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (OG[i][j] == 1 || (i == 0 && j == 0)) continue;
+                else dp[i][j] = (i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0);
+        return dp[m-1][n-1];
     }
 }
