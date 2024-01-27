8677f76bde32f84bb809c4698562820bf0e8f973
AnasImloul <pydev27@gmail.com>
2024-01-27 21:11:35 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 40.80 MB (Top 7.7%)

--- scripts/algorithms/I/Integer Break/Integer Break.java
+++ scripts/algorithms/I/Integer Break/Integer Break.java
@@ -1,14 +1,26 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 40.80 MB (Top 7.7%)
+
 class Solution {
     public int integerBreak(int n) {
-        //dp array: maximum product of splitling int i
-        int[] dp = new int[n + 1];
-        
-        // traverse
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i / 2; j++) {
-                 dp[i] = Math.max(Math.max(j * (i - j), j * dp[i - j]), dp[i]);
-            }
+        if (n <= 1) {
+            return 0;
         }
-        return dp[n];
+        int[] memo = new int[n + 1];
+        return maxProduct(n, memo);
+    }
+    
+    private int maxProduct(int n, int[] memo) {
+        if (n == 2) {
+            return 1;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, Math.max(i * (n - i), i * maxProduct(n - i, memo)));
+        }
+        memo[n] = max;
+        return max;
     }
 }
