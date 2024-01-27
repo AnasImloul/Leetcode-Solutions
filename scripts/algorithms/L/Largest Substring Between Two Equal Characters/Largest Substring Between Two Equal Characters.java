3ea7d037b1a764d216b47445accada3eb5bb7e51
AnasImloul <pydev27@gmail.com>
2024-01-27 21:31:34 +0100

    Runtime: 1 ms (Top 92.59%) | Memory: 41.10 MB (Top 80.55%)

--- scripts/algorithms/L/Largest Substring Between Two Equal Characters/Largest Substring Between Two Equal Characters.java
+++ scripts/algorithms/L/Largest Substring Between Two Equal Characters/Largest Substring Between Two Equal Characters.java
@@ -1,16 +1,26 @@
+// Runtime: 1 ms (Top 92.59%) | Memory: 41.10 MB (Top 80.55%)
+
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
+        int[] v1 = new int[26];
+        int[] v2 = new int[26];
+        Arrays.fill(v1, -1);
+        Arrays.fill(v2, -1);
         int ans = -1;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch  = s.charAt(i);
-            if (map.containsKey(ch)) {
-                ans = Math.max(ans, i - 1 - map.get(ch));
-            }
-            else {
-                map.put(ch, i);
+
+        for (int i = 0; i < s.length(); ++i) {
+            int temp = s.charAt(i) - 'a';
+
+            if (v1[temp] == -1) {
+                v1[temp] = i;
+            } else {
+                v2[temp] = i;
+                ans = Math.max(ans, v2[temp] - v1[temp] - 1);
             }
         }
+
         return ans;
     }
 }
+
+
