41110c312bf565105896beb13cedd4fcf50388c4
AnasImloul <pydev27@gmail.com>
2024-01-27 21:17:19 +0100

    Runtime: 1 ms (Top 100.0%) | Memory: 41.60 MB (Top 67.53%)

--- scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
+++ scripts/algorithms/L/Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
@@ -1,25 +1,27 @@
+// Runtime: 1 ms (Top 100.0%) | Memory: 41.60 MB (Top 67.53%)
+
 class Solution {
     public int longestSubsequence(String s, int k) {
-        int numOfZeros = 0;
-        int numOfOnes = 0;
+        int z=0;
+        //count zero
+        for(int i=0;i<s.length();i++)if(s.charAt(i)=='0')z++;
+
+        int num=0,base=1,len=0;
+		//take as many ones from right as possible until they do not make the num>k
+        for(int i=s.length()-1;i>=0;i--){
+            
+            if(num+base>k)break;
+            if(s.charAt(i)=='1'){
+               num+=base;
+            }
+            else {
+			//remove already taken zeros from zeros count
+                z--;
+            }
+            base*=2;
+            len++;
+        }
         
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '0'){
-                numOfZeros++;
-            }
-        }
-        int sum = 0;
-        for(int i = s.length() - 1; i >= 0; i--){
-            char ch = s.charAt(i);
-            if(ch == '1'){
-                double val = Math.pow(2, s.length() - i - 1);
-                sum += val;
-                if(sum <= k){
-                    numOfOnes++;
-                }
-            }
-        }
-        return numOfZeros + numOfOnes;
+        return len+z;
     }
 }
