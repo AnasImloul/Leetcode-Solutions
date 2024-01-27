340d74579947d490e34256ae9c521ae909a308e3
AnasImloul <pydev27@gmail.com>
2024-01-27 21:36:30 +0100

    Runtime: 24 ms (Top 68.74%) | Memory: 81.70 MB (Top 14.26%)

--- scripts/algorithms/A/Adding Spaces to a String/Adding Spaces to a String.java
+++ scripts/algorithms/A/Adding Spaces to a String/Adding Spaces to a String.java
@@ -1,22 +1,15 @@
+// Runtime: 24 ms (Top 68.74%) | Memory: 81.70 MB (Top 14.26%)
+
 class Solution {
     public String addSpaces(String s, int[] spaces) {
-        
-        int j = 0,curr = 0;
-        StringBuilder sb = new StringBuilder();
-        while(curr<s.length()){
-           
-             if(j<spaces.length&&spaces[j]==curr){
-                 
-                sb.append(" ");
-                j++;
-    
-                }
-                sb.append(s.charAt(curr));
-                curr++;
-
+        StringBuilder sb=new StringBuilder();
+        int k=0;
+        for(int i=0;i<spaces.length;i++){
+            sb.append(s.substring(k,spaces[i]));
+            k=spaces[i];
+            sb.append(" ");
         }
-
+        sb.append(s.substring(k,s.length()));
         return sb.toString();
-        
     }
 }
