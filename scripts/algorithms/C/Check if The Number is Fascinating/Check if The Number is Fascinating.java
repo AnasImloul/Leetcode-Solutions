e3b77483bfa7aa56a5468328ea2d3f4e11bc6245
AnasImloul <pydev27@gmail.com>
2024-01-27 21:27:48 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 40.80 MB (Top 65.52%)

---
+++ scripts/algorithms/C/Check if The Number is Fascinating/Check if The Number is Fascinating.java
@@ -0,0 +1,7 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 40.80 MB (Top 65.52%)
+
+class Solution {
+    public boolean isFascinating(int n) {
+        return n == 192 || n == 219 || n==273 || n==327;
+    }
+}
