6823c13aedb3e224da0114a3aba909715a7bdaab
AnasImloul <pydev27@gmail.com>
2024-01-27 20:59:13 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 41.30 MB (Top 42.36%)

--- scripts/algorithms/C/Cousins in Binary Tree/Cousins in Binary Tree.java
+++ scripts/algorithms/C/Cousins in Binary Tree/Cousins in Binary Tree.java
@@ -1,27 +1,78 @@
-// Runtime: 2 ms (Top 20.27%) | Memory: 42 MB (Top 40.06%)
+// Runtime: 0 ms (Top 100.0%) | Memory: 41.30 MB (Top 42.36%)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        Set<TreeNode> parentSet = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        
+        //If any of x or y is at root, it means they can't be at same depth. Return false.
+        if(root.val == x || root.val == y) return false;
+        
+        
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        boolean xFound = false;
+        boolean yFound = false;
+        
+        int parentX = 0;
+        int parentY = 0;
+        
+        //Do level-order traversal until x or y is found or queue is empty.
+        while(!queue.isEmpty() && !xFound && !yFound){
+            
+            int size = queue.size();
+			
+            //Traverse that level.
+            while(size-- > 0){
+                TreeNode node = queue.poll();
+				
+                //if x or y is found at left/right, save the parent and set the "found" flag to true.
+                //This flag will break the loop as soon as any one (x or y) is found. 
+				//we don't need to go deeper to find second if it isn't found at this level.
+				
+                if(node.left != null) {
+                    queue.offer(node.left);
+                    
+                    if(node.left.val == x){
+                        parentX = node.val;
+                        xFound = true;
+                    }
+                    if(node.left.val == y){
+                        parentY = node.val;
+                        yFound = true;
+                    }
+                    
+                }
 
-        while (!q.isEmpty()) {
-            int len = q.size();
-
-            for (int i = 0; i < len; i++) {
-                TreeNode parent = q.remove();
-
-                for (TreeNode child : new TreeNode[]{parent.left, parent.right}) {
-                    if(child != null) {
-                        q.add(child);
-                        if (child.val == x || child.val == y)
-                            parentSet.add(parent);
+                if(node.right != null) {
+                    queue.offer(node.right);
+                    
+                    if(node.right.val == x){
+                        parentX = node.val;
+                        xFound = true;
+                    }
+                    if(node.right.val == y){
+                        parentY = node.val;
+                        yFound = true;
                     }
                 }
             }
-            if (parentSet.size() > 0)
-                return parentSet.size() == 2; //if same parent -> set size wil be 1
+            
         }
-        return false;
+        return xFound && yFound && parentX != parentY;
     }
 }
