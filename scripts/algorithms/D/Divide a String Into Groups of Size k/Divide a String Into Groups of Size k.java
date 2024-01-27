9aab5df4c89321551bf9036c5de827631834bc07
AnasImloul <pydev27@gmail.com>
2024-01-27 21:34:40 +0100

    Runtime: 1 ms (Top 96.0%) | Memory: 42.30 MB (Top 49.23%)

--- scripts/algorithms/D/Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.java
+++ scripts/algorithms/D/Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.java
@@ -1,19 +1,28 @@
+// Runtime: 1 ms (Top 96.0%) | Memory: 42.30 MB (Top 49.23%)
+
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        int rem = 0;
-        if( s.length() % k != 0){
-             rem = k - s.length() % k; //counting the total positions where we have to fill the char "fill".
+        //the size of result array
+        String[] r = new String[(int) Math.ceil((s.length() * 1.0) / k)];
+        int idx;
+        //if the last one needs to be filled, then I do it
+        if (s.length() % k != 0) {
+            idx = s.length() - (s.length() % k);
+            StringBuilder sb = new StringBuilder();
+            sb.append(s.substring(idx));
+            for (int i = sb.length(); i < k; i++) {
+                sb.append(fill);
+            }
+            r[r.length - 1] = sb.toString();
+        } else {
+            //no need to fill the last, 
+            //that's why idx is equal to s length
+            idx = s.length();
         }
-        for(int i = 0; i < rem; i++){
-            s = s+fill; //appending the char to the String
+        for (int i = 0; i < idx; i+=k) {
+            r[i / k] = s.substring(i, i + k);
         }
         
-        String[] strs = new String[s.length()/k];  //the length will be String length / K because we are making pair of k length Strings.
-        int index = 0;  //index for array.
-        for(int i = 0; i < s.length(); i+=k){
-            strs[index] = s.substring(i, i+k); //substing of  current element to Kth element. 
-            index++;
-        }
-        return strs;
+        return r;
     }
 }
