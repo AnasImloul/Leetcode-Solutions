a688a622eb5f1bbf1d420a6d8410567e1f09380d
AnasImloul <pydev27@gmail.com>
2024-01-27 21:07:00 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 41.80 MB (Top 5.91%)

--- scripts/algorithms/M/Minimum Swaps to Make Strings Equal/Minimum Swaps to Make Strings Equal.java
+++ scripts/algorithms/M/Minimum Swaps to Make Strings Equal/Minimum Swaps to Make Strings Equal.java
@@ -1,17 +1,24 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 41.80 MB (Top 5.91%)
+
 class Solution {
     public int minimumSwap(String s1, String s2) {
-        if(s1.length() != s2.length()) return -1;
-		int n = s1.length();
-        int x = 0 , y = 0;
         
-        for(int i = 0 ; i < n ; i ++){
-            char c1 = s1.charAt(i) , c2 = s2.charAt(i);
-            if(c1 == 'x' && c2 == 'y') x++;
-            else if(c1 == 'y' && c2 == 'x') y++;
+        int count = 0;
+        int cnt_1 = 0;
+        int cnt_2 = 0;
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i] != b[i]){
+                count++;
+                if(a[i] == 'x') cnt_1++;
+                else cnt_2++;
+            }
         }
         
-        if(x % 2 == 0 && y % 2 == 0) return x/2 + y/2;
-        else if(x % 2 == 1 && y % 2 == 1) return x/2 + y/2 + 2;
-        return -1;
+        if(count%2 == 1)
+            return -1;
+        else return cnt_1/2+cnt_2/2+(cnt_1%2)*2;
     }
 }
