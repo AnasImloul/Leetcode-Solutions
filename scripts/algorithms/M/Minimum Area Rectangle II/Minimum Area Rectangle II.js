2d39e0f4aba2313441072ca8049ae80bade337e7
AnasImloul <pydev27@gmail.com>
2024-01-28 12:33:37 +0100

    Runtime: 98 ms (Top 66.67%) | Memory: 61.40 MB (Top 66.67%)

--- scripts/algorithms/M/Minimum Area Rectangle II/Minimum Area Rectangle II.js
+++ scripts/algorithms/M/Minimum Area Rectangle II/Minimum Area Rectangle II.js
@@ -1,73 +1,40 @@
-/**
- * @param {number[][]} points
- * @return {number}
- */
+// Runtime: 98 ms (Top 66.67%) | Memory: 61.40 MB (Top 66.67%)
+
 var minAreaFreeRect = function(points) {
-    let len = points.length;
-    
-    if (len < 4) return 0;
-    
-    // Helper function to calculate distance
-    // between two points, optionally return 
-    // without sqrt if want to use as key
-    const getDistance = (a, b, isKey = false) => {
-        let [xa, ya] = a;
-        let [xb, yb] = b;
-        
-        const distance = (xa - xb) * (xa - xb) + (ya - yb) * (ya - yb);
-        return isKey ? distance : Math.sqrt(distance);
-    }
-    
-    let map = new Map();
-    
-    // let's loop over all points and find all possible
-    // diagonals and calc - dis with midpoints and 
-    // save as key with co-ordinates as values
-    for (let i = 0; i < len; i += 1) {
-        for (let j = i + 1; j < len; j += 1) {
-            let disKey = getDistance(points[i], points[j], true);
-            let [xa, ya] = points[i];
-            let [xb, yb] = points[j];
-            
-            let x = (xa + xb) / 2;
-            let y = (ya + yb) / 2;
-            
-            let key = `${disKey}-${x}-${y}`;
-            
-            let list = [];
-            
-            if (!map.has(key)) {
-                map.set(key, list)
-            } else list = map.get(key);
-            
-            list.push([i, j]);
-            
-            map.set(key, list);
+    const diagonalMap = new Map();
+    let result = Infinity;
+
+    for (let a = 0; a < points.length - 1; a++) {
+        const [x1, y1] = points[a];
+        for (let b = a + 1; b < points.length; b++) {
+            const [x2, y2] = points[b];
+            const diagonal = calculateeDiagonal(x1, y1, x2, y2);
+            const centerX = (x1 + x2) / 2;
+            const centerY = (y1 + y2) / 2;
+            const key = `${diagonal}_${centerX}_${centerY}`;
+            const vectors = diagonalMap.get(key) ?? [];
+
+            vectors.push({ x1, x2, y1, y2 });
+            diagonalMap.set(key, vectors);
         }
     }
-    
-    // console.log(map);
-    let res = Number.MAX_VALUE;
-    
-    // loop over map of keys above and
-    // only iterate through the list where at least 
-    // 2 set of co-ordinates have been found above
-    map.forEach((list, key) => {
-        if (list.length > 1) {
-            for (let i = 0; i < list.length; i += 1) {
-                for (let j = i + 1; j < list.length; j += 1) {
-                    let p1 = list[i][0];
-                    let p2 = list[j][0];
-                    let p3 = list[j][1];
-                    
-                    let l = getDistance(points[p1], points[p2]);
-                    let b = getDistance(points[p1], points[p3]);
-                    res = Math.min(res, l * b);
-                }
+
+    for (const [key, vector] of diagonalMap) {
+        if (vector.length < 2) continue;
+        for (let a = 0; a < vector.length - 1; a++) {
+            const vectorA = vector[a];
+            for (let b = a + 1; b < vector.length; b++) {
+                const vectorB = vector[b];
+                const w = Math.sqrt(calculateeDiagonal(vectorA.x1, vectorA.y1, vectorB.x1, vectorB.y1));
+                const h = Math.sqrt(calculateeDiagonal(vectorA.x1, vectorA.y1, vectorB.x2, vectorB.y2));
+
+                result = Math.min(result, w * h);
             }
         }
-    })
-    
-    return res === Number.MAX_VALUE ? 0 : res;
-    
+    }
+    return result === Infinity ? 0 : result;
+
+    function calculateeDiagonal(x1, y1, x2, y2) {
+        return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+    }
 };
