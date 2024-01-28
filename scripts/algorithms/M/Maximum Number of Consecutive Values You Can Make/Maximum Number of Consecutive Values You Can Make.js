561386fd05cf4812669fbec5288c1a885afb263d
AnasImloul <pydev27@gmail.com>
2024-01-28 12:43:42 +0100

    Runtime: 134 ms (Top 91.67%) | Memory: 59.20 MB (Top 25.0%)

--- scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.js
+++ scripts/algorithms/M/Maximum Number of Consecutive Values You Can Make/Maximum Number of Consecutive Values You Can Make.js
@@ -1,16 +1,11 @@
-// we iterate from the smallest number while maintaining an integer sum. sum means that we can produce all number from 0 to sum.
-// when we can make numbers from 0 to sum, and we encounter a new number, lets say arr[2].
-// it is obvious that we can create all numbers from sum to sum + arr[2].
-// if there is a gap if(arr[i] > sum+1) it means no combinations could create that number and we stop.
+// Runtime: 134 ms (Top 91.67%) | Memory: 59.20 MB (Top 25.0%)
+
 var getMaximumConsecutive = function(coins) {
-    coins.sort((a,b) => a-b);
-    let sum = 1;
-    for(let i = 0; i < coins.length; i++) {
-        if(coins[i] <= sum) {
-            sum += coins[i];
-        } else {
-            break;
-        }
+    coins.sort((a, b) => a - b);
+    let maxConsec = 1;
+    
+    for(let num of coins) {
+        if(num <= maxConsec) maxConsec += num;
     }
-    return sum;
+    return maxConsec;    
 };
