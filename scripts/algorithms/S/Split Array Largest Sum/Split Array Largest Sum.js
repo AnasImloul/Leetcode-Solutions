e2569745b6407da427ca2a1eba801d71949a9a53
AnasImloul <pydev27@gmail.com>
2024-01-28 12:41:14 +0100

    Runtime: 50 ms (Top 77.73%) | Memory: 49.00 MB (Top 11.74%)

--- scripts/algorithms/S/Split Array Largest Sum/Split Array Largest Sum.js
+++ scripts/algorithms/S/Split Array Largest Sum/Split Array Largest Sum.js
@@ -1,39 +1,38 @@
-// Runtime: 2555 ms (Top 5.03%) | Memory: 70 MB (Top 5.53%)
+// Runtime: 50 ms (Top 77.73%) | Memory: 49.00 MB (Top 11.74%)
+
 var splitArray = function(nums, m) {
-const n = nums.length;
-var mat = [];
-var sumArr = [nums[0]];
-for(let i=1; i<n; i++){
-    sumArr.push(sumArr[i-1]+nums[i]);//find prefix sum
-}
-mat.push(sumArr);
-for(let i=0; i<n-1;i++){//form prefix matrix, i.e. every row i shows prefix sum starting from i
-    let arr = new Array(n).fill(0);
-    for(let j=i+1; j<n; j++){
-        arr[j] = sumArr[j] - sumArr[i];
+    let low = Math.max(...nums);
+    let high = 0;
+    let ans = 0;
+    
+    for(let num of nums) {
+        high += num;
     }
-    mat.push(arr);
-}
-let memo = new Map();
-let recursion = (m,lastPartition) => {// recursive partition finder
-    if(memo[m+'_'+lastPartition]!==undefined){
-        return memo[m+'_'+lastPartition];//memoised
+    
+    while(low <= high) {
+        let mid = Math.floor(low + (high - low) / 2); // to prevent overflow
+        
+        if(isPossible(nums, mid, m)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
-    if(m==1){//base case, only 1 partition left
-        memo[m+'_'+lastPartition] = mat[lastPartition][mat[0].length-1];
-        return memo[m+'_'+lastPartition];
-    }
-    let min = Infinity;
-    let maxSum = -Infinity;
-    let lastval = Infinity;
-    for(let i = lastPartition; i<=n-m; i++){//for mth partition, find the min sum for all possible partition placements
-        while(i>0 && i<n-m && mat[lastPartition][i] == lastval) i++;//if a large set of values are repeating, i.e. a lot of 0s in original array, skip them
-        lastval = mat[lastPartition][i];
-        maxSum = Math.max(mat[lastPartition][i],recursion(m-1,i+1));//max of current subarray sum with max from the rest of the partitions
-        min = Math.min(min, maxSum);//minimum sum for all the possible partition values
-    }
-    memo[m+'_'+lastPartition] = min;
-    return memo[m+'_'+lastPartition];
-}
-return recursion(m,0);
+    return ans;
 };
+
+function isPossible(arr, mid, noOfParts) {
+    let part = 1;
+    let sum = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        
+        if(sum > mid) {
+            part++;
+            sum = arr[i];
+        }
+    }
+    return part <= noOfParts; 
+}
