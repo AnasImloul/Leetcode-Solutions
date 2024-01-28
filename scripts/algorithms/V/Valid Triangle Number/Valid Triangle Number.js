9a5b4c91c696226a7b830ba15b8eea4f14467acf
AnasImloul <pydev27@gmail.com>
2024-01-28 12:39:23 +0100

    Runtime: 103 ms (Top 39.24%) | Memory: 50.90 MB (Top 13.92%)

--- scripts/algorithms/V/Valid Triangle Number/Valid Triangle Number.js
+++ scripts/algorithms/V/Valid Triangle Number/Valid Triangle Number.js
@@ -1,26 +1,20 @@
-class Solution {
-    public int triangleNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int count =0;
-        for(int k = n-1; k>=2; k--)
-        {
-            int i = 0;
-            int j = k-1;
-            while(i < j)
-            {
-                int sum = nums[i] +nums[j];
-                if(sum > nums[k])
-                {
-                    count += j-i;
-                    j--;
-                }
-                else
-                {
-                    i++;
-                }
-            }
+// Runtime: 103 ms (Top 39.24%) | Memory: 50.90 MB (Top 13.92%)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangleNumber = function(nums) {
+    nums.sort((a,b) => a-b);
+    let result = 0, n = nums.length, i, j, k;
+    for(i = 0; i<n ; i++) {
+        for(j = i+1, k = j+1; j<(n - 1) && k<=n;) {
+            if(k == n || nums[i]+nums[j] <= nums[k]){
+                if(k > (j+1))
+                    result += k - j - 1;
+                j++;
+            }else k++;
         }
-        return count;
     }
-}
+    return result;
+};
