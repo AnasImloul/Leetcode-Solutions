1beeb30607b653a3dc90127a3ce8b2e951f5d3b7
AnasImloul <pydev27@gmail.com>
2024-01-28 12:15:57 +0100

    Runtime: 131 ms (Top 92.86%) | Memory: 68.70 MB (Top 7.14%)

--- scripts/algorithms/G/Generate Random Point in a Circle/Generate Random Point in a Circle.js
+++ scripts/algorithms/G/Generate Random Point in a Circle/Generate Random Point in a Circle.js
@@ -1,37 +1,16 @@
-/**
- * @param {number} radius
- * @param {number} x_center
- * @param {number} y_center
- */
+// Runtime: 131 ms (Top 92.86%) | Memory: 68.70 MB (Top 7.14%)
+
 var Solution = function(radius, x_center, y_center) {
-  this.radius = radius;
-  this.x_center = x_center;
-  this.y_center = y_center;
+    this.radius = radius;
+    this.x_center = x_center;
+    this.y_center = y_center;
 };
 
-/**
- * @return {number[]}
- */
 Solution.prototype.randPoint = function() {
-  const randomX = randRange(this.x_center + this.radius, this.x_center - this.radius);
-  const randomY = randRange(this.y_center + this.radius, this.y_center - this.radius);
-  
-  const distanceInSquares = Math.pow(randomX - this.x_center, 2) + Math.pow(randomY - this.y_center, 2);
-  const isOutOfTheCircle = distanceInSquares > Math.pow(this.radius, 2);
-  
-  if (isOutOfTheCircle)) {
-    return this.randPoint();
-  }
-
-  return [randomX, randomY];
+    let r = this.radius * Math.sqrt(Math.random());
+    let theta = Math.random() * 2 * Math.PI;
+    let x = this.x_center + r * Math.cos(theta);
+    let y = this.y_center + r * Math.sin(theta);
+    return [x, y];
 };
 
-/** 
- * Your Solution object will be instantiated and called as such:
- * var obj = new Solution(radius, x_center, y_center)
- * var param_1 = obj.randPoint()
- */
-
-function randRange(maximum, minimum) {
-  return Math.random() * (maximum - minimum) + minimum;
-}
