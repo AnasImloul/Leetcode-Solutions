b95af4edd206a7becf5f270ba7ddadb7d65a0bf8
AnasImloul <pydev27@gmail.com>
2024-01-28 12:17:10 +0100

    Runtime: 42 ms (Top 99.85%) | Memory: 52.20 MB (Top 5.11%)

--- scripts/algorithms/S/String Compression/String Compression.js
+++ scripts/algorithms/S/String Compression/String Compression.js
@@ -1,18 +1,27 @@
+// Runtime: 42 ms (Top 99.85%) | Memory: 52.20 MB (Top 5.11%)
+
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
 var compress = function(chars) {
-    for(let i = 0; i < chars.length; i++){
-        let count = 1
-        while(chars[i] === chars[i+count]){
-            delete chars[i+count]
-            count++
-        }
-        if(count > 1){
-            let count2 = 1
-            String(count).split('').forEach((a) =>{
-                chars[i+count2] = a
-                count2++
-            })
-        }
-        i = i + count -1
+  if (!chars.length) return 0;
+  let j = 0;
+  let cur = chars[0];
+  let counter = 0;
+  for (let i = 0; i <= chars.length; i++) {
+    if (chars[i] === cur) {
+      counter++;
+    } else {
+      chars[j] = cur;
+      if (counter > 1) {
+        const s = counter.toString();
+        for (let k = 0; k < s.length; k++) chars[++j] = s[k];
+      }
+      j++;
+      cur = chars[i];
+      counter = 1;
     }
-    return chars.flat().filter((x) => x).length
+  }
+  return j;
 };
