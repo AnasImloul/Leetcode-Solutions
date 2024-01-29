1529fa52de9df1583dc899dab298c870e5516a96
AnasImloul <pydev27@gmail.com>
2024-01-29 14:46:28 +0100

    Runtime: 1014 ms (Top 83.89%) | Memory: 42.00 MB (Top 62.92%)

--- scripts/algorithms/S/Shifting Letters II/Shifting Letters II.py
+++ scripts/algorithms/S/Shifting Letters II/Shifting Letters II.py
@@ -1,17 +1,18 @@
+// Runtime: 1014 ms (Top 83.89%) | Memory: 42.00 MB (Top 62.92%)
+
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        totalShifts = [0] * (n + 1)
+        d = [0 for _ in range(n + 1)]
         for start, end, direction in shifts:
-            if not direction:
-                direction = -1
-            totalShifts[start] += direction
-            totalShifts[end + 1] -= direction
-        
-        for i in range(1, n):
-            totalShifts[i] += totalShifts[i - 1]
+            dir = 1 if direction else -1
+            d[start] += dir
+            d[end + 1] -= dir
         
         res = []
-        for c, totalShift in zip(s, totalShifts):
-            res.append(chr((ord(c) - 97 + totalShift) % 26 + 97))
-        return ''.join(res)
+        for i in range(n):
+            if i != 0: 
+                d[i] += d[i - 1]
+            new_chr_ascii = (ord(s[i]) - ord("a") + d[i]) % 26 + ord("a")
+            res.append(chr(new_chr_ascii))
+        return "".join(res)
