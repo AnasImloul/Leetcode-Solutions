a3438bbb5a1f5c7a5f2bae83a8ce22b786c1f162
AnasImloul <pydev27@gmail.com>
2024-01-29 14:42:41 +0100

    Runtime: 675 ms (Top 89.58%) | Memory: 23.80 MB (Top 95.52%)

--- scripts/algorithms/C/Convert 1D Array Into 2D Array/Convert 1D Array Into 2D Array.py
+++ scripts/algorithms/C/Convert 1D Array Into 2D Array/Convert 1D Array Into 2D Array.py
@@ -1,4 +1,9 @@
+// Runtime: 675 ms (Top 89.58%) | Memory: 23.80 MB (Top 95.52%)
+
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        return [original[i:i+n] for i in range(0, len(original), n)] if m*n == len(original) else []
-        
+        ans = []
+        if len(original) == m*n: 
+            for i in range(0, len(original), n): 
+                ans.append(original[i:i+n])
+        return ans 
