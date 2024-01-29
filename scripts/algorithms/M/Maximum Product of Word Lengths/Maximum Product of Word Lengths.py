5a1b32ff4143136911ae61992a18bccb9b018475
AnasImloul <pydev27@gmail.com>
2024-01-29 14:50:01 +0100

    Runtime: 332 ms (Top 87.93%) | Memory: 19.80 MB (Top 74.15%)

--- scripts/algorithms/M/Maximum Product of Word Lengths/Maximum Product of Word Lengths.py
+++ scripts/algorithms/M/Maximum Product of Word Lengths/Maximum Product of Word Lengths.py
@@ -1,15 +1,21 @@
+// Runtime: 332 ms (Top 87.93%) | Memory: 19.80 MB (Top 74.15%)
+
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        def check(w1, w2):
-            for i in w1:
-                if i in w2: 
-                    return False
-            return True
+        n=len(words)
         
-        n = len(words)   
-        Max = 0
+        bit_masks = [0] * n
+        lengths = [0] * n
+        
+        for i in range(n):             
+            for c in words[i]:
+                bit_masks[i]|=1<<(ord(c) - ord('a')) # set the character bit            
+            lengths[i]=len(words[i])
+                        
+        max_val = 0
         for i in range(n):
             for j in range(i+1, n):
-                if check(words[i], words[j]):
-                    Max = max(Max, len(words[i])*len(words[j]))      
-        return Max
+                if not (bit_masks[i] & bit_masks[j]):
+                    max_val=max(max_val, lengths[i] * lengths[j])
+        
+        return max_val  
