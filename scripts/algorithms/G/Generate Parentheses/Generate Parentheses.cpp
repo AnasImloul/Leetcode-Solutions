c9aa4235aead286d060f4778218f15c27d6f31bb
AnasImloul <pydev27@gmail.com>
2024-01-29 15:42:46 +0100

    Runtime: 0 ms (Top 100.0%) | Memory: 11.70 MB (Top 85.79%)

--- scripts/algorithms/G/Generate Parentheses/Generate Parentheses.cpp
+++ scripts/algorithms/G/Generate Parentheses/Generate Parentheses.cpp
@@ -1,29 +1,38 @@
+// Runtime: 0 ms (Top 100.0%) | Memory: 11.70 MB (Top 85.79%)
+
 class Solution {
+    private:
+    void solve(int cnt1,int cnt2,int n,vector<string> &ans,string &op,int ind)
+    {
+        if(ind>=n*2)
+        {
+            ans.push_back(op);
+            return;}
+        if(cnt1<n and ind<n*2-1)
+        {
+            op+='(';
+            solve(cnt1+1,cnt2,n,ans,op,ind+1);
+            op.pop_back();
+        }
+        
+        if(cnt2<cnt1 and ind>0)
+        {
+            op+=')';
+ 
+            solve(cnt1,cnt2+1,n,ans,op,ind+1);
+            op.pop_back();
+        }
+        
+    }
 public:
     vector<string> generateParenthesis(int n) {
+         string op="";
         vector<string> ans;
-        
-        generate(ans,"",n,n);
-        
+        solve(0,0,n,ans,op,0);
         return ans;
     }
-    
-    void generate(vector<string> &ans,string s,int open,int close)
-    {
-        if(open == 0 && close == 0)
-        {
-            ans.push_back(s);
-            return ;
-        }
-        
-        if(open > 0)
-        {
-            generate(ans,s+'(',open-1,close);
-        }
-        
-        if(open < close)
-        {
-            generate(ans,s+')',open,close-1);
-        }
-    }
 };
+
+
+
+
