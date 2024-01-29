d35a84f32ce1b3884feb1f9e68633f10b0b62a0d
AnasImloul <pydev27@gmail.com>
2024-01-29 14:48:41 +0100

    Runtime: 32 ms (Top 84.42%) | Memory: 16.40 MB (Top 68.29%)

--- scripts/algorithms/R/Reverse Prefix of Word/Reverse Prefix of Word.py
+++ scripts/algorithms/R/Reverse Prefix of Word/Reverse Prefix of Word.py
@@ -1,7 +1,27 @@
-# Runtime: 34 ms (Top 88.32%) | Memory: 13.8 MB (Top 97.42%)
+// Runtime: 32 ms (Top 84.42%) | Memory: 16.40 MB (Top 68.29%)
+
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        idx=word.find(ch)
-        if idx:
-            return word[:idx+1][::-1]+ word[idx+1:]
-        return word
+        """
+        #method 1:
+        for i in range(len(word)):
+            if word[i]==ch:
+                return word[:i+1][::-1]+word[i+1:]
+        return word"""
+        #method 2:
+        l=0
+        r=word.find(ch)
+        word=list(word)
+        while l<r:
+            word[l],word[r]=word[r],word[l]
+            l+=1
+            r-=1
+        return "".join(word)
+        
+
+                
+
+
+        
+
+            
