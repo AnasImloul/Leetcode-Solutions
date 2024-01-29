ce4d7309d8705baa58f679df584ad500f1c2edd2
AnasImloul <pydev27@gmail.com>
2024-01-29 14:47:40 +0100

    Runtime: 33 ms (Top 85.76%) | Memory: 16.50 MB (Top 60.81%)

--- scripts/algorithms/P/Plus One/Plus One.py
+++ scripts/algorithms/P/Plus One/Plus One.py
@@ -1,10 +1,14 @@
-# Runtime: 62 ms (Top 23.33%) | Memory: 13.9 MB (Top 12.32%)
-class Solution(object):
-    def plusOne(self, digits):
+// Runtime: 33 ms (Top 85.76%) | Memory: 16.50 MB (Top 60.81%)
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
         for i in range(len(digits)-1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
             else:
-                digits[i] += 1
+                digits[i] = digits[i] + 1
                 return digits
-        return [1] + digits
+        return [1] + digits 
+
+        
