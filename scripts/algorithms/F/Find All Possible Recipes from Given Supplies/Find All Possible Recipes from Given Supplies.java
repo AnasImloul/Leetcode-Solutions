31b1cf22ba75c06d3a8152ab49baaeec27709c32
AnasImloul <pydev27@gmail.com>
2024-01-30 13:57:52 +0100

    Runtime: 32 ms (Top 86.45%) | Memory: 46.50 MB (Top 57.39%)

--- scripts/algorithms/F/Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.java
+++ scripts/algorithms/F/Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.java
@@ -1,52 +1,62 @@
-// Runtime: 63 ms (Top 70.73%) | Memory: 73 MB (Top 78.21%)
+// Runtime: 32 ms (Top 86.45%) | Memory: 46.50 MB (Top 57.39%)
+
 class Solution {
-    private static final int NOT_VISITED = 0;
-    private static final int VISITING = 1;
-    private static final int VISITED = 2;
-
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Map<String, Integer> status = new HashMap<>();
-        Map<String, List<String>> prereqs = new HashMap<>();
-
-        for (int i = 0; i < recipes.length; ++ i) {
-            status.put(recipes[i], NOT_VISITED);
-            prereqs.put(recipes[i], ingredients.get(i));
+        HashSet<String> sup = new HashSet<>();
+        HashMap<String, Integer> index = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        
+        // create hashset of supplies
+        for(String s: supplies) {
+            sup.add(s);
         }
-
-        for (String s: supplies) {
-            status.put(s, VISITED);
+        
+        // store index of all recipes
+        for(int i = 0; i < recipes.length; i++) {
+            index.put(recipes[i], i);
         }
-
-        List<String> output = new ArrayList<>();
-        for (String s: recipes) {
-            dfs (s, prereqs, status, output);
-        }
-
-        return output;
-    }
-
-    public boolean dfs(String s, Map<String, List<String>> prereqs, Map<String, Integer> status, List<String> output) {
-        if (!status.containsKey(s)) {
-            return false;
-        }
-
-        if (status.get(s) == VISITING) {
-            return false;
-        }
-
-        if (status.get(s) == VISITED) {
-            return true;
-        }
-
-        status.put(s, VISITING);
-        for (String p: prereqs.get(s)) {
-            if (!dfs(p, prereqs, status, output)) {
-                return false;
+        
+        int[] indegree = new int[recipes.length];
+        // create a mapping of all the recipes that are Ingredients as well
+        // to the recipes they are ingredients for
+        for(int i = 0; i < recipes.length; i++) {
+            for(String need: ingredients.get(i)) {
+                if(sup.contains(need))
+                    continue;
+                
+                map.putIfAbsent(need, new ArrayList<String>());
+                map.get(need).add(recipes[i]);
+                indegree[i]++;
             }
         }
-        status.put(s, VISITED);
-        output.add(s);
-
-        return true;
+        
+        LinkedList<Integer> q = new LinkedList<>();
+        // add all the recipes with indegree 0 to the queue
+        for(int i = 0; i < recipes.length; i++) {
+            if(indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        
+        List<String> cooked = new ArrayList<>();
+        while(!q.isEmpty()) {
+            int i = q.poll();
+            cooked.add(recipes[i]);
+            
+            if(!map.containsKey(recipes[i])) {
+                // if the map does not contain this recipe, this means
+                // this recipe is not an ingredient for any other recipe
+                // and no further processing is required
+                continue;
+            }
+            
+            for(String recipe: map.get(recipes[i])) {
+                if(--indegree[index.get(recipe)] == 0) {
+                    q.add(index.get(recipe));
+                }
+            }
+        }
+        
+        return cooked;
     }
 }
