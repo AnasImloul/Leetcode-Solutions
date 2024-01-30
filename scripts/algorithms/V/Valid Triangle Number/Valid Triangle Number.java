b7c3d787df44dcd3691e171ba907477f9bd851bf
AnasImloul <pydev27@gmail.com>
2024-01-30 13:52:39 +0100

    Runtime: 23 ms (Top 95.13%) | Memory: 43.50 MB (Top 23.38%)

--- scripts/algorithms/V/Valid Triangle Number/Valid Triangle Number.java
+++ scripts/algorithms/V/Valid Triangle Number/Valid Triangle Number.java
@@ -1,24 +1,19 @@
+// Runtime: 23 ms (Top 95.13%) | Memory: 43.50 MB (Top 23.38%)
+
 class Solution {
-    public int triangleNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int count =0;
-        for(int k = n-1; k>=2; k--)
-        {
-            int i = 0;
-            int j = k-1;
-            while(i < j)
-            {
-                int sum = nums[i] +nums[j];
-                if(sum > nums[k])
-                {
-                    count += j-i;
-                    j--;
+    public int triangleNumber(int[] a) {
+        Arrays.sort(a);
+        int n=a.length;
+        int count=0;
+        for(int i=n-1;i>=1;i--){
+            int left=0,right=i-1;
+            while(left<right){
+                if(a[left]+a[right]>a[i]){
+                    count+=right-left;
+                    right--;
                 }
                 else
-                {
-                    i++;
-                }
+                left++;
             }
         }
         return count;
